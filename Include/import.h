ed97f6aa041b576a9b05aefa3e5ff9de6cdb8d98
Guido van Rossum <guido@python.org>
1990-10-26 14:59:11 +0000

    Added prototype for reload_module.

--- Include/import.h
+++ Include/import.h
@@ -1,7 +1,5 @@
 /* Module definition and import interface */
 
-void init_modules PROTO(());
-void close_modules PROTO(());
 object *new_module PROTO((char *name));
-void define_module PROTO((struct _context *ctx, char *name));
 object *import_module PROTO((struct _context *ctx, char *name));
+object *reload_module PROTO((struct _context *ctx, object *m));
