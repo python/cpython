17d82ce78a10d40c14f43b2eea4270690ecf4939
Guido van Rossum <guido@python.org>
1991-02-19 13:04:40 +0000

    Added readfile() and readopenfile() functions.

--- Lib/lib-old/util.py
+++ Lib/lib-old/util.py
@@ -1,9 +1,30 @@
-# Module 'util' -- some useful functions that dont fit elsewhere
+# Module 'util' -- some useful functions that don't fit elsewhere
 
-# Remove an item from a list at most once
+
+# Remove an item from a list.
+# No complaints if it isn't in the list at all.
+# If it occurs more than once, remove the first occurrence.
 #
 def remove(item, list):
 	for i in range(len(list)):
 		if list[i] = item:
 			del list[i]
 			break
+
+
+# Return a string containing a file's contents.
+#
+def readfile(fn):
+	return readopenfile(open(fn, 'r'))
+
+
+# Read an open file until EOF.
+#
+def readopenfile(fp):
+	BUFSIZE = 512*8
+	data = ''
+	while 1:
+		buf = fp.read(BUFSIZE)
+		if not buf: break
+		data = data + buf
+	return data
