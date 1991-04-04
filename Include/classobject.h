572fd57b5a016060ce864646a97d7ef01a34cf54
Guido van Rossum <guido@python.org>
1991-04-04 10:42:27 +0000

    Changed 'class member' to 'instance'.

--- Include/classobject.h
+++ Include/classobject.h
@@ -30,14 +30,14 @@ It should be possible to use other object types as base classes,
 but currently it isn't.  We'll see if we can fix that later, sigh...
 */
 
-extern typeobject Classtype, Classmembertype, Classmethodtype;
+extern typeobject Classtype, Instancetype, Classmethodtype;
 
 #define is_classobject(op) ((op)->ob_type == &Classtype)
-#define is_classmemberobject(op) ((op)->ob_type == &Classmembertype)
+#define is_instanceobject(op) ((op)->ob_type == &Classmembertype)
 #define is_classmethodobject(op) ((op)->ob_type == &Classmethodtype)
 
 extern object *newclassobject PROTO((object *, object *));
-extern object *newclassmemberobject PROTO((object *));
+extern object *newinstanceobject PROTO((object *));
 extern object *newclassmethodobject PROTO((object *, object *));
 
 extern object *classmethodgetfunc PROTO((object *));
