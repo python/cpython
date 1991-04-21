73de79fb3f2fab6ef4ddec5d611edbddd5de17e9
Guido van Rossum <guido@python.org>
1991-04-21 19:31:39 +0000

    Use built-in file.read() instead of util.readfile().

--- Lib/stdwin/filewin.py
+++ Lib/stdwin/filewin.py
@@ -2,17 +2,19 @@
 # File windows, a subclass of textwin (which is a subclass of gwin)
 
 import textwin
-from util import readfile
+import builtin
 
 
 # FILE WINDOW
 
 def open_readonly(fn): # Open a file window
-	w = textwin.open_readonly(fn, readfile(fn))
+	fp = builtin.open(fn, 'r')
+	w = textwin.open_readonly(fn, fp.read())
 	w.fn = fn
 	return w
 
 def open(fn): # Open a file window
-	w = textwin.open(fn, readfile(fn))
+	fp = builtin.open(fn, 'r')
+	w = textwin.open(fn, fp.read())
 	w.fn = fn
 	return w
