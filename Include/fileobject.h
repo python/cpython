5132c815aa58c87fcce6032e2a0e12c907a9d96a
Guido van Rossum <guido@python.org>
1991-06-07 13:57:38 +0000

    Add "close" function parameter to newopenfileobject().

--- Include/fileobject.h
+++ Include/fileobject.h
@@ -29,6 +29,7 @@ extern typeobject Filetype;
 #define is_fileobject(op) ((op)->ob_type == &Filetype)
 
 extern object *newfileobject PROTO((char *, char *));
-extern object *newopenfileobject PROTO((FILE *, char *, char *));
+extern object *newopenfileobject
+	PROTO((FILE *, char *, char *, int (*)FPROTO((FILE *))));
 extern FILE *getfilefile PROTO((object *));
 extern object *filegetline PROTO((object *, int));
