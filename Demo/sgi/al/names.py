c96f38eb1f9cca37eef819c29411e8ab79c610a3
Guido van Rossum <guido@python.org>
1991-09-15 21:26:30 +0000

    Initial revision

---
+++ Demo/sgi/al/names.py
@@ -0,0 +1,13 @@
+
+# My home directory/
+GUIDO = '/ufs/guido/'
+
+# Hack sys.path so AL can be found
+LIB = GUIDO + 'lib/python'
+if LIB not in sys.path: sys.path.insert(0, LIB)
+
+# Python binary to be used on remote machine
+PYTHON = GUIDO + 'bin/sgi/python'
+
+# Directory where the programs live
+AUDIODIR = GUIDO + 'src/python/demo/srg/al'
