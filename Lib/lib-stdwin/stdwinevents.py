c67eecf64d43caaa4957808d79d1f817a3d1b295
Guido van Rossum <guido@python.org>
1991-11-19 20:41:32 +0000

    Added improved comments.

--- Lib/lib-stdwin/stdwinevents.py
+++ Lib/lib-stdwin/stdwinevents.py
@@ -10,26 +10,26 @@
 # Values for type:
 
 WE_NULL       =  0	# not reported -- means 'no event' internally
-WE_ACTIVATE   =  1	# detail is <no object>
+WE_ACTIVATE   =  1	# detail is None
 WE_CHAR       =  2	# detail is the character
 WE_COMMAND    =  3	# detail is one of the WC_* constants below
 WE_MOUSE_DOWN =  4	# detail is ((h, v), clicks, button, mask)
 WE_MOUSE_MOVE =  5	# ditto
 WE_MOUSE_UP   =  6	# ditto
 WE_MENU       =  7	# detail is (menu, item)
-WE_SIZE       =  8	# detail is (width, height) [???]
+WE_SIZE       =  8	# detail is (width, height)
 WE_MOVE       =  9	# not reported -- reserved for future use
 WE_DRAW       = 10	# detail is ((left, top), (right, bottom))
-WE_TIMER      = 11	# detail is <no object>
-WE_DEACTIVATE = 12	# detail is <no object>
-WE_EXTERN     = 13	# detail is <no object>
+WE_TIMER      = 11	# detail is None
+WE_DEACTIVATE = 12	# detail is None
+WE_EXTERN     = 13	# detail is None
 WE_KEY        = 14	# detail is ???
 WE_LOST_SEL   = 15	# detail is selection number
-WE_CLOSE      = 16	# detail is <no object>
+WE_CLOSE      = 16	# detail is None
 
 # Values for detail when type is WE_COMMAND:
 
-WC_CLOSE      =  1	# user hit close box
+WC_CLOSE      =  1	# obsolete; now reported as WE_CLOSE
 WC_LEFT       =  2	# left arrow key
 WC_RIGHT      =  3	# right arrow key
 WC_UP         =  4	# up arrow key
