7a36c352e8a3f7bb5be6d3d9391769f2993640d2
Guido van Rossum <guido@python.org>
1991-12-24 13:29:10 +0000

    Don't rely on <string.h>

--- Python/strtod.c
+++ Python/strtod.c
@@ -1,17 +1,15 @@
 /* This is not a proper strtod() implementation, but sufficient for Python.
    Python won't detect floating point constant overflow, though. */
 
-#include <string.h>
-
+extern int strlen();
 extern double atof();
 
-/*ARGSUSED*/
 double
 strtod(p, pp)
 	char *p;
 	char **pp;
 {
 	if (pp)
-		*pp = strchr(p, '\0');
+		*pp = p + strlen(p);
 	return atof(p);
 }
