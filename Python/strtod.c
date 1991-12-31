5afc74757bae4a7274a5db0888985cd84ed92d32
Guido van Rossum <guido@python.org>
1991-12-31 13:15:19 +0000

    Clear errno, just to be sure.

--- Python/strtod.c
+++ Python/strtod.c
@@ -1,6 +1,8 @@
 /* This is not a proper strtod() implementation, but sufficient for Python.
    Python won't detect floating point constant overflow, though. */
 
+extern int errno;
+
 extern int strlen();
 extern double atof();
 
@@ -9,7 +11,12 @@ strtod(p, pp)
 	char *p;
 	char **pp;
 {
+	double res;
+
 	if (pp)
 		*pp = p + strlen(p);
-	return atof(p);
+	res = atof(p);
+	errno = 0;
+	return res;
+	
 }
