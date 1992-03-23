d510c786b8e99e03b3d9a8067c90a248a54b1256
Guido van Rossum <guido@python.org>
1992-03-23 18:20:04 +0000

    Added definitions of sys_trace and sys_profile

--- Include/sysmodule.h
+++ Include/sysmodule.h
@@ -28,3 +28,5 @@ object *sysget PROTO((char *));
 int sysset PROTO((char *, object *));
 FILE *sysgetfile PROTO((char *, FILE *));
 void initsys PROTO((void));
+
+extern object *sys_trace, *sys_profile;
