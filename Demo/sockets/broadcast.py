0aead9f1bac4f9fd962fedff2e377d69bb33fee6
Guido van Rossum <guido@python.org>
1992-09-08 21:20:12 +0000

    Allow broadcast using setsockopt()

--- Demo/sockets/broadcast.py
+++ Demo/sockets/broadcast.py
@@ -4,10 +4,11 @@ MYPORT = 50000
 
 import sys, time
 from socket import *
+from SOCKET import *
 
 s = socket(AF_INET, SOCK_DGRAM)
 s.bind('', 0)
-s.allowbroadcast(1)
+s.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
 
 while 1:
 	data = `time.time()` + '\n'
