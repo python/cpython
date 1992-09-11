d10d8291f1de5bef74628bd1c766403ee9444dca
Sjoerd Mullender <sjoerd@acm.org>
1992-09-11 15:19:27 +0000

    Added C++ support in thread.h; don't use signals if not strictly necessary, and when they are, use SIGKILL; when compiled with -DDEBUG, only print debug messages when "THREADDEBUG" is set in the environment.

--- Include/thread.h
+++ Include/thread.h
@@ -1,18 +1,23 @@
 #ifndef _THREAD_H_included
 #define _THREAD_H_included
 
-#ifdef __STDC__
+#if defined(__STDC__) || defined(__cplusplus)
 #define _P(args)	args
 #else
 #define _P(args)	()
+#endif
+
+typedef void *type_lock;
+typedef void *type_sema;
+
+#ifdef __cplusplus
+extern "C" {
 #endif
 
 void init_thread _P((void));
 int start_new_thread _P((void (*)(void *), void *));
 void exit_thread _P((void));
 void _exit_thread _P((void));
-
-typedef void *type_lock;
 
 type_lock allocate_lock _P((void));
 void free_lock _P((type_lock));
@@ -21,8 +26,6 @@ int acquire_lock _P((type_lock, int));
 #define NOWAIT_LOCK	0
 void release_lock _P((type_lock));
 
-typedef void *type_sema;
-
 type_sema allocate_sema _P((int));
 void free_sema _P((type_sema));
 void down_sema _P((type_sema));
@@ -30,6 +33,10 @@ void up_sema _P((type_sema));
 
 void exit_prog _P((int));
 void _exit_prog _P((int));
+
+#ifdef __cplusplus
+}
+#endif
 
 #undef _P
 
