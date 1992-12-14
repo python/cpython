57fc52578f8301aee4f1c5e920e21316e9af8964
Guido van Rossum <guido@python.org>
1992-12-14 15:06:05 +0000

    Get official parameter names

--- Demo/sgi/al/alwatch.py
+++ Demo/sgi/al/alwatch.py
@@ -4,13 +4,6 @@ import string
 
 dev = AL.DEFAULT_DEVICE
 
-parameter_name = [ \
-	'input source', 'left input atten', 'right input atten', \
-	'input rate', 'output rate', \
-	'left speaker gain', 'right speaker gain', \
-	'input count', 'output count', 'unused count', \
-	'sync input to aes', 'sync output to aes', \
-	]
 source_name = ['line', 'microphone', 'digital']
 
 params = al.queryparams(dev)
@@ -23,7 +16,7 @@ while 1:
 	if params <> old:
 		for i in range(0, len(params), 2):
 			if params[i+1] <> old[i+1]:
-				name = parameter_name[params[i]]
+				name = al.getname(dev, params[i])
 				if params[i] == AL.INPUT_SOURCE:
 					if 0 <= old[i+1] < len(source_name):
 						oldval = source_name[old[i+1]]
@@ -33,7 +26,7 @@ while 1:
 				else:
 					oldval = `old[i+1]`
 					newval = `params[i+1]`
-				print string.ljust(name, 20),
+				print string.ljust(name, 25),
 				print '(' + string.rjust(oldval, 10) + ')',
 				print '-->',
 				print string.rjust(newval, 10)
