bb5011bb995da5a0ae0d552dc3db633497d3f529
Guido van Rossum <guido@python.org>
1993-01-26 13:04:43 +0000

    * os.py: _exit doesn't exist in all variations of posix * Added fcmp() to test_support.py and use it in test*.py

--- Lib/test/test_support.py
+++ Lib/test/test_support.py
@@ -18,5 +18,24 @@ def forget(modname):
 		except os.error:
 			pass
 
+FUZZ = 1e-6
+
+def fcmp(x, y): # fuzzy comparison function
+	if type(x) == type(0.0) or type(y) == type(0.0):
+		try:
+			x, y = coerce(x, y)
+			fuzz = (abs(x) + abs(y)) * FUZZ
+			if abs(x-y) <= fuzz:
+				return 0
+		except:
+			pass
+	elif type(x) == type(y) and type(x) in (type(()), type([])):
+		for i in range(min(len(x), len(y))):
+			outcome = fcmp(x[i], y[i])
+			if outcome <> 0:
+				return outcome
+		return cmp(len(x), len(y))
+	return cmp(x, y)
+
 TESTFN = '@test' # Filename used for testing
 from os import unlink
