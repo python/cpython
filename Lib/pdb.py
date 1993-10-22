b7db71d83140bcb71be602fca8820cdd557efa70
Guido van Rossum <guido@python.org>
1993-10-22 13:56:35 +0000

    * profile.py, pdb.py: added help() function * builtin.py: b/w compat for builtin -> __builtin__ name change * string.py: added atof() and atol() and corresponding exceptions * test_types.py: added test for list sort with  user comparison function

--- Lib/pdb.py
+++ Lib/pdb.py
@@ -312,3 +312,15 @@ def test():
 	import linecache
 	linecache.checkcache()
 	run(TESTCMD)
+
+# print help
+def help():
+	for dirname in sys.path:
+		fullname = os.path.join(dirname, 'pdb.doc')
+		if os.path.exists(fullname):
+			sts = os.system('${PAGER-more} '+fullname)
+			if sts: print '*** Pager exit status:', sts
+			break
+	else:
+		print 'Sorry, can\'t find the help file "pdb.doc"',
+		print 'along the Python search path'
