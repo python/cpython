7a2dba2a00cee7e3dff61849e9111073684b6ab7
Guido van Rossum <guido@python.org>
1993-11-05 14:45:11 +0000

    * ext.tex: documentation for extending, reference counts, and embedding   (formerly ../misc/{EXTENDING,REFCNT,EMBEDDING}).  Also affects Makefile. * text2latex.py: script to do part of the conversion from an plain ASCI   text file (in my particular style) to LaTeX.   (Chapter/section/subsection headers, and verbatim sections.) * partparse.py, texipre.dat, fix.el, Makefile: Minor cleanup of latex ->   info conversion process (at least it works again, and with less   debugging output).  Removed fix.sh. * lib1.tex (section{Built-in Functions}): adapt description of str() and   repr() to new situation. * lib3.tex (Module os): added exec*() variants. * lib3.tex (Module posix): added execve(). * lib2.tex (Module array): documented reality; remove typecode and itemsize, add byteswap, rename read/write to fromfile/tofile, and re-alphabetized. * lib1.tex (Built-in Functions): renamed bagof() to filter().

--- Doc/tools/fix.el
+++ Doc/tools/fix.el
@@ -1,6 +1,6 @@
 ; load the new texinfo package (2.xx) if not installed by default
-(setq load-path
-      (cons "/ufs/jh/lib/emacs/texinfo-2.14" load-path))
-(find-file "@out.texi")
+; (setq load-path
+;      (cons "/ufs/jh/lib/emacs/texinfo-2.14" load-path))
+(find-file "lib.texi")
 (texinfo-all-menus-update t)
 (texinfo-all-menus-update t)
