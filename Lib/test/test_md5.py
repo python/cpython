a4c0e87465c55e19857db942dbe561f8c2ab9c03
Guido van Rossum <guido@python.org>
1993-12-14 15:54:01 +0000

    * wdbframewin.py (re_eval): set __privileged__ in globals so private   variables can still be seen by the debugger * ftplib.py (retrlines): args should be *args. * ChangeLog: entries for Sjoerd's addition sunau.py and changes to aiff.py * test_md5.py: test program for built-in md5 module

---
+++ Lib/test/test_md5.py
@@ -0,0 +1,24 @@
+# Testing md5 module
+
+import string
+from md5 import md5
+
+def hexstr(s):
+	h = string.hexdigits
+	r = ''
+	for c in s:
+		i = ord(c)
+		r = r + h[(i >> 4) & 0xF] + h[i & 0xF]
+	return r
+
+def md5test(s):
+	return 'MD5 ("' + s + '") = ' + hexstr(md5(s).digest())
+
+print 'MD5 test suite:'
+print md5test('')
+print md5test('a')
+print md5test('abc')
+print md5test('message digest')
+print md5test('abcdefghijklmnopqrstuvwxyz')
+print md5test('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789')
+print md5test('12345678901234567890123456789012345678901234567890123456789012345678901234567890')
