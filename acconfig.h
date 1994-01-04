18eb2bb6f7a648bb0dec9f0b5691b725d7c528bf
Guido van Rossum <guido@python.org>
1994-01-04 22:02:27 +0000

    redid build process; added --with-svr4; renamed USE_THREAD to WITH_THREAD; bumped version to 1.0.0 BETA 3

---
+++ acconfig.h
@@ -0,0 +1,29 @@
+/* Leave this blank line here -- autoheader needs it! */
+
+
+/* Define if your <unistd.h> contains bad prototypes for exec*()
+   (as it does on SGI IRIX 4.x) */
+#undef BAD_EXEC_PROTOTYPES
+
+/* Define if getpgrp() must be called as getpgrp(0)
+   and (consequently) setpgrp() as setpgrp(0, 0). */
+#undef GETPGRP_HAVE_ARGS
+
+/* Define if your compiler supports function prototypes */
+#undef HAVE_PROTOTYPES
+
+/* Define for SOLARIS 2.x */
+#undef SOLARIS
+
+/* Define if it's a bad idea to include <sys/select.h> and <sys/time.h>
+   in the same file (as it is on SCO ODT 3.0) */
+#undef SYS_SELECT_WITH_SYS_TIME
+
+/* Define if you want to compile in rudimentary thread support */
+#undef WITH_THREAD
+
+/* Define if you want to use the GNU readline library */
+#undef WITH_READLINE
+
+
+/* Leave that blank line there-- autoheader needs it! */
