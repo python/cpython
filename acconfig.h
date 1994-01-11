2712c162f258d919152b9a1aa0afaac17af9a0b1
Guido van Rossum <guido@python.org>
1994-01-11 12:00:38 +0000

    Lots of minor stuff, install prefix, LIBM/LIBC, bad static forward, irix4 and GNU dynamic loading, version 1.0.0 BETA 6

--- acconfig.h
+++ acconfig.h
@@ -5,6 +5,10 @@
    (as it does on SGI IRIX 4.x) */
 #undef BAD_EXEC_PROTOTYPES
 
+/* Define if your compiler botches static forward declarations
+   (as it does on SCI ODT 3.0) */
+#undef BAD_STATIC_FORWARD
+
 /* Define if getpgrp() must be called as getpgrp(0)
    and (consequently) setpgrp() as setpgrp(0, 0). */
 #undef GETPGRP_HAVE_ARGS
@@ -18,6 +22,24 @@
 /* Define if  you can safely include both <sys/select.h> and <sys/time.h>
    (which you can't on SCO ODT 3.0). */
 #undef SYS_SELECT_WITH_SYS_TIME
+
+/* Define if you want to use SGI (IRIX 4) dynamic linking.
+   This requires the "dl" library by Jack Jansen,
+   ftp://ftp.cwi.nl/pub/dynload/dl-1.6.tar.Z.
+   Don't bother on IRIX 5, it already has dynamic linking using SunOS
+   style shared libraries */ 
+#undef WITH_SGI_DL
+
+/* Define if you want to emulate SGI (IRIX 4) dynamic linking.
+   This is rumoured to work on VAX (Ultrix), Sun3 (SunOS 3.4),
+   Sequent Symmetry (Dynix), and Atari ST.
+   This requires the "dl-dld" library,
+   ftp://ftp.cwi.nl/pub/dynload/dl-dld-1.1.tar.Z,
+   as well as the "GNU dld" library,
+   ftp://ftp.cwi.nl/pub/dynload/dld-3.2.3.tar.Z.
+   Don't bother on SunOS 4 or 5, they already have dynamic linking using
+   shared libraries */ 
+#undef WITH_DL_DLD
 
 /* Define if you want to compile in rudimentary thread support */
 #undef WITH_THREAD
