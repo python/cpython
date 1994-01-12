b039f6edf1f48e95b0b65fb9a8cddc4298a30624
Guido van Rossum <guido@python.org>
1994-01-12 09:55:11 +0000

    Add test code for audioop and rgbimg (includes three binary files!)

---
+++ Lib/test/test_rgbimg.py
@@ -0,0 +1,39 @@
+# Testing rgbimg module
+
+import rgbimg, os
+
+error = 'test_rgbimg.error'
+
+print 'RGBimg test suite:'
+
+def testimg(rgb_file, raw_file):
+	width, height = rgbimg.sizeofimage(rgb_file)
+	rgb = rgbimg.longimagedata(rgb_file)
+	if len(rgb) != width * height * 4:
+		raise error, 'bad image length'
+	raw = open(raw_file, 'r').read()
+	if rgb != raw:
+		raise error, 'images don\'t match for '+rgb_file+' and '+raw_file
+	for depth in [1, 3, 4]:
+		rgbimg.longstoimage(rgb, width, height, depth, '@.rgb')
+	os.unlink('@.rgb')
+
+ttob = rgbimg.ttob(0)
+if ttob != 0:
+	raise error, 'ttob should start out as zero'
+
+testimg('test.rgb', 'test.rawimg')
+
+ttob = rgbimg.ttob(1)
+if ttob != 0:
+	raise error, 'ttob should be zero'
+
+testimg('test.rgb', 'test.rawimg.rev')
+
+ttob = rgbimg.ttob(0)
+if ttob != 1:
+	raise error, 'ttob should be one'
+
+ttob = rgbimg.ttob(0)
+if ttob != 0:
+	raise error, 'ttob should be zero'
