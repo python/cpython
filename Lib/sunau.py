b513c74b1b6b8e5500d7ec9c8ca4da41e77591bd
Sjoerd Mullender <sjoerd@acm.org>
1994-02-03 14:19:21 +0000

    wave.py: module to read and write .wav files with the same interface 	as aifc.py and sunau.py. sunau.py: small change in comment (au -> sunau).

--- Lib/sunau.py
+++ Lib/sunau.py
@@ -36,7 +36,7 @@
 # Usage.
 #
 # Reading audio files:
-#	f = au.open(file, 'r')
+#	f = sunau.open(file, 'r')
 # where file is either the name of a file or an open file pointer.
 # The open file pointer must have methods read(), seek(), and close().
 # When the setpos() and rewind() methods are not used, the seek()
@@ -69,7 +69,7 @@
 # is destroyed.
 #
 # Writing audio files:
-#	f = au.open(file, 'w')
+#	f = sunau.open(file, 'w')
 # where file is either the name of a file or an open file pointer.
 # The open file pointer must have methods write(), tell(), seek(), and
 # close().
