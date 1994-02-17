e69d3fec87c4624e1d88d66efb7232ba5818a516
Guido van Rossum <guido@python.org>
1994-02-17 12:36:58 +0000

    Use time.time() instead of time.millitimer()

--- Demo/rpc/T.py
+++ Demo/rpc/T.py
@@ -6,17 +6,17 @@ import sys, os, time
 def TSTART():
 	global t0, t1
 	u, s, cu, cs = os.times()
-	t0 = u+cu, s+cs, time.millitimer()
+	t0 = u+cu, s+cs, time.time()
 
 def TSTOP(*label):
 	global t0, t1
 	u, s, cu, cs = os.times()
-	t1 = u+cu, s+cs, time.millitimer()
+	t1 = u+cu, s+cs, time.time()
 	tt = []
 	for i in range(3):
 		tt.append(t1[i] - t0[i])
 	[u, s, r] = tt
 	msg = ''
 	for x in label: msg = msg + (x + ' ')
-	msg = msg + `u` + ' user, ' + `s` + ' sys, ' + `r*0.001` + ' real\n'
+	msg = msg + `u` + ' user, ' + `s` + ' sys, ' + `r` + ' real\n'
 	sys.stderr.write(msg)
