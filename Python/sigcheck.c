abe173a2255aa56413fd2b0e62b8ca8b314464d0
Guido van Rossum <guido@python.org>
1994-06-03 15:58:29 +0000

    Replace intrcheck() by sigcheck() in many places. In Pythonrun, tirn off SIGPIPE and move signal handlers elsewhere. In ceval.c, also move locals_2_fast/ fast_2_locals to frameobject.c, replaced local variable trace by f->f_trace, got rid of unused mergelocals() and unneeded calls to fast_2_locals, return the variable name on "fast variable not found", AND use "__call__" method if it exists to call an inmstance.  Whew!

---
+++ Python/sigcheck.c
@@ -0,0 +1,43 @@
+/***********************************************************
+Copyright 1991, 1992, 1993, 1994 by Stichting Mathematisch Centrum,
+Amsterdam, The Netherlands.
+
+                        All Rights Reserved
+
+Permission to use, copy, modify, and distribute this software and its 
+documentation for any purpose and without fee is hereby granted, 
+provided that the above copyright notice appear in all copies and that
+both that copyright notice and this permission notice appear in 
+supporting documentation, and that the names of Stichting Mathematisch
+Centrum or CWI not be used in advertising or publicity pertaining to
+distribution of the software without specific, written prior permission.
+
+STICHTING MATHEMATISCH CENTRUM DISCLAIMS ALL WARRANTIES WITH REGARD TO
+THIS SOFTWARE, INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND
+FITNESS, IN NO EVENT SHALL STICHTING MATHEMATISCH CENTRUM BE LIABLE
+FOR ANY SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT
+OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+******************************************************************/
+
+/* Sigcheck is similar to intrcheck() but sets an exception when an
+   interrupt occurs.  It can't be in the intrcheck.c file since that
+   file (and the whole directory it is in) doesn't know about objects
+   or exceptions.  It can't be in errors.c because it can be
+   overridden (at link time) by a more powerful version implemented in
+   signalmodule.c. */
+
+#include "allobjects.h"
+#include "intrcheck.h"
+
+/* ARGSUSED */
+int
+sigcheck()
+{
+	if (!intrcheck())
+		return 0;
+	err_set(KeyboardInterrupt);
+	return -1;
+}
