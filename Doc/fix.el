6938f06047a6d2170523cfc3ab8e797bae0a6c05
Guido van Rossum <guido@python.org>
1994-08-01 12:22:53 +0000

    Merge alpha100 branch back to main trunk

--- Doc/fix.el
+++ Doc/fix.el
@@ -1,6 +1,5 @@
 ; load the new texinfo package (2.xx) if not installed by default
-; (setq load-path
-;      (cons "/ufs/jh/lib/emacs/texinfo-2.14" load-path))
-(find-file "lib.texi")
+; (setq load-path (cons "/ufs/guido/lib/emacs/texinfo-2.14" load-path))
+(find-file "@lib.texi")
 (texinfo-all-menus-update t)
 (texinfo-all-menus-update t)
