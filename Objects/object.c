9776adf56561fa492f6261e1710ac2ac0e7fdc08
Guido van Rossum <guido@python.org>
1994-09-07 14:36:45 +0000

    rearranged code in debugging version of DELREF to avoid touching data after it has been freed.

--- Objects/object.c
+++ Objects/object.c
@@ -406,12 +406,13 @@ UNREF(op)
 DELREF(op)
 	object *op;
 {
+	destructor dealloc = op->ob_type->tp_dealloc;
 	UNREF(op);
 #ifdef COUNT_ALLOCS
 	op->ob_type->tp_free++;
 #endif
-	(*(op)->ob_type->tp_dealloc)(op);
 	op->ob_type = NULL;
+	(*dealloc)(op);
 }
 
 printrefs(fp)
