27f88474dadec74c2aab3d2ff40113e2857583f2
Guido van Rossum <guido@python.org>
1994-10-07 10:33:04 +0000

    print gl variables

---
+++ Demo/sgi/gl/glinfo.py
@@ -0,0 +1,20 @@
+#! /usr/local/bin/python
+
+# Print the values of all values that can be inquired with getgdesc().
+# See man getgdesc() for a description.
+
+import gl
+import GL
+
+def main():
+	names = []
+	maxlen = 0
+	for name in dir(GL):
+		if name[:3] == 'GD_':
+			names.append(name)
+			maxlen = max(maxlen, len(name))
+	for name in names:
+		print name + (maxlen - len(name))*' ' + '=',
+		print gl.getgdesc(getattr(GL, name))
+
+main()
