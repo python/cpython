fefde02e7060822b31848f1d0be20bf7ed807c04
Jack Jansen <jack.jansen@cwi.nl>
1995-01-18 13:53:49 +0000

    - Moved some useful routines from macosmodule.c here - Added GetFSSpec (tuple-style), BuildFSSpec and Pstring   (platform-independent correct conversion of C to Pascal strings)

--- Mac/Include/macglue.h
+++ Mac/Include/macglue.h
@@ -1,3 +1,12 @@
-char *macstrerror Py_PROTO((int));			/* strerror with mac errors */
-PyObject *PyErr_Mac Py_PROTO((PyObject *, int));	/* Exception with a mac error */
-int PyMac_Idle Py_PROTO((void));			/* Idle routine */
+#include <Files.h>
+#include <Types.h>
+#include <Resources.h>
+
+char *macstrerror PROTO((int));			/* strerror with mac errors */
+object *PyErr_Mac PROTO((object *, int));	/* Exception with a mac error */
+int PyMac_Idle PROTO((void));			/* Idle routine */
+int GetOSType PROTO((object *, ResType *));	/* argument parser for OSType */
+int GetStr255 PROTO((object *, Str255));	/* argument parser for Str255 */
+int GetFSSpec PROTO((object *, FSSpec *));	/* argument parser for FSSpec */
+object *PyMac_BuildFSSpec PROTO((FSSpec *));	/* Convert FSSpec to python object */
+
