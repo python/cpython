39bb411f529b532252f9b54d745de844817c4282
Jack Jansen <jack.jansen@cwi.nl>
1995-02-13 11:31:51 +0000

    Added getfiletype routine

--- Mac/Python/macsetfiletype.c
+++ Mac/Python/macsetfiletype.c
@@ -22,3 +22,16 @@ long creator, type;
 	return SetFInfo(pname, 0, &info);
 }
 
+long
+getfiletype(name)
+char *name;
+{
+	FInfo info;
+	unsigned char *pname;
+	
+	pname = (StringPtr) Pstring(name);
+	if ( GetFInfo(pname, 0, &info) < 0 )
+		return -1;
+	return info.fdType;
+}
+
