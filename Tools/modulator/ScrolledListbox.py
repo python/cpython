dea29a457dd7f44c490ef2a21c0999c379cb60dd
Guido van Rossum <guido@python.org>
1995-03-02 14:05:29 +0000

    checkin of Jack's original version

---
+++ Tools/modulator/ScrolledListbox.py
@@ -0,0 +1,37 @@
+# A ScrolledList widget feels like a list widget but also has a
+# vertical scroll bar on its right.  (Later, options may be added to
+# add a horizontal bar as well, to make the bars disappear
+# automatically when not needed, to move them to the other side of the
+# window, etc.)
+#
+# Configuration options are passed to the List widget.
+# A Frame widget is inserted between the master and the list, to hold
+# the Scrollbar widget.
+# Most methods calls are inherited from the List widget; Pack methods
+# are redirected to the Frame widget however.
+
+from Tkinter import *
+from Tkinter import _cnfmerge
+
+class ScrolledListbox(Listbox):
+	def __init__(self, master=None, cnf={}):
+		cnf = _cnfmerge(cnf)
+		fcnf = {}
+		vcnf = {'name': 'vbar',
+			Pack: {'side': 'right', 'fill': 'y'},}
+		for k in cnf.keys():
+			if type(k) == ClassType or k == 'name':
+				fcnf[k] = cnf[k]
+				del cnf[k]
+		self.frame = Frame(master, fcnf)
+		self.vbar = Scrollbar(self.frame, vcnf)
+		cnf[Pack] = {'side': 'left', 'fill': 'both', 'expand': 'yes'}
+		cnf['name'] = 'list'
+		Listbox.__init__(self, self.frame, cnf)
+		self['yscrollcommand'] = (self.vbar, 'set')
+		self.vbar['command'] = (self, 'yview')
+
+		# Copy Pack methods of self.frame -- hack!
+		for m in Pack.__dict__.keys():
+			if m[0] != '_' and m != 'config':
+				setattr(self, m, getattr(self.frame, m))
---
+++ Tools/modulator/varsubst.py
@@ -0,0 +1,61 @@
+#
+# Variable substitution. Variables are $delimited$
+#
+import string
+import regex
+import regsub
+
+error = 'varsubst.error'
+
+class Varsubst:
+    def __init__(self, dict):
+	self.dict = dict
+	self.prog = regex.compile('\$[a-zA-Z0-9_]*\$')
+	self.do_useindent = 0
+
+    def useindent(self, onoff):
+	self.do_useindent = onoff
+	
+    def subst(self, str):
+	rv = ''
+	while 1:
+	    pos = self.prog.search(str)
+	    if pos < 0:
+		return rv + str
+	    if pos:
+		rv = rv + str[:pos]
+		str = str[pos:]
+	    len = self.prog.match(str)
+	    if len == 2:
+		# Escaped dollar
+		rv = rv + '$'
+		str = str[2:]
+		continue
+	    name = str[1:len-1]
+	    str = str[len:]
+	    if not self.dict.has_key(name):
+		raise error, 'No such variable: '+name
+	    value = self.dict[name]
+	    if self.do_useindent and '\n' in value:
+		value = self._modindent(value, rv)
+	    rv = rv + value
+
+    def _modindent(self, value, old):
+	lastnl = string.rfind(old, '\n', 0) + 1
+	lastnl = len(old) - lastnl
+	sub = '\n' + (' '*lastnl)
+	return regsub.gsub('\n', sub, value)
+
+def _test():
+    import sys
+    import os
+
+    sys.stderr.write('-- Copying stdin to stdout with environment map --\n')
+    c = Varsubst(os.environ)
+    c.useindent(1)
+    d = sys.stdin.read()
+    sys.stdout.write(c.subst(d))
+    sys.exit(1)
+
+if __name__ == '__main__':
+    _test()
