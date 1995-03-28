3caad8c291691dbec58a75db2341d80522793f88
Guido van Rossum <guido@python.org>
1995-03-28 09:22:53 +0000

    adapted to new naming; clarify comments somewhat

--- Demo/embed/demo.c
+++ Demo/embed/demo.c
@@ -1,6 +1,6 @@
 /* Example of embedding Python in another program */
 
-#include "allobjects.h"
+#include "Python.h"
 
 static char *argv0;
 
@@ -12,21 +12,21 @@ main(argc, argv)
 	argv0 = argv[0];
 
 	/* Initialize the Python interpreter.  Required. */
-	initall();
+	Py_Initialize();
 
 	/* Define sys.argv.  It is up to the application if you
 	   want this; you can also let it undefined (since the Python 
 	   code is generally not a main program it has no business
 	   touching sys.argv...) */
-	setpythonargv(argc, argv);
+	PySys_SetArgv(argc, argv);
 
 	/* Do some application specific code */
 	printf("Hello, brave new world\n\n");
 
 	/* Execute some Python statements (in module __main__) */
-	run_command("import sys\n");
-	run_command("print sys.builtin_module_names\n");
-	run_command("print sys.argv\n");
+	PyRun_SimpleString("import sys\n");
+	PyRun_SimpleString("print sys.builtin_module_names\n");
+	PyRun_SimpleString("print sys.argv\n");
 
 	/* Note that you can call any public function of the Python
 	   interpreter here, e.g. call_object(). */
@@ -35,10 +35,11 @@ main(argc, argv)
 	printf("\nGoodbye, cruel world\n");
 
 	/* Exit, cleaning up the interpreter */
-	goaway(0);
+	Py_Exit(0);
 	/*NOTREACHED*/
 }
 
+/* This function is called by the interpreter to get its own name */
 char *
 getprogramname()
 {
