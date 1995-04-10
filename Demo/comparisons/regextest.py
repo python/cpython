21bc15b73da1e3e5ab60ff9b93a95bfd390fea3f
Guido van Rossum <guido@python.org>
1995-04-10 11:40:26 +0000

    commit -- why not...

---
+++ Demo/comparisons/regextest.py
@@ -0,0 +1,50 @@
+#! /usr/local/bin/python
+
+# 1)  Regular Expressions Test
+# 
+#     Read a file of (extended per egrep) regular expressions (one per line), 
+#     and apply those to all files whose names are listed on the command line.
+#     Basically, an 'egrep -f' simulator.  Test it with 20 "vt100" patterns
+#     against a five /etc/termcap files.  Tests using more elaborate patters
+#     would also be interesting.  Your code should not break if given hundreds
+#     of regular expressions or binary files to scan.  
+
+# This implementation:
+# - combines all patterns into a single one using ( ... | ... | ... )
+# - reads patterns from stdin, scans files given as command line arguments
+# - produces output in the format <file>:<lineno>:<line>
+# - is only about 2.5 times as slow as egrep (though I couldn't run
+#   Tom's test -- this system, a vanilla SGI, only has /etc/terminfo)
+
+import string
+import sys
+import regex
+from regex_syntax import *
+
+regex.set_syntax(RE_SYNTAX_EGREP)
+
+def main():
+	pats = map(chomp, sys.stdin.readlines())
+	bigpat = '(' + string.joinfields(pats, '|') + ')'
+	prog = regex.compile(bigpat)
+	
+	for file in sys.argv[1:]:
+		try:
+			fp = open(file, 'r')
+		except IOError, msg:
+			print "%s: %s" % (file, msg)
+			continue
+		lineno = 0
+		while 1:
+			line = fp.readline()
+			if not line:
+				break
+			lineno = lineno + 1
+			if prog.search(line) >= 0:
+				print "%s:%s:%s" % (file, lineno, line),
+
+def chomp(s):
+	if s[-1:] == '\n': return s[:-1]
+	else: return s
+
+main()
---
+++ Demo/comparisons/sortingtest.py
@@ -0,0 +1,50 @@
+#! /usr/local/bin/python
+
+# 2)  Sorting Test
+# 
+#     Sort an input file that consists of lines like this
+# 
+#         var1=23 other=14 ditto=23 fred=2
+# 
+#     such that each output line is sorted WRT to the number.  Order
+#     of output lines does not change.  Resolve collisions using the
+#     variable name.   e.g.
+# 
+#         fred=2 other=14 ditto=23 var1=23 
+# 
+#     Lines may be up to several kilobytes in length and contain
+#     zillions of variables.
+
+# This implementation:
+# - Reads stdin, writes stdout
+# - Uses any amount of whitespace to separate fields
+# - Allows signed numbers
+# - Treats illegally formatted fields as field=0
+# - Outputs the sorted fields with exactly one space between them
+# - Handles blank input lines correctly
+
+import regex
+import string
+import sys
+
+def main():
+	prog = regex.compile('^\(.*\)=\([-+]?[0-9]+\)')
+	def makekey(item, prog=prog):
+		if prog.match(item) >= 0:
+			var, num = prog.group(1, 2)
+			return string.atoi(num), var
+		else:
+			# Bad input -- pretend it's a var with value 0
+			return 0, item
+	while 1:
+		line = sys.stdin.readline()
+		if not line:
+			break
+		items = string.split(line)
+		items = map(makekey, items)
+		items.sort()
+		for num, var in items:
+			print "%s=%s" % (var, num),
+		print
+
+main()
