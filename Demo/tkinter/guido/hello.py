5b98ac5b14b7f01d24aecd3d371ed899ed3f671c
Guido van Rossum <guido@python.org>
1995-04-10 11:46:37 +0000

    added hello world

---
+++ Demo/tkinter/guido/hello.py
@@ -0,0 +1,17 @@
+# Display hello, world in a button; clicking it quits the program
+
+import sys
+from Tkinter import *
+
+def main():
+	root = Tk()
+	button = Button(root)
+	button['text'] = 'Hello, world'
+	button['command'] = quit_callback	# See below
+	button.pack()
+	root.mainloop()
+
+def quit_callback():
+	sys.exit(0)
+
+main()
