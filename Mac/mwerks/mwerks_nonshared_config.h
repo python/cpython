d7e75f98d0ef1d8f9b4f8555880563a592436cb3
Jack Jansen <jack.jansen@cwi.nl>
1995-06-14 14:43:41 +0000

    Split shared-library support and dynamic-loading support (they were both enabled with USE_SHARED_LIBRARY, now there's also USE_MAC_DYNAMIC_LOADING).
    Added a few other USE_ defines for optional modules to nonshared
    config (they're not included in the base shared library)

--- Mac/mwerks/mwerks_nonshared_config.h
+++ Mac/mwerks/mwerks_nonshared_config.h
@@ -1,2 +1,8 @@
 #define HAVE_CONFIG_H
 #define USE_STDWIN
+#define USE_MACTCP
+#define USE_BGEN
+#define USE_MACSPEECH
+#define USE_IMG
+#define USE_MACCTB
+#define USE_MAC_DYNAMIC_LOADING
--- Mac/mwerks/mwerks_shared_config.h
+++ Mac/mwerks/mwerks_shared_config.h
@@ -1,4 +1,7 @@
 #define HAVE_CONFIG_H
-#define USE_STDWIN
 #define USE_MAC_SHARED_LIBRARY
+#define USE_MAC_DYNAMIC_LOADING
 #define USE_MAC_APPLET_SUPPORT
+/* #define USE_MACTCP */
+/* #define USE_BGEN */
+/* #define USE_MACSPEECH */
