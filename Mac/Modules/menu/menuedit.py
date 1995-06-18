ace61c92e5a32331c91885fab7d0a5992bfc7343
Jack Jansen <jack.jansen@cwi.nl>
1995-06-18 20:17:27 +0000

    Added as_Resource() method to convert menu object to res object

--- Mac/Modules/menu/menuedit.py
+++ Mac/Modules/menu/menuedit.py
@@ -2,3 +2,12 @@ f = Function(void, 'OpenDeskAcc',
 	(Str255, 'name', InMode),
 )
 functions.append(f)
+
+as_resource_body = """
+return ResObj_New((Handle)_self->ob_itself);
+"""
+
+f = ManualGenerator("as_Resource", as_resource_body)
+f.docstring = lambda : "Return this Menu as a Resource"
+
+methods.append(f)
