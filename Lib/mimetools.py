a5cf179f028f0a6f69aca873f18551a60d7dbc8c
Guido van Rossum <guido@python.org>
1995-08-29 19:19:51 +0000

    encoding can be content-transfer-encoding or content-encoding

--- Lib/mimetools.py
+++ Lib/mimetools.py
@@ -15,7 +15,8 @@ class Message(rfc822.Message):
 	def __init__(self, fp, seekable = 1):
 		rfc822.Message.__init__(self, fp, seekable)
 		self.encodingheader = \
-			self.getheader('content-transfer-encoding')
+			self.getheader('content-transfer-encoding') or \
+			self.getheader('content-encoding')
 		self.typeheader = \
 			self.getheader('content-type')
 		self.parsetype()
