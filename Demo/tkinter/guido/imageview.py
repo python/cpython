3ab0e609bdb739568db4295db39473b9eea5937b
Guido van Rossum <guido@python.org>
1995-09-07 19:45:52 +0000

    simplified no Tk() sets _default_root

--- Demo/tkinter/guido/imageview.py
+++ Demo/tkinter/guido/imageview.py
@@ -4,9 +4,8 @@ import sys
 def main():
     filename = sys.argv[1]
     root = Tk()
-    label = Label(root)
     img = PhotoImage(file=filename)
-    label['image'] = img
+    label = Label(root, image=img)
     label.pack()
     root.mainloop()
 
