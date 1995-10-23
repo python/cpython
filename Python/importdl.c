0a72e8d4d2a3dd1abd2d8cdac4cf1a10fd72f95e
Jack Jansen <jack.jansen@cwi.nl>
1995-10-23 13:54:01 +0000

    Added missing include for <Aliases.h>

--- Python/importdl.c
+++ Python/importdl.c
@@ -150,6 +150,7 @@ typedef void (*dl_funcptr)();
 #endif
 
 #ifdef USE_MAC_DYNAMIC_LOADING
+#include <Aliases.h>
 #include <CodeFragments.h>
 #ifdef SYMANTEC__CFM68K__ /* Really just an older version of Universal Headers */
 #define CFragConnectionID ConnectionID
@@ -268,10 +269,7 @@ load_dynamic_module(name, pathname, fp)
 		
 		/* First resolve any aliases to find the real file */
 		(void)FSMakeFSSpec(0, 0, Pstring(pathname), &libspec);
-#if !(defined(__MWERKS__) && defined(__CFM68K__))
-		/* Bug: not in library */
 		err = ResolveAliasFile(&libspec, 1, &isfolder, &didsomething);
-#endif
 		if ( err ) {
 			sprintf(buf, "%s: %s", pathname, PyMac_StrError(err));
 			err_setstr(ImportError, buf);
