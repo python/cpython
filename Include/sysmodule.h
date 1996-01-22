b89f32e86938b9741d63e8c1d97e8426262f7c39
Jack Jansen <jack.jansen@cwi.nl>
1996-01-22 14:56:29 +0000

    Added prototype for PySys_SetArgv

--- Include/sysmodule.h
+++ Include/sysmodule.h
@@ -34,6 +34,7 @@ PyObject *PySys_GetObject Py_PROTO((char *));
 int PySys_SetObject Py_PROTO((char *, PyObject *));
 FILE *PySys_GetFile Py_PROTO((char *, FILE *));
 void PySys_Init Py_PROTO((void));
+void PySys_SetArgv Py_PROTO((int, char **));
 
 extern DL_IMPORT(PyObject *) _PySys_TraceFunc, *_PySys_ProfileFunc;
 extern DL_IMPORT(int) _PySys_CheckInterval;
