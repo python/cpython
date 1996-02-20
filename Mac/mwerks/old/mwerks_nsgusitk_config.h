5bfdc9281a9e22cbf66b70225b72e457d528c536
Jack Jansen <jack.jansen@cwi.nl>
1996-02-20 16:28:18 +0000

    Added config file for Tk-python Removed mwerksglue.c (which was useless anyway)

---
+++ Mac/mwerks/old/mwerks_nsgusitk_config.h
@@ -0,0 +1,13 @@
+#define HAVE_CONFIG_H
+// #define USE_STDWIN
+// #define USE_MACTCP
+#define USE_BGEN
+//#define USE_MACSPEECH
+// #define USE_IMG
+//#define USE_MACCTB
+#define USE_TK
+#define MAC_TCL
+#ifdef __powerc
+#define USE_MAC_DYNAMIC_LOADING
+#endif
+#define USE_GUSI
