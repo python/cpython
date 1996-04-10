8a452d6fa1905d4767db7a13b3c4be3a9d5673ab
Jack Jansen <jack.jansen@cwi.nl>
1996-04-10 14:41:08 +0000

    Interface to TextEdit toolbox

---
+++ Mac/Modules/te/tescan.py
@@ -0,0 +1,60 @@
+# Scan an Apple header file, generating a Python file of generator calls.
+
+import addpack
+addpack.addpack(':tools:bgen:bgen')
+from scantools import Scanner
+
+LONG = "TextEdit"
+SHORT = "te"
+OBJECT = "TEHandle"
+
+def main():
+	input = LONG + ".h"
+	output = SHORT + "gen.py"
+	defsoutput = LONG + ".py"
+	scanner = MyScanner(input, output, defsoutput)
+	scanner.scan()
+	scanner.close()
+	print "=== Done scanning and generating, now importing the generated code... ==="
+	exec "import " + SHORT + "support"
+	print "=== Done.  It's up to you to compile it now! ==="
+
+class MyScanner(Scanner):
+
+	def destination(self, type, name, arglist):
+		classname = "Function"
+		listname = "functions"
+		if arglist:
+			t, n, m = arglist[-1]
+			# This is non-functional today
+			if t == OBJECT and m == "InMode":
+				classname = "Method"
+				listname = "methods"
+		return classname, listname
+
+	def makeblacklistnames(self):
+		return [
+			"TEDispose",
+			"TEInit",
+			"TEGetHiliteRgn",
+			]
+
+	def makeblacklisttypes(self):
+		return [
+			"TEClickLoopUPP",
+			"UniversalProcPtr",
+			"WordBreakUPP"
+			]
+
+	def makerepairinstructions(self):
+		return [
+			([("void_ptr", "*", "InMode"), ("long", "*", "InMode")],
+			 [("InBuffer", "*", "*")]),
+			 
+			# TEContinuousStyle
+			([("short", "mode", "OutMode"), ("TextStyle", "aStyle", "OutMode")],
+			 [("short", "mode", "InOutMode"), ("TextStyle", "aStyle", "InOutMode")])
+			]
+			
+if __name__ == "__main__":
+	main()
