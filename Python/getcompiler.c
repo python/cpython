f857a6becb08af7be77506fd58278b5bd7d99cc8
Guido van Rossum <guido@python.org>
1996-05-23 22:50:26 +0000

    Added copyright. Include config.h which may define COMPILER.

--- Python/getcompiler.c
+++ Python/getcompiler.c
@@ -1,6 +1,36 @@
+/***********************************************************
+Copyright 1991-1995 by Stichting Mathematisch Centrum, Amsterdam,
+The Netherlands.
+
+                        All Rights Reserved
+
+Permission to use, copy, modify, and distribute this software and its 
+documentation for any purpose and without fee is hereby granted, 
+provided that the above copyright notice appear in all copies and that
+both that copyright notice and this permission notice appear in 
+supporting documentation, and that the names of Stichting Mathematisch
+Centrum or CWI not be used in advertising or publicity pertaining to
+distribution of the software without specific, written prior permission.
+
+STICHTING MATHEMATISCH CENTRUM DISCLAIMS ALL WARRANTIES WITH REGARD TO
+THIS SOFTWARE, INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND
+FITNESS, IN NO EVENT SHALL STICHTING MATHEMATISCH CENTRUM BE LIABLE
+FOR ANY SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT
+OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+******************************************************************/
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#ifndef COMPILER
 #ifdef __GNUC__
 #define COMPILER " [GCC " __VERSION__ "]"
 #endif
+#endif /* !COMPILER */
 
 #ifndef COMPILER
 #ifdef __cplusplus
@@ -8,7 +38,7 @@
 #else
 #define COMPILER "[C]"
 #endif
-#endif
+#endif /* !COMPILER */
 
 char *
 getcompiler()
