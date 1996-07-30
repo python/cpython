5d95dfec9a23408ea3bba9ed3760229686260990
Guido van Rossum <guido@python.org>
1996-07-30 19:05:56 +0000

    mac utility

---
+++ Demo/pdist/mac.py
@@ -0,0 +1,19 @@
+import sys
+import string
+import rcvs
+
+def main():
+	while 1:
+		try:
+			line = raw_input('$ ')
+		except EOFError:
+			break
+		words = string.split(line)
+		if not words:
+			continue
+		if words[0] != 'rcvs':
+			words.insert(0, 'rcvs')
+		sys.argv = words
+		rcvs.main()
+
+main()
