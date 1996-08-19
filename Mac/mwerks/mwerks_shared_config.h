cb7f3fc72c9e40bd9cb6771abb5505d96b2020a2
Jack Jansen <jack.jansen@cwi.nl>
1996-08-19 11:16:32 +0000

    Fixes by Guido: HAVE_CONFIG_H, USE_STACKCHECK and USE_MAC_DYNAMIC_LOADING are no longer needed. Also, WASTE and IMG removed in some places.

--- Mac/mwerks/mwerks_shared_config.h
+++ Mac/mwerks/mwerks_shared_config.h
@@ -1,9 +1,6 @@
-#define HAVE_CONFIG_H
 #define USE_MAC_SHARED_LIBRARY
-#define USE_MAC_DYNAMIC_LOADING
 #define USE_MAC_APPLET_SUPPORT
 /* #define USE_MACTCP */
 /* #define USE_BGEN */
 /* #define USE_MACSPEECH */
-#define USE_STACKCHECK
 
