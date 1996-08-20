e420178ba602bad8b3aca4c156bc5fdeaa3f6ab5
Jack Jansen <jack.jansen@cwi.nl>
1996-08-20 16:34:01 +0000

    Rationalized header files: - mwerks_nonshared_config.h for standalone pythons - mwerks_shared_config.h for shared pythons (PythonCore, app and   applet) - mwerks_plugin_config.h for plugins Only the _tkinter plugin uses a customized header.
    Old config files have gone to the "old" directory, for the time being.

--- Mac/mwerks/mwerks_nonshared_config.h
+++ Mac/mwerks/mwerks_nonshared_config.h
@@ -1,11 +1,20 @@
-#define HAVE_CONFIG_H
-#define USE_STDWIN
-#define USE_MACTCP
-#define USE_BGEN
-#define USE_MACSPEECH
-#define USE_IMG
-#define USE_MACCTB
-#define USE_STACKCHECK
-#ifdef __powerc
-#define USE_MAC_DYNAMIC_LOADING
-#endif
+/*
+** Configuration file for standalone 68k/ppc Python.
+**
+** Note: enabling the switches below is not enough to enable the
+** specific features, you may also need different sets of sources.
+*/
+
+#define USE_GUSI		/* Stdio implemented with GUSI */
+#define USE_TOOLBOX		/* Include toolbox modules in core Python */
+#define USE_QT			/* Include quicktime modules in core Python */
+#define USE_WASTE		/* Include waste module in core Python */
+#define USE_MACSPEECH		/* Include macspeech module in core Python */
+#define USE_IMG	       		/* Include img modules in core Python */
+#define USE_MACCTB		/* Include ctb module in core Python */
+/* #define USE_STDWIN		/* Include stdwin module in core Python */
+/* #define USE_MACTCP		/* Include mactcp (*not* socket) modules in core */
+/* #define USE_TK		/* Include _tkinter module in core Python */
+/* #define MAC_TCL		/* This *must* be on if USE_TK is on */
+/* #define USE_MAC_SHARED_LIBRARY	/* Enable code to add shared-library resources */
+/* #define USE_MAC_APPLET_SUPPORT	/* Enable code to run a PYC resource */
--- Mac/mwerks/mwerks_shared_config.h
+++ Mac/mwerks/mwerks_shared_config.h
@@ -1,6 +1,21 @@
-#define USE_MAC_SHARED_LIBRARY
-#define USE_MAC_APPLET_SUPPORT
-/* #define USE_MACTCP */
-/* #define USE_BGEN */
-/* #define USE_MACSPEECH */
+/*
+** Configuration file for dynamically loaded cfm68k/ppc PythonCore,
+** interpreter and Applet.
+**
+** Note: enabling the switches below is not enough to enable the
+** specific features, you may also need different sets of sources.
+*/
 
+#define USE_GUSI		/* Stdio implemented with GUSI */
+/* #define USE_TOOLBOX		/* Include toolbox modules in core Python */
+/* #define USE_QT		/* Include quicktime modules in core Python */
+/* #define USE_WASTE		/* Include waste module in core Python */
+/* #define USE_MACSPEECH	/* Include macspeech module in core Python */
+/* #define USE_IMG		/* Include img modules in core Python */
+/* #define USE_MACCTB		/* Include ctb module in core Python */
+/* #define USE_STDWIN		/* Include stdwin module in core Python */
+/* #define USE_MACTCP		/* Include mactcp (*not* socket) modules in core */
+/* #define USE_TK		/* Include _tkinter module in core Python */
+/* #define MAC_TCL		/* This *must* be on if USE_TK is on */
+#define USE_MAC_SHARED_LIBRARY		/* Enable code to add shared-library resources */
+#define USE_MAC_APPLET_SUPPORT		/* Enable code to run a PYC resource */
