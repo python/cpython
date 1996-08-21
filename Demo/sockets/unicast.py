4ec060ad28eacd0f80a9ee74d44a1f86553f566d
Guido van Rossum <guido@python.org>
1996-08-21 20:11:55 +0000

    unicast pendant for broadcast.py

---
+++ Demo/sockets/unicast.py
@@ -0,0 +1,16 @@
+# Send UDP broadcast packets
+
+MYPORT = 50000
+
+import sys, time
+from socket import *
+
+s = socket(AF_INET, SOCK_DGRAM)
+s.bind(('', 0))
+
+while 1:
+	data = `time.time()` + '\n'
+	s.sendto(data, ('', MYPORT))
+	time.sleep(2)
+
+	
