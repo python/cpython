5c8c91bbc5f2ce192871f0889fd5fb686c4c8c76
Guido van Rossum <guido@python.org>
1996-08-22 23:18:09 +0000

    Changes for Canvas by Fred

--- Lib/lib-tk/Tkinter.py
+++ Lib/lib-tk/Tkinter.py
@@ -935,20 +935,20 @@ class Canvas(Widget):
 		Widget.__init__(self, master, 'canvas', cnf, kw)
 	def addtag(self, *args):
 		self._do('addtag', args)
-	def addtag_above(self, tagOrId):
-		self.addtag('above', tagOrId)
-	def addtag_all(self):
-		self.addtag('all')
-	def addtag_below(self, tagOrId):
-		self.addtag('below', tagOrId)
-	def addtag_closest(self, x, y, halo=None, start=None):
-		self.addtag('closest', x, y, halo, start)
-	def addtag_enclosed(self, x1, y1, x2, y2):
-		self.addtag('enclosed', x1, y1, x2, y2)
-	def addtag_overlapping(self, x1, y1, x2, y2):
-		self.addtag('overlapping', x1, y1, x2, y2)
-	def addtag_withtag(self, tagOrId):
-		self.addtag('withtag', tagOrId)
+	def addtag_above(self, newtag, tagOrId):
+		self.addtag(newtag, 'above', tagOrId)
+	def addtag_all(self, newtag):
+		self.addtag(newtag, 'all')
+	def addtag_below(self, newtag, tagOrId):
+		self.addtag(newtag, 'below', tagOrId)
+	def addtag_closest(self, newtag, x, y, halo=None, start=None):
+		self.addtag(newtag, 'closest', x, y, halo, start)
+	def addtag_enclosed(self, newtag, x1, y1, x2, y2):
+		self.addtag(newtag, 'enclosed', x1, y1, x2, y2)
+	def addtag_overlapping(self, newtag, x1, y1, x2, y2):
+		self.addtag(newtag, 'overlapping', x1, y1, x2, y2)
+	def addtag_withtag(self, newtag, tagOrId):
+		self.addtag(newtag, 'withtag', tagOrId)
 	def bbox(self, *args):
 		return self._getints(self._do('bbox', args)) or None
 	def tag_unbind(self, tagOrId, sequence):
