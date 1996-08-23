bc17d299cc1e30661640f6433340d7740a7c05eb
Jack Jansen <jack.jansen@cwi.nl>
1996-08-23 15:46:12 +0000

    Turn on USE_TK and MAC_TCL by default

--- Mac/mwerks/mwerks_nonshared_config.h
+++ Mac/mwerks/mwerks_nonshared_config.h
@@ -14,7 +14,7 @@
 #define USE_MACCTB		/* Include ctb module in core Python */
 /* #define USE_STDWIN		/* Include stdwin module in core Python */
 /* #define USE_MACTCP		/* Include mactcp (*not* socket) modules in core */
-/* #define USE_TK		/* Include _tkinter module in core Python */
-/* #define MAC_TCL		/* This *must* be on if USE_TK is on */
+#define USE_TK			/* Include _tkinter module in core Python */
+#define MAC_TCL			/* This *must* be on if USE_TK is on */
 /* #define USE_MAC_SHARED_LIBRARY	/* Enable code to add shared-library resources */
 /* #define USE_MAC_APPLET_SUPPORT	/* Enable code to run a PYC resource */
