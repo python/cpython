57b1822459fdd8948c289f847c0fe69b41fd5007
Guido van Rossum <guido@python.org>
1996-08-29 18:10:41 +0000

    *** empty log message ***

---
+++ Python/hypot.c
@@ -0,0 +1,26 @@
+/* hypot() replacement */
+
+#include "config.h"
+#include "myproto.h"
+#include "mymath.h"
+
+double hypot(x, y)
+	double x;
+	double y;
+{
+	double yx;
+
+	x = fabs(x);
+	y = fabs(y);
+	if (x < y) {
+		double temp = x;
+		x = y;
+		y = temp;
+	}
+	if (x == 0.)
+		return 0.;
+	else {
+		yx = y/x;
+		return x*sqrt(1.+yx*yx);
+	}
+}
