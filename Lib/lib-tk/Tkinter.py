3c3e13eabac0cfc24e74fe064cab8106ecf76b43
Fred Drake <fdrake@acm.org>
1996-10-04 19:23:04 +0000

    (Tkinter.py):  Fixed dumb typo in Misc.tk_setPalette().

--- Lib/lib-tk/Tkinter.py
+++ Lib/lib-tk/Tkinter.py
@@ -141,8 +141,8 @@ class Misc:
 	def tk_bisque(self):
 		self.tk.call('tk_bisque')
 	def tk_setPalette(self, *args, **kw):
-		apply(self.tk.call, 'tk_setPalette',
-		      _flatten(args) + _flatten(kw.items()))
+		apply(self.tk.call, ('tk_setPalette',)
+		      + _flatten(args) + _flatten(kw.items()))
 	def tk_menuBar(self, *args):
 		pass # obsolete since Tk 4.0
 	def wait_variable(self, name='PY_VAR'):
