0b324590042b1f4a4390e06b226932dbd4d759de
Barry Warsaw <barry@python.org>
1996-12-09 21:57:25 +0000

    C coding standard for Python C files.
    Currently fairly minimal, but I'll be adding to this as needed.  I
    think it's pretty darn close.
    
    To use this, just load the file and in a C buffer type:
    
    M-x c-set-style RET python RET
    
    [there are ways to automate much of this!]

---
+++ Misc/ccpy-style.el
@@ -0,0 +1,35 @@
+;;; ccpy-style.el  ---  cc-mode style definition for Python C code
+;;
+;; Author:  1996 Barry A. Warsaw
+;; Created:  6-Dec-1996
+;; Version: $Revision$
+;; Last Modified: $Date$
+;; Keywords: c python languages oop
+
+;;; Commentary
+;; This file defines the standard C coding style for Python C files
+;; and modules.  It is compatible with cc-mode.el which should be a
+;; standard part of your Emacs distribution (or see
+;; <http://www.python.org/ftp/emacs/>).
+
+;; To use, make sure this file is on your Emacs load-path, and simply
+;; add this to your .emacs file:
+;;
+;; (add-hook 'c-mode-common-hook '(lambda () (require 'python-style)))
+
+;; This file will self-install on your c-style-alist variable,
+;; although you will have to install it on a per-file basis with:
+;;
+;; M-x c-set-style RET python
+
+;;; Code:
+
+(defconst python-cc-style
+  '((indent-tabs-mode . nil)
+    (c-basic-offset   . 8)
+    )
+  "Standard Python C coding style.")
+
+(require 'cc-mode)
+(if (not (assoc "python" c-style-alist))
+    (c-add-style "python" python-cc-style))
