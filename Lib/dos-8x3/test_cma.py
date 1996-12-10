5de1f8dac84da8688823688a4f90f36dbbc1af1d
Guido van Rossum <guido@python.org>
1996-12-10 16:02:14 +0000

    The usual; added several test modules, too.

---
+++ Lib/dos-8x3/test_cma.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python
+""" Simple test script for cmathmodule.c
+Roger E. Masse
+"""
+import cmath
+
+cmath.acos(1.0)
+cmath.acosh(1.0)
+cmath.asin(1.0)
+cmath.asinh(1.0)
+cmath.atan(0.2)
+cmath.atanh(0.3)
+cmath.cos(1.0)
+cmath.cosh(1.0)
+cmath.exp(1.0)
+cmath.log(1.0)
+cmath.log10(1.0)
+cmath.sin(1.0)
+cmath.sinh(1.0)
+cmath.sqrt(1.0)
+cmath.tan(1.0)
+cmath.tanh(1.0)
