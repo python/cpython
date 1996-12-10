0f150e41be9d962f8cdfb5e77c9ff3394b84315c
Barry Warsaw <barry@python.org>
1996-12-10 16:27:16 +0000

    Added test_new.

--- Lib/test/testall.py
+++ Lib/test/testall.py
@@ -3,6 +3,7 @@
 # It is a good idea to do this whenever you build a new interpreter.
 # Remember to add new tests when new features are added!
 
+import sys
 from test_support import *
 
 print 'test_grammar'
@@ -12,10 +13,13 @@ import test_grammar
 for t in ['test_opcodes', 'test_operations', 'test_builtin',
 	  'test_exceptions', 'test_types', 'test_math', 'test_time',
 	  'test_array', 'test_strop', 'test_md5', 'test_cmath',
-	  'test_crypt', 'test_dbm',
+	  'test_crypt', 'test_dbm', 'test_new',
 	  ]:
     print t
     unload(t)
-    __import__(t, globals(), locals())
+    try:
+	__import__(t, globals(), locals())
+    except ImportError, msg:
+	sys.stderr.write('%s.  Uninstalled optional module?\n' % msg)
 
 print 'Passed all tests.'
