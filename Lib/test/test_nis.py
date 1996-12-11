870d5c67ae2b4d916141814957c2d16bb813c4f8
Barry Warsaw <barry@python.org>
1996-12-11 16:07:30 +0000

    Just test one key/value pair for one NIS map.  I.e. don't test them all because it can take a really long time.

--- Lib/test/test_nis.py
+++ Lib/test/test_nis.py
@@ -5,15 +5,21 @@ if __name__ == '__main__':
     verbose = 1
 
 maps = nis.maps()
-for nismap in maps:
-    if verbose:
-	print nismap
-    mapping = nis.cat(nismap)
-    for k, v in mapping.items():
+try:
+    for nismap in maps:
 	if verbose:
-	    print '    ', k, v
-	if not k:
-	    continue
-	if nis.match(k, nismap) <> v:
-	    print "NIS match failed for key `%s' in map `%s'" % (k, nismap)
-
+	    print nismap
+	mapping = nis.cat(nismap)
+	for k, v in mapping.items():
+	    if verbose:
+		print '    ', k, v
+	    if not k:
+		continue
+	    if nis.match(k, nismap) <> v:
+		print "NIS match failed for key `%s' in map `%s'" % (k, nismap)
+	    else:
+		# just test the one key, otherwise this test could take a
+		# very long time
+		raise 'done'
+except 'done':
+    pass
