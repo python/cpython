8f1f24e07e961451a227bcae3cf9aceeddbcc720
Guido van Rossum <guido@python.org>
1996-12-11 16:28:30 +0000

    Rewrite without using try-except to break out of two loops.

--- Lib/test/test_nis.py
+++ Lib/test/test_nis.py
@@ -5,21 +5,22 @@ if __name__ == '__main__':
     verbose = 1
 
 maps = nis.maps()
-try:
-    for nismap in maps:
+done = 0
+for nismap in maps:
+    if verbose:
+	print nismap
+    mapping = nis.cat(nismap)
+    for k, v in mapping.items():
 	if verbose:
-	    print nismap
-	mapping = nis.cat(nismap)
-	for k, v in mapping.items():
-	    if verbose:
-		print '    ', k, v
-	    if not k:
-		continue
-	    if nis.match(k, nismap) <> v:
-		print "NIS match failed for key `%s' in map `%s'" % (k, nismap)
-	    else:
-		# just test the one key, otherwise this test could take a
-		# very long time
-		raise 'done'
-except 'done':
-    pass
+	    print '    ', k, v
+	if not k:
+	    continue
+	if nis.match(k, nismap) <> v:
+	    print "NIS match failed for key `%s' in map `%s'" % (k, nismap)
+	else:
+	    # just test the one key, otherwise this test could take a
+	    # very long time
+	    done = 1
+	    break
+    if done:
+	break
