1f983df3bdddfaf437e3c8d2f31cbe4011a380c5
Roger E. Masse <rmasse@newcnri.cnri.reston.va.us>
1997-01-03 22:39:03 +0000

    This is a very inobstrusive test for the existance of the al module and all it's attributes.  More comprehensive examples can be found in Demo/al

---
+++ Lib/test/test_al.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python
+"""Whimpy test script for the al module
+   Roger E. Masse
+"""
+import al
+from test_support import verbose
+
+alattrs = ['__doc__', '__name__', 'getdefault', 'getminmax', 'getname', 'getparams',
+	   'newconfig', 'openport', 'queryparams', 'setparams']
+
+# This is a very inobstrusive test for the existance of the al module and all it's
+# attributes.  More comprehensive examples can be found in Demo/al
+
+def main():
+    # touch all the attributes of al without doing anything
+    if verbose:
+	print 'Touching al module attributes...'
+    for attr in alattrs:
+	if verbose:
+	    print 'touching: ', attr
+	getattr(al, attr)
+
+main()
+
