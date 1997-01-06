573bd91d39d8cadbc96678de23f7e995807d427c
Barry Warsaw <barry@python.org>
1997-01-06 22:44:27 +0000

    added PyTuple_GET_SIZE macro

--- Include/tupleobject.h
+++ Include/tupleobject.h
@@ -69,6 +69,7 @@ extern int _PyTuple_Resize Py_PROTO((PyObject **, int, int));
 
 /* Macro, trading safety for speed */
 #define PyTuple_GET_ITEM(op, i) (((PyTupleObject *)(op))->ob_item[i])
+#define PyTuple_GET_SIZE(op)    (((PyTupleObject *)(op))->ob_size)
 
 /* Macro, *only* to be used to fill in brand new tuples */
 #define PyTuple_SET_ITEM(op, i, v) (((PyTupleObject *)(op))->ob_item[i] = v)
