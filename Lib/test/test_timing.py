f65b312233e5187e2fcd23f8065a11eb6f773c37
Barry Warsaw <barry@python.org>
1997-01-13 23:03:07 +0000

    Test of the timing module

---
+++ Lib/test/test_timing.py
@@ -0,0 +1,21 @@
+from test_support import verbose
+import timing
+
+r = range(100000)
+if verbose:
+    print 'starting...'
+timing.start()
+for i in r:
+    pass
+timing.finish()
+if verbose:
+    print 'finished'
+
+secs = timing.seconds()
+milli = timing.milli()
+micro = timing.micro()
+
+if verbose:
+    print 'seconds:', secs
+    print 'milli  :', milli
+    print 'micro  :', micro
