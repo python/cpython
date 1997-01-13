18c6db896b170e37cd89df904cc1c5c35eb81735
Barry Warsaw <barry@python.org>
1997-01-13 20:53:46 +0000

    Catch sunaudiodev.error on open() and re-raise TestFailed exception.

--- Lib/test/test_sunaudiodev.py
+++ Lib/test/test_sunaudiodev.py
@@ -14,9 +14,13 @@ def play_sound_file(path):
     fp = open(path, 'r')
     data = fp.read()
     fp.close()
-    a = sunaudiodev.open('w')
-    a.write(data)
-    a.close()
+    try:
+	a = sunaudiodev.open('w')
+    except sunaudiodev.error, msg:
+	raise TestFailed, msg
+    else:
+	a.write(data)
+	a.close()
 
 def test():
     play_sound_file(findfile('audiotest.au'))
