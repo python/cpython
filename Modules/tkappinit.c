7a652854253717d4f4908bdfbcfa2437b7e4ed3b
Barry Warsaw <barry@python.org>
1997-01-14 17:36:36 +0000

    Formatting.

--- Modules/tkappinit.c
+++ Modules/tkappinit.c
@@ -5,32 +5,31 @@
 
 int
 Tcl_AppInit (interp)
-     Tcl_Interp *interp;
+	Tcl_Interp *interp;
 {
-  Tk_Window main;
+	Tk_Window main;
 
-  main = Tk_MainWindow(interp);
+	main = Tk_MainWindow(interp);
 
-  if (Tcl_Init (interp) == TCL_ERROR)
-    return TCL_ERROR;
-  if (Tk_Init (interp) == TCL_ERROR)
-    return TCL_ERROR;
+	if (Tcl_Init (interp) == TCL_ERROR)
+		return TCL_ERROR;
+	if (Tk_Init (interp) == TCL_ERROR)
+		return TCL_ERROR;
 
 #ifdef WITH_MOREBUTTONS
-  {
-    extern Tcl_CmdProc studButtonCmd;
-    extern Tcl_CmdProc triButtonCmd;
+	{
+		extern Tcl_CmdProc studButtonCmd;
+		extern Tcl_CmdProc triButtonCmd;
 
-    Tcl_CreateCommand(interp, "studbutton", studButtonCmd,
-		    (ClientData) main, NULL);
-    Tcl_CreateCommand(interp, "tributton", triButtonCmd,
-		      (ClientData) main, NULL);
-  }
+		Tcl_CreateCommand(interp, "studbutton", studButtonCmd,
+				  (ClientData) main, NULL);
+		Tcl_CreateCommand(interp, "tributton", triButtonCmd,
+				  (ClientData) main, NULL);
+	}
 #endif
 #ifdef WITH_XXX
 
 #endif
-
-  return TCL_OK;
+	return TCL_OK;
 }
 
