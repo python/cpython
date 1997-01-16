c018d6e18f8e84537c8dd536b0d5b74b0e09cbed
Roger E. Masse <rmasse@newcnri.cnri.reston.va.us>
1997-01-16 17:10:22 +0000

    Renamed.

--- Modules/binascii.c
+++ Modules/binascii.c
@@ -143,7 +143,7 @@ static unsigned char table_a2b_hqx[256] = {
 };
 
 static unsigned char table_b2a_hqx[] =
-    "!\"#$%&'()*+,-012345689@ABCDEFGHIJKLMNPQRSTUVXYZ[`abcdefhijklmpqr";
+"!\"#$%&'()*+,-012345689@ABCDEFGHIJKLMNPQRSTUVXYZ[`abcdefhijklmpqr";
 
 static char table_a2b_base64[] = {
 	-1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1,
@@ -160,43 +160,43 @@ static char table_a2b_base64[] = {
 #define BASE64_MAXBIN 57	/* Max binary chunk size (76 char line) */
 
 static unsigned char table_b2a_base64[] =
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 
 
 
 static unsigned short crctab_hqx[256] = {
-    0x0000, 0x1021, 0x2042, 0x3063, 0x4084, 0x50a5, 0x60c6, 0x70e7,
-    0x8108, 0x9129, 0xa14a, 0xb16b, 0xc18c, 0xd1ad, 0xe1ce, 0xf1ef,
-    0x1231, 0x0210, 0x3273, 0x2252, 0x52b5, 0x4294, 0x72f7, 0x62d6,
-    0x9339, 0x8318, 0xb37b, 0xa35a, 0xd3bd, 0xc39c, 0xf3ff, 0xe3de,
-    0x2462, 0x3443, 0x0420, 0x1401, 0x64e6, 0x74c7, 0x44a4, 0x5485,
-    0xa56a, 0xb54b, 0x8528, 0x9509, 0xe5ee, 0xf5cf, 0xc5ac, 0xd58d,
-    0x3653, 0x2672, 0x1611, 0x0630, 0x76d7, 0x66f6, 0x5695, 0x46b4,
-    0xb75b, 0xa77a, 0x9719, 0x8738, 0xf7df, 0xe7fe, 0xd79d, 0xc7bc,
-    0x48c4, 0x58e5, 0x6886, 0x78a7, 0x0840, 0x1861, 0x2802, 0x3823,
-    0xc9cc, 0xd9ed, 0xe98e, 0xf9af, 0x8948, 0x9969, 0xa90a, 0xb92b,
-    0x5af5, 0x4ad4, 0x7ab7, 0x6a96, 0x1a71, 0x0a50, 0x3a33, 0x2a12,
-    0xdbfd, 0xcbdc, 0xfbbf, 0xeb9e, 0x9b79, 0x8b58, 0xbb3b, 0xab1a,
-    0x6ca6, 0x7c87, 0x4ce4, 0x5cc5, 0x2c22, 0x3c03, 0x0c60, 0x1c41,
-    0xedae, 0xfd8f, 0xcdec, 0xddcd, 0xad2a, 0xbd0b, 0x8d68, 0x9d49,
-    0x7e97, 0x6eb6, 0x5ed5, 0x4ef4, 0x3e13, 0x2e32, 0x1e51, 0x0e70,
-    0xff9f, 0xefbe, 0xdfdd, 0xcffc, 0xbf1b, 0xaf3a, 0x9f59, 0x8f78,
-    0x9188, 0x81a9, 0xb1ca, 0xa1eb, 0xd10c, 0xc12d, 0xf14e, 0xe16f,
-    0x1080, 0x00a1, 0x30c2, 0x20e3, 0x5004, 0x4025, 0x7046, 0x6067,
-    0x83b9, 0x9398, 0xa3fb, 0xb3da, 0xc33d, 0xd31c, 0xe37f, 0xf35e,
-    0x02b1, 0x1290, 0x22f3, 0x32d2, 0x4235, 0x5214, 0x6277, 0x7256,
-    0xb5ea, 0xa5cb, 0x95a8, 0x8589, 0xf56e, 0xe54f, 0xd52c, 0xc50d,
-    0x34e2, 0x24c3, 0x14a0, 0x0481, 0x7466, 0x6447, 0x5424, 0x4405,
-    0xa7db, 0xb7fa, 0x8799, 0x97b8, 0xe75f, 0xf77e, 0xc71d, 0xd73c,
-    0x26d3, 0x36f2, 0x0691, 0x16b0, 0x6657, 0x7676, 0x4615, 0x5634,
-    0xd94c, 0xc96d, 0xf90e, 0xe92f, 0x99c8, 0x89e9, 0xb98a, 0xa9ab,
-    0x5844, 0x4865, 0x7806, 0x6827, 0x18c0, 0x08e1, 0x3882, 0x28a3,
-    0xcb7d, 0xdb5c, 0xeb3f, 0xfb1e, 0x8bf9, 0x9bd8, 0xabbb, 0xbb9a,
-    0x4a75, 0x5a54, 0x6a37, 0x7a16, 0x0af1, 0x1ad0, 0x2ab3, 0x3a92,
-    0xfd2e, 0xed0f, 0xdd6c, 0xcd4d, 0xbdaa, 0xad8b, 0x9de8, 0x8dc9,
-    0x7c26, 0x6c07, 0x5c64, 0x4c45, 0x3ca2, 0x2c83, 0x1ce0, 0x0cc1,
-    0xef1f, 0xff3e, 0xcf5d, 0xdf7c, 0xaf9b, 0xbfba, 0x8fd9, 0x9ff8,
-    0x6e17, 0x7e36, 0x4e55, 0x5e74, 0x2e93, 0x3eb2, 0x0ed1, 0x1ef0,
+	0x0000, 0x1021, 0x2042, 0x3063, 0x4084, 0x50a5, 0x60c6, 0x70e7,
+	0x8108, 0x9129, 0xa14a, 0xb16b, 0xc18c, 0xd1ad, 0xe1ce, 0xf1ef,
+	0x1231, 0x0210, 0x3273, 0x2252, 0x52b5, 0x4294, 0x72f7, 0x62d6,
+	0x9339, 0x8318, 0xb37b, 0xa35a, 0xd3bd, 0xc39c, 0xf3ff, 0xe3de,
+	0x2462, 0x3443, 0x0420, 0x1401, 0x64e6, 0x74c7, 0x44a4, 0x5485,
+	0xa56a, 0xb54b, 0x8528, 0x9509, 0xe5ee, 0xf5cf, 0xc5ac, 0xd58d,
+	0x3653, 0x2672, 0x1611, 0x0630, 0x76d7, 0x66f6, 0x5695, 0x46b4,
+	0xb75b, 0xa77a, 0x9719, 0x8738, 0xf7df, 0xe7fe, 0xd79d, 0xc7bc,
+	0x48c4, 0x58e5, 0x6886, 0x78a7, 0x0840, 0x1861, 0x2802, 0x3823,
+	0xc9cc, 0xd9ed, 0xe98e, 0xf9af, 0x8948, 0x9969, 0xa90a, 0xb92b,
+	0x5af5, 0x4ad4, 0x7ab7, 0x6a96, 0x1a71, 0x0a50, 0x3a33, 0x2a12,
+	0xdbfd, 0xcbdc, 0xfbbf, 0xeb9e, 0x9b79, 0x8b58, 0xbb3b, 0xab1a,
+	0x6ca6, 0x7c87, 0x4ce4, 0x5cc5, 0x2c22, 0x3c03, 0x0c60, 0x1c41,
+	0xedae, 0xfd8f, 0xcdec, 0xddcd, 0xad2a, 0xbd0b, 0x8d68, 0x9d49,
+	0x7e97, 0x6eb6, 0x5ed5, 0x4ef4, 0x3e13, 0x2e32, 0x1e51, 0x0e70,
+	0xff9f, 0xefbe, 0xdfdd, 0xcffc, 0xbf1b, 0xaf3a, 0x9f59, 0x8f78,
+	0x9188, 0x81a9, 0xb1ca, 0xa1eb, 0xd10c, 0xc12d, 0xf14e, 0xe16f,
+	0x1080, 0x00a1, 0x30c2, 0x20e3, 0x5004, 0x4025, 0x7046, 0x6067,
+	0x83b9, 0x9398, 0xa3fb, 0xb3da, 0xc33d, 0xd31c, 0xe37f, 0xf35e,
+	0x02b1, 0x1290, 0x22f3, 0x32d2, 0x4235, 0x5214, 0x6277, 0x7256,
+	0xb5ea, 0xa5cb, 0x95a8, 0x8589, 0xf56e, 0xe54f, 0xd52c, 0xc50d,
+	0x34e2, 0x24c3, 0x14a0, 0x0481, 0x7466, 0x6447, 0x5424, 0x4405,
+	0xa7db, 0xb7fa, 0x8799, 0x97b8, 0xe75f, 0xf77e, 0xc71d, 0xd73c,
+	0x26d3, 0x36f2, 0x0691, 0x16b0, 0x6657, 0x7676, 0x4615, 0x5634,
+	0xd94c, 0xc96d, 0xf90e, 0xe92f, 0x99c8, 0x89e9, 0xb98a, 0xa9ab,
+	0x5844, 0x4865, 0x7806, 0x6827, 0x18c0, 0x08e1, 0x3882, 0x28a3,
+	0xcb7d, 0xdb5c, 0xeb3f, 0xfb1e, 0x8bf9, 0x9bd8, 0xabbb, 0xbb9a,
+	0x4a75, 0x5a54, 0x6a37, 0x7a16, 0x0af1, 0x1ad0, 0x2ab3, 0x3a92,
+	0xfd2e, 0xed0f, 0xdd6c, 0xcd4d, 0xbdaa, 0xad8b, 0x9de8, 0x8dc9,
+	0x7c26, 0x6c07, 0x5c64, 0x4c45, 0x3ca2, 0x2c83, 0x1ce0, 0x0cc1,
+	0xef1f, 0xff3e, 0xcf5d, 0xdf7c, 0xaf9b, 0xbfba, 0x8fd9, 0x9ff8,
+	0x6e17, 0x7e36, 0x4e55, 0x5e74, 0x2e93, 0x3eb2, 0x0ed1, 0x1ef0,
 };
 
 static char doc_a2b_uu[] = "(ascii) -> bin. Decode a line of uuencoded data";
@@ -204,7 +204,7 @@ static char doc_a2b_uu[] = "(ascii) -> bin. Decode a line of uuencoded data";
 static PyObject *
 binascii_a2b_uu(self, args)
 	PyObject *self;
-	PyObject *args;
+        PyObject *args;
 {
 	unsigned char *ascii_data, *bin_data;
 	int leftbits = 0;
@@ -235,8 +235,9 @@ binascii_a2b_uu(self, args)
 			this_ch = 0;
 	        } else {
 			/* Check the character for legality
-			** The 64 in stead of the expected 63 is because there are a few
-			** uuencodes out there that use '@' as zero in stead of space.
+			** The 64 in stead of the expected 63 is because
+			** there are a few uuencodes out there that use
+			** '@' as zero instead of space.
 			*/
 			if ( this_ch < ' ' || this_ch > (' ' + 64)) {
 				PyErr_SetString(Error, "Illegal char");
@@ -278,7 +279,7 @@ static char doc_b2a_uu[] = "(bin) -> ascii. Uuencode line of data";
 static PyObject *
 binascii_b2a_uu(self, args)
 	PyObject *self;
-	PyObject *args;
+        PyObject *args;
 {
 	unsigned char *ascii_data, *bin_data;
 	int leftbits = 0;
@@ -320,7 +321,8 @@ binascii_b2a_uu(self, args)
 	}
 	*ascii_data++ = '\n';	/* Append a courtesy newline */
 	
-	_PyString_Resize(&rv, (ascii_data - (unsigned char *)PyString_AsString(rv)));
+	_PyString_Resize(&rv, (ascii_data -
+			       (unsigned char *)PyString_AsString(rv)));
 	return rv;
 }
 
@@ -329,7 +331,7 @@ static char doc_a2b_base64[] = "(ascii) -> bin. Decode a line of base64 data";
 static PyObject *
 binascii_a2b_base64(self, args)
 	PyObject *self;
-	PyObject *args;
+        PyObject *args;
 {
 	unsigned char *ascii_data, *bin_data;
 	int leftbits = 0;
@@ -390,7 +392,7 @@ static char doc_b2a_base64[] = "(bin) -> ascii. Base64-code line of data";
 static PyObject *
 binascii_b2a_base64(self, args)
 	PyObject *self;
-	PyObject *args;
+        PyObject *args;
 {
 	unsigned char *ascii_data, *bin_data;
 	int leftbits = 0;
@@ -433,7 +435,8 @@ binascii_b2a_base64(self, args)
 	} 
 	*ascii_data++ = '\n';	/* Append a courtesy newline */
 	
-	_PyString_Resize(&rv, (ascii_data - (unsigned char *)PyString_AsString(rv)));
+	_PyString_Resize(&rv, (ascii_data -
+			       (unsigned char *)PyString_AsString(rv)));
 	return rv;
 }
 
@@ -442,7 +445,7 @@ static char doc_a2b_hqx[] = "ascii -> bin, done. Decode .hqx coding";
 static PyObject *
 binascii_a2b_hqx(self, args)
 	PyObject *self;
-	PyObject *args;
+        PyObject *args;
 {
 	unsigned char *ascii_data, *bin_data;
 	int leftbits = 0;
@@ -499,6 +502,7 @@ binascii_a2b_hqx(self, args)
 		Py_DECREF(rv);
 		return rrv;
 	}
+
 	return NULL;
 }
 
@@ -507,7 +511,7 @@ static char doc_rlecode_hqx[] = "Binhex RLE-code binary data";
 static PyObject *
 binascii_rlecode_hqx(self, args)
 	PyObject *self;
-	PyObject *args;
+PyObject *args;
 {
 	unsigned char *in_data, *out_data;
 	PyObject *rv;
@@ -532,7 +536,7 @@ binascii_rlecode_hqx(self, args)
 			/* Check how many following are the same */
 			for(inend=in+1;
 			    inend<len && in_data[inend] == ch &&
-			        inend < in+255;
+				    inend < in+255;
 			    inend++) ;
 			if ( inend - in > 3 ) {
 				/* More than 3 in a row. Output RLE. */
@@ -546,7 +550,8 @@ binascii_rlecode_hqx(self, args)
 			}
 		}
 	}
-	_PyString_Resize(&rv, (out_data - (unsigned char *)PyString_AsString(rv)));
+	_PyString_Resize(&rv, (out_data -
+			       (unsigned char *)PyString_AsString(rv)));
 	return rv;
 }
 
@@ -555,7 +560,7 @@ static char doc_b2a_hqx[] = "Encode .hqx data";
 static PyObject *
 binascii_b2a_hqx(self, args)
 	PyObject *self;
-	PyObject *args;
+        PyObject *args;
 {
 	unsigned char *ascii_data, *bin_data;
 	int leftbits = 0;
@@ -587,7 +592,8 @@ binascii_b2a_hqx(self, args)
 		leftchar <<= (6-leftbits);
 		*ascii_data++ = table_b2a_hqx[leftchar & 0x3f];
 	}
-	_PyString_Resize(&rv, (ascii_data - (unsigned char *)PyString_AsString(rv)));
+	_PyString_Resize(&rv, (ascii_data -
+			       (unsigned char *)PyString_AsString(rv)));
 	return rv;
 }
 
@@ -596,7 +602,7 @@ static char doc_rledecode_hqx[] = "Decode hexbin RLE-coded string";
 static PyObject *
 binascii_rledecode_hqx(self, args)
 	PyObject *self;
-	PyObject *args;
+        PyObject *args;
 {
 	unsigned char *in_data, *out_data;
 	unsigned char in_byte, in_repeat;
@@ -622,32 +628,33 @@ binascii_rledecode_hqx(self, args)
 	** end-of-buffer for input and output strings.
 	*/
 #define INBYTE(b) \
-    do { \
-	if ( --in_len < 0 ) { \
-		PyErr_SetString(Incomplete, ""); \
-	        Py_DECREF(rv); \
-		return NULL; \
-	} \
-	b = *in_data++; \
-    } while(0)
+	do { \
+	         if ( --in_len < 0 ) { \
+			   PyErr_SetString(Incomplete, ""); \
+			   Py_DECREF(rv); \
+			   return NULL; \
+		 } \
+		 b = *in_data++; \
+	} while(0)
 	    
 #define OUTBYTE(b) \
-    do { \
-	if ( --out_len_left < 0 ) { \
-		_PyString_Resize(&rv, 2*out_len); \
-		if ( rv == NULL ) return NULL; \
-		out_data = (unsigned char *)PyString_AsString(rv) + out_len; \
-		out_len_left = out_len-1; \
-		out_len = out_len * 2; \
-	} \
-	*out_data++ = b; \
-    } while(0)
+	do { \
+		 if ( --out_len_left < 0 ) { \
+			  _PyString_Resize(&rv, 2*out_len); \
+			  if ( rv == NULL ) return NULL; \
+			  out_data = (unsigned char *)PyString_AsString(rv) \
+								 + out_len; \
+			  out_len_left = out_len-1; \
+			  out_len = out_len * 2; \
+		 } \
+		 *out_data++ = b; \
+	} while(0)
 
-	/*
-	** Handle first byte separately (since we have to get angry
-	** in case of an orphaned RLE code).
-	*/
-	INBYTE(in_byte);
+		/*
+		** Handle first byte separately (since we have to get angry
+		** in case of an orphaned RLE code).
+		*/
+		INBYTE(in_byte);
 
 	if (in_byte == RUNCHAR) {
 		INBYTE(in_repeat);
@@ -683,16 +690,18 @@ binascii_rledecode_hqx(self, args)
 			OUTBYTE(in_byte);
 		}
 	}
-	_PyString_Resize(&rv, (out_data - (unsigned char *)PyString_AsString(rv)));
+	_PyString_Resize(&rv, (out_data -
+			       (unsigned char *)PyString_AsString(rv)));
 	return rv;
 }
 
-static char doc_crc_hqx[] = "(data, oldcrc) -> newcrc. Compute hqx CRC incrementally";
+static char doc_crc_hqx[] =
+"(data, oldcrc) -> newcrc. Compute hqx CRC incrementally";
 
 static PyObject *
 binascii_crc_hqx(self, args)
 	PyObject *self;
-	PyObject *args;
+PyObject *args;
 {
 	unsigned char *bin_data;
 	unsigned int crc;
@@ -714,15 +723,15 @@ static struct PyMethodDef binascii_module_methods[] = {
 	{"a2b_uu",		binascii_a2b_uu,	1,	doc_a2b_uu},
 	{"b2a_uu",		binascii_b2a_uu,	1,	doc_b2a_uu},
 	{"a2b_base64",		binascii_a2b_base64,	1,
-		 doc_a2b_base64},
+	 doc_a2b_base64},
 	{"b2a_base64",		binascii_b2a_base64,	1,
-		 doc_b2a_base64},
+	 doc_b2a_base64},
 	{"a2b_hqx",		binascii_a2b_hqx,	1,	doc_a2b_hqx},
 	{"b2a_hqx",		binascii_b2a_hqx,	1,	doc_b2a_hqx},
 	{"rlecode_hqx",		binascii_rlecode_hqx,	1,
-		 doc_rlecode_hqx},
+	 doc_rlecode_hqx},
 	{"rledecode_hqx",	binascii_rledecode_hqx,	1,
-		 doc_rledecode_hqx},
+	 doc_rledecode_hqx},
 	{"crc_hqx",		binascii_crc_hqx,	1,	doc_crc_hqx},
 	{NULL,			NULL}		/* sentinel */
 };
