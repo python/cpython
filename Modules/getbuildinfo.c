4edbab778eb8209876e8b7495a091b7ad0c647a2
Guido van Rossum <guido@python.org>
1997-01-20 18:34:26 +0000

    work build# into version string

---
+++ Modules/getbuildinfo.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+#ifndef DATE
+#ifdef __DATE__
+#define DATE __DATE__
+#else
+#define DATE "xx/xx/xx"
+#endif
+#endif
+
+#ifndef TIME
+#ifdef __TIME__
+#define TIME __TIME__
+#else
+#define TIME "xx:xx:xx"
+#endif
+#endif
+
+#ifndef BUILD
+#define BUILD 0
+#endif
+
+
+const char *
+Py_GetBuildInfo()
+{
+	static char buildinfo[40];
+	sprintf(buildinfo, "#%d, %.12s, %.8s", BUILD, DATE, TIME);
+	return buildinfo;
+}
