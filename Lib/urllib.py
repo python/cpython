838cb28290471a600ad04556fa909bdfe877e29e
Guido van Rossum <guido@python.org>
1997-02-10 17:51:56 +0000

    Put a new, more useful, set of references in the leading comment.

--- Lib/urllib.py
+++ Lib/urllib.py
@@ -1,16 +1,23 @@
 # Open an arbitrary URL
 #
-# See the following document for a tentative description of URLs:
-#     Uniform Resource Locators              Tim Berners-Lee
-#     INTERNET DRAFT                                    CERN
-#     IETF URL Working Group                    14 July 1993
-#     draft-ietf-uri-url-01.txt
+# See the following document for more info on URLs:
+# "Names and Addresses, URIs, URLs, URNs, URCs", at
+# http://www.w3.org/pub/WWW/Addressing/Overview.html
+#
+# See also the HTTP spec (from which the error codes are derived):
+# "HTTP - Hypertext Transfer Protocol", at
+# http://www.w3.org/pub/WWW/Protocols/
+#
+# Related standards and specs:
+# - RFC1808: the "relative URL" spec. (authoritative status)
+# - RFC1738 - the "URL standard". (authoritative status)
+# - RFC1630 - the "URI spec". (informational status)
 #
 # The object returned by URLopener().open(file) will differ per
 # protocol.  All you know is that is has methods read(), readline(),
 # readlines(), fileno(), close() and info().  The read*(), fileno()
 # and close() methods work like those of open files. 
-# The info() method returns an mimetools.Message object which can be
+# The info() method returns a mimetools.Message object which can be
 # used to query various info about the object, if available.
 # (mimetools.Message objects are queried with the getheader() method.)
 
