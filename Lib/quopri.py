e5477c5bc5d64096a87381b926b4050e6c9cd060
Guido van Rossum <guido@python.org>
1997-05-08 23:11:04 +0000

    Set correct RFC number.

--- Lib/quopri.py
+++ Lib/quopri.py
@@ -1,4 +1,4 @@
-# Conversions to/from quoted-printable transport encoding as per RFC-XXXX
+# Conversions to/from quoted-printable transport encoding as per RFC-1521
 # (Dec 1991 version).
 
 ESCAPE = '='
