141f9a0f8a5a7ab46e0150f0a0b934cb2c4a50ee
Jack Jansen <jack.jansen@cwi.nl>
1997-05-13 11:27:19 +0000

    Regenrated from CW11 IDE

--- Mac/Lib/toolbox/Metrowerks_Shell_Suite.py
+++ Mac/Lib/toolbox/Metrowerks_Shell_Suite.py
@@ -1,7 +1,7 @@
 """Suite Metrowerks Shell Suite: Events supported by the Metrowerks Project Shell
 Level 1, version 1
 
-Generated from flap:CW9 Gold:Metrowerks CodeWarrior:CodeWarrior IDE 1.6.3
+Generated from flap:Metrowerks:Metrowerks CodeWarrior:CodeWarrior IDE 1.7.4
 AETE/AEUT resource version 1/0, language 0, script 0
 """
 
@@ -18,13 +18,13 @@ _Enum_savo = {
 
 _Enum_ErrT = {
 	'information' : 'ErIn',	# 
-	'compiler_error' : 'ErCE',	# 
 	'compiler_warning' : 'ErCW',	# 
+	'compiler_error' : 'ErCE',	# 
 	'definition' : 'ErDf',	# 
-	'linker_error' : 'ErLE',	# 
 	'linker_warning' : 'ErLW',	# 
+	'linker_error' : 'ErLE',	# 
 	'find_result' : 'ErFn',	# 
-	'browser_error' : 'ErBr',	# 
+	'generic_error' : 'ErGn',	# 
 }
 
 _Enum_Mode = {
@@ -51,6 +51,22 @@ _Enum_PPrm = {
 _Enum_RefP = {
 	'Think_Reference' : 'DanR',	# 
 	'QuickView' : 'ALTV',	# 
+}
+
+_Enum_Lang = {
+	'C' : 'LC  ',	# 
+	'C_2b__2b_' : 'LC++',	# 
+	'Pascal' : 'LP  ',	# 
+	'Object_Pascal' : 'LP++',	# 
+	'Java' : 'LJav',	# 
+	'Assembler' : 'LAsm',	# 
+	'Unknown' : 'L?  ',	# 
+}
+
+_Enum_Acce = {
+	'public' : 'Publ',	# 
+	'protected' : 'Prot',	# 
+	'private' : 'Priv',	# 
 }
 
 class Metrowerks_Shell_Suite:
@@ -563,8 +579,8 @@ class Metrowerks_Shell_Suite:
 		if _arguments.has_key('----'):
 			return _arguments['----']
 
-	def Save_Message_Window_As(self, _object, _attributes={}, **_arguments):
-		"""Save Message Window As: Saves the message window as a text file
+	def Save_Error_Window_As(self, _object, _attributes={}, **_arguments):
+		"""Save Error Window As: Saves the Errors & Warnings window as a text file
 		Required argument: Destination file for Save Message Window As
 		Keyword argument _attributes: AppleEvent attribute dictionary
 		"""
@@ -731,59 +747,147 @@ class Metrowerks_Shell_Suite:
 		if _arguments.has_key('----'):
 			return _arguments['----']
 
+	def Open_browser(self, _object, _attributes={}, **_arguments):
+		"""Open browser: Display a class, member function, or data member object in a single class browser window
+		Required argument: an AE object reference
+		Keyword argument _attributes: AppleEvent attribute dictionary
+		"""
+		_code = 'MMPR'
+		_subcode = 'Brow'
+
+		if _arguments: raise TypeError, 'No optional args expected'
+		_arguments['----'] = _object
+
+
+		_reply, _arguments, _attributes = self.send(_code, _subcode,
+				_arguments, _attributes)
+		if _arguments.has_key('errn'):
+			raise aetools.Error, aetools.decodeerror(_arguments)
+		# XXXX Optionally decode result
+		if _arguments.has_key('----'):
+			return _arguments['----']
+
+	def Get_nonsimple_classes(self, _no_object=None, _attributes={}, **_arguments):
+		"""Get nonsimple classes: Returns an alphabetical list of classes with member functions, bases classes, or subclasses
+		Keyword argument _attributes: AppleEvent attribute dictionary
+		Returns: undocumented, typecode 'list'
+		"""
+		_code = 'MMPR'
+		_subcode = 'NsCl'
+
+		if _arguments: raise TypeError, 'No optional args expected'
+		if _no_object != None: raise TypeError, 'No direct arg expected'
+
+
+		_reply, _arguments, _attributes = self.send(_code, _subcode,
+				_arguments, _attributes)
+		if _arguments.has_key('errn'):
+			raise aetools.Error, aetools.decodeerror(_arguments)
+		# XXXX Optionally decode result
+		if _arguments.has_key('----'):
+			return _arguments['----']
+
+	def Get_member_function_names(self, _object, _attributes={}, **_arguments):
+		"""Get member function names: Returns a list containing the names of all the member functions of a class object
+		Required argument: must be a class object
+		Keyword argument _attributes: AppleEvent attribute dictionary
+		Returns: undocumented, typecode 'list'
+		"""
+		_code = 'MMPR'
+		_subcode = 'MbFN'
+
+		if _arguments: raise TypeError, 'No optional args expected'
+		_arguments['----'] = _object
+
+
+		_reply, _arguments, _attributes = self.send(_code, _subcode,
+				_arguments, _attributes)
+		if _arguments.has_key('errn'):
+			raise aetools.Error, aetools.decodeerror(_arguments)
+		# XXXX Optionally decode result
+		if _arguments.has_key('----'):
+			return _arguments['----']
+
 
 #    Class 'Access Paths' ('PATH') -- 'Contains the definitions of a project\325s access (search) paths.'
 #        property 'User Paths' ('PA01') 'PInf' -- 'To add an access path for the source files.' [mutable list]
 #        property 'System Paths' ('PA03') 'PInf' -- 'To add an access path for the include files. (Not supported in Pascal)' [mutable list]
 #        property 'Always Full Search' ('PA02') 'bool' -- 'To force the compiler to search for system includes like it searches for user includes.' [mutable]
 
-#    Class 'Document' ('docu') -- 'An open text file'
-#        property 'name' ('pnam') 'TEXT' -- 'The document\325s name' []
-#        property 'mode' ('Mode') 'Mode' -- 'The document\325s open mode' [enum]
-#        property 'disk file' ('file') 'fss ' -- 'The document\325s location on disk' []
-
 #    Class 'Editor' ('EDTR') -- ''
-#        property 'Syntax coloring' ('ED01') 'bool' -- 'Mark keywords and comments with color.' [mutable]
-#        property 'Text color' ('ED02') 'cRGB' -- 'The color for ordinary text.' [mutable]
-#        property 'Comment color' ('ED03') 'cRGB' -- 'The color for comments.' [mutable]
-#        property 'Keyword color' ('ED04') 'cRGB' -- 'The color for language keywords.' [mutable]
-#        property 'String color' ('ED05') 'cRGB' -- 'The color for strings.' [mutable]
-#        property 'Custom color' ('ED06') 'cRGB' -- 'The color for custom keywords.' [mutable]
-#        property 'Flash delay' ('ED07') 'long' -- 'The amount of time, in sixtieths of a second, the editor highlights a matching bracket.' [mutable]
-#        property 'Dynamic scroll' ('ED08') 'bool' -- 'Display a window\325s contents as you move the scroll box.' [mutable]
-#        property 'Balance' ('ED09') 'bool' -- 'Flash the matching opening bracket when you type a closing bracket.' [mutable]
-#        property 'Remember font' ('ED10') 'bool' -- 'Display a source file with its own font settings.' [mutable]
-#        property 'Remember window' ('ED11') 'bool' -- 'Restore the last size and position for a source file window when you open it.' [mutable]
-#        property 'Remember selection' ('ED12') 'bool' -- 'Restore the previous selection in a file when you open it.' [mutable]
-#        property 'Save on update' ('ED13') 'bool' -- 'Save all editor windows automatically when you choose the Update command.' [mutable]
-#        property 'Projector aware' ('ED14') 'bool' -- "Honor MPW Projector\325s 'CKID' resources in text files." [mutable]
+#        property 'Flash delay' ('ED01') 'long' -- 'The amount of time, in sixtieths of a second, the editor highlights a matching bracket.' [mutable]
+#        property 'Dynamic scroll' ('ED02') 'bool' -- 'Display a window\325s contents as you move the scroll box.' [mutable]
+#        property 'Balance' ('ED03') 'bool' -- 'Flash the matching opening bracket when you type a closing bracket.' [mutable]
+#        property 'Use Drag & Drop Editing' ('ED04') 'bool' -- 'Use Drag & Drop text editing.' [mutable]
+#        property 'Save on update' ('ED05') 'bool' -- 'Save all editor windows automatically when you choose the Update command.' [mutable]
+#        property 'Sort Function Popup' ('ED06') 'bool' -- '' [mutable]
+#        property 'Use Multiple Undo' ('ED07') 'bool' -- '' [mutable]
+#        property 'Remember font' ('ED08') 'bool' -- 'Display a source file with its own font settings.' [mutable]
+#        property 'Remember selection' ('ED09') 'bool' -- 'Restore the previous selection in a file when you open it.' [mutable]
+#        property 'Remember window' ('ED10') 'bool' -- 'Restore the last size and position for a source file window when you open it.' [mutable]
+#        property 'Main Text Color' ('ED12') 'cRGB' -- 'Main, default, color for text.' [mutable]
+#        property 'Background Color' ('ED13') 'cRGB' -- 'Color of the background of editor windows.' [mutable]
+#        property 'Context Popup Delay' ('ED14') 'bool' -- 'The amount of time, in sixtieths of a second, before the context popup is displayed if you click and hold on a browser symbol.' [mutable]
+
+#    Class 'Syntax Coloring' ('SNTX') -- ''
+#        property 'Syntax coloring' ('GH01') 'bool' -- 'Mark keywords and comments with color.' [mutable]
+#        property 'Comment color' ('GH02') 'cRGB' -- 'The color for comments.' [mutable]
+#        property 'Keyword color' ('GH03') 'cRGB' -- 'The color for language keywords.' [mutable]
+#        property 'String color' ('GH04') 'cRGB' -- 'The color for strings.' [mutable]
+#        property 'Custom color 1' ('GH05') 'cRGB' -- 'The color for the first set of custom keywords.' [mutable]
+#        property 'Custom color 2' ('GH06') 'cRGB' -- 'The color for the second set custom keywords.' [mutable]
+#        property 'Custom color 3' ('GH07') 'cRGB' -- 'The color for the third set of custom keywords.' [mutable]
+#        property 'Custom color 4' ('GH08') 'cRGB' -- 'The color for the fourth set of custom keywords.' [mutable]
+
+#    Class 'Custom Keywords' ('CUKW') -- ''
+#        property 'Custom color 1' ('KW01') 'cRGB' -- 'The color for the first set of custom keywords.' [mutable]
+#        property 'Custom color 2' ('KW02') 'cRGB' -- 'The color for the second set custom keywords.' [mutable]
+#        property 'Custom color 3' ('KW03') 'cRGB' -- 'The color for the third set of custom keywords.' [mutable]
+#        property 'Custom color 4' ('KW04') 'cRGB' -- 'The color for the fourth set of custom keywords.' [mutable]
+
+#    Class 'Browser Coloring' ('BRKW') -- 'Colors for Browser symbols.'
+#        property 'Browser Keywords' ('BW00') 'bool' -- 'Mark Browser symbols with color.' [mutable]
+#        property 'Classes Color' ('BW01') 'cRGB' -- 'The color for classes.' [mutable]
+#        property 'Constants Color' ('BW02') 'cRGB' -- 'The color for constants.' [mutable]
+#        property 'Enums Color' ('BW03') 'cRGB' -- 'The color for enums.' [mutable]
+#        property 'Functions Color' ('BW04') 'cRGB' -- 'Set color for functions.' [mutable]
+#        property 'Globals Color' ('BW05') 'cRGB' -- 'The color for globals' [mutable]
+#        property 'Macros Color' ('BW06') 'cRGB' -- 'The color for macros.' [mutable]
+#        property 'Templates Color' ('BW07') 'cRGB' -- 'Set color for templates.' [mutable]
+#        property 'Typedefs Color' ('BW08') 'cRGB' -- 'The color for typedefs.' [mutable]
 
 #    Class 'Error Information' ('ErrM') -- 'Describes a single error or warning from the compiler or the linker.'
 #        property 'kind' ('ErrT') 'ErrT' -- 'The type of error or warning.' [enum]
 #        property 'message' ('ErrS') 'TEXT' -- 'The error or warning message.' []
 #        property 'disk file' ('file') 'fss ' -- 'The file where the error occurred.  May not be returned for certain kinds of errors (eg, link errors).' []
-#        property 'line' ('ErrL') 'long' -- 'The line in the file where the error occurred.  May not be returned for certain kinds of errors (eg, link errors).' []
+#        property 'lineNumber' ('ErrL') 'long' -- 'The line in the file where the error occurred.  May not be returned for certain kinds of errors (eg, link errors).' []
 
-#    Class 'Extras' ('EXTR') -- ''
+#    Class 'Extras' ('GXTR') -- ''
 #        property 'Completion sound' ('EX01') 'bool' -- 'Play a sound when finished a Bring Up To Date or Make command.' [mutable]
 #        property 'Success sound' ('EX02') 'TEXT' -- 'The sound CodeWarrior plays when it successfully finishes a Bring Up To Date or Make command.' [mutable]
 #        property 'Failure sound' ('EX03') 'TEXT' -- 'The sound CodeWarrior plays when it cannot finish a Bring Up To Date or Make command.' [mutable]
+#        property 'Full screen zoom' ('EX07') 'bool' -- 'Zoom windows to the full screen width.' [mutable]
+#        property 'External reference' ('EX08') 'RefP' -- 'Which on-line function reference to use.' [mutable enum]
+#        property 'Use Script Menu' ('EX12') 'bool' -- 'Controls the use of the AppleScript menu' [mutable]
+#        property 'Use Editor Extensions' ('EX10') 'bool' -- 'Controls the use of the Editor Extensions menu' [mutable]
+#        property 'Use External Editor' ('EX11') 'bool' -- 'Controls whether CodeWarrior uses its own integrated editor or an external application for editing text files.' [mutable]
+#        property 'Honor Projector State for Projects' ('EX13') 'bool' -- 'Controls whether CodeWarrior opens files set to read-only by Projector.' [mutable]
+
+#    Class 'Build Extras' ('LXTR') -- ''
+#        property 'Browser active' ('EX09') 'bool' -- 'Allow the collection of browser information.' [mutable]
 #        property 'Modification date caching' ('EX04') 'bool' -- '' [mutable]
 #        property 'Generate Map' ('EX05') 'bool' -- 'Generate a Pascal Make map file.' [mutable]
 #        property 'Store analysis results' ('EX06') 'bool' -- '' [mutable]
-#        property 'Full screen zoom' ('EX07') 'bool' -- 'Zoom windows to the full screen width.' [mutable]
-#        property 'External reference' ('EX08') 'RefP' -- 'Which on-line function reference to use.' [mutable enum]
-#        property 'Browser active' ('EX09') 'bool' -- 'Allow the collection of browser information.' [mutable]
 
 #    Class 'Font' ('mFNT') -- ''
 #        property 'Auto indent' ('FN01') 'bool' -- 'Indent new lines automatically.' [mutable]
-#        property 'Tab size' ('FN02') 'shor' -- '' []
+#        property 'Tab size' ('FN02') 'shor' -- '' [mutable]
 #        property 'Text font' ('ptxf') 'TEXT' -- 'The font used in editing windows.' [mutable]
 #        property 'Text size' ('ptps') 'shor' -- 'The size of the text in an editing window.' [mutable]
 
 #    Class 'Function Information' ('FDef') -- 'Describes the location of any function or global data definition within the current project.'
 #        property 'disk file' ('file') 'fss ' -- 'The location on disk of the file containing the definition.' []
-#        property 'line' ('ErrL') 'long' -- 'The line number where the definition begins.' []
+#        property 'lineNumber' ('ErrL') 'long' -- 'The line number where the definition begins.' []
 
 #    Class 'Path Information' ('PInf') -- 'Contains all of the parameters that describe an access path.'
 #        property 'name' ('pnam') 'TEXT' -- 'The actual path name.' [mutable]
@@ -800,6 +904,7 @@ class Metrowerks_Shell_Suite:
 #        property 'symbols' ('SymG') 'bool' -- 'Are debugging symbols generated for this file ?' [mutable]
 #        property 'weak link' ('Weak') 'bool' -- 'Is this file imported weakly into the project ? [PowerPC only]' [mutable]
 #        property 'initialize before' ('Bfor') 'bool' -- 'Intiailize the shared library before the main application.' [mutable]
+#        property 'includes' ('IncF') 'fss ' -- '' []
 
 #    Class 'Segment' ('Seg ') -- 'A segment or group in the project'
 #        property 'name' ('pnam') 'TEXT' -- '' [mutable]
@@ -822,3 +927,66 @@ class Metrowerks_Shell_Suite:
 #        property 'Launchable' ('TA05') 'bool' -- '' [mutable]
 #        property 'Ignored by Make' ('TA06') 'bool' -- '' [mutable]
 #        property 'Compiler' ('TA07') 'TEXT' -- '' [mutable]
+
+#    Class 'class' ('Clas') -- 'A class, struct, or record type in the current project'
+#        property 'name' ('pnam') 'TEXT' -- '' []
+#        property 'language' ('Lang') 'Lang' -- 'Implementation language of this class' [enum]
+#        property 'declaration file' ('DcFl') 'fss ' -- 'Source file containing the class declaration' []
+#        property 'declaration start offset' ('DcSt') 'long' -- 'Start of class declaration source code' []
+#        property 'declaration end offset' ('DcEn') 'long' -- 'End of class declaration' []
+#        property 'subclasses' ('SubC') 'Clas' -- 'the immediate subclasses of this class' [list]
+#        property 'all subclasses' ('SubA') 'Clas' -- 'the classes directly or indirectly derived from this class' [list]
+#        element 'BsCl' as ['indx']
+#        element 'MbFn' as ['indx', 'name']
+#        element 'DtMb' as ['indx', 'name']
+
+#    Class 'classes' ('Clas') -- ''
+#        property '' ('c@#!') 'type' -- '' [0]
+
+#    Class 'member function' ('MbFn') -- 'A class member function or method.'
+#        property 'name' ('pnam') 'TEXT' -- '' []
+#        property 'access' ('Acce') 'Acce' -- '' [enum]
+#        property 'virtual' ('Virt') 'bool' -- '' []
+#        property 'static' ('Stat') 'bool' -- '' []
+#        property 'declaration file' ('DcFl') 'fss ' -- 'Source file containing the member function declaration' []
+#        property 'declaration start offset' ('DcSt') 'long' -- 'start of member function declaration source code' []
+#        property 'declaration end offset' ('DcEn') 'long' -- 'end of member function declaration' []
+#        property 'implementation file' ('DfFl') 'fss ' -- 'Source file containing the member function definition' []
+#        property 'implementation start offset' ('DfSt') 'long' -- 'start of member function definition source code' []
+#        property 'implementation end offset' ('DfEn') 'long' -- 'end of member function definition' []
+
+#    Class 'member functions' ('MbFn') -- ''
+#        property '' ('c@#!') 'type' -- '' [0]
+
+#    Class 'data member' ('DtMb') -- 'A class data member or field'
+#        property 'name' ('pnam') 'TEXT' -- '' []
+#        property 'access' ('Acce') 'Acce' -- '' [enum]
+#        property 'static' ('Stat') 'bool' -- '' []
+#        property 'declaration start offset' ('DcSt') 'long' -- '' []
+#        property 'declaration end offset' ('DcEn') 'long' -- '' []
+
+#    Class 'data members' ('DtMb') -- ''
+#        property '' ('c@#!') 'type' -- '' [0]
+
+#    Class 'base class' ('BsCl') -- 'A base class or super class of a class'
+#        property 'class' ('Clas') 'obj ' -- 'The class object corresponding to this base class' []
+#        property 'access' ('Acce') 'Acce' -- '' [enum]
+#        property 'virtual' ('Virt') 'bool' -- '' []
+
+#    Class 'base classes' ('BsCl') -- ''
+#        property '' ('c@#!') 'type' -- '' [0 enum]
+
+#    Class 'browser catalog' ('Cata') -- 'The browser symbol catalog for the current project'
+#        element 'Clas' as ['indx', 'name']
+
+#    Class 'VCS Setup' ('VCSs') -- 'The version control system perferences.'
+#        property 'VCS Active' ('VC01') 'bool' -- 'Use Version Control' [mutable]
+#        property 'Connection Method' ('VC02') 'TEXT' -- 'Name of Version Control System to use.' [mutable]
+#        property 'Username' ('VC03') 'TEXT' -- 'The user name for the VCS.' [mutable]
+#        property 'Password' ('VC04') 'TEXT' -- 'The password for the VCS.' [mutable]
+#        property 'Auto Connect' ('VC05') 'bool' -- 'Automatically connect to database when starting.' [mutable]
+#        property 'Store Password' ('VC06') 'bool' -- 'Store the password.' [mutable]
+#        property 'Always Prompt' ('VC07') 'bool' -- 'Always show login dialog' [mutable]
+#        property 'Mount Volume' ('VC08') 'bool' -- "Attempt to mount the database volume if it isn't available." [mutable]
+#        property 'Database Path' ('VC09') 'PInf' -- 'Path to the VCS database.' [mutable]
+#        property 'Local Root' ('VC10') 'PInf' -- 'Path to the local directory to checkout to.' [mutable]
