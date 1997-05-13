2ec9031b5589db81026f5fc3a11070002ec4d0d7
Guido van Rossum <guido@python.org>
1997-05-13 21:23:32 +0000

    Subsumed mappingobject.h in dictobject.h.

--- Include/dictobject.h
+++ Include/dictobject.h
@@ -35,9 +35,27 @@ PERFORMANCE OF THIS SOFTWARE.
 
 ******************************************************************/
 
-/* All in the sake of backward compatibility... */
+/* Dictionary object type -- mapping from hashable object to object */
 
-#include "mappingobject.h"
+extern DL_IMPORT(PyTypeObject) PyDict_Type;
+
+#define PyDict_Check(op) ((op)->ob_type == &PyDict_Type)
+
+extern PyObject *PyDict_New Py_PROTO((void));
+extern PyObject *PyDict_GetItem Py_PROTO((PyObject *mp, PyObject *key));
+extern int PyDict_SetItem Py_PROTO((PyObject *mp, PyObject *key, PyObject *item));
+extern int PyDict_DelItem Py_PROTO((PyObject *mp, PyObject *key));
+extern void PyDict_Clear Py_PROTO((PyObject *mp));
+extern int PyDict_Next
+	Py_PROTO((PyObject *mp, int *pos, PyObject **key, PyObject **value));
+extern PyObject *PyDict_Keys Py_PROTO((PyObject *mp));
+extern PyObject *PyDict_Values Py_PROTO((PyObject *mp));
+extern PyObject *PyDict_Items Py_PROTO((PyObject *mp));
+extern int PyDict_Size Py_PROTO((PyObject *mp));
+
+extern PyObject *PyDict_GetItemString Py_PROTO((PyObject *dp, char *key));
+extern int PyDict_SetItemString Py_PROTO((PyObject *dp, char *key, PyObject *item));
+extern int PyDict_DelItemString Py_PROTO((PyObject *dp, char *key));
 
 #ifdef __cplusplus
 }
