72be30690f4b590527feb7fe3b93f1b8c8b54085
Guido van Rossum <guido@python.org>
1997-05-20 18:03:22 +0000

    Added some minimal comments and tweaked lay-out a bit.

--- Lib/copy_reg.py
+++ Lib/copy_reg.py
@@ -1,17 +1,21 @@
+# Helper to provide extensibility for pickle/cPickle.
+
 dispatch_table = {}
 safe_constructors = {}
 
 def pickle(ob_type, pickle_function, constructor_ob = None):
     dispatch_table[ob_type] = pickle_function
 
-    if (constructor_ob is not None):
+    if constructor_ob is not None:
         constructor(constructor_ob)
 
 def constructor(object):
     safe_constructors[object] = 1
 
-def pickle_complex(c):
-    return complex,(c.real, c.imag)
+# Example: provide pickling support for complex numbers.
 
-pickle(type(1j),pickle_complex,complex)
+def pickle_complex(c):
+    return complex, (c.real, c.imag)
+
+pickle(type(1j), pickle_complex, complex)
 
