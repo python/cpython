2283b5513ad866e519c492a060f41f91e411da7f
Guido van Rossum <guido@python.org>
1997-05-26 06:11:35 +0000

    Initial revision

---
+++ Tools/faqwiz/faqcust.py
@@ -0,0 +1,11 @@
+SHORTNAME = "Generic"
+PASSWORD = "Spam"
+SECTION_TITLES = {
+    1: "General information and availability",
+    2: "Python in the real world",
+    3: "Building Python and Other Known Bugs",
+    4: "Programming in Python",
+    5: "Extending Python",
+    6: "Python's design",
+    7: "Using Python on non-UNIX platforms",
+}
