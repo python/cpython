3c298983a33a840e7a62b9c347ffbb6c6b2f1f08
Guido van Rossum <guido@python.org>
1997-06-02 23:14:37 +0000

    The usual

--- Lib/dos_8x3/test_typ.py
+++ Lib/dos_8x3/test_typ.py
@@ -180,3 +180,12 @@ d['a'] = 4
 if d['c'] <> 3 or d['a'] <> 4: raise TestFailed, 'dict item assignment'
 del d['b']
 if d <> {'a': 4, 'c': 3}: raise TestFailed, 'dict item deletion'
+d = {1:1, 2:2, 3:3}
+d.clear()
+if d != {}: raise TestFailed, 'dict clear'
+d.absorb({1:100})
+d.absorb({2:20})
+d.absorb({1:1, 2:2, 3:3})
+if d != {1:1, 2:2, 3:3}: raise TestFailed, 'dict absorb'
+if d.copy() != {1:1, 2:2, 3:3}: raise TestFailed, 'dict copy'
+if {}.copy() != {}: raise TestFailed, 'empty dict copy'
