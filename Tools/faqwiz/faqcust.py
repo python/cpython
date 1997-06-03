9115eb522e1227c3b31483a4bcf4cdfb4d63ec8a
Guido van Rossum <guido@python.org>
1997-06-03 22:04:10 +0000

    Added a variant of the epilogue that has the PythonPowered logo.

--- Tools/faqwiz/faqcust.py
+++ Tools/faqwiz/faqcust.py
@@ -9,3 +9,15 @@ SECTION_TITLES = {
     6: "Python's design",
     7: "Using Python on non-UNIX platforms",
 }
+
+EPILOGUE = '''
+<HR>
+<A HREF="%(HOMEURL)s">%(HOMENAME)s</A> /
+<A HREF="%(FAQCGI)s?req=home">%(FAQNAME)s Wizard %(WIZVERSION)s</A> /
+Feedback to <A HREF="mailto:%(OWNEREMAIL)s">%(OWNERNAME)s</A>
+<P><A HREF="http://www.python.org/"><IMG
+SRC="http://www.python.org/pics/PythonPowered1.gif" 
+ALIGN=top WIDTH=100 HEIGHT=40 ALT="Python Powered" BORDER=0></A><BR>
+</BODY>
+</HTML>
+'''
