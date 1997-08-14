a8a90075dcd03af9cc8b98216498fea66b8fa8d6
Guido van Rossum <guido@python.org>
1997-08-14 14:15:54 +0000

    Add dummies for create/delete filehandles, just so that vanilla Grail 0.3 won't break on Windows.

--- Lib/tkinter/Tkinter.py
+++ Lib/tkinter/Tkinter.py
@@ -15,6 +15,12 @@ TclVersion = _string.atof(_tkinter.TCL_VERSION)
 READABLE = _tkinter.READABLE
 WRITABLE = _tkinter.WRITABLE
 EXCEPTION = _tkinter.EXCEPTION
+
+# These are not always defined, e.g. not on Win32 with Tk 8.0 :-(
+try: _tkinter.createfilehandler
+except AttributeError: _tkinter.createfilehandler = None
+try: _tkinter.deletefilehandler
+except AttributeError: _tkinter.deletefilehandler = None
     
     
 def _flatten(tuple):
