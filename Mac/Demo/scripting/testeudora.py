2bc5c797bef2f601f4cf2e706291a425dc6bb914
Jack Jansen <jack.jansen@cwi.nl>
1997-08-27 14:07:37 +0000

    Modified for new scripting support

--- Mac/Demo/scripting/testeudora.py
+++ Mac/Demo/scripting/testeudora.py
@@ -4,10 +4,11 @@ import sys
 import aetools
 import Eudora_Suite
 import Required_Suite
+import Standard_Suite
 import MacOS
 
-class Eudora(aetools.TalkTo, Required_Suite.Required_Suite, \
-				Eudora_Suite.Eudora_Suite):
+class Eudora(Eudora_Suite.Eudora_Suite, Required_Suite.Required_Suite, \
+				Standard_Suite.Standard_Suite, aetools.TalkTo):
 	"""A class that can talk to Eudora"""
 	pass
 	
@@ -17,13 +18,19 @@ SIGNATURE="CSOm"
 def main():
 	talker = Eudora(SIGNATURE, start=1)
 	while 1:
-		print 'get, put, quit (eudora) or exit (this program) ?'
+		print 'get, put, name (of first folder), list (foldernames), quit (eudora) or exit (this program) ?'
 		line = sys.stdin.readline()
 		try:
 			if line[0] == 'g':
 				talker.connect(checking=1)
 			elif line[0] == 'p':
 				talker.connect(sending=1)
+			elif line[0] == 'n':
+				id = Eudora_Suite.mailbox(1).name
+				name = talker._get(id, as='text')
+				print "It is called", name
+			elif line[0] == 'l':
+				pass
 			elif line[0] == 'q':
 				talker.quit()
 			elif line[0] == 'e':
