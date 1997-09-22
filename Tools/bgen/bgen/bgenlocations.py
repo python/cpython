03be7f525e38306910fc2e5de28ff03cb74bb1b7
Guido van Rossum <guido@python.org>
1997-09-22 16:13:19 +0000

    Changes by jack so the generated modules export their types to Python.

--- Tools/bgen/bgen/bgenlocations.py
+++ Tools/bgen/bgen/bgenlocations.py
@@ -3,10 +3,11 @@
 #
 
 # Where to find the Universal Header include files:
-INCLUDEDIR="Sap:CW8 Gold:Metrowerks CodeWarrior:MacOS Support:Headers:Universal Headers:"
+MWERKSDIR="Moes:Metrowerks:Metrowerks CodeWarrior:"
+INCLUDEDIR=MWERKSDIR + "MacOS Support:Headers:Universal Headers:"
 
 # Where to put the python definitions file:
-TOOLBOXDIR="Moes:Development:Jack:Python 1.3:Mac:Lib:toolbox:"
+TOOLBOXDIR="Moes:Development:Jack:Python:Mac:Lib:toolbox:"
 
 # Creator for C files:
 CREATOR="CWIE"
