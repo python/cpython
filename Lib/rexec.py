426916e50e1209d8ecc12678855dc531863a48c5
Guido van Rossum <guido@python.org>
1997-10-22 20:56:32 +0000

    Add pcre to the list of safe modules.

--- Lib/rexec.py
+++ Lib/rexec.py
@@ -133,7 +133,7 @@ class RExec(ihooks._Verbose):
     ok_builtin_modules = ('audioop', 'array', 'binascii',
 			  'cmath', 'errno', 'imageop',
 			  'marshal', 'math', 'md5', 'operator',
-			  'parser', 'regex', 'rotor', 'select',
+			  'parser', 'regex', 'pcre', 'rotor', 'select',
 			  'strop', 'struct', 'time')
 
     ok_posix_names = ('error', 'fstat', 'listdir', 'lstat', 'readlink',
