76e448de6ba0228b6fd06ef6c89a4c949156e3ee
Guido van Rossum <guido@python.org>
1997-12-10 05:50:18 +0000

    Adding a new project: pythonw, or WinMain.c, containing a main program that doesn't have a console window attached.  stdout/stderr are lost. This is handy though for things like grail.

---
+++ PC/WinMain.c
@@ -0,0 +1,15 @@
+/* Minimal main program -- everything is loaded from the library */
+
+#include <windows.h>
+
+extern int Py_Main();
+
+int WINAPI WinMain(
+    HINSTANCE hInstance,      // handle to current instance
+    HINSTANCE hPrevInstance,  // handle to previous instance
+    LPSTR lpCmdLine,          // pointer to command line
+    int nCmdShow              // show state of window
+)
+{
+	return Py_Main(__argc, __argv);
+}
