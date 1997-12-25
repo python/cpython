7267b93660e7c95005b356bc32088b46bf994e97
Guido van Rossum <guido@python.org>
1997-12-25 04:51:41 +0000

    Just for fun, add a static module, "xyzzy" -- show that calling its initxyzzy() works.

--- Demo/embed/demo.c
+++ Demo/embed/demo.c
@@ -4,6 +4,8 @@
 
 static char *argv0;
 
+void initxyzzy(); /* Forward */
+
 main(argc, argv)
 	int argc;
 	char **argv;
@@ -13,6 +15,9 @@ main(argc, argv)
 
 	/* Initialize the Python interpreter.  Required. */
 	Py_Initialize();
+
+	/* Add a static module */
+	initxyzzy();
 
 	/* Define sys.argv.  It is up to the application if you
 	   want this; you can also let it undefined (since the Python 
@@ -26,6 +31,7 @@ main(argc, argv)
 	/* Execute some Python statements (in module __main__) */
 	PyRun_SimpleString("import sys\n");
 	PyRun_SimpleString("print sys.builtin_module_names\n");
+	PyRun_SimpleString("print sys.modules.keys()\n");
 	PyRun_SimpleString("print sys.argv\n");
 
 	/* Note that you can call any public function of the Python
@@ -44,4 +50,28 @@ char *
 getprogramname()
 {
 	return argv0;
+}
+
+/* A static module */
+
+static PyObject *
+xyzzy_foo(self, args)
+	PyObject *self; /* Not used */
+	PyObject *args;
+{
+	if (!PyArg_ParseTuple(args, ""))
+		return NULL;
+	return PyInt_FromLong(42L);
+}
+
+static PyMethodDef xyzzy_methods[] = {
+	{"foo",		xyzzy_foo,	1},
+	{NULL,		NULL}		/* sentinel */
+};
+
+void
+initxyzzy()
+{
+	PyImport_AddModule("xyzzy");
+	Py_InitModule("xyzzy", xyzzy_methods);
 }
