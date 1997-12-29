6ccaaf1a0857db8d836f6c9bd6214a6882d9ca9e
Fred Drake <fdrake@acm.org>
1997-12-29 16:58:46 +0000

    Change name of temporary file to avoid possible filesystem issues; matches similar renaming of the @buildno file elsewhere in the Python tree.

--- Doc/tools/fix.el
+++ Doc/tools/fix.el
@@ -1,5 +1,5 @@
 ; load the new texinfo package (2.xx) if not installed by default
 ; (setq load-path (cons "/ufs/guido/lib/emacs/texinfo-2.14" load-path))
-(find-file "@lib.texi")
+(find-file "lib1.texi")
 (texinfo-all-menus-update t)
 (texinfo-all-menus-update t)
