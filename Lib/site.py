cb186ef90dcc874d5d6e446d6dda64b21ad7eabf
Guido van Rossum <guido@python.org>
1998-01-13 18:32:40 +0000

    Update the doc string to emphasize non-Unix behavior earlier; sys.prefix is no longer empty on any platform that I know of.

--- Lib/site.py
+++ Lib/site.py
@@ -10,9 +10,11 @@ somewhere near the top of their code.  Because of the automatic
 import, this is no longer necessary (but code that does it still
 works).
 
-This will append site-specific paths to to the module search path.  It
-starts with sys.prefix and sys.exec_prefix (if different) and appends
-lib/python<version>/site-packages as well as lib/site-python.  The
+This will append site-specific paths to to the module search path.  On
+Unix, it starts with sys.prefix and sys.exec_prefix (if different) and
+appends lib/python<version>/site-packages as well as lib/site-python.
+On other platforms (mainly Mac and Windows), it uses just sys.prefix
+(and sys.exec_prefix, if different, but this is unlikely).  The
 resulting directories, if they exist, are appended to sys.path, and
 also inspected for path configuration files.
 
@@ -52,10 +54,6 @@ After these path manipulations, an attempt is made to import a module
 named sitecustomize, which can perform arbitrary additional
 site-specific customizations.  If this import fails with an
 ImportError exception, it is silently ignored.
-
-Note that for some non-Unix systems, sys.prefix and sys.exec_prefix
-are empty, and then the path manipulations are skipped; however the
-import of sitecustomize is still attempted.
 
 """
 
