c2368810d5cadd22449cb1152c3b3eccb3b8a386
Barry Warsaw <barry@python.org>
1998-01-27 03:19:00 +0000

    Initial revision

---
+++ Tools/pynche/Main.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python
+
+"""Pynche: The PYthon Natural Color and Hue Editor.
+
+Pynche is based largely on a similar color editor I wrote years ago for the
+Sunview window system.  That editor was called ICE: the Interactive Color
+Editor.  I'd always wanted to port the editor to X but didn't feel like
+hacking X and C code to do it.  Fast forward many years, to where Python +
+Tkinter + Pmw provides such a nice programming environment, with enough
+power, that I finally buckled down and implemented it.  I changed the name
+because these days, too many other systems have the acronym `ICE'.
+
+Usage: %(PROGRAM) [-h]
+
+Where:
+    --help
+    -h
+        print this message
+
+"""
+
+__version__ = '1.0'
+
