2e965add309265beed1a946fff12674a8b86d764
Barry Warsaw <barry@python.org>
1998-02-13 21:29:13 +0000

    __validate(): test for None-age, not not-ness

--- Tools/pynche/TypeinViewer.py
+++ Tools/pynche/TypeinViewer.py
@@ -84,7 +84,7 @@ class TypeinWidget(Pmw.MegaWidget):
 
     def __validate(self, text):
 	val = self.__str_to_int(text)
-	if val and val >= 0 and val < 256:
+	if (val is not None) and (val >= 0) and (val < 256):
 	    return 1
 	else:
 	    return -1
@@ -96,7 +96,7 @@ class TypeinWidget(Pmw.MegaWidget):
 	rgbs = map(self.__str_to_int, vals)
 	valids = map(self.__validate, vals)
 	delegate = self['delegate']
-	if None not in rgbs and -1 not in valids and delegate:
+	if (None not in rgbs) and (-1 not in valids) and delegate:
 	    delegate.set_color(self, rgbs)
 
     # called whenever the color option is changed
