8a1b2cfe6fa8cb747eca37777a5d5b6f8798d82e
Fred Drake <fdrake@acm.org>
1998-04-13 21:55:56 +0000

    Update to use the new release number.  Will generalize in the next round.

--- Doc/tools/mktarball.sh
+++ Doc/tools/mktarball.sh
@@ -12,30 +12,30 @@
 #
 #  with [tag]:  generate from the named tag
 
-VERSION=1.5
+RELEASE=1.5.1
 
 TEMPDIR=tmp.$$
 MYDIR=`pwd`
 
 TAG="$1"
 
-mkdirhier $TEMPDIR/Python-$VERSION/Doc || exit $?
+mkdirhier $TEMPDIR/Python-$RELEASE/Doc || exit $?
 if [ "$TAG" ] ; then
-    cvs export -r $TAG -d $TEMPDIR/Python-$VERSION/Doc python/dist/src/Doc \
+    cvs export -r $TAG -d $TEMPDIR/Python-$RELEASE/Doc python/dist/src/Doc \
      || exit $?
 else
-    cvs checkout -d $TEMPDIR/Python-$VERSION/Doc python/dist/src/Doc || exit $?
+    cvs checkout -d $TEMPDIR/Python-$RELEASE/Doc python/dist/src/Doc || exit $?
     rm -r `find $TEMPDIR -name CVS -print` || exit $?
 fi
 
-rm -f `find $TEMPDIR/Python-$VERSION -name .cvsignore -print`
+rm -f `find $TEMPDIR/Python-$RELEASE -name .cvsignore -print`
 
-rm -f $TEMPDIR/Python-$VERSION/Doc/ref/ref.pdf
-rm -f $TEMPDIR/Python-$VERSION/Doc/ref/ref.ps
+rm -f $TEMPDIR/Python-$RELEASE/Doc/ref/ref.pdf
+rm -f $TEMPDIR/Python-$RELEASE/Doc/ref/ref.ps
 
 cd $TEMPDIR
 
-(tar cf - Python-$VERSION | gzip -9 >$MYDIR/latex-$VERSION.tar.gz) || exit $?
+(tar cf - Python-$RELEASE | gzip -9 >$MYDIR/latex-$RELEASE.tar.gz) || exit $?
 cd $MYDIR
 rm -r $TEMPDIR || exit $?
 
