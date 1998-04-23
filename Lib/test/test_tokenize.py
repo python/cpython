e26132cf5e40352f81027f8ab717a45a66cd6367
Guido van Rossum <guido@python.org>
1998-04-23 20:13:30 +0000

    Move unified findfile() into test_support.py

--- Lib/test/test_tokenize.py
+++ Lib/test/test_tokenize.py
@@ -1,17 +1,5 @@
-from test_support import verbose
+from test_support import verbose, findfile
 import tokenize, os, sys
-
-def findfile(file):
-	if os.path.isabs(file): return file
-	path = sys.path
-	try:
-	    path = [os.path.dirname(__file__)] + path
-	except NameError:
-	    pass
-	for dn in path:
-		fn = os.path.join(dn, file)
-		if os.path.exists(fn): return fn
-	return file
 
 if verbose:
     print 'starting...'
