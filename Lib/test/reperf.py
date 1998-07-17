f6fe6d618df886fe0c34486f6a9bc323d1b6a6eb
Guido van Rossum <guido@python.org>
1998-07-17 21:10:42 +0000

    Measure performance of sub(), split(), findall().

---
+++ Lib/test/reperf.py
@@ -0,0 +1,23 @@
+import re
+import time
+
+def main():
+    s = "\13hello\14 \13world\14 " * 1000
+    p = re.compile(r"([\13\14])")
+    timefunc(10, p.sub, "", s)
+    timefunc(10, p.split, s)
+    timefunc(10, p.findall, s)
+
+def timefunc(n, func, *args, **kw):
+    t0 = time.clock()
+    try:
+        for i in range(n):
+            result = apply(func, args, kw)
+        return result
+    finally:
+        t1 = time.clock()
+        if n > 1:
+            print n, "times",
+        print func.__name__, "%.3f" % (t1-t0), "CPU seconds"
+
+main()
