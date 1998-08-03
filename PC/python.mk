19d9483fb28aac73e9c516c4793bb3d5ae582ba3
Guido van Rossum <guido@python.org>
1998-08-03 20:23:42 +0000

    Jim A's new versions of these

--- PC/python.mk
+++ PC/python.mk
@@ -1,5 +1,5 @@
-project : n:\python\python1.4beta3\pc\wat_os2\pyth_os2.exe n:\python\python1&
-.4beta3\pc\wat_dos\pyth_dos.exe .SYMBOLIC
+project : n:\python\python-1.5.1\pc\wat_os2\pyth_os2.exe n:\python\python-1.&
+5.1\pc\wat_dos\pyth_dos.exe .SYMBOLIC
 
-!include n:\python\python1.4beta3\pc\wat_os2\pyth_os2.mk1
-!include n:\python\python1.4beta3\pc\wat_dos\pyth_dos.mk1
+!include n:\python\python-1.5.1\pc\wat_os2\pyth_os2.mk1
+!include n:\python\python-1.5.1\pc\wat_dos\pyth_dos.mk1
