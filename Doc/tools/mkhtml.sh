dca192f9fbb82b63f41326b44c0cdb2f74f490db
Fred Drake <fdrake@acm.org>
1998-08-11 14:10:40 +0000

    Add a comment explaining that this script is probably not useful outside the standard Python documentation, since the mkhowto.sh script is.  Let there be no doubts....

--- Doc/tools/mkhtml.sh
+++ Doc/tools/mkhtml.sh
@@ -1,9 +1,11 @@
 #! /bin/sh
 #
-#  Drive HTML generation for a Python manual.
+# Drive HTML generation for a Python manual.
 #
-#  The first arg is required and is the designation for which manual to build;
-#  api, ext, lib, ref, or tut.  All other args are passed on to latex2html.
+# This is probably *not* useful outside of the standard Python documentation.
+#
+# The first arg is required and is the designation for which manual to build;
+# api, ext, lib, ref, or tut.  All other args are passed on to latex2html.
 
 WORKDIR=`pwd`
 cd `dirname $0`/..
@@ -19,7 +21,7 @@ if [ -d $part ] ; then
     rm -f $part/*.html
 fi
 
-echo "latex2html -init_file $srcdir/perl/l2hinit.perl -dir $part " \
+echo "latex2html -init_file $srcdir/perl/l2hinit.perl -dir $part" \
  "${1:+$@} $srcdir/$part/$part.tex"
 latex2html \
  -init_file $srcdir/perl/l2hinit.perl \
