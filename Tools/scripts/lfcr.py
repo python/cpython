6e8a052960f0ff1d9128add0d4785ded8221eaa4
Guido van Rossum <guido@python.org>
1998-09-14 15:46:15 +0000

    Utility to replace LF with CRLF in argument files.

---
+++ Tools/scripts/lfcr.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python
+
+"Replace LF with CRLF in argument files.  Print names of changed files."
+
+import sys, regsub, os
+for file in sys.argv[1:]:
+    if os.path.isdir(file):
+	print file, "Directory!"
+	continue
+    data = open(file, "rb").read()
+    if '\0' in data:
+	print file, "Binary!"
+	continue
+    newdata = regsub.gsub("\r?\n", "\r\n", data)
+    if newdata != data:
+	print file
+	f = open(file, "wb")
+	f.write(newdata)
+	f.close()
