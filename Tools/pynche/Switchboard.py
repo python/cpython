a7ba45b7b6e64b4183b3ee3a3848f43440615248
Barry Warsaw <barry@python.org>
1998-10-01 16:46:43 +0000

    update_views_current(): New method

--- Tools/pynche/Switchboard.py
+++ Tools/pynche/Switchboard.py
@@ -2,13 +2,22 @@ class Switchboard:
     def __init__(self, colordb):
         self.__views = []
         self.__colordb = colordb
+        self.__red = 0
+        self.__green = 0
+        self.__blue = 0
 
     def add_view(self, view):
         self.__views.append(view)
 
     def update_views(self, red, green, blue):
+        self.__red = red
+        self.__green = green
+        self.__blue = blue
         for v in self.__views:
             v.update_yourself(red, green, blue)
+
+    def update_views_current(self):
+        self.update_views(self.__red, self.__green, self.__blue)
 
     def colordb(self):
         return self.__colordb
