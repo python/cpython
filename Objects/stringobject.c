07d780089d96c228bbf5dfb3c07bc926b7d5eaa1
Guido van Rossum <guido@python.org>
1998-10-01 15:59:48 +0000

    Typo reported by Greg Stein: "modifiable" is the correct spelling.

--- Objects/stringobject.c
+++ Objects/stringobject.c
@@ -479,7 +479,7 @@ string_buffer_getwritebuf(self, index, ptr)
 	const void **ptr;
 {
 	PyErr_SetString(PyExc_TypeError,
-			"Cannot use string as modifyable buffer");
+			"Cannot use string as modifiable buffer");
 	return -1;
 }
 
