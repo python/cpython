d832f9e4253c5ca2a7660725b12697b4619e3716
Guido van Rossum <guido@python.org>
1998-10-07 13:18:17 +0000

    Fix append() calls with more than 1 argument.

--- Lib/shutil.py
+++ Lib/shutil.py
@@ -123,5 +123,5 @@ def _build_cmdtuple(path, cmdtuples):
         if os.path.isdir(real_f) and not os.path.islink(real_f):
             _build_cmdtuple(real_f, cmdtuples)
         else:
-            cmdtuples.append(os.remove, real_f)
-    cmdtuples.append(os.rmdir, path)
+            cmdtuples.append((os.remove, real_f))
+    cmdtuples.append((os.rmdir, path))
