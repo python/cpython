7e25fb600543befb6c4201363c1a64e6abed7d2f
Fred Drake <fdrake@acm.org>
1998-10-21 14:26:26 +0000

    Added --tools option, to package only the framework (no documents).

--- Doc/tools/mktarball.sh
+++ Doc/tools/mktarball.sh
@@ -5,12 +5,21 @@
 #    from the Makefile or someplace.
 #
 #  usage:
-#	./mktarball.sh release [tag]
+#	./mktarball.sh [-t|--tools] release [tag]
+#
+#  with -t|--tools:  doesn't include the documents, only the framework
 #
 #  without [tag]:  generate from the current version that's checked in
 #		   (*NOT* what's in the current directory!)
 #
 #  with [tag]:  generate from the named tag
+
+#  VERSION='$Revision$'
+
+if [ "$1" = "-t" -o "$1" = "--tools" ] ; then
+    shift 1
+    TOOLS_ONLY=true
+fi
 
 RELEASE=$1; shift
 
@@ -33,9 +42,18 @@ rm -f `find $TEMPDIR -name .cvsignore -print`
 rm -f $TEMPDIR/Python-$RELEASE/Doc/ref/ref.pdf
 rm -f $TEMPDIR/Python-$RELEASE/Doc/ref/ref.ps
 
-cd $TEMPDIR
 
-(tar cf - Python-$RELEASE | gzip -9 >$MYDIR/latex-$RELEASE.tgz) || exit $?
+if [ "$TOOLS_ONLY" ] ; then
+    cd $TEMPDIR/Python-$RELEASE/Doc
+    # remove the actual documents
+    rm -rf api ext lib mac ref tut
+    cd ..
+    (tar cf - Doc | gzip -9 >$MYDIR/tools-$RELEASE.tgz) || exit $?
+else
+    cd $TEMPDIR
+
+    (tar cf - Python-$RELEASE | gzip -9 >$MYDIR/latex-$RELEASE.tgz) || exit $?
+fi
 cd $MYDIR
 rm -r $TEMPDIR || exit $?
 
