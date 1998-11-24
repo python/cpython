6942e57f87c5821b0f80aed3f7c1ddbe251e9a94
Fred Drake <fdrake@acm.org>
1998-11-24 17:38:49 +0000

    Script to convert things like <C/> and <Cpp/> to &C; and &Cpp;.  This is something that con't be done with the DOM as far as I can tell.

---
+++ Doc/tools/sgmlconv/fixgenents.sh
@@ -0,0 +1,25 @@
+#! /bin/sh
+#
+#  Script to fix general entities that got translated from the LaTeX as empty
+#  elements.  Mostly pretty bogus, but works like a charm!
+
+if [ "$1" ]; then
+    exec <"$1"
+    shift 1
+fi
+
+if [ "$1" ]; then
+    exec >"$1"
+    shift 1
+fi
+
+sed '
+s|<ABC/>|\&ABC;|g
+s|<ASCII/>|\&ASCII;|g
+s|<C/>|\&C;|g
+s|<Cpp/>|\&Cpp;|g
+s|<EOF/>|\&EOF;|g
+s|<NULL/>|\&NULL;|g
+s|<POSIX/>|\&POSIX;|g
+s|<UNIX/>|\&UNIX;|g
+' || exit $?
