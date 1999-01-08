c58f346374be696b6351a0b678fe5c3b33a7092c
Fred Drake <fdrake@acm.org>
1999-01-08 15:27:17 +0000

    New support scripts for HTML->info conversion that use Michael Ernst's new conversion tools.

--- Doc/tools/fix.el
+++
@@ -1,5 +0,0 @@
-; load the new texinfo package (2.xx) if not installed by default
-; (setq load-path (cons "/ufs/guido/lib/emacs/texinfo-2.14" load-path))
-(find-file "temp.texi")
-(texinfo-all-menus-update t)
-(texinfo-all-menus-update t)
