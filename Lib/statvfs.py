0bdbe6c5f0dffa72cb885a5a6bb60707c67013b2
Guido van Rossum <guido@python.org>
1999-01-10 06:09:13 +0000

    Added some more comments.

--- Lib/statvfs.py
+++ Lib/statvfs.py
@@ -3,18 +3,20 @@
 # Defines constants for interpreting statvfs struct as returned
 # by os.statvfs() and os.fstatvfs() (if they exist).
 #
+# Suggested usage: from statvfs import *
+#
 
-# Indices for statvfs struct members in tuple returned by
-# os.statvfs() and os.fstatvfs()
+# Indices for statvfs struct members in the tuple returned by
+# os.statvfs() and os.fstatvfs().
 
-F_BSIZE   = 0
-F_FRSIZE  = 1
-F_BLOCKS  = 2
-F_BFREE   = 3
-F_BAVAIL  = 4
-F_FILES   = 5
-F_FFREE   = 6
-F_FAVAIL  = 7
-F_FSID    = 8
-F_FLAG    = 9
-F_NAMEMAX = 10
+F_BSIZE   = 0		# Preferred file system block size
+F_FRSIZE  = 1		# Fundamental file system block size
+F_BLOCKS  = 2		# Total number of file system blocks (FRSIZE)
+F_BFREE   = 3		# Total number of free blocks
+F_BAVAIL  = 4		# Free blocks available to non-superuser
+F_FILES   = 5		# Total number of file nodes
+F_FFREE   = 6		# Total number of free file nodes
+F_FAVAIL  = 7		# Free nodes available to non-superuser
+F_FSID    = 8		# File system ID (meaning is system dependent)
+F_FLAG    = 9		# Flags (see your local statvfs man page)
+F_NAMEMAX = 10		# Maximum file name length
