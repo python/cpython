e784f1efece5df6c270bf6019ea8338a690c1795
Guido van Rossum <guido@python.org>
1999-02-15 14:43:11 +0000

    Add PyModule_GetFilename().

--- Include/moduleobject.h
+++ Include/moduleobject.h
@@ -44,6 +44,7 @@ extern DL_IMPORT(PyTypeObject) PyModule_Type;
 extern DL_IMPORT(PyObject *) PyModule_New Py_PROTO((char *));
 extern DL_IMPORT(PyObject *) PyModule_GetDict Py_PROTO((PyObject *));
 extern DL_IMPORT(char *) PyModule_GetName Py_PROTO((PyObject *));
+extern DL_IMPORT(char *) PyModule_GetFilename Py_PROTO((PyObject *));
 extern DL_IMPORT(void) _PyModule_Clear Py_PROTO((PyObject *));
 
 #ifdef __cplusplus
