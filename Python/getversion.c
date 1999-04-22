eda232fdac4db6b8fc90d2492b133b8fda40e82f
Guido van Rossum <guido@python.org>
1999-04-22 12:03:40 +0000

    Allow longer strings (up to 80 chars each) for version, build, compiler info.

--- Python/getversion.c
+++ Python/getversion.c
@@ -38,8 +38,8 @@ PERFORMANCE OF THIS SOFTWARE.
 const char *
 Py_GetVersion()
 {
-	static char version[100];
-	sprintf(version, "%.10s (%.40s) %.40s", PY_VERSION,
+	static char version[250];
+	sprintf(version, "%.80s (%.80s) %.80s", PY_VERSION,
 		Py_GetBuildInfo(), Py_GetCompiler());
 	return version;
 }
