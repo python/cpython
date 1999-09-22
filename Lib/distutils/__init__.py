dbe22d31dc87eafa919aa88775732e12c3d16437
Greg Ward <gward@python.net>
1999-09-22 15:24:04 +0000

    Added docstring and RCS id (apparently some Windows tar extractors ignore zero-byte files: grr...).

---
+++ Lib/distutils/__init__.py
@@ -0,0 +1,11 @@
+"""distutils
+
+The main package for the Python Module Distribtion Utilities.  Normally
+used from a setup script as
+
+   from distutils.core import setup
+
+   setup (...)
+"""
+
+__rcsid__ = "$Id$"
