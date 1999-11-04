e4f13660f80d1324f066b32a6b54fce7d875635c
Fred Drake <fdrake@acm.org>
1999-11-04 19:19:48 +0000

    split() docstring:  Made signature and description for the first                     parameter match.  Error pointed out by François                     Pinard <pinard@iro.umontreal.ca> on c.l.py.

--- Lib/string.py
+++ Lib/string.py
@@ -100,11 +100,11 @@ def rstrip(s):
 # Split a string into a list of space/tab-separated words
 # NB: split(s) is NOT the same as splitfields(s, ' ')!
 def split(s, sep=None, maxsplit=0):
-    """split(str [,sep [,maxsplit]]) -> list of strings
+    """split(s [,sep [,maxsplit]]) -> list of strings
 
     Return a list of the words in the string s, using sep as the
     delimiter string.  If maxsplit is nonzero, splits into at most
-    maxsplit words If sep is not specified, any whitespace string
+    maxsplit words.  If sep is not specified, any whitespace string
     is a separator.  Maxsplit defaults to 0.
 
     (split and splitfields are synonymous)
