e721f3ddd7409a870e0263e3c6a98900986b4d5f
Jack Jansen <jack.jansen@cwi.nl>
1999-12-24 13:41:31 +0000

    Modified for new dll import source organisation by Greg.

--- Mac/mwerks/mwerks_small_config.h
+++ Mac/mwerks/mwerks_small_config.h
@@ -19,7 +19,7 @@
 /* #define MAC_TCL			/* This *must* be on if USE_TK is on */
 /* #define USE_MAC_SHARED_LIBRARY	/* Enable code to add shared-library resources */
 /* #define USE_MAC_APPLET_SUPPORT	/* Enable code to run a PYC resource */
-/* #define USE_MAC_DYNAMIC_LOADING		/* Enable dynamically loaded modules */
+/* #define HAVE_DYNAMIC_LOADING		/* Enable dynamically loaded modules */
 #define USE_MALLOC_DEBUG			/* Enable range checking and other malloc debugging */
 /* #define USE_GDBM		/* Include the gdbm module */
 /* #define USE_ZLIB		/* Include the zlib module */
