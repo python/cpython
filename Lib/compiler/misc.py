95b8614353eee05f350c794a0aa37cab39794eed
Jeremy Hylton <jeremy@alum.mit.edu>
2000-02-10 20:54:27 +0000

    add an __len__ to Set and Stack

--- Lib/compiler/misc.py
+++ Lib/compiler/misc.py
@@ -1,6 +1,8 @@
 class Set:
     def __init__(self):
 	self.elts = {}
+    def __len__(self):
+        return len(self.elts)
     def add(self, elt):
 	self.elts[elt] = elt
     def items(self):
@@ -14,6 +16,8 @@ class Stack:
     def __init__(self):
 	self.stack = []
 	self.pop = self.stack.pop
+    def __len__(self):
+        return len(self.stack)
     def push(self, elt):
 	self.stack.append(elt)
     def top(self):
