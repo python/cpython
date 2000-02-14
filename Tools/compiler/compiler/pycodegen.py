9e1cd688b5dad9c337092949b5adb2e65fb6a7d3
Jeremy Hylton <jeremy@alum.mit.edu>
2000-02-14 23:57:56 +0000

    edit a doc string (real intent is to test out rsync install)

--- Tools/compiler/compiler/pycodegen.py
+++ Tools/compiler/compiler/pycodegen.py
@@ -121,7 +121,12 @@ class ASTVisitor:
             return meth(node)
 
 class ExampleASTVisitor(ASTVisitor):
-    """Prints examples of the nodes that aren't visited"""
+    """Prints examples of the nodes that aren't visited
+
+    This visitor-driver is only useful for development, when it's
+    helpful to develop a visitor incremently, and get feedback on what
+    you still have to do.
+    """
     examples = {}
     
     def dispatch(self, node):
