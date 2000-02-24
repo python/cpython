e7f61b4aad77d60c836df2d961904733ea96c6fe
Guido van Rossum <guido@python.org>
2000-02-24 02:26:51 +0000

    Mark Favas discovered this: getatime() accidentally returned the MTIME! This fixes PR#211.

--- Lib/posixpath.py
+++ Lib/posixpath.py
@@ -143,7 +143,7 @@ def getmtime(filename):
 def getatime(filename):
     """Return the last access time of a file, reported by os.stat()."""
     st = os.stat(filename)
-    return st[stat.ST_MTIME]
+    return st[stat.ST_ATIME]
 
 
 # Is a path a symbolic link?
