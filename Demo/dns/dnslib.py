01b3aa4d08085083c5ecc622ade790441083420f
Andrew M. Kuchling <amk@amk.ca>
2000-02-28 17:25:14 +0000

    Fix use of list.append() with multiple arguments

--- Demo/dns/dnslib.py
+++ Demo/dns/dnslib.py
@@ -101,7 +101,7 @@ class Packer:
 			label = list[j]
 			n = len(label)
 			if offset + len(buf) < 0x3FFF:
-				index.append(keys[j], offset + len(buf))
+				index.append( (keys[j], offset + len(buf)) )
 			else:
 				print 'dnslib.Packer.addname:',
 				print 'warning: pointer too big'
