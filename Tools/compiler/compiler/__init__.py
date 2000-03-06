02ac5ae9aefbf30f089b66210dea77eb94391f69
Jeremy Hylton <jeremy@alum.mit.edu>
2000-03-06 19:04:14 +0000

    add a doc string import some useful functions from contained modules

---
+++ Tools/compiler/compiler/__init__.py
@@ -0,0 +1,20 @@
+"""Package for parsing and compiling Python source code
+
+There are several functions defined at the top level that are imported
+from modules contained in the package.
+
+parse(buf) -> AST
+    Donverts a string containing Python source code to an abstract
+    syntax tree (AST).  The AST is defined in compiler.ast.
+
+parseFile(path) -> AST
+    The same as parse(open(path))
+
+walk(ast, visitor, verbose=None)
+    Does a pre-order walk over the ast using the visitor instance.
+    See compiler.visitor for details.
+"""
+
+from transformer import parse, parseFile
+from visitor import walk
+
