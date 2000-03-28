39e675b92c1324f640fdf4d06465e50cc3adeaea
Guido van Rossum <guido@python.org>
2000-03-28 01:58:50 +0000

    MBCS codecs for Windows.  Contributed by Mark Hammond.

---
+++ Lib/encodings/mbcs.py
@@ -0,0 +1,37 @@
+""" Python 'mbcs' Codec for Windows
+
+
+Cloned by Mark Hammond (mhammond@skippinet.com.au) from ascii.py,
+which was written by Marc-Andre Lemburg (mal@lemburg.com).
+
+(c) Copyright CNRI, All Rights Reserved. NO WARRANTY.
+
+"""
+import codecs
+
+### Codec APIs
+
+class Codec(codecs.Codec):
+
+    # Note: Binding these as C functions will result in the class not
+    # converting them to methods. This is intended.
+    encode = codecs.mbcs_encode
+    decode = codecs.mbcs_decode
+
+class StreamWriter(Codec,codecs.StreamWriter):
+    pass
+        
+class StreamReader(Codec,codecs.StreamReader):
+    pass
+
+class StreamConverter(StreamWriter,StreamReader):
+
+    encode = codecs.mbcs_decode
+    decode = codecs.mbcs_encode
+
+### encodings module API
+
+def getregentry():
+
+    return (Codec.encode,Codec.decode,StreamReader,StreamWriter)
+
