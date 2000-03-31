f5d59f39375c82100578fec92787512006fa828d
Guido van Rossum <guido@python.org>
2000-03-31 14:58:54 +0000

    Added Fredrik Lundh's sre module and its supporting cast.
    NOTE: THIS IS VERY ROUGH ALPHA CODE!

---
+++ Lib/sre.py
@@ -0,0 +1,46 @@
+# -*- Mode: Python; tab-width: 4 -*-
+#
+# Secret Labs' Regular Expression Engine
+# $Id$
+#
+# re-compatible interface for the sre matching engine
+#
+# Copyright (c) 1998-2000 by Secret Labs AB.  All rights reserved.
+#
+# This code can only be used for 1.6 alpha testing.  All other use
+# require explicit permission from Secret Labs AB.
+#
+# Portions of this engine have been developed in cooperation with
+# CNRI.  Hewlett-Packard provided funding for 1.6 integration and
+# other compatibility work.
+#
+
+"""
+this is a long string
+"""
+
+import sre_compile
+
+# --------------------------------------------------------------------
+# public interface
+
+def compile(pattern, flags=0):
+    return sre_compile.compile(pattern, _fixflags(flags))
+
+def match(pattern, string, flags=0):
+    return compile(pattern, _fixflags(flags)).match(string)
+
+def search(pattern, string, flags=0):
+    assert flags == 0
+    return compile(pattern, _fixflags(flags)).search(string)
+
+# FIXME: etc
+
+# --------------------------------------------------------------------
+# helpers
+
+def _fixflags(flags):
+    # convert flag bitmask to sequence
+    assert flags == 0
+    return ()
+
