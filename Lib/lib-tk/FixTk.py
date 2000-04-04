285514d6744d8f742fbc7c11fe97814cfb3dbc3f
Guido van Rossum <guido@python.org>
2000-04-04 23:32:21 +0000

    Work the Tcl version number in the path we search for.

--- Lib/lib-tk/FixTk.py
+++ Lib/lib-tk/FixTk.py
@@ -1,4 +1,5 @@
-import sys, os
-v = os.path.join(sys.prefix, "tcl", "tcl8.3")
+import sys, os, _tkinter
+ver = str(_tkinter.TCL_VERSION)
+v = os.path.join(sys.prefix, "tcl", "tcl"+ver)
 if os.path.exists(os.path.join(v, "init.tcl")):
     os.environ["TCL_LIBRARY"] = v
