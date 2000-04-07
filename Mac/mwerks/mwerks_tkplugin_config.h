037649eaa675e5c11ce51bd5e2be116710d336b0
Jack Jansen <jack.jansen@cwi.nl>
2000-04-07 15:40:59 +0000

    Enable multithreading.

--- Mac/mwerks/mwerks_tkplugin_config.h
+++ Mac/mwerks/mwerks_tkplugin_config.h
@@ -4,5 +4,6 @@
 
 #define USE_GUSI1		/* Stdio implemented with GUSI */
 /* #define USE_GUSI2		/* Stdio implemented with GUSI */
+#define WITH_THREAD		/* Use thread support (needs GUSI 2, not GUSI 1) */
 #define USE_TK			/* Include _tkinter module in core Python */
 #define MAC_TCL			/* This *must* be on if USE_TK is on */
