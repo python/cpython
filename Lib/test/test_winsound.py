f221eb29907b8b04949f62b4faddbf8a287ddbb4
Guido van Rossum <guido@python.org>
2000-04-21 21:28:47 +0000

    Added test_winsound by Mark Hammond.

---
+++ Lib/test/test_winsound.py
@@ -0,0 +1,7 @@
+# Rediculously simple test of the winsound module for Windows.
+
+import winsound
+for i in range(100, 2000, 100):
+    winsound.Beep(i, 75)
+print "Hopefully you heard some sounds increasing in frequency!"
+
