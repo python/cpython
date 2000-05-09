4fe2c483bb200ed13412d4c42fa0029690eb8931
Jack Jansen <jack.jansen@cwi.nl>
2000-05-09 10:02:30 +0000

    USE_GUSI2 should be defined, not USE_GUSI1.

--- Mac/mwerks/mwerks_plugin_config.h
+++ Mac/mwerks/mwerks_plugin_config.h
@@ -2,8 +2,8 @@
 ** Config file for dynamically-loaded ppc/cfm68k plugin modules.
 */
 
-#define USE_GUSI1		/* Stdio implemented with GUSI */
-/* #define USE_GUSI2		/* Stdio implemented with GUSI */
+/* #define USE_GUSI1		/* Stdio implemented with GUSI */
+#define USE_GUSI2		/* Stdio implemented with GUSI */
 #define WITH_THREAD		/* Use thread support (needs GUSI 2, not GUSI 1) */
 #define USE_MSL			/* Use MSL libraries */
 #ifdef USE_MSL
