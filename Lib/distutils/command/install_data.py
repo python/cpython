e4dabb6799ad5fcfb3ba15f2eac8e789fe5c4f01
Gregory P. Smith <greg@mad-scientist.com>
2000-05-12 01:34:33 +0000

    Caught up with renaming in 'install_misc' base class.

--- Lib/distutils/command/install_data.py
+++ Lib/distutils/command/install_data.py
@@ -8,7 +8,4 @@ class install_data (install_misc):
         self._install_dir_from('install_data')
 
     def run (self):
-        self._copydata(self.distribution.data)
-
-    def get_outputs (self):
-        return self._outputdata(self.distribution.data)
+        self._copy_files(self.distribution.data)
