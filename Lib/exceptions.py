262e83cb1ae1b1436a653c4dd5a519adce656af3
Barry Warsaw <barry@python.org>
2000-05-26 19:06:13 +0000

    This module is obsolete now that the standard exceptions are built-in.

--- Lib/exceptions.py
+++
@@ -1,4 +0,0 @@
-# Standard class-base exceptions are now all implemented in C.  This remains
-# for backwards compatibility with pre-1.6 releases.
-from _exceptions import *
-from _exceptions import __doc__
