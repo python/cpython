e7012d455da86bbdb4aec77a7ee647f73860e1c4
Jack Jansen <jack.jansen@cwi.nl>
2000-06-04 22:10:25 +0000

    Removed support for very old CodeWarrior runtimes.

--- Mac/mwerks/macuseshlstart.c
+++ Mac/mwerks/macuseshlstart.c
@@ -7,13 +7,6 @@
 
 #include <setjmp.h>
 
-#ifdef PRE_CW8
-extern jmp_buf __program_exit;			/*	exit() does a longjmp() to here		*/
-extern void (*__atexit_hook)(void);		/*	atexit()  sets this up if it is ever called	*/
-extern void (*___atexit_hook)(void);	/*	_atexit() sets this up if it is ever called	*/
-extern int __aborting;					/*	abort() sets this and longjmps to __program_exit	*/
-#endif
-
 /*
  *	clear_stackframe_backlink	-	set 0(SP) to 0
  *
@@ -30,20 +23,7 @@ void __start(void)
 {
 	char *argv = 0;
 	
-#ifdef PRE_CW8
-	if (setjmp(__program_exit) == 0) {	//	set up jmp_buf for exit()
-		main(0, &argv);				//	call main(argc, argv)
-		if (__atexit_hook)
-			__atexit_hook();			//	call atexit() procs
-	}
-	if (!__aborting) {
-		if (___atexit_hook)
-			___atexit_hook();			//	call _atexit() procs
-	}
-//	ExitToShell();
-#else
 	clear_stackframe_backlink();
 	main(0, &argv);
 	exit(0);
-#endif
 }
