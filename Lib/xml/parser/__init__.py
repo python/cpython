c6e72e0b457d75b99a3bedf952190459e6f8df0a
Fred Drake <fdrake@acm.org>
2000-06-29 20:02:38 +0000

    Typo.

--- Lib/xml/parser/__init__.py
+++ Lib/xml/parser/__init__.py
@@ -1,6 +1,6 @@
 """Python interfaces to XML parsers.
 
-This package contains on module:
+This package contains one module:
 
 expat -- Python wrapper for James Clark's Expat parser, with namespace
          support.
