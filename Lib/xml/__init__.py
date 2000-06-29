e85c350600efc74d6fc520d3b9bcb8270289151e
Fred Drake <fdrake@acm.org>
2000-06-29 19:28:01 +0000

    Package docstring.

---
+++ Lib/xml/__init__.py
@@ -0,0 +1,13 @@
+"""Core XML support for Python.
+
+This package contains three sub-packages:
+
+dom -- The W3C Document Object Model.  This supports DOM Level 1 +
+       Namespaces.
+
+parser -- Python wrappers for XML parsers (currently only supports Expat).
+
+sax -- The Simple API for XML, developed by XML-Dev, led by David
+       Megginson.  This supports the SAX 2 API.
+
+"""
---
+++ Lib/xml/dom/__init__.py
@@ -0,0 +1,10 @@
+"""W3C Document Object Model implementation for Python.
+
+The Python mapping of the Document Object Model is documented in <...>.
+
+This package contains the following modules:
+
+minidom -- A simple implementation of the Level 1 DOM with namespace
+           support added (based on the Level 2 specification).
+
+"""
