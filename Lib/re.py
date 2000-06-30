ef7bae6b2f7ba194dee32a04cc4d85675ce940b5
Fredrik Lundh <fredrik@pythonware.com>
2000-06-30 22:01:36 +0000

    replaced <some suitable mail address> with something slightly more suitable.

--- Lib/re.py
+++ Lib/re.py
@@ -1,11 +1,32 @@
-# change this to "pre" if your regexps stopped working.  don't
-# forget to send a bug report to <some suitable address>
+#
+# Minimal "re" compatibility wrapper
+#
+# If your regexps don't work well under 2.0b1, you can switch
+# to the old engine ("pre") down below.
+#
+# To help us fix any remaining bugs in the new engine, please
+# report what went wrong.  You can either use the following web
+# page:
+#
+#    http://www.python.org/search/search_bugs.html
+#
+# or send a mail to SRE's author:
+#
+#    Fredrik Lundh <effbot@telia.com>
+#
+# Make sure to include the pattern, the string SRE failed to
+# match, and what result you expected.
+#
+# thanks /F
+#
 
 engine = "sre"
+# engine = "pre"
 
 if engine == "sre":
-    # new 2.0 engine
+    # New unicode-aware engine
     from sre import *
 else:
-    # old 1.5.2 engine.  will be removed in 2.0 final.
+    # Old 1.5.2 engine.  This one supports 8-bit strings only,
+    # and will be removed in 2.0 final.
     from pre import *
