513d9ef81dbcece268cac0ebff65b8f352ca7f5a
Tim Peters <tim.peters@gmail.com>
2000-07-04 17:44:48 +0000

    Removed Py_PROTO and switched to ANSI C declarations in the dict implementation.  This was really to test whether my new CVS+SSH setup is more usable than the old one -- and turns out it is (for whatever reason, it was impossible to do a commit before that involved more than one directory).

--- Include/dictobject.h
+++ Include/dictobject.h
@@ -20,23 +20,23 @@ extern DL_IMPORT(PyTypeObject) PyDict_Type;
 
 #define PyDict_Check(op) ((op)->ob_type == &PyDict_Type)
 
-extern DL_IMPORT(PyObject *) PyDict_New Py_PROTO((void));
-extern DL_IMPORT(PyObject *) PyDict_GetItem Py_PROTO((PyObject *mp, PyObject *key));
-extern DL_IMPORT(int) PyDict_SetItem Py_PROTO((PyObject *mp, PyObject *key, PyObject *item));
-extern DL_IMPORT(int) PyDict_DelItem Py_PROTO((PyObject *mp, PyObject *key));
-extern DL_IMPORT(void) PyDict_Clear Py_PROTO((PyObject *mp));
+extern DL_IMPORT(PyObject *) PyDict_New(void);
+extern DL_IMPORT(PyObject *) PyDict_GetItem(PyObject *mp, PyObject *key);
+extern DL_IMPORT(int) PyDict_SetItem(PyObject *mp, PyObject *key, PyObject *item);
+extern DL_IMPORT(int) PyDict_DelItem(PyObject *mp, PyObject *key);
+extern DL_IMPORT(void) PyDict_Clear(PyObject *mp);
 extern DL_IMPORT(int) PyDict_Next
-	Py_PROTO((PyObject *mp, int *pos, PyObject **key, PyObject **value));
-extern DL_IMPORT(PyObject *) PyDict_Keys Py_PROTO((PyObject *mp));
-extern DL_IMPORT(PyObject *) PyDict_Values Py_PROTO((PyObject *mp));
-extern DL_IMPORT(PyObject *) PyDict_Items Py_PROTO((PyObject *mp));
-extern DL_IMPORT(int) PyDict_Size Py_PROTO((PyObject *mp));
-extern DL_IMPORT(PyObject *) PyDict_Copy Py_PROTO((PyObject *mp));
+	(PyObject *mp, int *pos, PyObject **key, PyObject **value);
+extern DL_IMPORT(PyObject *) PyDict_Keys(PyObject *mp);
+extern DL_IMPORT(PyObject *) PyDict_Values(PyObject *mp);
+extern DL_IMPORT(PyObject *) PyDict_Items(PyObject *mp);
+extern DL_IMPORT(int) PyDict_Size(PyObject *mp);
+extern DL_IMPORT(PyObject *) PyDict_Copy(PyObject *mp);
 
 
-extern DL_IMPORT(PyObject *) PyDict_GetItemString Py_PROTO((PyObject *dp, char *key));
-extern DL_IMPORT(int) PyDict_SetItemString Py_PROTO((PyObject *dp, char *key, PyObject *item));
-extern DL_IMPORT(int) PyDict_DelItemString Py_PROTO((PyObject *dp, char *key));
+extern DL_IMPORT(PyObject *) PyDict_GetItemString(PyObject *dp, char *key);
+extern DL_IMPORT(int) PyDict_SetItemString(PyObject *dp, char *key, PyObject *item);
+extern DL_IMPORT(int) PyDict_DelItemString(PyObject *dp, char *key);
 
 #ifdef __cplusplus
 }
