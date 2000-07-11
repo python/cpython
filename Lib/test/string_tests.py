f7f72df20a1bfbbabafabe9907aee99ca0d842fd
Jeremy Hylton <jeremy@alum.mit.edu>
2000-07-11 03:31:55 +0000

    add more tests of string.join variants to run_method_tests

--- Lib/test/string_tests.py
+++ Lib/test/string_tests.py
@@ -4,6 +4,8 @@ import string
 
 transtable = '\000\001\002\003\004\005\006\007\010\011\012\013\014\015\016\017\020\021\022\023\024\025\026\027\030\031\032\033\034\035\036\037 !"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`xyzdefghijklmnopqrstuvwxyz{|}~\177\200\201\202\203\204\205\206\207\210\211\212\213\214\215\216\217\220\221\222\223\224\225\226\227\230\231\232\233\234\235\236\237\240\241\242\243\244\245\246\247\250\251\252\253\254\255\256\257\260\261\262\263\264\265\266\267\270\271\272\273\274\275\276\277\300\301\302\303\304\305\306\307\310\311\312\313\314\315\316\317\320\321\322\323\324\325\326\327\330\331\332\333\334\335\336\337\340\341\342\343\344\345\346\347\350\351\352\353\354\355\356\357\360\361\362\363\364\365\366\367\370\371\372\373\374\375\376\377'
 
+from UserList import UserList
+
 class Sequence:
     def __init__(self): self.seq = 'wxyz'
     def __len__(self): return len(self.seq)
@@ -39,10 +41,7 @@ def run_module_tests(test):
     test('join', 7, TypeError)
 
     test('join', BadSeq1(), TypeError)
-    test('join', BadSeq2(), IndexError)
-#   XXX should this test really raise an exception; lots of other core
-#   code will just ignore the lying __len__ method. 
-#   test('join', BadSeq2(), 'a b c')
+    test('join', BadSeq2(), 'a b c')
 
     # try a few long ones
     print string.join(['x' * 100] * 100, ':')
@@ -117,12 +116,16 @@ def run_method_tests(test):
     test('join', ' ', 'a b c d', ['a', 'b', 'c', 'd'])
     test('join', '', 'abcd', ('a', 'b', 'c', 'd'))
     test('join', ' ', 'w x y z', Sequence())
+    test('join', 'a', 'abc', ('abc',))
+    test('join', 'a', 'z', UserList(['z']))
+    test('join', u'.', u'a.b.c', ['a', 'b', 'c'])
+    test('join', '.', u'a.b.c', [u'a', 'b', 'c'])
+    test('join', '.', u'a.b.c', ['a', u'b', 'c'])
+    test('join', '.', u'a.b.c', ['a', 'b', u'c'])
+    test('join', '.', TypeError, ['a', u'b', 3])
 
     test('join', ' ', TypeError, BadSeq1())
-    test('join', ' ', IndexError, BadSeq2())
-#   XXX should this test really raise an exception; lots of other core
-#   code will just ignore the lying __len__ method. 
-#   test('join', BadSeq2(), 'a b c')
+    test('join', ' ', 'a b c', BadSeq2())
 
     test('splitlines', "abc\ndef\n\rghi", ['abc', 'def', '', 'ghi'])
     test('splitlines', "abc\ndef\n\r\nghi", ['abc', 'def', '', 'ghi'])
