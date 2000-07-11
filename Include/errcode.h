3490c32cc076f26cce477f9f2a2de14dd7b9645f
Fred Drake <fdrake@acm.org>
2000-07-11 19:49:15 +0000

    Fix incomplete merge of Ping's SyntaxError enhancement patch (my fault). Comment change only.

--- Include/errcode.h
+++ Include/errcode.h
@@ -30,7 +30,7 @@ redistribution of this file, and for a DISCLAIMER OF ALL WARRANTIES.
 #define E_NOMEM		15	/* Ran out of memory */
 #define E_DONE		16	/* Parsing complete */
 #define E_ERROR		17	/* Execution error */
-#define E_TABSPACE	18	/* Invalid indentation detected */
+#define E_TABSPACE	18	/* Inconsistent mixing of tabs and spaces */
 #define E_OVERFLOW      19	/* Node had too many children */
 #define E_TOODEEP	20	/* Too many indentation levels */
 #define E_DEDENT	21	/* No matching outer block for dedent */
