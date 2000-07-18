ad4123512dbf903d17d077cd85e009c04c821348
Jack Jansen <jack.jansen@cwi.nl>
2000-07-18 09:43:27 +0000

    'carb' resource for carbon application.

---
+++ Mac/Resources/Carbon.r
@@ -0,0 +1,15 @@
+/*
+ *  Permit this Carbon application to launch on OS X
+ *
+ *  by Josef W. Wankerl
+ *  04/11/00
+ */
+
+
+/*----------------------------carb € Carbon on OS X launch information --------------------------*/
+type 'carb' {
+};
+
+
+resource 'carb'(0) {
+};
