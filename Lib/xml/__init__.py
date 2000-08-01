327e1878ac325c0654b01493ae3abb9f3f92fb2a
Fred Drake <fdrake@acm.org>
2000-08-01 00:05:16 +0000

    Add magic to replace the xml package with _xmlplus at import time. Update docstring to reflect change of name for the parsers subpackage.

--- Lib/xml/__init__.py
+++ Lib/xml/__init__.py
@@ -5,9 +5,19 @@ This package contains three sub-packages:
 dom -- The W3C Document Object Model.  This supports DOM Level 1 +
        Namespaces.
 
-parser -- Python wrappers for XML parsers (currently only supports Expat).
+parsers -- Python wrappers for XML parsers (currently only supports Expat).
 
 sax -- The Simple API for XML, developed by XML-Dev, led by David
        Megginson and ported to Python by Lars Marius Garshol.  This 
        supports the SAX 2 API.
 """
+
+
+if __name__ == "xml":
+    try:
+        import _xmlplus
+    except ImportError:
+        pass
+    else:
+        import sys
+        sys.modules[__name__] = _xmlplus
