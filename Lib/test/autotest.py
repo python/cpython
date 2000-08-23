a48b5267456bbc7cd252c4a88f8785495fdbf586
Tim Peters <tim.peters@gmail.com>
2000-08-23 05:28:45 +0000

    Rehabilitate autotest.py. In README:  Write up (Guido's) rules for intra-test imports; warn against asserts; document test_support.use_large_resources.

--- Lib/test/autotest.py
+++ Lib/test/autotest.py
@@ -1,3 +1,6 @@
-# Backward compatibility -- you should use regrtest instead of this module.
+# This should be equivalent to running regrtest.py from the cmdline.
+# It can be especially handy if you're in an interactive shell, e.g.,
+# from test import autotest.
+
 import regrtest
 regrtest.main()
