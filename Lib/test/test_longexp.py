33438d23faa55e14d09c671dfc71a6355b6a352d
Fred Drake <fdrake@acm.org>
2000-08-24 00:35:38 +0000

    Charles G. Waldman <cgq@fnal.gov>: Update the test suite for the changes introduced by the EXTENDED_ARG opcode.
    This closes the regression test changes of SourceForge patch #100893.

--- Lib/test/test_longexp.py
+++ Lib/test/test_longexp.py
@@ -1,18 +1,4 @@
-REPS = 8192
+REPS = 65580
 
-try:
-    eval("2+2+" * REPS + "+3.14159265")
-except SyntaxError, msg:
-    print "Caught SyntaxError for long expression:", msg
-else:
-    print "Long expression did not raise SyntaxError"
-
-## This test prints "s_push: parser stack overflow" on stderr,
-    ## which seems to confuse the test harness
-##try:
-##    eval("(2+" * REPS + "0" + ")" * REPS)
-##except SyntaxError:
-##    pass
-##else:
-##    print "Deeply nested expression did not raised SyntaxError"
-
+l = eval("[" + "2," * REPS + "]") 
+print len(l)  
