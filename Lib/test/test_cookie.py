db9708fb3a527c6db9d33853ecf72ae3a7e72a89
Andrew M. Kuchling <amk@amk.ca>
2000-08-24 11:56:19 +0000

    Updated test suite: test repr() and str() of cookies, and test metadata     fields with quoted values (as in Path="/acme")

--- Lib/test/test_cookie.py
+++ Lib/test/test_cookie.py
@@ -13,10 +13,12 @@ cases = [
 
 for data, dict in cases:
     C = Cookie.SimpleCookie() ; C.load(data)
-    print C
+    print repr(C)
+    print str(C)
     for k, v in dict.items():
         print ' ', k, repr( C[k].value ), repr(v)
         assert C[k].value == v
+        print C[k]
 
 C = Cookie.SimpleCookie()
 C.load('Customer="WILE_E_COYOTE"; Version=1; Path=/acme')
@@ -28,4 +30,11 @@ assert C['Customer']['path'] == '/acme'
 print C.output(['path'])
 print C.js_output()
 print C.js_output(['path'])
+
+# Try cookie with quoted meta-data
+C = Cookie.SimpleCookie()
+C.load('Customer="WILE_E_COYOTE"; Version="1"; Path="/acme"')
+assert C['Customer'].value == 'WILE_E_COYOTE'
+assert C['Customer']['version'] == '1'
+assert C['Customer']['path'] == '/acme'
 
