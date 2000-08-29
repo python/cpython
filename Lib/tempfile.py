952684af0c4685162a5dd7d89eec6f2ffe580f43
Guido van Rossum <guido@python.org>
2000-08-29 14:55:03 +0000

    Patch by tg@FreeBSD.org to try /var/tmp first. This helps on 4.4BSD-based systems.

--- Lib/tempfile.py
+++ Lib/tempfile.py
@@ -23,7 +23,7 @@ def gettempdir():
         pwd = os.getcwd()
     except (AttributeError, os.error):
         pwd = os.curdir
-    attempdirs = ['/usr/tmp', '/tmp', pwd]
+    attempdirs = ['/var/tmp', '/usr/tmp', '/tmp', pwd]
     if os.name == 'nt':
         attempdirs.insert(0, 'C:\\TEMP')
         attempdirs.insert(0, '\\TEMP')
