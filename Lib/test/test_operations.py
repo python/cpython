26fb1d8cb707d3fe472515a9217ede63f7e6c958
Fred Drake <fdrake@acm.org>
2000-08-31 19:48:52 +0000

    Test case to exercise fix for error propogation bug in dictionarys.

--- Lib/test/test_operations.py
+++ Lib/test/test_operations.py
@@ -2,4 +2,27 @@
 
 
 print '3. Operations'
-print 'XXX Not yet implemented'
+print 'XXX Mostly not yet implemented'
+
+
+print '3.1 Dictionary lookups succeed even if __cmp__() raises an exception'
+
+# SourceForge bug #112558:
+# http://sourceforge.net/bugs/?func=detailbug&bug_id=112558&group_id=5470
+
+class BadDictKey: 
+    def __hash__(self): 
+        return hash(self.__class__) 
+
+    def __cmp__(self, other): 
+        if isinstance(other, self.__class__): 
+            print "raising error" 
+            raise RuntimeError, "gotcha" 
+        return other 
+
+d = {}
+x1 = BadDictKey()
+x2 = BadDictKey()
+d[x1] = 1
+d[x2] = 2
+print "No exception passed through."
