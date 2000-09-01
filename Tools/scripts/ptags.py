8e1368be9fe79c6d71f05ace434fc5e048034aa9
Guido van Rossum <guido@python.org>
2000-09-01 13:41:37 +0000

    Rob Hooft, Moshe Zadka: converted to 4 space indents and re instead of regex.

--- Tools/scripts/ptags.py
+++ Tools/scripts/ptags.py
@@ -10,41 +10,42 @@
 # Warns about files it cannot open.
 # No warnings about duplicate tags.
 
-import sys
-import regex
-import os
+import sys, re, os
 
-tags = []	# Modified global variable!
+tags = []    # Modified global variable!
 
 def main():
-	args = sys.argv[1:]
-	for file in args: treat_file(file)
-	if tags:
-		fp = open('tags', 'w')
-		tags.sort()
-		for s in tags: fp.write(s)
+    args = sys.argv[1:]
+    for file in args: treat_file(file)
+    if tags:
+        fp = open('tags', 'w')
+        tags.sort()
+        for s in tags: fp.write(s)
 
 
-expr = '^[ \t]*\(def\|class\)[ \t]+\([a-zA-Z0-9_]+\)[ \t]*[:(]'
-matcher = regex.compile(expr)
+expr = '^[ \t]*(def|class)[ \t]+([a-zA-Z0-9_]+)[ \t]*[:\(]'
+matcher = re.compile(expr)
 
 def treat_file(file):
-	try:
-		fp = open(file, 'r')
-	except:
-		print 'Cannot open', file
-		return
-	base = os.path.basename(file)
-	if base[-3:] == '.py': base = base[:-3]
-	s = base + '\t' + file + '\t' + '1\n'
-	tags.append(s)
-	while 1:
-		line = fp.readline()
-		if not line: break
-		if matcher.search(line) >= 0:
-			(a, b), (a1, b1), (a2, b2) = matcher.regs[:3]
-			name = line[a2:b2]
-			s = name + '\t' + file + '\t/^' + line[a:b] + '/\n'
-			tags.append(s)
+    try:
+        fp = open(file, 'r')
+    except:
+        sys.stderr.write('Cannot open %s\n' % file)
+        return
+    base = os.path.basename(file)
+    if base[-3:] == '.py':
+        base = base[:-3]
+    s = base + '\t' + file + '\t' + '1\n'
+    tags.append(s)
+    while 1:
+        line = fp.readline()
+        if not line: 
+            break
+        m = matcher.match(line)
+        if m:
+            content = m.group(0)
+            name = m.group(2)
+            s = name + '\t' + file + '\t/^' + content + '/\n'
+            tags.append(s)
 
 main()
