feb22e332a8add39e0f193092392637519482049
Guido van Rossum <guido@python.org>
2000-09-04 01:22:12 +0000

    Use periods, not semicolons between Copyright and All Rights Reserved.

--- Python/getcopyright.c
+++ Python/getcopyright.c
@@ -3,10 +3,10 @@
 #include "Python.h"
 
 static char cprt[] = 
-"Copyright (c) 2000 BeOpen.com; All Rights Reserved.\n\
-Copyright (c) 1995-2000 Corporation for National Research Initiatives;\n\
+"Copyright (c) 2000 BeOpen.com.  All Rights Reserved.\n\
+Copyright (c) 1995-2000 Corporation for National Research Initiatives.\n\
 All Rights Reserved.\n\
-Copyright (c) 1991-1995 Stichting Mathematisch Centrum, Amsterdam;\n\
+Copyright (c) 1991-1995 Stichting Mathematisch Centrum, Amsterdam.\n\
 All Rights Reserved.";
 
 const char *
