c5027eef3e5d34f5b1180adeef94d0ae07e8c592
Greg Ward <gward@python.net>
2000-09-16 01:54:46 +0000

    Include the Python version in the platform-specific build directories: with the recent change in 'get_platform()', we now have directory names like "build/lib-1.5-linux-i586".  Idea and original patch by Rene Liebscher.

--- Lib/distutils/command/build.py
+++ Lib/distutils/command/build.py
@@ -67,6 +67,8 @@ class build (Command):
         # hardware architecture!
         self.plat = get_platform ()
 
+        plat_specifier = sys.version[0:3] + '-' + self.plat
+
         # 'build_purelib' and 'build_platlib' just default to 'lib' and
         # 'lib.<plat>' under the base build directory.  We only use one of
         # them for a given distribution, though --
@@ -74,7 +76,7 @@ class build (Command):
             self.build_purelib = os.path.join (self.build_base, 'lib')
         if self.build_platlib is None:
             self.build_platlib = os.path.join (self.build_base,
-                                               'lib.' + self.plat)
+                                               'lib-' + plat_specifier)
 
         # 'build_lib' is the actual directory that we will use for this
         # particular module distribution -- if user didn't supply it, pick
@@ -89,7 +91,7 @@ class build (Command):
         # "build/temp.<plat>"
         if self.build_temp is None:
             self.build_temp = os.path.join (self.build_base,
-                                            'temp.' + self.plat)
+                                            'temp-' + plat_specifier)
         if self.build_scripts is None:
             self.build_scripts = os.path.join (self.build_base, 'scripts')
     # finalize_options ()
