57a4e9092278b9393cb2ee0da7f3b8b19a47c6cf
Fred Drake <fdrake@acm.org>
2000-09-18 17:56:58 +0000

    Fix up the cleanup of the temporary DB so it works for BSD DB's compatibility layer as well as "classic" ndbm.

--- Lib/test/test_dbm.py
+++ Lib/test/test_dbm.py
@@ -6,7 +6,7 @@ import dbm
 from dbm import error
 from test_support import verbose
 
-filename= '/tmp/delete_me'
+filename = '/tmp/delete_me'
 
 d = dbm.open(filename, 'c')
 d['a'] = 'b'
@@ -15,7 +15,7 @@ d.keys()
 if d.has_key('a'):
     if verbose:
         print 'Test dbm keys: ', d.keys()
-        
+
 d.close()
 d = dbm.open(filename, 'r')
 d.close()
@@ -28,7 +28,15 @@ d.close()
 
 try:
     import os
-    os.unlink(filename + '.dir')
-    os.unlink(filename + '.pag')
+    if dbm.library == "ndbm":
+        # classic dbm
+        os.unlink(filename + '.dir')
+        os.unlink(filename + '.pag')
+    elif dbm.library == "BSD db":
+        # BSD DB's compatibility layer
+        os.unlink(filename + '.db')
+    else:
+        # GNU gdbm compatibility layer
+        os.unlink(filename)
 except:
     pass
