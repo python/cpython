c76bceafb4c9343408d1efebef3e921726289956
Jeremy Hylton <jeremy@alum.mit.edu>
2000-09-20 02:35:19 +0000

    distutils setup script

---
+++ Tools/compiler/setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name = "compiler",
+      version = "1.0",
+      author = "Jeremy Hylton",
+      author_email = "jeremy@beopen.com",
+      packages = ["compiler"])
