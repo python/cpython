8ecbe5e264bb91d15379461036cad0c3f52a6378
Greg Ward <gward@python.net>
2000-09-25 01:25:06 +0000

    Added 'strtobool()' function: convert strings like "yes", "1", "no", "0", etc. to true/false.

--- Lib/distutils/util.py
+++ Lib/distutils/util.py
@@ -273,3 +273,18 @@ def execute (func, args, msg=None, verbose=0, dry_run=0):
         apply(func, args)
 
 # execute()
+
+
+def strtobool (val):
+    """Convert a string representation of truth to true (1) or false (0).
+    True values are 'y', 'yes', 't', 'true', 'on', and '1'; false values
+    are 'n', 'no', 'f', 'false', 'off', and '0'.  Raises ValueError if
+    'val' is anything else.
+    """
+    val = string.lower(val)
+    if val in ('y', 'yes', 't', 'true', 'on', '1'):
+        return 1
+    elif val in ('n', 'no', 'f', 'false', 'off', '0'):
+        return 0
+    else:
+        raise ValueError, "invalid truth value %s" % `val`
