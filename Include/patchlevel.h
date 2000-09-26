39d59b47b70d557e0aec5c8f35ede0a7d73f4edc
Guido van Rossum <guido@python.org>
2000-09-26 00:36:03 +0000

    It's.....
    Python 2.0b2!
    
    (Note: Jeremy will finish the release on Sept. 26; I have to go on an
    unexpected business trip.)

--- Include/patchlevel.h
+++ Include/patchlevel.h
@@ -23,13 +23,13 @@
 #define PY_MINOR_VERSION	0
 #define PY_MICRO_VERSION	0
 #define PY_RELEASE_LEVEL	PY_RELEASE_LEVEL_BETA
-#define PY_RELEASE_SERIAL	1
+#define PY_RELEASE_SERIAL	2
 
 /* Version as a string */
-#define PY_VERSION		"2.0b1"
+#define PY_VERSION		"2.0b2"
 
 /* Historic */
-#define PATCHLEVEL		"2.0b1"
+#define PATCHLEVEL		"2.0b2"
 
 /* Version as a single 4-byte hex number, e.g. 0x010502B2 == 1.5.2b2.
    Use this for numeric comparisons, e.g. #if PY_VERSION_HEX >= ... */
