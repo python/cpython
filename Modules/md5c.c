a6debecce5ddf1251f2430c159c7dc644748a30e
Fred Drake <fdrake@acm.org>
2000-09-28 02:54:51 +0000

    Fix includes; not including Python.h caused the module not to compile on some platforms.
    This *should* close SourceForge patch #115506.

--- Modules/md5c.c
+++ Modules/md5c.c
@@ -23,7 +23,7 @@ These notices must be retained in any copies of any part of this
 documentation and/or software.
  */
 
-#include "config.h"
+#include "Python.h"
 #include "md5.h"
 
 /* Constants for MD5Transform routine. */
