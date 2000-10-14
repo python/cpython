4cd6f2ab807e0f3a23a059a407a08c5edad6bb98
Greg Ward <gward@python.net>
2000-10-14 03:56:42 +0000

    Bastian Kleineidam: make 'check_lib()' more like AC_CHECK_LIB by adding an 'other_libraries()' parameter.

--- Lib/distutils/command/config.py
+++ Lib/distutils/command/config.py
@@ -325,16 +325,19 @@ class config (Command):
     # check_func ()
 
     def check_lib (self, library, library_dirs=None,
-                   headers=None, include_dirs=None):
+                   headers=None, include_dirs=None, other_libraries=[]):
         """Determine if 'library' is available to be linked against,
         without actually checking that any particular symbols are provided
         by it.  'headers' will be used in constructing the source file to
         be compiled, but the only effect of this is to check if all the
-        header files listed are available.
+        header files listed are available.  Any libraries listed in
+        'other_libraries' will be included in the link, in case 'library'
+        has symbols that depend on other libraries.
         """
         self._check_compiler()
         return self.try_link("int main (void) { }",
-                             headers, include_dirs, [library], library_dirs)
+                             headers, include_dirs,
+                             [library]+other_libraries, library_dirs)
 
     def check_header (self, header, include_dirs=None,
                       library_dirs=None, lang="c"):
