f7ef1ab5a7121808dc58c8c9934747c6bd350578
Jeremy Hylton <jeremy@alum.mit.edu>
2000-10-16 17:33:50 +0000

    the usual

--- Lib/dos-8x3/test_min.py
+++ Lib/dos-8x3/test_min.py
@@ -15,157 +15,157 @@ else:
 tstfile = os.path.join(os.path.dirname(base), "test.xml")
 del base
 
-def confirm( test, testname="Test" ):
+def confirm(test, testname = "Test"):
     if test: 
         print "Passed " + testname
     else: 
         print "Failed " + testname
         raise Exception
 
-Node._debug=1
+Node._debug = 1
 
 def testParseFromFile():
     from StringIO import StringIO
-    dom=parse( StringIO( open( tstfile ).read() ) )
+    dom = parse(StringIO(open(tstfile).read()))
     dom.unlink()
     confirm(isinstance(dom,Document))
 
-def testGetElementsByTagName( ):
-    dom=parse( tstfile )
-    confirm( dom.getElementsByTagName( "LI" )==\
-            dom.documentElement.getElementsByTagName( "LI" ) )
+def testGetElementsByTagName():
+    dom = parse(tstfile)
+    confirm(dom.getElementsByTagName("LI") == \
+            dom.documentElement.getElementsByTagName("LI"))
     dom.unlink()
 
-def testInsertBefore( ):
-    dom=parse( tstfile )
-    docel=dom.documentElement
+def testInsertBefore():
+    dom = parse(tstfile)
+    docel = dom.documentElement
     #docel.insertBefore( dom.createProcessingInstruction("a", "b"),
     #                        docel.childNodes[1])
                             
     #docel.insertBefore( dom.createProcessingInstruction("a", "b"),
     #                        docel.childNodes[0])
 
-    #confirm( docel.childNodes[0].tet=="a" )
-    #confirm( docel.childNodes[2].tet=="a" )
+    #confirm( docel.childNodes[0].tet == "a")
+    #confirm( docel.childNodes[2].tet == "a")
     dom.unlink()
 
 def testAppendChild():
-    dom=parse( tstfile )
-    dom.documentElement.appendChild( dom.createComment( u"Hello" ))
-    confirm( dom.documentElement.childNodes[-1].nodeName=="#comment" )
-    confirm( dom.documentElement.childNodes[-1].data=="Hello" )
+    dom = parse(tstfile)
+    dom.documentElement.appendChild(dom.createComment(u"Hello"))
+    confirm(dom.documentElement.childNodes[-1].nodeName == "#comment")
+    confirm(dom.documentElement.childNodes[-1].data == "Hello")
     dom.unlink()
 
 def testNonZero():
-    dom=parse( tstfile )
-    confirm( dom )# should not be zero
-    dom.appendChild( dom.createComment( "foo" ) )
-    confirm( not dom.childNodes[-1].childNodes )
+    dom = parse(tstfile)
+    confirm(dom)# should not be zero
+    dom.appendChild(dom.createComment("foo"))
+    confirm(not dom.childNodes[-1].childNodes)
     dom.unlink()
 
 def testUnlink():
-    dom=parse( tstfile )
+    dom = parse(tstfile)
     dom.unlink()
 
 def testElement():
-    dom=Document()
-    dom.appendChild( dom.createElement( "abc" ) )
-    confirm( dom.documentElement )
+    dom = Document()
+    dom.appendChild(dom.createElement("abc"))
+    confirm(dom.documentElement)
     dom.unlink()
 
 def testAAA():
-    dom=parseString( "<abc/>" )
-    el=dom.documentElement
-    el.setAttribute( "spam", "jam2" )
+    dom = parseString("<abc/>")
+    el = dom.documentElement
+    el.setAttribute("spam", "jam2")
     dom.unlink()
 
 def testAAB():
-    dom=parseString( "<abc/>" )
-    el=dom.documentElement
-    el.setAttribute( "spam", "jam" )
-    el.setAttribute( "spam", "jam2" )
+    dom = parseString("<abc/>")
+    el = dom.documentElement
+    el.setAttribute("spam", "jam")
+    el.setAttribute("spam", "jam2")
     dom.unlink()
 
 def testAddAttr():
-    dom=Document()
-    child=dom.appendChild( dom.createElement( "abc" ) )
+    dom = Document()
+    child = dom.appendChild(dom.createElement("abc"))
 
-    child.setAttribute( "def", "ghi" )
-    confirm( child.getAttribute( "def" )=="ghi" )
-    confirm( child.attributes["def"].value=="ghi" )
+    child.setAttribute("def", "ghi")
+    confirm(child.getAttribute("def") == "ghi")
+    confirm(child.attributes["def"].value == "ghi")
 
-    child.setAttribute( "jkl", "mno" )
-    confirm( child.getAttribute( "jkl" )=="mno" )
-    confirm( child.attributes["jkl"].value=="mno" )
+    child.setAttribute("jkl", "mno")
+    confirm(child.getAttribute("jkl") == "mno")
+    confirm(child.attributes["jkl"].value == "mno")
 
-    confirm( len( child.attributes )==2 )
+    confirm(len(child.attributes) == 2)
 
-    child.setAttribute( "def", "newval" )
-    confirm( child.getAttribute( "def" )=="newval" )
-    confirm( child.attributes["def"].value=="newval" )
+    child.setAttribute("def", "newval")
+    confirm(child.getAttribute("def") == "newval")
+    confirm(child.attributes["def"].value == "newval")
 
-    confirm( len( child.attributes )==2 )
+    confirm(len(child.attributes) == 2)
     dom.unlink()
 
 def testDeleteAttr():
-    dom=Document()
-    child=dom.appendChild( dom.createElement( "abc" ) )
+    dom = Document()
+    child = dom.appendChild(dom.createElement("abc"))
 
-    confirm( len( child.attributes)==0 )
-    child.setAttribute( "def", "ghi" )
-    confirm( len( child.attributes)==1 )
+    confirm(len(child.attributes) == 0)
+    child.setAttribute("def", "ghi")
+    confirm(len(child.attributes) == 1)
     del child.attributes["def"]
-    confirm( len( child.attributes)==0 )
+    confirm(len(child.attributes) == 0)
     dom.unlink()
 
 def testRemoveAttr():
-    dom=Document()
-    child=dom.appendChild( dom.createElement( "abc" ) )
+    dom = Document()
+    child = dom.appendChild(dom.createElement("abc"))
 
-    child.setAttribute( "def", "ghi" )
-    confirm( len( child.attributes)==1 )
-    child.removeAttribute("def" )
-    confirm( len( child.attributes)==0 )
+    child.setAttribute("def", "ghi")
+    confirm(len(child.attributes) == 1)
+    child.removeAttribute("def")
+    confirm(len(child.attributes) == 0)
 
     dom.unlink()
 
 def testRemoveAttrNS():
-    dom=Document()
-    child=dom.appendChild( 
-            dom.createElementNS( "http://www.python.org", "python:abc" ) )
-    child.setAttributeNS( "http://www.w3.org", "xmlns:python", 
-                                            "http://www.python.org" )
-    child.setAttributeNS( "http://www.python.org", "python:abcattr", "foo" )
-    confirm( len( child.attributes )==2 )
-    child.removeAttributeNS( "http://www.python.org", "abcattr" )
-    confirm( len( child.attributes )==1 )
+    dom = Document()
+    child = dom.appendChild(
+            dom.createElementNS("http://www.python.org", "python:abc"))
+    child.setAttributeNS("http://www.w3.org", "xmlns:python", 
+                                            "http://www.python.org")
+    child.setAttributeNS("http://www.python.org", "python:abcattr", "foo")
+    confirm(len(child.attributes) == 2)
+    child.removeAttributeNS("http://www.python.org", "abcattr")
+    confirm(len(child.attributes) == 1)
 
     dom.unlink()
     
 def testRemoveAttributeNode():
-    dom=Document()
-    child=dom.appendChild( dom.createElement( "foo" ) )
-    child.setAttribute( "spam", "jam" )
-    confirm( len( child.attributes )==1 )
-    node=child.getAttributeNode( "spam" )
-    child.removeAttributeNode( node )
-    confirm( len( child.attributes )==0 )
+    dom = Document()
+    child = dom.appendChild(dom.createElement("foo"))
+    child.setAttribute("spam", "jam")
+    confirm(len(child.attributes) == 1)
+    node = child.getAttributeNode("spam")
+    child.removeAttributeNode(node)
+    confirm(len(child.attributes) == 0)
 
     dom.unlink()
 
 def testChangeAttr():
-    dom=parseString( "<abc/>" )
-    el=dom.documentElement
-    el.setAttribute( "spam", "jam" )
-    confirm( len( el.attributes )==1 )
-    el.setAttribute( "spam", "bam" )
-    confirm( len( el.attributes )==1 )
-    el.attributes["spam"]="ham"
-    confirm( len( el.attributes )==1 )
-    el.setAttribute( "spam2", "bam" )
-    confirm( len( el.attributes )==2 )
-    el.attributes[ "spam2"]= "bam2"
-    confirm( len( el.attributes )==2 )
+    dom = parseString("<abc/>")
+    el = dom.documentElement
+    el.setAttribute("spam", "jam")
+    confirm(len(el.attributes) == 1)
+    el.setAttribute("spam", "bam")
+    confirm(len(el.attributes) == 1)
+    el.attributes["spam"] = "ham"
+    confirm(len(el.attributes) == 1)
+    el.setAttribute("spam2", "bam")
+    confirm(len(el.attributes) == 2)
+    el.attributes[ "spam2"] = "bam2"
+    confirm(len(el.attributes) == 2)
     dom.unlink()
 
 def testGetAttrList():
@@ -186,41 +186,41 @@ def testGetElementsByTagNameNS(): pass
 def testGetEmptyNodeListFromElementsByTagNameNS(): pass
 
 def testElementReprAndStr():
-    dom=Document()
-    el=dom.appendChild( dom.createElement( "abc" ) )
-    string1=repr( el )
-    string2=str( el )
-    confirm( string1==string2 )
+    dom = Document()
+    el = dom.appendChild(dom.createElement("abc"))
+    string1 = repr(el)
+    string2 = str(el)
+    confirm(string1 == string2)
     dom.unlink()
 
 # commented out until Fredrick's fix is checked in
 def _testElementReprAndStrUnicode():
-    dom=Document()
-    el=dom.appendChild( dom.createElement( u"abc" ) )
-    string1=repr( el )
-    string2=str( el )
-    confirm( string1==string2 )
+    dom = Document()
+    el = dom.appendChild(dom.createElement(u"abc"))
+    string1 = repr(el)
+    string2 = str(el)
+    confirm(string1 == string2)
     dom.unlink()
 
 # commented out until Fredrick's fix is checked in
 def _testElementReprAndStrUnicodeNS():
-    dom=Document()
-    el=dom.appendChild(
-        dom.createElementNS( u"http://www.slashdot.org", u"slash:abc" ))
-    string1=repr( el )
-    string2=str( el )
-    confirm( string1==string2 )
-    confirm( string1.find("slash:abc" )!=-1 )
+    dom = Document()
+    el = dom.appendChild(
+        dom.createElementNS(u"http://www.slashdot.org", u"slash:abc"))
+    string1 = repr(el)
+    string2 = str(el)
+    confirm(string1 == string2)
+    confirm(string1.find("slash:abc") != -1)
     dom.unlink()
-    confirm( len( Node.allnodes )==0 )
+    confirm(len(Node.allnodes) == 0)
 
 def testAttributeRepr():
-    dom=Document()
-    el=dom.appendChild( dom.createElement( u"abc" ) )
-    node=el.setAttribute( "abc", "def" )
-    confirm( str( node ) == repr( node ) )
+    dom = Document()
+    el = dom.appendChild(dom.createElement(u"abc"))
+    node = el.setAttribute("abc", "def")
+    confirm(str(node) == repr(node))
     dom.unlink()
-    confirm( len( Node.allnodes )==0 )
+    confirm(len(Node.allnodes) == 0)
 
 def testTextNodeRepr(): pass
 
@@ -230,7 +230,7 @@ def testWriteXML():
     domstr = dom.toxml()
     dom.unlink()
     confirm(str == domstr)
-    confirm( len( Node.allnodes )==0 )
+    confirm(len(Node.allnodes) == 0)
 
 def testProcessingInstruction(): pass
 
@@ -308,21 +308,87 @@ def testClonePIShallow(): pass
 
 def testClonePIDeep(): pass
 
+def testSiblings():
+    doc = parseString("<doc><?pi?>text?<elm/></doc>")
+    root = doc.documentElement
+    (pi, text, elm) = root.childNodes
 
-names=globals().keys()
+    confirm(pi.nextSibling is text and 
+            pi.previousSibling is None and 
+            text.nextSibling is elm and 
+            text.previousSibling is pi and 
+            elm.nextSibling is None and 
+            elm.previousSibling is text, "testSiblings")
+
+    doc.unlink()
+
+def testParents():
+    doc = parseString("<doc><elm1><elm2/><elm2><elm3/></elm2></elm1></doc>")
+    root = doc.documentElement
+    elm1 = root.childNodes[0]
+    (elm2a, elm2b) = elm1.childNodes
+    elm3 = elm2b.childNodes[0]
+
+    confirm(root.parentNode is doc and
+            elm1.parentNode is root and
+            elm2a.parentNode is elm1 and
+            elm2b.parentNode is elm1 and
+            elm3.parentNode is elm2b, "testParents")
+
+    doc.unlink()
+
+def testSAX2DOM():
+    from xml.dom import pulldom
+
+    sax2dom = pulldom.SAX2DOM()
+    sax2dom.startDocument()
+    sax2dom.startElement("doc", {})
+    sax2dom.characters("text")
+    sax2dom.startElement("subelm", {})
+    sax2dom.characters("text")
+    sax2dom.endElement("subelm")
+    sax2dom.characters("text")    
+    sax2dom.endElement("doc")
+    sax2dom.endDocument()
+
+    doc = sax2dom.document
+    root = doc.documentElement
+    (text1, elm1, text2) = root.childNodes
+    text3 = elm1.childNodes[0]
+
+    confirm(text1.previousSibling is None and
+            text1.nextSibling is elm1 and
+            elm1.previousSibling is text1 and
+            elm1.nextSibling is text2 and
+            text2.previousSibling is elm1 and
+            text2.nextSibling is None and
+            text3.previousSibling is None and
+            text3.nextSibling is None, "testSAX2DOM - siblings")
+
+    confirm(root.parentNode is doc and
+            text1.parentNode is root and
+            elm1.parentNode is root and
+            text2.parentNode is root and
+            text3.parentNode is elm1, "testSAX2DOM - parents")
+            
+    doc.unlink()
+
+# --- MAIN PROGRAM
+    
+names = globals().keys()
 names.sort()
 
-works=1
+works = 1
 
 for name in names:
-    if name.startswith( "test" ):
-        func=globals()[name]
+    if name.startswith("test"):
+        func = globals()[name]
         try:
             func()
             print "Test Succeeded", name
             confirm(len(Node.allnodes) == 0,
                     "assertion: len(Node.allnodes) == 0")
-            if len( Node.allnodes ):
+            if len(Node.allnodes):
                 print "Garbage left over:"
                 if verbose:
                     print Node.allnodes.items()[0:10]
@@ -330,13 +396,13 @@ for name in names:
                     # Don't print specific nodes if repeatable results
                     # are needed
                     print len(Node.allnodes)
-            Node.allnodes={}
-        except Exception, e :
-            works=0
+            Node.allnodes = {}
+        except Exception, e:
+            works = 0
             print "Test Failed: ", name
-            apply( traceback.print_exception, sys.exc_info() )
+            traceback.print_exception(*sys.exc_info())
             print `e`
-            Node.allnodes={}
+            Node.allnodes = {}
 
 if works:
     print "All tests succeeded"
--- Lib/dos-8x3/test_win.py
+++ Lib/dos-8x3/test_win.py
@@ -1,7 +1,147 @@
-# Ridiculously simple test of the winsound module for Windows.
+# Test the windows specific win32reg module.
+# Only win32reg functions not hit here: FlushKey, LoadKey and SaveKey
 
-import winsound
-for i in range(100, 2000, 100):
-    winsound.Beep(i, 75)
-print "Hopefully you heard some sounds increasing in frequency!"
+from _winreg import *
+import os, sys
+
+test_key_name = "SOFTWARE\\Python Registry Test Key - Delete Me"
+
+test_data = [
+    ("Int Value",     45,                                      REG_DWORD),
+    ("String Val",    "A string value",                        REG_SZ,),
+    (u"Unicode Val",  u"A Unicode value",                      REG_SZ,),
+    ("StringExpand",  "The path is %path%",                    REG_EXPAND_SZ),
+    ("UnicodeExpand", u"The path is %path%",                   REG_EXPAND_SZ),
+    ("Multi-string",  ["Lots", "of", "string", "values"],      REG_MULTI_SZ),
+    ("Multi-unicode", [u"Lots", u"of", u"unicode", u"values"], REG_MULTI_SZ),
+    ("Multi-mixed",   [u"Unicode", u"and", "string", "values"],REG_MULTI_SZ),
+    ("Raw Data",      ("binary"+chr(0)+"data"),                REG_BINARY),
+]
+
+def WriteTestData(root_key):
+    # Set the default value for this key.
+    SetValue(root_key, test_key_name, REG_SZ, "Default value")
+    key = CreateKey(root_key, test_key_name)
+    # Create a sub-key
+    sub_key = CreateKey(key, "sub_key")
+    # Give the sub-key some named values
+
+    for value_name, value_data, value_type in test_data:
+        SetValueEx(sub_key, value_name, 0, value_type, value_data)
+
+    # Check we wrote as many items as we thought.
+    nkeys, nvalues, since_mod = QueryInfoKey(key)
+    assert nkeys==1, "Not the correct number of sub keys"
+    assert nvalues==1, "Not the correct number of values"
+    nkeys, nvalues, since_mod = QueryInfoKey(sub_key)
+    assert nkeys==0, "Not the correct number of sub keys"
+    assert nvalues==len(test_data), "Not the correct number of values"
+    # Close this key this way...
+    # (but before we do, copy the key as an integer - this allows
+    # us to test that the key really gets closed).
+    int_sub_key = int(sub_key)
+    CloseKey(sub_key)
+    try:
+        QueryInfoKey(int_sub_key)
+        raise RuntimeError, "It appears the CloseKey() function does not close the actual key!"
+    except EnvironmentError:
+        pass
+    # ... and close that key that way :-)
+    int_key = int(key)
+    key.Close()
+    try:
+        QueryInfoKey(int_key)
+        raise RuntimeError, "It appears the key.Close() function does not close the actual key!"
+    except EnvironmentError:
+        pass
+
+def ReadTestData(root_key):
+    # Check we can get default value for this key.
+    val = QueryValue(root_key, test_key_name)
+    assert val=="Default value", "Registry didn't give back the correct value"
+
+    key = OpenKey(root_key, test_key_name)
+    # Read the sub-keys
+    sub_key = OpenKey(key, "sub_key")
+    # Check I can enumerate over the values.
+    index = 0
+    while 1:
+        try:
+            data = EnumValue(sub_key, index)
+        except EnvironmentError:
+            break
+        assert data in test_data, "Didn't read back the correct test data"
+        index = index + 1
+    assert index==len(test_data), "Didn't read the correct number of items"
+    # Check I can directly access each item
+    for value_name, value_data, value_type in test_data:
+        read_val, read_typ = QueryValueEx(sub_key, value_name)
+        assert read_val==value_data and read_typ == value_type, \
+               "Could not directly read the value"
+    sub_key.Close()
+    # Enumerate our main key.
+    read_val = EnumKey(key, 0)
+    assert read_val == "sub_key", "Read subkey value wrong"
+    try:
+        EnumKey(key, 1)
+        assert 0, "Was able to get a second key when I only have one!"
+    except EnvironmentError:
+        pass
+
+    key.Close()
+
+def DeleteTestData(root_key):
+    key = OpenKey(root_key, test_key_name, 0, KEY_ALL_ACCESS)
+    sub_key = OpenKey(key, "sub_key", 0, KEY_ALL_ACCESS)
+    # It is not necessary to delete the values before deleting
+    # the key (although subkeys must not exist).  We delete them
+    # manually just to prove we can :-)
+    for value_name, value_data, value_type in test_data:
+        DeleteValue(sub_key, value_name)
+
+    nkeys, nvalues, since_mod = QueryInfoKey(sub_key)
+    assert nkeys==0 and nvalues==0, "subkey not empty before delete"
+    sub_key.Close()
+    DeleteKey(key, "sub_key")
+
+    try:
+        # Shouldnt be able to delete it twice!
+        DeleteKey(key, "sub_key")
+        assert 0, "Deleting the key twice succeeded"
+    except EnvironmentError:
+        pass
+    key.Close()
+    DeleteKey(root_key, test_key_name)
+    # Opening should now fail!
+    try:
+        key = OpenKey(root_key, test_key_name)
+        assert 0, "Could open the non-existent key"
+    except WindowsError: # Use this error name this time
+        pass
+
+def TestAll(root_key):
+    WriteTestData(root_key)
+    ReadTestData(root_key)
+    DeleteTestData(root_key)
+
+# Test on my local machine.
+TestAll(HKEY_CURRENT_USER)
+print "Local registry tests worked"
+try:
+    remote_name = sys.argv[sys.argv.index("--remote")+1]
+except (IndexError, ValueError):
+    remote_name = None
+
+if remote_name is not None:
+    try:
+        remote_key = ConnectRegistry(remote_name, HKEY_CURRENT_USER)
+    except EnvironmentError, exc:
+        print "Could not connect to the remote machine -", exc.strerror
+        remote_key = None
+    if remote_key is not None:
+        TestAll(remote_key)
+        print "Remote registry tests worked"
+else:
+    print "Remote registry calls can be tested using",
+    print "'test_winreg.py --remote \\\\machine_name'"
 
