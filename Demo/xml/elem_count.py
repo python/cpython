ac5f7483e8f0ae21e3b565e4d7d97c99898817f7
Fred Drake <fdrake@acm.org>
2000-10-16 15:27:05 +0000

    Demos of the new XML support from Lars Marius Garshol <larsga@garshol.priv.no>.

---
+++ Demo/xml/elem_count.py
@@ -0,0 +1,36 @@
+import sys
+
+from xml.sax import make_parser, handler
+
+class FancyCounter(handler.ContentHandler):
+
+    def __init__(self):
+        self._elems = 0
+        self._attrs = 0
+        self._elem_types = {}
+        self._attr_types = {}
+
+    def startElement(self, name, attrs):
+        self._elems = self._elems + 1
+        self._attrs = self._attrs + len(attrs)
+        self._elem_types[name] = self._elem_types.get(name, 0) + 1
+
+        for name in attrs.keys():
+            self._attr_types[name] = self._attr_types.get(name, 0) + 1
+
+    def endDocument(self):
+        print "There were", self._elems, "elements."
+        print "There were", self._attrs, "attributes."
+
+        print "---ELEMENT TYPES"
+        for pair in  self._elem_types.items():
+            print "%20s %d" % pair
+
+        print "---ATTRIBUTE TYPES"
+        for pair in  self._attr_types.items():
+            print "%20s %d" % pair
+
+            
+parser = make_parser()
+parser.setContentHandler(FancyCounter())
+parser.parse(sys.argv[1])
---
+++ Demo/xml/roundtrip.py
@@ -0,0 +1,45 @@
+"""
+A simple demo that reads in an XML document and spits out an equivalent,
+but not necessarily identical, document.
+"""
+
+import sys, string
+
+from xml.sax import saxutils, handler, make_parser
+
+# --- The ContentHandler
+
+class ContentGenerator(handler.ContentHandler):
+
+    def __init__(self, out = sys.stdout):
+        handler.ContentHandler.__init__(self)
+        self._out = out
+
+    # ContentHandler methods
+        
+    def startDocument(self):
+        self._out.write('<?xml version="1.0" encoding="iso-8859-1"?>\n')
+
+    def startElement(self, name, attrs):
+        self._out.write('<' + name)
+        for (name, value) in attrs.items():
+            self._out.write(' %s="%s"' % (name, saxutils.escape(value)))
+        self._out.write('>')
+
+    def endElement(self, name):
+        self._out.write('</%s>' % name)
+
+    def characters(self, content):
+        self._out.write(saxutils.escape(content))
+
+    def ignorableWhitespace(self, content):
+        self._out.write(content)
+        
+    def processingInstruction(self, target, data):
+        self._out.write('<?%s %s?>' % (target, data))
+
+# --- The main program
+
+parser = make_parser()
+parser.setContentHandler(ContentGenerator())
+parser.parse(sys.argv[1])
