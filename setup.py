e205d818016344ce8f3ba8ab4ad21fb57f6b5a98
Andrew M. Kuchling <amk@amk.ca>
2001-02-28 19:49:57 +0000

    Fix for bug #404875: fix typo in setup.py

--- setup.py
+++ setup.py
@@ -398,7 +398,7 @@ class PyBuildExt(build_ext):
         # Curses support, requring the System V version of curses, often
         # provided by the ncurses library.
         if platform == 'sunos4':
-            include_dirs += ['/usr/5include']
+            inc_dirs += ['/usr/5include']
             lib_dirs += ['/usr/5lib']
 
         if (self.compiler.find_library_file(lib_dirs, 'ncurses')):
