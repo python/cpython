367d8565c9009479bd16de8143c6190b44add04c
Fred Drake <fdrake@acm.org>
2001-03-02 21:05:58 +0000

    Revise the scripts I use to update the documentation on the SourceForge site.  These now seem (slightly) more reliable, and easier to work with since update-docs.sh no longer needs to be installed ahead of time on my account at SF.

--- Doc/tools/update-docs.sh
+++ Doc/tools/update-docs.sh
@@ -1,39 +1,21 @@
 #! /bin/sh
 
-#  Script which determines if a new development snapshot of the
-#  documentation is available, and unpacks it into the "Python @
-#  SourceForge" website.
+# Script which installs a development snapshot of the documentation
+# into the "Python @ SourceForge" website.
 #
-#  A copy of this script is run periodically via cron.
+# The push-docs.sh script pushes this to the SourceForge when needed
+# and removes it when done.
 
 if [ -z "$HOME" ] ; then
     HOME=`grep fdrake /etc/passwd | sed 's|^.*:\([^:]*\):[^:]*$|\1|'`
     export HOME
 fi
 
-UPDATES=$HOME/python-docs-update.tar.bz2
-INFO=$HOME/python-docs-update.txt
+UPDATES=$HOME/tmp/python-docs-update.tar.bz2
 
-if [ -f "$UPDATES" ] ; then
-    cd /home/groups/python/htdocs
-    rm -rf devel-docs || exit $?
-    mkdir devel-docs || exit $?
-    cd devel-docs || exit $?
-    (bzip2 -dc "$UPDATES" | tar xf -) || exit $?
-    rm "$UPDATES" || exit $?
-    if [ -f "$INFO" ] ; then
-        EXPLANATION="`cat $INFO`"
-    else
-        EXPLANATION=''
-    fi
-    Mail -s '[development doc updates]' \
-     python-dev@python.org doc-sig@python.org \
-     <<EOF
-The development version of the documentation has been updated:
-
-	http://python.sourceforge.net/devel-docs/
-
-$EXPLANATION
-EOF
-    rm -f $HOME/python-docs-update.txt
-fi
+cd /home/groups/python/htdocs
+rm -rf devel-docs || exit $?
+mkdir devel-docs || exit $?
+cd devel-docs || exit $?
+(bzip2 -dc "$UPDATES" | tar xf -) || exit $?
+rm "$UPDATES" || exit $?
