2513391e6aedbbd3337bbf3a61fd32090f3ea4b5
Fred Drake <fdrake@acm.org>
2001-03-21 18:29:25 +0000

    Add test cases for the fnmatch module.

---
+++ Lib/test/test_fnmatch.py
@@ -0,0 +1,42 @@
+"""Test cases for the fnmatch module."""
+
+import re
+import test_support
+import unittest
+
+from fnmatch import fnmatch, fnmatchcase
+
+
+class FnmatchTestCase(unittest.TestCase):
+    def check_match(self, filename, pattern, should_match=1):
+        if should_match:
+            self.assert_(fnmatch(filename, pattern),
+                         "expected %r to match pattern %r"
+                         % (filename, pattern))
+        else:
+            self.assert_(not fnmatch(filename, pattern),
+                         "expected %r not to match pattern %r"
+                         % (filename, pattern))
+
+    def test_fnmatch(self):
+        check = self.check_match
+        check('abc', 'abc')
+        check('abc', '?*?')
+        check('abc', '???*')
+        check('abc', '*???')
+        check('abc', '???')
+        check('abc', '*')
+        check('abc', 'ab[cd]')
+        check('abc', 'ab[!de]')
+        check('abc', 'ab[de]', 0)
+        check('a', '??', 0)
+        check('a', 'b', 0)
+
+        # these test that '\' is handled correctly in character sets;
+        # see SF bug #???
+        check('\\', r'[\]')
+        check('a', r'[!\]')
+        check('\\', r'[!\]', 0)
+
+
+test_support.run_unittest(FnmatchTestCase)
