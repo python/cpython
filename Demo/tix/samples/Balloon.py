ea0982f104924175aa0b5b7b0cb03c46d1cc53fe
Martin v. Löwis <martin@v.loewis.de>
2001-03-21 07:42:07 +0000

    Patch #410231: Add the Python Tix library.

---
+++ Demo/tix/samples/Balloon.py
@@ -0,0 +1,45 @@
+#!/usr/local/bin/python
+# 
+# $Id$
+#
+# Tix Demostration Program
+#
+# This sample program is structured in such a way so that it can be
+# executed from the Tix demo program "tixwidgets": it must have a
+# procedure called "RunSample". It should also have the "if" statment
+# at the end of this file so that it can be run as a standalone
+# program.
+
+# This file demonstrates the use of the tixBalloon widget, which provides
+# a interesting way to give help tips about elements in your user interface.
+# Your can display the help message in a "balloon" and a status bar widget.
+#
+
+import Tix
+
+def RunSample(w):
+    status = Tix.Label(w, width=40, relief=Tix.SUNKEN, bd=1)
+    status.pack(side=Tix.BOTTOM, fill=Tix.Y, padx=2, pady=1)
+
+    # Create two mysterious widgets that need balloon help
+    button1 = Tix.Button(w, text='Something Unexpected',
+			 command=lambda w=w: w.destroy())
+    button2 = Tix.Button(w, text='Something Else Unexpected')
+    button2['command'] = lambda w=button2: w.destroy()
+    button1.pack(side=Tix.TOP, expand=1)
+    button2.pack(side=Tix.TOP, expand=1)
+
+    # Create the balloon widget and associate it with the widgets that we want
+    # to provide tips for:
+    b = Tix.Balloon(w, statusbar=status)
+
+    b.bind_widget(button1, balloonmsg='Close Window',
+		  statusmsg='Press this button to close this window')
+    b.bind_widget(button2, balloonmsg='Self-destruct button',
+		  statusmsg='Press this button and it will destroy itself')
+
+if __name__ == '__main__':
+    root = Tix.Tk()
+
+    RunSample(root)
+    root.mainloop()
---
+++ Demo/tix/samples/Tree.py
@@ -0,0 +1,80 @@
+#!/usr/local/bin/python
+# 
+# $Id$
+#
+# Tix Demostration Program
+#
+# This sample program is structured in such a way so that it can be
+# executed from the Tix demo program "tixwidgets": it must have a
+# procedure called "RunSample". It should also have the "if" statment
+# at the end of this file so that it can be run as a standalone
+# program.
+
+# This file demonstrates how to use the TixTree widget to display
+# dynamic hierachical data (the files in the Unix file system)
+#
+
+import Tix, os
+
+def RunSample(w):
+    top = Tix.Frame(w, relief=Tix.RAISED, bd=1)
+    tree = Tix.Tree(top, options='separator "/"')
+    tree.pack(expand=1, fill=Tix.BOTH, padx=10, pady=10, side=Tix.LEFT)
+    tree['opencmd'] = lambda dir=None, w=tree: opendir(w, dir)
+
+    # The / directory is added in the "open" mode. The user can open it
+    # and then browse its subdirectories ...
+    adddir(tree, "/")
+
+    box = Tix.ButtonBox(w, orientation=Tix.HORIZONTAL)
+    box.add('ok', text='Ok', underline=0, command=w.destroy, width=6)
+    box.add('cancel', text='Cancel', underline=0, command=w.destroy, width=6)
+    box.pack(side=Tix.BOTTOM, fill=Tix.X)
+    top.pack(side=Tix.TOP, fill=Tix.BOTH, expand=1)
+
+def adddir(tree, dir):
+    if dir == '/':
+	text = '/'
+    else:
+	text = os.path.basename(dir)
+    tree.hlist.add(dir, itemtype=Tix.IMAGETEXT, text=text,
+		   image=tree.tk.call('tix', 'getimage', 'folder'))
+    try:
+	os.listdir(dir)
+	tree.setmode(dir, 'open')
+    except os.error:
+	# No read permission ?
+	pass
+
+# This function is called whenever the user presses the (+) indicator or
+# double clicks on a directory whose mode is "open". It loads the files
+# inside that directory into the Tree widget.
+#
+# Note we didn't specify the closecmd option for the Tree widget, so it
+# performs the default action when the user presses the (-) indicator or
+# double clicks on a directory whose mode is "close": hide all of its child
+# entries
+def opendir(tree, dir):
+    entries = tree.hlist.info_children(dir)
+    if entries:
+	# We have already loaded this directory. Let's just
+	# show all the child entries
+	#
+	# Note: since we load the directory only once, it will not be
+	#       refreshed if the you add or remove files from this
+	#	directory.
+	#
+	for entry in entries:
+	    tree.hlist.show_entry(entry)
+    files = os.listdir(dir)
+    for file in files:
+	if os.path.isdir(dir + '/' + file):
+	    adddir(tree, dir + '/' + file)
+	else:
+	    tree.hlist.add(dir + '/' + file, itemtype=Tix.IMAGETEXT, text=file,
+			   image=tree.tk.call('tix', 'getimage', 'file'))
+
+if __name__ == '__main__':
+    root = Tix.Tk()
+    RunSample(root)
+    root.mainloop()
