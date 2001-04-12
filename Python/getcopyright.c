23f7aed2a792190c42209732f1520ccb7321be8e
Guido van Rossum <guido@python.org>
2001-04-12 20:53:31 +0000

    Because this code was derived from Python 1.6.1 (amongst others), the CNRI copyright should be updated to include 2001.

--- Python/getcopyright.c
+++ Python/getcopyright.c
@@ -10,7 +10,7 @@ All Rights Reserved.\n\
 Copyright (c) 2000 BeOpen.com.\n\
 All Rights Reserved.\n\
 \n\
-Copyright (c) 1995-2000 Corporation for National Research Initiatives.\n\
+Copyright (c) 1995-2001 Corporation for National Research Initiatives.\n\
 All Rights Reserved.\n\
 \n\
 Copyright (c) 1991-1995 Stichting Mathematisch Centrum, Amsterdam.\n\
