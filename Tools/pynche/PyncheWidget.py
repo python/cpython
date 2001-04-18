6a5522677a5325b333ef687e396f2b66ed52efa6
Barry Warsaw <barry@python.org>
2001-04-18 03:50:07 +0000

    Helpwin.__init__(): Removed an unused local variable (via import) reported by PyChecker.

--- Tools/pynche/PyncheWidget.py
+++ Tools/pynche/PyncheWidget.py
@@ -203,7 +203,7 @@ Unrecognized color file type in file:
 
 class Helpwin:
     def __init__(self, master, quitfunc):
-        from Main import __version__, docstring
+        from Main import docstring
         self.__root = root = Toplevel(master, class_='Pynche')
         root.protocol('WM_DELETE_WINDOW', self.__withdraw)
         root.title('Pynche Help Window')
