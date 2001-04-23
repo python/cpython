482559e4c0a965ab4d8ee72c51d9c19aa3719266
Andrew M. Kuchling <amk@amk.ca>
2001-04-23 17:13:03 +0000

    Fix typo in docstring

--- Lib/distutils/__init__.py
+++ Lib/distutils/__init__.py
@@ -1,6 +1,6 @@
 """distutils
 
-The main package for the Python Module Distribtion Utilities.  Normally
+The main package for the Python Module Distribution Utilities.  Normally
 used from a setup script as
 
    from distutils.core import setup
