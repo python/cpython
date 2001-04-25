8f3ef032de889ee5406fb08fd6529a30dfff9b84
Jack Jansen <jack.jansen@cwi.nl>
2001-04-25 22:11:24 +0000

    Files for 2.1 distribution.

--- Mac/Lib/lib-toolbox/Dialogs.py
+++ Mac/Lib/lib-toolbox/Dialogs.py
@@ -1,4 +1,4 @@
-# Generated from 'Dialogs.h'
+# Generated from 'Moes:SWDev:Metrowerks Codewarrior 6.0:Metrowerks CodeWarrior:MacOS Support:Universal:Interfaces:CIncludes:Dialogs.h'
 
 def FOUR_CHAR_CODE(x): return x
 kControlDialogItem = 4
