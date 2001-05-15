d4855ce9cef0cff647de64f98f7727d95efd4018
Marc-André Lemburg <mal@egenix.com>
2001-05-15 12:00:02 +0000

    This patch changes the way the string .encode() method works slightly and introduces a new method .decode().
    The major change is that strg.encode() will no longer try to convert
    Unicode returns from the codec into a string, but instead pass along
    the Unicode object as-is. The same is now true for all other codec
    return types. The underlying C APIs were changed accordingly.
    
    Note that even though this does have the potential of breaking
    existing code, the chances are low since conversion from Unicode
    previously took place using the default encoding which is normally
    set to ASCII rendering this auto-conversion mechanism useless for
    most Unicode encodings.
    
    The good news is that you can now use .encode() and .decode() with
    much greater ease and that the door was opened for better accessibility
    of the builtin codecs.
    
    As demonstration of the new feature, the patch includes a few new
    codecs which allow string to string encoding and decoding (rot13,
    hex, zip, uu, base64).
    
    Written by Marc-Andre Lemburg. Copyright assigned to the PSF.

---
+++ Lib/encodings/zlib_codec.py
@@ -0,0 +1,61 @@
+""" Python 'zlib_codec' Codec - zlib compression encoding
+
+    Unlike most of the other codecs which target Unicode, this codec
+    will return Python string objects for both encode and decode.
+
+    Written by Marc-Andre Lemburg (mal@lemburg.com).
+
+"""
+import codecs
+import zlib # this codec needs the optional zlib module !
+
+### Codec APIs
+
+def zlib_encode(input,errors='strict'):
+
+    """ Encodes the object input and returns a tuple (output
+        object, length consumed).
+
+        errors defines the error handling to apply. It defaults to
+        'strict' handling which is the only currently supported
+        error handling for this codec.
+
+    """
+    assert errors == 'strict'
+    output = zlib.compress(input)
+    return (output, len(input))
+
+def zlib_decode(input,errors='strict'):
+
+    """ Decodes the object input and returns a tuple (output
+        object, length consumed).
+
+        input must be an object which provides the bf_getreadbuf
+        buffer slot. Python strings, buffer objects and memory
+        mapped files are examples of objects providing this slot.
+
+        errors defines the error handling to apply. It defaults to
+        'strict' handling which is the only currently supported
+        error handling for this codec.
+
+    """
+    assert errors == 'strict'
+    output = zlib.decompress(input)
+    return (output, len(input))
+
+class Codec(codecs.Codec):
+
+    encode = zlib_encode
+    decode = zlib_decode
+
+class StreamWriter(Codec,codecs.StreamWriter):
+    pass
+        
+class StreamReader(Codec,codecs.StreamReader):
+    pass
+
+### encodings module API
+
+def getregentry():
+
+    return (zlib_encode,zlib_decode,StreamReader,StreamWriter)
