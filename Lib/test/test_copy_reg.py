7317d437181181c43c668af8e8b0414904b71d53
Fred Drake <fdrake@acm.org>
2001-05-22 20:38:44 +0000

    Convert copy_reg test to PyUnit.

--- Lib/test/test_copy_reg.py
+++ Lib/test/test_copy_reg.py
@@ -1,35 +1,25 @@
 import copy_reg
+import test_support
+import unittest
+
 
 class C:
     pass
 
 
-try:
-    copy_reg.pickle(C, None, None)
-except TypeError, e:
-    print "Caught expected TypeError:"
-    print e
-else:
-    print "Failed to catch expected TypeError when registering a class type."
+class CopyRegTestCase(unittest.TestCase):
+
+    def test_class(self):
+        self.assertRaises(TypeError, copy_reg.pickle,
+                          C, None, None)
+
+    def test_noncallable_reduce(self):
+        self.assertRaises(TypeError, copy_reg.pickle,
+                          type(1), "not a callable")
+
+    def test_noncallable_constructor(self):
+        self.assertRaises(TypeError, copy_reg.pickle,
+                          type(1), int, "not a callable")
 
 
-print
-try:
-    copy_reg.pickle(type(1), "not a callable")
-except TypeError, e:
-    print "Caught expected TypeError:"
-    print e
-else:
-    print "Failed to catch TypeError " \
-          "when registering a non-callable reduction function."
-
-
-print
-try:
-    copy_reg.pickle(type(1), int, "not a callable")
-except TypeError, e:
-    print "Caught expected TypeError:"
-    print e
-else:
-    print "Failed to catch TypeError " \
-          "when registering a non-callable constructor."
+test_support.run_unittest(CopyRegTestCase)
