d1f4a6955bb3babd161cdeb986f001d3ed7bd4c3
Fred Drake <fdrake@acm.org>
2001-05-22 20:22:06 +0000

    Simple conversion to PyUnit -- this test really needs more work!

--- Lib/test/test_xmllib.py
+++ Lib/test/test_xmllib.py
@@ -2,24 +2,29 @@
    Sjoerd Mullender
 '''
 
-from test_support import verbose
-
 testdoc = """\
 <?xml version="1.0" encoding="UTF-8" standalone='yes' ?>
 <!-- comments aren't allowed before the <?xml?> tag,
      but they are allowed before the <!DOCTYPE> tag -->
+<?processing instructions are allowed in the same places as comments ?>
 <!DOCTYPE greeting [
   <!ELEMENT greeting (#PCDATA)>
 ]>
 <greeting>Hello, world!</greeting>
 """
 
+import test_support
+import unittest
 import xmllib
-if verbose:
-    parser = xmllib.TestXMLParser()
-else:
-    parser = xmllib.XMLParser()
 
-for c in testdoc:
-    parser.feed(c)
-parser.close()
+
+class XMLParserTestCase(unittest.TestCase):
+
+    def test_simple(self):
+        parser = xmllib.XMLParser()
+        for c in testdoc:
+            parser.feed(c)
+        parser.close()
+
+
+test_support.run_unittest(XMLParserTestCase)
