a5ca7dd71ad0b3d05e884c43aacbc37619af9779
Jack Jansen <jack.jansen@cwi.nl>
2001-05-23 20:03:06 +0000

    One more macroman<->latin1 conversion victim.

--- Mac/Tools/IDE/PyEdit.py
+++ Mac/Tools/IDE/PyEdit.py
@@ -67,7 +67,7 @@ class Editor(W.Window):
 			else:
 				sourceOS = 'UNIX'
 				searchString = '\n'
-			change = EasyDialogs.AskYesNoCancel('≥%s≤ contains %s-style line feeds. '
+			change = EasyDialogs.AskYesNoCancel('ñ%sî contains %s-style line feeds. '
 					'Change them to MacOS carriage returns?' % (self.title, sourceOS), 1)
 			# bug: Cancel is treated as No
 			if change > 0:
@@ -224,14 +224,14 @@ class Editor(W.Window):
 		self.linefield.bind("<click>", self.clicklinefield)
 	
 	def makeoptionsmenu(self):
-		menuitems = [('Font settingsä', self.domenu_fontsettings), 
-				("Save optionsä", self.domenu_options),
+		menuitems = [('Font settingsÉ', self.domenu_fontsettings), 
+				("Save optionsÉ", self.domenu_options),
 				'-',
 				('\0' + chr(self.run_as_main) + 'Run as __main__', self.domenu_toggle_run_as_main), 
 				#('\0' + chr(self.run_with_interpreter) + 'Run with Interpreter', self.domenu_toggle_run_with_interpreter), 
 				#'-',
 				('Modularize', self.domenu_modularize),
-				('Browse namespaceä', self.domenu_browsenamespace), 
+				('Browse namespaceÉ', self.domenu_browsenamespace), 
 				'-']
 		if self.profiling:
 			menuitems = menuitems + [('Disable profiler', self.domenu_toggleprofiler)]
@@ -240,7 +240,7 @@ class Editor(W.Window):
 		if self.editgroup.editor._debugger:
 			menuitems = menuitems + [('Disable debugger', self.domenu_toggledebugger),
 				('Clear breakpoints', self.domenu_clearbreakpoints),
-				('Edit breakpointsä', self.domenu_editbreakpoints)]
+				('Edit breakpointsÉ', self.domenu_editbreakpoints)]
 		else:
 			menuitems = menuitems + [('Enable debugger', self.domenu_toggledebugger)]
 		self.editgroup.optionsmenu.set(menuitems)
@@ -285,7 +285,7 @@ class Editor(W.Window):
 	def domenu_modularize(self, *args):
 		modname = _filename_as_modname(self.title)
 		if not modname:
-			raise W.AlertError, 'Canπt modularize ≥%s≤' % self.title
+			raise W.AlertError, 'Can’t modularize ñ%sî' % self.title
 		run_as_main = self.run_as_main
 		self.run_as_main = 0
 		self.run()
@@ -360,7 +360,7 @@ class Editor(W.Window):
 			import EasyDialogs
 			import Qd
 			Qd.InitCursor() # XXX should be done by dialog
-			save = EasyDialogs.AskYesNoCancel('Save window ≥%s≤ before closing?' % self.title, 1)
+			save = EasyDialogs.AskYesNoCancel('Save window ñ%sî before closing?' % self.title, 1)
 			if save > 0:
 				if self.domenu_save():
 					return 1
@@ -420,7 +420,7 @@ class Editor(W.Window):
 			import buildtools
 		except ImportError:
 			# only have buildtools in Python >= 1.5.2
-			raise W.AlertError, "≥Save as Applet≤ is only supported in\rPython 1.5.2 and up."
+			raise W.AlertError, "ñSave as Appletî is only supported in\rPython 1.5.2 and up."
 		
 		buildtools.DEBUG = 0	# ouch.
 		
@@ -504,7 +504,7 @@ class Editor(W.Window):
 			if self.editgroup.editor.changed:
 				import EasyDialogs
 				import Qd; Qd.InitCursor()
-				save = EasyDialogs.AskYesNoCancel('Save ≥%s≤ before running?' % self.title, 1)
+				save = EasyDialogs.AskYesNoCancel('Save ñ%sî before running?' % self.title, 1)
 				if save > 0:
 					if self.domenu_save():
 						return
@@ -560,23 +560,23 @@ class Editor(W.Window):
 					classname = string.split(string.strip(line[6:]))[0]
 					classend = identifieRE_match(classname)
 					if classend < 1:
-						raise W.AlertError, 'Canπt find a class.'
+						raise W.AlertError, 'Can’t find a class.'
 					classname = classname[:classend]
 					break
 				elif line and line[0] not in '\t#':
-					raise W.AlertError, 'Canπt find a class.'
+					raise W.AlertError, 'Can’t find a class.'
 			else:
-				raise W.AlertError, 'Canπt find a class.'
+				raise W.AlertError, 'Can’t find a class.'
 			if globals.has_key(classname):
 				locals = globals[classname].__dict__
 			else:
-				raise W.AlertError, 'Canπt find class ≥%s≤.' % classname
+				raise W.AlertError, 'Can’t find class ñ%sî.' % classname
 			# dedent to top level
 			for i in range(len(lines)):
 				lines[i] = lines[i][1:]
 			pytext = string.join(lines, '\r')
 		elif indent > 0:
-			raise W.AlertError, 'Canπt run indented code.'
+			raise W.AlertError, 'Can’t run indented code.'
 		
 		# add "newlines" to fool compile/exec: 
 		# now a traceback will give the right line number
@@ -839,7 +839,7 @@ class SearchEngine:
 		self.buttons = [	("Find",		"cmdf",	 self.find), 
 					("Replace",	     "cmdr",	 self.replace), 
 					("Replace all",	 None,   self.replaceall), 
-					("Donπt find",  "cmdd",	 self.dont), 
+					("Don’t find",  "cmdd",	 self.dont), 
 					("Cancel",	      "cmd.",	 self.cancel)
 				]
 		for i in range(len(self.buttons)):
@@ -848,7 +848,7 @@ class SearchEngine:
 			self.w[title] = W.Button(bounds, title, callback)
 			if shortcut:
 				self.w.bind(shortcut, self.w[title].push)
-		self.w.setdefaultbutton(self.w["Donπt find"])
+		self.w.setdefaultbutton(self.w["Don’t find"])
 		self.w.find.edit.bind("<key>", self.key)
 		self.w.bind("<activate>", self.activate)
 		self.w.bind("<close>", self.close)
@@ -881,11 +881,11 @@ class SearchEngine:
 			else:
 				for title, cmd, call in self.buttons[:-2]:
 					self.w[title].enable(0)
-				self.w.setdefaultbutton(self.w["Donπt find"])
+				self.w.setdefaultbutton(self.w["Don’t find"])
 		else:
 			for title, cmd, call in self.buttons[:-2]:
 				self.w[title].enable(0)
-			self.w.setdefaultbutton(self.w["Donπt find"])
+			self.w.setdefaultbutton(self.w["Don’t find"])
 	
 	def find(self):
 		self.getparmsfromwindow()
@@ -1204,7 +1204,7 @@ class _EditorDefaultSettings:
 		self.template = "%s, %d point"
 		self.fontsettings, self.tabsettings, self.windowsize = geteditorprefs()
 		self.w = W.Dialog((328, 120), "Editor default settings")
-		self.w.setfontbutton = W.Button((8, 8, 80, 16), "Set fontä", self.dofont)
+		self.w.setfontbutton = W.Button((8, 8, 80, 16), "Set fontÉ", self.dofont)
 		self.w.fonttext = W.TextBox((98, 10, -8, 14), self.template % (self.fontsettings[0], self.fontsettings[2]))
 		
 		self.w.picksizebutton = W.Button((8, 50, 80, 16), "Front window", self.picksize)
