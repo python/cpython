f40d87eb23d220cca07da5adb37be9feb83715d4
Martin v. Löwis <martin@v.loewis.de>
2001-06-05 05:58:44 +0000

    Wrap with extern "C". Fixes bug #428419. Also protect against multiple inclusion.

--- Include/rangeobject.h
+++ Include/rangeobject.h
@@ -1,6 +1,12 @@
 
 /* Range object interface */
 
+#ifndef Py_RANGEOBJECT_H
+#define Py_RANGEOBJECT_H
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 /*
 A range object represents an integer range.  This is an immutable object;
 a range cannot change its value after creation.
@@ -14,3 +20,8 @@ extern DL_IMPORT(PyTypeObject) PyRange_Type;
 #define PyRange_Check(op) ((op)->ob_type == &PyRange_Type)
 
 extern DL_IMPORT(PyObject *) PyRange_New(long, long, long, int);
+
+#ifdef __cplusplus
+}
+#endif
+#endif /* !Py_RANGEOBJECT_H */
