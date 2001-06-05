ab5f21fbf66357680bb87e543f8af5cced1a757d
Martin v. Löwis <martin@v.loewis.de>
2001-06-05 05:33:19 +0000

    Fix bug #422702: Make flag argument to open optional, and document it that way.

--- Lib/dbhash.py
+++ Lib/dbhash.py
@@ -12,5 +12,5 @@ __all__ = ["error","open"]
 
 error = bsddb.error                     # Exported for anydbm
 
-def open(file, flag, mode=0666):
+def open(file, flag = 'r', mode=0666):
     return bsddb.hashopen(file, flag, mode)
