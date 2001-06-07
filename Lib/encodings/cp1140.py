581a7276e5dba8669c1d0f55a3a736f90be53daa
Martin v. Löwis <martin@v.loewis.de>
2001-06-07 19:39:25 +0000

    Patch #429957: Add support for cp1140, which is identical to cp037, with the addition of the euro character. Also added a few EDBDIC aliases.

---
+++ Lib/encodings/cp1140.py
@@ -0,0 +1,45 @@
+""" Python Character Mapping Codec for cp1140
+
+Written by Brian Quinlan(brian@sweetapp.com). NO WARRANTY.
+"""
+
+import codecs
+import copy
+import cp037
+
+### Codec APIs
+
+class Codec(codecs.Codec):
+
+    def encode(self,input,errors='strict'):
+
+        return codecs.charmap_encode(input,errors,encoding_map)
+        
+    def decode(self,input,errors='strict'):
+
+        return codecs.charmap_decode(input,errors,decoding_map)
+
+class StreamWriter(Codec,codecs.StreamWriter):
+    pass
+        
+class StreamReader(Codec,codecs.StreamReader):
+    pass
+
+### encodings module API
+
+def getregentry():
+
+    return (Codec().encode,Codec().decode,StreamReader,StreamWriter)
+
+### Decoding Map
+
+decoding_map = copy.copy(cp037.decoding_map)
+
+decoding_map.update({
+ 	0x009f: 0x20ac # EURO SIGN
+})
+
+### Encoding Map
+
+encoding_map = codecs.make_encoding_map(decoding_map)
+
