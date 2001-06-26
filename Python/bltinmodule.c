420cc143e88d427361abbdae414413ac07500cc7
Fredrik Lundh <fredrik@pythonware.com>
2001-06-26 17:46:10 +0000

    experimental UCS-4 support: don't assume that MS_WIN32 implies HAVE_USABLE_WCHAR_T

--- Python/bltinmodule.c
+++ Python/bltinmodule.c
@@ -16,7 +16,7 @@
 /* The default encoding used by the platform file system APIs
    Can remain NULL for all platforms that don't have such a concept
 */
-#ifdef MS_WIN32
+#if defined(MS_WIN32) && defined(HAVE_USABLE_WCHAR_T)
 const char *Py_FileSystemDefaultEncoding = "mbcs";
 #else
 const char *Py_FileSystemDefaultEncoding = NULL; /* use default */
