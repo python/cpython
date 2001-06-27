ac4094b31fe037623b020c8350b99fe1844d6b86
Thomas Wouters <thomas@python.org>
2001-06-27 13:04:24 +0000

    Backport of Tim's checkin 2.27:
    SF patch #418147 Fixes to allow compiling w/ Borland, from Stephen Hansen.

--- Include/pyport.h
+++ Include/pyport.h
@@ -434,6 +434,15 @@ typedef	struct fd_set {
 #endif
 #endif
 
+/*
+ * Rename some functions for the Borland compiler
+ */
+#ifdef __BORLANDC__
+#  include <io.h>
+#  define _chsize chsize
+#  define _setmode setmode
+#endif
+
 #ifdef __cplusplus
 }
 #endif
