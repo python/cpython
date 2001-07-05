7d8cf1f72885c5b1fa09100a2269bf3daecf8049
Guido van Rossum <guido@python.org>
2001-07-05 13:27:48 +0000

    Rip out the fancy behaviors of xrange that nobody uses: repeat, slice, contains, tolist(), and the start/stop/step attributes.  This includes removing the 4th ('repeat') argument to PyRange_New().

--- Include/rangeobject.h
+++ Include/rangeobject.h
@@ -19,7 +19,7 @@ extern DL_IMPORT(PyTypeObject) PyRange_Type;
 
 #define PyRange_Check(op) ((op)->ob_type == &PyRange_Type)
 
-extern DL_IMPORT(PyObject *) PyRange_New(long, long, long, int);
+extern DL_IMPORT(PyObject *) PyRange_New(long, long, long);
 
 #ifdef __cplusplus
 }
