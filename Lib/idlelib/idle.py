7711b5f4322b73ec6f7da519a7d80fde7608ea79
Kurt B. Kaiser <kbk@shore.net>
2001-07-14 05:21:37 +0000

    py-cvs-2001_07_13 (Rev 1.4) merge
    "Move the action of loading the configuration to the IdleConf module
    rather than the idle.py script.  This has advantages and
    disadvantages; the biggest advantage being that we can more easily
    have an alternative main program."  --GvR

--- Lib/idlelib/idle.py
+++ Lib/idlelib/idle.py
@@ -1,12 +1,4 @@
 #! /usr/bin/env python
 
-import os
-import sys
-import IdleConf
-
-idle_dir = os.path.dirname(IdleConf.__file__)
-IdleConf.load(idle_dir)
-
-# defer importing Pyshell until IdleConf is loaded
 import PyShell
 PyShell.main()
