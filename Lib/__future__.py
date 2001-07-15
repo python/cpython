56a76c32d7280110cf99b85f19fa4c1b7ee12ecd
Guido van Rossum <guido@python.org>
2001-07-15 21:08:29 +0000

    Preliminary support for "from __future__ import generators" to enable the yield statement.  I figure we have to have this in before I can release 2.2a1 on Wednesday.
    Note: test_generators is currently broken, I'm counting on Tim to fix
    this.

--- Lib/__future__.py
+++ Lib/__future__.py
@@ -67,3 +67,4 @@ class _Feature:
                             `self.getMandatoryRelease()` + ")"
 
 nested_scopes = _Feature((2, 1, 0, "beta", 1), (2, 2, 0, "alpha", 0))
+generators = _Feature((2, 2, 0, "alpha", 1), (2, 3, 0, "final", 0))
