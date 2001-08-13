3fd620b99893420985470b9f58c38b10d41147ab
Andrew M. Kuchling <amk@amk.ca>
2001-08-13 15:10:54 +0000

    Update e-mail address

--- Tools/compiler/setup.py
+++ Tools/compiler/setup.py
@@ -5,5 +5,5 @@ from distutils.core import setup
 setup(name = "compiler",
       version = "1.0",
       author = "Jeremy Hylton",
-      author_email = "jeremy@beopen.com",
+      author_email = "jeremy@zope.com",
       packages = ["compiler"])
