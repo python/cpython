e55e90c44ae871c232113725e7ac9031845cd599
Jack Jansen <jack.jansen@cwi.nl>
2001-08-19 22:13:00 +0000

    The modules with toolbox constants.

---
+++ Mac/Lib/Carbon/Lists.py
@@ -0,0 +1,32 @@
+# Generated from 'Lists.h'
+
+def FOUR_CHAR_CODE(x): return x
+listNotifyNothing = FOUR_CHAR_CODE('nada')
+listNotifyClick = FOUR_CHAR_CODE('clik')
+listNotifyDoubleClick = FOUR_CHAR_CODE('dblc')
+listNotifyPreClick = FOUR_CHAR_CODE('pclk')        
+lDoVAutoscrollBit = 1
+lDoHAutoscrollBit = 0
+lDoVAutoscroll = 2
+lDoHAutoscroll = 1
+lOnlyOneBit = 7
+lExtendDragBit = 6
+lNoDisjointBit = 5
+lNoExtendBit = 4
+lNoRectBit = 3
+lUseSenseBit = 2
+lNoNilHiliteBit = 1
+lOnlyOne = -128
+lExtendDrag = 64
+lNoDisjoint = 32
+lNoExtend = 16
+lNoRect = 8
+lUseSense = 4
+lNoNilHilite = 2
+lInitMsg = 0
+lDrawMsg = 1
+lHiliteMsg = 2
+lCloseMsg = 3
+kListDefUserProcType = 0
+kListDefStandardTextType = 1
+kListDefStandardIconType = 2
