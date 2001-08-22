60f018846d7b97eec71675c3a1c9e7a058232c73
Barry Warsaw <barry@python.org>
2001-08-22 19:24:42 +0000

    Merge changes from r22a2-branch back into trunk.  Also, change patch level to 2.2a2+

--- Lib/test/test_os.py
+++ Lib/test/test_os.py
@@ -6,6 +6,9 @@ import os
 import unittest
 import warnings
 
+warnings.filterwarnings("ignore", "tempnam", RuntimeWarning, __name__)
+warnings.filterwarnings("ignore", "tmpnam", RuntimeWarning, __name__)
+
 from test_support import TESTFN, run_unittest
 
 
