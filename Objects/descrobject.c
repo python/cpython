147b13c069b192a08774f86dea55d541d21c64ac
Guido van Rossum <guido@python.org>
2001-08-30 03:10:36 +0000

    Make getset subclassable.

--- Objects/descrobject.c
+++ Objects/descrobject.c
@@ -975,7 +975,7 @@ PyTypeObject PyGetSet_Type = {
 	PyObject_GenericGetAttr,		/* tp_getattro */
 	0,					/* tp_setattro */
 	0,					/* tp_as_buffer */
-	Py_TPFLAGS_DEFAULT,			/* tp_flags */
+	Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE, /* tp_flags */
  	getset_doc,				/* tp_doc */
  	0,					/* tp_traverse */
  	0,					/* tp_clear */
