ec71f38019771700a3786a5d152296a07edfdcda
Guido van Rossum <guido@python.org>
2001-09-06 09:54:47 +0000

    Skip instead of fail this test if the socket module has no ssl support.

--- Lib/test/test_socket_ssl.py
+++ Lib/test/test_socket_ssl.py
@@ -8,6 +8,9 @@ import test_support
 test_support.requires('network')
 
 import socket
+if not hasattr(socket, "ssl"):
+    raise test_support.TestSkipped("socket module has no ssl support")
+
 import urllib
 
 urllib.urlopen('https://sf.net')
