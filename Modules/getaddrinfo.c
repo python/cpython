86d58fa342427a1777874f179ebdb6352312e45e
Martin v. Löwis <martin@v.loewis.de>
2001-09-07 16:10:00 +0000

    Fix compiler warnings. This closes some of the #458880 problem.

--- Modules/getaddrinfo.c
+++ Modules/getaddrinfo.c
@@ -236,10 +236,8 @@ str_isnumber(p)
 }
 
 int
-getaddrinfo(hostname, servname, hints, res)
-	const char *hostname, *servname;
-	const struct addrinfo *hints;
-	struct addrinfo **res;
+getaddrinfo(const char*hostname, const char*servname,
+            const struct addrinfo *hints, struct addrinfo **res)
 {
 	struct addrinfo sentinel;
 	struct addrinfo *top = NULL;
@@ -367,15 +365,16 @@ getaddrinfo(hostname, servname, hints, res)
 			if ((sp = getservbyname(servname, proto)) == NULL)
 				ERR(EAI_SERVICE);
 			port = sp->s_port;
-			if (pai->ai_socktype == GAI_ANY)
+			if (pai->ai_socktype == GAI_ANY) {
 				if (strcmp(sp->s_proto, "udp") == 0) {
 					pai->ai_socktype = SOCK_DGRAM;
 					pai->ai_protocol = IPPROTO_UDP;
 				} else if (strcmp(sp->s_proto, "tcp") == 0) {
-					pai->ai_socktype = SOCK_STREAM;
-					pai->ai_protocol = IPPROTO_TCP;
-				} else
-					ERR(EAI_PROTOCOL);	/*xxx*/
+                                        pai->ai_socktype = SOCK_STREAM;
+                                        pai->ai_protocol = IPPROTO_TCP;
+                                } else
+                                        ERR(EAI_PROTOCOL);	/*xxx*/
+                        }
 		}
 	}
 	
