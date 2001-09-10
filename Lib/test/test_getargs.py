5c84659a8941635eea98edec8f3ae47f9052e079
Jeremy Hylton <jeremy@alum.mit.edu>
2001-09-10 01:57:12 +0000

    Test the failed-unicode-decoding bug in PyArg_ParseTuple().

---
+++ Lib/test/test_getargs.py
@@ -0,0 +1,24 @@
+"""Test the internal getargs.c implementation
+
+ PyArg_ParseTuple() is defined here.
+
+The test here is not intended to test all of the module, just the
+single case that failed between 2.1 and 2.2a2.
+"""
+
+# marshal.loads() uses PyArg_ParseTuple(args, "s#:loads")
+# The s code will cause a Unicode conversion to occur.  This test
+# verify that the error is propagated properly from the C code back to
+# Python.
+
+# XXX If the encoding succeeds using the current default encoding,
+# this test will fail because it does not test the right part of the
+# PyArg_ParseTuple() implementation.
+import marshal
+try:
+    marshal.loads(u"\222")
+except UnicodeError:
+    pass
+
+
+
