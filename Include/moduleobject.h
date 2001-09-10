965e13914a52207f9848544424434b48be8a807f
Guido van Rossum <guido@python.org>
2001-09-10 18:21:59 +0000

    PyModule_Check() now checks for subtype of module, as it should.

--- Include/moduleobject.h
+++ Include/moduleobject.h
@@ -9,7 +9,7 @@ extern "C" {
 
 extern DL_IMPORT(PyTypeObject) PyModule_Type;
 
-#define PyModule_Check(op) ((op)->ob_type == &PyModule_Type)
+#define PyModule_Check(op) PyObject_TypeCheck(op, &PyModule_Type)
 
 extern DL_IMPORT(PyObject *) PyModule_New(char *);
 extern DL_IMPORT(PyObject *) PyModule_GetDict(PyObject *);
