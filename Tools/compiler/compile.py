09b919a65a9392368d96f2c42ea472b9658b0e3f
Jeremy Hylton <jeremy@alum.mit.edu>
2001-09-17 18:08:40 +0000

    Add -p option to invoke Python profiler

--- Tools/compiler/compile.py
+++ Tools/compiler/compile.py
@@ -3,13 +3,14 @@ import getopt
 
 from compiler import compile, visitor
 
-##import profile
+import profile
 
 def main():
     VERBOSE = 0
     DISPLAY = 0
+    PROFILE = 0
     CONTINUE = 0
-    opts, args = getopt.getopt(sys.argv[1:], 'vqdc')
+    opts, args = getopt.getopt(sys.argv[1:], 'vqdcp')
     for k, v in opts:
         if k == '-v':
             VERBOSE = 1
@@ -24,6 +25,8 @@ def main():
             DISPLAY = 1
         if k == '-c':
             CONTINUE = 1
+        if k == '-p':
+            PROFILE = 1
     if not args:
         print "no files to compile"
     else:
@@ -31,9 +34,12 @@ def main():
             if VERBOSE:
                 print filename
             try:
-                compile(filename, DISPLAY)
-##                profile.run('compile(%s, %s)' % (`filename`, `DISPLAY`),
-##                            filename + ".prof")
+                if PROFILE:
+                    profile.run('compile(%s, %s)' % (`filename`, `DISPLAY`),
+                                filename + ".prof")
+                else:
+                    compile(filename, DISPLAY)
+                    
             except SyntaxError, err:
                 print err
                 print err.lineno
