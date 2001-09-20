b41d5c7bcb9426b5319b246c995691aaea3cfb29
Jeremy Hylton <jeremy@alum.mit.edu>
2001-09-20 15:02:27 +0000

    Remove setup.py, unnecessary since compiler package is the std library.
    Update README.

--- Tools/compiler/setup.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-from distutils.core import setup
-
-setup(name = "compiler",
-      version = "1.0",
-      author = "Jeremy Hylton",
-      author_email = "jeremy@zope.com",
-      packages = ["compiler"])
