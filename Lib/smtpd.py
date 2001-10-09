9127e72dc24a3eb3d3810b8bde6d8872aff7e52a
Barry Warsaw <barry@python.org>
2001-10-09 15:46:31 +0000

    SMTPServer.__init__(): The asyncore.dispatcher base class has a method set_reuse_addr() that does the setsockopt fiddling.  Use it instead.

--- Lib/smtpd.py
+++ Lib/smtpd.py
@@ -277,9 +277,7 @@ class SMTPServer(asyncore.dispatcher):
         asyncore.dispatcher.__init__(self)
         self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
         # try to re-use a server port if possible
-        self.socket.setsockopt(
-            socket.SOL_SOCKET, socket.SO_REUSEADDR,
-            self.socket.getsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR) | 1)
+        self.set_reuse_addr()
         self.bind(localaddr)
         self.listen(5)
         print >> DEBUGSTREAM, \
