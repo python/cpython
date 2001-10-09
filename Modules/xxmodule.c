ffa7aff751be2ba7d37bf631fa248b680cf48466
Martin v. Löwis <martin@v.loewis.de>
2001-10-09 10:46:58 +0000

    Add additional fields to Xxo_Type declaration.  Fixes bug #469250.

--- Modules/xxmodule.c
+++ Modules/xxmodule.c
@@ -112,6 +112,37 @@ statichere PyTypeObject Xxo_Type = {
 	0,			/*tp_as_sequence*/
 	0,			/*tp_as_mapping*/
 	0,			/*tp_hash*/
+        0,                      /*tp_call*/
+        0,                      /*tp_str*/
+        0,                      /*tp_getattro*/
+        0,                      /*tp_setattro*/
+        0,                      /*tp_as_buffer*/
+        Py_TPFLAGS_DEFAULT,     /*tp_flags*/
+        0,                      /*tp_doc*/
+        0,                      /*tp_traverse*/
+        0,                      /*tp_clear*/
+        0,                      /*tp_richcompare*/
+        0,                      /*tp_weaklistoffset*/
+        0,                      /*tp_iter*/
+        0,                      /*tp_iternext*/
+        0,                      /*tp_methods*/
+        0,                      /*tp_members*/
+        0,                      /*tp_getset*/
+        0,                      /*tp_base*/
+        0,                      /*tp_dict*/
+        0,                      /*tp_descr_get*/
+        0,                      /*tp_descr_set*/
+        0,                      /*tp_dictoffset*/
+        0,                      /*tp_init*/
+        0,                      /*tp_alloc*/
+        0,                      /*tp_new*/
+        0,                      /*tp_free*/
+        0,                      /*tp_is_gc*/
+        0,                      /*tp_bases*/
+        0,                      /*tp_mro*/
+        0,                      /*tp_defined*/
+        0,                      /*tp_subclasses*/
+        0,                      /*tp_weaklist*/
 };
 /* --------------------------------------------------------------------- */
 
