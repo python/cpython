3a40f32aa61c2ccd0799f25492c6b94cee651771
Fred Drake <fdrake@acm.org>
2001-10-12 21:00:48 +0000

    Add entry for HotShot.

--- setup.py
+++ setup.py
@@ -212,6 +212,7 @@ class PyBuildExt(build_ext):
         exts.append( Extension('regex', ['regexmodule.c', 'regexpr.c']) )
         exts.append( Extension('pcre', ['pcremodule.c', 'pypcre.c']) )
 
+        exts.append( Extension('_hotshot', ['_hotshot.c']) )
         exts.append( Extension('_weakref', ['_weakref.c']) )
         exts.append( Extension('_symtable', ['symtablemodule.c']) )
         exts.append( Extension('xreadlines', ['xreadlinesmodule.c']) )
