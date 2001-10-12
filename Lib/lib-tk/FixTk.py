ea32cbb24e24472f290f7575e494caacd1bfbe05
Guido van Rossum <guido@python.org>
2001-10-12 15:34:29 +0000

    Suggestion from SF patch #470433 to avoid clobbering TCL_LIBRARY et al. if already set.  Also adds TIX_LIBRARY (just in case). (Note that this is entirely Windows specific.)

--- Lib/lib-tk/FixTk.py
+++ Lib/lib-tk/FixTk.py
@@ -1,6 +1,11 @@
 import sys, os, _tkinter
+
 ver = str(_tkinter.TCL_VERSION)
-for t in "tcl", "tk":
-    v = os.path.join(sys.prefix, "tcl", t+ver)
-    if os.path.exists(os.path.join(v, "tclIndex")):
-        os.environ[t.upper() + "_LIBRARY"] = v
+for t in "tcl", "tk", "tix":
+    key = t.upper() + "_LIBRARY"
+    try:
+        v = os.environ[key]
+    except KeyError:
+        v = os.path.join(sys.prefix, "tcl", t+ver)
+        if os.path.exists(os.path.join(v, "tclIndex")):
+            os.environ[key] = v
