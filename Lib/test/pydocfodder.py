a7e1f43bd9c5940741d632bcfa8cf2e99b0205cd
Tim Peters <tim.peters@gmail.com>
2001-10-15 22:59:59 +0000

    Remove obsolete __dynamic__ distinction.

--- Lib/test/pydocfodder.py
+++ Lib/test/pydocfodder.py
@@ -78,10 +78,8 @@ class D_classic(B_classic, C_classic):
         "Method defined in D."
 
 
-class A_new_dynamic(object):
-    "A new-style dynamic class."
-
-    __dynamic__ = 1
+class A_new(object):
+    "A new-style class."
 
     def A_method(self):
         "Method defined in A."
@@ -119,10 +117,8 @@ class A_new_dynamic(object):
 
     A_int_alias = int
 
-class B_new_dynamic(A_new_dynamic):
-    "A new-style dynamic class, derived from A_new_dynamic."
-
-    __dynamic__ = 1
+class B_new(A_new):
+    "A new-style class, derived from A_new."
 
     def AB_method(self):
         "Method defined in A and B."
@@ -141,10 +137,8 @@ class B_new_dynamic(A_new_dynamic):
     def BCD_method(self):
         "Method defined in B, C and D."
 
-class C_new_dynamic(A_new_dynamic):
-    "A new-style dynamic class, derived from A_new_dynamic."
-
-    __dynamic__ = 1
+class C_new(A_new):
+    "A new-style class, derived from A_new."
 
     def AC_method(self):
         "Method defined in A and C."
@@ -163,121 +157,9 @@ class C_new_dynamic(A_new_dynamic):
     def CD_method(self):
         "Method defined in C and D."
 
-class D_new_dynamic(B_new_dynamic, C_new_dynamic):
-    """A new-style dynamic class, derived from B_new_dynamic and
-       C_new_dynamic.
+class D_new(B_new, C_new):
+    """A new-style class, derived from B_new and C_new.
     """
-
-    __dynamic__ = 1
-
-    def AD_method(self):
-        "Method defined in A and D."
-    def ABD_method(self):
-        "Method defined in A, B and D."
-    def ACD_method(self):
-        "Method defined in A, C and D."
-    def ABCD_method(self):
-        "Method defined in A, B, C and D."
-    def BD_method(self):
-        "Method defined in B and D."
-    def BCD_method(self):
-        "Method defined in B, C and D."
-    def CD_method(self):
-        "Method defined in C and D."
-    def D_method(self):
-        "Method defined in D."
-
-
-class A_new_static(object):
-    "A new-style static class."
-
-    __dynamic__ = 0
-
-    def A_method(self):
-        "Method defined in A."
-    def AB_method(self):
-        "Method defined in A and B."
-    def AC_method(self):
-        "Method defined in A and C."
-    def AD_method(self):
-        "Method defined in A and D."
-    def ABC_method(self):
-        "Method defined in A, B and C."
-    def ABD_method(self):
-        "Method defined in A, B and D."
-    def ACD_method(self):
-        "Method defined in A, C and D."
-    def ABCD_method(self):
-        "Method defined in A, B, C and D."
-
-    def A_classmethod(cls, x):
-        "A class method defined in A."
-    A_classmethod = classmethod(A_classmethod)
-
-    def A_staticmethod():
-        "A static method defined in A."
-    A_staticmethod = staticmethod(A_staticmethod)
-
-    def _getx(self):
-        "A property getter function."
-    def _setx(self, value):
-        "A property setter function."
-    def _delx(self):
-        "A property deleter function."
-    A_property = property(fdel=_delx, fget=_getx, fset=_setx,
-                          doc="A sample property defined in A.")
-
-    A_int_alias = int
-
-
-class B_new_static(A_new_static):
-    "A new-style static class, derived from A_new_static."
-
-    __dynamic__ = 0
-
-    def AB_method(self):
-        "Method defined in A and B."
-    def ABC_method(self):
-        "Method defined in A, B and C."
-    def ABD_method(self):
-        "Method defined in A, B and D."
-    def ABCD_method(self):
-        "Method defined in A, B, C and D."
-    def B_method(self):
-        "Method defined in B."
-    def BC_method(self):
-        "Method defined in B and C."
-    def BD_method(self):
-        "Method defined in B and D."
-    def BCD_method(self):
-        "Method defined in B, C and D."
-
-class C_new_static(A_new_static):
-    "A new-style static class, derived from A_new_static."
-
-    __dynamic__ = 0
-
-    def AC_method(self):
-        "Method defined in A and C."
-    def ABC_method(self):
-        "Method defined in A, B and C."
-    def ACD_method(self):
-        "Method defined in A, C and D."
-    def ABCD_method(self):
-        "Method defined in A, B, C and D."
-    def BC_method(self):
-        "Method defined in B and C."
-    def BCD_method(self):
-        "Method defined in B, C and D."
-    def C_method(self):
-        "Method defined in C."
-    def CD_method(self):
-        "Method defined in C and D."
-
-class D_new_static(B_new_static, C_new_static):
-    "A new-style static class, derived from B_new_static and C_new_static."
-
-    __dynamic__ = 0
 
     def AD_method(self):
         "Method defined in A and D."
