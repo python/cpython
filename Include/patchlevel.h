9978957937bb2cb612782b6832d0d1b6946880bf
Guido van Rossum <guido@python.org>
2001-10-19 14:47:47 +0000

    Add a '+' to the version to indicate this is a post-2.1.1-release CVS version.

--- Include/patchlevel.h
+++ Include/patchlevel.h
@@ -26,10 +26,10 @@
 #define PY_RELEASE_SERIAL	0
 
 /* Version as a string */
-#define PY_VERSION		"2.1.1"
+#define PY_VERSION		"2.1.1+"
 
 /* Historic */
-#define PATCHLEVEL		"2.1.1"
+#define PATCHLEVEL		"2.1.1+"
 
 /* Version as a single 4-byte hex number, e.g. 0x010502B2 == 1.5.2b2.
    Use this for numeric comparisons, e.g. #if PY_VERSION_HEX >= ... */
