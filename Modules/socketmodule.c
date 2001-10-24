861a65bc2fa83485bdc66f86cb13d4ad4798a68d
Martin v. Löwis <martin@v.loewis.de>
2001-10-24 14:36:00 +0000

    Include netdb.h to detect getaddrinfo. Work around problem with getaddrinfo not properly processing numeric IPv4 addresses. Fixes V5.1 part of #472675.

--- Modules/socketmodule.c
+++ Modules/socketmodule.c
@@ -622,6 +622,12 @@ setipaddr(char* name, struct sockaddr * addr_ret, int af)
 	memset(&hints, 0, sizeof(hints));
 	hints.ai_family = af;
 	error = getaddrinfo(name, NULL, &hints, &res);
+        if (error = EAI_NONAME && af == AF_UNSPEC) {
+          /* On OSF/1 V5.1, numeric-to-addr conversion
+             fails if no address family is given. Assume IPv4 for now.*/
+          hints.ai_family = AF_INET;
+          error = getaddrinfo(name, NULL, &hints, &res);
+        }
 	if (error) {
 		PyGAI_Err(error);
 		return -1;
