fa699126b5e1c649365aacbedb17a1b69e69bb49
Guido van Rossum <guido@python.org>
2001-10-30 03:17:30 +0000

    Fix SF bug #456386: test_commands regression failure (Andrew Dalke)
        test_commands does not work on IRIX
    
        It assumes the output of "ls /bin/ls" is a line
        that starts with a '-'. On IRIX that file is
        a symbolic link, so the first character is an l.
        This causes test_getstatus to fail.

--- Lib/test/test_commands.py
+++ Lib/test/test_commands.py
@@ -32,7 +32,7 @@ class CommandTests(unittest.TestCase):
     def test_getstatus(self):
         # This pattern should match 'ls -ld /bin/ls' on any posix
         # system, however perversely configured.
-        pat = r'''-..x..x..x   # It is executable.
+        pat = r'''[l-]..x..x..x # It is executable. (May be a symlink.)
                   \s+\d+       # It has some number of links.
                   \s+\w+\s+\w+ # It has a user and group, which may
                                #     be named anything.
