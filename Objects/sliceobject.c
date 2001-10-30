5c48f9f6ecf0c4dcda0b27e9d3e394ecb4f1971f
Guido van Rossum <guido@python.org>
2001-10-30 02:40:52 +0000

    Add values to tp_getattro and tp_flags so that dir(Ellipsis) will return the same as dir(None).

--- Objects/sliceobject.c
+++ Objects/sliceobject.c
@@ -24,20 +24,26 @@ ellipsis_repr(PyObject *op)
 
 static PyTypeObject PyEllipsis_Type = {
 	PyObject_HEAD_INIT(&PyType_Type)
-	0,
-	"ellipsis",
-	0,
-	0,
-	0,		/*tp_dealloc*/ /*never called*/
-	0,		/*tp_print*/
-	0,		/*tp_getattr*/
-	0,		/*tp_setattr*/
-	0,		/*tp_compare*/
-	(reprfunc)ellipsis_repr, /*tp_repr*/
-	0,		/*tp_as_number*/
-	0,		/*tp_as_sequence*/
-	0,		/*tp_as_mapping*/
-	0,		/*tp_hash */
+	0,					/* ob_size */
+	"ellipsis",				/* tp_name */
+	0,					/* tp_basicsize */
+	0,					/* tp_itemsize */
+	0, /*never called*/			/* tp_dealloc */
+	0,					/* tp_print */
+	0,					/* tp_getattr */
+	0,					/* tp_setattr */
+	0,					/* tp_compare */
+	(reprfunc)ellipsis_repr,		/* tp_repr */
+	0,					/* tp_as_number */
+	0,					/* tp_as_sequence */
+	0,					/* tp_as_mapping */
+	0,					/* tp_hash */
+	0,					/* tp_call */
+	0,					/* tp_str */
+	PyObject_GenericGetAttr,		/* tp_getattro */
+	0,					/* tp_setattro */
+	0,					/* tp_as_buffer */
+	Py_TPFLAGS_DEFAULT,			/* tp_flags */
 };
 
 PyObject _Py_EllipsisObject = {
