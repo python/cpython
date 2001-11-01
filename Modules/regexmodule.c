0af83c053c90a3b82c63050769b278bb6b69681c
Anthony Baxter <anthonybaxter@gmail.com>
2001-11-01 14:55:03 +0000

    backport of (merged) 1.43 and 1.44   initregex():  this function is declared void, so the recent change to   return NULL in an error case was itself an error.   initregex(): Check return value of PyErr_Warn() and propagate the exception   (if any.)

--- Modules/regexmodule.c
+++ Modules/regexmodule.c
@@ -660,8 +660,10 @@ initregex(void)
 	m = Py_InitModule("regex", regex_global_methods);
 	d = PyModule_GetDict(m);
 
-	PyErr_Warn(PyExc_DeprecationWarning,
-		"the regex module is deprecated; please use the re module");
+	if (PyErr_Warn(PyExc_DeprecationWarning,
+		       "the regex module is deprecated; "
+		       "please use the re module") < 0)
+		return;
 	
 	/* Initialize regex.error exception */
 	v = RegexError = PyErr_NewException("regex.error", NULL, NULL);
