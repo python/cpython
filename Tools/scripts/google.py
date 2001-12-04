f52a7b28dfd032f8b79a9a0744841559d2fc4c6f
Guido van Rossum <guido@python.org>
2001-12-04 15:23:47 +0000

    A tiny but useful script that fires off a search on Google. (Not sure if this is legal according to the Google terms of service. :-)

---
+++ Tools/scripts/google.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python
+
+import sys, webbrowser
+
+def main():
+    args = sys.argv[1:]
+    if not args:
+        print "Usage: %s querystring" % sys.argv[0]
+        return
+    list = []
+    for arg in args:
+        if '+' in arg:
+            arg = arg.replace('+', '%2B')
+        if ' ' in arg:
+            arg = '"%s"' % arg
+        arg = arg.replace(' ', '+')
+        list.append(arg)
+    s = '+'.join(list)
+    url = "http://google.com/search?q=%s" % s
+    webbrowser.open(url)
+
+main()
