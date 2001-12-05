34b8851c5fefb8bc8e99ac07c5af9d4202da329d
Steven M. Gava <elguavas@python.net>
2001-12-05 06:39:18 +0000

    remove cruft from other project

--- Lib/idlelib/tabpage.py
+++ Lib/idlelib/tabpage.py
@@ -1,30 +1,3 @@
-##---------------------------------------------------------------------------##
-##
-## pyChing -- a Python program to cast and interpret I Ching hexagrams
-##
-## Copyright (C) 1999,2000 Stephen M. Gava
-##
-## This program is free software; you can redistribute it and/or modify
-## it under the terms of the GNU General Public License as published by
-## the Free Software Foundation; either version 2 of the License, or
-## (at your option) any later version.
-##
-## This program is distributed in the hope that it will be of some
-## interest to somebody, but WITHOUT ANY WARRANTY; without even the 
-## implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-## See the GNU General Public License for more details.
-##
-## You should have received a copy of the GNU General Public License
-## along with this program; see the file COPYING or COPYING.txt. If not, 
-##  write to the Free Software Foundation, Inc.,
-## 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
-## The license can also be found at the GNU/FSF website: http://www.gnu.org
-##
-## Stephen M. Gava
-## <elguavas@users.sourceforge.net>
-## http://pyching.sourgeforge.net
-##
-##---------------------------------------------------------------------------##
 """
 a couple of classes for implementing partial tabbed-page like behaviour
 """
@@ -116,7 +89,7 @@ class TabPageSet(Frame):
         self.ChangePage()
 
 if __name__ == '__main__':
-    #test the dialog
+    #test dialog
     root=Tk()
     tabPage=TabPageSet(root,pageNames=['Foobar','Baz'])
     tabPage.pack(expand=TRUE,fill=BOTH)
