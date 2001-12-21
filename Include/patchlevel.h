1c7c18a960c25fbd000d545a4586ef5b33cc4ca3
Barry Warsaw <barry@python.org>
2001-12-21 20:05:33 +0000

    And we start all over again!

--- Include/patchlevel.h
+++ Include/patchlevel.h
@@ -20,13 +20,13 @@
 
 /* Version parsed out into numeric values */
 #define PY_MAJOR_VERSION	2
-#define PY_MINOR_VERSION	2
+#define PY_MINOR_VERSION	3
 #define PY_MICRO_VERSION	0
-#define PY_RELEASE_LEVEL	PY_RELEASE_LEVEL_GAMMA
+#define PY_RELEASE_LEVEL	PY_RELEASE_LEVEL_ALPHA
 #define PY_RELEASE_SERIAL	1
 
 /* Version as a string */
-#define PY_VERSION		"2.2c1+"
+#define PY_VERSION		"2.2+"
 
 /* Version as a single 4-byte hex number, e.g. 0x010502B2 == 1.5.2b2.
    Use this for numeric comparisons, e.g. #if PY_VERSION_HEX >= ... */
