f1472384dcf23f771e171dbf99b65bded0bf0631
Jack Jansen <jack.jansen@cwi.nl>
2002-01-01 22:39:07 +0000

    Updated to current pathnames.

--- Tools/bgen/bgen/bgenlocations.py
+++ Tools/bgen/bgen/bgenlocations.py
@@ -1,13 +1,13 @@
 #
 # Local customizations
 #
-
+import sys, os
 # Where to find the Universal Header include files:
-MWERKSDIR="Macintosh HD:SWDev:Codewarrior Pro 5:Metrowerks CodeWarrior:"
+MWERKSDIR="Moes:Applications (Mac OS 9):Metrowerks CodeWarrior 7.0:Metrowerks CodeWarrior:"
 INCLUDEDIR=MWERKSDIR + "MacOS Support:Universal:Interfaces:CIncludes:"
 
 # Where to put the python definitions file:
-TOOLBOXDIR="Macintosh HD:SWDev:Jack:Python:Mac:Lib:lib-toolbox:"
+TOOLBOXDIR=os.path.join(sys.prefix, ":Mac:Lib:Carbon:")
 
 # Creator for C files:
 CREATOR="CWIE"
