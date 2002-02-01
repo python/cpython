5f67a59ea4adeac4d422548d4d51148426873391
Jack Jansen <jack.jansen@cwi.nl>
2002-02-01 15:46:29 +0000

    Got rid of a few more NeXT ifdefs. The last, I think.

--- Include/pyport.h
+++ Include/pyport.h
@@ -453,8 +453,7 @@ typedef	struct fd_set {
  * Hide GCC attributes from compilers that don't support them.
  */
 #if (!defined(__GNUC__) || __GNUC__ < 2 || \
-     (__GNUC__ == 2 && __GNUC_MINOR__ < 7) || \
-     defined(NEXT) ) && \
+     (__GNUC__ == 2 && __GNUC_MINOR__ < 7) ) && \
     !defined(RISCOS)
 #define __attribute__(__x)
 #endif
