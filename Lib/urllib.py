b2056e1b909acc7f0d14cb2a9339e7a67eb9b791
Neal Norwitz <nnorwitz@gmail.com>
2002-02-11 18:06:21 +0000

    SF #515024 remove unused variable

--- Lib/urllib.py
+++ Lib/urllib.py
@@ -1158,7 +1158,6 @@ def urlencode(query,doseq=0):
         # sequences...
         try:
             # non-sequence items should not work with len()
-            x = len(query)
             # non-empty strings will fail this
             if len(query) and type(query[0]) != types.TupleType:
                 raise TypeError
