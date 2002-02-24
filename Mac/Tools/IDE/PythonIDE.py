c7d53629e78503c9fed481525ab5d976b24a0b79
Jack Jansen <jack.jansen@cwi.nl>
2002-02-24 23:16:25 +0000

    Backport of 1.9: Changes by Donovan Preston (and a few minor ones by me) to make IDE run under MachoPython. Mainly making sure we don't call routines that don't exist and representing pathnames in a os.separator-neutral format.
    These shouldn't interfere too much with Just's work on the next generation IDE,
    I hope.

--- Mac/Tools/IDE/PythonIDE.py
+++ Mac/Tools/IDE/PythonIDE.py
@@ -4,11 +4,13 @@
 # it like the "normal" interpreter.
 
 __version__ = '1.0.1'
-
+import sys
+import os
 
 def init():
 	import MacOS
-	MacOS.EnableAppswitch(-1)
+	if hasattr(MacOS, 'EnableAppswitch'):
+		MacOS.EnableAppswitch(-1)
 	
 	from Carbon import Qd, QuickDraw
 	Qd.SetCursor(Qd.GetCursor(QuickDraw.watchCursor).data)
@@ -16,11 +18,13 @@ def init():
 	import macresource
 	import sys, os
 	macresource.need('DITL', 468, "PythonIDE.rsrc")
-	widgetresfile = os.path.join(sys.exec_prefix, ":Mac:Tools:IDE:Widgets.rsrc")
+	widgetrespathsegs = [sys.exec_prefix, "Mac", "Tools", "IDE", "Widgets.rsrc"]
+	widgetresfile = os.path.join(*widgetrespathsegs)
 	refno = macresource.need('CURS', 468, widgetresfile)
 	if refno:
 		# We're not a fullblown application
-		ide_path = os.path.join(sys.exec_prefix, ":Mac:Tools:IDE")
+		idepathsegs = [sys.exec_prefix, "Mac", "Tools", "IDE"]
+		ide_path = os.path.join(*idepathsegs)
 	else:
 		# We are a fully frozen application
 		ide_path = sys.argv[0]
