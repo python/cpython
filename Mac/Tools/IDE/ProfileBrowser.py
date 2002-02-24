fef4a032e494ee7795775d024d0f0f4ab646bbab
Jack Jansen <jack.jansen@cwi.nl>
2002-02-24 23:10:14 +0000

    backport of 1.3: made radio button labels readable under OSX

--- Mac/Tools/IDE/ProfileBrowser.py
+++ Mac/Tools/IDE/ProfileBrowser.py
@@ -30,16 +30,17 @@ class ProfileBrowser:
 		self.w.divline = W.HorizontalLine((0, 20, 0, 0))
 		self.w.titlebar = W.TextBox((4, 4, 40, 12), 'Sort by:')
 		self.buttons = []
-		self.w.button_calls = W.RadioButton((54, 4, 45, 12), 'calls', self.buttons, self.setsort)
-		self.w.button_time = W.RadioButton((104, 4, 40, 12), 'time', self.buttons, self.setsort)
-		self.w.button_cumulative = W.RadioButton((154, 4, 75, 12), 'cumulative', self.buttons, self.setsort)
-		self.w.button_stdname = W.RadioButton((234, 4, 60, 12), 'stdname', self.buttons, self.setsort)
+		x = 54
+		width1 = 50
+		width2 = 75
+		for name in ["calls", "time", "cumulative", "stdname", "file", "line", "name"]:
+			if len(name) > 6:
+				width = width2
+			else:
+				width = width1
+			self.w["button_" + name] = W.RadioButton((x, 4, width, 12), name, self.buttons, self.setsort)
+			x += width + 10
 		self.w.button_calls.set(1)
-		self.w.button_file = W.RadioButton((304, 4, 40, 12), 'file', self.buttons, self.setsort)
-		self.w.button_line = W.RadioButton((354, 4, 50, 12), 'line', self.buttons, self.setsort)
-		self.w.button_name = W.RadioButton((404, 4, 50, 12), 'name', self.buttons, self.setsort)
-##		self.w.button_nfl = W.RadioButton((4, 4, 12, 12), 'nfl', self.buttons, self.setsort)
-##		self.w.button_pcalls = W.RadioButton((4, 4, 12, 12), 'pcalls', self.buttons, self.setsort)
 		self.w.text = W.TextEditor((0, 21, -15, -15), inset = (6, 5), 
 				readonly = 1, wrap = 0, fontsettings = ('Monaco', 0, 9, (0, 0, 0)))
 		self.w._bary = W.Scrollbar((-15, 20, 16, -14), self.w.text.vscroll, max = 32767)
