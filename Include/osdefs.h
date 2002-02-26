5e090fc98525308d2a86eaf5c535c3c2ba1a644c
Andrew MacIntyre <andymac@bullseye.apana.org.au>
2002-02-26 11:20:01 +0000

    OS/2 EMX port changes (Include part of patch #450267):   Include/     osdefs.h  // EMX promotes Un*x path separators     pyport.h

--- Include/osdefs.h
+++ Include/osdefs.h
@@ -17,9 +17,15 @@ extern "C" {
 /* Mod by chrish: QNX has WATCOM, but isn't DOS */
 #if !defined(__QNX__)
 #if defined(MS_WINDOWS) || defined(__BORLANDC__) || defined(__WATCOMC__) || defined(__DJGPP__) || defined(PYOS_OS2)
+#if defined(PYOS_OS2) && defined(PYCC_GCC)
+#define MAXPATHLEN 260
+#define SEP '/'
+#define ALTSEP '\\'
+#else
 #define SEP '\\'
 #define ALTSEP '/'
 #define MAXPATHLEN 256
+#endif
 #define DELIM ';'
 #endif
 #endif
