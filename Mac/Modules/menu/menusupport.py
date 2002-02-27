caa994db594f172c7b89273b30e6c9ab2b09d0fe
Jack Jansen <jack.jansen@cwi.nl>
2002-02-27 23:15:29 +0000

    Backport of 1.8-1.10 (of _Menumodule.c): - The output MenuRef of GetMenuItemHierarchicalMenu() may be NULL. - Added support for optional MenuObj arguments - Added a bunch of calls as functions with an optional   MenuObj first argument. The same calls already   exist as methods, but then the first arg isn't   optional... The method versions could go as far as I'm   concerned. Jack?

--- Mac/Modules/menu/menusupport.py
+++ Mac/Modules/menu/menusupport.py
@@ -24,6 +24,7 @@ from macsupport import *
 
 MenuHandle = OpaqueByValueType(OBJECTTYPE, OBJECTPREFIX)
 MenuRef = MenuHandle
+OptMenuRef = OpaqueByValueType(OBJECTTYPE, "Opt" + OBJECTPREFIX)
 Handle = OpaqueByValueType("Handle", "ResObj")
 MenuBarHandle = OpaqueByValueType("MenuBarHandle", "ResObj")
 MenuID = Type("MenuID", "h")
@@ -68,6 +69,29 @@ extern int _MenuObj_Convert(PyObject *, MenuHandle *);
 
 #define as_Menu(h) ((MenuHandle)h)
 #define as_Resource(h) ((Handle)h)
+
+
+/* Alternative version of MenuObj_New, which returns None for NULL argument */
+PyObject *OptMenuObj_New(MenuRef itself)
+{
+	if (itself == NULL) {
+		Py_INCREF(Py_None);
+		return Py_None;
+	}
+	return MenuObj_New(itself);
+}
+
+/* Alternative version of MenuObj_Convert, which returns NULL for a None argument */
+int OptMenuObj_Convert(PyObject *v, MenuRef *p_itself)
+{
+	PyObject *tmp;
+	
+	if ( v == Py_None ) {
+		*p_itself = NULL;
+		return 1;
+	}
+	return MenuObj_Convert(v, p_itself);
+}
 """
 
 initstuff = initstuff + """
