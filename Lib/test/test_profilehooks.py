290d31e2fc02a0d887da2c76fbe4e72377442a0a
Neal Norwitz <nnorwitz@gmail.com>
2002-03-03 15:12:58 +0000

    SF #506611, fix sys.setprofile(), sys.settrace() core dumps when no arguments are passed

--- Lib/test/test_profilehooks.py
+++ Lib/test/test_profilehooks.py
@@ -1,5 +1,7 @@
 from __future__ import generators
 
+from test_support import TestFailed
+
 import pprint
 import sys
 import unittest
@@ -329,6 +331,10 @@ protect_ident = ident(protect)
 
 
 def capture_events(callable, p=None):
+    try: sys.setprofile()
+    except TypeError: pass
+    else: raise TestFailed, 'sys.setprofile() did not raise TypeError'
+
     if p is None:
         p = HookWatcher()
     sys.setprofile(p.callback)
