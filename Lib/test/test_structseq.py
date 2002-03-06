145206ebd61c8a11f32c9277a33c8e031fe5c88a
Michael W. Hudson <mwh@python.net>
2002-03-06 17:18:15 +0000

    Test for
    [ 526039 ] devious code can crash structseqs
    
    Bugfix candidate.

--- Lib/test/test_structseq.py
+++ Lib/test/test_structseq.py
@@ -13,4 +13,16 @@ for i in range(-len(t), len(t)):
     for j in range(-len(t), len(t)):
         vereq(t[i:j], astuple[i:j])
 
+# Devious code could crash structseqs' contructors
+class C:
+    def __getitem__(self, i):
+        raise IndexError
+    def __len__(self):
+        return 9
+
+try:
+    repr(time.struct_time(C()))
+except:
+    pass
+
 # XXX more needed
