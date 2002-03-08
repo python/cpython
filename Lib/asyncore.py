be26accac519318894539d298793c80ddea8cda2
Andrew M. Kuchling <amk@amk.ca>
2002-03-08 18:19:59 +0000

    [Bug #517554] When a signal happens during the select call in 	asyncore.poll, the select fails with EINTR, which the 	code catches. However, the code fails to clear the 	r/w/e arrays (like poll3 does), which means it acts as 	if every descriptor had received all possible events.
    Bug report and patch by Cesar Eduardo Barros

--- Lib/asyncore.py
+++ Lib/asyncore.py
@@ -80,6 +80,7 @@ def poll (timeout=0.0, map=None):
         except select.error, err:
             if err[0] != EINTR:
                 raise
+            r = []; w = []; e = []
 
         if DEBUG:
             print r,w,e
