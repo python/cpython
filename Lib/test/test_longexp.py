e89f128a604d157254a873b4901b83f97272a999
Jack Jansen <jack.jansen@cwi.nl>
2002-03-15 13:50:54 +0000

    Skip test_longexp for MacPython on Mac OS X. It triggers a pathological realloc slowdown. Some tests with shorter expressions lead me to the conclusion that it will eventually finish, but it may take a few weeks:-)
    2.2.1 candidate.

--- Lib/test/test_longexp.py
+++ Lib/test/test_longexp.py
@@ -1,10 +1,14 @@
 # this test has a malloc problem on OS/2+EMX, so skip test in that environment
 
 import sys
-from test_support import TestFailed
+from test_support import TestFailed, TestSkipped
 
 REPS = 65580
 
+if sys.platform == 'mac':
+	import gestalt
+	if gestalt.gestalt('sysv') > 0x9ff:
+		raise TestSkipped, 'Triggers pathological malloc slowdown on OSX MacPython'
 if sys.platform == "os2emx":
     raise TestFailed, "OS/2+EMX port has malloc problems with long expressions"
 else:
