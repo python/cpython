eae11ba4d2237ab568289be3b79b85c493be3b9a
Michael W. Hudson <mwh@python.net>
2002-03-16 18:33:31 +0000

    Merge jackjansen's checking of revision 1.5.

--- Lib/test/test_longexp.py
+++ Lib/test/test_longexp.py
@@ -1,4 +1,12 @@
+import sys
+from test_support import TestSkipped
+
 REPS = 65580
+
+if sys.platform == 'mac':
+    import gestalt
+    if gestalt.gestalt('sysv') > 0x9ff:
+        raise TestSkipped, 'Triggers pathological malloc slowdown on OSX MacPython'
 
 l = eval("[" + "2," * REPS + "]")
 print len(l)
