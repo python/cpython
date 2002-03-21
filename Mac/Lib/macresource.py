77141d02de6888adf5d94619886aabcbba6b52d9
Jack Jansen <jack.jansen@cwi.nl>
2002-03-21 22:38:32 +0000

    Added an open_pathname() method which opens a resource file by pathname, possibly converting from AppleSingle.

--- Mac/Lib/macresource.py
+++ Mac/Lib/macresource.py
@@ -61,6 +61,18 @@ def need(restype, resid, filename=None, modname=None):
 	else:
 		raise ResourceFileNotFoundError, filename
 	
+	refno = open_pathname(pathname)
+	
+	# And check that the resource exists now
+	if type(resid) is type(1):
+		h = Res.GetResource(restype, resid)
+	else:
+		h = Res.GetNamedResource(restype, resid)
+	return refno
+	
+def open_pathname(pathname):
+	"""Open a resource file given by pathname, possibly decoding an
+	AppleSingle file"""
 	try:
 		refno = Res.FSpOpenResFile(pathname, 1)
 	except Res.Error, arg:
@@ -72,13 +84,6 @@ def need(restype, resid, filename=None, modname=None):
 				refno = Res.FSOpenResourceFile(pathname, u'', 1)
 			else:
 				raise
-				
-	
-	# And check that the resource exists now
-	if type(resid) is type(1):
-		h = Res.GetResource(restype, resid)
-	else:
-		h = Res.GetNamedResource(restype, resid)
 	return refno
 	
 def _decode(pathname):
