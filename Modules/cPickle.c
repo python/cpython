fa79c6523508cc908780b16ddfb20b03c2ab4aff
Neil Schemenauer <nascheme@enme.ucalgary.ca>
2002-03-22 23:02:53 +0000

    Match behavior of the pickle.py module more closely.

--- Modules/cPickle.c
+++ Modules/cPickle.c
@@ -3843,6 +3843,11 @@ load(Unpicklerobject *self) {
                     break;
                 continue;
 
+            case '\0':
+                /* end of file */
+                PyErr_SetNone(PyExc_EOFError);
+                break;
+                
             default:
                 cPickle_ErrFormat(UnpicklingError, "invalid load key, '%s'.",
                     "c", s[0]);
