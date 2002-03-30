fab12108941e195d14b4c10a5bd0ab2d13df3fe7
Jack Jansen <jack.jansen@cwi.nl>
2002-03-30 23:43:36 +0000

    Add Mac/Lib/lib-scriptpackages to sys.path too.

--- Mac/OSX/sample_sitecustomize.py
+++ Mac/OSX/sample_sitecustomize.py
@@ -1,4 +1,6 @@
 import sys
 import os
-_maclib = os.path.join(sys.prefix, 'Mac/Lib')
+_maclib = os.path.join(sys.prefix, 'Mac', 'Lib')
+_scriptlib = os.path.join(_maclib, 'lib-scriptpackages')
 sys.path.append(_maclib)
+sys.path.append(_scriptlib)
