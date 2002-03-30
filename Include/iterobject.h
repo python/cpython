522cf1f6fb2dab78702af1f1f06b0855fab75149
Martin v. Löwis <martin@v.loewis.de>
2002-03-30 08:57:12 +0000

    Patch #536908: Add missing #include guards/extern "C".

--- Include/iterobject.h
+++ Include/iterobject.h
@@ -1,4 +1,9 @@
+#ifndef Py_ITEROBJECT_H
+#define Py_ITEROBJECT_H
 /* Iterators (the basic kind, over a sequence) */
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 extern DL_IMPORT(PyTypeObject) PySeqIter_Type;
 
@@ -11,3 +16,8 @@ extern DL_IMPORT(PyTypeObject) PyCallIter_Type;
 #define PyCallIter_Check(op) ((op)->ob_type == &PyCallIter_Type)
 
 extern DL_IMPORT(PyObject *) PyCallIter_New(PyObject *, PyObject *);
+#ifdef __cplusplus
+}
+#endif
+#endif /* !Py_ITEROBJECT_H */
+
