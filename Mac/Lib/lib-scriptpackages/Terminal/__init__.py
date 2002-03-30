d7f8f616f8be43b0cf5e4a3773383d54130ac406
Jack Jansen <jack.jansen@cwi.nl>
2002-03-30 23:46:16 +0000

    Suite to talk to the OSX Terminal application.

---
+++ Mac/Lib/lib-scriptpackages/Terminal/__init__.py
@@ -0,0 +1,28 @@
+"""
+Package generated from /Applications/Utilities/Terminal.app/Contents/Resources/Terminal.rsrc
+Resource aete resid 0 Terminal Terminology
+"""
+# Note: hand-edited _signature to 'trmx'.
+
+import aetools
+Error = aetools.Error
+import Terminal_Suite
+
+
+_code_to_module = {
+	'trmx' : Terminal_Suite,
+}
+
+
+
+_code_to_fullname = {
+	'trmx' : ('Terminal.Terminal_Suite', 'Terminal_Suite'),
+}
+
+from Terminal_Suite import *
+
+
+class Terminal(Terminal_Suite_Events,
+		aetools.TalkTo):
+	_signature = 'trmx'
+
