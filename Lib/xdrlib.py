ba786f6c1f0b44641ac3d19021c70eac9babd8b9
Neal Norwitz <nnorwitz@gmail.com>
2002-03-31 13:59:18 +0000

    Derive exception classes from Exception

--- Lib/xdrlib.py
+++ Lib/xdrlib.py
@@ -13,7 +13,7 @@ except ImportError:
 __all__ = ["Error", "Packer", "Unpacker", "ConversionError"]
 
 # exceptions
-class Error:
+class Error(Exception):
     """Exception class for this module. Use:
 
     except xdrlib.Error, var:
