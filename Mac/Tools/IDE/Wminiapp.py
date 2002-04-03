e9c0358bf45bd6e0fe0b17720b41d20d618e6d9d
Jack Jansen <jack.jansen@cwi.nl>
2002-04-03 21:52:10 +0000

    e macresource.need() to open the W resource file in the application init code (if it isn't open already).
    PythonIDE still opens the resource file "manually" because it also uses
    presence of the CURS resource to determine whether it needs to adjust
    sys.path.

--- Mac/Tools/IDE/Wminiapp.py
+++ Mac/Tools/IDE/Wminiapp.py
@@ -8,7 +8,7 @@ class TestApp(Wapplication.Application):
 	
 	def __init__(self):
 		from Carbon import Res
-		macresource.open_pathname("Widgets.rsrc")
+#		macresource.open_pathname("Widgets.rsrc")
 		self._menustocheck = []
 		self.preffilepath = os.path.join("Python", "PythonIDE preferences")
 		Wapplication.Application.__init__(self, 'Pyth')
