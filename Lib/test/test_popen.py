9ff8d738b7b70faf0153e806cb73c488f02f4da5
Mark Hammond <mhammond@skippinet.com.au>
2002-04-03 01:47:00 +0000

    Fix bugs:  457466: popenx() argument mangling hangs python  226766: popen('python -c"...."') tends to hang
    Fixes argument quoting in w9xpopen.exe for Windows 9x.  w9xpopen.exe
    also never attempts to display a MessageBox when not executed
    interactively.
    
    Added test_popen() test.  This test currently just executes
    "python -c ..." as a child process, and checks that the expected
    arguments were all recieved correctly by the child process.  This
    test succeeds for me on Win9x, win2k and Linux, and I hope it does
    for other popen supported platforms too :)

---
+++ Lib/test/test_popen.py
@@ -0,0 +1,36 @@
+#! /usr/bin/env python
+"""Basic tests for os.popen()
+
+  Particularly useful for platforms that fake popen.
+"""
+
+import os
+import sys
+from test_support import TestSkipped
+from os import popen
+
+# Test that command-lines get down as we expect.
+# To do this we execute:
+#    python -c "import sys;print sys.argv" {rest_of_commandline}
+# This results in Python being spawned and printing the sys.argv list.
+# We can then eval() the result of this, and see what each argv was.
+def _do_test_commandline(cmdline, expected):
+    cmd = 'python -c "import sys;print sys.argv" %s' % (cmdline,)
+    data = popen(cmd).read()
+    got = eval(data)[1:] # strip off argv[0]
+    if got != expected:
+        print "Error in popen commandline handling."
+        print " executed '%s', expected '%r', but got '%r'" \
+                                                    % (cmdline, expected, got)
+
+def _test_commandline():
+    _do_test_commandline("foo bar", ["foo", "bar"])
+    _do_test_commandline('foo "spam and eggs" "silly walk"', ["foo", "spam and eggs", "silly walk"])
+    _do_test_commandline('foo "a \\"quoted\\" arg" bar', ["foo", 'a "quoted" arg', "bar"])
+    print "popen seemed to process the command-line correctly"
+
+def main():
+    print "Test popen:"
+    _test_commandline()
+
+main()
