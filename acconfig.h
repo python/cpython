0b6964b5692791f8c0eff9022314b187829a852e
Martin v. Löwis <martin@v.loewis.de>
2002-04-12 09:55:28 +0000

    Remove unused macros.

--- acconfig.h
+++ acconfig.h
@@ -1,29 +1,6 @@
 /* Leave this blank line here -- autoheader needs it! */
 
 
-/* Define this if you have gethostbyname() */
-#undef HAVE_GETHOSTBYNAME
-
-/* Define if you have termios available */
-#undef HAVE_TERMIOS_H
-
-/* Define as the size of the unicode type. */
-#undef Py_UNICODE_SIZE
-
-/* sizeof(void *) */
-#undef SIZEOF_VOID_P
-
-/* Define to `int' if <sys/types.h> doesn't define.  */
-#undef socklen_t
-
-/* Define for SOLARIS 2.x */
-#undef SOLARIS
-
-/* Define if you want to use BSD db. */
-#undef WITH_LIBDB
-
-/* Define if you want to use ndbm. */
-#undef WITH_LIBNDBM
 
 
 /* Leave that blank line there-- autoheader needs it! */
