c3a787e090dce76149ae86d70d4fecea3c285a40
Guido van Rossum <guido@python.org>
2002-06-04 05:52:47 +0000

    Surprising fix for SF bug 563060: module can be used as base class.
    Change the module constructor (module_init) to have the signature
    __init__(name:str, doc=None); this prevents the call from type_new()
    to succeed.  While we're at it, prevent repeated calling of
    module_init for the same module from leaking the dict, changing the
    semantics so that __dict__ is only initialized if NULL.
    
    Also adding a unittest, test_module.py.
    
    This is an incompatibility with 2.2, if anybody was instantiating the
    module class before, their argument list was probably empty; so this
    can't be backported to 2.2.x.

---
+++ Lib/test/test_module.py
@@ -0,0 +1,48 @@
+# Test the module type
+
+from test_support import verify, vereq, verbose, TestFailed
+
+import sys
+module = type(sys)
+
+# An uninitialized module has no __dict__ or __name__, and __doc__ is None
+foo = module.__new__(module)
+verify(foo.__dict__ is None)
+try:
+    s = foo.__name__
+except AttributeError:
+    pass
+else:
+    raise TestFailed, "__name__ = %s" % repr(s)
+vereq(foo.__doc__, None)
+
+# Regularly initialized module, no docstring
+foo = module("foo")
+vereq(foo.__name__, "foo")
+vereq(foo.__doc__, None)
+vereq(foo.__dict__, {"__name__": "foo", "__doc__": None})
+
+# ASCII docstring
+foo = module("foo", "foodoc")
+vereq(foo.__name__, "foo")
+vereq(foo.__doc__, "foodoc")
+vereq(foo.__dict__, {"__name__": "foo", "__doc__": "foodoc"})
+
+# Unicode docstring
+foo = module("foo", u"foodoc\u1234")
+vereq(foo.__name__, "foo")
+vereq(foo.__doc__, u"foodoc\u1234")
+vereq(foo.__dict__, {"__name__": "foo", "__doc__": u"foodoc\u1234"})
+
+# Reinitialization should not replace the __dict__
+foo.bar = 42
+d = foo.__dict__
+foo.__init__("foo", "foodoc")
+vereq(foo.__name__, "foo")
+vereq(foo.__doc__, "foodoc")
+vereq(foo.bar, 42)
+vereq(foo.__dict__, {"__name__": "foo", "__doc__": "foodoc", "bar": 42})
+verify(foo.__dict__ is d)
+
+if verbose:
+    print "All OK"
