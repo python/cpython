867de944b46b43af6b890dfdfe442f9d7b17a0b0
Fred Drake <fdrake@acm.org>
2002-06-25 19:20:10 +0000

    Add convenience module to run all the XML tests.

---
+++ Lib/test/xmltests.py
@@ -0,0 +1,16 @@
+# Convenience test module to run all of the XML-related tests in the
+# standard library.
+
+import sys
+
+def runtest(name):
+    __import__(name)
+    module = sys.modules[name]
+    if hasattr(module, "test_main"):
+        module.test_main()
+
+runtest("test.test_minidom")
+runtest("test.test_pyexpat")
+runtest("test.test_sax")
+runtest("test.test_xmllib")
+runtest("test.test_xmlrpc")
