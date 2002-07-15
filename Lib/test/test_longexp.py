bb8159e19bb3254804a6ac9741ad9fe611e5454c
Andrew MacIntyre <andymac@bullseye.apana.org.au>
2002-07-15 12:03:19 +0000

    Tim_one's change to aggressively overallocate nodes when adding child nodes (in Parser/node.c) resolves the gross memory consumption exhibited by the EMX runtime on OS/2, so the test should be exercised on this platform.

--- Lib/test/test_longexp.py
+++ Lib/test/test_longexp.py
@@ -1,12 +1,5 @@
-# this test has a malloc problem on OS/2+EMX, so skip test in that environment
-
-import sys
-from test_support import TestFailed, TestSkipped
 
 REPS = 65580
 
-if sys.platform == "os2emx":
-    raise TestFailed, "OS/2+EMX port has malloc problems with long expressions"
-else:
-    l = eval("[" + "2," * REPS + "]")
-    print len(l)
+l = eval("[" + "2," * REPS + "]")
+print len(l)
