d33d47401d96d3486f83f274e0318aaafd5a854d
Barry Warsaw <barry@python.org>
2002-07-19 22:44:23 +0000

    Shut the test up and add a missing import

--- Lib/test/test_email.py
+++ Lib/test/test_email.py
@@ -1,8 +1,9 @@
 # Copyright (C) 2001,2002 Python Software Foundation
 # email package unit tests
 
+import unittest
 # The specific tests now live in Lib/email/test
-from email.test.test_email import *
+from email.test.test_email import suite
 
 
 
--- Lib/test/test_email_codecs.py
+++ Lib/test/test_email_codecs.py
@@ -1,8 +1,9 @@
 # Copyright (C) 2002 Python Software Foundation
 # email package unit tests for (optional) Asian codecs
 
+import unittest
 # The specific tests now live in Lib/email/test
-from email.test.test_email_codecs import *
+from email.test.test_email_codecs import suite
 
 
 
