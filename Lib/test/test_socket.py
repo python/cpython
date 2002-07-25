b8a690d42a2476eeb308cb379382a3337f3a3c07
Jeremy Hylton <jeremy@alum.mit.edu>
2002-07-25 15:37:23 +0000

    Remove test that was none too picky about whether attributes exist.

--- Lib/test/test_socket.py
+++ Lib/test/test_socket.py
@@ -216,47 +216,6 @@ class GeneralModuleTests(unittest.TestCase):
         socket.SOL_SOCKET
         socket.SO_REUSEADDR
 
-    def testNonCrucialConstants(self):
-        """Testing for existance of non-crucial constants."""
-        for const in (
-                 "AF_UNIX",
-
-                 "SO_DEBUG", "SO_ACCEPTCONN", "SO_REUSEADDR", "SO_KEEPALIVE",
-                 "SO_DONTROUTE", "SO_BROADCAST", "SO_USELOOPBACK", "SO_LINGER",
-                 "SO_OOBINLINE", "SO_REUSEPORT", "SO_SNDBUF", "SO_RCVBUF",
-                 "SO_SNDLOWAT", "SO_RCVLOWAT", "SO_SNDTIMEO", "SO_RCVTIMEO",
-                 "SO_ERROR", "SO_TYPE", "SOMAXCONN",
-
-                 "MSG_OOB", "MSG_PEEK", "MSG_DONTROUTE", "MSG_EOR",
-                 "MSG_TRUNC", "MSG_CTRUNC", "MSG_WAITALL", "MSG_BTAG",
-                 "MSG_ETAG",
-
-                 "SOL_SOCKET",
-
-                 "IPPROTO_IP", "IPPROTO_ICMP", "IPPROTO_IGMP",
-                 "IPPROTO_GGP", "IPPROTO_TCP", "IPPROTO_EGP",
-                 "IPPROTO_PUP", "IPPROTO_UDP", "IPPROTO_IDP",
-                 "IPPROTO_HELLO", "IPPROTO_ND", "IPPROTO_TP",
-                 "IPPROTO_XTP", "IPPROTO_EON", "IPPROTO_BIP",
-                 "IPPROTO_RAW", "IPPROTO_MAX",
-
-                 "IPPORT_RESERVED", "IPPORT_USERRESERVED",
-
-                 "INADDR_ANY", "INADDR_BROADCAST", "INADDR_LOOPBACK",
-                 "INADDR_UNSPEC_GROUP", "INADDR_ALLHOSTS_GROUP",
-                 "INADDR_MAX_LOCAL_GROUP", "INADDR_NONE",
-
-                 "IP_OPTIONS", "IP_HDRINCL", "IP_TOS", "IP_TTL",
-                 "IP_RECVOPTS", "IP_RECVRETOPTS", "IP_RECVDSTADDR",
-                 "IP_RETOPTS", "IP_MULTICAST_IF", "IP_MULTICAST_TTL",
-                 "IP_MULTICAST_LOOP", "IP_ADD_MEMBERSHIP",
-                 "IP_DROP_MEMBERSHIP",
-                  ):
-            try:
-                getattr(socket, const)
-            except AttributeError:
-                pass
-
     def testHostnameRes(self):
         """Testing hostname resolution mechanisms."""
         hostname = socket.gethostname()
