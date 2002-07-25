e80c0d35808000a783f8cc667c8841f7aa0b1ad6
Fred Drake <fdrake@acm.org>
2002-07-25 20:13:03 +0000

    Add an XXX comment and a pointer to a full bug report.

--- Lib/site.py
+++ Lib/site.py
@@ -92,6 +92,8 @@ del dir, dircase, L
 
 # Append ./build/lib.<platform> in case we're running in the build dir
 # (especially for Guido :-)
+# XXX This should not be part of site.py, since it is needed even when
+# using the -S option for Python.  See http://www.python.org/sf/586680
 if os.name == "posix" and os.path.basename(sys.path[-1]) == "Modules":
     from distutils.util import get_platform
     s = "build/lib.%s-%.3s" % (get_platform(), sys.version)
