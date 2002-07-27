378864e31a5fad840b93e2a4dca6abd568e8a156
Piers Lauder <piers@cs.su.oz.au>
2002-07-27 07:08:38 +0000

    remove redundant code

--- Lib/test/test_imaplib.py
+++ Lib/test/test_imaplib.py
@@ -7,6 +7,6 @@ timevalues = [(2033, 5, 18, 3, 33, 20, 2, 138, 0), '"18-May-2033 13:33:20 +1000"
 
 for t in timevalues:
     t1 = imaplib.Time2Internaldate(t)
-    t2 = imaplib.Time2Internaldate(imaplib.Internaldate2tuple('INTERNALDATE ' + imaplib.Time2Internaldate(t)))
+    t2 = imaplib.Time2Internaldate(imaplib.Internaldate2tuple('INTERNALDATE ' + t1))
     if t1 <> t2:
         print 'incorrect result when converting', `t`
