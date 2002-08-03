72f8807e59cc16e3f3bc9b4e60930052627957f7
Piers Lauder <piers@cs.su.oz.au>
2002-08-03 11:14:43 +0000

    revert to version 1.2

--- Lib/test/test_imaplib.py
+++ Lib/test/test_imaplib.py
@@ -1,11 +1,12 @@
 import imaplib
 import time
 
-timevalues = [2000000000, 2000000000.0, time.localtime(2000000000),
-              '"18-May-2033 05:33:20 +0200"', '"18-May-2033 13:33:20 +1000"']
+# We can check only that it successfully produces a result,
+# not the correctness of the result itself, since the result
+# depends on the timezone the machine is in.
 
-check = timevalues[2]
+timevalues = [2000000000, 2000000000.0, time.localtime(2000000000),
+              "18-May-2033 05:33:20 +0200"]
 
 for t in timevalues:
-    if check <> imaplib.Internaldate2tuple('INTERNALDATE ' + imaplib.Time2Internaldate(t)):
-        print 'incorrect result when converting', `t`
+    imaplib.Time2Internaldate(t)
