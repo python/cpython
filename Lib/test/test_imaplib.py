8d30b1e6734c0faa57b6f3b82db8c36c5e933dae
Tim Peters <tim.peters@gmail.com>
2002-08-04 06:53:18 +0000

    I don't know what's going on with this test, but the last change from Piers obviously couldn't have passed on any platform.  Fiddling it so it works (for a meaning of "works" no stronger than "doesn't fail" <wink>).

--- Lib/test/test_imaplib.py
+++ Lib/test/test_imaplib.py
@@ -6,7 +6,7 @@ import time
 # depends on the timezone the machine is in.
 
 timevalues = [2000000000, 2000000000.0, time.localtime(2000000000),
-              "18-May-2033 05:33:20 +0200"]
+              '"18-May-2033 05:33:20 +0200"']
 
 for t in timevalues:
     imaplib.Time2Internaldate(t)
