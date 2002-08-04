7ea8143fe518b07f14f492295918567ecb11ab83
Jack Jansen <jack.jansen@cwi.nl>
2002-08-04 21:56:12 +0000

    Specify pathnames in a way that works on both OS9 and OSX. You'll still have to manually edit it, though...

--- Tools/bgen/bgen/bgenlocations.py
+++ Tools/bgen/bgen/bgenlocations.py
@@ -3,11 +3,11 @@
 #
 import sys, os
 # Where to find the Universal Header include files:
-MWERKSDIR="Moes:Applications (Mac OS 9):Metrowerks CodeWarrior 7.0:Metrowerks CodeWarrior:"
-INCLUDEDIR=MWERKSDIR + "MacOS Support:Universal:Interfaces:CIncludes:"
+MWERKSDIR="/Applications/Metrowerks CodeWarrior 7.0/Metrowerks CodeWarrior/"
+INCLUDEDIR=os.path.join(MWERKSDIR, "MacOS Support", "Universal", "Interfaces", "CIncludes")
 
 # Where to put the python definitions file:
-TOOLBOXDIR=os.path.join(sys.prefix, ":Mac:Lib:Carbon:")
+TOOLBOXDIR=os.path.join(sys.prefix, "Mac", "Lib", "Carbon")
 
 # Creator for C files:
 CREATOR="CWIE"
