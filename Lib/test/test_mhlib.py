b7a575bd8572ec273940d89fa6dbdff0c657ad65
Tim Peters <tim.peters@gmail.com>
2002-08-04 22:55:35 +0000

    We don't really need the name of the test in the "test skipped" msg, and having it there causes the line to wrap.

--- Lib/test/test_mhlib.py
+++ Lib/test/test_mhlib.py
@@ -21,7 +21,7 @@ if (sys.platform.startswith("win") or sys.platform=="riscos" or
     # link counts, and that causes test_listfolders() here to get back
     # an empty list from its call of listallfolders().
     # The other tests here pass on Windows.
-    raise TestSkipped("test_mhlib skipped on %s -- " % sys.platform +
+    raise TestSkipped("skipped on %s -- " % sys.platform +
                       "too many Unix assumptions")
 
 _mhroot = TESTFN+"_MH"
