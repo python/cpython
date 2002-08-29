344f425034cd197b21e4e32fb50013ece87d1dcc
Jeremy Hylton <jeremy@alum.mit.edu>
2002-08-29 20:12:26 +0000

    Fix SF bug [ 599838 ] httplib.connect broken in 2.1 branch
    Some IPv6-specific changes crept into the 2.1 branch when I backported
    other bug fixes.

--- Lib/httplib.py
+++ Lib/httplib.py
@@ -505,25 +505,10 @@ class HTTPConnection:
 
     def connect(self):
         """Connect to the host and port specified in __init__."""
-        msg = "getaddrinfo returns an empty list"
-        for res in socket.getaddrinfo(self.host, self.port, 0,
-                                      socket.SOCK_STREAM):
-            af, socktype, proto, canonname, sa = res
-            try:
-                self.sock = socket.socket(af, socktype, proto)
-                if self.debuglevel > 0:
-                    print "connect: (%s, %s)" % (self.host, self.port)
-                self.sock.connect(sa)
-            except socket.error, msg:
-                if self.debuglevel > 0:
-                    print 'connect fail:', (self.host, self.port)
-                if self.sock:
-                    self.sock.close()
-                self.sock = None
-                continue
-            break
-        if not self.sock:
-            raise socket.error, msg
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        if self.debuglevel > 0:
+            print "connect: (%s, %s)" % (self.host, self.port)
+        self.sock.connect((self.host, self.port))
 
     def close(self):
         """Close the connection to the HTTP server."""
