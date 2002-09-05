142e56b84be96ed44dd927251c54b03628f43a1e
Neal Norwitz <nnorwitz@gmail.com>
2002-09-05 21:08:25 +0000

    SF # 555779, import user doesn't work with CGIs

--- Lib/user.py
+++ Lib/user.py
@@ -26,6 +26,8 @@ import os
 home = os.curdir                        # Default
 if 'HOME' in os.environ:
     home = os.environ['HOME']
+elif os.name == 'posix':
+    home = os.path.expanduser("~/")
 elif os.name == 'nt':                   # Contributed by Jeff Bauer
     if 'HOMEPATH' in os.environ:
         if 'HOMEDRIVE' in os.environ:
