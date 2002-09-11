4e09d5c6d6f5e3eaf8175c0ec54901b6f7a005a1
Barry Warsaw <barry@python.org>
2002-09-11 02:32:14 +0000

    unquote(): Didn't properly de-backslash-ify.  This patch (adapted from Quinn Dunkan's mimelib SF patch #573204) fixes the problem.

--- Lib/rfc822.py
+++ Lib/rfc822.py
@@ -477,9 +477,9 @@ class Message:
 def unquote(str):
     """Remove quotes from a string."""
     if len(str) > 1:
-        if str[0] == '"' and str[-1:] == '"':
-            return str[1:-1]
-        if str[0] == '<' and str[-1:] == '>':
+        if str.startswith('"') and str.endswith('"'):
+            return str[1:-1].replace('\\\\', '\\').replace('\\"', '"')
+        if str.startswith('<') and str.endswith('>'):
             return str[1:-1]
     return str
 
