bf128b57c84ef66f7f2c369b2adc9bf1374da74e
Kurt B. Kaiser <kbk@shore.net>
2002-09-14 02:56:04 +0000

    MERGE DS_RPC_BRANCH into MAIN ToolTip.py     cleanup 05Aug

--- Lib/idlelib/ToolTip.py
+++ Lib/idlelib/ToolTip.py
@@ -1,3 +1,6 @@
+# general purpose 'tooltip' routines - currently unused in idlefork
+# (although the 'calltips' extension is partly based on this code)
+# may be useful for some purposes in (or almost in ;) the current project scope
 # Ideas gleaned from PySol
 
 import os
