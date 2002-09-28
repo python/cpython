4eb5c65885f029b80e7f70cb2303ddcdc4521b1b
Barry Warsaw <barry@python.org>
2002-09-28 20:25:15 +0000

    Add a pychecker suppression.

--- Lib/email/MIMENonMultipart.py
+++ Lib/email/MIMENonMultipart.py
@@ -12,9 +12,13 @@ from email import MIMEBase
 class MIMENonMultipart(MIMEBase.MIMEBase):
     """Base class for MIME multipart/* type messages."""
 
+    __pychecker__ = 'unusednames=payload'
+
     def attach(self, payload):
         # The public API prohibits attaching multiple subparts to MIMEBase
         # derived subtypes since none of them are, by definition, of content
         # type multipart/*
         raise Errors.MultipartConversionError(
             'Cannot attach additional subparts to non-multipart/*')
+
+    del __pychecker__
