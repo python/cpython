a93d7a12d2dd3add921bfc23b5224c5a3bafb64a
Barry Warsaw <barry@python.org>
2002-09-28 20:52:26 +0000

    Code cleanup and add docstrings.

--- Lib/email/__init__.py
+++ Lib/email/__init__.py
@@ -27,18 +27,33 @@ __all__ = ['Charset',
            ]
 
 
+try:
+    True, False
+except NameError:
+    True = 1
+    False = 0
+
+
 
 # Some convenience routines.  Don't import Parser and Message as side-effects
 # of importing email since those cascadingly import most of the rest of the
 # email package.
-def message_from_string(s, _class=None, strict=0):
+def message_from_string(s, _class=None, strict=False):
+    """Parse a string into a Message object model.
+
+    Optional _class and strict are passed to the Parser constructor.
+    """
     from email.Parser import Parser
     if _class is None:
         from email.Message import Message
         _class = Message
     return Parser(_class, strict=strict).parsestr(s)
 
-def message_from_file(fp, _class=None, strict=0):
+def message_from_file(fp, _class=None, strict=False):
+    """Read a file and parse its contents into a Message object model.
+
+    Optional _class and strict are passed to the Parser constructor.
+    """
     from email.Parser import Parser
     if _class is None:
         from email.Message import Message
