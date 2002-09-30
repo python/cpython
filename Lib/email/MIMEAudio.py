03a755965400d7117b80349a93d38804d4d5a4c5
Barry Warsaw <barry@python.org>
2002-09-30 21:29:10 +0000

    Docstring consistency with the updated .tex files.

--- Lib/email/MIMEAudio.py
+++ Lib/email/MIMEAudio.py
@@ -46,7 +46,7 @@ class MIMEAudio(MIMENonMultipart):
 
         _audiodata is a string containing the raw audio data.  If this data
         can be decoded by the standard Python `sndhdr' module, then the
-        subtype will be automatically included in the Content-Type: header.
+        subtype will be automatically included in the Content-Type header.
         Otherwise, you can specify  the specific audio subtype via the
         _subtype parameter.  If _subtype is not given, and no subtype can be
         guessed, a TypeError is raised.
@@ -55,11 +55,11 @@ class MIMEAudio(MIMENonMultipart):
         transport of the image data.  It takes one argument, which is this
         Image instance.  It should use get_payload() and set_payload() to
         change the payload to the encoded form.  It should also add any
-        Content-Transfer-Encoding: or other headers to the message as
+        Content-Transfer-Encoding or other headers to the message as
         necessary.  The default encoding is Base64.
 
         Any additional keyword arguments are passed to the base class
-        constructor, which turns them into parameters on the Content-Type:
+        constructor, which turns them into parameters on the Content-Type
         header.
         """
         if _subtype is None:
