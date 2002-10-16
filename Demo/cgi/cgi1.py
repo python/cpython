44da27850bd30da552933ef22de098f9f211eaa0
Guido van Rossum <guido@python.org>
2002-10-16 21:01:27 +0000

    Some really simple cgi examples. cgi3 is a MiniWiki.

---
+++ Demo/cgi/cgi1.py
@@ -0,0 +1,14 @@
+#!/usr/local/bin/python
+
+"""CGI test 1 - check server setup."""
+
+# Until you get this to work, your web server isn't set up right or
+# your Python isn't set up right.
+
+# If cgi0.sh works but cgi1.py, check the #! line and the file
+# permissions.  The docs for the cgi.py module have debugging tips.
+
+print "Content-type: text/html"
+print
+print "<h1>Hello world</h1>"
+print "<p>This is cgi1.py"
