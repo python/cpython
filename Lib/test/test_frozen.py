2e3160be1906d0a86ccded901c97ae13efe2b098
Finn Bock <bckfnn@worldonline.dk>
2002-11-01 11:33:00 +0000

    [SF bug 631713] use the import exeption message in the TestFailed exception.

--- Lib/test/test_frozen.py
+++ Lib/test/test_frozen.py
@@ -6,17 +6,17 @@ import sys, os
 try:
     import __hello__
 except ImportError, x:
-    raise TestFailed, "import __hello__ failed:", x
+    raise TestFailed, "import __hello__ failed:" + str(x)
 
 try:
     import __phello__
 except ImportError, x:
-    raise TestFailed, "import __phello__ failed:", x
+    raise TestFailed, "import __phello__ failed:" + str(x)
 
 try:
     import __phello__.spam
 except ImportError, x:
-    raise TestFailed, "import __phello__.spam failed:", x
+    raise TestFailed, "import __phello__.spam failed:" + str(x)
 
 try:
     import __phello__.foo
