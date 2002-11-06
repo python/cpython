aca49b065bbe7c27d47798bdb3cb059ef213fc16
Andrew M. Kuchling <amk@amk.ca>
2002-11-06 15:40:05 +0000

    Fix NameError exception ('name' undefined)

--- Lib/urllib2.py
+++ Lib/urllib2.py
@@ -771,6 +771,7 @@ class AbstractHTTPHandler(BaseHandler):
         sel_host, sel_path = splithost(sel)
         h.putheader('Host', sel_host or host)
         for args in self.parent.addheaders:
+            name, value = args
             if name not in req.headers:
                 h.putheader(*args)
         for k, v in req.headers.items():
