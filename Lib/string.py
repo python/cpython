dde5a0f05d9da12e4d7d154540d3e60bf88b4d60
Neal Norwitz <nnorwitz@gmail.com>
2002-11-14 03:31:32 +0000

    Update the docstring to match the code.  Will backport.

--- Lib/string.py
+++ Lib/string.py
@@ -75,10 +75,12 @@ def swapcase(s):
 
 # Strip leading and trailing tabs and spaces
 def strip(s, chars=None):
-    """strip(s) -> string
+    """strip(s [,chars]) -> string
 
     Return a copy of the string s with leading and trailing
     whitespace removed.
+    If chars is given and not None, remove characters in sep instead.
+    If chars is unicode, S will be converted to unicode before stripping.
 
     """
     return s.strip(chars)
