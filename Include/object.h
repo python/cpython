7d0b34959fa17c4605b30e32857e0492a41a4527
Neil Schemenauer <nascheme@enme.ucalgary.ca>
2002-11-17 17:52:44 +0000

    Remove _Py_ResetReferences.  Fixes bug #529750 "Circular reference makes Py_Init crash".  refchain cannot be cleared because objects can live across Py_Finalize() and Py_Initialize() if they are kept alive by circular references.

--- Include/object.h
+++ Include/object.h
@@ -559,7 +559,6 @@ PyAPI_FUNC(void) _Py_NewReference(PyObject *);
 PyAPI_FUNC(void) _Py_ForgetReference(PyObject *);
 PyAPI_FUNC(void) _Py_Dealloc(PyObject *);
 PyAPI_FUNC(void) _Py_PrintReferences(FILE *);
-PyAPI_FUNC(void) _Py_ResetReferences(void);
 
 #else
 /* Without Py_TRACE_REFS, there's little enough to do that we expand code
