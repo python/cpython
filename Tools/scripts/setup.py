da760c488fd5f36293abc7dd3ebbf9d62b8d3b5c
Skip Montanaro <skip@pobox.com>
2002-12-05 02:58:36 +0000

    simple setup.py to install some interesting scripts in $(prefix)/bin.

---
+++ Tools/scripts/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+  scripts=[
+    'byteyears.py',
+    'checkpyc.py',
+    'copytime.py',
+    'crlf.py',
+    'dutree.py',
+    'ftpmirror.py',
+    'h2py.py',
+    'lfcr.py',
+    '../../Lib/tabnanny.py',
+    'untabify.py',
+    ],
+  )
