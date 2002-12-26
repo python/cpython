9d48011c52df086d84c2986f526b614605a94853
Jack Jansen <jack.jansen@cwi.nl>
2002-12-26 21:17:42 +0000

    macfsn is gone.

--- Mac/Tools/macfreeze/macmodulefinder.py
+++ Mac/Tools/macfreeze/macmodulefinder.py
@@ -19,7 +19,7 @@ except ImportError:
 # Modules that must be included, and modules that need not be included
 # (but are if they are found)
 #
-MAC_INCLUDE_MODULES=['site', 'macfsn']
+MAC_INCLUDE_MODULES=['site']
 MAC_MAYMISS_MODULES=['posix', 'os2', 'nt', 'ntpath', 'dos', 'dospath', 
 		'win32api', 'ce', '_winreg',
 		'nturl2path', 'pwd', 'sitecustomize',
