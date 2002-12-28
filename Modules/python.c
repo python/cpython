ab1ec5ca50422e089b8aee96d3774e6b56529465
Tim Peters <tim.peters@gmail.com>
2002-12-28 21:56:08 +0000

    Apparently FreeBSD enables some HW floating-point exceptions by default. This can cause core dumps when Python runs.  Python relies on the 754- (and C99-) mandated default "non-stop" mode for FP exceptions.  This patch from Ben Laurie disables at least one FP exception on FreeBSD at Python startup time.

--- Modules/python.c
+++ Modules/python.c
@@ -2,8 +2,23 @@
 
 #include "Python.h"
 
+#ifdef __FreeBSD__
+#include <floatingpoint.h>
+#endif
+
 int
 main(int argc, char **argv)
 {
+	/* 754 requires that FP exceptions run in "no stop" mode by default,
+	 * and until C vendors implement C99's ways to control FP exceptions,
+	 * Python requires non-stop mode.  Alas, some platforms enable FP
+	 * exceptions by default.  Here we disable them.
+	 */
+#ifdef __FreeBSD__
+	fp_except_t m;
+
+	m = fpgetmask();
+	fpsetmask(m & ~FP_X_OFL);
+#endif
 	return Py_Main(argc, argv);
 }
