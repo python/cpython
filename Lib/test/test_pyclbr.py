ef115e414cc67315090073f99bd4fdf100d2787a
Raymond Hettinger <python@rcn.com>
2002-12-30 07:04:47 +0000

    Restore the test of the random module after including "Random" in the ignore tuple.
    The line, "from _random import Random as CoreGenerator", fools the test
    code which expects CoreGenerator.__name__ to be "CoreGenerator" instead
    of "Random".

--- Lib/test/test_pyclbr.py
+++ Lib/test/test_pyclbr.py
@@ -143,7 +143,7 @@ class PyclbrTest(TestCase):
         cm = self.checkModule
 
         # These were once about the 10 longest modules
-        ## cm('random') # This test crashes (probably due to the "import as").
+        cm('random', ignore=('Random'))  # from _random import Random as CoreGenerator
         cm('cgi', ignore=('log',))      # set with = in module
         cm('mhlib')
         cm('urllib', ignore=('getproxies_registry',
