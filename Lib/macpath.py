c695e8f6ba391789a8d2e308d47ddc664c071bf6
Neal Norwitz <nnorwitz@gmail.com>
2003-01-13 19:29:38 +0000

    supports_unicode_filenames should not have been backported, test___all__ now passed

--- Lib/macpath.py
+++ Lib/macpath.py
@@ -7,7 +7,7 @@ __all__ = ["normcase","isabs","join","splitdrive","split","splitext",
            "basename","dirname","commonprefix","getsize","getmtime",
            "getatime","islink","exists","isdir","isfile",
            "walk","expanduser","expandvars","normpath","abspath",
-           "realpath","supports_unicode_filenames"]
+           "realpath"]
 
 # Normalize the case of a pathname.  Dummy in Posix, but <s>.lower() here.
 
