fe8d84d5aea0f8ff9c27276473b1b8a1d69f5aeb
Guido van Rossum <guido@python.org>
2003-01-28 20:39:49 +0000

    Comment out a test that was anticipating SF patch 661536 -- but that isn't checked in yet. :-(

--- Lib/test/test_compile.py
+++ Lib/test/test_compile.py
@@ -133,9 +133,9 @@ expect_same("000000000000007", 7)
 expect_same("000000000000008.", 8.)
 expect_same("000000000000009.", 9.)
 
-# Verify treatment of unary minus on negative numbers SF bug #660455
-import warnings
-warnings.filterwarnings("ignore", "hex/oct constants", FutureWarning)
-# XXX Of course the following test will have to be changed in Python 2.4
-expect_same("0xffffffff", -1)
-expect_same("-0xffffffff", 1)
+## # Verify treatment of unary minus on negative numbers SF bug #660455
+## import warnings
+## warnings.filterwarnings("ignore", "hex/oct constants", FutureWarning)
+## # XXX Of course the following test will have to be changed in Python 2.4
+## expect_same("0xffffffff", -1)
+## expect_same("-0xffffffff", 1)
