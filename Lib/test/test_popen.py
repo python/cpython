6e8eef89bfecbe13523e6365e544ec91af2160c2
Tim Peters <tim.peters@gmail.com>
2003-03-07 21:10:21 +0000

    Don't quote the path to Python unless the path contains an embedded space. Quoting the path doesn't work on Win2K (cmd.exe) regardless, this is just a hack to let the test pass again on Win2K (so long as Python isn't installed in a path that does contain an embedded space).  On Win2K it looks like we'd also have to add a second pair of double quotes, around the entire command line.

--- Lib/test/test_popen.py
+++ Lib/test/test_popen.py
@@ -14,8 +14,11 @@ from os import popen
 #    python -c "import sys;print sys.argv" {rest_of_commandline}
 # This results in Python being spawned and printing the sys.argv list.
 # We can then eval() the result of this, and see what each argv was.
+python = sys.executable
+if ' ' in python:
+    python = '"' + python + '"'     # quote embedded space for cmdline
 def _do_test_commandline(cmdline, expected):
-    cmd = '"%s" -c "import sys;print sys.argv" %s' % (sys.executable, cmdline)
+    cmd = '%s -c "import sys;print sys.argv" %s' % (python, cmdline)
     data = popen(cmd).read()
     got = eval(data)[1:] # strip off argv[0]
     if got != expected:
