0fc192a9cde0482ed5f56ea37ecda218740fe41f
Jack Jansen <jack.jansen@cwi.nl>
2003-03-29 00:11:32 +0000

    - Prefer using events (in stead of poking around in resource files) to get terminology resources. Unfortunately there doesn't seem to be any application I can ask for the basic StdSuites terminology (?). - Prefer OSX-native versions of applications over OS9 versions.

--- Mac/scripts/genallsuites.py
+++ Mac/scripts/genallsuites.py
@@ -8,37 +8,43 @@ import os
 import gensuitemodule
 
 DSTDIR="/Users/jack/src/python/Lib/plat-mac/lib-scriptpackages"
+OS9DISK="/Volumes/Moes"
 
-APPLESCRIPT="/Volumes/Sap/System Folder/Extensions/AppleScript"
-CODEWARRIOR="/Volumes/Sap/Applications (Mac OS 9)/Metrowerks CodeWarrior 7.0/Metrowerks CodeWarrior/CodeWarrior IDE 4.2.5"
-EXPLORER="/Volumes/Sap/Applications (Mac OS 9)/Internet Explorer 5/Internet Explorer"
-FINDER="/Volumes/Sap/System Folder/Finder"
-NETSCAPE="/Volumes/Sap/Applications (Mac OS 9)/Netscape Communicator\xe2\x84\xa2 Folder/Netscape Communicator\xe2\x84\xa2"
-TERMINAL="/Applications/Utilities/Terminal.app/Contents/Resources/Terminal.rsrc"
+APPLESCRIPT=OS9DISK + "/Systeemmap/Extensies/AppleScript"
+SYSTEMEVENTS="/System/Library/CoreServices/System Events.app"
+
+CODEWARRIOR=OS9DISK + "/Applications (Mac OS 9)/Metrowerks CodeWarrior 7.0/Metrowerks CodeWarrior/CodeWarrior IDE 4.2.6"
+EXPLORER="/Applications/Internet Explorer.app"
+FINDER="/System/Library/CoreServices/Finder.app"
+NETSCAPE=OS9DISK + "/Applications (Mac OS 9)/Netscape Communicator\xe2\x84\xa2 Folder/Netscape Communicator\xe2\x84\xa2"
+TERMINAL="/Applications/Utilities/Terminal.app"
 
 gensuitemodule.processfile_fromresource(APPLESCRIPT,
 	output=os.path.join(DSTDIR, 'StdSuites'),
 	basepkgname='_builtinSuites',
 	edit_modnames=[])
-gensuitemodule.processfile_fromresource(CODEWARRIOR,
+gensuitemodule.processfile(SYSTEMEVENTS,
+	output=os.path.join(DSTDIR, 'SystemEvents'),
+	basepkgname='StdSuites',
+	edit_modnames=[])
+gensuitemodule.processfile(CODEWARRIOR,
 	output=os.path.join(DSTDIR, 'CodeWarrior'),
 	basepkgname='StdSuites',
 	edit_modnames=[])
-gensuitemodule.processfile_fromresource(EXPLORER,
+gensuitemodule.processfile(EXPLORER,
 	output=os.path.join(DSTDIR, 'Explorer'),
 	basepkgname='StdSuites',
 	edit_modnames=[])
-gensuitemodule.processfile_fromresource(FINDER,
+gensuitemodule.processfile(FINDER,
 	output=os.path.join(DSTDIR, 'Finder'),
 	basepkgname='StdSuites',
 	edit_modnames=[])
-gensuitemodule.processfile_fromresource(NETSCAPE,
+gensuitemodule.processfile(NETSCAPE,
 	output=os.path.join(DSTDIR, 'Netscape'),
 	basepkgname='StdSuites',
 	edit_modnames=[('WorldWideWeb_suite_2c__as_d', 'WorldWideWeb_suite')])
-gensuitemodule.processfile_fromresource(TERMINAL,
+gensuitemodule.processfile(TERMINAL,
 	output=os.path.join(DSTDIR, 'Terminal'),
 	basepkgname='StdSuites',
-	edit_modnames=[],
-	creatorsignature='trmx')
+	edit_modnames=[])
 	
