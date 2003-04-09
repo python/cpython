f22933e034fb7fc416486514ab19b15ba57d9620
Guido van Rossum <guido@python.org>
2003-04-09 19:57:06 +0000

    Add test for MessageBeep()

--- Lib/test/test_winsound.py
+++ Lib/test/test_winsound.py
@@ -1,6 +1,18 @@
 # Ridiculously simple test of the winsound module for Windows.
 
-import winsound
+import winsound, time
 for i in range(100, 2000, 100):
     winsound.Beep(i, 75)
 print "Hopefully you heard some sounds increasing in frequency!"
+winsound.MessageBeep()
+time.sleep(0.5)
+winsound.MessageBeep(winsound.MB_OK)
+time.sleep(0.5)
+winsound.MessageBeep(winsound.MB_ICONASTERISK)
+time.sleep(0.5)
+winsound.MessageBeep(winsound.MB_ICONEXCLAMATION)
+time.sleep(0.5)
+winsound.MessageBeep(winsound.MB_ICONHAND)
+time.sleep(0.5)
+winsound.MessageBeep(winsound.MB_ICONQUESTION)
+time.sleep(0.5)
