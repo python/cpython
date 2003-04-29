a251cae2c8381ef30872a494aaa49113a97b1af4
Guido van Rossum <guido@python.org>
2003-04-29 11:27:16 +0000

    Removing new files accidentally checked in on the trunk rather than on the idlefork-merge-branch.

--- Tools/idle/boolcheck.py
+++
@@ -1,9 +0,0 @@
-"boolcheck - import this module to ensure True, False, bool() builtins exist."
-try:
-    True
-except NameError:
-    import __builtin__
-    __builtin__.True = 1
-    __builtin__.False = 0
-    from operator import truth
-    __builtin__.bool = truth
