96f7d875b7092ec92a24647702757c76cdf9b6a5
Just van Rossum <just@letterror.com>
2003-05-03 00:27:23 +0000

    build the new autoGIL module on OSX

--- setup.py
+++ setup.py
@@ -748,6 +748,8 @@ class PyBuildExt(build_ext):
             exts.append( Extension('_CF', ['cf/_CFmodule.c', 'cf/pycfbridge.c'],
                         extra_link_args=['-framework', 'CoreFoundation']) )
 
+            exts.append( Extension('autoGIL', ['autoGIL.c'],
+                        extra_link_args=['-framework', 'CoreFoundation']) )
             exts.append( Extension('gestalt', ['gestaltmodule.c'],
                         extra_link_args=['-framework', 'Carbon']) )
             exts.append( Extension('MacOS', ['macosmodule.c'],
