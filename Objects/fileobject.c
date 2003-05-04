015dd821364f8c210baa9faf9d66dc766f9b591e
Tim Peters <tim.peters@gmail.com>
2003-05-04 04:16:52 +0000

    Somewhere along the way, the softspace attr of file objects became read- only.  Repaired, and added new tests to test_file.py.

--- Objects/fileobject.c
+++ Objects/fileobject.c
@@ -1962,7 +1962,8 @@ PyTypeObject PyFile_Type = {
 	0,					/* tp_call */
 	0,					/* tp_str */
 	PyObject_GenericGetAttr,		/* tp_getattro */
-	0,					/* tp_setattro */
+	/* softspace is writable:  we must supply tp_setattro */
+	PyObject_GenericSetAttr,		/* tp_setattro */
 	0,					/* tp_as_buffer */
 	Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE, /* tp_flags */
 	file_doc,				/* tp_doc */
