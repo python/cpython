8657845e02b1cd4e2e87ae85f538c66160ec187f
Raymond Hettinger <python@rcn.com>
2003-05-06 09:01:41 +0000

    SF bug #692959:  new.function ignores keyword arguments

--- Objects/funcobject.c
+++ Objects/funcobject.c
@@ -316,8 +316,11 @@ func_new(PyTypeObject* type, PyObject* args, PyObject* kw)
 	PyObject *closure = Py_None;
 	PyFunctionObject *newfunc;
 	int nfree, nclosure;
+	static char *kwlist[] = {"code", "globals", "name",
+				 "argdefs", "closure", 0};
 
-	if (!PyArg_ParseTuple(args, "O!O!|OOO:function",
+	if (!PyArg_ParseTupleAndKeywords(args, kw, "O!O!|OOO:function",
+			      kwlist,
 			      &PyCode_Type, &code,
 			      &PyDict_Type, &globals,
 			      &name, &defaults, &closure))
