11cb8135988ebe20631db08c60155d2a0b17c1b2
Tim Peters <tim.peters@gmail.com>
2003-05-12 19:29:36 +0000

    Close the file after tokenizing it.  Because the open file object was bound to a module global, the file object remained opened throughout the test suite run.

--- Lib/test/test_tokenize.py
+++ Lib/test/test_tokenize.py
@@ -3,7 +3,10 @@ import tokenize, os, sys
 
 if verbose:
     print 'starting...'
-file = open(findfile('tokenize_tests'+os.extsep+'py'))
-tokenize.tokenize(file.readline)
+
+f = file(findfile('tokenize_tests'+os.extsep+'py'))
+tokenize.tokenize(f.readline)
+f.close()
+
 if verbose:
     print 'finished'
