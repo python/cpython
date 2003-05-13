91ebf1ddb720d4a8920255c1f28ce99c6ca5caac
Brett Cannon <bcannon@gmail.com>
2003-05-13 06:42:59 +0000

    Fixed test_anydbm_creates to use proper paths for the created db.
    Made some stylistic fixes.

--- Lib/test/test_bsddb185.py
+++ Lib/test/test_bsddb185.py
@@ -1,4 +1,9 @@
+"""Tests for the bsddb185 module.
 
+The file 185test.db found in Lib/test/ is for testing purposes with this
+testing suite.
+
+"""
 from test.test_support import verbose, run_unittest, findfile
 import unittest
 import bsddb185
@@ -8,24 +13,25 @@ import os
 import tempfile
 
 class Bsddb185Tests(unittest.TestCase):
+
     def test_open_existing_hash(self):
-        "verify we can open a file known to be a hash v2 file"
+        # Verify we can open a file known to be a hash v2 file
         db = bsddb185.hashopen(findfile("185test.db"))
         self.assertEqual(db["1"], "1")
         db.close()
 
     def test_whichdb(self):
-        "verify that whichdb correctly sniffs the known hash v2 file"
+        # Verify that whichdb correctly sniffs the known hash v2 file
         self.assertEqual(whichdb.whichdb(findfile("185test.db")), "bsddb185")
 
     def test_anydbm_create(self):
-        "verify that anydbm.open does *not* create a bsddb185 file"
+        # Verify that anydbm.open does *not* create a bsddb185 file
         tmpdir = tempfile.mkdtemp()
         try:
             try:
                 dbfile = os.path.join(tmpdir, "foo.db")
-                anydbm.open(dbfile, "c").close()
-                ftype = whichdb.whichdb(findfile("foo.db"))
+                anydbm.open(os.path.splitext(dbfile)[0], "c").close()
+                ftype = whichdb.whichdb(dbfile)
                 self.assertNotEqual(ftype, "bsddb185")
             finally:
                 os.unlink(dbfile)
