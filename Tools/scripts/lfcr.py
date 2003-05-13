f21897554d05650b63b3f39e4e2647f138651423
Andrew M. Kuchling <amk@amk.ca>
2003-05-13 17:09:01 +0000

    [Bug #724767] Avoid use of 'file' as a variable name

--- Tools/scripts/lfcr.py
+++ Tools/scripts/lfcr.py
@@ -3,17 +3,17 @@
 "Replace LF with CRLF in argument files.  Print names of changed files."
 
 import sys, re, os
-for file in sys.argv[1:]:
-    if os.path.isdir(file):
-        print file, "Directory!"
+for filename in sys.argv[1:]:
+    if os.path.isdir(filename):
+        print filename, "Directory!"
         continue
-    data = open(file, "rb").read()
+    data = open(filename, "rb").read()
     if '\0' in data:
-        print file, "Binary!"
+        print filename, "Binary!"
         continue
     newdata = re.sub("\r?\n", "\r\n", data)
     if newdata != data:
-        print file
-        f = open(file, "wb")
+        print filename
+        f = open(filename, "wb")
         f.write(newdata)
         f.close()
