e9ac7201fd09eef09d6d9a4bae6454ee8856ef78
Jack Jansen <jack.jansen@cwi.nl>
2003-05-23 22:34:39 +0000

    Backport of 1.48: on Mac OS X don't use -R for runtime_library_dirs, use -L in stead. Fixes #723495.

--- Lib/distutils/unixccompiler.py
+++ Lib/distutils/unixccompiler.py
@@ -266,6 +266,9 @@ class UnixCCompiler (CCompiler):
         # the configuration data stored in the Python installation, so
         # we use this hack.
         compiler = os.path.basename(sysconfig.get_config_var("CC"))
+        if sys.platform[:6] == "darwin":
+            # MacOSX's linker doesn't understand the -R flag at all
+            return "-L" + dir
         if compiler == "gcc" or compiler == "g++":
             return "-Wl,-R" + dir
         else:
