8d1f11b0ef7ec31db75a4e640e9dfd75fc4ee08d
Kurt B. Kaiser <kbk@shore.net>
2003-05-26 22:20:34 +0000

    Whitespace Normalization Modified Files:	PyShell.py ScriptBinding.py buildapp.py

--- Lib/idlelib/buildapp.py
+++ Lib/idlelib/buildapp.py
@@ -1,5 +1,5 @@
 #
-# After running python setup.py install, run this program from the command 
+# After running python setup.py install, run this program from the command
 # line like so:
 #
 # % python2.3 buildapp.py build
@@ -10,8 +10,8 @@
 from bundlebuilder import buildapp
 
 buildapp(
-	name="IDLE",
-	mainprogram="idle.py",
-	argv_emulation=1,
-	iconfile="Icons/idle.icns",
+        name="IDLE",
+        mainprogram="idle.py",
+        argv_emulation=1,
+        iconfile="Icons/idle.icns",
 )
