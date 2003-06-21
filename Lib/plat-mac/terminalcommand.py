26b68f9e87cee1415b04584e260c9c5eb99c9f2f
Just van Rossum <just@letterror.com>
2003-06-21 14:49:14 +0000

    some old changes to this unused module

--- Lib/plat-mac/terminalcommand.py
+++ Lib/plat-mac/terminalcommand.py
@@ -28,26 +28,19 @@ SEND_MODE = kAENoReply  # kAEWaitReply hangs when run from Terminal.app itself
 def run(command):
     """Run a shell command in a new Terminal.app window."""
     termAddress = AE.AECreateDesc(typeApplSignature, TERMINAL_SIG)
-    theEvent = AE.AECreateAppleEvent(kAEMiscStandards, kAEActivate,
-                                     termAddress, kAutoGenerateReturnID,
-                                     kAnyTransactionID)
+    theEvent = AE.AECreateAppleEvent(kAECoreSuite, kAEDoScript, termAddress,
+                                     kAutoGenerateReturnID, kAnyTransactionID)
+    commandDesc = AE.AECreateDesc(typeChar, command)
+    theEvent.AEPutParamDesc(kAECommandClass, commandDesc)
 
     try:
-        theEvent.AESend(SEND_MODE, kAENormalPriority,
-                        kAEDefaultTimeout)
+        theEvent.AESend(SEND_MODE, kAENormalPriority, kAEDefaultTimeout)
     except AE.Error, why:
         if why[0] != -600:  # Terminal.app not yet running
             raise
         os.system(START_TERMINAL)
         time.sleep(1)
-        theEvent.AESend(SEND_MODE, kAENormalPriority,
-                        kAEDefaultTimeout)
-
-    theEvent = AE.AECreateAppleEvent(kAECoreSuite, kAEDoScript, termAddress,
-                                     kAutoGenerateReturnID, kAnyTransactionID)
-    commandDesc = AE.AECreateDesc(typeChar, command)
-    theEvent.AEPutParamDesc(kAECommandClass, commandDesc)
-    theEvent.AESend(SEND_MODE, kAENormalPriority, kAEDefaultTimeout)
+        theEvent.AESend(SEND_MODE, kAENormalPriority, kAEDefaultTimeout)
 
 
 if __name__ == "__main__":
