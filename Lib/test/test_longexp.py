f9f4c6945e14f5ad7f29085fcd756645348b13ae
Raymond Hettinger <python@rcn.com>
2003-08-30 22:54:55 +0000

    SF patch #736962:  Port tests to unittest (Contributed by Walter Dörwald).
    * Convert three test modules to unittest format.
    * Expanded coverage in test_structseq.py.
    * Raymond added a new test in test_sets.py

--- Lib/test/test_longexp.py
+++ Lib/test/test_longexp.py
@@ -1,5 +1,14 @@
+import unittest
+from test import test_support
 
-REPS = 65580
+class LongExpText(unittest.TestCase):
+    def test_longexp(self):
+        REPS = 65580
+        l = eval("[" + "2," * REPS + "]")
+        self.assertEqual(len(l), REPS)
 
-l = eval("[" + "2," * REPS + "]")
-print len(l)
+def test_main():
+    test_support.run_unittest(LongExpText)
+
+if __name__=="__main__":
+    test_main()
--- Lib/test/test_pep263.py
+++ Lib/test/test_pep263.py
@@ -1,3 +1,22 @@
 #! -*- coding: koi8-r -*-
-assert u"Питон".encode("utf-8") == '\xd0\x9f\xd0\xb8\xd1\x82\xd0\xbe\xd0\xbd'
-assert u"\П".encode("utf-8") == '\\\xd0\x9f'
+
+import unittest
+from test import test_support
+
+class PEP263Test(unittest.TestCase):
+
+    def test_pep263(self):
+        self.assertEqual(
+            u"Питон".encode("utf-8"),
+            '\xd0\x9f\xd0\xb8\xd1\x82\xd0\xbe\xd0\xbd'
+        )
+        self.assertEqual(
+            u"\П".encode("utf-8"),
+            '\\\xd0\x9f'
+        )
+
+def test_main():
+    test_support.run_unittest(PEP263Test)
+
+if __name__=="__main__":
+    test_main()
