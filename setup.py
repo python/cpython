125188cabf9e7fb9b8c1fccd93dacedd4862a28d
Gregory P. Smith <greg@mad-scientist.com>
2003-09-22 01:16:48 +0000

    Add BerkeleyDB 4.2 to the library search for the bsddb module.

--- setup.py
+++ setup.py
@@ -468,22 +468,29 @@ class PyBuildExt(build_ext):
         # Sleepycat Berkeley DB interface.  http://www.sleepycat.com
         #
         # This requires the Sleepycat DB code. The earliest supported version
-        # of that library is 3.0, the latest supported version is 4.1.  A list
+        # of that library is 3.1, the latest supported version is 4.2.  A list
         # of available releases can be found at
         #
         # http://www.sleepycat.com/update/index.html
+        #
+        # NOTE: 3.1 is only partially supported; expect the extended bsddb module
+        # test suite to show failures due to some missing methods and behaviours
+        # in BerkeleyDB 3.1.
 
         # when sorted in reverse order, keys for this dict must appear in the
         # order you wish to search - e.g., search for db4 before db3
         db_try_this = {
-            'db4': {'libs': ('db-4.1', 'db41', 'db-4.0', 'db4',),
-                    'libdirs': ('/usr/local/BerkeleyDB.4.1/lib',
+            'db4': {'libs': ('db-4.2', 'db42', 'db-4.1', 'db41', 'db-4.0', 'db4',),
+                    'libdirs': ('/usr/local/BerkeleyDB.4.2/lib',
+                                '/usr/local/BerkeleyDB.4.1/lib',
                                 '/usr/local/BerkeleyDB.4.0/lib',
                                 '/usr/local/lib',
                                 '/opt/sfw',
                                 '/sw/lib',
                                 ),
-                    'incdirs': ('/usr/local/BerkeleyDB.4.1/include',
+                    'incdirs': ('/usr/local/BerkeleyDB.4.2/include',
+                                '/usr/local/include/db42',
+                                '/usr/local/BerkeleyDB.4.1/include',
                                 '/usr/local/include/db41',
                                 '/usr/local/BerkeleyDB.4.0/include',
                                 '/usr/local/include/db4',
