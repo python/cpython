0a4a50dd85fb1f9f30ec6e6ff62c71124c52ade6
Jeremy Hylton <jeremy@alum.mit.edu>
2003-10-06 05:15:13 +0000

    SF patch [ 816787 ] urllib2.URLError don't calll IOError.__init__
    Not sure if this fix is great, but it's probably a small improvement.

--- Lib/urllib2.py
+++ Lib/urllib2.py
@@ -145,8 +145,12 @@ def install_opener(opener):
 
 class URLError(IOError):
     # URLError is a sub-type of IOError, but it doesn't share any of
-    # the implementation.  need to override __init__ and __str__
+    # the implementation.  need to override __init__ and __str__.
+    # It sets self.args for compatibility with other EnvironmentError
+    # subclasses, but args doesn't have the typical format with errno in
+    # slot 0 and strerror in slot 1.  This may be better than nothing.
     def __init__(self, reason):
+        self.args = reason,
         self.reason = reason
 
     def __str__(self):
