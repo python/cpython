8f1dd784a1562bc042143c711843e17962049247
Brett Cannon <bcannon@gmail.com>
2003-10-19 21:19:40 +0000

    Defined macros Py_RETURN_(TRUE|FALSE|NONE) as helper functions for returning the specified value.  All three Py_INCREF the singleton and then return it.

--- Include/boolobject.h
+++ Include/boolobject.h
@@ -23,6 +23,10 @@ PyAPI_DATA(PyIntObject) _Py_ZeroStruct, _Py_TrueStruct;
 #define Py_False ((PyObject *) &_Py_ZeroStruct)
 #define Py_True ((PyObject *) &_Py_TrueStruct)
 
+/* Macros for returning Py_True or Py_False, respectively */
+#define Py_RETURN_TRUE Py_INCREF(Py_True); return Py_True;
+#define Py_RETURN_FALSE Py_INCREF(Py_False); return Py_False;
+
 /* Function to return a bool from a C long */
 PyAPI_FUNC(PyObject *) PyBool_FromLong(long);
 
