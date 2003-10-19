d05235ec49de1cf12316e9632a2b3d339dae04c2
Brett Cannon <bcannon@gmail.com>
2003-10-19 21:19:40 +0000

    Defined macros Py_RETURN_(TRUE|FALSE|NONE) as helper functions for returning the specified value.  All three Py_INCREF the singleton and then return it.

--- Include/object.h
+++ Include/object.h
@@ -633,6 +633,9 @@ Don't forget to apply Py_INCREF() when returning this value!!!
 PyAPI_DATA(PyObject) _Py_NoneStruct; /* Don't use this directly */
 #define Py_None (&_Py_NoneStruct)
 
+/* Macro for returning Py_None from a function */
+#define Py_RETURN_NONE Py_INCREF(Py_None); return Py_None;
+
 /*
 Py_NotImplemented is a singleton used to signal that an operation is
 not implemented for a given type combination.
