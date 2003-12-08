88c0f01cc8d9aa86e5bbc51f3dfc8ceb5642092c
Walter Dörwald <walter@livinglogic.de>
2003-12-08 11:38:45 +0000

    Move list and tuple tests from test_types.py to their own scripts: test_tuple.py and test_list.py. Common tests for tuple, list and UserList are shared (in seq_tests.py and list_tests.py). Port tests to PyUnit. (From SF patch #736962)

---
+++ Lib/test/list_tests.py
@@ -0,0 +1,418 @@
+"""
+Tests common to list and UserList.UserList
+"""
+
+import sys
+
+import unittest
+from test import test_support, seq_tests
+
+class CommonTest(seq_tests.CommonTest):
+
+    def test_repr(self):
+        l0 = []
+        l2 = [0, 1, 2]
+        a0 = self.type2test(l0)
+        a2 = self.type2test(l2)
+
+        self.assertEqual(str(a0), str(l0))
+        self.assertEqual(repr(a0), repr(l0))
+        self.assertEqual(`a2`, `l2`)
+        self.assertEqual(str(a2), "[0, 1, 2]")
+        self.assertEqual(repr(a2), "[0, 1, 2]")
+
+    def test_setitem(self):
+        a = self.type2test([0, 1])
+        a[0] = 0
+        a[1] = 100
+        self.assertEqual(a, self.type2test([0, 100]))
+        a[-1] = 200
+        self.assertEqual(a, self.type2test([0, 200]))
+        a[-2] = 100
+        self.assertEqual(a, self.type2test([100, 200]))
+        self.assertRaises(IndexError, a.__setitem__, -3, 200)
+        self.assertRaises(IndexError, a.__setitem__, 2, 200)
+
+        a = self.type2test([])
+        self.assertRaises(IndexError, a.__setitem__, 0, 200)
+        self.assertRaises(IndexError, a.__setitem__, -1, 200)
+
+        self.assertRaises(TypeError, a.__setitem__)
+
+        a = self.type2test([0,1,2,3,4])
+        a[0L] = 1
+        a[1L] = 2
+        a[2L] = 3
+        self.assertEqual(a, self.type2test([1,2,3,3,4]))
+        a[0] = 5
+        a[1] = 6
+        a[2] = 7
+        self.assertEqual(a, self.type2test([5,6,7,3,4]))
+        a[-2L] = 88
+        a[-1L] = 99
+        self.assertEqual(a, self.type2test([5,6,7,88,99]))
+        a[-2] = 8
+        a[-1] = 9
+        self.assertEqual(a, self.type2test([5,6,7,8,9]))
+
+    def test_delitem(self):
+        a = self.type2test([0, 1])
+        del a[1]
+        self.assertEqual(a, [0])
+        del a[0]
+        self.assertEqual(a, [])
+
+        a = self.type2test([0, 1])
+        del a[-2]
+        self.assertEqual(a, [1])
+        del a[-1]
+        self.assertEqual(a, [])
+
+        a = self.type2test([0, 1])
+        self.assertRaises(IndexError, a.__delitem__, -3)
+        self.assertRaises(IndexError, a.__delitem__, 2)
+
+        a = self.type2test([])
+        self.assertRaises(IndexError, a.__delitem__, 0)
+
+        self.assertRaises(TypeError, a.__delitem__)
+
+    def test_setslice(self):
+        l = [0, 1]
+        a = self.type2test(l)
+
+        for i in range(-3, 4):
+            a[:i] = l[:i]
+            self.assertEqual(a, l)
+            a2 = a[:]
+            a2[:i] = a[:i]
+            self.assertEqual(a2, a)
+            a[i:] = l[i:]
+            self.assertEqual(a, l)
+            a2 = a[:]
+            a2[i:] = a[i:]
+            self.assertEqual(a2, a)
+            for j in range(-3, 4):
+                a[i:j] = l[i:j]
+                self.assertEqual(a, l)
+                a2 = a[:]
+                a2[i:j] = a[i:j]
+                self.assertEqual(a2, a)
+
+        aa2 = a2[:]
+        aa2[:0] = [-2, -1]
+        self.assertEqual(aa2, [-2, -1, 0, 1])
+        aa2[0:] = []
+        self.assertEqual(aa2, [])
+
+        a = self.type2test([1, 2, 3, 4, 5])
+        a[:-1] = a
+        self.assertEqual(a, self.type2test([1, 2, 3, 4, 5, 5]))
+        a = self.type2test([1, 2, 3, 4, 5])
+        a[1:] = a
+        self.assertEqual(a, self.type2test([1, 1, 2, 3, 4, 5]))
+        a = self.type2test([1, 2, 3, 4, 5])
+        a[1:-1] = a
+        self.assertEqual(a, self.type2test([1, 1, 2, 3, 4, 5, 5]))
+
+        a = self.type2test([])
+        a[:] = tuple(range(10))
+        self.assertEqual(a, self.type2test(range(10)))
+
+        self.assertRaises(TypeError, a.__setslice__, 0, 1, 5)
+
+        self.assertRaises(TypeError, a.__setslice__)
+
+    def test_delslice(self):
+        a = self.type2test([0, 1])
+        del a[1:2]
+        del a[0:1]
+        self.assertEqual(a, self.type2test([]))
+
+        a = self.type2test([0, 1])
+        del a[1L:2L]
+        del a[0L:1L]
+        self.assertEqual(a, self.type2test([]))
+
+        a = self.type2test([0, 1])
+        del a[-2:-1]
+        self.assertEqual(a, self.type2test([1]))
+
+        a = self.type2test([0, 1])
+        del a[-2L:-1L]
+        self.assertEqual(a, self.type2test([1]))
+
+        a = self.type2test([0, 1])
+        del a[1:]
+        del a[:1]
+        self.assertEqual(a, self.type2test([]))
+
+        a = self.type2test([0, 1])
+        del a[1L:]
+        del a[:1L]
+        self.assertEqual(a, self.type2test([]))
+
+        a = self.type2test([0, 1])
+        del a[-1:]
+        self.assertEqual(a, self.type2test([0]))
+
+        a = self.type2test([0, 1])
+        del a[-1L:]
+        self.assertEqual(a, self.type2test([0]))
+
+        a = self.type2test([0, 1])
+        del a[:]
+        self.assertEqual(a, self.type2test([]))
+
+    def test_append(self):
+        a = self.type2test([])
+        a.append(0)
+        a.append(1)
+        a.append(2)
+        self.assertEqual(a, self.type2test([0, 1, 2]))
+
+        self.assertRaises(TypeError, a.append)
+
+    def test_extend(self):
+        a1 = self.type2test([0])
+        a2 = self.type2test((0, 1))
+        a = a1[:]
+        a.extend(a2)
+        self.assertEqual(a, a1 + a2)
+
+        a.extend(self.type2test([]))
+        self.assertEqual(a, a1 + a2)
+
+        a.extend(a)
+        self.assertEqual(a, self.type2test([0, 0, 1, 0, 0, 1]))
+
+        a = self.type2test("spam")
+        a.extend("eggs")
+        self.assertEqual(a, list("spameggs"))
+
+        self.assertRaises(TypeError, a.extend, None)
+
+        self.assertRaises(TypeError, a.extend)
+
+    def test_insert(self):
+        a = self.type2test([0, 1, 2])
+        a.insert(0, -2)
+        a.insert(1, -1)
+        a.insert(2, 0)
+        self.assertEqual(a, [-2, -1, 0, 0, 1, 2])
+
+        b = a[:]
+        b.insert(-2, "foo")
+        b.insert(-200, "left")
+        b.insert(200, "right")
+        self.assertEqual(b, self.type2test(["left",-2,-1,0,0,"foo",1,2,"right"]))
+
+        self.assertRaises(TypeError, a.insert)
+
+    def test_pop(self):
+        a = self.type2test([-1, 0, 1])
+        a.pop()
+        self.assertEqual(a, [-1, 0])
+        a.pop(0)
+        self.assertEqual(a, [0])
+        self.assertRaises(IndexError, a.pop, 5)
+        a.pop(0)
+        self.assertEqual(a, [])
+        self.assertRaises(IndexError, a.pop)
+
+        self.assertRaises(TypeError, a.pop, 42, 42)
+
+    def test_remove(self):
+        a = self.type2test([0, 0, 1])
+        a.remove(1)
+        self.assertEqual(a, [0, 0])
+        a.remove(0)
+        self.assertEqual(a, [0])
+        a.remove(0)
+        self.assertEqual(a, [])
+
+        self.assertRaises(ValueError, a.remove, 0)
+        
+        self.assertRaises(TypeError, a.remove)
+
+        class BadExc(Exception):
+            pass
+
+        class BadCmp:
+            def __eq__(self, other):
+                if other == 2:
+                    raise BadExc()
+                return False
+
+        a = self.type2test([0, 1, 2, 3])
+        self.assertRaises(BadExc, a.remove, BadCmp())
+
+    def test_count(self):
+        a = self.type2test([0, 1, 2])*3
+        self.assertEqual(a.count(0), 3)
+        self.assertEqual(a.count(1), 3)
+        self.assertEqual(a.count(3), 0)
+
+        self.assertRaises(TypeError, a.count)
+        
+        class BadExc(Exception):
+            pass
+
+        class BadCmp:
+            def __eq__(self, other):
+                if other == 2:
+                    raise BadExc()
+                return False
+
+        self.assertRaises(BadExc, a.count, BadCmp())
+
+    def test_index(self):
+        u = self.type2test([0, 1])
+        self.assertEqual(u.index(0), 0)
+        self.assertEqual(u.index(1), 1)
+        self.assertRaises(ValueError, u.index, 2)
+
+        u = self.type2test([-2, -1, 0, 0, 1, 2])
+        self.assertEqual(u.count(0), 2)
+        self.assertEqual(u.index(0), 2)
+        self.assertEqual(u.index(0, 2), 2)
+        self.assertEqual(u.index(-2, -10), 0)
+        self.assertEqual(u.index(0, 3), 3)
+        self.assertEqual(u.index(0, 3, 4), 3)
+        self.assertRaises(ValueError, u.index, 2, 0, -10)
+
+        self.assertRaises(TypeError, u.index)
+
+        class BadExc(Exception):
+            pass
+
+        class BadCmp:
+            def __eq__(self, other):
+                if other == 2:
+                    raise BadExc()
+                return False
+
+        a = self.type2test([0, 1, 2, 3])
+        self.assertRaises(BadExc, a.index, BadCmp())
+
+        a = self.type2test([-2, -1, 0, 0, 1, 2])
+        self.assertEqual(a.index(0), 2)
+        self.assertEqual(a.index(0, 2), 2)
+        self.assertEqual(a.index(0, -4), 2)
+        self.assertEqual(a.index(-2, -10), 0)
+        self.assertEqual(a.index(0, 3), 3)
+        self.assertEqual(a.index(0, -3), 3)
+        self.assertEqual(a.index(0, 3, 4), 3)
+        self.assertEqual(a.index(0, -3, -2), 3)
+        self.assertEqual(a.index(0, -4*sys.maxint, 4*sys.maxint), 2)
+        self.assertRaises(ValueError, a.index, 0, 4*sys.maxint,-4*sys.maxint)
+        self.assertRaises(ValueError, a.index, 2, 0, -10)
+        a.remove(0)
+        self.assertRaises(ValueError, a.index, 2, 0, 4)
+        self.assertEqual(a, self.type2test([-2, -1, 0, 1, 2]))
+
+    def test_reverse(self):
+        u = self.type2test([-2, -1, 0, 1, 2])
+        u2 = u[:]
+        u.reverse()
+        self.assertEqual(u, [2, 1, 0, -1, -2])
+        u.reverse()
+        self.assertEqual(u, u2)
+        
+        self.assertRaises(TypeError, u.reverse, 42)
+
+    def test_sort(self):
+        u = self.type2test([1, 0])
+        u.sort()
+        self.assertEqual(u, [0, 1])
+        
+        u = self.type2test([2,1,0,-1,-2])
+        u.sort()
+        self.assertEqual(u, self.type2test([-2,-1,0,1,2]))
+        
+        self.assertRaises(TypeError, u.sort, 42, 42)
+
+        def revcmp(a, b):
+            return cmp(b, a)
+        u.sort(revcmp)
+        self.assertEqual(u, self.type2test([2,1,0,-1,-2]))
+
+        # The following dumps core in unpatched Python 1.5:
+        def myComparison(x,y):
+            return cmp(x%3, y%7)
+        z = self.type2test(range(12))
+        z.sort(myComparison)
+        
+        self.assertRaises(TypeError, z.sort, 2)
+        
+        def selfmodifyingComparison(x,y):
+            z.append(1)
+            return cmp(x, y)
+        self.assertRaises(ValueError, z.sort, selfmodifyingComparison)
+        
+        self.assertRaises(TypeError, z.sort, lambda x, y: 's')
+
+        self.assertRaises(TypeError, z.sort, 42, 42, 42, 42)
+
+    def test_slice(self):
+        u = self.type2test("spam")
+        u[:2] = "h"
+        self.assertEqual(u, list("ham"))
+
+    def test_iadd(self):
+        super(CommonTest, self).test_iadd()
+        u = self.type2test([0, 1])
+        u2 = u
+        u += [2, 3]
+        self.assert_(u is u2)
+
+        u = self.type2test("spam")
+        u += "eggs"
+        self.assertEqual(u, self.type2test("spameggs"))
+
+        self.assertRaises(TypeError, u.__iadd__, None)
+
+    def test_imul(self):
+        u = self.type2test([0, 1])
+        u *= 3
+        self.assertEqual(u, self.type2test([0, 1, 0, 1, 0, 1]))
+        u *= 0
+        self.assertEqual(u, self.type2test([]))
+
+    def test_extendedslicing(self):
+        #  subscript
+        a = self.type2test([0,1,2,3,4])
+
+        #  deletion
+        del a[::2]
+        self.assertEqual(a, self.type2test([1,3]))
+        a = self.type2test(range(5))
+        del a[1::2]
+        self.assertEqual(a, self.type2test([0,2,4]))
+        a = self.type2test(range(5))
+        del a[1::-2]
+        self.assertEqual(a, self.type2test([0,2,3,4]))
+        a = self.type2test(range(10))
+        del a[::1000]
+        self.assertEqual(a, self.type2test([1, 2, 3, 4, 5, 6, 7, 8, 9]))
+        #  assignment
+        a = self.type2test(range(10))
+        a[::2] = [-1]*5
+        self.assertEqual(a, self.type2test([-1, 1, -1, 3, -1, 5, -1, 7, -1, 9]))
+        a = self.type2test(range(10))
+        a[::-4] = [10]*3
+        self.assertEqual(a, self.type2test([0, 10, 2, 3, 4, 10, 6, 7, 8 ,10]))
+        a = self.type2test(range(4))
+        a[::-1] = a
+        self.assertEqual(a, self.type2test([3, 2, 1, 0]))
+        a = self.type2test(range(10))
+        b = a[:]
+        c = a[:]
+        a[2:3] = self.type2test(["two", "elements"])
+        b[slice(2,3)] = self.type2test(["two", "elements"])
+        c[2:3:] = self.type2test(["two", "elements"])
+        self.assertEqual(a, b)
+        self.assertEqual(a, c)
+        a = self.type2test(range(10))
+        a[::2] = tuple(range(5))
+        self.assertEqual(a, self.type2test([0, 1, 1, 3, 2, 5, 3, 7, 4, 9]))
--- Lib/test/test_userlist.py
+++ Lib/test/test_userlist.py
@@ -1,164 +1,21 @@
 # Check every path through every method of UserList
 
 from UserList import UserList
-import unittest, test.test_support
+import unittest
+from test import test_support, list_tests
 
-class UserListTest(unittest.TestCase):
-
-    def test_constructors(self):
-        l0 = []
-        l1 = [0]
-        l2 = [0, 1]
-
-        u = UserList()
-        u0 = UserList(l0)
-        u1 = UserList(l1)
-        u2 = UserList(l2)
-
-        uu = UserList(u)
-        uu0 = UserList(u0)
-        uu1 = UserList(u1)
-        uu2 = UserList(u2)
-
-        v = UserList(tuple(u))
-        class OtherList:
-            def __init__(self, initlist):
-                self.__data = initlist
-            def __len__(self):
-                return len(self.__data)
-            def __getitem__(self, i):
-                return self.__data[i]
-        v0 = UserList(OtherList(u0))
-        vv = UserList("this is also a sequence")
-
-    def test_repr(self):
-        l0 = []
-        l2 = [0, 1, 2]
-        u0 = UserList(l0)
-        u2 = UserList(l2)
-
-        self.assertEqual(str(u0), str(l0))
-        self.assertEqual(repr(u0), repr(l0))
-        self.assertEqual(`u2`, `l2`)
-
-    def test_cmplen(self):
-        l0 = []
-        l1 = [0]
-        l2 = [0, 1]
-
-        # Test constructors
-
-        u = UserList()
-        u0 = UserList(l0)
-        u1 = UserList(l1)
-        u2 = UserList(l2)
-
-        uu = UserList(u)
-        uu0 = UserList(u0)
-        uu1 = UserList(u1)
-        uu2 = UserList(u2)
-
-        def mycmp(x, y):
-            r = cmp(x, y)
-            if r < 0: return -1
-            if r > 0: return 1
-            return r
-
-        all = [l0, l1, l2, u, u0, u1, u2, uu, uu0, uu1, uu2]
-        for a in all:
-            for b in all:
-                self.assertEqual(mycmp(a, b), mycmp(len(a), len(b)))
-
-        self.assert_(u0 <= u2)
-        self.assert_(u2 >= u0)
-
-    def test_getitem(self):
-        u = UserList([0, 1, 2])
-        for i in xrange(len(u)):
-            self.assertEqual(u[i], i)
-
-    def test_setitem(self):
-        u = UserList([0, 1])
-        u[0] = 0
-        u[1] = 100
-        self.assertEqual(u, [0, 100])
-        self.assertRaises(IndexError, u.__setitem__, 2, 200)
-
-    def test_delitem(self):
-        u = UserList([0, 1])
-        del u[1]
-        del u[0]
-        self.assertRaises(IndexError, u.__delitem__, 0)
+class UserListTest(list_tests.CommonTest):
+    type2test = UserList
 
     def test_getslice(self):
-        l = [0, 1]
-        u = UserList(l)
-        for i in xrange(-3, 4):
+        super(UserListTest, self).test_getslice()
+        l = [0, 1, 2, 3, 4]
+        u = self.type2test(l)
+        for i in range(-3, 6):
             self.assertEqual(u[:i], l[:i])
             self.assertEqual(u[i:], l[i:])
-            for j in xrange(-3, 4):
+            for j in xrange(-3, 6):
                 self.assertEqual(u[i:j], l[i:j])
-
-    def test_setslice(self):
-        l = [0, 1]
-        u = UserList(l)
-
-        # Test __setslice__
-        for i in range(-3, 4):
-            u[:i] = l[:i]
-            self.assertEqual(u, l)
-            u2 = u[:]
-            u2[:i] = u[:i]
-            self.assertEqual(u2, u)
-            u[i:] = l[i:]
-            self.assertEqual(u, l)
-            u2 = u[:]
-            u2[i:] = u[i:]
-            self.assertEqual(u2, u)
-            for j in range(-3, 4):
-                u[i:j] = l[i:j]
-                self.assertEqual(u, l)
-                u2 = u[:]
-                u2[i:j] = u[i:j]
-                self.assertEqual(u2, u)
-
-        uu2 = u2[:]
-        uu2[:0] = [-2, -1]
-        self.assertEqual(uu2, [-2, -1, 0, 1])
-        uu2[0:] = []
-        self.assertEqual(uu2, [])
-
-    def test_contains(self):
-        u = UserList([0, 1, 2])
-        for i in u:
-            self.assert_(i in u)
-        for i in min(u)-1, max(u)+1:
-            self.assert_(i not in u)
-
-    def test_delslice(self):
-        u = UserList([0, 1])
-        del u[1:2]
-        del u[0:1]
-        self.assertEqual(u, [])
-
-        u = UserList([0, 1])
-        del u[1:]
-        del u[:1]
-        self.assertEqual(u, [])
-
-    def test_addmul(self):
-        u1 = UserList([0])
-        u2 = UserList([0, 1])
-        self.assertEqual(u1, u1 + [])
-        self.assertEqual(u1, [] + u1)
-        self.assertEqual(u1 + [1], u2)
-        self.assertEqual([-1] + u1, [-1, 0])
-        self.assertEqual(u2, u2*1)
-        self.assertEqual(u2, 1*u2)
-        self.assertEqual(u2+u2, u2*2)
-        self.assertEqual(u2+u2, 2*u2)
-        self.assertEqual(u2+u2+u2, u2*3)
-        self.assertEqual(u2+u2+u2, 3*u2)
 
     def test_add_specials(self):
         u = UserList("spam")
@@ -172,96 +29,32 @@ class UserListTest(unittest.TestCase):
         u2 = u.__radd__(UserList("spam"))
         self.assertEqual(u2, list("spameggs"))
 
-    def test_append(self):
-        u = UserList((0, ))
-        u.append(1)
-        self.assertEqual(u, [0, 1])
-
-    def test_insert(self):
-        u = UserList((0, 1))
-        u.insert(0, -1)
-        self.assertEqual(u, [-1, 0, 1])
-
-    def test_pop(self):
-        u = UserList((-1, 0, 1))
-        u.pop()
-        self.assertEqual(u, [-1, 0])
-        u.pop(0)
-        self.assertEqual(u, [0])
-
-    def test_remove(self):
-        u = UserList((0, 1))
-        u.remove(1)
-        self.assertEqual(u, [0])
-
-    def test_count(self):
-        u = UserList((0, 1))*3
-        self.assertEqual(u.count(0), 3)
-        self.assertEqual(u.count(1), 3)
-        self.assertEqual(u.count(2), 0)
-
-    def test_index(self):
-        u = UserList((0, 1))
-        self.assertEqual(u.index(0), 0)
-        self.assertEqual(u.index(1), 1)
-        self.assertRaises(ValueError, u.index, 2)
-
-        u = UserList([-2,-1,0,0,1,2])
-        self.assertEqual(u.count(0), 2)
-        self.assertEqual(u.index(0), 2)
-        self.assertEqual(u.index(0,2), 2)
-        self.assertEqual(u.index(-2,-10), 0)
-        self.assertEqual(u.index(0,3), 3)
-        self.assertEqual(u.index(0,3,4), 3)
-        self.assertRaises(ValueError, u.index, 2,0,-10)
-
-    def test_reverse(self):
-        u = UserList((0, 1))
-        u2 = u[:]
-        u.reverse()
-        self.assertEqual(u, [1, 0])
-        u.reverse()
-        self.assertEqual(u, u2)
-
-    def test_sort(self):
-        u = UserList([1, 0])
-        u.sort()
-        self.assertEqual(u, [0, 1])
-
-    def test_slice(self):
-        u = UserList("spam")
-        u[:2] = "h"
-        self.assertEqual(u, list("ham"))
-
     def test_iadd(self):
-        u = UserList((0, 1))
-        u += [0, 1]
-        self.assertEqual(u, [0, 1, 0, 1])
+        super(UserListTest, self).test_iadd()
+        u = [0, 1]
         u += UserList([0, 1])
-        self.assertEqual(u, [0, 1, 0, 1, 0, 1])
+        self.assertEqual(u, [0, 1, 0, 1])
 
-        u = UserList("spam")
-        u += "eggs"
-        self.assertEqual(u, list("spameggs"))
+    def test_mixedcmp(self):
+        u = self.type2test([0, 1])
+        self.assertEqual(u, [0, 1])
+        self.assertNotEqual(u, [0])
+        self.assertNotEqual(u, [0, 2])
 
-    def test_extend(self):
-        u1 = UserList((0, ))
-        u2 = UserList((0, 1))
-        u = u1[:]
-        u.extend(u2)
-        self.assertEqual(u, u1 + u2)
+    def test_mixedadd(self):
+        u = self.type2test([0, 1])
+        self.assertEqual(u + [], u)
+        self.assertEqual(u + [2], [0, 1, 2])
 
-        u = UserList("spam")
-        u.extend("eggs")
-        self.assertEqual(u, list("spameggs"))
-
-    def test_imul(self):
-        u = UserList((0, 1))
-        u *= 3
-        self.assertEqual(u, [0, 1, 0, 1, 0, 1])
+    def test_getitemoverwriteiter(self):
+        # Verify that __getitem__ overrides *are* recognized by __iter__
+        class T(self.type2test):
+            def __getitem__(self, key):
+                return str(key) + '!!!'
+        self.assertEqual(iter(T((1,2))).next(), "0!!!")
 
 def test_main():
-    test.test_support.run_unittest(UserListTest)
+    test_support.run_unittest(UserListTest)
 
 if __name__ == "__main__":
     test_main()
