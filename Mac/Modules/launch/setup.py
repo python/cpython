2af4ce8f3009d728d1c17758468de14920821b3d
Jack Jansen <jack.jansen@cwi.nl>
2003-12-09 14:51:21 +0000

    Made this module compatible with Python2.3, and added a temporary setup script that allows it to be built for that Python.

---
+++ Mac/Modules/launch/setup.py
@@ -0,0 +1,13 @@
+# This is a temporary setup script to allow distribution of
+# MacPython 2.4 modules for MacPython 2.3.
+
+from distutils.core import Extension, setup
+
+setup(name="LaunchServices", version="0.1",
+	ext_modules=[
+		Extension('_Launch', ['_Launchmodule.c'],
+		extra_link_args=['-framework', 'ApplicationServices'])
+	],
+	py_modules=['LaunchServices.Launch', 'LaunchServices.LaunchServices'],
+	package_dir={'LaunchServices':'../../../Lib/plat-mac/Carbon'}
+	)
