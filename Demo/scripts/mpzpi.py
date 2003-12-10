ba128ac3e9aa84fd3a374f38b42e633415865f6c
Raymond Hettinger <python@rcn.com>
2003-12-10 15:22:23 +0000

    Revert previous change which didn't make sense the next day :-)

--- Demo/scripts/mpzpi.py
+++ Demo/scripts/mpzpi.py
@@ -8,25 +8,27 @@
 # published by Prentice-Hall (UK) Ltd., 1990.
 
 import sys
+from mpz import mpz
 
 def main():
-        k, a, b, a1, b1 = 2, 4, 1, 12, 4
-        while 1:
-                # Next approximation
-                p, q, k = k*k, 2*k+1, k+1
-                a, b, a1, b1 = a1, b1, p*a+q*a1, p*b+q*b1
-                # Print common digits
-                d, d1 = a/b, a1/b1
-                while d == d1:
-                        output(d)
-                        a, a1 = 10*(a%b), 10*(a1%b1)
-                        d, d1 = a/b, a1/b1
+	mpzone, mpztwo, mpzten = mpz(1), mpz(2), mpz(10)
+	k, a, b, a1, b1 = mpz(2), mpz(4), mpz(1), mpz(12), mpz(4)
+	while 1:
+		# Next approximation
+		p, q, k = k*k, mpztwo*k+mpzone, k+mpzone
+		a, b, a1, b1 = a1, b1, p*a+q*a1, p*b+q*b1
+		# Print common digits
+		d, d1 = a/b, a1/b1
+		while d == d1:
+			output(d)
+			a, a1 = mpzten*(a%b), mpzten*(a1%b1)
+			d, d1 = a/b, a1/b1
 
 def output(d):
-        # Use write() to avoid spaces between the digits
-        # Use int(d) to avoid a trailing L after each digit
-        sys.stdout.write(`int(d)`)
-        # Flush so the output is seen immediately
-        sys.stdout.flush()
+	# Use write() to avoid spaces between the digits
+	# Use int(d) to avoid a trailing L after each digit
+	sys.stdout.write(`int(d)`)
+	# Flush so the output is seen immediately
+	sys.stdout.flush()
 
 main()
