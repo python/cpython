b994d7935849c07ceb6fcf8bf296d9b20bb1cb84
Raymond Hettinger <python@rcn.com>
2004-01-20 20:04:40 +0000

    Add a Guido inspired example for groupby().

--- Lib/test/test_itertools.py
+++ Lib/test/test_itertools.py
@@ -677,6 +677,20 @@ Samuele
 2 ['b', 'd', 'f']
 3 ['g']
 
+# Find runs of consecutive numbers using groupby.  The key to the solution
+# is differencing with a range so that consecutive numbers all appear in
+# same group.
+>>> data = [ 1,  4,5,6, 10, 15,16,17,18, 22, 25,26,27,28]
+>>> for k, g in groupby(enumerate(data), lambda (i,x):i-x):
+...     print map(operator.itemgetter(1), g)
+...
+[1]
+[4, 5, 6]
+[10]
+[15, 16, 17, 18]
+[22]
+[25, 26, 27, 28]
+
 >>> def take(n, seq):
 ...     return list(islice(seq, n))
 
