61dafcb6caee4c0971e8aa362b1b170ce19199eb
Fred Drake <fdrake@acm.org>
2004-02-12 19:30:17 +0000

    use existing test object instead of defining a new class

--- Lib/test/test_weakref.py
+++ Lib/test/test_weakref.py
@@ -602,22 +602,20 @@ class ReferencesTestCase(TestBase):
         thresholds = gc.get_threshold()
         gc.set_threshold(1, 1, 1)
         gc.collect()
-        class A:
-            pass
 
         def callback(*args):
             pass
 
-        referenced = A()
+        referenced = C()
 
-        a = A()
+        a = C()
         a.a = a
         a.wr = makeref(referenced)
 
         try:
             # now make sure the object and the ref get labeled as
             # cyclic trash:
-            a = A()
+            a = C()
             a.wrc = weakref.ref(referenced, callback)
 
         finally:
