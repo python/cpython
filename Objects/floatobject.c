884f85cd8e1c6eb2d1d4eb61f7858175289d59dc
Michael W. Hudson <mwh@python.net>
2004-02-26 13:16:03 +0000

    Oops, didn't mean to commit the removal of float_compare!

--- Objects/floatobject.c
+++ Objects/floatobject.c
@@ -852,7 +852,7 @@ PyTypeObject PyFloat_Type = {
 	(printfunc)float_print, 		/* tp_print */
 	0,					/* tp_getattr */
 	0,					/* tp_setattr */
-	0, 		/* tp_compare */
+	(cmpfunc)float_compare, 		/* tp_compare */
 	(reprfunc)float_repr,			/* tp_repr */
 	&float_as_number,			/* tp_as_number */
 	0,					/* tp_as_sequence */
