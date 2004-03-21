9303777f224c536ce8be9297d208f46527912b46
Andrew M. Kuchling <amk@amk.ca>
2004-03-21 19:26:00 +0000

    [Part of patch #909005] Handle POLLPRI flag, and various errors cases.  Fixes bug #887279

--- Lib/asyncore.py
+++ Lib/asyncore.py
@@ -82,10 +82,12 @@ def write(obj):
 
 def readwrite(obj, flags):
     try:
-        if flags & select.POLLIN:
+        if flags & (select.POLLIN | select.POLLPRI):
             obj.handle_read_event()
         if flags & select.POLLOUT:
             obj.handle_write_event()
+        if flags & (select.POLLERR | select.POLLHUP | select.POLLNVAL):
+            obj.handle_expt_event()
     except ExitNow:
         raise
     except:
