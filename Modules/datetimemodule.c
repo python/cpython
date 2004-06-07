d8b9346a0d1f0bf99fbb0f71007f9de81b8d1088
Tim Peters <tim.peters@gmail.com>
2004-06-07 23:17:48 +0000

    SF 952807:  Unpickling pickled instances of subclasses of datetime.date, datetime.datetime and datetime.time could yield insane objects.  Thanks to Jiwon Seo for a fix.

--- Modules/datetimemodule.c
+++ Modules/datetimemodule.c
@@ -2199,7 +2199,7 @@ date_new(PyTypeObject *type, PyObject *args, PyObject *kw)
 	{
 	    	PyDateTime_Date *me;
 
-		me = PyObject_New(PyDateTime_Date, type);
+		me = (PyDateTime_Date *) (type->tp_alloc(type, 0));
 		if (me != NULL) {
 			char *pdata = PyString_AS_STRING(state);
 			memcpy(me->data, pdata, _PyDateTime_DATE_DATASIZE);
@@ -3042,8 +3042,7 @@ time_new(PyTypeObject *type, PyObject *args, PyObject *kw)
 			}
 		}
 		aware = (char)(tzinfo != Py_None);
-		me = (PyDateTime_Time *) time_alloc(&PyDateTime_TimeType,
-						    aware);
+		me = (PyDateTime_Time *) (type->tp_alloc(type, aware));
 		if (me != NULL) {
 			char *pdata = PyString_AS_STRING(state);
 
@@ -3564,9 +3563,7 @@ datetime_new(PyTypeObject *type, PyObject *args, PyObject *kw)
 			}
 		}
 		aware = (char)(tzinfo != Py_None);
-		me = (PyDateTime_DateTime *) datetime_alloc(
-						&PyDateTime_DateTimeType,
-				     		aware);
+		me = (PyDateTime_DateTime *) (type->tp_alloc(type , aware));
 		if (me != NULL) {
 			char *pdata = PyString_AS_STRING(state);
 
