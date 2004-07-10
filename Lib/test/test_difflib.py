d2c5b4b549b24e637b16c60bdfae7292e1e75c06
Brett Cannon <bcannon@gmail.com>
2004-07-10 23:54:07 +0000

    SequenceMatcher(None, [], []).get_grouped_opcodes() now returns a generator that behaves as if both lists has an empty string in each of them.
    Closes bug #979794 (and duplicate bug #980117).

--- Lib/test/test_difflib.py
+++ Lib/test/test_difflib.py
@@ -12,6 +12,13 @@ class TestSFbugs(unittest.TestCase):
         self.assertEqual(s.quick_ratio(), 1)
         self.assertEqual(s.real_quick_ratio(), 1)
 
+    def test_comparing_empty_lists(self):
+        # Check fix for bug #979794
+        group_gen = difflib.SequenceMatcher(None, [], []).get_grouped_opcodes()
+        self.assertRaises(StopIteration, group_gen.next)
+        diff_gen = difflib.unified_diff([], [])
+        self.assertRaises(StopIteration, diff_gen.next)
+
 Doctests = doctest.DocTestSuite(difflib)
 
 test_support.run_unittest(TestSFbugs, Doctests)
