ceddfbcdf0a32b67073692afcabc2eeb7c0cc977
Marc-André Lemburg <mal@egenix.com>
2004-07-23 10:09:57 +0000

    Remove copyright notices from gencodec.py output.
    The script was originally used to create the initial set of
    codecs (and these were (c) CNRI). While the script itself still
    is (c) CNRI, the output certainly isn't anymore.

--- Tools/scripts/gencodec.py
+++ Tools/scripts/gencodec.py
@@ -154,11 +154,6 @@ def codegen(name,map,comments=1):
         '''\
 """ Python Character Mapping Codec generated from '%s' with gencodec.py.
 
-Written by Marc-Andre Lemburg (mal@lemburg.com).
-
-(c) Copyright CNRI, All Rights Reserved. NO WARRANTY.
-(c) Copyright 2000 Guido van Rossum.
-
 """#"
 
 import codecs
