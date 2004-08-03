3d10a5eaa863d8cbf88ad9d033a766bb64a83829
Martin v. Löwis <martin@v.loewis.de>
2004-08-03 12:41:42 +0000

    Patch #870382: Automatically add msvcr71 to the list of libraries if Python was built with VC 7.1.

--- Lib/distutils/cygwinccompiler.py
+++ Lib/distutils/cygwinccompiler.py
@@ -122,6 +122,17 @@ class CygwinCCompiler (UnixCCompiler):
                 "Consider upgrading to a newer version of gcc")
         else:
             self.dll_libraries=[]
+	    # Include the appropriate MSVC runtime library if Python was built
+	    # with MSVC 7.0 or 7.1.
+	    msc_pos = sys.version.find('MSC v.')
+	    if msc_pos != -1:
+		msc_ver = sys.version[msc_pos+6:msc_pos+10]
+		if msc_ver == '1300':
+		    # MSVC 7.0
+		    self.dll_libraries = ['msvcr70']
+		elif msc_ver == '1310':
+		    # MSVC 7.1
+		    self.dll_libraries = ['msvcr71']
 
     # __init__ ()
 
@@ -307,6 +318,18 @@ class Mingw32CCompiler (CygwinCCompiler):
 
         # no additional libraries needed
         self.dll_libraries=[]
+
+	# Include the appropriate MSVC runtime library if Python was built
+	# with MSVC 7.0 or 7.1.
+	msc_pos = sys.version.find('MSC v.')
+	if msc_pos != -1:
+	    msc_ver = sys.version[msc_pos+6:msc_pos+10]
+	    if msc_ver == '1300':
+		# MSVC 7.0
+		self.dll_libraries = ['msvcr70']
+	    elif msc_ver == '1310':
+		# MSVC 7.1
+		self.dll_libraries = ['msvcr71']
 
     # __init__ ()
 
