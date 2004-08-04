eb477d04f7be4bfbf356825a0d832c012dab1ac7
Vinay Sajip <vinay_sajip@yahoo.co.uk>
2004-08-04 08:38:08 +0000

    Updated Logger.log() docstring to clarify that lvl parameter should be an integer.

--- Lib/logging/__init__.py
+++ Lib/logging/__init__.py
@@ -967,7 +967,7 @@ class Logger(Filterer):
 
     def log(self, level, msg, *args, **kwargs):
         """
-        Log 'msg % args' with the severity 'level'.
+        Log 'msg % args' with the integer severity 'level'.
 
         To pass exception information, use the keyword argument exc_info with
         a true value, e.g.
