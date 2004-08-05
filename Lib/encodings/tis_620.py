c759f070ef4fff47ad515913a78521f481918872
Marc-André Lemburg <mal@egenix.com>
2004-08-05 12:43:30 +0000

    Added new codecs and aliases for ISO_8859-11, ISO_8859-16 and TIS-620.
    Closes SF bug #1001895: Adding missing ISO 8859 codecs, especially Thai.

---
+++ Lib/encodings/tis_620.py
@@ -0,0 +1,46 @@
+""" Python Character Mapping Codec for TIS-620.
+
+    According to
+    ftp://ftp.unicode.org/Public/MAPPINGS/ISO8859/8859-11.TXT the
+    TIS-620 is the identical to ISO_8859-11 with the 0xA0 (no-break
+    space) mapping removed.
+
+"""#"
+
+import codecs
+from encodings.iso8859_11 import decoding_map
+
+### Codec APIs
+
+class Codec(codecs.Codec):
+
+    def encode(self,input,errors='strict'):
+
+        return codecs.charmap_encode(input,errors,encoding_map)
+
+    def decode(self,input,errors='strict'):
+
+        return codecs.charmap_decode(input,errors,decoding_map)
+
+class StreamWriter(Codec,codecs.StreamWriter):
+    pass
+
+class StreamReader(Codec,codecs.StreamReader):
+    pass
+
+### encodings module API
+
+def getregentry():
+
+    return (Codec().encode,Codec().decode,StreamReader,StreamWriter)
+
+### Decoding Map
+
+decoding_map = decoding_map.copy()
+decoding_map.update({
+	0x00a0: None,
+})
+
+### Encoding Map
+
+encoding_map = codecs.make_encoding_map(decoding_map)
