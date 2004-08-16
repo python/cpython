715e64843687c11199e24dae4ee27e9e683c73c9
Matthias Klose <doko@ubuntu.com>
2004-08-16 11:35:51 +0000

    The attached patch fixes FTBFS on GNU/k*BSD.  The problem happens on GNU/k*BSD because GNU/k*BSD uses gnu pth to provide pthreads, but will also happen on any system that does the same.
    python fails to build because it doesn't detect gnu pth in pthread
    emulation. See C comments in patch for details.
    
    patch taken from http://bugs.debian.org/264315

--- Python/thread.c
+++ Python/thread.c
@@ -7,6 +7,16 @@
 
 #include "Python.h"
 
+
+#ifndef _POSIX_THREADS
+/* This means pthreads are not implemented in libc headers, hence the macro
+   not present in unistd.h. But they still can be implemented as an external
+   library (e.g. gnu pth in pthread emulation) */
+# ifdef HAVE_PTHREAD_H
+#  include <pthread.h> /* _POSIX_THREADS */
+# endif
+#endif
+
 #ifndef DONT_HAVE_STDIO_H
 #include <stdio.h>
 #endif
