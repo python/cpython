8158e849305d0e0ab3e19cdc93a86bb7d5fc0651
Raymond Hettinger <python@rcn.com>
2004-09-06 07:04:09 +0000

    Fix erroneous docstring comment.

--- Lib/heapq.py
+++ Lib/heapq.py
@@ -154,9 +154,10 @@ def heapreplace(heap, item):
     This is more efficient than heappop() followed by heappush(), and can be
     more appropriate when using a fixed-size heap.  Note that the value
     returned may be larger than item!  That constrains reasonable uses of
-    this routine unless written as part of a larger expression:
+    this routine unless written as part of a conditional replacement:
 
-        result = item <= heap[0] and item  or  heapreplace(heap, item)
+        if item > heap[0]:
+            item = heapreplace(heap, item)
     """
     returnitem = heap[0]    # raises appropriate IndexError if heap is empty
     heap[0] = item
