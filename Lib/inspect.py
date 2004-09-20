d1f1aa4f419c2c1bb908ed3b471960d34494b3e7
Skip Montanaro <skip@pobox.com>
2004-09-20 16:43:30 +0000

    Raymond reminded me to use DSU key

--- Lib/inspect.py
+++ Lib/inspect.py
@@ -557,8 +557,7 @@ def getsource(object):
 def walktree(classes, children, parent):
     """Recursive helper function for getclasstree()."""
     results = []
-    classes.sort(lambda a, b: cmp("%s.%s" % (a.__module__, a.__name__),
-                                  "%s.%s" % (b.__module__, b.__name__)))
+    classes.sort(key=lambda c: (c.__module__, c.__name__))
     for c in classes:
         results.append((c, c.__bases__))
         if c in children:
