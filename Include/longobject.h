b1350b95378b6cefe0bcd4c5e457d5a92c535b71
Tim Peters <tim.peters@gmail.com>
2004-09-23 01:56:02 +0000

    Removed redundant declaration of _PyLong_NumBits().

--- Include/longobject.h
+++ Include/longobject.h
@@ -53,7 +53,6 @@ PyAPI_FUNC(PyObject *) PyLong_FromUnicode(Py_UNICODE*, int, int);
 PyAPI_FUNC(int) _PyLong_Sign(PyObject *v);
 
 
-PyAPI_FUNC(size_t) _PyLong_NumBits(PyObject *v);
 /* _PyLong_NumBits.  Return the number of bits needed to represent the
    absolute value of a long.  For example, this returns 1 for 1 and -1, 2
    for 2 and -2, and 2 for 3 and -3.  It returns 0 for 0.
