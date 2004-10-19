cb9b4901c42bec43386a5323e02a21f361c5dd63
Andrew M. Kuchling <amk@amk.ca>
2004-10-19 19:36:09 +0000

    Display helpful message; print repr() of return value so the whitespace is clearer

--- Lib/curses/textpad.py
+++ Lib/curses/textpad.py
@@ -163,10 +163,11 @@ if __name__ == '__main__':
     def test_editbox(stdscr):
         ncols, nlines = 9, 4
         uly, ulx = 15, 20
+        stdscr.addstr(uly-2, ulx, "Use Ctrl-G to end editing.")
         win = curses.newwin(nlines, ncols, uly, ulx)
         rectangle(stdscr, uly-1, ulx-1, uly + nlines, ulx + ncols)
         stdscr.refresh()
         return Textbox(win).edit()
 
     str = curses.wrapper(test_editbox)
-    print str
+    print 'Contents of text box:', repr(str)
