a96c993c147f7089eb9485dfaaf55fd5ae6b293f
Anthony Baxter <anthonybaxter@gmail.com>
2004-11-15 15:03:25 +0000

    preparing for rc1

--- Include/patchlevel.h
+++ Include/patchlevel.h
@@ -14,7 +14,7 @@
 /* Values for PY_RELEASE_LEVEL */
 #define PY_RELEASE_LEVEL_ALPHA	0xA
 #define PY_RELEASE_LEVEL_BETA	0xB
-#define PY_RELEASE_LEVEL_GAMMA	0xC
+#define PY_RELEASE_LEVEL_GAMMA	0xC     /* For release candidates */
 #define PY_RELEASE_LEVEL_FINAL	0xF	/* Serial should be 0 here */
 					/* Higher for patch releases */
 
@@ -22,11 +22,11 @@
 #define PY_MAJOR_VERSION	2
 #define PY_MINOR_VERSION	4
 #define PY_MICRO_VERSION	0
-#define PY_RELEASE_LEVEL	PY_RELEASE_LEVEL_BETA
-#define PY_RELEASE_SERIAL	2
+#define PY_RELEASE_LEVEL	PY_RELEASE_LEVEL_GAMMA
+#define PY_RELEASE_SERIAL	1
 
 /* Version as a string */
-#define PY_VERSION		"2.4b2"
+#define PY_VERSION		"2.4c1"
 
 /* Version as a single 4-byte hex number, e.g. 0x010502B2 == 1.5.2b2.
    Use this for numeric comparisons, e.g. #if PY_VERSION_HEX >= ... */
