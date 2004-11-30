f6f2bb3fab9312f9349f63016585e27a4c574583
Peter Astrand <astrand@lysator.liu.se>
2004-11-30 18:06:42 +0000

    Corrected example for replacing shell pipeline. Fixes bug 1073790.

--- Lib/subprocess.py
+++ Lib/subprocess.py
@@ -229,7 +229,7 @@ Replacing shell pipe line
 output=`dmesg | grep hda`
 ==>
 p1 = Popen(["dmesg"], stdout=PIPE)
-p2 = Popen(["grep", "hda"], stdin=p1.stdout)
+p2 = Popen(["grep", "hda"], stdin=p1.stdout, stdout=PIPE)
 output = p2.communicate()[0]
 
 
