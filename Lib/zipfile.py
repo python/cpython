2ca7c190b61b891ef35a5eb7342b423055bb9082
Raymond Hettinger <python@rcn.com>
2005-02-16 09:27:49 +0000

    Remove dependency on order of mode flags

--- Lib/zipfile.py
+++ Lib/zipfile.py
@@ -193,7 +193,7 @@ class ZipFile:
         self.NameToInfo = {}    # Find file info given name
         self.filelist = []      # List of ZipInfo instances for archive
         self.compression = compression  # Method of compression
-        self.mode = key = mode[0].replace('b', '')
+        self.mode = key = mode.replace('b', '')[0]
 
         # Check if we were passed a file-like object
         if isinstance(file, basestring):
