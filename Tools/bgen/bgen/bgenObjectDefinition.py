4eff76547face3f6502ee89d8721dc39f9d8893e
Jack Jansen <jack.jansen@cwi.nl>
2005-06-23 22:32:59 +0000

    Added a missing newline Output().

--- Tools/bgen/bgen/bgenObjectDefinition.py
+++ Tools/bgen/bgen/bgenObjectDefinition.py
@@ -117,6 +117,7 @@ class ObjectDefinition(GeneratorGroup):
         "Override this method to apply additional checks/conversions"
 
     def outputConvert(self):
+        Output()
         Output("%sint %s_Convert(PyObject *v, %s *p_itself)", self.static, self.prefix,
                 self.itselftype)
         OutLbrace()
