f7d8cd997d2fb216357e10c5b2b8d9cb16db36d0
Georg Brandl <georg@python.org>
2005-08-22 18:02:59 +0000

    Bug #1266283: lexists() is not exported from os.path

--- Lib/posixpath.py
+++ Lib/posixpath.py
@@ -15,8 +15,8 @@ import stat
 
 __all__ = ["normcase","isabs","join","splitdrive","split","splitext",
            "basename","dirname","commonprefix","getsize","getmtime",
-           "getatime","getctime","islink","exists","isdir","isfile","ismount",
-           "walk","expanduser","expandvars","normpath","abspath",
+           "getatime","getctime","islink","exists","lexists","isdir","isfile",
+           "ismount","walk","expanduser","expandvars","normpath","abspath",
            "samefile","sameopenfile","samestat",
            "curdir","pardir","sep","pathsep","defpath","altsep","extsep",
            "devnull","realpath","supports_unicode_filenames"]
