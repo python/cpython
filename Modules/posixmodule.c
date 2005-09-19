c36179bf814b7075fe3341807ff0eac40c55dec7
Neal Norwitz <nnorwitz@gmail.com>
2005-09-19 06:42:30 +0000

    Fix problems reported by valgrind:  * Fix memory leak in posix.access()  * Fix use of uninitialized value in forkpty()    - from the manpage it isn't clear if there are conditions where master_fd      are uninitialized, but it's safer to initialize

--- Modules/posixmodule.c
+++ Modules/posixmodule.c
@@ -1119,6 +1119,7 @@ posix_access(PyObject *self, PyObject *args)
 	Py_BEGIN_ALLOW_THREADS
 	res = access(path, mode);
 	Py_END_ALLOW_THREADS
+	PyMem_Free(path);
 	return(PyBool_FromLong(res == 0));
 }
 
@@ -2950,7 +2951,7 @@ To both, return fd of newly opened pseudo-terminal.\n");
 static PyObject *
 posix_forkpty(PyObject *self, PyObject *noargs)
 {
-	int master_fd, pid;
+	int master_fd = -1, pid;
 
 	pid = forkpty(&master_fd, NULL, NULL, NULL);
 	if (pid == -1)
