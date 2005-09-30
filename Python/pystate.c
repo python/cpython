963dbdd74a921da6ab61c482fea57b5de07a2a9b
Michael W. Hudson <mwh@python.net>
2005-09-30 08:17:40 +0000

    Fix bug:
    [ 1307978 ] Unsatisfied symbols: _PyGILState_NoteThreadState (code)
    
    (note that this only happens in a threads-disabled build).
    
    Will forward port momentarily.

--- Python/pystate.c
+++ Python/pystate.c
@@ -53,7 +53,9 @@ static PyInterpreterState *interp_head = NULL;
 PyThreadState *_PyThreadState_Current = NULL;
 PyThreadFrameGetter _PyThreadState_GetFrame = NULL;
 
+#ifdef WITH_THREAD
 static void _PyGILState_NoteThreadState(PyThreadState* tstate);
+#endif
 
 
 PyInterpreterState *
@@ -188,7 +190,9 @@ PyThreadState_New(PyInterpreterState *interp)
 		tstate->c_profileobj = NULL;
 		tstate->c_traceobj = NULL;
 
+#ifdef WITH_THREAD
 		_PyGILState_NoteThreadState(tstate);
+#endif
 
 		HEAD_LOCK();
 		tstate->next = interp->tstate_head;
