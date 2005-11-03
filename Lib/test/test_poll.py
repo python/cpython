6e4524f633aa139c005d0d9c01858319ac96d851
Neal Norwitz <nnorwitz@gmail.com>
2005-11-03 05:11:17 +0000

    Backport (with output/test_poll):
    Bug #1346533, select.poll() doesn't raise an error if timeout > sys.maxint
    Need to check return result of PyInt_AsLong()

--- Lib/test/test_poll.py
+++ Lib/test/test_poll.py
@@ -168,5 +168,25 @@ def test_poll2():
     p.close()
     print 'Poll test 2 complete'
 
+def test_poll3():
+    # test int overflow
+    print 'Running poll test 3'
+    pollster = select.poll()
+    pollster.register(1)
+
+    try:
+        pollster.poll(1L << 64)
+    except OverflowError:
+        pass
+    else:
+        print 'Expected OverflowError with excessive timeout'
+
+    x = 2 + 3
+    if x != 5:
+        print 'Overflow must have occurred'
+    print 'Poll test 3 complete'
+    
+
 test_poll1()
 test_poll2()
+test_poll3()
