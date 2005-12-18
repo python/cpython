a3bdc2c2a5dbb6fd7f7f655f6970f7559dca0497
Barry Warsaw <barry@python.org>
2005-12-18 22:46:35 +0000

    Handle a couple of use cases discussed in python-dev w.r.t. calculating the Subversion revision number.
    First, in an svn export, there will be no .svn directory, so use an in-file
    $Revision$ keyword string with the keyword chrome stripped off.
    
    Also, use $(srcdir) in the Makefile.pre.in to handle the case where Python is
    build outside the source tree.

--- Modules/getbuildinfo.c
+++ Modules/getbuildinfo.c
@@ -21,20 +21,38 @@
 #endif
 
 #ifndef BUILD
-#define BUILD "0"
+#define BUILD "$Revision$"
 #endif
+
+const char *
+Py_GetBuildNumber(void)
+{
+	static char buildno[20];
+	static int buildno_okay;
+
+	if (!buildno_okay) {
+		char *build = BUILD;
+		int len = strlen(build);
+
+		if (len > 13 &&
+		    !strncmp(build, "$Revision: ", 11) &&
+		    !strcmp(build + len - 2, " $"))
+		{
+			memcpy(buildno, build + 11, len - 13);
+		}
+		else {
+			memcpy(buildno, build, 19);
+		}
+		buildno_okay = 1;
+	}
+	return buildno;
+}
 
 const char *
 Py_GetBuildInfo(void)
 {
 	static char buildinfo[50];
 	PyOS_snprintf(buildinfo, sizeof(buildinfo),
-		      "%s, %.20s, %.9s", BUILD, DATE, TIME);
+		      "#%s, %.20s, %.9s", Py_GetBuildNumber(), DATE, TIME);
 	return buildinfo;
-}
-
-const char *
-Py_GetBuildNumber(void)
-{
-	return BUILD;
 }
