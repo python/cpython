a91451a179ad47b4fd0e779a49331470ee977103
Martin v. Löwis <martin@v.loewis.de>
2006-01-03 06:29:53 +0000

    Ignore .svn instead of CVS now.

--- Tools/msi/msi.py
+++ Tools/msi/msi.py
@@ -907,7 +907,7 @@ def add_files(db):
     pydirs = [(root,"Lib")]
     while pydirs:
         parent, dir = pydirs.pop()
-        if dir == "CVS" or dir.startswith("plat-"):
+        if dir == ".svn" or dir.startswith("plat-"):
             continue
         elif dir in ["lib-tk", "idlelib", "Icons"]:
             if not have_tcl:
@@ -957,9 +957,9 @@ def add_files(db):
             lib.add_file("wininst-6.exe")
             lib.add_file("wininst-7.1.exe")
         if dir=="data" and parent.physical=="test" and parent.basedir.physical=="email":
-            # This should contain all non-CVS files listed in CVS
+            # This should contain all non-.svn files listed in subversion
             for f in os.listdir(lib.absolute):
-                if f.endswith(".txt") or f=="CVS":continue
+                if f.endswith(".txt") or f==".svn":continue
                 if f.endswith(".au") or f.endswith(".gif"):
                     lib.add_file(f)
                 else:
