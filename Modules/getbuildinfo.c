456af5f78b5b63da2ffb1995adb991445f8212e9
Martin v. Löwis <martin@v.loewis.de>
2006-01-06 00:43:42 +0000

    Deal with empty svn_revisions.

--- Modules/getbuildinfo.c
+++ Modules/getbuildinfo.c
@@ -25,7 +25,7 @@ Py_GetBuildInfo(void)
 {
 	static char buildinfo[50];
 	char *revision = Py_SubversionRevision();
-	char *sep = revision ? ":" : "";
+	char *sep = *revision ? ":" : "";
 	char *branch = Py_SubversionShortBranch();
 	PyOS_snprintf(buildinfo, sizeof(buildinfo),
 		      "%s%s%s, %.20s, %.9s", branch, sep, revision, 
