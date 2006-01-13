4edd989eaf5bae40f9e11424b36b22892c5ba459
Georg Brandl <georg@python.org>
2006-01-13 16:59:46 +0000

    Bug #1403410: The warnings module now doesn't get confused when it can't find out the module name it generates a warning for.

--- Lib/warnings.py
+++ Lib/warnings.py
@@ -63,7 +63,7 @@ def warn(message, category=None, stacklevel=1):
 def warn_explicit(message, category, filename, lineno,
                   module=None, registry=None):
     if module is None:
-        module = filename
+        module = filename or "<unknown>"
         if module[-3:].lower() == ".py":
             module = module[:-3] # XXX What about leading pathname?
     if registry is None:
