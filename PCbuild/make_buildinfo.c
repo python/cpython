62ffc078c2a393c0563dada1a83016ad4b0a06e0
Martin v. Löwis <martin@v.loewis.de>
2006-01-19 16:17:31 +0000

    Pass unquoted string to stat.

--- PCbuild/make_buildinfo.c
+++ PCbuild/make_buildinfo.c
@@ -37,11 +37,11 @@ int make_buildinfo2()
 	    type != REG_SZ)
 		/* Registry corrupted */
 		return 0;
-	strcat(command, "bin\\subwcrev.exe\"");
-	if (_stat(command, &st) < 0)
+	strcat(command, "bin\\subwcrev.exe");
+	if (_stat(command+1, &st) < 0)
 		/* subwcrev.exe not part of the release */
 		return 0;
-	strcat(command, " .. ..\\Modules\\getbuildinfo.c getbuildinfo2.c");
+	strcat(command, "\" .. ..\\Modules\\getbuildinfo.c getbuildinfo2.c");
 	puts(command); fflush(stdout);
 	if (system(command) < 0)
 		return 0;
