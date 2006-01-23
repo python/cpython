8797578a06aa65e3f2b3b0edb65870abcd45f20f
Georg Brandl <georg@python.org>
2006-01-23 21:31:00 +0000

    Add two missing markup tags.

--- Tools/scripts/texcheck.py
+++ Tools/scripts/texcheck.py
@@ -57,7 +57,7 @@ cmdstr = r"""
     \email \kwindex \refexmodindex \filenq \e \menuselection
     \exindex \linev \newsgroup \verbatim \setshortversion
     \author \authoraddress \paragraph \subparagraph \cmemberline
-    \textbar
+    \textbar \C \seelink
 """
 
 def matchclose(c_lineno, c_symbol, openers, pairmap):
