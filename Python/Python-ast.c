c27b579d4b198be3fd7d5fe441bd4d90bee3a8ec
Martin v. Löwis <martin@v.loewis.de>
2006-02-28 00:32:31 +0000

    Regenerate.

--- Python/Python-ast.c
+++ Python/Python-ast.c
@@ -2948,6 +2948,8 @@ init_ast(void)
         if (PyDict_SetItemString(d, "AST", (PyObject*)AST_type) < 0) return;
         if (PyModule_AddIntConstant(m, "PyCF_ONLY_AST", PyCF_ONLY_AST) < 0)
                 return;
+        if (PyModule_AddStringConstant(m, "__version__", "42635") < 0)
+                return;
         if(PyDict_SetItemString(d, "mod", (PyObject*)mod_type) < 0) return;
         if(PyDict_SetItemString(d, "Module", (PyObject*)Module_type) < 0)
            return;
