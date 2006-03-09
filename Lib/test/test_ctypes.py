e317d0e30724eb61ce66e268d6dca9a37f033f20
Thomas Heller <theller@ctypes.org>
2006-03-09 07:21:33 +0000

    Replace the trivial ctypes test (did only an import) with the real test suite.

--- Lib/test/test_ctypes.py
+++ Lib/test/test_ctypes.py
@@ -1,4 +1,12 @@
-# trivial test
+import unittest
 
-import _ctypes
-import ctypes
+from test.test_support import run_suite
+import ctypes.test
+
+def test_main():
+    skipped, testcases = ctypes.test.get_tests(ctypes.test, "test_*.py", verbosity=0)
+    suites = [unittest.makeSuite(t) for t in testcases]
+    run_suite(unittest.TestSuite(suites))
+
+if __name__ == "__main__":
+    test_main()
