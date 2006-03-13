53357874858da67fb24a9beafe06afaf6b6bd0e5
Thomas Heller <theller@ctypes.org>
2006-03-13 10:47:02 +0000

    Plug some refcount leaks when tests are run repeatedly.

--- Lib/ctypes/test/test_repr.py
+++ Lib/ctypes/test/test_repr.py
@@ -1,23 +1,28 @@
 from ctypes import *
 import unittest
 
-nums = [c_byte, c_short, c_int, c_long, c_longlong,
+subclasses = []
+for base in [c_byte, c_short, c_int, c_long, c_longlong,
         c_ubyte, c_ushort, c_uint, c_ulong, c_ulonglong,
-        c_float, c_double]
+        c_float, c_double]:
+    class X(base):
+        pass
+    subclasses.append(X)
+
+class X(c_char):
+    pass
+
+# This test checks if the __repr__ is correct for subclasses of simple types
 
 class ReprTest(unittest.TestCase):
     def test_numbers(self):
-        for typ in nums:
-            self.failUnless(repr(typ(42)).startswith(typ.__name__))
-            class X(typ):
-                pass
-            self.failUnlessEqual("<X object at", repr(X(42))[:12])
+        for typ in subclasses:
+            base = typ.__bases__[0]
+            self.failUnless(repr(base(42)).startswith(base.__name__))
+            self.failUnlessEqual("<X object at", repr(typ(42))[:12])
 
     def test_char(self):
         self.failUnlessEqual("c_char('x')", repr(c_char('x')))
-
-        class X(c_char):
-            pass
         self.failUnlessEqual("<X object at", repr(X('x'))[:12])
 
 if __name__ == "__main__":
