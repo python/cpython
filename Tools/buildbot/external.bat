35e859bdd5256d14eb2db98a462171fdcdec6a57
Martin v. Löwis <martin@v.loewis.de>
2006-03-13 13:48:05 +0000

    Update to bsddb 4.4.20.

--- Tools/buildbot/external.bat
+++ Tools/buildbot/external.bat
@@ -6,3 +6,8 @@ cd ..
 @rem bzip
 if not exist bzip2-1.0.3 svn export http://svn.python.org/projects/external/bzip2-1.0.3
 
+@rem Sleepycat db
+if not exist db-4.4.20 svn export http://svn.python.org/projects/external/db-4.4.20
+if not exist db-4.4.20\build_win32\debug\libdb44sd.lib (
+   devenv db-4.4.20\build_win32\Berkeley_DB.sln /build Debug /project db_static
+)
