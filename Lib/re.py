1e0c16250e45ecc8b6c2c1dfe944772a790b3553
Neal Norwitz <nnorwitz@gmail.com>
2006-03-16 06:27:37 +0000

    Remove re.py, in order to rename sre.py -> re.py (svn seems to require 2 steps).

--- Lib/re.py
+++
@@ -1,6 +0,0 @@
-"""Minimal "re" compatibility wrapper.  See "sre" for documentation."""
-
-engine = "sre" # Some apps might use this undocumented variable
-
-from sre import *
-from sre import __all__
