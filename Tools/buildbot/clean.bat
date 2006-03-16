03de6fd89f5cc50df7c310e7f2fcd564286f23f2
Tim Peters <tim.peters@gmail.com>
2006-03-16 02:33:55 +0000

    Merge rev 43063 from trunk.
    Oops!  Use python_d.exe _before_ it's destroyed :-)

--- Tools/buildbot/clean.bat
+++ Tools/buildbot/clean.bat
@@ -1,6 +1,6 @@
 @rem Used by the buildbot "clean" step.
 call "%VS71COMNTOOLS%vsvars32.bat"
 cd PCbuild
-devenv.com /clean Debug pcbuild.sln
 @echo Deleting .pyc/.pyo files ...
 python_d.exe rmpyc.py
+devenv.com /clean Debug pcbuild.sln
