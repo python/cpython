ea9f73e22ea74ab5f04491e4c1e08dfdcd644c6e
Neal Norwitz <nnorwitz@gmail.com>
2006-03-17 07:15:59 +0000

    Oops, copied the wrong code from keeprefs.  Get the right code this time and call gc.collect(), since there is some garbage.
    The original code didn't really leak (if gc.collect() was called).

--- Lib/test/leakers/test_ctypes.py
+++ Lib/test/leakers/test_ctypes.py
@@ -1,11 +1,16 @@
 
-# Taken from Lib/ctypes/test/test_keeprefs.py
+# Taken from Lib/ctypes/test/test_keeprefs.py, PointerToStructure.test().
 # When this leak is fixed, remember to remove from Misc/build.sh LEAKY_TESTS.
 
-from ctypes import Structure, c_int
+from ctypes import Structure, c_int, POINTER
+import gc
 
-def leak():
+def leak_inner():
     class POINT(Structure):
         _fields_ = [("x", c_int)]
     class RECT(Structure):
-        _fields_ = [("ul", POINT)]
+        _fields_ = [("a", POINTER(POINT))]
+
+def leak():
+    leak_inner()
+    gc.collect()
