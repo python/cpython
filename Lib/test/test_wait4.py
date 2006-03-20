05a45599d7c7e7584a80d7ef70dc47e984664619
Neal Norwitz <nnorwitz@gmail.com>
2006-03-20 06:30:08 +0000

    Patch #1309579: wait3 and wait4 were added to the posix module by Chad J. Schroeder.
    This was a fair amount of rework of the patch.  Refactored test_fork1 so it
    could be reused by the new tests for wait3/4.  Also made them into new style
    unittests (derive from unittest.TestCase).

--- Lib/test/test_wait4.py
+++ Lib/test/test_wait4.py
@@ -1,4 +1,4 @@
-"""This test checks for correct fork() behavior.
+"""This test checks for correct wait4() behavior.
 """
 
 import os
@@ -8,16 +8,22 @@ from test.test_support import TestSkipped, run_unittest
 try:
     os.fork
 except AttributeError:
-    raise TestSkipped, "os.fork not defined -- skipping test_fork1"
+    raise TestSkipped, "os.fork not defined -- skipping test_wait4"
 
-class ForkTest(ForkWait):
+try:
+    os.wait4
+except AttributeError:
+    raise TestSkipped, "os.wait4 not defined -- skipping test_wait4"
+
+class Wait4Test(ForkWait):
     def wait_impl(self, cpid):
-        spid, status = os.waitpid(cpid, 0)
+        spid, status, rusage = os.wait4(cpid, 0)
         self.assertEqual(spid, cpid)
         self.assertEqual(status, 0, "cause = %d, exit = %d" % (status&0xff, status>>8))
+        self.assertTrue(rusage)
 
 def test_main():
-    run_unittest(ForkTest)
+    run_unittest(Wait4Test)
 
 if __name__ == "__main__":
     test_main()
