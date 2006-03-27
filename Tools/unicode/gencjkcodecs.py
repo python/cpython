cff22083f1447d397394d663314b3e120ca1993e
Walter Dörwald <walter@livinglogic.de>
2006-03-27 15:11:56 +0000

    Whitespace for generated code.

--- Tools/unicode/gencjkcodecs.py
+++ Tools/unicode/gencjkcodecs.py
@@ -31,11 +31,14 @@ class Codec(codecs.Codec):
 class IncrementalEncoder(mbc.MultibyteIncrementalEncoder,
                          codecs.IncrementalEncoder):
     codec = codec
+
 class IncrementalDecoder(mbc.MultibyteIncrementalDecoder,
                          codecs.IncrementalDecoder):
     codec = codec
+
 class StreamReader(Codec, mbc.MultibyteStreamReader, codecs.StreamReader):
     codec = codec
+
 class StreamWriter(Codec, mbc.MultibyteStreamWriter, codecs.StreamWriter):
     codec = codec
 
