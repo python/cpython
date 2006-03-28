2e177b9a10182a064d31f5448bbc86ea099cdc68
Georg Brandl <georg@python.org>
2006-03-28 10:29:45 +0000

    Make uu use floor division instead of classic division. This was discovered by test_email failing with -Qnew.

--- Lib/uu.py
+++ Lib/uu.py
@@ -132,7 +132,7 @@ def decode(in_file, out_file=None, mode=None, quiet=0):
             data = binascii.a2b_uu(s)
         except binascii.Error, v:
             # Workaround for broken uuencoders by /Fredrik Lundh
-            nbytes = (((ord(s[0])-32) & 63) * 4 + 5) / 3
+            nbytes = (((ord(s[0])-32) & 63) * 4 + 5) // 3
             data = binascii.a2b_uu(s[:nbytes])
             if not quiet:
                 sys.stderr.write("Warning: %s\n" % v)
