0c469854bcb6f7180d261bc1f6adb67f27656419
Neal Norwitz <nnorwitz@gmail.com>
2006-04-11 07:21:20 +0000

    Adjust whitespace.

--- Lib/site.py
+++ Lib/site.py
@@ -69,7 +69,7 @@ def makepath(*paths):
 def abs__file__():
     """Set all module' __file__ attribute to an absolute path"""
     for m in sys.modules.values():
-        if hasattr(m,'__loader__'):
+        if hasattr(m, '__loader__'):
             continue   # don't mess with a PEP 302-supplied __file__
         try:
             m.__file__ = os.path.abspath(m.__file__)
