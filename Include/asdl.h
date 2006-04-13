b9406711864885c8434c9ba41da90f7ea1e18a47
Skip Montanaro <skip@pobox.com>
2006-04-13 09:37:01 +0000

    Use union to discriminate pointer types from enum/int types.

--- Include/asdl.h
+++ Include/asdl.h
@@ -19,18 +19,22 @@ typedef enum {false, true} bool;
 
 typedef struct {
     int size;
-    void *elements[1];
+    union {
+        void *elements[1];
+	unsigned int enum_type[1];
+    } elt;
 } asdl_seq;
 
 asdl_seq *asdl_seq_new(int size, PyArena *arena);
 
-#define asdl_seq_GET(S, I) (S)->elements[(I)]
+#define asdl_seq_GET(S, I) (S)->elt.elements[(I)]
+#define asdl_seq_GET_ENUM(S, I) (S)->elt.enum_type[(I)]
 #define asdl_seq_LEN(S) ((S) == NULL ? 0 : (S)->size)
 #ifdef Py_DEBUG
 #define asdl_seq_SET(S, I, V) { \
         int _asdl_i = (I); \
         assert((S) && _asdl_i < (S)->size); \
-        (S)->elements[_asdl_i] = (V); \
+        (S)->elt.elements[_asdl_i] = (V); \
 }
 #else
 #define asdl_seq_SET(S, I, V) (S)->elements[I] = (V)
