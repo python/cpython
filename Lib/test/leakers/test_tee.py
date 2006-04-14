c6f7b9b85b4ff9314cd86d2fe37e28a0aa3426f8
Tim Peters <tim.peters@gmail.com>
2006-04-14 18:34:14 +0000

    Whitespace normalization.

--- Lib/test/leakers/test_tee.py
+++ Lib/test/leakers/test_tee.py
@@ -6,20 +6,20 @@ from itertools import tee
 import gc
 
 def leak():
-  def inner():
-    def fib():
-        def yield_identity_forever(g):
-            while 1:
-                yield g
-        def _fib():
-            for i in yield_identity_forever(head):
-                yield i
-        head, tail, result = tee(_fib(), 3)
-        return result
+    def inner():
+        def fib():
+            def yield_identity_forever(g):
+                while 1:
+                    yield g
+            def _fib():
+                for i in yield_identity_forever(head):
+                    yield i
+            head, tail, result = tee(_fib(), 3)
+            return result
 
-    x = fib()
-    x.next()
-  inner()
-  gc.collect() ; gc.collect()
-  # this is expected to return 0
-  return gc.collect()
+        x = fib()
+        x.next()
+    inner()
+    gc.collect() ; gc.collect()
+    # this is expected to return 0
+    return gc.collect()
