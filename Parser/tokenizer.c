c72282b8fc6b88f6c0ec97fe9b52857b47176ff9
Skip Montanaro <skip@pobox.com>
2006-04-18 00:53:06 +0000

    C++ compiler cleanup: cast signed to unsigned

--- Parser/tokenizer.c
+++ Parser/tokenizer.c
@@ -1230,7 +1230,7 @@ tok_get(register struct tok_state *tok, char **p_start, char **p_end)
 		do {
 			*tp++ = c = tok_nextc(tok);
 		} while (c != EOF && c != '\n' &&
-			 tp - cbuf + 1 < sizeof(cbuf));
+			 (unsigned int)(tp - cbuf + 1) < sizeof(cbuf));
 		*tp = '\0';
 		for (cp = tabforms;
 		     cp < tabforms + sizeof(tabforms)/sizeof(tabforms[0]);
