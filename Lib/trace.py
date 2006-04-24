43d69aa7d973635efdd9df6780180d39a4c1ba2b
Tim Peters <tim.peters@gmail.com>
2006-04-24 22:45:13 +0000

    Whitespace normalization.

--- Lib/trace.py
+++ Lib/trace.py
@@ -40,7 +40,7 @@ Sample use, programmatically
   # create a Trace object, telling it what to ignore, and whether to
   # do tracing or line-counting or both.
   tracer = trace.Trace(ignoredirs=[sys.prefix, sys.exec_prefix,], trace=0,
-  		    count=1)
+                    count=1)
   # run the new command using the given tracer
   tracer.run('main()')
   # make a report, placing output in /tmp
