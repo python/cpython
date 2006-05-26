56089a296a756b98d01dd5fe9674837dab5bb3ee
Fredrik Lundh <fredrik@pythonware.com>
2006-05-26 17:26:39 +0000

    needforspeed: cleanup

--- Objects/stringobject.c
+++ Objects/stringobject.c
@@ -1544,8 +1544,8 @@ found, returns S and two empty strings.");
 static PyObject *
 string_partition(PyStringObject *self, PyObject *sep_obj)
 {
-	Py_ssize_t str_len = PyString_GET_SIZE(self), sep_len;
-	const char *str = PyString_AS_STRING(self), *sep;
+	const char *sep;
+	Py_ssize_t sep_len;
 
 	if (PyString_Check(sep_obj)) {
 		sep = PyString_AS_STRING(sep_obj);
@@ -1553,12 +1553,16 @@ string_partition(PyStringObject *self, PyObject *sep_obj)
 	}
 #ifdef Py_USING_UNICODE
 	else if (PyUnicode_Check(sep_obj))
-		return PyUnicode_Partition((PyObject *)self, sep_obj);
+		return PyUnicode_Partition((PyObject *) self, sep_obj);
 #endif
 	else if (PyObject_AsCharBuffer(sep_obj, &sep, &sep_len))
 		return NULL;
 
-	return partition((PyObject*)self, str, str_len, sep_obj, sep, sep_len);
+	return partition(
+		(PyObject*) self,
+		PyString_AS_STRING(self), PyString_GET_SIZE(self),
+		sep_obj, sep, sep_len
+		);
 }
 
 Py_LOCAL(PyObject *)
