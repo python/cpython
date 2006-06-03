02d6a04df7ec08453e1e1db70fbd37a1ecaaf690
Andrew M. Kuchling <amk@amk.ca>
2006-06-03 23:59:36 +0000

    Add more whitespace; use a better socket name

--- Demo/sockets/unixclient.py
+++ Demo/sockets/unixclient.py
@@ -1,7 +1,9 @@
 # Echo client demo using Unix sockets
 # Piet van Oostrum
+
 from socket import *
-FILE = 'blabla'
+
+FILE = 'unix-socket'
 s = socket(AF_UNIX, SOCK_STREAM)
 s.connect(FILE)
 s.send('Hello, world')
