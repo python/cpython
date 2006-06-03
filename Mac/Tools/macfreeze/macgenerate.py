da40d85c3fafed07846e8e9a11a2937c3664006f
Neal Norwitz <nnorwitz@gmail.com>
2006-06-03 04:49:00 +0000

    Remove Mac OS 9 support (http://mail.python.org/pipermail/python-dev/2006-June/065538.html)

--- Mac/Tools/macfreeze/macgenerate.py
+++
@@ -1,8 +0,0 @@
-"""macgenerate - Generate the out for macfreeze"""
-
-def generate(program, module_dict):
-    for name in module_dict.keys():
-        print 'Include %-20s\t'%name,
-        module = module_dict[name]
-        print module.gettype(), '\t', repr(module)
-    return 0
