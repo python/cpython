5bece007ac926b92f2e23170472b37de6ea455b3
Martin v. Löwis <martin@v.loewis.de>
2006-06-15 06:45:05 +0000

    Make import/lookup of mbcs fail on non-Windows systems.

--- Lib/encodings/mbcs.py
+++ Lib/encodings/mbcs.py
@@ -7,6 +7,10 @@ which was written by Marc-Andre Lemburg (mal@lemburg.com).
 (c) Copyright CNRI, All Rights Reserved. NO WARRANTY.
 
 """
+# Import them explicitly to cause an ImportError
+# on non-Windows systems
+from codecs import mbcs_encode, mbcs_decode
+# for IncrementalDecoder, IncrementalEncoder, ...
 import codecs
 
 ### Codec APIs
@@ -15,16 +19,16 @@ class Codec(codecs.Codec):
 
     # Note: Binding these as C functions will result in the class not
     # converting them to methods. This is intended.
-    encode = codecs.mbcs_encode
-    decode = codecs.mbcs_decode
+    encode = mbcs_encode
+    decode = mbcs_decode
 
 class IncrementalEncoder(codecs.IncrementalEncoder):
     def encode(self, input, final=False):
-        return codecs.mbcs_encode(input,self.errors)[0]
+        return mbcs_encode(input,self.errors)[0]
 
 class IncrementalDecoder(codecs.BufferedIncrementalDecoder):
     def _buffer_decode(self, input, errors, final):
-        return codecs.mbcs_decode(input,self.errors,final)
+        return mbcs_decode(input,self.errors,final)
 
 class StreamWriter(Codec,codecs.StreamWriter):
     pass
