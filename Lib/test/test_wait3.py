de506cb69c2ea8a479ea680f33a9a93215f21ea1
Neal Norwitz <nnorwitz@gmail.com>
2006-06-18 19:37:40 +0000

    The hppa ubuntu box sometimes hangs forever in these tests.  My guess is that the wait is failing for some reason.  Use WNOHANG, so we won't wait until the buildbot kills the test suite.
    I haven't been able to reproduce the failure, so I'm not sure if
    this will help or not.  Hopefully, this change will cause the test
    to fail, rather than hang.  That will be better since we will get
    the rest of the test results.  It may also help us debug the real problem.

--- Lib/test/test_wait3.py
+++ Lib/test/test_wait3.py
@@ -2,6 +2,7 @@
 """
 
 import os
+import time
 from test.fork_wait import ForkWait
 from test.test_support import TestSkipped, run_unittest
 
@@ -17,10 +18,14 @@ except AttributeError:
 
 class Wait3Test(ForkWait):
     def wait_impl(self, cpid):
-        while 1:
-            spid, status, rusage = os.wait3(0)
+        for i in range(10):
+            # wait3() shouldn't hang, but some of the buildbots seem to hang
+            # in the forking tests.  This is an attempt to fix the problem.
+            spid, status, rusage = os.wait3(os.WNOHANG)
             if spid == cpid:
                 break
+            time.sleep(1.0)
+
         self.assertEqual(spid, cpid)
         self.assertEqual(status, 0, "cause = %d, exit = %d" % (status&0xff, status>>8))
         self.assertTrue(rusage)
