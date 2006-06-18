d1a3977965805250d649ca0bce4b6bc2da132672
Neal Norwitz <nnorwitz@gmail.com>
2006-06-18 20:10:24 +0000

    Revert 47014 until it is more robust

--- Lib/test/test_wait3.py
+++ Lib/test/test_wait3.py
@@ -2,7 +2,6 @@
 """
 
 import os
-import time
 from test.fork_wait import ForkWait
 from test.test_support import TestSkipped, run_unittest
 
@@ -18,14 +17,10 @@ except AttributeError:
 
 class Wait3Test(ForkWait):
     def wait_impl(self, cpid):
-        for i in range(10):
-            # wait3() shouldn't hang, but some of the buildbots seem to hang
-            # in the forking tests.  This is an attempt to fix the problem.
-            spid, status, rusage = os.wait3(os.WNOHANG)
+        while 1:
+            spid, status, rusage = os.wait3(0)
             if spid == cpid:
                 break
-            time.sleep(1.0)
-
         self.assertEqual(spid, cpid)
         self.assertEqual(status, 0, "cause = %d, exit = %d" % (status&0xff, status>>8))
         self.assertTrue(rusage)
