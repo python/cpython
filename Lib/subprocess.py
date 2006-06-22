ff355f1adaa423c143c169221bd4a80775adf8bb
Peter Astrand <astrand@lysator.liu.se>
2006-06-22 20:21:26 +0000

    Applied patch #1506758: Prevent MemoryErrors with large MAXFD.

--- Lib/subprocess.py
+++ Lib/subprocess.py
@@ -941,7 +941,7 @@ class Popen(object):
 
 
         def _close_fds(self, but):
-            for i in range(3, MAXFD):
+            for i in xrange(3, MAXFD):
                 if i == but:
                     continue
                 try:
