04e38d94bf65b12c0fb4e5638b36140f81561233
Martin v. Löwis <martin@v.loewis.de>
2006-07-03 13:47:29 +0000

    Put method-wrappers into trashcan. Fixes #927248.

--- Lib/test/test_descr.py
+++ Lib/test/test_descr.py
@@ -3936,6 +3936,13 @@ def weakref_segfault():
     o.whatever = Provoker(o)
     del o
 
+def wrapper_segfault():
+    # SF 927248: deeply nested wrappers could cause stack overflow
+    f = lambda:None
+    for i in xrange(1000000):
+        f = f.__call__
+    f = None
+
 # Fix SF #762455, segfault when sys.stdout is changed in getattr
 def filefault():
     if verbose:
@@ -4075,6 +4082,7 @@ def notimplemented():
 
 def test_main():
     weakref_segfault() # Must be first, somehow
+    wrapper_segfault()
     do_this_first()
     class_docstrings()
     lists()
