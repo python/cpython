b62efad943443c89d8bd7d33715393984b04c0e0
Armin Rigo <arigo@tunes.org>
2006-07-25 18:38:39 +0000

    Document the crashers that will not go away soon as "won't fix", and explain why.

--- Lib/test/crashers/recursive_call.py
+++ Lib/test/crashers/recursive_call.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
 
 # No bug report AFAIK, mail on python-dev on 2006-01-10
+
+# This is a "won't fix" case.  It is known that setting a high enough
+# recursion limit crashes by overflowing the stack.  Unless this is
+# redesigned somehow, it won't go away.
+
 import sys
 
 sys.setrecursionlimit(1 << 30)
