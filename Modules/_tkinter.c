5f17d9a1dfe6db0d3e5069ff42b4e4fbfbc2aa2f
Neal Norwitz <nnorwitz@gmail.com>
2006-08-12 02:33:36 +0000

    Don't deref v if it's NULL.
    Klocwork #214

--- Modules/_tkinter.c
+++ Modules/_tkinter.c
@@ -2493,8 +2493,10 @@ Tkapp_CreateTimerHandler(PyObject *self, PyObject *args)
 	}
 
 	v = Tktt_New(func);
-	v->token = Tcl_CreateTimerHandler(milliseconds, TimerHandler,
-					  (ClientData)v);
+	if (v) {
+		v->token = Tcl_CreateTimerHandler(milliseconds, TimerHandler,
+						  (ClientData)v);
+	}
 
 	return (PyObject *) v;
 }
