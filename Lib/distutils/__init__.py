604c864f0f94d371dbd55f5676f38ca53f528f5a
Marc-André Lemburg <mal@egenix.com>
2006-08-16 08:13:26 +0000

    Revert to having static version numbers again.

--- Lib/distutils/__init__.py
+++ Lib/distutils/__init__.py
@@ -12,6 +12,12 @@ used from a setup script as
 
 __revision__ = "$Id$"
 
-import sys
-__version__ = "%d.%d.%d" % sys.version_info[:3]
-del sys
+# Distutils version
+#
+# Please coordinate with Marc-Andre Lemburg <mal@egenix.com> when adding
+# new features to distutils that would warrant bumping the version number.
+#
+# In general, major and minor version should loosely follow the Python
+# version number the distutils code was shipped with.
+#
+__version__ = "2.5.0"
