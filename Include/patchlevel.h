798ed8f0764379d285779a4966a1fee6db61e293
Anthony Baxter <anthonybaxter@gmail.com>
2006-08-16 03:58:37 +0000

    preparing for 2.5c1 - no, really this time

--- Include/patchlevel.h
+++ Include/patchlevel.h
@@ -23,7 +23,7 @@
 #define PY_MINOR_VERSION	5
 #define PY_MICRO_VERSION	0
 #define PY_RELEASE_LEVEL	PY_RELEASE_LEVEL_GAMMA
-#define PY_RELEASE_SERIAL	3
+#define PY_RELEASE_SERIAL	1
 
 /* Version as a string */
 #define PY_VERSION		"2.5c1"
