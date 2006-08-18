dac21f9dbcae7e49990b21df3301ad469d257c8e
Neal Norwitz <nnorwitz@gmail.com>
2006-08-18 04:58:33 +0000

    Try to get Windows bots working again

--- PC/pyconfig.h
+++ PC/pyconfig.h
@@ -280,9 +280,9 @@ Py_NO_ENABLE_SHARED to find out.  Also support MS_NO_COREDLL for b/w compat */
 			their Makefile (other compilers are generally
 			taken care of by distutils.) */
 #			ifdef _DEBUG
-#				pragma comment(lib,"python25_d.lib")
+#				pragma comment(lib,"python26_d.lib")
 #			else
-#				pragma comment(lib,"python25.lib")
+#				pragma comment(lib,"python26.lib")
 #			endif /* _DEBUG */
 #		endif /* _MSC_VER */
 #	endif /* Py_BUILD_CORE */
