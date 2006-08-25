8ea269cbf02b2eeec5dd8b70cd9c92266a3602e7
Neal Norwitz <nnorwitz@gmail.com>
2006-08-25 01:52:49 +0000

    importing types is not necessary if we use isinstance

--- Lib/encodings/__init__.py
+++ Lib/encodings/__init__.py
@@ -28,7 +28,7 @@ Written by Marc-Andre Lemburg (mal@lemburg.com).
 
 """#"
 
-import codecs, types
+import codecs
 from encodings import aliases
 
 _cache = {}
@@ -60,7 +60,7 @@ def normalize_encoding(encoding):
     """
     # Make sure we have an 8-bit string, because .translate() works
     # differently for Unicode strings.
-    if type(encoding) is types.UnicodeType:
+    if isinstance(encoding, unicode):
         # Note that .encode('latin-1') does *not* use the codec
         # registry, so this call doesn't recurse. (See unicodeobject.c
         # PyUnicode_AsEncodedString() for details)
