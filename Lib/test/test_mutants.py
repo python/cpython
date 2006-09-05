f810eea426a82439102b222b0d4a10f2bd5fc8b5
Neal Norwitz <nnorwitz@gmail.com>
2006-09-05 02:24:03 +0000

    Fix SF bug #1546288, crash in dict_equal.

--- Lib/test/test_mutants.py
+++ Lib/test/test_mutants.py
@@ -91,11 +91,16 @@ class Horrid:
         self.hashcode = random.randrange(1000000000)
 
     def __hash__(self):
+        return 42
         return self.hashcode
 
     def __cmp__(self, other):
         maybe_mutate()   # The point of the test.
         return cmp(self.i, other.i)
+
+    def __eq__(self, other):
+        maybe_mutate()   # The point of the test.
+        return self.i == other.i
 
     def __repr__(self):
         return "Horrid(%d)" % self.i
@@ -132,7 +137,10 @@ def test_one(n):
     while dict1 and len(dict1) == len(dict2):
         if verbose:
             print ".",
-        c = cmp(dict1, dict2)
+        if random.random() < 0.5:
+            c = cmp(dict1, dict2)
+        else:
+            c = dict1 == dict2
     if verbose:
         print
 
