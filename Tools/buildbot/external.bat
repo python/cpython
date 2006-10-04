c3554cd9cf398d3c77e2709ff20691b36c31f8dc
Martin v. Löwis <martin@v.loewis.de>
2006-10-04 16:34:35 +0000

    Remove bogus openssl-0.9.8a directory.

--- Tools/buildbot/external.bat
+++ Tools/buildbot/external.bat
@@ -8,5 +8,6 @@ if not exist bzip2-1.0.3 svn export http://svn.python.org/projects/external/bzip
 
 @rem OpenSSL
 if exist openssl-0.9.7d rd /s/q openssl-0.9.7d
+if exist openssl-0.9.8a rd /s/q openssl-0.9.8a
 if not exist openssl-0.9.7l svn export http://svn.python.org/projects/external/openssl-0.9.7l
 
