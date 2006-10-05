68e1e0372baab7d9734c219258cf84c82e28ab9d
Andrew M. Kuchling <amk@amk.ca>
2006-10-05 17:06:59 +0000

    [Backport r51255 | neal.norwitz]
    Really address the issue of where to place the assert for leftblock.
    (Followup of Klocwork 274)

--- Modules/collectionsmodule.c
+++ Modules/collectionsmodule.c
@@ -211,6 +211,7 @@ deque_popleft(dequeobject *deque, PyObject *unused)
 		PyErr_SetString(PyExc_IndexError, "pop from an empty deque");
 		return NULL;
 	}
+	assert(deque->leftblock != NULL);
 	item = deque->leftblock->data[deque->leftindex];
 	deque->leftindex++;
 	deque->len--;
