99887e143038b0d0ebd249e9379d165bf6c1806e
Tim Peters <tim.peters@gmail.com>
2006-10-09 23:18:44 +0000

    Move fetching of encoding test files from the end of the Windows builbot's "build" step to the start of its "test" step.
    This is poke-and-hope.  The hope is that compilation failures
    on Windows will become visible to the buildbot (bsddb has
    apparently been failing to compile in 2.4 on Windows "for
    some time" now, but the buildbots haven't noticed that).

--- Tools/buildbot/build.bat
+++ Tools/buildbot/build.bat
@@ -3,5 +3,3 @@ cmd /c Tools\buildbot\external.bat
 call "%VS71COMNTOOLS%vsvars32.bat"
 cd PCbuild
 devenv.com /useenv /build Debug pcbuild.sln
-@rem Fetch encoding test files.  Note that python_d needs to be built first.
-if not exist BIG5.TXT python_d.exe ..\Tools\buildbot\fetch_data_files.py
--- Tools/buildbot/test.bat
+++ Tools/buildbot/test.bat
@@ -1,3 +1,5 @@
 @rem Used by the buildbot "test" step.
 cd PCbuild
+@rem Fetch encoding test files.  Note that python_d needs to be built first.
+if not exist BIG5.TXT python_d.exe ..\Tools\buildbot\fetch_data_files.py
 call rt.bat -d -q -uall -rw
