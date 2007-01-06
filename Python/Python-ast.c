37ffcf7baee0e078eec9bee0efdc623429ad2310
Martin v. Löwis <martin@v.loewis.de>
2007-01-06 13:29:14 +0000

    Commit __version__ change.

--- Python/Python-ast.c
+++ Python/Python-ast.c
@@ -3126,7 +3126,7 @@ init_ast(void)
         if (PyDict_SetItemString(d, "AST", (PyObject*)AST_type) < 0) return;
         if (PyModule_AddIntConstant(m, "PyCF_ONLY_AST", PyCF_ONLY_AST) < 0)
                 return;
-        if (PyModule_AddStringConstant(m, "__version__", "52491") < 0)
+        if (PyModule_AddStringConstant(m, "__version__", "53170") < 0)
                 return;
         if (PyDict_SetItemString(d, "mod", (PyObject*)mod_type) < 0) return;
         if (PyDict_SetItemString(d, "Module", (PyObject*)Module_type) < 0)
