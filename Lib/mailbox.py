703e858f9e8065668a2178b5a7bd97336f19f292
Andrew M. Kuchling <amk@amk.ca>
2007-01-22 20:26:40 +0000

    Improve pattern used for mbox 'From' lines; add a simple test

--- Lib/mailbox.py
+++ Lib/mailbox.py
@@ -1995,8 +1995,10 @@ class UnixMailbox(_Mailbox):
     # necessary.  For convenience, we've added a PortableUnixMailbox class
     # which uses the more lenient _fromlinepattern regular expression.
 
-    _fromlinepattern = r"From \s*[^\s]+\s+\w\w\w\s+\w\w\w\s+\d?\d\s+" \
-                       r"\d?\d:\d\d(:\d\d)?(\s+[^\s]+)?\s+\d\d\d\d\s*$"
+    _fromlinepattern = (r"From \s*[^\s]+\s+\w\w\w\s+\w\w\w\s+\d?\d\s+"
+                        r"\d?\d:\d\d(:\d\d)?(\s+[^\s]+)?\s+\d\d\d\d\s*"
+                        r"[^\s]*\s*"
+                        "$")
     _regexp = None
 
     def _strict_isrealfromline(self, line):
