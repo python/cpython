0d8285faa673c5c49d4331ddca4f33517e0dbb9d
Lars Gustäbel <lars@gustaebel.de>
2007-02-06 18:38:13 +0000

    Patch #1652681: create nonexistent files in append mode and allow appending to empty files.

--- Lib/tarfile.py
+++ Lib/tarfile.py
@@ -1060,6 +1060,10 @@ class TarFile(object):
         self.mode = {"r": "rb", "a": "r+b", "w": "wb"}[mode]
 
         if not fileobj:
+            if self._mode == "a" and not os.path.exists(self.name):
+                # Create nonexistent files in append mode.
+                self._mode = "w"
+                self.mode = "wb"
             fileobj = file(self.name, self.mode)
             self._extfileobj = False
         else:
@@ -1093,7 +1097,8 @@ class TarFile(object):
                     self.fileobj.seek(0)
                     break
                 if tarinfo is None:
-                    self.fileobj.seek(- BLOCKSIZE, 1)
+                    if self.offset > 0:
+                        self.fileobj.seek(- BLOCKSIZE, 1)
                     break
 
         if self._mode in "aw":
@@ -1120,7 +1125,7 @@ class TarFile(object):
            'r:'         open for reading exclusively uncompressed
            'r:gz'       open for reading with gzip compression
            'r:bz2'      open for reading with bzip2 compression
-           'a' or 'a:'  open for appending
+           'a' or 'a:'  open for appending, creating the file if necessary
            'w' or 'w:'  open for writing without compression
            'w:gz'       open for writing with gzip compression
            'w:bz2'      open for writing with bzip2 compression
