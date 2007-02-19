2810e60235c39371b47315c28c8f0b9a9a6c334e
Raymond Hettinger <python@rcn.com>
2007-02-19 18:15:04 +0000

    Fixup docstrings for merge().

--- Lib/heapq.py
+++ Lib/heapq.py
@@ -312,8 +312,8 @@ def merge(*iterables):
     '''Merge multiple sorted inputs into a single sorted output.
 
     Similar to sorted(itertools.chain(*iterables)) but returns an iterable,
-    does not pull the data into memory all at once, and reduces the number
-    of comparisons by assuming that each of the input streams is already sorted.
+    does not pull the data into memory all at once, and assumes that each of
+    the input streams is already sorted (smallest to largest).
 
     >>> list(merge([1,3,5,7], [0,2,4,8], [5,10,15,20], [], [25]))
     [0, 1, 2, 3, 4, 5, 5, 7, 8, 10, 15, 20, 25]
