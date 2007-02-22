a7b055414000208a24e5250130f58d1786dcbd23
Brett Cannon <bcannon@gmail.com>
2007-02-22 04:45:13 +0000

    Fix test_iterlen by returning the iterator of dict views.  Problem is that iteritems and itervalues' previous object were both an iterator *and* and iterable.  The tests expected an iterator but were given an iterable.
    Should the 2to3 conversion for iter(values|items|keys) change the code to
    ``iter(dict.keys())`` to be more compatible?

--- Lib/test/test_iterlen.py
+++ Lib/test/test_iterlen.py
@@ -139,14 +139,14 @@ class TestDictItems(TestTemporarilyImmutable):
 
     def setUp(self):
         d = dict.fromkeys(xrange(n))
-        self.it = d.items()
+        self.it = iter(d.items())
         self.mutate = d.popitem
 
 class TestDictValues(TestTemporarilyImmutable):
 
     def setUp(self):
         d = dict.fromkeys(xrange(n))
-        self.it = d.values()
+        self.it = iter(d.values())
         self.mutate = d.popitem
 
 class TestSet(TestTemporarilyImmutable):
