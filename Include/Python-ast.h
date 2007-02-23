2a1aecf3bb98798116f5946133d0787303478c37
Thomas Wouters <thomas@python.org>
2007-02-23 20:11:29 +0000

    Prevent regeneration.

--- Include/Python-ast.h
+++ Include/Python-ast.h
@@ -516,3 +516,4 @@ keyword_ty _Py_keyword(identifier arg, expr_ty value, PyArena *arena);
 alias_ty _Py_alias(identifier name, identifier asname, PyArena *arena);
 
 PyObject* PyAST_mod2obj(mod_ty t);
+
