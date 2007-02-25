c150288e08b43a6937fd0771453af259c5295f0e
Jeremy Hylton <jeremy@alum.mit.edu>
2007-02-25 16:01:58 +0000

    Put declarations before code.

--- Python/ceval.c
+++ Python/ceval.c
@@ -4202,9 +4202,9 @@ exec_statement(PyFrameObject *f, PyObject *prog, PyObject *globals,
 	else if (PyFile_Check(prog)) {
 		FILE *fp = PyFile_AsFile(prog);
 		char *name = PyString_AsString(PyFile_Name(prog));
+		PyCompilerFlags cf;
                 if (name == NULL)
                         return -1;
-		PyCompilerFlags cf;
 		cf.cf_flags = 0;
 		if (PyEval_MergeCompilerFlags(&cf))
 			v = PyRun_FileFlags(fp, name, Py_file_input, globals,
