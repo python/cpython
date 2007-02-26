88516a60398fff92dbf83c6ad17b03233f6db680
Neal Norwitz <nnorwitz@gmail.com>
2007-02-26 22:41:45 +0000

    When printing an unraisable error, don't print exceptions. before the name. This duplicates the behavior whening normally printing exceptions.

--- Lib/test/test_generators.py
+++ Lib/test/test_generators.py
@@ -1681,7 +1681,7 @@ Our ill-behaved code should be invoked during GC:
 >>> g.next()
 >>> del g
 >>> sys.stderr.getvalue().startswith(
-...     "Exception exceptions.RuntimeError: 'generator ignored GeneratorExit' in "
+...     "Exception RuntimeError: 'generator ignored GeneratorExit' in "
 ... )
 True
 >>> sys.stderr = old
@@ -1798,7 +1798,7 @@ to test.
 ...     del l
 ...     err = sys.stderr.getvalue().strip()
 ...     err.startswith(
-...         "Exception exceptions.RuntimeError: RuntimeError() in <"
+...         "Exception RuntimeError: RuntimeError() in <"
 ...     )
 ...     err.endswith("> ignored")
 ...     len(err.splitlines())
