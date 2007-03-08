a30fcb4dae51705f182383a6ad5f3fb86a80f9fb
Brett Cannon <bcannon@gmail.com>
2007-03-08 23:58:11 +0000

    Introduce test.test_support.TransientResource.  It's a context manager to surround calls to resources that may or may not be available.  Specifying the expected exception and attributes to be raised if the resource is not available prevents overly broad catching of exceptions.
    This is meant to help suppress spurious failures by raising
    test.test_support.ResourceDenied if the exception matches.  It would probably
    be good to go through the various network tests and surround the calls to catch
    connection timeouts (as done with test_socket_ssl in this commit).

--- Lib/test/test_support.py
+++ Lib/test/test_support.py
@@ -312,6 +312,31 @@ class EnvironmentVarGuard(object):
         for unset in self._unset:
             del self._environ[unset]
 
+class TransientResource(object):
+
+    """Raise ResourceDenied if an exception is raised while the context manager
+    is in effect that matches the specified exception and attributes."""
+
+    def __init__(self, exc, **kwargs):
+        self.exc = exc
+        self.attrs = kwargs
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, type_=None, value=None, traceback=None):
+        """If type_ is a subclass of self.exc and value has attributes matching
+        self.attrs, raise ResourceDenied.  Otherwise let the exception
+        propagate (if any)."""
+        if type_ is not None and issubclass(self.exc, type_):
+            for attr, attr_value in self.attrs.iteritems():
+                if not hasattr(value, attr):
+                    break
+                if getattr(value, attr) != attr_value:
+                    break
+            else:
+                raise ResourceDenied("an optional resource is not available")
+
 
 #=======================================================================
 # Decorator for running a function in a different locale, correctly resetting
