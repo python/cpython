154bc7c9bdee408665afce867ea6cce8ec9ef6b6
Raymond Hettinger <python@rcn.com>
2007-03-08 19:23:25 +0000

    SF 1676321:  empty() returned wrong result

--- Lib/sched.py
+++ Lib/sched.py
@@ -72,7 +72,7 @@ class scheduler:
 
     def empty(self):
         """Check whether the queue is empty."""
-        return not not self.queue
+        return not self.queue
 
     def run(self):
         """Execute events until the queue is empty.
