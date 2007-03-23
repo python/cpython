a8ddae61496de68b3c70d8dab364a204f53c153b
Martin v. Löwis <martin@v.loewis.de>
2007-03-23 13:27:29 +0000

    Bug #978833: Revert r50844, as it broke _socketobject.dup.

--- Lib/httplib.py
+++ Lib/httplib.py
@@ -926,8 +926,8 @@ class HTTPConnection:
         self.__state = _CS_IDLE
 
         if response.will_close:
-            # Pass the socket to the response
-            self.sock = None
+            # this effectively passes the connection to the response
+            self.close()
         else:
             # remember this, so we can tell when it is complete
             self.__response = response
