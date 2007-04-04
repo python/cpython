10fe455529c00063a24d7a94e022d1f136a1c631
Collin Winter <collinw@gmail.com>
2007-04-04 18:36:30 +0000

    Make test_softspace use unittest.

--- Lib/test/test_softspace.py
+++ Lib/test/test_softspace.py
@@ -1,14 +1,23 @@
-from test import test_support
+from test.test_support import run_unittest
+import unittest
 import StringIO
 
-# SF bug 480215:  softspace confused in nested print
-f = StringIO.StringIO()
-class C:
-    def __str__(self):
-        print >> f, 'a'
-        return 'b'
+class SoftspaceTests(unittest.TestCase):
+    def test_bug_480215(self):
+        # SF bug 480215:  softspace confused in nested print
+        f = StringIO.StringIO()
+        class C:
+            def __str__(self):
+                print >> f, 'a'
+                return 'b'
 
-print >> f, C(), 'c ', 'd\t', 'e'
-print >> f, 'f', 'g'
-# In 2.2 & earlier, this printed ' a\nbc  d\te\nf g\n'
-test_support.vereq(f.getvalue(), 'a\nb c  d\te\nf g\n')
+        print >> f, C(), 'c ', 'd\t', 'e'
+        print >> f, 'f', 'g'
+        # In 2.2 & earlier, this printed ' a\nbc  d\te\nf g\n'
+        self.assertEqual(f.getvalue(), 'a\nb c  d\te\nf g\n')
+        
+def test_main():
+    run_unittest(SoftspaceTests)
+    
+if __name__ == '__main__':
+    test_main()
