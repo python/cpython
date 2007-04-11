d840722d2a39d56eeb114d63033ccf1fdc6cb946
Guido van Rossum <guido@python.org>
2007-04-11 17:08:28 +0000

    Make array().tofile() work with a new I/O object.

--- Lib/test/test_array.py
+++ Lib/test/test_array.py
@@ -147,7 +147,7 @@ class BaseTest(unittest.TestCase):
     def test_tofromfile(self):
         a = array.array(self.typecode, 2*self.example)
         self.assertRaises(TypeError, a.tofile)
-        self.assertRaises(TypeError, a.tofile, cStringIO.StringIO())
+        ##self.assertRaises(TypeError, a.tofile, cStringIO.StringIO())
         f = open(test_support.TESTFN, 'wb')
         try:
             a.tofile(f)
