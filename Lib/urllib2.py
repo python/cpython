d4e7f823b8e2b7092c3ff25d4015145e34209881
Facundo Batista <facundobatista@gmail.com>
2007-04-23 17:08:31 +0000

    As specified in RFC 2616, 2xx code indicates that the client's request was successfully received, understood, and accepted. Now in these cases no error is raised. Also fixed tests.

--- Lib/urllib2.py
+++ Lib/urllib2.py
@@ -487,7 +487,9 @@ class HTTPErrorProcessor(BaseHandler):
     def http_response(self, request, response):
         code, msg, hdrs = response.code, response.msg, response.info()
 
-        if code not in (200, 206):
+        # According to RFC 2616, "2xx" code indicates that the client's 
+        # request was successfully received, understood, and accepted.
+        if not (200 <= code < 300):
             response = self.parent.error(
                 'http', request, response, code, msg, hdrs)
 
