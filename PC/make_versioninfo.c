7724099cf982abea08a41e8f6712efb75cab32f9
Kristján Valur Jónsson <kristjan@ccpgames.com>
2007-05-02 15:55:14 +0000

    Merging of change 55024 from the truk to release25-maint branch. Complete revamp of PCBuild8 directory.  Use subdirectories for each project under the main pcbuild solution.  Now make extensive use of property sheets to simplify project configuration. x64 build fully supported, and the process for building PGO version (Profiler Guided Optimization) simplified. All projects are now present, except _ssl, which needs to be reimplemented.  Also, some of the projects that require external libraries need extra work to fully compile on x64.

--- PC/make_versioninfo.c
+++ PC/make_versioninfo.c
@@ -27,7 +27,12 @@ int main(int argc, char **argv)
 		PY_MICRO_VERSION*1000 + PY_RELEASE_LEVEL*10 + PY_RELEASE_SERIAL);
 	printf("#define MS_DLL_ID \"%d.%d\"\n",
 	       PY_MAJOR_VERSION, PY_MINOR_VERSION);
+	printf("#ifndef _DEBUG\n");
 	printf("#define PYTHON_DLL_NAME \"python%d%d.dll\"\n",
 	       PY_MAJOR_VERSION, PY_MINOR_VERSION);
+	printf("#else\n");
+	printf("#define PYTHON_DLL_NAME \"python%d%d_d.dll\"\n",
+	       PY_MAJOR_VERSION, PY_MINOR_VERSION);
+	printf("#endif\n");
 	return 0;
 }
