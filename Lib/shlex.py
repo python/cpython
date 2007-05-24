c90bc7599441632a0b6d0b9bfc92563fe7e19104
Georg Brandl <georg@python.org>
2007-05-24 16:49:29 +0000

    shlex.split() now has an optional "posix" parameter.

--- Lib/shlex.py
+++ Lib/shlex.py
@@ -271,8 +271,8 @@ class shlex:
             raise StopIteration
         return token
 
-def split(s, comments=False):
-    lex = shlex(s, posix=True)
+def split(s, comments=False, posix=True):
+    lex = shlex(s, posix)
     lex.whitespace_split = True
     if not comments:
         lex.commenters = ''
