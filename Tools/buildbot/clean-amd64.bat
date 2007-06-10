80958081df1687695dcb1649c911c299f716b724
Thomas Heller <theller@ctypes.org>
2007-06-10 18:27:54 +0000

    Need to set the environment variables, otherwise devenv.com is not found.

--- Tools/buildbot/clean-amd64.bat
+++ Tools/buildbot/clean-amd64.bat
@@ -1,5 +1,5 @@
 @rem Used by the buildbot "clean" step.
-REM call "%VS71COMNTOOLS%vsvars32.bat"
+call "%VS71COMNTOOLS%vsvars32.bat"
 cd PCbuild
 @echo Deleting .pyc/.pyo files ...
 python.exe rmpyc.py
