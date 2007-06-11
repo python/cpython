d1e5c732cfe17a7be7301d2625db48e67dc16a41
Neal Norwitz <nnorwitz@gmail.com>
2007-06-11 02:16:10 +0000

    Fix a bug when there was a newline in the string expandtabs was called on. This also catches another condition that can overflow.
    Will backport.

--- Objects/stringobject.c
+++ Objects/stringobject.c
@@ -3322,7 +3322,8 @@ string_expandtabs(PyStringObject *self, PyObject *args)
 	    if (tabsize > 0) {
 		j += tabsize - (j % tabsize);
 		if (old_j > j) {
-		    PyErr_SetString(PyExc_OverflowError, "new string is too long");
+		    PyErr_SetString(PyExc_OverflowError,
+				    "new string is too long");
 		    return NULL;
 		}
 		old_j = j;
@@ -3332,7 +3333,12 @@ string_expandtabs(PyStringObject *self, PyObject *args)
             j++;
             if (*p == '\n' || *p == '\r') {
                 i += j;
-                j = 0;
+                old_j = j = 0;
+                if (i < 0) {
+                    PyErr_SetString(PyExc_OverflowError,
+                                    "new string is too long");
+                    return NULL;
+                }
             }
         }
 
