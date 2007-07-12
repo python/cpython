1f498ef63da4caa07a44fcc97ac66894d4d29e86
Thomas Heller <theller@ctypes.org>
2007-07-12 11:59:52 +0000

    Fix a ctypes test.

--- Lib/ctypes/test/test_objects.py
+++ Lib/ctypes/test/test_objects.py
@@ -22,9 +22,9 @@ assigned from Python must be kept.
 
 >>> array[4] = 'foo bar'
 >>> array._objects
-{'4': 'foo bar'}
+{s'4': b'foo bar'}
 >>> array[4]
-'foo bar'
+s'foo bar'
 >>>
 
 It gets more complicated when the ctypes instance itself is contained
@@ -47,9 +47,9 @@ of 'x' ('_b_base_' is either None, or the root object owning the memory block):
 
 >>> x.array[0] = 'spam spam spam'
 >>> x._objects
-{'0:2': 'spam spam spam'}
+{s'0:2': b'spam spam spam'}
 >>> x.array._b_base_._objects
-{'0:2': 'spam spam spam'}
+{s'0:2': b'spam spam spam'}
 >>>
 
 '''
