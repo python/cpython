eee9f4aa8a98cf3dfeea43fc3ceb2d08de386e50
Thomas Heller <theller@ctypes.org>
2007-07-12 15:41:51 +0000

    Accept bytes in c_char_p and c_wchar_p types.

--- Lib/ctypes/test/test_bytes.py
+++ Lib/ctypes/test/test_bytes.py
@@ -14,5 +14,13 @@ class BytesTest(unittest.TestCase):
         c_wchar.from_param(b"x")
         (c_wchar * 3)(b"a", b"b", b"c")
 
+    def test_c_char_p(self):
+        c_char_p("foo bar")
+        c_char_p(b"foo bar")
+
+    def test_c_wchar_p(self):
+        c_wchar_p("foo bar")
+        c_wchar_p(b"foo bar")
+
 if __name__ == '__main__':
     unittest.main()
