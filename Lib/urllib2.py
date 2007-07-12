6476f992f50a6a8e03726ba6b9746b6c5b21cf3f
Georg Brandl <georg@python.org>
2007-07-12 08:05:45 +0000

    Patch #1752270, #1750931: complain if urllib2 add_handler called without handler.

--- Lib/urllib2.py
+++ Lib/urllib2.py
@@ -295,6 +295,10 @@ class OpenerDirector:
         self.process_request = {}
 
     def add_handler(self, handler):
+        if not hasattr(handler, "add_parent"):
+            raise TypeError("expected BaseHandler instance, got %r" %
+                            type(handler))
+
         added = False
         for meth in dir(handler):
             if meth in ["redirect_request", "do_open", "proxy_open"]:
