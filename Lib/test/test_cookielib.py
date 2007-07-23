de98b69e3cd69eb6e78689ca4aab92e71b2a73c2
Guido van Rossum <guido@python.org>
2007-07-23 03:46:37 +0000

    Don't use cwnon-ASCII characters in the source; use \ooo escapes. (This doesn't fix test_cookielib, just removes the SyntaxError.)

--- Lib/test/test_cookielib.py
+++ Lib/test/test_cookielib.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 """Tests for cookielib.py."""
 
 import re, os, time
@@ -1530,13 +1529,13 @@ class LWPCookieTests(TestCase):
                       "foo  =   bar; version    =   1")
 
         cookie = interact_2965(
-            c, "http://www.acme.com/foo%2f%25/<<%0anewå/æøå",
+            c, "http://www.acme.com/foo%2f%25/<<%0anew\345/\346\370\345",
             'bar=baz; path="/foo/"; version=1');
         version_re = re.compile(r'^\$version=\"?1\"?', re.I)
         self.assert_("foo=bar" in cookie and version_re.search(cookie))
 
         cookie = interact_2965(
-            c, "http://www.acme.com/foo/%25/<<%0anewå/æøå")
+            c, "http://www.acme.com/foo/%25/<<%0anew\345/\346\370\345")
         self.assert_(not cookie)
 
         # unicode URL doesn't raise exception
