9ab80a9fb4d78a9574c26950ef694f8837372017
Walter Dörwald <walter@livinglogic.de>
2007-08-17 16:58:43 +0000

    Move another variable declaration up.

--- Objects/unicodeobject.c
+++ Objects/unicodeobject.c
@@ -1542,6 +1542,8 @@ PyUnicode_DecodeUTF32Stateful(const char *s,
 #else
     int iorder[] = {3, 2, 1, 0};
 #endif
+    PyObject *errorHandler = NULL;
+    PyObject *exc = NULL;
     /* On narrow builds we split characters outside the BMP into two
        codepoints => count how much extra space we need. */
 #ifndef Py_UNICODE_WIDE
@@ -1549,8 +1551,6 @@ PyUnicode_DecodeUTF32Stateful(const char *s,
 	if (((Py_UCS4 *)s)[i] >= 0x10000)
 	    pairs++;
 #endif
-    PyObject *errorHandler = NULL;
-    PyObject *exc = NULL;
 
     /* This might be one to much, because of a BOM */
     unicode = _PyUnicode_New((size+3)/4+pairs);
