149dcf354fa54586e5895c2e27694ff3699bfd56
Neal Norwitz <nnorwitz@gmail.com>
2007-08-25 00:21:36 +0000

    Use unicode and add a "test" for syslog

---
+++ Lib/test/test_syslog.py
@@ -0,0 +1,37 @@
+
+import syslog
+import unittest
+from test import test_support
+
+# XXX(nnorwitz): This test sucks.  I don't know of a platform independent way
+# to verify that the messages were really logged.
+# The only purpose of this test is to verify the code doesn't crash or leak.
+
+class Test(unittest.TestCase):
+
+    def test_openlog(self):
+        syslog.openlog('python')
+
+    def test_syslog(self):
+        syslog.openlog('python')
+        syslog.syslog('test message from python test_syslog')
+        syslog.syslog(syslog.LOG_ERR, 'test error from python test_syslog')
+
+    def test_closelog(self):
+        syslog.openlog('python')
+        syslog.closelog()
+
+    def test_setlogmask(self):
+        syslog.setlogmask(syslog.LOG_DEBUG)
+
+    def test_log_mask(self):
+        syslog.LOG_MASK(syslog.LOG_INFO)
+
+    def test_log_upto(self):
+        syslog.LOG_UPTO(syslog.LOG_INFO)
+
+def test_main():
+    test_support.run_unittest(__name__)
+
+if __name__ == "__main__":
+    test_main()
