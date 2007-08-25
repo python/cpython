cb51c6e9530ee492d3eb9cd6484136c3811476c8
Neal Norwitz <nnorwitz@gmail.com>
2007-08-25 17:23:06 +0000

    Get test_pyclbr to pass after removing the email package

--- Lib/test/test_pyclbr.py
+++ Lib/test/test_pyclbr.py
@@ -174,7 +174,7 @@ class PyclbrTest(TestCase):
         cm('pydoc')
 
         # Tests for modules inside packages
-        cm('email.parser')
+        #cm('email.parser')
         cm('test.test_pyclbr')
 
 
