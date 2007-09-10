fbf83bbd77da90fc01ed62abed52320f614f244f
Martin v. Löwis <martin@v.loewis.de>
2007-09-10 10:22:05 +0000

    Beginnings of a "build MSI" step.

---
+++ Tools/buildbot/buildmsi.bat
@@ -0,0 +1,10 @@
+@rem Used by the buildbot "buildmsi" step.
+cmd /c Tools\buildbot\external.bat
+call "%VS71COMNTOOLS%vsvars32.bat"
+cmd /q/c Tools\buildbot\kill_python.bat
+devenv.com /useenv /build Release PCbuild\pcbuild.sln
+c:\cygwin\bin\bash.exe -c 'cd Doc;make PYTHON=python2.5 htmlhelp'
+"%ProgramFiles%\HTML Help Workshop\hhc.exe Doc\build\htmlhelp\pydoc.hhp
+cd Tools\msi
+del *.msi
+%HOST_PYTHON% msi.py
