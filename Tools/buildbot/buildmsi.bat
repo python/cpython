5c1b4f2e33d27ec78b83866f6d6d6ca562004d01
Martin v. Löwis <martin@v.loewis.de>
2007-09-13 13:27:27 +0000

    More path fixes.

--- Tools/buildbot/buildmsi.bat
+++ Tools/buildbot/buildmsi.bat
@@ -3,7 +3,7 @@
 cmd /c Tools\buildbot\external.bat
 @rem build release versions of things
 call "%VS71COMNTOOLS%vsvars32.bat"
-if not exist db-4.4.20\build_win32\release\libdb44s.lib (
+if not exist ..\db-4.4.20\build_win32\release\libdb44s.lib (
    devenv ..\db-4.4.20\build_win32\Berkeley_DB.sln /build Release /project db_static
 )
 
