5a885d7f3a65ee9aee351e7c557d65faf32676f5
Martin v. Löwis <martin@v.loewis.de>
2007-09-13 13:33:46 +0000

    Merged revisions 58133-58135 via svnmerge from svn+ssh://pythondev@svn.python.org/python/trunk
    ........
      r58133 | martin.v.loewis | 2007-09-13 11:59:00 +0200 (Do, 13 Sep 2007) | 2 lines
    
      Add more automated actions.
    ........
      r58134 | martin.v.loewis | 2007-09-13 12:38:42 +0200 (Do, 13 Sep 2007) | 2 lines
    
      Fix path.
    ........
      r58135 | martin.v.loewis | 2007-09-13 15:27:27 +0200 (Do, 13 Sep 2007) | 2 lines
    
      More path fixes.
    ........

--- Tools/buildbot/buildmsi.bat
+++ Tools/buildbot/buildmsi.bat
@@ -1,10 +1,24 @@
 @rem Used by the buildbot "buildmsi" step.
+
 cmd /c Tools\buildbot\external.bat
+@rem build release versions of things
 call "%VS71COMNTOOLS%vsvars32.bat"
+if not exist ..\db-4.4.20\build_win32\release\libdb44s.lib (
+   devenv ..\db-4.4.20\build_win32\Berkeley_DB.sln /build Release /project db_static
+)
+
+@rem build Python
 cmd /q/c Tools\buildbot\kill_python.bat
 devenv.com /useenv /build Release PCbuild\pcbuild.sln
+
+@rem build the documentation
 bash.exe -c 'cd Doc;make PYTHON=python2.5 update htmlhelp'
-"%ProgramFiles%\HTML Help Workshop\hhc.exe Doc\build\htmlhelp\pydoc.hhp
-cd Tools\msi
+"%ProgramFiles%\HTML Help Workshop\hhc.exe" Doc\build\htmlhelp\pydoc.hhp
+
+@rem buold the MSI file
+cd PC
+nmake /f icons.mak
+cd ..\Tools\msi
 del *.msi
+nmake /f msisupport.mak
 %HOST_PYTHON% msi.py
