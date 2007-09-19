692910a734a298fee76a7e5e645c164f6c6de110
Facundo Batista <facundobatista@gmail.com>
2007-09-19 15:10:06 +0000

    Issue #1772851.  Alters long.__hash__ from being *almost* completely predictable to being completely predictable.  The value of hash(n) is unchanged for any n that's small enough to be representable as an int, and also unchanged for the vast majority of long integers n of reasonable size.

--- Lib/test/test_hash.py
+++ Lib/test/test_hash.py
@@ -18,10 +18,21 @@ class HashEqualityTestCase(unittest.TestCase):
 
     def test_numeric_literals(self):
         self.same_hash(1, 1L, 1.0, 1.0+0.0j)
+        self.same_hash(0, 0L, 0.0, 0.0+0.0j)
+        self.same_hash(-1, -1L, -1.0, -1.0+0.0j)
+        self.same_hash(-2, -2L, -2.0, -2.0+0.0j)
 
     def test_coerced_integers(self):
         self.same_hash(int(1), long(1), float(1), complex(1),
                        int('1'), float('1.0'))
+        self.same_hash(int(-2**31), long(-2**31), float(-2**31))
+        self.same_hash(int(1-2**31), long(1-2**31), float(1-2**31))
+        self.same_hash(int(2**31-1), long(2**31-1), float(2**31-1))
+        # for 64-bit platforms
+        self.same_hash(int(2**31), long(2**31), float(2**31))
+        self.same_hash(int(-2**63), long(-2**63), float(-2**63))
+        self.same_hash(int(1-2**63), long(1-2**63))
+        self.same_hash(int(2**63-1), long(2**63-1))
 
     def test_coerced_floats(self):
         self.same_hash(long(1.23e300), float(1.23e300))
