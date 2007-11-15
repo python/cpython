a041ba56fb208e86a7e1dcc6f1449c4741bd0683
Thomas Heller <theller@ctypes.org>
2007-11-15 19:11:49 +0000

    On Win64, linking the python dll currently fails with 'unresolved external symbol PyObject_Unicode'.
    Building _ssl than hangs the build server because starting python.exe
    displayes a message box that 'Python30.dll' cannot be found.
    
    Temporary (?) solution: Look for existance of python30.dll and fail
    the _ssl build when it is missing.

--- PCbuild/build_ssl.bat
+++ PCbuild/build_ssl.bat
@@ -4,8 +4,10 @@ if "%1" == "ReleaseAMD64" call "%MSSdk%\SetEnv" /XP64 /RETAIL
 if not defined HOST_PYTHON (
   if %1 EQU Debug (
     set HOST_PYTHON=python_d.exe
+    if not exist python30_d.dll exit 1
   ) ELSE (
     set HOST_PYTHON=python.exe
+    if not exist python30.dll exit 1
   )
 )
 %HOST_PYTHON% build_ssl.py %1 %2
