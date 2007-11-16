1c8e5bc3b793597d2326d705119fb425d0ecf94d
Thomas Heller <theller@ctypes.org>
2007-11-16 07:00:30 +0000

    Use 'del' instead of rmpyc.py to remove the pyc and pyo files.

--- Tools/buildbot/clean.bat
+++ Tools/buildbot/clean.bat
@@ -2,6 +2,6 @@
 call "%VS71COMNTOOLS%vsvars32.bat"
 cd PCbuild
 @echo Deleting .pyc/.pyo files ...
-python_d.exe rmpyc.py
+del /s Lib\*.pyc Lib\*.pyo
 devenv.com /clean Release pcbuild.sln
 devenv.com /clean Debug pcbuild.sln
