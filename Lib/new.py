b61a1f5219fb52d3d5a6a8bee427b94efeee4894
Christian Heimes <christian@cheimes.de>
2007-11-27 21:35:44 +0000

    Added a deprecation warning to the 'new' module.

--- Lib/new.py
+++ Lib/new.py
@@ -3,6 +3,9 @@
 This module is no longer required except for backward compatibility.
 Objects of most types can now be created by calling the type object.
 """
+from warnings import warn as _warn
+_warn("The 'new' module is not supported in 3.x, use the 'types' module "
+    "instead.", DeprecationWarning, 2)
 
 from types import ClassType as classobj
 from types import FunctionType as function
