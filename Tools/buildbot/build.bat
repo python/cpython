0ebea7c100ca543a36e3593f23e7c17146bb79a5
Christian Heimes <christian@cheimes.de>
2007-12-31 14:59:26 +0000

    Fix paths for build bot

--- Tools/buildbot/build.bat
+++ Tools/buildbot/build.bat
@@ -2,4 +2,4 @@
 cmd /c Tools\buildbot\external.bat
 call "%VS71COMNTOOLS%vsvars32.bat"
 cmd /q/c Tools\buildbot\kill_python.bat
-devenv.com /useenv /build Debug PCbuild\pcbuild.sln
+devenv.com /useenv /build Debug PC\VS7.1\pcbuild.sln
--- Tools/buildbot/clean-amd64.bat
+++ Tools/buildbot/clean-amd64.bat
@@ -1,6 +1,6 @@
 @rem Used by the buildbot "clean" step.
 call "%VS71COMNTOOLS%vsvars32.bat"
-cd PCbuild
+cd PC\VS7.1
 @echo Deleting .pyc/.pyo files ...
 python.exe rmpyc.py
 devenv.com /clean ReleaseAMD64 pcbuild.sln
