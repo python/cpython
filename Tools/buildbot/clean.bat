57dddfbbd6b4bf8b327bc4c978befedaba5034e7
Christian Heimes <christian@cheimes.de>
2008-01-02 18:30:52 +0000

    Merged revisions 59642-59665 via svnmerge from svn+ssh://pythondev@svn.python.org/python/trunk
    ........
      r59653 | martin.v.loewis | 2008-01-01 22:05:17 +0100 (Tue, 01 Jan 2008) | 3 lines
    
      Return results from Python callbacks to Tcl as Tcl objects.
      Fixes Tk issue #1851526
    ........
      r59654 | martin.v.loewis | 2008-01-01 22:08:18 +0100 (Tue, 01 Jan 2008) | 4 lines
    
      Always convert Text.index result to string.
      This improves compatibility with Tcl 8.5, which would
      otherwise return textindex objects.
    ........
      r59655 | martin.v.loewis | 2008-01-01 22:09:07 +0100 (Tue, 01 Jan 2008) | 2 lines
    
      News item for r59653.
    ........
      r59656 | martin.v.loewis | 2008-01-02 00:00:00 +0100 (Wed, 02 Jan 2008) | 1 line
    
      Don't link with Tix; Tix is loaded dynamically by Tcl.
    ........
      r59657 | martin.v.loewis | 2008-01-02 00:00:48 +0100 (Wed, 02 Jan 2008) | 1 line
    
      Use Visual Studio 2009 on the build slaves.
    ........
      r59658 | martin.v.loewis | 2008-01-02 00:36:24 +0100 (Wed, 02 Jan 2008) | 1 line
    
      Test in PCbuild directory.
    ........
      r59661 | kurt.kaiser | 2008-01-02 05:11:28 +0100 (Wed, 02 Jan 2008) | 6 lines
    
      Issue1177
      r58207 and r58247 patch logic is reversed.  I noticed this when I
      tried to use urllib to retrieve a file which required auth.
    
      Fix that and add a test for 401 error to verify.
    ........
      r59662 | kurt.kaiser | 2008-01-02 06:23:38 +0100 (Wed, 02 Jan 2008) | 2 lines
    
      Change docstrings to comments so test output will display normally.
    ........
      r59665 | christian.heimes | 2008-01-02 18:43:40 +0100 (Wed, 02 Jan 2008) | 5 lines
    
      Removed PCbuild8/ directory and added a new build directory for VS 2005
      based on the VS 2008 build directory to PC/VS8.0. The script
      PCbuild/vs8to9.py was added to sync changes from PCbuild to PC/VS8.0.
    
      Kristjan, the initial creator of the PCbuild8 directory is fine with the replacement. I've moved the new version of the VS 2005 build directory next to the other legacy build directories. The new sync script is based on the work of wreck and syncs changes in the project, property and solution files.
    ........

--- Tools/buildbot/clean.bat
+++ Tools/buildbot/clean.bat
@@ -1,7 +1,7 @@
 @rem Used by the buildbot "clean" step.
-call "%VS71COMNTOOLS%vsvars32.bat"
-cd PC\VS7.1
+call "%VS90COMNTOOLS%vsvars32.bat"
+cd PCbuild
 @echo Deleting .pyc/.pyo files ...
 del /s Lib\*.pyc Lib\*.pyo
-devenv.com /clean Release pcbuild.sln
-devenv.com /clean Debug pcbuild.sln
+vcbuild /clean pcbuild.sln "Release|Win32"
+vcbuild /clean pcbuild.sln "Debug|Win32"
