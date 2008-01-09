5fb038e279a74461083509244d0a0b335612e287
Thomas Heller <theller@ctypes.org>
2008-01-09 21:35:43 +0000

    Change amd64 buildbot scripts to use Visual Studio 2008, and to use the required versions of external sources. External sources are not yet built, so the build-step fails to built some targets.

--- Tools/buildbot/build-amd64.bat
+++ Tools/buildbot/build-amd64.bat
@@ -1,6 +1,5 @@
 @rem Used by the buildbot "compile" step.
-setlocal
 cmd /c Tools\buildbot\external-amd64.bat
-call "%VS71COMNTOOLS%vsvars32.bat"
+call "%VS90COMNTOOLS%vsvars32.bat"
 REM cmd /q/c Tools\buildbot\kill_python.bat
-devenv.com /build ReleaseAMD64 PC\VS7.1\pcbuild.sln
+vcbuild PCbuild\pcbuild.sln "Debug|x64"
--- Tools/buildbot/clean-amd64.bat
+++ Tools/buildbot/clean-amd64.bat
@@ -1,6 +1,7 @@
 @rem Used by the buildbot "clean" step.
-call "%VS71COMNTOOLS%vsvars32.bat"
-cd PC\VS7.1
+call "%VS90COMNTOOLS%vsvars32.bat"
+cd PCbuild
 @echo Deleting .pyc/.pyo files ...
 del /s Lib\*.pyc Lib\*.pyo
-devenv.com /clean ReleaseAMD64 pcbuild.sln
+vcbuild /clean pcbuild.sln "Release|x64"
+vcbuild /clean pcbuild.sln "Debug|x64"
