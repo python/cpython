c682614df098700c689eabb47c6857f244e85bed
Thomas Heller <theller@ctypes.org>
2008-01-11 19:34:06 +0000

    Raise an error instead of crashing with a segfault when a NULL function pointer is called.
    Will backport to release25-maint.

--- Modules/_ctypes/_ctypes.c
+++ Modules/_ctypes/_ctypes.c
@@ -3312,6 +3312,11 @@ CFuncPtr_call(CFuncPtrObject *self, PyObject *inargs, PyObject *kwds)
 
 
 	pProc = *(void **)self->b_ptr;
+	if (pProc == NULL) {
+		PyErr_SetString(PyExc_ValueError,
+				"attempt to call NULL function pointer");
+		return NULL;
+	}
 #ifdef MS_WIN32
 	if (self->index) {
 		/* It's a COM method */
