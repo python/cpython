1d0256acdc25d509c57fdbcd7928dca57251d016
Eric Smith <eric@trueblade.com>
2008-01-11 00:32:16 +0000

    Added the test cases I actually meant to add.

--- Lib/test/test_builtin.py
+++ Lib/test/test_builtin.py
@@ -562,6 +562,11 @@ class BuiltinTest(unittest.TestCase):
         self.assertRaises(TypeError, format, object(), 4)
         self.assertRaises(TypeError, format, object(), object())
 
+        # first argument to object.__format__ must be string
+        self.assertRaises(TypeError, object().__format__, 3)
+        self.assertRaises(TypeError, object().__format__, object())
+        self.assertRaises(TypeError, object().__format__, None)
+
         # make sure we can take a subclass of str as a format spec
         self.assertEqual(format(0, C('10')), '         0')
 
