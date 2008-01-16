7155a09c444a9e6229c7387468265b505680ff51
Georg Brandl <georg@python.org>
2008-01-16 20:27:56 +0000

    Add Python-specific content to Doc dir. Update configuration file to work with the newest Sphinx.

--- Doc/conf.py
+++ Doc/conf.py
@@ -2,22 +2,28 @@
 #
 # Python documentation build configuration file
 #
+# This file is execfile()d with the current directory set to its containing dir.
+#
 # The contents of this file are pickled, so don't put values in the namespace
 # that aren't pickleable (module imports are okay, they're removed automatically).
 
 # General configuration
 # ---------------------
 
+# General substitutions.
+project = 'Python'
+copyright = '1990-2007, Python Software Foundation'
+
 # The default replacements for |version| and |release|.
-# If 'auto', Sphinx looks for the Include/patchlevel.h file in the current Python
+# If '<auto>', Sphinx looks for the Include/patchlevel.h file in the current Python
 # source tree and replaces the values accordingly.
 #
 # The short X.Y version.
 # version = '2.6'
-version = 'auto'
+version = '<auto>'
 # The full version, including alpha/beta/rc tags.
 # release = '2.6a0'
-release = 'auto'
+release = '<auto>'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
@@ -38,6 +44,9 @@ unused_files = [
     'library/xml.etree.rst',
 ]
 
+# Relative filename of the reference count data file.
+refcount_file = 'data/refcounts.dat'
+
 # If true, '()' will be appended to :func: etc. cross-reference text.
 add_function_parentheses = True
 
@@ -49,9 +58,6 @@ add_module_names = True
 # Options for HTML output
 # -----------------------
 
-# The base URL for download links.
-html_download_base_url = 'http://docs.python.org/ftp/python/doc/'
-
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
 html_last_updated_fmt = '%b %d, %Y'
@@ -60,12 +66,71 @@ html_last_updated_fmt = '%b %d, %Y'
 # typographically correct entities.
 html_use_smartypants = True
 
+# Content template for the index page, filename relative to this file.
+html_index = 'tools/sphinxext/indexcontent.html'
+
+# Custom sidebar templates, filenames relative to this file.
+html_sidebars = {
+    'index': 'tools/sphinxext/indexsidebar.html',
+}
+
+# Additional templates that should be rendered to pages.
+html_additional_pages = {
+    'download': 'tools/sphinxext/download.html',
+}
+
+# Output file base name for HTML help builder.
+htmlhelp_basename = 'pydoc'
+
 
 # Options for LaTeX output
 # ------------------------
 
-# The paper size ("letter" or "a4").
-latex_paper_size = "a4"
+# The paper size ('letter' or 'a4').
+latex_paper_size = 'a4'
 
-# The font size ("10pt", "11pt" or "12pt").
-latex_font_size = "10pt"
+# The font size ('10pt', '11pt' or '12pt').
+latex_font_size = '10pt'
+
+# Grouping the document tree into LaTeX files. List of tuples
+# (source start file, target name, title, author, document class [howto/manual]).
+_stdauthor = r'Guido van Rossum\\Fred L. Drake, Jr., editor'
+latex_documents = [
+    ('c-api/index.rst', 'c-api.tex',
+     'The Python/C API', _stdauthor, 'manual'),
+    ('distutils/index.rst', 'distutils.tex',
+     'Distributing Python Modules', _stdauthor, 'manual'),
+    ('documenting/index.rst', 'documenting.tex',
+     'Documenting Python', 'Georg Brandl', 'manual'),
+    ('extending/index.rst', 'extending.tex',
+     'Extending and Embedding Python', _stdauthor, 'manual'),
+    ('install/index.rst', 'install.tex',
+     'Installing Python Modules', _stdauthor, 'manual'),
+    ('library/index.rst', 'library.tex',
+     'The Python Library Reference', _stdauthor, 'manual'),
+    ('reference/index.rst', 'reference.tex',
+     'The Python Language Reference', _stdauthor, 'manual'),
+    ('tutorial/index.rst', 'tutorial.tex',
+     'Python Tutorial', _stdauthor, 'manual'),
+    ('using/index.rst', 'using.tex',
+     'Using Python', _stdauthor, 'manual'),
+    ('whatsnew/' + version + '.rst', 'whatsnew.tex',
+     'What\'s New in Python', 'A. M. Kuchling', 'howto'),
+]
+# Collect all HOWTOs individually
+import os
+latex_documents.extend(('howto/' + fn, 'howto-' + fn[:-4] + '.tex',
+                        'HOWTO', _stdauthor, 'howto')
+                       for fn in os.listdir('howto')
+                       if fn.endswith('.rst') and fn != 'index.rst')
+
+# Additional stuff for the LaTeX preamble.
+latex_preamble = r'''
+\authoraddress{
+  \strong{Python Software Foundation}\\
+  Email: \email{docs@python.org}
+}
+'''
+
+# Documents to append as an appendix to all manuals.
+latex_appendices = ['glossary.rst', 'about.rst', 'license.rst', 'copyright.rst']
