15ce880cc8c3de29e91e2e867b2db0b19a48e5f3
Andrew M. Kuchling <amk@amk.ca>
2008-01-19 20:12:04 +0000

    Bug 1277: make Maildir use the user-provided factory instead of hard-wiring MaildirMessage. 2.5.2 bugfix candidate.

--- Lib/mailbox.py
+++ Lib/mailbox.py
@@ -315,7 +315,10 @@ class Maildir(Mailbox):
         subpath = self._lookup(key)
         f = open(os.path.join(self._path, subpath), 'r')
         try:
-            msg = MaildirMessage(f)
+            if self._factory:
+                msg = self._factory(f)
+            else:
+                msg = MaildirMessage(f)
         finally:
             f.close()
         subdir, name = os.path.split(subpath)
