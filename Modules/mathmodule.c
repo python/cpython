fe424f754f408840981bdf9cb6de31243f79a7c8
Raymond Hettinger <python@rcn.com>
2008-02-02 05:24:44 +0000

    Neaten-up a bit.

--- Modules/mathmodule.c
+++ Modules/mathmodule.c
@@ -157,17 +157,13 @@ FUNC1(tanh, tanh,
 static PyObject *
 math_trunc(PyObject *self, PyObject *number)
 {
-        /* XXX: The py3k branch gets better errors for this by using
-           _PyType_Lookup(), but since float's mro isn't set in py2.6,
-           we just use PyObject_CallMethod here. */
 	return PyObject_CallMethod(number, "__trunc__", NULL);
 }
 
 PyDoc_STRVAR(math_trunc_doc,
 "trunc(x:Real) -> Integral\n"
 "\n"
-"Truncates x to the nearest Integral toward 0. Uses the __trunc__ magic"
-"method.");
+"Truncates x to the nearest Integral toward 0. Uses the __trunc__ magic method.");
 
 static PyObject *
 math_frexp(PyObject *self, PyObject *arg)
