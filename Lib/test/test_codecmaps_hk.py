01612e7dec9fcbd0733137aa90f0f21cfa49299f
Hye-Shik Chang <hyeshik@gmail.com>
2008-02-08 17:10:20 +0000

    Update big5hkscs codec to conform to the HKSCS:2004 revision.

--- Lib/test/test_codecmaps_hk.py
+++ Lib/test/test_codecmaps_hk.py
@@ -11,10 +11,11 @@ import unittest
 class TestBig5HKSCSMap(test_multibytecodec_support.TestBase_Mapping,
                        unittest.TestCase):
     encoding = 'big5hkscs'
-    mapfileurl = 'http://people.freebsd.org/~perky/i18n/BIG5HKSCS.TXT'
+    mapfileurl = 'http://people.freebsd.org/~perky/i18n/BIG5HKSCS-2004.TXT'
 
 def test_main():
     test_support.run_unittest(__name__)
 
 if __name__ == "__main__":
+    test_support.use_resources = ['urlfetch']
     test_main()
