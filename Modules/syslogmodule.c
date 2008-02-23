f0476e816950928246ee11d873fb13a86c691fb6
Christian Heimes <christian@cheimes.de>
2008-02-23 17:42:31 +0000

    Patch #1957: syslogmodule: Release GIL when calling syslog(3)

--- Modules/syslogmodule.c
+++ Modules/syslogmodule.c
@@ -92,7 +92,9 @@ syslog_syslog(PyObject * self, PyObject * args)
 			return NULL;
 	}
 
+	Py_BEGIN_ALLOW_THREADS;
 	syslog(priority, "%s", message);
+	Py_END_ALLOW_THREADS;
 	Py_INCREF(Py_None);
 	return Py_None;
 }
