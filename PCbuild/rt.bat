377cc761aae59e65c15015c899f395665bfd8d0c
Martin v. Löwis <martin@v.loewis.de>
2008-03-14 13:56:09 +0000

    Patch #2284: add -x64 option to rt.bat.

--- PCbuild/rt.bat
+++ PCbuild/rt.bat
@@ -1,11 +1,13 @@
 @echo off
 rem Run Tests.  Run the regression test suite.
-rem Usage:  rt [-d] [-O] [-q] regrtest_args
+rem Usage:  rt [-d] [-O] [-q] [-x64] regrtest_args
 rem -d   Run Debug build (python_d.exe).  Else release build.
 rem -O   Run python.exe or python_d.exe (see -d) with -O.
 rem -q   "quick" -- normally the tests are run twice, the first time
 rem      after deleting all the .py[co] files reachable from Lib/.
 rem      -q runs the tests just once, and without deleting .py[co] files.
+rem -x64 Run the 64-bit build of python (or python_d if -d was specified)
+rem      from the 'amd64' dir instead of the 32-bit build in this dir.
 rem All leading instances of these switches are shifted off, and
 rem whatever remains is passed to regrtest.py.  For example,
 rem     rt -O -d -x test_thread
@@ -24,16 +26,20 @@ rem     rt -u "network,largefile"
 
 setlocal
 
-set exe=python
+set prefix=.\
+set suffix=
 set qmode=
 set dashO=
-PATH %PATH%;..\..\tcltk\bin
+set tcltk=
 
 :CheckOpts
 if "%1"=="-O" (set dashO=-O)     & shift & goto CheckOpts
 if "%1"=="-q" (set qmode=yes)    & shift & goto CheckOpts
-if "%1"=="-d" (set exe=python_d) & shift & goto CheckOpts
+if "%1"=="-d" (set suffix=_d)    & shift & goto CheckOpts
+if "%1"=="-x64" (set prefix=amd64) & (set tcltk=tcltk64) & shift & goto CheckOpts
 
+PATH %PATH%;..\..\%tcltk%\bin
+set exe=%prefix%\python%suffix%
 set cmd=%exe% %dashO% -E -tt ../lib/test/regrtest.py %1 %2 %3 %4 %5 %6 %7 %8 %9
 if defined qmode goto Qmode
 
