08976cb6967a071682612211579385e35fd68eda
Christian Heimes <christian@cheimes.de>
2008-03-16 00:32:36 +0000

    Merged revisions 61404-61407 via svnmerge from svn+ssh://pythondev@svn.python.org/python/trunk
    ........
      r61404 | raymond.hettinger | 2008-03-15 21:02:04 +0100 (Sat, 15 Mar 2008) | 17 lines
    
      Removed Exact/Inexact after discussion with Yasskin.
    
      Unlike Scheme where exactness is implemented as taints, the Python
      implementation associated exactness with data types.  This created
      inheritance issues (making an exact subclass of floats would result
      in the subclass having both an explicit Exact registration and an
      inherited Inexact registration).  This was a problem for the
      decimal module which was designed to span both exact and inexact
      arithmetic.  There was also a question of use cases and no examples
      were found where ABCs for exactness could be used to improve code.
      One other issue was having separate tags for both the affirmative
      and negative cases.  This is at odds with the approach taken
      elsewhere in the Python (i.e. we don't have an ABC both Hashable
      and Unhashable).
    ........
      r61405 | raymond.hettinger | 2008-03-15 21:37:50 +0100 (Sat, 15 Mar 2008) | 1 line
    
      Zap one more use of Exact/Inexact.
    ........
      r61406 | neal.norwitz | 2008-03-15 23:03:18 +0100 (Sat, 15 Mar 2008) | 9 lines
    
      Add a warning for code like:
        assert (0, 'message')
    
      An empty tuple does not create a warning.  While questionable usage:
        assert (), 'message'
    
      should not display a warning.  Tested manually.
      The warning message could be improved.  Feel free to update it.
    ........
      r61407 | neal.norwitz | 2008-03-15 23:36:01 +0100 (Sat, 15 Mar 2008) | 1 line
    
      Handle memory allocation failure.  Found by Adam Olsen
    ........

--- Lib/test/test_abstract_numbers.py
+++ Lib/test/test_abstract_numbers.py
@@ -4,7 +4,6 @@ import math
 import operator
 import unittest
 from numbers import Complex, Real, Rational, Integral
-from numbers import Exact, Inexact
 from numbers import Number
 from test import test_support
 
@@ -12,8 +11,6 @@ class TestNumbers(unittest.TestCase):
     def test_int(self):
         self.failUnless(issubclass(int, Integral))
         self.failUnless(issubclass(int, Complex))
-        self.failUnless(issubclass(int, Exact))
-        self.failIf(issubclass(int, Inexact))
 
         self.assertEqual(7, int(7).real)
         self.assertEqual(0, int(7).imag)
@@ -24,8 +21,6 @@ class TestNumbers(unittest.TestCase):
     def test_float(self):
         self.failIf(issubclass(float, Rational))
         self.failUnless(issubclass(float, Real))
-        self.failIf(issubclass(float, Exact))
-        self.failUnless(issubclass(float, Inexact))
 
         self.assertEqual(7.3, float(7.3).real)
         self.assertEqual(0, float(7.3).imag)
@@ -34,8 +29,6 @@ class TestNumbers(unittest.TestCase):
     def test_complex(self):
         self.failIf(issubclass(complex, Real))
         self.failUnless(issubclass(complex, Complex))
-        self.failIf(issubclass(complex, Exact))
-        self.failUnless(issubclass(complex, Inexact))
 
         c1, c2 = complex(3, 2), complex(4,1)
         # XXX: This is not ideal, but see the comment in math_trunc().
