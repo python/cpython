4f4738f015c5d16cac3440adfe40b4ac52ba2316
Trent Nelson <trent.nelson@snakebite.org>
2008-03-19 09:23:08 +0000

    Lets have another try at getting the Windows buildbots in a consistent state before rebuilding using the new process.

--- Tools/buildbot/external.bat
+++ Tools/buildbot/external.bat
@@ -1,46 +1,17 @@
 @rem Fetches (and builds if necessary) external dependencies
 
 @rem Assume we start inside the Python source directory
-cd ..
-call "%VS90COMNTOOLS%vsvars32.bat"
+call "Tools\buildbot\external-common.bat"
+call "%VS90COMNTOOLS%\vsvars32.bat"
 
-@rem bzip
-if not exist bzip2-1.0.3 svn export http://svn.python.org/projects/external/bzip2-1.0.3
-
-@rem Sleepycat db
-@rem Remove VS 2003 builds
-if exist db-4.4.20 if not exist db-4.4.20\build_win32\this_is_for_vs9 (
-   echo Removing old build
-   rd /s/q db-4.4.20
-)
-if not exist db-4.4.20 svn export http://svn.python.org/projects/external/db-4.4.20-vs9 db-4.4.20
-if not exist db-4.4.20\build_win32\debug\libdb44sd.lib (
-   vcbuild db-4.4.20\build_win32\db_static.vcproj "Debug|Win32"
+if not exist tcltk\bin\tcl84g.dll (
+    cd tcl-8.4.18.2\win
+    nmake -f makefile.vc COMPILERFLAGS=-DWINVER=0x0500 DEBUG=1 INSTALLDIR=..\..\tcltk clean all install
+    cd ..\..
 )
 
-@rem OpenSSL
-if not exist openssl-0.9.8g (
-  if exist openssl-0.9.8a rd /s/q openssl-0.9.8a
-  svn export http://svn.python.org/projects/external/openssl-0.9.8g
+if not exist tcltk\bin\tk84g.dll (
+    cd tk-8.4.18.1\win    
+    nmake -f makefile.vc COMPILERFLAGS=-DWINVER=0x0500 DEBUG=1 INSTALLDIR=..\..\tcltk TCLDIR=..\..\tcl-8.4.18.2 clean all install
+    cd ..\..
 )
-
-@rem tcltk
-if not exist tcl8.4.16 (
-   if exist tcltk rd /s/q tcltk
-   if exist tcl8.4.12 rd /s/q tcl8.4.12
-   if exist tk8.4.12 rd /s/q tk8.4.12
-   svn export http://svn.python.org/projects/external/tcl8.4.16
-   svn export http://svn.python.org/projects/external/tk8.4.16
-   cd tcl8.4.16\win
-   nmake -f makefile.vc COMPILERFLAGS=-DWINVER=0x0500
-   nmake -f makefile.vc COMPILERFLAGS=-DWINVER=0x0500 INSTALLDIR=..\..\tcltk install
-   cd ..\..
-   cd tk8.4.16\win
-   nmake -f makefile.vc COMPILERFLAGS=-DWINVER=0x0500 TCLDIR=..\..\tcl8.4.16
-   nmake -f makefile.vc COMPILERFLAGS=-DWINVER=0x0500 TCLDIR=..\..\tcl8.4.16 INSTALLDIR=..\..\tcltk install
-   cd ..\..
-)
-
-@rem sqlite
-if not exist sqlite-source-3.3.4 svn export http://svn.python.org/projects/external/sqlite-source-3.3.4
-if not exist build\PCbuild\sqlite3.dll copy sqlite-source-3.3.4\sqlite3.dll build\PCbuild
