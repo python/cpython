3de10d54e99ada6ad6d1db22580e9adc1b7f5cb3
Trent Nelson <trent.nelson@snakebite.org>
2008-03-19 21:11:55 +0000

    Fix the x64 Windows build environment used by the buildbots.  %VS90COMNTOOLS%\vsvars32.bat is fine for 32-bit builds, but doesn't work for x64 builds, regardless of /MACHINE:AMD64 and /USECL:MS_OPTERON flags passed to cl.exe.  Launch the x86_64 cross compilation environment via '%VS90COMNTOOLS%\..\..\VC\vcvarsall.bat x86_amd64'.  I don't have access to any systems *without* Visual Studio 2008 Professional installed (i.e. just Express Edition), so I can't test if x64 compilation works w/ VS Express at the moment.
    Additionally, force a clean in our build.bat files before building, just whilst we're going through these build system updates.
    
    And finally, add in the missing MACHINE=AMD64 option to our Tcl/Tk x64 build.

--- Tools/buildbot/build-amd64.bat
+++ Tools/buildbot/build-amd64.bat
@@ -1,5 +1,6 @@
 @rem Used by the buildbot "compile" step.
 cmd /c Tools\buildbot\external-amd64.bat
-call "%VS90COMNTOOLS%vsvars32.bat"
+call "%VS90COMNTOOLS%\..\..\VC\vcvarsall.bat" x86_amd64
 REM cmd /q/c Tools\buildbot\kill_python.bat
+cmd /c Tools\buildbot\clean-amd64.bat
 vcbuild PCbuild\pcbuild.sln "Debug|x64"
--- Tools/buildbot/external-common.bat
+++ Tools/buildbot/external-common.bat
@@ -2,19 +2,20 @@
 @rem fetching external components into the root\.. buildbot directories.
 
 cd ..
-@rem XXX: Force the buildbots to start from a fresh environment.  Will be removed.
-if exist bzip2-1.0.3 rd /s/q bzip2-1.0.3
-if exist tcltk rd /s/q tcltk
-if exist tcltk64 rd /s/q tcltk64
-if exist tcl8.4.12 rd /s/q tcl8.4.12
-if exist tcl8.4.16 rd /s/q tcl8.4.16
-if exist tcl-8.4.18.1 rd /s/q tcl-8.4.18.1
-if exist tk8.4.12 rd /s/q tk8.4.12
-if exist tk8.4.16 rd /s/q tk8.4.16
-if exist tk-8.4.18.1 rd /s/q tk-8.4.18.1
-if exist db-4.4.20 rd /s/q db-4.4.20
-if exist openssl-0.9.8g rd /s/q openssl-0.9.8g
-if exist sqlite-source-3.3.4 rd /s/q sqlite-source-3.3.4    
+@rem XXX: If you need to force the buildbots to start from a fresh environment, uncomment
+@rem the following, check it in, then check it out, comment it out, then check it back in.
+@rem if exist bzip2-1.0.3 rd /s/q bzip2-1.0.3
+@rem if exist tcltk rd /s/q tcltk
+@rem if exist tcltk64 rd /s/q tcltk64
+@rem if exist tcl8.4.12 rd /s/q tcl8.4.12
+@rem if exist tcl8.4.16 rd /s/q tcl8.4.16
+@rem if exist tcl-8.4.18.1 rd /s/q tcl-8.4.18.1
+@rem if exist tk8.4.12 rd /s/q tk8.4.12
+@rem if exist tk8.4.16 rd /s/q tk8.4.16
+@rem if exist tk-8.4.18.1 rd /s/q tk-8.4.18.1
+@rem if exist db-4.4.20 rd /s/q db-4.4.20
+@rem if exist openssl-0.9.8g rd /s/q openssl-0.9.8g
+@rem if exist sqlite-source-3.3.4 rd /s/q sqlite-source-3.3.4    
 
 @rem bzip
 if not exist bzip2-1.0.3 svn export http://svn.python.org/projects/external/bzip2-1.0.3
