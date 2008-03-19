611c86b7d18b2d2f9e188d656f27c8dffee235ed
Trent Nelson <trent.nelson@snakebite.org>
2008-03-19 21:11:55 +0000

    Fix the x64 Windows build environment used by the buildbots.  %VS90COMNTOOLS%\vsvars32.bat is fine for 32-bit builds, but doesn't work for x64 builds, regardless of /MACHINE:AMD64 and /USECL:MS_OPTERON flags passed to cl.exe.  Launch the x86_64 cross compilation environment via '%VS90COMNTOOLS%\..\..\VC\vcvarsall.bat x86_amd64'.  I don't have access to any systems *without* Visual Studio 2008 Professional installed (i.e. just Express Edition), so I can't test if x64 compilation works w/ VS Express at the moment.
    Additionally, force a clean in our build.bat files before building, just whilst we're going through these build system updates.
    
    And finally, add in the missing MACHINE=AMD64 option to our Tcl/Tk x64 build.

--- Tools/buildbot/external-amd64.bat
+++ Tools/buildbot/external-amd64.bat
@@ -2,16 +2,16 @@
 
 @rem Assume we start inside the Python source directory
 call "Tools\buildbot\external-common.bat"
-call "%VS90COMNTOOLS%\vsvars32.bat"
+call "%VS90COMNTOOLS%\..\..\VC\vcvarsall.bat" x86_amd64
 
 if not exist tcltk64\bin\tcl84g.dll (
     cd tcl-8.4.18.2\win
-    nmake -f makefile.vc COMPILERFLAGS=-DWINVER=0x0500 DEBUG=1 INSTALLDIR=..\..\tcltk64 clean all install
+    nmake -f makefile.vc COMPILERFLAGS=-DWINVER=0x0500 DEBUG=1 MACHINE=AMD64 INSTALLDIR=..\..\tcltk64 clean all install
     cd ..\..
 )
 
 if not exist tcltk64\bin\tk84g.dll (
     cd tk-8.4.18.1\win    
-    nmake -f makefile.vc COMPILERFLAGS=-DWINVER=0x0500 DEBUG=1 INSTALLDIR=..\..\tcltk64 TCLDIR=..\..\tcl-8.4.18.2 clean all install
+    nmake -f makefile.vc COMPILERFLAGS=-DWINVER=0x0500 DEBUG=1 MACHINE=AMD64 INSTALLDIR=..\..\tcltk64 TCLDIR=..\..\tcl-8.4.18.2 clean all install
     cd ..\..
 )
