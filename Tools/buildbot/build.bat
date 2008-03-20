6666ad3e289ada01fe9c4aa4886f0b57b124f9da
Trent Nelson <trent.nelson@snakebite.org>
2008-03-20 01:18:35 +0000

    Merged revisions 61639 via svnmerge from svn+ssh://pythondev@svn.python.org/python/trunk
    ........
      r61639 | trent.nelson | 2008-03-19 16:11:55 -0500 (Wed, 19 Mar 2008) | 6 lines
    
      Fix the x64 Windows build environment used by the buildbots.  %VS90COMNTOOLS%\vsvars32.bat is fine for 32-bit builds, but doesn't work for x64 builds, regardless of /MACHINE:AMD64 and /USECL:MS_OPTERON flags passed to cl.exe.  Launch the x86_64 cross compilation environment via '%VS90COMNTOOLS%\..\..\VC\vcvarsall.bat x86_amd64'.  I don't have access to any systems *without* Visual Studio 2008 Professional installed (i.e. just Express Edition), so I can't test if x64 compilation works w/ VS Express at the moment.
    
      Additionally, force a clean in our build.bat files before building, just whilst we're going through these build system updates.
    
      And finally, add in the missing MACHINE=AMD64 option to our Tcl/Tk x64 build.
    ........

--- Tools/buildbot/build.bat
+++ Tools/buildbot/build.bat
@@ -2,5 +2,6 @@
 cmd /c Tools\buildbot\external.bat
 call "%VS90COMNTOOLS%vsvars32.bat"
 cmd /q/c Tools\buildbot\kill_python.bat
+cmd /c Tools\buildbot\clean.bat
 vcbuild /useenv PCbuild\pcbuild.sln "Debug|Win32"
 
