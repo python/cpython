db59084710d258e6cec250996fe8223eea3578ee
Mark Dickinson <dickinsm@gmail.com>
2008-03-25 14:33:23 +0000

    Issue #2478: Decimal(sqrt(0)) failed when the decimal context was not explicitly supplied.

--- Lib/decimal.py
+++ Lib/decimal.py
@@ -2453,6 +2453,9 @@ class Decimal(object):
 
     def sqrt(self, context=None):
         """Return the square root of self."""
+        if context is None:
+            context = getcontext()
+
         if self._is_special:
             ans = self._check_nans(context=context)
             if ans:
@@ -2465,9 +2468,6 @@ class Decimal(object):
             # exponent = self._exp // 2.  sqrt(-0) = -0
             ans = _dec_from_triple(self._sign, '0', self._exp // 2)
             return ans._fix(context)
-
-        if context is None:
-            context = getcontext()
 
         if self._sign == 1:
             return context._raise_error(InvalidOperation, 'sqrt(-x), x > 0')
