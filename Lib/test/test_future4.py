62627d0e0b8718e0e52a54150f4ba250f6907c30
Christian Heimes <christian@cheimes.de>
2008-03-26 22:55:31 +0000

    I forgot to svn add the future test

---
+++ Lib/test/test_future4.py
@@ -0,0 +1,24 @@
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import unittest
+from test import test_support
+
+class TestFuture(unittest.TestCase):
+    def assertType(self, obj, typ):
+        self.assert_(type(obj) is typ,
+            "type(%r) is %r, not %r" % (obj, type(obj), typ))
+
+    def test_unicode_strings(self):
+        self.assertType("", unicode)
+        self.assertType(r"", unicode)
+        self.assertType(u"", unicode)
+        self.assertType(ur"", unicode)
+        self.assertType(b"", str)
+        self.assertType(br"", str)
+
+def test_main():
+    test_support.run_unittest(TestFuture)
+
+if __name__ == "__main__":
+    test_main()
