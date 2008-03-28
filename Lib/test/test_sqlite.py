03c8f1177d437635b288d345ad523a2e6489cf53
Gregory P. Smith <greg@mad-scientist.com>
2008-03-28 08:32:09 +0000

    Accept patch issue2426 by Paul Kippes (kippesp).
    Adds sqlite3.Connection.iterdump to allow dumping of databases.

--- Lib/test/test_sqlite.py
+++ Lib/test/test_sqlite.py
@@ -5,12 +5,13 @@ try:
 except ImportError:
     raise TestSkipped('no sqlite available')
 from sqlite3.test import (dbapi, types, userfunctions, py25tests,
-                                factory, transactions, hooks, regression)
+                                factory, transactions, hooks, regression,
+                                dump)
 
 def test_main():
     run_unittest(dbapi.suite(), types.suite(), userfunctions.suite(),
                  py25tests.suite(), factory.suite(), transactions.suite(),
-                 hooks.suite(), regression.suite())
+                 hooks.suite(), regression.suite(), dump.suite())
 
 if __name__ == "__main__":
     test_main()
