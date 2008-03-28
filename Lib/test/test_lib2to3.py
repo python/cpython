a365c9dffc04d249dca59c9484c5f7a5b99866b5
Martin v. Löwis <martin@v.loewis.de>
2008-03-28 05:29:57 +0000

    Merged revisions 61988-61990 via svnmerge from svn+ssh://pythondev@svn.python.org/python/trunk
    ................
      r61988 | martin.v.loewis | 2008-03-28 06:25:36 +0100 (Fr, 28 Mär 2008) | 2 lines
    
      Disable test that depends on #2412 being fixed.
    ................
      r61989 | martin.v.loewis | 2008-03-28 06:26:10 +0100 (Fr, 28 Mär 2008) | 2 lines
    
      Run 2to3 tests.
    ................
      r61990 | martin.v.loewis | 2008-03-28 06:27:44 +0100 (Fr, 28 Mär 2008) | 13 lines
    
      Merged revisions 61825-61989 via svnmerge from
      svn+ssh://pythondev@svn.python.org/sandbox/trunk/2to3/lib2to3
    
      ........
        r61899 | collin.winter | 2008-03-25 17:53:41 +0100 (Di, 25 Mär 2008) | 1 line
    
        Add a missing explicit fixer to test_all_fixers.
      ........
        r61983 | collin.winter | 2008-03-28 03:19:46 +0100 (Fr, 28 Mär 2008) | 2 lines
    
        Fix http://bugs.python.org/issue2453: support empty excepts in fix_except.
      ........
    ................

---
+++ Lib/test/test_lib2to3.py
@@ -0,0 +1,15 @@
+# Skipping test_parser and test_all_fixers
+# because of running
+from lib2to3.tests import test_fixers, test_pytree, test_util
+import unittest
+from test.test_support import run_unittest
+
+def suite():
+    tests = unittest.TestSuite()
+    loader = unittest.TestLoader()
+    for m in (test_fixers,test_pytree,test_util):
+        tests.addTests(loader.loadTestsFromModule(m))
+    return tests
+
+def test_main():
+    run_unittest(suite())
