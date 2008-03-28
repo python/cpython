69b747b7358780c84d1cac1317bfb7d5baa4e6e3
Amaury Forgeot d'Arc <amauryfa@gmail.com>
2008-03-28 20:30:50 +0000

    Fix a reference leak found by Georg, when compiling a class nested in another class. Now "regrtest.py -R:: test_compile" is satisfied.
    Will backport.

--- Python/compile.c
+++ Python/compile.c
@@ -1431,6 +1431,7 @@ compiler_class(struct compiler *c, stmt_ty s)
 	if (!compiler_enter_scope(c, s->v.ClassDef.name, (void *)s,
 				  s->lineno))
 		return 0;
+	Py_XDECREF(c->u->u_private);
 	c->u->u_private = s->v.ClassDef.name;
 	Py_INCREF(c->u->u_private);
 	str = PyString_InternFromString("__name__");
