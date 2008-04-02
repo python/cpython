c321b40a52246ede73d6ec24b6024b03ad0e6ead
Barry Warsaw <barry@python.org>
2008-04-02 23:33:27 +0000

    release.py induced and manual editing steps for 3.0a4.

--- Lib/distutils/__init__.py
+++ Lib/distutils/__init__.py
@@ -18,4 +18,7 @@ __revision__ = "$Id$"
 # In general, major and minor version should loosely follow the Python
 # version number the distutils code was shipped with.
 #
-__version__ = "2.6.0"
+
+#--start constants--
+__version__ = "3.0a4"
+#--end constants--
