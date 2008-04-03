d6dffbcc285ff3eb30eb30ad27616bbafceb1ce0
Trent Nelson <trent.nelson@snakebite.org>
2008-04-03 18:27:06 +0000

    Reimplement kill_python.  The existing version had a number of flaws, namely, it didn't work for x64 and it wasn't precise about which python_d.exe it was killing -- it just killed the first one it came across that happened to have 'pcbuild\python_d.exe' or 'build\python_d.exe' in it's path.  The new version has been rewritten from the ground up and now lives in PCbuild, instead of Tools\buildbot, and it has also been incorporated into the Visual Studio solution (pcbuild.sln) as 'kill_python'.  The solution has also been altered such that kill_python is called where necessary in the build process in order to prevent any linking errors due to open file locks.  In lieu of this, all of the existing bits and pieces in Tools\buildbot that called out to kill_python at various points have also been removed as they are now obsolete.  Tested on both Win32 and x64.
    Change set (included to improve usefulness of svnmerge log entry):
    M      PCbuild\pythoncore.vcproj
    M      PCbuild\pcbuild.sln
    M      PCbuild\release.vsprops
    A      PCbuild\kill_python.vcproj
    M      PCbuild\debug.vsprops
    A      PCbuild\kill_python.c
    D      Tools\buildbot\kill_python.bat
    D      Tools\buildbot\kill_python.mak
    M      Tools\buildbot\build.bat
    D      Tools\buildbot\Makefile
    M      Tools\buildbot\build-amd64.bat
    M      Tools\buildbot\buildmsi.bat
    D      Tools\buildbot\kill_python.c

--- Tools/buildbot/build.bat
+++ Tools/buildbot/build.bat
@@ -1,7 +1,6 @@
 @rem Used by the buildbot "compile" step.
 cmd /c Tools\buildbot\external.bat
 call "%VS90COMNTOOLS%vsvars32.bat"
-cmd /q/c Tools\buildbot\kill_python.bat
 cmd /c Tools\buildbot\clean.bat
 vcbuild /useenv PCbuild\pcbuild.sln "Debug|Win32"
 
