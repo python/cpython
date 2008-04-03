bcf18c32dd33eb3a105ed3f88d8acdfaf89c0a2b
Barry Warsaw <barry@python.org>
2008-04-03 04:13:12 +0000

    Post tag version tweak

--- Include/patchlevel.h
+++ Include/patchlevel.h
@@ -23,7 +23,7 @@
 #define PY_RELEASE_SERIAL	4
 
 /* Version as a string */
-#define PY_VERSION      	"3.0a4"
+#define PY_VERSION      	"3.0a4+"
 /*--end constants--*/
 
 /* Subversion Revision number of this file (not of the repository) */
