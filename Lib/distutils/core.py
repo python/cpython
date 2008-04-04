fe7056240b081ccf51ff08b3a15afbfad52a87ad
Fred Drake <fdrake@acm.org>
2008-04-04 11:31:14 +0000

    my previous change did what I said it should not: it changed the current directory to the directory in which the setup.py script lived (which made __file__ wrong)
    fixed, with test that the script is run in the current directory of the caller

--- Lib/distutils/core.py
+++ Lib/distutils/core.py
@@ -212,7 +212,6 @@ def run_setup (script_name, script_args=None, stop_after="run"):
     save_argv = sys.argv
     g = {'__file__': script_name}
     l = {}
-    os.chdir(os.path.dirname(script_name) or os.curdir)
     try:
         try:
             sys.argv[0] = script_name
