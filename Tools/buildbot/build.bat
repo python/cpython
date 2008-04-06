a04b119e55f6319b57236b4fe8bb67cab1015341
Trent Nelson <trent.nelson@snakebite.org>
2008-04-06 20:51:23 +0000

    Don't run kill_python as part of the build process.  Change the buildbots so they have to call it explicitly instead.

--- Tools/buildbot/build.bat
+++ Tools/buildbot/build.bat
@@ -2,5 +2,6 @@
 cmd /c Tools\buildbot\external.bat
 call "%VS90COMNTOOLS%vsvars32.bat"
 cmd /c Tools\buildbot\clean.bat
+vcbuild /useenv PCbuild\kill_python.vcproj "Debug|Win32" && PCbuild\kill_python_d.exe
 vcbuild /useenv PCbuild\pcbuild.sln "Debug|Win32"
 
