22bbef9cfe4fa372741e203f70dc56ab2d3404f2
Gregory P. Smith <greg@mad-scientist.com>
2008-04-10 19:54:13 +0000

    backport r62271 from trunk. remove the now obsoleted assertion.

--- Objects/stringobject.c
+++ Objects/stringobject.c
@@ -53,7 +53,6 @@ PyObject *
 PyString_FromStringAndSize(const char *str, Py_ssize_t size)
 {
 	register PyStringObject *op;
-	assert(size >= 0);
 	if (size < 0) {
 		PyErr_SetString(PyExc_SystemError,
 		    "Negative size passed to PyString_FromStringAndSize");
