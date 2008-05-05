c3ad68c88a5c7eab2c70fa3a58aab547f60f61e0
Martin v. Löwis <martin@v.loewis.de>
2008-05-05 17:47:06 +0000

    Fix Unicode filename test.

--- Lib/test/test_zipfile.py
+++ Lib/test/test_zipfile.py
@@ -556,11 +556,13 @@ class OtherTests(unittest.TestCase):
     def testUnicodeFilenames(self):
         zf = zipfile.ZipFile(TESTFN, "w")
         zf.writestr(u"foo.txt", "Test for unicode filename")
-        zf.writestr(u"fo\xf6.txt", "Test for unicode filename")
-        assert isinstance(zf.infolist()[0].filename, unicode)
+        zf.writestr(u"\xf6.txt", "Test for unicode filename")
+        self.assertTrue(isinstance(zf.infolist()[0].filename, unicode))
         zf.close()
-        zf = zipfile.ZipFile(TESTFN, "w")
-
+        zf = zipfile.ZipFile(TESTFN, "r")
+        self.assertEqual(zf.filelist[0].filename, "foo.txt")
+        self.assertEqual(zf.filelist[1].filename, u"\xf6.txt")
+        zf.close()
 
     def testCreateNonExistentFileForAppend(self):
         if os.path.exists(TESTFN):
