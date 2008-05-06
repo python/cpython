8e4997390ccb92f889ac709c9a45e589472bdee2
Gregory P. Smith <greg@mad-scientist.com>
2008-05-06 07:05:18 +0000

    fix issue2707 - os.walk docstring example correctness typo.

--- Lib/os.py
+++ Lib/os.py
@@ -267,8 +267,9 @@ def walk(top, topdown=True, onerror=None, followlinks=False):
 
     Example:
 
+    import os
     from os.path import join, getsize
-    for root, dirs, files in walk('python/Lib/email'):
+    for root, dirs, files in os.walk('python/Lib/email'):
         print root, "consumes",
         print sum([getsize(join(root, name)) for name in files]),
         print "bytes in", len(files), "non-directory files"
