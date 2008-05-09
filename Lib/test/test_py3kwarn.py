d7265d64831f870f46b7c61244b2c86dc803f292
Brett Cannon <bcannon@gmail.com>
2008-05-09 05:18:40 +0000

    Deprecate the new module for removal in 3.0.

--- Lib/test/test_py3kwarn.py
+++ Lib/test/test_py3kwarn.py
@@ -126,7 +126,7 @@ class TestPy3KWarnings(unittest.TestCase):
 
 class TestStdlibRemovals(unittest.TestCase):
 
-    all_platforms = ('audiodev', 'imputil', 'mutex', 'user')
+    all_platforms = ('audiodev', 'imputil', 'mutex', 'user', 'new')
 
     def check_removal(self, module_name):
         """Make sure the specified module, when imported, raises a
