025ed733e538de19c06bbe5a5f07e690c4662a70
Georg Brandl <georg@python.org>
2008-05-11 20:53:55 +0000

    #2809 followup: even better split docstring.

--- Objects/stringobject.c
+++ Objects/stringobject.c
@@ -1502,8 +1502,8 @@ PyDoc_STRVAR(split__doc__,
 Return a list of the words in the string S, using sep as the\n\
 delimiter string.  If maxsplit is given, at most maxsplit\n\
 splits are done. If sep is not specified or is None, any\n\
-whitespace string is a separator and leading and trailing\n\
-whitespace is stripped before splitting.");
+whitespace string is a separator and empty strings are removed\n\
+from the result.");
 
 static PyObject *
 string_split(PyStringObject *self, PyObject *args)
