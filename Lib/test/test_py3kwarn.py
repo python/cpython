ddf949f19432ef22b7b42a7054412f83985ff77f
Brett Cannon <bcannon@gmail.com>
2008-05-14 20:31:38 +0000

    The CD and cd modules for IRIX are deprecated for 3.0.

--- Lib/test/test_py3kwarn.py
+++ Lib/test/test_py3kwarn.py
@@ -131,7 +131,7 @@ class TestStdlibRemovals(unittest.TestCase):
     all_platforms = ('audiodev', 'imputil', 'mutex', 'user', 'new', 'rexec',
                         'Bastion', 'compiler', 'dircache', 'fpformat',
                         'ihooks', 'mhlib')
-    inclusive_platforms = {'irix' : ('pure', 'AL', 'al'),
+    inclusive_platforms = {'irix' : ('pure', 'AL', 'al', 'CD', 'cd'),
                           'darwin' : ('autoGIL', 'Carbon', 'OSATerminology',
                                       'icglue', 'Nav', 'MacOS', 'aepack',
                                       'aetools', 'aetypes', 'applesingle',
--- Modules/cdmodule.c
+++ Modules/cdmodule.c
@@ -758,6 +758,10 @@ void
 initcd(void)
 {
 	PyObject *m, *d;
+	
+	if (PyErr_WarnPy3k("the cd module has been removed in "
+	                   "Python 3.0", 2) < 0)
+	    return;
 
 	m = Py_InitModule("cd", CD_methods);
 	if (m == NULL)
