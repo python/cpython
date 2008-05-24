3d49010375ca66f43701883f578f41d20198f6fb
Georg Brandl <georg@python.org>
2008-05-24 18:08:33 +0000

    socketserver renaming reversal part 1: remove stub module.

--- Lib/lib-old/SocketServer.py
+++
@@ -1,8 +0,0 @@
-import sys
-from warnings import warnpy3k
-
-warnpy3k("the SocketServer module has been renamed "
-         "to 'socketserver' in Python 3.0", stacklevel=2)
-
-import socketserver
-sys.modules[__name__] = socketserver
