c8214253f25b3263f57fc5e7f85de931207dcd61
Georg Brandl <georg@python.org>
2008-05-25 07:08:35 +0000

    Queue renaming reversal part 1: remove stub module.

--- Lib/lib-old/Queue.py
+++
@@ -1,8 +0,0 @@
-import sys
-from warnings import warnpy3k
-
-warnpy3k("the Queue module has been renamed "
-         "to 'queue' in Python 3.0", stacklevel=2)
-
-import queue
-sys.modules[__name__] = queue
