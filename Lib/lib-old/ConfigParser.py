9a1aa8c9b29c4a9855055047424564f49e108ad2
Georg Brandl <georg@python.org>
2008-05-25 07:21:41 +0000

    ConfigParser renaming reversal part 1: remove stub module.

--- Lib/lib-old/ConfigParser.py
+++
@@ -1,8 +0,0 @@
-import sys
-from warnings import warnpy3k
-
-warnpy3k("the ConfigParser module has been renamed "
-         "to 'configparser' in Python 3.0", stacklevel=2)
-
-import configparser
-sys.modules[__name__] = configparser
