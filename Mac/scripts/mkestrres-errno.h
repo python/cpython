94c84967977ccb0d28b1682c779f70e33f1d03b9
Benjamin Peterson <benjamin@python.org>
2008-06-01 13:34:59 +0000

    remove old mac scripts including bgen

--- Mac/scripts/mkestrres-errno.h
+++
@@ -1,7 +0,0 @@
-/* These are defined in MSL errno.h, but unfortunately not documented */
-#define EFPOS		35		/* File positioning error */
-#define	ESIGPARM	36		/* Signal argument error */
-#define	ENOMEM		37		/* Cannot allocate memory */
-#define	EACCES		38		/* Permission denied */
-#define	ENOENT		39		/* No such file or directory */
-#define	ENOSYS		40		/* Function not implemented */
