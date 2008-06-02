137d824148f9322c741b16d90ada72fc558b9a1c
Gregory P. Smith <greg@mad-scientist.com>
2008-06-02 04:05:52 +0000

    Fix issue 2782: be less strict about the format string type in strftime. Accept unicode and anything else ParseTuple "s#" can deal with.  This matches the time.strftime behavior.

--- Lib/test/test_datetime.py
+++ Lib/test/test_datetime.py
@@ -850,8 +850,12 @@ class TestDate(HarmlessMixedComparison, unittest.TestCase):
         self.assertRaises(TypeError, t.strftime, "one", "two") # too many args
         self.assertRaises(TypeError, t.strftime, 42) # arg wrong type
 
+        # test that unicode input is allowed (issue 2782)
+        self.assertEqual(t.strftime(u"%m"), "03")
+
         # A naive object replaces %z and %Z w/ empty strings.
         self.assertEqual(t.strftime("'%z' '%Z'"), "'' ''")
+
 
     def test_format(self):
         dt = self.theclass(2007, 9, 10)
