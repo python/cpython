320675b4d7a2aeeeafbaa60385149f6964c7f52d
Georg Brandl <georg@python.org>
2008-06-04 11:30:26 +0000

    Fix misspelling.

--- Objects/bytearrayobject.c
+++ Objects/bytearrayobject.c
@@ -947,7 +947,7 @@ bytes_richcompare(PyObject *self, PyObject *other, int op)
         PyObject_IsInstance(other, (PyObject*)&PyUnicode_Type)) {
         if (Py_BytesWarningFlag && op == Py_EQ) {
             if (PyErr_WarnEx(PyExc_BytesWarning,
-                            "Comparsion between bytearray and string", 1))
+                            "Comparison between bytearray and string", 1))
                 return NULL;
         }
 
