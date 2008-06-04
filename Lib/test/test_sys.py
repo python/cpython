29982421131fc6cf99606d890bdb17f6e276d236
Georg Brandl <georg@python.org>
2008-06-04 14:03:46 +0000

    Remove tabcheck from the expected flags in sys.flags.

--- Lib/test/test_sys.py
+++ Lib/test/test_sys.py
@@ -324,7 +324,7 @@ class SysModuleTest(unittest.TestCase):
         self.failUnless(sys.flags)
         attrs = ("debug", "division_warning",
                  "inspect", "interactive", "optimize", "dont_write_bytecode",
-                 "no_site", "ignore_environment", "tabcheck", "verbose",
+                 "no_site", "ignore_environment", "verbose",
                  "bytes_warning")
         for attr in attrs:
             self.assert_(hasattr(sys.flags, attr), attr)
