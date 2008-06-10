d53e9ccdbbb8b5ca3df003ff9e1adb98a9863634
Benjamin Peterson <benjamin@python.org>
2008-06-10 22:39:25 +0000

    backport of 64096

--- Lib/test/test_platform.py
+++ Lib/test/test_platform.py
@@ -72,7 +72,7 @@ class PlatformTest(unittest.TestCase):
         else:
             have_toolbox_glue = True
 
-        if have_toolbox_glue and os.uname()[0] == 'Darwin':
+        if have_toolbox_glue and platform.uname()[0] == 'Darwin':
             # We're on a MacOSX system, check that
             # the right version information is returned
             fd = os.popen('sw_vers', 'r')
