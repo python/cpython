953698294b2c1f3f5e11dead494a233aedcd12a4
Amaury Forgeot d'Arc <amauryfa@gmail.com>
2008-06-12 23:03:41 +0000

    Slowly apply part of #2065: py3k can be compiled with VS8.0

--- PC/pyconfig.h
+++ PC/pyconfig.h
@@ -158,11 +158,11 @@ WIN32 is still required for the locale module.
 /* set the version macros for the windows headers */
 #ifdef MS_WINX64
 /* 64 bit only runs on XP or greater */
-#define Py_WINVER _WIN32_WINNT_WINXP
+#define Py_WINVER 0x0501 /* _WIN32_WINNT_WINXP */
 #define Py_NTDDI NTDDI_WINXP
 #else
 /* Python 2.6+ requires Windows 2000 or greater */
-#define Py_WINVER _WIN32_WINNT_WIN2K
+#define Py_WINVER 0x0500 /* _WIN32_WINNT_WIN2K */
 #define Py_NTDDI NTDDI_WIN2KSP4
 #endif
 
