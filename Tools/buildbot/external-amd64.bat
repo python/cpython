bd6e1249cde346158be477df3325384098317c84
Amaury Forgeot d'Arc <amauryfa@gmail.com>
2008-06-18 21:33:58 +0000

    Typo in a directory name. Should help the AMD64 buildbots

--- Tools/buildbot/external-amd64.bat
+++ Tools/buildbot/external-amd64.bat
@@ -12,7 +12,7 @@ if not exist tcltk64\bin\tcl85g.dll (
 )
 
 if not exist tcltk64\bin\tk85g.dll (
-    cd tk-8.5.2.1\win    
+    cd tk-8.5.2.0\win    
     nmake -f makefile.vc COMPILERFLAGS=-DWINVER=0x0500 OPTS=noxp DEBUG=1 MACHINE=AMD64 INSTALLDIR=..\..\tcltk64 TCLDIR=..\..\tcl-8.5.2.1 clean
     nmake -f makefile.vc COMPILERFLAGS=-DWINVER=0x0500 OPTS=noxp DEBUG=1 MACHINE=AMD64 INSTALLDIR=..\..\tcltk64 TCLDIR=..\..\tcl-8.5.2.1 all
     nmake -f makefile.vc COMPILERFLAGS=-DWINVER=0x0500 OPTS=noxp DEBUG=1 MACHINE=AMD64 INSTALLDIR=..\..\tcltk64 TCLDIR=..\..\tcl-8.5.2.1 install
