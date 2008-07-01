bdd941fac3631e0c971d6c1a4e9f9b39a787b14e
Amaury Forgeot d'Arc <amauryfa@gmail.com>
2008-07-01 20:38:04 +0000

    #3242: fix a crash in "print", if sys.stdout is set to a custom object, whose write() method installs another sys.stdout.
    Will backport.

--- Lib/test/test_file.py
+++ Lib/test/test_file.py
@@ -503,13 +503,31 @@ class FileThreadingTests(unittest.TestCase):
         self._test_close_open_io(io_func)
 
 
+class StdoutTests(unittest.TestCase):
+
+    def test_move_stdout_on_write(self):
+        # Issue 3242: sys.stdout can be replaced (and freed) during a
+        # print statement; prevent a segfault in this case
+        save_stdout = sys.stdout
+
+        class File:
+            def write(self, data):
+                if '\n' in data:
+                    sys.stdout = save_stdout
+
+        try:
+            sys.stdout = File()
+            print "some text"
+        finally:
+            sys.stdout = save_stdout
+
 
 def test_main():
     # Historically, these tests have been sloppy about removing TESTFN.
     # So get rid of it no matter what.
     try:
         run_unittest(AutoFileTests, OtherFileTests, FileSubclassTests,
-            FileThreadingTests)
+            FileThreadingTests, StdoutTests)
     finally:
         if os.path.exists(TESTFN):
             os.unlink(TESTFN)
