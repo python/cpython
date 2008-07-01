b15bdf7f7a736f49f783ab75904e6b2438449743
Amaury Forgeot d'Arc <amauryfa@gmail.com>
2008-07-01 20:59:49 +0000

    #Issue3088 in-progress: Race condition with instances of classes derived from threading.local:
    When a thread touches such an object for the first time, a new thread-local __dict__ is created,
    and the __init__ method is run.
    But a thread switch can occur here; if the other thread touches the same object, it installs another
    __dict__; when the first thread resumes, it updates the dictionary of the second...
    
    This is the deep cause of the failures in test_multiprocessing involving "managers" objects.
    
    Backport of r64601.

--- Lib/test/test_threading_local.py
+++ Lib/test/test_threading_local.py
@@ -2,6 +2,34 @@ import unittest
 from doctest import DocTestSuite
 from test import test_support
 
+class ThreadingLocalTest(unittest.TestCase):
+    def test_derived(self):
+        # Issue 3088: if there is a threads switch inside the __init__
+        # of a threading.local derived class, the per-thread dictionary
+        # is created but not correctly set on the object.
+        # The first member set may be bogus.
+        import threading
+        import time
+        class Local(threading.local):
+            def __init__(self):
+                time.sleep(0.01)
+        local = Local()
+
+        def f(i):
+            local.x = i
+            # Simply check that the variable is correctly set
+            self.assertEqual(local.x, i)
+
+        threads= []
+        for i in range(10):
+            t = threading.Thread(target=f, args=(i,))
+            t.start()
+            threads.append(t)
+
+        for t in threads:
+            t.join()
+
+
 def test_main():
     suite = DocTestSuite('_threading_local')
 
@@ -19,6 +47,7 @@ def test_main():
         suite.addTest(DocTestSuite('_threading_local',
                                    setUp=setUp, tearDown=tearDown)
                       )
+        suite.addTest(unittest.makeSuite(ThreadingLocalTest))
 
     test_support.run_suite(suite)
 
