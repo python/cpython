d4c7e636a98323c1d88ab6936318d4052117dc10
Georg Brandl <georg@python.org>
2008-07-23 15:17:09 +0000

    Move opcode handling to Python's extension.

--- Doc/tools/sphinxext/pyspecific.py
+++ Doc/tools/sphinxext/pyspecific.py
@@ -85,6 +85,28 @@ class PydocTopicsBuilder(Builder):
             f.close()
 
 
+# Support for documenting Opcodes
+
+import re
+from sphinx import addnodes
+
+opcode_sig_re = re.compile(r'(\w+(?:\+\d)?)\s*\((.*)\)')
+
+def parse_opcode_signature(env, sig, signode):
+    """Transform an opcode signature into RST nodes."""
+    m = opcode_sig_re.match(sig)
+    if m is None:
+        raise ValueError
+    opname, arglist = m.groups()
+    signode += addnodes.desc_name(opname, opname)
+    paramlist = addnodes.desc_parameterlist()
+    signode += paramlist
+    paramlist += addnodes.desc_parameter(arglist, arglist)
+    return opname.strip()
+
+
 def setup(app):
     app.add_role('issue', issue_role)
     app.add_builder(PydocTopicsBuilder)
+    app.add_description_unit('opcode', 'opcode', '%s (opcode)',
+                             parse_opcode_signature)
