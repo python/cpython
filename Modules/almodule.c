886bdbc56951ed674de731a5dbc1ef7657fc5316
Neal Norwitz <nnorwitz@gmail.com>
2008-07-28 05:06:20 +0000

    Backport code from r65182:
    Issue #2620: Overflow checking when allocating or reallocating memory
    was not always being done properly in some python types and extension
    modules.  PyMem_MALLOC, PyMem_REALLOC, PyMem_NEW and PyMem_RESIZE have
    all been updated to perform better checks and places in the code that
    would previously leak memory on the error path when such an allocation
    failed have been fixed.

--- Modules/almodule.c
+++ Modules/almodule.c
@@ -1633,9 +1633,11 @@ al_QueryValues(PyObject *self, PyObject *args)
 	if (nvals < 0)
 		goto cleanup;
 	if (nvals > setsize) {
+		ALvalue *old_return_set = return_set;
 		setsize = nvals;
 		PyMem_RESIZE(return_set, ALvalue, setsize);
 		if (return_set == NULL) {
+			return_set = old_return_set;
 			PyErr_NoMemory();
 			goto cleanup;
 		}
