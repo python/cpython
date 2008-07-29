fbf66bd61705624f8c5b4b68061eeb9e5296a871
Benjamin Peterson <benjamin@python.org>
2008-07-29 15:55:50 +0000

    a much cleaner fix from Antoine

--- Lib/doctest.py
+++ Lib/doctest.py
@@ -1360,10 +1360,8 @@ class DocTestRunner:
             linecache.getlines = self.save_linecache_getlines
             if clear_globs:
                 test.globs.clear()
-                if __name__ == "__main__":
-                    __builtins__._ = None
-                else:
-                    __builtins__['_'] = None
+                import builtins
+                builtins._ = None
 
     #/////////////////////////////////////////////////////////////////
     # Summarization
