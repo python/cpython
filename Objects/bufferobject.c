6da0893c3133cbac7ee11c9839cd4469c46ceefc
Brett Cannon <bcannon@gmail.com>
2008-08-08 04:15:53 +0000

    Change the warning emitted for using the buffer() object; memoryview() in 3.0 is not an equivalent.

--- Objects/bufferobject.c
+++ Objects/bufferobject.c
@@ -236,8 +236,7 @@ buffer_new(PyTypeObject *type, PyObject *args, PyObject *kw)
 	Py_ssize_t offset = 0;
 	Py_ssize_t size = Py_END_OF_BUFFER;
 
-	if (PyErr_WarnPy3k("buffer() not supported in 3.x; "
-			 "use memoryview()", 1) < 0)
+	if (PyErr_WarnPy3k("buffer() not supported in 3.x", 1) < 0)
 		return NULL;
 	
 	if (!_PyArg_NoKeywords("buffer()", kw))
