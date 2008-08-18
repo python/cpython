6640d7262874520ad405173c0f2d4b48b1837a8d
Benjamin Peterson <benjamin@python.org>
2008-08-18 18:16:46 +0000

    Merged revisions 65826 via svnmerge from svn+ssh://pythondev@svn.python.org/python/trunk
    ........
      r65826 | benjamin.peterson | 2008-08-18 13:13:17 -0500 (Mon, 18 Aug 2008) | 1 line
    
      bring back the old API
    ........

--- Lib/threading.py
+++ Lib/threading.py
@@ -653,6 +653,18 @@ class Thread(_Verbose):
             raise RuntimeError("cannot set daemon status of active thread");
         self._daemonic = daemonic
 
+    def isDaemon(self):
+        return self.daemon
+
+    def setDaemon(self, daemonic):
+        self.daemon = daemonic
+
+    def getName(self):
+        return self.name
+
+    def setName(self, name):
+        self.name = name
+
 # The timer class was contributed by Itamar Shtull-Trauring
 
 def Timer(*args, **kwargs):
