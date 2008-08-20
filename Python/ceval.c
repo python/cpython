6e7740c790bf41f520e304ec17abf79a5373b5cc
Benjamin Peterson <benjamin@python.org>
2008-08-20 23:23:34 +0000

    apply a fix for #3611 where the current exception context was deleted with a generator causing a segfault

--- Python/ceval.c
+++ Python/ceval.c
@@ -2453,7 +2453,7 @@ fast_block_end:
 
 			if (b->b_type == EXCEPT_HANDLER) {
 				UNWIND_EXCEPT_HANDLER(b);
-				if (why == WHY_EXCEPTION) {
+				if (why == WHY_EXCEPTION && !throwflag) {
 					Py_CLEAR(tstate->exc_type);
 					Py_CLEAR(tstate->exc_value);
 					Py_CLEAR(tstate->exc_traceback);
