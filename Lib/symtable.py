346111f7075da135374067af1d603a1add99a5bd
Benjamin Peterson <benjamin@python.org>
2008-08-20 12:55:31 +0000

    revert 65897

--- Lib/symtable.py
+++ Lib/symtable.py
@@ -128,8 +128,8 @@ class Function(SymbolTable):
     __globals = None
 
     def __idents_matching(self, test_func):
-        return frozenset(ident for ident in self.get_identifiers()
-                         if test_func(self._table.symbols[ident]))
+        return tuple([ident for ident in self.get_identifiers()
+                      if test_func(self._table.symbols[ident])])
 
     def get_parameters(self):
         if self.__params is None:
@@ -164,7 +164,7 @@ class Class(SymbolTable):
             d = {}
             for st in self._table.children:
                 d[st.name] = 1
-            self.__methods = frozenset(d)
+            self.__methods = tuple(d)
         return self.__methods
 
 
