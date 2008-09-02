3e7dd78344668b1921f4c181a648ca812c1d0064
Hirokazu Yamamoto <ocean-city@m2.ccsnet.ne.jp>
2008-09-02 20:36:44 +0000

    Issue #3759: test_asyncore.py leaked handle. Reviewed by Amaury Forgeot d'Arc

--- Lib/test/test_asyncore.py
+++ Lib/test/test_asyncore.py
@@ -389,6 +389,7 @@ if hasattr(asyncore, 'file_wrapper'):
         def test_recv(self):
             fd = os.open(TESTFN, os.O_RDONLY)
             w = asyncore.file_wrapper(fd)
+            os.close(fd)
 
             self.assertNotEqual(w.fd, fd)
             self.assertNotEqual(w.fileno(), fd)
@@ -402,6 +403,7 @@ if hasattr(asyncore, 'file_wrapper'):
             d2 = "I want to buy some cheese."
             fd = os.open(TESTFN, os.O_WRONLY | os.O_APPEND)
             w = asyncore.file_wrapper(fd)
+            os.close(fd)
 
             w.write(d1)
             w.send(d2)
