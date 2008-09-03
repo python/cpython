2ab344428449792e293c0c4d276fc66d26e919e5
Christian Heimes <christian@cheimes.de>
2008-09-03 20:31:07 +0000

    Fixed wording of python --help text. The -b option was still using the old name 'buffer' instead of 'bytearray'.

--- Modules/main.c
+++ Modules/main.c
@@ -56,8 +56,8 @@ static char *usage_line =
 /* Long usage message, split into parts < 512 bytes */
 static char *usage_1 = "\
 Options and arguments (and corresponding environment variables):\n\
--b     : issue warnings about str(bytes_instance), str(buffer_instance)\n\
-         and comparing bytes/buffer with str. (-bb: issue errors)\n\
+-b     : issue warnings about str(bytes_instance), str(bytearray_instance)\n\
+         and comparing bytes/bytearray with str. (-bb: issue errors)\n\
 -B     : don't write .py[co] files on import; also PYTHONDONTWRITEBYTECODE=x\n\
 -c cmd : program passed in as string (terminates option list)\n\
 -d     : debug output from parser; also PYTHONDEBUG=x\n\
