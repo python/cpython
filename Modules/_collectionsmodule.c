23b6cd529b399e639d0cec572e3dde94ed38ef36
Amaury Forgeot d'Arc <amauryfa@gmail.com>
2008-09-10 22:04:45 +0000

    #3743: PY_FORMAT_SIZE_T is designed for the OS "printf" functions, not for PyString_FromFormat which has an independent implementation, and uses "%zd".
    This makes a difference on win64, where printf needs "%Id" to display
    64bit values. For example, queue.__repr__ was incorrect.
    
    Reviewed by Martin von Loewis.

--- Modules/_collectionsmodule.c
+++ Modules/_collectionsmodule.c
@@ -670,7 +670,7 @@ deque_repr(PyObject *deque)
 		return NULL;
 	}
 	if (((dequeobject *)deque)->maxlen != -1)
-		fmt = PyString_FromFormat("deque(%%r, maxlen=%" PY_FORMAT_SIZE_T "d)", 
+		fmt = PyString_FromFormat("deque(%%r, maxlen=%zd)", 
 					((dequeobject *)deque)->maxlen);
 	else
 		fmt = PyString_FromString("deque(%r)");  
