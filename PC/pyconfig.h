c2dfd8c081d5dfe3c761d91fae332d19e24198e1
Martin v. Löwis <martin@v.loewis.de>
2008-10-03 10:59:55 +0000

    Update version number to 2.7.

--- PC/pyconfig.h
+++ PC/pyconfig.h
@@ -323,9 +323,9 @@ Py_NO_ENABLE_SHARED to find out.  Also support MS_NO_COREDLL for b/w compat */
 			their Makefile (other compilers are generally
 			taken care of by distutils.) */
 #			ifdef _DEBUG
-#				pragma comment(lib,"python26_d.lib")
+#				pragma comment(lib,"python27_d.lib")
 #			else
-#				pragma comment(lib,"python26.lib")
+#				pragma comment(lib,"python27.lib")
 #			endif /* _DEBUG */
 #		endif /* _MSC_VER */
 #	endif /* Py_BUILD_CORE */
