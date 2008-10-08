e78d352385999a9e9300ad289efaecbd9ebd8635
Georg Brandl <georg@python.org>
2008-10-08 18:57:31 +0000

    Backport r66857: make all whatsnew docs accessible.

--- Doc/conf.py
+++ Doc/conf.py
@@ -41,12 +41,6 @@ today_fmt = '%B %d, %Y'
 
 # List of files that shouldn't be included in the build.
 unused_docs = [
-    'whatsnew/2.0',
-    'whatsnew/2.1',
-    'whatsnew/2.2',
-    'whatsnew/2.3',
-    'whatsnew/2.4',
-    'whatsnew/2.5',
     'maclib/scrap',
     'library/xmllib',
     'library/xml.etree',
