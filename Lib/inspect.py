feedfb62e146f9c3d05d20b2f4bf051db1024f89
Benjamin Peterson <benjamin@python.org>
2008-10-21 22:28:54 +0000

    Merged revisions 66995-66996 via svnmerge from svn+ssh://pythondev@svn.python.org/python/trunk
    ........
      r66995 | benjamin.peterson | 2008-10-21 17:18:29 -0500 (Tue, 21 Oct 2008) | 1 line
    
      return ArgInfo from inspect.getargvalues #4092
    ........
      r66996 | benjamin.peterson | 2008-10-21 17:20:31 -0500 (Tue, 21 Oct 2008) | 1 line
    
      add NEWs note for last change
    ........

--- Lib/inspect.py
+++ Lib/inspect.py
@@ -819,7 +819,7 @@ def getargvalues(frame):
     'varargs' and 'varkw' are the names of the * and ** arguments or None.
     'locals' is the locals dictionary of the given frame."""
     args, varargs, varkw = getargs(frame.f_code)
-    return args, varargs, varkw, frame.f_locals
+    return ArgInfo(args, varargs, varkw, frame.f_locals)
 
 def joinseq(seq):
     if len(seq) == 1:
