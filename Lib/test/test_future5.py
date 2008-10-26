9aebc61ad8e3b0eddd43dc563380025cc2fd8d02
Benjamin Peterson <benjamin@python.org>
2008-10-26 20:58:53 +0000

    Merged revisions 67030-67031 via svnmerge from svn+ssh://pythondev@svn.python.org/python/trunk
    ........
      r67030 | benjamin.peterson | 2008-10-26 15:21:13 -0500 (Sun, 26 Oct 2008) | 1 line
    
      fix __future__ imports when multiple features are given
    ........
      r67031 | benjamin.peterson | 2008-10-26 15:33:19 -0500 (Sun, 26 Oct 2008) | 1 line
    
      add forgotten test for r67030
    ........

---
+++ Lib/test/test_future5.py
@@ -0,0 +1,21 @@
+# Check that multiple features can be enabled.
+from __future__ import unicode_literals, print_function
+
+import sys
+import unittest
+from . import support
+
+
+class TestMultipleFeatures(unittest.TestCase):
+
+    def test_unicode_literals(self):
+        self.assertTrue(isinstance("", str))
+
+    def test_print_function(self):
+        with support.captured_output("stderr") as s:
+            print("foo", file=sys.stderr)
+        self.assertEqual(s.getvalue(), "foo\n")
+
+
+def test_main():
+    support.run_unittest(TestMultipleFeatures)
