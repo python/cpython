e3049f347c481ed102259c22743bac4696aa5014
Benjamin Peterson <benjamin@python.org>
2008-11-21 22:27:24 +0000

    don't segfault when \N escapes are used and unicodedata fails to load
    Fixes #4367

--- Python/ast.c
+++ Python/ast.c
@@ -1294,13 +1294,14 @@ ast_for_atom(struct compiling *c, const node *n)
             if (PyErr_ExceptionMatches(PyExc_UnicodeError)){
                 PyObject *type, *value, *tback, *errstr;
                 PyErr_Fetch(&type, &value, &tback);
-                errstr = ((PyUnicodeErrorObject *)value)->reason;
+                errstr = PyObject_Str(value);
                 if (errstr) {
                     char *s = "";
                     char buf[128];
                     s = PyString_AsString(errstr);
                     PyOS_snprintf(buf, sizeof(buf), "(unicode error) %s", s);
                     ast_error(n, buf);
+                    Py_DECREF(errstr);
                 } else {
                     ast_error(n, "(unicode error) unknown error");
                 }
