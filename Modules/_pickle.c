dcafe4d997f5f42ae7e7ad5a952817a65022604e
Amaury Forgeot d'Arc <amauryfa@gmail.com>
2008-11-25 21:11:54 +0000

    #4373: Reference leak in the pickle module.
    Reviewed by Brett Cannon.

--- Modules/_pickle.c
+++ Modules/_pickle.c
@@ -486,11 +486,13 @@ unpickler_read(UnpicklerObject *self, char **s, Py_ssize_t n)
         PyErr_SetString(PyExc_ValueError,
                         "read() from the underlying stream did not"
                         "return bytes");
+        Py_DECREF(data);
         return -1;
     }
 
     if (PyBytes_GET_SIZE(data) != n) {
         PyErr_SetNone(PyExc_EOFError);
+        Py_DECREF(data);
         return -1;
     }
 
