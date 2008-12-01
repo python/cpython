6825e17e1e4bc0034b6aba665d5e641e3febecfa
Martin v. Löwis <martin@v.loewis.de>
2008-12-01 04:44:45 +0000

    Add script forgotten in r67464.

---
+++ Demo/distutils/test2to3/maintest.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+# The above line should get replaced with the path to the Python
+# interpreter; the block below should get 2to3-converted.
+
+try:
+    from test2to3.hello import hello
+except ImportError, e:
+    print "Import failed", e
+hello()
