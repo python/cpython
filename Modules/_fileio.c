1efc23cc35e0224de9831362f0a6c6bc1e606948
Benjamin Peterson <benjamin@python.org>
2008-12-29 18:02:28 +0000

    Merged revisions 68016 via svnmerge from svn+ssh://pythondev@svn.python.org/python/trunk
    ........
      r68016 | benjamin.peterson | 2008-12-29 11:56:58 -0600 (Mon, 29 Dec 2008) | 1 line
    
      #4764 in io.open, set IOError.filename when trying to open a directory on POSIX platforms
    ........

--- Modules/_fileio.c
+++ Modules/_fileio.c
@@ -116,7 +116,7 @@ fileio_new(PyTypeObject *type, PyObject *args, PyObject *kews)
    directories, so we need a check.  */
 
 static int
-dircheck(PyFileIOObject* self)
+dircheck(PyFileIOObject* self, char *name)
 {
 #if defined(HAVE_FSTAT) && defined(S_IFDIR) && defined(EISDIR)
 	struct stat buf;
@@ -128,8 +128,8 @@ dircheck(PyFileIOObject* self)
 		if (internal_close(self))
 			return -1;
 
-		exc = PyObject_CallFunction(PyExc_IOError, "(is)",
-					    EISDIR, msg);
+		exc = PyObject_CallFunction(PyExc_IOError, "(iss)",
+					    EISDIR, msg, name);
 		PyErr_SetObject(PyExc_IOError, exc);
 		Py_XDECREF(exc);
 		return -1;
@@ -290,7 +290,7 @@ fileio_init(PyObject *oself, PyObject *args, PyObject *kwds)
 #endif
 			goto error;
 		}
-		if(dircheck(self) < 0)
+		if(dircheck(self, name) < 0)
 			goto error;
 	}
 
