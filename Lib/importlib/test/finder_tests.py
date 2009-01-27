8b0c6375198f46952752ce970390b1859cfcbc74
Brett Cannon <bcannon@gmail.com>
2009-01-27 01:34:30 +0000

    Make importlib.test.finder_tests an ABC.

--- Lib/importlib/test/finder_tests.py
+++ Lib/importlib/test/finder_tests.py
@@ -1,39 +1,39 @@
-# top-level.
-# Package.
-# module in pacakge.
-# Package within a package.
-# At least one tests with 'path'.
-# Module that is not handled.
-
+import abc
 import unittest
 
 
-class FinderTests(unittest.TestCase):
+class FinderTests(unittest.TestCase, metaclass=abc.ABCMeta):
 
     """Basic tests for a finder to pass."""
 
+    @abc.abstractmethod
     def test_module(self):
         # Test importing a top-level module.
-        raise NotImplementedError
+        pass
 
+    @abc.abstractmethod
     def test_package(self):
         # Test importing a package.
-        raise NotImplementedError
+        pass
 
+    @abc.abstractmethod
     def test_module_in_package(self):
         # Test importing a module contained within a package.
         # A value for 'path' should be used if for a meta_path finder.
-        raise NotImplementedError
+        pass
 
+    @abc.abstractmethod
     def test_package_in_package(self):
         # Test importing a subpackage.
         # A value for 'path' should be used if for a meta_path finder.
-        raise NotImplementedError
+        pass
 
+    @abc.abstractmethod
     def test_package_over_module(self):
         # Test that packages are chosen over modules.
-        raise NotImplementedError
+        pass
 
+    @abc.abstractmethod
     def test_failure(self):
         # Test trying to find a module that cannot be handled.
-        raise NotImplementedError
+        pass
