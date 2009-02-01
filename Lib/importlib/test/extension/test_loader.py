b3da7f2e9d9ea145eb03abe85e3ea7250fd81f4f
Brett Cannon <bcannon@gmail.com>
2009-02-01 00:49:41 +0000

    Move extension module loader tests over to importlib.test.abc.LoaderTests.

--- Lib/importlib/test/extension/test_loader.py
+++ Lib/importlib/test/extension/test_loader.py
@@ -1,12 +1,13 @@
 import importlib
 from . import test_path_hook
+from .. import abc
 from .. import support
 
 import sys
 import unittest
 
 
-class LoaderTests(unittest.TestCase):
+class LoaderTests(abc.LoaderTests):
 
     """Test load_module() for extension modules."""
 
@@ -16,7 +17,7 @@ class LoaderTests(unittest.TestCase):
                                                 False)
         return loader.load_module(fullname)
 
-    def test_success(self):
+    def test_module(self):
         with support.uncache(test_path_hook.NAME):
             module = self.load_module(test_path_hook.NAME)
             for attr, value in [('__name__', test_path_hook.NAME),
@@ -24,7 +25,25 @@ class LoaderTests(unittest.TestCase):
                 self.assertEqual(getattr(module, attr), value)
             self.assert_(test_path_hook.NAME in sys.modules)
 
-    def test_failure(self):
+    def test_package(self):
+        # Extensions are not found in packages.
+        pass
+
+    def test_lacking_parent(self):
+        # Extensions are not found in packages.
+        pass
+
+    def test_module_reuse(self):
+        with support.uncache(test_path_hook.NAME):
+            module1 = self.load_module(test_path_hook.NAME)
+            module2 = self.load_module(test_path_hook.NAME)
+            self.assert_(module1 is module2)
+
+    def test_state_after_failure(self):
+        # No easy way to trigger a failure after a successful import.
+        pass
+
+    def test_unloadable(self):
         self.assertRaises(ImportError, self.load_module, 'asdfjkl;')
 
 
