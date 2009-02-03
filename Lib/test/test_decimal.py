45fd4763fffeaeb9d3fbb3dee2f861fad85b8d2b
Raymond Hettinger <python@rcn.com>
2009-02-03 03:42:07 +0000

    Register decimals as numbers.Number

--- Lib/test/test_decimal.py
+++ Lib/test/test_decimal.py
@@ -30,6 +30,7 @@ import os, sys
 import pickle, copy
 import unittest
 from decimal import *
+import numbers
 from test.test_support import (TestSkipped, run_unittest, run_doctest,
                                is_resource_enabled)
 import random
@@ -1333,6 +1334,12 @@ class DecimalUsabilityTest(unittest.TestCase):
 
 
 class DecimalPythonAPItests(unittest.TestCase):
+
+    def test_abc(self):
+        self.assert_(issubclass(Decimal, numbers.Number))
+        self.assert_(not issubclass(Decimal, numbers.Real))
+        self.assert_(isinstance(Decimal(0), numbers.Number))
+        self.assert_(not isinstance(Decimal(0), numbers.Real))
 
     def test_pickle(self):
         d = Decimal('-3.141590000')
