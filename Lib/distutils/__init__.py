97a69f0c102fec17cd928d2a3d2a97d831aaa9ce
Tarek Ziadé <ziade.tarek@gmail.com>
2009-02-05 09:06:23 +0000

    Fix comment for #1835

--- Lib/distutils/__init__.py
+++ Lib/distutils/__init__.py
@@ -12,13 +12,8 @@ __revision__ = "$Id$"
 
 # Distutils version
 #
-# Please coordinate with Marc-Andre Lemburg <mal@egenix.com> when adding
-# new features to distutils that would warrant bumping the version number.
+# Updated automatically by the Python release process.
 #
-# In general, major and minor version should loosely follow the Python
-# version number the distutils code was shipped with.
-#
-
 #--start constants--
 __version__ = "2.6"
 #--end constants--
