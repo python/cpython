3a20c2d5da5800eb06a8474b3a887f981716eacb
Mark Dickinson <dickinsm@gmail.com>
2009-02-08 14:42:28 +0000

    Issue #789290: make sure that hash(2**63) == hash(2.**63) on 64-bit platforms.  The previous code was fragile, depending on the twin accidents that:
      (1) in C, casting the double value 2.**63 to long returns the integer
          value -2**63, and
      (2) in Python, hash(-2**63) == hash(2**63).
    
    There's already a test for this in test_hash.

--- Objects/object.c
+++ Objects/object.c
@@ -1028,7 +1028,7 @@ _Py_HashDouble(double v)
 	fractpart = modf(v, &intpart);
 	if (fractpart == 0.0) {
 		/* This must return the same hash as an equal int or long. */
-		if (intpart > LONG_MAX || -intpart > LONG_MAX) {
+		if (intpart > LONG_MAX/2 || -intpart > LONG_MAX/2) {
 			/* Convert to long and use its hash. */
 			PyObject *plong;	/* converted to Python long */
 			if (Py_IS_INFINITY(intpart))
