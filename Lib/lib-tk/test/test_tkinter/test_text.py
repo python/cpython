6bd83b006745fa8a871912b0b331761fee15e8ce
Guilherme Polo <ggpolo@gmail.com>
2009-02-09 19:21:21 +0000

    Some tests for Tkinter.Text.search

---
+++ Lib/lib-tk/test/test_tkinter/test_text.py
@@ -0,0 +1,39 @@
+import unittest
+import Tkinter
+from test.test_support import requires, run_unittest
+from ttk import setup_master
+
+requires('gui')
+
+class TextTest(unittest.TestCase):
+
+    def setUp(self):
+        self.root = setup_master()
+        self.text = Tkinter.Text(self.root)
+
+    def tearDown(self):
+        self.text.destroy()
+
+
+    def test_search(self):
+        text = self.text
+
+        # pattern and index are obligatory arguments.
+        self.failUnlessRaises(Tkinter.TclError, text.search, None, '1.0')
+        self.failUnlessRaises(Tkinter.TclError, text.search, 'a', None)
+        self.failUnlessRaises(Tkinter.TclError, text.search, None, None)
+
+        # Invalid text index.
+        self.failUnlessRaises(Tkinter.TclError, text.search, '', 0)
+
+        # Check if we are getting the indices as strings -- you are likely
+        # to get Tcl_Obj under Tk 8.5 if Tkinter doesn't convert it.
+        text.insert('1.0', 'hi-test')
+        self.failUnlessEqual(text.search('-test', '1.0', 'end'), '1.2')
+        self.failUnlessEqual(text.search('test', '1.0', 'end'), '1.3')
+
+
+tests_gui = (TextTest, )
+
+if __name__ == "__main__":
+    run_unittest(*tests_gui)
