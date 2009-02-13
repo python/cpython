0d5e86cc5a834774ea972790917824e641fdfbbf
Tarek Ziadé <ziade.tarek@gmail.com>
2009-02-13 23:07:18 +0000

    Merged revisions 69600 via svnmerge from svn+ssh://pythondev@svn.python.org/python/branches/py3k
    ................
      r69600 | tarek.ziade | 2009-02-14 00:04:17 +0100 (Sat, 14 Feb 2009) | 9 lines
    
      Merged revisions 69598 via svnmerge from
      svn+ssh://pythondev@svn.python.org/python/trunk
    
      ........
        r69598 | tarek.ziade | 2009-02-14 00:00:43 +0100 (Sat, 14 Feb 2009) | 1 line
    
        Fixed #4524: distutils build_script command failed with --with-suffix=3
      ........
    ................

--- Lib/distutils/tests/test_build_scripts.py
+++ Lib/distutils/tests/test_build_scripts.py
@@ -5,6 +5,7 @@ import unittest
 
 from distutils.command.build_scripts import build_scripts
 from distutils.core import Distribution
+from distutils import sysconfig
 
 from distutils.tests import support
 
@@ -73,6 +74,33 @@ class BuildScriptsTestCase(support.TempdirManager,
         f.write(text)
         f.close()
 
+    def test_version_int(self):
+        source = self.mkdtemp()
+        target = self.mkdtemp()
+        expected = self.write_sample_scripts(source)
+
+
+        cmd = self.get_build_scripts_cmd(target,
+                                         [os.path.join(source, fn)
+                                          for fn in expected])
+        cmd.finalize_options()
+
+        # http://bugs.python.org/issue4524
+        #
+        # On linux-g++-32 with command line `./configure --enable-ipv6
+        # --with-suffix=3`, python is compiled okay but the build scripts
+        # failed when writing the name of the executable
+        old = sysconfig._config_vars.get('VERSION')
+        sysconfig._config_vars['VERSION'] = 4
+        try:
+            cmd.run()
+        finally:
+            if old is not None:
+                sysconfig._config_vars['VERSION'] = old
+
+        built = os.listdir(target)
+        for name in expected:
+            self.assert_(name in built)
 
 def test_suite():
     return unittest.makeSuite(BuildScriptsTestCase)
