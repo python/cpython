6903e05e63d24c73b3db8711f0e5664e0221e0a7
Georg Brandl <georg@python.org>
2009-02-13 11:10:04 +0000

    Fix warnings GCC emits where the argument of PyErr_Format is a single variable.

--- Modules/_ctypes/_ctypes.c
+++ Modules/_ctypes/_ctypes.c
@@ -599,13 +599,14 @@ CDataType_in_dll(PyObject *type, PyObject *args)
 #else
 	address = (void *)ctypes_dlsym(handle, name);
 	if (!address) {
-		PyErr_Format(PyExc_ValueError,
 #ifdef __CYGWIN__
 /* dlerror() isn't very helpful on cygwin */
+		PyErr_Format(PyExc_ValueError,
 			     "symbol '%s' not found (%s) ",
-			     name,
+			     name);
+#else
+		PyErr_SetString(PyExc_ValueError, ctypes_dlerror());
 #endif
-			     ctypes_dlerror());
 		return NULL;
 	}
 #endif
@@ -3283,13 +3284,14 @@ CFuncPtr_FromDll(PyTypeObject *type, PyObject *args, PyObject *kwds)
 #else
 	address = (PPROC)ctypes_dlsym(handle, name);
 	if (!address) {
-		PyErr_Format(PyExc_AttributeError,
 #ifdef __CYGWIN__
 /* dlerror() isn't very helpful on cygwin */
+		PyErr_Format(PyExc_AttributeError,
 			     "function '%s' not found (%s) ",
-			     name,
+			     name);
+#else
+		PyErr_SetString(PyExc_AttributeError, ctypes_dlerror());
 #endif
-			     ctypes_dlerror());
 		return NULL;
 	}
 #endif
