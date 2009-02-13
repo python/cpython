9e8a250ed98b9d280e128b9cd607c8adc5f612bd
Antoine Pitrou <solipsis@pitrou.net>
2009-02-13 13:57:40 +0000

    Fix compiler warning (gcc)

--- Objects/object.c
+++ Objects/object.c
@@ -1076,7 +1076,7 @@ _Py_HashPointer(void *p)
 	size_t y = (size_t)p;
 	/* bottom 3 or 4 bits are likely to be 0; rotate y by 4 to avoid
 	   excessive hash collisions for dicts and sets */
-	y = (y >> 4) | (y << 8*SIZEOF_VOID_P - 4);
+	y = (y >> 4) | (y << (8 * SIZEOF_VOID_P - 4));
 	x = (long)y;
 	if (x == -1)
 		x = -2;
