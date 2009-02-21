ccefa77934d8a3007cd8795c260f33620fc919ed
Raymond Hettinger <python@rcn.com>
2009-02-21 23:20:57 +0000

    In Py3.x, a list comprehension is now faster than list(map(itemgetter(0), iterable)).

--- Lib/heapq.py
+++ Lib/heapq.py
@@ -130,7 +130,6 @@ __all__ = ['heappush', 'heappop', 'heapify', 'heapreplace', 'merge',
            'nlargest', 'nsmallest', 'heappushpop']
 
 from itertools import islice, repeat, count, tee, chain
-from operator import itemgetter
 import bisect
 
 def heappush(heap, item):
@@ -377,13 +376,13 @@ def nsmallest(n, iterable, key=None):
     if key is None:
         it = zip(iterable, count())                         # decorate
         result = _nsmallest(n, it)
-        return list(map(itemgetter(0), result))             # undecorate
+        return [r[0] for r in result]                       # undecorate
 
     # General case, slowest method
     in1, in2 = tee(iterable)
     it = zip(map(key, in1), count(), in2)                   # decorate
     result = _nsmallest(n, it)
-    return list(map(itemgetter(2), result))                 # undecorate
+    return [r[2] for r in result]                           # undecorate
 
 _nlargest = nlargest
 def nlargest(n, iterable, key=None):
@@ -415,13 +414,13 @@ def nlargest(n, iterable, key=None):
     if key is None:
         it = zip(iterable, count(0,-1))                     # decorate
         result = _nlargest(n, it)
-        return list(map(itemgetter(0), result))             # undecorate
+        return [r[0] for r in result]                       # undecorate
 
     # General case, slowest method
     in1, in2 = tee(iterable)
     it = zip(map(key, in1), count(0,-1), in2)               # decorate
     result = _nlargest(n, it)
-    return list(map(itemgetter(2), result))                 # undecorate
+    return [r[2] for r in result]                           # undecorate
 
 if __name__ == "__main__":
     # Simple sanity test
