b017b10f927ac0f7fe6ab6e74ce162271f3a2b59
Guilherme Polo <ggpolo@gmail.com>
2009-03-07 01:47:49 +0000

    Fixed issue #4792: Prevent a segfault in _tkinter by using the guaranteed to be safe interp argument given to the PythonCmd in place of the Tcl interpreter taken from a PythonCmd_ClientData.

--- Modules/_tkinter.c
+++ Modules/_tkinter.c
@@ -2070,7 +2070,7 @@ PythonCmd(ClientData clientData, Tcl_Interp *interp, int argc, char *argv[])
 		return PythonCmd_Error(interp);
 	}
 	else {
-		Tcl_SetObjResult(Tkapp_Interp(self), obj_res);
+		Tcl_SetObjResult(interp, obj_res);
 		rv = TCL_OK;
 	}
 
