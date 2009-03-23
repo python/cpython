797eaf305a1e4cbaf2041e9b28b125398e2c235a
Benjamin Peterson <benjamin@python.org>
2009-03-23 21:25:15 +0000

    complain when there's no last exception

--- Lib/traceback.py
+++ Lib/traceback.py
@@ -241,6 +241,8 @@ def format_exc(limit=None):
 def print_last(limit=None, file=None):
     """This is a shorthand for 'print_exception(sys.last_type,
     sys.last_value, sys.last_traceback, limit, file)'."""
+    if not hasattr(sys, "last_type"):
+        raise ValueError("no last exception")
     if file is None:
         file = sys.stderr
     print_exception(sys.last_type, sys.last_value, sys.last_traceback,
