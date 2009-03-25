152f222775e2d9341830996873685d95419ae2cd
Marc-André Lemburg <mal@egenix.com>
2009-03-25 19:52:04 +0000

    Merged revisions 70594-70595 via svnmerge from svn+pythonssh://pythondev@svn.python.org/python/trunk
    ........
      r70594 | marc-andre.lemburg | 2009-03-25 20:44:58 +0100 (Wed, 25 Mar 2009) | 9 lines
    
      Remove the sys.version_info shortcut, since they cause the APIs
      to return different information than the _sys_version() output
      used in previous Python versions.
    
      This also fixes issue5561: platform.python_version_tuple returns tuple of ints, should be strings
    
      Added more tests for the various platform functions.
    ........
      r70595 | marc-andre.lemburg | 2009-03-25 20:45:33 +0100 (Wed, 25 Mar 2009) | 3 lines
    
      News item for the platform.py fix (r70594).
    ........

--- Lib/test/test_platform.py
+++ Lib/test/test_platform.py
@@ -25,39 +25,48 @@ class PlatformTest(unittest.TestCase):
             finally:
                 os.remove(link)
 
-    def test_machine(self):
-        res = platform.machine()
-
-    def test_node(self):
-        res = platform.node()
-
     def test_platform(self):
         for aliased in (False, True):
             for terse in (False, True):
                 res = platform.platform(aliased, terse)
 
+    def test_system(self):
+        res = platform.system()
+
+    def test_node(self):
+        res = platform.node()
+
+    def test_release(self):
+        res = platform.release()
+
+    def test_version(self):
+        res = platform.version()
+
+    def test_machine(self):
+        res = platform.machine()
+
     def test_processor(self):
         res = platform.processor()
+
+    def test_python_implementation(self):
+        res = platform.python_implementation()
+
+    def test_python_version(self):
+        res1 = platform.python_version()
+        res2 = platform.python_version_tuple()
+        self.assertEqual(res1, ".".join(res2))
+
+    def test_python_branch(self):
+        res = platform.python_branch()
+
+    def test_python_revision(self):
+        res = platform.python_revision()
 
     def test_python_build(self):
         res = platform.python_build()
 
     def test_python_compiler(self):
         res = platform.python_compiler()
-
-    def test_version(self):
-        res1 = platform.version()
-        res2 = platform.version_tuple()
-        self.assertEqual(res1, ".".join(res2))
-
-    def test_release(self):
-        res = platform.release()
-
-    def test_system(self):
-        res = platform.system()
-
-    def test_version(self):
-        res = platform.version()
 
     def test_system_alias(self):
         res = platform.system_alias(
