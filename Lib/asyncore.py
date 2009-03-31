aba27dd6d9bcd6de8f028505d837915b6a1828b7
Josiah Carlson <josiah.carlson@gmail.com>
2009-03-31 21:49:36 +0000

    Made handle_expt_event() be called last, so that we don't accidentally read after closing the socket.

--- Lib/asyncore.py
+++ Lib/asyncore.py
@@ -97,14 +97,14 @@ def _exception(obj):
 
 def readwrite(obj, flags):
     try:
-        if flags & select.POLLPRI:
-            obj.handle_expt_event()
         if flags & select.POLLIN:
             obj.handle_read_event()
         if flags & select.POLLOUT:
             obj.handle_write_event()
         if flags & (select.POLLHUP | select.POLLERR | select.POLLNVAL):
             obj.handle_close()
+        if flags & select.POLLPRI:
+            obj.handle_expt_event()
     except _reraised_exceptions:
         raise
     except:
