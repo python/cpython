9e47ce49a06075adbc391e04b4457707fa3892a7
Tarek Ziadé <ziade.tarek@gmail.com>
2009-03-31 22:27:23 +0000

    #5583 Added optional Extensions in Distutils

--- Doc/distutils/setupscript.rst
+++ Doc/distutils/setupscript.rst
@@ -334,6 +334,10 @@ Other options
 
 There are still some other options which can be used to handle special cases.
 
+The :option:`optional` option is a boolean; if it is true, that specifies that
+a build failure in the extension should not abort the build process, but simply
+not install the failing extension.
+
 The :option:`extra_objects` option is a list of object files to be passed to the
 linker. These files must not have extensions, as the default extension for the
 compiler is used.
--- Lib/distutils/extension.py
+++ Lib/distutils/extension.py
@@ -83,6 +83,9 @@ class Extension:
       language : string
         extension language (i.e. "c", "c++", "objc"). Will be detected
         from the source extensions if not provided.
+      optional : boolean
+        specifies that a build failure in the extension should not abort the
+        build process, but simply not install the failing extension.
     """
 
     # When adding arguments to this constructor, be sure to update
@@ -101,6 +104,7 @@ class Extension:
                   swig_opts = None,
                   depends=None,
                   language=None,
+                  optional=None,
                   **kw                      # To catch unknown keywords
                  ):
         assert type(name) is StringType, "'name' must be a string"
@@ -123,6 +127,7 @@ class Extension:
         self.swig_opts = swig_opts or []
         self.depends = depends or []
         self.language = language
+        self.optional = optional
 
         # If there are unknown keyword options, warn about them
         if len(kw):
