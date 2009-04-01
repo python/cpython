63959bb973cd1a27701342851aeb5b9466d7e776
Georg Brandl <georg@python.org>
2009-04-01 04:32:39 +0000

    #5631: add upload to list of possible commands, which is presented in --help-commands.

--- Lib/distutils/command/__init__.py
+++ Lib/distutils/command/__init__.py
@@ -22,6 +22,8 @@ __all__ = ['build',
            'bdist_dumb',
            'bdist_rpm',
            'bdist_wininst',
+           'upload',
+
            # These two are reserved for future use:
            #'bdist_sdux',
            #'bdist_pkgtool',
