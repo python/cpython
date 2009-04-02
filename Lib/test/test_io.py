bfb959404f01fafafd3e2bd5d0ee8539ba17246e
Benjamin Peterson <benjamin@python.org>
2009-04-02 01:13:40 +0000

    Merged revisions 71014 via svnmerge from svn+ssh://pythondev@svn.python.org/python/trunk
    ........
      r71014 | benjamin.peterson | 2009-04-01 20:03:17 -0500 (Wed, 01 Apr 2009) | 1 line
    
      handle SEEK_ constants in test_io
    ........

--- Lib/test/test_io.py
+++ Lib/test/test_io.py
@@ -1996,13 +1996,13 @@ class MiscIOTest(unittest.TestCase):
     def test___all__(self):
         for name in self.io.__all__:
             obj = getattr(self.io, name, None)
-            self.assert_(obj is not None, name)
+            self.assertTrue(obj is not None, name)
             if name == "open":
                 continue
             elif "error" in name.lower():
-                self.assert_(issubclass(obj, Exception), name)
-            else:
-                self.assert_(issubclass(obj, self.IOBase), name)
+                self.assertTrue(issubclass(obj, Exception), name)
+            elif not name.startswith("SEEK_"):
+                self.assertTrue(issubclass(obj, self.IOBase))
 
     def test_attributes(self):
         f = self.open(support.TESTFN, "wb", buffering=0)
