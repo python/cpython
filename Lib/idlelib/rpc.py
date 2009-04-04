8ad9bbdb2573b7275bc72f9545fc8a6cba0bc7c2
Kurt B. Kaiser <kbk@shore.net>
2009-04-04 07:03:48 +0000

    Allow multiple IDLE GUI/subprocess pairs to exist simultaneously. Thanks to David Scherer for suggesting the use of an ephemeral port for the GUI. Patch 1529142 Weeble.

--- Lib/idlelib/rpc.py
+++ Lib/idlelib/rpc.py
@@ -518,8 +518,6 @@ class RPCClient(SocketIO):
 
     def __init__(self, address, family=socket.AF_INET, type=socket.SOCK_STREAM):
         self.listening_sock = socket.socket(family, type)
-        self.listening_sock.setsockopt(socket.SOL_SOCKET,
-                                       socket.SO_REUSEADDR, 1)
         self.listening_sock.bind(address)
         self.listening_sock.listen(1)
 
