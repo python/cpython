90971e0cd278a9697684eff14d741c8de58ae5d4
Barry Warsaw <barry@python.org>
2009-04-07 01:54:02 +0000

    Bump to 2.6.2c1.

--- Include/patchlevel.h
+++ Include/patchlevel.h
@@ -22,12 +22,12 @@
 /*--start constants--*/
 #define PY_MAJOR_VERSION	2
 #define PY_MINOR_VERSION	6
-#define PY_MICRO_VERSION	1
+#define PY_MICRO_VERSION	2
 #define PY_RELEASE_LEVEL	PY_RELEASE_LEVEL_FINAL
 #define PY_RELEASE_SERIAL	0
 
 /* Version as a string */
-#define PY_VERSION      	"2.6.1+"
+#define PY_VERSION      	"2.6.2c1"
 /*--end constants--*/
 
 /* Subversion Revision number of this file (not of the repository) */
