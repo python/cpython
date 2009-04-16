48fd10af0013ca288346bc5a3ac2e7b6402e36cc
Thomas Heller <theller@ctypes.org>
2009-04-16 19:13:35 +0000

    Merged revisions 71640-71641 via svnmerge from svn+ssh://pythondev@svn.python.org/python/trunk
    ........
      r71640 | thomas.heller | 2009-04-16 08:26:33 +0200 (Do, 16 Apr 2009) | 1 line
    
      Remove unneeded code.
    ........
      r71641 | thomas.heller | 2009-04-16 08:42:02 +0200 (Do, 16 Apr 2009) | 2 lines
    
      Remove duplicated function declaration.
      Make _pagesize static.
    ........

--- Modules/_ctypes/ctypes.h
+++ Modules/_ctypes/ctypes.h
@@ -189,8 +189,6 @@ extern PyTypeObject StructType_Type;
 extern PyObject *
 CreateArrayType(PyObject *itemtype, Py_ssize_t length);
 
-extern void init_callbacks_in_module(PyObject *m);
-
 extern PyMethodDef module_methods[];
 
 extern CThunkObject *AllocFunctionCallback(PyObject *callable,
@@ -353,7 +351,6 @@ struct tagPyCArgObject {
 };
 
 extern PyTypeObject PyCArg_Type;
-extern PyCArgObject *new_CArgObject(void);
 #define PyCArg_CheckExact(v)	    ((v)->ob_type == &PyCArg_Type)
 extern PyCArgObject *new_CArgObject(void);
 
