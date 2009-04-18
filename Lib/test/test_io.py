20c3b5562213d70c67b0380292167202a323419a
Benjamin Peterson <benjamin@python.org>
2009-04-18 14:49:19 +0000

    use the more offical skipping api

--- Lib/test/test_io.py
+++ Lib/test/test_io.py
@@ -509,11 +509,11 @@ class IOTest(unittest.TestCase):
         # Issue #1174606: reading from an unbounded stream such as /dev/zero.
         zero = "/dev/zero"
         if not os.path.exists(zero):
-            raise unittest.SkipTest("{0} does not exist".format(zero))
+            self.skip("{0} does not exist".format(zero))
         if sys.maxsize > 0x7FFFFFFF:
-            raise unittest.SkipTest("test can only run in a 32-bit address space")
+            self.skip("test can only run in a 32-bit address space")
         if support.real_max_memuse < support._2G:
-            raise unittest.SkipTest("test requires at least 2GB of memory")
+            self.skip("test requires at least 2GB of memory")
         with open(zero, "rb", buffering=0) as f:
             self.assertRaises(OverflowError, f.read)
         with open(zero, "rb") as f:
