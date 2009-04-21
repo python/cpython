b88402f91f9be88a62f8c05e5744676a056dccc7
Raymond Hettinger <python@rcn.com>
2009-04-21 03:09:17 +0000

    Forward port r70471: Add object_pairs_hook.  Issue 5381.

--- Lib/json/tests/test_decode.py
+++ Lib/json/tests/test_decode.py
@@ -1,7 +1,9 @@
 import decimal
 from unittest import TestCase
+from io import StringIO
 
 import json
+from collections import OrderedDict
 
 class TestDecode(TestCase):
     def test_decimal(self):
@@ -13,3 +15,20 @@ class TestDecode(TestCase):
         rval = json.loads('1', parse_int=float)
         self.assert_(isinstance(rval, float))
         self.assertEquals(rval, 1.0)
+
+    def test_object_pairs_hook(self):
+        s = '{"xkd":1, "kcw":2, "art":3, "hxm":4, "qrt":5, "pad":6, "hoy":7}'
+        p = [("xkd", 1), ("kcw", 2), ("art", 3), ("hxm", 4),
+             ("qrt", 5), ("pad", 6), ("hoy", 7)]
+        self.assertEqual(json.loads(s), eval(s))
+        self.assertEqual(json.loads(s, object_pairs_hook = lambda x: x), p)
+        self.assertEqual(json.load(StringIO(s),
+                                   object_pairs_hook=lambda x: x), p)
+        od = json.loads(s, object_pairs_hook = OrderedDict)
+        self.assertEqual(od, OrderedDict(p))
+        self.assertEqual(type(od), OrderedDict)
+        # the object_pairs_hook takes priority over the object_hook
+        self.assertEqual(json.loads(s,
+                                    object_pairs_hook = OrderedDict,
+                                    object_hook = lambda x: None),
+                         OrderedDict(p))
