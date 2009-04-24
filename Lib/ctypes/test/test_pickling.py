5f1f7f28811c72a8772405882f50796747d604a7
Thomas Heller <theller@ctypes.org>
2009-04-24 20:00:41 +0000

    Issue 5041: ctypes unwilling to allow pickling wide character.

--- Lib/ctypes/test/test_pickling.py
+++ Lib/ctypes/test/test_pickling.py
@@ -66,6 +66,11 @@ class PickleTest(unittest.TestCase):
             ]:
             self.assertRaises(ValueError, lambda: self.dumps(item))
 
+    def test_wchar(self):
+        pickle.dumps(c_char("x"))
+        # Issue 5049
+        pickle.dumps(c_wchar(u"x"))
+
 class PickleTest_1(PickleTest):
     def dumps(self, item):
         return pickle.dumps(item, 1)
