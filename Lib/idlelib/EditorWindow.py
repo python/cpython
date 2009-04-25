d6cec0c61e797a7007e8a5a528e0d4cda73f6d22
Kurt B. Kaiser <kbk@shore.net>
2009-04-25 17:30:40 +0000

    Merged revisions 68801 via svnmerge from svn+ssh://pythondev@svn.python.org/python/trunk
    ........
      r68801 | raymond.hettinger | 2009-01-20 05:46:23 -0500 (Tue, 20 Jan 2009) | 5 lines
    
      Use Georg's new permalinks to documentation by version number.
      That assures that IDLE's help always points to the correct
      version and the latest update with all bug fixes.
    ........

--- Lib/idlelib/EditorWindow.py
+++ Lib/idlelib/EditorWindow.py
@@ -88,7 +88,7 @@ class EditorWindow(object):
                     # Safari requires real file:-URLs
                     EditorWindow.help_url = 'file://' + EditorWindow.help_url
             else:
-                EditorWindow.help_url = "http://www.python.org/doc/current"
+                EditorWindow.help_url = "http://docs.python.org/%d.%d" % sys.version_info[:2]
         currentTheme=idleConf.CurrentTheme()
         self.flist = flist
         root = root or flist.root
