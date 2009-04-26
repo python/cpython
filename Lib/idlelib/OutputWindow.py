8099e9c16705b9abc3882a656691df36e60f1692
Kurt B. Kaiser <kbk@shore.net>
2009-04-26 23:22:11 +0000

    Right click 'go to file/line' not working if spaces in path.  Bug 5559.

--- Lib/idlelib/OutputWindow.py
+++ Lib/idlelib/OutputWindow.py
@@ -63,6 +63,7 @@ class OutputWindow(EditorWindow):
         r'file "([^"]*)", line (\d+)',
         r'([^\s]+)\((\d+)\)',
         r'([^\s]+):\s*(\d+):',
+        r'^\s*(\S+.*?):\s*(\d+):',  # Win path with spaces, trim leading spaces
     ]
 
     file_line_progs = None
@@ -96,16 +97,16 @@ class OutputWindow(EditorWindow):
 
     def _file_line_helper(self, line):
         for prog in self.file_line_progs:
-            m = prog.search(line)
-            if m:
-                break
+            match = prog.search(line)
+            if match:
+                filename, lineno = match.group(1, 2)
+                try:
+                    f = open(filename, "r")
+                    f.close()
+                    break
+                except IOError:
+                    continue
         else:
-            return None
-        filename, lineno = m.group(1, 2)
-        try:
-            f = open(filename, "r")
-            f.close()
-        except IOError:
             return None
         try:
             return filename, int(lineno)
@@ -139,19 +140,3 @@ class OnDemandOutputWindow:
                 text.tag_configure(tag, **cnf)
         text.tag_raise('sel')
         self.write = self.owin.write
-
-#class PseudoFile:
-#
-#      def __init__(self, owin, tags, mark="end"):
-#          self.owin = owin
-#          self.tags = tags
-#          self.mark = mark
-
-#      def write(self, s):
-#          self.owin.write(s, self.tags, self.mark)
-
-#      def writelines(self, l):
-#          map(self.write, l)
-
-#      def flush(self):
-#          pass
