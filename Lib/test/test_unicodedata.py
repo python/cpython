be9e16a4ed2deb93402e757a50502fe731efcfb1
Walter Dörwald <walter@livinglogic.de>
2009-04-26 19:12:55 +0000

    Merged revisions 71972 via svnmerge from svn+ssh://pythondev@svn.python.org/python/trunk
    ........
      r71972 | walter.doerwald | 2009-04-26 21:11:43 +0200 (So, 26 Apr 2009) | 2 lines
    
      Fix typo.
    ........

--- Lib/test/test_unicodedata.py
+++ Lib/test/test_unicodedata.py
@@ -270,7 +270,7 @@ class UnicodeMiscTest(UnicodeDatabaseTest):
             [0]
         )
 
-    def test_buf_4971(self):
+    def test_bug_4971(self):
         # LETTER DZ WITH CARON: DZ, Dz, dz
         self.assertEqual(u"\u01c4".title(), u"\u01c5")
         self.assertEqual(u"\u01c5".title(), u"\u01c5")
