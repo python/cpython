99f277933e172cd035f049f2de4a9033f43a55dc
Martin v. Löwis <martin@v.loewis.de>
2009-04-26 00:53:18 +0000

    Issue #4971: Fix titlecase for characters that are their own titlecase, but not their own uppercase.

--- Lib/test/test_unicodedata.py
+++ Lib/test/test_unicodedata.py
@@ -20,7 +20,7 @@ encoding = 'utf-8'
 class UnicodeMethodsTest(unittest.TestCase):
 
     # update this, if the database changes
-    expectedchecksum = 'b7db9b5f1d804976fa921d2009cbef6f025620c1'
+    expectedchecksum = '6ec65b65835614ec00634c674bba0e50cd32c189'
 
     def test_method_checksum(self):
         h = hashlib.sha1()
@@ -270,6 +270,11 @@ class UnicodeMiscTest(UnicodeDatabaseTest):
             [0]
         )
 
+    def test_buf_4971(self):
+        # LETTER DZ WITH CARON: DZ, Dz, dz
+        self.assertEqual(u"\u01c4".title(), u"\u01c5")
+        self.assertEqual(u"\u01c5".title(), u"\u01c5")
+        self.assertEqual(u"\u01c6".title(), u"\u01c5")
 
 def test_main():
     test.test_support.run_unittest(
