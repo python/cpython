b43d32552d49b35d1d61ade1d663d8e62cfd8cd7
Benjamin Peterson <benjamin@python.org>
2009-05-05 23:14:15 +0000

    Merged revisions 72367 via svnmerge from svn+ssh://pythondev@svn.python.org/python/trunk
    ........
      r72367 | benjamin.peterson | 2009-05-05 18:00:48 -0500 (Tue, 05 May 2009) | 1 line
    
      tabify :(
    ........

--- Modules/_testcapimodule.c
+++ Modules/_testcapimodule.c
@@ -228,10 +228,10 @@ test_lazy_hash_inheritance(PyObject* self)
 
 	type = &_HashInheritanceTester_Type;
 
-        if (type->tp_dict != NULL)
-            /* The type has already been initialized. This probably means -R
-             is being used. */
-            Py_RETURN_NONE;
+	if (type->tp_dict != NULL)
+		/* The type has already been initialized. This probably means
+		   -R is being used. */
+		Py_RETURN_NONE;
 
 
 	obj = PyObject_New(PyObject, type);
@@ -277,7 +277,7 @@ test_lazy_hash_inheritance(PyObject* self)
 		return NULL;
 	}
 
-        Py_DECREF(obj);
+	Py_DECREF(obj);
 
 	Py_RETURN_NONE;
 }
