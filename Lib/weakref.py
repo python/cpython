464d53d42a3f9f094e70c90e1e0668c3c43653a0
Antoine Pitrou <solipsis@pitrou.net>
2009-05-15 17:04:50 +0000

    Merged revisions 72669 via svnmerge from svn+ssh://pythondev@svn.python.org/python/trunk
    ........
      r72669 | antoine.pitrou | 2009-05-15 18:54:52 +0200 (ven., 15 mai 2009) | 3 lines
    
      Issue #2116: Weak references and weak dictionaries now support copy()ing and deepcopy()ing.
    ........

--- Lib/weakref.py
+++ Lib/weakref.py
@@ -85,6 +85,17 @@ class WeakValueDictionary(collections.MutableMapping):
                 new[key] = o
         return new
 
+    __copy__ = copy
+
+    def __deepcopy__(self, memo):
+        from copy import deepcopy
+        new = self.__class__()
+        for key, wr in self.data.items():
+            o = wr()
+            if o is not None:
+                new[deepcopy(key, memo)] = o
+        return new
+
     def get(self, key, default=None):
         try:
             wr = self.data[key]
@@ -249,6 +260,17 @@ class WeakKeyDictionary(collections.MutableMapping):
             o = key()
             if o is not None:
                 new[o] = value
+        return new
+
+    __copy__ = copy
+
+    def __deepcopy__(self, memo):
+        from copy import deepcopy
+        new = self.__class__()
+        for key, value in self.data.items():
+            o = key()
+            if o is not None:
+                new[o] = deepcopy(value, memo)
         return new
 
     def get(self, key, default=None):
