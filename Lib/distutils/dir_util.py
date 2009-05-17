513a8b7d99611d78558fd651243f756212117006
Tarek Ziadé <ziade.tarek@gmail.com>
2009-05-17 11:22:36 +0000

    pep8-fied distutils.dir_util

--- Lib/distutils/dir_util.py
+++ Lib/distutils/dir_util.py
@@ -5,7 +5,6 @@ Utility functions for manipulating directories and directory trees."""
 __revision__ = "$Id$"
 
 import os, sys
-from types import *
 from distutils.errors import DistutilsFileError, DistutilsInternalError
 from distutils import log
 
@@ -16,20 +15,21 @@ _path_created = {}
 # I don't use os.makedirs because a) it's new to Python 1.5.2, and
 # b) it blows up if the directory already exists (I want to silently
 # succeed in that case).
-def mkpath (name, mode=0777, verbose=1, dry_run=0):
-    """Create a directory and any missing ancestor directories.  If the
-       directory already exists (or if 'name' is the empty string, which
-       means the current directory, which of course exists), then do
-       nothing.  Raise DistutilsFileError if unable to create some
-       directory along the way (eg. some sub-path exists, but is a file
-       rather than a directory).  If 'verbose' is true, print a one-line
-       summary of each mkdir to stdout.  Return the list of directories
-       actually created."""
+def mkpath(name, mode=0777, verbose=1, dry_run=0):
+    """Create a directory and any missing ancestor directories.
+
+    If the directory already exists (or if 'name' is the empty string, which
+    means the current directory, which of course exists), then do nothing.
+    Raise DistutilsFileError if unable to create some directory along the way
+    (eg. some sub-path exists, but is a file rather than a directory).
+    If 'verbose' is true, print a one-line summary of each mkdir to stdout.
+    Return the list of directories actually created.
+    """
 
     global _path_created
 
     # Detect a common bug -- name is None
-    if not isinstance(name, StringTypes):
+    if not isinstance(name, basestring):
         raise DistutilsInternalError, \
               "mkpath: 'name' must be a string (got %r)" % (name,)
 
@@ -77,19 +77,16 @@ def mkpath (name, mode=0777, verbose=1, dry_run=0):
         _path_created[abs_head] = 1
     return created_dirs
 
-# mkpath ()
+def create_tree(base_dir, files, mode=0777, verbose=1, dry_run=0):
+    """Create all the empty directories under 'base_dir' needed to put 'files'
+    there.
 
-
-def create_tree (base_dir, files, mode=0777, verbose=1, dry_run=0):
-
-    """Create all the empty directories under 'base_dir' needed to
-       put 'files' there.  'base_dir' is just the a name of a directory
-       which doesn't necessarily exist yet; 'files' is a list of filenames
-       to be interpreted relative to 'base_dir'.  'base_dir' + the
-       directory portion of every file in 'files' will be created if it
-       doesn't already exist.  'mode', 'verbose' and 'dry_run' flags are as
-       for 'mkpath()'."""
-
+    'base_dir' is just the a name of a directory which doesn't necessarily
+    exist yet; 'files' is a list of filenames to be interpreted relative to
+    'base_dir'.  'base_dir' + the directory portion of every file in 'files'
+    will be created if it doesn't already exist.  'mode', 'verbose' and
+    'dry_run' flags are as for 'mkpath()'.
+    """
     # First get the list of directories to create
     need_dir = {}
     for file in files:
@@ -101,35 +98,27 @@ def create_tree (base_dir, files, mode=0777, verbose=1, dry_run=0):
     for dir in need_dirs:
         mkpath(dir, mode, verbose=verbose, dry_run=dry_run)
 
-# create_tree ()
+def copy_tree(src, dst, preserve_mode=1, preserve_times=1,
+              preserve_symlinks=0, update=0, verbose=1, dry_run=0):
+    """Copy an entire directory tree 'src' to a new location 'dst'.
 
+    Both 'src' and 'dst' must be directory names.  If 'src' is not a
+    directory, raise DistutilsFileError.  If 'dst' does not exist, it is
+    created with 'mkpath()'.  The end result of the copy is that every
+    file in 'src' is copied to 'dst', and directories under 'src' are
+    recursively copied to 'dst'.  Return the list of files that were
+    copied or might have been copied, using their output name.  The
+    return value is unaffected by 'update' or 'dry_run': it is simply
+    the list of all files under 'src', with the names changed to be
+    under 'dst'.
 
-def copy_tree (src, dst,
-               preserve_mode=1,
-               preserve_times=1,
-               preserve_symlinks=0,
-               update=0,
-               verbose=1,
-               dry_run=0):
-
-    """Copy an entire directory tree 'src' to a new location 'dst'.  Both
-       'src' and 'dst' must be directory names.  If 'src' is not a
-       directory, raise DistutilsFileError.  If 'dst' does not exist, it is
-       created with 'mkpath()'.  The end result of the copy is that every
-       file in 'src' is copied to 'dst', and directories under 'src' are
-       recursively copied to 'dst'.  Return the list of files that were
-       copied or might have been copied, using their output name.  The
-       return value is unaffected by 'update' or 'dry_run': it is simply
-       the list of all files under 'src', with the names changed to be
-       under 'dst'.
-
-       'preserve_mode' and 'preserve_times' are the same as for
-       'copy_file'; note that they only apply to regular files, not to
-       directories.  If 'preserve_symlinks' is true, symlinks will be
-       copied as symlinks (on platforms that support them!); otherwise
-       (the default), the destination of the symlink will be copied.
-       'update' and 'verbose' are the same as for 'copy_file'."""
-
+    'preserve_mode' and 'preserve_times' are the same as for
+    'copy_file'; note that they only apply to regular files, not to
+    directories.  If 'preserve_symlinks' is true, symlinks will be
+    copied as symlinks (on platforms that support them!); otherwise
+    (the default), the destination of the symlink will be copied.
+    'update' and 'verbose' are the same as for 'copy_file'.
+    """
     from distutils.file_util import copy_file
 
     if not dry_run and not os.path.isdir(src):
@@ -174,10 +163,8 @@ def copy_tree (src, dst,
 
     return outputs
 
-# copy_tree ()
-
-# Helper for remove_tree()
 def _build_cmdtuple(path, cmdtuples):
+    """Helper for remove_tree()."""
     for f in os.listdir(path):
         real_f = os.path.join(path,f)
         if os.path.isdir(real_f) and not os.path.islink(real_f):
@@ -186,10 +173,11 @@ def _build_cmdtuple(path, cmdtuples):
             cmdtuples.append((os.remove, real_f))
     cmdtuples.append((os.rmdir, path))
 
+def remove_tree(directory, verbose=1, dry_run=0):
+    """Recursively remove an entire directory tree.
 
-def remove_tree (directory, verbose=1, dry_run=0):
-    """Recursively remove an entire directory tree.  Any errors are ignored
-    (apart from being reported to stdout if 'verbose' is true).
+    Any errors are ignored (apart from being reported to stdout if 'verbose'
+    is true).
     """
     from distutils.util import grok_environment_error
     global _path_created
@@ -211,10 +199,10 @@ def remove_tree (directory, verbose=1, dry_run=0):
             log.warn(grok_environment_error(
                     exc, "error removing %s: " % directory))
 
-
 def ensure_relative(path):
-    """Take the full path 'path', and make it a relative path so
-    it can be the second argument to os.path.join().
+    """Take the full path 'path', and make it a relative path.
+
+    This is useful to make 'path' the second argument to os.path.join().
     """
     drive, path = os.path.splitdrive(path)
     if path[0:1] == os.sep:
