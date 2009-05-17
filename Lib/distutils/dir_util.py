f764638232d328b19b95eb6f5cdb8532076e7418
Tarek Ziadé <ziade.tarek@gmail.com>
2009-05-17 11:11:57 +0000

    removed sys.platform == 'mac' usage in distutils.dir_util

--- Lib/distutils/dir_util.py
+++ Lib/distutils/dir_util.py
@@ -212,14 +212,11 @@ def remove_tree (directory, verbose=1, dry_run=0):
                     exc, "error removing %s: " % directory))
 
 
-def ensure_relative (path):
+def ensure_relative(path):
     """Take the full path 'path', and make it a relative path so
     it can be the second argument to os.path.join().
     """
     drive, path = os.path.splitdrive(path)
-    if sys.platform == 'mac':
-        return os.sep + path
-    else:
-        if path[0:1] == os.sep:
-            path = drive + path[1:]
-        return path
+    if path[0:1] == os.sep:
+        path = drive + path[1:]
+    return path
