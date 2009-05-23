376fe8a162f674d0628d0673747427241232d79a
R. David Murray <rdmurray@bitdance.com>
2009-05-23 02:36:15 +0000

    Merged revisions 72838-72839 via svnmerge from svn+ssh://pythondev@svn.python.org/python/trunk
    ........
      r72838 | r.david.murray | 2009-05-22 22:16:58 -0400 (Fri, 22 May 2009) | 3 lines
    
      Don't be so wordy in requires('network') in case other tests
      are added later, and skip the existing test if SSL is not available.
    ........
      r72839 | r.david.murray | 2009-05-22 22:19:36 -0400 (Fri, 22 May 2009) | 2 lines
    
      Fix spelling left over from testing.
    ........

--- Lib/test/test_smtpnet.py
+++ Lib/test/test_smtpnet.py
@@ -4,17 +4,14 @@ import unittest
 from test import support
 import smtplib
 
-support.requires(
-    "network",
-    "use of network resource is not enabled and "
-    "test requires Internet access for communication with smtp.gmail.com:465",
-    )
+support.requires("network")
 
 class SmtpSSLTest(unittest.TestCase):
     testServer = 'smtp.gmail.com'
     remotePort = 465
 
     def test_connect(self):
+        support.get_attribute(smtplib, 'SMTP_SSL')
         server = smtplib.SMTP_SSL(self.testServer, self.remotePort)
         server.ehlo()
         server.quit()
