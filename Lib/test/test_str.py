4f2d7cdd3bc1bbdacd3173dafb2eeaad1a824323
Eric Smith <eric@trueblade.com>
2009-05-23 14:04:31 +0000

    Merged revisions 72848 via svnmerge from svn+ssh://pythondev@svn.python.org/python/trunk
    ........
      r72848 | eric.smith | 2009-05-23 09:56:13 -0400 (Sat, 23 May 2009) | 1 line
    
      Issue 6089: str.format raises SystemError.
    ........

--- Lib/test/test_str.py
+++ Lib/test/test_str.py
@@ -351,6 +351,10 @@ class StrTest(
         self.assertRaises(ValueError, "{:s}".format)
         self.assertRaises(ValueError, "{}".format)
 
+        # issue 6089
+        self.assertRaises(ValueError, "{0[0]x}".format, [None])
+        self.assertRaises(ValueError, "{0[0](10)}".format, [None])
+
         # can't have a replacement on the field name portion
         self.assertRaises(TypeError, '{0[{1}]}'.format, 'abcdefg', 4)
 
