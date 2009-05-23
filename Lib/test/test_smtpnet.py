060c691616e20a66432d209b50deddb4693a9a53
R. David Murray <rdmurray@bitdance.com>
2009-05-23 02:42:08 +0000

    Merged revisions 72841 via svnmerge from svn+ssh://pythondev@svn.python.org/python/branches/py3k
    ................
      r72841 | r.david.murray | 2009-05-22 22:36:15 -0400 (Fri, 22 May 2009) | 14 lines
    
      Merged revisions 72838-72839 via svnmerge from
      svn+ssh://pythondev@svn.python.org/python/trunk
    
      ........
        r72838 | r.david.murray | 2009-05-22 22:16:58 -0400 (Fri, 22 May 2009) | 3 lines
    
        Don't be so wordy in requires('network') in case other tests
        are added later, and skip the existing test if SSL is not available.
      ........
        r72839 | r.david.murray | 2009-05-22 22:19:36 -0400 (Fri, 22 May 2009) | 2 lines
    
        Fix spelling left over from testing.
      ........
    ................

--- Lib/test/test_smtpnet.py
+++ Lib/test/test_smtpnet.py
@@ -4,17 +4,15 @@ import unittest
 from test import support
 import smtplib
 
-support.requires(
-    "network",
-    "use of network resource is not enabled and "
-    "test requires Internet access for communication with smtp.gmail.com:465",
-    )
+support.requires("network")
 
 class SmtpSSLTest(unittest.TestCase):
     testServer = 'smtp.gmail.com'
     remotePort = 465
 
     def test_connect(self):
+        #Skip test silently if no SSL; in 3.1 we use SkipTest
+        if not hasattr(smtplib, 'SMTP_SSL'): return
         server = smtplib.SMTP_SSL(self.testServer, self.remotePort)
         server.ehlo()
         server.quit()
