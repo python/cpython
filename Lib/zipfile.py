0b09c42ffec7b56b7f94c11d160ca083944c9cf8
Martin v. Löwis <martin@v.loewis.de>
2009-05-24 19:30:52 +0000

    Issue #6050: Don't fail extracting a directory from a zipfile if the directory already exists.

--- Lib/zipfile.py
+++ Lib/zipfile.py
@@ -971,7 +971,8 @@ class ZipFile:
             os.makedirs(upperdirs)
 
         if member.filename[-1] == '/':
-            os.mkdir(targetpath)
+            if not os.path.isdir(targetpath):
+                os.mkdir(targetpath)
             return targetpath
 
         source = self.open(member, pwd=pwd)
