e6fa26b699b1c909153bd4f7e8385bed8a1bab80
Raymond Hettinger <python@rcn.com>
2009-06-03 17:21:17 +0000

    Another hidden treasure.

--- Lib/antigravity.py
+++ Lib/antigravity.py
@@ -1,4 +1,17 @@
 
 import webbrowser
+import hashlib
 
 webbrowser.open("http://xkcd.com/353/")
+
+def geohash(latitude, longitude, datedow):
+    '''Compute geohash() using the Munroe algorithm.
+
+    >>> geohash(37.421542, -122.085589, b'2005-05-26-10458.68')
+    37.857713 -122.544543
+
+    '''
+    # http://xkcd.com/426/
+    h = hashlib.md5(datedow).hexdigest()
+    p, q = [('%f' % float.fromhex('0.' + x)) for x in (h[:16], h[16:32])]
+    print('%d%s %d%s' % (latitude, p[1:], longitude, q[1:]))
