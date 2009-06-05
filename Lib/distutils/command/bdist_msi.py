3b5d5fb12348c988c8e370c8d485368b9c00b4c5
Tarek Ziadé <ziade.tarek@gmail.com>
2009-06-05 13:37:29 +0000

    reverting r72823 : Python trunk has to use latin-1 encoding

--- Lib/distutils/command/bdist_msi.py
+++ Lib/distutils/command/bdist_msi.py
@@ -1,5 +1,5 @@
-# -*- coding: utf-8 -*-
-# Copyright (C) 2005, 2006 Martin von LÃ¶wis
+# -*- coding: iso-8859-1 -*-
+# Copyright (C) 2005, 2006 Martin von Löwis
 # Licensed to PSF under a Contributor Agreement.
 # The bdist_wininst command proper
 # based on bdist_wininst
