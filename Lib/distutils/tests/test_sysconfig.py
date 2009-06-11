6b16c00b926bf5e725cb41a4c48024bf324f23c2
Tarek Ziadé <ziade.tarek@gmail.com>
2009-06-11 08:26:40 +0000

    Merged revisions 73341 via svnmerge from svn+ssh://pythondev@svn.python.org/python/trunk
    ........
      r73341 | tarek.ziade | 2009-06-11 10:12:20 +0200 (Thu, 11 Jun 2009) | 1 line
    
      Fixed #5201: now distutils.sysconfig.parse_makefile() understands '53264' in Makefiles
    ........

--- Lib/distutils/tests/test_sysconfig.py
+++ Lib/distutils/tests/test_sysconfig.py
@@ -2,11 +2,20 @@
 
 from distutils import sysconfig
 import os
+import test
 import unittest
 
 from test.test_support import TESTFN
 
 class SysconfigTestCase(unittest.TestCase):
+    def setUp(self):
+        super(SysconfigTestCase, self).setUp()
+        self.makefile = None
+
+    def tearDown(self):
+        if self.makefile is not None:
+            os.unlink(self.makefile)
+        super(SysconfigTestCase, self).tearDown()
 
     def test_get_config_h_filename(self):
         config_h = sysconfig.get_config_h_filename()
@@ -51,8 +60,22 @@ class SysconfigTestCase(unittest.TestCase):
         self.assert_(isinstance(cvars, dict))
         self.assert_(cvars)
 
+    def test_parse_makefile_literal_dollar(self):
+        self.makefile = test.test_support.TESTFN
+        fd = open(self.makefile, 'w')
+        fd.write(r"CONFIG_ARGS=  '--arg1=optarg1' 'ENV=\$$LIB'" '\n')
+        fd.write('VAR=$OTHER\nOTHER=foo')
+        fd.close()
+        d = sysconfig.parse_makefile(self.makefile)
+        self.assertEquals(d, {'CONFIG_ARGS': r"'--arg1=optarg1' 'ENV=\$LIB'",
+                              'OTHER': 'foo'})
+
 
 def test_suite():
     suite = unittest.TestSuite()
     suite.addTest(unittest.makeSuite(SysconfigTestCase))
     return suite
+
+
+if __name__ == '__main__':
+    test.test_support.run_unittest(test_suite())
