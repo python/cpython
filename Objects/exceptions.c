cd70a455db67c579b835067b2a10e293e75e7a6e
Georg Brandl <georg@python.org>
2009-06-16 17:43:44 +0000

    Remove unused macro.

--- Objects/exceptions.c
+++ Objects/exceptions.c
@@ -9,7 +9,6 @@
 #include "structmember.h"
 #include "osdefs.h"
 
-#define MAKE_IT_NONE(x) (x) = Py_None; Py_INCREF(Py_None);
 #define EXC_MODULE_NAME "exceptions."
 
 /* NOTE: If the exception class hierarchy changes, don't forget to update
