e240316eeb8973ed93ba2196466b8b451e9f31c7
Benjamin Peterson <benjamin@python.org>
2009-06-27 22:03:18 +0000

    version to 3.1.1a0

--- Include/patchlevel.h
+++ Include/patchlevel.h
@@ -18,12 +18,12 @@
 /*--start constants--*/
 #define PY_MAJOR_VERSION	3
 #define PY_MINOR_VERSION	1
-#define PY_MICRO_VERSION	0
-#define PY_RELEASE_LEVEL	PY_RELEASE_LEVEL_FINAL
+#define PY_MICRO_VERSION	1
+#define PY_RELEASE_LEVEL	PY_RELEASE_LEVEL_ALPHA
 #define PY_RELEASE_SERIAL	0
 
 /* Version as a string */
-#define PY_VERSION      	"3.1"
+#define PY_VERSION      	"3.1.1a0"
 /*--end constants--*/
 
 /* Subversion Revision number of this file (not of the repository) */
