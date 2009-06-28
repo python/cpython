47065b01e73fdcd4d326d98259a18fa42065bd5e
Georg Brandl <georg@python.org>
2009-06-28 12:10:18 +0000

    Remove stray pychecker directive.

--- Lib/email/mime/nonmultipart.py
+++ Lib/email/mime/nonmultipart.py
@@ -14,13 +14,9 @@ from email.mime.base import MIMEBase
 class MIMENonMultipart(MIMEBase):
     """Base class for MIME multipart/* type messages."""
 
-    __pychecker__ = 'unusednames=payload'
-
     def attach(self, payload):
         # The public API prohibits attaching multiple subparts to MIMEBase
         # derived subtypes since none of them are, by definition, of content
         # type multipart/*
         raise errors.MultipartConversionError(
             'Cannot attach additional subparts to non-multipart/*')
-
-    del __pychecker__
