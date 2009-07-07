5f587b41669885f94fe71b4cba83701d8d81bd2d
Amaury Forgeot d'Arc <amauryfa@gmail.com>
2009-07-07 06:53:22 +0000

    Merged revisions 73874 via svnmerge from svn+ssh://pythondev@svn.python.org/python/branches/py3k
    ................
      r73874 | amaury.forgeotdarc | 2009-07-07 08:51:26 +0200 (mar., 07 juil. 2009) | 9 lines
    
      Merged revisions 73873 via svnmerge from
      svn+ssh://pythondev@svn.python.org/python/trunk
    
      ........
        r73873 | amaury.forgeotdarc | 2009-07-07 08:49:41 +0200 (mar., 07 juil. 2009) | 2 lines
    
        #6420: Fix a compilation warning in the nis module, for OpenBSD and FreeBSD.
      ........
    ................

--- Modules/nismodule.c
+++ Modules/nismodule.c
@@ -89,7 +89,7 @@ nis_mapname (char *map, int *pfix)
 	return map;
 }
 
-#ifdef __APPLE__
+#if defined(__APPLE__) || defined(__OpenBSD__) || defined(__FreeBSD__)
 typedef int (*foreachfunc)(unsigned long, char *, int, char *, int, void *);
 #else
 typedef int (*foreachfunc)(int, char *, int, char *, int, char *);
