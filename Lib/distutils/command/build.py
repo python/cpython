15ccb3d3f741ceffd7b2dca67870bbee5cfe3a49
Tarek Ziadé <ziade.tarek@gmail.com>
2009-07-10 10:03:20 +0000

    Merged revisions 73925-73926 via svnmerge from svn+ssh://pythondev@svn.python.org/python/trunk
    ........
      r73925 | tarek.ziade | 2009-07-10 11:57:15 +0200 (Fri, 10 Jul 2009) | 1 line
    
      Added test coverage for distutils.command.build
    ........
      r73926 | tarek.ziade | 2009-07-10 12:00:21 +0200 (Fri, 10 Jul 2009) | 1 line
    
      cleaned up distutils.command.build
    ........

--- Lib/distutils/command/build.py
+++ Lib/distutils/command/build.py
@@ -9,11 +9,9 @@ from distutils.core import Command
 from distutils.errors import DistutilsOptionError
 from distutils.util import get_platform
 
-
 def show_compilers():
     from distutils.ccompiler import show_compilers
     show_compilers()
-
 
 class build(Command):
 
@@ -127,7 +125,6 @@ class build(Command):
         for cmd_name in self.get_sub_commands():
             self.run_command(cmd_name)
 
-
     # -- Predicates for the sub-command list ---------------------------
 
     def has_pure_modules(self):
@@ -141,7 +138,6 @@ class build(Command):
 
     def has_scripts(self):
         return self.distribution.has_scripts()
-
 
     sub_commands = [('build_py',      has_pure_modules),
                     ('build_clib',    has_c_libraries),
