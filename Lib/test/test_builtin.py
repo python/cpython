b222e514325a5fafdd9c1cd89b78879394f21754
Georg Brandl <georg@python.org>
2009-07-22 12:06:11 +0000

    Merged revisions 74169 via svnmerge from svn+ssh://svn.python.org/python/branches/py3k
    ................
      r74169 | georg.brandl | 2009-07-22 14:03:59 +0200 (Mi, 22 Jul 2009) | 9 lines
    
      Merged revisions 74167 via svnmerge from
      svn+ssh://pythondev@svn.python.org/python/trunk
    
      ........
        r74167 | georg.brandl | 2009-07-22 13:57:15 +0200 (Mi, 22 Jul 2009) | 1 line
    
        Issue #6540: Fixed crash for bytearray.translate() with invalid parameters.
      ........
    ................

--- Lib/test/test_builtin.py
+++ Lib/test/test_builtin.py
@@ -1229,6 +1229,11 @@ class BuiltinTest(unittest.TestCase):
         self.assertEqual(bin(-(2**65)), '-0b1' + '0' * 65)
         self.assertEqual(bin(-(2**65-1)), '-0b' + '1' * 65)
 
+    def test_bytearray_translate(self):
+        x = bytearray(b"abc")
+        self.assertRaises(ValueError, x.translate, b"1", 1)
+        self.assertRaises(TypeError, x.translate, b"1"*256, 1)
+
 class TestSorted(unittest.TestCase):
 
     def test_basic(self):
