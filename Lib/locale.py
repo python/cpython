b48828085917a0b1d1edaf310b8412a84e924edb
Mark Dickinson <dickinsm@gmail.com>
2009-08-04 21:57:18 +0000

    Merged revisions 74312 via svnmerge from svn+ssh://pythondev@svn.python.org/python/trunk
    ........
      r74312 | mark.dickinson | 2009-08-04 22:56:04 +0100 (Tue, 04 Aug 2009) | 4 lines
    
      Issue #6620: Slightly safer code for _grouping_intervals in the locale
      module.  Fixes a 'possible use before assignment' warning from pylint.
      Thanks Vincent Legoll.
    ........

--- Lib/locale.py
+++ Lib/locale.py
@@ -121,12 +121,15 @@ def localeconv():
 
 # Iterate over grouping intervals
 def _grouping_intervals(grouping):
+    last_interval = None
     for interval in grouping:
         # if grouping is -1, we are done
         if interval == CHAR_MAX:
             return
         # 0: re-use last group ad infinitum
         if interval == 0:
+            if last_interval is None:
+                raise ValueError("invalid grouping")
             while True:
                 yield last_interval
         yield interval
