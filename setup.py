bd4357384f03d16084b19c895d044f9772fcaf42
Guilherme Polo <ggpolo@gmail.com>
2009-08-16 14:44:14 +0000

    Merged revisions 74471-74472 via svnmerge from svn+ssh://pythondev/python/trunk
    ........
      r74471 | guilherme.polo | 2009-08-16 11:34:26 -0300 (Sun, 16 Aug 2009) | 1 line
    
      Issue #6244: Allow detect_tkinter to look for Tcl/Tk 8.6.
    ........
      r74472 | guilherme.polo | 2009-08-16 11:38:57 -0300 (Sun, 16 Aug 2009) | 1 line
    
      Wrong place for issue #6244.
    ........

--- setup.py
+++ setup.py
@@ -1314,8 +1314,8 @@ class PyBuildExt(build_ext):
         # The versions with dots are used on Unix, and the versions without
         # dots on Windows, for detection by cygwin.
         tcllib = tklib = tcl_includes = tk_includes = None
-        for version in ['8.5', '85', '8.4', '84', '8.3', '83', '8.2',
-                        '82', '8.1', '81', '8.0', '80']:
+        for version in ['8.6', '86', '8.5', '85', '8.4', '84', '8.3', '83',
+                        '8.2', '82', '8.1', '81', '8.0', '80']:
             tklib = self.compiler_obj.find_library_file(lib_dirs,
                                                         'tk' + version)
             tcllib = self.compiler_obj.find_library_file(lib_dirs,
