d22b9519d120d3a6a678f516c1ee020601c3ac3a
Georg Brandl <georg@python.org>
2009-09-04 08:17:04 +0000

    Issue #5275: In Cookie's Cookie.load(), properly handle non-string arguments as documented.

--- Lib/Cookie.py
+++ Lib/Cookie.py
@@ -624,7 +624,9 @@ class BaseCookie(dict):
         if type(rawdata) == type(""):
             self.__ParseString(rawdata)
         else:
-            self.update(rawdata)
+            # self.update() wouldn't call our custom __setitem__
+            for k, v in rawdata.items():
+                self[k] = v
         return
     # end load()
 
