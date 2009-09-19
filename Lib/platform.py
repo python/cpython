52c1794b98c959dfc4aee069c8e9af9b6e89814e
Georg Brandl <georg@python.org>
2009-09-19 08:43:16 +0000

    Update bug tracker reference.

--- Lib/platform.py
+++ Lib/platform.py
@@ -10,7 +10,7 @@
 """
 #    This module is maintained by Marc-Andre Lemburg <mal@egenix.com>.
 #    If you find problems, please submit bug reports/patches via the
-#    Python SourceForge Project Page and assign them to "lemburg".
+#    Python bug tracker (http://bugs.python.org) and assign them to "lemburg".
 #
 #    Note: Please keep this module compatible to Python 1.5.2.
 #
