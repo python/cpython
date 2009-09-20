814583820cd848d301a4cd30f7abcb214e414400
Ronald Oussoren <ronaldoussoren@mac.com>
2009-09-20 18:54:16 +0000

    Merge a newer version of libffi_osx, based on the version of libffi in OSX 10.6.1.
    This fixes issue6918

--- Modules/_ctypes/libffi_osx/x86/x86-ffi64.c
+++ Modules/_ctypes/libffi_osx/x86/x86-ffi64.c
@@ -55,7 +55,7 @@ ffi_call_unix64(
 /*	Register class used for passing given 64bit part of the argument.
 	These represent classes as documented by the PS ABI, with the exception
 	of SSESF, SSEDF classes, that are basically SSE class, just gcc will
-	use SF or DFmode move instead of DImode to avoid reformatting penalties.
+	use SF or DFmode move instead of DImode to avoid reformating penalties.
 
 	Similary we play games with INTEGERSI_CLASS to use cheaper SImode moves
 	whenever possible (upper half does contain padding).  */
