2a13b38edbfa90f05d37616fb72238b5e9c68590
Tarek Ziadé <ziade.tarek@gmail.com>
2009-09-21 13:41:08 +0000

    #6954: Fixed crash when using DISTUTILS_DEBUG flag in Distutils.

--- Lib/distutils/tests/test_dist.py
+++ Lib/distutils/tests/test_dist.py
@@ -200,6 +200,13 @@ class DistributionTestCase(support.TempdirManager,
         self.assertEquals(cmds, ['distutils.command', 'one', 'two'])
 
 
+    def test_announce(self):
+        # make sure the level is known
+        dist = Distribution()
+        args = ('ok',)
+        kwargs = {'level': 'ok2'}
+        self.assertRaises(ValueError, dist.announce, args, kwargs)
+
 class MetadataTestCase(support.TempdirManager, support.EnvironGuard,
                        unittest.TestCase):
 
