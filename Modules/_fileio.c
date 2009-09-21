31a508729d067be022f907b6396ab81e629b8b3a
Antoine Pitrou <solipsis@pitrou.net>
2009-09-21 21:36:48 +0000

    Merged revisions 75007 via svnmerge from svn+ssh://pythondev@svn.python.org/python/trunk
    ........
      r75007 | antoine.pitrou | 2009-09-21 23:17:48 +0200 (lun., 21 sept. 2009) | 7 lines
    
      Issue #6236, #6348: Fix various failures in the io module under AIX
      and other platforms, when using a non-gcc compiler. Patch by egreen.
    
      In addition, I made explicit the signedness of all bitfields in the
      IO library.
    ........

--- Modules/_fileio.c
+++ Modules/_fileio.c
@@ -30,10 +30,10 @@
 typedef struct {
 	PyObject_HEAD
 	int fd;
-	unsigned readable : 1;
-	unsigned writable : 1;
-	int seekable : 2; /* -1 means unknown */
-	int closefd : 1;
+	unsigned int readable : 1;
+	unsigned int writable : 1;
+	signed int seekable : 2; /* -1 means unknown */
+	signed int closefd : 1;
 	PyObject *weakreflist;
 } PyFileIOObject;
 
