db6e67c13cac55c58b112267cbba33c2d7cbaa10
Ezio Melotti <ezio.melotti@gmail.com>
2009-09-26 11:23:16 +0000

    Merged revisions 75070 via svnmerge from svn+ssh://pythondev@svn.python.org/python/trunk
    ........
      r75070 | ezio.melotti | 2009-09-26 14:20:53 +0300 (Sat, 26 Sep 2009) | 1 line
    
      #7000: document "sep" in capwords. Add a few tests
    ........

--- Lib/string.py
+++ Lib/string.py
@@ -43,15 +43,17 @@ del l
 
 # Capitalize the words in a string, e.g. " aBc  dEf " -> "Abc Def".
 def capwords(s, sep=None):
-    """capwords(s, [sep]) -> string
+    """capwords(s [,sep]) -> string
 
     Split the argument into words using split, capitalize each
     word using capitalize, and join the capitalized words using
-    join. Note that this replaces runs of whitespace characters by
-    a single space.
+    join.  If the optional second argument sep is absent or None,
+    runs of whitespace characters are replaced by a single space
+    and leading and trailing whitespace are removed, otherwise
+    sep is used to split and join the words.
 
     """
-    return (sep or ' ').join([x.capitalize() for x in s.split(sep)])
+    return (sep or ' ').join(x.capitalize() for x in s.split(sep))
 
 
 # Construct a translation string
