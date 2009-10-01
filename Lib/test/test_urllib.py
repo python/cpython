91abd6e68dfa4b0d9d91d7c5c05d5d384c2b2718
Senthil Kumaran <orsenthil@gmail.com>
2009-10-01 01:07:03 +0000

    Fix for issue7026 test_urllib: unsetting missing 'env' variable.

--- Lib/test/test_urllib.py
+++ Lib/test/test_urllib.py
@@ -103,7 +103,7 @@ class ProxyTests(unittest.TestCase):
         # Delete all proxy related env vars
         for k, v in os.environ.iteritems():
             if 'proxy' in k.lower():
-                del env[k]
+                del self.env[k]
 
     def tearDown(self):
         # Restore all proxy related env vars
