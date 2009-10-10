bd7164cbfabf7b5bca9d3c2a6671bb5b5f77d994
Georg Brandl <georg@python.org>
2009-10-10 21:47:31 +0000

    Show use of range() step argument nicely.

--- Demo/scripts/beer.py
+++ Demo/scripts/beer.py
@@ -1,14 +1,20 @@
 #! /usr/bin/env python
+
 # By GvR, demystified after a version by Fredrik Lundh.
+
 import sys
+
 n = 100
-if sys.argv[1:]: n = int(sys.argv[1])
+if sys.argv[1:]:
+    n = int(sys.argv[1])
+
 def bottle(n):
     if n == 0: return "no more bottles of beer"
     if n == 1: return "one bottle of beer"
     return str(n) + " bottles of beer"
-for i in range(n):
-    print bottle(n-i), "on the wall,"
-    print bottle(n-i) + "."
+
+for i in range(n, 0, -1):
+    print bottle(i), "on the wall,"
+    print bottle(i) + "."
     print "Take one down, pass it around,"
-    print bottle(n-i-1), "on the wall."
+    print bottle(i-1), "on the wall."
