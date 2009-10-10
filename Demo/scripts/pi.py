d3ae590d0420b632cc50c3ed3773138e5b4e41c0
Georg Brandl <georg@python.org>
2009-10-10 21:13:21 +0000

    Merged revisions 75315 via svnmerge from svn+ssh://pythondev@svn.python.org/python/trunk
    ........
      r75315 | georg.brandl | 2009-10-10 23:10:05 +0200 (Sa, 10 Okt 2009) | 1 line
    
      Remove unneeded "L" suffixes.
    ........

--- Demo/scripts/pi.py
+++ Demo/scripts/pi.py
@@ -12,7 +12,7 @@ import sys
 
 def main():
     k, a, b, a1, b1 = 2, 4, 1, 12, 4
-    while 1:
+    while True:
         # Next approximation
         p, q, k = k*k, 2*k+1, k+1
         a, b, a1, b1 = a1, b1, p*a+q*a1, p*b+q*b1
@@ -25,7 +25,6 @@ def main():
 
 def output(d):
     # Use write() to avoid spaces between the digits
-    # Use str() to avoid the 'L'
     sys.stdout.write(str(d))
     # Flush so the output is seen immediately
     sys.stdout.flush()
