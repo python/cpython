4271ecaf72f53e5bd4be964609ecddb9b95825ed
Georg Brandl <georg@python.org>
2009-10-11 08:48:28 +0000

    Update primes script.

--- Demo/scripts/primes.py
+++ Demo/scripts/primes.py
@@ -2,26 +2,29 @@
 
 # Print prime numbers in a given range
 
-def main():
-    import sys
-    min, max = 2, 0x7fffffff
-    if sys.argv[1:]:
-        min = int(eval(sys.argv[1]))
-        if sys.argv[2:]:
-            max = int(eval(sys.argv[2]))
-    primes(min, max)
-
 def primes(min, max):
-    if 2 >= min: print 2
+    if 2 >= min:
+        print 2
     primes = [2]
     i = 3
     while i <= max:
         for p in primes:
-            if i%p == 0 or p*p > i: break
-        if i%p <> 0:
+            if i % p == 0 or p*p > i:
+                break
+        if i % p != 0:
             primes.append(i)
-            if i >= min: print i
-        i = i+2
+            if i >= min:
+                print i
+        i += 2
+
+def main():
+    import sys
+    min, max = 2, 0x7fffffff
+    if sys.argv[1:]:
+        min = int(sys.argv[1])
+        if sys.argv[2:]:
+            max = int(sys.argv[2])
+    primes(min, max)
 
 if __name__ == "__main__":
     main()
