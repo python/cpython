dc0900b525b6c7fc2e07c4670ef1452a6d74a09c
Antoine Pitrou <solipsis@pitrou.net>
2009-10-19 18:22:37 +0000

    Merged revisions 75531-75532 via svnmerge from svn+ssh://pythondev@svn.python.org/python/trunk
    ........
      r75531 | antoine.pitrou | 2009-10-19 20:17:18 +0200 (lun., 19 oct. 2009) | 4 lines
    
      Issue #5833: Fix extra space character in readline completion with the
      GNU readline library version 6.0.
    ........
      r75532 | antoine.pitrou | 2009-10-19 20:20:21 +0200 (lun., 19 oct. 2009) | 3 lines
    
      NEWS entry for r75531.
    ........

--- Modules/readline.c
+++ Modules/readline.c
@@ -800,6 +800,10 @@ on_completion(const char *text, int state)
 static char **
 flex_complete(char *text, int start, int end)
 {
+#ifdef HAVE_RL_COMPLETION_APPEND_CHARACTER
+	rl_completion_append_character ='\0';
+	rl_completion_suppress_append = 0;
+#endif
 	Py_XDECREF(begidx);
 	Py_XDECREF(endidx);
 	begidx = PyLong_FromLong((long) start);
@@ -842,9 +846,6 @@ setup_readline(void)
 	rl_completer_word_break_characters =
 		strdup(" \t\n`~!@#$%^&*()-=+[{]}\\|;:'\",<>/?");
 		/* All nonalphanums except '.' */
-#ifdef HAVE_RL_COMPLETION_APPEND_CHARACTER
-	rl_completion_append_character ='\0';
-#endif
 
 	begidx = PyLong_FromLong(0L);
 	endidx = PyLong_FromLong(0L);
