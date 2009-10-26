9948da007bdede8ee745635104cc09ab8a19b14f
R. David Murray <rdmurray@bitdance.com>
2009-10-26 03:27:32 +0000

    Convert py2 print statement to print function in test skip.

--- Lib/test/test_threading.py
+++ Lib/test/test_threading.py
@@ -438,8 +438,8 @@ class ThreadJoinOnShutdown(unittest.TestCase):
         # Skip platforms with known problems forking from a worker thread.
         # See http://bugs.python.org/issue3863.
         if sys.platform in ('freebsd4', 'freebsd5', 'freebsd6', 'os2emx'):
-            print >>sys.stderr, ('Skipping test_3_join_in_forked_from_thread'
-                                 ' due to known OS bugs on'), sys.platform
+            print('Skipping test_3_join_in_forked_from_thread'
+                  ' due to known OS bugs on', sys.platform, file=sys.stderr)
             return
         script = """if 1:
             main_thread = threading.current_thread()
