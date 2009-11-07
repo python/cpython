bf4f928b261c1d384aeffa99551b779a3de5c06d
Benjamin Peterson <benjamin@python.org>
2009-11-07 01:04:38 +0000

    spelling

--- Parser/tokenizer.c
+++ Parser/tokenizer.c
@@ -943,7 +943,7 @@ tok_backup(register struct tok_state *tok, register int c)
 {
 	if (c != EOF) {
 		if (--tok->cur < tok->buf)
-			Py_FatalError("tok_backup: begin of buffer");
+			Py_FatalError("tok_backup: beginning of buffer");
 		if (*tok->cur != c)
 			*tok->cur = c;
 	}
