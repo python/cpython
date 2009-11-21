b9a49b7630ce7458f15a5c4d9dbefd5a927dab8d
Jesse Noller <jnoller@gmail.com>
2009-11-21 18:09:38 +0000

    Merged revisions 76438 via svnmerge from svn+ssh://pythondev@svn.python.org/python/trunk
    ........
      r76438 | jesse.noller | 2009-11-21 09:38:23 -0500 (Sat, 21 Nov 2009) | 1 line
    
      issue6615: Additional test for logging support in multiprocessing
    ........

--- Lib/test/test_multiprocessing.py
+++ Lib/test/test_multiprocessing.py
@@ -1723,6 +1723,26 @@ class _TestLogging(BaseTestCase):
         root_logger.setLevel(root_level)
         logger.setLevel(level=LOG_LEVEL)
 
+
+class _TestLoggingProcessName(BaseTestCase):
+
+    def handle(self, record):
+        assert record.processName == multiprocessing.current_process().name
+        self.__handled = True
+
+    def test_logging(self):
+        handler = logging.Handler()
+        handler.handle = self.handle
+        self.__handled = False
+        # Bypass getLogger() and side-effects
+        logger = logging.getLoggerClass()(
+                'multiprocessing.test.TestLoggingProcessName')
+        logger.addHandler(handler)
+        logger.propagate = False
+
+        logger.warn('foo')
+        assert self.__handled
+
 #
 # Test to verify handle verification, see issue 3321
 #
