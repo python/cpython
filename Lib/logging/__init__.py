ec1cced4d499fb865ed7960669e2cf0da159ddbe
Vinay Sajip <vinay_sajip@yahoo.co.uk>
2009-11-27 15:36:32 +0000

    Issue #7403: logging: Fixed possible race condition in lock creation.

--- Lib/logging/__init__.py
+++ Lib/logging/__init__.py
@@ -166,7 +166,10 @@ def addLevelName(level, levelName):
 #the lock would already have been acquired - so we need an RLock.
 #The same argument applies to Loggers and Manager.loggerDict.
 #
-_lock = None
+if thread:
+    _lock = threading.RLock()
+else:
+    _lock = None
 
 def _acquireLock():
     """
@@ -174,9 +177,6 @@ def _acquireLock():
 
     This should be released with _releaseLock().
     """
-    global _lock
-    if (not _lock) and thread:
-        _lock = threading.RLock()
     if _lock:
         _lock.acquire()
 
