875ada4b0fefd2e399719d728073b681a04d31b4
Mark Dickinson <dickinsm@gmail.com>
2009-11-28 12:52:39 +0000

    Merged revisions 76566 via svnmerge from svn+ssh://pythondev@svn.python.org/python/branches/py3k
    ................
      r76566 | mark.dickinson | 2009-11-28 12:48:43 +0000 (Sat, 28 Nov 2009) | 18 lines
    
      Merged revisions 76432,76558 via svnmerge from
      svn+ssh://pythondev@svn.python.org/python/trunk
    
      ........
        r76432 | mark.dickinson | 2009-11-20 19:30:22 +0000 (Fri, 20 Nov 2009) | 5 lines
    
        Issue #7272:  Add configure test to detect whether sem_open works
        properly, and use this to skip test_multiprocessing on platforms
        where sem_open raises a signal.  This should fix some FreeBSD buildbot
        failures for test_multiprocessing.
      ........
        r76558 | mark.dickinson | 2009-11-28 10:44:20 +0000 (Sat, 28 Nov 2009) | 4 lines
    
        Issue #7272, continued:  don't re-use existing HAVE_BROKEN_POSIX_SEMAPHORES
        to indicate that semaphores aren't available;  define a new variable
        POSIX_SEMAPHORES_NOT_ENABLED instead.
      ........
    ................

--- Modules/_multiprocessing/multiprocessing.c
+++ Modules/_multiprocessing/multiprocessing.c
@@ -263,7 +263,8 @@ PyInit__multiprocessing(void)
 	Py_INCREF(&ConnectionType);	
 	PyModule_AddObject(module, "Connection", (PyObject*)&ConnectionType);
 
-#if defined(MS_WINDOWS) || defined(HAVE_SEM_OPEN)
+#if defined(MS_WINDOWS) ||						\
+  (defined(HAVE_SEM_OPEN) && !defined(POSIX_SEMAPHORES_NOT_ENABLED))
 	/* Add SemLock type to module */
 	if (PyType_Ready(&SemLockType) < 0)
 		return NULL;
@@ -311,7 +312,7 @@ PyInit__multiprocessing(void)
 		Py_DECREF(temp); Py_DECREF(value); return NULL; }	  \
 	Py_DECREF(value)
 	
-#ifdef HAVE_SEM_OPEN
+#if defined(HAVE_SEM_OPEN) && !defined(POSIX_SEMAPHORES_NOT_ENABLED)
 	ADD_FLAG(HAVE_SEM_OPEN);
 #endif
 #ifdef HAVE_SEM_TIMEDWAIT
