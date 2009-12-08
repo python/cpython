702798a2abb238a87cd94947dd13c82ad10fc67c
Antoine Pitrou <solipsis@pitrou.net>
2009-12-08 15:57:31 +0000

    Merged revisions 76708 via svnmerge from svn+ssh://pythondev@svn.python.org/python/trunk
    ........
      r76708 | antoine.pitrou | 2009-12-08 16:40:51 +0100 (mar., 08 déc. 2009) | 4 lines
    
      Issue #6986: Fix crash in the JSON C accelerator when called with the
      wrong parameter types.  Patch by Victor Stinner.
    ........

--- Lib/json/tests/test_speedups.py
+++ Lib/json/tests/test_speedups.py
@@ -1,8 +1,7 @@
 import decimal
 from unittest import TestCase
 
-from json import decoder
-from json import encoder
+from json import decoder, encoder, scanner
 
 class TestSpeedups(TestCase):
     def test_scanstring(self):
@@ -13,3 +12,13 @@ class TestSpeedups(TestCase):
         self.assertEquals(encoder.encode_basestring_ascii.__module__, "_json")
         self.assertTrue(encoder.encode_basestring_ascii is
                           encoder.c_encode_basestring_ascii)
+
+class TestDecode(TestCase):
+    def test_make_scanner(self):
+        self.assertRaises(AttributeError, scanner.c_make_scanner, 1)
+
+    def test_make_encoder(self):
+        self.assertRaises(TypeError, encoder.c_make_encoder,
+            (True, False),
+            b"\xCD\x7D\x3D\x4E\x12\x4C\xF9\x79\xD7\x52\xBA\x82\xF2\x27\x4A\x7D\xA0\xCA\x75",
+            None)
