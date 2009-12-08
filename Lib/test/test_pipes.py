e2e190d35926b1b2301187d7168d976e66e0b6c5
Antoine Pitrou <solipsis@pitrou.net>
2009-12-08 19:53:23 +0000

    Merged revisions 76720 via svnmerge from svn+ssh://pythondev@svn.python.org/python/trunk
    ........
      r76720 | antoine.pitrou | 2009-12-08 20:46:38 +0100 (mar., 08 déc. 2009) | 3 lines
    
      Make test_pipes a little bit more robust.
    ........

--- Lib/test/test_pipes.py
+++ Lib/test/test_pipes.py
@@ -2,7 +2,7 @@ import pipes
 import os
 import string
 import unittest
-from test.support import TESTFN, run_unittest, unlink
+from test.support import TESTFN, run_unittest, unlink, reap_children
 
 if os.name != 'posix':
     raise unittest.SkipTest('pipes module only works on posix')
@@ -36,7 +36,11 @@ class SimplePipeTests(unittest.TestCase):
         open(TESTFN, 'w').write('hello world #2')
         t = pipes.Template()
         t.append(s_command + ' < $IN', pipes.FILEIN_STDOUT)
-        self.assertEqual(t.open(TESTFN, 'r').read(), 'HELLO WORLD #2')
+        f = t.open(TESTFN, 'r')
+        try:
+            self.assertEqual(f.read(), 'HELLO WORLD #2')
+        finally:
+            f.close()
 
     def testEmptyPipeline1(self):
         # copy through empty pipe
@@ -52,7 +56,11 @@ class SimplePipeTests(unittest.TestCase):
         d = 'empty pipeline test READ'
         open(TESTFN, 'w').write(d)
         t=pipes.Template()
-        self.assertEqual(t.open(TESTFN, 'r').read(), d)
+        f = t.open(TESTFN, 'r')
+        try:
+            self.assertEqual(f.read(), d)
+        finally:
+            f.close()
 
     def testEmptyPipeline3(self):
         # write through empty pipe
@@ -185,6 +193,7 @@ class SimplePipeTests(unittest.TestCase):
 
 def test_main():
     run_unittest(SimplePipeTests)
+    reap_children()
 
 if __name__ == "__main__":
     test_main()
