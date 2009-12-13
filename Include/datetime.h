e3cbabd6d842ee074eda2d8e624928b9c1f79dbe
Benjamin Peterson <benjamin@python.org>
2009-12-13 21:21:43 +0000

    initialize to NULL

--- Include/datetime.h
+++ Include/datetime.h
@@ -183,7 +183,7 @@ typedef struct {
 #else
 
 /* Define global variable for the C API and a macro for setting it. */
-static PyDateTime_CAPI *PyDateTimeAPI;
+static PyDateTime_CAPI *PyDateTimeAPI = NULL;
 
 #define PyDateTime_IMPORT \
         PyDateTimeAPI = (PyDateTime_CAPI*) PyCObject_Import("datetime", \
