4b2c064ed85eb8efa2ec6ecc0e0da7bdf0a48175
Martin v. Löwis <martin@v.loewis.de>
2010-01-02 09:53:18 +0000

    Make script work with 2.5.

--- PCbuild/build_ssl.py
+++ PCbuild/build_ssl.py
@@ -1,3 +1,4 @@
+from __future__ import with_statement
 # Script for building the _ssl and _hashlib modules for Windows.
 # Uses Perl to setup the OpenSSL environment correctly
 # and build OpenSSL, then invokes a simple nmake session
