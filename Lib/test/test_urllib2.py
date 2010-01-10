2e3da14d8b6fc13f1514dce8420c992d7aaf13f8
Senthil Kumaran <orsenthil@gmail.com>
2010-01-10 17:35:05 +0000

    Fixed issue7648 - test_urllib2 fails on Windows if not run from C:

--- Lib/test/test_urllib2.py
+++ Lib/test/test_urllib2.py
@@ -21,8 +21,7 @@ class TrivialTests(unittest.TestCase):
 
         # XXX Name hacking to get this to work on Windows.
         fname = os.path.abspath(urllib2.__file__).replace('\\', '/')
-        if fname[1:2] == ":":
-            fname = fname[2:]
+
         # And more hacking to get it to work on MacOS. This assumes
         # urllib.pathname2url works, unfortunately...
         if os.name == 'mac':
@@ -32,7 +31,11 @@ class TrivialTests(unittest.TestCase):
             fname = os.expand(fname)
             fname = fname.translate(string.maketrans("/.", "./"))
 
-        file_url = "file://%s" % fname
+        if os.name == 'nt':
+            file_url = "file:///%s" % fname
+        else:
+            file_url = "file://%s" % fname
+
         f = urllib2.urlopen(file_url)
 
         buf = f.read()
