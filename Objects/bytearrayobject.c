41d619a24bf48477128ea02aa5d1768d0dc9f005
Ezio Melotti <ezio.melotti@gmail.com>
2010-01-25 11:24:37 +0000

    #7775: fixed docstring for rpartition

--- Objects/bytearrayobject.c
+++ Objects/bytearrayobject.c
@@ -2112,7 +2112,7 @@ bytearray_partition(PyByteArrayObject *self, PyObject *sep_obj)
 }
 
 PyDoc_STRVAR(rpartition__doc__,
-"B.rpartition(sep) -> (tail, sep, head)\n\
+"B.rpartition(sep) -> (head, sep, tail)\n\
 \n\
 Searches for the separator sep in B, starting at the end of B,\n\
 and returns the part before it, the separator itself, and the\n\
