a2d465374099c582ef9bc042c2fe1be4776ee641
Ezio Melotti <ezio.melotti@gmail.com>
2010-01-30 07:22:54 +0000

    #7092: silence py3k warnings for deprecated modules

--- Lib/test/test_xmllib.py
+++ Lib/test/test_xmllib.py
@@ -15,13 +15,10 @@ testdoc = """\
 
 nsdoc = "<foo xmlns='URI' attr='val'/>"
 
-import warnings
-warnings.filterwarnings("ignore", ".* xmllib .* obsolete.*",
-                        DeprecationWarning, r'xmllib$')
-
 from test import test_support
 import unittest
-import xmllib
+# Silence Py3k warning
+xmllib = test_support.import_module('xmllib', deprecated=True)
 
 class XMLParserTestCase(unittest.TestCase):
 
