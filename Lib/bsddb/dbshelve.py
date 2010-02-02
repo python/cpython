8d3f130d41d0c3f437f37a18c624955ce8bf65d3
Ezio Melotti <ezio.melotti@gmail.com>
2010-02-02 15:57:45 +0000

    Fix idioms and a couple of py3k warnings. Patch by Florent Xicluna.

--- Lib/bsddb/dbshelve.py
+++ Lib/bsddb/dbshelve.py
@@ -157,7 +157,7 @@ class DBShelf(MutableMapping):
 
 
     def keys(self, txn=None):
-        if txn != None:
+        if txn is not None:
             return self.db.keys(txn)
         else:
             return self.db.keys()
@@ -185,7 +185,7 @@ class DBShelf(MutableMapping):
 
 
     def items(self, txn=None):
-        if txn != None:
+        if txn is not None:
             items = self.db.items(txn)
         else:
             items = self.db.items()
@@ -196,7 +196,7 @@ class DBShelf(MutableMapping):
         return newitems
 
     def values(self, txn=None):
-        if txn != None:
+        if txn is not None:
             values = self.db.values(txn)
         else:
             values = self.db.values()
