2d798ac619788e32cf15425802db0fa8179d6865
Benjamin Peterson <benjamin@python.org>
2010-02-06 18:26:27 +0000

    post release updates

--- Include/patchlevel.h
+++ Include/patchlevel.h
@@ -27,7 +27,7 @@
 #define PY_RELEASE_SERIAL	3
 
 /* Version as a string */
-#define PY_VERSION      	"2.7a3"
+#define PY_VERSION      	"2.7a3+"
 /*--end constants--*/
 
 /* Subversion Revision number of this file (not of the repository) */
