e64c442ff707c4278363247d6a51fb317a13f9e0
Georg Brandl <georg@python.org>
2010-02-08 22:37:52 +0000

    Fix undefined local.

--- Lib/plat-irix6/cdplayer.py
+++ Lib/plat-irix6/cdplayer.py
@@ -85,7 +85,7 @@ class Cdplayer:
         new.write(self.id + '.title:\t' + self.title + '\n')
         new.write(self.id + '.artist:\t' + self.artist + '\n')
         for i in range(1, len(self.track)):
-            new.write('%s.track.%r:\t%s\n' % (self.id, i, track))
+            new.write('%s.track.%r:\t%s\n' % (self.id, i, self.track[i]))
         old.close()
         new.close()
         posix.rename(filename + '.new', filename)
