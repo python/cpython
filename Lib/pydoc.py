c4d34b15caca7a81af559ec822f0747d45331bc7
Ezio Melotti <ezio.melotti@gmail.com>
2010-02-16 23:31:04 +0000

    Merged revisions 78207 via svnmerge from svn+ssh://pythondev@svn.python.org/python/trunk
    ........
      r78207 | ezio.melotti | 2010-02-17 01:26:09 +0200 (Wed, 17 Feb 2010) | 1 line
    
      #7930: fix stripid
    ........

--- Lib/pydoc.py
+++ Lib/pydoc.py
@@ -123,9 +123,7 @@ _re_stripid = re.compile(r' at 0x[0-9a-f]{6,16}(>+)$', re.IGNORECASE)
 def stripid(text):
     """Remove the hexadecimal id from a Python object representation."""
     # The behaviour of %p is implementation-dependent in terms of case.
-    if _re_stripid.search(repr(Exception)):
-        return _re_stripid.sub(r'\1', text)
-    return text
+    return _re_stripid.sub(r'\1', text)
 
 def _is_some_method(obj):
     return inspect.ismethod(obj) or inspect.ismethoddescriptor(obj)
