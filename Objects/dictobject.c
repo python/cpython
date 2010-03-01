56f1682a4462559b735c01c6cb8c06fd1afdc11b
Ezio Melotti <ezio.melotti@gmail.com>
2010-03-01 04:05:56 +0000

    Merged revisions 78515-78516,78522 via svnmerge from svn+ssh://pythondev@svn.python.org/python/trunk
    ........
      r78515 | georg.brandl | 2010-02-28 20:19:17 +0200 (Sun, 28 Feb 2010) | 1 line
    
      #8030: make builtin type docstrings more consistent: use "iterable" instead of "seq(uence)", use "new" to show that set() always returns a new object.
    ........
      r78516 | georg.brandl | 2010-02-28 20:26:37 +0200 (Sun, 28 Feb 2010) | 1 line
    
      The set types can also be called without arguments.
    ........
      r78522 | ezio.melotti | 2010-03-01 01:59:00 +0200 (Mon, 01 Mar 2010) | 1 line
    
      #8030: more docstring fix for builtin types.
    ........

--- Objects/dictobject.c
+++ Objects/dictobject.c
@@ -2222,12 +2222,12 @@ dict_iter(PyDictObject *dict)
 }
 
 PyDoc_STRVAR(dictionary_doc,
-"dict() -> new empty dictionary.\n"
+"dict() -> new empty dictionary\n"
 "dict(mapping) -> new dictionary initialized from a mapping object's\n"
-"    (key, value) pairs.\n"
-"dict(seq) -> new dictionary initialized as if via:\n"
+"    (key, value) pairs\n"
+"dict(iterable) -> new dictionary initialized as if via:\n"
 "    d = {}\n"
-"    for k, v in seq:\n"
+"    for k, v in iterable:\n"
 "        d[k] = v\n"
 "dict(**kwargs) -> new dictionary initialized with the name=value pairs\n"
 "    in the keyword argument list.  For example:  dict(one=1, two=2)");
