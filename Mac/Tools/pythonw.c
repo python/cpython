2a4ab816332a67150fcc8ece255fb1ee8e66af83
Ronald Oussoren <ronaldoussoren@mac.com>
2010-03-07 09:04:06 +0000

    Fix for issue #7998: pythonw didn't work when --with-framework-name was specified

--- Mac/Tools/pythonw.c
+++ Mac/Tools/pythonw.c
@@ -79,7 +79,7 @@ static char* get_python_path(void)
 	if (end[1] == '.') {
 		end++;
 	}
-	strcpy(end, "Resources/Python.app/Contents/MacOS/Python");
+	strcpy(end, "Resources/Python.app/Contents/MacOS/" PYTHONFRAMEWORK);
 
 	return g_path;
 }
