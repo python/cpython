3a629d1abef033bb3af6450e5b93378b783cf9af
Benjamin Peterson <benjamin@python.org>
2010-03-11 21:53:25 +0000

    use proper shebang lines

--- Lib/bsddb/dbshelve.py
+++ Lib/bsddb/dbshelve.py
@@ -1,4 +1,4 @@
-#!/bin/env python
+#!/usr/bin/env python
 #------------------------------------------------------------------------
 #           Copyright (c) 1997-2001 by Total Control Software
 #                         All Rights Reserved
