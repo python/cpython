b01f94324a95657b613f7d1ef7dca1a4e31af123
Georg Brandl <georg@python.org>
2010-03-13 10:59:09 +0000

    Merged revisions 78910 via svnmerge from svn+ssh://pythondev@svn.python.org/python/trunk
    ........
      r78910 | georg.brandl | 2010-03-13 11:54:12 +0100 (Sa, 13 Mär 2010) | 1 line
    
      Bump externals versions for doc build.
    ........

--- Doc/make.bat
+++ Doc/make.bat
@@ -34,10 +34,10 @@ echo.
 goto end
 
 :checkout
-svn co %SVNROOT%/external/Sphinx-0.6.3/sphinx tools/sphinx
-svn co %SVNROOT%/external/docutils-0.5/docutils tools/docutils
-svn co %SVNROOT%/external/Jinja-2.1.1/jinja2 tools/jinja2
-svn co %SVNROOT%/external/Pygments-0.11.1/pygments tools/pygments
+svn co %SVNROOT%/external/Sphinx-0.6.5/sphinx tools/sphinx
+svn co %SVNROOT%/external/docutils-0.6/docutils tools/docutils
+svn co %SVNROOT%/external/Jinja-2.3.1/jinja2 tools/jinja2
+svn co %SVNROOT%/external/Pygments-1.3.1/pygments tools/pygments
 goto end
 
 :update
