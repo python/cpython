08b36bdab44eaf6fb041780f6be967105e8c722c
Victor Stinner <victor.stinner@haypocalc.com>
2010-03-13 00:19:17 +0000

    Merged revisions 78886 via svnmerge from svn+ssh://pythondev@svn.python.org/python/trunk
    ........
      r78886 | victor.stinner | 2010-03-13 01:13:22 +0100 (sam., 13 mars 2010) | 2 lines
    
      Issue #7818: set().test_c_api() doesn't expect a set('abc'), modify the set.
    ........

--- Objects/setobject.c
+++ Objects/setobject.c
@@ -2360,11 +2360,25 @@ test_c_api(PySetObject *so)
 	PyObject *elem=NULL, *dup=NULL, *t, *f, *dup2, *x;
 	PyObject *ob = (PyObject *)so;
 	long hash;
+	PyObject *str;
 
-	/* Verify preconditions and exercise type/size checks */
+	/* Verify preconditions */
 	assert(PyAnySet_Check(ob));
 	assert(PyAnySet_CheckExact(ob));
 	assert(!PyFrozenSet_CheckExact(ob));
+
+	/* so.clear(); so |= set("abc"); */
+	str = PyUnicode_FromString("abc");
+	if (str == NULL)
+		return NULL;
+	set_clear_internal(so);
+	if (set_update_internal(so, str) == -1) {
+		Py_DECREF(str);
+		return NULL;
+	}
+	Py_DECREF(str);
+
+	/* Exercise type/size checks */
 	assert(PySet_Size(ob) == 3);
 	assert(PySet_GET_SIZE(ob) == 3);
 
