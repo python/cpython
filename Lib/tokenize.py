1613ed810801df8327ae6f55b7785fec3a9dc6bb
Benjamin Peterson <benjamin@python.org>
2010-03-18 22:34:15 +0000

    fix for files with coding cookies and BOMs

--- Lib/tokenize.py
+++ Lib/tokenize.py
@@ -333,9 +333,11 @@ def detect_encoding(readline):
             # This behaviour mimics the Python interpreter
             raise SyntaxError("unknown encoding: " + encoding)
 
-        if bom_found and codec.name != 'utf-8':
-            # This behaviour mimics the Python interpreter
-            raise SyntaxError('encoding problem: utf-8')
+        if bom_found:
+            if codec.name != 'utf-8':
+                # This behaviour mimics the Python interpreter
+                raise SyntaxError('encoding problem: utf-8')
+            encoding += '-sig'
         return encoding
 
     first = read_or_stop()
