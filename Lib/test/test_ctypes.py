f08a0176a77503a74079155f34a0174f3546510d
Florent Xicluna <florent.xicluna@gmail.com>
2010-03-22 15:02:46 +0000

    Get rid of buffer() in test_ctypes:  backport the 3.x tests.

--- Lib/test/test_ctypes.py
+++ Lib/test/test_ctypes.py
@@ -1,17 +1,15 @@
 import unittest
 
-from test.test_support import run_unittest, import_module, check_py3k_warnings
+from test.test_support import run_unittest, import_module
 #Skip tests if _ctypes module does not exist
 import_module('_ctypes')
 
 
 def test_main():
-    with check_py3k_warnings(("buffer.. not supported", DeprecationWarning),
-                             ("classic (int|long) division", DeprecationWarning)):
-        import ctypes.test
-        skipped, testcases = ctypes.test.get_tests(ctypes.test, "test_*.py", verbosity=0)
-        suites = [unittest.makeSuite(t) for t in testcases]
-        run_unittest(unittest.TestSuite(suites))
+    import ctypes.test
+    skipped, testcases = ctypes.test.get_tests(ctypes.test, "test_*.py", verbosity=0)
+    suites = [unittest.makeSuite(t) for t in testcases]
+    run_unittest(unittest.TestSuite(suites))
 
 if __name__ == "__main__":
     test_main()
