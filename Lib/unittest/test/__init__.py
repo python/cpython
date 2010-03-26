96d5de4385a1f3b2248a94cb621bbe1ced4a8c6d
Michael Foord <fuzzyman@voidspace.org.uk>
2010-03-26 00:03:38 +0000

    Remove incorrect docstring in unittest.test

--- Lib/unittest/test/__init__.py
+++ Lib/unittest/test/__init__.py
@@ -1,17 +1,3 @@
-"""Test suite for distutils.
-
-This test suite consists of a collection of test modules in the
-distutils.tests package.  Each test module has a name starting with
-'test' and contains a function test_suite().  The function is expected
-to return an initialized unittest.TestSuite instance.
-
-Tests for the command classes in the distutils.command package are
-included in distutils.tests as well, instead of using a separate
-distutils.command.tests package, since command identification is done
-by import rather than matching pre-defined names.
-
-"""
-
 import os
 import sys
 import unittest
