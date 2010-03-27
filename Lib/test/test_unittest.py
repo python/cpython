bf1fab4aa0b73376af66f3e42b4329d353c53629
Michael Foord <fuzzyman@voidspace.org.uk>
2010-03-27 13:45:04 +0000

    Merged revisions 79468 via svnmerge from svn+ssh://pythondev@svn.python.org/python/trunk
    ........
      r79468 | michael.foord | 2010-03-27 13:42:34 +0000 (Sat, 27 Mar 2010) | 1 line
    
      Rename the unittest test_suite function to not clash with a test module name (unittest.test.test_suite is now unambiguous).
    ........

--- Lib/test/test_unittest.py
+++ Lib/test/test_unittest.py
@@ -4,9 +4,8 @@ from test import support
 
 
 def test_main():
-    support.run_unittest(unittest.test.test_suite())
+    support.run_unittest(unittest.test.suite())
     support.reap_children()
-
 
 if __name__ == "__main__":
     test_main()
