e84089e3f1000ba1d060931e416dc148fe79d6eb
Martin v. Löwis <martin@v.loewis.de>
2010-04-08 17:38:32 +0000

    Issue #8204: Fix test_ttk notebook test by forcing focus.

--- Lib/lib-tk/test/test_ttk/test_widgets.py
+++ Lib/lib-tk/test/test_ttk/test_widgets.py
@@ -704,15 +704,19 @@ class NotebookTest(unittest.TestCase):
         self.nb.select(0)
 
         support.simulate_mouse_click(self.nb, 5, 5)
+        self.nb.focus_force()
         self.nb.event_generate('<Control-Tab>')
         self.assertEqual(self.nb.select(), str(self.child2))
+        self.nb.focus_force()
         self.nb.event_generate('<Shift-Control-Tab>')
         self.assertEqual(self.nb.select(), str(self.child1))
+        self.nb.focus_force()
         self.nb.event_generate('<Shift-Control-Tab>')
         self.assertEqual(self.nb.select(), str(self.child2))
 
         self.nb.tab(self.child1, text='a', underline=0)
         self.nb.enable_traversal()
+        self.nb.focus_force()
         self.nb.event_generate('<Alt-a>')
         self.assertEqual(self.nb.select(), str(self.child1))
 
