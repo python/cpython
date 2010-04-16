0f6fe9d7c04a6eff1835e3689cb15b532035de91
Victor Stinner <victor.stinner@haypocalc.com>
2010-04-16 13:28:05 +0000

    Issue #7606: XML-RPC traceback stored in X-traceback is now encoded to ASCII using backslashreplace error handler.

--- Lib/xmlrpc/server.py
+++ Lib/xmlrpc/server.py
@@ -499,7 +499,9 @@ class SimpleXMLRPCRequestHandler(BaseHTTPRequestHandler):
             if hasattr(self.server, '_send_traceback_header') and \
                     self.server._send_traceback_header:
                 self.send_header("X-exception", str(e))
-                self.send_header("X-traceback", traceback.format_exc())
+                trace = traceback.format_exc()
+                trace = str(trace.encode('ASCII', 'backslashreplace'), 'ASCII')
+                self.send_header("X-traceback", trace)
 
             self.send_header("Content-length", "0")
             self.end_headers()
