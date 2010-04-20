8e6ca8fb1909416be6dd6d25ad5e5a772f19e67e
Ronald Oussoren <ronaldoussoren@mac.com>
2010-04-20 20:59:37 +0000

    Fix for issue 7852: the DTD for OSX Plists has changed due to a change in the company name for Apple.

--- Lib/plistlib.py
+++ Lib/plistlib.py
@@ -224,7 +224,7 @@ def _escapeAndEncode(text):
 
 PLISTHEADER = """\
 <?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
 """
 
 class PlistWriter(DumbXMLWriter):
