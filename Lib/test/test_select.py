67f79e8c48ec459b2f66847abaa6c535f2ba96be
Benjamin Peterson <benjamin@python.org>
2010-04-20 21:02:36 +0000

    skip this test on windows, too #8459

--- Lib/test/test_select.py
+++ Lib/test/test_select.py
@@ -20,6 +20,10 @@ class SelectTestCase(unittest.TestCase):
         self.assertRaises(TypeError, select.select, [], [], [], "not a number")
 
     def test_returned_list_identity(self):
+        if sys.platform[:3] in ('win', 'mac', 'os2'):
+            if test_support.verbose:
+                print "can't easily test on this system"
+            return
         # See issue #8329
         r, w, x = select.select([], [], [], 1)
         self.assertFalse(r is w)
