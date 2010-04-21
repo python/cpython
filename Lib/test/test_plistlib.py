78118992ad6fa5c9a5c9d829ae2733a4bac96cd8
Ronald Oussoren <ronaldoussoren@mac.com>
2010-04-21 06:00:35 +0000

    Sync test_plistlib.py with plistlib.py

--- Lib/test/test_plistlib.py
+++ Lib/test/test_plistlib.py
@@ -9,7 +9,7 @@ from test import test_support
 
 # This test data was generated through Cocoa's NSDictionary class
 TESTDATA = """<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" \
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" \
 "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
 <plist version="1.0">
 <dict>
