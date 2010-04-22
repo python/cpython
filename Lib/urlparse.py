a01359dbbd5da853903981c17e3cfb29cea02172
Senthil Kumaran <orsenthil@gmail.com>
2010-04-22 06:07:30 +0000

    IPv6 parsing is not present in this branch. Removing the reference from the module docstring.

--- Lib/urlparse.py
+++ Lib/urlparse.py
@@ -5,9 +5,6 @@ urlparse module is based upon the following RFC specifications.
 RFC 3986 (STD66): "Uniform Resource Identifiers" by T. Berners-Lee, R. Fielding
 and L.  Masinter, January 2005.
 
-RFC 2732 : "Format for Literal IPv6 Addresses in URL's by R.Hinden, B.Carpenter
-and L.Masinter, December 1999.
-
 RFC2396:  "Uniform Resource Identifiers (URI)": Generic Syntax by T.
 Berners-Lee, R. Fielding, and L. Masinter, August 1998.
 
