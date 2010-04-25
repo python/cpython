f0f8f6a41729b58386cdb61dfe2c6eb719633122
Antoine Pitrou <solipsis@pitrou.net>
2010-04-25 22:09:32 +0000

    Merged revisions 80480 via svnmerge from svn+ssh://pythondev@svn.python.org/python/trunk
    ........
      r80480 | antoine.pitrou | 2010-04-25 23:15:50 +0200 (dim., 25 avril 2010) | 3 lines
    
      Replace a Lock with a better suited Event.
    ........

--- Lib/test/test_httpservers.py
+++ Lib/test/test_httpservers.py
@@ -32,14 +32,14 @@ class TestServerThread(threading.Thread):
         threading.Thread.__init__(self)
         self.request_handler = request_handler
         self.test_object = test_object
-        self.test_object.lock.acquire()
 
     def run(self):
         self.server = HTTPServer(('', 0), self.request_handler)
         self.test_object.PORT = self.server.socket.getsockname()[1]
-        self.test_object.lock.release()
+        self.test_object.server_started.set()
+        self.test_object = None
         try:
-            self.server.serve_forever()
+            self.server.serve_forever(0.05)
         finally:
             self.server.server_close()
 
@@ -49,13 +49,12 @@ class TestServerThread(threading.Thread):
 
 class BaseTestCase(unittest.TestCase):
     def setUp(self):
-        self.lock = threading.Lock()
+        self.server_started = threading.Event()
         self.thread = TestServerThread(self, self.request_handler)
         self.thread.start()
-        self.lock.acquire()
+        self.server_started.wait()
 
     def tearDown(self):
-        self.lock.release()
         self.thread.stop()
 
     def request(self, uri, method='GET', body=None, headers={}):
