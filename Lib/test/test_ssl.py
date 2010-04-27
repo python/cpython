ba5520ae2a70c75dd81c628333a3408c82667d45
Antoine Pitrou <solipsis@pitrou.net>
2010-04-27 22:08:08 +0000

    Merged revisions 80557 via svnmerge from svn+ssh://pythondev@svn.python.org/python/trunk
    (the bug was already fixed in py3k, just merging in the tests)
    
    ........
      r80557 | antoine.pitrou | 2010-04-28 00:03:37 +0200 (mer., 28 avril 2010) | 4 lines
    
      Issue #8086: In :func:`ssl.DER_cert_to_PEM_cert()`, fix missing newline
      before the certificate footer.  Patch by Kyle VanderBeek.
    ........

--- Lib/test/test_ssl.py
+++ Lib/test/test_ssl.py
@@ -95,6 +95,10 @@ class BasicTests(unittest.TestCase):
         p2 = ssl.DER_cert_to_PEM_cert(d1)
         d2 = ssl.PEM_cert_to_DER_cert(p2)
         self.assertEqual(d1, d2)
+        if not p2.startswith(ssl.PEM_HEADER + '\n'):
+            self.fail("DER-to-PEM didn't include correct header:\n%r\n" % p2)
+        if not p2.endswith('\n' + ssl.PEM_FOOTER + '\n'):
+            self.fail("DER-to-PEM didn't include correct footer:\n%r\n" % p2)
 
     def test_openssl_version(self):
         n = ssl.OPENSSL_VERSION_NUMBER
