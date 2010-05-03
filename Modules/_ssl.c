949476ee58affad6d428500d37a3a29f94e48c7d
Brett Cannon <bcannon@gmail.com>
2010-05-03 23:41:23 +0000

    Remove unneeded variable initialization.
    Found using Clang's static analyzer.

--- Modules/_ssl.c
+++ Modules/_ssl.c
@@ -464,7 +464,6 @@ static PyObject *PySSL_SSLdo_handshake(PySSLObject *self)
 
 	/* Actually negotiate SSL connection */
 	/* XXX If SSL_do_handshake() returns 0, it's also a failure. */
-	sockstate = 0;
 	do {
 		PySSL_BEGIN_ALLOW_THREADS
 		ret = SSL_do_handshake(self->ssl);
@@ -1198,7 +1197,6 @@ static PyObject *PySSL_SSLwrite(PySSLObject *self, PyObject *args)
 		goto error;
 	}
 	do {
-		err = 0;
 		PySSL_BEGIN_ALLOW_THREADS
 		len = SSL_write(self->ssl, buf.buf, buf.len);
 		err = SSL_get_error(self->ssl, len);
@@ -1317,7 +1315,6 @@ static PyObject *PySSL_SSLread(PySSLObject *self, PyObject *args)
 		}
 	}
 	do {
-		err = 0;
 		PySSL_BEGIN_ALLOW_THREADS
 		count = SSL_read(self->ssl, PyString_AsString(buf), len);
 		err = SSL_get_error(self->ssl, count);
