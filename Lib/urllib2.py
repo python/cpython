6057ba1f97ea19c484b03d5dd2f431e7767fefd1
Senthil Kumaran <orsenthil@gmail.com>
2010-05-08 03:11:50 +0000

    Fix Issue8656 - urllib2 mangles file://-scheme URLs

--- Lib/urllib2.py
+++ Lib/urllib2.py
@@ -1291,7 +1291,7 @@ class FileHandler(BaseHandler):
             if not host or \
                 (not port and socket.gethostbyname(host) in self.get_names()):
                 return addinfourl(open(localfile, 'rb'),
-                                  headers, 'file:'+file)
+                                  headers, 'file://'+ host + file)
         except OSError, msg:
             # urllib2 users shouldn't expect OSErrors coming from urlopen()
             raise URLError(msg)
