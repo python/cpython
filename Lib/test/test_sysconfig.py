c167fb5b7b8ac342722eb5100d56146ff7c1b476
Benjamin Peterson <benjamin@python.org>
2010-05-08 15:51:23 +0000

    Merged revisions 80986-80987 via svnmerge from svn+ssh://pythondev@svn.python.org/python/trunk
    ........
      r80986 | benjamin.peterson | 2010-05-08 10:41:44 -0500 (Sat, 08 May 2010) | 1 line
    
      r80967 introduced a new scheme
    ........
      r80987 | benjamin.peterson | 2010-05-08 10:42:29 -0500 (Sat, 08 May 2010) | 1 line
    
      add underscore
    ........

--- Lib/test/test_sysconfig.py
+++ Lib/test/test_sysconfig.py
@@ -234,8 +234,8 @@ class TestSysConfig(unittest.TestCase):
         self.assertTrue(os.path.isfile(config_h), config_h)
 
     def test_get_scheme_names(self):
-        wanted = ('nt', 'nt_user', 'os2', 'os2_home', 'posix_home',
-                  'posix_prefix', 'posix_user')
+        wanted = ('nt', 'nt_user', 'os2', 'os2_home', 'osx_framework_user',
+                  'posix_home', 'posix_prefix', 'posix_user')
         self.assertEquals(get_scheme_names(), wanted)
 
     def test_symlink(self):
