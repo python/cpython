6f137ca4693a9c37ec012846aebc02f6863547be
Jean-Paul Calderone <exarkun@divmod.com>
2010-05-09 03:18:57 +0000

    Merged revisions 81007 via svnmerge from svn+ssh://pythondev@svn.python.org/python/trunk
    ........
      r81007 | jean-paul.calderone | 2010-05-08 16:06:02 -0400 (Sat, 08 May 2010) | 1 line
    
      Skip signal handler re-installation if it is not necessary.  Issue 8354.
    ........

--- Python/pythonrun.c
+++ Python/pythonrun.c
@@ -2254,6 +2254,10 @@ PyOS_sighandler_t
 PyOS_setsig(int sig, PyOS_sighandler_t handler)
 {
 #ifdef HAVE_SIGACTION
+	/* Some code in Modules/signalmodule.c depends on sigaction() being
+	 * used here if HAVE_SIGACTION is defined.  Fix that if this code
+	 * changes to invalidate that assumption.
+	 */
 	struct sigaction context, ocontext;
 	context.sa_handler = handler;
 	sigemptyset(&context.sa_mask);
