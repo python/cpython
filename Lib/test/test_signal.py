06ba9ade85be733e4c6c3708c088317068ae20d9
Stefan Krah <stefan@bytereef.org>
2010-05-15 09:41:27 +0000

    Merged revisions 81185 via svnmerge from svn+ssh://pythondev@svn.python.org/python/trunk
    ........
      r81185 | stefan.krah | 2010-05-15 11:31:08 +0200 (Sat, 15 May 2010) | 4 lines
    
      If the timeout is exceeded, count the tests as skipped instead of just
      issuing a warning.
    ........

--- Lib/test/test_signal.py
+++ Lib/test/test_signal.py
@@ -431,9 +431,8 @@ class ItimerTest(unittest.TestCase):
             if signal.getitimer(self.itimer) == (0.0, 0.0):
                 break # sig_vtalrm handler stopped this itimer
         else: # Issue 8424
-            sys.stdout.write("test_itimer_virtual: timeout: likely cause: "
-                             "machine too slow or load too high.\n")
-            return
+            raise unittest.SkipTest("timeout: likely cause: machine too slow "
+                                    "or load too high")
 
         # virtual itimer should be (0.0, 0.0) now
         self.assertEquals(signal.getitimer(self.itimer), (0.0, 0.0))
@@ -455,9 +454,8 @@ class ItimerTest(unittest.TestCase):
             if signal.getitimer(self.itimer) == (0.0, 0.0):
                 break # sig_prof handler stopped this itimer
         else: # Issue 8424
-            sys.stdout.write("test_itimer_prof: timeout: likely cause: "
-                             "machine too slow or load too high.\n")
-            return
+            raise unittest.SkipTest("timeout: likely cause: machine too slow "
+                                    "or load too high")
 
         # profiling itimer should be (0.0, 0.0) now
         self.assertEquals(signal.getitimer(self.itimer), (0.0, 0.0))
