01f90767311f8b38b0d35add7ed86c9cf01aa0e3
Amaury Forgeot d'Arc <amauryfa@gmail.com>
2010-05-15 20:35:12 +0000

    Remove unused variable, and fix a compilation warning on Windows.

--- Modules/posixmodule.c
+++ Modules/posixmodule.c
@@ -4062,7 +4062,7 @@ Kill a process with a signal.");
 static PyObject *
 win32_kill(PyObject *self, PyObject *args)
 {
-    PyObject *result, handle_obj;
+    PyObject *result;
     DWORD pid, sig, err;
     HANDLE handle;
 
