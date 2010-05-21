7be6a4397e38e3e4b3adc6011d9fa167acc1e126
Antoine Pitrou <solipsis@pitrou.net>
2010-05-21 17:33:14 +0000

    Merged revisions 81400 via svnmerge from svn+ssh://pythondev@svn.python.org/python/branches/py3k
    ................
      r81400 | antoine.pitrou | 2010-05-21 19:25:34 +0200 (ven., 21 mai 2010) | 12 lines
    
      Merged revisions 81398 via svnmerge from
      svn+ssh://pythondev@svn.python.org/python/trunk
    
      ........
        r81398 | antoine.pitrou | 2010-05-21 19:12:38 +0200 (ven., 21 mai 2010) | 6 lines
    
        Issue #5753: A new C API function, :cfunc:`PySys_SetArgvEx`, allows
        embedders of the interpreter to set sys.argv without also modifying
        sys.path.  This helps fix `CVE-2008-5983
        <http://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2008-5983>`_.
      ........
    ................

--- Include/sysmodule.h
+++ Include/sysmodule.h
@@ -10,6 +10,7 @@ extern "C" {
 PyAPI_FUNC(PyObject *) PySys_GetObject(const char *);
 PyAPI_FUNC(int) PySys_SetObject(const char *, PyObject *);
 PyAPI_FUNC(void) PySys_SetArgv(int, wchar_t **);
+PyAPI_FUNC(void) PySys_SetArgvEx(int, wchar_t **, int);
 PyAPI_FUNC(void) PySys_SetPath(const wchar_t *);
 
 PyAPI_FUNC(void) PySys_WriteStdout(const char *format, ...)
