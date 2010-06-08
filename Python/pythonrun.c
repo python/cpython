a7feb19d9c1952ba9572ba06c9d0b537d89ea37c
Victor Stinner <victor.stinner@haypocalc.com>
2010-06-08 21:00:13 +0000

    Py_FatalError(): don't sys sys.last_xxx variables
    Call PyErr_PrintEx(0) instead of PyErr_Print() to avoid a crash if
    Py_FatalError() is called in an early stage of Python initialization (if PySys
    is not yet initialized).

--- Python/pythonrun.c
+++ Python/pythonrun.c
@@ -2055,7 +2055,7 @@ Py_FatalError(const char *msg)
     fprintf(stderr, "Fatal Python error: %s\n", msg);
     fflush(stderr); /* it helps in Windows debug build */
     if (PyErr_Occurred()) {
-        PyErr_Print();
+        PyErr_PrintEx(0);
     }
 #ifdef MS_WINDOWS
     {
