f269d103a4e925c79d6fda9dc3f454dc8fa8a829
Benjamin Peterson <benjamin@python.org>
2010-06-08 15:12:17 +0000

    Merged revisions 81823,81835 via svnmerge from svn+ssh://pythondev@svn.python.org/python/branches/py3k
    ................
      r81823 | benjamin.peterson | 2010-06-07 17:31:26 -0500 (Mon, 07 Jun 2010) | 9 lines
    
      Merged revisions 81820 via svnmerge from
      svn+ssh://pythondev@svn.python.org/python/trunk
    
      ........
        r81820 | benjamin.peterson | 2010-06-07 17:23:23 -0500 (Mon, 07 Jun 2010) | 1 line
    
        correctly overflow when indexes are too large
      ........
    ................
      r81835 | benjamin.peterson | 2010-06-08 09:57:22 -0500 (Tue, 08 Jun 2010) | 9 lines
    
      Merged revisions 81834 via svnmerge from
      svn+ssh://pythondev@svn.python.org/python/trunk
    
      ........
        r81834 | benjamin.peterson | 2010-06-08 09:53:29 -0500 (Tue, 08 Jun 2010) | 1 line
    
        kill extra word
      ........
    ................

--- Lib/test/test_unicode.py
+++ Lib/test/test_unicode.py
@@ -687,6 +687,9 @@ class UnicodeTest(
         self.assertRaises(IndexError, "{:}".format)
         self.assertRaises(IndexError, "{:s}".format)
         self.assertRaises(IndexError, "{}".format)
+        big = "23098475029384702983476098230754973209482573"
+        self.assertRaises(ValueError, ("{" + big + "}").format)
+        self.assertRaises(ValueError, ("{[" + big + "]}").format, [0])
 
         # issue 6089
         self.assertRaises(ValueError, "{0[0]x}".format, [None])
