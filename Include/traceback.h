dfc34241cdabb9a16faa205ecff684b825d38af9
Victor Stinner <victor.stinner@haypocalc.com>
2010-06-17 23:23:37 +0000

    Merged revisions 82059,82061 via svnmerge from svn+ssh://pythondev@svn.python.org/python/branches/py3k
    ........
      r82059 | victor.stinner | 2010-06-18 01:08:50 +0200 (ven., 18 juin 2010) | 5 lines
    
      Issue #6543: Write the traceback in the terminal encoding instead of utf-8.
      Fix the encoding of the modules filename.
    
      Reindent also traceback.h, just because I hate tabs :-)
    ........
      r82061 | victor.stinner | 2010-06-18 01:17:37 +0200 (ven., 18 juin 2010) | 2 lines
    
      Issue #6543: Mention the author of the patch, Amaury Forgeot d'Arc
    ........

--- Include/traceback.h
+++ Include/traceback.h
@@ -10,16 +10,16 @@ struct _frame;
 /* Traceback interface */
 
 typedef struct _traceback {
-	PyObject_HEAD
-	struct _traceback *tb_next;
-	struct _frame *tb_frame;
-	int tb_lasti;
-	int tb_lineno;
+    PyObject_HEAD
+    struct _traceback *tb_next;
+    struct _frame *tb_frame;
+    int tb_lasti;
+    int tb_lineno;
 } PyTracebackObject;
 
 PyAPI_FUNC(int) PyTraceBack_Here(struct _frame *);
 PyAPI_FUNC(int) PyTraceBack_Print(PyObject *, PyObject *);
-PyAPI_FUNC(int) _Py_DisplaySourceLine(PyObject *, const char *, int, int);
+PyAPI_FUNC(int) _Py_DisplaySourceLine(PyObject *, PyObject *, int, int);
 
 /* Reveal traceback type so we can typecheck traceback objects */
 PyAPI_DATA(PyTypeObject) PyTraceBack_Type;
