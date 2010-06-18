5c9bd237ca7bec3dd97880547dddd62ed2481d71
Jean-Paul Calderone <exarkun@divmod.com>
2010-06-18 20:00:17 +0000

    Revert r60115
    This revision introduced quoting for strings containing | based
    on a misunderstanding of the commonly used quoting rules used
    on Windows.
    
    | is interpreted by cmd.exe, not by the MS C runtime argv initializer.
    It only needs to be quoted if it is part of an argument passed through
    cmd.exe.
    
    See issue1300, issue7839, and issue8972.

--- Lib/test/test_subprocess.py
+++ Lib/test/test_subprocess.py
@@ -486,8 +486,6 @@ class ProcessTestCase(BaseTestCase):
                          '"a\\\\b\\ c" d e')
         self.assertEqual(subprocess.list2cmdline(['ab', '']),
                          'ab ""')
-        self.assertEqual(subprocess.list2cmdline(['echo', 'foo|bar']),
-                         'echo "foo|bar"')
 
 
     def test_poll(self):
