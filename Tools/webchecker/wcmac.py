7f4d8ea83cc6797bba6465386a26fb1c5899d3a8
Benjamin Peterson <benjamin@python.org>
2010-06-25 23:37:15 +0000

    remove old, outdated tool

--- Tools/webchecker/wcmac.py
+++
@@ -1,9 +0,0 @@
-import webchecker, sys
-webchecker.DEFROOT = "http://www.python.org/python/"
-webchecker.MAXPAGE = 50000
-webchecker.verbose = 2
-sys.argv.append('-x')
-webchecker.main()
-sys.stdout.write("\nCR to exit: ")
-sys.stdout.flush()
-sys.stdin.readline()
