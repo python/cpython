ba2a60c2faef8cccabf466dd0451786a926fe25e
Ronald Oussoren <ronaldoussoren@mac.com>
2010-06-27 12:37:46 +0000

    Merged revisions 82272 via svnmerge from svn+ssh://pythondev@svn.python.org/python/trunk
    ........
      r82272 | ronald.oussoren | 2010-06-27 14:36:16 +0200 (Sun, 27 Jun 2010) | 8 lines
    
      Two small fixes for the support for SDKs on MacOSX:
    
      1) The code that checks if an path should be located in the SDK
         explicitly excludes /usr/local. This fixes issue9046
    
      2) The SDK variant for filtering "db_dirs_to_check" in setup.py
         was not doing anything because of a missing assignment.
    ........

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ def is_macosx_sdk_path(path):
     """
     Returns True if 'path' can be located in an OSX SDK
     """
-    return path.startswith('/usr/') or path.startswith('/System/')
+    return (path.startswith('/usr/') and not path.startswith('/usr/local')) or path.startswith('/System/')
 
 def find_file(filename, std_dirs, paths):
     """Searches for the directory where a given file is located,
@@ -862,6 +862,7 @@ class PyBuildExt(build_ext):
                         else:
                             if os.path.isdir(dn):
                                 tmp.append(dn)
+                    db_dirs_to_check = tmp
 
                     db_dirs_to_check = tmp
 
