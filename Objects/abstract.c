ff149a3600a28ee3a4ea3c22d192586f4c5cacef
Benjamin Peterson <benjamin@python.org>
2010-06-28 19:46:35 +0000

    Merged revisions 82342 via svnmerge from svn+ssh://pythondev@svn.python.org/python/branches/py3k
    ........
      r82342 | benjamin.peterson | 2010-06-28 14:43:42 -0500 (Mon, 28 Jun 2010) | 1 line
    
      update error message
    ........

--- Objects/abstract.c
+++ Objects/abstract.c
@@ -2533,8 +2533,7 @@ recursive_isinstance(PyObject *inst, PyObject *cls)
     }
     else {
         if (!check_class(cls,
-            "isinstance() arg 2 must be a class, type,"
-            " or tuple of classes and types"))
+            "isinstance() arg 2 must be a type or tuple of types"))
             return -1;
         icls = PyObject_GetAttr(inst, __class__);
         if (icls == NULL) {
