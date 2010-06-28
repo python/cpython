930049bda96c74c39db004c9045e5f0f9498ce35
Senthil Kumaran <orsenthil@gmail.com>
2010-06-28 14:12:18 +0000

    Merged revisions 82326 via svnmerge from svn+ssh://pythondev@svn.python.org/python/branches/py3k
    ................
      r82326 | senthil.kumaran | 2010-06-28 19:38:00 +0530 (Mon, 28 Jun 2010) | 9 lines
    
      Merged revisions 82324 via svnmerge from
      svn+ssh://pythondev@svn.python.org/python/trunk
    
      ........
        r82324 | senthil.kumaran | 2010-06-28 19:26:46 +0530 (Mon, 28 Jun 2010) | 3 lines
    
        Fix Issue8653 - Docstring for urlunsplit function.
      ........
    ................

--- Lib/urllib/parse.py
+++ Lib/urllib/parse.py
@@ -208,6 +208,11 @@ def urlunparse(components):
     return urlunsplit((scheme, netloc, url, query, fragment))
 
 def urlunsplit(components):
+    """Combine the elements of a tuple as returned by urlsplit() into a
+    complete URL as a string. The data argument can be any five-item iterable.
+    This may result in a slightly different, but equivalent URL, if the URL that
+    was parsed originally had unnecessary delimiters (for example, a ? with an
+    empty query; the RFC states that these are equivalent)."""
     scheme, netloc, url, query, fragment = components
     if netloc or (scheme and scheme in uses_netloc and url[:2] != '//'):
         if url and url[:1] != '/': url = '/' + url
