0565dedad7a5af33fefc4f3d4dec8e8931766806
Benjamin Peterson <benjamin@python.org>
2010-06-30 17:11:08 +0000

    mark test depending on ref counting

--- Lib/test/test_os.py
+++ Lib/test/test_os.py
@@ -48,6 +48,7 @@ class FileTests(unittest.TestCase):
         os.closerange(first, first + 2)
         self.assertRaises(OSError, os.write, first, "a")
 
+    @test_support.cpython_only
     def test_rename(self):
         path = unicode(test_support.TESTFN)
         old = sys.getrefcount(path)
