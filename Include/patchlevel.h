9a1c1106d021e7d7ab2af832f00b05059deed59d
Benjamin Peterson <benjamin@python.org>
2010-07-03 14:51:25 +0000

    prepare for 2.7.1

--- Include/patchlevel.h
+++ Include/patchlevel.h
@@ -22,12 +22,12 @@
 /*--start constants--*/
 #define PY_MAJOR_VERSION	2
 #define PY_MINOR_VERSION	7
-#define PY_MICRO_VERSION	0
-#define PY_RELEASE_LEVEL	PY_RELEASE_LEVEL_FINAL
+#define PY_MICRO_VERSION	1
+#define PY_RELEASE_LEVEL	PY_RELEASE_LEVEL_ALPHA
 #define PY_RELEASE_SERIAL	0
 
 /* Version as a string */
-#define PY_VERSION      	"2.7"
+#define PY_VERSION      	"2.7.1a0"
 /*--end constants--*/
 
 /* Subversion Revision number of this file (not of the repository) */
--- Lib/idlelib/idlever.py
+++ Lib/idlelib/idlever.py
@@ -1,1 +1,1 @@
-IDLE_VERSION = "2.7"
+IDLE_VERSION = "2.7.1a0"
