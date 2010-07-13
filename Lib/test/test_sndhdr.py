94ee95951dce510504c8840d830a9b10b74b16fe
Victor Stinner <victor.stinner@haypocalc.com>
2010-07-13 23:31:11 +0000

    Add missing file from the previous commit (r82859): test_sndhdr.py

---
+++ Lib/test/test_sndhdr.py
@@ -0,0 +1,23 @@
+import sndhdr
+import unittest
+from test.support import findfile
+
+class TestFormats(unittest.TestCase):
+    def test_data(self):
+        for filename, expected in (
+            ('sndhdr.8svx', ('8svx', 0, 1, 0, 8)),
+            ('sndhdr.aifc', ('aifc', 44100, 2, 5, 16)),
+            ('sndhdr.aiff', ('aiff', 44100, 2, 5, 16)),
+            ('sndhdr.au', ('au', 44100, 2, 5.0, 16)),
+            ('sndhdr.hcom', ('hcom', 22050.0, 1, -1, 8)),
+            ('sndhdr.sndt', ('sndt', 44100, 1, 5, 8)),
+            ('sndhdr.voc', ('voc', 0, 1, -1, 8)),
+            ('sndhdr.wav', ('wav', 44100, 2, -1, 16)),
+        ):
+            filename = findfile(filename, subdir="sndhdrdata")
+            what = sndhdr.what(filename)
+            self.assertNotEqual(what, None, filename)
+            self.assertSequenceEqual(what, expected)
+
+if __name__ == '__main__':
+    unittest.main()
