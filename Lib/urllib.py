0fdd385e2ce7b197e4eb6be4d2d03a115a5268ae
Senthil Kumaran <orsenthil@gmail.com>
2010-07-14 20:22:17 +0000

    Merged revisions 82895 via svnmerge from svn+ssh://pythondev@svn.python.org/python/branches/py3k
    ........
      r82895 | senthil.kumaran | 2010-07-15 01:40:52 +0530 (Thu, 15 Jul 2010) | 3 lines
    
      Fix a mistake, https proxy shoud be https://
    ........

--- Lib/urllib.py
+++ Lib/urllib.py
@@ -1453,7 +1453,7 @@ elif os.name == 'nt':
                         proxies['http'] = proxyServer
                     else:
                         proxies['http'] = 'http://%s' % proxyServer
-                        proxies['https'] = 'http://%s' % proxyServer
+                        proxies['https'] = 'https://%s' % proxyServer
                         proxies['ftp'] = 'ftp://%s' % proxyServer
             internetSettings.Close()
         except (WindowsError, ValueError, TypeError):
