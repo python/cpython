04f31b8ae6cc2a1aea70ea184d16b3a3dc2048dd
Senthil Kumaran <orsenthil@gmail.com>
2010-07-14 20:10:52 +0000

    Fix a mistake, https proxy shoud be https://

--- Lib/urllib/request.py
+++ Lib/urllib/request.py
@@ -2279,7 +2279,7 @@ elif os.name == 'nt':
                         proxies['http'] = proxyServer
                     else:
                         proxies['http'] = 'http://%s' % proxyServer
-                        proxies['https'] = 'http://%s' % proxyServer
+                        proxies['https'] = 'https://%s' % proxyServer
                         proxies['ftp'] = 'ftp://%s' % proxyServer
             internetSettings.Close()
         except (WindowsError, ValueError, TypeError):
