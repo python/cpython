b71a9a0902fd111d25e791cd8b9d5382c611d1a2
Stefan Krah <stefan@bytereef.org>
2010-07-19 18:01:13 +0000

    Merged revisions 82978 via svnmerge from svn+ssh://pythondev@svn.python.org/python/branches/py3k
    ........
      r82978 | stefan.krah | 2010-07-19 19:58:26 +0200 (Mon, 19 Jul 2010) | 3 lines
    
      Sub-issue of #9036: Fix incorrect use of Py_CHARMASK.
    ........

--- Objects/unicodeobject.c
+++ Objects/unicodeobject.c
@@ -9304,7 +9304,7 @@ PyObject *PyUnicode_Format(PyObject *format,
                 else if (c >= '0' && c <= '9') {
                     prec = c - '0';
                     while (--fmtcnt >= 0) {
-                        c = Py_CHARMASK(*fmt++);
+                        c = *fmt++;
                         if (c < '0' || c > '9')
                             break;
                         if ((prec*10) / 10 != prec) {
