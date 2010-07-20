4c13e53e8188e291eb18500247a8e6b627c2708e
Benjamin Peterson <benjamin@python.org>
2010-07-20 22:37:19 +0000

    move test_trace.py so as not to conflict with future tests for the trace module

--- Python/ceval.c
+++ Python/ceval.c
@@ -2052,6 +2052,7 @@ PyEval_EvalFrameEx(PyFrameObject *f, int throwflag)
             break;
 
         TARGET(LOAD_NAME)
+        TARGET(LOAD_NAME_LOCAL_ONLY)
             w = GETITEM(names, oparg);
             if ((v = f->f_locals) == NULL) {
                 PyErr_Format(PyExc_SystemError,
@@ -2073,15 +2074,14 @@ PyEval_EvalFrameEx(PyFrameObject *f, int throwflag)
                 }
             }
             if (x == NULL) {
-                x = PyDict_GetItem(f->f_globals, w);
+                if (opcode != LOAD_NAME_LOCAL_ONLY) {
+                    x = PyDict_GetItem(f->f_globals, w);
+                    if (x == NULL)
+                        x = PyDict_GetItem(f->f_builtins, w);
+                }
                 if (x == NULL) {
-                    x = PyDict_GetItem(f->f_builtins, w);
-                    if (x == NULL) {
-                        format_exc_check_arg(
-                                    PyExc_NameError,
-                                    NAME_ERROR_MSG, w);
-                        break;
-                    }
+                    format_exc_check_arg(PyExc_NameError, NAME_ERROR_MSG, w);
+                    break;
                 }
                 Py_INCREF(x);
             }
