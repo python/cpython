54cf4b568f54f9d90c94e3ab8987b326d4fd4427
Georg Brandl <georg@python.org>
2010-08-01 21:18:52 +0000

    Merged revisions 83201 via svnmerge from svn+ssh://pythondev@svn.python.org/python/branches/py3k
    ........
      r83201 | georg.brandl | 2010-07-28 10:19:35 +0200 (Mi, 28 Jul 2010) | 1 line
    
      #9354: Provide getsockopt() in asyncore file_wrapper().  Patch by Lukas Langa.
    ........

--- Lib/test/test_asyncore.py
+++ Lib/test/test_asyncore.py
@@ -428,6 +428,19 @@ class FileWrapperTest(unittest.TestCase):
         w.close()
         self.assertEqual(file(TESTFN).read(), self.d + d1 + d2)
 
+    @unittest.skipUnless(hasattr(asyncore, 'file_dispatcher'),
+                         'asyncore.file_dispatcher required')
+    def test_dispatcher(self):
+        fd = os.open(TESTFN, os.O_RDONLY)
+        data = []
+        class FileDispatcher(asyncore.file_dispatcher):
+            def handle_read(self):
+                data.append(self.recv(29))
+        s = FileDispatcher(fd)
+        os.close(fd)
+        asyncore.loop(timeout=0.01, use_poll=True, count=2)
+        self.assertEqual(b"".join(data), self.d)
+
 
 class BaseTestHandler(asyncore.dispatcher):
 
