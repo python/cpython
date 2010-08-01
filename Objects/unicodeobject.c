8efb4313f7efa5873444f06f360313020bcb2fb4
Georg Brandl <georg@python.org>
2010-08-01 20:54:22 +0000

    Recorded merge of revisions 83444 via svnmerge from svn+ssh://svn.python.org/python/branches/py3k
    ........
      r83444 | georg.brandl | 2010-08-01 22:51:02 +0200 (So, 01 Aug 2010) | 1 line
    
      Revert r83395, it introduces test failures and is not necessary anyway since we now have to nul-terminate the string anyway.
    ........

--- Objects/unicodeobject.c
+++ Objects/unicodeobject.c
@@ -3597,7 +3597,7 @@ PyObject *PyUnicode_EncodeUnicodeEscape(const Py_UNICODE *s,
 
             ch2 = *s++;
             size--;
-            if (ch2 >= 0xDC00 && ch2 <= 0xDFFF && size) {
+            if (ch2 >= 0xDC00 && ch2 <= 0xDFFF) {
                 ucs = (((ch & 0x03FF) << 10) | (ch2 & 0x03FF)) + 0x00010000;
                 *p++ = '\\';
                 *p++ = 'U';
@@ -3839,7 +3839,7 @@ PyObject *PyUnicode_EncodeRawUnicodeEscape(const Py_UNICODE *s,
 
                 ch2 = *s++;
                 size--;
-                if (ch2 >= 0xDC00 && ch2 <= 0xDFFF && size) {
+                if (ch2 >= 0xDC00 && ch2 <= 0xDFFF) {
                     ucs = (((ch & 0x03FF) << 10) | (ch2 & 0x03FF)) + 0x00010000;
                     *p++ = '\\';
                     *p++ = 'U';
