007a1a985fe4bf06a1c9084eff9e11059cfd34ec
Georg Brandl <georg@python.org>
2010-08-02 17:36:05 +0000

    Merged revisions 83531 via svnmerge from svn+ssh://pythondev@svn.python.org/python/branches/py3k
    ........
      r83531 | georg.brandl | 2010-08-02 19:24:49 +0200 (Mo, 02 Aug 2010) | 1 line
    
      #7372: fix regression in pstats: a previous fix to handle cProfile data in add_callers broke handling of profile data.
    ........

--- Lib/test/test_pstats.py
+++ Lib/test/test_pstats.py
@@ -10,10 +10,16 @@ class AddCallersTestCase(unittest.TestCase):
     def test_combine_results(self):
         """pstats.add_callers should combine the call results of both target
         and source by adding the call time. See issue1269."""
+        # new format: used by the cProfile module
         target = {"a": (1, 2, 3, 4)}
         source = {"a": (1, 2, 3, 4), "b": (5, 6, 7, 8)}
         new_callers = pstats.add_callers(target, source)
         self.assertEqual(new_callers, {'a': (2, 4, 6, 8), 'b': (5, 6, 7, 8)})
+        # old format: used by the profile module
+        target = {"a": 1}
+        source = {"a": 1, "b": 5}
+        new_callers = pstats.add_callers(target, source)
+        self.assertEqual(new_callers, {'a': 2, 'b': 5})
 
 
 def test_main():
