818afb55c9da56f370112e0b04dad66b8e067710
Tim Golden <mail@timgolden.me.uk>
2010-08-08 16:18:18 +0000

    Issue #2304: Add additional quotes when using cmd shell on Windows. Original patch from Gabriel Genellina

--- Lib/subprocess.py
+++ Lib/subprocess.py
@@ -829,7 +829,7 @@ class Popen(object):
                 startupinfo.dwFlags |= _subprocess.STARTF_USESHOWWINDOW
                 startupinfo.wShowWindow = _subprocess.SW_HIDE
                 comspec = os.environ.get("COMSPEC", "cmd.exe")
-                args = comspec + " /c " + args
+                args = comspec + " /c " + '"%s"' % args
                 if (_subprocess.GetVersion() >= 0x80000000 or
                         os.path.basename(comspec).lower() == "command.com"):
                     # Win9x, or using command.com on NT. We need to
