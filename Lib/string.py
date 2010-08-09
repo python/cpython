5dba4600ac91ff93e0fd4f80675eba03e32d584a
Senthil Kumaran <orsenthil@gmail.com>
2010-08-09 08:47:15 +0000

    Fix Issue5416  - explain negative value of maxplit in str.replace.

--- Lib/string.py
+++ Lib/string.py
@@ -513,7 +513,8 @@ def replace(s, old, new, maxsplit=-1):
 
     Return a copy of string str with all occurrences of substring
     old replaced by new. If the optional argument maxsplit is
-    given, only the first maxsplit occurrences are replaced.
+    given, only the first maxsplit occurrences are replaced. A
+    negative value of maxsplit signifies all occurances.
 
     """
     return s.replace(old, new, maxsplit)
