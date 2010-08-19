12844e6df6d6ae2a8b51e6440b8b15b10a78949a
Amaury Forgeot d'Arc <amauryfa@gmail.com>
2010-08-19 21:32:38 +0000

    Add tests for r84209 (crashes in the Ast builder) Also remove one tab, and move a check closer to the possible failure.

--- Lib/test/test_syntax.py
+++ Lib/test/test_syntax.py
@@ -484,6 +484,34 @@ SyntaxError: can't delete ()
 Traceback (most recent call last):
 SyntaxError: can't assign to literal
 
+Corner-cases that used to fail to raise the correct error:
+
+    >>> def f(*, x=lambda __debug__:0): pass
+    Traceback (most recent call last):
+    SyntaxError: assignment to keyword
+
+    >>> def f(*args:(lambda __debug__:0)): pass
+    Traceback (most recent call last):
+    SyntaxError: assignment to keyword
+
+    >>> def f(**kwargs:(lambda __debug__:0)): pass
+    Traceback (most recent call last):
+    SyntaxError: assignment to keyword
+
+    >>> with (lambda *:0): pass
+    Traceback (most recent call last):
+    SyntaxError: named arguments must follow bare *
+
+Corner-cases that used to crash:
+
+    >>> def f(**__debug__): pass
+    Traceback (most recent call last):
+    SyntaxError: assignment to keyword
+
+    >>> def f(*xx, __debug__): pass
+    Traceback (most recent call last):
+    SyntaxError: assignment to keyword
+
 """
 
 import re
