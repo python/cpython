fc13c76bcad60501c8e579e0315f1ea37e2c000d
Amaury Forgeot d'Arc <amauryfa@gmail.com>
2010-08-19 21:32:38 +0000

    Add tests for r84209 (crashes in the Ast builder) Also remove one tab, and move a check closer to the possible failure.

--- Python/ast.c
+++ Python/ast.c
@@ -689,7 +689,7 @@ handle_keywordonly_args(struct compiling *c, const node *n, int start,
                 if (i + 1 < NCH(n) && TYPE(CHILD(n, i + 1)) == EQUAL) {
                     expression = ast_for_expr(c, CHILD(n, i + 2));
                     if (!expression)
-		      goto error;
+                        goto error;
                     asdl_seq_SET(kwdefaults, j, expression);
                     i += 2; /* '=' and test */
                 }
@@ -892,14 +892,14 @@ ast_for_arguments(struct compiling *c, const node *n)
                 ch = CHILD(n, i+1);  /* tfpdef */
                 assert(TYPE(ch) == tfpdef || TYPE(ch) == vfpdef);
                 kwarg = NEW_IDENTIFIER(CHILD(ch, 0));
+                if (!kwarg)
+                    return NULL;
                 if (NCH(ch) > 1) {
                     /* there is an annotation on the kwarg */
                     kwargannotation = ast_for_expr(c, CHILD(ch, 2));
                     if (!kwargannotation)
                         return NULL;
                 }
-                if (!kwarg)
-                    return NULL;
                 if (forbidden_name(kwarg, CHILD(ch, 0), 0))
                     return NULL;
                 i += 3;
