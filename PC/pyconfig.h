0ceb671affe47c29776b433e73c7ffd4d1fedda0
Tim Golden <mail@timgolden.me.uk>
2010-08-20 08:31:26 +0000

    Remove long-redundant plat-win from sys.path on Windows

--- PC/pyconfig.h
+++ PC/pyconfig.h
@@ -80,7 +80,7 @@ WIN32 is still required for the locale module.
 #define MS_WIN32 /* only support win32 and greater. */
 #define MS_WINDOWS
 #ifndef PYTHONPATH
-#	define PYTHONPATH L".\\DLLs;.\\lib;.\\lib\\plat-win"
+#	define PYTHONPATH L".\\DLLs;.\\lib"
 #endif
 #define NT_THREADS
 #define WITH_THREAD
