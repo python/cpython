dd806cef090256f88dfe0ad88bdcba200be4cb78
Antoine Pitrou <solipsis@pitrou.net>
2010-09-04 17:34:12 +0000

    Merged revisions 84492 via svnmerge from svn+ssh://pythondev@svn.python.org/python/branches/py3k
    ........
      r84492 | antoine.pitrou | 2010-09-04 19:32:06 +0200 (sam., 04 sept. 2010) | 4 lines
    
      Issue #9581: Fix non-working PosixGroupsTester test case
      (it only runs as root, which is why nobody bothered about the failure)
    ........

--- Lib/test/test_posix.py
+++ Lib/test/test_posix.py
@@ -400,13 +400,7 @@ class PosixGroupsTester(unittest.TestCase):
     def test_initgroups(self):
         # find missing group
 
-        groups = sorted(self.saved_groups)
-        for g1,g2 in zip(groups[:-1], groups[1:]):
-            g = g1 + 1
-            if g < g2:
-                break
-        else:
-            g = g2 + 1
+        g = max(self.saved_groups) + 1
         name = pwd.getpwuid(posix.getuid()).pw_name
         posix.initgroups(name, g)
         self.assertIn(g, posix.getgroups())
