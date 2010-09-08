716d27ff679740678752f5baf599b8cd6ca08bd9
Éric Araujo <merwok@netwok.org>
2010-09-08 00:02:00 +0000

    Merged revisions 84614 via svnmerge from svn+ssh://pythondev@svn.python.org/python/branches/py3k
    ........
      r84614 | eric.araujo | 2010-09-08 02:00:45 +0200 (mer., 08 sept. 2010) | 5 lines
    
      Follow-up to #9199: Fix str.join use, add newlines.
    
      Thanks to Konrad Delong for writing a test for upload_docs
      --show-response in distutils2, letting me catch my mistake.
    ........

--- Lib/distutils/command/upload.py
+++ Lib/distutils/command/upload.py
@@ -194,5 +194,5 @@ class upload(PyPIRCCommand):
             self.announce('Upload failed (%s): %s' % (r.status, r.reason),
                           log.ERROR)
         if self.show_response:
-            msg = ''.join('-' * 75, r.read(), '-' * 75)
+            msg = '\n'.join(('-' * 75, r.read(), '-' * 75))
             self.announce(msg, log.INFO)
