ca556177feaddcf935110fd710c1832b43902f7f
Alexander Belopolsky <alexander.belopolsky@gmail.com>
2010-09-13 18:38:54 +0000

    Merged revisions 84780-84781 via svnmerge from svn+ssh://pythondev@svn.python.org/python/branches/py3k with some manual fixes ........   r84780 | alexander.belopolsky | 2010-09-13 14:14:34 -0400 (Mon, 13 Sep 2010) | 3 lines
      Issue #9315: Fix for the trace module to record correct class name
      when tracing methods.  Unit tests. Patch by Eli Bendersky.
    ........
      r84781 | alexander.belopolsky | 2010-09-13 14:15:33 -0400 (Mon, 13 Sep 2010) | 1 line
    
      Removed debugging setting
    ........

---
+++ Lib/test/tracedmodules/__init__.py
@@ -0,0 +1,9 @@
+"""This package contains modules that help testing the trace.py module. Note
+that the exact location of functions in these modules is important, as trace.py
+takes the real line numbers into account.
+"""
+"""This directory contains modules that help testing the trace.py module. Note
+that the exact location of functions in these modules is important, as trace.py
+takes the real line numbers into account.
+
+"""
