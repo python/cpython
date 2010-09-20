c5d37e4496b3477e713970807fcc22f90a0ebb4e
Hirokazu Yamamoto <ocean-city@m2.ccsnet.ne.jp>
2010-09-20 09:59:55 +0000

    Windows7 buildbot debug trial. (Probably impossible)

--- Tools/buildbot/external-common.bat
+++ Tools/buildbot/external-common.bat
@@ -2,6 +2,21 @@
 @rem fetching external components into the root\.. buildbot directories.
 
 cd ..
+
+@rem XXX: This will be gone soon after Windows7(py3k) buildbot will run.
+
+@rem Print nasmw version
+for %%i in (nasmw.exe) do (
+	if not "%%=$PATH" == "" (
+		%%~$PATH:i -v
+	)
+)
+
+@rem Compare asm files
+for %%i in (openssl-1.0.0a\asm32\*.asm) do (
+	fc %%~fi openssl-1.0.0a\tmp32\%%~nxi
+)
+
 @rem XXX: If you need to force the buildbots to start from a fresh environment, uncomment
 @rem the following, check it in, then check it out, comment it out, then check it back in.
 @rem if exist bzip2-1.0.5 rd /s/q bzip2-1.0.5
@@ -14,7 +29,7 @@ cd ..
 @rem if exist tk8.4.16 rd /s/q tk8.4.16
 @rem if exist tk-8.4.18.1 rd /s/q tk-8.4.18.1
 @rem if exist db-4.4.20 rd /s/q db-4.4.20
-@rem if exist openssl-1.0.0a rd /s/q openssl-1.0.0a
+if exist openssl-1.0.0a rd /s/q openssl-1.0.0a
 @rem if exist sqlite-3.6.21 rd /s/q sqlite-3.6.21    
 
 @rem bzip
