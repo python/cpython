4c98404011c5925e361f0bff5e95e82540a32b7c
Antoine Pitrou <solipsis@pitrou.net>
2010-09-21 16:26:09 +0000

    Issue #9899: Fix test_tkinter.test_font on various platforms.  Patch by Ned Deily.

--- Lib/tkinter/test/test_tkinter/test_font.py
+++ Lib/tkinter/test/test_tkinter/test_font.py
@@ -2,13 +2,21 @@ import unittest
 import tkinter
 from tkinter import font
 from test.support import requires, run_unittest
+import tkinter.test.support as support
 
 requires('gui')
 
 class FontTest(unittest.TestCase):
+
+    def setUp(self):
+        support.root_deiconify()
+
+    def tearDown(self):
+        support.root_withdraw()
+
     def test_font_eq(self):
-        font1 = font.nametofont("system")
-        font2 = font.nametofont("system")
+        font1 = font.nametofont("TkDefaultFont")
+        font2 = font.nametofont("TkDefaultFont")
         self.assertIsNot(font1, font2)
         self.assertEqual(font1, font2)
         self.assertNotEqual(font1, font1.copy())
