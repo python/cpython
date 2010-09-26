639f0e24def57a272006abb14f878b539338213a
Vinay Sajip <vinay_sajip@yahoo.co.uk>
2010-09-26 11:04:10 +0000

    logging: NullHandler optimisation.

--- Lib/logging/__init__.py
+++ Lib/logging/__init__.py
@@ -1656,8 +1656,14 @@ class NullHandler(Handler):
     a NullHandler and add it to the top-level logger of the library module or
     package.
     """
+    def handle(self, record):
+        pass
+
     def emit(self, record):
         pass
+
+    def createLock(self):
+        self.lock = None
 
 # Warnings integration
 
