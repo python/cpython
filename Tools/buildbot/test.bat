bb4400f3fca1ddff2401e6822bc74f5161fafa01
Hirokazu Yamamoto <ocean-city@m2.ccsnet.ne.jp>
2010-09-28 17:55:53 +0000

    Reverted to r70917.

--- Tools/buildbot/test.bat
+++ Tools/buildbot/test.bat
@@ -1,5 +1,4 @@
 @rem Used by the buildbot "test" step.
 cd PCbuild
-call rt.bat -d -q -uall -rw -n -x test_ttk_guionly
-call rt.bat -d -q -uall -rw -n -v test_ttk_guionly
+call rt.bat -d -q -uall -rw -n
 
