94426f2196e03f55287ba5d55e11b0d24726f92b
R. David Murray <rdmurray@bitdance.com>
2010-09-29 01:22:20 +0000

    Have test_sqlite print version info when run in verbose mode.

--- Lib/test/test_sqlite.py
+++ Lib/test/test_sqlite.py
@@ -1,13 +1,18 @@
-from test.support import run_unittest, import_module
+from test.support import run_unittest, import_module, verbose
 
 # Skip test if _sqlite3 module not installed
 import_module('_sqlite3')
 
+import sqlite3
 from sqlite3.test import (dbapi, types, userfunctions,
                                 factory, transactions, hooks, regression,
                                 dump)
 
 def test_main():
+    if verbose:
+        print("test_sqlite: testing with version",
+              "{!r}, sqlite_version {!r}".format(sqlite3.version,
+                                                 sqlite3.sqlite_version))
     run_unittest(dbapi.suite(), types.suite(), userfunctions.suite(),
                  factory.suite(), transactions.suite(),
                  hooks.suite(), regression.suite(), dump.suite())
