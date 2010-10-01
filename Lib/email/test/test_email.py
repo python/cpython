5bb7ce03e915964cd9c65ec55d0a4787b274d023
R. David Murray <rdmurray@bitdance.com>
2010-10-01 15:40:20 +0000

    #10004: in Q encoded word ignore '=xx' when xx is not valid hex.
    Bug report and fix by Thomas Guettler.

--- Lib/email/test/test_email.py
+++ Lib/email/test/test_email.py
@@ -1659,6 +1659,12 @@ Re: =?mac-iceland?q?r=8Aksm=9Arg=8Cs?= baz foo bar =?mac-iceland?q?r=8Aksm?=
             dh = decode_header(s % q)
             self.assertEqual(dh, [(a, 'iso-8859-1')])
 
+    def test_rfc2047_Q_invalid_digits(self):
+        # issue 10004.
+        s = '=?iso-8659-1?Q?andr=e9=zz?='
+        self.assertEqual(decode_header(s),
+                        [(b'andr\xe9=zz', 'iso-8659-1')])
+
 
 # Test the MIMEMessage class
 class TestMIMEMessage(TestEmailBase):
