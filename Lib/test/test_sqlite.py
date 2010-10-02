8f7bcb3f8557ebae74a710610aeb4db63421e496
R. David Murray <rdmurray@bitdance.com>
2010-10-02 13:29:13 +0000

    Make the printing of sqlite version in verbose mode work with regrtest -w.

--- Lib/test/test_sqlite.py
+++ Lib/test/test_sqlite.py
@@ -1,7 +1,7 @@
-from test.support import run_unittest, import_module, verbose
+import test.support
 
 # Skip test if _sqlite3 module not installed
-import_module('_sqlite3')
+test.support.import_module('_sqlite3')
 
 import sqlite3
 from sqlite3.test import (dbapi, types, userfunctions,
@@ -9,13 +9,15 @@ from sqlite3.test import (dbapi, types, userfunctions,
                                 dump)
 
 def test_main():
-    if verbose:
+    if test.support.verbose:
         print("test_sqlite: testing with version",
               "{!r}, sqlite_version {!r}".format(sqlite3.version,
                                                  sqlite3.sqlite_version))
-    run_unittest(dbapi.suite(), types.suite(), userfunctions.suite(),
-                 factory.suite(), transactions.suite(),
-                 hooks.suite(), regression.suite(), dump.suite())
+    test.support.run_unittest(dbapi.suite(), types.suite(),
+                               userfunctions.suite(),
+                               factory.suite(), transactions.suite(),
+                               hooks.suite(), regression.suite(),
+                               dump.suite())
 
 if __name__ == "__main__":
     test_main()
