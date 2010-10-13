7f9393920349bce3356d0b97f139db51b6d52d19
Antoine Pitrou <solipsis@pitrou.net>
2010-10-13 16:17:14 +0000

    Issue #10041: The signature of optional arguments in socket.makefile() didn't match that of io.open(), and they also didn't get forwarded properly to TextIOWrapper in text mode.  Patch by Kai Zhu.

--- Lib/socket.py
+++ Lib/socket.py
@@ -133,7 +133,7 @@ class socket(_socket.socket):
         return socket(self.family, self.type, self.proto, fileno=fd), addr
 
     def makefile(self, mode="r", buffering=None, *,
-                 encoding=None, newline=None):
+                 encoding=None, errors=None, newline=None):
         """makefile(...) -> an I/O stream connected to the socket
 
         The arguments are as for io.open() after the filename,
@@ -171,7 +171,7 @@ class socket(_socket.socket):
             buffer = io.BufferedWriter(raw, buffering)
         if binary:
             return buffer
-        text = io.TextIOWrapper(buffer, encoding, newline)
+        text = io.TextIOWrapper(buffer, encoding, errors, newline)
         text.mode = mode
         return text
 
