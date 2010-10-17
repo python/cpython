9b10200779a75c6cc90b3e7608239e09aed36703
Georg Brandl <georg@python.org>
2010-10-17 11:48:07 +0000

    Merged revisions 81936 via svnmerge from svn+ssh://svn.python.org/python/branches/py3k
    ........
      r81936 | mark.dickinson | 2010-06-12 11:10:14 +0200 (Sa, 12 Jun 2010) | 2 lines
    
      Silence 'unused variable' gcc warning.  Patch by Éric Araujo.
    ........

--- Objects/unicodeobject.c
+++ Objects/unicodeobject.c
@@ -2616,10 +2616,11 @@ PyUnicode_DecodeUTF32Stateful(const char *s,
     Py_UNICODE *p;
 #ifndef Py_UNICODE_WIDE
     int pairs = 0;
+    const unsigned char *qq;
 #else
     const int pairs = 0;
 #endif
-    const unsigned char *q, *e, *qq;
+    const unsigned char *q, *e;
     int bo = 0;       /* assume native ordering by default */
     const char *errmsg = "";
     /* Offsets from q for retrieving bytes in the right order. */
