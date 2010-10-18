9a90900da59816b4d376c47b94b5513b849db070
Victor Stinner <victor.stinner@haypocalc.com>
2010-10-18 20:59:24 +0000

    PyUnicode_FromFormatV(): Fix %A format
    It was not completly implemented. Add a test.

--- Lib/test/test_unicode.py
+++ Lib/test/test_unicode.py
@@ -1380,19 +1380,22 @@ class UnicodeTest(string_tests.CommonTest,
         self.assertEqual("{}".format(s), '__str__ overridden')
 
     def test_from_format(self):
-        # Ensure that PyUnicode_FromFormat() raises an error for a non-ascii
-        # format string.
         from _testcapi import format_unicode
 
         # ascii format, non-ascii argument
         text = format_unicode(b'ascii\x7f=%U', 'unicode\xe9')
         self.assertEqual(text, 'ascii\x7f=unicode\xe9')
 
-        # non-ascii format, ascii argument
+        # non-ascii format, ascii argument: ensure that PyUnicode_FromFormat()
+        # raises an error for a non-ascii format string.
         self.assertRaisesRegexp(ValueError,
             '^PyUnicode_FromFormatV\(\) expects an ASCII-encoded format '
             'string, got a non-ASCII byte: 0xe9$',
             format_unicode, b'unicode\xe9=%s', 'ascii')
+
+        # other tests
+        text = format_unicode(b'%%A:%A', 'abc\xe9\uabcd\U0010ffff')
+        self.assertEqual(text, r"%A:'abc\xe9\uabcd\U0010ffff'")
 
     # Test PyUnicode_AsWideChar()
     def test_aswidechar(self):
