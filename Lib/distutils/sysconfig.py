75d8c5cea26912dd58f27689fbe9f7679e45aeb8
Victor Stinner <victor.stinner@haypocalc.com>
2010-10-23 17:02:31 +0000

    Issue #6011: sysconfig and distutils.sysconfig use the surrogateescape error handler to parse the Makefile file. Avoid a UnicodeDecodeError if the source code directory name contains a non-ASCII character and the locale encoding is ASCII.

--- Lib/distutils/sysconfig.py
+++ Lib/distutils/sysconfig.py
@@ -271,7 +271,7 @@ def parse_makefile(fn, g=None):
     used instead of a new dictionary.
     """
     from distutils.text_file import TextFile
-    fp = TextFile(fn, strip_comments=1, skip_blanks=1, join_lines=1)
+    fp = TextFile(fn, strip_comments=1, skip_blanks=1, join_lines=1, errors="surrogateescape")
 
     if g is None:
         g = {}
