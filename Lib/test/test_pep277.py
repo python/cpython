fc6f5a402043a2b1fc0940860ab7fbcdb5fdcd45
Victor Stinner <victor.stinner@haypocalc.com>
2010-10-28 22:57:03 +0000

    test_pep277: disable filenames 11, 12, 13, 14 on darwin
    Because darwin "normalizes" these filenames differently than Python's NFD
    normalization.

--- Lib/test/test_pep277.py
+++ Lib/test/test_pep277.py
@@ -16,11 +16,6 @@ filenames = [
     '9_\u66e8\u05e9\u3093\u0434\u0393\xdf',
     # Specific code points: fn, NFC(fn) and NFKC(fn) all differents
     '10_\u1fee\u1ffd',
-    # Specific code points: NFC(fn), NFD(fn), NFKC(fn) and NFKD(fn) all differents
-    '11_\u0385\u03d3\u03d4',
-    '12_\u00a8\u0301\u03d2\u0301\u03d2\u0308', # == NFD('\u0385\u03d3\u03d4')
-    '13_\u0020\u0308\u0301\u038e\u03ab',       # == NFKC('\u0385\u03d3\u03d4')
-    '14_\u1e9b\u1fc1\u1fcd\u1fce\u1fcf\u1fdd\u1fde\u1fdf\u1fed',
     ]
 
 # Mac OS X decomposes Unicode names, using Normal Form D.
@@ -31,6 +26,12 @@ filenames = [
 # U+2FAFF are not decomposed."
 if sys.platform != 'darwin':
     filenames.extend([
+        # Specific code points: NFC(fn), NFD(fn), NFKC(fn) and NFKD(fn) all differents
+        '11_\u0385\u03d3\u03d4',
+        '12_\u00a8\u0301\u03d2\u0301\u03d2\u0308', # == NFD('\u0385\u03d3\u03d4')
+        '13_\u0020\u0308\u0301\u038e\u03ab',       # == NFKC('\u0385\u03d3\u03d4')
+        '14_\u1e9b\u1fc1\u1fcd\u1fce\u1fcf\u1fdd\u1fde\u1fdf\u1fed',
+
         # Specific code points: fn, NFC(fn) and NFKC(fn) all differents
         '15_\u1fee\u1ffd\ufad1',
         '16_\u2000\u2000\u2000A',
