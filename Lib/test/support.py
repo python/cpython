64474542ebec7095d3f66828de4d917699388e9e
Antoine Pitrou <solipsis@pitrou.net>
2010-10-31 11:34:47 +0000

    PY_CFLAGS doesn't exist under Windows

--- Lib/test/support.py
+++ Lib/test/support.py
@@ -890,7 +890,7 @@ def gc_collect():
 
 def python_is_optimized():
     """Find if Python was built with optimizations."""
-    cflags = sysconfig.get_config_vars()['PY_CFLAGS']
+    cflags = sysconfig.get_config_var('PY_CFLAGS') or ''
     final_opt = ""
     for opt in cflags.split():
         if opt.startswith('-O'):
