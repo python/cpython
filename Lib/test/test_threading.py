48717b2d0d3a7e155bfbd17cfde4e4cf158308c6
Brian Curtin <brian.curtin@gmail.com>
2010-11-02 04:01:17 +0000

    Merged revisions 86107 via svnmerge from svn+ssh://pythondev@svn.python.org/python/branches/py3k
    ........
      r86107 | brian.curtin | 2010-11-01 22:59:09 -0500 (Mon, 01 Nov 2010) | 2 lines
    
      Clean up ResourceWarnings. Explictly close stdout from the subprocess.
    ........

--- Lib/test/test_threading.py
+++ Lib/test/test_threading.py
@@ -416,6 +416,7 @@ class ThreadJoinOnShutdown(unittest.TestCase):
         p = subprocess.Popen([sys.executable, "-c", script], stdout=subprocess.PIPE)
         rc = p.wait()
         data = p.stdout.read().decode().replace('\r', '')
+        p.stdout.close()
         self.assertEqual(data, "end of main\nend of thread\n")
         self.assertFalse(rc == 2, "interpreter was blocked")
         self.assertTrue(rc == 0, "Unexpected error")
