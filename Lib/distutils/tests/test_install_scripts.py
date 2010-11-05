5fd730e0fbfe2b655722d89626075d5b7f2d77e7
Éric Araujo <merwok@netwok.org>
2010-11-05 23:59:32 +0000

    Of course, I forgot one file in r86223.

--- Lib/distutils/tests/test_install_scripts.py
+++ Lib/distutils/tests/test_install_scripts.py
@@ -42,8 +42,10 @@ class InstallScriptsTestCase(support.TempdirManager,
         def write_script(name, text):
             expected.append(name)
             f = open(os.path.join(source, name), "w")
-            f.write(text)
-            f.close()
+            try:
+                f.write(text)
+            finally:
+                f.close()
 
         write_script("script1.py", ("#! /usr/bin/env python2.3\n"
                                     "# bogus script w/ Python sh-bang\n"
