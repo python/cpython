3a72b250eb63d5bafda8da8dba1bf6b90ec381c6
Martin v. Löwis <martin@v.loewis.de>
2010-11-14 18:13:49 +0000

    Merged revisions 84339,84487 via svnmerge from svn+ssh://pythondev@svn.python.org/python/branches/py3k
    ........
      r84339 | martin.v.loewis | 2010-08-28 15:40:10 +0200 (Sa, 28 Aug 2010) | 1 line
    
      Stop packaging versioncheck.
    ........
      r84487 | martin.v.loewis | 2010-09-04 16:38:09 +0200 (Sa, 04 Sep 2010) | 3 lines
    
      Issue #1303434: Include PDBs in release.
      Patch by James Lee and Daniel Stutzbach.
    ........

--- Tools/msi/msi.py
+++ Tools/msi/msi.py
@@ -1,7 +1,7 @@
 # Python MSI Generator
 # (C) 2003 Martin v. Loewis
 # See "FOO" in comments refers to MSDN sections with the title FOO.
-import msilib, schema, sequence, os, glob, time, re, shutil
+import msilib, schema, sequence, os, glob, time, re, shutil, zipfile
 from msilib import Feature, CAB, Directory, Dialog, Binary, add_data
 import uisample
 from win32com.client import constants
@@ -28,6 +28,8 @@ have_tcl = True
 PCBUILD="PCbuild"
 # msvcrt version
 MSVCR = "90"
+# Make a zip file containing the PDB files for this build?
+pdbzip = True
 
 try:
     from config import *
@@ -1116,7 +1118,7 @@ def add_files(db):
     # Add tools
     tools.set_current()
     tooldir = PyDirectory(db, cab, root, "Tools", "Tools", "TOOLS|Tools")
-    for f in ['i18n', 'pynche', 'Scripts', 'versioncheck', 'webchecker']:
+    for f in ['i18n', 'pynche', 'Scripts', 'webchecker']:
         lib = PyDirectory(db, cab, tooldir, f, f, "%s|%s" % (tooldir.make_short(f), f))
         lib.glob("*.py")
         lib.glob("*.pyw", exclude=['pydocgui.pyw'])
@@ -1293,6 +1295,16 @@ def add_registry(db):
               ])
     db.Commit()
 
+def build_pdbzip():
+    pdbexclude = ['kill_python.pdb', 'make_buildinfo.pdb',
+                  'make_versioninfo.pdb']
+    path = "python-%s%s-pdb.zip" % (full_current_version, msilib.arch_ext)
+    pdbzip = zipfile.ZipFile(path, 'w')
+    for f in glob.glob1(os.path.join(srcdir, PCBUILD), "*.pdb"):
+        if f not in pdbexclude and not f.endswith('_d.pdb'):
+            pdbzip.write(os.path.join(srcdir, PCBUILD, f), f)
+    pdbzip.close()
+
 db = build_database()
 try:
     add_features(db)
@@ -1303,3 +1315,6 @@ try:
     db.Commit()
 finally:
     del db
+
+if pdbzip:
+    build_pdbzip()
