44c1db05fb2d0b753d18d13a415fa703aa7c0795
Brian Curtin <brian.curtin@gmail.com>
2010-11-21 23:48:14 +0000

    Merged revisions 86651 via svnmerge from svn+ssh://pythondev@svn.python.org/python/branches/py3k
    ........
      r86651 | brian.curtin | 2010-11-21 17:45:10 -0600 (Sun, 21 Nov 2010) | 5 lines
    
      Fix #6378. Start IDLE using the proper version of Python based on the directory
      tree that idle.bat resides in.
    
      Works with any/all versions of Python installed concurrently.
    ........

--- Lib/idlelib/idle.bat
+++ Lib/idlelib/idle.bat
@@ -1,3 +1,4 @@
 @echo off
-rem Working IDLE bat for Windows - uses start instead of absolute pathname
-start idle.pyw %1 %2 %3 %4 %5 %6 %7 %8 %9
+rem Start IDLE using the appropriate Python interpreter
+set CURRDIR=%~dp0
+start %CURRDIR%..\..\pythonw.exe %CURRDIR%idle.pyw %1 %2 %3 %4 %5 %6 %7 %8 %9
