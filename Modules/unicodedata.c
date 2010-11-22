e03c7787b98f406d65714b0e7c9509131ac5828a
Martin v. Löwis <martin@v.loewis.de>
2010-11-22 10:53:46 +0000

    Issue #10459: Update CJK character names to Unicode 5.2.

--- Modules/unicodedata.c
+++ Modules/unicodedata.c
@@ -869,8 +869,9 @@ is_unified_ideograph(Py_UCS4 code)
 {
     return (
         (0x3400 <= code && code <= 0x4DB5) || /* CJK Ideograph Extension A */
-        (0x4E00 <= code && code <= 0x9FBB) || /* CJK Ideograph */
-        (0x20000 <= code && code <= 0x2A6D6));/* CJK Ideograph Extension B */
+        (0x4E00 <= code && code <= 0x9FCB) || /* CJK Ideograph, Unicode 5.2 */
+        (0x20000 <= code && code <= 0x2A6D6) || /* CJK Ideograph Extension B */
+        (0x2A700 <= code && code <= 0x2B734));  /* CJK Ideograph Extension C */
 }
 
 static int
