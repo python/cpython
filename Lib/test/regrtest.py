4c4c0f2fe6e6d9058911c9383bac57e4deefddb8
Nick Coghlan <ncoghlan@gmail.com>
2010-12-03 07:44:33 +0000

    Partially revert r78719 - it removed a check that is still needed in some cases (i.e. this will allow Michael to add the test.__main__ support that broke the buildbots previously)

--- Lib/test/regrtest.py
+++ Lib/test/regrtest.py
@@ -1468,7 +1468,20 @@ class _ExpectedSkips:
         assert self.isvalid()
         return self.expected
 
+
 if __name__ == '__main__':
+    # Remove regrtest.py's own directory from the module search path. Despite
+    # the elimination of implicit relative imports, this is still needed to
+    # ensure that submodules of the test package do not inappropriately appear
+    # as top-level modules even when people (or buildbots!) invoke regrtest.py
+    # directly instead of using the -m switch
+    mydir = os.path.abspath(os.path.normpath(os.path.dirname(sys.argv[0])))
+    i = len(sys.path)
+    while i >= 0:
+        i -= 1
+        if os.path.abspath(os.path.normpath(sys.path[i])) == mydir:
+            del sys.path[i]
+
     # findtestdir() gets the dirname out of __file__, so we have to make it
     # absolute before changing the working directory.
     # For example __file__ may be relative when running trace or profile.
