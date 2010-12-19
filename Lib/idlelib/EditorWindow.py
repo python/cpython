aedd2899c27c71dde84837fab9ac119425b7f4dc
Georg Brandl <georg@python.org>
2010-12-19 10:10:32 +0000

    #6075: make idle work with both Carbon AquaTk and Cocoa AquaTk. Patch by Kevin Walzer and Ned Deily.

--- Lib/idlelib/EditorWindow.py
+++ Lib/idlelib/EditorWindow.py
@@ -387,7 +387,7 @@ class EditorWindow(object):
             underline, label = prepstr(label)
             menudict[name] = menu = Menu(mbar, name=name)
             mbar.add_cascade(label=label, menu=menu, underline=underline)
-        if macosxSupport.runningAsOSXApp():
+        if macosxSupport.isCarbonAquaTk(self.root):
             # Insert the application menu
             menudict['application'] = menu = Menu(mbar, name='apple')
             mbar.add_cascade(label='IDLE', menu=menu)
