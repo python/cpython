d699255b42fc075b2fc7259e1394a50bab928459
Éric Araujo <merwok@netwok.org>
2010-12-23 18:41:33 +0000

    Fix small inaccuracy: there is no index function

--- Lib/numbers.py
+++ Lib/numbers.py
@@ -303,7 +303,7 @@ class Integral(Rational):
         raise NotImplementedError
 
     def __index__(self):
-        """index(self)"""
+        """someobject[self]"""
         return int(self)
 
     @abstractmethod
