9d194f3169f1a62f33292e54a20a85084436e201
Alexander Belopolsky <alexander.belopolsky@gmail.com>
2010-12-24 00:27:27 +0000

    Merged revisions 87463 via svnmerge from svn+ssh://pythondev@svn.python.org/python/branches/py3k
    ........
      r87463 | alexander.belopolsky | 2010-12-23 19:24:11 -0500 (Thu, 23 Dec 2010) | 1 line
    
      Issue #9063: Corrected the tzinfo example.
    ........

--- Doc/includes/tzinfo-examples.py
+++ Doc/includes/tzinfo-examples.py
@@ -71,7 +71,7 @@ class LocalTimezone(tzinfo):
     def _isdst(self, dt):
         tt = (dt.year, dt.month, dt.day,
               dt.hour, dt.minute, dt.second,
-              dt.weekday(), 0, -1)
+              dt.weekday(), 0, 0)
         stamp = _time.mktime(tt)
         tt = _time.localtime(stamp)
         return tt.tm_isdst > 0
