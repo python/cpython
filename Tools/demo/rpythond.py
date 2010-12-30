7fafbc95c0c963438197c9a43fe893c4ea6fe759
Georg Brandl <georg@python.org>
2010-12-30 21:33:07 +0000

    More cleanup: Move some demos into a dedicated Tools/demo dir, move 2to3 demo to Tools, and remove all the other Demo content.

--- Tools/demo/rpythond.py
+++ Tools/demo/rpythond.py
@@ -6,7 +6,7 @@
 # from any host on the Internet!
 
 import sys
-from socket import *
+from socket import socket, AF_INET, SOCK_STREAM
 import io
 import traceback
 
@@ -15,23 +15,23 @@ BUFSIZE = 1024
 
 def main():
     if len(sys.argv) > 1:
-        port = int(eval(sys.argv[1]))
+        port = int(sys.argv[1])
     else:
         port = PORT
     s = socket(AF_INET, SOCK_STREAM)
     s.bind(('', port))
     s.listen(1)
-    while 1:
+    while True:
         conn, (remotehost, remoteport) = s.accept()
-        print('connected by', remotehost, remoteport)
-        request = ''
+        print('connection from', remotehost, remoteport)
+        request = b''
         while 1:
             data = conn.recv(BUFSIZE)
             if not data:
                 break
-            request = request + data
-        reply = execute(request)
-        conn.send(reply)
+            request += data
+        reply = execute(request.decode())
+        conn.send(reply.encode())
         conn.close()
 
 def execute(request):
@@ -49,4 +49,7 @@ def execute(request):
         sys.stdout = stdout
     return fakefile.getvalue()
 
-main()
+try:
+    main()
+except KeyboardInterrupt:
+    pass
