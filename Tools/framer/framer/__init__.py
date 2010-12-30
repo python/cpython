5684a9ac30c09bf78f15be16864be7f1147cc2bb
Georg Brandl <georg@python.org>
2010-12-30 17:32:22 +0000

    Clean up tools: remove "world" and "framer", move single SSL script to scripts/.

--- Tools/framer/framer/__init__.py
+++
@@ -1,6 +0,0 @@
-"""A tool to generate basic framework for C extension types.
-
-The basic ideas is the same as modulator, but the code generates code
-using many of the new features introduced in Python 2.2.  It also
-takes a more declarative approach to generating code.
-"""
