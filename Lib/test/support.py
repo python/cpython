90be5fb8175705320c8d2cdabb35292a40257cf7
Nick Coghlan <ncoghlan@gmail.com>
2011-01-11 10:05:20 +0000

    Issue 10556: test_zipimport_support implicitly imports too many modules (including _ssl) to safely clobber sys.modules after each test

--- Lib/test/support.py
+++ Lib/test/support.py
@@ -1199,6 +1199,12 @@ def modules_cleanup(oldmodules):
                  if k.startswith('encodings.')]
     sys.modules.clear()
     sys.modules.update(encodings)
+    # XXX: This kind of problem can affect more than just encodings. In particular
+    # extension modules (such as _ssl) don't cope with releoding properly.
+    # Really, test modules should be cleaning out the test specific modules they
+    # know they added (ala test_runpy) rather than relying on this function (as
+    # test_importhooks and test_pkg do currently).
+    # Implicitly imported *real* modules should be left alone (see issue 10556).
     sys.modules.update(oldmodules)
 
 #=======================================================================
