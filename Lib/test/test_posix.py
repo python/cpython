b633eab98e53dbbdb0c0d019e77d409b9b0adf58
Antoine Pitrou <solipsis@pitrou.net>
2011-01-12 18:50:35 +0000

    Merged revisions 87958 via svnmerge from svn+ssh://pythondev@svn.python.org/python/branches/py3k
    ........
      r87958 | antoine.pitrou | 2011-01-12 19:45:27 +0100 (mer., 12 janv. 2011) | 4 lines
    
      Issue #10822: Fix test_posix:test_getgroups failure under Solaris.  Patch
      by Ross Lagerwall.
    ........

--- Lib/test/test_posix.py
+++ Lib/test/test_posix.py
@@ -274,6 +274,7 @@ class PosixTester(unittest.TestCase):
                 os.chdir(curdir)
                 support.rmtree(base_path)
 
+    @unittest.skipUnless(hasattr(os, 'getegid'), "test needs os.getegid()")
     def test_getgroups(self):
         with os.popen('id -G') as idg:
             groups = idg.read().strip()
@@ -283,9 +284,11 @@ class PosixTester(unittest.TestCase):
 
         # 'id -G' and 'os.getgroups()' should return the same
         # groups, ignoring order and duplicates.
+        # #10822 - it is implementation defined whether posix.getgroups()
+        # includes the effective gid so we include it anyway, since id -G does
         self.assertEqual(
                 set([int(x) for x in groups.split()]),
-                set(posix.getgroups()))
+                set(posix.getgroups() + [posix.getegid()]))
 
 class PosixGroupsTester(unittest.TestCase):
 
