6dc50f34ddb8a5c3b11570575348c9b94919b024
Georg Brandl <georg@python.org>
2011-01-15 16:45:03 +0000

    Update to 1.0.7.

--- Doc/make.bat
+++ Doc/make.bat
@@ -34,7 +34,7 @@ echo.
 goto end
 
 :checkout
-svn co %SVNROOT%/external/Sphinx-1.0.5/sphinx tools/sphinx
+svn co %SVNROOT%/external/Sphinx-1.0.7/sphinx tools/sphinx
 svn co %SVNROOT%/external/docutils-0.6/docutils tools/docutils
 svn co %SVNROOT%/external/Jinja-2.3.1/jinja2 tools/jinja2
 svn co %SVNROOT%/external/Pygments-1.3.1/pygments tools/pygments
