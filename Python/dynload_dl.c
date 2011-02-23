3eeee833915b96a15c60eafc317bb6822af2084c
Victor Stinner <victor.stinner@haypocalc.com>
2011-02-23 11:29:28 +0000

    dynload_dl.c: replace tabs by spaces

--- Python/dynload_dl.c
+++ Python/dynload_dl.c
@@ -10,17 +10,17 @@
 extern char *Py_GetProgramName(void);
 
 const struct filedescr _PyImport_DynLoadFiletab[] = {
-	{".o", "rb", C_EXTENSION},
-	{"module.o", "rb", C_EXTENSION},
-	{0, 0}
+    {".o", "rb", C_EXTENSION},
+    {"module.o", "rb", C_EXTENSION},
+    {0, 0}
 };
 
 
 dl_funcptr _PyImport_GetDynLoadFunc(const char *shortname,
-				    const char *pathname, FILE *fp)
+                                    const char *pathname, FILE *fp)
 {
-	char funcname[258];
+    char funcname[258];
 
-	PyOS_snprintf(funcname, sizeof(funcname), "PyInit_%.200s", shortname);
-	return dl_loadmod(Py_GetProgramName(), pathname, funcname);
+    PyOS_snprintf(funcname, sizeof(funcname), "PyInit_%.200s", shortname);
+    return dl_loadmod(Py_GetProgramName(), pathname, funcname);
 }
