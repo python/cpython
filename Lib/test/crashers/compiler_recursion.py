952c0782b39d2112c648f0731abf5e566fa20c37
Nick Coghlan <ncoghlan@gmail.com>
2011-03-19 15:22:59 +1000

    Last attempt to get compiler recursion crasher to fail reliably across platforms before giving up and skipping it as unreliably platform dependent

--- Lib/test/crashers/compiler_recursion.py
+++ Lib/test/crashers/compiler_recursion.py
@@ -9,5 +9,5 @@ Recorded on the tracker as http://bugs.python.org/issue11383
 # e.g. '1*'*10**5+'1' will die in compiler_visit_expr
 
 # The exact limit to destroy the stack will vary by platform
-# but 1M should do the trick most places
-compile('()'*10**6, '?', 'exec')
+# but 10M should do the trick even with huge stack allocations
+compile('()'*10**7, '?', 'exec')
