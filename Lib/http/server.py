1b407fe658fb298986d1ed3f8a71937ac1b3460c
Senthil Kumaran <orsenthil@gmail.com>
2011-03-20 10:44:30 +0800

    Issue #11567 - Let's have the DOCTYPE as HTML instead of XHTML.

--- Lib/http/server.py
+++ Lib/http/server.py
@@ -103,11 +103,10 @@ import copy
 
 # Default error message template
 DEFAULT_ERROR_MESSAGE = """\
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
+        "http://www.w3.org/TR/html4/strict.dtd">
     <head>
-        <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+        <meta http-equiv="Content-Type" content="text/html;charset=utf-8">
         <title>Error response</title>
     </head>
     <body>
