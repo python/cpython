88ada7ef67865652dd3c12eb5ab719f872fde363
Antoine Pitrou <solipsis@pitrou.net>
2011-03-26 21:15:47 +0100

    In the Windows clean script for buildbots, also clear the build dir (so that stale test files, which can be very large, get wiped out)

--- Tools/buildbot/clean.bat
+++ Tools/buildbot/clean.bat
@@ -2,6 +2,9 @@
 call "%VS90COMNTOOLS%vsvars32.bat"
 @echo Deleting .pyc/.pyo files ...
 del /s Lib\*.pyc Lib\*.pyo
+@echo Deleting test leftovers ...
+rmdir /s /q build
 cd PCbuild
 vcbuild /clean pcbuild.sln "Release|Win32"
 vcbuild /clean pcbuild.sln "Debug|Win32"
+cd ..
