8a6a198abff504067fb74b669c672901a97a44b6
Steven Bethard <steven.bethard@gmail.com>
2011-03-27 13:53:53 +0200

    Issue #9026: Fix order of argparse sub-commands in help messages.

--- Lib/argparse.py
+++ Lib/argparse.py
@@ -82,6 +82,7 @@ __all__ = [
 ]
 
 
+import collections as _collections
 import copy as _copy
 import os as _os
 import re as _re
@@ -1041,7 +1042,7 @@ class _SubParsersAction(Action):
 
         self._prog_prefix = prog
         self._parser_class = parser_class
-        self._name_parser_map = {}
+        self._name_parser_map = _collections.OrderedDict()
         self._choices_actions = []
 
         super(_SubParsersAction, self).__init__(
