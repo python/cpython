ea4e98afc2ad61db1fc0f8c317504d87eea8313e
Brian Quinlan <brian@sweetapp.com>
2011-04-08 08:19:33 +1000

    Issue #11777: Executor.map does not submit futures until iter.next() is called

--- Lib/concurrent/futures/_base.py
+++ Lib/concurrent/futures/_base.py
@@ -536,15 +536,19 @@ class Executor(object):
 
         fs = [self.submit(fn, *args) for args in zip(*iterables)]
 
-        try:
-            for future in fs:
-                if timeout is None:
-                    yield future.result()
-                else:
-                    yield future.result(end_time - time.time())
-        finally:
-            for future in fs:
-                future.cancel()
+        # Yield must be hidden in closure so that the futures are submitted
+        # before the first iterator value is required.
+        def result_iterator():
+            try:
+                for future in fs:
+                    if timeout is None:
+                        yield future.result()
+                    else:
+                        yield future.result(end_time - time.time())
+            finally:
+                for future in fs:
+                    future.cancel()
+        return result_iterator()
 
     def shutdown(self, wait=True):
         """Clean-up the resources associated with the Executor.
