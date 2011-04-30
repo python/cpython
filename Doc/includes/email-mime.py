bb754b503c617821db907a88ae148e2bb0304d31
R David Murray <rdmurray@bitdance.com>
2011-04-30 17:26:32 -0400

    #11883: fix email examples by adding 'localhost' to SMTP constructor calls

--- Doc/includes/email-mime.py
+++ Doc/includes/email-mime.py
@@ -26,6 +26,6 @@ for file in pngfiles:
     msg.attach(img)
 
 # Send the email via our own SMTP server.
-s = smtplib.SMTP()
+s = smtplib.SMTP('localhost')
 s.sendmail(me, family, msg.as_string())
 s.quit()
