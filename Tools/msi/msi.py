c8425043cf02c4ba6ba89ab976e319143e3266b6
Brian Curtin <brian@python.org>
2011-05-03 13:36:34 -0500

    Denote 3.3.0 as the last supported Windows 2000 release.
    This corresponds with change b9390aa12855 to PEP-11.

--- Tools/msi/msi.py
+++ Tools/msi/msi.py
@@ -505,7 +505,7 @@ def add_ui(db):
       "    would still be Python for DOS.")
 
     c = exit_dialog.text("warning", 135, 200, 220, 40, 0x30003,
-            "{\\VerdanaRed9}Warning: Python 2.7.x is the last "
+            "{\\VerdanaRed9}Warning: Python 3.3.0 is the last "
             "Python release for Windows 2000.")
     c.condition("Hide", "VersionNT > 500")
 
