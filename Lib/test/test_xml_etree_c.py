0a92d1849e17dd4008f0d190ecc47b5c55260f3b
Victor Stinner <victor.stinner@haypocalc.com>
2011-05-03 15:09:24 +0200

    Issue #8651: PyArg_Parse*() functions raise an OverflowError if the file doesn't have PY_SSIZE_T_CLEAN define and the size doesn't fit in an int (length bigger than 2^31-1 bytes).

--- Lib/test/test_xml_etree_c.py
+++ Lib/test/test_xml_etree_c.py
@@ -1,6 +1,8 @@
 # xml.etree test for cElementTree
 
 from test import test_support
+from test.test_support import precisionbigmemtest, _2G
+import unittest
 
 cET = test_support.import_module('xml.etree.cElementTree')
 
@@ -13,6 +15,20 @@ def sanity():
 
     >>> from xml.etree import cElementTree
     """
+
+
+class MiscTests(unittest.TestCase):
+    # Issue #8651.
+    @precisionbigmemtest(size=_2G + 100, memuse=1)
+    def test_length_overflow(self, size):
+        if size < _2G + 100:
+            self.skipTest("not enough free memory, need at least 2 GB")
+        data = b'x' * size
+        parser = cET.XMLParser()
+        try:
+            self.assertRaises(OverflowError, parser.feed, data)
+        finally:
+            data = None
 
 
 def test_main():
