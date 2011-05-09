2fa5b11e10b7df3ebaeb4bf569f186bf98bd9184
Martin v. Löwis <martin@v.loewis.de>
2011-05-09 08:05:43 +0200

    Stop trying to use _xmlplus in the xml module. Closes #11164. Patch by Arfrever Frehtes Taifersar Arahesis.

--- Lib/xml/__init__.py
+++ Lib/xml/__init__.py
@@ -18,24 +18,3 @@ etree -- The ElementTree XML library.  This is a subset of the full
 
 
 __all__ = ["dom", "parsers", "sax", "etree"]
-
-_MINIMUM_XMLPLUS_VERSION = (0, 8, 4)
-
-
-try:
-    import _xmlplus
-except ImportError:
-    pass
-else:
-    try:
-        v = _xmlplus.version_info
-    except AttributeError:
-        # _xmlplus is too old; ignore it
-        pass
-    else:
-        if v >= _MINIMUM_XMLPLUS_VERSION:
-            import sys
-            _xmlplus.__path__.extend(__path__)
-            sys.modules[__name__] = _xmlplus
-        else:
-            del v
