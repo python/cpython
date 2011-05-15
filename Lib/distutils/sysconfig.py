73f4642ea0d1b4efe5eb3fa4d94fa089d9130dc3
Ronald Oussoren <ronaldoussoren@mac.com>
2011-05-15 16:46:11 +0200

    Issue #9516: avoid errors in sysconfig when MACOSX_DEPLOYMENT_TARGET is set in shell.
    Without this patch python will fail to start properly when the environment
    variable MACOSX_DEPLOYMENT_TARGET is set on MacOSX and has a value that is
    not compatible with the value during Python's build. This is caused by code
    in sysconfig that was only meant to be used in disutils.

--- Lib/distutils/sysconfig.py
+++ Lib/distutils/sysconfig.py
@@ -428,7 +428,7 @@ def _init_posix():
         cur_target = os.getenv('MACOSX_DEPLOYMENT_TARGET', '')
         if cur_target == '':
             cur_target = cfg_target
-            os.putenv('MACOSX_DEPLOYMENT_TARGET', cfg_target)
+            os.environ['MACOSX_DEPLOYMENT_TARGET'] = cfg_target
         elif [int(x) for x in cfg_target.split('.')] > [int(x) for x in cur_target.split('.')]:
             my_msg = ('$MACOSX_DEPLOYMENT_TARGET mismatch: now "%s" but "%s" during configure'
                 % (cur_target, cfg_target))
