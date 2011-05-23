32140f8955d454ba9bc5b4dc56e2b612beb2a4ad
Barry Warsaw <barry@python.org>
2011-05-23 15:27:52 -0400

    Replay changeset 70249:b571c7a8cf2e from fubar branch.  Original commit message:
    Merging post 2.6.7rc2 changes from Subversion.

--- Include/patchlevel.h
+++ Include/patchlevel.h
@@ -27,7 +27,7 @@
 #define PY_RELEASE_SERIAL	2
 
 /* Version as a string */
-#define PY_VERSION      	"2.6.7rc2"
+#define PY_VERSION      	"2.6.7rc2+"
 /*--end constants--*/
 
 /* Subversion Revision number of this file (not of the repository) */
