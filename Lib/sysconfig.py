0f72edb615919f1999d3104506879e422aac2920
Éric Araujo <merwok@netwok.org>
2011-05-25 18:21:43 +0200

    Minor cleanup in sysconfig.
    Also remove outdated and unhelpful docstrings in test_sysconfig.

--- Lib/sysconfig.py
+++ Lib/sysconfig.py
@@ -656,11 +656,10 @@ def get_platform():
         # to. This makes the compatibility story a bit more sane because the
         # machine is going to compile and link as if it were
         # MACOSX_DEPLOYMENT_TARGET.
-        #
         cfgvars = get_config_vars()
         macver = cfgvars.get('MACOSX_DEPLOYMENT_TARGET')
 
-        if 1:
+        if True:
             # Always calculate the release of the running machine,
             # needed to determine if we can build fat binaries or not.
 
@@ -755,7 +754,7 @@ def _main():
     print('Platform: "%s"' % get_platform())
     print('Python version: "%s"' % get_python_version())
     print('Current installation scheme: "%s"' % _get_default_scheme())
-    print('')
+    print()
     _print_dict('Paths', get_paths())
     print()
     _print_dict('Variables', get_config_vars())
