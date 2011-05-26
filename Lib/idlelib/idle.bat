a5392455f6b632755e568f46ca6492918f1fe779
Kurt B. Kaiser <kbk@shore.net>
2011-05-25 22:36:51 -0400

    IDLE: #6378: Further adjust idle.bat to start associated Python

--- Lib/idlelib/idle.bat
+++ Lib/idlelib/idle.bat
@@ -1,4 +1,4 @@
 @echo off
 rem Start IDLE using the appropriate Python interpreter
 set CURRDIR=%~dp0
-start "%CURRDIR%..\..\pythonw.exe" "%CURRDIR%idle.pyw" %1 %2 %3 %4 %5 %6 %7 %8 %9
+start "IDLE" "%CURRDIR%..\..\pythonw.exe" "%CURRDIR%idle.pyw" %1 %2 %3 %4 %5 %6 %7 %8 %9
