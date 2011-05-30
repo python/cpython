6a62ab806fe764133ce5876617e5bf29bc985e1a
Benjamin Peterson <benjamin@python.org>
2011-05-30 11:12:38 -0500

    don't restrict unexpected EOF errors to the first line (closes #12216)

--- Lib/test/test_grammar.py
+++ Lib/test/test_grammar.py
@@ -125,6 +125,13 @@ the \'lazy\' dog.\n\
         self.assertTrue(x is Ellipsis)
         self.assertRaises(SyntaxError, eval, ".. .")
 
+    def test_eof_error(self):
+        samples = ("def foo(", "\ndef foo(", "def foo(\n")
+        for s in samples:
+            with self.assertRaises(SyntaxError) as cm:
+                compile(s, "<test>", "exec")
+            self.assertIn("unexpected EOF", str(cm.exception))
+
 class GrammarTests(unittest.TestCase):
 
     # single_input: NEWLINE | simple_stmt | compound_stmt NEWLINE
