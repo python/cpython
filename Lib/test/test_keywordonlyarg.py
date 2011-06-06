cfe6a4169a42c1342ecc6839de6216ce6836e812
Benjamin Peterson <benjamin@python.org>
2011-06-05 22:04:07 -0500

    greatly improve argument parsing error messages (closes #12265)

--- Lib/test/test_keywordonlyarg.py
+++ Lib/test/test_keywordonlyarg.py
@@ -78,7 +78,7 @@ class KeywordOnlyArgTestCase(unittest.TestCase):
             pass
         with self.assertRaises(TypeError) as exc:
             f(1, 2, 3)
-        expected = "f() takes at most 2 positional arguments (3 given)"
+        expected = "f() takes from 1 to 2 positional arguments but 3 were given"
         self.assertEqual(str(exc.exception), expected)
 
     def testSyntaxErrorForFunctionCall(self):
