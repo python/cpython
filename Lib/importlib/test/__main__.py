ef3062f7afec3a6f291050beaa6ef5d56f7047f3
Éric Araujo <merwok@netwok.org>
2011-06-07 17:58:50 +0200

    Kill dead code in importlib.test.__main__ (#12019, reviewed by Brett Cannon)

--- Lib/importlib/test/__main__.py
+++ Lib/importlib/test/__main__.py
@@ -4,7 +4,6 @@ Specifying the ``--builtin`` flag will run tests, where applicable, with
 builtins.__import__ instead of importlib.__import__.
 
 """
-import importlib
 from importlib.test.import_ import util
 import os.path
 from test.support import run_unittest
@@ -13,11 +12,7 @@ import unittest
 
 
 def test_main():
-    if '__pycache__' in __file__:
-        parts = __file__.split(os.path.sep)
-        start_dir = sep.join(parts[:-2])
-    else:
-        start_dir = os.path.dirname(__file__)
+    start_dir = os.path.dirname(__file__)
     top_dir = os.path.dirname(os.path.dirname(start_dir))
     test_loader = unittest.TestLoader()
     if '--builtin' in sys.argv:
