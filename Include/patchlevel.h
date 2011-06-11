d8887d675738c8031cbe2efeb6b0119f92756c0f
Benjamin Peterson <benjamin@python.org>
2011-06-11 09:58:58 -0500

    bump to 3.1.4

--- Include/patchlevel.h
+++ Include/patchlevel.h
@@ -19,11 +19,11 @@
 #define PY_MAJOR_VERSION	3
 #define PY_MINOR_VERSION	1
 #define PY_MICRO_VERSION	4
-#define PY_RELEASE_LEVEL	PY_RELEASE_LEVEL_GAMMA
-#define PY_RELEASE_SERIAL	1
+#define PY_RELEASE_LEVEL	PY_RELEASE_LEVEL_FINAL
+#define PY_RELEASE_SERIAL	0
 
 /* Version as a string */
-#define PY_VERSION      	"3.1.4rc1"
+#define PY_VERSION      	"3.1.4"
 /*--end constants--*/
 
 /* Subversion Revision number of this file (not of the repository). Empty
