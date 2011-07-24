19455f37f02bcfa406323c15326c4ed8be6d899d
Antoine Pitrou <solipsis@pitrou.net>
2011-07-24 02:40:25 +0200

    Add a test for issue #1813: getlocale() failing under a Turkish locale (not a problem under 3.x)

--- Lib/test/test_locale.py
+++ Lib/test/test_locale.py
@@ -391,6 +391,19 @@ class TestMiscellaneous(unittest.TestCase):
         # crasher from bug #7419
         self.assertRaises(locale.Error, locale.setlocale, 12345)
 
+    def test_getsetlocale_issue1813(self):
+        # Issue #1813: setting and getting the locale under a Turkish locale
+        oldlocale = locale.getlocale()
+        self.addCleanup(locale.setlocale, locale.LC_CTYPE, oldlocale)
+        try:
+            locale.setlocale(locale.LC_CTYPE, 'tr_TR')
+        except locale.Error:
+            # Unsupported locale on this system
+            self.skipTest('test needs Turkish locale')
+        loc = locale.getlocale()
+        locale.setlocale(locale.LC_CTYPE, loc)
+        self.assertEqual(loc, locale.getlocale())
+
 
 def test_main():
     tests = [
