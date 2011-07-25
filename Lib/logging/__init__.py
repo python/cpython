45dedaafc2d8f83c3021df28251a4a7dae067774
Vinay Sajip <vinay_sajip@yahoo.co.uk>
2011-07-25 19:53:28 +0100

    Fixes #12637: Last resort messages now correctly handled. Thanks to Xavier de Gaye for the patch."

--- Lib/logging/__init__.py
+++ Lib/logging/__init__.py
@@ -18,7 +18,7 @@
 Logging package for Python. Based on PEP 282 and comments thereto in
 comp.lang.python, and influenced by Apache's log4j system.
 
-Copyright (C) 2001-2010 Vinay Sajip. All Rights Reserved.
+Copyright (C) 2001-2011 Vinay Sajip. All Rights Reserved.
 
 To use, simply 'import logging' and log away!
 """
@@ -1440,7 +1440,8 @@ class Logger(Filterer):
                 c = c.parent
         if (found == 0):
             if lastResort:
-                lastResort.handle(record)
+                if record.levelno >= lastResort.level:
+                    lastResort.handle(record)
             elif raiseExceptions and not self.manager.emittedNoHandlerWarning:
                 sys.stderr.write("No handlers could be found for logger"
                                  " \"%s\"\n" % self.name)
