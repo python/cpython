c90fac40b9b00cf1b46d6b773877dba508d4153a
Senthil Kumaran <senthil@uthcode.com>
2011-08-06 12:24:33 +0800

    Fix closes issue12698 - make the no_proxy environment variable handling a bit lenient (accomodate spaces in between the items)

--- Lib/test/test_urllib.py
+++ Lib/test/test_urllib.py
@@ -114,6 +114,9 @@ class ProxyTests(unittest.TestCase):
         proxies = urllib.getproxies_environment()
         # getproxies_environment use lowered case truncated (no '_proxy') keys
         self.assertEqual('localhost', proxies['no'])
+        # List of no_proxies with space.
+        self.env.set('NO_PROXY', 'localhost, anotherdomain.com, newdomain.com')
+        self.assertTrue(urllib.proxy_bypass_environment('anotherdomain.com'))
 
 
 class urlopen_HttpTests(unittest.TestCase):
