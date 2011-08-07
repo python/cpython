1381a31e39ceb775585cacbd5e7f43e91a162786
Sandro Tosi <sandro.tosi@gmail.com>
2011-08-07 17:09:15 +0200

    #12677: correct turtle orientation in doc

--- Lib/lib-tk/turtle.py
+++ Lib/lib-tk/turtle.py
@@ -27,10 +27,10 @@ Turtle graphics is a popular way for introducing programming to
 kids. It was part of the original Logo programming language developed
 by Wally Feurzig and Seymour Papert in 1966.
 
-Imagine a robotic turtle starting at (0, 0) in the x-y plane. Give it
+Imagine a robotic turtle starting at (0, 0) in the x-y plane. After an ``import turtle``, give it
 the command turtle.forward(15), and it moves (on-screen!) 15 pixels in
 the direction it is facing, drawing a line as it moves. Give it the
-command turtle.left(25), and it rotates in-place 25 degrees clockwise.
+command turtle.right(25), and it rotates in-place 25 degrees clockwise.
 
 By combining together these and similar commands, intricate shapes and
 pictures can easily be drawn.
