6f453a130145f5658b3bc074d2530c9bce8cc1e0
Benjamin Peterson <benjamin@python.org>
2011-08-16 22:26:48 -0500

    fix possible refleaks

--- Objects/typeobject.c
+++ Objects/typeobject.c
@@ -2093,8 +2093,10 @@ type_new(PyTypeObject *metatype, PyObject *args, PyObject *kwds)
                  PyUnicode_CompareWithASCIIString(tmp, "__weakref__") == 0))
                 continue;
             tmp =_Py_Mangle(name, tmp);
-            if (!tmp)
+            if (!tmp) {
+                Py_DECREF(newslots);
                 goto bad_slots;
+            }
             PyList_SET_ITEM(newslots, j, tmp);
             j++;
         }
