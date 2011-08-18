7a6b44ab6275aeeb8959437f10ae9ca17aa0b2e4
Benjamin Peterson <benjamin@python.org>
2011-08-18 13:51:47 -0500

    the named of the character is actually NUL

--- Objects/unicodeobject.c
+++ Objects/unicodeobject.c
@@ -1897,7 +1897,7 @@ PyUnicode_FSConverter(PyObject* arg, void* addr)
     size = PyBytes_GET_SIZE(output);
     data = PyBytes_AS_STRING(output);
     if (size != strlen(data)) {
-        PyErr_SetString(PyExc_TypeError, "embedded NULL character");
+        PyErr_SetString(PyExc_TypeError, "embedded NUL character");
         Py_DECREF(output);
         return 0;
     }
