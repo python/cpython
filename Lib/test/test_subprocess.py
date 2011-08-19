200a92c3869efdb6f3ac241f773be323a18a1c87
Nadeem Vawda <nadeem.vawda@gmail.com>
2011-08-19 05:22:24 +0200

    Fix resource leaks in test_subprocess.

--- Lib/test/test_subprocess.py
+++ Lib/test/test_subprocess.py
@@ -955,6 +955,8 @@ class POSIXProcessTestCase(BaseTestCase):
                               'time.sleep(0.2)'],
                              stdout=subprocess.PIPE,
                              stderr=subprocess.PIPE)
+        self.addCleanup(p.stdout.close)
+        self.addCleanup(p.stderr.close)
         ident = id(p)
         pid = p.pid
         del p
@@ -972,6 +974,8 @@ class POSIXProcessTestCase(BaseTestCase):
                               'time.sleep(3)'],
                              stdout=subprocess.PIPE,
                              stderr=subprocess.PIPE)
+        self.addCleanup(p.stdout.close)
+        self.addCleanup(p.stderr.close)
         ident = id(p)
         pid = p.pid
         del p
