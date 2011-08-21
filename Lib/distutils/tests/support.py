3b98e7e2445e76b120f87b738e9d5c7b6a2bd734
Éric Araujo <merwok@netwok.org>
2011-08-21 12:53:37 +0200

    Add missing closing paren in docstring (thanks Ezio)

--- Lib/distutils/tests/support.py
+++ Lib/distutils/tests/support.py
@@ -138,7 +138,7 @@ def copy_xxmodule_c(directory):
 
         def test_compile(self):
             copy_xxmodule_c(self.tmpdir)
-            self.assertIn('xxmodule.c', os.listdir(self.tmpdir)
+            self.assertIn('xxmodule.c', os.listdir(self.tmpdir))
 
     If the source file can be found, it will be copied to *directory*.  If not,
     the test will be skipped.  Errors during copy are not caught.
