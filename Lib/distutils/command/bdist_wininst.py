d5a9811dbe89e52e6c4d0bb389154afd72fabb13
Éric Araujo <merwok@netwok.org>
2011-08-29 21:48:39 +0200

    Make bdist_* commands respect --skip-build passed to bdist (#10946)

--- Lib/distutils/command/bdist_wininst.py
+++ Lib/distutils/command/bdist_wininst.py
@@ -65,13 +65,15 @@ class bdist_wininst(Command):
         self.dist_dir = None
         self.bitmap = None
         self.title = None
-        self.skip_build = 0
+        self.skip_build = None
         self.install_script = None
         self.pre_install_script = None
         self.user_access_control = None
 
 
     def finalize_options(self):
+        self.set_undefined_options('bdist', ('skip_build', 'skip_build'))
+
         if self.bdist_dir is None:
             if self.skip_build and self.plat_name:
                 # If build is skipped and plat_name is overridden, bdist will
@@ -81,8 +83,10 @@ class bdist_wininst(Command):
                 # next the command will be initialized using that name
             bdist_base = self.get_finalized_command('bdist').bdist_base
             self.bdist_dir = os.path.join(bdist_base, 'wininst')
+
         if not self.target_version:
             self.target_version = ""
+
         if not self.skip_build and self.distribution.has_ext_modules():
             short_version = get_python_version()
             if self.target_version and self.target_version != short_version:
