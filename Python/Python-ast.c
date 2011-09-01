6d9adb0014ee94ea1097e1ff4a0354bcb31b8f90
Benjamin Peterson <benjamin@python.org>
2011-08-31 22:13:03 -0400

    accept bytes for the AST 'string' type
    This is a temporary kludge and all is well in 3.3.

--- Python/Python-ast.c
+++ Python/Python-ast.c
@@ -611,7 +611,7 @@ static int obj2ast_identifier(PyObject* obj, PyObject** out, PyArena* arena)
 
 static int obj2ast_string(PyObject* obj, PyObject** out, PyArena* arena)
 {
-    if (!PyUnicode_CheckExact(obj)) {
+    if (!PyUnicode_CheckExact(obj) && !PyBytes_CheckExact(obj)) {
         PyErr_SetString(PyExc_TypeError, "AST string must be of type str");
         return 1;
     }
