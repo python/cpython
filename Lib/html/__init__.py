9b684d3807828b3376cf93596d8dd2b7e8158d81
Senthil Kumaran <senthil@uthcode.com>
2011-09-13 07:14:13 +0800

    Fix issue12938 - Update the docstring of html.escape. Include the information on single quote.

--- Lib/html/__init__.py
+++ Lib/html/__init__.py
@@ -13,7 +13,8 @@ def escape(s, quote=True):
     """
     Replace special characters "&", "<" and ">" to HTML-safe sequences.
     If the optional flag quote is true (the default), the quotation mark
-    character (") is also translated.
+    characters, both double quote (") and single quote (') characters are also
+    translated.
     """
     if quote:
         return s.translate(_escape_map_full)
