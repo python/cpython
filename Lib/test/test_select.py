21d29c377baa305c04d918cc490220836b434adb
Victor Stinner <victor.stinner@haypocalc.com>
2011-10-12 21:01:46 +0200

    Issue #12367: Add a test on error attribute of select.error
    Thanks to the PEP 3151, select.error (which is just an alias to OSError) has
    now an error attribute.

--- Lib/test/test_select.py
+++ Lib/test/test_select.py
@@ -1,8 +1,9 @@
-from test import support
-import unittest
-import select
+import errno
 import os
+import select
 import sys
+import unittest
+from test import support
 
 @unittest.skipIf(sys.platform[:3] in ('win', 'os2', 'riscos'),
                  "can't easily test on this system")
@@ -21,6 +22,17 @@ class SelectTestCase(unittest.TestCase):
         self.assertRaises(TypeError, select.select, [self.Almost()], [], [])
         self.assertRaises(TypeError, select.select, [], [], [], "not a number")
         self.assertRaises(ValueError, select.select, [], [], [], -1)
+
+    def test_errno(self):
+        with open(__file__, 'rb') as fp:
+            fd = fp.fileno()
+            fp.close()
+            try:
+                select.select([fd], [], [])
+            except select.error as err:
+                self.assertEqual(err.errno, errno.EBADF)
+            else:
+                self.fail("exception not raised")
 
     def test_returned_list_identity(self):
         # See issue #8329
