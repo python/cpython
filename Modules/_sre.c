202a3c422b400ee46c61cb9b22ca8ec4b80dfdfd
Senthil Kumaran <senthil@uthcode.com>
2011-10-20 02:15:36 +0800

    3.2 - Fix Issue 12604 - Use a proper no-op macro expansion for VTRACE macro in _sre.c

--- Modules/_sre.c
+++ Modules/_sre.c
@@ -2760,7 +2760,7 @@ _compile(PyObject* self_, PyObject* args)
 #if defined(VVERBOSE)
 #define VTRACE(v) printf v
 #else
-#define VTRACE(v)
+#define VTRACE(v) do {} while(0)  /* do nothing */
 #endif
 
 /* Report failure */
