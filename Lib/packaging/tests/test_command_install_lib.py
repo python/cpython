a8ced971a9e77d2a88a9b71a515313b8f230e3e9
Éric Araujo <merwok@netwok.org>
2011-11-03 05:08:28 +0100

    Improve byte-compilation in packaging to be independent of -O or -B.
    The code I fixed to comply with PEP 3147 still had one bug: When run
    under python -O, some paths for pyc files would be pyo, because I called
    imp.cache_from_source without explicit debug_override argument in some
    places, and under -O that would return .pyo (this is well explained in
    the imp docs).  Now all code (util.byte_compile, build_py, install_lib)
    can create .pyo files according to options given by users,
    without interference from the calling Python’s own optimize mode.
    
    On a related topic, I also removed the code that prevented byte
    compilation under python -B.  The rationale is that packaging gives
    control over the creation of pyc files to the user with its own explicit
    option, and the behavior should not be changed if the calling Python
    happens to run with -B for whatever reason.  I will argue that this is a
    bug fix and ask to be allowed to backport this change to distutils.
    
    Finally, I moved one nugget of information about the --compile and
    --optimize options from the source into the doc.  It clears up a
    misunderstanding that I (and maybe other people) had.

--- Lib/packaging/tests/test_command_install_lib.py
+++ Lib/packaging/tests/test_command_install_lib.py
@@ -17,7 +17,7 @@ class InstallLibTestCase(support.TempdirManager,
     restore_environ = ['PYTHONPATH']
 
     def test_finalize_options(self):
-        pkg_dir, dist = self.create_dist()
+        dist = self.create_dist()[1]
         cmd = install_lib(dist)
 
         cmd.finalize_options()
@@ -34,71 +34,73 @@ class InstallLibTestCase(support.TempdirManager,
         cmd.finalize_options()
         self.assertEqual(cmd.optimize, 2)
 
-    @unittest.skipIf(sys.dont_write_bytecode, 'byte-compile disabled')
     def test_byte_compile(self):
-        pkg_dir, dist = self.create_dist()
-        os.chdir(pkg_dir)
+        project_dir, dist = self.create_dist()
+        os.chdir(project_dir)
         cmd = install_lib(dist)
         cmd.compile = True
         cmd.optimize = 1
 
-        f = os.path.join(pkg_dir, 'foo.py')
+        f = os.path.join(project_dir, 'foo.py')
         self.write_file(f, '# python file')
         cmd.byte_compile([f])
-        pyc_file = imp.cache_from_source('foo.py')
+        pyc_file = imp.cache_from_source('foo.py', debug_override=True)
         pyo_file = imp.cache_from_source('foo.py', debug_override=False)
         self.assertTrue(os.path.exists(pyc_file))
         self.assertTrue(os.path.exists(pyo_file))
 
-    def test_get_outputs(self):
-        pkg_dir, dist = self.create_dist()
-        cmd = install_lib(dist)
-
-        # setting up a dist environment
-        cmd.compile = True
-        cmd.optimize = 1
-        cmd.install_dir = pkg_dir
-        f = os.path.join(pkg_dir, '__init__.py')
-        self.write_file(f, '# python package')
-        cmd.distribution.ext_modules = [Extension('foo', ['xxx'])]
-        cmd.distribution.packages = [pkg_dir]
-
-        # make sure the build_lib is set the temp dir
-        build_dir = os.path.split(pkg_dir)[0]
-        cmd.get_finalized_command('build_py').build_lib = build_dir
-
-        # get_output should return 4 elements
-        self.assertEqual(len(cmd.get_outputs()), 4)
-
-    def test_get_inputs(self):
-        pkg_dir, dist = self.create_dist()
-        cmd = install_lib(dist)
-
-        # setting up a dist environment
-        cmd.compile = True
-        cmd.optimize = 1
-        cmd.install_dir = pkg_dir
-        f = os.path.join(pkg_dir, '__init__.py')
-        self.write_file(f, '# python package')
-        cmd.distribution.ext_modules = [Extension('foo', ['xxx'])]
-        cmd.distribution.packages = [pkg_dir]
-
-        # get_input should return 2 elements
-        self.assertEqual(len(cmd.get_inputs()), 2)
-
-    def test_dont_write_bytecode(self):
-        # makes sure byte_compile is not used
-        pkg_dir, dist = self.create_dist()
-        cmd = install_lib(dist)
-        cmd.compile = True
-        cmd.optimize = 1
-
+    def test_byte_compile_under_B(self):
+        # make sure byte compilation works under -B (dont_write_bytecode)
         self.addCleanup(setattr, sys, 'dont_write_bytecode',
                         sys.dont_write_bytecode)
         sys.dont_write_bytecode = True
-        cmd.byte_compile([])
+        self.test_byte_compile()
 
-        self.assertIn('byte-compiling is disabled', self.get_logs()[0])
+    def test_get_outputs(self):
+        project_dir, dist = self.create_dist()
+        os.chdir(project_dir)
+        os.mkdir('spam')
+        cmd = install_lib(dist)
+
+        # setting up a dist environment
+        cmd.compile = True
+        cmd.optimize = 1
+        cmd.install_dir = self.mkdtemp()
+        f = os.path.join(project_dir, 'spam', '__init__.py')
+        self.write_file(f, '# python package')
+        cmd.distribution.ext_modules = [Extension('foo', ['xxx'])]
+        cmd.distribution.packages = ['spam']
+
+        # make sure the build_lib is set the temp dir  # XXX what?  this is not
+        # needed in the same distutils test and should work without manual
+        # intervention
+        build_dir = os.path.split(project_dir)[0]
+        cmd.get_finalized_command('build_py').build_lib = build_dir
+
+        # get_outputs should return 4 elements: spam/__init__.py, .pyc and
+        # .pyo, foo.import-tag-abiflags.so / foo.pyd
+        outputs = cmd.get_outputs()
+        self.assertEqual(len(outputs), 4, outputs)
+
+    def test_get_inputs(self):
+        project_dir, dist = self.create_dist()
+        os.chdir(project_dir)
+        os.mkdir('spam')
+        cmd = install_lib(dist)
+
+        # setting up a dist environment
+        cmd.compile = True
+        cmd.optimize = 1
+        cmd.install_dir = self.mkdtemp()
+        f = os.path.join(project_dir, 'spam', '__init__.py')
+        self.write_file(f, '# python package')
+        cmd.distribution.ext_modules = [Extension('foo', ['xxx'])]
+        cmd.distribution.packages = ['spam']
+
+        # get_inputs should return 2 elements: spam/__init__.py and
+        # foo.import-tag-abiflags.so / foo.pyd
+        inputs = cmd.get_inputs()
+        self.assertEqual(len(inputs), 2, inputs)
 
 
 def test_suite():
