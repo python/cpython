5d032c0f95d4794edcf38253aa54a0e970e5249b
Jason R. Coombs <jaraco@jaraco.com>
2011-11-17 18:03:24 -0500

    PDB now will properly escape backslashes in the names of modules it executes. Fixes #7750

--- Lib/pdb.py
+++ Lib/pdb.py
@@ -1229,7 +1229,7 @@ see no sign that the breakpoint was reached.
         self._wait_for_mainpyfile = 1
         self.mainpyfile = self.canonic(filename)
         self._user_requested_quit = 0
-        statement = 'execfile( "%s")' % filename
+        statement = 'execfile(%r)' % filename
         self.run(statement)
 
 # Simplified interface
