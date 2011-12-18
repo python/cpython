165b1283ffe5922dd9f64ef7edb5534e6983d2f5
Antoine Pitrou <solipsis@pitrou.net>
2011-12-18 20:20:17 +0100

    Followup to #7502: add __hash__ method and tests.

--- Lib/doctest.py
+++ Lib/doctest.py
@@ -454,6 +454,10 @@ class Example:
     def __ne__(self, other):
         return not self == other
 
+    def __hash__(self):
+        return hash((self.source, self.want, self.lineno, self.indent,
+                     self.exc_msg))
+
 
 class DocTest:
     """
@@ -516,6 +520,9 @@ class DocTest:
 
     def __ne__(self, other):
         return not self == other
+
+    def __hash__(self):
+        return hash((self.docstring, self.name, self.filename, self.lineno))
 
     # This lets us sort tests by name:
     def __lt__(self, other):
@@ -2244,6 +2251,10 @@ class DocTestCase(unittest.TestCase):
 
     def __ne__(self, other):
         return not self == other
+
+    def __hash__(self):
+        return hash((self._dt_optionflags, self._dt_setUp, self._dt_tearDown,
+                     self._dt_checker))
 
     def __repr__(self):
         name = self._dt_test.name.split('.')
