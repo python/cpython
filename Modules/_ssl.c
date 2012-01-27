43450d5951b40f7b4648c27c814b03389d865e42
Antoine Pitrou <solipsis@pitrou.net>
2012-01-27 09:48:47 +0100

    Issue #13885: CVE-2011-3389: the _ssl module would always disable the CBC IV attack countermeasure.

--- Modules/_ssl.c
+++ Modules/_ssl.c
@@ -365,7 +365,8 @@ newPySSLObject(PySocketSockObject *Sock, char *key_file, char *cert_file,
     }
 
     /* ssl compatibility */
-    SSL_CTX_set_options(self->ctx, SSL_OP_ALL);
+    SSL_CTX_set_options(self->ctx,
+                        SSL_OP_ALL & ~SSL_OP_DONT_INSERT_EMPTY_FRAGMENTS);
 
     verification_mode = SSL_VERIFY_NONE;
     if (certreq == PY_SSL_CERT_OPTIONAL)
