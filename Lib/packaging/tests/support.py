67db8c01bd8b32417822091fd8a083ae98683b76
Ned Deily <nad@acm.org>
2012-02-03 02:46:37 +0100

    Issue #13901: Prevent test_packaging failures on OS X with --enable-shared.

--- Lib/packaging/tests/support.py
+++ Lib/packaging/tests/support.py
@@ -366,6 +366,9 @@ def fixup_build_ext(cmd):
         cmd = build_ext(dist)
         support.fixup_build_ext(cmd)
         cmd.ensure_finalized()
+
+    Unlike most other Unix platforms, Mac OS X embeds absolute paths
+    to shared libraries into executables, so the fixup is not needed there.
     """
     if os.name == 'nt':
         cmd.debug = sys.executable.endswith('_d.exe')
@@ -377,9 +380,11 @@ def fixup_build_ext(cmd):
         if runshared is None:
             cmd.library_dirs = ['.']
         else:
-            name, equals, value = runshared.partition('=')
-            cmd.library_dirs = value.split(os.pathsep)
-
+            if sys.platform == 'darwin':
+                cmd.library_dirs = []
+            else:
+                name, equals, value = runshared.partition('=')
+                cmd.library_dirs = value.split(os.pathsep)
 
 try:
     from test.support import skip_unless_symlink
