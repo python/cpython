476a0d3e29b2be2a420278caa90a423c6f38f76f
Charles-François Natali <neologix@free.fr>
2012-02-04 15:12:08 +0100

    Issue #8184: Fix a potential file descriptor leak when a multiprocessing.Connection socket can't be bound.

--- Lib/multiprocessing/connection.py
+++ Lib/multiprocessing/connection.py
@@ -575,10 +575,14 @@ class SocketListener(object):
     '''
     def __init__(self, address, family, backlog=1):
         self._socket = socket.socket(getattr(socket, family))
-        self._socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self._socket.bind(address)
-        self._socket.listen(backlog)
-        self._address = self._socket.getsockname()
+        try:
+            self._socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            self._socket.bind(address)
+            self._socket.listen(backlog)
+            self._address = self._socket.getsockname()
+        except OSError:
+            self._socket.close()
+            raise
         self._family = family
         self._last_accepted = None
 
