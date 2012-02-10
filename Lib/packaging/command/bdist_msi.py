1454d30dfb7e7b4baa2349194cfb46bf96215a96
Éric Araujo <merwok@netwok.org>
2012-02-10 05:20:53 +0100

    Use sys.version_info instead of sys.version in packaging.
    The contents of this attribute are an implementation detail, as
    documented for #9442, so we should not parse it, to support non-CPython
    VMs with distutils2 in the future.
    
    Unfortunately, one use comes directly from PEP 345, so an edit will have
    to be agreed before fixing the code (see comment in p7g.markers).
    
    Other remaining uses are found in p7g.compiler and could be replaced by
    the platform module (which also parses sys.version, but then it wouldn’t
    be my fault :)

--- Lib/packaging/command/bdist_msi.py
+++ Lib/packaging/command/bdist_msi.py
@@ -7,9 +7,8 @@ import sys
 import os
 import msilib
 
-
-from sysconfig import get_python_version
 from shutil import rmtree
+from sysconfig import get_python_version
 from packaging.command.cmd import Command
 from packaging.version import NormalizedVersion
 from packaging.errors import PackagingOptionError
@@ -204,7 +203,7 @@ class bdist_msi(Command):
             target_version = self.target_version
             if not target_version:
                 assert self.skip_build, "Should have already checked this"
-                target_version = sys.version[0:3]
+                target_version = '%s.%s' % sys.version_info[:2]
             plat_specifier = ".%s-%s" % (self.plat_name, target_version)
             build = self.get_finalized_command('build')
             build.build_lib = os.path.join(build.build_base,
