54411c1784bc404448f496e7a9bf657eaab14daf
Antoine Pitrou <solipsis@pitrou.net>
2012-02-12 19:14:17 +0100

    Issue #10287: nntplib now queries the server's CAPABILITIES again after authenticating (since the result may change, according to RFC 4643). Patch by Hynek Schlawack.

--- Lib/nntplib.py
+++ Lib/nntplib.py
@@ -364,7 +364,7 @@ class _NNTPBase:
             self.nntp_implementation = None
             try:
                 resp, caps = self.capabilities()
-            except NNTPPermanentError:
+            except (NNTPPermanentError, NNTPTemporaryError):
                 # Server doesn't support capabilities
                 self._caps = {}
             else:
@@ -941,6 +941,9 @@ class _NNTPBase:
                 resp = self._shortcmd('authinfo pass ' + password)
                 if not resp.startswith('281'):
                     raise NNTPPermanentError(resp)
+        # Capabilities might have changed after login
+        self._caps = None
+        self.getcapabilities()
         # Attempt to send mode reader if it was requested after login.
         if self.readermode_afterauth:
             self._setreadermode()
