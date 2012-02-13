63ff2c8bf06ac7704f69ae5b3ba92676f483bea2
Ezio Melotti <ezio.melotti@gmail.com>
2012-02-13 20:20:00 +0200

    Fix an index and clean up comments.

--- Lib/html/parser.py
+++ Lib/html/parser.py
@@ -270,12 +270,13 @@ class HTMLParser(_markupbase.ParserBase):
         if rawdata[i:i+2] != '<!':
             self.error('unexpected call to parse_html_declaration()')
         if rawdata[i:i+4] == '<!--':
+            # this case is actually already handled in goahead()
             return self.parse_comment(i)
         elif rawdata[i:i+3] == '<![':
             return self.parse_marked_section(i)
         elif rawdata[i:i+9].lower() == '<!doctype':
             # find the closing >
-            gtpos = rawdata.find('>', 9)
+            gtpos = rawdata.find('>', i+9)
             if gtpos == -1:
                 return -1
             self.handle_decl(rawdata[i+2:gtpos])
