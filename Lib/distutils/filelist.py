ac73e083f584b34d88cf39af4101bc9e34996a5f
Éric Araujo <merwok@netwok.org>
2012-02-25 16:13:53 +0100

    Fix long-standing bugs with MANIFEST.in parsing on Windows (#6884).
    These regex changes fix a number of issues for distutils on Windows:
    - #6884: impossible to include a file starting with 'build'
    - #9691 and #14004: sdist includes too many files
    - #13193: test_filelist failures
    
    This commit replaces the incorrect changes done in 557a973709de,
    c566a3447ba1 and 3925081a7ca0 to fix #13193; we were too eager to fix
    the test failures and I did not study the code enough before
    greenlighting patches.  This time we have unit tests from the problems
    reported by users to be sure we have the right fix.
    
    Thanks to Nadeem Vawda for his help.

--- Lib/distutils/filelist.py
+++ Lib/distutils/filelist.py
@@ -210,6 +210,7 @@ class FileList:
 
         Return 1 if files are found.
         """
+        # XXX docstring lying about what the special chars are?
         files_found = 0
         pattern_re = translate_pattern(pattern, anchor, prefix, is_regex)
         self.debug_print("include_pattern: applying regex r'%s'" %
@@ -297,11 +298,14 @@ def glob_to_re(pattern):
     # IMHO is wrong -- '?' and '*' aren't supposed to match slash in Unix,
     # and by extension they shouldn't match such "special characters" under
     # any OS.  So change all non-escaped dots in the RE to match any
-    # character except the special characters.
-    # XXX currently the "special characters" are just slash -- i.e. this is
-    # Unix-only.
-    pattern_re = re.sub(r'((?<!\\)(\\\\)*)\.', r'\1[^/]', pattern_re)
-
+    # character except the special characters (currently: just os.sep).
+    sep = os.sep
+    if os.sep == '\\':
+        # we're using a regex to manipulate a regex, so we need
+        # to escape the backslash twice
+        sep = r'\\\\'
+    escaped = r'\1[^%s]' % sep
+    pattern_re = re.sub(r'((?<!\\)(\\\\)*)\.', escaped, pattern_re)
     return pattern_re
 
 
@@ -328,8 +332,10 @@ def translate_pattern(pattern, anchor=1, prefix=None, is_regex=0):
         # ditch end of pattern character
         empty_pattern = glob_to_re('')
         prefix_re = glob_to_re(prefix)[:-len(empty_pattern)]
-        # paths should always use / in manifest templates
-        pattern_re = "^%s/.*%s" % (prefix_re, pattern_re)
+        sep = os.sep
+        if os.sep == '\\':
+            sep = r'\\'
+        pattern_re = "^" + sep.join((prefix_re, ".*" + pattern_re))
     else:                               # no prefix -- respect anchor flag
         if anchor:
             pattern_re = "^" + pattern_re
