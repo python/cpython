f2c8af01c1b2d1b00e4c97662505aa75f5665fa9
Stefan Krah <skrah@bytereef.org>
2012-03-06 15:27:31 +0100

    Whitespace.

--- Objects/abstract.c
+++ Objects/abstract.c
@@ -649,7 +649,7 @@ PyBuffer_FillContiguousStrides(int nd, Py_ssize_t *shape,
 
 int
 PyBuffer_FillInfo(Py_buffer *view, PyObject *obj, void *buf, Py_ssize_t len,
-              int readonly, int flags)
+                  int readonly, int flags)
 {
     if (view == NULL) return 0; /* XXX why not -1? */
     if (((flags & PyBUF_WRITABLE) == PyBUF_WRITABLE) &&
