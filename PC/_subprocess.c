3aee8e9612238a887f7974d30d3acb8baa37dadc
Antoine Pitrou <solipsis@pitrou.net>
2012-03-11 19:29:12 +0100

    Issue #14252: Fix subprocess.Popen.terminate() to not raise an error under Windows when the child process has already exited.

--- PC/_subprocess.c
+++ PC/_subprocess.c
@@ -670,4 +670,5 @@ init_subprocess()
     defint(d, "WAIT_OBJECT_0", WAIT_OBJECT_0);
     defint(d, "CREATE_NEW_CONSOLE", CREATE_NEW_CONSOLE);
     defint(d, "CREATE_NEW_PROCESS_GROUP", CREATE_NEW_PROCESS_GROUP);
+    defint(d, "STILL_ACTIVE", STILL_ACTIVE);
 }
