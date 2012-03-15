e9bc2f773ff86780ca86a36a21d43f3ecfd61357
Barry Warsaw <barry@python.org>
2012-03-14 17:10:41 -0700

    - Issue #14234: CVE-2012-0876: Randomize hashes of xml attributes in the hash   table internal to the pyexpat module's copy of the expat library to avoid a   denial of service due to hash collisions.  Patch by David Malcolm with some   modifications by the expat project.

--- Modules/pyexpat.c
+++ Modules/pyexpat.c
@@ -1355,6 +1355,8 @@ newxmlparseobject(char *encoding, char *namespace_separator, PyObject *intern)
     else {
         self->itself = XML_ParserCreate(encoding);
     }
+    XML_SetHashSalt(self->itself,
+                    (unsigned long)_Py_HashSecret.prefix);
     self->intern = intern;
     Py_XINCREF(self->intern);
 #ifdef Py_TPFLAGS_HAVE_GC
