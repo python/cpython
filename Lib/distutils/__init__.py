f9e8ca70a3ad5458daef05b3cc777f219b8c0467
Barry Warsaw <barry@python.org>
2012-03-17 18:19:15 -0400

    Bump to 2.6.8rc2

--- Lib/distutils/__init__.py
+++ Lib/distutils/__init__.py
@@ -22,5 +22,5 @@ __revision__ = "$Id$"
 #
 
 #--start constants--
-__version__ = "2.6.8rc1"
+__version__ = "2.6.8rc2"
 #--end constants--
--- Lib/idlelib/idlever.py
+++ Lib/idlelib/idlever.py
@@ -1,1 +1,1 @@
-IDLE_VERSION = "2.6.8rc1"
+IDLE_VERSION = "2.6.8rc2"
