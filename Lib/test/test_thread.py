14a47ae653b6d2e12423f70172472ac424b9fe22
Benjamin Peterson <benjamin@python.org>
2012-04-02 14:22:50 -0400

    remove uneeded line

--- Lib/test/test_thread.py
+++ Lib/test/test_thread.py
@@ -134,7 +134,6 @@ class ThreadRunningTests(BasicThreadTest):
         # See issue #14474
         def task():
             started.release()
-            sys.stderr = stderr
             raise SyntaxError
         def mywrite(self, *args):
             try:
