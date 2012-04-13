7688168ed164af4d49f6ee908f2e5ccb86753e61
Michael Foord <michael@voidspace.org.uk>
2012-04-13 16:57:22 +0100

    unittest.mock.PropertyMock return value and attributes are now standard MagicMocks

--- Lib/unittest/mock.py
+++ Lib/unittest/mock.py
@@ -2166,6 +2166,9 @@ class PropertyMock(Mock):
     Fetching a `PropertyMock` instance from an object calls the mock, with
     no args. Setting it calls the mock with the value being set.
     """
+    def _get_child_mock(self, **kwargs):
+        return MagicMock(**kwargs)
+
     def __get__(self, obj, obj_type):
         return self()
     def __set__(self, obj, val):
