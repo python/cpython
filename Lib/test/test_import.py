09ec129b86b370eba2965a4f865e2e47e84092d2
Brett Cannon <brett@python.org>
2012-04-20 17:34:59 -0400

    Fix a cleanup.

--- Lib/test/test_import.py
+++ Lib/test/test_import.py
@@ -456,7 +456,7 @@ class PathsTests(unittest.TestCase):
 
     # Regression test for http://bugs.python.org/issue3677.
     @unittest.skipUnless(sys.platform == 'win32', 'Windows-specific')
-    def _test_UNC_path(self):
+    def test_UNC_path(self):
         with open(os.path.join(self.path, 'test_trailing_slash.py'), 'w') as f:
             f.write("testdata = 'test_trailing_slash'")
         # Create the UNC path, like \\myhost\c$\foo\bar.
