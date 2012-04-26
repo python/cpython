9eebeeef72f89c7bb77adfcfef90b68581af553c
Brett Cannon <brett@python.org>
2012-04-25 20:49:19 -0400

    Silence DeprecationWarning for cgi.escape() usage in test_cgi.

--- Lib/test/test_cgi.py
+++ Lib/test/test_cgi.py
@@ -4,6 +4,7 @@ import os
 import sys
 import tempfile
 import unittest
+import warnings
 from io import StringIO, BytesIO
 
 class HackedSysModule:
@@ -119,9 +120,13 @@ def gen_result(data, environ):
 class CgiTests(unittest.TestCase):
 
     def test_escape(self):
-        self.assertEqual("test &amp; string", cgi.escape("test & string"))
-        self.assertEqual("&lt;test string&gt;", cgi.escape("<test string>"))
-        self.assertEqual("&quot;test string&quot;", cgi.escape('"test string"', True))
+        # cgi.escape() is deprecated.
+        with warnings.catch_warnings():
+            warnings.filterwarnings('ignore', 'cgi\.escape',
+                                     DeprecationWarning)
+            self.assertEqual("test &amp; string", cgi.escape("test & string"))
+            self.assertEqual("&lt;test string&gt;", cgi.escape("<test string>"))
+            self.assertEqual("&quot;test string&quot;", cgi.escape('"test string"', True))
 
     def test_strict(self):
         for orig, expect in parse_strict_test_cases:
