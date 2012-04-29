cc41bbbeb99d0513e713988d4e49384a3ce32f72
Brett Cannon <brett@python.org>
2012-04-29 14:38:11 -0400

    Issues #13959, 14647: Re-implement imp.reload() in Lib/imp.py.
    Thanks to Eric Snow for the patch.

--- Python/pystate.c
+++ Python/pystate.c
@@ -69,7 +69,6 @@ PyInterpreterState_New(void)
             Py_FatalError("Can't initialize threads for interpreter");
 #endif
         interp->modules = NULL;
-        interp->modules_reloading = NULL;
         interp->modules_by_index = NULL;
         interp->sysdict = NULL;
         interp->builtins = NULL;
@@ -114,7 +113,6 @@ PyInterpreterState_Clear(PyInterpreterState *interp)
     Py_CLEAR(interp->codec_error_registry);
     Py_CLEAR(interp->modules);
     Py_CLEAR(interp->modules_by_index);
-    Py_CLEAR(interp->modules_reloading);
     Py_CLEAR(interp->sysdict);
     Py_CLEAR(interp->builtins);
     Py_CLEAR(interp->importlib);
