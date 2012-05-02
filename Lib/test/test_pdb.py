cb172041d356b1a61588b8176985303c7a7d2c98
Senthil Kumaran <senthil@uthcode.com>
2012-05-02 08:00:22 +0800

    fix windows test failure - issue13183

--- Lib/test/test_pdb.py
+++ Lib/test/test_pdb.py
@@ -653,7 +653,7 @@ class PdbTestCase(unittest.TestCase):
         """
         bar = """
             def bar():
-                print('1')
+                pass
         """
         with open('bar.py', 'w') as f:
             f.write(textwrap.dedent(bar))
