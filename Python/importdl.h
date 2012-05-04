21f09ca26983b1239dc61779deceeb7356b29062
Brett Cannon <brett@python.org>
2012-05-04 16:13:30 -0400

    Issue #13959: Move module type constants to Lib/imp.py.

--- Python/importdl.h
+++ Python/importdl.h
@@ -6,21 +6,6 @@ extern "C" {
 #endif
 
 
-/* Definitions for dynamic loading of extension modules */
-enum filetype {
-    SEARCH_ERROR,
-    PY_SOURCE,
-    PY_COMPILED,
-    C_EXTENSION,
-    PY_RESOURCE, /* Mac only */
-    PKG_DIRECTORY,
-    C_BUILTIN,
-    PY_FROZEN,
-    PY_CODERESOURCE, /* Mac only */
-    IMP_HOOK
-};
-
-
 extern const char *_PyImport_DynLoadFiletab[];
 
 extern PyObject *_PyImport_LoadDynamicModule(PyObject *name, PyObject *pathname,
