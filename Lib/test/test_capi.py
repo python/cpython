73954040f6b9459e59803fd62af14c547a7b3636
Vinay Sajip <vinay_sajip@yahoo.co.uk>
2012-05-06 11:34:50 +0100

    Issue #9116: Allowed test to pass on Windows by adjusting the test condition slightly to allow for a Windows-specific error message.

--- Lib/test/test_capi.py
+++ Lib/test/test_capi.py
@@ -48,9 +48,9 @@ class CAPITest(unittest.TestCase):
         (out, err) = p.communicate()
         self.assertEqual(out, b'')
         # This used to cause an infinite loop.
-        self.assertEqual(err.rstrip(),
+        self.assertTrue(err.rstrip().startswith(
                          b'Fatal Python error:'
-                         b' PyThreadState_Get: no current thread')
+                         b' PyThreadState_Get: no current thread'))
 
     def test_memoryview_from_NULL_pointer(self):
         self.assertRaises(ValueError, _testcapi.make_memoryview_from_NULL_pointer)
