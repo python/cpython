e634031c298cedbae0b5073ad2654401ce7a3271
Brian Curtin <brian@python.org>
2012-05-13 18:59:26 -0500

    Correct PlatformTarget->Platform usage for building the solution

--- Tools/buildbot/build-amd64.bat
+++ Tools/buildbot/build-amd64.bat
@@ -4,4 +4,4 @@ call "%VS100COMNTOOLS%\..\..\VC\vcvarsall.bat" x86_amd64
 cmd /c Tools\buildbot\clean-amd64.bat
 msbuild /p:useenv=true PCbuild\kill_python.vcxproj /p:Configuration=Debug /p:PlatformTarget=x64
 PCbuild\amd64\kill_python_d.exe
-msbuild /p:useenv=true PCbuild\pcbuild.sln /p:Configuration=Debug /p:PlatformTarget=x64
+msbuild /p:useenv=true PCbuild\pcbuild.sln /p:Configuration=Debug /p:Platform=x64
--- Tools/buildbot/build.bat
+++ Tools/buildbot/build.bat
@@ -4,5 +4,5 @@ call "%VS100COMNTOOLS%vsvars32.bat"
 cmd /c Tools\buildbot\clean.bat
 msbuild /p:useenv=true PCbuild\kill_python.vcxproj /p:Configuration=Debug /p:PlatformTarget=x86
 PCbuild\kill_python_d.exe
-msbuild /p:useenv=true PCbuild\pcbuild.sln /p:Configuration=Debug /p:PlatformTarget=x86
+msbuild /p:useenv=true PCbuild\pcbuild.sln /p:Configuration=Debug /p:Platform=Win32
 
