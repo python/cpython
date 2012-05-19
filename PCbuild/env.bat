02c0b20148f4de74f4c6584713de833033156c20
Kristján Valur Jónsson <kristjan@ccpgames.com>
2012-05-19 21:10:14 +0000

    Clean up the PCBuild project files, removing redundant settings and use "references" to link to dependent projects. Update readme and batch files.

--- PCbuild/env.bat
+++ PCbuild/env.bat
@@ -1,5 +1,9 @@
 @echo off
-set VS9=%ProgramFiles%\Microsoft Visual Studio 9.0
+set VS10=%ProgramFiles(x86)%\Microsoft Visual Studio 10.0
+IF EXIST "%VS10%" GOTO ok
+set VS10=%ProgramFiles%\Microsoft Visual Studio 10.0
+:ok
+
 echo Build environments: x86, ia64, amd64, x86_amd64, x86_ia64
 echo.
-call "%VS9%\VC\vcvarsall.bat" %1
+call "%VS10%\VC\vcvarsall.bat" %1
