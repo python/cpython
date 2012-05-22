e045dbb0fd15963b449c52168aa2e9e92c1ecf50
Brian Curtin <brian@python.org>
2012-05-22 11:04:32 -0500

    Update build.bat for VS2010

--- PCbuild/build.bat
+++ PCbuild/build.bat
@@ -5,14 +5,15 @@ rem just for convenience.
 setlocal
 set platf=Win32
 set conf=Release
-set build=
+set target=build
+set dir=%~dp0
 
 :CheckOpts
 if "%1"=="-c" (set conf=%2) & shift & shift & goto CheckOpts
 if "%1"=="-p" (set platf=%2) & shift & shift & goto CheckOpts
-if "%1"=="-r" (set build=/rebuild) & shift & goto CheckOpts
+if "%1"=="-r" (set target=rebuild) & shift & goto CheckOpts
 if "%1"=="-d" (set conf=Debug) & shift & goto CheckOpts
 
-set cmd=vcbuild /useenv pcbuild.sln %build% "%conf%|%platf%"
+set cmd=msbuild /p:useenv=true %dir%pcbuild.sln /t:%target% /p:Configuration=%conf% /p:Platform=%platf%
 echo %cmd%
 %cmd%
