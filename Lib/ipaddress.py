e9d600ccbe025960636f34a1c24bbe46a3d3b762
Nick Coghlan <ncoghlan@gmail.com>
2012-05-27 00:57:25 +1000

    Issue #14814: Cleanup ipaddress header comments

--- Lib/ipaddress.py
+++ Lib/ipaddress.py
@@ -1,17 +1,5 @@
 # Copyright 2007 Google Inc.
 #  Licensed to PSF under a Contributor Agreement.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
-# implied. See the License for the specific language governing
-# permissions and limitations under the License.
 
 """A fast, lightweight IPv4/IPv6 manipulation library in Python.
 
