cca2ffeba9ed089b709b515fa41ad5748e1e6cbe
Martin v. Löwis <martin@v.loewis.de>
2012-06-03 12:03:29 +0200

    Drop unused constant.

--- Lib/idlelib/AutoComplete.py
+++ Lib/idlelib/AutoComplete.py
@@ -9,9 +9,6 @@ import string
 
 from idlelib.configHandler import idleConf
 
-# This string includes all chars that may be in a file name (without a path
-# separator)
-FILENAME_CHARS = string.ascii_letters + string.digits + os.curdir + "._~#$:-"
 # This string includes all chars that may be in an identifier
 ID_CHARS = string.ascii_letters + string.digits + "_"
 
