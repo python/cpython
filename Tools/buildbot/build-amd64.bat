437bdea96b51aa28b650f890a8d1553a6d0769b3
Stefan Krah <skrah@bytereef.org>
2012-06-23 18:57:45 +0200

    Issue #15102: Allow platform-specific settings for the current project to override environment variables.

--- Tools/buildbot/build-amd64.bat
+++ Tools/buildbot/build-amd64.bat
@@ -2,6 +2,6 @@
 cmd /c Tools\buildbot\external-amd64.bat
 call "%VS100COMNTOOLS%\..\..\VC\vcvarsall.bat" x86_amd64
 cmd /c Tools\buildbot\clean-amd64.bat
-msbuild /p:useenv=true PCbuild\kill_python.vcxproj /p:Configuration=Debug /p:PlatformTarget=x64
+msbuild PCbuild\kill_python.vcxproj /p:Configuration=Debug /p:PlatformTarget=x64
 PCbuild\amd64\kill_python_d.exe
-msbuild /p:useenv=true PCbuild\pcbuild.sln /p:Configuration=Debug /p:Platform=x64
+msbuild PCbuild\pcbuild.sln /p:Configuration=Debug /p:Platform=x64
