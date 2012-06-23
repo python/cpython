2193536e79c33654cadf85f4b6d8bbe27b43fd2c
Brian Curtin <brian@python.org>
2012-06-22 22:48:06 -0500

    Fix #15148. Make the shutil.which docstring more thorough

--- Lib/shutil.py
+++ Lib/shutil.py
@@ -973,7 +973,9 @@ def get_terminal_size(fallback=(80, 24)):
 
 def which(cmd, mode=os.F_OK | os.X_OK, path=None):
     """Given a command, mode, and a path string, return the path which
-    conforms to the given mode on the path."""
+    conforms to the given mode on the PATH, or None if there is no such file.
+    `mode` defaults to os.F_OK | os.X_OK. `path` defaults to the result of
+    os.environ.get("PATH"), or can be overridden with a custom search path."""
     # Check that a given file can be accessed with the correct mode.
     # Additionally check that `file` is not a directory, as on Windows
     # directories pass the os.access check.
