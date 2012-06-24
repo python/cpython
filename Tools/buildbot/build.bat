3c0db67bf349cc379ad4d096cfb5bc0854378e95
Stefan Krah <skrah@bytereef.org>
2012-06-24 22:00:44 +0200

    Drop useenv=true also from the 32-bit build script: It is not necessary and leads to failures in successive 64-bit/32-bit builds in the same shell window.

--- Tools/buildbot/build.bat
+++ Tools/buildbot/build.bat
@@ -2,7 +2,7 @@
 cmd /c Tools\buildbot\external.bat
 call "%VS100COMNTOOLS%vsvars32.bat"
 cmd /c Tools\buildbot\clean.bat
-msbuild /p:useenv=true PCbuild\kill_python.vcxproj /p:Configuration=Debug /p:PlatformTarget=x86
+msbuild PCbuild\kill_python.vcxproj /p:Configuration=Debug /p:PlatformTarget=x86
 PCbuild\kill_python_d.exe
-msbuild /p:useenv=true PCbuild\pcbuild.sln /p:Configuration=Debug /p:Platform=Win32
+msbuild PCbuild\pcbuild.sln /p:Configuration=Debug /p:Platform=Win32
 
