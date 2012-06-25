1edab78859b52fcd033011303be0905b3fa2cb1a
Stefan Krah <skrah@bytereef.org>
2012-06-25 14:41:37 +0200

    Update test script to Visual Studio 2010.

--- Modules/_decimal/tests/runall.bat
+++ Modules/_decimal/tests/runall.bat
@@ -2,37 +2,49 @@
 
 rem Test all machine configurations, pydebug, refleaks, release build.
 
-cd ..
+cd ..\..\..\
 
-call vcvarsall x64
+
+echo.
+echo # ======================================================================
+echo #                            Building Python
+echo # ======================================================================
+echo.
+
+call "%VS100COMNTOOLS%\..\..\VC\vcvarsall.bat" x64
+msbuild /noconsolelogger /target:clean PCbuild\pcbuild.sln /p:Configuration=Release /p:PlatformTarget=x64
+msbuild /noconsolelogger /target:clean PCbuild\pcbuild.sln /p:Configuration=Debug /p:PlatformTarget=x64
+msbuild /noconsolelogger PCbuild\pcbuild.sln /p:Configuration=Release /p:Platform=x64
+msbuild /noconsolelogger PCbuild\pcbuild.sln /p:Configuration=Debug /p:Platform=x64
+
+call "%VS100COMNTOOLS%\..\..\VC\vcvarsall.bat" x86
+msbuild /noconsolelogger PCbuild\pcbuild.sln /p:Configuration=Release /p:Platform=Win32
+msbuild /noconsolelogger PCbuild\pcbuild.sln /p:Configuration=Debug /p:Platform=Win32
+echo.
+echo.
+
 echo.
 echo # ======================================================================
 echo #                       test_decimal: platform=x64
 echo # ======================================================================
 echo.
 
-cd ..\..\PCbuild
+cd PCbuild\amd64
+
 echo # ==================== refleak tests =======================
 echo.
-echo building python ...
+python_d.exe -m test -uall -R 2:2 test_decimal
 echo.
-vcbuild /clean pcbuild.sln > NUL 2>&1
-vcbuild pcbuild.sln "Debug|x64" > NUL 2>&1
-amd64\python_d.exe -m test -uall -R 2:2 test_decimal
 echo.
 
 echo # ==================== regular tests =======================
 echo.
-echo building python ...
-echo.
-vcbuild /clean pcbuild.sln > NUL 2>&1
-vcbuild pcbuild.sln "Release|x64" > NUL 2>&1
-amd64\python.exe -m test -uall test_decimal
+python.exe -m test -uall test_decimal
 echo.
 echo.
 
+cd ..
 
-call vcvarsall x86
 echo.
 echo # ======================================================================
 echo #                       test_decimal: platform=x86
@@ -41,75 +53,53 @@ echo.
 
 echo # ==================== refleak tests =======================
 echo.
-echo building python ...
-echo.
-vcbuild /clean pcbuild.sln > NUL 2>&1
-vcbuild pcbuild.sln "Debug|win32" > NUL 2>&1
 python_d.exe -m test -uall -R 2:2 test_decimal
+echo.
 echo.
 
 echo # ==================== regular tests =======================
 echo.
-echo building python ...
-echo.
-vcbuild /clean pcbuild.sln > NUL 2>&1
-vcbuild pcbuild.sln "Release|win32" > NUL 2>&1
 python.exe -m test -uall test_decimal
 echo.
 echo.
 
+cd amd64
 
-call vcvarsall x64
 echo.
 echo # ======================================================================
 echo #                         deccheck: platform=x64
 echo # ======================================================================
 echo.
-echo.
+
 echo # ==================== debug build =======================
 echo.
-echo building python ...
-echo.
-vcbuild /clean pcbuild.sln > NUL 2>&1
-vcbuild pcbuild.sln "Debug|x64" > NUL 2>&1
-amd64\python_d.exe ..\Modules\_decimal\tests\deccheck.py
+python_d.exe ..\..\Modules\_decimal\tests\deccheck.py
 echo.
 echo.
 
 echo # =================== release build ======================
 echo.
-echo building python ...
-echo.
-vcbuild /clean pcbuild.sln > NUL 2>&1
-vcbuild pcbuild.sln "Release|x64" > NUL 2>&1
-amd64\python.exe ..\Modules\_decimal\tests\deccheck.py
+python.exe ..\..\Modules\_decimal\tests\deccheck.py
 echo.
 echo.
 
+cd ..
 
-call vcvarsall x86
 echo.
 echo # ======================================================================
 echo #                         deccheck: platform=x86
 echo # ======================================================================
 echo.
 echo.
+
 echo # ==================== debug build =======================
 echo.
-echo building python ...
-echo.
-vcbuild /clean pcbuild.sln > NUL 2>&1
-vcbuild pcbuild.sln "Debug|win32" > NUL 2>&1
 python_d.exe ..\Modules\_decimal\tests\deccheck.py
 echo.
 echo.
 
 echo # =================== release build ======================
 echo.
-echo building python ...
-echo.
-vcbuild /clean pcbuild.sln > NUL 2>&1
-vcbuild pcbuild.sln "Release|win32" > NUL 2>&1
 python.exe ..\Modules\_decimal\tests\deccheck.py
 echo.
 echo.
