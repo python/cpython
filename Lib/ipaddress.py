7ce7564871d78a4dea1a2c657b4e09551da3fe53
Nick Coghlan <ncoghlan@gmail.com>
2012-07-08 21:38:12 +1000

    Issue 14814: Remove dead function (noticed by Serhiy Storchaka)

--- Lib/ipaddress.py
+++ Lib/ipaddress.py
@@ -183,24 +183,6 @@ def _find_address_range(addresses):
     return (first, last)
 
 
-def _get_prefix_length(number1, number2, bits):
-    """Get the number of leading bits that are same for two numbers.
-
-    Args:
-        number1: an integer.
-        number2: another integer.
-        bits: the maximum number of bits to compare.
-
-    Returns:
-        The number of leading bits that are the same for two numbers.
-
-    """
-    for i in range(bits):
-        if number1 >> i == number2 >> i:
-            return bits - i
-    return 0
-
-
 def _count_righthand_zero_bits(number, bits):
     """Count the number of zero bits on the right hand side.
 
