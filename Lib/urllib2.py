01fe5fa8ea52ba49063d20a5044fb84efdbabe48
Senthil Kumaran <senthil@uthcode.com>
2012-07-07 17:37:53 -0700

    Fix issue14826 - make urllib.request.Request quoted url consistent with URLOpener open method. Patch contributed by Stephen Thorne.

--- Lib/urllib2.py
+++ Lib/urllib2.py
@@ -110,7 +110,7 @@ except ImportError:
     from StringIO import StringIO
 
 from urllib import (unwrap, unquote, splittype, splithost, quote,
-     addinfourl, splitport, splittag,
+     addinfourl, splitport, splittag, toBytes,
      splitattr, ftpwrapper, splituser, splitpasswd, splitvalue)
 
 # support for FileHandler, proxies via environment variables
@@ -196,7 +196,8 @@ class Request:
     def __init__(self, url, data=None, headers={},
                  origin_req_host=None, unverifiable=False):
         # unwrap('<URL:type://host/path>') --> 'type://host/path'
-        self.__original = unwrap(url)
+        self.__original = unwrap(toBytes(url))
+        self.__original = quote(self.__original, safe="%/:=&?~#+!$,;'@()*[]|")
         self.__original, self.__fragment = splittag(self.__original)
         self.type = None
         # self.__r_type is what's left after doing the splittype
