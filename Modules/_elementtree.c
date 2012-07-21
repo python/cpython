9c2b544837aa276934814732ef20a05347e0e7c9
Eli Bendersky <eliben@gmail.com>
2012-07-21 20:28:46 +0300

    Raise ImportError if pyexpat's version is incompatible

--- Modules/_elementtree.c
+++ Modules/_elementtree.c
@@ -3114,11 +3114,14 @@ PyInit__elementtree(void)
             expat_capi->size < sizeof(struct PyExpat_CAPI) ||
             expat_capi->MAJOR_VERSION != XML_MAJOR_VERSION ||
             expat_capi->MINOR_VERSION != XML_MINOR_VERSION ||
-            expat_capi->MICRO_VERSION != XML_MICRO_VERSION)
-            expat_capi = NULL;
-    }
-    if (!expat_capi)
+            expat_capi->MICRO_VERSION != XML_MICRO_VERSION) {
+            PyErr_SetString(PyExc_ImportError,
+                            "pyexpat version is incompatible");
+            return NULL;
+        }
+    } else {
         return NULL;
+    }
 #endif
 
     elementtree_parseerror_obj = PyErr_NewException(
