106ac119f841b7f6b843bea47a30ead0ebbdee81
Antoine Pitrou <solipsis@pitrou.net>
2012-09-05 20:11:49 +0200

    Issue #15841: The readable(), writable() and seekable() methods of io.BytesIO and io.StringIO objects now raise ValueError when the object has been closed. Patch by Alessandro Moura.

--- Lib/_pyio.py
+++ Lib/_pyio.py
@@ -883,12 +883,18 @@ class BytesIO(BufferedIOBase):
         return pos
 
     def readable(self):
+        if self.closed:
+            raise ValueError("I/O operation on closed file.")
         return True
 
     def writable(self):
+        if self.closed:
+            raise ValueError("I/O operation on closed file.")
         return True
 
     def seekable(self):
+        if self.closed:
+            raise ValueError("I/O operation on closed file.")
         return True
 
 
@@ -1546,6 +1552,8 @@ class TextIOWrapper(TextIOBase):
         return self._buffer
 
     def seekable(self):
+        if self.closed:
+            raise ValueError("I/O operation on closed file.")
         return self._seekable
 
     def readable(self):
