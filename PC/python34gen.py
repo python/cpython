70c5cc294067d98e45d136263c233dc539bbaf41
Georg Brandl <georg@python.org>
2012-09-29 09:34:13 +0200

    Bump version to 3.4.0 alpha 0.

--- PC/python34gen.py
+++ PC/python34gen.py
@@ -1,9 +1,9 @@
-# Generate python33stub.def out of python3.def
+# Generate python34stub.def out of python3.def
 # The regular import library cannot be used,
 # since it doesn't provide the right symbols for
 # data forwarding
-out = open("python33stub.def", "w")
-out.write('LIBRARY "python33"\n')
+out = open("python34stub.def", "w")
+out.write('LIBRARY "python34"\n')
 out.write('EXPORTS\n')
 
 inp = open("python3.def")
@@ -14,7 +14,7 @@ line = inp.readline() # LIBRARY
 assert line.strip()=='EXPORTS'
 
 for line in inp:
-    # SYM1=python33.SYM2[ DATA]
+    # SYM1=python34.SYM2[ DATA]
     head, tail = line.split('.')
     if 'DATA' in tail:
         symbol, tail = tail.split(' ')
