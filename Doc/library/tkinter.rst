093464324cda0e188c6f90fddc2b101f80c5e0cb
Andrew Svetlov <andrew.svetlov@gmail.com>
2012-10-04 22:10:09 +0300

    Issue #14446: Remove deprecated tkinter functions
    Thanks to  Michael Driscoll

--- Doc/library/tkinter.rst
+++ Doc/library/tkinter.rst
@@ -750,32 +750,6 @@ Entry widget indexes (index, view index, etc.)
    displayed.  You can use these :mod:`tkinter` functions to access these special
    points in text widgets:
 
-.. function:: AtEnd()
-      refers to the last position in the text
-
-   .. deprecated:: 3.3
-
-.. function:: AtInsert()
-      refers to the point where the text cursor is
-
-   .. deprecated:: 3.3
-
-.. function:: AtSelFirst()
-      indicates the beginning point of the selected text
-
-   .. deprecated:: 3.3
-
-.. function:: AtSelLast()
-      denotes the last point of the selected text and finally
-
-   .. deprecated:: 3.3
-
-.. function:: At(x[, y])
-      refers to the character at pixel location *x*, *y* (with *y* not used in the
-      case of a text entry widget, which contains a single line of text).
-
-   .. deprecated:: 3.3
-
 Text widget indexes
    The index notation for Text widgets is very rich and is best described in the Tk
    man pages.
--- Lib/tkinter/__init__.py
+++ Lib/tkinter/__init__.py
@@ -2146,45 +2146,6 @@ class Button(Widget):
         """
         return self.tk.call(self._w, 'invoke')
 
-
-# Indices:
-# XXX I don't like these -- take them away
-def AtEnd():
-    warnings.warn("tkinter.AtEnd will be removed in 3.4",
-                  DeprecationWarning, stacklevel=2)
-    return 'end'
-
-
-def AtInsert(*args):
-    warnings.warn("tkinter.AtInsert will be removed in 3.4",
-                  DeprecationWarning, stacklevel=2)
-    s = 'insert'
-    for a in args:
-        if a: s = s + (' ' + a)
-    return s
-
-
-def AtSelFirst():
-    warnings.warn("tkinter.AtSelFirst will be removed in 3.4",
-                  DeprecationWarning, stacklevel=2)
-    return 'sel.first'
-
-
-def AtSelLast():
-    warnings.warn("tkinter.AtSelLast will be removed in 3.4",
-                  DeprecationWarning, stacklevel=2)
-    return 'sel.last'
-
-
-def At(x, y=None):
-    warnings.warn("tkinter.At will be removed in 3.4",
-                  DeprecationWarning, stacklevel=2)
-    if y is None:
-        return '@%r' % (x,)
-    else:
-        return '@%r,%r' % (x, y)
-
-
 class Canvas(Widget, XView, YView):
     """Canvas widget to display graphical elements like lines or text."""
     def __init__(self, master=None, cnf={}, **kw):
