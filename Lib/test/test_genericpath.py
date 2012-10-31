49369f7088d8678f9fa31b964b02d1b5d51baa10
Victor Stinner <victor.stinner@gmail.com>
2012-10-31 23:01:30 +0100

    Issue #15441, #15478: Reenable test_nonascii_abspath() on Windows
    The real bug (issue #15478) has been fixed correctly.

--- Lib/test/test_genericpath.py
+++ Lib/test/test_genericpath.py
@@ -311,14 +311,10 @@ class CommonTest(GenericTest):
     @unittest.skipIf(sys.platform == 'darwin',
         "Mac OS X denies the creation of a directory with an invalid utf8 name")
     def test_nonascii_abspath(self):
-        name = b'\xe7w\xf0'
-        if sys.platform == 'win32':
-            try:
-                os.fsdecode(name)
-            except UnicodeDecodeError:
-                self.skipTest("the filename %a is not decodable "
-                              "from the ANSI code page %s"
-                              % (name, sys.getfilesystemencoding()))
+        if support.TESTFN_UNDECODABLE:
+            name = support.TESTFN_UNDECODABLE
+        else:
+            name = b'a\xffb\xe7w\xf0'
 
         # Test non-ASCII, non-UTF8 bytes in the path.
         with warnings.catch_warnings():
