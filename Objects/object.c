42124a727d0516e8b98b4b2d3dbae4044d34a836
Benjamin Peterson <benjamin@python.org>
2012-10-30 23:41:54 -0400

    initialize map/filter/zip in _PyBuiltin_Init rather than the catch-all function

--- Objects/object.c
+++ Objects/object.c
@@ -1708,15 +1708,6 @@ _Py_ReadyTypes(void)
     if (PyType_Ready(&PyMemberDescr_Type) < 0)
         Py_FatalError("Can't initialize member descriptor type");
 
-    if (PyType_Ready(&PyFilter_Type) < 0)
-        Py_FatalError("Can't initialize filter type");
-
-    if (PyType_Ready(&PyMap_Type) < 0)
-        Py_FatalError("Can't initialize map type");
-
-    if (PyType_Ready(&PyZip_Type) < 0)
-        Py_FatalError("Can't initialize zip type");
-
     if (PyType_Ready(&_PyNamespace_Type) < 0)
         Py_FatalError("Can't initialize namespace type");
 
