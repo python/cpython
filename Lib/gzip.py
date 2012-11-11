04050b80b1d467737aa8c0192877fcc4cf72adc8
Nadeem Vawda <nadeem.vawda@gmail.com>
2012-11-11 13:52:10 +0100

    Issue #15677: Document that zlib and gzip accept a compression level of 0 to mean 'no compression'.
    Patch by Brian Brazil.

--- Lib/gzip.py
+++ Lib/gzip.py
@@ -66,9 +66,10 @@ class GzipFile(io.BufferedIOBase):
         Be aware that only the 'rb', 'ab', and 'wb' values should be used
         for cross-platform portability.
 
-        The compresslevel argument is an integer from 1 to 9 controlling the
+        The compresslevel argument is an integer from 0 to 9 controlling the
         level of compression; 1 is fastest and produces the least compression,
-        and 9 is slowest and produces the most compression.  The default is 9.
+        and 9 is slowest and produces the most compression. 0 is no compression
+        at all. The default is 9.
 
         The mtime argument is an optional numeric timestamp to be written
         to the stream when compressing.  All gzip compressed streams
