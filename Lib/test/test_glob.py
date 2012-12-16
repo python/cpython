124ee8b1abb8f9d5b03eae20abb9405ce3fa5d7b
Antoine Pitrou <solipsis@pitrou.net>
2012-12-16 13:55:47 +0100

    Issue #16626: Fix infinite recursion in glob.glob() on Windows when the pattern contains a wildcard in the drive or UNC path. Patch by Serhiy Storchaka.

--- Lib/test/test_glob.py
+++ Lib/test/test_glob.py
@@ -3,6 +3,7 @@ from test.test_support import run_unittest, TESTFN
 import glob
 import os
 import shutil
+import sys
 
 
 class GlobTests(unittest.TestCase):
@@ -109,6 +110,14 @@ class GlobTests(unittest.TestCase):
             eq(self.glob('sym*'), [self.norm('sym1'), self.norm('sym2')])
             eq(self.glob('sym1'), [self.norm('sym1')])
             eq(self.glob('sym2'), [self.norm('sym2')])
+
+    @unittest.skipUnless(sys.platform == "win32", "Win32 specific test")
+    def test_glob_magic_in_drive(self):
+        eq = self.assertSequencesEqual_noorder
+        eq(glob.glob('*:'), [])
+        eq(glob.glob(u'*:'), [])
+        eq(glob.glob('?:'), [])
+        eq(glob.glob(u'?:'), [])
 
 
 def test_main():
