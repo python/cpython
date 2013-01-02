a708adfcf663e871dd3acdbea358f9bc047ec64b
Benjamin Peterson <benjamin@python.org>
2013-01-02 12:21:32 -0600

    call PyErr_Clear() when ignoring error from PyNumber_Int (closes #15516)
    Patch from Tom Tromey.

--- Objects/stringobject.c
+++ Objects/stringobject.c
@@ -4489,7 +4489,10 @@ PyString_Format(PyObject *format, PyObject *args)
                     }
                     else {
                         iobj = PyNumber_Int(v);
-                        if (iobj==NULL) iobj = PyNumber_Long(v);
+                        if (iobj==NULL) {
+			    PyErr_Clear();
+			    iobj = PyNumber_Long(v);
+			}
                     }
                     if (iobj!=NULL) {
                         if (PyInt_Check(iobj)) {
