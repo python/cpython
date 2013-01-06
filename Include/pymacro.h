1a19196a9ea3bb1db929ed9df7e9172f21d89861
Christian Heimes <christian@cheimes.de>
2013-01-06 16:41:56 +0100

    Issue #16881: Fix Py_ARRAY_LENGTH macro for GCC < 3.1.

--- Include/pymacro.h
+++ Include/pymacro.h
@@ -29,8 +29,11 @@
    parameters. With correct compiler support, such usage will cause a build
    error (see Py_BUILD_ASSERT_EXPR).
 
-   Written by Rusty Russell, public domain, http://ccodearchive.net/ */
-#if (defined(__GNUC__) && !defined(__STRICT_ANSI__))
+   Written by Rusty Russell, public domain, http://ccodearchive.net/
+
+   Requires at GCC 3.1+ */
+#if (defined(__GNUC__) && !defined(__STRICT_ANSI__) && \
+    ((__GNUC__ == 3) && (__GNU_MINOR__ >= 1)) || (__GNUC__ >= 4))
 /* Two gcc extensions.
    &a[0] degrades to a pointer: a different type from an array */
 #define Py_ARRAY_LENGTH(array) \
