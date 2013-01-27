4511e276d57f2ff1a902e3f5ef8a24103292e7db
Ned Deily <nad@acm.org>
2013-01-26 18:08:22 -0800

    Issue #14018: fix merge error

--- setup.py
+++ setup.py
@@ -47,7 +47,6 @@ def is_macosx_sdk_path(path):
     """
     Returns True if 'path' can be located in an OSX SDK
     """
-    return (path.startswith('/usr/') and not path.startswith('/usr/local')) or path.startswith('/System/')
     return ( (path.startswith('/usr/') and not path.startswith('/usr/local'))
                 or path.startswith('/System/')
                 or path.startswith('/Library/') )
