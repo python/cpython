a5e7f8f8e043ff22889ad39c62130966706e36cb
R David Murray <rdmurray@bitdance.com>
2013-02-09 12:53:29 -0500

    #16564: test to confirm behavior that regressed in python3.
    Also add running of test_email_renamed to the email regrtest.  It contains
    tests that the base email/tests/test_email.py does not, which I discovered
    while trying to backport this test for confirmation of the behavior.

--- Lib/test/test_email.py
+++ Lib/test/test_email.py
@@ -3,10 +3,12 @@
 
 # The specific tests now live in Lib/email/test
 from email.test.test_email import suite
+from email.test.test_email_renamed import suite as suite2
 from test import test_support
 
 def test_main():
     test_support.run_unittest(suite())
+    test_support.run_unittest(suite2())
 
 if __name__ == '__main__':
     test_main()
