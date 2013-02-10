497cee456c29202918739275fcc3d1bd0183ae06
Serhiy Storchaka <storchaka@gmail.com>
2013-02-10 14:43:46 +0200

    Fix a test for SpooledTemporaryFile (added in issue #10355).

--- Lib/test/test_tempfile.py
+++ Lib/test/test_tempfile.py
@@ -854,7 +854,7 @@ class test_SpooledTemporaryFile(TC):
         self.assertTrue(f._rolled)
         self.assertEqual(f.mode, 'w+')
         self.assertIsNotNone(f.name)
-        self.assertEqual(f.newlines, '\n')
+        self.assertEqual(f.newlines, os.linesep)
         self.assertIsNotNone(f.encoding)
 
     def test_text_newline_and_encoding(self):
