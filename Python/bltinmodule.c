7f9a753bc39c6341fc43fe6686dc99221e1c226b
Ezio Melotti <ezio.melotti@gmail.com>
2013-02-15 23:35:14 +0200

    #17178: update any()/all() docstrings to document their behavior with empty iterables.  Patch by Ankur Ankan.

--- Python/bltinmodule.c
+++ Python/bltinmodule.c
@@ -262,7 +262,8 @@ builtin_all(PyObject *self, PyObject *v)
 PyDoc_STRVAR(all_doc,
 "all(iterable) -> bool\n\
 \n\
-Return True if bool(x) is True for all values x in the iterable.");
+Return True if bool(x) is True for all values x in the iterable.\n\
+If the iterable is empty, return True.");
 
 static PyObject *
 builtin_any(PyObject *self, PyObject *v)
@@ -304,7 +305,8 @@ builtin_any(PyObject *self, PyObject *v)
 PyDoc_STRVAR(any_doc,
 "any(iterable) -> bool\n\
 \n\
-Return True if bool(x) is True for any x in the iterable.");
+Return True if bool(x) is True for any x in the iterable.\n\
+If the iterable is empty, return False.");
 
 static PyObject *
 builtin_ascii(PyObject *self, PyObject *v)
