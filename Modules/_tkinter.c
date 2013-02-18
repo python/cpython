26333245f9e2961beebc94bac5f54b1a47d30325
Serhiy Storchaka <storchaka@gmail.com>
2013-02-18 13:00:08 +0200

    Issue #13153: Tkinter functions now raise TclError instead of ValueError when a unicode argument contains non-BMP character.

--- Modules/_tkinter.c
+++ Modules/_tkinter.c
@@ -987,8 +987,10 @@ AsObj(PyObject *value)
         for (i = 0; i < size; i++) {
             if (inbuf[i] >= 0x10000) {
                 /* Tcl doesn't do UTF-16, yet. */
-                PyErr_SetString(PyExc_ValueError,
-                                "unsupported character");
+                PyErr_Format(Tkinter_TclError,
+                             "character U+%x is above the range "
+                             "(U+0000-U+FFFF) allowed by Tcl",
+                             (int)inbuf[i]);
                 ckfree(FREECAST outbuf);
                 return NULL;
             }
