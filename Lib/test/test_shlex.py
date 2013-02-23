79e4f3229c605ad40dbd457c04fccd5a0b9dc97c
Petri Lehtinen <petri@digip.org>
2013-02-23 22:07:39 +0100

    Issue #16121: Fix line number accounting in shlex

--- Lib/test/test_shlex.py
+++ Lib/test/test_shlex.py
@@ -178,6 +178,15 @@ class ShlexTest(unittest.TestCase):
                              "%s: %s != %s" %
                              (self.data[i][0], l, self.data[i][1:]))
 
+    def testLineNumbers(self):
+        data = '"a \n b \n c"\n"x"\n"y"'
+        for is_posix in (True, False):
+            s = shlex.shlex(data, posix=is_posix)
+            for i in (1, 4, 5):
+                s.read_token()
+                self.assertEqual(s.lineno, i)
+
+
 # Allow this test to be used with old shlex.py
 if not getattr(shlex, "split", None):
     for methname in dir(ShlexTest):
