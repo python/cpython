4c02bb92ad464c2f66e518a164160eb65e8c2471
Chris Jerdonek <chris.jerdonek@gmail.com>
2013-02-27 09:02:53 -0800

    Issue #17283: Share code between __main__.py and regrtest.py in Lib/test.
    This commit also removes TESTCWD from regrtest.py's global namespace.

--- Lib/test/__main__.py
+++ Lib/test/__main__.py
@@ -1,13 +1,3 @@
-from test import regrtest, support
+from test import regrtest
 
-
-TEMPDIR, TESTCWD = regrtest._make_temp_dir_for_build(regrtest.TEMPDIR)
-regrtest.TEMPDIR = TEMPDIR
-regrtest.TESTCWD = TESTCWD
-
-# Run the tests in a context manager that temporary changes the CWD to a
-# temporary and writable directory. If it's not possible to create or
-# change the CWD, the original CWD will be used. The original CWD is
-# available from support.SAVEDCWD.
-with support.temp_cwd(TESTCWD, quiet=True):
-    regrtest.main()
+regrtest.main_in_temp_cwd()
