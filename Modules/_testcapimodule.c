03e667d15ac2ec7409e94e151cc04b2d9dd2005d
Ezio Melotti <ezio.melotti@gmail.com>
2013-03-07 21:18:45 +0200

    Fix memory leak introduced in 15190138d3f3.

--- Modules/_testcapimodule.c
+++ Modules/_testcapimodule.c
@@ -1423,11 +1423,15 @@ test_widechar(PyObject *self)
     if (wide == NULL)
         return NULL;
     PyUnicode_AS_UNICODE(wide)[0] = invalid[0];
-    if (_PyUnicode_Ready(wide) < 0)
+    if (_PyUnicode_Ready(wide) < 0) {
+        Py_DECREF(wide);
         PyErr_Clear();
-    else
+    }
+    else {
+        Py_DECREF(wide);
         return raiseTestError("test_widechar",
                               "PyUnicode_Ready() didn't fail");
+    }
 #endif
 
     Py_RETURN_NONE;
