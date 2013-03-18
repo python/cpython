46b73d076b9a19d28f9fad5d3ba55dafa55eed2d
R David Murray <rdmurray@bitdance.com>
2013-03-18 17:42:42 -0400

    #5024: whichhdr now returns the frame count for WAV files.
    Patch by Ned Jackson Lovely based on a suggestion by Robert Pyle.

--- Lib/sndhdr.py
+++ Lib/sndhdr.py
@@ -137,14 +137,17 @@ tests.append(test_voc)
 
 
 def test_wav(h, f):
+    import wave
     # 'RIFF' <len> 'WAVE' 'fmt ' <len>
     if not h.startswith(b'RIFF') or h[8:12] != b'WAVE' or h[12:16] != b'fmt ':
         return None
-    style = get_short_le(h[20:22])
-    nchannels = get_short_le(h[22:24])
-    rate = get_long_le(h[24:28])
-    sample_bits = get_short_le(h[34:36])
-    return 'wav', rate, nchannels, -1, sample_bits
+    f.seek(0)
+    try:
+        w = wave.openfp(f, 'r')
+    except (EOFError, wave.Error):
+        return None
+    return ('wav', w.getframerate(), w.getnchannels(),
+                   w.getnframes(), 8*w.getsampwidth())
 
 tests.append(test_wav)
 
