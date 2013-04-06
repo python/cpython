41e2f6ff7b268913f3064367e7bdcf4a16960419
Georg Brandl <georg@python.org>
2013-04-06 09:40:02 +0200

    Bump to 3.3.1.

--- Include/patchlevel.h
+++ Include/patchlevel.h
@@ -19,11 +19,11 @@
 #define PY_MAJOR_VERSION	3
 #define PY_MINOR_VERSION	3
 #define PY_MICRO_VERSION	1
-#define PY_RELEASE_LEVEL	PY_RELEASE_LEVEL_GAMMA
-#define PY_RELEASE_SERIAL	1
+#define PY_RELEASE_LEVEL	PY_RELEASE_LEVEL_FINAL
+#define PY_RELEASE_SERIAL	0
 
 /* Version as a string */
-#define PY_VERSION      	"3.3.1rc1"
+#define PY_VERSION      	"3.3.1"
 /*--end constants--*/
 
 /* Version as a single 4-byte hex number, e.g. 0x010502B2 == 1.5.2b2.
