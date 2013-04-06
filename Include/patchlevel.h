789d274e89dc71d14c49cfa2d48b23034c794fd6
Georg Brandl <georg@python.org>
2013-04-06 16:43:06 +0200

    Post-release update for 3.3.1

--- Include/patchlevel.h
+++ Include/patchlevel.h
@@ -23,7 +23,7 @@
 #define PY_RELEASE_SERIAL	0
 
 /* Version as a string */
-#define PY_VERSION      	"3.3.1"
+#define PY_VERSION      	"3.3.1+"
 /*--end constants--*/
 
 /* Version as a single 4-byte hex number, e.g. 0x010502B2 == 1.5.2b2.
