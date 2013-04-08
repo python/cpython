371432b961b7f5b410c13b0022e23111115f1d59
Serhiy Storchaka <storchaka@gmail.com>
2013-04-08 22:33:55 +0300

    Close #17666: Fix reading gzip files with an extra field.

--- Lib/test/test_gzip.py
+++ Lib/test/test_gzip.py
@@ -306,6 +306,13 @@ class TestGzip(unittest.TestCase):
             with gzip.GzipFile(fileobj=io.BytesIO(truncated[:i])) as f:
                 self.assertRaises(EOFError, f.read, 1)
 
+    def test_read_with_extra(self):
+        # Gzip data with an extra field
+        gzdata = (b'\x1f\x8b\x08\x04\xb2\x17cQ\x02\xff'
+                  b'\x05\x00Extra'
+                  b'\x0bI-.\x01\x002\xd1Mx\x04\x00\x00\x00')
+        with gzip.GzipFile(fileobj=io.BytesIO(gzdata)) as f:
+            self.assertEqual(f.read(), b'Test')
 
 def test_main(verbose=None):
     test_support.run_unittest(TestGzip)
