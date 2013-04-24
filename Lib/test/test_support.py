68915d75108e924d1ff74e707444c0d3ab405186
Antoine Pitrou <solipsis@pitrou.net>
2013-04-24 23:31:38 +0200

    Issue #17835: Fix test_io when the default OS pipe buffer size is larger than one million bytes.

--- Lib/test/test_support.py
+++ Lib/test/test_support.py
@@ -400,6 +400,15 @@ def fcmp(x, y): # fuzzy comparison function
         return (len(x) > len(y)) - (len(x) < len(y))
     return (x > y) - (x < y)
 
+
+# A constant likely larger than the underlying OS pipe buffer size, to
+# make writes blocking.
+# Windows limit seems to be around 512 B, and many Unix kernels have a
+# 64 KiB pipe buffer size or 16 * PAGE_SIZE: take a few megs to be sure.
+# (see issue #17835 for a discussion of this number).
+PIPE_MAX_SIZE = 4 *1024 * 1024 + 1
+
+
 try:
     unicode
     have_unicode = True
