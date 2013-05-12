1e788e7aadde792e7263d6ab2a806ab88121c7bd
Georg Brandl <georg@python.org>
2013-05-12 12:28:20 +0200

    Bump to version 3.2.5.

--- Include/patchlevel.h
+++ Include/patchlevel.h
@@ -18,12 +18,12 @@
 /*--start constants--*/
 #define PY_MAJOR_VERSION	3
 #define PY_MINOR_VERSION	2
-#define PY_MICRO_VERSION	4
+#define PY_MICRO_VERSION	5
 #define PY_RELEASE_LEVEL	PY_RELEASE_LEVEL_FINAL
 #define PY_RELEASE_SERIAL	0
 
 /* Version as a string */
-#define PY_VERSION      	"3.2.4"
+#define PY_VERSION      	"3.2.5"
 /*--end constants--*/
 
 /* Subversion Revision number of this file (not of the repository). Empty
