bb6b51ca2543967d0cbc5398500eee5a0826b3ea
Vinay Sajip <vinay_sajip@yahoo.co.uk>
2013-05-16 22:47:47 +0100

    Issue #17981: Closed socket on error in SysLogHandler.

--- Lib/logging/handlers.py
+++ Lib/logging/handlers.py
@@ -856,6 +856,7 @@ class SysLogHandler(logging.Handler):
                 try:
                     self.socket.send(msg)
                 except socket.error:
+                    self.socket.close() # See issue 17981
                     self._connect_unixsocket(self.address)
                     self.socket.send(msg)
             elif self.socktype == socket.SOCK_DGRAM:
