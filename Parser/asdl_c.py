ae2e33b4b5ecb73fbac14cf40ee66091993dfdc4
Benjamin Peterson <benjamin@python.org>
2013-05-20 10:28:48 -0700

    change AST codegen to use PyModule_AddIntMacro

--- Parser/asdl_c.py
+++ Parser/asdl_c.py
@@ -1011,7 +1011,7 @@ class ASTModuleVisitor(PickleVisitor):
         self.emit("if (!m) return NULL;", 1)
         self.emit("d = PyModule_GetDict(m);", 1)
         self.emit('if (PyDict_SetItemString(d, "AST", (PyObject*)&AST_type) < 0) return NULL;', 1)
-        self.emit('if (PyModule_AddIntConstant(m, "PyCF_ONLY_AST", PyCF_ONLY_AST) < 0)', 1)
+        self.emit('if (PyModule_AddIntMacro(m, PyCF_ONLY_AST) < 0)', 1)
         self.emit("return NULL;", 2)
         for dfn in mod.dfns:
             self.visit(dfn)
