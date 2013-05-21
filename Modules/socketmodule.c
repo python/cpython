7745d2074b55177d1fb9adee456babc2bd0cee26
Charles-François Natali <cf.natali@gmail.com>
2013-05-21 09:49:18 +0200

    Issue #17683: socket module: return AF_UNIX addresses in Linux abstract namespace as string.

--- Modules/socketmodule.c
+++ Modules/socketmodule.c
@@ -1018,7 +1018,7 @@ makesockaddr(SOCKET_T sockfd, struct sockaddr *addr, size_t addrlen, int proto)
 #ifdef linux
         if (a->sun_path[0] == 0) {  /* Linux abstract namespace */
             addrlen -= offsetof(struct sockaddr_un, sun_path);
-            return PyBytes_FromStringAndSize(a->sun_path, addrlen);
+            return PyUnicode_DecodeFSDefaultAndSize(a->sun_path, addrlen);
         }
         else
 #endif /* linux */
