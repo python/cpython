fc75bede26b22baad71d38da223f3030bdd0b895
Senthil Kumaran <senthil@uthcode.com>
2013-06-02 11:59:47 -0700

    Fix #17967 - Fix related to regression on Windows.
    os.path.join(*self.dirs) produces an invalid path on windows.
    ftp paths are always forward-slash seperated like this. /pub/dir.

--- Lib/urllib/request.py
+++ Lib/urllib/request.py
@@ -2276,7 +2276,8 @@ class ftpwrapper:
         self.ftp = ftplib.FTP()
         self.ftp.connect(self.host, self.port, self.timeout)
         self.ftp.login(self.user, self.passwd)
-        self.ftp.cwd(os.path.join(*self.dirs))
+        _target = '/'.join(self.dirs)
+        self.ftp.cwd(_target)
 
     def retrfile(self, file, type):
         import ftplib
