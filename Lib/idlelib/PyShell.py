73bd2121dd121d51ff00830b99c922560736bc7d
Victor Stinner <victor.stinner@gmail.com>
2013-06-25 00:17:37 +0200

    Issue #18081: Workaround "./python -m test_idle test_logging" failure
    "import idlelib" should not install hooks on the warning modules, hooks should
    only be installed when IDLE is started.

--- Lib/idlelib/PyShell.py
+++ Lib/idlelib/PyShell.py
@@ -61,7 +61,6 @@ else:
                                               lineno, line=line))
         except OSError:
             pass  ## file (probably __stderr__) is invalid, warning dropped.
-    warnings.showwarning = idle_showwarning
     def idle_formatwarning(message, category, filename, lineno, line=None):
         """Format warnings the IDLE way"""
         s = "\nWarning (from warnings module):\n"
@@ -73,7 +72,6 @@ else:
             s += "    %s\n" % line
         s += "%s: %s\n>>> " % (category.__name__, message)
         return s
-    warnings.formatwarning = idle_formatwarning
 
 def extended_linecache_checkcache(filename=None,
                                   orig_checkcache=linecache.checkcache):
@@ -1424,6 +1422,9 @@ echo "import sys; print(sys.argv)" | idle - "foobar"
 
 def main():
     global flist, root, use_subprocess
+
+    warnings.showwarning = idle_showwarning
+    warnings.formatwarning = idle_formatwarning
 
     use_subprocess = True
     enable_shell = False
