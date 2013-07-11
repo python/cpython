526daabf341a2e0e5c2624ebcf82968bd48b5019
Victor Stinner <victor.stinner@gmail.com>
2013-07-11 23:17:33 +0200

    Issue #18408: parsetok() must not write into stderr on memory allocation error
    The caller gets an error code and can raise a classic Python exception.

--- Parser/parsetok.c
+++ Parser/parsetok.c
@@ -138,7 +138,6 @@ parsetok(struct tok_state *tok, grammar *g, int start, perrdetail *err_ret,
     int started = 0;
 
     if ((ps = PyParser_New(g, start)) == NULL) {
-        fprintf(stderr, "no mem for new parser\n");
         err_ret->error = E_NOMEM;
         PyTokenizer_Free(tok);
         return NULL;
