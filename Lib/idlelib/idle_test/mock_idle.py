c8a198ce0297139b6dabeddca15919f8680e35fa
Terry Jan Reedy <tjreedy@udel.edu>
2013-07-13 02:34:35 -0400

    Issue #18279: Add tests for idlelib/RstripExtension.py. Original patch by Phil Webster. With that available, modify RstripExtension.py to stop deleting null slices, which caused a file to be marked as changed when it was not.

---
+++ Lib/idlelib/idle_test/mock_idle.py
@@ -0,0 +1,27 @@
+'''Mock classes that imitate idlelib modules or classes.
+
+Attributes and methods will be added as needed for tests.
+'''
+
+from idlelib.idle_test.mock_tk import Text
+
+class Editor(object):
+    '''Minimally imitate EditorWindow.EditorWindow class.
+    '''
+    def __init__(self, flist=None, filename=None, key=None, root=None):
+        self.text = Text()
+        self.undo = UndoDelegator()
+
+    def get_selection_indices(self):
+        first = self.text.index('1.0')
+        last = self.text.index('end')
+        return first, last
+
+class UndoDelegator(object):
+    '''Minimally imitate UndoDelegator,UndoDelegator class.
+    '''
+    # A real undo block is only needed for user interaction.
+    def undo_block_start(*args):
+        pass
+    def undo_block_stop(*args):
+        pass
