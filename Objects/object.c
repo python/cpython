bf51a36818d698a4e3a5bf124951a608fe553113
Victor Stinner <victor.stinner@gmail.com>
2013-07-18 01:49:30 +0200

    Issue #18408: PyObject_Str(), PyObject_Repr() and type_call() now fail with an assertion error if they are called with an exception set (PyErr_Occurred()).
    As PyEval_EvalFrameEx(), they may clear the current exception and so the caller
    looses its exception.

--- Objects/object.c
+++ Objects/object.c
@@ -377,6 +377,14 @@ PyObject_Repr(PyObject *v)
     if (Py_TYPE(v)->tp_repr == NULL)
         return PyUnicode_FromFormat("<%s object at %p>",
                                     v->ob_type->tp_name, v);
+
+#ifdef Py_DEBUG
+    /* PyObject_Repr() must not be called with an exception set,
+       because it may clear it (directly or indirectly) and so the
+       caller looses its exception */
+    assert(!PyErr_Occurred());
+#endif
+
     res = (*v->ob_type->tp_repr)(v);
     if (res == NULL)
         return NULL;
@@ -408,6 +416,7 @@ PyObject_Str(PyObject *v)
 #endif
     if (v == NULL)
         return PyUnicode_FromString("<NULL>");
+
     if (PyUnicode_CheckExact(v)) {
 #ifndef Py_DEBUG
         if (PyUnicode_READY(v) < 0)
@@ -418,6 +427,13 @@ PyObject_Str(PyObject *v)
     }
     if (Py_TYPE(v)->tp_str == NULL)
         return PyObject_Repr(v);
+
+#ifdef Py_DEBUG
+    /* PyObject_Str() must not be called with an exception set,
+       because it may clear it (directly or indirectly) and so the
+       caller looses its exception */
+    assert(!PyErr_Occurred());
+#endif
 
     /* It is possible for a type to have a tp_str representation that loops
        infinitely. */
