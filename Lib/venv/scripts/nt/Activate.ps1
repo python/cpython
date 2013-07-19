c286b51707fe0e89e6cb25cb672d729a9e2ca63d
Vinay Sajip <vinay_sajip@yahoo.co.uk>
2013-07-19 11:03:55 +0100

    Closes #18479: Changed venv Activate.ps1 to make deactivate a function, and removed Deactivate.ps1.

--- Lib/venv/scripts/nt/Activate.ps1
+++ Lib/venv/scripts/nt/Activate.ps1
@@ -1,25 +1,40 @@
+function global:deactivate ([switch]$NonDestructive) {
+    # Revert to original values
+    if (Test-Path function:_OLD_VIRTUAL_PROMPT) {
+        copy-item function:_OLD_VIRTUAL_PROMPT function:prompt
+        remove-item function:_OLD_VIRTUAL_PROMPT
+    }
+
+    if (Test-Path env:_OLD_VIRTUAL_PYTHONHOME) {
+        copy-item env:_OLD_VIRTUAL_PYTHONHOME env:PYTHONHOME
+        remove-item env:_OLD_VIRTUAL_PYTHONHOME
+    }
+
+    if (Test-Path env:_OLD_VIRTUAL_PATH) {
+        copy-item env:_OLD_VIRTUAL_PATH env:PATH
+        remove-item env:_OLD_VIRTUAL_PATH
+    }
+
+    if (Test-Path env:VIRTUAL_ENV) {
+        remove-item env:VIRTUAL_ENV
+    }
+
+    if (!$NonDestructive) {
+        # Self destruct!
+        remove-item function:deactivate
+    }
+}
+
+deactivate -nondestructive
+
 $env:VIRTUAL_ENV="__VENV_DIR__"
 
-# Revert to original values
-if (Test-Path function:_OLD_VIRTUAL_PROMPT) {
-    copy-item function:_OLD_VIRTUAL_PROMPT function:prompt
-    remove-item function:_OLD_VIRTUAL_PROMPT
-}
-
-if (Test-Path env:_OLD_VIRTUAL_PYTHONHOME) {
-    copy-item env:_OLD_VIRTUAL_PYTHONHOME env:PYTHONHOME
-    remove-item env:_OLD_VIRTUAL_PYTHONHOME
-}
-
-if (Test-Path env:_OLD_VIRTUAL_PATH) {
-    copy-item env:_OLD_VIRTUAL_PATH env:PATH
-    remove-item env:_OLD_VIRTUAL_PATH
-}
-
 # Set the prompt to include the env name
+# Make sure _OLD_VIRTUAL_PROMPT is global
+function global:_OLD_VIRTUAL_PROMPT {""}
 copy-item function:prompt function:_OLD_VIRTUAL_PROMPT
-function prompt {
-    Write-Host -NoNewline -ForegroundColor Green '[__VENV_NAME__]'
+function global:prompt {
+    Write-Host -NoNewline -ForegroundColor Green '__VENV_NAME__'
     _OLD_VIRTUAL_PROMPT
 }
 
