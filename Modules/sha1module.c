a408e7765a4de223e66d50e20a761b836e4cb7d3
Christian Heimes <christian@cheimes.de>
2013-07-30 15:32:57 +0200

    Issue #18599: Fix name attribute of _sha1.sha1() object. It now returns 'SHA1' instead of 'SHA'.

--- Modules/sha1module.c
+++ Modules/sha1module.c
@@ -415,7 +415,7 @@ SHA1_get_block_size(PyObject *self, void *closure)
 static PyObject *
 SHA1_get_name(PyObject *self, void *closure)
 {
-    return PyUnicode_FromStringAndSize("SHA1", 3);
+    return PyUnicode_FromStringAndSize("SHA1", 4);
 }
 
 static PyObject *
