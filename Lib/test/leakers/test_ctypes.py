ea67ed6e12e3e6b999a4785a4c62c7f362658a2f
Ezio Melotti <ezio.melotti@gmail.com>
2013-08-13 12:53:40 +0300

    #18687: remove obsolete comment.  Patch by Févry Thibault.

--- Lib/test/leakers/test_ctypes.py
+++ Lib/test/leakers/test_ctypes.py
@@ -1,6 +1,5 @@
 
 # Taken from Lib/ctypes/test/test_keeprefs.py, PointerToStructure.test().
-# When this leak is fixed, remember to remove from Misc/build.sh LEAKY_TESTS.
 
 from ctypes import Structure, c_int, POINTER
 import gc
