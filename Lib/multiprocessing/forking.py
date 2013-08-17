30b9d5d3af043fc2687ad11a188a34fe355e20ef
Ezio Melotti <ezio.melotti@gmail.com>
2013-08-17 15:50:46 +0300

    #18705: fix a number of typos.  Patch by Févry Thibault.

--- Lib/multiprocessing/forking.py
+++ Lib/multiprocessing/forking.py
@@ -337,7 +337,7 @@ else:
 
     def main():
         '''
-        Run code specifed by data received over pipe
+        Run code specified by data received over pipe
         '''
         assert is_forking(sys.argv)
 
