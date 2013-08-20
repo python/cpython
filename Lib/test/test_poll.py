c360389453d50ff3fdebea052a0f436be41cc8c4
Serhiy Storchaka <storchaka@gmail.com>
2013-08-20 20:38:21 +0300

    Issue #8865: Concurrent invocation of select.poll.poll() now raises a RuntimeError exception.  Patch by Christian Schubert.

--- Lib/test/test_poll.py
+++ Lib/test/test_poll.py
@@ -1,8 +1,16 @@
 # Test case for the os.poll() function
 
-import os, select, random, unittest
+import os
+import random
+import select
 import _testcapi
-from test.test_support import TESTFN, run_unittest
+try:
+    import threading
+except ImportError:
+    threading = None
+import time
+import unittest
+from test.test_support import TESTFN, run_unittest, reap_threads
 
 try:
     select.poll
@@ -159,6 +167,36 @@ class PollTests(unittest.TestCase):
                           _testcapi.USHRT_MAX + 1)
         self.assertRaises(OverflowError, pollster.poll, _testcapi.INT_MAX + 1)
         self.assertRaises(OverflowError, pollster.poll, _testcapi.UINT_MAX + 1)
+
+    @unittest.skipUnless(threading, 'Threading required for this test.')
+    @reap_threads
+    def test_threaded_poll(self):
+        r, w = os.pipe()
+        self.addCleanup(os.close, r)
+        self.addCleanup(os.close, w)
+        rfds = []
+        for i in range(10):
+            fd = os.dup(r)
+            self.addCleanup(os.close, fd)
+            rfds.append(fd)
+        pollster = select.poll()
+        for fd in rfds:
+            pollster.register(fd, select.POLLIN)
+
+        t = threading.Thread(target=pollster.poll)
+        t.start()
+        try:
+            time.sleep(0.5)
+            # trigger ufds array reallocation
+            for fd in rfds:
+                pollster.unregister(fd)
+            pollster.register(w, select.POLLOUT)
+            self.assertRaises(RuntimeError, pollster.poll)
+        finally:
+            # and make the call to poll() from the thread return
+            os.write(w, b'spam')
+            t.join()
+
 
 def test_main():
     run_unittest(PollTests)
