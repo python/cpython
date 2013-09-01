e48464a8ada3fc3201c0e78e2dea835813921947
Raymond Hettinger <python@rcn.com>
2013-08-31 21:34:24 -0700

    Update copyright.

--- Objects/setobject.c
+++ Objects/setobject.c
@@ -3,7 +3,7 @@
    Written and maintained by Raymond D. Hettinger <python@rcn.com>
    Derived from Lib/sets.py and Objects/dictobject.c.
 
-   Copyright (c) 2003-2008 Python Software Foundation.
+   Copyright (c) 2003-2013 Python Software Foundation.
    All rights reserved.
 */
 
