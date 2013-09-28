0879b168bf73d3b7e1e6443bdf162c3ecf4f55b7
Antoine Pitrou <solipsis@pitrou.net>
2013-09-29 01:48:40 +0200

    Issue #4366: Fix building extensions on all platforms when --enable-shared is used.

--- Lib/distutils/command/build_ext.py
+++ Lib/distutils/command/build_ext.py
@@ -231,12 +231,10 @@ class build_ext (Command):
                 # building python standard extensions
                 self.library_dirs.append('.')
 
-        # for extensions under Linux or Solaris with a shared Python library,
+        # For building extensions with a shared Python library,
         # Python's library directory must be appended to library_dirs
-        sysconfig.get_config_var('Py_ENABLE_SHARED')
-        if ((sys.platform.startswith('linux') or sys.platform.startswith('gnu')
-             or sys.platform.startswith('sunos'))
-            and sysconfig.get_config_var('Py_ENABLE_SHARED')):
+        # See Issues: #1600860, #4366
+        if (sysconfig.get_config_var('Py_ENABLE_SHARED')):
             if sys.executable.startswith(os.path.join(sys.exec_prefix, "bin")):
                 # building third party extensions
                 self.library_dirs.append(sysconfig.get_config_var('LIBDIR'))
