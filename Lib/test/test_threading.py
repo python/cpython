038ad9306f2e5e139dfc20c0148e884c4edcdf22
Tim Peters <tim@python.org>
2013-10-08 21:29:27 -0500

    Whitespace normalization.

--- Lib/test/test_threading.py
+++ Lib/test/test_threading.py
@@ -600,22 +600,22 @@ class ThreadTests(BaseTestCase):
         self.assertIn(LOOKING_FOR, repr(t)) # we waited at least 5 seconds
 
     def test_BoundedSemaphore_limit(self):
-       # BoundedSemaphore should raise ValueError if released too often.
-       for limit in range(1, 10):
-           bs = threading.BoundedSemaphore(limit)
-           threads = [threading.Thread(target=bs.acquire)
-                      for _ in range(limit)]
-           for t in threads:
-               t.start()
-           for t in threads:
-               t.join()
-           threads = [threading.Thread(target=bs.release)
-                      for _ in range(limit)]
-           for t in threads:
-               t.start()
-           for t in threads:
-               t.join()
-           self.assertRaises(ValueError, bs.release)
+        # BoundedSemaphore should raise ValueError if released too often.
+        for limit in range(1, 10):
+            bs = threading.BoundedSemaphore(limit)
+            threads = [threading.Thread(target=bs.acquire)
+                       for _ in range(limit)]
+            for t in threads:
+                t.start()
+            for t in threads:
+                t.join()
+            threads = [threading.Thread(target=bs.release)
+                       for _ in range(limit)]
+            for t in threads:
+                t.start()
+            for t in threads:
+                t.join()
+            self.assertRaises(ValueError, bs.release)
 
 class ThreadJoinOnShutdown(BaseTestCase):
 
