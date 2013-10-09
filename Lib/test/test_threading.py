7d11bdbebe4646ffd21d4a4669479dca33326987
Tim Peters <tim@python.org>
2013-10-08 20:55:51 -0500

    Issue 19158:  a rare race in BoundedSemaphore could allow .release() too often.

--- Lib/test/test_threading.py
+++ Lib/test/test_threading.py
@@ -468,6 +468,24 @@ class ThreadTests(BaseTestCase):
                 self.assertEqual(0, status)
 
 
+    def test_BoundedSemaphore_limit(self):
+        # BoundedSemaphore should raise ValueError if released too often.
+        for limit in range(1, 10):
+            bs = threading.BoundedSemaphore(limit)
+            threads = [threading.Thread(target=bs.acquire)
+                       for _ in range(limit)]
+            for t in threads:
+                t.start()
+            for t in threads:
+                t.join()
+            threads = [threading.Thread(target=bs.release)
+                       for _ in range(limit)]
+            for t in threads:
+                t.start()
+            for t in threads:
+                t.join()
+            self.assertRaises(ValueError, bs.release)
+
 class ThreadJoinOnShutdown(BaseTestCase):
 
     # Between fork() and exec(), only async-safe functions are allowed (issues
