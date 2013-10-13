5605fb0b0196747e06984f33ee6d8005da3e8693
Christian Heimes <christian@cheimes.de>
2013-10-13 02:21:33 +0200

    Issue #19218: set __name__ of _collections_abc to collections.abc in order to fix tests and keep beautiful qualified names.

--- Lib/_collections_abc.py
+++ Lib/_collections_abc.py
@@ -18,6 +18,12 @@ __all__ = ["Hashable", "Iterable", "Iterator",
            "ByteString",
            ]
 
+# This module has been renamed from collections.abc to _collections_abc to
+# speed up interpreter startup. Some of the types such as MutableMapping are
+# required early but collections module imports a lot of other modules.
+# See issue #19218
+__name__ = "collections.abc"
+
 # Private list of types that we want to register with the various ABCs
 # so that they will pass tests like:
 #       it = iter(somebytearray)
