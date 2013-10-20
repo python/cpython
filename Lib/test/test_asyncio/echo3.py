d49c47bfb0ea3bc2e7d8778d8002c0f628d97488
Guido van Rossum <guido@dropbox.com>
2013-10-19 21:26:34 -0700

    Break out of loop on EOF in asyncio echo test programs.

--- Lib/test/test_asyncio/echo3.py
+++ Lib/test/test_asyncio/echo3.py
@@ -3,6 +3,8 @@ import os
 if __name__ == '__main__':
     while True:
         buf = os.read(0, 1024)
+        if not buf:
+            break
         try:
             os.write(1, b'OUT:'+buf)
         except OSError as ex:
