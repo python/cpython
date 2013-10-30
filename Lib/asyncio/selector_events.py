d052f6e4739dc8aa0e9383e4d0c9d262e9dd9798
Guido van Rossum <guido@dropbox.com>
2013-10-30 14:36:58 -0700

    asyncio: When not closing the connection after receiving EOF, still remove the read handler.

--- Lib/asyncio/selector_events.py
+++ Lib/asyncio/selector_events.py
@@ -468,7 +468,12 @@ class _SelectorSocketTransport(_SelectorTransport):
                 self._protocol.data_received(data)
             else:
                 keep_open = self._protocol.eof_received()
-                if not keep_open:
+                if keep_open:
+                    # We're keeping the connection open so the
+                    # protocol can write more, but we still can't
+                    # receive more, so remove the reader callback.
+                    self._loop.remove_reader(self._sock_fd)
+                else:
                     self.close()
 
     def write(self, data):
