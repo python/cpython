b72e67ff90a6fd4fc012ae9cdc7b6bb6da3dfc28
Guido van Rossum <guido@dropbox.com>
2013-11-01 14:12:50 -0700

    asyncio: Pause accepting whenever accept() returns certain errors. Fixes asyncio issue #78.

--- Lib/asyncio/constants.py
+++ Lib/asyncio/constants.py
@@ -1,4 +1,7 @@
 """Constants."""
 
-
+# After the connection is lost, log warnings after this many write()s.
 LOG_THRESHOLD_FOR_CONNLOST_WRITES = 5
+
+# Seconds to wait before retrying accept().
+ACCEPT_RETRY_DELAY = 1
