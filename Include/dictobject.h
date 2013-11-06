c34bf26d998792043c5935672a3adb0020a33c0c
Victor Stinner <victor.stinner@gmail.com>
2013-11-06 18:58:22 +0100

    Issue #19512: Add a new _PyDict_DelItemId() function, similar to PyDict_DelItemString() but using an identifier for the key

--- Include/dictobject.h
+++ Include/dictobject.h
@@ -109,6 +109,7 @@ PyAPI_FUNC(PyObject *) _PyDict_GetItemId(PyObject *dp, struct _Py_Identifier *ke
 PyAPI_FUNC(int) PyDict_SetItemString(PyObject *dp, const char *key, PyObject *item);
 PyAPI_FUNC(int) _PyDict_SetItemId(PyObject *dp, struct _Py_Identifier *key, PyObject *item);
 PyAPI_FUNC(int) PyDict_DelItemString(PyObject *dp, const char *key);
+PyAPI_FUNC(int) _PyDict_DelItemId(PyObject *mp, struct _Py_Identifier *key);
 
 #ifndef Py_LIMITED_API
 int _PyObjectDict_SetItem(PyTypeObject *tp, PyObject **dictptr, PyObject *name, PyObject *value);
