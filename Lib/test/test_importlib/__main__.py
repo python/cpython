2b8051752c092f677cb07a36a1acd0a1bf721cbe
Brett Cannon <brett@python.org>
2013-11-08 13:35:34 -0500

    Simplify test.test_importlib.__main__

--- Lib/test/test_importlib/__main__.py
+++ Lib/test/test_importlib/__main__.py
@@ -4,15 +4,6 @@ Specifying the ``--builtin`` flag will run tests, where applicable, with
 builtins.__import__ instead of importlib.__import__.
 
 """
-from . import test_main
-
-
 if __name__ == '__main__':
-    import argparse
-
-    parser = argparse.ArgumentParser(description='Execute the importlib test '
-                                                  'suite')
-    parser.add_argument('-b', '--builtin', action='store_true', default=False,
-                        help='use builtins.__import__() instead of importlib')
-    args = parser.parse_args()
+    from . import test_main
     test_main()
