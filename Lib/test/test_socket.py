49ad0a5301c8e9aeef71223fb9a3249a7ff80bcc
Jason R. Coombs <jaraco@jaraco.com>
2013-11-10 14:02:04 -0500

    Normalize whitespace

--- Lib/test/test_socket.py
+++ Lib/test/test_socket.py
@@ -980,14 +980,14 @@ class GeneralModuleTests(unittest.TestCase):
                 return
         except ImportError:
             return
-        
+
         if sys.platform == "win32":
             try:
                 inet_pton(AF_INET6, '::')
             except OSError as e:
                 if e.winerror == 10022:
                     return # IPv6 might not be installed on this PC
-        
+
         f = lambda a: inet_pton(AF_INET6, a)
         assertInvalid = lambda a: self.assertRaises(
             (OSError, ValueError), f, a
