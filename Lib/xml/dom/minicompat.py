09390ed6a0052ed30586fc6244428b083d5ba6d2
Andrew Kuchling <amk@amk.ca>
2013-11-12 10:25:15 -0500

    Closes #12828: add docstring text noting this is an internal-only module

--- Lib/xml/dom/minicompat.py
+++ Lib/xml/dom/minicompat.py
@@ -1,4 +1,8 @@
-"""Python version compatibility support for minidom."""
+"""Python version compatibility support for minidom.
+
+This module contains internal implementation details and
+should not be imported; use xml.dom.minidom instead.
+"""
 
 # This module should only be imported using "import *".
 #
