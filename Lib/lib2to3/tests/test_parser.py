dcb1ed18a5f3876d6f60b5b4e3fd36d63fe8b7db
Serhiy Storchaka <storchaka@gmail.com>
2013-11-14 23:49:58 +0200

    Issue #19592: Use specific asserts in lib2to3 tests.

--- Lib/lib2to3/tests/test_parser.py
+++ Lib/lib2to3/tests/test_parser.py
@@ -168,8 +168,8 @@ class TestParserIdempotency(support.TestCase):
         for filepath in support.all_project_files():
             with open(filepath, "rb") as fp:
                 encoding = tokenize.detect_encoding(fp.readline)[0]
-            self.assertTrue(encoding is not None,
-                            "can't detect encoding for %s" % filepath)
+            self.assertIsNotNone(encoding,
+                                 "can't detect encoding for %s" % filepath)
             with open(filepath, "r", encoding=encoding) as fp:
                 source = fp.read()
             try:
