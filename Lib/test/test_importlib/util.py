ad0f05395e1ecd2581d0db774eb1a8d9db3779a1
Brett Cannon <brett@python.org>
2013-11-22 14:47:09 -0500

    Make test_importlib output easier to trace back to the failing test class.

--- Lib/test/test_importlib/util.py
+++ Lib/test/test_importlib/util.py
@@ -20,6 +20,7 @@ def test_both(test_class, **kwargs):
                                    (test_class, unittest.TestCase))
     source_tests = types.new_class('Source_'+test_class.__name__,
                                    (test_class, unittest.TestCase))
+    frozen_tests.__module__ = source_tests.__module__ = test_class.__module__
     for attr, (frozen_value, source_value) in kwargs.items():
         setattr(frozen_tests, attr, frozen_value)
         setattr(source_tests, attr, source_value)
