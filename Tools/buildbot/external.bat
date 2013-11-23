9830cfb7014582f91df9476df3e866843341a06d
Martin v. Löwis <martin@v.loewis.de>
2013-11-23 23:05:27 +0100

    Update Tcl/Tk to 8.6.1.

--- Tools/buildbot/external.bat
+++ Tools/buildbot/external.bat
@@ -4,18 +4,18 @@
 call "Tools\buildbot\external-common.bat"
 call "%VS100COMNTOOLS%\vsvars32.bat"
 
-if not exist tcltk\bin\tcl85g.dll (
+if not exist tcltk\bin\tcl86tg.dll (
     @rem all and install need to be separate invocations, otherwise nmakehlp is not found on install
-    cd tcl-8.5.11.0\win
+    cd tcl-8.6.1.0\win
     nmake -f makefile.vc DEBUG=1 INSTALLDIR=..\..\tcltk clean all 
     nmake -f makefile.vc DEBUG=1 INSTALLDIR=..\..\tcltk install
     cd ..\..
 )
 
-if not exist tcltk\bin\tk85g.dll (
-    cd tk-8.5.11.0\win
-    nmake -f makefile.vc OPTS=noxp DEBUG=1 INSTALLDIR=..\..\tcltk TCLDIR=..\..\tcl-8.5.11.0 clean
-    nmake -f makefile.vc OPTS=noxp DEBUG=1 INSTALLDIR=..\..\tcltk TCLDIR=..\..\tcl-8.5.11.0 all
-    nmake -f makefile.vc OPTS=noxp DEBUG=1 INSTALLDIR=..\..\tcltk TCLDIR=..\..\tcl-8.5.11.0 install
+if not exist tcltk\bin\tk86tg.dll (
+    cd tk-8.6.1.0\win
+    nmake -f makefile.vc OPTS=noxp DEBUG=1 INSTALLDIR=..\..\tcltk TCLDIR=..\..\tcl-8.6.1.0 clean
+    nmake -f makefile.vc OPTS=noxp DEBUG=1 INSTALLDIR=..\..\tcltk TCLDIR=..\..\tcl-8.6.1.0 all
+    nmake -f makefile.vc OPTS=noxp DEBUG=1 INSTALLDIR=..\..\tcltk TCLDIR=..\..\tcl-8.6.1.0 install
     cd ..\..
 )
