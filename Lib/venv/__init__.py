f9e2f09f80ffc12e1670fa1c59aa58039f976b50
Nick Coghlan <ncoghlan@gmail.com>
2013-11-24 11:53:03 +1000

    Issue #19734: venv still needs isolated mode

--- Lib/venv/__init__.py
+++ Lib/venv/__init__.py
@@ -237,7 +237,7 @@ class EnvBuilder:
         # We run ensurepip in isolated mode to avoid side effects from
         # environment vars, the current directory and anything else
         # intended for the global Python environment
-        cmd = [context.env_exe, '-m', 'ensurepip', '--upgrade',
+        cmd = [context.env_exe, '-Im', 'ensurepip', '--upgrade',
                                                     '--default-pip']
         subprocess.check_output(cmd, stderr=subprocess.STDOUT)
 
