c02c6df2c7a7b9fe8bc5de25ed22fa12a7c34c9a
Mark Dickinson <dickinsm@gmail.com>
2013-11-26 17:02:46 +0000

    Issue #19638: Skip large digit string tests on 32-bit platforms.

--- Lib/test/test_strtod.py
+++ Lib/test/test_strtod.py
@@ -251,6 +251,9 @@ class StrtodTests(unittest.TestCase):
 
     @test_support.bigmemtest(minsize=5 * test_support._1G, memuse=1)
     def test_oversized_digit_strings(self, maxsize):
+        if sys.maxsize <= 0x7fffffff:
+            self.skipTest("Only applies on 64-bit systems.")
+
         # Input string whose length doesn't fit in an INT.
         s = "1." + "1" * int(2.2e9)
         with self.assertRaises(ValueError):
