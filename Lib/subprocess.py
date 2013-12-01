91142a654e76d171cabb27f7dbe14747c6e1e591
Gregory P. Smith <greg@krypto.org>
2013-12-01 00:12:24 -0800

    Undo supposed fix for Issue #15798 until I understand why this is causing test_multiprocessing_forkserver and test_multiprocessing_spawn failures on head (3.4).

--- Lib/subprocess.py
+++ Lib/subprocess.py
@@ -1368,10 +1368,7 @@ class Popen(object):
                         executable_list = tuple(
                             os.path.join(os.fsencode(dir), executable)
                             for dir in os.get_exec_path(env))
-                    # Never close stdin, stdout and stderr for the child.
-                    fds_to_keep = {0,1,2}
-                    fds_to_keep.update(pass_fds)
-                    # Our child uses this one to signal error before exec().
+                    fds_to_keep = set(pass_fds)
                     fds_to_keep.add(errpipe_write)
                     self.pid = _posixsubprocess.fork_exec(
                             args, executable_list,
