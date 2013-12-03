510d61ce575e6fedc735a5b1c8fd2aedfc9dff0b
Antoine Pitrou <solipsis@pitrou.net>
2013-12-03 09:41:35 +0100

    Issue #19872: remove unused imports in pathlib.  Patch by Vajrasky Kok.

--- Lib/pathlib.py
+++ Lib/pathlib.py
@@ -6,19 +6,12 @@ import os
 import posixpath
 import re
 import sys
-import weakref
-try:
-    import threading
-except ImportError:
-    import dummy_threading as threading
-
-from collections import Sequence, defaultdict
+from collections import Sequence
 from contextlib import contextmanager
 from errno import EINVAL, ENOENT
-from itertools import chain, count
 from operator import attrgetter
 from stat import S_ISDIR, S_ISLNK, S_ISREG, S_ISSOCK, S_ISBLK, S_ISCHR, S_ISFIFO
-from urllib.parse import quote as urlquote, quote_from_bytes as urlquote_from_bytes
+from urllib.parse import quote_from_bytes as urlquote_from_bytes
 
 
 supports_symlinks = True
