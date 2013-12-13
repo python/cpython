cd0cb8ccd376186f08fd3ee2be0085f45f66b3d3
Victor Stinner <victor.stinner@gmail.com>
2013-12-13 03:22:00 +0100

    Close #19787: PyThread_set_key_value() now always set the value. In Python 3.3, the function did nothing if the key already exists (if the current value is a non-NULL pointer).
    _testcapi.run_in_subinterp() now correctly sets the new Python thread state of
    the current thread when a subinterpreter is created.

--- Python/thread_nt.h
+++ Python/thread_nt.h
@@ -389,20 +389,11 @@ PyThread_delete_key(int key)
     TlsFree(key);
 }
 
-/* We must be careful to emulate the strange semantics implemented in thread.c,
- * where the value is only set if it hasn't been set before.
- */
 int
 PyThread_set_key_value(int key, void *value)
 {
     BOOL ok;
-    void *oldvalue;
 
-    assert(value != NULL);
-    oldvalue = TlsGetValue(key);
-    if (oldvalue != NULL)
-        /* ignore value if already set */
-        return 0;
     ok = TlsSetValue(key, value);
     if (!ok)
         return -1;
