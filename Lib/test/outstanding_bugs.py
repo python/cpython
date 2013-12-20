bdce938af2af03b2eb0d0e4731f1b8c4b965eaf5
Zachary Ware <zachary.ware@gmail.com>
2013-12-20 13:25:07 -0600

    Update test.outstanding_bugs.py

--- Lib/test/outstanding_bugs.py
+++ Lib/test/outstanding_bugs.py
@@ -10,79 +10,9 @@ import unittest
 from test import support
 
 #
-# One test case for outstanding bugs at the moment:
+# No test cases for outstanding bugs at the moment.
 #
 
-# test_io
-import io
-class TextIOWrapperTest(unittest.TestCase):
-
-    def setUp(self):
-        self.testdata = b"AAA\r\nBBB\rCCC\r\nDDD\nEEE\r\n"
-        self.normalized = b"AAA\nBBB\nCCC\nDDD\nEEE\n".decode("ASCII")
-
-    def tearDown(self):
-        support.unlink(support.TESTFN)
-
-
-    def test_issue1395_1(self):
-        txt = io.TextIOWrapper(io.BytesIO(self.testdata), encoding="ASCII")
-
-        # read one char at a time
-        reads = ""
-        while True:
-            c = txt.read(1)
-            if not c:
-                break
-            reads += c
-        self.assertEqual(reads, self.normalized)
-
-    def test_issue1395_2(self):
-        txt = io.TextIOWrapper(io.BytesIO(self.testdata), encoding="ASCII")
-        txt._CHUNK_SIZE = 4
-
-        reads = ""
-        while True:
-            c = txt.read(4)
-            if not c:
-                break
-            reads += c
-        self.assertEqual(reads, self.normalized)
-
-    def test_issue1395_3(self):
-        txt = io.TextIOWrapper(io.BytesIO(self.testdata), encoding="ASCII")
-        txt._CHUNK_SIZE = 4
-
-        reads = txt.read(4)
-        reads += txt.read(4)
-        reads += txt.readline()
-        reads += txt.readline()
-        reads += txt.readline()
-        self.assertEqual(reads, self.normalized)
-
-    def test_issue1395_4(self):
-        txt = io.TextIOWrapper(io.BytesIO(self.testdata), encoding="ASCII")
-        txt._CHUNK_SIZE = 4
-
-        reads = txt.read(4)
-        reads += txt.read()
-        self.assertEqual(reads, self.normalized)
-
-    def test_issue1395_5(self):
-        txt = io.TextIOWrapper(io.BytesIO(self.testdata), encoding="ASCII")
-        txt._CHUNK_SIZE = 4
-
-        reads = txt.read(4)
-        pos = txt.tell()
-        txt.seek(0)
-        txt.seek(pos)
-        self.assertEqual(txt.read(4), "BBB\n")
-
-
-
-def test_main():
-    support.run_unittest(
-        TextIOWrapperTest)
 
 if __name__ == "__main__":
-    test_main()
+    unittest.main()
