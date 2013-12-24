b883883c2f9698fe90414aca76f3570c9ea58817
R David Murray <rdmurray@bitdance.com>
2013-12-24 16:13:32 -0500

    #16832: s/integer/object/ in docs/docstring, and add whatsnew entry.

--- Lib/abc.py
+++ Lib/abc.py
@@ -241,8 +241,8 @@ class ABC(metaclass=ABCMeta):
 def get_cache_token():
     """Returns the current ABC cache token.
 
-    The token is an opaque integer identifying the current version of
-    the ABC cache for virtual subclasses. This number changes with
-    every call to ``register()`` on any ABC.
+    The token is an opaque object (supporting equality testing) identifying the
+    current version of the ABC cache for virtual subclasses. The token changes
+    with every call to ``register()`` on any ABC.
     """
     return ABCMeta._abc_invalidation_counter
