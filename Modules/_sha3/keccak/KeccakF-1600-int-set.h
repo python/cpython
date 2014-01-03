bc2295afe88cec2b3f2c81a2e967b465e2b4c631
Martin v. Löwis <martin@v.loewis.de>
2014-01-03 14:05:06 +0100

    * Issue #16113: Remove sha3 module again. Patch by Christian Heimes, with modifications.

--- Modules/_sha3/keccak/KeccakF-1600-int-set.h
+++
@@ -1,6 +0,0 @@
-#define ProvideFast576
-#define ProvideFast832
-#define ProvideFast1024
-#define ProvideFast1088
-#define ProvideFast1152
-#define ProvideFast1344
--- Modules/_sha3/keccak/KeccakF-1600-opt64-settings.h
+++
@@ -1,9 +0,0 @@
-/*
-#define Unrolling 24
-#define UseBebigokimisa
-#define UseSSE
-#define UseOnlySIMD64
-#define UseMMX
-#define UseSHLD
-#define UseXOP
-*/
