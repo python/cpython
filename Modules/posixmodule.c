54684b7d11efabbdd2f3d11d30850d8c3b30028c
Benjamin Peterson <benjamin@python.org>
2014-01-10 09:22:40 -0600

    fix build when SCHED_SPORADIC is defined (closes #20217)

--- Modules/posixmodule.c
+++ Modules/posixmodule.c
@@ -11863,7 +11863,7 @@ all_ins(PyObject *d)
     if (ins(d, "SCHED_FIFO", (long)SCHED_FIFO)) return -1;
     if (ins(d, "SCHED_RR", (long)SCHED_RR)) return -1;
 #ifdef SCHED_SPORADIC
-    if (ins(d, "SCHED_SPORADIC", (long)SCHED_SPORADIC) return -1;
+    if (ins(d, "SCHED_SPORADIC", (long)SCHED_SPORADIC)) return -1;
 #endif
 #ifdef SCHED_BATCH
     if (ins(d, "SCHED_BATCH", (long)SCHED_BATCH)) return -1;
