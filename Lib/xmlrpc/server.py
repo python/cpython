a63952a86efbf486fd9ea300bfb1369f6f2792b9
Senthil Kumaran <senthil@uthcode.com>
2014-01-12 16:06:58 -0800

    Issue #19082: Working xmlrpc.server and xmlrpc.client examples. Both in modules and in documentation.

--- Lib/xmlrpc/server.py
+++ Lib/xmlrpc/server.py
@@ -967,10 +967,24 @@ class DocCGIXMLRPCRequestHandler(   CGIXMLRPCRequestHandler,
 
 
 if __name__ == '__main__':
+    import datetime
+
+    class ExampleService:
+        def getData(self):
+            return '42'
+
+        class currentTime:
+            @staticmethod
+            def getCurrentTime():
+                return datetime.datetime.now()
+
     server = SimpleXMLRPCServer(("localhost", 8000))
     server.register_function(pow)
     server.register_function(lambda x,y: x+y, 'add')
+    server.register_instance(ExampleService(), allow_dotted_names=True)
+    server.register_multicall_functions()
     print('Serving XML-RPC on localhost port 8000')
+    print('It is advisable to run this example server within a secure, closed network.')
     try:
         server.serve_forever()
     except KeyboardInterrupt:
