e5ce6fc418f06dfa6c01d503342928aae2c47a50
Georg Brandl <georg@python.org>
2014-01-25 09:02:18 +0100

    #16042: CVE-2013-1752: smtplib fix for unlimited readline() from socket

--- Lib/test/mock_socket.py
+++ Lib/test/mock_socket.py
@@ -21,8 +21,13 @@ class MockFile:
     """
     def __init__(self, lines):
         self.lines = lines
-    def readline(self):
-        return self.lines.pop(0) + b'\r\n'
+    def readline(self, limit=-1):
+        result = self.lines.pop(0) + b'\r\n'
+        if limit >= 0:
+            # Re-insert the line, removing the \r\n we added.
+            self.lines.insert(0, result[limit:-2])
+            result = result[:limit]
+        return result
     def close(self):
         pass
 
