48ec57ba96e828652025d0be46a899ac857423b2
Victor Stinner <victor.stinner@gmail.com>
2014-02-04 09:49:14 +0100

    Issue #17162: Fix compilation, replace non-breaking space with an ASCII space

--- Objects/typeobject.c
+++ Objects/typeobject.c
@@ -2644,7 +2644,7 @@ PyType_FromSpec(PyType_Spec *spec)
 void *
 PyType_GetSlot(PyTypeObject *type, int slot)
 {
-    if (!PyType_HasFeature(type, Py_TPFLAGS_HEAPTYPE)) {
+    if (!PyType_HasFeature(type, Py_TPFLAGS_HEAPTYPE)) {
         PyErr_BadInternalCall();
         return NULL;
     }
