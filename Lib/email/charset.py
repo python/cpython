7b7bbfd14798ac0ee5bffd21dd3c939adc9a0e3b
R David Murray <rdmurray@bitdance.com>
2014-02-07 12:46:17 -0500

    #20531: Apply the 3.3 version of the #19063 fix.
    So passing unicode to set_payload works again (but still doesn't
    do what you want when the message is serialized).

--- Lib/email/charset.py
+++ Lib/email/charset.py
@@ -386,7 +386,8 @@ class Charset:
         string using the ascii codec produces the correct string version
         of the content.
         """
-        # 7bit/8bit encodings return the string unchanged (module conversions)
+        if not string:
+            return string
         if self.body_encoding is BASE64:
             if isinstance(string, str):
                 string = string.encode(self.output_charset)
@@ -398,13 +399,9 @@ class Charset:
             # character set, then, we must turn it into pseudo bytes via the
             # latin1 charset, which will encode any byte as a single code point
             # between 0 and 255, which is what body_encode is expecting.
-            #
-            # Note that this clause doesn't handle the case of a _payload that
-            # is already bytes.  It never did, and the semantics of _payload
-            # being bytes has never been nailed down, so fixing that is a
-            # longer term TODO.
             if isinstance(string, str):
-                string = string.encode(self.output_charset).decode('latin1')
+                string = string.encode(self.output_charset)
+            string = string.decode('latin1')
             return email.quoprimime.body_encode(string)
         else:
             if isinstance(string, str):
