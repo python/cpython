49b02e29a263cf3aa1c479424268c3fbc784f185
Donald Stufft <donald@stufft.io>
2014-02-20 20:53:27 -0500

    Upgrade pip from 1.5.2 to 1.5.3

--- Lib/ensurepip/__init__.py
+++ Lib/ensurepip/__init__.py
@@ -10,7 +10,7 @@ __all__ = ["version", "bootstrap"]
 
 _SETUPTOOLS_VERSION = "2.1"
 
-_PIP_VERSION = "1.5.2"
+_PIP_VERSION = "1.5.3"
 
 # pip currently requires ssl support, so we try to provide a nicer
 # error message when that is missing (http://bugs.python.org/issue19744)
