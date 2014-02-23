c3111fcbc7c431fe30dbcd6f4b31f4ad849c0eaf
Terry Jan Reedy <tjreedy@udel.edu>
2014-02-23 00:37:16 -0500

    Issue #20730: Fix typo reported by Claudiu Popa.

--- Lib/idlelib/GrepDialog.py
+++ Lib/idlelib/GrepDialog.py
@@ -98,7 +98,7 @@ class GrepDialog(SearchDialogBase):
     def findfiles(self, dir, base, rec):
         try:
             names = os.listdir(dir or os.curdir)
-        except OSerror as msg:
+        except OSError as msg:
             print(msg)
             return []
         list = []
