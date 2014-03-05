d9e012bf927c662a8e8469ef03544b7d48291dd3
Kristján Valur Jónsson <sweskman@gmail.com>
2014-03-05 13:47:57 +0000

    Make the various iterators' "setstate" sliently and consistently clip the index.  This avoids the possibility of setting an iterator to an invalid state.

--- Modules/arraymodule.c
+++ Modules/arraymodule.c
@@ -2798,6 +2798,8 @@ arrayiter_setstate(arrayiterobject *it, PyObject *state)
         return NULL;
     if (index < 0)
         index = 0;
+    else if (index > Py_SIZE(it->ao))
+        index = Py_SIZE(it->ao); /* iterator exhausted */
     it->index = index;
     Py_RETURN_NONE;
 }
--- Objects/listobject.c
+++ Objects/listobject.c
@@ -2803,6 +2803,8 @@ listiter_setstate(listiterobject *it, PyObject *state)
     if (it->it_seq != NULL) {
         if (index < 0)
             index = 0;
+        else if (index > PyList_GET_SIZE(it->it_seq))
+            index = PyList_GET_SIZE(it->it_seq); /* iterator exhausted */
         it->it_index = index;
     }
     Py_RETURN_NONE;
