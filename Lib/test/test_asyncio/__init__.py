5c54c69313d72fde1f343b3c971593d2ddd74392
Yury Selivanov <yselivanov@sprymix.com>
2014-03-27 12:21:20 -0400

    asyncio.tests: Autodiscover asyncio tests. Patch by Vajrasky Kok. Closes #20668

--- Lib/test/test_asyncio/__init__.py
+++ Lib/test/test_asyncio/__init__.py
@@ -10,20 +10,18 @@ import_module('concurrent.futures')
 
 
 def suite():
-    tests_file = os.path.join(os.path.dirname(__file__), 'tests.txt')
-    with open(tests_file) as fp:
-        test_names = fp.read().splitlines()
     tests = unittest.TestSuite()
     loader = unittest.TestLoader()
-    for test_name in test_names:
-        mod_name = 'test.' + test_name
-        try:
-            __import__(mod_name)
-        except unittest.SkipTest:
-            pass
-        else:
-            mod = sys.modules[mod_name]
-            tests.addTests(loader.loadTestsFromModule(mod))
+    for fn in os.listdir(os.path.dirname(__file__)):
+        if fn.startswith("test") and fn.endswith(".py"):
+            mod_name = 'test.test_asyncio.' + fn[:-3]
+            try:
+                __import__(mod_name)
+            except unittest.SkipTest:
+                pass
+            else:
+                mod = sys.modules[mod_name]
+                tests.addTests(loader.loadTestsFromModule(mod))
     return tests
 
 
