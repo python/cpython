5ce3f7bff73424a64e90873f6a568013d5911c09
Zachary Ware <zachary.ware@gmail.com>
2014-04-07 23:33:29 -0500

    Issue #21059: Temporary measure to make the Windows buildbots useful again.
    Hopefully.

--- Tools/buildbot/test.bat
+++ Tools/buildbot/test.bat
@@ -1,3 +1,6 @@
 @rem Used by the buildbot "test" step.
+
+rem The following line should be removed before #20035 is closed
+set TCL_LIBRARY=%CD%\..\tcltk\lib\tcl8.6
 cd PCbuild
 call rt.bat -d -q -uall -rwW -n --timeout=3600 %1 %2 %3 %4 %5 %6 %7 %8 %9
