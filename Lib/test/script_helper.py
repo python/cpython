ffef2447eea4e40278af93e2444c97cd3032178e
Antoine Pitrou <solipsis@pitrou.net>
2014-05-11 19:13:43 +0200

    Try to fix issue #21425 workaround for shared library builds

--- Lib/test/script_helper.py
+++ Lib/test/script_helper.py
@@ -92,8 +92,8 @@ def spawn_python(*args, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, **kw):
     # - http://reinout.vanrees.org/weblog/2009/08/14/readline-invisible-character-hack.html
     # - http://stackoverflow.com/questions/15760712/python-readline-module-prints-escape-character-during-import
     # - http://lists.gnu.org/archive/html/bug-readline/2007-08/msg00004.html
-    env = kw.setdefault('env', {})
-    env.setdefault('TERM', 'vt100')
+    env = kw.setdefault('env', dict(os.environ))
+    env['TERM'] = 'vt100'
     return subprocess.Popen(cmd_line, stdin=subprocess.PIPE,
                             stdout=stdout, stderr=stderr,
                             **kw)
