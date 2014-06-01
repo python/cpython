d1bd035fe8714d052cc97b77a9f25dc2dda37c30
Gregory P. Smith <greg@krypto.org>
2014-06-01 15:27:28 -0700

    Skip test_subprocess test_close_fds_when_max_fd_is_lowered on FreeBSD when fdescfs is not mounted on /dev/fd.

--- Lib/test/test_subprocess.py
+++ Lib/test/test_subprocess.py
@@ -1927,6 +1927,9 @@ class POSIXProcessTestCase(BaseTestCase):
         self.assertIn(1, remaining_fds, "Subprocess failed")
 
 
+    @unittest.skipIf(sys.platform.startswith("freebsd") and
+                     os.stat("/dev").st_dev == os.stat("/dev/fd").st_dev,
+                     "Requires fdescfs mounted on /dev/fd on FreeBSD.")
     def test_close_fds_when_max_fd_is_lowered(self):
         """Confirm that issue21618 is fixed (may fail under valgrind)."""
         fd_status = support.findfile("fd_status.py", subdir="subprocessdata")
