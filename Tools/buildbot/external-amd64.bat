9789185e36be41ee82024ce462f29901a7cc1a76
Zachary Ware <zachary.ware@gmail.com>
2014-06-08 14:28:13 -0500

    Issue #21683: Add Tix build to the Windows buildbot scripts.

--- Tools/buildbot/external-amd64.bat
+++ Tools/buildbot/external-amd64.bat
@@ -12,10 +12,17 @@ if not exist tcltk64\bin\tcl86tg.dll (
 )
 
 if not exist tcltk64\bin\tk86tg.dll (
-    cd tk-8.6.1.0\win    
+    cd tk-8.6.1.0\win
     nmake -f makefile.vc OPTS=symbols MACHINE=AMD64 INSTALLDIR=..\..\tcltk64 TCLDIR=..\..\tcl-8.6.1.0 clean
     nmake -f makefile.vc OPTS=symbols MACHINE=AMD64 INSTALLDIR=..\..\tcltk64 TCLDIR=..\..\tcl-8.6.1.0 all
     nmake -f makefile.vc OPTS=symbols MACHINE=AMD64 INSTALLDIR=..\..\tcltk64 TCLDIR=..\..\tcl-8.6.1.0 install-binaries install-libraries
     cd ..\..
 )
 
+if not exist tcltk64\lib\tix8.4.3\tix84g.dll (
+    cd tix-8.4.3.4\win
+    nmake -f python.mak DEBUG=1 MACHINE=AMD64 TCL_DIR=..\..\tcl-8.6.1.0 TK_DIR=..\..\tk-8.6.1.0 INSTALL_DIR=..\..\tcltk64 clean
+    nmake -f python.mak DEBUG=1 MACHINE=AMD64 TCL_DIR=..\..\tcl-8.6.1.0 TK_DIR=..\..\tk-8.6.1.0 INSTALL_DIR=..\..\tcltk64 all
+    nmake -f python.mak DEBUG=1 MACHINE=AMD64 TCL_DIR=..\..\tcl-8.6.1.0 TK_DIR=..\..\tk-8.6.1.0 INSTALL_DIR=..\..\tcltk64 install
+    cd ..\..
+)
