8d24d77c63bdbf25d68bf0a6cad408d06abf2d00
Benjamin Peterson <benjamin@python.org>
2014-06-14 18:36:29 -0700

    url unquote the path before checking if it refers to a CGI script (closes #21766)

--- Lib/test/test_httpservers.py
+++ Lib/test/test_httpservers.py
@@ -510,6 +510,11 @@ class CGIHTTPServerTestCase(BaseTestCase):
                 (res.read(), res.getheader('Content-type'), res.status))
         self.assertEqual(os.environ['SERVER_SOFTWARE'], signature)
 
+    def test_urlquote_decoding_in_cgi_check(self):
+        res = self.request('/cgi-bin%2ffile1.py')
+        self.assertEqual((b'Hello World\n', 'text/html', 200),
+                (res.read(), res.getheader('Content-type'), res.status))
+
 
 class SimpleHTTPRequestHandlerTestCase(unittest.TestCase):
     """ Test url parsing """
