bea1575ea7b19f2d0fd69d15c4e890de0f92e02b
Terry Jan Reedy <tjreedy@udel.edu>
2014-06-16 03:05:37 -0400

    Issue #19362: Tweek len() doc and docstring to expand the indicated range of arguments. Original patch by Gareth Rees.

--- Python/bltinmodule.c
+++ Python/bltinmodule.c
@@ -1327,7 +1327,7 @@ builtin_len(PyObject *self, PyObject *v)
 PyDoc_STRVAR(len_doc,
 "len(object)\n\
 \n\
-Return the number of items of a sequence or mapping.");
+Return the number of items of a sequence or collection.");
 
 
 static PyObject *
