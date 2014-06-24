3eb76fc10b8c77b1e094fb9a1029ff5ebebff590
R David Murray <rdmurray@bitdance.com>
2014-06-24 16:49:24 -0400

    #20155: use fake HTTP method names so windows doesn't hang the tests.
    Windows was seeing the 'GET' generated by these tests as invalid and forcibly
    closing the socket, causing the test to fail.
    
    Patch by Jeff Allen.

--- Lib/test/test_httpservers.py
+++ Lib/test/test_httpservers.py
@@ -189,7 +189,7 @@ class BaseHTTPServerTestCase(BaseTestCase):
 
     def test_request_line_trimming(self):
         self.con._http_vsn_str = 'HTTP/1.1\n'
-        self.con.putrequest('GET', '/')
+        self.con.putrequest('XYZBOGUS', '/')
         self.con.endheaders()
         res = self.con.getresponse()
         self.assertEqual(res.status, 501)
@@ -216,8 +216,9 @@ class BaseHTTPServerTestCase(BaseTestCase):
         self.assertEqual(res.status, 501)
 
     def test_version_none(self):
+        # Test that a valid method is rejected when not HTTP/1.x
         self.con._http_vsn_str = ''
-        self.con.putrequest('PUT', '/')
+        self.con.putrequest('CUSTOM', '/')
         self.con.endheaders()
         res = self.con.getresponse()
         self.assertEqual(res.status, 400)
