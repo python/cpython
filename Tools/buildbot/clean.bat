e12fa657447c91356b3c52dac9b1e4da937bd26c
Zachary Ware <zachary.ware@gmail.com>
2014-07-07 13:39:59 -0500

    Issue #21907: Improved the batch scripts provided for building Python.
    The user-facing scripts in PCbuild have been updated to be easier to use
    and the buildbot scripts in Tools\buildbot have been updated to use the
    user-facing scripts in PCbuild wherever possible.

--- Tools/buildbot/clean.bat
+++ Tools/buildbot/clean.bat
@@ -1,8 +1,22 @@
-@rem Used by the buildbot "clean" step.
-call "%VS100COMNTOOLS%vsvars32.bat"
-@echo Deleting test leftovers ...
-rmdir /s /q build
-cd PCbuild
-msbuild /target:clean pcbuild.sln /p:Configuration=Release /p:PlatformTarget=x86
-msbuild /target:clean pcbuild.sln /p:Configuration=Debug /p:PlatformTarget=x86
-cd ..
+@echo off
+rem Used by the buildbot "clean" step.
+
+setlocal
+set root=%~dp0..\..
+set pcbuild=%root%\PCbuild
+
+echo.Attempting to kill Pythons...
+for %%k in (kill_python.exe
+            kill_python_d.exe
+            amd64\kill_python.exe
+            amd64\kill_python_d.exe
+            ) do (
+    if exist "%pcbuild%\%%k" (
+        echo.Calling %pcbuild%\%%k...
+        "%pcbuild%\%%k"
+    )
+)
+
+echo Purging all non-tracked files with `hg purge`
+echo on
+hg -R "%root%" --config extensions.purge= purge --all -X "%root%\Lib\test\data"
