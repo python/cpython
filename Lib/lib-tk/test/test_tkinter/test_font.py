186f66540dfcce1abec59d87d24e944c7987a53c
Serhiy Storchaka <storchaka@gmail.com>
2014-07-24 17:48:28 +0300

    Issue #1730136: Fix comparison between a tk Font object and an object of a different type.

---
+++ Lib/lib-tk/test/test_tkinter/test_font.py
@@ -0,0 +1,35 @@
+import unittest
+import Tkinter
+#from Tkinter
+import tkFont as font
+from test.test_support import requires, run_unittest
+import test_ttk.support as support
+
+requires('gui')
+
+class FontTest(unittest.TestCase):
+
+    def setUp(self):
+        support.root_deiconify()
+
+    def tearDown(self):
+        support.root_withdraw()
+
+    def test_font_eq(self):
+        fontname = "TkDefaultFont"
+        try:
+            f = font.Font(name=fontname, exists=True)
+        except tkinter._tkinter.TclError:
+            f = font.Font(name=fontname, exists=False)
+        font1 = font.nametofont(fontname)
+        font2 = font.nametofont(fontname)
+        self.assertIsNot(font1, font2)
+        self.assertEqual(font1, font2)
+        self.assertNotEqual(font1, font1.copy())
+        self.assertNotEqual(font1, 0)
+        self.assertNotIn(font1, [0])
+
+tests_gui = (FontTest, )
+
+if __name__ == "__main__":
+    run_unittest(*tests_gui)
