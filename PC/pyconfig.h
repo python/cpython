6d8f29a623f27d72bf1622132f2b2c4084eff9e3
Zachary Ware <zachary.ware@gmail.com>
2014-07-25 14:34:19 -0500

    Issue #21958: Define HAVE_ROUND when building with VS 2013 and above. Patch by Zachary Turner.

--- PC/pyconfig.h
+++ PC/pyconfig.h
@@ -435,6 +435,11 @@ Py_NO_ENABLE_SHARED to find out.  Also support MS_NO_COREDLL for b/w compat */
 /* Define to 1 if you have the `copysign' function. */
 #define HAVE_COPYSIGN 1
 
+/* Define to 1 if you have the `round' function. */
+#if _MSC_VER >= 1800
+#define HAVE_ROUND 1
+#endif
+
 /* Define to 1 if you have the `isinf' macro. */
 #define HAVE_DECL_ISINF 1
 
