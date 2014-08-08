9cf402c9784077a1584c1433395e898e86194042
Zachary Ware <zachary.ware@gmail.com>
2014-08-08 13:32:16 -0500

    Issue #22060: Clean up/simplify test_ctypes, use test discovery

--- Lib/test/test_ctypes.py
+++ Lib/test/test_ctypes.py
@@ -1,16 +1,6 @@
 import unittest
 
-from test.support import import_module
-
-# Skip tests if _ctypes module was not built.
-import_module('_ctypes')
-
-import ctypes.test
-
-def load_tests(*args):
-    skipped, testcases = ctypes.test.get_tests(ctypes.test, "test_*.py", verbosity=0)
-    suites = [unittest.makeSuite(t) for t in testcases]
-    return unittest.TestSuite(suites)
+from ctypes.test import load_tests
 
 if __name__ == "__main__":
     unittest.main()
