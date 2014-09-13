7c8b89e156a0d50a115946c63fd7226f2a9ef508
Benjamin Peterson <benjamin@python.org>
2014-09-13 01:44:34 -0400

    make hg link secure

--- Doc/tools/sphinxext/pyspecific.py
+++ Doc/tools/sphinxext/pyspecific.py
@@ -10,7 +10,7 @@
 """
 
 ISSUE_URI = 'http://bugs.python.org/issue%s'
-SOURCE_URI = 'http://hg.python.org/cpython/file/3.4/%s'
+SOURCE_URI = 'https://hg.python.org/cpython/file/3.4/%s'
 
 from docutils import nodes, utils
 
