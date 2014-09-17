cdabc372345a8fd53f50329d88dc7747a80e95d9
Senthil Kumaran <senthil@uthcode.com>
2014-09-17 16:27:06 +0800

    Issue #22419: Limit the length of incoming HTTP request in wsgiref server to 65536 bytes.

--- Lib/wsgiref/simple_server.py
+++ Lib/wsgiref/simple_server.py
@@ -113,7 +113,14 @@ class WSGIRequestHandler(BaseHTTPRequestHandler):
     def handle(self):
         """Handle a single HTTP request"""
 
-        self.raw_requestline = self.rfile.readline()
+        self.raw_requestline = self.rfile.readline(65537)
+        if len(self.raw_requestline) > 65536:
+            self.requestline = ''
+            self.request_version = ''
+            self.command = ''
+            self.send_error(414)
+            return
+
         if not self.parse_request(): # An error code has been sent, just exit
             return
 
