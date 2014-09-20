b17c1e08d9effb79d6c9ea27d30dc2d2fc6bdbe7
Terry Jan Reedy <tjreedy@udel.edu>
2014-09-19 22:54:09 -0400

    Update Idle's online doc url.

--- Lib/idlelib/EditorWindow.py
+++ Lib/idlelib/EditorWindow.py
@@ -153,7 +153,7 @@ class EditorWindow(object):
                     # Safari requires real file:-URLs
                     EditorWindow.help_url = 'file://' + EditorWindow.help_url
             else:
-                EditorWindow.help_url = "http://docs.python.org/%d.%d" % sys.version_info[:2]
+                EditorWindow.help_url = "https://docs.python.org/%d.%d/" % sys.version_info[:2]
         currentTheme=idleConf.CurrentTheme()
         self.flist = flist
         root = root or flist.root
