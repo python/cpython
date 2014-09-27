21c8d5c7d90e82202f680f0b7af8b9d7c0020949
Berker Peksag <berker.peksag@gmail.com>
2014-09-27 22:37:15 +0300

    Issue #16537: Check whether self.extensions is empty in setup.py.
    Patch by Jonathan Hosmer.

--- setup.py
+++ setup.py
@@ -252,7 +252,9 @@ class PyBuildExt(build_ext):
 
         build_ext.build_extensions(self)
 
-        longest = max([len(e.name) for e in self.extensions])
+        longest = 0
+        if self.extensions:
+            longest = max([len(e.name) for e in self.extensions])
         if self.failed:
             longest = max(longest, max([len(name) for name in self.failed]))
 
