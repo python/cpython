b10e773b60801b0cb488bde9135bda2806473960
Georg Brandl <georg@python.org>
2014-09-30 14:08:04 +0200

    Issue #16037: HTTPMessage.readheaders() raises an HTTPException when more than 100 headers are read.  Adapted from patch by Jyrki Pulliainen.

--- Lib/http/client.py
+++ Lib/http/client.py
@@ -206,6 +206,8 @@ MAXAMOUNT = 1048576
 
 # maximal line length when calling readline().
 _MAXLINE = 65536
+_MAXHEADERS = 100
+
 
 class HTTPMessage(email.message.Message):
     # XXX The only usage of this method is in
@@ -253,6 +255,8 @@ def parse_headers(fp, _class=HTTPMessage):
         if len(line) > _MAXLINE:
             raise LineTooLong("header line")
         headers.append(line)
+        if len(headers) > _MAXHEADERS:
+            raise HTTPException("got more than %d headers" % _MAXHEADERS)
         if line in (b'\r\n', b'\n', b''):
             break
     hstring = b''.join(headers).decode('iso-8859-1')
