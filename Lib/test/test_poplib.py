d37cfdda0568f53bccf9d57aa377d50d4b11d85b
Georg Brandl <georg@python.org>
2014-09-30 14:45:39 +0200

    Issue #16041: CVE-2013-1752: poplib: Limit maximum line lengths to 2048 to prevent readline() calls from consuming too much memory.  Patch by Jyrki Pulliainen.

--- Lib/test/test_poplib.py
+++ Lib/test/test_poplib.py
@@ -83,7 +83,7 @@ class DummyPOP3Handler(asynchat.async_chat):
 
     def cmd_list(self, arg):
         if arg:
-            self.push('+OK %s %s' %(arg, arg))
+            self.push('+OK %s %s' % (arg, arg))
         else:
             self.push('+OK')
             asynchat.async_chat.push(self, LIST_RESP)
@@ -203,6 +203,10 @@ class TestPOP3Class(TestCase):
                     113)
         foo = self.client.retr('foo')
         self.assertEqual(foo, expected)
+
+    def test_too_long_lines(self):
+        self.assertRaises(poplib.error_proto, self.client._shortcmd,
+                          'echo +%s' % ((poplib._MAXLINE + 10) * 'a'))
 
     def test_dele(self):
         self.assertOK(self.client.dele('foo'))
