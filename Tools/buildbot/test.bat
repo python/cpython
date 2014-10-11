a7d8a23625707ac751007c92ac653de4140b4f92
Zachary Ware <zachary.ware@gmail.com>
2014-10-11 01:43:35 -0500

    Issue #21907: Try to make the pre-Vista Windows buildbots happier.
    Initial patch by David Bolen.

--- Tools/buildbot/test.bat
+++ Tools/buildbot/test.bat
@@ -3,4 +3,14 @@
 rem The following line should be removed before #20035 is closed
 set TCL_LIBRARY=%~dp0..\..\..\tcltk\lib\tcl8.6
 
+ver | findstr "Version 6." >nul
+if %ERRORLEVEL% == 1 goto xp
+
 "%~dp0..\..\PCbuild\python_d.exe" "%~dp0..\scripts\run_tests.py" -j 1 -u all -W --timeout=3600 %*
+goto done
+
+:xp
+cd PCbuild
+call rt.bat -d -q -uall -rwW -n --timeout=3600 %1 %2 %3 %4 %5 %6 %7 %8 %9
+
+:done
