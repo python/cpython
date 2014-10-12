8783ee594837b60c194faac5deaef63c4fb0085c
Georg Brandl <georg@python.org>
2014-10-12 08:50:11 +0200

    #16040: fix unlimited read from connection in nntplib.

--- Lib/test/test_nntplib.py
+++ Lib/test/test_nntplib.py
@@ -563,6 +563,11 @@ class NNTPv1Handler:
                 <a4929a40-6328-491a-aaaf-cb79ed7309a2@q2g2000vbk.googlegroups.com>
                 <f30c0419-f549-4218-848f-d7d0131da931@y3g2000vbm.googlegroups.com>
                 .""")
+        elif (group == 'comp.lang.python' and
+              date_str in ('20100101', '100101') and
+              time_str == '090000'):
+            self.push_lit('too long line' * 3000 +
+                          '\n.')
         else:
             self.push_lit("""\
                 230 An empty list of newsarticles follows
@@ -1157,6 +1162,11 @@ class NNTPv1v2TestsMixin:
             self.server.ihave("<another.message.id>", self.sample_post)
         self.assertEqual(cm.exception.response,
                          "435 Article not wanted")
+
+    def test_too_long_lines(self):
+        dt = datetime.datetime(2010, 1, 1, 9, 0, 0)
+        self.assertRaises(nntplib.NNTPDataError,
+                          self.server.newnews, "comp.lang.python", dt)
 
 
 class NNTPv1Tests(NNTPv1v2TestsMixin, MockedNNTPTestsMixin, unittest.TestCase):
