6b04dc9bcc3f87d6ec5d298b49f2fa7644356c37
Terry Jan Reedy <tjreedy@udel.edu>
2014-10-12 01:11:05 -0400

    Issue #14105: Change comment to reflect fix.  Patch by Saimadhav Heblikar.

--- Lib/idlelib/PyShell.py
+++ Lib/idlelib/PyShell.py
@@ -227,13 +227,8 @@ class PyShellEditorWindow(EditorWindow):
         #     This is necessary to keep the saved breaks synched with the
         #     saved file.
         #
-        #     Breakpoints are set as tagged ranges in the text.  Certain
-        #     kinds of edits cause these ranges to be deleted: Inserting
-        #     or deleting a line just before a breakpoint, and certain
-        #     deletions prior to a breakpoint.  These issues need to be
-        #     investigated and understood.  It's not clear if they are
-        #     Tk issues or IDLE issues, or whether they can actually
-        #     be fixed.  Since a modified file has to be saved before it is
+        #     Breakpoints are set as tagged ranges in the text.
+        #     Since a modified file has to be saved before it is
         #     run, and since self.breakpoints (from which the subprocess
         #     debugger is loaded) is updated during the save, the visible
         #     breaks stay synched with the subprocess even if one of these
