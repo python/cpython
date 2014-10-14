26e825273c63cad3b0992cdd24ee64e1cec73fe4
Terry Jan Reedy <tjreedy@udel.edu>
2014-10-14 16:56:36 -0400

    Issue #22632: replace dead link with version-specific doc link.

--- Lib/idlelib/aboutDialog.py
+++ Lib/idlelib/aboutDialog.py
@@ -62,7 +62,8 @@ class AboutDialog(Toplevel):
                            justify=LEFT, fg=self.fg, bg=self.bg)
         labelEmail.grid(row=6, column=0, columnspan=2,
                         sticky=W, padx=10, pady=0)
-        labelWWW = Label(frameBg, text='www:  http://www.python.org/idle/',
+        labelWWW = Label(frameBg, text='https://docs.python.org/' +
+                         sys.version[:3] + '/library/idle.html',
                          justify=LEFT, fg=self.fg, bg=self.bg)
         labelWWW.grid(row=7, column=0, columnspan=2, sticky=W, padx=10, pady=0)
         Frame(frameBg, borderwidth=1, relief=SUNKEN,
