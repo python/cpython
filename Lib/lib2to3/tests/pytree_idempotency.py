cc2408c6aedd47165cfd854508e3a0a676dec648
Zachary Ware <zachary.ware@gmail.com>
2014-10-29 12:24:59 -0500

    Issue #22173: Update lib2to3 tests to use unittest test discovery.

--- Lib/lib2to3/tests/pytree_idempotency.py
+++ Lib/lib2to3/tests/pytree_idempotency.py
@@ -4,6 +4,8 @@
 
 """Main program for testing the infrastructure."""
 
+from __future__ import print_function
+
 __author__ = "Guido van Rossum <guido@python.org>"
 
 # Support imports (need to be imported first)
