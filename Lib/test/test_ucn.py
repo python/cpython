a1b439de9ec8981c88127d6491a27ce90e9acd3f
Georg Brandl <georg@python.org>
2014-11-06 14:37:49 +0100

    #22650: test suite: load Unicode test data files from www.pythontest.net

--- Lib/test/test_ucn.py
+++ Lib/test/test_ucn.py
@@ -172,7 +172,7 @@ class UnicodeNamesTest(unittest.TestCase):
 
     def test_named_sequences_full(self):
         # Check all the named sequences
-        url = ("http://www.unicode.org/Public/%s/ucd/NamedSequences.txt" %
+        url = ("http://www.pythontest.net/unicode/%s/NamedSequences.txt" %
                unicodedata.unidata_version)
         try:
             testdata = support.open_urlresource(url, encoding="utf-8",
