225c65397cc4183af9a6ff20a29bcbd676fa765d
Serhiy Storchaka <storchaka@gmail.com>
2014-11-10 10:42:26 +0200

    Issue #22821: Fixed fcntl() with integer argument on 64-bit big-endian platforms.

--- Modules/fcntlmodule.c
+++ Modules/fcntlmodule.c
@@ -34,7 +34,7 @@ fcntl_fcntl(PyObject *self, PyObject *args)
 {
     int fd;
     int code;
-    long arg;
+    int arg;
     int ret;
     char *str;
     Py_ssize_t len;
@@ -61,7 +61,7 @@ fcntl_fcntl(PyObject *self, PyObject *args)
     PyErr_Clear();
     arg = 0;
     if (!PyArg_ParseTuple(args,
-         "O&i|l;fcntl requires a file or file descriptor,"
+         "O&i|I;fcntl requires a file or file descriptor,"
          " an integer and optionally a third integer or a string",
                           conv_descriptor, &fd, &code, &arg)) {
       return NULL;
