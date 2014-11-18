8361438c9e99d479a7d6251c3321bcc289248adf
Serhiy Storchaka <storchaka@gmail.com>
2014-11-18 17:30:15 +0200

    Issue #18637: Fixed an error in _PyNode_SizeOf declaration. Patch by Roumen Petrov.

--- Include/node.h
+++ Include/node.h
@@ -21,7 +21,7 @@ PyAPI_FUNC(int) PyNode_AddChild(node *n, int type,
                                       char *str, int lineno, int col_offset);
 PyAPI_FUNC(void) PyNode_Free(node *n);
 #ifndef Py_LIMITED_API
-Py_ssize_t _PyNode_SizeOf(node *n);
+PyAPI_FUNC(Py_ssize_t) _PyNode_SizeOf(node *n);
 #endif
 
 /* Node access functions */
