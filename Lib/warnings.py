e6b42438fa53f7bcadc12bdcfa491b99f280f115
Serhiy Storchaka <storchaka@gmail.com>
2014-12-10 23:05:33 +0200

    Issue #23016: A warning no longer produces an AttributeError when sys.stderr is None.

--- Lib/warnings.py
+++ Lib/warnings.py
@@ -26,6 +26,9 @@ def _show_warning(message, category, filename, lineno, file=None, line=None):
     """Hook to write a warning to a file; replace if you like."""
     if file is None:
         file = sys.stderr
+        if file is None:
+            # sys.stderr is None - warnings get lost
+            return
     try:
         file.write(formatwarning(message, category, filename, lineno, line))
     except IOError:
