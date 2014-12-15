1f6c29a914e442601ca6e71aa40b93689ed0fbe1
Steve Dower <steve.dower@microsoft.com>
2014-12-15 15:03:44 -0800

    Removes bdist_wininst dependency on MFC.

--- PC/bdist_wininst/resource.h
+++ PC/bdist_wininst/resource.h
@@ -6,10 +6,6 @@
   CHECKED IN AS WELL!
 */
 
-//{{NO_DEPENDENCIES}}
-// Microsoft Developer Studio generated include file.
-// Used by install.rc
-//
 #define IDD_DIALOG1                     101
 #define IDB_BITMAP1                     103
 #define IDD_INTRO                       107
@@ -33,14 +29,3 @@
 #define IDC_BUILD_INFO                  1024
 #define IDC_BITMAP                      1025
 #define IDC_OTHERPYTHON                 1026
-
-// Next default values for new objects
-// 
-#ifdef APSTUDIO_INVOKED
-#ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        112
-#define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1028
-#define _APS_NEXT_SYMED_VALUE           101
-#endif
-#endif
