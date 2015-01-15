cbd3bd59c0322e3407c17843a7f61ddebbd15875
Steve Dower <steve.dower@microsoft.com>
2015-01-15 09:16:38 -0800

    Closes #23160: Respect the environment variable SVNROOT in external-common.bat (patch by anselm.kruis)

--- Tools/buildbot/external-common.bat
+++ Tools/buildbot/external-common.bat
@@ -1,6 +1,8 @@
 @rem Common file shared between external.bat and external-amd64.bat.  Responsible for
 @rem fetching external components into the root\.. buildbot directories.
 
+if "%SVNROOT%"=="" set SVNROOT=http://svn.python.org/projects/external/
+
 if not exist externals mkdir externals
 cd externals
 @rem XXX: If you need to force the buildbots to start from a fresh environment, uncomment
@@ -18,35 +20,35 @@ cd externals
 @rem bzip
 if not exist bzip2-1.0.6 (
    rd /s/q bzip2-1.0.5
-  svn export http://svn.python.org/projects/external/bzip2-1.0.6
+  svn export %SVNROOT%bzip2-1.0.6
 )
 
 @rem NASM, for OpenSSL build
 @rem if exist nasm-2.11.06 rd /s/q nasm-2.11.06
-if not exist nasm-2.11.06 svn export http://svn.python.org/projects/external/nasm-2.11.06
+if not exist nasm-2.11.06 svn export %SVNROOT%nasm-2.11.06
 
 @rem OpenSSL
 if not exist openssl-1.0.1j (
     rd /s/q openssl-1.0.1i
-    svn export http://svn.python.org/projects/external/openssl-1.0.1j
+    svn export %SVNROOT%openssl-1.0.1j
 )
 
 @rem tcl/tk
 if not exist tcl-8.6.1.0 (
    rd /s/q tcltk tcltk64 tcl-8.5.11.0 tk-8.5.11.0
-   svn export http://svn.python.org/projects/external/tcl-8.6.1.0
+   svn export %SVNROOT%tcl-8.6.1.0
 )
-if not exist tk-8.6.1.0 svn export http://svn.python.org/projects/external/tk-8.6.1.0
-if not exist tix-8.4.3.4 svn export http://svn.python.org/projects/external/tix-8.4.3.4
+if not exist tk-8.6.1.0 svn export %SVNROOT%tk-8.6.1.0
+if not exist tix-8.4.3.4 svn export %SVNROOT%tix-8.4.3.4
 
 @rem sqlite3
 if not exist sqlite-3.8.3.1 (
   rd /s/q sqlite-source-3.8.1
-  svn export http://svn.python.org/projects/external/sqlite-3.8.3.1
+  svn export %SVNROOT%sqlite-3.8.3.1
 )
 
 @rem lzma
 if not exist xz-5.0.5 (
   rd /s/q xz-5.0.3
-  svn export http://svn.python.org/projects/external/xz-5.0.5
+  svn export %SVNROOT%xz-5.0.5
 )
