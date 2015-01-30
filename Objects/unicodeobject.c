227526dc5c47151d3829823f39ba122e7a348ed1
Serhiy Storchaka <storchaka@gmail.com>
2015-01-31 01:15:29 +0200

    Issue #23055: Fixed read-past-the-end error in PyUnicode_FromFormatV.

--- Objects/unicodeobject.c
+++ Objects/unicodeobject.c
@@ -738,6 +738,8 @@ PyUnicode_FromFormatV(const char *format, va_list vargs)
              f++;
              while (*f && *f != '%' && !isalpha((unsigned)*f))
                  f++;
+             if (!*f)
+                 break;
              if (*f == 's' || *f=='S' || *f=='R')
                  ++callcount;
          }
