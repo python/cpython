c86ca26d3201d58a33cdc28dfbd49f9a84122d06
Serhiy Storchaka <storchaka@gmail.com>
2015-02-15 14:18:32 +0200

    Issue #23096: Pickle representation of floats with protocol 0 now is the same for both Python and C implementations.

--- Modules/_pickle.c
+++ Modules/_pickle.c
@@ -1979,7 +1979,7 @@ save_float(PicklerObject *self, PyObject *obj)
         if (_Pickler_Write(self, &op, 1) < 0)
             goto done;
 
-        buf = PyOS_double_to_string(x, 'g', 17, 0, NULL);
+        buf = PyOS_double_to_string(x, 'r', 0, Py_DTSF_ADD_DOT_0, NULL);
         if (!buf) {
             PyErr_NoMemory();
             goto done;
