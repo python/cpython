3654c8c1a863dea7c718e46409099fb282f2f2ec
Steve Dower <steve.dower@microsoft.com>
2015-02-23 07:56:13 -0800

    Issue 23314: SuppressCrashReports now disables CRT assertions SuppressCrashReports should be used in test subprocesses that test invalid conditions.

--- Lib/test/tf_inherit_check.py
+++ Lib/test/tf_inherit_check.py
@@ -4,22 +4,24 @@
 
 import sys
 import os
+from test.support import SuppressCrashReport
 
-verbose = (sys.argv[1] == 'v')
-try:
-    fd = int(sys.argv[2])
-
+with SuppressCrashReport():
+    verbose = (sys.argv[1] == 'v')
     try:
-        os.write(fd, b"blat")
-    except OSError:
-        # Success -- could not write to fd.
-        sys.exit(0)
-    else:
-        if verbose:
-            sys.stderr.write("fd %d is open in child" % fd)
-        sys.exit(1)
+        fd = int(sys.argv[2])
 
-except Exception:
-    if verbose:
-        raise
-    sys.exit(1)
+        try:
+            os.write(fd, b"blat")
+        except OSError:
+            # Success -- could not write to fd.
+            sys.exit(0)
+        else:
+            if verbose:
+                sys.stderr.write("fd %d is open in child" % fd)
+            sys.exit(1)
+
+    except Exception:
+        if verbose:
+            raise
+        sys.exit(1)
