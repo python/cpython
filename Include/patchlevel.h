320bec1922376e449325a9b3f63178e25ea77532
doko@ubuntu.com <doko@ubuntu.com>
2015-04-02 15:59:13 +0200

    - Set PY_VERSION to "2.7.10b0", PY_MICRO_VERSION to 10 and   PY_RELEASE_LEVEL to PY_RELEASE_LEVEL_BETA.

--- Include/patchlevel.h
+++ Include/patchlevel.h
@@ -22,12 +22,12 @@
 /*--start constants--*/
 #define PY_MAJOR_VERSION	2
 #define PY_MINOR_VERSION	7
-#define PY_MICRO_VERSION	9
-#define PY_RELEASE_LEVEL	PY_RELEASE_LEVEL_FINAL
+#define PY_MICRO_VERSION	10
+#define PY_RELEASE_LEVEL	PY_RELEASE_LEVEL_BETA
 #define PY_RELEASE_SERIAL	0
 
 /* Version as a string */
-#define PY_VERSION      	"2.7.9+"
+#define PY_VERSION      	"2.7.10b0"
 /*--end constants--*/
 
 /* Subversion Revision number of this file (not of the repository). Empty
