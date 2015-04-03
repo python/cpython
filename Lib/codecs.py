c811328e4485bba17901e8da6c119ccd3fb83878
Serhiy Storchaka <storchaka@gmail.com>
2015-04-03 18:12:32 +0300

    Escaped backslashes in docstrings.

--- Lib/codecs.py
+++ Lib/codecs.py
@@ -1057,7 +1057,7 @@ def make_encoding_map(decoding_map):
         during translation.
 
         One example where this happens is cp875.py which decodes
-        multiple character to \u001a.
+        multiple character to \\u001a.
 
     """
     m = {}
