c7d957fe4a7a4ad4d6ee9d337b2d323447bbd1aa
Donald Stufft <donald@stufft.io>
2015-04-07 07:09:06 -0400

    Update pip to 6.1.1

--- Lib/ensurepip/__init__.py
+++ Lib/ensurepip/__init__.py
@@ -14,7 +14,7 @@ __all__ = ["version", "bootstrap"]
 
 _SETUPTOOLS_VERSION = "15.0"
 
-_PIP_VERSION = "6.1.0"
+_PIP_VERSION = "6.1.1"
 
 # pip currently requires ssl support, so we try to provide a nicer
 # error message when that is missing (http://bugs.python.org/issue19744)
