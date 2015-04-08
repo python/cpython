52c0c3382d95a8a4877a5dfaaacf61b436f0b1bf
Berker Peksag <berker.peksag@gmail.com>
2015-04-08 11:24:27 +0300

    Skip nntplib tests when transient errors captured.

--- Lib/test/support/__init__.py
+++ Lib/test/support/__init__.py
@@ -13,6 +13,7 @@ import gc
 import importlib
 import importlib.util
 import logging.handlers
+import nntplib
 import os
 import platform
 import re
@@ -1340,6 +1341,10 @@ def transient_internet(resource_name, *, timeout=30.0, errnos=()):
         if timeout is not None:
             socket.setdefaulttimeout(timeout)
         yield
+    except nntplib.NNTPTemporaryError as err:
+        if verbose:
+            sys.stderr.write(denied.args[0] + "\n")
+        raise denied from err
     except OSError as err:
         # urllib can wrap original socket errors multiple times (!), we must
         # unwrap to get at the original error.
