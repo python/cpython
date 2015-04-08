139c8312dba21fc9f5e7f2a1b58e55da4956f7ff
Berker Peksag <berker.peksag@gmail.com>
2015-04-08 09:47:14 +0300

    Issue #23883: Add missing entries to traceback.__all__.

--- Lib/traceback.py
+++ Lib/traceback.py
@@ -7,8 +7,9 @@ import operator
 __all__ = ['extract_stack', 'extract_tb', 'format_exception',
            'format_exception_only', 'format_list', 'format_stack',
            'format_tb', 'print_exc', 'format_exc', 'print_exception',
-           'print_last', 'print_stack', 'print_tb',
-           'clear_frames']
+           'print_last', 'print_stack', 'print_tb', 'clear_frames',
+           'FrameSummary', 'StackSummary', 'TracebackException',
+           'walk_stack', 'walk_tb']
 
 #
 # Formatting and printing lists of traceback lines.
