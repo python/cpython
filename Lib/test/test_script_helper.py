ac825a5d7e2d9192243d970ad4eb0f7966f958cb
Antoine Pitrou <solipsis@pitrou.net>
2015-04-13 19:41:47 +0200

    Issue #23309: Avoid a deadlock at shutdown if a daemon thread is aborted while it is holding a lock to a buffered I/O object, and the main thread tries to use the same I/O object (typically stdout or stderr).  A fatal error is emitted instead.

--- Lib/test/test_script_helper.py
+++ Lib/test/test_script_helper.py
@@ -8,26 +8,27 @@ from unittest import mock
 
 
 class TestScriptHelper(unittest.TestCase):
-    def test_assert_python_expect_success(self):
-        t = script_helper._assert_python(True, '-c', 'import sys; sys.exit(0)')
+
+    def test_assert_python_ok(self):
+        t = script_helper.assert_python_ok('-c', 'import sys; sys.exit(0)')
         self.assertEqual(0, t[0], 'return code was not 0')
 
-    def test_assert_python_expect_failure(self):
+    def test_assert_python_failure(self):
         # I didn't import the sys module so this child will fail.
-        rc, out, err = script_helper._assert_python(False, '-c', 'sys.exit(0)')
+        rc, out, err = script_helper.assert_python_failure('-c', 'sys.exit(0)')
         self.assertNotEqual(0, rc, 'return code should not be 0')
 
-    def test_assert_python_raises_expect_success(self):
+    def test_assert_python_ok_raises(self):
         # I didn't import the sys module so this child will fail.
         with self.assertRaises(AssertionError) as error_context:
-            script_helper._assert_python(True, '-c', 'sys.exit(0)')
+            script_helper.assert_python_ok('-c', 'sys.exit(0)')
         error_msg = str(error_context.exception)
         self.assertIn('command line was:', error_msg)
         self.assertIn('sys.exit(0)', error_msg, msg='unexpected command line')
 
-    def test_assert_python_raises_expect_failure(self):
+    def test_assert_python_failure_raises(self):
         with self.assertRaises(AssertionError) as error_context:
-            script_helper._assert_python(False, '-c', 'import sys; sys.exit(0)')
+            script_helper.assert_python_failure('-c', 'import sys; sys.exit(0)')
         error_msg = str(error_context.exception)
         self.assertIn('Process return code is 0,', error_msg)
         self.assertIn('import sys; sys.exit(0)', error_msg,
