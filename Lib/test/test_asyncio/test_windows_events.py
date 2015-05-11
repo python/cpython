59eb9a4da504315345b5b0d8fb8eebddc22ccb92
Yury Selivanov <yselivanov@sprymix.com>
2015-05-11 14:48:38 -0400

    asyncio: async() function is deprecated in favour of ensure_future().

--- Lib/test/test_asyncio/test_windows_events.py
+++ Lib/test/test_asyncio/test_windows_events.py
@@ -37,7 +37,7 @@ class ProactorTests(test_utils.TestCase):
     def test_close(self):
         a, b = self.loop._socketpair()
         trans = self.loop._make_socket_transport(a, asyncio.Protocol())
-        f = asyncio.async(self.loop.sock_recv(b, 100))
+        f = asyncio.ensure_future(self.loop.sock_recv(b, 100))
         trans.close()
         self.loop.run_until_complete(f)
         self.assertEqual(f.result(), b'')
