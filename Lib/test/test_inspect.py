e9ff4e251fd2aeefe51742039cd0152442d0be68
Yury Selivanov <yselivanov@sprymix.com>
2015-05-14 18:47:17 -0400

    Issue 22547: Implement informative __repr__ for inspect.BoundArguments

--- Lib/test/test_inspect.py
+++ Lib/test/test_inspect.py
@@ -3149,6 +3149,13 @@ class TestBoundArguments(unittest.TestCase):
                 ba_pickled = pickle.loads(pickle.dumps(ba, ver))
                 self.assertEqual(ba, ba_pickled)
 
+    def test_signature_bound_arguments_repr(self):
+        def foo(a, b, *, c:1={}, **kw) -> {42:'ham'}: pass
+        sig = inspect.signature(foo)
+        ba = sig.bind(20, 30, z={})
+        self.assertRegex(repr(ba),
+                         r'<BoundArguments at 0x[a-fA-F0-9]+ \(a=20,.*\}\}\)>')
+
 
 class TestSignaturePrivateHelpers(unittest.TestCase):
     def test_signature_get_bound_param(self):
