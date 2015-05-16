75fb816c78be5668d5274df9c9a0c26d841339ad
Terry Jan Reedy <tjreedy@udel.edu>
2015-05-15 23:55:21 -0400

    Issue #23184: idle tests, remove unused names and imports.

--- Lib/idlelib/idle_test/test_searchdialogbase.py
+++ Lib/idlelib/idle_test/test_searchdialogbase.py
@@ -6,13 +6,13 @@ testing skipping of suite when self.needwrapbutton is false.
 '''
 import unittest
 from test.support import requires
-from tkinter import Tk, Toplevel, Frame, Label, BooleanVar, StringVar
+from tkinter import Tk, Toplevel, Frame ##, BooleanVar, StringVar
 from idlelib import SearchEngine as se
 from idlelib import SearchDialogBase as sdb
 from idlelib.idle_test.mock_idle import Func
-from idlelib.idle_test.mock_tk import Var, Mbox
+## from idlelib.idle_test.mock_tk import Var
 
-# The following could help make some tests gui-free.
+# The ## imports above & following could help make some tests gui-free.
 # However, they currently make radiobutton tests fail.
 ##def setUpModule():
 ##    # Replace tk objects used to initialize se.SearchEngine.
