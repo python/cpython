a71a4f4ed32e9f1c6b43c7ac660dcc58cb0ce9bc
Benjamin Peterson <benjamin@python.org>
2015-05-22 17:53:06 -0500

    make idlever.py self-updating (closes #24199)

--- Lib/idlelib/idlever.py
+++ Lib/idlelib/idlever.py
@@ -1,1 +1,4 @@
-IDLE_VERSION = "2.7.10rc1"
+"""Unused by Idle: there is no separate Idle version anymore.
+Kept only for possible existing extension use."""
+from sys import version
+IDLE_VERSION = version[:version.index(' ')]
