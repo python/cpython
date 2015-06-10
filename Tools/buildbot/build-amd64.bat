6250df81bfbe567bb12c6dc29af149c62f35afa6
Zachary Ware <zachary.ware@gmail.com>
2015-06-09 23:16:52 -0500

    Clean up/refactor the batch scripts used for building on Windows.
    This is mostly a backport of issue #21907, but also includes a few
    extras necessary to make the bulidbot scripts as thin as possible.

--- Tools/buildbot/build-amd64.bat
+++ Tools/buildbot/build-amd64.bat
@@ -1,6 +1,2 @@
 @rem Used by the buildbot "compile" step.
-cmd /c Tools\buildbot\external-amd64.bat
-call "%VS100COMNTOOLS%\..\..\VC\vcvarsall.bat" x86_amd64
-cmd /c Tools\buildbot\clean-amd64.bat
-
-msbuild PCbuild\pcbuild.sln /p:Configuration=Debug /p:Platform=x64
+call "%~dp0build.bat" -p x64 %*
