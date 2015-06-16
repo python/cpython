14acd40001a41fe2d437dcd287332e14febd9219
Zachary Ware <zachary.ware@gmail.com>
2015-06-16 10:56:14 -0500

    Deprecate unused scripts in Tools/buildbot.
    I would just outright delete them, but the readme in PCbuild recommended
    their use, so I figure it would be nice to leave them there for a while.

--- Tools/buildbot/build-amd64.bat
+++ Tools/buildbot/build-amd64.bat
@@ -1,2 +1,5 @@
-@rem Used by the buildbot "compile" step.
+@rem Formerly used by the buildbot "compile" step.
+@echo This script is no longer used and may be removed in the future.
+@echo To get the same effect as this script, use
+@echo     PCbuild\build.bat -d -e -k -p x64
 call "%~dp0build.bat" -p x64 %*
