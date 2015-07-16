27fbb96a109bff6e82764a87f77a20ca02494cf1
Steve Dower <steve.dower@microsoft.com>
2015-07-16 16:33:55 -0700

    Adds option to install launcher for all users even when installing Python just-for-me. This helps mitigate issues when the incompatible Python 3.4 launcher is installed. Enables installer builds with the the text marker. Allows simple installs to include a custom description.

--- Tools/msi/build.bat
+++ Tools/msi/build.bat
@@ -6,12 +6,14 @@ set PCBUILD=%D%..\..\PCBuild\
 set BUILDX86=
 set BUILDX64=
 set BUILDDOC=
+set BUILDPX=
 
 :CheckOpts
 if "%~1" EQU "-h" goto Help
 if "%~1" EQU "-x86" (set BUILDX86=1) && shift && goto CheckOpts
 if "%~1" EQU "-x64" (set BUILDX64=1) && shift && goto CheckOpts
 if "%~1" EQU "--doc" (set BUILDDOC=1) && shift && goto CheckOpts
+if "%~1" EQU "--test-marker" (set BUILDPX=1) && shift && goto CheckOpts
 
 if not defined BUILDX86 if not defined BUILDX64 (set BUILDX86=1) && (set BUILDX64=1)
 
@@ -35,22 +37,28 @@ if defined BUILDDOC (
     if errorlevel 1 goto :eof
 )
 
+set BUILD_CMD="%D%bundle\snapshot.wixproj"
+if defined BUILDPX (
+    set BUILD_CMD=%BUILD_CMD% /p:UseTestMarker=true
+)
+
 if defined BUILDX86 (
     "%PCBUILD%win32\python.exe" "%D%get_wix.py"
-    msbuild "%D%bundle\snapshot.wixproj"
+    msbuild %BUILD_CMD%
     if errorlevel 1 goto :eof
 )
 if defined BUILDX64 (
     "%PCBUILD%amd64\python.exe" "%D%get_wix.py"
-    msbuild "%D%bundle\snapshot.wixproj" /p:Platform=x64
+    msbuild /p:Platform=x64 %BUILD_CMD%
     if errorlevel 1 goto :eof
 )
 
 exit /B 0
 
 :Help
-echo build.bat [-x86] [-x64] [--doc] [-h]
+echo build.bat [-x86] [-x64] [--doc] [-h] [--test-marker]
 echo.
 echo    -x86                Build x86 installers
 echo    -x64                Build x64 installers
 echo    --doc               Build CHM documentation
+echo    --test-marker       Build installers with 'x' markers
