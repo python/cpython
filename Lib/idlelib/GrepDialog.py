58785d545082d800a6388f8d80423acce970df40
Terry Jan Reedy <tjreedy@udel.edu>
2015-07-19 18:32:06 -0400

    Issue #24671: Finish print conversion, idlelib GrepDialog and WidgetRedirector.

--- Lib/idlelib/GrepDialog.py
+++ Lib/idlelib/GrepDialog.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 import os
 import fnmatch
 import re  # for htest
--- Lib/idlelib/WidgetRedirector.py
+++ Lib/idlelib/WidgetRedirector.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 from Tkinter import TclError
 
 class WidgetRedirector:
@@ -161,7 +162,7 @@ def _widget_redirector(parent):  # htest #
     text.focus_set()
     redir = WidgetRedirector(text)
     def my_insert(*args):
-        print "insert", args
+        print("insert", args)
         original_insert(*args)
     original_insert = redir.register("insert", my_insert)
     root.mainloop()
