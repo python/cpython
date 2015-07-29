222c0fde44816b58bcfc0966d19ab52e8de12ea6
Ned Deily <nad@acm.org>
2015-07-28 23:22:23 -0700

    Issue #19450: Update OS X installer builds to use SQLite 3.8.11.

--- Mac/BuildScript/build-installer.py
+++ Mac/BuildScript/build-installer.py
@@ -339,9 +339,9 @@ def library_recipes():
                   ),
           ),
           dict(
-              name="SQLite 3.8.3.1",
-              url="http://www.sqlite.org/2014/sqlite-autoconf-3080301.tar.gz",
-              checksum='509ff98d8dc9729b618b7e96612079c6',
+              name="SQLite 3.8.11",
+              url="https://www.sqlite.org/2015/sqlite-autoconf-3081100.tar.gz",
+              checksum='77b451925121028befbddbf45ea2bc49',
               extra_cflags=('-Os '
                             '-DSQLITE_ENABLE_FTS4 '
                             '-DSQLITE_ENABLE_FTS3_PARENTHESIS '
