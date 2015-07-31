852cec63bf582a37485cb335e9bcf9b2e8103ec2
Raymond Hettinger <python@rcn.com>
2015-07-31 12:03:20 -0700

    Fix minor typo in a comment

--- Modules/_collectionsmodule.c
+++ Modules/_collectionsmodule.c
@@ -1047,7 +1047,7 @@ deque_clear(dequeobject *deque)
 static int
 valid_index(Py_ssize_t i, Py_ssize_t limit)
 {
-    /* The cast to size_t let us use just a single comparison
+    /* The cast to size_t lets us use just a single comparison
        to check whether i is in the range: 0 <= i < limit */
     return (size_t) i < (size_t) limit;
 }
