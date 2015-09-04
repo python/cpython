a6deff2588a27b9e7a68c1ae10171e191e8a8621
Zachary Ware <zachary.ware@gmail.com>
2015-09-04 01:10:23 -0500

    Issue #24986: Save some bandwidth from svn.python.org
    Don't download sources that won't be used.

--- PCbuild/build.bat
+++ PCbuild/build.bat
@@ -66,13 +66,15 @@ if "%~1"=="-V" shift & goto Version
 rem These use the actual property names used by MSBuild.  We could just let
 rem them in through the environment, but we specify them on the command line
 rem anyway for visibility so set defaults after this
-if "%~1"=="-e" (set IncludeExternals=true) & call "%dir%get_externals.bat" & shift & goto CheckOpts
+if "%~1"=="-e" (set IncludeExternals=true) & shift & goto CheckOpts
 if "%~1"=="--no-ssl" (set IncludeSSL=false) & shift & goto CheckOpts
 if "%~1"=="--no-tkinter" (set IncludeTkinter=false) & shift & goto CheckOpts
 
 if "%IncludeExternals%"=="" set IncludeExternals=false
 if "%IncludeSSL%"=="" set IncludeSSL=true
 if "%IncludeTkinter%"=="" set IncludeTkinter=true
+
+if "%IncludeExternals%"=="true" call "%dir%get_externals.bat"
 
 if "%platf%"=="x64" (set vs_platf=x86_amd64)
 
