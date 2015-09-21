f6e7ca3ca1ea1a7a2dd819f89ea1645ab36c57a8
Terry Jan Reedy <tjreedy@udel.edu>
2015-09-21 01:44:06 -0400

    Issue #24861: Add docstring to idlelib.__init__ with 'private' warning.

--- Lib/idlelib/__init__.py
+++ Lib/idlelib/__init__.py
@@ -1,1 +1,8 @@
-# Dummy file to make this a package.
+"""The idlelib package implements the Idle application.
+
+Idle includes an interactive shell and editor.
+Use the files named idle.* to start Idle.
+
+The other files are private implementations.  Their details are subject to
+change.  See PEP 434 for more.  Import them at your own risk.
+"""
