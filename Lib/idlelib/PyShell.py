ca33d56f005589967d5b3d77aa807270d5c376ed
Terry Jan Reedy <tjreedy@udel.edu>
2015-09-22 21:10:22 -0400

    Issue #24570: Right-click for context menus now work on Mac Aqual also. Patch by Mark Roseman.

--- Lib/idlelib/PyShell.py
+++ Lib/idlelib/PyShell.py
@@ -1556,6 +1556,14 @@ def main():
     root.withdraw()
     flist = PyShellFileList(root)
     macosxSupport.setupApp(root, flist)
+    
+    if macosxSupport.isAquaTk():
+        # There are some screwed up <2> class bindings for text
+        # widgets defined in Tk which we need to do away with.
+        # See issue #24801.
+        root.unbind_class('Text', '<B2>')
+        root.unbind_class('Text', '<B2-Motion>')
+        root.unbind_class('Text', '<<PasteSelection>>')
 
     if enable_edit:
         if not (cmd or script):
