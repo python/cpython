ed887de7a33fea77fe56f0cd1f723673666eebab
Terry Jan Reedy <tjreedy@udel.edu>
2015-09-25 22:22:55 -0400

    Issue #25173: Replace 'master' with 'parent' in tkinter.messagebox calls. This associates the message box with the widget and is better for Mac OSX. Patch by Mark Roseman.

--- Lib/idlelib/run.py
+++ Lib/idlelib/run.py
@@ -174,7 +174,7 @@ def show_socket_error(err, address):
         tkMessageBox.showerror("IDLE Subprocess Error", msg, parent=root)
     else:
         tkMessageBox.showerror("IDLE Subprocess Error",
-                               "Socket Error: %s" % err.args[1])
+                               "Socket Error: %s" % err.args[1], parent=root)
     root.destroy()
 
 def print_exception():
