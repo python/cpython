a4005b1437c54d96f45df1b6b33ccb2489d59c2f
Victor Stinner <victor.stinner@gmail.com>
2015-09-29 13:47:15 +0200

    test

--- Lib/test/libregrtest/main.py
+++ Lib/test/libregrtest/main.py
@@ -246,9 +246,7 @@ def main(tests=None, **kwargs):
         random.shuffle(selected)
     if ns.trace:
         import trace, tempfile
-        tracer = trace.Trace(ignoredirs=[sys.base_prefix, sys.base_exec_prefix,
-                                         tempfile.gettempdir()],
-                             trace=False, count=True)
+        tracer = trace.Trace(trace=False, count=True)
 
     test_times = []
     support.verbose = ns.verbose      # Tell tests to be moderately quiet
