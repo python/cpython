7c6420a552eb540e03f0d6c75f5a0557803c3850
Martin Panter <vadmium+py@gmail.com>
2015-10-10 11:04:44 +0000

    Issue #25322: Fix test for nested contextlib.suppress

--- Lib/test/test_contextlib.py
+++ Lib/test/test_contextlib.py
@@ -828,9 +828,11 @@ class TestSuppress(unittest.TestCase):
         with ignore_exceptions:
             len(5)
         with ignore_exceptions:
-            1/0
             with ignore_exceptions: # Check nested usage
                 len(5)
+            outer_continued = True
+            1/0
+        self.assertTrue(outer_continued)
 
 if __name__ == "__main__":
     unittest.main()
