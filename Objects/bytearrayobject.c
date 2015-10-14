7bf8bc4b3ac69b0c70ce0112f75c4e7da2a2afbd
Victor Stinner <victor.stinner@gmail.com>
2015-10-14 11:59:46 +0200

    Issue #25401: Remove now unused hex_digit_to_int() function

--- Objects/bytearrayobject.c
+++ Objects/bytearrayobject.c
@@ -2789,22 +2789,6 @@ bytearray_splitlines_impl(PyByteArrayObject *self, int keepends)
         );
 }
 
-static int
-hex_digit_to_int(Py_UCS4 c)
-{
-    if (c >= 128)
-        return -1;
-    if (Py_ISDIGIT(c))
-        return c - '0';
-    else {
-        if (Py_ISUPPER(c))
-            c = Py_TOLOWER(c);
-        if (c >= 'a' && c <= 'f')
-            return c - 'a' + 10;
-    }
-    return -1;
-}
-
 /*[clinic input]
 @classmethod
 bytearray.fromhex
