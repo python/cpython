2f9945fad9e3685abcbf97f5f4a3424a29de42e6
Zachary Ware <zachary.ware@gmail.com>
2015-10-24 23:22:16 -0500

    Issue #25456: Fix test_idle when Tcl/Tk DLLs are loaded from python.exe's home rather than from elsewhere on PATH.

--- Lib/test/test_idle.py
+++ Lib/test/test_idle.py
@@ -1,11 +1,13 @@
 import unittest
-from test import support
-from test.support import import_module
+from test.support import import_module, import_fresh_module
 
 # Skip test if _thread or _tkinter wasn't built or idlelib was deleted.
 import_module('threading')  # imported by PyShell, imports _thread
 tk = import_module('tkinter')  # imports _tkinter
 idletest = import_module('idlelib.idle_test')
+# Make sure TCL_LIBRARY is set properly on Windows.  Note that this will
+# cause a warning about test_idle modifying the environment
+import_fresh_module('tkinter._fix')
 
 # Without test_main present, regrtest.runtest_inner (line1219) calls
 # unittest.TestLoader().loadTestsFromModule(this_module) which calls
