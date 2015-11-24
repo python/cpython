d2f0726f86d5d2b2e88100996d765d6b4d37d2c9
Raymond Hettinger <python@rcn.com>
2015-11-23 21:00:45 -0800

    Fix non-ascii character

--- Lib/collections.py
+++ Lib/collections.py
@@ -1,5 +1,5 @@
 '''This module implements specialized container datatypes providing
-alternatives to Python’s general purpose built-in containers, dict,
+alternatives to Python's general purpose built-in containers, dict,
 list, set, and tuple.
 
 * namedtuple   factory function for creating tuple subclasses with named fields
