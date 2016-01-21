1538b3d3dfff284c8976f19c5e475b99717bacd8
Senthil Kumaran <senthil@uthcode.com>
2016-01-21 09:37:28 -0800

    issue25909 - Correct the documentation of PyMapping_Items, PyMapping_Keys and PyMapping_Values in Include/abstract.h and Doc/c-api/mapping.rst.
    Patch contributed by Sonali Gupta.

--- Include/abstract.h
+++ Include/abstract.h
@@ -1216,23 +1216,23 @@ xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx*/
      PyAPI_FUNC(PyObject *) PyMapping_Keys(PyObject *o);
 
        /*
-     On success, return a list or tuple of the keys in object o.
-     On failure, return NULL.
+     On success, return a list, a tuple or a dictionary view in case of a dict,
+     of the keys in object o. On failure, return NULL.
        */
 
      PyAPI_FUNC(PyObject *) PyMapping_Values(PyObject *o);
 
        /*
-     On success, return a list or tuple of the values in object o.
-     On failure, return NULL.
+     On success, return a list, a tuple or a dictionary view in case of a dict,
+     of the values in object o. On failure, return NULL.
        */
 
      PyAPI_FUNC(PyObject *) PyMapping_Items(PyObject *o);
 
        /*
-     On success, return a list or tuple of the items in object o,
-     where each item is a tuple containing a key-value pair.
-     On failure, return NULL.
+     On success, return a list, a tuple or a dictionary view in case of a dict,
+     of the items in object o, where each item is a tuple containing a key-value
+     pair. On failure, return NULL.
 
        */
 
