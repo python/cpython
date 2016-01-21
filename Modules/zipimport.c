64ea192b73e39e877d8b39ce6584fa580eb0e9b4
Benjamin Peterson <benjamin@python.org>
2016-01-20 22:23:44 -0800

    prevent buffer overflow in get_data (closes #26171)

--- Modules/zipimport.c
+++ Modules/zipimport.c
@@ -895,6 +895,11 @@ get_data(char *archive, PyObject *toc_entry)
         PyMarshal_ReadShortFromFile(fp);        /* local header size */
     file_offset += l;           /* Start of file data */
 
+    if (data_size > LONG_MAX - 1) {
+        fclose(fp);
+        PyErr_NoMemory();
+        return NULL;
+    }
     raw_data = PyString_FromStringAndSize((char *)NULL, compress == 0 ?
                                           data_size : data_size + 1);
     if (raw_data == NULL) {
