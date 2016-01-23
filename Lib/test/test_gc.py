843a1fb1e78bf2d374daccaaa5c616f63afdf053
Victor Stinner <victor.stinner@gmail.com>
2016-01-23 13:29:02 +0100

    test_gc: remove unused imports

--- Lib/test/test_gc.py
+++ Lib/test/test_gc.py
@@ -682,7 +682,6 @@ class GCTests(unittest.TestCase):
         # Create a reference cycle through the __main__ module and check
         # it gets collected at interpreter shutdown.
         code = """if 1:
-            import weakref
             class C:
                 def __del__(self):
                     print('__del__ called')
@@ -696,7 +695,6 @@ class GCTests(unittest.TestCase):
         # Same as above, but with a non-__main__ module.
         with temp_dir() as script_dir:
             module = """if 1:
-                import weakref
                 class C:
                     def __del__(self):
                         print('__del__ called')
