4df0699ed696b580ae76f8a907b0df48e29c88de
Steve Dower <steve.dower@microsoft.com>
2016-03-08 10:26:52 -0800

    Issue #26465: Update Windows builds to use OpenSSL 1.0.2g.

--- PCbuild/prepare_ssl.bat
+++ PCbuild/prepare_ssl.bat
@@ -1,6 +1,6 @@
 @echo off
 if not defined HOST_PYTHON (
-  if %1 EQU Debug (
+  if "%1" EQU "Debug" (
     shift
     set HOST_PYTHON=python_d.exe
     if not exist python35_d.dll exit 1
@@ -9,4 +9,4 @@ if not defined HOST_PYTHON (
     if not exist python35.dll exit 1
   )
 )
-%HOST_PYTHON% prepare_ssl.py %1
+%HOST_PYTHON% "%~dp0prepare_ssl.py" %1
