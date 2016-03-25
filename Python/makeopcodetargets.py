0ef927e573dec897e826d60935eb594b3515cd22
Victor Stinner <victor.stinner@gmail.com>
2016-03-25 11:54:47 +0100

    Modernize Python/makeopcodetargets.py
    * Simply use "import opcode" to import the opcode module instead of tricks
      using the imp module
    * Use context manager for the output file
    * Move code into a new main() function
    * Replace assert with a regular if to check the number of arguments
    * Import modules at top level

--- Python/makeopcodetargets.py
+++ Python/makeopcodetargets.py
@@ -3,24 +3,14 @@
 (for compilers supporting computed gotos or "labels-as-values", such as gcc).
 """
 
-# This code should stay compatible with Python 2.3, at least while
-# some of the buildbots have Python 2.3 as their system Python.
-
-import imp
+import opcode
 import os
+import sys
 
-
-def find_module(modname):
-    """Finds and returns a module in the local dist/checkout.
-    """
-    modpath = os.path.join(
-        os.path.dirname(os.path.dirname(__file__)), "Lib")
-    return imp.load_module(modname, *imp.find_module(modname, [modpath]))
 
 def write_contents(f):
     """Write C code contents to the target file object.
     """
-    opcode = find_module("opcode")
     targets = ['_unknown_opcode'] * 256
     for opname, op in opcode.opmap.items():
         targets[op] = "TARGET_%s" % opname
@@ -29,15 +19,17 @@ def write_contents(f):
     f.write("\n};\n")
 
 
-if __name__ == "__main__":
-    import sys
-    assert len(sys.argv) < 3, "Too many arguments"
+def main():
+    if len(sys.argv) >= 3:
+        sys.exit("Too many arguments")
     if len(sys.argv) == 2:
         target = sys.argv[1]
     else:
         target = "Python/opcode_targets.h"
-    f = open(target, "w")
-    try:
+    with open(target, "w") as f:
         write_contents(f)
-    finally:
-        f.close()
+    print("Jump table written into %s" % target)
+
+
+if __name__ == "__main__":
+    main()
