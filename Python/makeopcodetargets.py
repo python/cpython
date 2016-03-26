03e4f4171772a3638cc1598e86ca823d373fe9ef
Victor Stinner <victor.stinner@gmail.com>
2016-03-26 01:04:37 +0100

    makeopcodetargets.py: we need to import Lib/opcode.py
    Issue #20021: use importlib.machinery to import Lib/opcode.py and not an opcode
    module coming from somewhere else. makeopcodetargets.py is part of the Python
    build process and it is run by an external Python program, not the built Python
    program.
    
    Patch written by Serhiy Storchaka.

--- Python/makeopcodetargets.py
+++ Python/makeopcodetargets.py
@@ -3,14 +3,34 @@
 (for compilers supporting computed gotos or "labels-as-values", such as gcc).
 """
 
-import opcode
 import os
 import sys
+
+
+try:
+    from importlib.machinery import SourceFileLoader
+except ImportError:
+    import imp
+
+    def find_module(modname):
+        """Finds and returns a module in the local dist/checkout.
+        """
+        modpath = os.path.join(
+            os.path.dirname(os.path.dirname(__file__)), "Lib")
+        return imp.load_module(modname, *imp.find_module(modname, [modpath]))
+else:
+    def find_module(modname):
+        """Finds and returns a module in the local dist/checkout.
+        """
+        modpath = os.path.join(
+            os.path.dirname(os.path.dirname(__file__)), "Lib", modname + ".py")
+        return SourceFileLoader(modname, modpath).load_module()
 
 
 def write_contents(f):
     """Write C code contents to the target file object.
     """
+    opcode = find_module('opcode')
     targets = ['_unknown_opcode'] * 256
     for opname, op in opcode.opmap.items():
         targets[op] = "TARGET_%s" % opname
