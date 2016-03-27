ce913877e42b7fa03434c2e765ace891e0f5c4dc
Berker Peksag <berker.peksag@gmail.com>
2016-03-28 00:30:02 +0300

    Issue #25195: Fix a regression in mock.MagicMock
    _Call is a subclass of tuple (changeset 3603bae63c13 only works
    for classes) so we need to implement __ne__ ourselves.
    
    Patch by Andrew Plummer.

--- Lib/unittest/mock.py
+++ Lib/unittest/mock.py
@@ -2029,6 +2029,9 @@ class _Call(tuple):
         return (other_args, other_kwargs) == (self_args, self_kwargs)
 
 
+    __ne__ = object.__ne__
+
+
     def __call__(self, *args, **kwargs):
         if self.name is None:
             return _Call(('', args, kwargs), name='()')
