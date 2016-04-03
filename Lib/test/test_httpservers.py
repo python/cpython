44b23adc6139d8c073978c3f0b3859a94b3da634
Martin Panter <vadmium+py@gmail.com>
2016-04-03 01:28:53 +0000

    Issue #26586: Simple enhancements to BaseHTTPRequestHandler by Xiang Zhang

--- Lib/test/test_httpservers.py
+++ Lib/test/test_httpservers.py
@@ -855,7 +855,7 @@ class BaseHTTPRequestHandlerTestCase(unittest.TestCase):
         # Issue #6791: same for headers
         result = self.send_typical_request(
             b'GET / HTTP/1.1\r\nX-Foo: bar' + b'r' * 65537 + b'\r\n\r\n')
-        self.assertEqual(result[0], b'HTTP/1.1 400 Line too long\r\n')
+        self.assertEqual(result[0], b'HTTP/1.1 431 Line too long\r\n')
         self.assertFalse(self.handler.get_called)
         self.assertEqual(self.handler.requestline, 'GET / HTTP/1.1')
 
