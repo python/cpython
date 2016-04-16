5beda80fe9a6ad0d75f5158ebb951622cf2069ea
Martin Panter <vadmium+py@gmail.com>
2016-04-16 09:28:57 +0000

    Fix typos in code comments and documentation

--- Lib/posixpath.py
+++ Lib/posixpath.py
@@ -372,7 +372,7 @@ symbolic links encountered in the path."""
     path, ok = _joinrealpath(filename[:0], filename, {})
     return abspath(path)
 
-# Join two paths, normalizing ang eliminating any symbolic links
+# Join two paths, normalizing and eliminating any symbolic links
 # encountered in the second path.
 def _joinrealpath(path, rest, seen):
     if isinstance(path, bytes):
