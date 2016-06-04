f60574f782f76757f442e3a80eac3d4708aa4230
Martin Panter <vadmium+py@gmail.com>
2016-06-04 04:57:19 +0000

    Fix typos in code comment and documentation

--- Lib/asyncio/locks.py
+++ Lib/asyncio/locks.py
@@ -111,7 +111,7 @@ class Lock(_ContextManagerMixin):
     acquire() is a coroutine and should be called with 'yield from'.
 
     Locks also support the context management protocol.  '(yield from lock)'
-    should be used as context manager expression.
+    should be used as the context manager expression.
 
     Usage:
 
