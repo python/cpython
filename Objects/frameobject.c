d37781eb2ea9263c846286117170c7b195c31076
Serhiy Storchaka <storchaka@gmail.com>
2016-06-04 20:30:43 +0300

    Issue #20041: Fixed TypeError when frame.f_trace is set to None. Patch by Xavier de Gaye.

--- Objects/frameobject.c
+++ Objects/frameobject.c
@@ -364,15 +364,13 @@ frame_gettrace(PyFrameObject *f, void *closure)
 static int
 frame_settrace(PyFrameObject *f, PyObject* v, void *closure)
 {
-    PyObject* old_value;
-
     /* We rely on f_lineno being accurate when f_trace is set. */
     f->f_lineno = PyFrame_GetLineNumber(f);
 
-    old_value = f->f_trace;
+    if (v == Py_None)
+        v = NULL;
     Py_XINCREF(v);
-    f->f_trace = v;
-    Py_XDECREF(old_value);
+    Py_XSETREF(f->f_trace, v);
 
     return 0;
 }
