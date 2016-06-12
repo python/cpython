50c7e056595aadc25c5aa483f94384c30584a233
Serhiy Storchaka <storchaka@gmail.com>
2016-06-12 15:45:14 +0300

    Issue #25455: Clean up reference loops created in tests for recursive functools.partial objects.

--- Lib/test/test_functools.py
+++ Lib/test/test_functools.py
@@ -239,21 +239,36 @@ class TestPartial(unittest.TestCase):
     def test_recursive_pickle(self):
         f = self.partial(capture)
         f.__setstate__((f, (), {}, {}))
-        for proto in range(pickle.HIGHEST_PROTOCOL + 1):
-            with self.assertRaises(RuntimeError):
-                pickle.dumps(f, proto)
+        try:
+            for proto in range(pickle.HIGHEST_PROTOCOL + 1):
+                with self.assertRaises(RuntimeError):
+                    pickle.dumps(f, proto)
+        finally:
+            f.__setstate__((capture, (), {}, {}))
 
         f = self.partial(capture)
         f.__setstate__((capture, (f,), {}, {}))
-        for proto in range(pickle.HIGHEST_PROTOCOL + 1):
-            f_copy = pickle.loads(pickle.dumps(f, proto))
-            self.assertIs(f_copy.args[0], f_copy)
+        try:
+            for proto in range(pickle.HIGHEST_PROTOCOL + 1):
+                f_copy = pickle.loads(pickle.dumps(f, proto))
+                try:
+                    self.assertIs(f_copy.args[0], f_copy)
+                finally:
+                    f_copy.__setstate__((capture, (), {}, {}))
+        finally:
+            f.__setstate__((capture, (), {}, {}))
 
         f = self.partial(capture)
         f.__setstate__((capture, (), {'a': f}, {}))
-        for proto in range(pickle.HIGHEST_PROTOCOL + 1):
-            f_copy = pickle.loads(pickle.dumps(f, proto))
-            self.assertIs(f_copy.keywords['a'], f_copy)
+        try:
+            for proto in range(pickle.HIGHEST_PROTOCOL + 1):
+                f_copy = pickle.loads(pickle.dumps(f, proto))
+                try:
+                    self.assertIs(f_copy.keywords['a'], f_copy)
+                finally:
+                    f_copy.__setstate__((capture, (), {}, {}))
+        finally:
+            f.__setstate__((capture, (), {}, {}))
 
     # Issue 6083: Reference counting bug
     def test_setstate_refcount(self):
