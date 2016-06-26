d6de6f4a913f60e8e8c1055de41ea089556da511
Terry Jan Reedy <tjreedy@udel.edu>
2016-06-26 17:48:02 -0400

    Issue 27372: Stop test_idle from changing locale, so test passes. In 3.6, the warning is now called an error, making it harder to ignore.

--- Lib/idlelib/__init__.py
+++ Lib/idlelib/__init__.py
@@ -7,3 +7,4 @@ Use the files named idle.* to start Idle.
 The other files are private implementations.  Their details are subject to
 change.  See PEP 434 for more.  Import them at your own risk.
 """
+testing = False  # Set True by test.test_idle.
