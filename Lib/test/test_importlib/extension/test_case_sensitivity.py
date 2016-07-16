7ade4533fed6f0b5d457a14d063fe0d3b18515ad
Brett Cannon <brett@python.org>
2016-07-16 10:44:13 -0700

    Fix regressions introduced by fixes for issue #27083.

--- Lib/test/test_importlib/extension/test_case_sensitivity.py
+++ Lib/test/test_importlib/extension/test_case_sensitivity.py
@@ -5,6 +5,7 @@ import unittest
 
 from .. import util
 
+importlib = util.import_importlib('importlib')
 machinery = util.import_importlib('importlib.machinery')
 
 
@@ -12,7 +13,7 @@ machinery = util.import_importlib('importlib.machinery')
 
 @unittest.skipIf(util.EXTENSIONS.filename is None, '_testcapi not available')
 @util.case_insensitive_tests
-class ExtensionModuleCaseSensitivityTest:
+class ExtensionModuleCaseSensitivityTest(util.CASEOKTestBase):
 
     def find_module(self):
         good_name = util.EXTENSIONS.name
@@ -26,25 +27,22 @@ class ExtensionModuleCaseSensitivityTest:
     def test_case_sensitive(self):
         with support.EnvironmentVarGuard() as env:
             env.unset('PYTHONCASEOK')
-            if b'PYTHONCASEOK' in _bootstrap_external._os.environ:
-                self.skipTest('os.environ changes not reflected in '
-                              '_os.environ')
+            self.caseok_env_changed(should_exist=False)
             loader = self.find_module()
             self.assertIsNone(loader)
 
     def test_case_insensitivity(self):
         with support.EnvironmentVarGuard() as env:
             env.set('PYTHONCASEOK', '1')
-            if b'PYTHONCASEOK' not in _bootstrap_external._os.environ:
-                self.skipTest('os.environ changes not reflected in '
-                              '_os.environ')
+            self.caseok_env_changed(should_exist=True)
             loader = self.find_module()
             self.assertTrue(hasattr(loader, 'load_module'))
 
 
 (Frozen_ExtensionCaseSensitivity,
  Source_ExtensionCaseSensitivity
- ) = util.test_both(ExtensionModuleCaseSensitivityTest, machinery=machinery)
+ ) = util.test_both(ExtensionModuleCaseSensitivityTest, importlib=importlib,
+                    machinery=machinery)
 
 
 if __name__ == '__main__':
