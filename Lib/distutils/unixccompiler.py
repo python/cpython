2246f39c4002d1e2398e60a459a0cd320d89f59e
Stefan Krah <skrah@bytereef.org>
2016-08-03 11:23:31 +0200

    Issue #20767: Fix -R option for FreeBSD/clang.

--- Lib/distutils/unixccompiler.py
+++ Lib/distutils/unixccompiler.py
@@ -230,6 +230,8 @@ class UnixCCompiler(CCompiler):
         if sys.platform[:6] == "darwin":
             # MacOSX's linker doesn't understand the -R flag at all
             return "-L" + dir
+        elif sys.platform[:7] == "freebsd":
+            return "-Wl,-rpath=" + dir
         elif sys.platform[:5] == "hp-ux":
             if self._is_gcc(compiler):
                 return ["-Wl,+s", "-L" + dir]
