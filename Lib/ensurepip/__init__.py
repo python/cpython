285a2537c86947bdf757802b6fcff7ad6af4df5b
Donald Stufft <donald@stufft.io>
2016-08-14 16:08:42 -0400

    Upgrade setuptools/pip to to 25.2.0/8.1.2

--- Lib/ensurepip/__init__.py
+++ Lib/ensurepip/__init__.py
@@ -12,9 +12,9 @@ import tempfile
 __all__ = ["version", "bootstrap"]
 
 
-_SETUPTOOLS_VERSION = "20.10.1"
+_SETUPTOOLS_VERSION = "25.2.0"
 
-_PIP_VERSION = "8.1.1"
+_PIP_VERSION = "8.1.2"
 
 # pip currently requires ssl support, so we try to provide a nicer
 # error message when that is missing (http://bugs.python.org/issue19744)
