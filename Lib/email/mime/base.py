b7cdee1d0bcc739300e489f78af658a6ad6c1393
R David Murray <rdmurray@bitdance.com>
2016-09-07 16:48:35 -0400

    #27331: add policy keyword argument to all MIME subclasses.
    Patch by Berker Peksag.

--- Lib/email/mime/base.py
+++ Lib/email/mime/base.py
@@ -6,6 +6,8 @@
 
 __all__ = ['MIMEBase']
 
+import email.policy
+
 from email import message
 
 
@@ -13,14 +15,16 @@ from email import message
 class MIMEBase(message.Message):
     """Base class for MIME specializations."""
 
-    def __init__(self, _maintype, _subtype, **_params):
+    def __init__(self, _maintype, _subtype, *, policy=None, **_params):
         """This constructor adds a Content-Type: and a MIME-Version: header.
 
         The Content-Type: header is taken from the _maintype and _subtype
         arguments.  Additional parameters for this header are taken from the
         keyword arguments.
         """
-        message.Message.__init__(self)
+        if policy is None:
+            policy = email.policy.compat32
+        message.Message.__init__(self, policy=policy)
         ctype = '%s/%s' % (_maintype, _subtype)
         self.add_header('Content-Type', ctype, **_params)
         self['MIME-Version'] = '1.0'
--- Lib/email/mime/message.py
+++ Lib/email/mime/message.py
@@ -14,7 +14,7 @@ from email.mime.nonmultipart import MIMENonMultipart
 class MIMEMessage(MIMENonMultipart):
     """Class representing message/* MIME documents."""
 
-    def __init__(self, _msg, _subtype='rfc822'):
+    def __init__(self, _msg, _subtype='rfc822', *, policy=None):
         """Create a message/* type MIME document.
 
         _msg is a message object and must be an instance of Message, or a
@@ -24,7 +24,7 @@ class MIMEMessage(MIMENonMultipart):
         default is "rfc822" (this is defined by the MIME standard, even though
         the term "rfc822" is technically outdated by RFC 2822).
         """
-        MIMENonMultipart.__init__(self, 'message', _subtype)
+        MIMENonMultipart.__init__(self, 'message', _subtype, policy=policy)
         if not isinstance(_msg, message.Message):
             raise TypeError('Argument is not an instance of Message')
         # It's convenient to use this base class method.  We need to do it
