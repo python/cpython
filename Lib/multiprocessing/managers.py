280290adb8dc3d2337f5b2bc25b824d93a9434e8
Davin Potts <python@discontinuity.net>
2016-09-08 13:07:13 -0500

    Issue #21201: Improves readability of multiprocessing error message from server to client for certain exceptions

--- Lib/multiprocessing/managers.py
+++ Lib/multiprocessing/managers.py
@@ -287,7 +287,7 @@ class Server(object):
                 try:
                     send(msg)
                 except Exception, e:
-                    send(('#UNSERIALIZABLE', repr(msg)))
+                    send(('#UNSERIALIZABLE', format_exc()))
             except Exception, e:
                 util.info('exception in thread serving %r',
                         threading.current_thread().name)
