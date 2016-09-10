3b24d1ba08f93c0f561c58fe44dac97f3e262ee8
Gregory P. Smith ext:(%20%5BGoogle%20Inc.%5D) <greg@krypto.org>
2016-09-09 18:18:52 -0700

    Issue #25969: Update the lib2to3 grammar to handle the unpacking generalizations added in 3.5.

--- Lib/lib2to3/fixes/fix_intern.py
+++ Lib/lib2to3/fixes/fix_intern.py
@@ -25,6 +25,16 @@ class FixIntern(fixer_base.BaseFix):
     """
 
     def transform(self, node, results):
+        if results:
+            # I feel like we should be able to express this logic in the
+            # PATTERN above but I don't know how to do it so...
+            obj = results['obj']
+            if obj:
+                if obj.type == self.syms.star_expr:
+                    return  # Make no change.
+                if (obj.type == self.syms.argument and
+                    obj.children[0].value == '**'):
+                    return  # Make no change.
         names = ('sys', 'intern')
         new = ImportAndCall(node, results, names)
         touch_import(None, 'sys', node)
