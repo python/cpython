feb7edc153465fca68a8551081f2beb4cbb26d96
Benjamin Peterson <benjamin@python.org>
2016-09-09 18:09:52 -0700

    dummy dtrace probes are a good place to use inline functions

--- Include/pydtrace.h
+++ Include/pydtrace.h
@@ -22,25 +22,25 @@
 
 /* Without DTrace, compile to nothing. */
 
-#define PyDTrace_LINE(arg0, arg1, arg2) do ; while (0)
-#define PyDTrace_FUNCTION_ENTRY(arg0, arg1, arg2)  do ; while (0)
-#define PyDTrace_FUNCTION_RETURN(arg0, arg1, arg2) do ; while (0)
-#define PyDTrace_GC_START(arg0)               do ; while (0)
-#define PyDTrace_GC_DONE(arg0)                do ; while (0)
-#define PyDTrace_INSTANCE_NEW_START(arg0)     do ; while (0)
-#define PyDTrace_INSTANCE_NEW_DONE(arg0)      do ; while (0)
-#define PyDTrace_INSTANCE_DELETE_START(arg0)  do ; while (0)
-#define PyDTrace_INSTANCE_DELETE_DONE(arg0)   do ; while (0)
+inline void PyDTrace_LINE(const char *arg0, const char *arg1, int arg2) {}
+inline void PyDTrace_FUNCTION_ENTRY(const char *arg0, const char *arg1, int arg2)  {}
+inline void PyDTrace_FUNCTION_RETURN(const char *arg0, const char *arg1, int arg2) {}
+inline void PyDTrace_GC_START(int arg0) {}
+inline void PyDTrace_GC_DONE(int arg0) {}
+inline void PyDTrace_INSTANCE_NEW_START(int arg0) {}
+inline void PyDTrace_INSTANCE_NEW_DONE(int arg0) {}
+inline void PyDTrace_INSTANCE_DELETE_START(int arg0) {}
+inline void PyDTrace_INSTANCE_DELETE_DONE(int arg0) {}
 
-#define PyDTrace_LINE_ENABLED()                  (0)
-#define PyDTrace_FUNCTION_ENTRY_ENABLED()        (0)
-#define PyDTrace_FUNCTION_RETURN_ENABLED()       (0)
-#define PyDTrace_GC_START_ENABLED()              (0)
-#define PyDTrace_GC_DONE_ENABLED()               (0)
-#define PyDTrace_INSTANCE_NEW_START_ENABLED()    (0)
-#define PyDTrace_INSTANCE_NEW_DONE_ENABLED()     (0)
-#define PyDTrace_INSTANCE_DELETE_START_ENABLED() (0)
-#define PyDTrace_INSTANCE_DELETE_DONE_ENABLED()  (0)
+inline int PyDTrace_LINE_ENABLED(void) { return 0; }
+inline int PyDTrace_FUNCTION_ENTRY_ENABLED(void) { return 0; }
+inline int PyDTrace_FUNCTION_RETURN_ENABLED(void) { return 0; }
+inline int PyDTrace_GC_START_ENABLED(void) { return 0; }
+inline int PyDTrace_GC_DONE_ENABLED(void) { return 0; }
+inline int PyDTrace_INSTANCE_NEW_START_ENABLED(void) { return 0; }
+inline int PyDTrace_INSTANCE_NEW_DONE_ENABLED(void) { return 0; }
+inline int PyDTrace_INSTANCE_DELETE_START_ENABLED(void) { return 0; }
+inline int PyDTrace_INSTANCE_DELETE_DONE_ENABLED(void) { return 0; }
 
 #endif /* !WITH_DTRACE */
 
