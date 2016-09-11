d7296a1a014e79ea6cbd4e0565d936d1f826f41a
Xavier de Gaye <xdegaye@users.sourceforge.net>
2016-09-11 22:22:24 +0200

    Issue #28046: get_sysconfigdata_name() uses the _PYTHON_SYSCONFIGDATA_NAME environment variable that is defined when cross-compiling.

--- Lib/distutils/sysconfig.py
+++ Lib/distutils/sysconfig.py
@@ -418,11 +418,12 @@ _config_vars = None
 def _init_posix():
     """Initialize the module as appropriate for POSIX systems."""
     # _sysconfigdata is generated at build time, see the sysconfig module
-    name = '_sysconfigdata_{abi}_{platform}_{multiarch}'.format(
+    name = os.environ.get('_PYTHON_SYSCONFIGDATA_NAME',
+        '_sysconfigdata_{abi}_{platform}_{multiarch}'.format(
         abi=sys.abiflags,
         platform=sys.platform,
         multiarch=getattr(sys.implementation, '_multiarch', ''),
-    )
+    ))
     _temp = __import__(name, globals(), locals(), ['build_time_vars'], 0)
     build_time_vars = _temp.build_time_vars
     global _config_vars
