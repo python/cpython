3522183291b2b62619c097f53ca6f8e5b266744f
Raymond Hettinger <python@rcn.com>
2016-09-11 23:18:03 -0700

    Issue #17582: xml.etree.ElementTree nows preserves whitespaces in attributes (Patch by Duane Griffin.  Reviewed and approved by Stefan Behnel.)

--- Lib/xml/etree/ElementTree.py
+++ Lib/xml/etree/ElementTree.py
@@ -1083,8 +1083,19 @@ def _escape_attrib(text):
             text = text.replace(">", "&gt;")
         if "\"" in text:
             text = text.replace("\"", "&quot;")
+        # The following business with carriage returns is to satisfy
+        # Section 2.11 of the XML specification, stating that 
+        # CR or CR LN should be replaced with just LN
+        # http://www.w3.org/TR/REC-xml/#sec-line-ends
+        if "\r\n" in text:
+            text = text.replace("\r\n", "\n")
+        if "\r" in text:
+            text = text.replace("\r", "\n")
+        #The following four lines are issue 17582
         if "\n" in text:
             text = text.replace("\n", "&#10;")
+        if "\t" in text:
+            text = text.replace("\t", "&#09;")
         return text
     except (TypeError, AttributeError):
         _raise_serialization_error(text)
