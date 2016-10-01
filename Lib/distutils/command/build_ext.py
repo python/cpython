1a95031b10a7af02993a8c68208523a87a9d79d4
Zachary Ware <zachary.ware@gmail.com>
2016-10-01 16:15:09 -0500

    Issue #13756: Fix building extensions modules on Cygwin
    Patch by Roumen Petrov, based on original patch by Jason Tishler.

--- Lib/distutils/command/build_ext.py
+++ Lib/distutils/command/build_ext.py
@@ -715,13 +715,6 @@ class build_ext(Command):
                 return ext.libraries + [pythonlib]
             else:
                 return ext.libraries
-        elif sys.platform[:6] == "cygwin":
-            template = "python%d.%d"
-            pythonlib = (template %
-                   (sys.hexversion >> 24, (sys.hexversion >> 16) & 0xff))
-            # don't extend ext.libraries, it may be shared with other
-            # extensions, it is a reference to the original list
-            return ext.libraries + [pythonlib]
         elif sys.platform[:6] == "atheos":
             from distutils import sysconfig
 
