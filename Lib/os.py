b42c69dbb9bce00f0803656d1c77f5be0b54e4fa
Ned Deily <nad@python.org>
2016-10-01 21:12:35 -0400

    Issue #28324: Remove vestigal MacOS 9 references in os.py docstring. Patch by Chi Hsuan Yen.

--- Lib/os.py
+++ Lib/os.py
@@ -4,9 +4,9 @@ This exports:
   - all functions from posix or nt, e.g. unlink, stat, etc.
   - os.path is either posixpath or ntpath
   - os.name is either 'posix' or 'nt'
-  - os.curdir is a string representing the current directory ('.' or ':')
-  - os.pardir is a string representing the parent directory ('..' or '::')
-  - os.sep is the (or a most common) pathname separator ('/' or ':' or '\\')
+  - os.curdir is a string representing the current directory (always '.')
+  - os.pardir is a string representing the parent directory (always '..')
+  - os.sep is the (or a most common) pathname separator ('/' or '\\')
   - os.extsep is the extension separator (always '.')
   - os.altsep is the alternate pathname separator (None or '/')
   - os.pathsep is the component separator used in $PATH etc
