90be06bd9038561b11b9ef509067d25d1dc18dee
Serhiy Storchaka <storchaka@gmail.com>
2016-10-02 10:33:46 +0300

    Issue #28257: Improved error message when pass a non-iterable as a var-positional argument.  Added opcode BUILD_TUPLE_UNPACK_WITH_CALL.

--- Include/opcode.h
+++ Include/opcode.h
@@ -125,6 +125,7 @@ extern "C" {
 #define FORMAT_VALUE            155
 #define BUILD_CONST_KEY_MAP     156
 #define BUILD_STRING            157
+#define BUILD_TUPLE_UNPACK_WITH_CALL 158
 
 /* EXCEPT_HANDLER is a special, implicit block type which is created when
    entering an except handler. It is not an opcode but we define it here
