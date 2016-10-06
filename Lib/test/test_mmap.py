6986995c25a4c0f758ab96ec90d565ece7d010b2
Benjamin Peterson <benjamin@python.org>
2016-10-05 22:00:05 -0700

    skip test if resizing is not supported

--- Lib/test/test_mmap.py
+++ Lib/test/test_mmap.py
@@ -717,7 +717,10 @@ class MmapTests(unittest.TestCase):
         m = mmap.mmap(-1, 8192)
         self.addCleanup(m.close)
         m.read(5000)
-        m.resize(4096)
+        try:
+            m.resize(4096)
+        except SystemError:
+            self.skipTest("resizing not supported")
         self.assertEqual(m.read(14), b'')
         self.assertRaises(ValueError, m.read_byte,)
         self.assertRaises(ValueError, m.write_byte, 42)
