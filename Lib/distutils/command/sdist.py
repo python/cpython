7677ff209c9e4d59aeb2773ada1b7684a7b68dbb
Jason R. Coombs <jaraco@jaraco.com>
2016-10-14 14:08:28 -0400

    Remove unused import and reorganize imports of modules.

--- Lib/distutils/command/sdist.py
+++ Lib/distutils/command/sdist.py
@@ -8,7 +8,9 @@ from glob import glob
 from warnings import warn
 
 from distutils.core import Command
-from distutils import dir_util, dep_util, file_util, archive_util
+from distutils import dir_util
+from distutils import file_util
+from distutils import archive_util
 from distutils.text_file import TextFile
 from distutils.filelist import FileList
 from distutils import log
