c9bba2bcc05ac019d916995894cc471914f94506
Jason R. Coombs <jaraco@jaraco.com>
2016-10-14 14:06:28 -0400

    Remove wildcard imports from distutils.command.sdist

--- Lib/distutils/command/sdist.py
+++ Lib/distutils/command/sdist.py
@@ -4,17 +4,17 @@ Implements the Distutils 'sdist' command (create a source distribution)."""
 
 import os
 import sys
-from types import *
 from glob import glob
 from warnings import warn
 
 from distutils.core import Command
 from distutils import dir_util, dep_util, file_util, archive_util
 from distutils.text_file import TextFile
-from distutils.errors import *
 from distutils.filelist import FileList
 from distutils import log
 from distutils.util import convert_path
+from distutils.errors import DistutilsTemplateError, DistutilsOptionError
+
 
 def show_formats():
     """Print all possible values for the 'formats' option (used by
