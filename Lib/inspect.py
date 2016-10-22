52e5004b59d89e5f1bf2ceaba0d2c9e63368454b
Guido van Rossum <guido@python.org>
2016-10-22 07:55:18 -0700

    Issue #27989: Tweak inspect.formatannotation() to improve pydoc rendering of function annotations. Ivan L.

--- Lib/inspect.py
+++ Lib/inspect.py
@@ -1152,6 +1152,8 @@ def getargvalues(frame):
     return ArgInfo(args, varargs, varkw, frame.f_locals)
 
 def formatannotation(annotation, base_module=None):
+    if getattr(annotation, '__module__', None) == 'typing':
+        return repr(annotation).replace('typing.', '')
     if isinstance(annotation, type):
         if annotation.__module__ in ('builtins', base_module):
             return annotation.__qualname__
