59addd608ce1f7f85cab045a7dbd389c1dd1746d
Serhiy Storchaka <storchaka@gmail.com>
2016-10-23 22:54:43 +0300

    Some distutils tests require zlib for creating tar.gz source distribution.

--- Lib/distutils/tests/test_bdist_rpm.py
+++ Lib/distutils/tests/test_bdist_rpm.py
@@ -8,6 +8,11 @@ import shutil
 
 from test.test_support import run_unittest
 
+try:
+    import zlib
+except ImportError:
+    zlib = None
+
 from distutils.core import Distribution
 from distutils.command.bdist_rpm import bdist_rpm
 from distutils.tests import support
@@ -44,6 +49,7 @@ class BuildRpmTestCase(support.TempdirManager,
     # spurious sdtout/stderr output under Mac OS X
     @unittest.skipUnless(sys.platform.startswith('linux'),
                          'spurious sdtout/stderr output under Mac OS X')
+    @unittest.skipUnless(zlib, "requires zlib")
     @unittest.skipIf(find_executable('rpm') is None,
                      'the rpm command is not found')
     @unittest.skipIf(find_executable('rpmbuild') is None,
@@ -86,6 +92,7 @@ class BuildRpmTestCase(support.TempdirManager,
     # spurious sdtout/stderr output under Mac OS X
     @unittest.skipUnless(sys.platform.startswith('linux'),
                          'spurious sdtout/stderr output under Mac OS X')
+    @unittest.skipUnless(zlib, "requires zlib")
     # http://bugs.python.org/issue1533164
     @unittest.skipIf(find_executable('rpm') is None,
                      'the rpm command is not found')
