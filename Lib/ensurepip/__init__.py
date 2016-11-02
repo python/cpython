c6c0aa4a3bf4686ecb9ef757583afa0cc365f2f8
Donald Stufft <donald@stufft.io>
2016-11-02 16:01:17 -0400

    Update pip to 9.0.0 and setuptools to 28.7.1

--- Lib/ensurepip/__init__.py
+++ Lib/ensurepip/__init__.py
@@ -12,9 +12,9 @@ import tempfile
 __all__ = ["version", "bootstrap"]
 
 
-_SETUPTOOLS_VERSION = "27.1.2"
+_SETUPTOOLS_VERSION = "28.7.1"
 
-_PIP_VERSION = "8.1.2"
+_PIP_VERSION = "9.0.0"
 
 # pip currently requires ssl support, so we try to provide a nicer
 # error message when that is missing (http://bugs.python.org/issue19744)
