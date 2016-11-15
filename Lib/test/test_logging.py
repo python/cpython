49e8f2d2040d3c15d61a713dcbe14caae41c4f0d
Xavier de Gaye <xdegaye@users.sourceforge.net>
2016-11-15 16:57:52 +0100

    Issue 28668: Skip tests where instanciation of multiprocessing.Queue would raise ImportError

--- Lib/test/test_logging.py
+++ Lib/test/test_logging.py
@@ -3066,6 +3066,7 @@ if hasattr(logging.handlers, 'QueueListener'):
             self.assertEqual(mock_handle.call_count, 5 * self.repeat,
                              'correct number of handled log messages')
 
+        @support.requires_multiprocessing_queue
         @patch.object(logging.handlers.QueueListener, 'handle')
         def test_handle_called_with_mp_queue(self, mock_handle):
             for i in range(self.repeat):
@@ -3082,6 +3083,7 @@ if hasattr(logging.handlers, 'QueueListener'):
             except queue.Empty:
                 return []
 
+        @support.requires_multiprocessing_queue
         def test_no_messages_in_queue_after_stop(self):
             """
             Five messages are logged then the QueueListener is stopped. This
