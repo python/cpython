a5e637603fcc0143bffbe122e03567ef0bd06dce
Serhiy Storchaka <storchaka@gmail.com>
2016-11-22 07:58:08 +0200

    Issue #28761: The fields name and doc of structures PyMemberDef, PyGetSetDef, PyStructSequence_Field, PyStructSequence_Desc, and wrapperbase are now of type "const char *" rather of "char *".

--- Include/structseq.h
+++ Include/structseq.h
@@ -8,13 +8,13 @@ extern "C" {
 #endif
 
 typedef struct PyStructSequence_Field {
-    char *name;
-    char *doc;
+    const char *name;
+    const char *doc;
 } PyStructSequence_Field;
 
 typedef struct PyStructSequence_Desc {
-    char *name;
-    char *doc;
+    const char *name;
+    const char *doc;
     struct PyStructSequence_Field *fields;
     int n_in_sequence;
 } PyStructSequence_Desc;
