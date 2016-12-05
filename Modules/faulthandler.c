e0a7196217ef914de6fea59e73c67c99698676d7
Victor Stinner <victor.stinner@gmail.com>
2016-12-05 17:56:36 +0100

    Issue #28152: Fix -Wunreachable-code warnings on Clang
    Don't declare dead code when the code is declared with Clang.

--- Modules/faulthandler.c
+++ Modules/faulthandler.c
@@ -980,12 +980,21 @@ faulthandler_sigsegv(PyObject *self, PyObject *args)
 static void
 faulthandler_fatal_error_thread(void *plock)
 {
+#ifndef __clang__
     PyThread_type_lock *lock = (PyThread_type_lock *)plock;
+#endif
 
     Py_FatalError("in new thread");
 
+#ifndef __clang__
+    /* Issue #28152: Py_FatalError() is declared with
+       __attribute__((__noreturn__)).  GCC emits a warning without
+       "PyThread_release_lock()" (compiler bug?), but Clang is smarter and
+       emits a warning on the return. */
+
     /* notify the caller that we are done */
     PyThread_release_lock(lock);
+#endif
 }
 
 static PyObject *
