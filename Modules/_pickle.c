55ba38a48097e4b21b406cc617df1481727f5c25
Victor Stinner <victor.stinner@gmail.com>
2016-12-09 16:09:30 +0100

    Use _PyObject_CallMethodIdObjArgs()
    Issue #28915: Replace _PyObject_CallMethodId() with
    _PyObject_CallMethodIdObjArgs() in various modules when the format string was
    only made of "O" formats, PyObject* arguments.
    
    _PyObject_CallMethodIdObjArgs() avoids the creation of a temporary tuple and
    doesn't have to parse a format string.

--- Modules/_pickle.c
+++ Modules/_pickle.c
@@ -4571,8 +4571,8 @@ find_class(UnpicklerObject *self, PyObject *module_name, PyObject *global_name)
 {
     _Py_IDENTIFIER(find_class);
 
-    return _PyObject_CallMethodId((PyObject *)self, &PyId_find_class, "OO",
-                                  module_name, global_name);
+    return _PyObject_CallMethodIdObjArgs((PyObject *)self, &PyId_find_class,
+                                         module_name, global_name, NULL);
 }
 
 static Py_ssize_t
@@ -5184,7 +5184,7 @@ instantiate(PyObject *cls, PyObject *args)
     else {
         _Py_IDENTIFIER(__new__);
 
-        result = _PyObject_CallMethodId(cls, &PyId___new__, "O", cls);
+        result = _PyObject_CallMethodIdObjArgs(cls, &PyId___new__, cls, NULL);
     }
     return result;
 }
