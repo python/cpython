13f1c331a5dd5b4ff3b1e4c9e47d6d3ae75e0f8f
Xavier de Gaye <xdegaye@users.sourceforge.net>
2016-12-10 16:45:53 +0100

    Issue #28918: Fix the cross compilation of xxlimited when Python has been built with Py_DEBUG defined.

--- setup.py
+++ setup.py
@@ -1630,7 +1630,7 @@ class PyBuildExt(build_ext):
 ##         ext = Extension('xx', ['xxmodule.c'])
 ##         self.extensions.append(ext)
 
-        if 'd' not in sys.abiflags:
+        if 'd' not in sysconfig.get_config_var('ABIFLAGS'):
             ext = Extension('xxlimited', ['xxlimited.c'],
                             define_macros=[('Py_LIMITED_API', '0x03050000')])
             self.extensions.append(ext)
