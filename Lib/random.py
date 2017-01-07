9b7ae96b98238627751e2f508ba5d600363555ef
Raymond Hettinger <python@rcn.com>
2017-01-06 16:13:37 -0800

    Issue #29023:  Clarify that ints and longs are always deterministic seeds for random.

--- Lib/random.py
+++ Lib/random.py
@@ -98,12 +98,14 @@ class Random(_random.Random):
         self.gauss_next = None
 
     def seed(self, a=None):
-        """Initialize internal state from hashable object.
+        """Initialize internal state of the random number generator.
 
         None or no argument seeds from current time or from an operating
         system specific randomness source if available.
 
-        If a is not None or an int or long, hash(a) is used instead.
+        If a is not None or is an int or long, hash(a) is used instead.
+        Hash values for some types are nondeterministic when the
+        PYTHONHASHSEED environment variable is enabled.
         """
 
         if a is None:
