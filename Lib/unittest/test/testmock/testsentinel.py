f692b835ece1e73d840355359dfb6a9215f41bd3
Serhiy Storchaka <storchaka@gmail.com>
2017-01-11 20:13:03 +0200

    Issue #20804: The unittest.mock.sentinel attributes now preserve their identity when they are copied or pickled.

--- Lib/unittest/test/testmock/testsentinel.py
+++ Lib/unittest/test/testmock/testsentinel.py
@@ -1,4 +1,6 @@
 import unittest
+import copy
+import pickle
 from unittest.mock import sentinel, DEFAULT
 
 
@@ -22,6 +24,17 @@ class SentinelTest(unittest.TestCase):
     def testBases(self):
         # If this doesn't raise an AttributeError then help(mock) is broken
         self.assertRaises(AttributeError, lambda: sentinel.__bases__)
+
+    def testPickle(self):
+        for proto in range(pickle.HIGHEST_PROTOCOL+1):
+            with self.subTest(protocol=proto):
+                pickled = pickle.dumps(sentinel.whatever, proto)
+                unpickled = pickle.loads(pickled)
+                self.assertIs(unpickled, sentinel.whatever)
+
+    def testCopy(self):
+        self.assertIs(copy.copy(sentinel.whatever), sentinel.whatever)
+        self.assertIs(copy.deepcopy(sentinel.whatever), sentinel.whatever)
 
 
 if __name__ == '__main__':
