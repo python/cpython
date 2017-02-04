b3e4e4e182fbd6597e635401ee8d68d7aa45712d
Serhiy Storchaka <storchaka@gmail.com>
2017-02-04 12:13:20 +0200

    Issue #20186: Converted the symtable module to Argument Clinic. Original patch by Georg Brandl.

---
+++ Modules/clinic/symtablemodule.c.h
@@ -0,0 +1,39 @@
+/*[clinic input]
+preserve
+[clinic start generated code]*/
+
+PyDoc_STRVAR(_symtable_symtable__doc__,
+"symtable($module, str, filename, startstr, /)\n"
+"--\n"
+"\n"
+"Return symbol and scope dictionaries used internally by compiler.");
+
+#define _SYMTABLE_SYMTABLE_METHODDEF    \
+    {"symtable", (PyCFunction)_symtable_symtable, METH_FASTCALL, _symtable_symtable__doc__},
+
+static PyObject *
+_symtable_symtable_impl(PyObject *module, const char *str,
+                        PyObject *filename, const char *startstr);
+
+static PyObject *
+_symtable_symtable(PyObject *module, PyObject **args, Py_ssize_t nargs, PyObject *kwnames)
+{
+    PyObject *return_value = NULL;
+    const char *str;
+    PyObject *filename;
+    const char *startstr;
+
+    if (!_PyArg_ParseStack(args, nargs, "sO&s:symtable",
+        &str, PyUnicode_FSDecoder, &filename, &startstr)) {
+        goto exit;
+    }
+
+    if (!_PyArg_NoStackKeywords("symtable", kwnames)) {
+        goto exit;
+    }
+    return_value = _symtable_symtable_impl(module, str, filename, startstr);
+
+exit:
+    return return_value;
+}
+/*[clinic end generated code: output=071dee4d836e2cfd input=a9049054013a1b77]*/
