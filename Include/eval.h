75ce79d4b7a353ed1000fdd554a2950fbcfc5f44
Victor Stinner <victor.stinner@gmail.com>
2017-02-10 14:19:36 +0100

    Backed out changeset f23fa1f7b68f
    Sorry, I didn't want to push this change before the review :-( I was pushing a
    change into the 2.7 branch.

--- Include/eval.h
+++ Include/eval.h
@@ -10,24 +10,14 @@ extern "C" {
 PyAPI_FUNC(PyObject *) PyEval_EvalCode(PyObject *, PyObject *, PyObject *);
 
 PyAPI_FUNC(PyObject *) PyEval_EvalCodeEx(PyObject *co,
-                                         PyObject *globals,
-                                         PyObject *locals,
-                                         PyObject **args, int argc,
-                                         PyObject **kwds, int kwdc,
-                                         PyObject **defs, int defc,
-                                         PyObject *kwdefs, PyObject *closure);
+					PyObject *globals,
+					PyObject *locals,
+					PyObject **args, int argc,
+					PyObject **kwds, int kwdc,
+					PyObject **defs, int defc,
+					PyObject *kwdefs, PyObject *closure);
 
 #ifndef Py_LIMITED_API
-PyAPI_FUNC(PyObject *) _PyEval_EvalCodeWithName(
-    PyObject *co,
-    PyObject *globals, PyObject *locals,
-    PyObject **args, Py_ssize_t argcount,
-    PyObject **kwnames, PyObject **kwargs,
-    Py_ssize_t kwcount, int kwstep,
-    PyObject **defs, Py_ssize_t defcount,
-    PyObject *kwdefs, PyObject *closure,
-    PyObject *name, PyObject *qualname);
-
 PyAPI_FUNC(PyObject *) _PyEval_CallTracing(PyObject *func, PyObject *args);
 #endif
 
