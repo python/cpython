f5d12fba9ea1911d0a0d26464fa045f819be5663
INADA Naoki <methane@users.noreply.github.com>
2017-02-16 09:26:01 +0900

    bpo-29548: Fix some inefficient call API usage (GH-97)

--- Objects/weakrefobject.c
+++ Objects/weakrefobject.c
@@ -461,7 +461,7 @@ proxy_checkref(PyWeakReference *proxy)
 
 WRAP_BINARY(proxy_getattr, PyObject_GetAttr)
 WRAP_UNARY(proxy_str, PyObject_Str)
-WRAP_TERNARY(proxy_call, PyEval_CallObjectWithKeywords)
+WRAP_TERNARY(proxy_call, PyObject_Call)
 
 static PyObject *
 proxy_repr(PyWeakReference *proxy)
