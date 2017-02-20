a47f396ead8b02ce505abd06884f75ea1c294adf
Daniel Himmelstein <daniel.himmelstein@gmail.com>
2017-02-20 12:06:02 -0500

    Add indent support for json.tool

--- Lib/json/tool.py
+++ Lib/json/tool.py
@@ -16,6 +16,18 @@ import json
 import sys
 
 
+def parse_indent(indent):
+    """Parse the argparse indent argument."""
+    if indent == 'None':
+        return None
+    if indent == r'\t':
+        return '\t'
+    try:
+        return int(indent)
+    except ValueError:
+        return indent
+
+
 def main():
     prog = 'python -m json.tool'
     description = ('A simple command line interface for json module '
@@ -25,24 +37,33 @@ def main():
                         help='a JSON file to be validated or pretty-printed')
     parser.add_argument('outfile', nargs='?', type=argparse.FileType('w'),
                         help='write the output of infile to outfile')
+    parser.add_argument('--indent', default='4', type=parse_indent,
+                        help='Indent level or str for pretty-printing. '
+                             'Use None for the most compact representation. '
+                             r'Use "\t" for tab indentation.')
     parser.add_argument('--sort-keys', action='store_true', default=False,
                         help='sort the output of dictionaries alphabetically by key')
     options = parser.parse_args()
 
+    # Read input JSON
     infile = options.infile or sys.stdin
-    outfile = options.outfile or sys.stdout
-    sort_keys = options.sort_keys
     with infile:
         try:
-            if sort_keys:
+            if options.sort_keys:
                 obj = json.load(infile)
             else:
                 obj = json.load(infile,
                                 object_pairs_hook=collections.OrderedDict)
         except ValueError as e:
             raise SystemExit(e)
+
+    # Export JSON
+    outfile = options.outfile or sys.stdout
     with outfile:
-        json.dump(obj, outfile, sort_keys=sort_keys, indent=4)
+        json.dump(obj, outfile,
+                  indent=options.indent,
+                  sort_keys=options.sort_keys,
+                  )
         outfile.write('\n')
 
 
