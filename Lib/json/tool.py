3c67514a2c92386f17d009809b3b3a53742f5b57
Daniel Himmelstein <daniel.himmelstein@gmail.com>
2017-02-27 10:31:45 -0500

    Add --indent / --no-indent arguments to json.tool
    From https://github.com/python/cpython/pull/201 which is being split into
    two pull requests.
    
    See http://bugs.python.org/issue29636

--- Lib/json/tool.py
+++ Lib/json/tool.py
@@ -27,6 +27,12 @@ def main():
     parser.add_argument('outfile', nargs='?', type=argparse.FileType('w'),
                         default=sys.stdout,
                         help='write the output of infile to outfile')
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument('--indent', default=4, type=int,
+                       help='Indent level (number of spaces) for '
+                       'pretty-printing. Defaults to 4.')
+    group.add_argument('--no-indent', action='store_const', dest='indent',
+                       const=None, help='Use compact mode.')
     parser.add_argument('--sort-keys', action='store_true',
                         help='sort the output of dictionaries by key')
     options = parser.parse_args()
@@ -39,7 +45,10 @@ def main():
             raise SystemExit(e)
 
     with options.outfile as outfile:
-        json.dump(obj, outfile, sort_keys=options.sort_keys, indent=4)
+        json.dump(obj, outfile,
+                  indent=options.indent,
+                  sort_keys=options.sort_keys,
+                  )
         outfile.write('\n')
 
 
