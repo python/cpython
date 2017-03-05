5563f7507f6f03b75c1b353f3306051237b47221
Dillon Brock <okay19@users.noreply.github.com>
2017-03-05 12:27:06 -0500

    Change assertRaises to assertRaisesRegex in test_xmlrpc (#481)
    (cherry picked from commit c6b448b36d22769c684bb3276f85c1b47d15ab63)

--- Lib/test/test_xmlrpc.py
+++ Lib/test/test_xmlrpc.py
@@ -412,7 +412,7 @@ class SimpleXMLRPCDispatcherTestCase(unittest.TestCase):
 
         dispatcher = xmlrpc.server.SimpleXMLRPCDispatcher()
         dispatcher.register_function(None, name='method')
-        with self.assertRaises(Exception, expected_regex='method'):
+        with self.assertRaisesRegex(Exception, 'method'):
             dispatcher._dispatch('method', ('param',))
 
     def test_instance_has_no_func(self):
@@ -420,14 +420,14 @@ class SimpleXMLRPCDispatcherTestCase(unittest.TestCase):
 
         dispatcher = xmlrpc.server.SimpleXMLRPCDispatcher()
         dispatcher.register_instance(object())
-        with self.assertRaises(Exception, expected_regex='method'):
+        with self.assertRaisesRegex(Exception, 'method'):
             dispatcher._dispatch('method', ('param',))
 
     def test_cannot_locate_func(self):
         """Calls a function that the dispatcher cannot locate"""
 
         dispatcher = xmlrpc.server.SimpleXMLRPCDispatcher()
-        with self.assertRaises(Exception, expected_regex='method'):
+        with self.assertRaisesRegex(Exception, 'method'):
             dispatcher._dispatch('method', ('param',))
 
 
