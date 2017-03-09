0c2ed76fda39e95b9c3ebebefa20d7e03db5dde1
Serhiy Storchaka <storchaka@gmail.com>
2017-03-09 10:52:50 +0200

    [2.7] bpo-29768: Fixed compile-time check for expat version. (#577)
    (cherry picked from commit 22e707fa04476710ba5cc7e2206e4ac66743931b)

--- Modules/pyexpat.c
+++ Modules/pyexpat.c
@@ -1319,7 +1319,7 @@ newxmlparseobject(char *encoding, char *namespace_separator, PyObject *intern)
         return NULL;
     }
 
-#if ((XML_MAJOR_VERSION >= 2) && (XML_MINOR_VERSION >= 1)) || defined(XML_HAS_SET_HASH_SALT)
+#if XML_COMBINED_VERSION >= 20100 || defined(XML_HAS_SET_HASH_SALT)
     /* This feature was added upstream in libexpat 2.1.0.  Our expat copy
      * has a backport of this feature where we also define XML_HAS_SET_HASH_SALT
      * to indicate that we can still use it. */
