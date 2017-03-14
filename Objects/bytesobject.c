9e165f9b49b942c91c049a667f8a3f0ce0a8cf50
Xiang Zhang <angwerzx@126.com>
2017-03-14 11:28:49 +0800

    add comment and NEWS

--- Objects/bytesobject.c
+++ Objects/bytesobject.c
@@ -566,7 +566,9 @@ format_obj(PyObject *v, const char **pbuf, Py_ssize_t *plen)
         *plen = PyBytes_GET_SIZE(result);
         return result;
     }
-    /* does it support buffer protocol? */
+    /* does it support buffer protocol?
+       maybe we can avoid making a copy of the buffer object here?
+    */
     if (PyObject_CheckBuffer(v)) {
         result = _PyBytes_FromBuffer(v);
         if (result == NULL)
