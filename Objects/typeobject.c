6a2affe7238a148451dc19160019943de1365382
Serhiy Storchaka <storchaka@gmail.com>
2017-03-17 21:40:05 +0200

    bpo-29838: Add asserts for checking results of sq_length and mq_length slots.

--- Objects/typeobject.c
+++ Objects/typeobject.c
@@ -5374,8 +5374,10 @@ getindex(PyObject *self, PyObject *arg)
         PySequenceMethods *sq = Py_TYPE(self)->tp_as_sequence;
         if (sq && sq->sq_length) {
             Py_ssize_t n = (*sq->sq_length)(self);
-            if (n < 0)
+            if (n < 0) {
+                assert(PyErr_Occurred());
                 return -1;
+            }
             i += n;
         }
     }
