5568539e2d708c8bbfef524f9e9e4aaf6b716016
AraHaan <seandhunt_7@yahoo.com>
2017-03-20 22:47:04 -0500

    bpo-29866: Added datetime_diff to datetime.py.
    The datetime_diff function compares two datetime objects and returns the
    time since the prior datetime objects (based on the current datetime
    object) in a what that is readable by humans.
    
    This is useful when one might need to compare two datetime objects,
    keeping ones codebase as small as possible (to ensure fast Python code),
    and to reduce 'hacks' in their code to make the comparison more readable
    by humans

--- Lib/datetime.py
+++ Lib/datetime.py
@@ -332,6 +332,53 @@ def _divide_and_round(a, b):
 
     return q
 
+def datetime_diff(datetime_prior, datetime_current):
+    """
+    Compares two different datetime objects
+    and returns the time elapsed since.
+
+    for example, a datetime object when turned
+    into a string (being 2015-12-03 03:34:57.281000)
+    when compared with a new datetime object
+    (equivalent to the current time) one can determine
+    how many years, months, days, hours, minutes, and
+    seconds ago the prior datetime object was.
+    This returns the data as a string readable by humans
+    denoting how much time has passed since the specified
+    prior datetime object.
+    """
+    elyear = datetime_current.year - datetime_prior.year
+    elmonth = datetime_current.month - datetime_prior.month
+    elday = datetime_current.day - datetime_prior.day
+    elhour = datetime_current.hour - datetime_prior.hour
+    elminute = datetime_current.minute - datetime_prior.minute
+    elsecond = datetime_current.second - datetime_prior.second
+    reply = ""
+    if elyear > 0:
+        reply += "{0} years".format(elyear)
+    if elmonth > 0:
+        if elyear > 0:
+            reply += ", "
+        reply += "{0} months".format(elmonth)
+    if elday > 0:
+        if elmonth > 0:
+            reply += ", "
+        reply += "{0} days".format(elday)
+    if elhour > 0:
+        if elday > 0:
+            reply += ", "
+        reply += "{0} hours".format(elhour)
+    if elminute > 0:
+        if elhour > 0:
+            reply += ", "
+        reply += "{0} minutes".format(elminute)
+    if elsecond > 0:
+        if elminute > 0:
+            reply += ", "
+        reply += "{0} seconds".format(elsecond)
+    reply += " ago."
+    return reply
+
 
 class timedelta:
     """Represent the difference between two datetime objects.
