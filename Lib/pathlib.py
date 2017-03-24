8c8785b2f8e4048cef350f89c686266f4519b67c
Serhiy Storchaka <storchaka@gmail.com>
2017-03-24 21:46:25 +0200

    bpo-25803: Avoid incorrect errors raised by Path.mkdir(exist_ok=True)… (#806)
    when the OS gives priority to errors such as EACCES over EEXIST.
    
    (cherry picked from commit af7b9ec5c855366feef4c67dc492d64b3baf84ca)

--- Lib/pathlib.py
+++ Lib/pathlib.py
@@ -1220,25 +1220,23 @@ class Path(PurePath):
         os.close(fd)
 
     def mkdir(self, mode=0o777, parents=False, exist_ok=False):
+        """
+        Create a new directory at this given path.
+        """
         if self._closed:
             self._raise_closed()
-        if not parents:
-            try:
-                self._accessor.mkdir(self, mode)
-            except FileExistsError:
-                if not exist_ok or not self.is_dir():
-                    raise
-        else:
-            try:
-                self._accessor.mkdir(self, mode)
-            except FileExistsError:
-                if not exist_ok or not self.is_dir():
-                    raise
-            except OSError as e:
-                if e.errno != ENOENT or self.parent == self:
-                    raise
-                self.parent.mkdir(parents=True)
-                self._accessor.mkdir(self, mode)
+        try:
+            self._accessor.mkdir(self, mode)
+        except FileNotFoundError:
+            if not parents or self.parent == self:
+                raise
+            self.parent.mkdir(parents=True)
+            self._accessor.mkdir(self, mode)
+        except OSError:
+            # Cannot rely on checking for EEXIST, since the operating system
+            # could give priority to other errors like EACCES or EROFS
+            if not exist_ok or not self.is_dir():
+                raise
 
     def chmod(self, mode):
         """
