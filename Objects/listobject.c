bf4bb2e43030661e568d5d4b046e8b9351cc164c
Serhiy Storchaka <storchaka@gmail.com>
2017-03-30 19:46:59 +0300

    bpo-29935: Fixed error messages in the index() method of tuple, list and deque (#887) (#907)
    when pass indices of wrong type.
    (cherry picked from commit d4edfc9abffca965e76ebc5957a92031a4d6c4d4)

--- Objects/listobject.c
+++ Objects/listobject.c
@@ -2153,8 +2153,8 @@ listindex(PyListObject *self, PyObject *args)
     PyObject *v;
 
     if (!PyArg_ParseTuple(args, "O|O&O&:index", &v,
-                                _PyEval_SliceIndex, &start,
-                                _PyEval_SliceIndex, &stop))
+                                _PyEval_SliceIndexNotNone, &start,
+                                _PyEval_SliceIndexNotNone, &stop))
         return NULL;
     if (start < 0) {
         start += Py_SIZE(self);
