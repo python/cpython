8e5b52a8da07e781bda50ba0a7065b1058495a37
Serhiy Storchaka <storchaka@gmail.com>
2017-04-16 12:04:45 +0300

    bpo-30017: Allowed calling the close() method of the zip entry writer object (#1041) (#1092)
    multiple times.  Writing to closed zip entry writer object now always produce
    a ValueError.
    (cherry picked from commit 4c0d9ea995da595e90e08813b89510de59907802)

--- Lib/zipfile.py
+++ Lib/zipfile.py
@@ -981,6 +981,8 @@ class _ZipWriteFile(io.BufferedIOBase):
         return True
 
     def write(self, data):
+        if self.closed:
+            raise ValueError('I/O operation on closed file.')
         nbytes = len(data)
         self._file_size += nbytes
         self._crc = crc32(data, self._crc)
@@ -991,6 +993,8 @@ class _ZipWriteFile(io.BufferedIOBase):
         return nbytes
 
     def close(self):
+        if self.closed:
+            return
         super().close()
         # Flush any data from the compressor, and update header info
         if self._compressor:
