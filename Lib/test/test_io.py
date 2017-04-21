537bf54c1173542b45cc8c441036bb317455e462
Victor Stinner <victor.stinner@gmail.com>
2017-04-21 13:50:48 +0200

    bpo-30107: don't dump core on expected test_io crash
    test_io has two unit tests which trigger a deadlock:
    
    * test_daemon_threads_shutdown_stdout_deadlock()
    * test_daemon_threads_shutdown_stderr_deadlock()
    
    These tests call Py_FatalError() if the expected bug is triggered
    which calls abort(). Use test.support.SuppressCrashReport to prevent
    the creation on a core dump, to fix the warning:
    
    Warning -- files was modified by test_io
      Before: []
      After:  ['python.core']

--- Lib/test/test_io.py
+++ Lib/test/test_io.py
@@ -3732,6 +3732,7 @@ class CMiscIOTest(MiscIOTest):
             import sys
             import time
             import threading
+            from test.support import SuppressCrashReport
 
             file = sys.{stream_name}
 
@@ -3739,6 +3740,10 @@ class CMiscIOTest(MiscIOTest):
                 while True:
                     file.write('.')
                     file.flush()
+
+            crash = SuppressCrashReport()
+            crash.__enter__()
+            # don't call __exit__(): the crash occurs at Python shutdown
 
             thread = threading.Thread(target=run)
             thread.daemon = True
