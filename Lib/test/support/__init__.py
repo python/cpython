ce95137815dc0d4dd9e9d37c4f2d42459217926a
Serhiy Storchaka <storchaka@gmail.com>
2017-04-28 19:17:26 +0300

    bpo-30197: Enhance functions swap_attr() and swap_item() in test.support. (#1341)
    * bpo-30197: Enhance functions swap_attr() and swap_item() in test.support.
    
    They now work when delete replaced attribute or item inside the with
    statement.  The old value of the attribute or item (or None if it doesn't
    exist) now will be assigned to the target of the "as" clause, if there is
    one.
    
    * Update docstrings.

--- Lib/test/support/__init__.py
+++ Lib/test/support/__init__.py
@@ -2119,12 +2119,15 @@ def swap_attr(obj, attr, new_val):
         restoring the old value at the end of the block. If `attr` doesn't
         exist on `obj`, it will be created and then deleted at the end of the
         block.
+
+        The old value (or None if it doesn't exist) will be assigned to the
+        target of the "as" clause, if there is one.
     """
     if hasattr(obj, attr):
         real_val = getattr(obj, attr)
         setattr(obj, attr, new_val)
         try:
-            yield
+            yield real_val
         finally:
             setattr(obj, attr, real_val)
     else:
@@ -2132,7 +2135,8 @@ def swap_attr(obj, attr, new_val):
         try:
             yield
         finally:
-            delattr(obj, attr)
+            if hasattr(obj, attr):
+                delattr(obj, attr)
 
 @contextlib.contextmanager
 def swap_item(obj, item, new_val):
@@ -2146,12 +2150,15 @@ def swap_item(obj, item, new_val):
         restoring the old value at the end of the block. If `item` doesn't
         exist on `obj`, it will be created and then deleted at the end of the
         block.
+
+        The old value (or None if it doesn't exist) will be assigned to the
+        target of the "as" clause, if there is one.
     """
     if item in obj:
         real_val = obj[item]
         obj[item] = new_val
         try:
-            yield
+            yield real_val
         finally:
             obj[item] = real_val
     else:
@@ -2159,7 +2166,8 @@ def swap_item(obj, item, new_val):
         try:
             yield
         finally:
-            del obj[item]
+            if item in obj:
+                del obj[item]
 
 def strip_python_stderr(stderr):
     """Strip the stderr of a Python process from potential debug output
