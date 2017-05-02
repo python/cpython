a25dc3162ba19d7c487ff02d3a88fc9b8fd2847d
Ivan Levkivskyi <levkivskyi@gmail.com>
2017-05-02 08:46:06 +0200

    Remove occasionally included files

--- tst.py
+++
@@ -1,8 +0,0 @@
-from typing import NamedTuple
-
-class Employee(NamedTuple):
-    """Represents an employee."""
-    name: str
-    id: int = 3
-    def __repr__(self) -> str:
-        return f'<Employee {self.name}, id={self.id}>'
