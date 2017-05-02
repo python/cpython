9b854debea4dbd91c9ffc36f084a90fa225d5197
Victor Stinner <victor.stinner@gmail.com>
2017-05-02 23:06:39 +0200

    Issue 27372: Stop test_idle from changing locale, so test passes. (#1397)
    In 3.6, the warning is now called an error, making it harder to ignore.
    
    (cherry picked from commit 7c1534141d3a159a32db9742e1d201d5c7a9ba81)

--- Lib/idlelib/EditorWindow.py
+++ Lib/idlelib/EditorWindow.py
@@ -25,8 +25,8 @@ from idlelib import help
 
 # The default tab setting for a Text widget, in average-width characters.
 TK_TABWIDTH_DEFAULT = 8
-
 _py_version = ' (%s)' % platform.python_version()
+
 
 def _sphinx_version():
     "Format sys.version_info to produce the Sphinx version string used to install the chm docs"
@@ -92,11 +92,12 @@ class EditorWindow(object):
     from idlelib.Percolator import Percolator
     from idlelib.ColorDelegator import ColorDelegator, color_config
     from idlelib.UndoDelegator import UndoDelegator
-    from idlelib.IOBinding import IOBinding, filesystemencoding, encoding
+    from idlelib.IOBinding import IOBinding, encoding
     from idlelib import Bindings
     from tkinter import Toplevel
     from idlelib.MultiStatusBar import MultiStatusBar
 
+    filesystemencoding = sys.getfilesystemencoding()  # for file names
     help_url = None
 
     def __init__(self, flist=None, filename=None, key=None, root=None):
@@ -1686,5 +1687,8 @@ def _editor_window(parent):  # htest #
     # edit.text.bind("<<close-window>>", edit.close_event)
 
 if __name__ == '__main__':
+    import unittest
+    unittest.main('idlelib.idle_test.test_editor', verbosity=2, exit=False)
+
     from idlelib.idle_test.htest import run
     run(_editor_window)
--- Lib/test/test_idle.py
+++ Lib/test/test_idle.py
@@ -2,15 +2,18 @@ import unittest
 from test import support
 from test.support import import_module
 
-# Skip test if _thread or _tkinter wasn't built or idlelib was deleted.
+# Skip test if _thread or _tkinter wasn't built, or idlelib is missing,
+# or if tcl/tk version before 8.5, which is needed for ttk widgets.
+
 import_module('threading')  # imported by PyShell, imports _thread
 tk = import_module('tkinter')  # imports _tkinter
-idletest = import_module('idlelib.idle_test')
+idlelib = import_module('idlelib')
+idlelib.testing = True  # Avoid locale-changed test error
 
-# Without test_main present, regrtest.runtest_inner (line1219) calls
-# unittest.TestLoader().loadTestsFromModule(this_module) which calls
-# load_tests() if it finds it. (Unittest.main does the same.)
-load_tests = idletest.load_tests
+# Without test_main present, test.libregrtest.runtest.runtest_inner
+# calls (line 173) unittest.TestLoader().loadTestsFromModule(module)
+# which calls load_tests() if it finds it. (Unittest.main does the same.)
+from idlelib.idle_test import load_tests
 
 if __name__ == '__main__':
     unittest.main(verbosity=2, exit=False)
