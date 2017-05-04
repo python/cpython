b40056c3e96894671eb77b31a2101db06d96c1ba
Louie Lu <me@louie.lu>
2017-05-04 17:16:25 +0800

    Fix re.sub compare string

--- Lib/idlelib/idle_test/test_calltips.py
+++ Lib/idlelib/idle_test/test_calltips.py
@@ -68,7 +68,12 @@ class Get_signatureTest(unittest.TestCase):
 
         import re
         p = re.compile('')
-        gtest(re.sub, '(pattern, repl, string, count=0, flags=0')
+        gtest(re.sub, '''(pattern, repl, string, count=0, flags=0)
+Return the string obtained by replacing the leftmost
+non-overlapping occurrences of the pattern in string by the
+replacement repl.  repl can be either a string or a callable;
+if a string, backslash escapes in it are processed.  If it is
+a callable, it's passed the match object and must return''')
         gtest(p.sub, '(repl, string, count=0)')
 
     def test_signature_wrap(self):
