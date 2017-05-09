7be2f08dd8f915f7a15d83a748098423982e6c77
Xiang Zhang <angwerzx@126.com>
2017-05-09 11:38:32 +0800

    bpo-29990: Fix range checking in GB18030 decoder (#1495)
    When decoding a 4-byte GB18030 sequence, the first and third byte cannot exceed 0xFE.

--- Modules/cjkcodecs/_codecs_cn.c
+++ Modules/cjkcodecs/_codecs_cn.c
@@ -279,7 +279,9 @@ DECODER(gb18030)
             REQUIRE_INBUF(4);
             c3 = INBYTE3;
             c4 = INBYTE4;
-            if (c < 0x81 || c3 < 0x81 || c4 < 0x30 || c4 > 0x39)
+            if (c  < 0x81 || c  > 0xFE ||
+                c3 < 0x81 || c3 > 0xFE ||
+                c4 < 0x30 || c4 > 0x39)
                 return 1;
             c -= 0x81;  c2 -= 0x30;
             c3 -= 0x81; c4 -= 0x30;
