fc929d4eafc3c165f763526bf6340a1ed6e1340a
jkleint <jkleint@users.noreply.github.com>
2017-05-10 19:39:11 -0700

    Better rewording for assertCountEqual docstring.
    Verbiage from @rhettinger.

--- Lib/unittest/case.py
+++ Lib/unittest/case.py
@@ -1141,9 +1141,8 @@ class TestCase(object):
 
 
     def assertCountEqual(self, first, second, msg=None):
-        """An unordered iterable comparison asserting the same elements,
-        regardless of order.  If the same element occurs more than once,
-        it verifies that the elements occur the same number of times.
+        """Asserts that two iterables have the same elements, the same number of
+        times, without regard to order.
 
             self.assertEqual(Counter(list(first)),
                              Counter(list(second)))
