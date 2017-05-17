88aadeb2e11b513a33419ff14852c7a93ff616cc
Eric Snow <ericsnowcurrently@gmail.com>
2017-05-17 16:55:14 -0700

    _PyModule_Create2 -> _PyModule_CreateInitialized

--- Python/sysmodule.c
+++ Python/sysmodule.c
@@ -1930,7 +1930,7 @@ _PySys_BeginInit(void)
     PyObject *m, *sysdict, *version_info;
     int res;
 
-    m = _PyModule_Create2(&sysmodule, PYTHON_API_VERSION);
+    m = _PyModule_CreateInitialized(&sysmodule, PYTHON_API_VERSION);
     if (m == NULL)
         return NULL;
     sysdict = PyModule_GetDict(m);
