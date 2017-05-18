dadfa9d6c1d303acb76b76dcced9ec2fc61634d3
Senthil Kumaran <senthil@uthcode.com>
2017-05-17 19:10:51 -0700

    Removing the docstring changes.

--- Lib/urllib/parse.py
+++ Lib/urllib/parse.py
@@ -3,26 +3,25 @@
 urlparse module is based upon the following RFC specifications.
 
 RFC 3986 (STD66): "Uniform Resource Identifiers" by T. Berners-Lee, R. Fielding
-and L. Masinter, January 2005.
+and L.  Masinter, January 2005.
 
 RFC 2732 : "Format for Literal IPv6 Addresses in URL's by R.Hinden, B.Carpenter
-and L. Masinter, December 1999.
+and L.Masinter, December 1999.
 
 RFC 2396:  "Uniform Resource Identifiers (URI)": Generic Syntax by T.
 Berners-Lee, R. Fielding, and L. Masinter, August 1998.
 
-RFC 2368: "The mailto URL scheme", by P.Hoffman, L Masinter, J. Zawinski,
-July 1998.
+RFC 2368: "The mailto URL scheme", by P.Hoffman , L Masinter, J. Zawinski, July 1998.
 
-RFC 1808: "Relative Uniform Resource Locators", by R. Fielding, UC Irvine,
-June 1995.
+RFC 1808: "Relative Uniform Resource Locators", by R. Fielding, UC Irvine, June
+1995.
 
-RFC 1738: "Uniform Resource Locators (URL)" by T. Berners-Lee, L. Masinter,
-M. McCahill, December 1994
+RFC 1738: "Uniform Resource Locators (URL)" by T. Berners-Lee, L. Masinter, M.
+McCahill, December 1994
 
 RFC 3986 is considered the current standard and any future changes to
 urlparse module should conform with it.  The urlparse module is
-currently not entirely compliant with this RFC due to de facto
+currently not entirely compliant with this RFC due to defacto
 scenarios for parsing, and for backward compatibility purposes, some
 parsing quirks from older RFCs are retained. The testcases in
 test_urlparse.py provides a good indicator of parsing behavior.
