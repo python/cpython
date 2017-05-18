808aff4c01a0642431ab5c5daa4878e062c76bd4
Eric Snow <ericsnowcurrently@gmail.com>
2017-05-18 15:30:42 -0700

    Not all platforms show a 0x prefix for memory addresses.

--- Lib/test/test_capi.py
+++ Lib/test/test_capi.py
@@ -398,8 +398,8 @@ class EmbeddingTests(unittest.TestCase):
         # --- Pass 1 ---
         # ...
 
-        interp_pat = (r"^interp (\d+) <(0x[\da-f]+)>, "
-                      r"thread state <(0x[\da-f]+)>: "
+        interp_pat = (r"^interp (\d+) <((?:0x)?[\da-fA-F]+)>, "
+                      r"thread state <((?:0x)?[\da-fA-F]+)>: "
                       r"id\(modules\) = ([\d]+)$")
         Interp = namedtuple("Interp", "id interp tstate modules")
 
