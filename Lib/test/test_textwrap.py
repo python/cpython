e9c12d9a35e22b76a53943e7aba75c25c491c531
Jonathan Eunice <jonathan.eunice@gmail.com>
2017-06-07 15:41:17 -0400

    added test for textwrap backtracking

--- Lib/test/test_textwrap.py
+++ Lib/test/test_textwrap.py
@@ -559,6 +559,20 @@ class MaxLinesTestCase(BaseTestCase):
                         placeholder=' [truncated]...')
         self.check_wrap(self.text, 80, [self.text], placeholder='.' * 1000)
 
+    def test_placeholder_backtrack(self):
+        # Test a relatively rare case which seems to occur
+        # when max_lines insufficient, but the what would be
+        # the last wrapped line so long that the placeholder
+        # cannot be added there without violence. So, textwrap
+        # backtracks and adds the placeholder to the penultimate
+        # line. This test exercises several lines that otherwise
+        # have no test coverage.
+        text = 'Good grief Python features are advancing quickly!'
+        self.check_wrap(text, 12,
+                ['Good grief', 'Python*****'],
+                max_lines=3,
+                placeholder='*****')
+
 
 class LongWordTestCase (BaseTestCase):
     def setUp(self):
