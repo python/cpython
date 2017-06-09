964f17511441b96de8e8123756323b92585e8a99
Giampaolo Rodola <g.rodola@gmail.com>
2017-06-09 21:29:51 +0200

    use != instead of 'is not'

--- Lib/selectors.py
+++ Lib/selectors.py
@@ -259,7 +259,7 @@ class _BaseSelectorImpl(BaseSelector):
         if events != key.events:
             self.unregister(fileobj)
             key = self.register(fileobj, events, data)
-        elif data is not key.data:
+        elif data != key.data:
             # Use a shortcut to update the data.
             key = key._replace(data=data)
             self._fd_to_key[key.fd] = key
@@ -391,7 +391,7 @@ class _PollLikeSelector(_BaseSelectorImpl):
                 super().unregister(fileobj)
                 raise
             changed = True
-        if data is not key.data:
+        if data != key.data:
             changed = True
 
         if changed:
