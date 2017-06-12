997dce2614b29af87a2f2999ba46b4dfbdfb8542
Cheryl Sabella <cheryl.sabella@gmail.com>
2017-06-12 09:33:42 -0400

    bpo-30617: IDLE: docstrings and unittest for outwin.py

--- Lib/idlelib/idle_test/test_outwin.py
+++ Lib/idlelib/idle_test/test_outwin.py
@@ -3,33 +3,139 @@
 
 import unittest
 import unittest.mock as mock
-import re
+from tkinter import Tk, Text
+from idlelib.idle_test.mock_tk import Mbox_func
+
+from idlelib import filelist
 from idlelib import outwin
+from test.support import requires
+requires('gui')
 
 
-class DummyOutputWindow:
-    _file_line_helper = outwin.OutputWindow._file_line_helper
-    goto_file_line = outwin.OutputWindow.goto_file_line
-    file_line_pats = outwin.OutputWindow.file_line_pats
+class OutputWindowTest(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        requires('gui')
+        cls.root = Tk()
+        cls.root.withdraw()
+        cls.window = outwin.OutputWindow(None, None, None, cls.root)
+        cls.text = Text(cls.root)
+        cls.window.text = cls.text
+
+    @classmethod
+    def tearDownClass(cls):
+        del cls.text, cls.window
+        cls.root.destroy()
+        del cls.root
+
+    def tearDown(self):
+        self.text.delete('1.0', 'end')
+
+    def test_window_title(self):
+        self.assertEqual(self.window.top.title(), "Output")
+
+    def test_ispythonsource(self):
+        self.assertFalse(self.window.ispythonsource("test.py"))
+
+    def test_maybesave(self):
+        self.window.get_saved = mock.Mock(return_value=False)
+        self.assertEqual(self.window.maybesave(), "no")
+        self.window.get_saved.assert_called_once()
+
+    def test_write(self):
+        text = self.text
+        write = self.window.write
+        test_text = "test text\nLine 2"
+        self.assertEqual(write(test_text), len(test_text))
+        self.assertEqual(text.get('1.0', '1.end'), "test text")
+        self.assertEqual(text.get('insert linestart', 'insert lineend'),
+                         'Line 2')
+
+    def test_writelines(self):
+        text = self.text
+        writelines = self.window.writelines
+        writelines(["Line 1\n", "Line 2\n", "Line 3\n"])
+        self.assertEqual(text.get('1.0', '1.end'), "Line 1")
+        self.assertEqual(text.get('3.0', '3.end'), "Line 3")
 
 
-class OutputWindowFunctionTest(unittest.TestCase):
+class GotoFileLineTest(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        requires('gui')
+        cls.root = Tk()
+        cls.root.withdraw()
+        cls.orig_error = outwin.messagebox.showerror
+        outwin.messagebox.showerror = Mbox_func()
+        cls.flist = filelist.FileList(cls.root)
+        cls.window = outwin.OutputWindow(cls.flist, None, None, None)
+        cls.text = Text(cls.root)
+        cls.window.text = cls.text
+
+    @classmethod
+    def tearDownClass(cls):
+        outwin.messagebox.showerror = cls.orig_error
+        del cls.text, cls.window, cls.flist, cls.orig_error
+        cls.root.destroy()
+        del cls.root
+
+    def tearDown(self):
+        self.window.text.delete('1.0', 'end')
+
+    def test_curr_line(self):
+        text = self.text
+        goto_file_line = self.window.goto_file_line
+        self.window.flist.gotofileline = mock.Mock()
+
+        text.insert('insert', '{}: 42: spam\n'.format(str(__file__)))
+        text.insert('insert', '{}: 21: spam'.format(str(__file__)))
+        self.assertIsNone(goto_file_line())
+        self.window.flist.gotofileline.assert_called_with(str(__file__), 21)
+
+    def test_no_line(self):
+        text = self.text
+        goto_file_line = self.window.goto_file_line
+        self.window.flist.gotofileline = mock.Mock()
+
+        text.insert('insert', 'Not a file line')
+        self.assertIsNone(goto_file_line())
+        self.window.flist.gotofileline.assert_not_called()
+        self.assertEqual(outwin.messagebox.showerror.title, 'No special line')
+
+    def test_prev_line(self):
+        text = self.text
+        goto_file_line = self.window.goto_file_line
+        self.window.flist.gotofileline = mock.Mock()
+
+        text.insert('insert', '{}: 42: spam\n'.format(str(__file__)))
+        text.insert('insert', 'Not a file line')
+        self.assertIsNone(goto_file_line())
+        self.window.flist.gotofileline.assert_called_with(str(__file__), 42)
+
+
+class ModuleFunctionTest(unittest.TestCase):
+
+    def test_compile_progs(self):
+        outwin.compile_progs()
+        for pat, regex in zip(outwin.file_line_pats, outwin.file_line_progs):
+            self.assertEqual(regex.pattern, pat)
 
     @mock.patch('builtins.open')
     def test_file_line_helper(self, mock_open):
-        dummy = DummyOutputWindow()
-
-        l = []
-        for pat in dummy.file_line_pats:
-            l.append(re.compile(pat, re.IGNORECASE))
-        dummy.file_line_progs = l
-
-        text = ((r'file "filepasstest1", line 42, text', ('filepasstest1', 42)),
-                (r'filepasstest2(42)', ('filepasstest2', 42)),
-                (r'filepasstest3: 42: text text\n', ('filepasstest3', 42)),
-                (r'filefailtest 10 text', None))
-        for line, expected_output in text:
-            self.assertEqual(dummy._file_line_helper(line), expected_output)
+        file_line_helper = outwin.file_line_helper
+        test_lines = (
+            (r'foo file "testfile1", line 42, bar', ('testfile1', 42)),
+            (r'foo testfile2(21) bar', ('testfile2', 21)),
+            (r'  testfile3  : 42: foo bar\n', ('  testfile3  ', 42)),
+            (r'foo testfile4.py :1: ', ('foo testfile4.py ', 1)),
+            ('testfile5: \u19D4\u19D2: ', ('testfile5', 42)),
+            (r'testfile6: 42', None),       # only one :
+            (r'testfile7 42 text', None)    # no separators
+            )
+        for line, expected_output in test_lines:
+            self.assertEqual(file_line_helper(line), expected_output)
             if expected_output:
                 mock_open.assert_called_with(expected_output[0], 'r')
 
--- Lib/idlelib/outwin.py
+++ Lib/idlelib/outwin.py
@@ -3,11 +3,57 @@
 
 import re
 
-from tkinter import *
-import tkinter.messagebox as tkMessageBox
+from tkinter import messagebox
 
 from idlelib.editor import EditorWindow
 from idlelib import iomenu
+
+
+file_line_pats = [
+    # order of patterns matters
+    r'file "([^"]*)", line (\d+)',
+    r'([^\s]+)\((\d+)\)',
+    r'^(\s*\S.*?):\s*(\d+):',  # Win filename, maybe starting with spaces
+    r'([^\s]+):\s*(\d+):',     # filename or path, ltrim
+    r'^\s*(\S.*?):\s*(\d+):',  # Win abs path with embedded spaces, ltrim
+]
+
+file_line_progs = None
+
+
+def compile_progs():
+    "Compile the patterns for matching to file name and line number."
+    global file_line_progs
+    file_line_progs = [re.compile(pat, re.IGNORECASE)
+                       for pat in file_line_pats]
+
+
+def file_line_helper(line):
+    """Extract file name and line number from line of text.
+
+    Check if line of text contains one of the file/line patterns.
+    If it does and if the file and line are valid, return
+    a tuple of the file name and line number.  If it doesn't match
+    or if the file or line is invalid, return None.
+    """
+    if not file_line_progs:
+        compile_progs()
+    for prog in file_line_progs:
+        match = prog.search(line)
+        if match:
+            filename, lineno = match.group(1, 2)
+            try:
+                f = open(filename, "r")
+                f.close()
+                break
+            except OSError:
+                continue
+    else:
+        return None
+    try:
+        return filename, int(lineno)
+    except TypeError:
+        return None
 
 
 class OutputWindow(EditorWindow):
@@ -18,14 +64,23 @@ class OutputWindow(EditorWindow):
 
     Adds binding to open a file at a line to the text widget.
     """
+
+    # Our own right-button menu
+    rmenu_specs = [
+        ("Cut", "<<cut>>", "rmenu_check_cut"),
+        ("Copy", "<<copy>>", "rmenu_check_copy"),
+        ("Paste", "<<paste>>", "rmenu_check_paste"),
+        (None, None, None),
+        ("Go to file/line", "<<goto-file-line>>", None),
+    ]
+
     def __init__(self, *args):
         EditorWindow.__init__(self, *args)
         self.text.bind("<<goto-file-line>>", self.goto_file_line)
 
     # Customize EditorWindow
-
     def ispythonsource(self, filename):
-        "Control highlighting of Python source."
+        "Control colorization of Python source."
         # No colorization needed
         return False
 
@@ -36,13 +91,9 @@ class OutputWindow(EditorWindow):
     def maybesave(self):
         "Customize EditorWindow to not display save file messagebox."
         # Override base class method -- don't ask any questions
-        if self.get_saved():
-            return "yes"
-        else:
-            return "no"
+        return 'yes' if self.get_saved() else 'no'
 
     # Act as output file
-
     def write(self, s, tags=(), mark="insert"):
         """Write text to text widget.
 
@@ -78,27 +129,6 @@ class OutputWindow(EditorWindow):
         """
         pass
 
-    # Our own right-button menu
-
-    rmenu_specs = [
-        ("Cut", "<<cut>>", "rmenu_check_cut"),
-        ("Copy", "<<copy>>", "rmenu_check_copy"),
-        ("Paste", "<<paste>>", "rmenu_check_paste"),
-        (None, None, None),
-        ("Go to file/line", "<<goto-file-line>>", None),
-    ]
-
-    file_line_pats = [
-        # order of patterns matters
-        r'file "([^"]*)", line (\d+)',
-        r'([^\s]+)\((\d+)\)',
-        r'^(\s*\S.*?):\s*(\d+):',  # Win filename, maybe starting with spaces
-        r'([^\s]+):\s*(\d+):',     # filename or path, ltrim
-        r'^\s*(\S.*?):\s*(\d+):',  # Win abs path with embedded spaces, ltrim
-    ]
-
-    file_line_progs = None
-
     def goto_file_line(self, event=None):
         """Handle request to open file/line.
 
@@ -108,57 +138,24 @@ class OutputWindow(EditorWindow):
 
         Otherwise, display an error messagebox.
         """
-        if self.file_line_progs is None:
-            l = []
-            for pat in self.file_line_pats:
-                l.append(re.compile(pat, re.IGNORECASE))
-            self.file_line_progs = l
-        # x, y = self.event.x, self.event.y
-        # self.text.mark_set("insert", "@%d,%d" % (x, y))
         line = self.text.get("insert linestart", "insert lineend")
-        result = self._file_line_helper(line)
+        result = file_line_helper(line)
         if not result:
             # Try the previous line.  This is handy e.g. in tracebacks,
             # where you tend to right-click on the displayed source line
             line = self.text.get("insert -1line linestart",
                                  "insert -1line lineend")
-            result = self._file_line_helper(line)
+            result = file_line_helper(line)
             if not result:
-                tkMessageBox.showerror(
+                messagebox.showerror(
                     "No special line",
                     "The line you point at doesn't look like "
                     "a valid file name followed by a line number.",
                     parent=self.text)
                 return None
         filename, lineno = result
-        edit = self.flist.open(filename)
-        edit.gotoline(lineno)
+        self.flist.gotofileline(filename, lineno)
         return None
-
-    def _file_line_helper(self, line):
-        """Extract file name and line number from line of text.
-
-        Check if line of text contains one of the file/line patterns.
-        If it does and if the file and line are valid, return
-        a tuple of the file name and line number.  If it doesn't match
-        or if the file or line is invalid, return None.
-        """
-        for prog in self.file_line_progs:
-            match = prog.search(line)
-            if match:
-                filename, lineno = match.group(1, 2)
-                try:
-                    f = open(filename, "r")
-                    f.close()
-                    break
-                except OSError:
-                    continue
-        else:
-            return None
-        try:
-            return filename, int(lineno)
-        except TypeError:
-            return None
 
 
 # These classes are currently not used but might come in handy
@@ -187,3 +184,7 @@ class OnDemandOutputWindow:
                 text.tag_configure(tag, **cnf)
         text.tag_raise('sel')
         self.write = self.owin.write
+
+if __name__ == '__main__':
+    import unittest
+    unittest.main('idlelib.idle_test.test_outwin', verbosity=2, exit=False)
