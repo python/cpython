24fb8e2d5f6a92b681fcd2e3fabf5fbaf131d8dd
Brett Cannon <brettcannon@users.noreply.github.com>
2017-06-14 14:12:16 -0700

    Drop a period from the exception message

--- Lib/importlib/util.py
+++ Lib/importlib/util.py
@@ -90,7 +90,7 @@ def find_spec(name, package=None):
             except AttributeError as e:
                 raise ModuleNotFoundError(
                     f"__path__ attribute not found on {parent_name!r}"
-                    f"while trying to find {fullname!r}.", name=fullname) from e
+                    f"while trying to find {fullname!r}", name=fullname) from e
         else:
             parent_path = None
         return _find_spec(fullname, parent_path)
