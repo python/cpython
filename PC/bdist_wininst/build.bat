79bebb0869217092b2929c5629f6fafbbdb7b5f0
Steve Dower <steve.dower@microsoft.com>
2017-06-19 10:39:36 -0700

    bpo-30687: Fixes build scripts to find msbuild.exe and stop relying on vcvarsall.bat (#2252)
    * Fixes build scripts to find msbuild.exe and stop relying on vcvarsall.bat
    Also fixes bdist_wininst.vcxproj to use correct version in generated name.

--- PC/bdist_wininst/build.bat
+++ PC/bdist_wininst/build.bat
@@ -7,16 +7,13 @@ set PCBUILD=%~dp0..\..\PCBuild\
 
 echo Building Lib\distutils\command\wininst-xx.0.exe
 
-call "%PCBUILD%env.bat" x86
-if errorlevel 1 goto :eof
+call "%PCBUILD%find_msbuild.bat" %MSBUILD%
+if ERRORLEVEL 1 (echo Cannot locate MSBuild.exe on PATH or as MSBUILD variable & exit /b 2)
 
-msbuild "%D%bdist_wininst.vcxproj" "/p:SolutionDir=%PCBUILD%\" /p:Configuration=Release /p:Platform=Win32
+%MSBUILD% "%D%bdist_wininst.vcxproj" "/p:SolutionDir=%PCBUILD%\" /p:Configuration=Release /p:Platform=Win32
 if errorlevel 1 goto :eof
 
 
 echo Building Lib\distutils\command\wininst-xx.0-amd64.exe
 
-call "%PCBUILD%env.bat" x86_amd64
-if errorlevel 1 goto :eof
-
-msbuild "%D%bdist_wininst.vcxproj" "/p:SolutionDir=%PCBUILD%\" /p:Configuration=Release /p:Platform=x64
+%MSBUILD% "%D%bdist_wininst.vcxproj" "/p:SolutionDir=%PCBUILD%\" /p:Configuration=Release /p:Platform=x64
