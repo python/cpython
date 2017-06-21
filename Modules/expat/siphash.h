8d812df0bfceb98548d7508a48aaa37eb7026658
Victor Stinner <victor.stinner@gmail.com>
2017-06-21 15:57:50 +0200

    siphash.h: Address lack of stdint.h for Visual Studio 2003-2008
    (cherry picked from libexpat commit c74d380b8f0b5215153d840d2af9455d9c53b960)

--- Modules/expat/siphash.h
+++ Modules/expat/siphash.h
@@ -12,6 +12,9 @@
  * --------------------------------------------------------------------------
  * HISTORY:
  *
+ * 2017-06-18  (Sebastian Pipping)
+ *   - Address lack of stdint.h for Visual Studio 2003 to 2008
+ *
  * 2017-06-10  (Sebastian Pipping)
  *   - Clarify license note in the header
  *   - Address C89 issues:
@@ -76,7 +79,15 @@
 #define SIPHASH_H
 
 #include <stddef.h> /* size_t */
-#include <stdint.h> /* uint64_t uint32_t uint8_t */
+
+#if defined(_WIN32) && defined(_MSC_VER) && (_MSC_VER < 1600)
+  /* For vs2003/7.1 up to vs2008/9.0; _MSC_VER 1600 is vs2010/10.0 */
+  typedef unsigned __int8   uint8_t;
+  typedef unsigned __int32 uint32_t;
+  typedef unsigned __int64 uint64_t;
+#else
+ #include <stdint.h> /* uint64_t uint32_t uint8_t */
+#endif
 
 
 #define SIP_ROTL(x, b) (uint64_t)(((x) << (b)) | ( (x) >> (64 - (b))))
