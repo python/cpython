0500aa38402096064b44c838eeb87b9552095382
Victor Stinner <victor.stinner@gmail.com>
2017-06-28 18:34:42 +0200

    bpo-29585: Define PYTHONFRAMEWORK in PC/pyconfig.h (#2477)
    * bpo-29585: Fix PC/pyconfig.h whitespaces
    
    Run "make patchcheck".
    
    * bpo-29585: Define PYTHONFRAMEWORK in PC/pyconfig.h
    
    * site: Fix path separator in _get_path() on Windows

--- Lib/site.py
+++ Lib/site.py
@@ -266,7 +266,7 @@ def _get_path(userbase):
     version = sys.version_info
 
     if os.name == 'nt':
-        return f'{userbase}/Python{version[0]}{version[1]}/site-packages'
+        return f'{userbase}\\Python{version[0]}{version[1]}\\site-packages'
 
     if sys.platform == 'darwin' and sys._framework:
         return f'{userbase}/lib/python/site-packages'
