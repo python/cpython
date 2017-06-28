ac892c3971428aabdbb367754ee34f285b33843f
Antoine Pitrou <pitrou@free.fr>
2017-06-28 11:21:52 +0200

    bpo-30775: Fix refleaks in test_multiprocessing (#2467)
    Forgetting to call Process.join() can keep some resources alive.

--- Lib/test/_test_multiprocessing.py
+++ Lib/test/_test_multiprocessing.py
@@ -1253,10 +1253,19 @@ class Bunch(object):
         self._can_exit = namespace.Event()
         if not wait_before_exit:
             self._can_exit.set()
+
+        threads = []
         for i in range(n):
             p = namespace.Process(target=self.task)
             p.daemon = True
             p.start()
+            threads.append(p)
+
+        def finalize(threads):
+            for p in threads:
+                p.join()
+
+        self._finalizer = weakref.finalize(self, finalize, threads)
 
     def task(self):
         pid = os.getpid()
@@ -1278,6 +1287,9 @@ class Bunch(object):
 
     def do_finish(self):
         self._can_exit.set()
+
+    def close(self):
+        self._finalizer()
 
 
 class AppendTrue(object):
@@ -1311,8 +1323,11 @@ class _TestBarrier(BaseTestCase):
 
     def run_threads(self, f, args):
         b = Bunch(self, f, args, self.N-1)
-        f(*args)
-        b.wait_for_finished()
+        try:
+            f(*args)
+            b.wait_for_finished()
+        finally:
+            b.close()
 
     @classmethod
     def multipass(cls, barrier, results, n):
