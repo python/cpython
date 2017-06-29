4f490a6c5b40b431b61a70e7773c988c542a5364
INADA Naoki <songofacandy@gmail.com>
2017-06-29 14:44:37 +0900

    Fix non-framework build on macOS
    `PYTHONFRAMEWORK` is defined in `Makefile` and it shoulnd't be used
    in `pyconfig.h`.
    
    `sysconfig.py --generate-posix-vars` reads config vars from Makefile
    and `pyconfig.h`.  Conflicting variables should be avoided.
    
    Especially, string config variables in Makefile are unquoted, but
    in `pyconfig.h` are keep quoted.  So it should be private (starts with
    underscore).

--- PC/pyconfig.h
+++ PC/pyconfig.h
@@ -694,6 +694,6 @@ Py_NO_ENABLE_SHARED to find out.  Also support MS_NO_COREDLL for b/w compat */
 #define HAVE_INET_PTON 1
 
 /* framework name */
-#define PYTHONFRAMEWORK ""
+#define _PYTHONFRAMEWORK ""
 
 #endif /* !Py_CONFIG_H */
