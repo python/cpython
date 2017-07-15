fadb45e81a0ac5a4a78fa62ecc0013ca93959f2b
Daniel Himmelstein <daniel.himmelstein@gmail.com>
2017-07-15 14:48:10 -0400

    bpo-30971: Improve code readability of json.tool
    Originall from https://github.com/python/cpython/pull/2720
    
    Set default option for infile and outfile as per
    https://github.com/python/cpython/pull/2720#pullrequestreview-50224377
    
    Use --sort-keys help message based on the json.tool docs at
    https://docs.python.org/3.6/library/json.html#basic-usage:
    
    Remove commens as per https://bugs.python.org/msg298692.
    Code was descriptive without the comments.

--- Lib/json/tool.py
+++ Lib/json/tool.py
@@ -22,27 +22,24 @@ def main():
                    'to validate and pretty-print JSON objects.')
     parser = argparse.ArgumentParser(prog=prog, description=description)
     parser.add_argument('infile', nargs='?', type=argparse.FileType(),
+                        default=sys.stdin,
                         help='a JSON file to be validated or pretty-printed')
     parser.add_argument('outfile', nargs='?', type=argparse.FileType('w'),
+                        default=sys.stdout,
                         help='write the output of infile to outfile')
-    parser.add_argument('--sort-keys', action='store_true', default=False,
-                        help='sort the output of dictionaries alphabetically by key')
+    parser.add_argument('--sort-keys', action='store_true',
+                        help='sort the output of dictionaries by key')
     options = parser.parse_args()
 
-    infile = options.infile or sys.stdin
-    outfile = options.outfile or sys.stdout
-    sort_keys = options.sort_keys
-    with infile:
+    hook = collections.OrderedDict if options.sort_keys else None
+    with options.infile as infile:
         try:
-            if sort_keys:
-                obj = json.load(infile)
-            else:
-                obj = json.load(infile,
-                                object_pairs_hook=collections.OrderedDict)
+            obj = json.load(infile, object_pairs_hook=hook)
         except ValueError as e:
             raise SystemExit(e)
-    with outfile:
-        json.dump(obj, outfile, sort_keys=sort_keys, indent=4)
+
+    with options.outfile as outfile:
+        json.dump(obj, outfile, sort_keys=options.sort_keys, indent=4)
         outfile.write('\n')
 
 
