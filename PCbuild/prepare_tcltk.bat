68d663cf85d1ac5eaf83482eed39c0a6f8093601
Steve Dower <steve.dower@microsoft.com>
2017-07-17 11:15:48 +0200

    [bpo-30916] Pre-build OpenSSL and Tcl/Tk for Windows (#2688)
    Updates ssl and tkinter projects to use pre-built externals

--- PCbuild/prepare_tcltk.bat
+++ PCbuild/prepare_tcltk.bat
@@ -5,7 +5,7 @@ goto Run
 :Usage
 echo.%~nx0 [flags and arguments]
 echo.
-echo.Download and build OpenSSL. This should only be performed in order to
+echo.Download and build Tcl/Tk. This should only be performed in order to
 echo.update the binaries kept online - in most cases, the files downloaded
 echo.by the get_externals.bat script are sufficient for building CPython.
 echo.
@@ -20,9 +20,11 @@ exit /b 127
 
 :Run
 setlocal
+
 if "%PCBUILD%"=="" (set PCBUILD=%~dp0)
 if "%EXTERNALS_DIR%"=="" (set EXTERNALS_DIR=%PCBUILD%\..\externals)
 
+set CERT_SETTING=
 set ORG_SETTING=
 
 :CheckOpts
@@ -39,15 +41,15 @@ goto Usage
 call "%PCBUILD%find_msbuild.bat" %MSBUILD%
 if ERRORLEVEL 1 (echo Cannot locate MSBuild.exe on PATH or as MSBUILD variable & exit /b 2)
 
-call "%PCBUILD%find_python.bat" "%PYTHON%"
-if ERRORLEVEL 1 (echo Cannot locate python.exe on PATH or as PYTHON variable & exit /b 3)
+rem call "%PCBUILD%find_python.bat" "%PYTHON%"
+rem if ERRORLEVEL 1 (echo Cannot locate python.exe on PATH or as PYTHON variable & exit /b 3)
 
-call "%PCBUILD%get_externals.bat" --openssl-src %ORG_SETTING%
+call "%PCBUILD%get_externals.bat" --tkinter-src %ORG_SETTING%
 
-if "%PERL%" == "" where perl > "%TEMP%\perl.loc" 2> nul && set /P PERL= <"%TEMP%\perl.loc" & del "%TEMP%\perl.loc"
-if "%PERL%" == "" (echo Cannot locate perl.exe on PATH or as PERL variable & exit /b 4)
+%MSBUILD% "%PCBUILD%tcl.vcxproj" /p:Configuration=Release /p:Platform=Win32
+%MSBUILD% "%PCBUILD%tk.vcxproj" /p:Configuration=Release /p:Platform=Win32
+%MSBUILD% "%PCBUILD%tix.vcxproj" /p:Configuration=Release /p:Platform=Win32
 
-%MSBUILD% "%PCBUILD%openssl.vcxproj" /p:Configuration=Release /p:Platform=Win32
-if errorlevel 1 exit /b
-%MSBUILD% "%PCBUILD%openssl.vcxproj" /p:Configuration=Release /p:Platform=x64
-if errorlevel 1 exit /b
+%MSBUILD% "%PCBUILD%tcl.vcxproj" /p:Configuration=Release /p:Platform=x64
+%MSBUILD% "%PCBUILD%tk.vcxproj" /p:Configuration=Release /p:Platform=x64
+%MSBUILD% "%PCBUILD%tix.vcxproj" /p:Configuration=Release /p:Platform=x64
