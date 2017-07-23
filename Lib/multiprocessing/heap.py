d0667cabac75f2bc88e84e0e5abc57c15c99e752
Antoine Pitrou <pitrou@free.fr>
2017-07-23 13:05:26 +0200

    bpo-30919: shared memory allocation performance regression in multiprocessing (#2708)
    * Fix #30919: shared memory allocation performance regression in multiprocessing
    
    * Change strategy for Arena directory choice
    
    * Add blurb

--- Lib/multiprocessing/heap.py
+++ Lib/multiprocessing/heap.py
@@ -60,25 +60,31 @@ if sys.platform == 'win32':
 else:
 
     class Arena(object):
+        if sys.platform == 'linux':
+            _dir_candidates = ['/dev/shm']
+        else:
+            _dir_candidates = []
 
         def __init__(self, size, fd=-1):
             self.size = size
             self.fd = fd
             if fd == -1:
                 self.fd, name = tempfile.mkstemp(
-                     prefix='pym-%d-'%os.getpid(), dir=util.get_temp_dir())
+                     prefix='pym-%d-'%os.getpid(),
+                     dir=self._choose_dir(size))
                 os.unlink(name)
                 util.Finalize(self, os.close, (self.fd,))
-                with open(self.fd, 'wb', closefd=False) as f:
-                    bs = 1024 * 1024
-                    if size >= bs:
-                        zeros = b'\0' * bs
-                        for _ in range(size // bs):
-                            f.write(zeros)
-                        del zeros
-                    f.write(b'\0' * (size % bs))
-                    assert f.tell() == size
+                os.ftruncate(self.fd, size)
             self.buffer = mmap.mmap(self.fd, self.size)
+
+        def _choose_dir(self, size):
+            # Choose a non-storage backed directory if possible,
+            # to improve performance
+            for d in self._dir_candidates:
+                st = os.statvfs(d)
+                if st.f_bavail * st.f_frsize >= size:  # enough free space?
+                    return d
+            return util.get_temp_dir()
 
     def reduce_arena(a):
         if a.fd == -1:
