f65e3e6dbefbec345ea0989f1a336ec335578e52
Antoine Pitrou <antoine@python.org>
2017-07-23 21:34:25 +0200

    Cleanup

--- Python/peephole.c
+++ Python/peephole.c
@@ -391,8 +391,6 @@ markblocks(_Py_CODEUNIT *code, Py_ssize_t len)
             case END_ITER:
             case SETUP_EXCEPT:
             case SETUP_FINALLY:
-//             case SETUP_WITH:
-//             case SETUP_ASYNC_WITH:
                 j = GETJUMPTGT(code, i);
                 assert(j < len);
                 blocks[j] = 1;
@@ -751,8 +749,6 @@ PyCode_Optimize(PyObject *code, PyObject* consts, PyObject *names,
             case JUMP_FORWARD:
             case SETUP_EXCEPT:
             case SETUP_FINALLY:
-//             case SETUP_WITH:
-//             case SETUP_ASYNC_WITH:
                 j = blocks[j / sizeof(_Py_CODEUNIT) + i + 1] - blocks[i] - 1;
                 j *= sizeof(_Py_CODEUNIT);
                 break;
