python/cpython

commit f77be6500b45a884c0584e47195cecb8144eafc8

Actually run the fuzz tests...
(Screwed up my refactoring when I moved the fuzz definition to CPython.)

Modules/_fuzz/fuzzer.cpp
@@ -73,7 +73,7 @@ static int fuzz_builtin_unicode(const char* data, size_t size) {
 
 // Run fuzzer and abort on failure.
 static int _run_fuzz(const uint8_t *data, size_t size, int(*fuzzer)(const char* , size_t)) {
-    int rv = fuzzer("", 0);
+    int rv = fuzzer(data, size);
     if (PyErr_Occurred()) {
         // Fuzz tests should handle expected errors for themselves.
         PyErr_Print();

commit dee024fdebe19c7bac8c80004de479c92d3b7b59

Fix build break I accidentally introduced in f77be65.
Not sure how I didn't notice it. :/

Modules/_fuzz/fuzzer.cpp
@@ -73,7 +73,7 @@ static int fuzz_builtin_unicode(const char* data, size_t size) {
 
 /* Run fuzzer and abort on failure. */
 static int _run_fuzz(const uint8_t *data, size_t size, int(*fuzzer)(const char* , size_t)) {
-    int rv = fuzzer(data, size);
+    int rv = fuzzer((const char*) data, size);
     if (PyErr_Occurred()) {
         /* Fuzz tests should handle expected errors for themselves. */
         PyErr_Print();