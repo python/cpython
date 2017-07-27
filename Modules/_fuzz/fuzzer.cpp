bc3d33f7dce84cd069092a46f07aa907903255e9
Devin Jeanpierre <jeanpierreda@google.com>
2017-07-26 17:49:52 -0700

    Use C-style comments. (For porting to Python 2.7 and C89).

--- Modules/_fuzz/fuzzer.cpp
+++ Modules/_fuzz/fuzzer.cpp
@@ -1,23 +1,23 @@
-// A fuzz test for CPython.
-//
-// Unusually for CPython, this is written in C++ for the benefit of linking with
-// libFuzzer.
-//
-// The only exposed function is LLVMFuzzerTestOneInput, which is called by
-// fuzzers and by the _fuzz module for smoke tests.
-//
-// To build exactly one fuzz test, as when running in oss-fuzz etc.,
-// build with -D _Py_FUZZ_ONE and -D _Py_FUZZ_<test_name>. e.g. to build
-// LLVMFuzzerTestOneInput to only run "fuzz_builtin_float", build this file with
-// -D _Py_FUZZ_ONE -D _Py_FUZZ_fuzz_builtin_float.
-//
-// See the source code for LLVMFuzzerTestOneInput for details.
+/* A fuzz test for CPython.
+
+  Unusually for CPython, this is written in C++ for the benefit of linking with
+  libFuzzer.
+
+  The only exposed function is LLVMFuzzerTestOneInput, which is called by
+  fuzzers and by the _fuzz module for smoke tests.
+
+  To build exactly one fuzz test, as when running in oss-fuzz etc.,
+  build with -D _Py_FUZZ_ONE and -D _Py_FUZZ_<test_name>. e.g. to build
+  LLVMFuzzerTestOneInput to only run "fuzz_builtin_float", build this file with
+      -D _Py_FUZZ_ONE -D _Py_FUZZ_fuzz_builtin_float.
+
+  See the source code for LLVMFuzzerTestOneInput for details. */
 
 #include <Python.h>
 #include <stdlib.h>
 #include <inttypes.h>
 
-// Fuzz PyFloat_FromString as a proxy for float(str).
+/*  Fuzz PyFloat_FromString as a proxy for float(str). */
 static int fuzz_builtin_float(const char* data, size_t size) {
     PyObject* s = PyBytes_FromStringAndSize(data, size);
     if (s == NULL) return 0;
@@ -31,7 +31,7 @@ static int fuzz_builtin_float(const char* data, size_t size) {
     return 0;
 }
 
-// Fuzz PyLong_FromUnicodeObject as a proxy for int(str).
+/* Fuzz PyLong_FromUnicodeObject as a proxy for int(str). */
 static int fuzz_builtin_int(const char* data, size_t size) {
     int base = _Py_HashBytes(data, size) % 36;
     if (base == 1) {
@@ -61,7 +61,7 @@ static int fuzz_builtin_int(const char* data, size_t size) {
     return 0;
 }
 
-// Fuzz PyUnicode_FromStringAndSize as a proxy for unicode(str).
+/* Fuzz PyUnicode_FromStringAndSize as a proxy for unicode(str). */
 static int fuzz_builtin_unicode(const char* data, size_t size) {
     PyObject* s = PyUnicode_FromStringAndSize(data, size);
     if (PyErr_Occurred() && PyErr_ExceptionMatches(PyExc_UnicodeDecodeError)) {
@@ -71,33 +71,33 @@ static int fuzz_builtin_unicode(const char* data, size_t size) {
     return 0;
 }
 
-// Run fuzzer and abort on failure.
+/* Run fuzzer and abort on failure. */
 static int _run_fuzz(const uint8_t *data, size_t size, int(*fuzzer)(const char* , size_t)) {
     int rv = fuzzer(data, size);
     if (PyErr_Occurred()) {
-        // Fuzz tests should handle expected errors for themselves.
+        /* Fuzz tests should handle expected errors for themselves. */
         PyErr_Print();
         abort();
     }
-    // Someday the return value might mean something, propagate it.
+    /* Someday the return value might mean something, propagate it. */
     return rv;
 }
 
-// CPython generates a lot of leak warnings for whatever reason.
+/* CPython generates a lot of leak warnings for whatever reason. */
 extern "C" int __lsan_is_turned_off(void) { return 1; }
 
-// Fuzz test interface.
-// This returns the bitwise or of all fuzz test's return values.
-//
-// All fuzz tests must return 0, as all nonzero return codes are reserved for
-// future use -- we propagate the return values for that future case.
-// (And we bitwise or when running multiple tests to verify that normally we
-// only return 0.)
+/* Fuzz test interface.
+   This returns the bitwise or of all fuzz test's return values.
+
+   All fuzz tests must return 0, as all nonzero return codes are reserved for
+   future use -- we propagate the return values for that future case.
+   (And we bitwise or when running multiple tests to verify that normally we
+   only return 0.) */
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
     if (!Py_IsInitialized()) {
-        // LLVMFuzzerTestOneInput is called repeatedly from the same process, with
-        // no separate initialization phase, sadly, so we need to initialize CPython
-        // ourselves on the first run.
+        /* LLVMFuzzerTestOneInput is called repeatedly from the same process,
+           with no separate initialization phase, sadly, so we need to
+           initialize CPython ourselves on the first run. */
         Py_InitializeEx(0);
     }
 
