ff0d6c2a21e5583329ff3d8e9da9b08c115a6a5a
gc <gc@syncosmic.io>
2017-08-11 08:00:52 -0700

    bpo-31183: dis can disassemble async gens/coros
    Extended issue 21947 to handle async generator and coroutine objects.

--- Lib/dis.py
+++ Lib/dis.py
@@ -32,7 +32,8 @@ def _try_compile(source, name):
     return c
 
 def dis(x=None, *, file=None, depth=None):
-    """Disassemble classes, methods, functions, generators, or code.
+    """Disassemble classes, methods, functions, generators, 
+       asynchronous generators, coroutines, or code.
 
     With no argument, disassemble the last traceback.
 
@@ -46,6 +47,10 @@ def dis(x=None, *, file=None, depth=None):
         x = x.__code__
     if hasattr(x, 'gi_code'):  # Generator
         x = x.gi_code
+    if hasattr(x, 'ag_code'):  # Async generator
+        x = x.ag_code
+    if hasattr(x, 'cr_code'):  # Coroutine
+        x = x.cr_code
     if hasattr(x, '__dict__'):  # Class or module
         items = sorted(x.__dict__.items())
         for name, x1 in items:
@@ -107,13 +112,19 @@ def pretty_flags(flags):
     return ", ".join(names)
 
 def _get_code_object(x):
-    """Helper to handle methods, functions, generators, strings and raw code objects"""
+    """Helper to handle methods, functions, generators, 
+       asynchronous generators, coroutines, strings and
+       raw code objects"""
     if hasattr(x, '__func__'): # Method
         x = x.__func__
     if hasattr(x, '__code__'): # Function
         x = x.__code__
     if hasattr(x, 'gi_code'):  # Generator
         x = x.gi_code
+    if hasattr(x, 'ag_code'):  # Async generator
+        x = x.ag_code
+    if hasattr(x, 'cr_code'):  # Coroutine
+        x = x.cr_code
     if isinstance(x, str):     # Source code
         x = _try_compile(x, "<disassembly>")
     if hasattr(x, 'co_code'):  # Code object
