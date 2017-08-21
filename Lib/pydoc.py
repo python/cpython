b8c5858559a0055e01362839a1d665baecfade36
Victor Stinner <victor.stinner@gmail.com>
2017-08-21 23:24:40 +0200

    bpo-31238: pydoc ServerThread.stop() now joins itself (GH-3151)
    ServerThread.stop() now joins itself to wait until
    DocServer.serve_until_quit() completes and then explicitly sets
    its docserver attribute to None to break a reference cycle.
    
    (cherry picked from commit 4cab2cd0c05fcda5fcb128c9eb230253fff88c21)

--- Lib/pydoc.py
+++ Lib/pydoc.py
@@ -2273,6 +2273,10 @@ def _start_server(urlhandler, port):
         def stop(self):
             """Stop the server and this thread nicely"""
             self.docserver.quit = True
+            self.join()
+            # explicitly break a reference cycle: DocServer.callback
+            # has indirectly a reference to ServerThread.
+            self.docserver = None
             self.serving = False
             self.url = None
 
