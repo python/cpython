4ad9b6bd42d802fa195749ed1a359a7d88705b57
Antoine Pitrou <antoine@python.org>
2017-08-30 23:23:44 +0200

    Avoid masking SIGTERM in forkserver.  It's not necessary and causes a race condition in test_many_processes.

--- Lib/multiprocessing/forkserver.py
+++ Lib/multiprocessing/forkserver.py
@@ -171,9 +171,8 @@ def main(listener_fd, alive_r, preload, main_path=None, sys_path=None):
     handlers = {
         # unblocking SIGCHLD allows the wakeup fd to notify our event loop
         signal.SIGCHLD: sigchld_handler,
-        # protect the process from ^C and "killall python" etc
+        # protect the process from ^C
         signal.SIGINT: signal.SIG_IGN,
-        signal.SIGTERM: signal.SIG_IGN,
         }
     old_handlers = {sig: signal.signal(sig, val)
                     for (sig, val) in handlers.items()}
