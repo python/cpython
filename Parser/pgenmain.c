66c0114fc576066999e8ccc635b2b3acdb275844
Eric Snow <ericsnowcurrently@gmail.com>
2017-09-06 16:33:01 -0700

    Fix pgen.

--- Parser/pgenmain.c
+++ Parser/pgenmain.c
@@ -16,17 +16,18 @@
 #define PGEN
 
 #include "Python.h"
+#include "internal/mem.h"
+#include "internal/pystate.h"
 #include "pgenheaders.h"
 #include "grammar.h"
 #include "node.h"
 #include "parsetok.h"
 #include "pgen.h"
-#include "internal/mem.h"
 
 int Py_DebugFlag;
 int Py_VerboseFlag;
 int Py_IgnoreEnvironmentFlag;
-struct pyruntimestate _PyRuntime = {};
+_PyRuntimeState _PyRuntime = {};
 
 /* Forward */
 grammar *getgrammar(const char *filename);
