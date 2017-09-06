b9a2da7c16f45305cfc3c785be0834258a3da7fa
Benjamin Peterson <benjamin@python.org>
2017-09-05 19:08:16 -0700

    correct initialization code
    Explicitly initialize struct members rather than relying on compiler extensions.

--- Python/pystate.c
+++ Python/pystate.c
@@ -37,8 +37,7 @@ extern "C" {
 void
 _PyRuntimeState_Init(_PyRuntimeState *runtime)
 {
-    _PyRuntimeState initial = {};
-    *runtime = initial;
+    memset(runtime, 0, sizeof(*runtime));
 
     _PyObject_Initialize(&runtime->obj);
     _PyMem_Initialize(&runtime->mem);
