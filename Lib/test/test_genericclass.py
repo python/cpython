c2d8ac2f8dd8c53ba6c655544f10b33b957305cd
Ivan Levkivskyi <levkivslyi@gmail.com>
2017-09-07 12:11:18 +0200

    Start adding tests

---
+++ Lib/test/test_genericclass.py
@@ -0,0 +1,42 @@
+import unittest
+
+
+class TestSubclassBase(unittest.TestCase):
+    def test_subclass_base(self):
+        pass
+
+    def test_subclass_base_with_builtins(self):
+        pass
+
+    def test_subclass_base_errors(self):
+        pass
+
+    def test_subclass_base_wrong(self):
+        pass
+
+    def test_subclass_base_metaclass(self):
+        pass
+
+    def test_subclass_base_type_call(self):
+        pass
+
+
+class TestClassGetitem(unittest.TestCase):
+    def test_class_getitem(self):
+        pass
+    
+    def test_class_getitem_with_builtins(self):
+        pass
+    
+    def test_class_getitem_errors(self):
+        pass
+    
+    def test_class_getitem_inheritance(self):
+        pass
+    
+    def test_class_getitem_metaclass(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
