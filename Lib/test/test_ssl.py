1a213d8132aa0842731788db10123120e442765a
Victor Stinner <victor.stinner@gmail.com>
2017-09-11 09:34:24 -0700

    test_ssl: Implement timeout in ssl_io_loop() (#3500)
    The timeout parameter was not used.

--- Lib/test/test_ssl.py
+++ Lib/test/test_ssl.py
@@ -1708,8 +1708,11 @@ class SimpleBackgroundTests(unittest.TestCase):
         # A simple IO loop. Call func(*args) depending on the error we get
         # (WANT_READ or WANT_WRITE) move data between the socket and the BIOs.
         timeout = kwargs.get('timeout', 10)
+        deadline = time.monotonic() + timeout
         count = 0
         while True:
+            if time.monotonic() > deadline:
+                self.fail("timeout")
             errno = None
             count += 1
             try:
