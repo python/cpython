7862d8a02fd4b63b88767c68a23f14f3ac73c56f
wohlganger <charles.wohlganger@gmail.com>
2017-09-13 10:59:39 -0500

    fix stupid mistake
    copied wrong file to wrong place between where I can test code and my git folder.

--- Lib/idlelib/ParenClose.py
+++ Lib/idlelib/ParenClose.py
@@ -7,7 +7,7 @@ automatically creates the closing paren or tick.
 Author: Charles M. Wohlganger
         charles.wohlganger@gmail.com
 
-Last Updated: 12-Aug-2017 by Charles Wohlganger
+Last Updated: 13-Aug-2017 by Charles Wohlganger
 
 Add to the end of config-extensions.def :
 
@@ -19,7 +19,7 @@ Add to the end of config-extensions.def :
     comment_space = True
     [ParenClose_cfgBindings]
     p-open = <Key-parenleft> <Key-bracketleft> <Key-braceleft>
-    t-open = <Key-'> <Key-">
+    t-open = <Key-quotedbl> <Key-quoteright>
     p-close = <Key-parenright> <Key-bracketright> <Key-braceright>
 
 """
@@ -37,54 +37,44 @@ class ParenClose:
     symbol is typed and the same one is to the right of it, that symbols is
     deleted before the new one is typed, effectively skipping over the closure.
     '''
-    def __init__(self, editwin=None): #setting default to none makes testing easier
+    def __init__(self, editwin=None): # Setting default to none makes testing easier.
         if editwin:
             self.text = editwin.text
         else:
             self.text=None
         self.paren_close = idleConf.GetOption(
-            'extensions', 'ParenClose', 'paren_close', default=True)
+            'extensions', 'ParenClose', 'paren_close', type = bool, default=True)
         self.tick_close = idleConf.GetOption(
-            'extensions', 'ParenClose', 'tick_close', default=True)
+            'extensions', 'ParenClose', 'tick_close', type = bool, default=True)
         self.skip_closures = idleConf.GetOption(
-            'extensions', 'ParenClose', 'skip_closures', default=True)
-        #sometimes idleConf loads boolean False as string "False"
-        if self.paren_close == 'False':
-            self.paren_close = False
-        if self.tick_close == 'False':
-            self.tick_close = False
-        if self.skip_closures == 'False':
-            self.skip_closures = False
+            'extensions', 'ParenClose', 'skip_closures', type = bool, default=True)
 
     def p_open_event(self, event):
         if self.paren_close:
-            closer = {'(': ')', '[': ']', '{': '}'}[event.char]
+            closer = {'(': ')', '[': ']', '{': '}', "'":"'", '"':'"'}[event.char]
             pos = self.text.index('insert')
             self.text.insert(pos, closer)
             self.text.mark_set('insert', pos)
-
+    
     def p_close_event(self, event):
         pos = self.text.index('insert')
         if self.skip_closures \
            and self.text.get(pos, pos + ' +1c') == event.char:
             self.text.delete(pos, pos + '+1c')
+            return True
+        return False
 
     def t_open_event(self, event):
         if self.tick_close:
             pos = self.text.index('insert')
             if self.text.get(pos + ' -2c', pos) == event.char * 2 \
                and self.text.get(pos, pos + ' +1c') != event.char:
-                # Instead of one tick, add two ticks if there are two;
+                # Instead of one tick, add three ticks if there are two;
                 # user wants to make docstring or multiline.
                 self.text.insert(pos, event.char * 3)
                 self.text.mark_set('insert', pos)
-            else:
-                if self.skip_closures \
-                   and self.text.get(pos, pos + ' +1c') == event.char:
-                    self.text.delete(pos, pos + ' +1c')
-                else:
-                    self.text.insert(pos, event.char)
-                    self.text.mark_set('insert', pos)
+            elif not self.p_close_event(event):
+                self.p_open_event(event)
 
 if __name__ == '__main__':
     import unittest
