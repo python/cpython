26323e2ab11e958b930831cdde7add34f40b52e3
wohlganger <charles.wohlganger@gmail.com>
2017-09-13 10:31:40 -0500

    Changes made per csabella request.
    ' and " changed to quotedbl and quoteright.
    Fixed keybindings appearing in config.
    Comment style fix.
    GetOption bool.
    Code deduplication on p_close_event, p_open_event, t_open_event.
    Needed to change test to use instantiated ParenClose object rather than class methods as a result of previous changes. Probably makes for a better test.

--- Lib/idlelib/idle_test/test_parenclose.py
+++ Lib/idlelib/idle_test/test_parenclose.py
@@ -13,13 +13,11 @@ class MockEvent(object):
 
 class ParenCloseTest(unittest.TestCase):
     def test_parenClose(self):
-        self.p_open_event = ParenClose.p_open_event
-        self.p_close_event = ParenClose.p_close_event
-        self.t_open_event = ParenClose.t_open_event
-        self.text = Text()
-        p_open = self.p_open_event
-        p_close = self.p_close_event
-        t_open = self.t_open_event
+        p = ParenClose()
+        p.text = Text()
+        p_open = p.p_open_event
+        p_close = p.p_close_event
+        t_open = p.t_open_event
         for paren_close_set, tick_close_set, skip_closures_set, \
             insert, func, pos, char, result, posend in [
               (1, 1, 1, 'def abuse', p_open, '1.9', '(',
@@ -56,23 +54,23 @@ class ParenCloseTest(unittest.TestCase):
                'tinny = []]', '1.10'),
               (1, 1, 0, 'gone = ""', t_open, '1.8', '"',
                'gone = """"', '1.9')]:
-                    # reset text and self
-                    self.paren_close = paren_close_set
-                    self.tick_close = tick_close_set
-                    self.skip_closures = skip_closures_set
-                    self.text.delete('1.0', 'end')
-                    # write text, move to current position, write character
-                    self.text.insert('1.0', insert)
-                    self.text.mark_set('insert', pos)
+                    # Reset text and self.
+                    p.paren_close = paren_close_set
+                    p.tick_close = tick_close_set
+                    p.skip_closures = skip_closures_set
+                    p.text.delete('1.0', 'end')
+                    # Write text, move to current position, write character.
+                    p.text.insert('1.0', insert)
+                    p.text.mark_set('insert', pos)
                     event = MockEvent(char)
-                    func(self, event)
-                    p = self.text.index('insert')
-                    self.text.insert(p, char)
-                    # checking position and text at the same time
-                    # makes it easier to spot where errors occur
-                    p = self.text.index('insert')
-                    actual = self.text.get('1.0', 'end')
-                    self.assertTupleEqual((actual, p), (result+'\n', posend))
+                    func(event)
+                    pos = p.text.index('insert')
+                    p.text.insert(pos, char)
+                    # Checking position and text at the same time
+                    # makes it easier to spot where errors occur.
+                    pos = p.text.index('insert')
+                    actual = p.text.get('1.0', 'end')
+                    self.assertTupleEqual((actual, pos), (result+'\n', posend))
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
