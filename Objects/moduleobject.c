dd59f4d108fec7fceb38caa8ceda94c5323332f3
Oren Milman <orenmn@gmail.com>
2017-09-16 23:39:23 +0300

    add an assert in module_getattro()

--- Objects/moduleobject.c
+++ Objects/moduleobject.c
@@ -680,6 +680,7 @@ static PyObject*
 module_getattro(PyModuleObject *m, PyObject *name)
 {
     PyObject *attr, *mod_name;
+    assert(PyUnicode_Check(name));
     attr = PyObject_GenericGetAttr((PyObject *)m, name);
     if (attr || !PyErr_ExceptionMatches(PyExc_AttributeError))
         return attr;
