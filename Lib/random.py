c6ce8fba07ea6798eac46ab2808167afecd4d90b
Miss Islington (bot) <31488909+miss-islington@users.noreply.github.com>
2017-09-19 10:56:33 -0700

    [3.6] bpo-31482:  Missing bytes support for random.seed() version 1 (GH-3614) (GH-3659)
    (cherry picked from commit 132a7d7cdbc7cb89fa1c1f4e8192241c3d68f549)

--- Lib/random.py
+++ Lib/random.py
@@ -109,9 +109,10 @@ class Random(_random.Random):
         """
 
         if version == 1 and isinstance(a, (str, bytes)):
+            a = a.decode('latin-1') if isinstance(a, bytes) else a
             x = ord(a[0]) << 7 if a else 0
-            for c in a:
-                x = ((1000003 * x) ^ ord(c)) & 0xFFFFFFFFFFFFFFFF
+            for c in map(ord, a):
+                x = ((1000003 * x) ^ c) & 0xFFFFFFFFFFFFFFFF
             x ^= len(a)
             a = -2 if x == -1 else x
 
