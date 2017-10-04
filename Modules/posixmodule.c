dda787ae2e0720dc0078484236834d4bba1b2301
Victor Stinner <victor.stinner@gmail.com>
2017-10-04 02:00:07 +0200

    Rewrite the comment thanks to Steve Dower's feedback

--- Modules/posixmodule.c
+++ Modules/posixmodule.c
@@ -10559,9 +10559,9 @@ check_ShellExecute()
     /* only recheck */
     if (-1 == has_ShellExecute) {
         Py_BEGIN_ALLOW_THREADS
-        /* Security note: this call is not vulnerable to "DLL injection".
-           SHELL32 is part of "KnownDLL" and so Windows always load
-           the system SHELL32.DLL, even if they are other SHELL32.DLL
+        /* Security note: this call is not vulnerable to "DLL hijacking".
+           SHELL32 is part of "KnownDLLs" and so Windows always load
+           the system SHELL32.DLL, even if there is another SHELL32.DLL
            in the DLL search path. */
         hShell32 = LoadLibraryW(L"SHELL32");
         Py_END_ALLOW_THREADS
