bbe3a0ebb700ba2c1fbf348e74ca4b2dcfd84a48
Liel Fridman <lielft@gmail.com>
2017-10-11 00:40:58 +0300

    bpo-31746: Fixed Segfaults in the sqlite module when uninitialized.

--- Modules/_sqlite/connection.c
+++ Modules/_sqlite/connection.c
@@ -250,6 +250,12 @@ pysqlite_connection_dealloc(pysqlite_Connection *self)
  */
 int pysqlite_connection_register_cursor(pysqlite_Connection* connection, PyObject* cursor)
 {
+    if (!connection || !connection->cursors) {
+        PyErr_Format(PyExc_RuntimeError,
+                        "Tried to get a cursor of an uninitialized connection."
+                    );
+                    goto error;
+    }
     PyObject* weakref;
 
     weakref = PyWeakref_NewRef((PyObject*)cursor, NULL);
@@ -1225,6 +1231,9 @@ int pysqlite_check_thread(pysqlite_Connection* self)
 
 static PyObject* pysqlite_connection_get_isolation_level(pysqlite_Connection* self, void* unused)
 {
+    if (!self || !self->isolation_level) {
+        Py_RETURN_NONE;
+    }
     return Py_NewRef(self->isolation_level);
 }
 
