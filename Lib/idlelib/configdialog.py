d483245f9e1575203b9f3c9fb6ffbe8d4a1b1c76
Terry Jan Reedy <tjreedy@udel.edu>
2017-10-11 23:56:22 -0400

    Add more asian chars.

--- Lib/idlelib/configdialog.py
+++ Lib/idlelib/configdialog.py
@@ -539,7 +539,10 @@ class FontPage(Frame):
             '\u0915\u0917\u0919\u091a\u091c\u091e\u0924\u0926\u0928\u092a'
             '\u092c\u092e\u092f\u0930\u0932\u0935\u0936\u0938\u0939\u0965\n'
             '\n<East Asian>\n'
-            '\u3007\u4e00\u4e8c\u4e09\u56db\u4e94\u516d\u4e03\u516b\u4e5d'
+            '\u3007\u4e00\u4e8c\u4e09\u56db\u4e94\u516d\u4e03\u516b\u4e5d\n'
+            '\u6c49\u5b57\u6f22\u5b57\u4eba\u6728\u706b\u571f\u91d1\u6c34\n'
+            '\u1161\u1166\u1169\u11a8\u11ab\uac00\uacb0\uac75\uacf4\uadf9\n'
+            '\u3042\u3044\u3046\u3048\u304a\u304b\u3055\u305f\u306a\u307e\n'
             )
         temp_font = tkFont.Font(self, ('courier', 10, 'normal'))
         self.font_sample = Label(frame_sample, text=sample, font=temp_font)
