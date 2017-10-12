0d8f0663167aa53ef9e0f48ccdf8ed36228c9036
Victor Stinner <victor.stinner@gmail.com>
2017-10-12 11:25:38 +0200

    bpo-25588: Fix regrtest when run inside IDLE
    When regrtest in run inside IDLE, sys.stdout and sys.stderr are not
    TextIOWrapper objects and have no file descriptor associated:
    sys.stderr.fileno() raises io.UnsupportedOperation.
    
    Disable faulthandler and don't replace sys.stdout in that case.

--- Lib/test/libregrtest/setup.py
+++ Lib/test/libregrtest/setup.py
@@ -1,6 +1,7 @@
 import atexit
 import faulthandler
 import os
+import io
 import signal
 import sys
 import unittest
@@ -14,17 +15,24 @@ from test.libregrtest.refleak import warm_caches
 
 
 def setup_tests(ns):
-    # Display the Python traceback on fatal errors (e.g. segfault)
-    faulthandler.enable(all_threads=True)
+    try:
+        stderr_fd = sys.stderr.fileno()
+    except io.UnsupportedOperation:
+        # For example, in IDLE, sys.stderr has no file descriptor,
+        # so faulthandler cannot be used.
+        stderr_fd = None
+    else:
+        # Display the Python traceback on fatal errors (e.g. segfault)
+        faulthandler.enable(all_threads=True, file=stderr_fd)
 
-    # Display the Python traceback on SIGALRM or SIGUSR1 signal
-    signals = []
-    if hasattr(signal, 'SIGALRM'):
-        signals.append(signal.SIGALRM)
-    if hasattr(signal, 'SIGUSR1'):
-        signals.append(signal.SIGUSR1)
-    for signum in signals:
-        faulthandler.register(signum, chain=True)
+        # Display the Python traceback on SIGALRM or SIGUSR1 signal
+        signals = []
+        if hasattr(signal, 'SIGALRM'):
+            signals.append(signal.SIGALRM)
+        if hasattr(signal, 'SIGUSR1'):
+            signals.append(signal.SIGUSR1)
+        for signum in signals:
+            faulthandler.register(signum, chain=True, file=stderr_fd)
 
     replace_stdout()
     support.record_original_stdout(sys.stdout)
@@ -109,7 +117,14 @@ def replace_stdout():
     """Set stdout encoder error handler to backslashreplace (as stderr error
     handler) to avoid UnicodeEncodeError when printing a traceback"""
     stdout = sys.stdout
-    sys.stdout = open(stdout.fileno(), 'w',
+    try:
+        fd = stdout.fileno()
+    except io.UnsupportedOperation:
+        # On IDLE, sys.stdout has no file descriptor and is not a TextIOWrapper
+        # object. Leaving sys.stdout unchanged.
+        return
+
+    sys.stdout = open(fd, 'w',
         encoding=stdout.encoding,
         errors="backslashreplace",
         closefd=False,
