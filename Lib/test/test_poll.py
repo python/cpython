f2431dc60faefc597339b1e1f6eacb0d9442f2db
Riccardo Coccioli <volans-@users.noreply.github.com>
2017-10-14 10:22:54 +0200

    bpo-31334: Address PR comments
    * Move infinite timeouts at the end of the tuple in the test.
    * Add a timeout smaller than the command output generation between the
      first one and the second one (500).
    * Adjusted the multiplier of the timeout (-1,) to 3, so to have only
      one additional call to poll after the last output from the command.

--- Lib/test/test_poll.py
+++ Lib/test/test_poll.py
@@ -130,7 +130,7 @@ class PollTests(unittest.TestCase):
         p = proc.stdout
         pollster = select.poll()
         pollster.register( p, select.POLLIN )
-        for tout in (None, -1000, 0, 1000, 2000, 4000, 8000, 16000) + (-1,)*10:
+        for tout in (0, 500, 1000, 2000, 4000, 8000, 16000, None, -1000) + (-1,)*3:
             fdlist = pollster.poll(tout)
             if (fdlist == []):
                 continue
