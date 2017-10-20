181b7dd033a9e3082f8699e3ee9344defefc9e6c
Victor Stinner <victor.stinner@gmail.com>
2017-10-20 16:08:31 +0200

    bpo-31827: Remove os.stat_float_times()

--- Lib/test/test_os.py
+++ Lib/test/test_os.py
@@ -488,17 +488,6 @@ class UtimeTests(unittest.TestCase):
         os.mkdir(self.dirname)
         create_file(self.fname)
 
-        def restore_float_times(state):
-            with ignore_deprecation_warnings('stat_float_times'):
-                os.stat_float_times(state)
-
-        # ensure that st_atime and st_mtime are float
-        with ignore_deprecation_warnings('stat_float_times'):
-            old_float_times = os.stat_float_times(-1)
-            self.addCleanup(restore_float_times, old_float_times)
-
-            os.stat_float_times(True)
-
     def support_subsecond(self, filename):
         # Heuristic to check if the filesystem supports timestamp with
         # subsecond resolution: check if float and int timestamps are different
