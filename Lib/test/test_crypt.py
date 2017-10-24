a71da728bff1287c00e15e5338204ba501d5b5bb
Serhiy Storchaka <storchaka@gmail.com>
2017-10-24 23:11:42 +0300

    [2.7] bpo-25287: Backport new tests for crypt and skip test_crypt on OpenBSD. (GH-4111). (cherry picked from commit f52dff611cff2fb9e90340b4787eda50ab2d40c6)

--- Lib/test/test_crypt.py
+++ Lib/test/test_crypt.py
@@ -1,14 +1,20 @@
+import sys
 from test import test_support
 import unittest
 
 crypt = test_support.import_module('crypt')
 
+if sys.platform.startswith('openbsd'):
+    raise unittest.SkipTest('The only supported method on OpenBSD is Blowfish')
+
 class CryptTestCase(unittest.TestCase):
 
     def test_crypt(self):
-        c = crypt.crypt('mypassword', 'ab')
-        if test_support.verbose:
-            print 'Test encryption: ', c
+        cr = crypt.crypt('mypassword', 'ab')
+        if cr is not None:
+            cr2 = crypt.crypt('mypassword', cr)
+            self.assertEqual(cr2, cr)
+
 
 def test_main():
     test_support.run_unittest(CryptTestCase)
