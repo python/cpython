17aba7c333ec03f87e4f3b4e721aef2e4647dcd7
Victor Stinner <victor.stinner@gmail.com>
2017-10-24 18:08:43 +0200

    Fix _thread.TIMEOUT_MAX rounding on Windows

--- Modules/_threadmodule.c
+++ Modules/_threadmodule.c
@@ -1364,8 +1364,10 @@ PyInit__thread(void)
         return NULL;
 
     timeout_max = (double)PY_TIMEOUT_MAX * 1e-6;
-    time_max = floor(_PyTime_AsSecondsDouble(_PyTime_MAX));
+    time_max = _PyTime_AsSecondsDouble(_PyTime_MAX);
     timeout_max = Py_MIN(timeout_max, time_max);
+    /* Round towards minus infinity */
+    timeout_max = floor(timeout_max);
 
     v = PyFloat_FromDouble(timeout_max);
     if (!v)
