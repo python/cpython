python/cpython

commit 3161893d933538f4bd93c674e52cc8043885694e

The device/emulator distributions include now only the shared libraries from the external libraries

Android/build.mk
@@ -8,7 +8,8 @@ native_build_dir := $(BUILD_DIR)/python-native
 py_name := python$(py_version)
 export BUILD_TYPE := android-$(ANDROID_API)-$(ANDROID_ARCH)
 py_host_dir := $(BUILD_DIR)/$(py_name)-$(BUILD_TYPE)
-export PY_DESTDIR := $(BUILD_DIR)/$(py_name)-install-$(BUILD_TYPE)
+export PY_EXTDIR := $(BUILD_DIR)/$(py_name)-extlibs-$(BUILD_TYPE)
+PY_DESTDIR := $(BUILD_DIR)/$(py_name)-install-$(BUILD_TYPE)
 abiflags = $(shell echo $$(cat $(py_host_dir)/Makefile | \
     sed -e "s/^ABIFLAGS[= \t]*\(.*\)[ \t]*$$\|^.*$$/\1/" -e "/^$$/d"))
 py_fullname = python$(py_version)$(abiflags)
@@ -59,8 +60,8 @@ native_python: $(native_build_dir)/config.status $(native_build_dir)/Modules/Set
 	$(MAKE) -C $(native_build_dir)
 
 # Target-specific exported variables.
-$(config_status):           export CPPFLAGS := -I$(PY_DESTDIR)/$(SYS_EXEC_PREFIX)/include
-$(config_status):           export LDFLAGS := -L$(PY_DESTDIR)/$(SYS_EXEC_PREFIX)/lib
+$(config_status):           export CPPFLAGS := -I$(PY_EXTDIR)/$(SYS_EXEC_PREFIX)/include
+$(config_status):           export LDFLAGS := -L$(PY_EXTDIR)/$(SYS_EXEC_PREFIX)/lib
 build configure host python_dist: \
                             export PATH := $(native_build_dir):$(PATH)
 external_libraries:         export CC := $(CC)
@@ -118,6 +119,8 @@ host:
 python_dist: $(python)
 	@echo "---> Install Python for $(BUILD_TYPE)."
 	$(MAKE) DESTDIR=$(PY_DESTDIR) -C $(py_host_dir) install
+	cp --no-dereference $(PY_EXTDIR)/$(SYS_EXEC_PREFIX)/lib/*.so* $(PY_DESTDIR)/$(SYS_EXEC_PREFIX)/lib
+	chmod u+w $(PY_DESTDIR)/$(SYS_EXEC_PREFIX)/lib/*.so*
 ifdef DEVICE_PREFIXES
 	# This won't be needed anymore when issue 31046 is fixed.
 	rm -rf $(PY_DESTDIR)/usr
@@ -184,6 +187,7 @@ clean: distclean
 	rm -rf $(py_host_dir)
 	rm -rf $(BUILD_DIR)/external-libraries/*-$(BUILD_TYPE)
 	-rmdir $(BUILD_DIR)/external-libraries
+	rm -rf $(PY_EXTDIR)
 	-rmdir $(BUILD_DIR)
 	-rmdir $(avd_dir)
 	rm -rf $(DIST_DIR)/gdb/android-$(ANDROID_API)-$(ANDROID_ARCH)

Android/external-libraries/Makefile
@@ -6,7 +6,7 @@ SQLITE := sqlite3
 SQLITE_FNAME := sqlite-autoconf-3080803
 LIBFFI := libffi-3.2.1
 BUILD_DIR := $(BUILD_DIR)/external-libraries
-py_install_dir := $(PY_DESTDIR)/$(SYS_EXEC_PREFIX)
+py_install_dir := $(PY_EXTDIR)/$(SYS_EXEC_PREFIX)
 
 all: $(py_install_dir)/lib/libncursesw.a $(py_install_dir)/lib/libreadline.a \
         $(py_install_dir)/lib/libsqlite3.a $(py_install_dir)/lib/libffi.a

Android/external-libraries/Makefile.openssl
@@ -14,7 +14,7 @@ export HOSTCC := gcc
 export CROSS_COMPILE := $(ANDROID_HOST)-
 
 BUILD_DIR := $(BUILD_DIR)/external-libraries
-py_install_dir := $(PY_DESTDIR)/$(SYS_EXEC_PREFIX)
+py_install_dir := $(PY_EXTDIR)/$(SYS_EXEC_PREFIX)
 BUILD_OPENSSL := $(py_install_dir)/lib/libcrypto.so
 ifeq ($(ANDROID_ARCH), x86)
     export MACHINE := i686

commit 23172d83221d7415b24ab19a6a5c81a88bf15731

Fix a regression from 3161893d933538f4bd93c674e52cc8043885694e
terminfo/l/linux is now again installed on the emulator.

Android/build.mk
@@ -137,6 +137,8 @@ python_dist: $(python)
 	$(MAKE) DESTDIR=$(PY_DESTDIR) -C $(py_host_dir) install
 	cp --no-dereference $(PY_EXTDIR)/$(SYS_EXEC_PREFIX)/lib/*.so* $(PY_DESTDIR)/$(SYS_EXEC_PREFIX)/lib
 	chmod u+w $(PY_DESTDIR)/$(SYS_EXEC_PREFIX)/lib/*.so*
+	tdir=$(SYS_EXEC_PREFIX)/share/terminfo/l; mkdir -p $(PY_DESTDIR)/$$tdir && \
+	    cp $(PY_EXTDIR)/$$tdir/linux $(PY_DESTDIR)/$$tdir
 ifdef DEVICE_PREFIXES
 	# This won't be needed anymore when issue 31046 is fixed.
 	rm -rf $(PY_DESTDIR)/usr