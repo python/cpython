c060c7e3d1000f867f6c54f54ebbc49e0c6d6b38
Barry Warsaw <barry@python.org>
2017-11-07 09:05:15 -0800

    Fix a typo (#4323)

--- Lib/asyncio/constants.py
+++ Lib/asyncio/constants.py
@@ -7,6 +7,6 @@ LOG_THRESHOLD_FOR_CONNLOST_WRITES = 5
 ACCEPT_RETRY_DELAY = 1
 
 # Number of stack entries to capture in debug mode.
-# The large the number, the slower the operation in debug mode
-# (see extract_stack() in events.py)
+# The larger the number, the slower the operation in debug mode
+# (see extract_stack() in events.py).
 DEBUG_STACK_DEPTH = 10
