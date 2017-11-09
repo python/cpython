d92533531512d5dd2f20e725cd0a88376a2ae287
Tyler Bell <mrbell321@gmail.com>
2017-11-09 12:45:03 -0800

    Improved incompleteReadError text when expected is None, including test and news for issue 34215

--- Lib/test/test_asyncio/test_streams.py
+++ Lib/test/test_asyncio/test_streams.py
@@ -476,14 +476,15 @@ class StreamTests(test_utils.TestCase):
     def test_readuntil_eof(self):
         stream = asyncio.StreamReader(loop=self.loop,
                                       _asyncio_internal=True)
-        stream.feed_data(b'some dataAA')
+        data = b'some dataAA'
+        stream.feed_data(data)
         stream.feed_eof()
 
         separator = b'AAA'
         with self.assertRaises(asyncio.IncompleteReadError) as cm:
             self.loop.run_until_complete(stream.readuntil(separator))
-        self.assertEqual(cm.exception.partial, b'some dataAA')
-        self.assertEqual(cm.exception.expected, separator)
+        self.assertEqual(cm.exception.partial, data)
+        self.assertIsNone(cm.exception.expected)
         self.assertEqual(b'', stream._buffer)
 
     def test_readuntil_limit_found_sep(self):
