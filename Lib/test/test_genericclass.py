734c7e9a79796c6f2f685bde1de188ff0b439446
Ivan Levkivskyi <levkivskyi@gmail.com>
2017-11-11 11:13:13 +0100

    Test error message

--- Lib/test/test_genericclass.py
+++ Lib/test/test_genericclass.py
@@ -136,7 +136,7 @@ class TestMROEntry(unittest.TestCase):
         c = C()
         with self.assertRaisesRegex(TypeError,
                                     "MRO entry resolution; "
-                                    "use type.new_class()"):
+                                    "use types.new_class()"):
             type('Bad', (c,), {})
 
 
