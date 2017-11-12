1682be8607492065ca81eb3f8716dd3d84e4a7ae
nitishch <nitishchandrachinta@gmail.com>
2017-11-12 15:01:49 +0530

    By mistake had the changes of doc branch in this branch

--- Lib/_collections_abc.py
+++ Lib/_collections_abc.py
@@ -899,10 +899,6 @@ class Sequence(Reversible, Collection):
     def index(self, value, start=0, stop=None):
         '''S.index(value, [start, [stop]]) -> integer -- return first index of value.
            Raises ValueError if the value is not present.
-
-           It is not required that all concrete implementations of
-           the Sequence class support start and stop attributes.
-
         '''
         if start is not None and start < 0:
             start = max(len(self) + start, 0)
