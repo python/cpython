ddbce1378644f9d5ad0651e1c9035bd8c6502edc
Serhiy Storchaka <storchaka@gmail.com>
2017-11-15 17:39:37 +0200

    bpo-32023: Disallow genexprs without parenthesis in class definitions. (#4400)

--- Lib/test/test_syntax.py
+++ Lib/test/test_syntax.py
@@ -153,6 +153,10 @@ Traceback (most recent call last):
 SyntaxError: Generator expression must be parenthesized
 >>> f((x for x in L), 1)
 [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+>>> class C(x for x in L):
+...     pass
+Traceback (most recent call last):
+SyntaxError: invalid syntax
 
 >>> def g(*args, **kwargs):
 ...     print(args, sorted(kwargs.items()))
