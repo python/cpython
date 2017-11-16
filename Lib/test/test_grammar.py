53ffbddcbe8186e7b0dd182c724a665ffe71eafd
David Cuthbert <dacut@kanga.org>
2017-11-16 02:04:25 -0800

    Fix return statements to allow unpacking without parenthesis.

--- Lib/test/test_grammar.py
+++ Lib/test/test_grammar.py
@@ -824,11 +824,17 @@ class GrammarTests(unittest.TestCase):
         test_inner()
 
     def test_return(self):
-        # 'return' [testlist]
+        # 'return' [testlist_star_expr]
         def g1(): return
         def g2(): return 1
+        def g3():
+            z = [2, 3]
+            return 1, *z
+
         g1()
         x = g2()
+        y = g3()
+        self.assertEqual(y, (1, 2, 3), "unparenthesized star expr return")
         check_syntax_error(self, "class foo:return 1")
 
     def test_break_in_finally(self):
