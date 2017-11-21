b64e8ddffc4bc09589f7506176a742c230dfe1a7
Cheryl Sabella <cheryl.sabella@gmail.com>
2017-11-21 18:02:42 -0500

    Add unit test for PathBrowser class.

--- Lib/idlelib/idle_test/test_pathbrowser.py
+++ Lib/idlelib/idle_test/test_pathbrowser.py
@@ -1,10 +1,62 @@
+""" Test idlelib.pathbrowser.
+"""
+
 import unittest
 import os
 import sys
+import pyclbr
+
 import idlelib
 from idlelib import pathbrowser
+from idlelib.tree import TreeNode
+from test.support import requires
+from tkinter import Tk
+from idlelib.idle_test.mock_idle import Func
+
 
 class PathBrowserTest(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        requires('gui')
+        cls.root = Tk()
+        cls.root.withdraw()
+        cls.pb = pathbrowser.PathBrowser(cls.root, _utest=True)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.pb.close()
+        cls.root.destroy()
+        del cls.root, cls.pb
+
+    def test_init(self):
+        pb = self.pb
+        eq = self.assertEqual
+        eq(pb.master, self.root)
+        eq(pyclbr._modules, {})
+        self.assertIsInstance(pb.node, TreeNode)
+
+    def test_settitle(self):
+        pb = self.pb
+        self.assertEqual(pb.top.title(), 'Path Browser')
+        self.assertEqual(pb.top.iconname(), 'Path Browser')
+
+    def test_rootnode(self):
+        pb = self.pb
+        rn = pb.rootnode()
+        self.assertIsInstance(rn, pathbrowser.PathBrowserTreeItem)
+
+    def test_close(self):
+        pb = self.pb
+        pb.top.destroy = Func()
+        pb.node.destroy = Func()
+        pb.close()
+        self.assertTrue(pb.top.destroy.called)
+        self.assertTrue(pb.node.destroy.called)
+        del pb.top.destroy, pb.node.destroy
+
+
+class DirBrowserTreeItemTest(unittest.TestCase):
 
     def test_DirBrowserTreeItem(self):
         # Issue16226 - make sure that getting a sublist works
@@ -16,12 +68,16 @@ class PathBrowserTest(unittest.TestCase):
         self.assertEqual(d.ispackagedir(dir), True)
         self.assertEqual(d.ispackagedir(dir + '/Icons'), False)
 
+
+class PathBrowserTreeItemTest(unittest.TestCase):
+
     def test_PathBrowserTreeItem(self):
         p = pathbrowser.PathBrowserTreeItem()
         self.assertEqual(p.GetText(), 'sys.path')
         sub = p.GetSubList()
         self.assertEqual(len(sub), len(sys.path))
         self.assertEqual(type(sub[0]), pathbrowser.DirBrowserTreeItem)
+
 
 if __name__ == '__main__':
     unittest.main(verbosity=2, exit=False)
