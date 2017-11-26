be1faabeef0821f188a28dd7cc6b744b89e85e94
Zachary Ware <zachary.ware@gmail.com>
2017-11-26 11:55:46 -0600

    bpo-30855: Bump Tcl/Tk to 8.5.19 on Windows (GH-4550)

--- PCbuild/get_externals.bat
+++ PCbuild/get_externals.bat
@@ -49,8 +49,8 @@ set libraries=%libraries%                                    bzip2-1.0.6
 if NOT "%IncludeBsddb%"=="false" set libraries=%libraries%   bsddb-4.7.25.0
 if NOT "%IncludeSSL%"=="false" set libraries=%libraries%     openssl-1.0.2k
 set libraries=%libraries%                                    sqlite-3.14.2.0
-if NOT "%IncludeTkinter%"=="false" set libraries=%libraries% tcl-8.5.15.0
-if NOT "%IncludeTkinter%"=="false" set libraries=%libraries% tk-8.5.15.0
+if NOT "%IncludeTkinter%"=="false" set libraries=%libraries% tcl-8.5.19.0
+if NOT "%IncludeTkinter%"=="false" set libraries=%libraries% tk-8.5.19.0
 if NOT "%IncludeTkinter%"=="false" set libraries=%libraries% tix-8.4.3.5
 
 for %%e in (%libraries%) do (
