939f069e6bed27ca2ed2f7076b2882657cf40dd9
Jesse Bakker <j.bakker@scisports.com>
2017-11-29 15:22:17 +0100

    Fix tests for new Traceback implementation

--- Lib/tracemalloc.py
+++ Lib/tracemalloc.py
@@ -216,6 +216,8 @@ class Traceback(Sequence):
                 frame_slice = self[-limit:]
             else:
                 frame_slice = self[:limit]
+        else:
+            frame_slice = self
                 
         if most_recent_first:
             frame_slice = reversed(frame_slice)
