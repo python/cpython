caf923898257a51b2c3c6575c98ae4a1e25738b5
Ivan Levkivskyi <levkivslyi@gmail.com>
2017-12-12 14:24:38 +0100

    Re-organize code and fix formatting

--- Python/bltinmodule.c
+++ Python/bltinmodule.c
@@ -48,33 +48,33 @@ _Py_IDENTIFIER(stderr);
 #include "clinic/bltinmodule.c.h"
 
 static PyObject*
-update_bases(PyObject* bases, PyObject** args, int nargs)
+update_bases(PyObject *bases, PyObject *const *args, int nargs)
 {
     int i, j;
     PyObject *base, *meth, *new_base, *result, *new_bases = NULL;
     PyObject *stack[1] = {bases};
     assert(PyTuple_Check(bases));
 
-    /* We have a separate cycle to calculate replacements with the idea that in
-       most cases we just scroll quickly though it and return original bases */
     for (i = 0; i < nargs; i++) {
         base  = args[i];
         if (PyType_Check(base)) {
             if (new_bases) {
+                /* If we already have made a replacement, then we append every normal base,
+                   otherwise just skip it. */
                 if (PyList_Append(new_bases, base) < 0) {
                     goto error;
                 }
             }
             continue;
         }
-        if (!(meth = _PyObject_GetAttrId(base, &PyId___mro_entries__))) {
+        meth = _PyObject_GetAttrId(base, &PyId___mro_entries__);
+        if (!meth) {
             if (!PyErr_ExceptionMatches(PyExc_AttributeError)) {
                 goto error;
             }
             PyErr_Clear();
             if (new_bases) {
                 if (PyList_Append(new_bases, base) < 0) {
-                    Py_DECREF(meth);
                     goto error;
                 }
             }
@@ -85,29 +85,29 @@ update_bases(PyObject* bases, PyObject** args, int nargs)
         if (!new_base) {
             goto error;
         }
-        if (PyTuple_Check(new_base)) {
-            if (!new_bases) {
-                /* If this is a first successful conversion, create new_bases list and
-                copy previously encountered bases. */
-                new_bases = PyList_New(i);
-                for (j = 0; j < i; j++) {
-                    base = args[j];
-                    PyList_SET_ITEM(new_bases, j, base);
-                    Py_INCREF(base);
-                }
-            }
-            if (!_PyList_Extend((PyListObject *)new_bases, new_base)) {
-                goto error;
-            }
-            Py_DECREF(Py_None);
-            Py_DECREF(new_base);
-        }
-        else {
+        if (!PyTuple_Check(new_base)) {
             PyErr_SetString(PyExc_TypeError,
                             "__mro_entries__ must return a tuple");
             Py_DECREF(new_base);
             goto error;
         }
+        if (!new_bases) {
+            /* If this is a first successful replacement, create new_bases list and
+               copy previously encountered bases. */
+            if (!(new_bases = PyList_New(i))) {
+                goto error;
+            }
+            for (j = 0; j < i; j++) {
+                base = args[j];
+                PyList_SET_ITEM(new_bases, j, base);
+                Py_INCREF(base);
+            }
+        }
+        j = PyList_GET_SIZE(new_bases);
+        if (PyList_SetSlice(new_bases, j, j, new_base) < 0) {
+            goto error;
+        }
+        Py_DECREF(new_base);
     }
     if (!new_bases) {
         return bases;
@@ -115,6 +115,7 @@ update_bases(PyObject* bases, PyObject** args, int nargs)
     result = PyList_AsTuple(new_bases);
     Py_DECREF(new_bases);
     return result;
+
 error:
     Py_XDECREF(new_bases);
     return NULL;
