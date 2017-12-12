7afa5f842cf56be1c70185a8bb131700be9b6676
Serhiy Storchaka <storchaka@gmail.com>
2017-12-12 19:03:08 +0200

    Fix implementation dependent assertion in test_plistlib. (#4813)
    It is failed with an advanced optimizer.

--- Lib/test/test_plistlib.py
+++ Lib/test/test_plistlib.py
@@ -320,7 +320,8 @@ class TestPlistlib(unittest.TestCase):
                     'second': [1, 2],
                     'third': [3, 4],
                 })
-                self.assertIsNot(pl2['first'], pl2['second'])
+                if fmt != plistlib.FMT_BINARY:
+                    self.assertIsNot(pl2['first'], pl2['second'])
 
     def test_list_members(self):
         pl = {
