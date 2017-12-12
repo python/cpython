b57b8ac646271e2d49addabe1e43c78e1dbb381a
Nitish Chandra <nitishchandrachinta@gmail.com>
2017-12-12 15:52:30 +0530

    bpo-31942: Document optional support of start and stop attributes in Sequence.index method (#4277)

--- Lib/_collections_abc.py
+++ Lib/_collections_abc.py
@@ -899,6 +899,9 @@ class Sequence(Reversible, Collection):
     def index(self, value, start=0, stop=None):
         '''S.index(value, [start, [stop]]) -> integer -- return first index of value.
            Raises ValueError if the value is not present.
+
+           Supporting start and stop arguments is optional, but
+           recommended.
         '''
         if start is not None and start < 0:
             start = max(len(self) + start, 0)
