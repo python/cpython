6efcb6d3d5911aaf699f9df3bb3bc26e94f38e6d
Victor Stinner <victor.stinner@gmail.com>
2017-12-18 23:42:55 +0100

    bpo-32030: Fix compilation on FreeBSD, #include <fenv.h> (#4919)
    * main.c: add missing #include <fenv.h> on FreeBSD
    * indent also other #ifdef in main.c
    * cleanup Programs/python.c

--- Programs/python.c
+++ Programs/python.c
@@ -1,12 +1,6 @@
 /* Minimal main program -- everything is loaded from the library */
 
 #include "Python.h"
-#include "internal/pystate.h"
-#include <locale.h>
-
-#ifdef __FreeBSD__
-#include <fenv.h>
-#endif
 
 #ifdef MS_WINDOWS
 int
@@ -15,8 +9,6 @@ wmain(int argc, wchar_t **argv)
     return Py_Main(argc, argv);
 }
 #else
-
-
 int
 main(int argc, char **argv)
 {
