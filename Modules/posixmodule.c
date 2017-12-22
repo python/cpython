b895046d099aaa768f51d1007fbb9856108fd1ec
Michael Felt <aixtools@gmail.com>
2017-12-22 09:13:29 +0000

    Modify syntax to satisify xlC compiler requirements. Error message was: "./Modules/posixmodule.c", line 9328.64: 1506-280 (S) Function argument assignment between types "unsigned long" and "struct fsid_t" is not allowed.

--- Modules/posixmodule.c
+++ Modules/posixmodule.c
@@ -9325,7 +9325,7 @@ _pystatvfs_fromstructstatvfs(struct statvfs st) {
     PyStructSequence_SET_ITEM(v, 8, PyLong_FromLong((long) st.f_flag));
     PyStructSequence_SET_ITEM(v, 9, PyLong_FromLong((long) st.f_namemax));
 #endif
-    PyStructSequence_SET_ITEM(v, 10, PyLong_FromUnsignedLong(st.f_fsid));
+    PyStructSequence_SET_ITEM(v, 10, PyLong_FromUnsignedLong(st.f_fsid.val[0]));
     if (PyErr_Occurred()) {
         Py_DECREF(v);
         return NULL;
