dbf52e02f18dac6f5f0a64f78932f3dc6efc056b
Benjamin Peterson <benjamin@python.org>
2018-01-02 09:25:41 -0800

    bpo-31530: fix crash when multiple threads iterate over a file, round 2 (#5060)
    Multiple threads iterating over a file can corrupt the file's internal readahead
    buffer resulting in crashes. To fix this, cache buffer state thread-locally for
    the duration of a file_iternext call and only update the file's internal state
    after reading completes.
    
    No attempt is made to define or provide "reasonable" semantics for iterating
    over a file on multiple threads. (Non-crashing) races are still
    present. Duplicated, corrupt, and missing data will happen.
    
    This was originally fixed by 6401e5671781eb217ee1afb4603cc0d1b0367ae6, which
    raised an exception from seek() and next() when concurrent operations were
    detected. Alas, this simpler solution breaks legitimate use cases such as
    capturing the standard streams when multiple threads are logging.

--- Lib/test/test_file2k.py
+++ Lib/test/test_file2k.py
@@ -653,18 +653,15 @@ class FileThreadingTests(unittest.TestCase):
         self._test_close_open_io(io_func)
 
     def test_iteration_torture(self):
-        # bpo-31530: Crash when concurrently iterate over a file.
+        # bpo-31530
         with open(self.filename, "wb") as fp:
             for i in xrange(2**20):
                 fp.write(b"0"*50 + b"\n")
         with open(self.filename, "rb") as f:
-            def iterate():
-                try:
-                    for l in f:
-                        pass
-                except IOError:
+            def it():
+                for l in f:
                     pass
-            self._run_workers(iterate, 10)
+            self._run_workers(it, 10)
 
     def test_iteration_seek(self):
         # bpo-31530: Crash when concurrently seek and iterate over a file.
@@ -674,16 +671,14 @@ class FileThreadingTests(unittest.TestCase):
         with open(self.filename, "rb") as f:
             it = iter([1] + [0]*10)  # one thread reads, others seek
             def iterate():
-                try:
-                    if next(it):
-                        for l in f:
-                            pass
-                    else:
-                        for i in range(100):
-                            f.seek(i*100, 0)
-                except IOError:
-                    pass
+                if next(it):
+                    for l in f:
+                        pass
+                else:
+                    for i in xrange(100):
+                        f.seek(i*100, 0)
             self._run_workers(iterate, 10)
+
 
 @unittest.skipUnless(os.name == 'posix', 'test requires a posix system.')
 class TestFileSignalEINTR(unittest.TestCase):
