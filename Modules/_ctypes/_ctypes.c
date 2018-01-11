8e03e3a900b9971b863c75c10454acb950333d5b
Michael Felt <aixtools@gmail.com>
2018-01-11 10:41:53 +0000

    Added RTLD_MEMBER, and RTLD_NOW - so they could be imported FYI: RTLD_NOW is already used, but buried in current code These are added so that they can be imported rather than static define in .py code Note - in Python3 these will be os.RTLD_MEMBER and os.RTLD_NOW

--- Modules/_ctypes/_ctypes.c
+++ Modules/_ctypes/_ctypes.c
@@ -5737,6 +5737,12 @@ init_ctypes(void)
 
     PyModule_AddObject(m, "RTLD_LOCAL", PyInt_FromLong(RTLD_LOCAL));
     PyModule_AddObject(m, "RTLD_GLOBAL", PyInt_FromLong(RTLD_GLOBAL));
+#ifdef RTLD_NOW
+    PyModule_AddObject(m, "RTLD_NOW", PyInt_FromLong(RTLD_NOW));
+#endif
+#ifdef RTLD_MEMBER
+    PyModule_AddObject(m, "RTLD_MEMBER", PyInt_FromLong(RTLD_MEMBER));
+#endif
 
     PyExc_ArgError = PyErr_NewException("ctypes.ArgumentError", NULL, NULL);
     if (PyExc_ArgError) {
