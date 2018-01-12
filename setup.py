b1d951e6a5d5fe757937b5757aa5174d7abf49cc
Christian Heimes <christian@python.org>
2018-01-12 15:26:32 +0100

    bpo-32521: nis libtirpc (GH-5137)
    glibc has removed Sun RPC. Use replacement libtirpc headers and library in
    nis module
    
    Signed-off-by: Christian Heimes <christian@python.org>
    (cherry picked from commit f3031b8a7ad71d3b6ed05da7f3041d9efbe773cf)

--- setup.py
+++ setup.py
@@ -1349,12 +1349,19 @@ class PyBuildExt(build_ext):
             # Sun yellow pages. Some systems have the functions in libc.
             if (host_platform not in ['cygwin', 'atheos', 'qnx6'] and
                 find_file('rpcsvc/yp_prot.h', inc_dirs, []) is not None):
-                if (self.compiler.find_library_file(lib_dirs, 'nsl')):
-                    libs = ['nsl']
-                else:
-                    libs = []
+                nis_libs = []
+                nis_includes = []
+                if self.compiler.find_library_file(lib_dirs, 'nsl'):
+                    nis_libs.append('nsl')
+                if self.compiler.find_library_file(lib_dirs, 'tirpc'):
+                    # Sun RPC has been moved from glibc to libtirpc
+                    # rpcsvc/yp_prot.h is still in /usr/include, but
+                    # rpc/rpc.h has been moved into tirpc/ subdir.
+                    nis_libs.append('tirpc')
+                    nis_includes.append('/usr/include/tirpc')
                 exts.append( Extension('nis', ['nismodule.c'],
-                                       libraries = libs) )
+                                       libraries = nis_libs,
+                                       include_dirs=nis_includes) )
             else:
                 missing.append('nis')
         else:
