a2951dd89a1ba556b34e1aa2ce8c8ff4fc4e338b
Serhiy Storchaka <storchaka@gmail.com>
2018-01-13 00:28:31 +0200

    bpo-31993: Do not use memoryview when pickle large strings. (#5154)
    PyMemoryView_FromMemory() created a memoryview referring to
    the internal data of the string.  When the string is destroyed
    the memoryview become referring to a freed memory.

--- Modules/_pickle.c
+++ Modules/_pickle.c
@@ -2184,8 +2184,9 @@ _Pickler_write_bytes(PicklerObject *self,
         /* Stream write the payload into the file without going through the
            output buffer. */
         if (payload == NULL) {
-            payload = mem = PyMemoryView_FromMemory((char *) data, data_size,
-                                                    PyBUF_READ);
+            /* TODO: It would be better to use a memoryview with a linked
+               original string if this is possible. */
+            payload = mem = PyBytes_FromStringAndSize(data, data_size);
             if (payload == NULL) {
                 return -1;
             }
