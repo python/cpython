fb5a7ad421ac20c49218ee4b86fb0d85ca4cd664
Nathaniel J. Smith <njs@pobox.com>
2018-01-24 12:14:33 -0800

    bpo-32636: Fix @asyncio.coroutine debug mode bug exposed by gh-5250 (#5291)

--- Lib/test/test_asyncio/test_tasks.py
+++ Lib/test/test_asyncio/test_tasks.py
@@ -9,6 +9,7 @@ import io
 import random
 import re
 import sys
+import textwrap
 import types
 import unittest
 import weakref
@@ -3089,6 +3090,22 @@ class CompatibilityTests(test_utils.TestCase):
 
         result = self.loop.run_until_complete(inner())
         self.assertEqual(['ok1', 'ok2'], result)
+
+    def test_debug_mode_interop(self):
+        # https://bugs.python.org/issue32636
+        code = textwrap.dedent("""
+            import asyncio
+
+            async def native_coro():
+                pass
+
+            @asyncio.coroutine
+            def old_style_coro():
+                yield from native_coro()
+
+            asyncio.run(old_style_coro())
+        """)
+        assert_python_ok("-c", code, PYTHONASYNCIODEBUG="1")
 
 
 if __name__ == '__main__':
