1f3fbc0edeef7cccfd0d298b7f585d623ce6e2c7
Yury Selivanov <yury@magic.io>
2018-01-25 14:18:55 -0500

    bpo-32436: Fix a refleak; var GC tracking; a GCC warning (#5326)
    The refleak in question wasn't really important, as context vars
    are usually created at the toplevel and live as long as the interpreter
    lives, so the context var name isn't ever GCed anyways.

--- Python/hamt.c
+++ Python/hamt.c
@@ -449,7 +449,7 @@ hamt_bitcount(uint32_t i)
     */
     i = i - ((i >> 1) & 0x55555555);
     i = (i & 0x33333333) + ((i >> 2) & 0x33333333);
-    return ((i + (i >> 4) & 0xF0F0F0F) * 0x1010101) >> 24;
+    return (((i + (i >> 4)) & 0xF0F0F0F) * 0x1010101) >> 24;
 }
 
 static inline uint32_t
