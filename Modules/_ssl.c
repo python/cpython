5c37f24316b9ca04697d50ebbbe69c68c54fface
Bernard Spil <brnrd@FreeBSD.org>
2018-01-26 12:49:03 +0100

    Merge tiran:bpo-32378-libressl-npn-2

--- Modules/_ssl.c
+++ Modules/_ssl.c
@@ -135,18 +135,6 @@ static void _PySSLFixErrno(void) {
 #  define OPENSSL_VERSION_1_1 1
 #endif
 
-/* LibreSSL quirks
- *
- * LibreSSL 2.6.1 no longer provides NPN support but does not set the
- * designated OPENSSL_NO_NEXTPROTONEG feature flag. See upstream issue
- * https://github.com/libressl-portable/portable/issues/368
- */
-#if defined(LIBRESSL_VERSION_NUMBER) && !defined(TLSEXT_TYPE_next_proto_neg)
-#  ifndef OPENSSL_NO_NEXTPROTONEG
-#    define OPENSSL_NO_NEXTPROTONEG 1
-#  endif
-#endif
-
 /* Openssl comes with TLSv1.1 and TLSv1.2 between 1.0.0h and 1.0.1
     http://www.openssl.org/news/changelog.html
  */
@@ -169,6 +157,11 @@ static void _PySSLFixErrno(void) {
 # define HAVE_ALPN
 #endif
 
+/* NPN support defines changed in OpenSSL 1.1
+ * LibreSSL 2.6.1 no longer supports NPN but does not set the
+ * designated OPENSSL_NO_NEXTPROTONEG feature flag. See upstream issue
+ * https://github.com/libressl-portable/portable/issues/368
+ */
 #ifdef OPENSSL_VERSION_1_1
 # ifdef OPENSSL_NO_NEXTPROTONEG
 #  define HAVE_NPN 0
@@ -176,7 +169,7 @@ static void _PySSLFixErrno(void) {
 #  define HAVE_NPN 1
 # endif
 #else /* OpenSSL < 1.1.0 */
-# ifndef OPENSSL_NPN_NEGOTIATED
+# if !defined(OPENSSL_NPN_NEGOTIATED) || defined(LIBRESSL_VERSION_NUMBER) && !defined(TLSEXT_TYPE_next_proto_neg)
 #  define HAVE_NPN 0
 # else
 #  define HAVE_NPN 1
