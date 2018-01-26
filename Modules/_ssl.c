1499ac9900dbe7a733e8f17c9970e69cc8d593e9
Bernard Spil <Sp1l@users.noreply.github.com>
2018-01-26 11:41:43 +0100

    Fix build with OpenSSL 1.1 without NPN
    Omission in 72ed233167b10d3a488d30a8ec3a17e412a7dd69

--- Modules/_ssl.c
+++ Modules/_ssl.c
@@ -2048,7 +2048,7 @@ static PyMethodDef PySSLMethods[] = {
      PySSL_peercert_doc},
     {"cipher", (PyCFunction)PySSL_cipher, METH_NOARGS},
     {"version", (PyCFunction)PySSL_version, METH_NOARGS},
-#ifdef OPENSSL_NPN_NEGOTIATED
+#if defined(OPENSSL_NPN_NEGOTIATED) && !defined(OPENSSL_NO_NEXTPROTONEG)
     {"selected_npn_protocol", (PyCFunction)PySSL_selected_npn_protocol, METH_NOARGS},
 #endif
 #ifdef HAVE_ALPN
