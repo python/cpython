a66b08c066ba3c1e92c646b7356ee4477b49ce45
INADA Naoki <songofacandy@gmail.com>
2018-01-27 14:47:16 +0900

    Build on Windows

--- PC/config.c
+++ PC/config.c
@@ -5,6 +5,7 @@
 
 #include "Python.h"
 
+extern PyObject* PyInit__abc(void);
 extern PyObject* PyInit_array(void);
 extern PyObject* PyInit_audioop(void);
 extern PyObject* PyInit_binascii(void);
@@ -78,6 +79,7 @@ extern PyObject* PyInit_imp(void);
 
 struct _inittab _PyImport_Inittab[] = {
 
+    {"_abc", PyInit__abc},
     {"array", PyInit_array},
     {"_ast", PyInit__ast},
     {"audioop", PyInit_audioop},
