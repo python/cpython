3bb6255c2fba85d31d0673d766fbdc812b2ff849
Eric V. Smith <eric@trueblade.com>
2018-01-27 15:42:03 -0500

    More work on all possible cases of hash, eq, order. Add a table to data-drive the decision making process. The table matches what we talked  about in bpo-32513.

--- Lib/dataclasses.py
+++ Lib/dataclasses.py
@@ -18,90 +18,140 @@ __all__ = ['dataclass',
            'is_dataclass',
            ]
 
-# Conditions for adding methods:
-# added = method is added
-#       = method is not added
-# raise = TypeError is raised
+# Conditions for adding methods.  The boxes indicate what action the
+#  dataclass decorator takes.  For all of these tables, when I talk
+#  about init=, repr=, eq=, order=, hash=, or frozen=, I'm referring
+#  to the arguments to the @dataclass decorator.  When checking if a
+#  dunder method already exists, I mean check for an entry in the
+#  class's __dict__.  I never check to see if an attribute is defined
+#  in a base class.
+
+# Key:
+# +=========+=========================================+
+# + Value   | Meaning                                 |
+# +=========+=========================================+
+# | <blank> | No action: no method is added.          |
+# +---------+-----------------------------------------+
+# | add     | Generated method is added.              |
+# +---------+-----------------------------------------+
+# | add*    | Generated method is added only if the   |
+# |         |  existing attribute is None and if the  |
+# |         |  user supplied a __eq__ method in the   |
+# |         |  class definition.                      |
+# +---------+-----------------------------------------+
+# | raise   | TypeError is raised.                    |
+# +---------+-----------------------------------------+
+# | None    | Attribute is set to None.               |
+# +=========+=========================================+
 
 # __init__
 #
-#   +--- init=?
+#   +--- init= parameter
 #   |
-#   v    | yes   | no    |  <--- class has __init__?
-# -------+-------+-------+
-#  False |       |       |
-# -------+-------+-------+
-#  True  |       | added |  <- the default
-# -------+-------+-------+
+#   v     |       |       |
+#         |  no   |  yes  |  <--- class has __init__ in __dict__?
+# +=======+=======+=======+
+# | False |       |       |
+# +-------+-------+-------+
+# | True  | add   |       |  <- the default
+# +=======+=======+=======+
 
 # __repr__
 #
-#   +--- repr=?
-#   |
-#   v    | yes   | no    |  <--- class has __repr__?
-# -------+-------+-------+
-#  False |       |       |
-# -------+-------+-------+
-#  True  |       | added |  <- the default
-# -------+-------+-------+
+#    +--- repr= parameter
+#    |
+#    v    |       |       |
+#         |  no   |  yes  |  <--- class has __repr__ in __dict__?
+# +=======+=======+=======+
+# | False |       |       |
+# +-------+-------+-------+
+# | True  | add   |       |  <- the default
+# +=======+=======+=======+
+
 
 # __setattr__
 # __delattr__
 #
-#   +--- frozen=?
-#   |
-#   v    | yes   | no    |  <--- class has __setattr__ or __delattr__?
-# -------+-------+-------+
-#  False |       |       |  <- the default
-# -------+-------+-------+
-#  True  | raise | added |
-# -------+-------+-------+
+#    +--- frozen= parameter
+#    |
+#    v    |       |       |
+#         |  no   |  yes  |  <--- class has __setattr__ or __delattr__ in __dict__?
+# +=======+=======+=======+
+# | False |       |       |  <- the default
+# +-------+-------+-------+
+# | True  | add   | raise |
+# +=======+=======+=======+
+# Raise because not adding these methods would break the "frozen-ness"
+#  of the class.
 
 # __eq__
 #
-#   +--- eq=?
-#   |
-#   v    | yes   | no    |  <--- class has __eq__?
-# -------+-------+-------+
-#  False |       |       |
-# -------+-------+-------+
-#  True  |       | added |  <- the default
-# -------+-------+-------+
+#    +--- eq= parameter
+#    |
+#    v    |       |       |
+#         |  no   |  yes  |  <--- class has __eq__ in __dict__?
+# +=======+=======+=======+
+# | False |       |       |
+# +-------+-------+-------+
+# | True  | add   |       |  <- the default
+# +=======+=======+=======+
 
 # __lt__
 # __le__
 # __gt__
 # __ge__
 #
-#   +--- order=?
-#   |
-#   v    | yes   | no    |  <--- class has comparison method?
-# -------+-------+-------+
-#  False |       |       |  <- the default
-# -------+-------+-------+
-#  True  | raise | added |
-# -------+-------+-------+
+#    +--- order= parameter
+#    |
+#    v    |       |       |
+#         |  no   |  yes  |  <--- class has any comparison method in __dict__?
+# +=======+=======+=======+
+# | False |       |       |  <- the default
+# +-------+-------+-------+
+# | True  | add   | raise |
+# +=======+=======+=======+
+# Raise because to allow this case would interfere with using
+#  functools.total_ordering.
 
 # __hash__
 
-#   +------------------- hash=?
-#   |       +----------- eq=?
-#   |       |       +--- frozen=?
-#   |       |       |
-#   v    |  v    |  v    | yes   | no    |  <--- class has __hash__?
-# -------+-------+-------+-------+-------+
-#  False | False | False |       |       |
-#  False | False | True  |       |       |
-#  False | True  | False |       |       |
-#  False | True  | True  |       |       |
-#  True  | False | False |       | added |
-#  True  | False | True  |       | added |
-#  True  | True  | False |       | added |
-#  True  | True  | True  |       | added |
-#  None  | False | False |       |       |
-#  None  | False | True  |       |       |
-#  None  | True  | False |       | None  | <-- the default
-#  None  | True  | True  |       | added |
+#      +------------------- hash= parameter
+#      |       +----------- eq= parameter
+#      |       |       +--- frozen= parameter
+#      |       |       |
+#      v       v       v    |        |        |
+#                           |   no   |  yes   |  <--- class has __hash__ in __dict__?
+# +=========+=======+=======+========+========+
+# | 1 None  | False | False |        |        | No __eq__, use the base class __hash__
+# +---------+-------+-------+--------+--------+
+# | 2 None  | False | True  |        |        | No __eq__, use the base class __hash__
+# +---------+-------+-------+--------+--------+
+# | 3 None  | True  | False | None   |        | <-- the default, not hashable
+# +---------+-------+-------+--------+--------+
+# | 4 None  | True  | True  | add    | add*   | Frozen, so hashable
+# +---------+-------+-------+--------+--------+
+# | 5 False | False | False |        |        |
+# +---------+-------+-------+--------+--------+
+# | 6 False | False | True  |        |        |
+# +---------+-------+-------+--------+--------+
+# | 7 False | True  | False |        |        |
+# +---------+-------+-------+--------+--------+
+# | 8 False | True  | True  |        |        |
+# +---------+-------+-------+--------+--------+
+# | 9 True  | False | False | add    | add*   | Has no __eq__, but hashable
+# +---------+-------+-------+--------+--------+
+# |10 True  | False | True  | add    | add*   | Has no __eq__, but hashable
+# +---------+-------+-------+--------+--------+
+# |11 True  | True  | False | add    | add*   | Not frozen, but hashable
+# +---------+-------+-------+--------+--------+
+# |12 True  | True  | True  | add    | add*   | Frozen, so hashable
+# +=========+=======+=======+========+========+
+# For boxes that are blank, __hash__ is untouched and therefore
+#  inherited from the base class.  If the base is object, then
+#  id-based hashing is used.
+# Note that a class may have already __hash__=None if it specified an
+#  __eq__ method in the class body (not one that was created by
+#  @dataclass).
 
 
 # Raised when an attempt is made to modify a frozen class.
@@ -498,8 +548,9 @@ def _find_fields(cls):
             for a_name, a_type in annotations.items()]
 
 
-def _set_attribute(cls, name, value):
-    exists = name in cls.__dict__
+def _set_new_attribute(cls, name, value):
+    # Never overwrites an existing attribute.  Returns True if the
+    #  attribute already exists.
     if name in cls.__dict__:
         return True
     setattr(cls, name, value)
@@ -554,6 +605,9 @@ def _process_class(cls, repr, eq, order, hash, init, frozen):
     #  be inherited down.
     is_frozen = frozen or cls.__setattr__ is _frozen_setattr
 
+    # Was this class defined with an __eq__?  Used in __hash__ logic.
+    auto_hash_test= '__eq__' in cls.__dict__ and getattr(cls.__dict__, '__hash__', MISSING) is None
+
     # If we're generating ordering methods, we must be generating
     #  the eq methods.
     if order and not eq:
@@ -566,15 +620,15 @@ def _process_class(cls, repr, eq, order, hash, init, frozen):
         # Include InitVars and regular fields (so, not ClassVars).
         flds = [f for f in fields.values()
                 if f._field_type in (_FIELD, _FIELD_INITVAR)]
-        _set_attribute(cls, '__init__',
-                       _init_fn(flds,
-                                is_frozen,
-                                has_post_init,
-                                # The name to use for the "self" param
-                                #  in __init__.  Use "self" if possible.
-                                '__dataclass_self__' if 'self' in fields
-                                    else 'self',
-                                ))
+        _set_new_attribute(cls, '__init__',
+                           _init_fn(flds,
+                                    is_frozen,
+                                    has_post_init,
+                                    # The name to use for the "self" param
+                                    #  in __init__.  Use "self" if possible.
+                                    '__dataclass_self__' if 'self' in fields
+                                            else 'self',
+                          ))
 
     # Get the fields as a list, and include only real fields.  This is
     #  used in all of the following methods.
@@ -582,7 +636,7 @@ def _process_class(cls, repr, eq, order, hash, init, frozen):
 
     if repr:
         flds = [f for f in field_list if f.repr]
-        _set_attribute(cls, '__repr__', _repr_fn(flds))
+        _set_new_attribute(cls, '__repr__', _repr_fn(flds))
 
     if eq:
         # Create _eq__ method.  There's no need for a __ne__ method,
@@ -590,8 +644,9 @@ def _process_class(cls, repr, eq, order, hash, init, frozen):
         flds = [f for f in field_list if f.compare]
         self_tuple = _tuple_str('self', flds)
         other_tuple = _tuple_str('other', flds)
-        _set_attribute(cls, '__eq__', _cmp_fn('__eq__', '==',
-                                              self_tuple, other_tuple))
+        _set_new_attribute(cls, '__eq__',
+                           _cmp_fn('__eq__', '==',
+                                   self_tuple, other_tuple))
 
     if order:
         # Create and set the ordering methods.
@@ -603,8 +658,8 @@ def _process_class(cls, repr, eq, order, hash, init, frozen):
                          ('__gt__', '>'),
                          ('__ge__', '>='),
                          ]:
-            if _set_attribute(cls, name,
-                              _cmp_fn(name, op, self_tuple, other_tuple)):
+            if _set_new_attribute(cls, name,
+                                  _cmp_fn(name, op, self_tuple, other_tuple)):
                 raise TypeError(f'Cannot overwrite attribute {name} '
                                 f'in {cls.__name__}. Consider using '
                                 'functools.total_ordering')
@@ -612,30 +667,40 @@ def _process_class(cls, repr, eq, order, hash, init, frozen):
     if is_frozen:
         for name, fn in [('__setattr__', _frozen_setattr),
                          ('__delattr__', _frozen_delattr)]:
-            if _set_attribute(cls, name, fn):
+            if _set_new_attribute(cls, name, fn):
                 raise TypeError(f'Cannot overwrite attribute {name} '
                                 f'in {cls.__name__}')
 
-    generate_hash = False
-    if hash is None:
-        if eq and frozen:
-            # Generate a hash function.
-            generate_hash = True
-        elif eq and not frozen:
-            # Not hashable.
-            _set_attribute(cls, '__hash__', None)
-        elif not eq:
-            # Otherwise, use the base class definition of hash().  That is,
-            #  don't set anything on this class.
-            pass
-        else:
-            assert "can't get here"
+    # Decide if/how we're going to create a hash function.
+    generate_hash = {(None,  False, False): ('',     ''),
+                     (None,  False, True):  ('',     ''),
+                     (None,  True,  False): ('none', ''),
+                     (None,  True,  True):  ('fn',   'fn-x'),
+                     (False, False, False): ('',     ''),
+                     (False, False, True):  ('',     ''),
+                     (False, True,  False): ('',     ''),
+                     (False, True,  True):  ('',     ''),
+                     (True,  False, False): ('fn',   'fn-x'),
+                     (True,  False, True):  ('fn',   'fn-x'),
+                     (True,  True,  False): ('fn',   'fn-x'),
+                     (True,  True,  True):  ('fn',   'fn-x'),
+                     }[hash if hash is None else bool(hash),   # Force bool() if not None.
+                       bool(eq),
+                       bool(frozen)]['__hash__' in cls.__dict__]
+    # No need to call _set_new_attribute here, since we already know if
+    #  we're overwriting a __hash__ or not.
+    if generate_hash == '':
+        # Do nothing.
+        pass
+    elif generate_hash == 'none':
+        cls.__hash__ = None
+    elif generate_hash in ('fn', 'fn-x'):
+        if generate_hash == 'fn' or auto_hash_test:
+            flds = [f for f in field_list
+                    if (f.compare if f.hash is None else f.hash)]
+            cls.__hash__ = _hash_fn(flds)
     else:
-        generate_hash = hash
-    if generate_hash:
-        flds = [f for f in field_list
-                if (f.compare if f.hash is None else f.hash)]
-        _set_attribute(cls, '__hash__', _hash_fn(flds))
+        assert f"can't get here: {generate_hash}"
 
     if not getattr(cls, '__doc__'):
         # Create a class doc-string.
