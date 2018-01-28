dd2abdaa077a6109f0352cb6bf3aab8d1c8fc176
Ivan Levkivskyi <levkivslyi@gmail.com>
2018-01-28 18:44:30 +0000

    Split the class into two separate implementations

--- Lib/abc.py
+++ Lib/abc.py
@@ -124,7 +124,7 @@ class abstractproperty(property):
     __isabstractmethod__ = True
 
 
-class ABCMeta(type):
+class _Py_ABCMeta(type):
     """Metaclass for defining Abstract Base Classes (ABCs).
 
     Use this metaclass to create an ABC.  An ABC can be subclassed
@@ -143,14 +143,10 @@ class ABCMeta(type):
     # negative cache to be cleared before its next use.
     # Note: this counter is private. Use `abc.get_cache_token()` for
     #       external code.
-    if not _C_speedup:
-        _abc_invalidation_counter = 0
+    _abc_invalidation_counter = 0
 
     def __new__(mcls, name, bases, namespace, **kwargs):
         cls = super().__new__(mcls, name, bases, namespace, **kwargs)
-        if _C_speedup:
-            _abc_init(cls)
-            return cls
         # Compute set of abstract method names
         abstracts = {name
                      for name, value in namespace.items()
@@ -190,15 +186,6 @@ class ABCMeta(type):
         """Debug helper to print the ABC registry."""
         print(f"Class: {cls.__module__}.{cls.__qualname__}", file=file)
         print(f"Inv. counter: {get_cache_token()}", file=file)
-        if _C_speedup:
-            (_abc_registry, _abc_cache, _abc_negative_cache,
-             _abc_negative_cache_version) = _get_dump(cls)
-            print(f"_abc_registry: {_abc_registry!r}", file=file)
-            print(f"_abc_cache: {_abc_cache!r}", file=file)
-            print(f"_abc_negative_cache: {_abc_negative_cache!r}", file=file)
-            print(f"_abc_negative_cache_version: {_abc_negative_cache_version!r}",
-                  file=file)
-            return
         for name in cls.__dict__:
             if name.startswith("_abc_"):
                 value = getattr(cls, name)
@@ -261,10 +248,48 @@ class ABCMeta(type):
         cls._abc_negative_cache.add(subclass)
         return False
 
+
+class _C_ABCMeta(type):
+    def __new__(mcls, name, bases, namespace, **kwargs):
+        cls = super().__new__(mcls, name, bases, namespace, **kwargs)
+        _abc_init(cls)
+        return cls
+
+    def register(cls, subclass):
+        """Register a virtual subclass of an ABC.
+
+        Returns the subclass, to allow usage as a class decorator.
+        """
+        return _abc_register(cls, subclass)
+
+    def __instancecheck__(cls, instance):
+        """Override for isinstance(instance, cls)."""
+        return _abc_instancecheck(cls, instance)
+
+    def __subclasscheck__(cls, subclass):
+        """Override for issubclass(subclass, cls)."""
+        return _abc_subclasscheck(cls, subclass)
+
+    def _dump_registry(cls, file=None):
+        """Debug helper to print the ABC registry."""
+        print(f"Class: {cls.__module__}.{cls.__qualname__}", file=file)
+        print(f"Inv. counter: {get_cache_token()}", file=file)
+        (_abc_registry, _abc_cache, _abc_negative_cache,
+         _abc_negative_cache_version) = _get_dump(cls)
+        print(f"_abc_registry: {_abc_registry!r}", file=file)
+        print(f"_abc_cache: {_abc_cache!r}", file=file)
+        print(f"_abc_negative_cache: {_abc_negative_cache!r}", file=file)
+        print(f"_abc_negative_cache_version: {_abc_negative_cache_version!r}",
+              file=file)
+
+
 if _C_speedup:
-    ABCMeta.register = _abc_register
-    ABCMeta.__instancecheck__ = _abc_instancecheck
-    ABCMeta.__subclasscheck__ = _abc_subclasscheck
+    ABCMeta = _C_ABCMeta
+else:
+    ABCMeta = _Py_ABCMeta
+
+ABCMeta.__doc__ = _Py_ABCMeta.__doc__
+ABCMeta.__name__ = ABCMeta.__qualname__ = 'ABCMeta'
 
 
 class ABC(metaclass=ABCMeta):
