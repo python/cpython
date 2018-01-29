88d112b4ec6420f7001c919288600015b8896f3b
Xiang Zhang <angwerzx@126.com>
2018-01-29 11:32:12 +0800

    bpo-32688: Make why the line is commented out clear (GH-5405)

--- Python/ceval.c
+++ Python/ceval.c
@@ -3525,7 +3525,7 @@ fast_yield:
                                 tstate, f,
                                 PyTrace_RETURN, retval)) {
                 Py_CLEAR(retval);
-                /* why = WHY_EXCEPTION; */
+                /* why = WHY_EXCEPTION; useless yet but cause compiler warnings */
             }
         }
     }
