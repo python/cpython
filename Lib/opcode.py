19d808a96cd97f5f79234c1e78993450ef1141f3
Mark Shannon <mark@hotpy.org>
2018-01-30 00:41:04 +0000

    bpo-32550. Remove the STORE_ANNOTATION bytecode. (GH-5181)

--- Lib/opcode.py
+++ Lib/opcode.py
@@ -169,7 +169,6 @@ def_op('STORE_FAST', 125)       # Local variable number
 haslocal.append(125)
 def_op('DELETE_FAST', 126)      # Local variable number
 haslocal.append(126)
-name_op('STORE_ANNOTATION', 127) # Index in name list
 
 def_op('RAISE_VARARGS', 130)    # Number of raise arguments (1, 2, or 3)
 def_op('CALL_FUNCTION', 131)    # #args
