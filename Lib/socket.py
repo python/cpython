bc1770af750683634bcd7fcaedef4430b4eeccf1
youngil.yang <youngil.yang@navercorp.com>
2018-01-31 15:18:47 +0900

    socket exception error handling change if e is None

--- Lib/socket.py
+++ Lib/socket.py
@@ -591,8 +591,9 @@ class SocketIO(io.RawIOBase):
                 self._timeout_occurred = True
                 raise
             except error as e:
-                if e.args[0] in _blocking_errnos:
-                    return None
+                if e.args:
+                    if e.args[0] in _blocking_errnos:
+                        return None
                 raise
 
     def write(self, b):
