20d140e2e99b249e141d63075463ce3a8e45a9a9
Alin Mindroc <mindroca@amazon.com>
2018-02-03 22:55:43 +0100

    fix error message when scrypt function returns 0

--- Modules/_hashopenssl.c
+++ Modules/_hashopenssl.c
@@ -820,7 +820,8 @@ _hashlib_scrypt_impl(PyObject *module, Py_buffer *password, Py_buffer *salt,
     if (!retval) {
         /* sorry, can't do much better */
         PyErr_SetString(PyExc_ValueError,
-                        "Invalid paramemter combination for n, r, p, maxmem.");
+                        "Invalid parameter combination for n, r, p, maxmem. "
+                        "Make sure maxmem is high enough.");
         return NULL;
    }
 
