c242ad4f5e78ef6548e9aac15b0386e044df0f97
Cheryl Sabella <cheryl.sabella@gmail.com>
2018-02-04 11:30:16 -0500

    Add Startup tab to configdialog

--- Lib/idlelib/idle_test/test_config.py
+++ Lib/idlelib/idle_test/test_config.py
@@ -357,11 +357,11 @@ class IdleConfTest(unittest.TestCase):
 
         self.assertCountEqual(
             conf.GetSectionList('default', 'main'),
-            ['General', 'EditorWindow', 'Indent', 'Theme',
+            ['General', 'EditorWindow', 'ShellWindow', 'Indent', 'Theme',
              'Keys', 'History', 'HelpFiles'])
         self.assertCountEqual(
             conf.GetSectionList('user', 'main'),
-            ['General', 'EditorWindow', 'Indent', 'Theme',
+            ['General', 'EditorWindow', 'ShellWindow', 'Indent', 'Theme',
              'Keys', 'History', 'HelpFiles'])
 
         with self.assertRaises(config.InvalidConfigSet):
