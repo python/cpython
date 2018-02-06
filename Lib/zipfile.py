b4981c2193bf8e1bb9d8ef7a3445a42d59a822a1
Peter Bengtsson <mail@peterbe.com>
2018-02-06 12:22:45 -0500

    improvement to doc string and rst doc

--- Lib/zipfile.py
+++ Lib/zipfile.py
@@ -1580,7 +1580,7 @@ class ZipFile:
         """Extract all members from the archive to the current working
            directory. `path' specifies a different directory to extract to.
            `members' is optional and must be a subset of the list returned
-           by infolist().
+           by infolist() or namelist().
         """
         if members is None:
             members = self.infolist()
