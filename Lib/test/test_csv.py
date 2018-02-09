48d8f06c9bb9941b93159cea2bb89157b3f9d878
Serhiy Storchaka <storchaka@gmail.com>
2018-02-09 20:00:49 +0200

    bpo-30157: Fix csv.Sniffer.sniff() regex pattern. (GH-5601)
    Co-authored-by: Jake Davis <jcdavis@awedge.net>

--- Lib/test/test_csv.py
+++ Lib/test/test_csv.py
@@ -986,6 +986,16 @@ Stonecutters Seafood and Chop House+ Lemont+ IL+ 12/19/02+ Week Back
         self.assertEqual(sniffer.has_header(self.header2 + self.sample8),
                          True)
 
+    def test_guess_quote_and_delimiter(self):
+        sniffer = csv.Sniffer()
+        for header in (";'123;4';", "'123;4';", ";'123;4'", "'123;4'"):
+            with self.subTest(header):
+                dialect = sniffer.sniff(header, ",;")
+                self.assertEqual(dialect.delimiter, ';')
+                self.assertEqual(dialect.quotechar, "'")
+                self.assertIs(dialect.doublequote, False)
+                self.assertIs(dialect.skipinitialspace, False)
+
     def test_sniff(self):
         sniffer = csv.Sniffer()
         dialect = sniffer.sniff(self.sample1)
