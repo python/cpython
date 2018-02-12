743b6c07e8c79ffee38ab77f1284b542d865ce6e
Miss Islington (bot) <31488909+miss-islington@users.noreply.github.com>
2018-02-11 23:35:56 -0800

    Fix typo in Include/objimpl.h, the word "has" was missing (GH-5568) (GH-5570)
    It now reads: ...be aware that Python has no control over...
    (cherry picked from commit 517da1e58f4c489d4b31579852cde5f7113da08e)
    
    Co-authored-by: Alexey <forestbiiird@gmail.com>

--- Include/objimpl.h
+++ Include/objimpl.h
@@ -56,7 +56,7 @@ must use the platform malloc heap(s), or shared memory, or C++ local storage or
 operator new), you must first allocate the object with your custom allocator,
 then pass its pointer to PyObject_{Init, InitVar} for filling in its Python-
 specific fields:  reference count, type pointer, possibly others.  You should
-be aware that Python no control over these objects because they don't
+be aware that Python has no control over these objects because they don't
 cooperate with the Python memory manager.  Such objects may not be eligible
 for automatic garbage collection and you have to make sure that they are
 released accordingly whenever their destructor gets called (cf. the specific
