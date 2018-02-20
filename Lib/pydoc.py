bd313ff1d97212ce3efc7a690fa4f3bdf7167813
Sanyam Khurana <sanyam.khurana01@gmail.com>
2018-02-21 00:12:56 +0530

    bpo-8525: Don't display subclasses for object

--- Lib/pydoc.py
+++ Lib/pydoc.py
@@ -1259,7 +1259,7 @@ location listed above.
             cls.__name__ for cls in object.__subclasses__()
             if cls.__module__ == 'builtins'
         ]
-        if subclasses:
+        if subclasses and object.__class__ != type:
             push("Built-in subclasses:")
             for subclassname in sorted(subclasses):
                 push('    ' + subclassname)
