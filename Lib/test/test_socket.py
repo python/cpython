f43db09a3cb0ee6b1fb5afd461bfda3e5ff141f2
animalize <malincns@163.com>
2018-02-20 08:59:12 +0800

    For Changes requested

--- Lib/test/test_socket.py
+++ Lib/test/test_socket.py
@@ -5894,20 +5894,12 @@ class TestMSWindowsTCPFlags(unittest.TestCase):
                        'TCP_KEEPINTVL'
                        }
 
-    def testNewTCPFlags(self):
+    def test_new_tcp_flags(self):
         provided = [s for s in dir(socket) if s.startswith('TCP')]
         unknown = [s for s in provided if s not in self.knownTCPFlags]
 
-        if unknown:
-            msg = ("\nFound new TCP flags %s, probably you are building"
-                   " CPython with a newer Windows SDK than official build's.\n"
-                   "If you insist on building CPython with the current "
-                   "Windows SDK, maybe you need to remove them on older "
-                   "version MS-Windows during run-time via hard code patch, "
-                   "see issue32394.\n"
-                   "Full avaliable TCP flags on this system: %s"
-                    ) % (unknown, provided)
-            raise Exception(msg)
+        self.assertEqual([], unknown,
+            "New TCP flags were discovered. See bpo-32394 for more information")
 
 def test_main():
     tests = [GeneralModuleTests, BasicTCPTest, TCPCloserTest, TCPTimeoutTest,
--- Modules/socketmodule.c
+++ Modules/socketmodule.c
@@ -305,16 +305,15 @@ http://cvsweb.netbsd.org/bsdweb.cgi/src/lib/libc/net/getaddrinfo.c.diff?r1=1.82&
 /* Provides the IsWindows7SP1OrGreater() function */
 #include <VersionHelpers.h>
 
-/* This block removes some flags on older version Windows during run-time.
+/* remove some flags on older version Windows during run-time.
    https://msdn.microsoft.com/en-us/library/windows/desktop/ms738596.aspx */
-#if 1
 typedef struct {
     DWORD build_number;  /* available starting with this Win10 BuildNumber */
     const char flag_name[20];
 } FlagRuntimeInfo;
 
 /* IMPORTANT: make sure the list ordered by descending build_number */
-static FlagRuntimeInfo flags[] = {
+static FlagRuntimeInfo win_runtime_flags[] = {
     /* available starting with Windows 10 1709 */
     {16299, "TCP_KEEPIDLE"},
     {16299, "TCP_KEEPINTVL"},
@@ -348,8 +347,8 @@ remove_unusable_flags(PyObject *m)
     VER_SET_CONDITION(dwlConditionMask, VER_MINORVERSION, VER_GREATER_EQUAL);
     VER_SET_CONDITION(dwlConditionMask, VER_BUILDNUMBER, VER_GREATER_EQUAL);
 
-    for (int i = 0; i < sizeof(flags)/sizeof(FlagRuntimeInfo); i++) {
-        info.dwBuildNumber = flags[i].build_number;
+    for (int i=0; i<sizeof(win_runtime_flags)/sizeof(FlagRuntimeInfo); i++) {
+        info.dwBuildNumber = win_runtime_flags[i].build_number;
         /* greater than or equal to the specified version? 
            Compatibility Mode will not cheat VerifyVersionInfo(...) */
         if (VerifyVersionInfo(
@@ -359,14 +358,17 @@ remove_unusable_flags(PyObject *m)
             break;
         }
         else {
-            if (PyDict_GetItemString(dict, flags[i].flag_name) != NULL) {
-                PyDict_DelItemString(dict, flags[i].flag_name);
+            if (PyDict_GetItemString(
+                    dict,
+                    win_runtime_flags[i].flag_name) != NULL) {
+                PyDict_DelItemString(
+                    dict,
+                    win_runtime_flags[i].flag_name);
             }
         }
     }
     return m;
 }
-#endif
 
 #endif
 
@@ -7948,8 +7950,9 @@ PyInit__socket(void)
 #endif
 
 #ifdef MS_WINDOWS
-    return remove_unusable_flags(m);
-#else
-    return m;
+    /* removes some flags on older version Windows during run-time */
+    remove_unusable_flags(m);
 #endif
+
+    return m;
 }
