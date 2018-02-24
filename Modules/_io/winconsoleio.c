5519874eb9be7a78938aeb568fe0daf57a25a67a
Serhiy Storchaka <storchaka@gmail.com>
2018-02-24 18:55:51 +0200

    bpo-31966: Fixed WindowsConsoleIO.write() for writing empty data. (GH-5754)

--- Modules/_io/winconsoleio.c
+++ Modules/_io/winconsoleio.c
@@ -964,6 +964,9 @@ _io__WindowsConsoleIO_write_impl(winconsoleio *self, Py_buffer *b)
     if (!self->writable)
         return err_mode("writing");
 
+    if (!b->len) {
+        return PyLong_FromLong(0);
+    }
     if (b->len > BUFMAX)
         len = BUFMAX;
     else
