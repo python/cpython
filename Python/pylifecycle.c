084256d43d3667c48b8518035f8aa5be6fe8b378
Oscar Shi <Hao.shi@windriver.com>
2018-03-02 11:53:09 -0600

    switch default encoding to ascii

--- Python/pylifecycle.c
+++ Python/pylifecycle.c
@@ -277,8 +277,10 @@ get_locale_encoding(void)
         return NULL;
     }
     return get_codec_name(codeset);
-#elif defined(__ANDROID__) || defined(__VXWORKS__)
+#elif defined(__ANDROID__) 
     return get_codec_name("UTF-8");
+#elif defined(__VXWORKS__)
+    return get_codec_name("ascii");
 #else
     PyErr_SetNone(PyExc_NotImplementedError);
     return NULL;
