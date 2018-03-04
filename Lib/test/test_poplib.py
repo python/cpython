d36d8adc3b515a473166d6d69be61c0e4cf02af2
Jamie Davis <davisjam@vt.edu>
2018-03-04 00:33:32 -0500

    bpo-32981: Fix catastrophic backtracking vulns (#5955)
    * Prevent low-grade poplib REDOS (CVE-2018-1060)
    
    The regex to test a mail server's timestamp is susceptible to
    catastrophic backtracking on long evil responses from the server.
    
    Happily, the maximum length of malicious inputs is 2K thanks
    to a limit introduced in the fix for CVE-2013-1752.
    
    A 2KB evil response from the mail server would result in small slowdowns
    (milliseconds vs. microseconds) accumulated over many apop calls.
    This is a potential DOS vector via accumulated slowdowns.
    
    Replace it with a similar non-vulnerable regex.
    
    The new regex is RFC compliant.
    The old regex was non-compliant in edge cases.
    
    * Prevent difflib REDOS (CVE-2018-1061)
    
    The default regex for IS_LINE_JUNK is susceptible to
    catastrophic backtracking.
    This is a potential DOS vector.
    
    Replace it with an equivalent non-vulnerable regex.
    
    Also introduce unit and REDOS tests for difflib.
    
    Co-authored-by: Tim Peters <tim.peters@gmail.com>
    Co-authored-by: Christian Heimes <christian@python.org>

--- Lib/test/test_poplib.py
+++ Lib/test/test_poplib.py
@@ -308,8 +308,18 @@ class TestPOP3Class(TestCase):
     def test_rpop(self):
         self.assertOK(self.client.rpop('foo'))
 
-    def test_apop(self):
+    def test_apop_normal(self):
         self.assertOK(self.client.apop('foo', 'dummypassword'))
+
+    def test_apop_REDOS(self):
+        # Replace welcome with very long evil welcome.
+        # NB The upper bound on welcome length is currently 2048.
+        # At this length, evil input makes each apop call take
+        # on the order of milliseconds instead of microseconds.
+        evil_welcome = b'+OK' + (b'<' * 1000000)
+        with test_support.swap_attr(self.client, 'welcome', evil_welcome):
+            # The evil welcome is invalid, so apop should throw.
+            self.assertRaises(poplib.error_proto, self.client.apop, 'a', 'kb')
 
     def test_top(self):
         expected =  (b'+OK 116 bytes',
