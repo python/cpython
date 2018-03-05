3e999789f18df7b71a57cce706e652f4322e377c
Steve Dower <steve.dower@python.org>
2018-03-05 13:22:59 -0800

    Remove invalid test

--- Lib/test/test_os.py
+++ Lib/test/test_os.py
@@ -2181,8 +2181,6 @@ class Win32SymlinkTests(unittest.TestCase):
             ('\\' + path, segment),
             # overflow dest with relative src
             (segment, path),
-            # overflow dest when appending '\\' for join
-            (segment, path[:261]),
             # overflow when joining src
             (path[:180], path[:180]),
         ]
