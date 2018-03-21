e645bdb8865acdee24e3b9157896e49d4806b56c
Zackery Spytz <zspytz@gmail.com>
2018-03-21 00:02:37 -0600

    Fix typos in mmap() error messages (GH-6173)

--- Modules/mmapmodule.c
+++ Modules/mmapmodule.c
@@ -1067,7 +1067,7 @@ new_mmap_object(PyTypeObject *type, PyObject *args, PyObject *kwdict)
         return NULL;
     if (map_size < 0) {
         PyErr_SetString(PyExc_OverflowError,
-                        "memory mapped length must be postiive");
+                        "memory mapped length must be positive");
         return NULL;
     }
     if (offset < 0) {
@@ -1253,7 +1253,7 @@ new_mmap_object(PyTypeObject *type, PyObject *args, PyObject *kwdict)
 
     if (map_size < 0) {
         PyErr_SetString(PyExc_OverflowError,
-                        "memory mapped length must be postiive");
+                        "memory mapped length must be positive");
         return NULL;
     }
     if (offset < 0) {
