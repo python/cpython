a1853a66199b0710bb29b9ea55b1d13f809d8f5c
Mark Nemec <mrknmc@me.com>
2018-03-25 16:41:46 +0100

    Mark ThreadPoolExecutor shutdown from worker thread if shutting down

--- Lib/concurrent/futures/thread.py
+++ Lib/concurrent/futures/thread.py
@@ -87,6 +87,10 @@ def _worker(executor_reference, work_queue, initializer, initargs):
             #   - The executor that owns the worker has been collected OR
             #   - The executor that owns the worker has been shutdown.
             if _shutdown or executor is None or executor._shutdown:
+                # Flag the executor as shutting down as early as possible if it
+                # is not gc-ed yet.
+                if executor is not None:
+                    executor._shutdown = True
                 # Notice other workers
                 work_queue.put(None)
                 return
