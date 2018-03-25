7ab57c3df974d95d57d4379bba159429c8f2c204
Garvit Khatri <garvitdelhi@gmail.com>
2018-03-25 13:30:08 +0530

    Fixing misc entry filename and typo

--- Lib/tkinter/test/test_ttk/test_widgets.py
+++ Lib/tkinter/test/test_ttk/test_widgets.py
@@ -1667,7 +1667,7 @@ class TreeviewTest(AbstractWidgetTest, unittest.TestCase):
         self.assertEqual(itemid, '0')
         itemid = self.tv.insert('', 'end', 0.0)
         self.assertEqual(itemid, '0.0')
-        # this is because False is resolveds to 0 and element with 0 id is already present
+        # this is because False resolves to 0 and element with 0 iid is already present
         self.assertRaises(tkinter.TclError, self.tv.insert, '', 'end',
             False)
         self.assertRaises(tkinter.TclError, self.tv.insert, '', 'end',
