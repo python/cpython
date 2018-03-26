f86b0984d0f2489c83087eab199ac4e877becf84
Miss Islington (bot) <31488909+miss-islington@users.noreply.github.com>
2018-03-26 03:57:37 -0700

    Fix typo and edit for clarity in the docstrings of sys.float_info. (GH-2251)
    (cherry picked from commit 0301c9bdd1ebd788d1334cf3fe06c48f35bab0dc)
    
    Co-authored-by: Stefano Taschini <taschini@users.noreply.github.com>

--- Objects/floatobject.c
+++ Objects/floatobject.c
@@ -1,4 +1,3 @@
-
 /* Float object implementation */
 
 /* XXX There should be overflow checks here, but it's hard to check
@@ -48,7 +47,7 @@ static PyStructSequence_Field floatinfo_fields[] = {
                     "is representable"},
     {"max_10_exp",      "DBL_MAX_10_EXP -- maximum int e such that 10**e "
                     "is representable"},
-    {"min",             "DBL_MIN -- Minimum positive normalizer float"},
+    {"min",             "DBL_MIN -- Minimum positive normalized float"},
     {"min_exp",         "DBL_MIN_EXP -- minimum int e such that radix**(e-1) "
                     "is a normalized float"},
     {"min_10_exp",      "DBL_MIN_10_EXP -- minimum int e such that 10**e is "
@@ -58,7 +57,7 @@ static PyStructSequence_Field floatinfo_fields[] = {
     {"epsilon",         "DBL_EPSILON -- Difference between 1 and the next "
                     "representable float"},
     {"radix",           "FLT_RADIX -- radix of exponent"},
-    {"rounds",          "FLT_ROUNDS -- addition rounds"},
+    {"rounds",          "FLT_ROUNDS -- rounding mode"},
     {0}
 };
 
