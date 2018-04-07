b9444ee63fdeef74a7987adb2ef175ca6b456d4a
Alex Gaynor <alex.gaynor@gmail.com>
2018-04-07 16:09:42 -0400

    bpo-29613: Added support for SameSite cookies (GH-6413)
    * bpo-29613: Added support for SameSite cookies
    
    Implemented as per draft
    https://tools.ietf.org/html/draft-west-first-party-cookies-07
    
    * Documented SameSite
    
    And suggestions by members.
    
    * Missing space :(
    
    * Updated News and contributors
    
    * Added version changed details.
    
    * Fix in documentation
    
    * fix in documentation
    
    * Clubbed test cases for same attribute into single.
    
    * Updates
    
    * Style nits + expand tests
    
    * review feedback

--- Lib/http/cookies.py
+++ Lib/http/cookies.py
@@ -281,6 +281,7 @@ class Morsel(dict):
         "secure"   : "Secure",
         "httponly" : "HttpOnly",
         "version"  : "Version",
+        "samesite" : "SameSite",
     }
 
     _flags = {'secure', 'httponly'}
