0a60e48412c430554219db3b3d3aa211c37550a6
nixawk <hap.ddup@gmail.com>
2018-04-16 22:02:21 -0500

    Fix secrets.randbelow docstring

--- Lib/secrets.py
+++ Lib/secrets.py
@@ -25,7 +25,7 @@ randbits = _sysrand.getrandbits
 choice = _sysrand.choice
 
 def randbelow(exclusive_upper_bound):
-    """Return a random int in the range [0, n)."""
+    """Return a random int in the range(0, n)."""
     if exclusive_upper_bound <= 0:
         raise ValueError("Upper bound must be positive.")
     return _sysrand._randbelow(exclusive_upper_bound)
