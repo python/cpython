fa108bb90e010f7c7d4bae3d42cd94e830e89dca
Serhiy Storchaka <storchaka@gmail.com>
2018-04-19 08:28:04 +0300

    bpo-33299: Return an object itself for some types in _PyCode_ConstantKey(). (GH-6513)

--- Python/compile.c
+++ Python/compile.c
@@ -5299,10 +5299,12 @@ dict_keys_inorder(PyObject *dict, Py_ssize_t offset)
         return NULL;
     while (PyDict_Next(dict, &pos, &k, &v)) {
         i = PyLong_AS_LONG(v);
-        /* The keys of the dictionary are tuples. (see compiler_add_o
-         * and _PyCode_ConstantKey). The object we want is always second,
-         * though. */
-        k = PyTuple_GET_ITEM(k, 1);
+        /* The keys of the dictionary can be tuples wrapping a contant.
+         * (see compiler_add_o and _PyCode_ConstantKey). In that case
+         * the object we want is always second. */
+        if (PyTuple_CheckExact(k)) {
+            k = PyTuple_GET_ITEM(k, 1);
+        }
         Py_INCREF(k);
         assert((i - offset) < size);
         assert((i - offset) >= 0);
