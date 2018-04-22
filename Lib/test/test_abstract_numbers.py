8766002e107cc0af7a7e165f0884421a3b4a58e5
Barry Devlin <devlinb@tcd.ie>
2018-04-22 15:35:29 +0100

    add test

--- Lib/test/test_abstract_numbers.py
+++ Lib/test/test_abstract_numbers.py
@@ -3,6 +3,7 @@
 import math
 import operator
 import unittest
+from random import random
 from numbers import Complex, Real, Rational, Integral
 
 class TestNumbers(unittest.TestCase):
@@ -38,6 +39,80 @@ class TestNumbers(unittest.TestCase):
         self.assertRaises(TypeError, operator.floordiv, c1, c2)
         self.assertRaises(TypeError, float, c1)
         self.assertRaises(TypeError, int, c1)
+
+
+class TestAbstractNumbers(unittest.TestCase):
+    def testComplex(self):
+
+        class SubComplex(Complex):
+            def __complex__(self):
+                pass
+
+            @property
+            def real(self):
+                pass
+
+            @property
+            def imag(self):
+                pass
+
+            def __add__(self, other):
+                pass
+
+            def __radd__(self, other):
+                pass
+
+            def __neg__(self):
+                pass
+
+            def __pos__(self):
+                pass
+
+            def __mul__(self, other):
+                pass
+
+            def __rmul__(self, other):
+                pass
+
+            def __truediv__(self, other):
+                pass
+
+            def __rtruediv__(self, other):
+                pass
+
+            def __pow__(self, exponent):
+                pass
+
+            def __rpow__(self, base):
+                pass
+
+            def __abs__(self):
+                pass
+
+            def conjugate(self):
+                pass
+
+            def __init__(self):
+                pass
+
+            def __eq__(self, other):
+                return isinstance(other, SubComplex) and self.imag == other.imag and self.real == other.real
+
+        sc = SubComplex()
+        self.assertIsInstance(sc, SubComplex)
+
+        nonInstances = [None, "hat", lambda x: x + 1]
+
+        for nonInstance in nonInstances:
+            self.assertNotIsInstance(nonInstance, Complex)
+
+        self.assertFalse(SubComplex.__bool__(0))
+        for i in range(100):
+            self.assertTrue(SubComplex.__bool__(random() + 1e-6))
+            x = random()
+            y = random()
+            self.assertEqual(SubComplex.__sub__(x, y), x - y)
+            self.assertEqual(SubComplex.__rsub__(x, y), - x + y)
 
 
 if __name__ == "__main__":
