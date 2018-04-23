7f4fcdf6e7bab9c144c0c818e8b21db32f359c1f
Ethan Smith <ethan@ethanhs.me>
2018-04-23 16:45:52 -0700

    Fix silly indentation mistake

--- Python/ast.c
+++ Python/ast.c
@@ -1691,7 +1691,7 @@ ast_for_decorated(struct compiling *c, const node *n)
     } else if (TYPE(CHILD(n, 1)) == async_funcdef) {
         return ast_for_async_funcdef(c, CHILD(n, 1), decorator_seq);
     } else {
-		Py_UNREACHABLE();
+        Py_UNREACHABLE();
     }
 }
 
