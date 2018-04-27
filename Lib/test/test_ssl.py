c368a78cab4075da7f64e8b8a40757ee6900e0d3
Pablo Galindo <pablogsal@gmail.com>
2018-04-27 11:44:08 +0100

    Clean the test fix and correct typo in NEWS entry

--- Lib/test/test_ssl.py
+++ Lib/test/test_ssl.py
@@ -6,7 +6,6 @@ import unittest
 from test import test_support as support
 from test.script_helper import assert_python_ok
 import asyncore
-import exceptions
 import socket
 import select
 import time
@@ -33,13 +32,6 @@ IS_OPENSSL_1_1 = not IS_LIBRESSL and ssl.OPENSSL_VERSION_INFO >= (1, 1, 0)
 
 def data_file(*name):
     return os.path.join(os.path.dirname(__file__), *name)
-
-def _can_decode_unicode(unicode_str):
-    try:
-        unicode_str.decode()
-        return True
-    except exceptions.UnicodeEncodeError:
-        return False
 
 # The custom key and certificate files used in test_ssl are generated
 # using Lib/test/make_ssl_certs.py.
@@ -996,8 +988,14 @@ class ContextTests(unittest.TestCase):
             ctx.load_verify_locations(cadata=b"broken")
 
 
-    @unittest.skipIf(not _can_decode_unicode(u'dhpäräm.pem'), "cannot decode unicode path")
     def test_load_dh_params(self):
+        filename = u'dhpäräm.pem'
+        fs_encoding = sys.getfilesystemencoding()
+        try:
+            filename.encode(fs_encoding)
+        except UnicodeEncodeError:
+            self.skipTest("filename %r cannot be encoded to the filesystem encoding %r" % (filename, fs_encoding))
+
         ctx = ssl.SSLContext(ssl.PROTOCOL_TLSv1)
         ctx.load_dh_params(DHFILE)
         if os.name != 'nt':
@@ -1010,7 +1008,7 @@ class ContextTests(unittest.TestCase):
         with self.assertRaises(ssl.SSLError) as cm:
             ctx.load_dh_params(CERTFILE)
         with support.temp_dir() as d:
-            fname = os.path.join(d, u'dhpäräm.pem')
+            fname = os.path.join(d, filename)
             shutil.copy(DHFILE, fname)
             ctx.load_dh_params(fname)
 
