b17c2a8a212bce05f4d417d3c0f08a4a0b51f98f
Serhiy Storchaka <storchaka@gmail.com>
2018-04-30 11:59:15 +0300

    Use long_long_meth as a getter.

--- Objects/longobject.c
+++ Objects/longobject.c
@@ -5286,12 +5286,6 @@ long_long_meth(PyObject *self, PyObject *Py_UNUSED(ignored))
     return long_long(self);
 }
 
-static PyObject *
-long_long_getter(PyObject *self, void *Py_UNUSED(ignored))
-{
-    return long_long(self);
-}
-
 static PyMethodDef long_methods[] = {
     {"conjugate",       long_long_meth, METH_NOARGS,
      "Returns self, the complex conjugate of any int."},
@@ -5319,7 +5313,7 @@ static PyMethodDef long_methods[] = {
 
 static PyGetSetDef long_getset[] = {
     {"real",
-     long_long_getter, (setter)NULL,
+     (getter)long_long_meth, (setter)NULL,
      "the real part of a complex number",
      NULL},
     {"imag",
@@ -5327,7 +5321,7 @@ static PyGetSetDef long_getset[] = {
      "the imaginary part of a complex number",
      NULL},
     {"numerator",
-     long_long_getter, (setter)NULL,
+     (getter)long_long_meth, (setter)NULL,
      "the numerator of a rational number in lowest terms",
      NULL},
     {"denominator",
