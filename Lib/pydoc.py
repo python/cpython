502683519b65f5f06026841bbd4b94b502405ed7
Andrés Delfino <adelfino@gmail.com>
2018-05-05 13:07:32 -0300

    [2.7] bpo-33422: Fix quotation marks getting deleted when looking up byte/string literals on pydoc. (GH-6701)
    Also update the list of string prefixes..
    (cherry picked from commit b2043bbe6034b53f5ad337887f4741b74b70b00d)
    
    Co-authored-by: Andrés Delfino <adelfino@gmail.com>

--- Lib/pydoc.py
+++ Lib/pydoc.py
@@ -1647,8 +1647,9 @@ class Helper:
     }
     # Either add symbols to this dictionary or to the symbols dictionary
     # directly: Whichever is easier. They are merged later.
+    _strprefixes = [p + q for p in ('b', 'r', 'u') for q in ("'", '"')]
     _symbols_inverse = {
-        'STRINGS' : ("'", "'''", "r'", "u'", '"""', '"', 'r"', 'u"'),
+        'STRINGS' : ("'", "'''", '"""', '"', *_strprefixes),
         'OPERATORS' : ('+', '-', '*', '**', '/', '//', '%', '<<', '>>', '&',
                        '|', '^', '~', '<', '>', '<=', '>=', '==', '!=', '<>'),
         'COMPARISON' : ('<', '>', '<=', '>=', '==', '!=', '<>'),
@@ -1811,7 +1812,9 @@ has the same effect as typing a particular string at the help> prompt.
                 if not request: break
             except (KeyboardInterrupt, EOFError):
                 break
-            request = strip(replace(request, '"', '', "'", ''))
++           if (len(request) > 2 and request[0] == request[-1] in ("'", '"')
++                   and request[0] not in request[1:-1]):
++               request = request[1:-1]
             if lower(request) in ('q', 'quit'): break
             self.help(request)
 
