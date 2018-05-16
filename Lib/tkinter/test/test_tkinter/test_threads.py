534c57d47e35871deff459dd2f593f436e2605e7
Ivan Pozdeev <vano@mail.mipt.ru>
2018-05-16 19:16:09 +0300

    replace test case

--- Lib/tkinter/test/test_tkinter/test_threads.py
+++ Lib/tkinter/test/test_tkinter/test_threads.py
@@ -1,10 +1,10 @@
-#!/usr/bin/python3
-
-import sys
-import time
-import threading
-import random
+from __future__ import print_function
 import tkinter
+import random
+import string
+import sys
+import threading
+import time
 
 import unittest
 class TestThreads(unittest.TestCase):
@@ -31,103 +31,46 @@ class TestThreads(unittest.TestCase):
 
 running = True
 
-class Track(threading.Thread):
-    """
-    Calculates coordinates for a ballistic track
-    with random angle and velocity
-    and fires the callback to draw each consecutive segment.
-    """
-    def __init__(self, track_number, draw_callback):
-        """
-        :param track_number: ordinal
-        :param draw_callback: fn(track_number, x, y)
-            that draws the extention of the specified track
-            to the specified point. The callback must keep track
-            of any previous coordinates itself.
-        """
-        threading.Thread.__init__(self)
-        #self.setDaemon(True)
-        self.track_number = track_number
-        self.draw_callback = draw_callback
-
+class EventThread(threading.Thread):
+    def __init__(self,target):
+        super(EventThread,self).__init__()
+        self.target = target
     def run(self):
-        #starting point for the track
-        y = 0.0001 #height
-        x = 999.0 #range
-        #initial velocities
-        yVel = 400. + random.random() * 200.
-        xVel = -200. - random.random() * 150.
-        # Stop drawing when the track hits the ground
-        while y > 0 and running:
-            #How long to sleep, in seconds, between track updates
-            time.sleep(0.01) #realism: >1   Fun: 0.01
+        while running:
+            time.sleep(0.02)
+            c = random.choice(string.ascii_letters)
+            self.target.event_generate(c)
 
-            yVel -=  9.8 #gravity, more or less
-            xVel *= .9998 #air resistance
+class Main(object):
+    def __init__(self):
+        self.root = tkinter.Tk()
+        self.root.bind('<Key>',dummy_handler)
+        self.threads=[]
 
-            y += yVel
-            x += xVel
+        self.t_cleanup = threading.Thread(target=self.tf_stop)
 
-            if running:
-                self.draw_callback(self.track_number, x, y)
-
-
-class App:
-    """
-    The main app logic
-    """
-    def __init__(self, window):
-        self.track_no = 0 #last index of track added
-        self.track_coordinates = {}  #coords of track tips
-        self.threads = []
-
-        self.window = window
-        self.frame = tkinter.Frame(window)
-        self.frame.pack()
-        self.graph = tkinter.Canvas(self.frame)
-        self.graph.pack()
-
-        self.t_cleanup = threading.Thread(target=self.tf_cleanup)
-        self.window.after(0, self.add_tracks, 5)
-        self.window.after(1000, self.t_cleanup.start)
-
-    def add_tracks(self,depth):
-        if depth>0: self.window.after(5, self.add_tracks, depth-1)
-        tracks_to_add = 40
-        start_no = self.track_no
-        self.track_no += tracks_to_add
-        for self.track_no in range(start_no, self.track_no):
-            #self.window.after(t, self.add_tracks)
-            #self.track_no += 1 #Make a new track number
-            #if (self.track_no > 40):
-            t = Track(self.track_no, self.draw_track_segment)
-            self.threads.append(t)
-            t.start()
-
-    def tf_cleanup(self):
+    def go(self):
+        self.root.after(0,self.add_threads)
+        self.root.after(500,self.stop)
+        self.root.protocol("WM_DELETE_WINDOW",self.stop)
+        self.root.mainloop()
+        self.t_cleanup.join()
+    def stop(self):
+        self.t_cleanup.start()
+    def tf_stop(self):
         global running
         running = False
         for t in self.threads: t.join()
-        self.window.after(0,self.window.destroy)
+        self.root.after(0,self.root.destroy)
+    def add_threads(self):
+        for _ in range(20):
+            t = EventThread(self.root)
+            self.threads.append(t)
+            t.start()
 
-    def draw_track_segment(self, track_no, x, y):
-        # x & y are virtual coordinates for the purpose of simulation.
-        #To convert them to screen coordinates,
-        # we scale them down so the graphs fit into the window, and move the origin.
-        # Y is also inverted because in canvas, the UL corner is the origin.
-        newsx, newsy = (250.+x/100., 150.-y/100.)
+def dummy_handler(event):
+    pass
 
-        try:
-            (oldsx, oldsy) = self.track_coordinates[track_no]
-        except KeyError:
-            pass
-        else:
-            self.graph.create_line(oldsx, oldsy,
-                                   newsx, newsy)
-        self.track_coordinates[track_no] = (newsx, newsy)
-
-    def go(self):
-        self.window.mainloop()
 
 tests_gui = (TestThreads,)
 
@@ -138,7 +81,7 @@ if __name__=='__main__':
             import ctypes
             #the bug causes crashes
             ctypes.windll.kernel32.SetErrorMode(3)
-        App(tkinter.Tk()).go()
+        Main().go()
     else:
         from test.support import run_unittest
         run_unittest(*tests_gui)
