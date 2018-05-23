454bc0c0f053f495a522e540695b13fd8b924f54
William Grzybowski <wg@FreeBSD.org>
2018-05-23 16:45:48 -0300

    Release GIL for `grp` and `pwd` single methods.
    Especially when using more complex nss modules the call might that an
    unknown amount of time depending on the service in question.
    This makes sures others threads are not blocked waiting the call to
    finish.

--- Modules/grpmodule.c
+++ Modules/grpmodule.c
@@ -96,7 +96,8 @@ static PyObject *
 grp_getgrgid_impl(PyObject *module, PyObject *id)
 /*[clinic end generated code: output=30797c289504a1ba input=15fa0e2ccf5cda25]*/
 {
-    PyObject *py_int_id;
+    PyObject *py_int_id, *retval = NULL;
+    char *buf;
     gid_t gid;
     struct group *p;
 
@@ -119,8 +120,38 @@ grp_getgrgid_impl(PyObject *module, PyObject *id)
         }
         Py_DECREF(py_int_id);
     }
+#ifdef HAVE_GETGRGID_R
+    Py_BEGIN_ALLOW_THREADS
+    int status, bufsize = NSS_BUFLEN_GROUP;
+    struct group *grpbuf = NULL;
 
-    if ((p = getgrgid(gid)) == NULL) {
+    p = malloc(sizeof(struct group));
+    buf = malloc(bufsize);
+
+    do {
+        status = getgrgid_r(gid, p, buf, bufsize, &grpbuf);
+        if(grpbuf == NULL && status == ERANGE) {
+            free(buf);
+            if((bufsize << 1) > (NSS_BUFLEN_GROUP << 10)) {
+                errno = ERANGE;
+                break;
+            }
+            bufsize <<= 1;
+            buf = malloc(bufsize);
+
+        }
+    } while (grpbuf == NULL && status == ERANGE);
+
+    if (status != 0 || grpbuf == NULL) {
+        free(buf);
+        free(p);
+        p = NULL;
+    }
+    Py_END_ALLOW_THREADS
+#else
+    p = getgrgid(gid);
+#endif
+    if (p == NULL) {
         PyObject *gid_obj = _PyLong_FromGid(gid);
         if (gid_obj == NULL)
             return NULL;
@@ -128,7 +159,12 @@ grp_getgrgid_impl(PyObject *module, PyObject *id)
         Py_DECREF(gid_obj);
         return NULL;
     }
-    return mkgrent(p);
+    retval = mkgrent(p);
+#ifdef HAVE_GETGRGID_R
+    free(buf);
+    free(p);
+#endif
+    return retval;
 }
 
 /*[clinic input]
@@ -145,7 +181,7 @@ static PyObject *
 grp_getgrnam_impl(PyObject *module, PyObject *name)
 /*[clinic end generated code: output=67905086f403c21c input=08ded29affa3c863]*/
 {
-    char *name_chars;
+    char *buf, *name_chars;
     struct group *p;
     PyObject *bytes, *retval = NULL;
 
@@ -154,12 +190,46 @@ grp_getgrnam_impl(PyObject *module, PyObject *name)
     /* check for embedded null bytes */
     if (PyBytes_AsStringAndSize(bytes, &name_chars, NULL) == -1)
         goto out;
+#ifdef HAVE_GETGRNAM_R
+    Py_BEGIN_ALLOW_THREADS
+    int status, bufsize = NSS_BUFLEN_GROUP;
+    struct group *grpbuf = NULL;
 
-    if ((p = getgrnam(name_chars)) == NULL) {
+    p = malloc(sizeof(struct group));
+    buf = malloc(bufsize);
+
+    do {
+        status = getgrnam_r(name_chars, p, buf, bufsize, &grpbuf);
+        if(grpbuf == NULL && status == ERANGE) {
+            free(buf);
+            if((bufsize << 1) > (NSS_BUFLEN_GROUP << 10)) {
+                errno = ERANGE;
+                break;
+            }
+            bufsize <<= 1;
+            buf = malloc(bufsize);
+
+        }
+    } while (grpbuf == NULL && status == ERANGE);
+
+    if (status != 0 || grpbuf == NULL) {
+        free(buf);
+        free(p);
+        p = NULL;
+    }
+    Py_END_ALLOW_THREADS
+#else
+    p = getgrnam(name_chars);
+#endif
+    if (p == NULL) {
         PyErr_Format(PyExc_KeyError, "getgrnam(): name not found: %s", name_chars);
         goto out;
     }
     retval = mkgrent(p);
+#ifdef HAVE_GETGRNAM_R
+    free(buf);
+    free(p);
+#endif
 out:
     Py_DECREF(bytes);
     return retval;
--- Modules/pwdmodule.c
+++ Modules/pwdmodule.c
@@ -116,8 +116,10 @@ static PyObject *
 pwd_getpwuid(PyObject *module, PyObject *uidobj)
 /*[clinic end generated code: output=c4ee1d4d429b86c4 input=ae64d507a1c6d3e8]*/
 {
+    PyObject *retval = NULL;
     uid_t uid;
     struct passwd *p;
+    char *buf;
 
     if (!_Py_Uid_Converter(uidobj, &uid)) {
         if (PyErr_ExceptionMatches(PyExc_OverflowError))
@@ -125,7 +127,38 @@ pwd_getpwuid(PyObject *module, PyObject *uidobj)
                          "getpwuid(): uid not found");
         return NULL;
     }
-    if ((p = getpwuid(uid)) == NULL) {
+#ifdef HAVE_GETPWUID_R
+    Py_BEGIN_ALLOW_THREADS
+    int status, bufsize = NSS_BUFLEN_PASSWD;
+    struct passwd *pwdbuf = NULL;
+
+    p = malloc(sizeof(struct passwd));
+    buf = malloc(bufsize);
+
+    do {
+        status = getpwuid_r(uid, p, buf, bufsize, &pwdbuf);
+        if(pwdbuf == NULL && status == ERANGE) {
+            free(buf);
+            if((bufsize << 1) > (NSS_BUFLEN_PASSWD << 10)) {
+                errno = ERANGE;
+                break;
+            }
+            bufsize <<= 1;
+            buf = malloc(bufsize);
+
+        }
+    } while (pwdbuf == NULL && status == ERANGE);
+
+    if (status != 0 || pwdbuf == NULL) {
+        free(buf);
+        free(p);
+        p = NULL;
+    }
+    Py_END_ALLOW_THREADS
+#else
+    p = getpwuid(uid);
+#endif
+    if (p == NULL) {
         PyObject *uid_obj = _PyLong_FromUid(uid);
         if (uid_obj == NULL)
             return NULL;
@@ -134,7 +167,12 @@ pwd_getpwuid(PyObject *module, PyObject *uidobj)
         Py_DECREF(uid_obj);
         return NULL;
     }
-    return mkpwent(p);
+    retval = mkpwent(p);
+#ifdef HAVE_GETPWUID_R
+    free(buf);
+    free(p);
+#endif
+    return retval;
 }
 
 /*[clinic input]
@@ -152,7 +190,7 @@ static PyObject *
 pwd_getpwnam_impl(PyObject *module, PyObject *arg)
 /*[clinic end generated code: output=6abeee92430e43d2 input=d5f7e700919b02d3]*/
 {
-    char *name;
+    char *buf, *name;
     struct passwd *p;
     PyObject *bytes, *retval = NULL;
 
@@ -161,12 +199,46 @@ pwd_getpwnam_impl(PyObject *module, PyObject *arg)
     /* check for embedded null bytes */
     if (PyBytes_AsStringAndSize(bytes, &name, NULL) == -1)
         goto out;
-    if ((p = getpwnam(name)) == NULL) {
+#ifdef HAVE_GETPWNAM_R
+    Py_BEGIN_ALLOW_THREADS
+    int status, bufsize = NSS_BUFLEN_PASSWD;
+    struct passwd *pwdbuf = NULL;
+
+    p = malloc(sizeof(struct passwd));
+    buf = malloc(bufsize);
+
+    do {
+        status = getpwnam_r(name, p, buf, bufsize, &pwdbuf);
+        if(pwdbuf == NULL && status == ERANGE) {
+            free(buf);
+            if((bufsize << 1) > (NSS_BUFLEN_PASSWD << 10)) {
+                errno = ERANGE;
+                break;
+            }
+            bufsize <<= 1;
+            buf = malloc(bufsize);
+
+        }
+    } while (pwdbuf == NULL && status == ERANGE);
+
+    if(status != 0 || pwdbuf == NULL) {
+        free(p);
+        p = NULL;
+    }
+    Py_END_ALLOW_THREADS
+#else
+    p = getpwnam(name);
+#endif
+    if (p == NULL) {
         PyErr_Format(PyExc_KeyError,
                      "getpwnam(): name not found: %s", name);
         goto out;
     }
     retval = mkpwent(p);
+#ifdef HAVE_GETPWNAM_R
+    free(buf);
+    free(p);
+#endif
 out:
     Py_DECREF(bytes);
     return retval;
