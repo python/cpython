a755bbbc8e1ecc9e72418fad57764adaa93f48ba
twisteroid ambassador <twisteroidambassador@users.noreply.github.com>
2018-05-24 20:14:30 +0800

    Implement Happy Eyeballs in asyncio.
    Added two keyword arguments, `delay` and `interleave`, to
    `BaseEventLoop.create_connection`. Happy eyeballs is activated if
    `delay` is specified.

--- Lib/asyncio/events.py
+++ Lib/asyncio/events.py
@@ -305,7 +305,8 @@ class AbstractEventLoop:
             *, ssl=None, family=0, proto=0,
             flags=0, sock=None, local_addr=None,
             server_hostname=None,
-            ssl_handshake_timeout=None):
+            ssl_handshake_timeout=None,
+            delay=None, interleave=None):
         raise NotImplementedError
 
     async def create_server(
