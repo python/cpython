28be4c1023c8eaf5842d2e8514805faf00d7b0c1
Giampaolo Rodola <g.rodola@gmail.com>
2018-05-28 01:23:03 -0700

    release GIL

--- Modules/posixmodule.c
+++ Modules/posixmodule.c
@@ -8793,7 +8793,12 @@ static PyObject *
 os__win32copyfile_impl(PyObject *module, path_t *src, path_t *dst)
 /*[clinic end generated code: output=9df245926c468843 input=00817871f5770bdc]*/
 {
-    if (CopyFileW(src->wide, dst->wide, FALSE) == 0) {
+    int ret;
+
+    Py_BEGIN_ALLOW_THREADS
+    ret = CopyFileW(src->wide, dst->wide, FALSE);
+    Py_END_ALLOW_THREADS
+    if (ret == 0) {
         win32_error_object("_win32copyfile", src->object);
         return NULL;
     }
