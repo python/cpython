38811ef5eabb612a723e999465cf5b43554cd42b
Serhiy Storchaka <storchaka@gmail.com>
2018-06-04 05:55:32 +0300

    [2.7] bpo-33759: Fix test.test_xmlrpc.ServerProxyTestCase. (GH-7362)
    It depended on a global variable set by other tests..
    (cherry picked from commit 7cfd8c6a1b53a7dbdea14b6f414f2629dcd130a2)
    
    Co-authored-by: Serhiy Storchaka <storchaka@gmail.com>

--- Lib/test/test_xmlrpc.py
+++ Lib/test/test_xmlrpc.py
@@ -854,13 +854,9 @@ class GzipServerTestCase(BaseServerTestCase):
 class ServerProxyTestCase(unittest.TestCase):
     def setUp(self):
         unittest.TestCase.setUp(self)
-        if threading:
-            self.url = URL
-        else:
-            # Without threading, http_server() and http_multi_server() will not
-            # be executed and URL is still equal to None. 'http://' is a just
-            # enough to choose the scheme (HTTP)
-            self.url = 'http://'
+        # Actual value of the URL doesn't matter if it is a string in
+        # the correct format.
+        self.url = 'http://fake.localhost'
 
     def test_close(self):
         p = xmlrpclib.ServerProxy(self.url)
