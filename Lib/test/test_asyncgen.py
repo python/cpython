81827b3facd54a44be2f6e932ef1bcc2c5b3e339
Yury Selivanov <yury@magic.io>
2018-06-07 11:03:34 -0400

    Make compare_generators test helper use async iter protocol correctly

--- Lib/test/test_asyncgen.py
+++ Lib/test/test_asyncgen.py
@@ -111,6 +111,31 @@ class AsyncGenTest(unittest.TestCase):
         def async_iterate(g):
             res = []
             while True:
+                an = g.__anext__()
+                try:
+                    while True:
+                        try:
+                            an.__next__()
+                        except StopIteration as ex:
+                            if ex.args:
+                                res.append(ex.args[0])
+                                break
+                            else:
+                                res.append('EMPTY StopIteration')
+                                break
+                        except StopAsyncIteration:
+                            raise
+                        except Exception as ex:
+                            res.append(str(type(ex)))
+                            break
+                except StopAsyncIteration:
+                    res.append('STOP')
+                    break
+            return res
+
+        def async_iterate(g):
+            res = []
+            while True:
                 try:
                     g.__anext__().__next__()
                 except StopAsyncIteration:
