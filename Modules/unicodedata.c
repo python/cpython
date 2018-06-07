7c69c1c0fba8c1c8ff3969bce4c1135736a4cc58
Benjamin Peterson <benjamin@python.org>
2018-06-06 20:14:28 -0700

    update to Unicode 11.0.0 (closes bpo-33778) (GH-7439)
    Also, standardize indentation of generated tables.

--- Modules/unicodedata.c
+++ Modules/unicodedata.c
@@ -921,7 +921,7 @@ is_unified_ideograph(Py_UCS4 code)
 {
     return
         (0x3400 <= code && code <= 0x4DB5)   || /* CJK Ideograph Extension A */
-        (0x4E00 <= code && code <= 0x9FEA)   || /* CJK Ideograph */
+        (0x4E00 <= code && code <= 0x9FEF)   || /* CJK Ideograph */
         (0x20000 <= code && code <= 0x2A6D6) || /* CJK Ideograph Extension B */
         (0x2A700 <= code && code <= 0x2B734) || /* CJK Ideograph Extension C */
         (0x2B740 <= code && code <= 0x2B81D) || /* CJK Ideograph Extension D */
