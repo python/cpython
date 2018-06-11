fd88f319a4f40682b989b63f0b6378d69465fda4
Terry Jan Reedy <tjreedy@udel.edu>
2018-06-11 15:45:27 -0400

    bpo-33656: Add enum name for argument of Windows call. (GH-7642)
    Change suggested by Eryk Sun in a comment on PR 7137 after it was merged.

--- Lib/idlelib/pyshell.py
+++ Lib/idlelib/pyshell.py
@@ -9,10 +9,13 @@ except ImportError:
           "Your Python may not be configured for Tk. **", file=sys.__stderr__)
     raise SystemExit(1)
 
+# Valid arguments for the ...Awareness call below are defined in the following.
+# https://msdn.microsoft.com/en-us/library/windows/desktop/dn280512(v=vs.85).aspx
 if sys.platform == 'win32':
     import ctypes
+    PROCESS_SYSTEM_DPI_AWARE = 1
     try:
-        ctypes.OleDLL('shcore').SetProcessDpiAwareness(1)
+        ctypes.OleDLL('shcore').SetProcessDpiAwareness(PROCESS_SYSTEM_DPI_AWARE)
     except (AttributeError, OSError):
         pass
 
