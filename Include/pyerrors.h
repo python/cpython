5683ab5b7be733e0b01fd816e6e440e0873aaa09
Serhiy Storchaka <storchaka@gmail.com>
2018-06-11 15:01:47 +0300

    Clean up after bpo-33738. (GH-7627)
    * Add declarations even if they are overridden by macros.
    * Make the declaration and the definition of PyExceptionClass_Name
      consistent.

--- Include/pyerrors.h
+++ Include/pyerrors.h
@@ -140,11 +140,10 @@ PyAPI_FUNC(void) _PyErr_ChainExceptions(PyObject *, PyObject *, PyObject *);
 #define PyExceptionInstance_Check(x)                    \
     PyType_FastSubclass((x)->ob_type, Py_TPFLAGS_BASE_EXC_SUBCLASS)
 
+PyAPI_FUNC(char *) PyExceptionClass_Name(PyObject *);
 #ifndef Py_LIMITED_API
 #define PyExceptionClass_Name(x) \
      ((char *)(((PyTypeObject *)(x))->tp_name))
-#else
-     PyAPI_FUNC(const char *) PyExceptionClass_Name(PyObject *);
 #endif
 
 #define PyExceptionInstance_Class(x) ((PyObject*)((x)->ob_type))
