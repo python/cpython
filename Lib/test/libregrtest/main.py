7510359dff84e150d9c307083b8b71869c7bed4d
Pablo Galindo <pablogsal@gmail.com>
2018-06-16 01:32:21 +0100

    fixup! Add check for invalid --huntrleaks/-R parameters

--- Lib/test/libregrtest/main.py
+++ Lib/test/libregrtest/main.py
@@ -529,8 +529,10 @@ class Regrtest:
         if self.ns.huntrleaks:
             warmup, repetitions, _ = self.ns.huntrleaks
             if warmup < 1 or repetitions < 1:
-                print("Invalid values for the --huntrleaks/-R parameters",
-                      file=sys.stderr, flush=True)
+                msg = ("Invalid values for the --huntrleaks/-R parameters. The "
+                       "number of warmups and repetitions must be at least 1 "
+                       "each (1:1).")
+                print(msg, file=sys.stderr, flush=True)
                 sys.exit(2)
 
         if self.ns.slaveargs is not None:
