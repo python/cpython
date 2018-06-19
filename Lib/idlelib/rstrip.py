4d92158f4c3917fc4fbfebff15224e74782abf79
Terry Jan Reedy <tjreedy@udel.edu>
2018-06-19 19:12:52 -0400

    bpo-33855: Still more edits and minimal tests for IDLE  (GH-7784)
    Part 3 of 3, continuing PR #7689. This covers 14 idlelib modules and their tests,
    rpc to zoomheight except for run (already done) and tooltip (being done separately).

--- Lib/idlelib/rstrip.py
+++ Lib/idlelib/rstrip.py
@@ -25,5 +25,5 @@ class RstripExtension:
         undo.undo_block_stop()
 
 if __name__ == "__main__":
-    import unittest
-    unittest.main('idlelib.idle_test.test_rstrip', verbosity=2, exit=False)
+    from unittest import main
+    main('idlelib.idle_test.test_rstrip', verbosity=2,)
