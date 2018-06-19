a86c3e1eef06bd31d8bdec9888d4add6b6cc82db
Terry Jan Reedy <tjreedy@udel.edu>
2018-06-19 17:19:18 -0400

    Edit windows and add test_windows.

--- Lib/idlelib/windows.py
+++ Lib/idlelib/windows.py
@@ -90,3 +90,8 @@ class ListedToplevel(Toplevel):
             # This can happen when the window menu was torn off.
             # Simply ignore it.
             pass
+
+
+if __name__ == "__main__":
+    import unittest
+    unittest.main('idlelib.idle_test.test_windows', verbosity=2, exit=False)
