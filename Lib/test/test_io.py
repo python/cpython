31a9b9193e7fd69a6c35cf68f6d7b1ebdb5ce324
Zackery Spytz <zspytz@gmail.com>
2018-06-29 04:14:58 -0600

    bpo-25862: Fix assertion failures in io.TextIOWrapper.tell(). (GH-3918)

--- Lib/test/test_io.py
+++ Lib/test/test_io.py
@@ -3549,6 +3549,17 @@ class TextIOWrapperTest(unittest.TestCase):
         expected = 'linesep' + os.linesep + 'LF\nLF\nCR\rCRLF\r\n'
         self.assertEqual(txt.detach().getvalue().decode('ascii'), expected)
 
+    def test_issue25862(self):
+        # Assertion failures occurred in tell() after read() and write().
+        t = self.TextIOWrapper(self.BytesIO(b'test'), encoding='ascii')
+        t.read(1)
+        t.read()
+        t.tell()
+        t = self.TextIOWrapper(self.BytesIO(b'test'), encoding='ascii')
+        t.read(1)
+        t.write('x')
+        t.tell()
+
 
 class MemviewBytesIO(io.BytesIO):
     '''A BytesIO object whose read method returns memoryviews
