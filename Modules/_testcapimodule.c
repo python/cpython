2cbd1bb1f342760ce7c1b78d1ef5697c32f1e299
Miss Islington (bot) <31488909+miss-islington@users.noreply.github.com>
2018-07-03 23:08:57 -0700

    Make GenericAlias_Type and Generic_Type static. (GH-8076)
    (cherry picked from commit 97ae32c92ecc7b3c29f8829a2b79f0f8f8bbf2cc)
    
    Co-authored-by: Benjamin Peterson <benjamin@python.org>

--- Modules/_testcapimodule.c
+++ Modules/_testcapimodule.c
@@ -5200,7 +5200,7 @@ static PyMethodDef generic_alias_methods[] = {
     {NULL}  /* sentinel */
 };
 
-PyTypeObject GenericAlias_Type = {
+static PyTypeObject GenericAlias_Type = {
     PyVarObject_HEAD_INIT(NULL, 0)
     "GenericAlias",
     sizeof(PyGenericAliasObject),
@@ -5237,7 +5237,7 @@ static PyMethodDef generic_methods[] = {
     {NULL}  /* sentinel */
 };
 
-PyTypeObject Generic_Type = {
+static PyTypeObject Generic_Type = {
     PyVarObject_HEAD_INIT(NULL, 0)
     "Generic",
     sizeof(PyGenericObject),
