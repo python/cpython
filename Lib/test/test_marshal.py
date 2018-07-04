f97d50c78455b0654db6abd9b6b557592a9ec763
Serhiy Storchaka <storchaka@gmail.com>
2018-07-04 09:42:57 +0300

    Increase the maximum marshal recursion depth on Windows debug builds.

--- Lib/test/test_marshal.py
+++ Lib/test/test_marshal.py
@@ -222,13 +222,7 @@ class BugsTestCase(unittest.TestCase):
         # Create a deeply nested structure.
         head = last = []
         # The max stack depth should match the value in Python/marshal.c.
-        # BUG: https://bugs.python.org/issue33720
-        # Windows always limits the maximum depth on release and debug builds
-        #if os.name == 'nt' and hasattr(sys, 'gettotalrefcount'):
-        if os.name == 'nt':
-            MAX_MARSHAL_STACK_DEPTH = 1000
-        else:
-            MAX_MARSHAL_STACK_DEPTH = 2000
+        MAX_MARSHAL_STACK_DEPTH = 2000
         for i in range(MAX_MARSHAL_STACK_DEPTH - 2):
             last.append([0])
             last = last[-1]
