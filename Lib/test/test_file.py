28abe5462693e3d7045c441c8819d5d0b43f7fba
Bradley Laney <bradley.laney@gmail.com>
2018-07-10 05:46:44 -0400

    bpo-34031: fix incorrect usage of self.fail in two tests (GH-8091)
    Contributed by Bradley Laney.

--- Lib/test/test_file.py
+++ Lib/test/test_file.py
@@ -241,8 +241,7 @@ class OtherFileTests:
         # Test for appropriate errors mixing read* and iteration
         for methodname, args in methods:
             f = self.open(TESTFN, 'rb')
-            if next(f) != filler:
-                self.fail, "Broken testfile"
+            self.assertEqual(next(f), filler)
             meth = getattr(f, methodname)
             meth(*args)  # This simply shouldn't fail
             f.close()
