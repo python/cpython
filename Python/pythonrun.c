25ec40ea31f82804be666e60b9e8cf5f29c8f314
Serhiy Storchaka <storchaka@gmail.com>
2018-07-12 00:17:53 +0300

    bpo-34080: Fix a memory leak in the compiler. (GH-8222) (cherry picked from commit 993030aac576710a46b3dd0b4864f819d4a94145)
    Co-authored-by: Serhiy Storchaka <storchaka@gmail.com>

--- Python/pythonrun.c
+++ Python/pythonrun.c
@@ -1590,7 +1590,7 @@ err_input(perrdetail *err)
     errtype = PyExc_SyntaxError;
     switch (err->error) {
     case E_ERROR:
-        return;
+        goto cleanup;
     case E_SYNTAX:
         errtype = PyExc_IndentationError;
         if (err->expected == INDENT)
