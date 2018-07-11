caed995a85c7d497bc1ad918958e316f6ecb8712
Derek B. Kim <bluewhale8202@gmail.com>
2018-07-11 19:22:28 +0900

    Simplify __all__ in multiprocessing (GH-6856)

--- Lib/multiprocessing/context.py
+++ Lib/multiprocessing/context.py
@@ -5,7 +5,7 @@ import threading
 from . import process
 from . import reduction
 
-__all__ = []            # things are copied from here to __init__.py
+__all__ = ()
 
 #
 # Exceptions
@@ -24,7 +24,7 @@ class AuthenticationError(ProcessError):
     pass
 
 #
-# Base type for contexts
+# Base type for contexts. Bound methods of an instance of this type are included in __all__ of __init__.py
 #
 
 class BaseContext(object):
@@ -260,8 +260,6 @@ class DefaultContext(BaseContext):
                 return ['fork', 'spawn', 'forkserver']
             else:
                 return ['fork', 'spawn']
-
-DefaultContext.__all__ = [x for x in dir(DefaultContext) if x[0] != '_']
 
 #
 # Context types for fixed start method
