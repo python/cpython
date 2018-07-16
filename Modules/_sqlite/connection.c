1d00a99845510a1b88fc5a5d1ab5a4e8bd4f0ad1
Sergey Fedoseev <fedoseev.sergey@gmail.com>
2018-07-16 18:21:56 +0500

    Do not remove functions from the dict on fail.

--- Modules/_sqlite/connection.c
+++ Modules/_sqlite/connection.c
@@ -856,7 +856,6 @@ PyObject* pysqlite_connection_create_function(pysqlite_Connection* self, PyObjec
                                  NULL);
 
     if (rc != SQLITE_OK) {
-        PyDict_DelItem(self->function_pinboard, func);
         /* Workaround for SQLite bug: no error code or string is available here */
         PyErr_SetString(pysqlite_OperationalError, "Error creating function");
         return NULL;
@@ -887,7 +886,6 @@ PyObject* pysqlite_connection_create_aggregate(pysqlite_Connection* self, PyObje
     }
     rc = sqlite3_create_function(self->db, name, n_arg, SQLITE_UTF8, (void*)aggregate_class, 0, &_pysqlite_step_callback, &_pysqlite_final_callback);
     if (rc != SQLITE_OK) {
-        PyDict_DelItem(self->function_pinboard, aggregate_class);
         /* Workaround for SQLite bug: no error code or string is available here */
         PyErr_SetString(pysqlite_OperationalError, "Error creating aggregate");
         return NULL;
@@ -1010,7 +1008,6 @@ static PyObject* pysqlite_connection_set_authorizer(pysqlite_Connection* self, P
     }
     rc = sqlite3_set_authorizer(self->db, _authorizer_callback, (void*)authorizer_cb);
     if (rc != SQLITE_OK) {
-        PyDict_DelItem(self->function_pinboard, authorizer_cb);
         PyErr_SetString(pysqlite_OperationalError, "Error setting authorizer callback");
         return NULL;
     }
