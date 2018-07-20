57b85d3ddaae134b2eb06aa67b2a759660ec61e4
Larry Hastings <larry@hastings.org>
2018-07-19 19:31:08 -0700

    Post-release version bump for 3.4.9rc1.

--- Include/patchlevel.h
+++ Include/patchlevel.h
@@ -23,7 +23,7 @@
 #define PY_RELEASE_SERIAL	1
 
 /* Version as a string */
-#define PY_VERSION      	"3.4.9rc1"
+#define PY_VERSION      	"3.4.9rc1+"
 /*--end constants--*/
 
 /* Version as a single 4-byte hex number, e.g. 0x010502B2 == 1.5.2b2.
