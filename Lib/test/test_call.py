d545026b4c5eec483813a5ac71d57a59dfe3c805
Xtreak <tirkarthi@users.noreply.github.com>
2018-07-23 01:43:26 +0530

    bpo-34127: Fix grammar in error message with respect to argument count (GH-8395)

--- Lib/test/test_call.py
+++ Lib/test/test_call.py
@@ -143,6 +143,22 @@ class CFunctionCallsErrorMessages(unittest.TestCase):
         msg = r"^from_bytes\(\) takes at most 2 positional arguments \(3 given\)"
         self.assertRaisesRegex(TypeError, msg, int.from_bytes, b'a', 'little', False)
 
+    def test_varargs4(self):
+        msg = r"get expected at least 1 argument, got 0"
+        self.assertRaisesRegex(TypeError, msg, {}.get)
+
+    def test_varargs5(self):
+        msg = r"getattr expected at least 2 arguments, got 0"
+        self.assertRaisesRegex(TypeError, msg, getattr)
+
+    def test_varargs6(self):
+        msg = r"input expected at most 1 argument, got 2"
+        self.assertRaisesRegex(TypeError, msg, input, 1, 2)
+
+    def test_varargs7(self):
+        msg = r"get expected at most 2 arguments, got 3"
+        self.assertRaisesRegex(TypeError, msg, {}.get, 1, 2, 3)
+
     def test_varargs1_kw(self):
         msg = r"__contains__\(\) takes no keyword arguments"
         self.assertRaisesRegex(TypeError, msg, {}.__contains__, x=2)
