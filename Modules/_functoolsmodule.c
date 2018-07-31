f491abff5bc18507b9fc83a84e202f98aa2f5975
Sergey Fedoseev <fedoseev.sergey@gmail.com>
2018-07-31 14:07:14 +0500

    bpo-34303: Micro-optimizations in functools.reduce().

--- Modules/_functoolsmodule.c
+++ Modules/_functoolsmodule.c
@@ -626,10 +626,11 @@ functools_reduce(PyObject *self, PyObject *args)
         if (result == NULL)
             result = op2;
         else {
-            PyTuple_SetItem(args, 0, result);
-            PyTuple_SetItem(args, 1, op2);
-            if ((result = PyEval_CallObject(func, args)) == NULL)
+            Py_XSETREF(_PyTuple_ITEMS(args)[0], result);
+            Py_XSETREF(_PyTuple_ITEMS(args)[1], op2);
+            if ((result = PyObject_Call(func, args, NULL)) == NULL) {
                 goto Fail;
+            }
         }
     }
 
