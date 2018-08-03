3ad38c44cc7d4dd8941d2ab87e1d06ac8e4b8e9a
Stefan Otte <stefan.otte@gmail.com>
2018-08-03 22:49:42 +0200

    bpo-34329: Doc'd how to remove suffix of pathlib.Path() (GH-8655) (cherry picked from commit 46dc4e34ed8005a688d7f3512844ef227a3465f4)
    Co-authored-by: Stefan Otte <stefan.otte@gmail.com>

--- Lib/pathlib.py
+++ Lib/pathlib.py
@@ -823,8 +823,10 @@ class PurePath(object):
                                        self._parts[:-1] + [name])
 
     def with_suffix(self, suffix):
-        """Return a new path with the file suffix changed (or added, if none)."""
-        # XXX if suffix is None, should the current suffix be removed?
+        """Return a new path with the file suffix changed.  If the path
+        has no suffix, add given suffix.  If the given suffix is an empty
+        string, remove the suffix from the path.
+        """
         f = self._flavour
         if f.sep in suffix or f.altsep and f.altsep in suffix:
             raise ValueError("Invalid suffix %r" % (suffix))
