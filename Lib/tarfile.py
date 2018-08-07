60627796cd856f996d6f89d046a694609370aed7
TROUVERIE Joachim <jtrouverie@itrust.fr>
2018-08-07 10:01:23 +0200

    bpo-29612 : Fix hard link creation in tar extraction when it points to themselves

--- Lib/tarfile.py
+++ Lib/tarfile.py
@@ -2185,7 +2185,11 @@ class TarFile(object):
           (platform limitation), we try to make a copy of the referenced file
           instead of a link.
         """
-        try:
+        # do not create link if target path is equal to link target
+        if getattr(tarinfo, "_link_target", None) == targetpath:
+            return
+
+        if hasattr(os, "symlink") and hasattr(os, "link"):
             # For systems that support symbolic and hard links.
             if tarinfo.issym():
                 os.symlink(tarinfo.linkname, targetpath)
