f6dd02234c18ac3e4f2ebca1fd7809f4f5ba80f6
Steve Dower <steve.dower@microsoft.com>
2018-08-15 13:29:24 -0700

    bpo-34405: Updated to OpenSSL 1.1.0i for Windows builds. (GH-8775)

--- PCbuild/get_externals.bat
+++ PCbuild/get_externals.bat
@@ -49,7 +49,7 @@ echo.Fetching external libraries...
 
 set libraries=
 set libraries=%libraries%                                       bzip2-1.0.6
-if NOT "%IncludeSSLSrc%"=="false" set libraries=%libraries%     openssl-1.1.0h
+if NOT "%IncludeSSLSrc%"=="false" set libraries=%libraries%     openssl-1.1.0i
 set libraries=%libraries%                                       sqlite-3.21.0.0
 if NOT "%IncludeTkinterSrc%"=="false" set libraries=%libraries% tcl-core-8.6.8.0
 if NOT "%IncludeTkinterSrc%"=="false" set libraries=%libraries% tk-8.6.8.0
@@ -72,7 +72,7 @@ for %%e in (%libraries%) do (
 echo.Fetching external binaries...
 
 set binaries=
-if NOT "%IncludeSSL%"=="false"     set binaries=%binaries% openssl-bin-1.1.0h
+if NOT "%IncludeSSL%"=="false"     set binaries=%binaries% openssl-bin-1.1.0i
 if NOT "%IncludeTkinter%"=="false" set binaries=%binaries% tcltk-8.6.8.0
 if NOT "%IncludeSSLSrc%"=="false"  set binaries=%binaries% nasm-2.11.06
 
