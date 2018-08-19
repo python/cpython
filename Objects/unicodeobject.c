1e596d3a20a1a9d1ef15218ef33795bc9e651b7a
Miss Islington (bot) <31488909+miss-islington@users.noreply.github.com>
2018-08-19 16:17:53 -0400

    bpo-34435: Add missing NULL check to unicode_encode_ucs1(). (GH-8823)
    Reported by Svace static analyzer.
    (cherry picked from commit 74a307d48ef8b278c4629ca0ef2139be1c9a34e6)
    
    Co-authored-by: Alexey Izbyshev <izbyshev@users.noreply.github.com>

--- Objects/unicodeobject.c
+++ Objects/unicodeobject.c
@@ -6812,8 +6812,6 @@ unicode_encode_ucs1(PyObject *unicode,
                     str = _PyBytesWriter_WriteBytes(&writer, str,
                                                     PyBytes_AS_STRING(rep),
                                                     PyBytes_GET_SIZE(rep));
-                    if (str == NULL)
-                        goto onError;
                 }
                 else {
                     assert(PyUnicode_Check(rep));
@@ -6835,6 +6833,9 @@ unicode_encode_ucs1(PyObject *unicode,
                                                     PyUnicode_DATA(rep),
                                                     PyUnicode_GET_LENGTH(rep));
                 }
+                if (str == NULL)
+                    goto onError;
+
                 pos = newpos;
                 Py_CLEAR(rep);
             }
