f6aafee4d9568272e3f1b66875092dae84c3dbc8
nealpy <nilanjan.ray@cognizant.com>
2018-08-22 16:22:42 +0530

    bpo-34461: Availability of parser files in etree initilizer of xml package

--- Lib/xml/etree/__init__.py
+++ Lib/xml/etree/__init__.py
@@ -31,3 +31,7 @@
 
 # Licensed to PSF under a Contributor Agreement.
 # See http://www.python.org/psf/license for licensing details.
+
+# Occupy all the parser files in initializer.
+
+from . import (ElementInclude, ElementPath, ElementTree, cElementTree)
