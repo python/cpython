6b97482cc0b2813d9d0df86097361f259762abe6
Alexey Izbyshev <izbyshev@ispras.ru>
2018-08-22 07:55:16 +0300

    bpo-34457: Python/ast.c: Add missing NULL check to alias_for_import_name(). (GH-8852)
    Reported by Svace static analyzer.

--- Python/ast.c
+++ Python/ast.c
@@ -3252,6 +3252,8 @@ alias_for_import_name(struct compiling *c, const node *n, int store)
             break;
         case STAR:
             str = PyUnicode_InternFromString("*");
+            if (!str)
+                return NULL;
             if (PyArena_AddPyObject(c->c_arena, str) < 0) {
                 Py_DECREF(str);
                 return NULL;
