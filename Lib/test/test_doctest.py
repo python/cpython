673a0cabd0582714c358a7e183a6ca3352c4e4fd
Mark Dickinson <dickinsm@gmail.com>
2018-08-25 13:22:48 +0100

    One more test_doctest fix.

--- Lib/test/test_doctest.py
+++ Lib/test/test_doctest.py
@@ -665,7 +665,7 @@ plain ol' Python and is guaranteed to be available.
     True
     >>> real_tests = [t for t in tests if len(t.examples) > 0]
     >>> len(real_tests) # objects that actually have doctests
-    8
+    9
     >>> for t in real_tests:
     ...     print('{}  {}'.format(len(t.examples), t.name))
     ...
