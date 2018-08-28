ae3d790e123a9753076b07ad73c9f482e41884a2
Victor Stinner <vstinner@redhat.com>
2018-08-28 10:16:08 +0200

    bpo-34530: Fix distutils find_executable()
    distutils.spawn.find_executable() has been reimplemented using
    shutil.which() to fix two issues: fallback on os.defpath if the PATH
    environment variable is not set, and respect the PATHEXT environment
    variable on Windows.

--- Lib/distutils/spawn.py
+++ Lib/distutils/spawn.py
@@ -6,8 +6,9 @@ Also provides the 'find_executable()' to search the path for a given
 executable name.
 """
 
-import sys
 import os
+import shutil
+import sys
 
 from distutils.errors import DistutilsPlatformError, DistutilsExecError
 from distutils.debug import DEBUG
@@ -172,21 +173,4 @@ def find_executable(executable, path=None):
     A string listing directories separated by 'os.pathsep'; defaults to
     os.environ['PATH'].  Returns the complete filename or None if not found.
     """
-    if path is None:
-        path = os.environ['PATH']
-
-    paths = path.split(os.pathsep)
-    base, ext = os.path.splitext(executable)
-
-    if (sys.platform == 'win32') and (ext != '.exe'):
-        executable = executable + '.exe'
-
-    if not os.path.isfile(executable):
-        for p in paths:
-            f = os.path.join(p, executable)
-            if os.path.isfile(f):
-                # the file exists, we have a shot at spawn working
-                return f
-        return None
-    else:
-        return executable
+    return shutil.which(executable, path=path)
