88b0e519d6d3c47538e616038647b089cb69cd36
Victor Stinner <vstinner@redhat.com>
2018-08-28 23:26:33 +0200

    bpo-34485: Enhance init_sys_streams() (GH-8978)
    Python now gets the locale encoding with C code to initialize the encoding
    of standard streams like sys.stdout. Moreover, the encoding is now
    initialized to the Python codec name to get a normalized encoding name and
    to ensure that the codec is loaded. The change avoids importing
    _bootlocale and _locale modules at startup by default.
    
    When the PYTHONIOENCODING environment variable only contains an encoding,
    the error handler is now is now set explicitly to "strict".
    
    Rename also get_default_standard_stream_error_handler() to
    get_stdio_errors().
    
    Reduce the buffer to format the "cpXXX" string (Windows locale encoding).

--- Lib/test/test_utf8_mode.py
+++ Lib/test/test_utf8_mode.py
@@ -139,16 +139,16 @@ class UTF8ModeTests(unittest.TestCase):
         out = self.get_output('-X', 'utf8', '-c', code,
                               PYTHONIOENCODING="latin1")
         self.assertEqual(out.splitlines(),
-                         ['stdin: latin1/strict',
-                          'stdout: latin1/strict',
-                          'stderr: latin1/backslashreplace'])
+                         ['stdin: iso8859-1/strict',
+                          'stdout: iso8859-1/strict',
+                          'stderr: iso8859-1/backslashreplace'])
 
         out = self.get_output('-X', 'utf8', '-c', code,
                               PYTHONIOENCODING=":namereplace")
         self.assertEqual(out.splitlines(),
-                         ['stdin: UTF-8/namereplace',
-                          'stdout: UTF-8/namereplace',
-                          'stderr: UTF-8/backslashreplace'])
+                         ['stdin: utf-8/namereplace',
+                          'stdout: utf-8/namereplace',
+                          'stderr: utf-8/backslashreplace'])
 
     def test_io(self):
         code = textwrap.dedent('''
