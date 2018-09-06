f44236f3d58cd4aa03857699b3014b109d4bebb9
Tal Einat <taleinat@gmail.com>
2018-09-06 09:56:05 +0300

    bpo-30977: make uuid.UUID use __slots__ to reduce its memory footprint
    Based on original patch by Wouter Bolsterlee.

--- Lib/test/test_uuid.py
+++ Lib/test/test_uuid.py
@@ -4,6 +4,7 @@ import builtins
 import contextlib
 import io
 import os
+import pickle
 import shutil
 import subprocess
 
@@ -186,6 +187,9 @@ class BaseTestUUID:
                 for v in equivalents:
                     equal(u, v)
 
+            # Test pickle-unpickle round-trip
+            equal(u, pickle.loads(pickle.dumps(u)))
+
             # Bug 7380: "bytes" and "bytes_le" should give the same type.
             equal(type(u.bytes), builtins.bytes)
             equal(type(u.bytes_le), builtins.bytes)
@@ -310,6 +314,32 @@ class BaseTestUUID:
         # Test it again to ensure consistency.
         node2 = self.uuid.getnode()
         self.assertEqual(node1, node2, '%012x != %012x' % (node1, node2))
+
+    def test_unpickle_previous_python_versions(self):
+        u = self.uuid.UUID('12345678123456781234567812345678')
+
+        # Python 2.7 protocol 0-2 pickles of u
+        py27_pickles = [
+            b'ccopy_reg\n_reconstructor\np0\n(cuuid\nUUID\np1\nc__builtin__\nobject\np2\nNtp3\nRp4\n(dp5\nS\'int\'\np6\nL24197857161011715162171839636988778104L\nsb.',
+            b'ccopy_reg\n_reconstructor\nq\x00(cuuid\nUUID\nq\x01c__builtin__\nobject\nq\x02Ntq\x03Rq\x04}q\x05U\x03intq\x06L24197857161011715162171839636988778104L\nsb.',
+            b'\x80\x02cuuid\nUUID\nq\x00)\x81q\x01}q\x02U\x03intq\x03\x8a\x10xV4\x12xV4\x12xV4\x12xV4\x12sb.',
+        ]
+        # Python 3.6 protocol 0-4 pickles of u
+        py36_pickles = [
+            b'ccopy_reg\n_reconstructor\np0\n(cuuid\nUUID\np1\nc__builtin__\nobject\np2\nNtp3\nRp4\n(dp5\nVint\np6\nL24197857161011715162171839636988778104L\nsb.',
+            b'ccopy_reg\n_reconstructor\nq\x00(cuuid\nUUID\nq\x01c__builtin__\nobject\nq\x02Ntq\x03Rq\x04}q\x05X\x03\x00\x00\x00intq\x06L24197857161011715162171839636988778104L\nsb.',
+            b'\x80\x02cuuid\nUUID\nq\x00)\x81q\x01}q\x02X\x03\x00\x00\x00intq\x03\x8a\x10xV4\x12xV4\x12xV4\x12xV4\x12sb.',
+            b'\x80\x03cuuid\nUUID\nq\x00)\x81q\x01}q\x02X\x03\x00\x00\x00intq\x03\x8a\x10xV4\x12xV4\x12xV4\x12xV4\x12sb.',
+            b'\x80\x04\x950\x00\x00\x00\x00\x00\x00\x00\x8c\x04uuid\x94\x8c\x04UUID\x94\x93\x94)\x81\x94}\x94\x8c\x03int\x94\x8a\x10xV4\x12xV4\x12xV4\x12xV4\x12sb.',
+        ]
+
+        for pickled in py27_pickles + py36_pickles:
+            unpickled = pickle.loads(pickled)
+            self.assertEqual(unpickled, u)
+            # is_safe was added in 3.7.  When unpickling values from older
+            # versions, is_safe will be missing, so it should be set to
+            # SafeUUID.unknown.
+            self.assertEqual(unpickled.is_safe, self.uuid.SafeUUID.unknown)
 
     # bpo-32502: UUID1 requires a 48-bit identifier, but hardware identifiers
     # need not necessarily be 48 bits (e.g., EUI-64).
