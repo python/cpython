f0819840f37947c9db0ac217013683dd6987df39
Zackery Spytz <zspytz@gmail.com>
2018-09-10 00:27:31 -0600

    Remove unneeded PyUnicode_READY() in tokenizer.c  (GH-9114)

--- Parser/tokenizer.c
+++ Parser/tokenizer.c
@@ -1324,7 +1324,7 @@ verify_identifier(struct tok_state *tok)
     if (tok->decoding_erred)
         return 0;
     s = PyUnicode_DecodeUTF8(tok->start, tok->cur - tok->start, NULL);
-    if (s == NULL || PyUnicode_READY(s) == -1) {
+    if (s == NULL) {
         if (PyErr_ExceptionMatches(PyExc_UnicodeDecodeError)) {
             PyErr_Clear();
             tok->done = E_IDENTIFIER;
