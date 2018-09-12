6d726868cd1743623a28b8e048e31b9c3c52a399
Miss Islington (bot) <31488909+miss-islington@users.noreply.github.com>
2018-09-12 15:00:11 -0700

    bpo-34649: Add missing NULL checks to _encoded_const() (GH-9225)
    Reported by Svace static analyzer.
    (cherry picked from commit 6f82bffd2df63a4072b3f0483cdbe93ddedb87e9)
    
    Co-authored-by: Alexey Izbyshev <izbyshev@ispras.ru>

--- Modules/_json.c
+++ Modules/_json.c
@@ -1376,7 +1376,7 @@ _encoded_const(PyObject *obj)
         if (s_null == NULL) {
             s_null = PyUnicode_InternFromString("null");
         }
-        Py_INCREF(s_null);
+        Py_XINCREF(s_null);
         return s_null;
     }
     else if (obj == Py_True) {
@@ -1384,7 +1384,7 @@ _encoded_const(PyObject *obj)
         if (s_true == NULL) {
             s_true = PyUnicode_InternFromString("true");
         }
-        Py_INCREF(s_true);
+        Py_XINCREF(s_true);
         return s_true;
     }
     else if (obj == Py_False) {
@@ -1392,7 +1392,7 @@ _encoded_const(PyObject *obj)
         if (s_false == NULL) {
             s_false = PyUnicode_InternFromString("false");
         }
-        Py_INCREF(s_false);
+        Py_XINCREF(s_false);
         return s_false;
     }
     else {
