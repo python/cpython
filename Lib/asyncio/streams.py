da64424d22a09c7e5069d794d4210f76e48d5939
Andrew Svetlov <andrew.svetlov@gmail.com>
2018-09-13 16:53:49 -0700

    bpo-34666: Implement stream.awrite() and stream.aclose() (GH-9274)

--- Lib/asyncio/streams.py
+++ Lib/asyncio/streams.py
@@ -348,7 +348,7 @@ class StreamWriter:
         # a reader can be garbage collected
         # after connection closing
         self._protocol._untrack_reader()
-        return self._transport.close()
+        self._transport.close()
 
     def is_closing(self):
         return self._transport.is_closing()
@@ -380,6 +380,14 @@ class StreamWriter:
             # would not see an error when the socket is closed.
             await sleep(0, loop=self._loop)
         await self._protocol._drain_helper()
+
+    async def aclose(self):
+        self.close()
+        await self.wait_closed()
+
+    async def awrite(self, data):
+        self.write(data)
+        await self.drain()
 
 
 class StreamReader:
