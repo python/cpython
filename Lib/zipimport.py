33fa5a467b9131a2d47918b1117a23ce2b6c6cba
Serhiy Storchaka <storchaka@gmail.com>
2018-09-17 14:19:33 +0300

    Add additional comments for the cp437 table.

--- Lib/zipimport.py
+++ Lib/zipimport.py
@@ -440,6 +440,7 @@ def _read_directory(archive):
 # Break out of this dependency by using the translation table for
 # the cp437 encoding.
 cp437_table = (
+    # ASCII part, 8 rows x 16 chars
     '\x00\x01\x02\x03\x04\x05\x06\x07\x08\t\n\x0b\x0c\r\x0e\x0f'
     '\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1a\x1b\x1c\x1d\x1e\x1f'
     ' !"#$%&\'()*+,-./'
@@ -448,7 +449,7 @@ cp437_table = (
     'PQRSTUVWXYZ[\\]^_'
     '`abcdefghijklmno'
     'pqrstuvwxyz{|}~\x7f'
-
+    # non-ASCII part, 16 rows x 8 chars
     '\xc7\xfc\xe9\xe2\xe4\xe0\xe5\xe7'
     '\xea\xeb\xe8\xef\xee\xec\xc4\xc5'
     '\xc9\xe6\xc6\xf4\xf6\xf2\xfb\xf9'
