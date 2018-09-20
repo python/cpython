476177005e8c8d4ece3070c8c378f8b8c068e76f
Miss Islington (bot) <31488909+miss-islington@users.noreply.github.com>
2018-09-20 08:23:45 -0700

    bpo-19756: Prevent test failures due to EADDRNOTAVAIL (GH-9446)
    (cherry picked from commit 8213eaddf3ce8e87564d2949454903a1484748b5)
    
    Co-authored-by: Berker Peksag <berker.peksag@gmail.com>

--- Lib/test/support/__init__.py
+++ Lib/test/support/__init__.py
@@ -1408,6 +1408,9 @@ def transient_internet(resource_name, *, timeout=30.0, errnos=()):
         ('EHOSTUNREACH', 113),
         ('ENETUNREACH', 101),
         ('ETIMEDOUT', 110),
+        # socket.create_connection() fails randomly with
+        # EADDRNOTAVAIL on Travis CI.
+        ('EADDRNOTAVAIL', 99),
     ]
     default_gai_errnos = [
         ('EAI_AGAIN', -3),
