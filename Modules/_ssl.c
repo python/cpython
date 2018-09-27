aef1ad311a9d08c6113a2f17776f0f27320fbdc6
Zackery Spytz <zspytz@gmail.com>
2018-09-27 11:10:32 -0600

    bpo-34824: Fix a possible NULL pointer dereference in _ssl.c
    On failure, _PyBytes_Resize() will deallocate the bytes object and set
    "result" to NULL.

--- Modules/_ssl.c
+++ Modules/_ssl.c
@@ -4712,7 +4712,6 @@ _ssl_MemoryBIO_read_impl(PySSLMemoryBIO *self, int len)
     nbytes = BIO_read(self->bio, PyBytes_AS_STRING(result), len);
     /* There should never be any short reads but check anyway. */
     if ((nbytes < len) && (_PyBytes_Resize(&result, len) < 0)) {
-        Py_DECREF(result);
         return NULL;
     }
 
