f16d6f9aa9e04e4b2e079c0351f253abf2dc5d8e
Petter Strandmark <petter.strandmark@gmail.com>
2018-10-05 16:04:32 +0200

    bpo-34547: Handle client connection terminations in wsgiref.
    Previously, long stack traces with Python TypeErrors and AttributeErrors
    were printed (see bug).
    
    The wsgiref server is used quite a lot in the community, notably by the
    Django development server. I see these huge stack traces daily.

--- Lib/test/test_wsgiref.py
+++ Lib/test/test_wsgiref.py
@@ -788,6 +788,18 @@ class HandlerTests(TestCase):
             b"Hello, world!",
             written)
 
+    def testConnectionAbortedError(self):
+        class AbortingWriter:
+            def write(self, b):
+                raise ConnectionAbortedError()
+
+            def flush(self):
+                pass
+
+        environ = {"SERVER_PROTOCOL": "HTTP/1.0"}
+        h = SimpleHandler(BytesIO(), AbortingWriter(), sys.stderr, environ)
+        h.run(hello_app)
+
 
 if __name__ == "__main__":
     unittest.main()
