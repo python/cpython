64a9d728191fca96f591cb4edcfbc782d99b1c81
animalize <animalize@users.noreply.github.com>
2018-10-08 16:20:54 -0500

    bpo-32174: Let .chm document display non-ASCII characters properly (GH-9758)
    Let .chm document display non-ASCII characters properly
    
    Escape the `body` part of .chm source file to 7-bit ASCII, to fix visual effect on some MBCS Windows systems.

--- Doc/conf.py
+++ Doc/conf.py
@@ -14,7 +14,7 @@ sys.path.append(os.path.abspath('includes'))
 # ---------------------
 
 extensions = ['sphinx.ext.coverage', 'sphinx.ext.doctest',
-              'pyspecific', 'c_annotations']
+              'pyspecific', 'c_annotations', 'escape4chm']
 
 # General substitutions.
 project = 'Python'
