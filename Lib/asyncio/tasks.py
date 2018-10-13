7b4c2b4e433093beb020cfd33b8f1397d42bf4d3
Andrew Svetlov <andrew.svetlov@gmail.com>
2018-10-13 21:12:40 +0300

    bpo-34970: Protect tasks weak set manipulation in asyncio.all_tasks() (GH-9837)
    https://bugs.python.org/issue34970

--- Lib/asyncio/tasks.py
+++ Lib/asyncio/tasks.py
@@ -42,7 +42,9 @@ def all_tasks(loop=None):
     """Return a set of all tasks for the loop."""
     if loop is None:
         loop = events.get_running_loop()
-    return {t for t in _all_tasks
+    # NB: set(_all_tasks) is required to protect
+    # from https://bugs.python.org/issue34970 bug
+    return {t for t in list(_all_tasks)
             if futures._get_loop(t) is loop and not t.done()}
 
 
@@ -52,7 +54,9 @@ def _all_tasks_compat(loop=None):
     # method.
     if loop is None:
         loop = events.get_event_loop()
-    return {t for t in _all_tasks if futures._get_loop(t) is loop}
+    # NB: set(_all_tasks) is required to protect
+    # from https://bugs.python.org/issue34970 bug
+    return {t for t in list(_all_tasks) if futures._get_loop(t) is loop}
 
 
 def _set_task_name(task, name):
