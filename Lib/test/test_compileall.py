377f565bc940e3cfcc28cfef62bc5aaf82ddb610
Stéphane Wirtel <stephane@wirtel.be>
2018-10-15 20:52:25 +0200

    Add a test with compileall and the .pyc file with a timestamp >= 2018-01-20 Co-authored-by: Karthikeyan Singaravelan <tir.karthi@gmail.com>

--- Lib/test/test_compileall.py
+++ Lib/test/test_compileall.py
@@ -200,7 +200,10 @@ class CompileallTestsWithSourceEpoch(CompileallTestsBase,
                                      unittest.TestCase,
                                      metaclass=SourceDateEpochTestMeta,
                                      source_date_epoch=True):
-    pass
+    def test_compile_all_2038(self):
+        with open(self.source_path, 'r') as f:
+            os.utime(f.name, (2147558400, 2147558400)) # Jan 20, 2038 as touch
+        self.assertTrue(compileall.compile_file(pathlib.Path(self.source_path)))
 
 
 class CompileallTestsWithoutSourceEpoch(CompileallTestsBase,
