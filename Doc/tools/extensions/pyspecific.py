e2c3bc7e79c3506609845f7f62ba102e6c7e9993
Miss Islington (bot) <31488909+miss-islington@users.noreply.github.com>
2018-10-15 23:47:07 -0700

    bpo-34967: Sphinx is deprecating add_description_unit, use add_object_type (GH-9827)
    (cherry picked from commit e385d0661ecf8bc9ba95c4395d9a11262c2cbfec)
    
    Co-authored-by: Stéphane Wirtel <stephane@wirtel.be>

--- Doc/tools/extensions/pyspecific.py
+++ Doc/tools/extensions/pyspecific.py
@@ -388,11 +388,9 @@ def setup(app):
     app.add_directive('deprecated-removed', DeprecatedRemoved)
     app.add_builder(PydocTopicsBuilder)
     app.add_builder(suspicious.CheckSuspiciousMarkupBuilder)
-    app.add_description_unit('opcode', 'opcode', '%s (opcode)',
-                             parse_opcode_signature)
-    app.add_description_unit('pdbcommand', 'pdbcmd', '%s (pdb command)',
-                             parse_pdb_command)
-    app.add_description_unit('2to3fixer', '2to3fixer', '%s (2to3 fixer)')
+    app.add_object_type('opcode', 'opcode', '%s (opcode)', parse_opcode_signature)
+    app.add_object_type('pdbcommand', 'pdbcmd', '%s (pdb command)', parse_pdb_command)
+    app.add_object_type('2to3fixer', '2to3fixer', '%s (2to3 fixer)')
     app.add_directive_to_domain('py', 'decorator', PyDecoratorFunction)
     app.add_directive_to_domain('py', 'decoratormethod', PyDecoratorMethod)
     app.add_directive_to_domain('py', 'coroutinefunction', PyCoroutineFunction)
