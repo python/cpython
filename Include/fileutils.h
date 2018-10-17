86fa808f9bfba6abe73760373f369654c654ea2c
Stéphane Wirtel <stephane@wirtel.be>
2018-10-17 16:28:26 +0200

    Update with the review of Victor

--- Include/fileutils.h
+++ Include/fileutils.h
@@ -83,13 +83,12 @@ PyAPI_FUNC(PyObject *) _Py_device_encoding(int);
 
 #if defined(MS_WINDOWS) || defined(__APPLE__)
 /* On Windows, the count parameter of read() is an int.
-   macOS fails when reading or writing more than 2GB, see #24658
-*/
-#define _PY_READ_MAX  INT_MAX
-#define _PY_WRITE_MAX INT_MAX
+ * macOS fails when reading or writing more than 2GB, see bpo-24658 */
+#   define _PY_READ_MAX  INT_MAX
+#   define _PY_WRITE_MAX INT_MAX
 #else
-#define _PY_READ_MAX  PY_SSIZE_T_MAX
-#define _PY_WRITE_MAX PY_SSIZE_T_MAX
+#   define _PY_READ_MAX  PY_SSIZE_T_MAX
+#   define _PY_WRITE_MAX PY_SSIZE_T_MAX
 /* write() should truncate count to PY_SSIZE_T_MAX, but it's safer
  * to do it ourself to have a portable behaviour */
 #endif
