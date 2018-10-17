468dbbe6edd0c22cc71164cc2636aa1462e82e64
Stéphane Wirtel <stephane@wirtel.be>
2018-10-17 14:56:06 +0200

    Add the missing symbols from test.support

--- Lib/test/test_largefile.py
+++ Lib/test/test_largefile.py
@@ -5,7 +5,7 @@ import os
 import stat
 import sys
 import unittest
-from test.support import TESTFN, requires, unlink
+from test.support import TESTFN, requires, unlink, bigmemtest, _2G
 import io  # C implementation of io
 import _pyio as pyio # Python implementation of io
 
