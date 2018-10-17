dbe15f9a93e078ff77e6fd90d62a31dff50e2ee7
Stéphane Wirtel <stephane@wirtel.be>
2018-10-17 11:41:54 +0200

    Remove the usage of .keys()

--- Lib/cProfile.py
+++ Lib/cProfile.py
@@ -141,7 +141,7 @@ def main():
     parser.add_option('-s', '--sort', dest="sort",
         help="Sort order when printing to stdout, based on pstats.Stats class",
         default=-1,
-        choices=list(pstats.Stats.sort_arg_dict_default.keys()))
+        choices=list(pstats.Stats.sort_arg_dict_default))
     parser.add_option('-m', dest="module", action="store_true",
         help="Profile a library module", default=False)
 
