e3f264989032d043e13ad9690258aca8441f2482
Juliette Monsel <j_4321@protonmail.com>
2018-10-18 19:05:40 +0200

    Fix tkinter.Spinbox.selection_ methods docstrings

--- Lib/tkinter/__init__.py
+++ Lib/tkinter/__init__.py
@@ -3750,7 +3750,7 @@ class Spinbox(Widget, XView):
         select to commands. If the selection isn't currently in
         the spinbox, then a new selection is created to include
         the characters between index and the most recent selection
-        anchor point, inclusive. Returns an empty string.
+        anchor point, inclusive. Returns an empty tuple.
         """
         return self.selection("adjust", index)
 
@@ -3758,7 +3758,7 @@ class Spinbox(Widget, XView):
         """Clear the selection
 
         If the selection isn't in this widget then the
-        command has no effect. Returns an empty string.
+        command has no effect. Returns an empty tuple.
         """
         return self.selection("clear")
 
@@ -3766,7 +3766,7 @@ class Spinbox(Widget, XView):
         """Sets or gets the currently selected element.
 
         If a spinbutton element is specified, it will be
-        displayed depressed
+        displayed depressed.
         """
         return self.selection("element", element)
 
