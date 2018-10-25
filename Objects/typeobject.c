7c1342082cf568594680821474fd0dc2e7bce8da
Victor Stinner <vstinner@redhat.com>
2018-10-26 01:20:02 +0200

    bpo-9263: Use _PyObject_ASSERT() in typeobject.c
    Replace assert() with _PyObject_ASSERT() in Objects/typeobject.c
    to dump the faulty object on assertion failure to ease debugging.

--- Objects/typeobject.c
+++ Objects/typeobject.c
@@ -1116,7 +1116,7 @@ subtype_dealloc(PyObject *self)
 
     /* Extract the type; we expect it to be a heap type */
     type = Py_TYPE(self);
-    assert(type->tp_flags & Py_TPFLAGS_HEAPTYPE);
+    _PyObject_ASSERT(self, type->tp_flags & Py_TPFLAGS_HEAPTYPE);
 
     /* Test whether the type has GC exactly once */
 
@@ -2214,9 +2214,10 @@ subtype_getweakref(PyObject *obj, void *context)
                         "This object has no __weakref__");
         return NULL;
     }
-    assert(Py_TYPE(obj)->tp_weaklistoffset > 0);
-    assert(Py_TYPE(obj)->tp_weaklistoffset + sizeof(PyObject *) <=
-           (size_t)(Py_TYPE(obj)->tp_basicsize));
+    _PyObject_ASSERT(obj, Py_TYPE(obj)->tp_weaklistoffset > 0);
+    _PyObject_ASSERT(obj,
+                     (Py_TYPE(obj)->tp_weaklistoffset + sizeof(PyObject *)) \
+                     <= (size_t)(Py_TYPE(obj)->tp_basicsize));
     weaklistptr = (PyObject **)
         ((char *)obj + Py_TYPE(obj)->tp_weaklistoffset);
     if (*weaklistptr == NULL)
@@ -3279,7 +3280,7 @@ type_dealloc(PyTypeObject *type)
     PyObject *tp, *val, *tb;
 
     /* Assert this is a heap-allocated type object */
-    assert(type->tp_flags & Py_TPFLAGS_HEAPTYPE);
+    _PyObject_ASSERT((PyObject *)type, type->tp_flags & Py_TPFLAGS_HEAPTYPE);
     _PyObject_GC_UNTRACK(type);
     PyErr_Fetch(&tp, &val, &tb);
     remove_all_subclasses(type, type->tp_bases);
@@ -3503,7 +3504,7 @@ type_clear(PyTypeObject *type)
     PyDictKeysObject *cached_keys;
     /* Because of type_is_gc(), the collector only calls this
        for heaptypes. */
-    assert(type->tp_flags & Py_TPFLAGS_HEAPTYPE);
+    _PyObject_ASSERT((PyObject *)type, type->tp_flags & Py_TPFLAGS_HEAPTYPE);
 
     /* We need to invalidate the method cache carefully before clearing
        the dict, so that other objects caught in a reference cycle
@@ -5117,7 +5118,8 @@ PyType_Ready(PyTypeObject *type)
         assert(_PyType_CheckConsistency(type));
         return 0;
     }
-    assert((type->tp_flags & Py_TPFLAGS_READYING) == 0);
+    _PyObject_ASSERT((PyObject *)type,
+                     (type->tp_flags & Py_TPFLAGS_READYING) == 0);
 
     type->tp_flags |= Py_TPFLAGS_READYING;
 
