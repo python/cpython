53c5c677807e020c414c99ff75c2ffc89209795e
Quentin <quentin.agren@sciencespo.fr>
2018-10-25 13:04:28 +0200

    importlib: Fix typo in SourceLoader.path_stats docstring (GH-10052)

--- Lib/importlib/_bootstrap_external.py
+++ Lib/importlib/_bootstrap_external.py
@@ -785,15 +785,16 @@ class SourceLoader(_LoaderBasics):
 
     def path_mtime(self, path):
         """Optional method that returns the modification time (an int) for the
-        specified path, where path is a str.
+        specified path (a str).
 
         Raises OSError when the path cannot be handled.
         """
         raise OSError
 
     def path_stats(self, path):
-        """Optional method returning a metadata dict for the specified path
-        to by the path (str).
+        """Optional method returning a metadata dict for the specified
+        path (a str).
+
         Possible keys:
         - 'mtime' (mandatory) is the numeric timestamp of last source
           code modification;
