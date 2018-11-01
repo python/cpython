fe44d8446bc4009ae2204b7d4be8ab620161cae2
Victor Stinner <vstinner@redhat.com>
2018-11-01 03:15:58 +0100

    bpo-35081: And pycore_lifecycle.h and pycore_pathconfig.h (GH-10273)
    * And pycore_lifecycle.h and pycore_pathconfig.h headers to
      Include/internal/
    * Move Py_BUILD_CORE specific code from coreconfig.h and
      pylifecycle.h to pycore_pathconfig.h and pycore_lifecycle.h
    * Move _Py_wstrlist_XXX() definitions and _PyPathConfig code
      from pycore_state.h to pycore_pathconfig.h
    * Move "Init" and "Fini" function definitions from pylifecycle.c to
      pycore_lifecycle.h.

--- Include/internal/pycore_state.h
+++ Include/internal/pycore_state.h
@@ -11,8 +11,9 @@ extern "C" {
 #include "pystate.h"
 #include "pythread.h"
 
-#include "pycore_mem.h"
 #include "pycore_ceval.h"
+#include "pycore_mem.h"
+#include "pycore_pathconfig.h"
 #include "pycore_warnings.h"
 
 
@@ -39,52 +40,6 @@ struct _gilstate_runtime_state {
    If set to non-zero, PyGILState_Check() always return 1. */
 #define _PyGILState_check_enabled _PyRuntime.gilstate.check_enabled
 
-
-typedef struct _PyPathConfig {
-    /* Full path to the Python program */
-    wchar_t *program_full_path;
-    wchar_t *prefix;
-#ifdef MS_WINDOWS
-    wchar_t *dll_path;
-#else
-    wchar_t *exec_prefix;
-#endif
-    /* Set by Py_SetPath(), or computed by _PyPathConfig_Init() */
-    wchar_t *module_search_path;
-    /* Python program name */
-    wchar_t *program_name;
-    /* Set by Py_SetPythonHome() or PYTHONHOME environment variable */
-    wchar_t *home;
-    /* isolated and site_import are used to set Py_IsolatedFlag and
-       Py_NoSiteFlag flags on Windows in read_pth_file(). These fields
-       are ignored when their value are equal to -1 (unset). */
-    int isolated;
-    int site_import;
-} _PyPathConfig;
-
-#define _PyPathConfig_INIT \
-    {.module_search_path = NULL, \
-     .isolated = -1, \
-     .site_import = -1}
-/* Note: _PyPathConfig_INIT sets other fields to 0/NULL */
-
-PyAPI_DATA(_PyPathConfig) _Py_path_config;
-
-PyAPI_FUNC(_PyInitError) _PyPathConfig_Calculate_impl(
-    _PyPathConfig *config,
-    const _PyCoreConfig *core_config);
-PyAPI_FUNC(void) _PyPathConfig_ClearGlobal(void);
-
-PyAPI_FUNC(void) _Py_wstrlist_clear(
-    int len,
-    wchar_t **list);
-PyAPI_FUNC(wchar_t**) _Py_wstrlist_copy(
-    int len,
-    wchar_t **list);
-PyAPI_FUNC(_PyInitError) _Py_wstrlist_append(
-    int *len,
-    wchar_t ***list,
-    const wchar_t *str);
 
 /* interpreter state */
 
