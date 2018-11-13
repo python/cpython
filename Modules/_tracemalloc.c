db39553987cde00e6c9c726c64157461b543e148
Victor Stinner <vstinner@redhat.com>
2018-11-13 15:14:58 +0100

    bpo-29564:_PyMem_DumpTraceback() suggests enabling tracemalloc (GH-10510)
    If tracemalloc is not tracing Python memory allocations,
    _PyMem_DumpTraceback() now suggests to enable tracemalloc
    to get the traceback where the memory block has been allocated.

--- Modules/_tracemalloc.c
+++ Modules/_tracemalloc.c
@@ -1492,6 +1492,12 @@ _PyMem_DumpTraceback(int fd, const void *ptr)
     traceback_t *traceback;
     int i;
 
+    if (!tracemalloc_config.tracing) {
+        PUTS(fd, "Enable tracemalloc to get the memory block "
+                 "allocation traceback\n\n");
+        return;
+    }
+
     traceback = tracemalloc_get_traceback(DEFAULT_DOMAIN, (uintptr_t)ptr);
     if (traceback == NULL)
         return;
