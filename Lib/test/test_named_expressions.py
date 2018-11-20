bc2fed3a0f2efa8ae25b6c049677afae90543db1
Emily Morehouse <emilyemorehouse@gmail.com>
2018-11-20 11:30:30 -0700

    Tests - Update error message tests to reflect improved messaging convention (s/can't/cannot)

--- Lib/test/test_named_expressions.py
+++ Lib/test/test_named_expressions.py
@@ -31,7 +31,7 @@ class NamedExpressionInvalidTest(unittest.TestCase):
     def test_named_expression_invalid_06(self):
         code = """((a, b) := (1, 2))"""
 
-        with self.assertRaisesRegex(SyntaxError, "can't use named assignment with tuple"):
+        with self.assertRaisesRegex(SyntaxError, "cannot use named assignment with tuple"):
             exec(code, {}, {})
 
     def test_named_expression_invalid_07(self):
@@ -89,7 +89,7 @@ class NamedExpressionInvalidTest(unittest.TestCase):
         code = """(lambda: x := 1)"""
 
         with self.assertRaisesRegex(SyntaxError,
-            "can't use named assignment with lambda"):
+            "cannot use named assignment with lambda"):
             exec(code, {}, {})
 
     def test_named_expression_invalid_16(self):
