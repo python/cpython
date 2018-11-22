5f66a512a8f437b35218187fcc3e44eb0aa84049
Victor Stinner <vstinner@redhat.com>
2018-11-22 16:32:57 +0100

    bpo-9263: Fix _PyObject_Dump() for freed object (#10661)
    If _PyObject_Dump() detects that the object is freed, don't try to
    dump it (exit immediately).
    
    Enhance also _PyObject_IsFreed(): it now detects if the pointer
    itself looks like freed memory.
    
    (cherry picked from commit 2cf5d32fd9e61488e8b0be55a2e92a752ba8b06b)

--- Objects/object.c
+++ Objects/object.c
@@ -421,6 +421,10 @@ _Py_BreakPoint(void)
 int
 _PyObject_IsFreed(PyObject *op)
 {
+    uintptr_t ptr = (uintptr_t)op;
+    if (_PyMem_IsFreed(&ptr, sizeof(ptr))) {
+        return 1;
+    }
     int freed = _PyMem_IsFreed(&op->ob_type, sizeof(op->ob_type));
     /* ignore op->ob_ref: the value can have be modified
        by Py_INCREF() and Py_DECREF(). */
@@ -446,6 +450,7 @@ _PyObject_Dump(PyObject* op)
         /* It seems like the object memory has been freed:
            don't access it to prevent a segmentation fault. */
         fprintf(stderr, "<freed object>\n");
+        return;
     }
 
     PyGILState_STATE gil;
