01e579949ab546cd4cdd0d6d18e3ef41ce94f46e
Miss Islington (bot) <31488909+miss-islington@users.noreply.github.com>
2018-11-23 09:13:32 -0800

    bpo-34812: subprocess._args_from_interpreter_flags(): add isolated (GH-10675)
    The "-I" command line option (run Python in isolated mode) is now
    also copied by the multiprocessing and distutils modules when
    spawning child processes. Previously, only -E and -s options (enabled
    by -I) were copied.
    
    subprocess._args_from_interpreter_flags() now copies the -I flag.
    (cherry picked from commit 9de363271519e0616f4a7b59427057c4810d3acc)
    
    Co-authored-by: Victor Stinner <vstinner@redhat.com>

--- Lib/subprocess.py
+++ Lib/subprocess.py
@@ -261,9 +261,7 @@ def _args_from_interpreter_flags():
         # 'inspect': 'i',
         # 'interactive': 'i',
         'dont_write_bytecode': 'B',
-        'no_user_site': 's',
         'no_site': 'S',
-        'ignore_environment': 'E',
         'verbose': 'v',
         'bytes_warning': 'b',
         'quiet': 'q',
@@ -274,6 +272,14 @@ def _args_from_interpreter_flags():
         v = getattr(sys.flags, flag)
         if v > 0:
             args.append('-' + opt * v)
+
+    if sys.flags.isolated:
+        args.append('-I')
+    else:
+        if sys.flags.ignore_environment:
+            args.append('-E')
+        if sys.flags.no_user_site:
+            args.append('-s')
 
     # -W options
     warnopts = sys.warnoptions[:]
