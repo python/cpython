11f91c2c9a206b8bdc7f9af0036eeab986dc0d36
Michael Saah <mike.saah@gmail.com>
2018-11-27 08:02:30 -0800

    fixed time.strftime call in test, moved test logic to own test method

--- Lib/test/datetimetester.py
+++ Lib/test/datetimetester.py
@@ -1350,16 +1350,16 @@ class TestDate(HarmlessMixedComparison, unittest.TestCase):
         #check that this standard extension works
         t.strftime("%f")
 
-        # bpo-35066: make sure trailing '%' don't cause
-        # datetime's strftime to complain
-        failed = False
+    def test_strftime_trailing_percent(self):
+    # bpo-35066: make sure trailing '%' doesn't cause
+    # datetime's strftime to complain
+        t = self.theclass(2005, 3, 2)
         try:
-            t.strftime('%')
+            _time.strftime('%')
         except ValueError:
-            failed = True
-        if not failed:
-            self.assertEqual(t.strftime('%'), '%')
-            self.assertEqual(t.strftime("m:%m d:%d y:%y %"), "m:03 d:02 y:05 %")
+            self.skipTest('time module does not support trailing %')
+        self.assertEqual(t.strftime('%'), '%')
+        self.assertEqual(t.strftime("m:%m d:%d y:%y %"), "m:03 d:02 y:05 %")
 
     def test_format(self):
         dt = self.theclass(2007, 9, 10)
