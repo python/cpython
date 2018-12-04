ffff5d15b78e6eb9cb2e4409c1c83fe92a01cf11
Raymond Hettinger <raymond.hettinger@gmail.com>
2018-12-04 12:29:44 -0800

    Remove unnecessary and over-restrictive type check

--- Lib/random.py
+++ Lib/random.py
@@ -718,8 +718,6 @@ class SystemRandom(Random):
         """getrandbits(k) -> x.  Generates an int with k random bits."""
         if k <= 0:
             raise ValueError('number of bits must be greater than zero')
-        if k != int(k):
-            raise TypeError('number of bits should be an integer')
         numbytes = (k + 7) // 8                       # bits / 8 and rounded up
         x = int.from_bytes(_urandom(numbytes), 'big')
         return x >> (numbytes * 8 - k)                # trim excess bits
