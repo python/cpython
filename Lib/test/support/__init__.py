29a4cbff92862207eb9df9a970b3636b8b06ff5d
native-api <ivan_pozdeev@mail.ru>
2018-12-06 12:43:37 +0300

    [2.7] bpo-33709: test_ntpath and test_posixpath fail in Windows with ACP!=1252. (GH-7278) (GH-7279)

--- Lib/test/support/__init__.py
+++ Lib/test/support/__init__.py
@@ -657,8 +657,12 @@ if have_unicode:
         unichr(0x20AC),
     ):
         try:
-            character.encode(sys.getfilesystemencoding())\
-                     .decode(sys.getfilesystemencoding())
+            # In Windows, 'mbcs' is used, and encode() returns '?'
+            # for characters missing in the ANSI codepage
+            if character.encode(sys.getfilesystemencoding())\
+                        .decode(sys.getfilesystemencoding())\
+                    != character:
+                raise UnicodeError
         except UnicodeError:
             pass
         else:
