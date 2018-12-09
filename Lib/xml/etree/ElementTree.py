e0d6f6ae8f76b2b2156f28169085a9d4f57f20b2
Maarten ter Huurne <maarten@treewalker.org>
2018-12-09 09:32:08 +0100

    Rename internal variable for clarity
    The 'qname' argument is not only used for element names, so calling
    the non-namespace part 'tag' is confusing. The XML namespaces spec
    calls it 'local part', so I picked 'local' as the new name.

--- Lib/xml/etree/ElementTree.py
+++ Lib/xml/etree/ElementTree.py
@@ -848,7 +848,7 @@ def _namespaces(elem, default_namespace=None):
         # calculate serialized qname representation
         try:
             if qname[:1] == "{":
-                uri, tag = qname[1:].rsplit("}", 1)
+                uri, local = qname[1:].rsplit("}", 1)
                 prefix = namespaces.get(uri)
                 if prefix is None:
                     prefix = _namespace_map.get(uri)
@@ -857,9 +857,9 @@ def _namespaces(elem, default_namespace=None):
                     if prefix != "xml":
                         namespaces[uri] = prefix
                 if prefix:
-                    qnames[qname] = "%s:%s" % (prefix, tag)
+                    qnames[qname] = "%s:%s" % (prefix, local)
                 else:
-                    qnames[qname] = tag # default element
+                    qnames[qname] = local # default element
             else:
                 if default_namespace:
                     # FIXME: can this be handled in XML 1.0?
