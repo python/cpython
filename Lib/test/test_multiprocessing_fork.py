0bba4342b5cb3f4b8449729368867d5439a7c96b
Ned Deily <nad@python.org>
2018-12-09 01:50:15 -0500

    bpo-33725: skip test_multiprocessing_fork on macOS (GH-11043)

--- Lib/test/test_multiprocessing_fork.py
+++ Lib/test/test_multiprocessing_fork.py
@@ -1,11 +1,14 @@
 import unittest
 import test._test_multiprocessing
 
+import sys
 from test import support
 
 if support.PGO:
     raise unittest.SkipTest("test is not helpful for PGO")
 
+if sys.platform == 'darwin':
+    raise unittest.SkipTest("test may crash on macOS (bpo-33725)")
 
 test._test_multiprocessing.install_tests_in_module_dict(globals(), 'fork')
 
