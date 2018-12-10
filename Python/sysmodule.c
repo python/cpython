12d8ffe2495fb63b8d8d4175983e789af8f2514c
Serhiy Storchaka <storchaka@gmail.com>
2018-12-10 13:50:22 +0200

    bpo-35452: Make PySys_HasWarnOptions() never raising an exception. (GH-11075)

--- Python/sysmodule.c
+++ Python/sysmodule.c
@@ -1845,7 +1845,8 @@ int
 PySys_HasWarnOptions(void)
 {
     PyObject *warnoptions = _PySys_GetObjectId(&PyId_warnoptions);
-    return (warnoptions != NULL && (PyList_Size(warnoptions) > 0)) ? 1 : 0;
+    return (warnoptions != NULL && PyList_Check(warnoptions)
+            && PyList_GET_SIZE(warnoptions) > 0);
 }
 
 static PyObject *
