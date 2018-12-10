d7ebc2181076554f49738fecfa4e19c7f34a42ed
Charalampos Stratakis <cstratak@redhat.com>
2018-12-10 17:25:14 +0100

    Refer to the issue number in the bpo-* format in comments.

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ py_cflags_nodist = sysconfig.get_config_var('PY_CFLAGS_NODIST')
 sysconfig.get_config_vars()['CFLAGS'] = cflags + ' ' + py_cflags_nodist
 
 # Add special LDFLAGS reserved for building the interpreter and the stdlib
-# modules (Issue #35257).
+# modules (bpo-35257).
 ldflags = sysconfig.get_config_var('LDFLAGS')
 py_ldflags_nodist = sysconfig.get_config_var('PY_LDFLAGS_NODIST')
 sysconfig.get_config_vars()['LDFLAGS'] = ldflags + ' ' + py_ldflags_nodist
