5684eefc517f24809d7592efc43acb83c0e88b2e
Pablo Galindo <Pablogsal@gmail.com>
2018-12-11 11:31:16 +0000

    bpo-35412: Skip test_multiprocessing_fork and test_multiprocessing_forkserver on Windows (GH-11086)
    Forkserver and fork are not available on Windows and therefore these test must be skipped.

--- Lib/test/test_multiprocessing_fork.py
+++ Lib/test/test_multiprocessing_fork.py
@@ -7,6 +7,9 @@ from test import support
 if support.PGO:
     raise unittest.SkipTest("test is not helpful for PGO")
 
+if sys.platform == "win32":
+    raise unittest.SkipTest("fork is not available on Windows")
+
 if sys.platform == 'darwin':
     raise unittest.SkipTest("test may crash on macOS (bpo-33725)")
 
