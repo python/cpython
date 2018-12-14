a61964c6a23070d7005bde39a7a47e77c032f087
Victor Stinner <vstinner@redhat.com>
2018-12-14 13:06:50 +0100

    bpo-34279: regrtest consider that skipped tests are ran (GH-11132)
    bpo-34279, bpo-35412: support.run_unittest() no longer raises
    TestDidNotRun if a test result contains skipped tests. The
    exception is now only raised if no test have been run and no test
    have been skipped.
    (cherry picked from commit 3a8f4fef4a4dd0e4a800545468eef9542e126181)
    
    Co-authored-by: Victor Stinner <vstinner@redhat.com>

--- Lib/test/test_regrtest.py
+++ Lib/test/test_regrtest.py
@@ -1009,6 +1009,7 @@ class ArgsTestCase(BaseTestCase):
         output = self.run_tests("-w", testname, exitcode=2)
         self.check_executed_tests(output, [testname],
                                   failed=testname, rerun=testname)
+
     def test_no_tests_ran(self):
         code = textwrap.dedent("""
             import unittest
@@ -1021,6 +1022,19 @@ class ArgsTestCase(BaseTestCase):
 
         output = self.run_tests(testname, "-m", "nosuchtest", exitcode=0)
         self.check_executed_tests(output, [testname], no_test_ran=testname)
+
+    def test_no_tests_ran_skip(self):
+        code = textwrap.dedent("""
+            import unittest
+
+            class Tests(unittest.TestCase):
+                def test_skipped(self):
+                    self.skipTest("because")
+        """)
+        testname = self.create_test(code=code)
+
+        output = self.run_tests(testname, exitcode=0)
+        self.check_executed_tests(output, [testname])
 
     def test_no_tests_ran_multiple_tests_nonexistent(self):
         code = textwrap.dedent("""
