3405406065074df9eb8b59257d2a2f068b8760a8
xtreak <tir.karthi@gmail.com>
2018-12-23 01:55:08 +0530

    More tests and note segfault scenario in news entry

--- Lib/test/test_float.py
+++ Lib/test/test_float.py
@@ -705,16 +705,20 @@ class FormatTestCase(unittest.TestCase):
         self.assertEqual(format(123.0, '00'), '123.0')
         self.assertEqual(format(123.34, '00f'), '123.340000')
         self.assertEqual(format(123.34, '00e'), '1.233400e+02')
-        self.assertEqual(format(123.340, '00g'), '123.34')
-        self.assertEqual(format(123.340, '00.10f'), '123.3400000000')
-        self.assertEqual(format(123.340, '0010f'), '123.340000')
+        self.assertEqual(format(123.34, '00g'), '123.34')
+        self.assertEqual(format(123.34, '00.10f'), '123.3400000000')
+        self.assertEqual(format(123.34, '00.10e'), '1.2334000000e+02')
+        self.assertEqual(format(123.34, '00.10g'), '123.34')
+        self.assertEqual(format(123.34, '01f'), '123.340000')
 
         self.assertEqual(format(-123.0, '00'), '-123.0')
         self.assertEqual(format(-123.34, '00f'), '-123.340000')
         self.assertEqual(format(-123.34, '00e'), '-1.233400e+02')
-        self.assertEqual(format(-123.340, '00g'), '-123.34')
-        self.assertEqual(format(-123.340, '00.10f'), '-123.3400000000')
-        self.assertEqual(format(-123.340, '0010f'), '-123.340000')
+        self.assertEqual(format(-123.34, '00g'), '-123.34')
+        self.assertEqual(format(-123.34, '00.10f'), '-123.3400000000')
+        self.assertEqual(format(-123.34, '00.10f'), '-123.3400000000')
+        self.assertEqual(format(-123.34, '00.10e'), '-1.2334000000e+02')
+        self.assertEqual(format(-123.34, '00.10g'), '-123.34')
 
 class ReprTestCase(unittest.TestCase):
     def test_repr(self):
