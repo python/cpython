d71d2f33d84318310c86153fb6a1ede3db9d8e98
Dylan Houlihan <dylan@breakingbits.net>
2018-12-21 22:07:09 -0500

    Revise the `b16.decode` docstring
    Since the `casefold` argument is removed, alter the docstring to reflect that and get rid of the reference to a second argument

--- Lib/base64.py
+++ Lib/base64.py
@@ -252,10 +252,7 @@ def b16encode(s):
 
 def b16decode(s):
     """Decode the Base16 encoded bytes-like object or ASCII string s.
-
-    Optional casefold is a flag specifying whether a lowercase alphabet is
-    acceptable as input.  For security purposes, the default is False.
-
+    
     The result is returned as a bytes object.  A binascii.Error is raised if
     s is incorrectly padded or if there are non-alphabet characters present
     in the input.
