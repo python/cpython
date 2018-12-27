77e9c16dca38f9b07e9065fa15e9cab11ce3669d
xtreak <tir.karthi@gmail.com>
2018-12-27 16:22:55 +0530

    Refactor cookie path check as per RFC 6265

--- Lib/http/cookiejar.py
+++ Lib/http/cookiejar.py
@@ -1190,11 +1190,14 @@ class DefaultCookiePolicy(CookiePolicy):
     def path_return_ok(self, path, request):
         _debug("- checking cookie path=%s", path)
         req_path = request_path(request)
-        if not req_path.startswith(path):
-            _debug("  %s does not path-match %s", req_path, path)
-            return False
-        return True
+        if req_path == path:
+            return True
+        elif (req_path.startswith(path) and
+              (path.endswith("/") or req_path[len(path)] == "/")):
+            return True
 
+        _debug("  %s does not path-match %s", req_path, path)
+        return False
 
 def vals_sorted_by_key(adict):
     keys = sorted(adict.keys())
