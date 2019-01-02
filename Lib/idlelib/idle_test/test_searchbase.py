654856daeea1415b0eb0f2f1ba85a00b1591c8a4
Terry Jan Reedy <tjreedy@udel.edu>
2019-01-02 18:25:35 -0500

    Use workaround for test_make_frame.
    See terryjreedy comment in issue 33987 pr 11395.

--- Lib/idlelib/idle_test/test_searchbase.py
+++ Lib/idlelib/idle_test/test_searchbase.py
@@ -98,11 +98,12 @@ class SearchDialogBaseTest(unittest.TestCase):
         self.dialog.top = self.root
         frame, label = self.dialog.make_frame()
         self.assertEqual(label, '')
-        self.assertIsInstance(frame, Frame)
-
+        self.assertEqual(str(type(frame)), "<class 'tkinter.ttk.Frame'>")
+        # self.assertIsInstance(frame, Frame) fails when test is run by
+        # test_idle not run from IDLE editor.  See issue 33987 PR.
+ 
         frame, label = self.dialog.make_frame('testlabel')
         self.assertEqual(label['text'], 'testlabel')
-        self.assertIsInstance(frame, Frame)
 
     def btn_test_setup(self, meth):
         self.dialog.top = self.root
