3e343e3a57006bacd02a989e1b20fc107f21105e
Ivan Levkivskyi <levkivskyi@gmail.com>
2019-01-06 21:29:26 +0000

    Regenerate nodes

--- Python/ast.c
+++ Python/ast.c
@@ -938,6 +938,8 @@ copy_location(expr_ty e, const node *n)
     if (e) {
         e->lineno = LINENO(n);
         e->col_offset = n->n_col_offset;
+        e->end_lineno = n->n_end_lineno;
+        e->end_col_offset = n->n_end_col_offset;
     }
     return e;
 }
