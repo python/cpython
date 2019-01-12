ca64f1d93510b236882f51a95eb8a098570ec0f2
Opher Shachar <ophers@users.noreply.github.com>
2019-01-12 02:54:52 +0200

    Update install_data.py

--- Lib/distutils/command/install_data.py
+++ Lib/distutils/command/install_data.py
@@ -28,7 +28,7 @@ class install_data(Command):
         self.install_dir = None
         self.outfiles = []
         self.root = None
-        self.force = 0
+        self.force = None
         self.data_files = self.distribution.data_files
         self.warn_dir = 1
 
