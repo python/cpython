db6d26072e75e4545c02ff9a983e40c11965329c
Serhiy Storchaka <storchaka@gmail.com>
2019-01-12 09:22:29 +0200

    bpo-33817: Fix _PyBytes_Resize() for empty bytes object. (GH-11516)
    Add also tests for PyUnicode_FromFormat() and PyBytes_FromFormat()
    with empty result.

--- Objects/bytesobject.c
+++ Objects/bytesobject.c
@@ -2991,8 +2991,21 @@ _PyBytes_Resize(PyObject **pv, Py_ssize_t newsize)
         /* return early if newsize equals to v->ob_size */
         return 0;
     }
+    if (Py_SIZE(v) == 0) {
+        if (newsize == 0) {
+            return 0;
+        }
+        *pv = _PyBytes_FromSize(newsize, 0);
+        Py_DECREF(v);
+        return (*pv == NULL) ? -1 : 0;
+    }
     if (Py_REFCNT(v) != 1) {
         goto error;
+    }
+    if (newsize == 0) {
+        *pv = _PyBytes_FromSize(0, 0);
+        Py_DECREF(v);
+        return (*pv == NULL) ? -1 : 0;
     }
     /* XXX UNREF/NEWREF interface should be more symmetrical */
     _Py_DEC_REFTOTAL;
