d27832785252bdb47f9c4246b0c31bb206065017
Opher Shachar <ophers@users.noreply.github.com>
2019-01-12 02:56:08 +0200

    Update install_lib.py

--- Lib/distutils/command/install_lib.py
+++ Lib/distutils/command/install_lib.py
@@ -52,7 +52,7 @@ class install_lib(Command):
         # let the 'install' command dictate our installation directory
         self.install_dir = None
         self.build_dir = None
-        self.force = 0
+        self.force = None
         self.compile = None
         self.optimize = None
         self.skip_build = None
