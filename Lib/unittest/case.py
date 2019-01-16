00190f8e2a9456bba518fc36b885cb4efb077bab
nanjekyejoannah <nanjekyejoannah@gmail.com>
2019-01-16 12:21:58 +0300

    fix space

--- Lib/unittest/case.py
+++ Lib/unittest/case.py
@@ -1253,7 +1253,7 @@ class TestCase(object):
             if (len(first) > self._diffThreshold or
                 len(second) > self._diffThreshold):
                 self._baseAssertEqual(first, second, msg)
-            firstlines = first.splitlines(keepends=True) 
+            firstlines = first.splitlines(keepends=True)
             secondlines = second.splitlines(keepends=True)
             if len(firstlines) == 1 and first.strip('\r\n') == first:
                 firstlines = [first + '\n']
