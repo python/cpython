dcf86e88a7ca4a8b40a0223da0f6b6e41d68a2f2
Guido van Rossum <guido@python.org>
2019-01-30 08:23:52 -0800

    Brace block in parsetok.c

--- Parser/parsetok.c
+++ Parser/parsetok.c
@@ -36,8 +36,9 @@ growable_int_array_add(growable_int_array *arr, int item) {
     if (arr->num_items >= arr->size) {
         arr->size *= 2;
         arr->items = realloc(arr->items, arr->size * sizeof(*arr->items));
-        if (!arr->items)
+        if (!arr->items) {
             return 0;
+        }
     }
 
     arr->items[arr->num_items] = item;
