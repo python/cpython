d255168634940c8091b0a6fd2a5651147082fc9a
Raymond Hettinger <raymond.hettinger@gmail.com>
2019-02-02 20:17:05 -0800

    Add timings for data structure access. Make easier to run on 2.7.  Organize output.

--- Tools/scripts/var_access_benchmark.py
+++ Tools/scripts/var_access_benchmark.py
@@ -2,12 +2,26 @@
 
 # Please leave this code so that it runs under older versions of
 # Python 3 (no f-strings).  That will allow benchmarking for
-# cross-version comparisons.
+# cross-version comparisons.  To run the benchmark on Python 2,
+# comment-out the nonlocal reads and writes.
 
-from collections import namedtuple
+from collections import deque, namedtuple
 
 trials = [None] * 500
 steps_per_trial = 25
+
+class A(object):
+    def m(self):
+        pass
+
+class B(object):
+    __slots__ = 'x'
+    def __init__(self, x):
+        self.x = x
+
+class C(object):
+    def __init__(self, x):
+        self.x = x
 
 def read_local(trials=trials):
     v_local = 1
@@ -49,13 +63,6 @@ def read_builtin(trials=trials):
         oct; oct; oct; oct; oct
         oct; oct; oct; oct; oct
 
-class A:
-    def m(self):
-        pass
-
-class B(object):
-    __slots__ = 'x'
-
 def read_classvar(trials=trials, A=A):
     A.x = 1
     for t in trials:
@@ -65,8 +72,7 @@ def read_classvar(trials=trials, A=A):
         A.x;    A.x;    A.x;    A.x;    A.x
         A.x;    A.x;    A.x;    A.x;    A.x
 
-def read_instancevar(trials=trials, a=A()):
-    a.x = 1
+def read_instancevar(trials=trials, a=C(1)):
     for t in trials:
         a.x;    a.x;    a.x;    a.x;    a.x
         a.x;    a.x;    a.x;    a.x;    a.x
@@ -74,8 +80,7 @@ def read_instancevar(trials=trials, a=A()):
         a.x;    a.x;    a.x;    a.x;    a.x
         a.x;    a.x;    a.x;    a.x;    a.x
 
-def read_instancevar_slots(trials=trials, a=B()):
-    a.x = 1
+def read_instancevar_slots(trials=trials, a=B(1)):
     for t in trials:
         a.x;    a.x;    a.x;    a.x;    a.x
         a.x;    a.x;    a.x;    a.x;    a.x
@@ -83,8 +88,8 @@ def read_instancevar_slots(trials=trials, a=B()):
         a.x;    a.x;    a.x;    a.x;    a.x
         a.x;    a.x;    a.x;    a.x;    a.x
 
-def read_namedtuple(trials=trials, C=namedtuple('C', ['x'])):
-    a = C(1)
+def read_namedtuple(trials=trials, D=namedtuple('D', ['x'])):
+    a = D(1)
     for t in trials:
         a.x;    a.x;    a.x;    a.x;    a.x
         a.x;    a.x;    a.x;    a.x;    a.x
@@ -141,7 +146,7 @@ def write_classvar(trials=trials, A=A):
         A.x = 1;    A.x = 1;    A.x = 1;    A.x = 1;    A.x = 1
         A.x = 1;    A.x = 1;    A.x = 1;    A.x = 1;    A.x = 1
 
-def write_instancevar(trials=trials, a=A()):
+def write_instancevar(trials=trials, a=C(1)):
     for t in trials:
         a.x = 1;    a.x = 1;    a.x = 1;    a.x = 1;    a.x = 1
         a.x = 1;    a.x = 1;    a.x = 1;    a.x = 1;    a.x = 1
@@ -149,13 +154,79 @@ def write_instancevar(trials=trials, a=A()):
         a.x = 1;    a.x = 1;    a.x = 1;    a.x = 1;    a.x = 1
         a.x = 1;    a.x = 1;    a.x = 1;    a.x = 1;    a.x = 1
 
-def write_instancevar_slots(trials=trials, a=B()):
+def write_instancevar_slots(trials=trials, a=B(1)):
     for t in trials:
         a.x = 1;    a.x = 1;    a.x = 1;    a.x = 1;    a.x = 1
         a.x = 1;    a.x = 1;    a.x = 1;    a.x = 1;    a.x = 1
         a.x = 1;    a.x = 1;    a.x = 1;    a.x = 1;    a.x = 1
         a.x = 1;    a.x = 1;    a.x = 1;    a.x = 1;    a.x = 1
         a.x = 1;    a.x = 1;    a.x = 1;    a.x = 1;    a.x = 1
+
+def read_list(trials=trials, a=[1]):
+    for t in trials:
+        a[0];   a[0];   a[0];   a[0];   a[0]
+        a[0];   a[0];   a[0];   a[0];   a[0]
+        a[0];   a[0];   a[0];   a[0];   a[0]
+        a[0];   a[0];   a[0];   a[0];   a[0]
+        a[0];   a[0];   a[0];   a[0];   a[0]
+
+def read_deque(trials=trials, a=deque([1])):
+    for t in trials:
+        a[0];   a[0];   a[0];   a[0];   a[0]
+        a[0];   a[0];   a[0];   a[0];   a[0]
+        a[0];   a[0];   a[0];   a[0];   a[0]
+        a[0];   a[0];   a[0];   a[0];   a[0]
+        a[0];   a[0];   a[0];   a[0];   a[0]
+
+def read_dict(trials=trials, a={0: 1}):
+    for t in trials:
+        a[0];   a[0];   a[0];   a[0];   a[0]
+        a[0];   a[0];   a[0];   a[0];   a[0]
+        a[0];   a[0];   a[0];   a[0];   a[0]
+        a[0];   a[0];   a[0];   a[0];   a[0]
+        a[0];   a[0];   a[0];   a[0];   a[0]
+
+def list_append_pop(trials=trials, a=[1]):
+    ap, pop = a.append, a.pop
+    for t in trials:
+        ap(1); pop(); ap(1); pop(); ap(1); pop(); ap(1); pop(); ap(1); pop();
+        ap(1); pop(); ap(1); pop(); ap(1); pop(); ap(1); pop(); ap(1); pop();
+        ap(1); pop(); ap(1); pop(); ap(1); pop(); ap(1); pop(); ap(1); pop();
+        ap(1); pop(); ap(1); pop(); ap(1); pop(); ap(1); pop(); ap(1); pop();
+        ap(1); pop(); ap(1); pop(); ap(1); pop(); ap(1); pop(); ap(1); pop();
+
+def deque_append_pop(trials=trials, a=deque([1])):
+    ap, pop = a.append, a.pop
+    for t in trials:
+        ap(1); pop(); ap(1); pop(); ap(1); pop(); ap(1); pop(); ap(1); pop();
+        ap(1); pop(); ap(1); pop(); ap(1); pop(); ap(1); pop(); ap(1); pop();
+        ap(1); pop(); ap(1); pop(); ap(1); pop(); ap(1); pop(); ap(1); pop();
+        ap(1); pop(); ap(1); pop(); ap(1); pop(); ap(1); pop(); ap(1); pop();
+        ap(1); pop(); ap(1); pop(); ap(1); pop(); ap(1); pop(); ap(1); pop();
+
+def write_list(trials=trials, a=[1]):
+    for t in trials:
+        a[0]=1; a[0]=1; a[0]=1; a[0]=1; a[0]=1
+        a[0]=1; a[0]=1; a[0]=1; a[0]=1; a[0]=1
+        a[0]=1; a[0]=1; a[0]=1; a[0]=1; a[0]=1
+        a[0]=1; a[0]=1; a[0]=1; a[0]=1; a[0]=1
+        a[0]=1; a[0]=1; a[0]=1; a[0]=1; a[0]=1
+
+def write_deque(trials=trials, a=deque([1])):
+    for t in trials:
+        a[0]=1; a[0]=1; a[0]=1; a[0]=1; a[0]=1
+        a[0]=1; a[0]=1; a[0]=1; a[0]=1; a[0]=1
+        a[0]=1; a[0]=1; a[0]=1; a[0]=1; a[0]=1
+        a[0]=1; a[0]=1; a[0]=1; a[0]=1; a[0]=1
+        a[0]=1; a[0]=1; a[0]=1; a[0]=1; a[0]=1
+
+def write_dict(trials=trials, a={0: 1}):
+    for t in trials:
+        a[0]=1; a[0]=1; a[0]=1; a[0]=1; a[0]=1
+        a[0]=1; a[0]=1; a[0]=1; a[0]=1; a[0]=1
+        a[0]=1; a[0]=1; a[0]=1; a[0]=1; a[0]=1
+        a[0]=1; a[0]=1; a[0]=1; a[0]=1; a[0]=1
+        a[0]=1; a[0]=1; a[0]=1; a[0]=1; a[0]=1
 
 def loop_overhead(trials=trials):
     for t in trials:
@@ -166,13 +237,25 @@ if __name__=='__main__':
 
     from timeit import Timer
 
-    print('Speed of different kinds of variable accesses:')
-    for f in [read_local, read_nonlocal, read_global, read_builtin,
+    for f in [
+              'Variable and attribute read access:',
+              read_local, read_nonlocal, read_global, read_builtin,
               read_classvar, read_instancevar, read_instancevar_slots,
               read_namedtuple, read_boundmethod,
+              '\nVariable and attribute write access:',
               write_local, write_nonlocal, write_global,
               write_classvar, write_instancevar, write_instancevar_slots,
+              '\nData structure read access:',
+              read_list, read_deque, read_dict,
+              '\nData structure write access:',
+              write_list, write_deque, write_dict,
+              '\nStack (or queue) operations:',
+              list_append_pop, deque_append_pop,
+              '\nTiming loop overhead:',
               loop_overhead]:
+        if isinstance(f, str):
+            print(f)
+            continue
         timing = min(Timer(f).repeat(7, 1000))
         timing *= 1000000 / (len(trials) * steps_per_trial)
-        print('{:6.1f} \N{greek small letter mu}s\t{}'.format(timing, f.__name__))
+        print(u'{:6.1f} \N{greek small letter mu}s\t{}'.format(timing, f.__name__))
