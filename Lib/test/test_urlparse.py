36d2ed223b3b850b514b57269c3fee7a9aa0f1b9
Stéphane Wirtel <stephane@wirtel.be>
2019-02-06 10:24:09 +0100

    limit to 72 colums

--- Lib/test/test_urlparse.py
+++ Lib/test/test_urlparse.py
@@ -1037,7 +1037,8 @@ class Utility_Tests(unittest.TestCase):
                          ('example.net', '/file#'))
 
         # bpo-35906: # avoid the header injection
-        self.assertEqual(splithost('//127.0.0.1:1234/?q=HTTP/1.1\r\nHeader: Value'),
+        self.assertEqual(
+            splithost('//127.0.0.1:1234/?q=HTTP/1.1\r\nHeader: Value'),
             ('127.0.0.1:1234', '/?q=HTTP/1.1'))
 
     def test_splituser(self):
