382993cb52ec82fac2c5cf695a00aad66a1aa6cb
Ivan Levkivskyi <levkivskyi@gmail.com>
2019-02-10 15:39:49 +0000

    Fix compiler warnings about end_col_offset and end_lineno (GH-11735)

--- Python/ast.c
+++ Python/ast.c
@@ -3871,11 +3871,8 @@ static void
 get_last_end_pos(asdl_seq *s, int *end_lineno, int *end_col_offset)
 {
     int tot = asdl_seq_LEN(s);
-    // Suite should not be empty, but it is safe to just ignore it
-    // if it will ever occur.
-    if (!tot) {
-        return;
-    }
+    // There must be no empty suites.
+    assert(tot > 0);
     stmt_ty last = asdl_seq_GET(s, tot - 1);
     *end_lineno = last->end_lineno;
     *end_col_offset = last->end_col_offset;
