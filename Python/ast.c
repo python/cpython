fdcef82c422efb9e591e82fdea7b6d304fe004ee
Guido van Rossum <guido@python.org>
2019-02-11 08:10:42 -0800

    bpo-35766 follow-up: Add an error check to new_type_comment() (#11766)
    If PyUnicode_DecodeUTF8() returns NULL, PyArena_AddPyObject() would crash.
    Found by @msullivan for https://github.com/python/typed_ast/pull/93.

--- Python/ast.c
+++ Python/ast.c
@@ -702,6 +702,8 @@ static string
 new_type_comment(const char *s, struct compiling *c)
 {
     PyObject *res = PyUnicode_DecodeUTF8(s, strlen(s), NULL);
+    if (res == NULL)
+        return NULL;
     if (PyArena_AddPyObject(c->c_arena, res) < 0) {
         Py_DECREF(res);
         return NULL;
