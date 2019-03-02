f790df93cca90173fa06ce77804cf1c8ff5809a9
Alex Gaynor <alex.gaynor@gmail.com>
2019-03-01 23:37:34 -0500

    Remove some code which has been dead since 1994 (#12136)

--- Parser/grammar1.c
+++ Parser/grammar1.c
@@ -12,21 +12,10 @@ dfa *
 PyGrammar_FindDFA(grammar *g, int type)
 {
     dfa *d;
-#if 1
     /* Massive speed-up */
     d = &g->g_dfa[type - NT_OFFSET];
     assert(d->d_type == type);
     return d;
-#else
-    /* Old, slow version */
-    int i;
-
-    for (i = g->g_ndfas, d = g->g_dfa; --i >= 0; d++) {
-        if (d->d_type == type)
-            return d;
-    }
-    Py_UNREACHABLE();
-#endif
 }
 
 const char *
