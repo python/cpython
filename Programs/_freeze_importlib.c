272ea8c70472edd2accfef456c66a37399625e25
Victor Stinner <vstinner@redhat.com>
2019-03-05 02:01:27 +0100

    bpo-36142: Add _PyPreConfig structure (GH-12172)
    * Add _PyPreConfig structure
    * Move 'ignored' and 'use_environment' fields from _PyCoreConfig
      to _PyPreConfig
    * Add a new "_PyPreConfig preconfig;" field to _PyCoreConfig

--- Programs/_freeze_importlib.c
+++ Programs/_freeze_importlib.c
@@ -77,9 +77,9 @@ main(int argc, char *argv[])
     text[text_size] = '\0';
 
     _PyCoreConfig config = _PyCoreConfig_INIT;
+    config.preconfig.use_environment = 0;
     config.user_site_directory = 0;
     config.site_import = 0;
-    config.use_environment = 0;
     config.program_name = L"./_freeze_importlib";
     /* Don't install importlib, since it could execute outdated bytecode. */
     config._install_importlib = 0;
