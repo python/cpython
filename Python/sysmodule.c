daddb71936ecf78c95052674adbf4039fc35853e
Victor Stinner <vstinner@redhat.com>
2019-03-05 02:19:53 +0100

    bpo-36142: Add _PyPreConfig.utf8_mode
    * Move following fields from _PyCoreConfig to _PyPreConfig:
    
      * coerce_c_locale
      * coerce_c_locale_warn
      * legacy_windows_stdio
      * utf8_mode
    
    * _PyPreConfig_ReadFromArgv() is now responsible to choose the
      filesystem encoding
    * _PyPreConfig_Write() now sets the LC_CTYPE locale

--- Python/sysmodule.c
+++ Python/sysmodule.c
@@ -2181,7 +2181,7 @@ make_flags(void)
     SetFlag(config->use_hash_seed == 0 || config->hash_seed != 0);
     SetFlag(config->preconfig.isolated);
     PyStructSequence_SET_ITEM(seq, pos++, PyBool_FromLong(config->dev_mode));
-    SetFlag(config->utf8_mode);
+    SetFlag(config->preconfig.utf8_mode);
 #undef SetFlag
 
     if (PyErr_Occurred()) {
