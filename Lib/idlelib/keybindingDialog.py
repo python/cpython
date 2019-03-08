498468d9c3f53d9cfdd79cf1dc83251316d6d3df
Terry Jan Reedy <tjreedy@udel.edu>
2019-03-07 22:16:07 -0500

    [2.7] IDLE: Fix typo in keybindingDialog.py (GH-2322) (GH-12231)
    Cherry picked by hand from a0e911b

--- Lib/idlelib/keybindingDialog.py
+++ Lib/idlelib/keybindingDialog.py
@@ -182,7 +182,7 @@ class GetKeysDialog(Toplevel):
 
     def LoadFinalKeyList(self):
         #these tuples are also available for use in validity checks
-        self.functionKeys=('F1','F2','F2','F4','F5','F6','F7','F8','F9',
+        self.functionKeys=('F1','F2','F3','F4','F5','F6','F7','F8','F9',
                 'F10','F11','F12')
         self.alphanumKeys=tuple(string.ascii_lowercase+string.digits)
         self.punctuationKeys=tuple('~!@#%^&*()_-+={}[]|;:,.<>/?')
