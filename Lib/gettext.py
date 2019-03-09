1b65977025b67c9e030e7d69f904587c5a2ac332
Julien Palard <julien@palard.fr>
2019-03-09 23:50:26 +0100

    FIX: Skip comments in gettext infos.

--- Lib/gettext.py
+++ Lib/gettext.py
@@ -417,6 +417,8 @@ class GNUTranslations(NullTranslations):
                     item = b_item.decode().strip()
                     if not item:
                         continue
+                    if item.startswith('#-#-#-#-#') and item.endswith('#-#-#-#-#'):
+                        continue
                     k = v = None
                     if ':' in item:
                         k, v = item.split(':', 1)
