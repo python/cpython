7633bf1c6e2f5e2d545b4e562a91d3b09eaf3446
Guido van Rossum <guido@python.org>
2019-03-13 11:37:07 -0700

    Check for errors in make_kind()

--- Python/ast.c
+++ Python/ast.c
@@ -5428,13 +5428,17 @@ static expr_ty
 make_str_node_and_del(PyObject **str, struct compiling *c, const node* n)
 {
     PyObject *s = *str;
+    PyObject *kind = NULL;
     *str = NULL;
     assert(PyUnicode_CheckExact(s));
     if (PyArena_AddPyObject(c->c_arena, s) < 0) {
         Py_DECREF(s);
         return NULL;
     }
-    return Constant(s, make_kind(c, n), LINENO(n), n->n_col_offset,
+    kind = make_kind(c, n);
+    if (kind == NULL && PyErr_Occurred())
+        return NULL;
+    return Constant(s, kind, LINENO(n), n->n_col_offset,
                     n->n_end_lineno, n->n_end_col_offset, c->c_arena);
 }
 
