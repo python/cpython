2086069cf3136baadd3d705dd066e4900ee8d37f
Rémi Lapeyre <remi.lapeyre@henki.fr>
2019-03-15 07:53:34 +0100

    bpo-36272: Logging now propagates RecursionError (GH-12312)

--- Lib/logging/__init__.py
+++ Lib/logging/__init__.py
@@ -1032,6 +1032,8 @@ class Handler(Filterer):
                     sys.stderr.write('Message: %r\n'
                                      'Arguments: %s\n' % (record.msg,
                                                           record.args))
+                except RecursionError:  # See issue 36272
+                    raise
                 except Exception:
                     sys.stderr.write('Unable to print the message and arguments'
                                      ' - possible formatting error.\nUse the'
@@ -1094,6 +1096,8 @@ class StreamHandler(Handler):
             # issue 35046: merged two stream.writes into one.
             stream.write(msg + self.terminator)
             self.flush()
+        except RecursionError:  # See issue 36272
+            raise
         except Exception:
             self.handleError(record)
 
