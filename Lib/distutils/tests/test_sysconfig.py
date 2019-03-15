37f6971777c05b5ca9c157606896b7ff458756a5
Victor Stinner <vstinner@redhat.com>
2019-03-15 16:03:44 +0100

    bpo-36235: Fix CFLAGS in distutils customize_compiler() (GH-12236) (GH-12349)
    Fix CFLAGS in customize_compiler() of distutils.sysconfig: when the
    CFLAGS environment variable is defined, don't override CFLAGS variable with
    the OPT variable anymore.
    
    Initial patch written by David Malcolm.
    
    Co-Authored-By: David Malcolm <dmalcolm@redhat.com>
    (cherry picked from commit 86082c22d23285995a32aabb491527c9f5629556)

--- Lib/distutils/tests/test_sysconfig.py
+++ Lib/distutils/tests/test_sysconfig.py
@@ -8,8 +8,9 @@ import sys
 import textwrap
 
 from distutils import sysconfig
+from distutils.ccompiler import get_default_compiler
 from distutils.tests import support
-from test.test_support import TESTFN
+from test.test_support import TESTFN, swap_item
 
 class SysconfigTestCase(support.EnvironGuard,
                         unittest.TestCase):
@@ -49,6 +50,30 @@ class SysconfigTestCase(support.EnvironGuard,
         self.assertTrue(os.path.isdir(inc_dir), inc_dir)
         python_h = os.path.join(inc_dir, "Python.h")
         self.assertTrue(os.path.isfile(python_h), python_h)
+
+    @unittest.skipUnless(get_default_compiler() == 'unix',
+                         'not testing if default compiler is not unix')
+    def test_customize_compiler(self):
+        os.environ['AR'] = 'my_ar'
+        os.environ['CC'] = 'my_cc'
+        os.environ['ARFLAGS'] = '--myarflags'
+        os.environ['CFLAGS'] = '--mycflags'
+
+        # make sure AR gets caught
+        class compiler:
+            compiler_type = 'unix'
+
+            def set_executables(self, **kw):
+                self.exes = kw
+
+        # Make sure that sysconfig._config_vars is initialized
+        sysconfig.get_config_vars()
+
+        comp = compiler()
+        with swap_item(sysconfig._config_vars, 'CFLAGS', '--sysconfig-cflags'):
+            sysconfig.customize_compiler(comp)
+        self.assertEqual(comp.exes['archiver'], 'my_ar --myarflags')
+        self.assertEqual(comp.exes['compiler'], 'my_cc --sysconfig-cflags --mycflags')
 
     def test_parse_makefile_base(self):
         self.makefile = test.test_support.TESTFN
