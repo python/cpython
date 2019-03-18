814034d5313df39ee5f83f9943baa54d30826894
Paul Monson <paulmon@microsoft.com>
2019-03-18 12:42:21 -0700

    optionally install cygwin with required packages

--- PCbuild/prepare_libffi.bat
+++ PCbuild/prepare_libffi.bat
@@ -3,7 +3,7 @@ goto :Run
 
 :Usage
 echo.
-echo Before running %~nx0 
+echo Before running prepare_libffi.bat 
 echo   LIBFFI_SOURCE environment variable must be set to the location of
 echo     of python-source-deps clone of libffi branch
 echo   VCVARSALL must be set to location of vcvarsall.bat
@@ -16,12 +16,41 @@ echo Include the following cygwin packages in cygwin configuration:
 echo     make, autoconf, automake, libtool, dejagnu
 echo.
 echo NOTE: dejagnu is only required for running tests.
+echo       set LIBFFI_TEST=1 to run tests (optional)
 echo.
 echo Based on https://github.com/libffi/libffi/blob/master/.appveyor.yml
 echo.
+echo.
+echo.Available flags:
+echo.  -x64    build for x64
+echo.  -x86    build for x86
+echo.  -?      this help
+echo.  --install-cygwin  install cygwin to c:\cygwin
 exit /b 127
 
 :Run
+
+set BUILD_X64=
+set BUILD_X86=
+set INSTALL_CYGWIN=
+
+:CheckOpts
+if "%1"=="" goto :CheckOptsDone
+if /I "%1"=="-x64" (set BUILD_X64=1) & shift & goto :CheckOpts
+if /I "%1"=="-x86" (set BUILD_X86=1) & shift & goto :CheckOpts
+if /I "%1"=="-?" goto :Usage
+if /I "%1"=="--install-cygwin" (set INSTALL_CYGWIN=1) & shift & goto :CheckOpts
+goto :Usage
+
+:CheckOptsDone
+
+if "%BUILD_X64%"=="" if "%BUILD_X86%"=="" if "%BUILD_ARM32%"=="" (
+    set BUILD_X64=1
+    set BUILD_X86=1
+)
+
+if "%INSTALL_CYGWIN%"=="1" call :InstallCygwin
+
 setlocal
 if "%SH%" EQU "" if exist c:\cygwin\bin\sh.exe set SH=c:\cygwin\bin\sh.exe
 
@@ -40,6 +69,7 @@ if not exist %LIBFFI_SOURCE% echo ERROR %LIBFFI_SOURCE% does not exist && goto :
 set OLDPWD=%LIBFFI_SOURCE%
 pushd %LIBFFI_SOURCE%
 
+%SH% --login -lc "cygcheck -dc cygwin"
 set GET_MSVCC=%SH% -lc "cd $OLDPWD; export MSVCC=`/usr/bin/find $PWD -name msvcc.sh`; echo ${MSVCC};"
 FOR /F "usebackq delims==" %%i IN (`%GET_MSVCC%`) do @set MSVCC=%%i
 
@@ -50,7 +80,7 @@ echo LIBFFI_SOURCE: %LIBFFI_SOURCE%
 echo MSVCC        : %MSVCC%
 echo.
 
-%SH% -lc "(cd $LIBFFI_SOURCE; ./autogen.sh;)"
+if not exist Makefile.in (%SH% -lc "(cd $LIBFFI_SOURCE; ./autogen.sh;)")
 
 call :BuildOne x86 i686-pc-cygwin i686-pc-cygwin
 call :BuildOne x64 x86_64-w64-cygwin x86_64-w64-cygwin
@@ -70,19 +100,22 @@ REM Initialize variables
 set VCVARS_PLATFORM=%1
 set BUILD=%2
 set HOST=%3
-set ML=
-set SRC_CPU=x86
+set ASSEMBLER=
+set SRC_ARCHITECTURE=x86
 
 if "%VCVARS_PLATFORM%" EQU "" echo ERROR bad VCVARS_PLATFORM&&exit /b 123
-if /I "%VCVARS_PLATFORM%" EQU "x64" (set ML=-m64)
 
 if /I "%VCVARS_PLATFORM%" EQU "x64" (
     set ARCH=amd64
     set ARTIFACTS=%LIBFFI_SOURCE%\x86_64-w64-cygwin
+    set ASSEMBLER=-m64
+    set SRC_ARCHITECTURE=x86
 )
 if /I "%VCVARS_PLATFORM%" EQU "x86" (
     set ARCH=win32
     set ARTIFACTS=%LIBFFI_SOURCE%\i686-pc-cygwin
+    set ASSEMBLER=
+    set SRC_ARCHITECTURE=x86
 )
 
 set LIBFFI_OUT=%~dp0\..\externals\libffi-bin-3.3.0-rc0-r1\%ARCH%
@@ -94,10 +127,10 @@ echo clean %LIBFFI_OUT%
 if exist %LIBFFI_OUT% (rd %LIBFFI_OUT% /s/q)
 
 echo Configure the build to generate fficonfig.h and ffi.h
-%SH% -lc "(cd $OLDPWD; ./configure CC='%MSVCC% %ML%' CXX='%MSVCC% %ML%' LD='link' CPP='cl -nologo -EP' CXXCPP='cl -nologo -EP' CPPFLAGS='-DFFI_BUILDING_DLL' NM='dumpbin -symbols' STRIP=':' --build=$BUILD --host=$HOST;)"
+%SH% -lc "(cd $OLDPWD; ./configure CC='%MSVCC% %ASSEMBLER%' CXX='%MSVCC% %ASSEMBLER%' LD='link' CPP='cl -nologo -EP' CXXCPP='cl -nologo -EP' CPPFLAGS='-DFFI_BUILDING_DLL' NM='dumpbin -symbols' STRIP=':' --build=$BUILD --host=$HOST;)"
 
 echo Building libffi
-%SH% -lc "(cd $OLDPWD; export PATH=/usr/bin:$PATH; cp src/%SRC_CPU%/ffitarget.h include; make; find .;)"
+%SH% -lc "(cd $OLDPWD; export PATH=/usr/bin:$PATH; cp src/%SRC_ARCHITECTURE%/ffitarget.h include; make; find .;)"
 
 REM Tests are not needed to produce artifacts
 if "%LIBFFI_TEST%" EQU "1" (
@@ -118,4 +151,17 @@ copy %ARTIFACTS%\include\*.h %LIBFFI_OUT%\include
 endlocal
 exit /b
 
+:InstallCygwin
+setlocal
+
+if "%CYG_ROOT%"=="" (set CYG_ROOT=c:/cygwin)
+if "%CYG_CACHE%"=="" (set CYG_CACHE=C:/cygwin/var/cache/setup)
+if "%CYG_MIRROR%"=="" (set CYG_MIRROR=http://mirrors.kernel.org/sourceware/cygwin/)
+
+powershell -c "md $env:CYG_ROOT -ErrorAction SilentlyContinue"
+powershell -c "$setup = $env:CYG_ROOT+'/setup.exe'; if (!(Test-Path $setup)){invoke-webrequest https://cygwin.com/setup-x86.exe -outfile $setup}
+%CYG_ROOT%/setup.exe -qnNdO -R "%CYG_ROOT%" -s "%CYG_MIRROR%" -l "%CYG_CACHE%" -P make -P autoconf -P automake -P libtool -P dejagnu
+
 endlocal
+exit /b
+
