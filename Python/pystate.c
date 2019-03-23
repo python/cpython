6d5ee973f0600a3a9444f569dcf0dd346bfa2a11
Victor Stinner <vstinner@redhat.com>
2019-03-23 12:05:43 +0100

    bpo-36301: Add _PyRuntimeState.preconfig (GH-12506)
    _PyPreConfig_Write() now writes the applied pre-configuration into
    _PyRuntimeState.preconfig.

--- Python/pystate.c
+++ Python/pystate.c
@@ -41,6 +41,7 @@ _PyRuntimeState_Init_impl(_PyRuntimeState *runtime)
 
     _PyGC_Initialize(&runtime->gc);
     _PyEval_Initialize(&runtime->ceval);
+    runtime->preconfig = _PyPreConfig_INIT;
 
     runtime->gilstate.check_enabled = 1;
 
@@ -96,6 +97,8 @@ _PyRuntimeState_Fini(_PyRuntimeState *runtime)
         PyThread_free_lock(runtime->xidregistry.mutex);
         runtime->xidregistry.mutex = NULL;
     }
+
+    _PyPreConfig_Clear(&runtime->preconfig);
 
     PyMem_SetAllocator(PYMEM_DOMAIN_RAW, &old_alloc);
 }
