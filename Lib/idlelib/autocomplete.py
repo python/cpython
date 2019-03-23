b479e0b914fee40dbc1c270792630f4b8a220b59
Terry Jan Reedy <tjreedy@udel.edu>
2019-03-23 03:50:15 -0400

    bpo-36405: Use dict unpacking in idlelib (#12507)
    Remove now unneeded imports.

--- Lib/idlelib/autocomplete.py
+++ Lib/idlelib/autocomplete.py
@@ -14,7 +14,6 @@ COMPLETE_ATTRIBUTES, COMPLETE_FILES = range(1, 2+1)
 from idlelib import autocomplete_w
 from idlelib.config import idleConf
 from idlelib.hyperparser import HyperParser
-import __main__
 
 # This string includes all chars that may be in an identifier.
 # TODO Update this here and elsewhere.
@@ -182,8 +181,7 @@ class AutoComplete:
         else:
             if mode == COMPLETE_ATTRIBUTES:
                 if what == "":
-                    namespace = __main__.__dict__.copy()
-                    namespace.update(__main__.__builtins__.__dict__)
+                    namespace = {**__builtins__.__dict__, **globals()}
                     bigl = eval("dir()", namespace)
                     bigl.sort()
                     if "__all__" in bigl:
@@ -218,10 +216,8 @@ class AutoComplete:
             return smalll, bigl
 
     def get_entity(self, name):
-        """Lookup name in a namespace spanning sys.modules and __main.dict__"""
-        namespace = sys.modules.copy()
-        namespace.update(__main__.__dict__)
-        return eval(name, namespace)
+        "Lookup name in a namespace spanning sys.modules and globals()."
+        return eval(name, {**sys.modules, **globals()})
 
 
 AutoComplete.reload()
