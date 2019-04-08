28058cb52d8b4bfb1372b8dbbf4836f72eb858d5
Giampaolo Rodola <g.rodola@gmail.com>
2019-04-09 00:34:02 +0200

    bpo-35934: Add socket.create_server() utility function (GH-11784)

--- Lib/test/test_epoll.py
+++ Lib/test/test_epoll.py
@@ -41,9 +41,7 @@ except OSError as e:
 class TestEPoll(unittest.TestCase):
 
     def setUp(self):
-        self.serverSocket = socket.socket()
-        self.serverSocket.bind(('127.0.0.1', 0))
-        self.serverSocket.listen()
+        self.serverSocket = socket.create_server(('127.0.0.1', 0))
         self.connections = [self.serverSocket]
 
     def tearDown(self):
