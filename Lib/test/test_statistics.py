b1726a9d27dad337fec598516406205d3d1279fb
Raymond Hettinger <raymond.hettinger@gmail.com>
2019-04-09 08:34:03 -0700

    Cover more cases

--- Lib/test/test_statistics.py
+++ Lib/test/test_statistics.py
@@ -2183,11 +2183,21 @@ class TestQuantiles(unittest.TestCase):
         while len(set(data)) != total:
             data.append(random.expovariate(0.2))
         data.sort()
+
+        # Cases where the group size exactly divides the total
         for n in (1, 2, 5, 10, 20, 50, 100, 200, 500, 1000, 2000, 5000, 10000):
             group_size = total // n
             self.assertEqual(
                 [bisect.bisect(data, q) for q in quantiles(data, n=n)],
                 list(range(group_size, total, group_size)))
+
+        # When the group sizes can't be exactly equal, they should
+        # differ by no more than one
+        for n in (13, 19, 59, 109, 211, 571, 1019, 1907, 5261, 9769):
+            group_sizes = {total // n, total // n + 1}
+            pos = [bisect.bisect(data, q) for q in quantiles(data, n=n)]
+            sizes = {q - p for p, q in zip(pos, pos[1:])}
+            self.assertTrue(sizes <= group_sizes)
 
     def test_error_cases(self):
         quantiles = statistics.quantiles
