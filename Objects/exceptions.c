116cfc1cd41974fbddeadd525ab31efef5dfaeee
Inada Naoki <songofacandy@gmail.com>
2019-04-09 17:18:38 +0900

    fix

--- Objects/exceptions.c
+++ Objects/exceptions.c
@@ -2014,8 +2014,8 @@ SimpleExtendsException(PyExc_Warning, UnicodeWarning,
  *    BytesWarning extends Warning
  */
 SimpleExtendsException(PyExc_Warning, BytesWarning,
-    "Base class for warnings about str and bytearray related problems, mostly\n"
-    "related to comparing to unicode.");
+    "Base class for warnings about bytes and bytearray related problems, \n"
+    "mostly related to conversion from str or comparing to str.");
 
 /* Pre-computed MemoryError instance.  Best to create this as early as
  * possible and not wait until a MemoryError is actually raised!
