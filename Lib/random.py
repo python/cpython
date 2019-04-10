3c145d1cb8f767c826221199e95e697ff46b6aa4
Christian Heimes <christian@python.org>
2019-04-10 22:18:02 +0200

    bpo-36559: random module: optimize sha512 import (GH-12742)
    The random module now prefers the lean internal _sha512 module over hashlib
    for seed(version=2) to optimize import time.
    
    Signed-off-by: Christian Heimes <christian@python.org>

--- Lib/random.py
+++ Lib/random.py
@@ -42,10 +42,17 @@ from math import log as _log, exp as _exp, pi as _pi, e as _e, ceil as _ceil
 from math import sqrt as _sqrt, acos as _acos, cos as _cos, sin as _sin
 from os import urandom as _urandom
 from _collections_abc import Set as _Set, Sequence as _Sequence
-from hashlib import sha512 as _sha512
 from itertools import accumulate as _accumulate, repeat as _repeat
 from bisect import bisect as _bisect
 import os as _os
+
+try:
+    # hashlib is pretty heavy to load, try lean internal module first
+    from _sha512 import sha512 as _sha512
+except ImportError:
+    # fallback to official implementation
+    from hashlib import sha512 as _sha512
+
 
 __all__ = ["Random","seed","random","uniform","randint","choice","sample",
            "randrange","shuffle","normalvariate","lognormvariate",
