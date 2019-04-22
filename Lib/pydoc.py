47b1e4ee6e716bc4c297f81425f97de9ca1d86ce
eamanu <eamanu@eamanu.com>
2019-04-22 17:17:33 -0300

    Add follow_wrapped to help builtin
    When print the docstring using help builtin for an wrapper function
    it doesn't correspond to the function wrapped. For avoid this
    this PR add the follow_wrapped to help().

--- Lib/pydoc.py
+++ Lib/pydoc.py
@@ -1669,9 +1669,11 @@ def render_doc(thing, title='Python Library Documentation: %s', forceload=0,
     return title % desc + '\n\n' + renderer.document(object, name)
 
 def doc(thing, title='Python Library Documentation: %s', forceload=0,
-        output=None):
+        output=None, follow_wrapped=False):
     """Display text documentation, given an object or a path to an object."""
     try:
+        if follow_wrapped:
+            thing = inspect.unwrap(thing)
         if output is None:
             pager(render_doc(thing, title, forceload))
         else:
@@ -1892,9 +1894,9 @@ class Helper:
                                      self.__class__.__qualname__)
 
     _GoInteractive = object()
-    def __call__(self, request=_GoInteractive):
+    def __call__(self, request=_GoInteractive, follow_wrapped=False):
         if request is not self._GoInteractive:
-            self.help(request)
+            self.help(request, follow_wrapped=follow_wrapped)
         else:
             self.intro()
             self.interact()
@@ -1935,7 +1937,7 @@ has the same effect as typing a particular string at the help> prompt.
             self.output.flush()
             return self.input.readline()
 
-    def help(self, request):
+    def help(self, request, follow_wrapped=False):
         if type(request) is type(''):
             request = request.strip()
             if request == 'keywords': self.listkeywords()
@@ -1947,13 +1949,16 @@ has the same effect as typing a particular string at the help> prompt.
             elif request in self.symbols: self.showsymbol(request)
             elif request in ['True', 'False', 'None']:
                 # special case these keywords since they are objects too
-                doc(eval(request), 'Help on %s:')
+                doc(eval(request), 'Help on %s:', follow_wrapped=follow_wrapped)
             elif request in self.keywords: self.showtopic(request)
             elif request in self.topics: self.showtopic(request)
-            elif request: doc(request, 'Help on %s:', output=self._output)
-            else: doc(str, 'Help on %s:', output=self._output)
+            elif request: doc(request, 'Help on %s:', output=self._output, 
+                              follow_wrapped=follow_wrapped)
+            else: doc(str, 'Help on %s:', output=self._output,
+                      follow_wrapped=follow_wrapped)
         elif isinstance(request, Helper): self()
-        else: doc(request, 'Help on %s:', output=self._output)
+        else: doc(request, 'Help on %s:', output=self._output,
+                  follow_wrapped=follow_wrapped)
         self.output.write('\n')
 
     def intro(self):
