3ab55ac65d9249e524309cbbf7afbe41e2aa33b7
Zackery Spytz <zspytz@gmail.com>
2019-04-22 17:22:21 -0600

    bpo-9194: Fix the bounds checking in winreg.c's fixupMultiSZ() (GH-12687)
    (cherry picked from commit 56ed86490cb8221c874d432461d77702437f63e5)

--- PC/_winreg.c
+++ PC/_winreg.c
@@ -727,7 +727,7 @@ fixupMultiSZ(char **str, char *data, int len)
     Q = data + len;
     for (P = data, i = 0; P < Q && *P != '\0'; P++, i++) {
         str[i] = P;
-        for(; *P != '\0'; P++)
+        for (; P < Q && *P != '\0'; P++)
             ;
     }
 }
