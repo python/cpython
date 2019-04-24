cc0b3a87985af206791feec4e855b2ceff9c0fac
Victor Stinner <vstinner@redhat.com>
2019-04-25 01:46:31 +0200

    bpo-36465: Fix test_regrtest on Windows (GH-12945)
    Fix Py_DEBUG constant: check for sys.gettotalrefcount attribute
    rather than sys.getobjects.
    
    Update also SpecialBuilds.txt documentation.

--- Lib/test/test_regrtest.py
+++ Lib/test/test_regrtest.py
@@ -21,7 +21,7 @@ from test import support
 from test.libregrtest import utils
 
 
-Py_DEBUG = hasattr(sys, 'getobjects')
+Py_DEBUG = hasattr(sys, 'gettotalrefcount')
 ROOT_DIR = os.path.join(os.path.dirname(__file__), '..', '..')
 ROOT_DIR = os.path.abspath(os.path.normpath(ROOT_DIR))
 
