462fd2e184ece4801cabc90b981fc9c9625bc34b
Guilherme Salgado <gsalgado@gmail.com>
2019-04-26 23:07:45 +0100

    Address review comments

--- Lib/test/_test_multiprocessing.py
+++ Lib/test/_test_multiprocessing.py
@@ -2701,7 +2701,7 @@ class FooBar(object):
     def f(self):
         return 'f()'
     def g(self):
-        raise ValueError
+        raise ValueError  # exception raised here
     def _h(self):
         return '_h()'
 
@@ -2767,9 +2767,10 @@ class _TestMyManager(BaseTestCase):
         self.assertEqual(foo.f(), 'f()')
         self.assertRaises(ValueError, foo.g)
         with self.assertRaises(ValueError) as ctx:
-            foo._callmethod('g')
+            foo.g()
         cause = ctx.exception.__cause__
         self.assertIsInstance(cause, multiprocessing.pool.RemoteTraceback)
+        self.assertIn('# exception raised here', str(cause))
         self.assertEqual(foo._callmethod('f'), 'f()')
         self.assertRaises(RemoteError, foo._callmethod, '_h')
 
