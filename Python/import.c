17c272148c0a7d955f5828bc4397c3b4eb5d9644
Victor Stinner <vstinner@redhat.com>
2019-04-26 02:32:01 +0200

    bpo-36710: Add runtime parameter in gcmodule.c (GH-12958)
    Add 'state' or 'runtime' parameter to functions in gcmodule.c to
    avoid to rely directly on the global variable _PyRuntime.

--- Python/import.c
+++ Python/import.c
@@ -535,7 +535,7 @@ PyImport_Cleanup(void)
     _PyGC_CollectNoFail();
     /* Dump GC stats before it's too late, since it uses the warnings
        machinery. */
-    _PyGC_DumpShutdownStats();
+    _PyGC_DumpShutdownStats(&_PyRuntime);
 
     /* Now, if there are any modules left alive, clear their globals to
        minimize potential leaks.  All C extension modules actually end
