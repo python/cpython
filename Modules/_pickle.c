4450b7b998315b1328619a3fda27135dccd59c8e
Nick Coghlan <ncoghlan@gmail.com>
2019-04-27 03:13:37 +1000

    Fix reduce result length checking error message

--- Modules/_pickle.c
+++ Modules/_pickle.c
@@ -3675,7 +3675,7 @@ save_reduce(PicklerObject *self, PyObject *args, PyObject *obj)
     size = PyTuple_Size(args);
     if (size < 2 || size > 6) {
         PyErr_SetString(st->PicklingError, "tuple returned by "
-                        "__reduce__ must contain 2 through 5 elements");
+                        "__reduce__ must contain 2 through 6 elements");
         return -1;
     }
 
