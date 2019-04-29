55b206fbae14ce20b76d12105c3c785ac30868c4
xdegaye <xdegaye@gmail.com>
2019-04-29 14:53:30 +0200

    bpo-35952: Fix test.pythoninfo when the compiler is missing (GH-13007)

--- Lib/test/pythoninfo.py
+++ Lib/test/pythoninfo.py
@@ -571,10 +571,17 @@ def collect_cc(info_add):
     except ImportError:
         args = CC.split()
     args.append('--version')
-    proc = subprocess.Popen(args,
-                            stdout=subprocess.PIPE,
-                            stderr=subprocess.STDOUT,
-                            universal_newlines=True)
+    try:
+        proc = subprocess.Popen(args,
+                                stdout=subprocess.PIPE,
+                                stderr=subprocess.STDOUT,
+                                universal_newlines=True)
+    except OSError:
+        # Cannot run the compiler, for example when Python has been
+        # cross-compiled and installed on the target platform where the
+        # compiler is missing.
+        return
+
     stdout = proc.communicate()[0]
     if proc.returncode:
         # CC --version failed: ignore error
