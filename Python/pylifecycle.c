db1ded4f7e0dc2b878d98bda104f808c7ba73727
Victor Stinner <vstinner@redhat.com>
2019-05-02 11:28:57 -0400

    bpo-36775: Add _Py_FORCE_UTF8_FS_ENCODING macro (GH-13056)
    Add _Py_FORCE_UTF8_LOCALE and _Py_FORCE_UTF8_FS_ENCODING macros to
    avoid factorize "#if defined(__ANDROID__) || defined(__VXWORKS__)"
    and "#if defined(__APPLE__)".
    
    Cleanup also config_init_fs_encoding().

--- Python/pylifecycle.c
+++ Python/pylifecycle.c
@@ -366,8 +366,7 @@ _Py_CoerceLegacyLocale(int warn)
             const char *new_locale = setlocale(LC_CTYPE,
                                                target->locale_name);
             if (new_locale != NULL) {
-#if !defined(__APPLE__) && !defined(__ANDROID__) && \
-defined(HAVE_LANGINFO_H) && defined(CODESET)
+#if !defined(_Py_FORCE_UTF8_LOCALE) && defined(HAVE_LANGINFO_H) && defined(CODESET)
                 /* Also ensure that nl_langinfo works in this locale */
                 char *codeset = nl_langinfo(CODESET);
                 if (!codeset || *codeset == '\0') {
