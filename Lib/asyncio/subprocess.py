d2af3e141c05a1dab5e2560d26ac5d57208ed237
Andrew Svetlov <andrew.svetlov@gmail.com>
2019-05-07 16:53:19 -0400

    bpo-36801: Fix waiting in StreamWriter.drain for closing SSL transport (GH-13098)
    https://bugs.python.org/issue36801

--- Lib/asyncio/subprocess.py
+++ Lib/asyncio/subprocess.py
@@ -26,6 +26,7 @@ class SubprocessStreamProtocol(streams.FlowControlMixin,
         self._transport = None
         self._process_exited = False
         self._pipe_fds = []
+        self._stdin_closed = self._loop.create_future()
 
     def __repr__(self):
         info = [self.__class__.__name__]
@@ -80,6 +81,10 @@ class SubprocessStreamProtocol(streams.FlowControlMixin,
             if pipe is not None:
                 pipe.close()
             self.connection_lost(exc)
+            if exc is None:
+                self._stdin_closed.set_result(None)
+            else:
+                self._stdin_closed.set_exception(exc)
             return
         if fd == 1:
             reader = self.stdout
@@ -105,6 +110,10 @@ class SubprocessStreamProtocol(streams.FlowControlMixin,
         if len(self._pipe_fds) == 0 and self._process_exited:
             self._transport.close()
             self._transport = None
+
+    def _get_close_waiter(self, stream):
+        if stream is self.stdin:
+            return self._stdin_closed
 
 
 class Process:
