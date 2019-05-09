75ceee0236ddee2a6cd781ea037fd68405642493
Anthony Shaw <anthonyshaw@apache.org>
2019-05-09 14:40:43 +1000

    Add VS2019 to env.bat

--- PCbuild/env.bat
+++ PCbuild/env.bat
@@ -10,6 +10,7 @@ rem 'v110', 'v120' or 'v140') to the build script.
 echo Build environments: x86, amd64, x86_amd64
 echo.
 set VSTOOLS=%VS140COMNTOOLS%
+if "%VSTOOLS%"=="" set VSTOOLS=%VS160COMNTOOLS%
 if "%VSTOOLS%"=="" set VSTOOLS=%VS120COMNTOOLS%
 if "%VSTOOLS%"=="" set VSTOOLS=%VS110COMNTOOLS%
 if "%VSTOOLS%"=="" set VSTOOLS=%VS100COMNTOOLS%
