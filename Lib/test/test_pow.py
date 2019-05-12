11dec1eaf8f91560b9d4efe6bdf8aaafc64b0f99
Mark Dickinson <dickinsm@gmail.com>
2019-05-12 19:04:41 +0100

    Remove a redundant test.

--- Lib/test/test_pow.py
+++ Lib/test/test_pow.py
@@ -145,11 +145,6 @@ class PowTest(unittest.TestCase):
                         with self.assertRaises(ValueError):
                             pow(a, -1001, m)
 
-        # Modulus zero should still raise, even in the case below where
-        # the base and the modulus are still relatively prime.
-        with self.assertRaises(ValueError):
-            pow(1, -1, 0)
-
 
 if __name__ == "__main__":
     unittest.main()
