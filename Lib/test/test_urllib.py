064ec257b3c2475a15c921d99dbddcc1f68d2689
Karthikeyan Singaravelan <tir.karthi@gmail.com>
2019-05-14 21:42:05 +0530

    Revert "Don't close the object which is again closed by destructor"
    This reverts commit 510350e2c20ef350ab30f4f73829f9ca26bf77d6.

--- Lib/test/test_urllib.py
+++ Lib/test/test_urllib.py
@@ -79,6 +79,8 @@ def fakehttp(fakedata):
 
         def close(self):
             self.io_refs -= 1
+            if self.io_refs == 0:
+                io.BytesIO.close(self)
 
     class FakeHTTPConnection(http.client.HTTPConnection):
 
