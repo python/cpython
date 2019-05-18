94372304cc1ef65bac283efbfefd68e87436fefb
Anthony Sottile <asottile@umich.edu>
2019-05-17 18:34:47 -0700

    Treat line continuation at EOF as a `SyntaxError`
    This makes the parser consistent with the tokenize module (already the case
    in `pypy`).
    
    sample
    ------
    
    ```python
    x = 5\
    ```
    
    before
    ------
    
    ```console
    $ python3 t.py
    $ python3 -mtokenize t.py
    t.py:2:0: error: EOF in multi-line statement
    ```
    
    after
    -----
    
    ```console
    $ ./python t.py
      File "t.py", line 3
        x = 5\
    
             ^
    SyntaxError: unexpected EOF while parsing
    $ ./python -m tokenize t.py
    t.py:2:0: error: EOF in multi-line statement
    ```

--- Lib/test/test_eof.py
+++ Lib/test/test_eof.py
@@ -24,5 +24,11 @@ class EOFTestCase(unittest.TestCase):
         else:
             raise support.TestFailed
 
+    def test_line_continuation_EOF(self):
+        expect = 'unexpected EOF while parsing (<string>, line 1)'
+        with self.assertRaises(SyntaxError) as excinfo:
+            exec('x = 5\\')
+        self.assertEqual(str(excinfo.exception), expect)
+
 if __name__ == "__main__":
     unittest.main()
