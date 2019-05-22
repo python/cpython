466b225f37c031d96e11610324c6fe0519786c01
Victor Stinner <victor.stinner@gmail.com>
2019-05-22 22:15:01 +0200

    bpo-35907, CVE-2019-9948: urllib rejects local_file:// scheme (GH-13474)
    CVE-2019-9948: Avoid file reading as disallowing the unnecessary URL
    scheme in URLopener().open() and URLopener().retrieve()
    of urllib.request.
    
    Co-Authored-By: SH <push0ebp@gmail.com>

--- Lib/test/test_urllib.py
+++ Lib/test/test_urllib.py
@@ -1481,6 +1481,19 @@ class URLopener_Tests(FakeHTTPMixin, unittest.TestCase):
         filename, _ = urllib.request.URLopener().retrieve(url)
         self.assertEqual(os.path.splitext(filename)[1], ".txt")
 
+    @support.ignore_warnings(category=DeprecationWarning)
+    def test_local_file_open(self):
+        # bpo-35907, CVE-2019-9948: urllib must reject local_file:// scheme
+        class DummyURLopener(urllib.request.URLopener):
+            def open_local_file(self, url):
+                return url
+        for url in ('local_file://example', 'local-file://example'):
+            self.assertRaises(OSError, urllib.request.urlopen, url)
+            self.assertRaises(OSError, urllib.request.URLopener().open, url)
+            self.assertRaises(OSError, urllib.request.URLopener().retrieve, url)
+            self.assertRaises(OSError, DummyURLopener().open, url)
+            self.assertRaises(OSError, DummyURLopener().retrieve, url)
+
 
 # Just commented them out.
 # Can't really tell why keep failing in windows and sparc.
--- Lib/urllib/request.py
+++ Lib/urllib/request.py
@@ -1745,7 +1745,7 @@ class URLopener:
         name = 'open_' + urltype
         self.type = urltype
         name = name.replace('-', '_')
-        if not hasattr(self, name):
+        if not hasattr(self, name) or name == 'open_local_file':
             if proxy:
                 return self.open_unknown_proxy(proxy, fullurl, data)
             else:
