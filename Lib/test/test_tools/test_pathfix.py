98fac5299a769073ce3e8d902ad09b865a45b0ae
Patrik Kopkan <pkopkan@redhat.com>
2019-05-24 17:03:15 +0200

    reworked tests to unittests

--- Lib/test/test_tools/test_pathfix.py
+++ Lib/test/test_tools/test_pathfix.py
@@ -1,48 +1,56 @@
-import pytest, sys
-from pathfix import fixline, parse_shebang
-
-test_cases = [
-    (b'#!/usr/bin/python'       , b'#! /usr/bin/python -f\n'),
-    (b'#!python -f'             , b'#! /usr/bin/python -f\n'),
-    (b'#! /usr/bin/python -s'    , b'#! /usr/bin/python -fs\n'),
-    (b'#!/usr/bin/python -f sfj', b'#! /usr/bin/python -f sfj\n'),
-    (b'#!/usr/python -s sfj'    , b'#! /usr/bin/python -fs sfj\n'),
-]
-
-test_cases_for_parsing = [
-    (b'#!/usr/bin/python'             , (b'',b'')),
-    (b'#! /usr/bin/python -f'          , (b'f',b'')),
-    (b'#!/usr/bin/python -f sfj'      , (b'f',b'sfj')),
-    (b'#!/usr/bin/python -f sfj af bg', (b'f', b'sfj af bg')),
-]
-
-test_cases_without_f_option = [
-    b'#!/usr/bin/python',
-    b'#!python -f',
-    b'#! /usr/bin/python -s',
-    b'#!/usr/bin/python -f sfj',
-    b'#!/usr/python -s sfj',
-]
+import unittest
+from test.test_tools import skip_if_missing, import_tool
 
 
-@pytest.mark.parametrize("line,output", test_cases_for_parsing)
-def test_parse_shebangs(line, output):
-    assert parse_shebang(line) == output
+class TestPathFix(unittest.TestCase):
+
+    def setUp(self):
+        self.pathfix = import_tool('pathfix')
+    test_cases = [
+        (b'#!/usr/bin/python'       , b'#! /usr/bin/python -f\n'),
+        (b'#!python -f'             , b'#! /usr/bin/python -f\n'),
+        (b'#! /usr/bin/python -s'    , b'#! /usr/bin/python -fs\n'),
+        (b'#!/usr/bin/python -f sfj', b'#! /usr/bin/python -f sfj\n'),
+        (b'#!/usr/python -s sfj'    , b'#! /usr/bin/python -fs sfj\n'),
+    ]
+
+    test_cases_for_parsing = [
+        (b'#!/usr/bin/python'             , (b'',b'')),
+        (b'#! /usr/bin/python -f'          , (b'f',b'')),
+        (b'#!/usr/bin/python -f sfj'      , (b'f',b'sfj')),
+        (b'#!/usr/bin/python -f sfj af bg', (b'f', b'sfj af bg')),
+    ]
+
+    test_cases_without_f_option = [
+        b'#!/usr/bin/python',
+        b'#!python -f',
+        b'#! /usr/bin/python -s',
+        b'#!/usr/bin/python -f sfj',
+        b'#!/usr/python -s sfj',
+    ]
+
+    def test_parse_shebangs(self):
+        for line, output in self.test_cases_for_parsing:
+            self.assertEqual(self.pathfix.parse_shebang(line), output)
 
 
-@pytest.mark.parametrize("line,output", test_cases)
-def test_fixline(line, output):
-    this = sys.modules['pathfix']
-    this.add_flag = b'f'
-    this.new_interpreter = b'/usr/bin/python'
-    testing_output = fixline(line)
-    assert testing_output == output
+    def test_fixline(self):
+        for line, output in self.test_cases:
+            this = self.pathfix
+            this.add_flag = b'f'
+            this.new_interpreter = b'/usr/bin/python'
+            testing_output = this.fixline(line)
+            self.assertEqual(testing_output, output)
 
 
-@pytest.mark.parametrize("line", test_cases_without_f_option)
-def test_fixline_without_f_option(line):
-    this = sys.modules['pathfix']
-    this.new_interpreter = b'/usr/bin/python'
-    this.add_flag = None
-    testing_output = fixline(line)
-    assert testing_output == b'#! ' + this.new_interpreter + b'\n'
+    def test_fixline_without_f_option(self):
+        for line in self.test_cases_without_f_option:
+            this = self.pathfix
+            this.new_interpreter = b'/usr/bin/python'
+            this.add_flag = None
+            testing_output = this.fixline(line)
+            self.assertEqual(testing_output, b'#! ' + this.new_interpreter + b'\n')
+
+
+if __name__ == '__main__':
+    unittest.main()
