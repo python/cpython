a5374370ac3a9e59188d86d7a9e2a888d13a0d26
Terry Jan Reedy <tjreedy@udel.edu>
2019-05-25 16:59:18 -0400

    On Windows, zoom less to avoid underlapping Win10 taskbar.

--- Lib/idlelib/zoomheight.py
+++ Lib/idlelib/zoomheight.py
@@ -28,26 +28,20 @@ def zoom_height(top):
         return
     width, height, x, y = map(int, m.groups())
     newheight = top.winfo_screenheight()
+
+    # The constants below for Windows and Mac Aqua are visually determined
+    # to avoid taskbar or menubar and app icons.
     if sys.platform == 'win32':
         newy = 0
-        newheight = newheight - 72
-
+        newheight = newheight - 114
     elif macosx.isAquaTk():
-        # The '88' below is a magic number that avoids placing the bottom
-        # of the window below the panel on my machine. I don't know how
-        # to calculate the correct value for this with tkinter.
         newy = 22
         newheight = newheight - newy - 88
-
     else:
-        #newy = 24
         newy = 0
-        #newheight = newheight - 96
         newheight = newheight - 88
-    if height >= newheight:
-        newgeom = ""
-    else:
-        newgeom = "%dx%d+%d+%d" % (width, newheight, x, newy)
+
+    newgeom = ('' if height >= newheight else f"{width}x{newheight}+{x}+{newy}")
     top.wm_geometry(newgeom)
     return newgeom != ""
 
