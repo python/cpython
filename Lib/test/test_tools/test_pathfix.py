fa6d07df093f3ad1f78ff5e482509c9b2b83d82d
Patrik Kopkan <pkopkan@redhat.com>
2019-05-27 14:20:05 +0200

    fixing typos

--- Lib/test/test_tools/test_pathfix.py
+++ Lib/test/test_tools/test_pathfix.py
@@ -1,5 +1,5 @@
 import unittest
-from test.test_tools import skip_if_missing, import_tool
+from test.test_tools import import_tool
 
 
 class TestPathFix(unittest.TestCase):
@@ -9,14 +9,14 @@ class TestPathFix(unittest.TestCase):
     test_cases = [
         (b'#!/usr/bin/python'       , b'#! /usr/bin/python -f\n'),
         (b'#!python -f'             , b'#! /usr/bin/python -f\n'),
-        (b'#! /usr/bin/python -s'    , b'#! /usr/bin/python -fs\n'),
+        (b'#! /usr/bin/python -s'   , b'#! /usr/bin/python -fs\n'),
         (b'#!/usr/bin/python -f sfj', b'#! /usr/bin/python -f sfj\n'),
         (b'#!/usr/python -s sfj'    , b'#! /usr/bin/python -fs sfj\n'),
     ]
 
     test_cases_for_parsing = [
         (b'#!/usr/bin/python'             , (b'',b'')),
-        (b'#! /usr/bin/python -f'          , (b'f',b'')),
+        (b'#! /usr/bin/python -f'         , (b'f',b'')),
         (b'#!/usr/bin/python -f sfj'      , (b'f',b'sfj')),
         (b'#!/usr/bin/python -f sfj af bg', (b'f', b'sfj af bg')),
     ]
@@ -33,7 +33,6 @@ class TestPathFix(unittest.TestCase):
         for line, output in self.test_cases_for_parsing:
             self.assertEqual(self.pathfix.parse_shebang(line), output)
 
-
     def test_fixline(self):
         for line, output in self.test_cases:
             this = self.pathfix
@@ -41,7 +40,6 @@ class TestPathFix(unittest.TestCase):
             this.new_interpreter = b'/usr/bin/python'
             testing_output = this.fixline(line)
             self.assertEqual(testing_output, output)
-
 
     def test_fixline_without_f_option(self):
         for line in self.test_cases_without_f_option:
