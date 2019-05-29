4f06dae5d8d4400ba38d8502da620f07d4a5696e
Victor Stinner <vstinner@redhat.com>
2019-05-29 04:30:48 +0200

    bpo-35907, CVE-2019-9948: urllib rejects local_file:// scheme (GH-13513)
    CVE-2019-9948: Avoid file reading by disallowing local-file:// and
    local_file:// URL schemes in URLopener().open() and
    URLopener().retrieve() of urllib.request.
    
    Co-Authored-By: SH <push0ebp@gmail.com>
    (cherry picked from commit 0c2b6a3943aa7b022e8eb4bfd9bffcddebf9a587)
    (cherry picked from commit 34bab215596671d0dec2066ae7d7450cd73f638b)

--- Lib/urllib/request.py
+++ Lib/urllib/request.py
@@ -1747,7 +1747,7 @@ class URLopener:
         name = 'open_' + urltype
         self.type = urltype
         name = name.replace('-', '_')
-        if not hasattr(self, name):
+        if not hasattr(self, name) or name == 'open_local_file':
             if proxy:
                 return self.open_unknown_proxy(proxy, fullurl, data)
             else:
