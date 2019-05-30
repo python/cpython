80b00486be1729ce1bf657bba2de02076706615a
Berker Peksag <berker.peksag@gmail.com>
2019-05-31 01:48:46 +0300

    bpo-35950: Raise UnsupportedOperation in BufferedReader.truncate()
    The truncate() method of io.BufferedReader() should raise
    UnsupportedOperation when it is called on a read-only
    io.BufferedReader() instance.

--- Modules/_io/bufferedio.c
+++ Modules/_io/bufferedio.c
@@ -1314,15 +1314,20 @@ _io__Buffered_truncate_impl(buffered *self, PyObject *pos)
     PyObject *res = NULL;
 
     CHECK_INITIALIZED(self)
+    CHECK_CLOSED(self, "truncate of closed file")
     if (!ENTER_BUFFERED(self))
         return NULL;
-
-    if (self->writable) {
-        res = buffered_flush_and_rewind_unlocked(self);
-        if (res == NULL)
-            goto end;
-        Py_CLEAR(res);
+    if (!self->writable) {
+        LEAVE_BUFFERED(self)
+        return bufferediobase_unsupported("truncate");
     }
+
+    res = buffered_flush_and_rewind_unlocked(self);
+    if (res == NULL) {
+        goto end;
+    }
+    Py_CLEAR(res);
+
     res = PyObject_CallMethodObjArgs(self->raw, _PyIO_str_truncate, pos, NULL);
     if (res == NULL)
         goto end;
