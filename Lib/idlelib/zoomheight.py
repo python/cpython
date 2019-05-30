dd16ea5ae297627d36896fb9cd4ea7c041200f1e
Tal Einat <taleinat@gmail.com>
2019-05-30 12:21:07 +0300

    bpo-37039: Make IDLE's Zoom Height behave better across platforms

--- Lib/idlelib/zoomheight.py
+++ Lib/idlelib/zoomheight.py
@@ -1,9 +1,6 @@
 "Zoom a window to maximum height."
 
 import re
-import sys
-
-from idlelib import macosx
 
 
 class ZoomHeight:
@@ -11,33 +8,51 @@ class ZoomHeight:
     def __init__(self, editwin):
         self.editwin = editwin
 
+        # cached values for maximized window dimensions
+        self._max_height_and_y_coord = None
+
+    @property
+    def top(self):
+        return self.editwin.top
+
     def zoom_height_event(self, event=None):
-        top = self.editwin.top
-        zoomed = zoom_height(top)
+        zoomed = self.zoom_height()
         menu_status = 'Restore' if zoomed else 'Zoom'
         self.editwin.update_menu_label(menu='options', index='* Height',
                                        label=f'{menu_status} Height')
         return "break"
 
+    def zoom_height(self):
+        width, height, x, y = get_window_geometry(self.top)
 
-def zoom_height(top):
+        maxheight, maxy = self.max_height_and_y_coord
+
+        # .wm_geometry('') makes the window revert to the size requested
+        # by the widgets it contains.
+        newgeom = \
+            '' if height >= maxheight else f"{width}x{maxheight}+{x}+{maxy}"
+        self.top.wm_geometry(newgeom)
+        return newgeom != ''
+
+    @property
+    def max_height_and_y_coord(self):
+        if self._max_height_and_y_coord is None:
+            # Maximize the window to get the appropriate height and Y
+            # coordinate.
+            self.top.wm_state('zoomed')
+            maxwidth, maxheight, maxx, maxy = get_window_geometry(self.top)
+            self._max_height_and_y_coord = maxheight, maxy
+        return self._max_height_and_y_coord
+
+
+def get_window_geometry(top):
     geom = top.wm_geometry()
     m = re.match(r"(\d+)x(\d+)\+(-?\d+)\+(-?\d+)", geom)
     if not m:
         top.bell()
         return
     width, height, x, y = map(int, m.groups())
-    newheight = top.winfo_screenheight()
-
-    # The constants below for Windows and Mac Aqua are visually determined
-    # to avoid taskbar or menubar and app icons.
-    newy, bot_y = ((0, 72) if sys.platform == 'win32' else
-                   (22, 88) if macosx.isAquaTk() else
-                   (0, 88) ) # Guess for anything else.
-    newheight = newheight - newy - bot_y
-    newgeom = '' if height >= newheight else f"{width}x{newheight}+{x}+{newy}"
-    top.wm_geometry(newgeom)
-    return newgeom != ""
+    return width, height, x, y
 
 
 if __name__ == "__main__":
