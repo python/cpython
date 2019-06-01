5726df183c58b9ce5f7029f985b5c76d2de63765
Berker Peksag <berker.peksag@gmail.com>
2019-06-01 07:41:09 +0300

    bpo-14014: Clarify StreamWriter.reset() documentation

--- Lib/codecs.py
+++ Lib/codecs.py
@@ -386,7 +386,7 @@ class StreamWriter(Codec):
 
     def reset(self):
 
-        """ Flushes and resets the codec buffers used for keeping state.
+        """ Resets the codec buffers used for keeping internal state.
 
             Calling this method should ensure that the data on the
             output is put into a clean state, that allows appending
@@ -620,7 +620,7 @@ class StreamReader(Codec):
 
     def reset(self):
 
-        """ Resets the codec buffers used for keeping state.
+        """ Resets the codec buffers used for keeping internal state.
 
             Note that no stream repositioning should take place.
             This method is primarily intended to be able to recover
