b02410c913290abf3fa3755b8f92c57560e0fe42
Steve Dower <steve.dower@python.org>
2019-06-04 09:02:24 -0700

    bpo-36742: Corrects fix to handle decomposition in usernames

--- Lib/test/test_urlparse.py
+++ Lib/test/test_urlparse.py
@@ -647,13 +647,14 @@ class UrlParseTestCase(unittest.TestCase):
         with self.assertRaises(ValueError):
             urlparse.urlsplit(u'http://\u30d5\u309a\ufe1380')
 
-        for scheme in [u"http", u"https", u"ftp"]:
-            for c in denorm_chars:
-                url = u"{}://netloc{}false.netloc/path".format(scheme, c)
-                if test_support.verbose:
-                    print "Checking %r" % url
-                with self.assertRaises(ValueError):
-                    urlparse.urlsplit(url)
+        for scheme in ["http", "https", "ftp"]:
+            for netloc in ["netloc{}false.netloc", "n{}user@netloc"]:
+                for c in denorm_chars:
+                    url = "{}://{}/path".format(scheme, netloc.format(c))
+                    if test_support.verbose:
+                        print "Checking %r" % url
+                    with self.assertRaises(ValueError):
+                        urlparse.urlsplit(url)
 
 def test_main():
     test_support.run_unittest(UrlParseTestCase)
