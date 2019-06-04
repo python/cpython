699080a28f7020bb81c6ef794d2270c2528cb523
Łukasz Langa <lukasz@langa.pl>
2019-06-04 19:44:34 +0200

    Python 3.8.0b1

--- Doc/tools/extensions/pyspecific.py
+++ Doc/tools/extensions/pyspecific.py
@@ -36,7 +36,7 @@ import suspicious
 
 
 ISSUE_URI = 'https://bugs.python.org/issue%s'
-SOURCE_URI = 'https://github.com/python/cpython/tree/master/%s'
+SOURCE_URI = 'https://github.com/python/cpython/tree/3.8/%s'
 
 # monkey-patch reST parser to disable alphabetic and roman enumerated lists
 from docutils.parsers.rst.states import Body
