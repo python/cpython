28be388e69c6e88d5c04abcc714db5ec526e4f5e
Miss Islington (bot) <31488909+miss-islington@users.noreply.github.com>
2019-06-06 03:39:30 -0700

    Don't report deleted attributes in __dir__ (GHGH-10148)
    When an attribute is deleted from a Mock, a sentinel is added rather
    than just deleting the attribute. This commit checks for such sentinels
    when returning the child mocks in the __dir__ method as users won't
    expect deleted attributes to appear when performing dir(mock).
    (cherry picked from commit 0df635c7f8aa69e56a092bd4f142f0f164741ab2)
    
    Co-authored-by: Mario Corchero <mariocj89@gmail.com>

--- Lib/unittest/mock.py
+++ Lib/unittest/mock.py
@@ -684,12 +684,14 @@ class NonCallableMock(Base):
         extras = self._mock_methods or []
         from_type = dir(type(self))
         from_dict = list(self.__dict__)
+        from_child_mocks = [
+            m_name for m_name, m_value in self._mock_children.items()
+            if m_value is not _deleted]
 
         from_type = [e for e in from_type if not e.startswith('_')]
         from_dict = [e for e in from_dict if not e.startswith('_') or
                      _is_magic(e)]
-        return sorted(set(extras + from_type + from_dict +
-                          list(self._mock_children)))
+        return sorted(set(extras + from_type + from_dict + from_child_mocks))
 
 
     def __setattr__(self, name, value):
