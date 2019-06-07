685b806549cc956aeeb3a57fe15ee5a4d1704aed
Miss Islington (bot) <31488909+miss-islington@users.noreply.github.com>
2019-06-06 23:37:32 -0700

    bpo-37177: make IDLE's search dialogs transient (GH-13869)
    This avoids the search dialogs being hidden behind the editor window.
    (cherry picked from commit 554450fb4e95066e825bdb4a2d544a490daeebdc)
    
    Co-authored-by: Tal Einat <taleinat@gmail.com>

--- Lib/idlelib/idle_test/test_searchbase.py
+++ Lib/idlelib/idle_test/test_searchbase.py
@@ -4,7 +4,7 @@
 
 import unittest
 from test.support import requires
-from tkinter import Tk
+from tkinter import Text, Tk, Toplevel
 from tkinter.ttk import Frame
 from idlelib import searchengine as se
 from idlelib import searchbase as sdb
@@ -47,14 +47,15 @@ class SearchDialogBaseTest(unittest.TestCase):
         # open calls create_widgets, which needs default_command
         self.dialog.default_command = None
 
-        # Since text parameter of .open is not used in base class,
-        # pass dummy 'text' instead of tk.Text().
-        self.dialog.open('text')
+        toplevel = Toplevel(self.root)
+        self.addCleanup(toplevel.destroy)
+        text = Text(toplevel)
+        self.dialog.open(text)
         self.assertEqual(self.dialog.top.state(), 'normal')
         self.dialog.close()
         self.assertEqual(self.dialog.top.state(), 'withdrawn')
 
-        self.dialog.open('text', searchphrase="hello")
+        self.dialog.open(text, searchphrase="hello")
         self.assertEqual(self.dialog.ent.get(), 'hello')
         self.dialog.close()
 
