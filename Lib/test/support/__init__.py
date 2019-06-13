158c529efec8acefa9c721662f556b1e178dbaa3
Victor Stinner <vstinner@redhat.com>
2019-06-13 13:56:11 +0200

    Fix typos and update the doc

--- Lib/test/support/__init__.py
+++ Lib/test/support/__init__.py
@@ -3040,18 +3040,17 @@ class catch_unraisable_exception:
     """
     Context manager catching unraisable exception using sys.unraisablehook.
 
-    The the *object* attribute of the unraisable hook is set and the object is
-    being finalized, the object is resurected because the context manager
+    If the *object* attribute of the unraisable hook is set and the object is
+    being finalized, the object is resurrected because the context manager
     stores a strong reference to it (cm.unraisable.object).
 
     Storing the exception value (cm.unraisable.exc_value) creates a reference
     cycle. The reference cycle is broken explicitly when the context manager
-    exists.
+    exits.
 
     Exiting the context manager clears the stored unraisable exception. It can
-    trigger a new unraisable exception (ex: resurected object is being
-    finalized again and raises the same exception): it is silently ignored in
-    this case.
+    trigger a new unraisable exception (ex: the resurrected object is finalized
+    again and raises the same exception): it is silently ignored in this case.
 
     Usage:
 
