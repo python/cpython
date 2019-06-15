455cbc70067e8d91c31022ec3ff2b0be1e9974c8
Pablo Galindo <pablogsal@gmail.com>
2019-06-15 14:36:51 +0100

    fixup! Remove 'if False' handling in the peephole optimizer

--- Python/peephole.c
+++ Python/peephole.c
@@ -311,6 +311,9 @@ PyCode_Optimize(PyObject *code, PyObject* consts, PyObject *names,
                 }
                 PyObject* cnt = PyList_GET_ITEM(consts, get_arg(codestr, i));
                 int is_true = PyObject_IsTrue(cnt);
+                if (is_true == -1) {
+                    goto exitError;
+                }
                 if (is_true == 1) {
                     fill_nops(codestr, op_start, nexti + 1);
                     cumlc = 0;
