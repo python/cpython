e5d67f1e31381d28b24f6e1c0f8388d9bf0bfc5f
Andrew Svetlov <andrew.svetlov@gmail.com>
2019-06-15 14:56:27 +0300

    [3.7] bpo-37279: Fix asyncio sendfile support when  extra data are sent in fallback mode. (GH-14075). (GH-14103)
    (cherry picked from commit ef2152354f03a165c5e3adb53e2276934fabd50a)
    
    Co-authored-by: Andrew Svetlov <andrew.svetlov@gmail.com>

--- Lib/asyncio/base_events.py
+++ Lib/asyncio/base_events.py
@@ -827,7 +827,7 @@ class BaseEventLoop(events.AbstractEventLoop):
                 read = await self.run_in_executor(None, file.readinto, view)
                 if not read:
                     break  # EOF
-                await self.sock_sendall(sock, view)
+                await self.sock_sendall(sock, view[:read])
                 total_sent += read
             return total_sent
         finally:
@@ -1082,7 +1082,7 @@ class BaseEventLoop(events.AbstractEventLoop):
                 if not read:
                     return total_sent  # EOF
                 await proto.drain()
-                transp.write(view)
+                transp.write(view[:read])
                 total_sent += read
         finally:
             if total_sent > 0 and hasattr(file, 'seek'):
