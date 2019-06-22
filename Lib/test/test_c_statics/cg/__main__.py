cfdf57fd4c9700968137dcf4f65a98a3253e62c6
Eric Snow <ericsnowcurrently@gmail.com>
2019-06-21 18:54:55 -0600

    Stub out the "check" and "show" commands.

--- Lib/test/test_c_statics/cg/__main__.py
+++ Lib/test/test_c_statics/cg/__main__.py
@@ -1,26 +1,78 @@
 import argparse
 import sys
 
+from . import KNOWN_FILE, IGNORED_FILE, SOURCE_DIRS
+
+
+def check(cmd, dirs=SOURCE_DIRS, *, ignored=IGNORED_FILE, known=KNOWN_FILE):
+    """
+    Fail if there are unsupported statics variables.
+
+    In the failure case, the list of unsupported variables
+    will be printed out.
+    """
+
+
+def show(cmd, dirs=SOURCE_DIRS, *, ignored=IGNORED_FILE, known=KNOWN_FILE):
+    """
+    print out the list of found static variables.
+
+    The variables will be distinguished as "supported" or "unsupported".
+    """
+
 
 #############################
 # the script
 
-def parse_args(prog=sys.argv[0], argv=sys.argv[1:]):
+COMMANDS = {
+        'check': check,
+        'show': show,
+        }
+
+PROG = sys.argv[0]
+PROG = 'c-statics.py'
+
+
+def parse_args(prog=PROG, argv=sys.argv[1:]):
+    common = argparse.ArgumentParser(add_help=False)
+    common.add_argument('--ignored', metavar='FILE',
+                        default=IGNORED_FILE,
+                        help='path to file that lists ignored vars')
+    common.add_argument('--known', metavar='FILE',
+                        default=KNOWN_FILE,
+                        help='path to file that lists known types')
+    common.add_argument('dirs', metavar='DIR', nargs='*',
+                        default=SOURCE_DIRS,
+                        help='a directory to check')
+
     parser = argparse.ArgumentParser(
             prog=prog,
             )
-    parser.set_defaults(cmd=None)
+    subs = parser.add_subparsers(dest='cmd')
 
+    check = subs.add_parser('check', parents=[common])
+
+    check = subs.add_parser('show', parents=[common])
+
+    # Now parse the args.
     args = parser.parse_args(argv)
     ns = vars(args)
 
     cmd = ns.pop('cmd')
+    if not cmd:
+        parser.error('missing command')
 
     return cmd, ns
 
 
-def main(cmd, cmdkwargs):
-    return
+def main(cmd, cmdkwargs=None, *, _COMMANDS=COMMANDS):
+    try:
+        cmdfunc = _COMMANDS[cmd]
+    except KeyError:
+        raise ValueError(
+            f'unsupported cmd {cmd!r}' if cmd else 'missing cmd')
+
+    cmdfunc(cmd, **cmdkwargs or {})
 
 
 if __name__ == '__main__':
--- Lib/test/test_c_statics/test_cg.py
+++ Lib/test/test_c_statics/test_cg.py
@@ -1,18 +1,112 @@
 import unittest
 
+from . import cg
 from .cg.__main__ import parse_args, main
 
 
 class ParseArgsTests(unittest.TestCase):
 
     def test_no_args(self):
-        cmd, cmdkwargs = parse_args('cg', [])
+        with self.assertRaises(SystemExit):
+            parse_args('cg', [])
 
-        self.assertIs(cmd, None)
-        self.assertEqual(cmdkwargs, {})
+    def test_check_no_args(self):
+        cmd, cmdkwargs = parse_args('cg', [
+            'check',
+            ])
+
+        self.assertEqual(cmd, 'check')
+        self.assertEqual(cmdkwargs, {
+            'ignored': cg.IGNORED_FILE,
+            'known': cg.KNOWN_FILE,
+            'dirs': cg.SOURCE_DIRS,
+            })
+
+    def test_check_full_args(self):
+        cmd, cmdkwargs = parse_args('cg', [
+            'check',
+            '--ignored', 'spam.tsv',
+            '--known', 'eggs.tsv',
+            'dir1',
+            'dir2',
+            'dir3',
+            ])
+
+        self.assertEqual(cmd, 'check')
+        self.assertEqual(cmdkwargs, {
+            'ignored': 'spam.tsv',
+            'known': 'eggs.tsv',
+            'dirs': ['dir1', 'dir2', 'dir3']
+            })
+
+    def test_show_no_args(self):
+        cmd, cmdkwargs = parse_args('cg', [
+            'show',
+            ])
+
+        self.assertEqual(cmd, 'show')
+        self.assertEqual(cmdkwargs, {
+            'ignored': cg.IGNORED_FILE,
+            'known': cg.KNOWN_FILE,
+            'dirs': cg.SOURCE_DIRS,
+            })
+
+    def test_show_full_args(self):
+        cmd, cmdkwargs = parse_args('cg', [
+            'show',
+            '--ignored', 'spam.tsv',
+            '--known', 'eggs.tsv',
+            'dir1',
+            'dir2',
+            'dir3',
+            ])
+
+        self.assertEqual(cmd, 'show')
+        self.assertEqual(cmdkwargs, {
+            'ignored': 'spam.tsv',
+            'known': 'eggs.tsv',
+            'dirs': ['dir1', 'dir2', 'dir3']
+            })
+
+
+def new_stub_commands(*names):
+    calls = []
+    def cmdfunc(cmd, **kwargs):
+        calls.append((cmd, kwargs))
+    commands = {name: cmdfunc for name in names}
+    return commands, calls
 
 
 class MainTests(unittest.TestCase):
 
     def test_no_command(self):
-        main(None, {})
+        with self.assertRaises(ValueError):
+            main(None, {})
+
+    def test_check(self):
+        commands, calls = new_stub_commands('check', 'show')
+
+        cmdkwargs = {
+            'ignored': 'spam.tsv',
+            'known': 'eggs.tsv',
+            'dirs': ['dir1', 'dir2', 'dir3'],
+            }
+        main('check', cmdkwargs, _COMMANDS=commands)
+
+        self.assertEqual(calls, [
+            ('check', cmdkwargs),
+            ])
+
+    def test_show(self):
+        commands, calls = new_stub_commands('check', 'show')
+
+        cmdkwargs = {
+            'ignored': 'spam.tsv',
+            'known': 'eggs.tsv',
+            'dirs': ['dir1', 'dir2', 'dir3'],
+            }
+        main('show', cmdkwargs, _COMMANDS=commands)
+
+        self.assertEqual(calls, [
+            ('show', cmdkwargs),
+            ])
