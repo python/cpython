4dda3069bef1baa23586e56ce35b5f471a940fc5
Steve Dower <steve.dower@python.org>
2019-06-29 10:34:11 -0700

    bpo-37369: Fix initialization of sys members when launched via an app container (GH-14428)
    sys._base_executable is now always defined on all platforms, and can be overridden through configuration.
    Also adds test.support.PythonSymlink to encapsulate platform-specific logic for symlinking sys.executable

--- Include/cpython/initconfig.h
+++ Include/cpython/initconfig.h
@@ -373,10 +373,11 @@ typedef struct {
                                        module_search_paths_set is equal
                                        to zero. */
 
-    wchar_t *executable;    /* sys.executable */
-    wchar_t *prefix;        /* sys.prefix */
-    wchar_t *base_prefix;   /* sys.base_prefix */
-    wchar_t *exec_prefix;   /* sys.exec_prefix */
+    wchar_t *executable;        /* sys.executable */
+    wchar_t *base_executable;   /* sys._base_executable */
+    wchar_t *prefix;            /* sys.prefix */
+    wchar_t *base_prefix;       /* sys.base_prefix */
+    wchar_t *exec_prefix;       /* sys.exec_prefix */
     wchar_t *base_exec_prefix;  /* sys.base_exec_prefix */
 
     /* --- Parameter only used by Py_Main() ---------- */
