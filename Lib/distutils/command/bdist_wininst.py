11137fc66b113db0246954dd493851e325a2f4f4
Miro Hrončok <miro@hroncok.cz>
2019-07-01 13:09:20 +0200

    fixup! bpo-10945: Drop support for bdist_wininst on non-Windows systems

--- Lib/distutils/command/bdist_wininst.py
+++ Lib/distutils/command/bdist_wininst.py
@@ -56,7 +56,8 @@ class bdist_wininst(Command):
     boolean_options = ['keep-temp', 'no-target-compile', 'no-target-optimize',
                        'skip-build']
 
-    _unsupported = platform.system() != "Windows"
+    # bpo-10945: bdist_wininst requires mbcs encoding only available on Windows
+    _unsupported = (sys.platform != "win32")
 
     def initialize_options(self):
         self.bdist_dir = None
