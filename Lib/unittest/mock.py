67c9299d8ff95a8b132b5413aeb34bbbaf19d747
Xtreak <tir.karthi@gmail.com>
2019-07-03 12:55:01 +0530

    Reword comment regarding call object lookup logic

--- Lib/unittest/mock.py
+++ Lib/unittest/mock.py
@@ -739,9 +739,10 @@ class NonCallableMock(Base):
 
     def _get_call_signature_from_name(self, name):
         """
-        If there is no name like the case for call check against functions
-        with tuples then return the self's spec signature.
-        If the name is not empty then remove () and split by '.' to get
+        * If call objects are asserted against a method/function like obj.meth1
+        then there could be no name for the call object to lookup. Hence just
+        return the spec_signature of the method/function being asserted against.
+        * If the name is not empty then remove () and split by '.' to get
         list of names to iterate through the children until a potential
         match is found. A child mock is created only during attribute access
         so if we get a _SpecState then no attributes of the spec were accessed
