58322a52ee672f640a3d7b7abcfa868287782580
Pablo Galindo <pablogsal@gmail.com>
2019-07-06 16:32:55 +0100

    Reorder tests

--- Lib/test/test_syntax.py
+++ Lib/test/test_syntax.py
@@ -699,10 +699,10 @@ class SyntaxTestCase(unittest.TestCase):
     def test_yield_outside_function(self):
         self._check_error("if 0: yield",                "outside function")
         self._check_error("if 0: yield\nelse:  x=1",    "outside function")
-        self._check_error("class C:\n    if 0: yield",  "outside function")
         self._check_error("if 1: pass\nelse: yield",    "outside function")
         self._check_error("while 0: yield",             "outside function")
         self._check_error("while 0: yield\nelse:  x=1", "outside function")
+        self._check_error("class C:\n  if 0: yield",    "outside function")
         self._check_error("class C:\n  if 1: pass\n  else: yield",
                           "outside function")
         self._check_error("class C:\n  while 0: yield", "outside function")
@@ -712,14 +712,15 @@ class SyntaxTestCase(unittest.TestCase):
     def test_return_outside_function(self):
         self._check_error("if 0: return",                "outside function")
         self._check_error("if 0: return\nelse:  x=1",    "outside function")
-        self._check_error("class C:\n    if 0: return",  "outside function")
         self._check_error("if 1: pass\nelse: return",    "outside function")
         self._check_error("while 0: return",             "outside function")
         self._check_error("class C:\n  if 0: return",    "outside function")
-        self._check_error("class C:\n  if 0: return\n  else: x= 1",
-                          "outside function")
         self._check_error("class C:\n  while 0: return", "outside function")
         self._check_error("class C:\n  while 0: return\n  else:  x=1",
+                          "outside function")
+        self._check_error("class C:\n  if 0: return\n  else: x= 1",
+                          "outside function")
+        self._check_error("class C:\n  if 1: pass\n  else: return",
                           "outside function")
 
     def test_break_outside_loop(self):
