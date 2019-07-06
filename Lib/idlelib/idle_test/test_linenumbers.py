8f02e7bdb79b76e725cccb675ae591bebf8ca176
Tal Einat <taleinat@gmail.com>
2019-07-06 22:23:47 +0300

    refactor and expand tests, also fixing test_delete

--- Lib/idlelib/idle_test/test_linenumbers.py
+++ Lib/idlelib/idle_test/test_linenumbers.py
@@ -1,4 +1,5 @@
 """Unittest for idlelib.LineNumber"""
+from itertools import chain
 import unittest
 from test.support import requires
 import tkinter as tk
@@ -11,6 +12,7 @@ from idlelib.linenumbers import LineNumbers
 class Dummy_editwin:
     def __init__(self, text):
         self.text = text
+        self.text_frame = self.text.master
         self.per = Percolator(text)
         self.undo = Delegator()
         self.per.insertfilter(self.undo)
@@ -34,7 +36,7 @@ class LineNumberTest(unittest.TestCase):
         cls.editwin.per.close()
         cls.text.destroy()
         cls.root.destroy()
-        del cls.text, cls.root
+        del cls.text, cls.editwin, cls.root
 
     def setUp(self):
         self.linenumber = LineNumbers(self.editwin)
@@ -42,69 +44,85 @@ class LineNumberTest(unittest.TestCase):
     def tearDown(self):
         self.text.delete('1.0', 'end')
 
+    def assert_state_disabled(self):
+        state = self.linenumber.sidebar_text.config()['state']
+        self.assertEqual(state[-1], tk.DISABLED)
+
+    def get_sidebar_text_contents(self):
+        return self.linenumber.sidebar_text.get('1.0', tk.END)
+
+    def assert_sidebar_n_lines(self, n_lines):
+        expected = '\n'.join(chain(map(str, range(1, n_lines + 1)), ['']))
+        self.assertEqual(self.get_sidebar_text_contents(), expected)
+
+    def assert_text_equals(self, expected):
+        return self.assertEqual(self.text.get('1.0', 'end'), expected)
+
     def test_init_empty(self):
-        get = self.linenumber.sidebar_text.get
-        self.assertEqual(get('1.0', 'end'), '1\n')
+        self.assert_sidebar_n_lines(1)
 
     def test_init_not_empty(self):
         self.text.insert('insert', 'foo bar\n'*3)
-        linenumber = LineNumbers(self.editwin)
-        get = linenumber.sidebar_text.get
-        self.assertEqual(get('1.0', 'end'), '1\n2\n3\n4\n')
+        self.assert_text_equals('foo bar\n'*3 + '\n')
+        self.assert_sidebar_n_lines(4)
 
     def test_toggle_linenumbering(self):
-        self.linenumber.side = tk.LEFT
-        self.linenumber.toggle_line_numbers_event('')
+        self.assertEqual(self.linenumber.side, tk.LEFT)
+        self.linenumber.toggle_line_numbers_event(None)
         self.assertEqual(self.linenumber.side, None)
-
-        self.linenumber.side = None
-        self.linenumber.toggle_line_numbers_event('')
+        self.linenumber.toggle_line_numbers_event(None)
         self.assertEqual(self.linenumber.side, tk.LEFT)
 
     def test_insert(self):
-        text = self.editwin.text
-        get = self.linenumber.sidebar_text.get
-        config = self.linenumber.sidebar_text.config
+        self.text.insert('insert', 'foobar')
+        self.assert_text_equals('foobar\n')
+        self.assert_sidebar_n_lines(1)
+        self.assert_state_disabled()
 
-        text.insert('insert', 'foobar')
-        self.assertEqual(get('1.0', 'end'), '1\n')
-        self.assertEqual(config()['state'][-1], tk.DISABLED)
+        self.text.insert('insert', '\nfoo')
+        self.assert_text_equals('foobar\nfoo\n')
+        self.assert_sidebar_n_lines(2)
+        self.assert_state_disabled()
 
-        text.insert('insert', '\nfoo')
-        self.assertEqual(get('1.0', 'end'), '1\n2\n')
-        self.assertEqual(config()['state'][-1], tk.DISABLED)
+        self.text.insert('insert', 'hello\n'*2)
+        self.assert_text_equals('foobar\nfoohello\nhello\n\n')
+        self.assert_sidebar_n_lines(4)
+        self.assert_state_disabled()
 
-        text.insert('insert', 'hello\n'*2)
-        self.assertEqual(get('1.0', 'end'), '1\n2\n3\n4\n')
-        self.assertEqual(config()['state'][-1], tk.DISABLED)
-
-        text.insert('insert', '\nworld')
-        self.assertEqual(get('1.0', 'end'), '1\n2\n3\n4\n5\n')
-        self.assertEqual(config()['state'][-1], tk.DISABLED)
+        self.text.insert('insert', '\nworld')
+        self.assert_text_equals('foobar\nfoohello\nhello\n\nworld\n')
+        self.assert_sidebar_n_lines(5)
+        self.assert_state_disabled()
 
     def test_delete(self):
-        text = self.editwin.text
-        get = self.linenumber.sidebar_text.get
-        config = self.linenumber.sidebar_text.config
+        self.text.insert('insert', 'foobar')
+        self.assert_text_equals('foobar\n')
+        self.text.delete('1.1', '1.3')
+        self.assert_text_equals('fbar\n')
+        self.assert_sidebar_n_lines(1)
+        self.assert_state_disabled()
 
-        text.insert('insert', 'foobar')
-        text.delete('1.1', '1.3')
-        self.assertEqual(get('1.0', 'end'), '1\n')
-        self.assertEqual(config()['state'][-1], tk.DISABLED)
+        self.text.insert('insert', 'foo\n'*2)
+        self.assert_text_equals('fbarfoo\nfoo\n\n')
+        self.assert_sidebar_n_lines(3)
+        self.assert_state_disabled()
 
-        text.insert('insert', 'foo\n'*2)
-        text.delete('3.1')
-        text.delete('2.0', '2.end')
-        self.assertEqual(get('1.0', 'end'), '1\n2\n')
-        self.assertEqual(config()['state'][-1], tk.DISABLED)
+        # note: deleting up to "2.end" doesn't delete the final newline
+        self.text.delete('2.0', '2.end')
+        self.assert_text_equals('fbarfoo\n\n\n')
+        self.assert_sidebar_n_lines(3)
+        self.assert_state_disabled()
 
-        text.delete('1.3', 'end')
-        self.assertEqual(get('1.0', 'end'), '1\n')
-        self.assertEqual(config()['state'][-1], tk.DISABLED)
+        self.text.delete('1.3', 'end')
+        self.assert_text_equals('fba\n')
+        self.assert_sidebar_n_lines(1)
+        self.assert_state_disabled()
 
-        text.delete('1.0', 'end')
-        self.assertEqual(get('1.0', 'end'), '1\n')
-        self.assertEqual(config()['state'][-1], tk.DISABLED)
+        # note: Text widgets always keep a single '\n' character at the end
+        self.text.delete('1.0', 'end')
+        self.assert_text_equals('\n')
+        self.assert_sidebar_n_lines(1)
+        self.assert_state_disabled()
 
     def test_sidebar_text_width(self):
         """
@@ -113,26 +131,65 @@ class LineNumberTest(unittest.TestCase):
         """
         def get_width():
             return self.linenumber.sidebar_text.config()['width'][-1]
-        text = self.text
 
+        self.assert_sidebar_n_lines(1)
         self.assertEqual(get_width(), 1)
 
-        text.insert('insert', 'foo')
+        self.text.insert('insert', 'foo')
+        self.assert_sidebar_n_lines(1)
         self.assertEqual(get_width(), 1)
 
-        text.insert('insert', 'foo\n'*10)
+        self.text.insert('insert', 'foo\n'*8)
+        self.assert_sidebar_n_lines(9)
+        self.assertEqual(get_width(), 1)
+
+        self.text.insert('insert', 'foo\n')
+        self.assert_sidebar_n_lines(10)
         self.assertEqual(get_width(), 2)
 
-        text.insert('insert', 'foo\n'*100)
+        self.text.insert('insert', 'foo\n')
+        self.assert_sidebar_n_lines(11)
+        self.assertEqual(get_width(), 2)
+
+        self.text.delete('insert -1l linestart', 'insert linestart')
+        self.assert_sidebar_n_lines(10)
+        self.assertEqual(get_width(), 2)
+
+        self.text.delete('insert -1l linestart', 'insert linestart')
+        self.assert_sidebar_n_lines(9)
+        self.assertEqual(get_width(), 1)
+
+        self.text.insert('insert', 'foo\n'*90)
+        self.assert_sidebar_n_lines(99)
+        self.assertEqual(get_width(), 2)
+
+        self.text.insert('insert', 'foo\n')
+        self.assert_sidebar_n_lines(100)
         self.assertEqual(get_width(), 3)
 
-        text.delete('50.0', 'end')
+        self.text.insert('insert', 'foo\n')
+        self.assert_sidebar_n_lines(101)
+        self.assertEqual(get_width(), 3)
+
+        self.text.delete('insert -1l linestart', 'insert linestart')
+        self.assert_sidebar_n_lines(100)
+        self.assertEqual(get_width(), 3)
+
+        self.text.delete('insert -1l linestart', 'insert linestart')
+        self.assert_sidebar_n_lines(99)
         self.assertEqual(get_width(), 2)
 
-        text.delete('5.0', 'end')
+        self.text.delete('50.0', 'end')
+        self.assert_sidebar_n_lines(49)
+        self.assertEqual(get_width(), 2)
+
+        self.text.delete('5.0', 'end')
+        self.assert_sidebar_n_lines(4)
         self.assertEqual(get_width(), 1)
 
-        text.delete('1.0', 'end')
+        # note: Text widgets always keep a single '\n' character at the end
+        self.text.delete('1.0', 'end')
+        self.assert_sidebar_n_lines(1)
         self.assertEqual(get_width(), 1)
 
 
