fa52a8372571a5f61d30ab3145bf6abb5e693780
Marco Paolini <markopaolini@gmail.com>
2019-07-13 20:54:13 +0100

    Mimimize the conditionals and the numer of ops inside the json.loads hot path.
    This partially reverts the previous commit as we found out
    the culprit wasn't the MOV but the strictness checks

--- Modules/_json.c
+++ Modules/_json.c
@@ -434,19 +434,20 @@ scanstring_unicode(PyObject *pystr, Py_ssize_t end, int strict, Py_ssize_t *next
     while (1) {
         /* Find the end of the string or the next escape */
         Py_UCS4 c = 0;
-        next = end;
-        /* This is a tight loop in a hot path so we try to avoid
-           MOV from the register variable into memory. See bpo-37587 */
-        for (Py_UCS4 c_in = 0; next < len; next++) {
-            c_in = PyUnicode_READ(kind, buf, next);
-            if (c_in == '"' || c_in == '\\') {
-                c = c_in;
+        Py_ssize_t invalid = -1;
+        for (next = end; next < len; next++) {
+            c = PyUnicode_READ(kind, buf, next);
+            if (c == '"' || c == '\\') {
                 break;
             }
-            else if (c_in <= 0x1f && strict) {
-                raise_errmsg("Invalid control character at", pystr, next);
-                goto bail;
-            }
+            /* Defer the strict error until outside this (hot) loop. */
+            /* See bpo-37587 */
+            if (c <= 0x1f && invalid < 0)
+              invalid = next;
+        }
+        if (strict && invalid >= 0) {
+            raise_errmsg("Invalid control character at", pystr, invalid);
+            goto bail;
         }
         if (!(c == '"' || c == '\\')) {
             raise_errmsg("Unterminated string starting at", pystr, begin);
