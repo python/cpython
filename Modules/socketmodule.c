604bea9bfda6a7bdfa662e2db4055ed2cb262dd9
Karl <karlding@users.noreply.github.com>
2019-07-23 11:44:58 -0700

    bpo-37085: Only use #ifdef for CAN_FD_FRAME
    The CAN_FD_FRAME flag was introduced in the 4.8 kernel, while the other
    ones were present since SocketCAN drivers were mainlined in 2.6.25. As
    such, it is probably unnecessary to guard against these constants being
    missing.

--- Modules/socketmodule.c
+++ Modules/socketmodule.c
@@ -7656,6 +7656,8 @@ PyInit__socket(void)
 #endif
 #ifdef HAVE_LINUX_CAN_BCM_H
     PyModule_AddIntMacro(m, CAN_BCM);
+
+    /* BCM opcodes */
     PyModule_AddIntConstant(m, "CAN_BCM_TX_SETUP", TX_SETUP);
     PyModule_AddIntConstant(m, "CAN_BCM_TX_DELETE", TX_DELETE);
     PyModule_AddIntConstant(m, "CAN_BCM_TX_READ", TX_READ);
@@ -7668,40 +7670,21 @@ PyInit__socket(void)
     PyModule_AddIntConstant(m, "CAN_BCM_RX_STATUS", RX_STATUS);
     PyModule_AddIntConstant(m, "CAN_BCM_RX_TIMEOUT", RX_TIMEOUT);
     PyModule_AddIntConstant(m, "CAN_BCM_RX_CHANGED", RX_CHANGED);
-#ifdef  SETTIMER
+
+    /* BCM flags */
     PyModule_AddIntConstant(m, "CAN_BCM_SETTIMER", SETTIMER);
-#endif
-#ifdef  STARTTIMER
     PyModule_AddIntConstant(m, "CAN_BCM_STARTTIMER", STARTTIMER);
-#endif
-#ifdef  TX_COUNTEVT
     PyModule_AddIntConstant(m, "CAN_BCM_TX_COUNTEVT", TX_COUNTEVT);
-#endif
-#ifdef  TX_ANNOUNCE
     PyModule_AddIntConstant(m, "CAN_BCM_TX_ANNOUNCE", TX_ANNOUNCE);
-#endif
-#ifdef  TX_CP_CAN_ID
     PyModule_AddIntConstant(m, "CAN_BCM_TX_CP_CAN_ID", TX_CP_CAN_ID);
-#endif
-#ifdef  RX_FILTER_ID
     PyModule_AddIntConstant(m, "CAN_BCM_RX_FILTER_ID", RX_FILTER_ID);
-#endif
-#ifdef  RX_CHECK_DLC
     PyModule_AddIntConstant(m, "CAN_BCM_RX_CHECK_DLC", RX_CHECK_DLC);
-#endif
-#ifdef  RX_NO_AUTOTIMER
     PyModule_AddIntConstant(m, "CAN_BCM_RX_NO_AUTOTIMER", RX_NO_AUTOTIMER);
-#endif
-#ifdef  RX_ANNOUNCE_RESUME
     PyModule_AddIntConstant(m, "CAN_BCM_RX_ANNOUNCE_RESUME", RX_ANNOUNCE_RESUME);
-#endif
-#ifdef  TX_RESET_MULTI_IDX
     PyModule_AddIntConstant(m, "CAN_BCM_TX_RESET_MULTI_IDX", TX_RESET_MULTI_IDX);
-#endif
-#ifdef  RX_RTR_FRAME
     PyModule_AddIntConstant(m, "CAN_BCM_RX_RTR_FRAME", RX_RTR_FRAME);
-#endif
 #ifdef  CAN_FD_FRAME
+    /* CAN_FD_FRAME was only introduced in the 4.8.x kernel series */
     PyModule_AddIntConstant(m, "CAN_BCM_CAN_FD_FRAME", CAN_FD_FRAME);
 #endif
 #endif
