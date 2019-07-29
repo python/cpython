d8cc08634f3594051dc5a738b7527bfd52d8af9f
Anthony Sottile <asottile@umich.edu>
2019-07-29 08:05:55 -0700

    Add additional test for multi-line SyntaxError (GH-15003)

--- Lib/test/test_cmd_line_script.py
+++ Lib/test/test_cmd_line_script.py
@@ -627,6 +627,22 @@ class CmdLineTest(unittest.TestCase):
                 ],
             )
 
+    def test_syntaxerror_invalid_escape_sequence_multi_line(self):
+        script = 'foo = """\\q\n"""\n'
+        with support.temp_dir() as script_dir:
+            script_name = _make_test_script(script_dir, 'script', script)
+            exitcode, stdout, stderr = assert_python_failure(
+                '-Werror', script_name,
+            )
+            self.assertEqual(
+                stderr.splitlines()[-3:],
+                [
+                    b'    foo = """\\q',
+                    b'          ^',
+                    b'SyntaxError: invalid escape sequence \\q',
+                ],
+            )
+
     def test_consistent_sys_path_for_direct_execution(self):
         # This test case ensures that the following all give the same
         # sys.path configuration:
