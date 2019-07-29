bf5115c6e7784ccca366a8454e3643c2091e5d57
Anthony Sottile <asottile@umich.edu>
2019-07-29 06:59:13 -0700

    Fix `SyntaxError` indicator printing too many spaces for multi-line strings (GH-14433)

--- Lib/test/test_cmd_line_script.py
+++ Lib/test/test_cmd_line_script.py
@@ -613,6 +613,20 @@ class CmdLineTest(unittest.TestCase):
             self.assertNotIn("\f", text)
             self.assertIn("\n    1 + 1 = 2\n    ^", text)
 
+    def test_syntaxerror_multi_line_fstring(self):
+        script = 'foo = f"""{}\nfoo"""\n'
+        with support.temp_dir() as script_dir:
+            script_name = _make_test_script(script_dir, 'script', script)
+            exitcode, stdout, stderr = assert_python_failure(script_name)
+            self.assertEqual(
+                stderr.splitlines()[-3:],
+                [
+                    b'    foo = f"""{}',
+                    b'          ^',
+                    b'SyntaxError: f-string: empty expression not allowed',
+                ],
+            )
+
     def test_consistent_sys_path_for_direct_execution(self):
         # This test case ensures that the following all give the same
         # sys.path configuration:
