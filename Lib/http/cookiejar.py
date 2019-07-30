03f7ffcfd388bd971e16146d3cfaff34ecd86e8b
Ngalim Siregar <ngalim.siregar@gmail.com>
2019-07-30 21:39:44 +0700

    bpo-37246: Check expiry with current timestamp in DefaultCookiePolicy

--- Lib/http/cookiejar.py
+++ Lib/http/cookiejar.py
@@ -1125,7 +1125,7 @@ class DefaultCookiePolicy(CookiePolicy):
         return True
 
     def return_ok_expires(self, cookie, request):
-        if cookie.is_expired(self._now):
+        if cookie.is_expired():
             _debug("   cookie expired")
             return False
         return True
