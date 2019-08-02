1002c433214c0937a7ef649de0f960b35be651f5
btwood <4839861+btwood@users.noreply.github.com>
2019-08-02 08:41:01 -0600

    Thai Language Aliases
    Adding aliases for Thai language support. The current code page is an implementation of the windows code page.
    This will alias '874', 'ms874', and 'windows_874' to cp874, adding Thai language support for those users.

--- Lib/encodings/aliases.py
+++ Lib/encodings/aliases.py
@@ -203,6 +203,11 @@ aliases = {
     'cp_gr'              : 'cp869',
     'csibm869'           : 'cp869',
     'ibm869'             : 'cp869',
+    
+    # cp874 codec
+    '874'                : 'cp874',
+    'ms874'              : 'cp874',
+    'windows_874'        : 'cp874',
 
     # cp932 codec
     '932'                : 'cp932',
