b1d2525a28f9d588f11460da985437902f9ba9d9
Ngalim Siregar <ngalim.siregar@gmail.com>
2019-08-03 12:46:02 +0700

    bpo-37444: Update differing exception between builtins and importlib (GH-14869)
    Imports now raise `TypeError` instead of `ValueError` for relative import failures. This makes things consistent between `builtins.__import__` and `importlib.__import__` as well as using a more natural import for the failure.
    
    
    https://bugs.python.org/issue37444
    
    
    
    Automerge-Triggered-By: @brettcannon

--- Lib/importlib/_bootstrap.py
+++ Lib/importlib/_bootstrap.py
@@ -873,7 +873,7 @@ def _resolve_name(name, package, level):
     """Resolve a relative module name to an absolute one."""
     bits = package.rsplit('.', level - 1)
     if len(bits) < level:
-        raise ValueError('attempted relative import beyond top-level package')
+        raise ImportError('attempted relative import beyond top-level package')
     base = bits[0]
     return '{}.{}'.format(base, name) if name else base
 
