2606e0ec26a20cb7458a12fd035fffc475e926b5
qigangxu <9651234@qq.com>
2019-08-03 21:43:33 +0800

    bpo-37752: Delete redundant Py_CHARMASK in normalizestring()

--- Python/codecs.c
+++ Python/codecs.c
@@ -73,7 +73,7 @@ PyObject *normalizestring(const char *string)
         if (ch == ' ' || ch == '-')
             ch = '_';
         else
-            ch = Py_TOLOWER(Py_CHARMASK(ch));
+            ch = Py_TOLOWER(ch);
         p[i] = ch;
     }
     p[i] = '\0';
