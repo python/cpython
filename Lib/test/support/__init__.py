0a35d22cd977bb1ea78fa812b63babae708bf9c9
Serhiy Storchaka <storchaka@gmail.com>
2019-08-04 12:38:46 +0300

    [3.7] bpo-37685: Fixed comparisons of datetime.timedelta and datetime.timezone. (GH-14996)
    There was a discrepancy between the Python and C implementations.
    
    Add singletons ALWAYS_EQ, LARGEST and SMALLEST in test.support
    to test mixed type comparison..
    (cherry picked from commit 17e52649c0e7e9389f1cc2444a53f059e24e6bca)
    
    Co-authored-by: Serhiy Storchaka <storchaka@gmail.com>

--- Lib/test/support/__init__.py
+++ Lib/test/support/__init__.py
@@ -109,6 +109,7 @@ __all__ = [
     "run_with_locale", "swap_item",
     "swap_attr", "Matcher", "set_memlimit", "SuppressCrashReport", "sortdict",
     "run_with_tz", "PGO", "missing_compiler_executable", "fd_count",
+    "ALWAYS_EQ", "LARGEST", "SMALLEST"
     ]
 
 class Error(Exception):
@@ -2919,3 +2920,39 @@ class FakePath:
             raise self.path
         else:
             return self.path
+
+
+class _ALWAYS_EQ:
+    """
+    Object that is equal to anything.
+    """
+    def __eq__(self, other):
+        return True
+    def __ne__(self, other):
+        return False
+
+ALWAYS_EQ = _ALWAYS_EQ()
+
+@functools.total_ordering
+class _LARGEST:
+    """
+    Object that is greater than anything (except itself).
+    """
+    def __eq__(self, other):
+        return isinstance(other, _LARGEST)
+    def __lt__(self, other):
+        return False
+
+LARGEST = _LARGEST()
+
+@functools.total_ordering
+class _SMALLEST:
+    """
+    Object that is less than anything (except itself).
+    """
+    def __eq__(self, other):
+        return isinstance(other, _SMALLEST)
+    def __gt__(self, other):
+        return False
+
+SMALLEST = _SMALLEST()
