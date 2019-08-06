5090880fe19ad7179b8ec67d60830ab5ef8aa37c
Ben Lewis <benjimin@users.noreply.github.com>
2019-08-07 09:12:17 +1000

    Update Lib/test/test_builtin.py
    Co-Authored-By: Brett Cannon <54418+brettcannon@users.noreply.github.com>

--- Lib/test/test_builtin.py
+++ Lib/test/test_builtin.py
@@ -160,7 +160,7 @@ class BuiltinTest(unittest.TestCase):
         self.assertRaises(TypeError, __import__, 1, 2, 3, 4)
         self.assertRaises(ValueError, __import__, '')
         self.assertRaises(TypeError, __import__, 'sys', name='sys')
-        # relative import with no parent package, issue37409
+        # Relative import outside of a package with no __package__ or __spec__ (bpo-37409).
         self.assertRaises(ImportError, __import__, '',
                           {'__package__': None, '__spec__': None, '__name__': '__main__'},
                           locals={}, fromlist=('foo',), level=1)
