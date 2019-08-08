1d6677f66cbfdc42c1c4c0ff161f7c5eb667bc3c
Pablo Galindo <Pablogsal@gmail.com>
2019-08-08 23:25:46 +0100

    bpo-37795: Capture DeprecationWarnings in the test suite (GH-15184)

--- Lib/test/test_httplib.py
+++ Lib/test/test_httplib.py
@@ -7,6 +7,7 @@ import array
 import re
 import socket
 import threading
+import warnings
 
 import unittest
 TestCase = unittest.TestCase
@@ -1759,8 +1760,11 @@ class HTTPSTest(TestCase):
         self.assertIs(h._context, context)
         self.assertFalse(h._context.post_handshake_auth)
 
-        h = client.HTTPSConnection('localhost', 443, context=context,
-                                   cert_file=CERT_localhost)
+        with warnings.catch_warnings():
+            warnings.filterwarnings('ignore', 'key_file, cert_file and check_hostname are deprecated',
+                                    DeprecationWarning)
+            h = client.HTTPSConnection('localhost', 443, context=context,
+                                       cert_file=CERT_localhost)
         self.assertTrue(h._context.post_handshake_auth)
 
 
