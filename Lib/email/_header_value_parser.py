0849ffb78e3f37e093f8bae2b1d41806388ae1b2
Abhilash Raj <raj.abhilash1@gmail.com>
2019-08-10 13:23:05 -0700

    Make the check of length more explicit.

--- Lib/email/_header_value_parser.py
+++ Lib/email/_header_value_parser.py
@@ -584,7 +584,7 @@ class DisplayName(Phrase):
             for x in self:
                 if x.token_type == 'quoted-string':
                     quote = True
-        if self and quote:
+        if len(self) != 0 and quote:
             pre = post = ''
             if self[0].token_type=='cfws' or self[0][0].token_type=='cfws':
                 pre = ' '
