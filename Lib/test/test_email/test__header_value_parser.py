1a0887cae25238128b0bc2f0bb2c2525096d4ccf
Abhilash Raj <maxking@users.noreply.github.com>
2019-08-11 13:45:09 -0700

    bpo-32178: Fix IndexError trying to parse 'To' header starting with ':'. (GH-15044)
    This should fix the IndexError trying to retrieve `DisplayName.display_name` and `DisplayName.value` when the `value` is basically an empty string.
    
    
    
    
    https://bugs.python.org/issue32178

--- Lib/test/test_email/test__header_value_parser.py
+++ Lib/test/test_email/test__header_value_parser.py
@@ -1700,6 +1700,14 @@ class TestParser(TestParserMixin, TestEmailBase):
         self.assertEqual(display_name[3].comments, ['with trailing comment'])
         self.assertEqual(display_name.display_name, 'simple phrase.')
 
+    def test_get_display_name_for_invalid_address_field(self):
+        # bpo-32178: Test that address fields starting with `:` don't cause
+        # IndexError when parsing the display name.
+        display_name = self._test_get_x(
+            parser.get_display_name,
+            ':Foo ', '', '', [errors.InvalidHeaderDefect], ':Foo ')
+        self.assertEqual(display_name.value, '')
+
     # get_name_addr
 
     def test_get_name_addr_angle_addr_only(self):
