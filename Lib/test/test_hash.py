bc3ac6b8709eb64e8703a1febad0ce5f7e0e33c9
Ashwin Ramaswami <aramaswamis@gmail.com>
2019-08-14 00:44:00 +0000

    fix test order

--- Lib/test/test_hash.py
+++ Lib/test/test_hash.py
@@ -329,11 +329,11 @@ class DatetimeDatetimeTests(DatetimeFastTests, unittest.TestCase):
 class DatetimeDatetimeWithTimezoneFastTests(DatetimeFastTests, unittest.TestCase):
     repr_ = repr(datetime.datetime(1, 2, 3, 4, 5, 6, 7, datetime.timezone.utc))
 
-class DatetimeTimeWithTimezoneFastTests(DatetimeFastTests, unittest.TestCase):
-    repr_ = repr(datetime.time(0, 0, 0, 0, datetime.timezone.utc))
-
 class DatetimeTimeFastTests(DatetimeFastTests, unittest.TestCase):
     repr_ = repr(datetime.time(0))
+
+class DatetimeTimeWithTimezoneFastTests(DatetimeFastTests, unittest.TestCase):
+    repr_ = repr(datetime.time(0, 0, 0, 0, datetime.timezone.utc))
 
 class DatetimeDatePureTests(DatetimePureTests, unittest.TestCase):
     repr_ = repr(datetime.date(1066, 10, 14))
