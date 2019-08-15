995fbd9af53d21b6b9730d8739634788ced90f7b
Greg Price <gnprice@gmail.com>
2019-08-14 18:18:53 -0700

    bpo-37760: Avoid cluttering work tree with downloaded Unicode files. (GH-15128)

--- Tools/unicode/makeunicodedata.py
+++ Tools/unicode/makeunicodedata.py
@@ -887,15 +887,18 @@ def merge_old_version(version, new, old):
                         normalization_changes))
 
 
+DATA_DIR = os.path.join('Tools', 'unicode', 'data')
+
 def open_data(template, version):
-    local = template % ('-'+version,)
+    local = os.path.join(DATA_DIR, template % ('-'+version,))
     if not os.path.exists(local):
         import urllib.request
         if version == '3.2.0':
             # irregular url structure
-            url = 'http://www.unicode.org/Public/3.2-Update/' + local
+            url = ('http://www.unicode.org/Public/3.2-Update/'+template) % ('-'+version,)
         else:
             url = ('http://www.unicode.org/Public/%s/ucd/'+template) % (version, '')
+        os.makedirs(DATA_DIR, exist_ok=True)
         urllib.request.urlretrieve(url, filename=local)
     if local.endswith('.txt'):
         return open(local, encoding='utf-8')
