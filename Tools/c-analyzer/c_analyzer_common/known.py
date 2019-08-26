d58c7c49a9ee11026c54e649038bfd2a469d793b
Eric Snow <ericsnowcurrently@gmail.com>
2019-08-26 13:42:01 -0600

    known_from_file() -> known.from_file().

--- Tools/c-analyzer/c_analyzer_common/known.py
+++ Tools/c-analyzer/c_analyzer_common/known.py
@@ -4,9 +4,9 @@ from . import info, util
 
 
 # XXX need tests:
-# * known_from_file()
+# * from_file()
 
-def known_from_file(infile, fmt=None):
+def from_file(infile, fmt=None):
     """Yield StaticVar for each ignored var in the file."""
     raise NotImplementedError
     return {
