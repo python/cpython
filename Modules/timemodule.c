3af3e196713e61ae6c6565c92105e678a2e937e6
vrajivk <3413293+vrajivk@users.noreply.github.com>
2019-08-26 21:13:12 -0700

    bpo-36205: Fix the rusage implementation of time.process_time() (GH-15538)

--- Modules/timemodule.c
+++ Modules/timemodule.c
@@ -1208,7 +1208,7 @@ _PyTime_GetProcessTimeWithInfo(_PyTime_t *tp, _Py_clock_info_t *info)
             return -1;
         }
 
-        _PyTime_t total = utime + utime;
+        _PyTime_t total = utime + stime;
         *tp = total;
         return 0;
     }
