2e1e0dd2c76da1fe9569f891878fe9ffd8fdc76a
Daniel Fortunov <asqui@users.noreply.github.com>
2019-08-28 06:38:09 +0200

    bpo-36582: Make collections.UserString.encode() return bytes, not str (GH-13138)

--- Lib/collections/__init__.py
+++ Lib/collections/__init__.py
@@ -1184,12 +1184,10 @@ class UserString(_collections_abc.Sequence):
         if isinstance(sub, UserString):
             sub = sub.data
         return self.data.count(sub, start, end)
-    def encode(self, encoding=None, errors=None): # XXX improve this?
-        if encoding:
-            if errors:
-                return self.__class__(self.data.encode(encoding, errors))
-            return self.__class__(self.data.encode(encoding))
-        return self.__class__(self.data.encode())
+    def encode(self, encoding='utf-8', errors='strict'):
+        encoding = 'utf-8' if encoding is None else encoding
+        errors = 'strict' if errors is None else errors
+        return self.data.encode(encoding, errors)
     def endswith(self, suffix, start=0, end=_sys.maxsize):
         return self.data.endswith(suffix, start, end)
     def expandtabs(self, tabsize=8):
