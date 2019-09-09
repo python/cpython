483e5dc308c85e14e4b16d05af64865e8880f098
neonene <53406459+neonene@users.noreply.github.com>
2019-09-09 21:33:43 +0900

    bpo-37702: Fix SSL's certificate-store leak on Windows (GH-15632)
    ssl_collect_certificates function in _ssl.c has a memory leak.
    Calling CertOpenStore() and CertAddStoreToCollection(), a store's refcnt gets incremented by 2.
    But CertCloseStore() is called only once and the refcnt leaves 1.
    (cherry picked from commit ed70129e15ea028469145111044a4349960a4e6f)
    
    Co-authored-by: neonene <53406459+neonene@users.noreply.github.com>

--- Modules/_ssl.c
+++ Modules/_ssl.c
@@ -5448,6 +5448,7 @@ ssl_collect_certificates(const char *store_name)
             if (result) {
                 ++storesAdded;
             }
+            CertCloseStore(hSystemStore, 0);  /* flag must be 0 */
         }
     }
     if (storesAdded == 0) {
