cec68c31e8507159534b6eec3fb5d801cd3dbf8c
Stéphane Wirtel <stephane@wirtel.be>
2019-09-11 19:36:06 +0200

    [3.7] bpo-38117: Test with OpenSSL 1.1.1d (GH-15983) (GH-15994)
    Signed-off-by: Christian Heimes <christian@python.org>.
    (cherry picked from commit 58ab13479d854491ac9207bacfae25e8b18b044a)
    
    Co-authored-by: Christian Heimes <christian@python.org>
    
    
    https://bugs.python.org/issue38117
    
    
    
    Automerge-Triggered-By: @matrixise

--- Tools/ssl/multissltests.py
+++ Tools/ssl/multissltests.py
@@ -45,16 +45,16 @@ OPENSSL_OLD_VERSIONS = [
 ]
 
 OPENSSL_RECENT_VERSIONS = [
-    "1.0.2s",
-    "1.1.0k",
-    "1.1.1c",
+    "1.0.2t",
+    "1.1.0l",
+    "1.1.1d",
 ]
 
 LIBRESSL_OLD_VERSIONS = [
 ]
 
 LIBRESSL_RECENT_VERSIONS = [
-    "2.7.4",
+    "2.9.2",
 ]
 
 # store files in ../multissl
