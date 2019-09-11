528ba1cbe0b8aad739a2650d9eb6c40e3c1c6c24
blhsing <github@ydooby.com>
2019-09-11 06:17:07 -0700

    bpo-37972: delegate only attributes of tuple instead to __getattr__

--- Lib/unittest/mock.py
+++ Lib/unittest/mock.py
@@ -2429,7 +2429,7 @@ class _Call(tuple):
 
 
     def __getattribute__(self, attr):
-        if attr not in tuple.__getattribute__(self, '__dict__'):
+        if attr in tuple.__dict__:
             raise AttributeError
         return tuple.__getattribute__(self, attr)
 
