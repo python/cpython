ec63183c3b20fd4cd33b64bf37f7102be02648f2
Joannah Nanjekye <nanjekyejoannah@gmail.com>
2019-09-12 17:01:57 +0000

    Implementation of all methods and relevant documentation

--- Doc/library/interpreters.rst
+++ Doc/library/interpreters.rst
@@ -13,32 +13,159 @@ level :mod:`_interpreters` module.
 
 .. versionchanged:: 3.9
 
+Interpreter Objects
+-------------------
 
-This module defines the following functions:
+The Interpreter object represents a single interpreter.
 
+.. class:: Interpreter(id)
+
+    The class implementing a subinterpreter object.
+
+    .. method:: is_running()
+
+       Return whether or not the identified interpreter is running.
+
+    .. method:: destroy()
+
+       Destroy the identified interpreter. Attempting to destroy the current
+       interpreter results in a RuntimeError. So does an unrecognized ID.
+
+    .. method:: run(self, src_str, /, *, channels=None):
+
+       Run the given source code in the interpreter. This blocks the current
+       thread until done.
+
+RecvChannel Objects
+-------------------
+
+The RecvChannel object represents a recieving channel.
+
+.. class:: RecvChannel(id)
+
+    This class represents the receiving end of a channel.
+
+    .. method:: recv()
+
+        Get the next object from the channel, and wait if none have been
+        sent. Associate the interpreter with the channel.
+
+    .. method:: recv_nowait(default=None)
+
+        Like ``recv()``, but return the default instead of waiting.
+
+     .. method:: release()
+
+        No longer associate the current interpreter with the channel
+        (on the sending end).
+
+    .. method:: close(force=False)
+
+        Close the channel in all interpreters.
+
+
+SendChannel Objects
+--------------------
+
+The SendChannel object represents a sending channel.
+
+.. class:: SendChannel(id)
+
+    This class represents the receiving end of a channel.
+
+    .. method:: send(obj)
+
+       Send the object (i.e. its data) to the receiving end of the channel
+       and wait.Associate the interpreter with the channel.
+
+    .. method:: send_nowait(obj)
+
+        Like ``send()``, but return False if not received.
+
+    .. method:: send_buffer(obj)
+
+       Send the object's buffer to the receiving end of the channel and wait.
+       Associate the interpreter with the channel.
+
+    .. method:: send_buffer_nowait(obj)
+
+       Like ``send_buffer()``, but return False if not received.
+
+    .. method:: release()
+
+       No longer associate the current interpreter with the channel
+       (on the sending end).
+
+    .. method:: close(force=False)
+
+        Close the channel in all interpreters.
+
+
+This module defines the following global functions:
+
+
+.. function:: is_shareable(obj)
+
+   Return `True` if the object's data can be shared between interpreters.
+
+.. function:: create_channel()
+
+   Create a new channel for passing data between interpreters.
+
+.. function:: list_all_channels()
+
+   Return all open channels.
 
 .. function:: create()
 
-   Create a new interpreter and return a unique generated ID.
-
-.. function:: list_all()
-
-   Return a list containing the ID of every existing interpreter.
+   Initialize a new (idle) Python interpreter.
 
 .. function:: get_current()
 
-   Return the ID of the currently running interpreter.
+   Get the currently running interpreter.
 
-.. function:: destroy(id)
+.. function:: list_all()
 
-   Destroy the interpreter whose ID is *id*. Attempting to destroy the current
-   interpreter results in a `RuntimeError`. So does an unrecognized ID.
+   Get all existing interpreters.
 
-.. function:: get_main()
+This module also defines the following exceptions.
 
-   Return the ID of the main interpreter.
+.. exception:: RunFailedError
 
-.. function:: run_string()
+   This exception, a subclass of :exc:`RuntimeError`, is raised when the
+   ``Interpreter.run()`` results in an uncaught exception.
 
-    Execute the provided string in the identified interpreter.
-    See `PyRun_SimpleStrings`.
+.. exception:: ChannelError
+
+   This exception, a subclass of :exc:`Exception`, and is the base class for
+   channel-related exceptions.
+
+.. exception:: ChannelNotFoundError
+
+   This exception, a subclass of :exc:`ChannelError`, is raised when the
+   the identified channel was not found.
+
+.. exception:: ChannelEmptyError
+
+   This exception, a subclass of :exc:`ChannelError`, is raised when
+   the channel is unexpectedly empty.
+
+.. exception:: ChannelNotEmptyError
+
+   This exception, a subclass of :exc:`ChannelError`, is raised when
+   the channel is unexpectedly not empty.
+
+.. exception:: NotReceivedError
+
+   This exception, a subclass of :exc:`ChannelError`, is raised when
+   nothing was waiting to receive a sent object.
+
+.. exception:: ChannelClosedError
+
+   This exception, a subclass of :exc:`ChannelError`, is raised when
+   the channel is closed.
+
+.. exception:: ChannelReleasedError
+
+   This exception, a subclass of :exc:`ChannelClosedError`, is raised when
+   the channel is released (but not yet closed).
--- Lib/interpreters.py
+++ Lib/interpreters.py
@@ -1,58 +1,224 @@
-"""Sub-interpreters High Level Module."""
-
-__all__ = ['create', 'list_all', 'get_current', 'get_main',
-            'run_string', 'destroy']
-
+"""Subinterpreters High Level Module."""
 
 import _interpreters
+import logger
 
-# Rename so that "from interpreters import *" is safe
-_list_all = _interpreters.list_all
-_get_current = _interpreters.get_current
-_get_main = _interpreters.get_main
-_run_string = _interpreters.run_string
+__all__ = _all__ = ['create', 'list_all', 'get_current', 'get_main',
+                    'run_string', 'destroy']
+
+
+class Interpreter:
+
+    def __init__(self, id):
+        self.id = id
+
+    def is_running(self):
+        """is_running() -> bool
+
+        Return whether or not the identified interpreter is running.
+        """
+        return _interpreters.is_running(self.id)
+
+    def destroy(self):
+        """destroy()
+
+        Destroy the identified interpreter.
+
+        Attempting to destroy the current
+        interpreter results in a RuntimeError. So does an unrecognized ID
+        """
+        return _interpreters.destroy(self.id)
+
+    def run(self, src_str, /, *, channels=None):
+        """run(src_str, /, *, channels=None)
+
+        Run the given source code in the interpreter.
+        This blocks the current thread until done.
+        """
+        try:
+            _interpreters.run_string(self.id, src_str)
+        except RunFailedError as err:
+            logger.error(err)
+            raise
+
+def wait(self, timeout):
+    #The implementation for wait
+    # will be non trivial to be useful
+    import time
+    time.sleep(timeout)
+
+def associate_interp_to_channel(id, cid):
+    pass
+
+class RecvChannel:
+
+    def __init__(self, id):
+        self.id = id
+        self.interpreters = _interpreters.list_all()
+
+    def recv(self, timeout=2):
+        """ channel_recv() -> obj
+
+
+        Get the next object from the channel,
+        and wait if none have been sent.
+        Associate the interpreter with the channel.
+        """
+        obj = _interpreters.channel_recv(self.id)
+        if obj == None:
+            wait(timeout)
+            obj = obj = _interpreters.channel_recv(self.id)
+
+        associate_interp_to_channel(interpId, Cid)
+
+        return obj
+
+    def recv_nowait(self, default=None):
+        """recv_nowait(default=None) -> object
+
+        Like recv(), but return the default instead of waiting.
+        """
+        return _interpreters.channel_recv(self.id)
+
+    def release(self):
+        """ release()
+
+        No longer associate the current interpreterwith the channel
+        (on the sending end).
+        """
+        pass
+
+    def close(self, force=False):
+        """close(force=False)
+
+        Close the channel in all interpreters..
+        """
+        return _interpreters.channel_close(self.id, force)
+
+
+class SendChannel:
+
+    def __init__(self, id):
+        self.id = id
+        self.interpreters = _interpreters.list_all()
+
+    def send(self, obj, timeout=2):
+        """ send(obj)
+
+        Send the object (i.e. its data) to the receiving end of the channel
+        and wait. Associate the interpreter with the channel.
+        """
+        obj = _interpreters.channel_send(self.id, obj)
+        wait(timeout)
+        associate_interp_to_channel(interpId, Cid)
+
+    def send_nowait(self, obj):
+        """ send_nowait(obj)
+
+        Like send(), but return False if not received.
+        """
+        try:
+            obj = _interpreters.channel_send(self.id, obj)
+        except:
+            return False
+
+        return obj
+
+    def release(self):
+        """ release()
+
+        No longer associate the current interpreterwith the channel
+        (on the sending end).
+        """
+        pass
+
+    def close(self, force=False):
+        """ close(force=False)
+
+        No longer associate the current interpreterwith the channel
+        (on the sending end).
+        """
+        return _interpreters.channel_close(self.id, force)
+
+
+class ChannelError(Exception):
+    pass
+
+
+class ChannelNotFoundError(ChannelError):
+    pass
+
+
+class ChannelEmptyError(ChannelError):
+    pass
+
+
+class ChannelNotEmptyError(ChannelError):
+    pass
+
+
+class NotReceivedError(ChannelError):
+    pass
+
+
+class ChannelClosedError(ChannelError):
+    pass
+
+
+class ChannelReleasedError(ChannelClosedError):
+    pass
+
+
+class RunFailedError(RuntimeError):
+    pass
+
 
 # Global API functions
+
+def is_shareable(obj):
+    """ is_shareable(obj) -> Bool
+
+    Return `True` if the object's data can be shared between
+    interpreters.
+    """
+    return _interpreters.is_shareable(obj)
+
+def create_channel():
+    """ create_channel() -> (RecvChannel, SendChannel)
+
+    Create a new channel for passing data between interpreters.
+    """
+
+    cid = _interpreters.channel_create()
+    return (RecvChannel(cid), SendChannel(cid))
+
+def list_all_channels():
+    """ list_all_channels() -> [(RecvChannel, SendChannel)]
+
+    Return all open channels.
+    """
+    cid = _interpreters.channel_list_all()
+    return (RecvChannel(cid), SendChannel(cid))
 
 def create():
     """ create() -> Interpreter
 
-    Create a new interpreter and return a unique generated ID.
+   Initialize a new (idle) Python interpreter.
     """
-    return _interpreters.create()
+    id = _interpreters.create()
+    return Interpreter(id)
 
 def list_all():
-    """list_all() -> [Interpreter]
+    """ list_all() -> [Interpreter]
 
-    Return a list containing the ID of every existing interpreter.
+    Get all existing interpreters.
     """
-    return _list_all()
+    return [Interpreter(id) for id in _interpreters.list_all()]
 
 def get_current():
-    """get_current() -> Interpreter
+    """ get_current() -> Interpreter
 
-    Return the ID of the currently running interpreter.
+    Get the currently running interpreter.
     """
-    return _get_current()
-
-def get_main():
-    """get_main() -> ID
-
-    Return the ID of the main interpreter.
-    """
-    return _get_main()
-
-def destroy(id):
-    """destroy(id) -> None
-
-    Destroy the identified interpreter.
-    """
-    return _interpreters.destroy(id)
-
-def run_string(id, script, shared):
-    """run_string(id, script, shared) -> None
-
-    Execute the provided string in the identified interpreter.
-    See PyRun_SimpleStrings.
-    """
-    return _run_string(id, script, shared)
+    id = _interpreters.get_current()
+    return Interpreter(id)
