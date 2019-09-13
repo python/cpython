f14fcbf2995b43c8607a1448402633ac558a745f
Julien Palard <julien@palard.fr>
2019-09-13 12:20:14 +0200

    Revert "bpo-37785: Fix xgettext warning in argparse (GH-15161)" (GH-16082)
    This reverts commit b50eff65906f8e9b4597cb0128ea1729341346fc because
    it's an incompatible change that would have broken the existing
    translations.

--- Lib/argparse.py
+++ Lib/argparse.py
@@ -1207,9 +1207,8 @@ class FileType(object):
             return open(string, self._mode, self._bufsize, self._encoding,
                         self._errors)
         except OSError as e:
-            args = {'filename': string, 'error': e}
-            message = _("can't open '%(filename)s': %(error)s")
-            raise ArgumentTypeError(message % args)
+            message = _("can't open '%s': %s")
+            raise ArgumentTypeError(message % (string, e))
 
     def __repr__(self):
         args = self._mode, self._bufsize
