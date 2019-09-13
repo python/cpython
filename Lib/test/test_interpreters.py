80f680867593fd7562c642ed19881147006cdbba
Joannah Nanjekye <33177550+nanjekyejoannah@users.noreply.github.com>
2019-09-13 05:30:29 -0300

    Update Lib/test/test_interpreters.py
    Co-Authored-By: Eric Snow <ericsnowcurrently@gmail.com>

--- Lib/test/test_interpreters.py
+++ Lib/test/test_interpreters.py
@@ -24,6 +24,44 @@ def clean_up_interpreters():
             pass  # already destroyed
 
 
+class LowLevelStub:
+    # set these as appropriate in tests
+    errors = ()
+    return_create = ()
+    ...
+    def __init__(self):
+        self._calls = []
+    def _add_call(self, name, args=(), kwargs=None):
+        self.calls.append(
+            (name, args, kwargs or {}))
+    def _maybe_error(self):
+        if not self.errors:
+            return
+        err = self.errors.pop(0)
+        if err is not None:
+            raise err
+    def check_calls(self, test, expected):
+        test.assertEqual(self._calls, expected)
+        for returns in [self.errors, self.return_create, ...]:
+            test.assertEqual(tuple(returns), ())  # make sure all were used
+        
+    # the stubbed methods
+    def create(self):
+        self._add_call('create')
+        self._maybe_error()
+        return self.return_create.pop(0)
+    def list_all(self):
+        ...
+    def get_current(self):
+        ...
+    def get_main(self):
+        ...
+    def destroy(self, id):
+        ...
+    def run_string(self, id, text, ...):
+        ...
+
+
 class TestBase(unittest.TestCase):
 
     def tearDown(self):
