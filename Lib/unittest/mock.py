4ff492ea4fb4e2f24b7f95fb1c24c3f6a284cfac
Elizabeth Uselton <elizabeth.uselton@gmail.com>
2019-09-13 08:54:32 -0700

    bpo-37555: Update _CallList.__contains__ to respect ANY (#14700)
    * Flip equality to use mock calls' __eq__
    
    * bpo-37555: Regression test demonstrating assert_has_calls not working with ANY and spec_set
    
    Co-authored-by: Neal Finne <neal@nealfinne.com>
    
    * Revert "Flip equality to use mock calls' __eq__"
    
    This reverts commit 94ddf54c5a8aab7d00d9ab93e1cc5695c28d73e7.
    
    * bpo-37555: Add regression tests for mock ANY ordering issues
    
    Add regression tests for whether __eq__ is order agnostic on _Call and _CallList, which is useful for comparisons involving ANY, especially if the ANY comparison is to a class not defaulting __eq__ to NotImplemented.
    
    Co-authored-by: Neal Finne <neal@nealfinne.com>
    
    * bpo-37555: Fix _CallList and _Call order sensitivity
    
    _Call and _CallList depend on ordering to correctly process that an object being compared to ANY with __eq__ should return True. This fix updates the comparison to check both a == b and b == a and return True if either condition is met, fixing situations from the tests in the previous two commits where assertEqual would not be commutative if checking _Call or _CallList objects. This seems like a reasonable fix considering that the Python data model specifies that if an object doesn't know how to compare itself to another object it should return NotImplemented, and that on getting NotImplemented from a == b, it should try b == a, implying that good behavior for __eq__ is commutative. This also flips the order of comparison in _CallList's __contains__ method, guaranteeing ANY will be on the left and have it's __eq__ called for equality checking, fixing the interaction between assert_has_calls and ANY.
    
    Co-author: Neal Finne <neal@neal.finne.com>
    
    * bpo-37555: Ensure _call_matcher returns _Call object
    
    * Adding ACK and news entry
    
    * bpo-37555: Replacing __eq__ with == to sidestep NotImplemented
    
    bool(NotImplemented) returns True, so it's necessary to use ==
    instead of __eq__ in this comparison.
    
    * bpo-37555: cleaning up changes unnecessary to the final product
    
    * bpo-37555: Fixed call on bound arguments to respect args and kwargs
    
    * Revert "bpo-37555: Add regression tests for mock ANY ordering issues"
    
    This reverts commit 49c5310ad493c4356dd3bc58c03653cd9466c4fa.
    
    * Revert "bpo-37555: cleaning up changes unnecessary to the final product"
    
    This reverts commit 18e964ba0126d8964d89842cb95534b63c2d326e.
    
    * Revert "bpo-37555: Replacing __eq__ with == to sidestep NotImplemented"
    
    This reverts commit f295eaca5bceac6636c0e2b10e6c7d9a8ee8296a.
    
    * Revert "bpo-37555: Fix _CallList and _Call order sensitivity"
    
    This reverts commit 874fb697b8376fcea130116e56189061f944fde6.
    
    * Updated NEWS.d
    
    * bpo-37555: Add tests checking every function using _call_matcher both with and without spec
    
    * bpo-37555: Ensure all assert methods using _call_matcher are actually passing calls
    
    * Remove AnyCompare and use call objects everywhere.
    
    * Revert "Remove AnyCompare and use call objects everywhere."
    
    This reverts commit 24973c0b32ce7d796a7f4eeaf259832222aae0f5.
    
    * Check for exception in assert_any_await

--- Lib/unittest/mock.py
+++ Lib/unittest/mock.py
@@ -852,7 +852,8 @@ class NonCallableMock(Base):
             else:
                 name, args, kwargs = _call
             try:
-                return name, sig.bind(*args, **kwargs)
+                bound_call = sig.bind(*args, **kwargs)
+                return call(name, bound_call.args, bound_call.kwargs)
             except TypeError as e:
                 return e.with_traceback(None)
         else:
@@ -901,9 +902,9 @@ class NonCallableMock(Base):
         def _error_message():
             msg = self._format_mock_failure_message(args, kwargs)
             return msg
-        expected = self._call_matcher((args, kwargs))
+        expected = self._call_matcher(_Call((args, kwargs), two=True))
         actual = self._call_matcher(self.call_args)
-        if expected != actual:
+        if actual != expected:
             cause = expected if isinstance(expected, Exception) else None
             raise AssertionError(_error_message()) from cause
 
@@ -963,10 +964,10 @@ class NonCallableMock(Base):
         The assert passes if the mock has *ever* been called, unlike
         `assert_called_with` and `assert_called_once_with` that only pass if
         the call is the most recent one."""
-        expected = self._call_matcher((args, kwargs))
+        expected = self._call_matcher(_Call((args, kwargs), two=True))
+        cause = expected if isinstance(expected, Exception) else None
         actual = [self._call_matcher(c) for c in self.call_args_list]
-        if expected not in actual:
-            cause = expected if isinstance(expected, Exception) else None
+        if cause or expected not in _AnyComparer(actual):
             expected_string = self._format_mock_call_signature(args, kwargs)
             raise AssertionError(
                 '%s call not found' % expected_string
@@ -1018,6 +1019,22 @@ class NonCallableMock(Base):
             return ""
         return f"\n{prefix}: {safe_repr(self.mock_calls)}."
 
+
+class _AnyComparer(list):
+    """A list which checks if it contains a call which may have an
+    argument of ANY, flipping the components of item and self from
+    their traditional locations so that ANY is guaranteed to be on
+    the left."""
+    def __contains__(self, item):
+        for _call in self:
+            if len(item) != len(_call):
+                continue
+            if all([
+                expected == actual
+                for expected, actual in zip(item, _call)
+            ]):
+                return True
+        return False
 
 
 def _try_iter(obj):
@@ -2171,9 +2188,9 @@ class AsyncMockMixin(Base):
             msg = self._format_mock_failure_message(args, kwargs, action='await')
             return msg
 
-        expected = self._call_matcher((args, kwargs))
+        expected = self._call_matcher(_Call((args, kwargs), two=True))
         actual = self._call_matcher(self.await_args)
-        if expected != actual:
+        if actual != expected:
             cause = expected if isinstance(expected, Exception) else None
             raise AssertionError(_error_message()) from cause
 
@@ -2192,10 +2209,10 @@ class AsyncMockMixin(Base):
         """
         Assert the mock has ever been awaited with the specified arguments.
         """
-        expected = self._call_matcher((args, kwargs))
+        expected = self._call_matcher(_Call((args, kwargs), two=True))
+        cause = expected if isinstance(expected, Exception) else None
         actual = [self._call_matcher(c) for c in self.await_args_list]
-        if expected not in actual:
-            cause = expected if isinstance(expected, Exception) else None
+        if cause or expected not in _AnyComparer(actual):
             expected_string = self._format_mock_call_signature(args, kwargs)
             raise AssertionError(
                 '%s await not found' % expected_string
