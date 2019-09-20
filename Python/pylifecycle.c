33adb39541119b6ddd14d4c580d4f34ff2e0efe4
Victor Stinner <vstinner@python.org>
2019-09-21 00:05:45 +0200

    bpo-38234: Remove _PyPathConfig.dll_path
    The DLL path is not computed from any user configuration and cannot
    be configured by PyConfig. Instead, add a new _Py_dll_path global variable.
    
    Remove _PyConfig_SetPathConfig(): replaced with _PyPathConfig_Init().

--- Python/pylifecycle.c
+++ Python/pylifecycle.c
@@ -475,7 +475,7 @@ pyinit_core_reconfigure(_PyRuntimeState *runtime,
     config = &interp->config;
 
     if (config->_install_importlib) {
-        status = _PyConfig_SetPathConfig(config);
+        status = _PyPathConfig_Init();
         if (_PyStatus_EXCEPTION(status)) {
             return status;
         }
@@ -646,7 +646,7 @@ pycore_init_import_warnings(PyThreadState *tstate, PyObject *sysmod)
     }
 
     if (config->_install_importlib) {
-        status = _PyConfig_SetPathConfig(config);
+        status = _PyPathConfig_Init();
         if (_PyStatus_EXCEPTION(status)) {
             return status;
         }
