8d4c52ffb47a4d3590758220f297ba58108e1ef3
Miss Islington (bot) <31488909+miss-islington@users.noreply.github.com>
2019-09-20 22:40:42 -0700

    Doc: Fix spelling errors of 'initial' in enum.py (GH-16314)
    (cherry picked from commit bb16fb2cb8dca1fa1d67621c3175793eda7dbdc2)
    
    Co-authored-by: HongWeipeng <hongweichen8888@sina.com>

--- Lib/enum.py
+++ Lib/enum.py
@@ -682,7 +682,7 @@ class Flag(Enum):
         Generate the next value when not given.
 
         name: the name of the member
-        start: the initital start value or None
+        start: the initial start value or None
         count: the number of existing members
         last_value: the last value assigned or None
         """
