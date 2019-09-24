0cd2024688a09cf4a86a7204fc3d2105b2d5d0e2
Victor Stinner <vstinner@redhat.com>
2019-09-24 14:19:48 +0200

    bpo-37123: multiprocessing test_mymanager() accepts SIGTERM (GH-16349)
    Multiprocessing test test_mymanager() now also expects -SIGTERM, not
    only exitcode 0.
    
    bpo-30356: BaseManager._finalize_manager() sends SIGTERM to the
    manager process if it takes longer than 1 second to stop, which
    happens on slow buildbots.
    (cherry picked from commit b0e1ae5f5430433766e023c1a6936aeba0f2b84e)
    
    Co-authored-by: Victor Stinner <vstinner@redhat.com>

--- Lib/test/_test_multiprocessing.py
+++ Lib/test/_test_multiprocessing.py
@@ -2799,16 +2799,17 @@ class _TestMyManager(BaseTestCase):
         self.common(manager)
         manager.shutdown()
 
-        # If the manager process exited cleanly then the exitcode
-        # will be zero.  Otherwise (after a short timeout)
-        # terminate() is used, resulting in an exitcode of -SIGTERM.
-        self.assertEqual(manager._process.exitcode, 0)
+        # bpo-30356: BaseManager._finalize_manager() sends SIGTERM
+        # to the manager process if it takes longer than 1 second to stop,
+        # which happens on slow buildbots.
+        self.assertIn(manager._process.exitcode, (0, -signal.SIGTERM))
 
     def test_mymanager_context(self):
         with MyManager() as manager:
             self.common(manager)
         # bpo-30356: BaseManager._finalize_manager() sends SIGTERM
-        # to the manager process if it takes longer than 1 second to stop.
+        # to the manager process if it takes longer than 1 second to stop,
+        # which happens on slow buildbots.
         self.assertIn(manager._process.exitcode, (0, -signal.SIGTERM))
 
     def test_mymanager_context_prestarted(self):
