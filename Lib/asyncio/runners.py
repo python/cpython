d69b7f927fbe229d391cb26f6355d05c22e36bb2
Kyle Stanley <aeros167@gmail.com>
2019-09-27 00:13:43 -0400

    Fix indent

--- Lib/asyncio/runners.py
+++ Lib/asyncio/runners.py
@@ -21,8 +21,8 @@ def run(main, *, debug=False):
     It should be used as a main entry point for asyncio programs, and should
     ideally only be called once.
 
-   Return the result of *coro* execution. If *coro* is not a :term:`coroutine`,
-   a :exc:`ValueError` is raised.
+    Return the result of *coro* execution. If *coro* is not a :term:`coroutine`,
+    a :exc:`ValueError` is raised.
 
     Example:
 
