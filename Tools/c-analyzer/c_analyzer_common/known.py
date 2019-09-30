9b475964c9d3b47bbb22c726a6be342cbb0f7fc6
Eric Snow <ericsnowcurrently@gmail.com>
2019-09-30 11:52:12 -0600

    Add known.look_up_variable().

--- Tools/c-analyzer/c_analyzer_common/known.py
+++ Tools/c-analyzer/c_analyzer_common/known.py
@@ -5,15 +5,16 @@ from . import DATA_DIR
 from .info import ID, UNKNOWN, Variable
 from .util import read_tsv
 
+# XXX need tests:
+# * from_file()
+# * look_up_variable()
+
 
 DATA_FILE = os.path.join(DATA_DIR, 'known.tsv')
 
 COLUMNS = ('filename', 'funcname', 'name', 'kind', 'declaration')
 HEADER = '\t'.join(COLUMNS)
 
-
-# XXX need tests:
-# * from_file()
 
 def from_file(infile, *,
               _read_tsv=read_tsv,
@@ -70,3 +71,43 @@ def _get_storage(decl):
         return 'extern'
     # implicit or local
     return None
+
+
+def look_up_variable(varid, knownvars, *,
+                     match_files=(lambda f1, f2: f1 == f2),
+                     ):
+    """Return the known variable matching the given ID.
+
+    "knownvars" is a mapping of ID to Variable.
+
+    "match_files" is used to verify if two filenames point to
+    the same file.
+
+    If no match is found then None is returned.
+    """
+    if not knownvars:
+        return None
+
+    if varid.funcname == UNKNOWN:
+        if not varid.filename or varid.filename == UNKNOWN:
+            for varid in knownvars:
+                if not varid.funcname:
+                    continue
+                if varid.name == varid.name:
+                    return knownvars[varid]
+            else:
+                return None
+        else:
+            for varid in knownvars:
+                if not varid.funcname:
+                    continue
+                if not match_files(varid.filename, varid.filename):
+                    continue
+                if varid.name == varid.name:
+                    return knownvars[varid]
+            else:
+                return None
+    elif not varid.filename or varid.filename == UNKNOWN:
+        raise NotImplementedError
+    else:
+        return knownvars.get(varid.id)
