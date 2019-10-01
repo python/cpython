950abeb0c0956eb67efecc651bb1ec571af6a30b
Victor Stinner <vstinner@redhat.com>
2019-10-01 10:56:37 +0200

    bpo-38304: Remove PyConfig.struct_size (GH-16500) (GH-16508)
    For now, we'll rely on the fact that the config structures aren't covered by the stable ABI.
    
    We may revisit this in the future if we further explore the idea of offering a stable embedding API.
    
    (cherry picked from commit bdace21b769998396d0ccc8da99a8ca9b507bfdf)

--- Python/pystate.c
+++ Python/pystate.c
@@ -61,11 +61,7 @@ _PyRuntimeState_Init_impl(_PyRuntimeState *runtime)
     _PyGC_Initialize(&runtime->gc);
     _PyEval_Initialize(&runtime->ceval);
 
-    runtime->preconfig.struct_size = sizeof(PyPreConfig);
-    PyStatus status = PyPreConfig_InitPythonConfig(&runtime->preconfig);
-    if (_PyStatus_EXCEPTION(status)) {
-        return status;
-    }
+    PyPreConfig_InitPythonConfig(&runtime->preconfig);
 
     runtime->gilstate.check_enabled = 1;
 
@@ -209,7 +205,6 @@ PyInterpreterState_New(void)
     memset(interp, 0, sizeof(*interp));
     interp->id_refcount = -1;
 
-    interp->config.struct_size = sizeof(PyConfig);
     PyStatus status = PyConfig_InitPythonConfig(&interp->config);
     if (_PyStatus_EXCEPTION(status)) {
         /* Don't report status to caller: PyConfig_InitPythonConfig()
