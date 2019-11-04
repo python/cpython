02e901f7d8103ad0b7de9defa338c860355abd60
Marco Paolini <mpaolini@users.noreply.github.com>
2019-11-04 17:07:34 +0000

    Update Modules/arraymodule.c
    Co-Authored-By: Brandt Bucher <brandtbucher@gmail.com>

--- Modules/arraymodule.c
+++ Modules/arraymodule.c
@@ -3047,7 +3047,8 @@ array_modexec(PyObject *m)
     if (PyModule_AddObject(m, "array", (PyObject *)&Arraytype) < 0) {
         Py_DECREF((PyObject *)&Arraytype);
         Py_CLEAR(m);
-      return -1;
+        return -1;
+    }
 
     for (descr=descriptors; descr->typecode != '\0'; descr++) {
         size++;
