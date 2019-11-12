b330207e28087b98ad2689ba754afa9578dad1f6
Raymond Hettinger <rhettinger@users.noreply.github.com>
2019-11-11 23:35:06 -0800

    bpo-38385: Fix iterator/iterable terminology in statistics docs (GH-17111) (cherry picked from commit 733b9a308e3c49855888e2e12397ae56d831e780)
    Co-authored-by: Raymond Hettinger <rhettinger@users.noreply.github.com>

--- Lib/statistics.py
+++ Lib/statistics.py
@@ -744,7 +744,7 @@ def variance(data, xbar=None):
 def pvariance(data, mu=None):
     """Return the population variance of ``data``.
 
-    data should be a sequence or iterator of Real-valued numbers, with at least one
+    data should be a sequence or iterable of Real-valued numbers, with at least one
     value. The optional argument mu, if given, should be the mean of
     the data. If it is missing or None, the mean is automatically calculated.
 
