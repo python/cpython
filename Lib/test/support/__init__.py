da9763c8a0b0c62385a22ab849a19dc504ba5033
Pablo Galindo <pablogsal@gmail.com>
2019-11-19 21:44:42 +0000

    fixup! fixup! Update Lib/test/support/__init__.py

--- Lib/test/support/__init__.py
+++ Lib/test/support/__init__.py
@@ -2072,7 +2072,7 @@ def _is_full_match_test(pattern):
     return ('.' in pattern) and (not re.search(r'[?*\[\]]', pattern))
 
 
- def set_match_tests(accept_patterns=None, ignore_patterns=None):
+def set_match_tests(accept_patterns=None, ignore_patterns=None):
     global _match_test_func, _accept_test_patterns, _ignore_test_patterns
 
 
