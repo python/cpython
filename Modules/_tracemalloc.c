2ec36427cffba647b135e3791f84b12bd5872d92
Brandt Bucher <brandtbucher@gmail.com>
2019-11-20 02:00:31 -0800

    bpo-38823: Fix refleak in _tracemalloc init error handling (GH-17235) (cherry picked from commit d51a363a4379385fdfe9c09a56324631465ede29)
    Co-authored-by: Brandt Bucher <brandtbucher@gmail.com>

--- Modules/_tracemalloc.c
+++ Modules/_tracemalloc.c
@@ -1655,8 +1655,10 @@ PyInit__tracemalloc(void)
     if (m == NULL)
         return NULL;
 
-    if (tracemalloc_init() < 0)
+    if (tracemalloc_init() < 0) {
+        Py_DECREF(m);
         return NULL;
+    }
 
     return m;
 }
