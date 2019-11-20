31399047e88e266ff9062a8d324dff644eda7d2a
Xavier de Gaye <xdegaye@gmail.com>
2019-11-20 17:43:27 +0100

    Fix "NameError: name 'SharedMemoryManager' is not defined"

--- Lib/multiprocessing/managers.py
+++ Lib/multiprocessing/managers.py
@@ -8,7 +8,7 @@
 # Licensed to PSF under a Contributor Agreement.
 #
 
-__all__ = [ 'BaseManager', 'SyncManager', 'BaseProxy', 'Token']
+__all__ = [ 'BaseManager', 'SyncManager', 'BaseProxy', 'Token' ]
 
 #
 # Imports
@@ -34,7 +34,7 @@ from . import get_context
 try:
     from . import shared_memory
     HAS_SHMEM = True
-    __all__.append(SharedMemoryManager)
+    __all__.append('SharedMemoryManager')
 except ImportError:
     HAS_SHMEM = False
 
