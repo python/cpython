python/cpython

commit b588dec6a80c7e113ef9fe7e22749914810f4810

Do nothing on __del__
We don't want to delete the underlying file explicitly as the expected
behaviour is for the file to be deleted *after* it falls out of scope.

Lib/tempfile.py
@@ -686,7 +686,10 @@ class SpooledTemporaryFile(_io.IOBase):
         return self._file.__iter__()
 
     def __del__(self):
-        return self._file.__del__()
+        """Do nothing. Underlying file will be deleted as a consequence
+        of falling out of scope, anyway, if nothing else is otherwise
+        deliberately referencing it."""
+        pass
 
     def close(self):
         self._file.close()

commit be094b38039b22c6145bee16ddf67358e44b7e8a

Add test for rolled file finalisation
The underlying file should be deleted when the SpooledTemporaryFile
object is deleted. This follows the same behaviour as TemporaryFile and
is consistent with the documentation. `__del__` should have the same
consequences as using `del` or allowing the object to fall out of scope;
an expectation which b588dec (reverted) broke somewhat.

Lib/test/test_tempfile.py
@@ -1016,6 +1016,21 @@ class TestSpooledTemporaryFile(BaseTestCase):
         finally:
             os.rmdir(dir)
 
+    def test_del_rolled_file(self):
+        # The rolled file should be deleted when the
+        # SpooledTemporaryFile object is deleted. This should raise a
+        # ResourceWarning since the file was not explicitly closed.
+        f = self.do_create(max_size=2)
+        f.write(b'foo')
+        filename = f.name
+        self.assertTrue(os.path.exists(filename))
+        with self.assertWarns(ResourceWarning):
+            f.__del__()
+        self.assertFalse(
+            os.path.exists(filename),
+            "Rolled SpooledTemporaryFile not deleted after __del__"
+        )
+
     def test_rewrite_small(self):
         # A SpooledTemporaryFile can be written to multiple within the max_size
         f = self.do_create(max_size=30)