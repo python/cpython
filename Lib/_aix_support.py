897a2851ee018f69fcf307c95d110302cccdb3ba
Michael Felt <michael@felt.demon.nl>
2019-11-30 16:11:36 +0000

    move all bootstrap issues into support module

--- Lib/_aix_support.py
+++ Lib/_aix_support.py
@@ -1,46 +1,36 @@
 """Shared AIX support functions."""
 
-import subprocess
 import sys
 from sysconfig import get_config_var
 
-"""
-AIX filesets are identified by four decimal values: V.R.M.F.
-V (version) and R (release) can be retreived using ``uname``
-M(aintenance) and F(ix) values are not available via uname
-For determining ABI compatibility the values V.R.M
-are essential - suplemented by a final value known as
-`builddate` that is expressed as YYWW (Year WeekofYear)
+# subprocess is not available early in the build process
+# if not available, the config_vars are also not available
+# supply substitutes to bootstrap the build
+try:
+    import subprocess
 
-The values for V.R.M of the system Python was built on are available in
-the variable sysconfig.get_config_var("BUILD_GNU_TYPE"). The system builddate
-value may be available in sys.config.get_config_var("AIX_BUILDDATE")
-When all of these values on a running system are greater or equal to the
-values of the build system then the bdist modules are binary compatible
-with the AIX kernel.
+    _subprocess_rdy = True
+    _tmp = str(get_config_var("AIX_BUILDDATE"))
+    _bgt = get_config_var("BUILD_GNU_TYPE")
+except ImportError:  # pragma: no cover
+    _subprocess_rdy = False
+    _tmp = "None"
+    _bgt = "powerpc-ibm-aix6.1.7.0"
 
-For pep425 purposes the AIX platform tag becomes:
-"AIX-{:1x}{:1d}{:02d}-{:04d}-{}".format(v, r, tl, builddate, bitsize)
-e.g., "AIX-6107-1415-32" for AIX 6.1 TL7 bd 1415, 32-bit
-and, "AIX-6107-1415-64" for AIX 6.1 TL7 bd 1415, 64-bit
-"""
 
-# _bd - set to impossible setting year98-week98 if None
-_tmp = str(get_config_var("AIX_BUILDDATE"))
+# if get_config_var("AIX_BUILDDATE") was unknown, provide a substitute,
+# impossible builddate to specify 'unknown'
 _bd = 9898 if (_tmp == "None") else int(_tmp)
-_bgt = get_config_var("BUILD_GNU_TYPE")
-_is_32bit = sys.maxsize == 2147483647
+_sz = 32 if sys.maxsize == 2147483647 else 64
 
 
-def _aix_tag(v, bd):
+def _aix_tag(vrtl, bd):
     # type: (List[int], int) -> str
-    # v is used as variable name so line below passes pep8 length test
-    # v[version, release, technology_level]
-    sz = 32 if _is_32bit else 64
-    return "AIX-{:1x}{:1d}{:02d}-{:04d}-{}".format(v[0], v[1], v[2], bd, sz)
+    # vrtl[version, release, technology_level]
+    return "AIX-{:1x}{:1d}{:02d}-{:04d}-{}".format(vrtl[0], vrtl[1], vrtl[2], bd, _sz)
 
 
-# compute vrtl from the VRMF string
+# extract version, release and technology level from a VRMF string
 def _aix_vrtl(vrmf):
     # type: (str) -> List[int]
     v, r, tl = vrmf.split(".")[:3]
@@ -50,19 +40,41 @@ def _aix_vrtl(vrmf):
 def _aix_bosmp64():
     # type: () -> Tuple[str, int]
     """
+    Return a Tuple[str, int] e.g., ['7.1.4.34', 1806]
     The fileset bos.mp64 is the AIX kernel. It's VRMF and builddate
-    reflect the current levels of the runtime environment.
+    reflect the current ABI levels of the runtime environment.
     """
-    out = subprocess.check_output(["/usr/bin/lslpp", "-Lqc", "bos.mp64"])
-    out = out.decode("utf-8").strip().split(":")  # type: ignore
-    # vrmf, bd = list(out[index] for index in [2, -1])  # type: ignore
-    # e.g., ['7.1.4.34', '1806']
-    # Use str() and int() to help mypy see types
-    return str(out[2]), int(out[-1])
+    if _subprocess_rdy:
+        out = subprocess.check_output(["/usr/bin/lslpp", "-Lqc", "bos.mp64"])
+        out = out.decode("utf-8").strip().split(":")  # type: ignore
+        # Use str() and int() to help mypy see types
+        return str(out[2]), int(out[-1])
+    else:
+        from os import uname
+
+        osname, host, release, version, machine = uname()
+        return "{}.{}.0.0".format(version, release), 9898
 
 
 def aix_platform():
     # type: () -> str
+    """
+    AIX filesets are identified by four decimal values: V.R.M.F.
+    V (version) and R (release) can be retreived using ``uname``
+    Since 2007, starting with AIX 5.3 TL7, the M value has been
+    included with the fileset bos.mp64 and represents the Technology
+    Level (TL) of AIX. The F (Fix) value also increases, but is not
+    relevant for comparing releases and binary compatibility.
+    For binary compatibility the so-called builddate is needed.
+    Again, the builddate of an AIX release is associated with bos.mp64.
+    AIX ABI compatibility is described  as guaranteed at: https://www.ibm.com/\
+    support/knowledgecenter/en/ssw_aix_72/install/binary_compatability.html
+
+    For pep425 purposes the AIX platform tag becomes:
+    "AIX-{:1x}{:1d}{:02d}-{:04d}-{}".format(v, r, tl, builddate, bitsize)
+    e.g., "AIX-6107-1415-32" for AIX 6.1 TL7 bd 1415, 32-bit
+    and, "AIX-6107-1415-64" for AIX 6.1 TL7 bd 1415, 64-bit
+    """
     vrmf, bd = _aix_bosmp64()
     return _aix_tag(_aix_vrtl(vrmf), bd)
 
@@ -71,9 +83,12 @@ def aix_platform():
 def _aix_bgt():
     # type: () -> List[int]
     assert _bgt
-    return _aix_vrtl(_bgt)
+    return _aix_vrtl(vrmf=_bgt)
 
 
 def aix_buildtag():
     # type: () -> str
+    """
+    Return the platform_tag of the system Python was built on.
+    """
     return _aix_tag(_aix_bgt(), _bd)
