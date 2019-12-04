5044c889dfced2f43e2cccb673d889a4882f6b3b
Miss Islington (bot) <31488909+miss-islington@users.noreply.github.com>
2019-12-04 12:29:22 -0800

    bpo-38965: Fix faulthandler._stack_overflow() on GCC 10 (GH-17467)
    Use the "volatile" keyword to prevent tail call optimization
    on any compiler, rather than relying on compiler specific pragma.
    (cherry picked from commit 8b787964e0a647caa0558b7c29ae501470d727d9)
    
    Co-authored-by: Victor Stinner <vstinner@python.org>

--- Modules/faulthandler.c
+++ Modules/faulthandler.c
@@ -1094,18 +1094,14 @@ faulthandler_fatal_error_py(PyObject *self, PyObject *args)
 #if defined(HAVE_SIGALTSTACK) && defined(HAVE_SIGACTION)
 #define FAULTHANDLER_STACK_OVERFLOW
 
-#ifdef __INTEL_COMPILER
-   /* Issue #23654: Turn off ICC's tail call optimization for the
-    * stack_overflow generator. ICC turns the recursive tail call into
-    * a loop. */
-#  pragma intel optimization_level 0
-#endif
-static
-uintptr_t
+static uintptr_t
 stack_overflow(uintptr_t min_sp, uintptr_t max_sp, size_t *depth)
 {
-    /* allocate 4096 bytes on the stack at each call */
-    unsigned char buffer[4096];
+    /* Allocate (at least) 4096 bytes on the stack at each call.
+
+       bpo-23654, bpo-38965: use volatile keyword to prevent tail call
+       optimization. */
+    volatile unsigned char buffer[4096];
     uintptr_t sp = (uintptr_t)&buffer;
     *depth += 1;
     if (sp < min_sp || max_sp < sp)
