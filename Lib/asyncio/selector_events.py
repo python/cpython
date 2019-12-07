f07314a28f011111b274c178fe6dbec9d62e4db9
idomic <michael.ido@gmail.com>
2019-12-07 06:52:36 -0500

    [3.8] bpo-37404: Raising value error if an SSLSocket is passed to asyncio functions (GH-16457)
    https://bugs.python.org/issue37404
    (cherry picked from commit 892f9e0777f262d366d4747a54c33a1c15a49da6)
    
    Co-authored-by: idomic <michael.ido@gmail.com>

--- Lib/asyncio/selector_events.py
+++ Lib/asyncio/selector_events.py
@@ -348,6 +348,8 @@ class BaseSelectorEventLoop(base_events.BaseEventLoop):
         The maximum amount of data to be received at once is specified by
         nbytes.
         """
+        if isinstance(sock, ssl.SSLSocket):
+            raise TypeError("Socket cannot be of type SSLSocket")
         if self._debug and sock.gettimeout() != 0:
             raise ValueError("the socket must be non-blocking")
         try:
@@ -386,6 +388,8 @@ class BaseSelectorEventLoop(base_events.BaseEventLoop):
         The received data is written into *buf* (a writable buffer).
         The return value is the number of bytes written.
         """
+        if isinstance(sock, ssl.SSLSocket):
+            raise TypeError("Socket cannot be of type SSLSocket")
         if self._debug and sock.gettimeout() != 0:
             raise ValueError("the socket must be non-blocking")
         try:
@@ -425,6 +429,8 @@ class BaseSelectorEventLoop(base_events.BaseEventLoop):
         raised, and there is no way to determine how much data, if any, was
         successfully processed by the receiving end of the connection.
         """
+        if isinstance(sock, ssl.SSLSocket):
+            raise TypeError("Socket cannot be of type SSLSocket")
         if self._debug and sock.gettimeout() != 0:
             raise ValueError("the socket must be non-blocking")
         try:
@@ -472,6 +478,8 @@ class BaseSelectorEventLoop(base_events.BaseEventLoop):
 
         This method is a coroutine.
         """
+        if isinstance(sock, ssl.SSLSocket):
+            raise TypeError("Socket cannot be of type SSLSocket")
         if self._debug and sock.gettimeout() != 0:
             raise ValueError("the socket must be non-blocking")
 
@@ -533,6 +541,8 @@ class BaseSelectorEventLoop(base_events.BaseEventLoop):
         object usable to send and receive data on the connection, and address
         is the address bound to the socket on the other end of the connection.
         """
+        if isinstance(sock, ssl.SSLSocket):
+            raise TypeError("Socket cannot be of type SSLSocket")
         if self._debug and sock.gettimeout() != 0:
             raise ValueError("the socket must be non-blocking")
         fut = self.create_future()
