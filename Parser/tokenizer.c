0ef5a7f0cf52e7e84f6ebde35014c8094c314baf
Miss Islington (bot) <31488909+miss-islington@users.noreply.github.com>
2019-12-08 20:56:19 -0800

    bpo-38673: dont switch to ps2 if the line starts with comment or whitespace (GH-17421)
    https://bugs.python.org/issue38673
    (cherry picked from commit 109fc2792a490ee5cd8a423e17d415fbdedec5c8)
    
    Co-authored-by: Batuhan Taşkaya <47358913+isidentical@users.noreply.github.com>
    (cherry picked from commit 184a3812b81e2f7d4bc6453bf7ceabe8ac590202)
    
    Co-authored-by: Miss Islington (bot) <31488909+miss-islington@users.noreply.github.com>

--- Parser/tokenizer.c
+++ Parser/tokenizer.c
@@ -1395,6 +1395,12 @@ tok_get(struct tok_state *tok, char **p_start, char **p_end)
             if (col == 0 && c == '\n' && tok->prompt != NULL) {
                 blankline = 0; /* Let it through */
             }
+            else if (tok->prompt != NULL && tok->lineno == 1) {
+                /* In interactive mode, if the first line contains
+                   only spaces and/or a comment, let it through. */
+                blankline = 0;
+                col = altcol = 0;
+            }
             else {
                 blankline = 1; /* Ignore completely */
             }
