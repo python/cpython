aa5b196c180d4c1dbe5a8593e55066e630d38c09
Benjamin Peterson <benjamin@python.org>
2020-01-02 19:10:06 -0800

    [2.7] Bring Python into the next decade. (GH-17805)
    (cherry picked from commit 946b29ea0b3b386ed05e87e60b8617c9dc19cd53)
    
    Co-authored-by: Benjamin Peterson <benjamin@python.org>

--- Python/getcopyright.c
+++ Python/getcopyright.c
@@ -4,7 +4,7 @@
 
 static char cprt[] = 
 "\
-Copyright (c) 2001-2019 Python Software Foundation.\n\
+Copyright (c) 2001-2020 Python Software Foundation.\n\
 All Rights Reserved.\n\
 \n\
 Copyright (c) 2000 BeOpen.com.\n\
