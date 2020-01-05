76b8fe1b420311334a1f884f69f815a8c281caec
Serhiy Storchaka <storchaka@gmail.com>
2020-01-05 14:15:50 +0200

    bpo-39055: Reject a trailing \n in base64.b64decode() with validate=True. (GH-17616)

--- Lib/base64.py
+++ Lib/base64.py
@@ -82,7 +82,7 @@ def b64decode(s, altchars=None, validate=False):
         altchars = _bytes_from_decode_data(altchars)
         assert len(altchars) == 2, repr(altchars)
         s = s.translate(bytes.maketrans(altchars, b'+/'))
-    if validate and not re.match(b'^[A-Za-z0-9+/]*={0,2}$', s):
+    if validate and not re.fullmatch(b'[A-Za-z0-9+/]*={0,2}', s):
         raise binascii.Error('Non-base64 digit found')
     return binascii.a2b_base64(s)
 
