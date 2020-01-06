277f1ecd03fc7697b98656856787ef5f84953a5c
Jason R. Coombs <jaraco@jaraco.com>
2020-01-05 22:32:19 -0500

    bpo-38907: In http.server script, restore binding to IPv4 on Windows. (GH-17851)

--- Lib/http/server.py
+++ Lib/http/server.py
@@ -1282,4 +1282,16 @@ if __name__ == '__main__':
     else:
         handler_class = partial(SimpleHTTPRequestHandler,
                                 directory=args.directory)
-    test(HandlerClass=handler_class, port=args.port, bind=args.bind)
+
+    # ensure dual-stack is not disabled; ref #38907
+    class DualStackServer(ThreadingHTTPServer):
+        def server_bind(self):
+            self.socket.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_V6ONLY, 0)
+            return super().server_bind()
+
+    test(
+        HandlerClass=handler_class,
+        ServerClass=DualStackServer,
+        port=args.port,
+        bind=args.bind,
+    )
