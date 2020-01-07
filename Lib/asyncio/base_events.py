4112a3da2e01ecc19e9f54b8ac7b383b6f5e85c8
Andrew Svetlov <andrew.svetlov@gmail.com>
2020-01-07 16:55:19 +0200

    [3.8] bpo-39191: Fix RuntimeWarning in asyncio test (GH-17863) (#17894)
    https://bugs.python.org/issue39191.
    (cherry picked from commit 10ac0cded26d91c3468e5e5a87cecad7fc0bcebd)
    
    Co-authored-by: Andrew Svetlov <andrew.svetlov@gmail.com>

--- Lib/asyncio/base_events.py
+++ Lib/asyncio/base_events.py
@@ -547,7 +547,7 @@ class BaseEventLoop(events.AbstractEventLoop):
                     'asyncgen': agen
                 })
 
-    def _check_runnung(self):
+    def _check_running(self):
         if self.is_running():
             raise RuntimeError('This event loop is already running')
         if events._get_running_loop() is not None:
@@ -557,7 +557,7 @@ class BaseEventLoop(events.AbstractEventLoop):
     def run_forever(self):
         """Run until stop() is called."""
         self._check_closed()
-        self._check_runnung()
+        self._check_running()
         self._set_coroutine_origin_tracking(self._debug)
         self._thread_id = threading.get_ident()
 
@@ -589,7 +589,7 @@ class BaseEventLoop(events.AbstractEventLoop):
         Return the Future's result, or raise its exception.
         """
         self._check_closed()
-        self._check_runnung()
+        self._check_running()
 
         new_task = not futures.isfuture(future)
         future = tasks.ensure_future(future, loop=self)
