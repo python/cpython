03e58231aa917fc209453bf4b19dfa3bad1d237f
Dan Arad <dan1994@gmail.com>
2020-01-11 11:32:13 +0200

    Refactored do_help method

--- Lib/cmd.py
+++ Lib/cmd.py
@@ -324,49 +324,60 @@ class Cmd:
 
     def do_help(self, arg):
         'List available commands with "help" or detailed help with "help cmd".'
-        if arg:
-            # XXX check arg syntax
-            try:
-                func = getattr(self, 'help_' + arg)
-            except AttributeError:
-                try:
-                    doc=getattr(self, 'do_' + arg).__doc__
-                    if doc:
-                        self.stdout.write("%s\n"%str(doc))
-                        return
-                except AttributeError:
-                    pass
-                self.stdout.write("%s\n"%str(self.nohelp % (arg,)))
-                return
-            func()
+        if len(arg) > 0:
+            self.print_cmd_help(arg)
         else:
-            names = self.get_names()
-            cmds_doc = []
-            cmds_undoc = []
-            help = {}
-            for name in names:
-                if name[:5] == 'help_':
-                    help[name[5:]]=1
-            names.sort()
-            # There can be duplicates if routines overridden
-            prevname = ''
-            for name in names:
-                if name[:3] == 'do_':
-                    if name == prevname:
-                        continue
-                    prevname = name
-                    cmd=name[3:]
-                    if cmd in help:
-                        cmds_doc.append(cmd)
-                        del help[cmd]
-                    elif getattr(self, name).__doc__:
-                        cmds_doc.append(cmd)
-                    else:
-                        cmds_undoc.append(cmd)
-            self.stdout.write("%s\n"%str(self.doc_leader))
-            self.print_topics(self.doc_header,   cmds_doc,   15,80)
-            self.print_topics(self.misc_header,  list(help.keys()),15,80)
-            self.print_topics(self.undoc_header, cmds_undoc, 15,80)
+            self.print_general_help()
+
+    def print_cmd_help(self, cmd):
+        try:
+            self.invoke_cmd_help_method(cmd)
+        except AttributeError:
+            try:
+                self.print_cmd_function_docstring(cmd)
+            except AttributeError:
+                self.print_default_cmd_help(cmd)
+
+    def invoke_cmd_help_method(self, cmd):
+        func = getattr(self, 'help_' + cmd)
+        func()
+
+    def print_cmd_function_docstring(self, cmd):
+        doc = getattr(self, 'do_' + cmd).__doc__
+        if doc:
+            self.stdout.write("%s\n"%str(doc))
+
+    def print_default_cmd_help(self, cmd):
+        self.stdout.write("%s\n"%str(self.nohelp % (cmd,)))
+
+    def print_general_help(self):
+        names = self.get_names()
+        cmds_doc = []
+        cmds_undoc = []
+        help = {}
+        for name in names:
+            if name[:5] == 'help_':
+                help[name[5:]]=1
+        names.sort()
+        # There can be duplicates if routines overridden
+        prevname = ''
+        for name in names:
+            if name[:3] == 'do_':
+                if name == prevname:
+                    continue
+                prevname = name
+                cmd=name[3:]
+                if cmd in help:
+                    cmds_doc.append(cmd)
+                    del help[cmd]
+                elif getattr(self, name).__doc__:
+                    cmds_doc.append(cmd)
+                else:
+                    cmds_undoc.append(cmd)
+        self.stdout.write("%s\n"%str(self.doc_leader))
+        self.print_topics(self.doc_header,   cmds_doc,   15,80)
+        self.print_topics(self.misc_header,  list(help.keys()),15,80)
+        self.print_topics(self.undoc_header, cmds_undoc, 15,80)
 
     def print_topics(self, header, cmds, cmdlen, maxcol):
         if cmds:
