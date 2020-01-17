e1255dd3724d949b58c6d45a14e45e5048f5291c
Pablo Galindo <pablogsal@gmail.com>
2020-01-17 03:37:38 +0000

    Add another test for the cycle-find algorithm

--- Lib/test/test_functools.py
+++ Lib/test/test_functools.py
@@ -1228,6 +1228,8 @@ class TestTopologicalSort(unittest.TestCase):
         # Multiple cycles
         self._assert_cycle({1: {2}, 2: {1}, 3: {4}, 4: {5}, 6: {7}, 7: {6}},
                            [1, 2, 1])
+        # Cycle in the middle of the graph
+        self._assert_cycle({1: {2}, 2: {3}, 3: {2, 4}, 4: {5}}, [3, 2])
 
     def test_calls_before_preapare(self):
         ts = functools.TopologicalSorter()
