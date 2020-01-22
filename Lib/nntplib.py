e97ebb2d99d3d982d30f70208a085b2d9921f52f
Dong-hee Na <donghee.na92@gmail.com>
2020-01-23 06:59:43 +0900

    bpo-39366: Remove xpath() and xgtitle() methods of NNTP (GH-18035)

--- Lib/nntplib.py
+++ Lib/nntplib.py
@@ -67,7 +67,6 @@ import re
 import socket
 import collections
 import datetime
-import warnings
 import sys
 
 try:
@@ -833,44 +832,6 @@ class _NNTPBase:
         resp, lines = self._longcmdstring(cmd, file)
         fmt = self._getoverviewfmt()
         return resp, _parse_overview(lines, fmt)
-
-    def xgtitle(self, group, *, file=None):
-        """Process an XGTITLE command (optional server extension) Arguments:
-        - group: group name wildcard (i.e. news.*)
-        Returns:
-        - resp: server response if successful
-        - list: list of (name,title) strings"""
-        warnings.warn("The XGTITLE extension is not actively used, "
-                      "use descriptions() instead",
-                      DeprecationWarning, 2)
-        line_pat = re.compile('^([^ \t]+)[ \t]+(.*)$')
-        resp, raw_lines = self._longcmdstring('XGTITLE ' + group, file)
-        lines = []
-        for raw_line in raw_lines:
-            match = line_pat.search(raw_line.strip())
-            if match:
-                lines.append(match.group(1, 2))
-        return resp, lines
-
-    def xpath(self, id):
-        """Process an XPATH command (optional server extension) Arguments:
-        - id: Message id of article
-        Returns:
-        resp: server response if successful
-        path: directory path to article
-        """
-        warnings.warn("The XPATH extension is not actively used",
-                      DeprecationWarning, 2)
-
-        resp = self._shortcmd('XPATH {0}'.format(id))
-        if not resp.startswith('223'):
-            raise NNTPReplyError(resp)
-        try:
-            [resp_num, path] = resp.split()
-        except ValueError:
-            raise NNTPReplyError(resp) from None
-        else:
-            return resp, path
 
     def date(self):
         """Process the DATE command.
