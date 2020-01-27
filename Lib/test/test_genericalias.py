447fbe9308de63b15d99c2c3e6c6c5b960eeb801
Guido van Rossum <guido@python.org>
2020-01-27 10:27:04 -0800

    PEP 585 step 4: Write some tests

---
+++ Lib/test/test_genericalias.py
@@ -0,0 +1,24 @@
+"""Tests for C-implemented GenericAlias."""
+
+import unittest
+
+
+class BaseTest(unittest.TestCase):
+    """Test basics."""
+
+    def test_subscriptable(self):
+        for t in tuple, list, dict, set, frozenset:
+            tname = t.__name__
+            with self.subTest(f"Testing {tname}"):
+                self.assertEqual(t[int], t)
+
+    def test_unsubscriptable(self):
+        for t in int, str, float:
+            tname = t.__name__
+            with self.subTest(f"Testing {tname}"):
+                with self.assertRaises(TypeError):
+                    t[int]
+
+
+if __name__ == "__main__":
+    unittest.main()
