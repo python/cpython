fb3e8d389394660b35673d084c056b58fde2e0bf
Guido van Rossum <guido@python.org>
2020-01-27 16:51:58 -0800

    More tests

--- Lib/test/test_genericalias.py
+++ Lib/test/test_genericalias.py
@@ -10,8 +10,9 @@ class BaseTest(unittest.TestCase):
         for t in tuple, list, dict, set, frozenset:
             tname = t.__name__
             with self.subTest(f"Testing {tname}"):
-                t[int]
-                ## self.assertEqual(t[int], t)
+                alias = t[int]
+                self.assertIs(alias.__origin__, t)
+                self.assertEqual(alias.__parameters__, (int,))
 
     def test_unsubscriptable(self):
         for t in int, str, float:
@@ -19,6 +20,27 @@ class BaseTest(unittest.TestCase):
             with self.subTest(f"Testing {tname}"):
                 with self.assertRaises(TypeError):
                     t[int]
+
+    def test_instantiate(self):
+        for t in tuple, list, dict, set, frozenset:
+            tname = t.__name__
+            with self.subTest(f"Testing {tname}"):
+                alias = t[int]
+                self.assertEqual(alias(), t())
+                if t is dict:
+                    self.assertEqual(alias(iter([('a', 1), ('b', 2)])), dict(a=1, b=2))
+                    self.assertEqual(alias(a=1, b=2), dict(a=1, b=2))
+                else:
+                    self.assertEqual(alias(iter((1, 2, 3))), t((1, 2, 3)))
+
+    def test_unbound_methods(self):
+        t = list[int]
+        a = t()
+        t.append(a, 'foo')
+        self.assertEqual(a, ['foo'])
+        x = t.__getitem__(a, 0)
+        self.assertEqual(x, 'foo')
+        self.assertEqual(t.__len__(a), 1)
 
 
 if __name__ == "__main__":
