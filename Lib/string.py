52d8edb8409292f5687dd51908131d94e70ab9f2
Sergey Zakharov <mrsergeycity@gmail.com>
2020-01-29 11:41:11 +0300

    Add symbol b'\xe2\x80\x93' to punctuation symbols
    This symbol looks very similar to b'-', and isn't matched when using string.punctuation as reference

--- Lib/string.py
+++ Lib/string.py
@@ -28,7 +28,7 @@ ascii_letters = ascii_lowercase + ascii_uppercase
 digits = '0123456789'
 hexdigits = digits + 'abcdef' + 'ABCDEF'
 octdigits = '01234567'
-punctuation = r"""!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"""
+punctuation = r"""!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~–"""
 printable = digits + ascii_letters + punctuation + whitespace
 
 # Functions which aren't available as string methods.
