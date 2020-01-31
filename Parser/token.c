c5c884220ba023acc09b7e228452e5e63998b24d
Hai Shi <shihai1992@gmail.com>
2020-01-31 22:25:39 +0800

    compiler dosen't support in(de)crement operation

--- Parser/token.c
+++ Parser/token.c
@@ -132,11 +132,13 @@ PyToken_TwoChars(int c1, int c2)
         break;
     case '+':
         switch (c2) {
+        case '+': return ERRORTOKEN;
         case '=': return PLUSEQUAL;
         }
         break;
     case '-':
         switch (c2) {
+        case '-': return ERRORTOKEN;
         case '=': return MINEQUAL;
         case '>': return RARROW;
         }
