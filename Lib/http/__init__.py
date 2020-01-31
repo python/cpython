074eba9add1daadce34714d5d708f92aa6d1920d
Dong-hee Na <donghee.na92@gmail.com>
2020-01-31 23:59:59 +0900

    bpo-39509: Update HTTP status code to follow IANA

--- Lib/http/__init__.py
+++ Lib/http/__init__.py
@@ -31,6 +31,7 @@ class HTTPStatus(IntEnum):
     SWITCHING_PROTOCOLS = (101, 'Switching Protocols',
             'Switching to new protocol; obey Upgrade header')
     PROCESSING = 102, 'Processing'
+    EARLY_HINTS = 103, 'Early Hints'
 
     # success
     OK = 200, 'OK', 'Request fulfilled, document follows'
@@ -105,6 +106,7 @@ class HTTPStatus(IntEnum):
     UNPROCESSABLE_ENTITY = 422, 'Unprocessable Entity'
     LOCKED = 423, 'Locked'
     FAILED_DEPENDENCY = 424, 'Failed Dependency'
+    TOO_EARLY = 425, 'Too Early'
     UPGRADE_REQUIRED = 426, 'Upgrade Required'
     PRECONDITION_REQUIRED = (428, 'Precondition Required',
         'The origin server requires the request to be conditional')
