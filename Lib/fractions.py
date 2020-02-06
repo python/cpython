1eb17cdb99c38932bc0f5ad96c113ab7caf0cd4e
Sebastian Berg <sebastian@sipsolutions.net>
2020-02-06 06:54:05 -0800

    bpo-39274: Ensure Fraction.__bool__() returns a bool (GH-18017)
    Some numerator types used (specifically NumPy) decides to not
    return a Python boolean for the "a != b" operation. Using the equivalent
    call to bool() guarantees a bool return also for such types.

--- Lib/fractions.py
+++ Lib/fractions.py
@@ -625,7 +625,9 @@ class Fraction(numbers.Rational):
 
     def __bool__(a):
         """a != 0"""
-        return a._numerator != 0
+        # bpo-39274: Use bool() because (a._numerator != 0) can return an
+        # object which is not a bool.
+        return bool(a._numerator)
 
     # support for pickling, copy, and deepcopy
 
