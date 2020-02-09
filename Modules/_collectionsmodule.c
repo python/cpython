66115dd6e963910153f9c701327f90edbeea2bd3
sweeneyde <36520290+sweeneyde@users.noreply.github.com>
2020-02-09 03:16:43 -0500

    bpo-39590: make deque.__contains__ and deque.count hold strong references (GH-18421) (cherry picked from commit c6dedde160a9fce5d049e860f586ad8f93aec822)
    Co-authored-by: sweeneyde <36520290+sweeneyde@users.noreply.github.com>

--- Modules/_collectionsmodule.c
+++ Modules/_collectionsmodule.c
@@ -966,7 +966,9 @@ deque_count(dequeobject *deque, PyObject *v)
     while (--n >= 0) {
         CHECK_NOT_END(b);
         item = b->data[index];
+        Py_INCREF(item);
         cmp = PyObject_RichCompareBool(item, v, Py_EQ);
+        Py_DECREF(item);
         if (cmp < 0)
             return NULL;
         count += cmp;
@@ -1003,7 +1005,9 @@ deque_contains(dequeobject *deque, PyObject *v)
     while (--n >= 0) {
         CHECK_NOT_END(b);
         item = b->data[index];
+        Py_INCREF(item);
         cmp = PyObject_RichCompareBool(item, v, Py_EQ);
+        Py_DECREF(item);
         if (cmp) {
             return cmp;
         }
