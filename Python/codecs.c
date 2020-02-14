7386a70746cf9aaf2d95db75d9201fb124f085df
Andy Lester <andy@petdance.com>
2020-02-13 22:42:56 -0600

    closes bpo-39630: Update pointers to string literals to be const char *. (GH-18510)

--- Python/codecs.c
+++ Python/codecs.c
@@ -1407,7 +1407,7 @@ static PyObject *surrogateescape_errors(PyObject *self, PyObject *exc)
 static int _PyCodecRegistry_Init(void)
 {
     static struct {
-        char *name;
+        const char *name;
         PyMethodDef def;
     } methods[] =
     {
