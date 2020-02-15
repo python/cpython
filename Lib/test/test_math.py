3074e34fcb5f8524046c3e6e67b3a3b267127e82
ananthan-123 <60189117+ananthan-123@users.noreply.github.com>
2020-02-15 18:34:08 +0530

    Update test_math.py

--- Lib/test/test_math.py
+++ Lib/test/test_math.py
@@ -976,6 +976,7 @@ class MathTests(unittest.TestCase):
 
     def test_lcm(self):
         lcm = math.lcm
+        self.assertEqual(lcm(0, 0), 0)
         self.assertEqual(lcm(1, 0), 0)
         self.assertEqual(lcm(-1, 0), 0)
         self.assertEqual(lcm(0, 1), 0)
@@ -1004,8 +1005,6 @@ class MathTests(unittest.TestCase):
             self.assertEqual(lcm(-b, a), d)
             self.assertEqual(lcm(-a, -b), d)
             self.assertEqual(lcm(-b, -a), d)
-        self.assertRaises(TypeError, lcm, 120.0, 84)
-        self.assertRaises(TypeError, lcm, 120, 84.0)
 
     def testLdexp(self):
         self.assertRaises(TypeError, math.ldexp)
