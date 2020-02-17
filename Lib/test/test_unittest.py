d89915f95269c22cbedd88f349b4a5b67a4b1763
Ido Michael <michael.ido@gmail.com>
2020-02-16 20:01:05 -0500

    [bpo-10572] Fixed hardcoded Lib.test.unittest_test and lib2to3 path

--- Lib/test/test_unittest.py
+++ Lib/test/test_unittest.py
@@ -1,15 +1,15 @@
-import Lib.test.unittest_test as unittest
+import Lib.test.unittest_test as unittest_test
 from test import support
 
 
 def test_main():
     # used by regrtest
-    support.run_unittest(unittest.suite())
+    support.run_unittest(unittest_test.suite())
     support.reap_children()
 
 def load_tests(*_):
     # used by unittest
-    return unittest.suite()
+    return unittest_test.suite()
 
 if __name__ == "__main__":
     test_main()
