2daa758d26998f232a5e74a9d18565985a16ffe5
Pete Wicken <2273100+JamoBox@users.noreply.github.com>
2020-03-09 22:33:45 +0000

    [3.6] bpo-28577: Special case added to IP v4 and v6 hosts for /32 and /128 networks (GH-18757)
    The `.hosts()` method now returns the single address present in a /32 or /128 network..
    (cherry picked from commit 8e9c47a947954c997d4b725f4551d50a1d896722)
    
    Co-authored-by: Pete Wicken <2273100+JamoBox@users.noreply.github.com>

--- Lib/ipaddress.py
+++ Lib/ipaddress.py
@@ -1523,6 +1523,8 @@ class IPv4Network(_BaseV4, _BaseNetwork):
 
         if self._prefixlen == (self._max_prefixlen - 1):
             self.hosts = self.__iter__
+        elif self._prefixlen == (self._max_prefixlen):
+            self.hosts = lambda: [IPv4Address(addr)]
 
     @property
     @functools.lru_cache()
@@ -2201,6 +2203,8 @@ class IPv6Network(_BaseV6, _BaseNetwork):
 
         if self._prefixlen == (self._max_prefixlen - 1):
             self.hosts = self.__iter__
+        elif self._prefixlen == self._max_prefixlen:
+            self.hosts = lambda: [IPv6Address(addr)]
 
     def hosts(self):
         """Generate Iterator over usable hosts in a network.
