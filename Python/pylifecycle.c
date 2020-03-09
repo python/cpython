9ad58acbe8b90b4d0f2d2e139e38bb5aa32b7fb6
Victor Stinner <vstinner@python.org>
2020-03-09 23:37:49 +0100

    bpo-19466: Py_Finalize() clears daemon threads earlier (GH-18848)
    Clear the frames of daemon threads earlier during the Python shutdown to
    call objects destructors. So "unclosed file" resource warnings are now
    emitted for daemon threads in a more reliable way.
    
    Cleanup _PyThreadState_DeleteExcept() code: rename "garbage" to
    "list".

--- Python/pylifecycle.c
+++ Python/pylifecycle.c
@@ -1373,6 +1373,16 @@ Py_FinalizeEx(void)
     runtime->initialized = 0;
     runtime->core_initialized = 0;
 
+    /* Destroy the state of all threads of the interpreter, except of the
+       current thread. In practice, only daemon threads should still be alive,
+       except if wait_for_thread_shutdown() has been cancelled by CTRL+C.
+       Clear frames of other threads to call objects destructors. Destructors
+       will be called in the current Python thread. Since
+       _PyRuntimeState_SetFinalizing() has been called, no other Python thread
+       can take the GIL at this point: if they try, they will exit
+       immediately. */
+    _PyThreadState_DeleteExcept(runtime, tstate);
+
     /* Flush sys.stdout and sys.stderr */
     if (flush_std_files() < 0) {
         status = -1;
