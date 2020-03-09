448a7d80dc9eb8aed6fb5fe937e92c039198a664
Pablo Galindo <Pablogsal@gmail.com>
2020-03-09 13:48:01 +0000

    [3.7] [3.8] bpo-39850: Add support for abstract sockets in multiprocessing (GH-18866) (cherry picked from commit 6012f30beff7fa8396718dfb198ccafc333c565b)
    Co-authored-by: Pablo Galindo <Pablogsal@gmail.com>.
    (cherry picked from commit 5e217bb454e3e5a866ad7602387b58b49cda0f1c)
    
    Co-authored-by: Pablo Galindo <Pablogsal@gmail.com>

--- Lib/multiprocessing/forkserver.py
+++ Lib/multiprocessing/forkserver.py
@@ -116,7 +116,8 @@ class ForkServer(object):
             with socket.socket(socket.AF_UNIX) as listener:
                 address = connection.arbitrary_address('AF_UNIX')
                 listener.bind(address)
-                os.chmod(address, 0o600)
+                if not util.is_abstract_socket_namespace(address):
+                    os.chmod(address, 0o600)
                 listener.listen()
 
                 # all client processes own the write end of the "alive" pipe;
