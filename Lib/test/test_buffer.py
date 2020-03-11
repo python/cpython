92b0e84c0d1a7f1a1db700621fbeb7ff00283804
Petr Viktorin <pviktori@redhat.com>
2020-03-11 16:27:10 +0100

    test_buffer: Use subTest() to help debug a failure

--- Lib/test/test_buffer.py
+++ Lib/test/test_buffer.py
@@ -2754,54 +2754,56 @@ class TestBufferProtocol(unittest.TestCase):
         # be 1D, at least one format must be 'c', 'b' or 'B'.
         for _tshape in gencastshapes():
             for char in fmtdict['@']:
-                tfmt = ('', '@')[randrange(2)] + char
-                tsize = struct.calcsize(tfmt)
-                n = prod(_tshape) * tsize
-                obj = 'memoryview' if is_byte_format(tfmt) else 'bytefmt'
-                for fmt, items, _ in iter_format(n, obj):
-                    size = struct.calcsize(fmt)
-                    shape = [n] if n > 0 else []
-                    tshape = _tshape + [size]
+                with self.subTest(_tshape=_tshape, char=char):
+                    tfmt = ('', '@')[randrange(2)] + char
+                    tsize = struct.calcsize(tfmt)
+                    n = prod(_tshape) * tsize
+                    obj = 'memoryview' if is_byte_format(tfmt) else 'bytefmt'
+                    for fmt, items, _ in iter_format(n, obj):
+                        size = struct.calcsize(fmt)
+                        shape = [n] if n > 0 else []
+                        tshape = _tshape + [size]
 
-                    ex = ndarray(items, shape=shape, format=fmt)
-                    m = memoryview(ex)
+                        ex = ndarray(items, shape=shape, format=fmt)
+                        m = memoryview(ex)
 
-                    titems, tshape = cast_items(ex, tfmt, tsize, shape=tshape)
+                        titems, tshape = cast_items(ex, tfmt, tsize,
+                                                    shape=tshape)
 
-                    if titems is None:
-                        self.assertRaises(TypeError, m.cast, tfmt, tshape)
-                        continue
-                    if titems == 'nan':
-                        continue # NaNs in lists are a recipe for trouble.
+                        if titems is None:
+                            self.assertRaises(TypeError, m.cast, tfmt, tshape)
+                            continue
+                        if titems == 'nan':
+                            continue # NaNs in lists are a recipe for trouble.
 
-                    # 1D -> ND
-                    nd = ndarray(titems, shape=tshape, format=tfmt)
+                        # 1D -> ND
+                        nd = ndarray(titems, shape=tshape, format=tfmt)
 
-                    m2 = m.cast(tfmt, shape=tshape)
-                    ndim = len(tshape)
-                    strides = nd.strides
-                    lst = nd.tolist()
-                    self.verify(m2, obj=ex,
-                                itemsize=tsize, fmt=tfmt, readonly=True,
-                                ndim=ndim, shape=tshape, strides=strides,
-                                lst=lst, cast=True)
+                        m2 = m.cast(tfmt, shape=tshape)
+                        ndim = len(tshape)
+                        strides = nd.strides
+                        lst = nd.tolist()
+                        self.verify(m2, obj=ex,
+                                    itemsize=tsize, fmt=tfmt, readonly=True,
+                                    ndim=ndim, shape=tshape, strides=strides,
+                                    lst=lst, cast=True)
 
-                    # ND -> 1D
-                    m3 = m2.cast(fmt)
-                    m4 = m2.cast(fmt, shape=shape)
-                    ndim = len(shape)
-                    strides = ex.strides
-                    lst = ex.tolist()
+                        # ND -> 1D
+                        m3 = m2.cast(fmt)
+                        m4 = m2.cast(fmt, shape=shape)
+                        ndim = len(shape)
+                        strides = ex.strides
+                        lst = ex.tolist()
 
-                    self.verify(m3, obj=ex,
-                                itemsize=size, fmt=fmt, readonly=True,
-                                ndim=ndim, shape=shape, strides=strides,
-                                lst=lst, cast=True)
+                        self.verify(m3, obj=ex,
+                                    itemsize=size, fmt=fmt, readonly=True,
+                                    ndim=ndim, shape=shape, strides=strides,
+                                    lst=lst, cast=True)
 
-                    self.verify(m4, obj=ex,
-                                itemsize=size, fmt=fmt, readonly=True,
-                                ndim=ndim, shape=shape, strides=strides,
-                                lst=lst, cast=True)
+                        self.verify(m4, obj=ex,
+                                    itemsize=size, fmt=fmt, readonly=True,
+                                    ndim=ndim, shape=shape, strides=strides,
+                                    lst=lst, cast=True)
 
         if ctypes:
             # format: "T{>l:x:>d:y:}"
