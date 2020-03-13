fb667fad848c7102a92dae959c79a995314473dc
Victor Stinner <vstinner@python.org>
2020-03-13 17:53:06 +0100

    Fix typo

--- Include/pystate.h
+++ Include/pystate.h
@@ -28,7 +28,7 @@ PyAPI_FUNC(PyInterpreterState *) PyInterpreterState_New(void);
 PyAPI_FUNC(void) PyInterpreterState_Clear(PyInterpreterState *);
 PyAPI_FUNC(void) PyInterpreterState_Delete(PyInterpreterState *);
 
-#if !defined(Py_LIMITED_API) || Py_LIMITED_API+0 >= 0x03080000
+#if !defined(Py_LIMITED_API) || Py_LIMITED_API+0 >= 0x03090000
 /* New in 3.9 */
 /* Get the current interpreter state.
 
