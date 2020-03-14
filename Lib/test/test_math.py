4dcfe5f2e4bad8e03990de75d8d4ec20e8aa23b3
Mark Dickinson <mdickinson@enthought.com>
2020-03-14 11:51:53 +0000

    [3.7] bpo-39871: Fix possible SystemError in atan2, copysign and remainder (GH-18806) (GH-18990)
    In math_2(), the first PyFloat_AsDouble() call should be checked
    for failure before the second call.
    
    Co-authored-by: Mark Dickinson <dickinsm@gmail.com>.
    (cherry picked from commit 5208b4b37953a406db0ed6a9db545c2948dde989)
    
    Co-authored-by: Zackery Spytz <zspytz@gmail.com>

--- Lib/test/test_math.py
+++ Lib/test/test_math.py
@@ -1415,6 +1415,22 @@ class MathTests(unittest.TestCase):
             self.fail('Failures in test_mtestfile:\n  ' +
                       '\n  '.join(failures))
 
+    def test_issue39871(self):
+        # A SystemError should not be raised if the first arg to atan2(),
+        # copysign(), or remainder() cannot be converted to a float.
+        class F:
+            def __float__(self):
+                self.converted = True
+                1/0
+        for func in math.atan2, math.copysign, math.remainder:
+            y = F()
+            with self.assertRaises(TypeError):
+                func("not a number", y)
+
+            # There should not have been any attempt to convert the second
+            # argument to a float.
+            self.assertFalse(getattr(y, "converted", False))
+
     # Custom assertions.
 
     def assertIsNaN(self, value):
