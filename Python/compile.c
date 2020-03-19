26b3f32c0f2c595943f00aad589fa8d131bb9dc6
Batuhan Taşkaya <47358913+isidentical@users.noreply.github.com>
2020-03-19 14:35:44 +0300

    bpo-39562: Allow executing asynchronous comprehensions in the asyncio REPL (GH-18968)
    Co-authored-by: Pablo Galindo <Pablogsal@gmail.com>

--- Python/compile.c
+++ Python/compile.c
@@ -4520,11 +4520,14 @@ compiler_comprehension(struct compiler *c, expr_ty e, int type,
     PyCodeObject *co = NULL;
     comprehension_ty outermost;
     PyObject *qualname = NULL;
-    int is_async_function = c->u->u_ste->ste_coroutine;
     int is_async_generator = 0;
 
-    outermost = (comprehension_ty) asdl_seq_GET(generators, 0);
+    if (IS_TOP_LEVEL_AWAIT(c)) {
+        c->u->u_ste->ste_coroutine = 1;
+    }
+    int is_async_function = c->u->u_ste->ste_coroutine;
 
+    outermost = (comprehension_ty) asdl_seq_GET(generators, 0);
     if (!compiler_enter_scope(c, name, COMPILER_SCOPE_COMPREHENSION,
                               (void *)e, e->lineno))
     {
