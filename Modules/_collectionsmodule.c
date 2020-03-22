ad96fa7fdd27abf7872f16994a70cf15770818d7
Dong-hee Na <donghee.na92@gmail.com>
2020-03-23 01:17:34 +0900

    bpo-40024: Add PyModule_AddType() helper function (GH-19088)

--- Modules/_collectionsmodule.c
+++ Modules/_collectionsmodule.c
@@ -2565,21 +2565,13 @@ collections_exec(PyObject *module) {
         &PyODict_Type,
         &dequeiter_type,
         &dequereviter_type,
-        &tuplegetter_type,
-        NULL,
+        &tuplegetter_type
     };
 
     defdict_type.tp_base = &PyDict_Type;
 
-    for (int i = 0; typelist[i] != NULL; i++) {
-        PyTypeObject *type = typelist[i];
-        if (PyType_Ready(type) < 0) {
-            return -1;
-        }
-        const char *name = _PyType_Name(type);
-        Py_INCREF(type);
-        if (PyModule_AddObject(module, name, (PyObject *)type) < 0) {
-            Py_DECREF(type);
+    for (size_t i = 0; i < Py_ARRAY_LENGTH(typelist); i++) {
+        if (PyModule_AddType(module, typelist[i]) < 0) {
             return -1;
         }
     }
