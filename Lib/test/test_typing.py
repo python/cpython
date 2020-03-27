19ef00370134f1c6dd52ebaab1ff145981610e53
furkanonder <furkantahaonder@gmail.com>
2020-03-27 16:22:13 +0300

    bpo-40086: remove test_etree test case

--- Lib/test/test_typing.py
+++ Lib/test/test_typing.py
@@ -358,20 +358,6 @@ class UnionTests(BaseTestCase):
         A = Union[str, Pattern]
         A
 
-    def test_etree(self):
-        # See https://github.com/python/typing/issues/229
-        # (Only relevant for Python 2.)
-        try:
-            from xml.etree.cElementTree import Element
-        except ImportError:
-            raise SkipTest("cElementTree not found")
-        Union[Element, str]  # Shouldn't crash
-
-        def Elem(*args):
-            return Element(*args)
-
-        Union[Elem, str]  # Nor should this
-
 
 class TupleTests(BaseTestCase):
 
