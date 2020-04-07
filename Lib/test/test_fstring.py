0b1cba050685a9dab9a22cc16437697a3554eea0
yangyang <yangyang@xiachufang.com>
2020-04-07 13:59:26 +0800

    make test_fstring accord with fixed col_offsets

--- Lib/test/test_fstring.py
+++ Lib/test/test_fstring.py
@@ -324,8 +324,8 @@ non-important content
         self.assertEqual(binop.lineno, 4)
         self.assertEqual(binop.left.lineno, 4)
         self.assertEqual(binop.right.lineno, 6)
-        self.assertEqual(binop.col_offset, 4)
-        self.assertEqual(binop.left.col_offset, 4)
+        self.assertEqual(binop.col_offset, 3)
+        self.assertEqual(binop.left.col_offset, 3)
         self.assertEqual(binop.right.col_offset, 7)
 
     def test_docstring(self):
