9c15d1fb73c026a52823cc56e2c8ce43d61aa632
Rémi Lapeyre <remi.lapeyre@lenstra.fr>
2020-04-08 15:22:11 +0200

    Check message in tests

--- Lib/test/test_ast.py
+++ Lib/test/test_ast.py
@@ -378,8 +378,10 @@ class AST_Tests(unittest.TestCase):
 
         self.assertRaises(TypeError, ast.Num, 1, None, 2)
         self.assertRaises(TypeError, ast.Num, 1, None, 2, lineno=0)
-        self.assertRaises(TypeError, ast.Num, 1, n=2)
-        self.assertRaises(TypeError, ast.Constant, 1, value=2)
+        with self.assertRaisesRegex(TypeError, "Num got multiple values for argument 'n'"):
+            ast.Num(1, n=2)
+        with self.assertRaisesRegex(TypeError, "Constant got multiple values for argument 'value'"):
+            ast.Constant(1, value=2)
 
         self.assertEqual(ast.Num(42).n, 42)
         self.assertEqual(ast.Num(4.25).n, 4.25)
