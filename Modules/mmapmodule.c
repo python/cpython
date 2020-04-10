7c4185d62d4aec486d82c3ad02acd878db2d3537
Ethan Smith <ethan@ethanhs.me>
2020-04-09 21:25:53 -0700

    bpo-39481: PEP 585 for enumerate, AsyncGeneratorType, mmap (GH-19421)

--- Modules/mmapmodule.c
+++ Modules/mmapmodule.c
@@ -816,6 +816,8 @@ static struct PyMethodDef mmap_object_methods[] = {
 #ifdef MS_WINDOWS
     {"__sizeof__",      (PyCFunction) mmap__sizeof__method,     METH_NOARGS},
 #endif
+    {"__class_getitem__",    (PyCFunction)Py_GenericAlias, METH_O|METH_CLASS,
+     PyDoc_STR("See PEP 585")},
     {NULL,         NULL}       /* sentinel */
 };
 
