c77a3528baf1a7d1740006d7a0832355c9a13dbf
Ethan Smith <ethan@ethanhs.me>
2020-04-12 23:58:02 -0700

    Make weakref and WeakSet generic

--- Lib/test/test_genericalias.py
+++ Lib/test/test_genericalias.py
@@ -28,6 +28,7 @@ from tempfile import TemporaryDirectory, SpooledTemporaryFile
 from urllib.parse import SplitResult, ParseResult
 from unittest.case import _AssertRaisesContext
 from queue import Queue, SimpleQueue
+from weakref import WeakSet, ReferenceType, ref
 import typing
 
 from typing import TypeVar
@@ -67,6 +68,7 @@ class BaseTest(unittest.TestCase):
                   Array, LibraryLoader,
                   SplitResult, ParseResult,
                   ValueProxy, ApplyResult,
+                  WeakSet, ReferenceType, ref,
                   ShareableList, SimpleQueue,
                   Future, _WorkItem,
                   Morsel,
