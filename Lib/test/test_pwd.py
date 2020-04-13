8c925d99a807b65dbe235792534c9489d4a88137
Zackery Spytz <zspytz@gmail.com>
2020-04-13 08:46:27 -0600

    bpo-32033: Fix test_pwd failures on Android

--- Lib/test/test_pwd.py
+++ Lib/test/test_pwd.py
@@ -21,7 +21,8 @@ class PwdTest(unittest.TestCase):
             self.assertEqual(e[3], e.pw_gid)
             self.assertIsInstance(e.pw_gid, int)
             self.assertEqual(e[4], e.pw_gecos)
-            self.assertIsInstance(e.pw_gecos, str)
+            if e.pw_gecos is not None:
+                self.assertIsInstance(e.pw_gecos, str)
             self.assertEqual(e[5], e.pw_dir)
             self.assertIsInstance(e.pw_dir, str)
             self.assertEqual(e[6], e.pw_shell)
