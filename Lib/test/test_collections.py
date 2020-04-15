d25b3ba70c7f50163df824107f31e23d55a0c462
Ammar Askar <ammar@ammaraskar.com>
2020-04-14 22:09:08 -0700

    Add tests for __doc__ case and NEWS entry

--- Lib/test/test_collections.py
+++ Lib/test/test_collections.py
@@ -417,6 +417,12 @@ class TestNamedTuple(unittest.TestCase):
         self.assertEqual(repr(Point.x), "_tuplegetter(0, 'Alias for field number 0')")
         self.assertEqual(repr(Point.y), "_tuplegetter(1, 'Alias for field number 1')")
 
+        Point.x.__doc__ = 'The x-coordinate'
+        Point.y.__doc__ = 'The y-coordinate'
+
+        self.assertEqual(repr(Point.x), "_tuplegetter(0, 'The x-coordinate')")
+        self.assertEqual(repr(Point.y), "_tuplegetter(1, 'The y-coordinate')")
+
     def test_name_fixer(self):
         for spec, renamed in [
             [('efg', 'g%hi'),  ('efg', '_1')],                              # field with non-alpha char
