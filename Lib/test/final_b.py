9a6f4f9aaa95219ebec06b9039461315964d21d0
Eddie Elizondo <eelizondo@fb.com>
2020-04-15 19:13:27 -0400

    Remove module usage at runtime destruction

--- Lib/test/final_b.py
+++ Lib/test/final_b.py
@@ -11,9 +11,10 @@ class C:
     def __del__(self):
         # Inspect module globals and builtins
         print("x =", x)
-        print("final_a.x =", test.final_a.x)
         print("shutil.rmtree =", getattr(shutil.rmtree, '__name__', None))
         print("len =", getattr(len, '__name__', None))
 
 c = C()
 _underscored = C()
+del _underscored
+del c
