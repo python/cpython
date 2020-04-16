94981cc032d6b2cb9260ea2bde24069ed20e30c3
Alessandro Molina <amol@turbogears.org>
2020-04-16 23:42:52 +0200

    bpo-40307: Preserve manager Client in multiprocessing managers

--- Lib/test/_test_multiprocessing.py
+++ Lib/test/_test_multiprocessing.py
@@ -2952,6 +2952,31 @@ class _TestRemoteManager(BaseTestCase):
         # Make queue finalizer run before the server is stopped
         del queue
 
+    def test_client_propagation(self):
+        authkey = os.urandom(32)
+
+        manager = QueueManager(
+            address=(test.support.HOST, 0), authkey=authkey, serializer=SERIALIZER
+            )
+        manager.start()
+        self.addCleanup(manager.shutdown)
+
+        manager2 = QueueManager2(
+            address=manager.address, authkey=authkey, serializer=SERIALIZER
+            )
+
+        def MyXmlClient(*args, **kwargs):
+            return manager._Client(*args, **kwargs)
+
+        manager2._Client = MyXmlClient
+        manager2.connect()
+        queue = manager2.get_queue()
+
+        self.assertIsNot(queue._Client, manager._Client)
+        self.assertIs(queue._Client, manager2._Client)
+
+        del queue
+
 class _TestManagerRestart(BaseTestCase):
 
     @classmethod
