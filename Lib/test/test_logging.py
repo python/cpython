3f1fc37baa7e005e39865f5e2fed172164abb42c
Serhiy Storchaka <storchaka@gmail.com>
2020-04-20 12:42:15 +0300

    Move TestHandler to logging_helper.

--- Lib/test/test_logging.py
+++ Lib/test/test_logging.py
@@ -43,6 +43,7 @@ import sys
 import tempfile
 from test.support.script_helper import assert_python_ok, assert_python_failure
 from test import support
+from test.support.logging_helper import TestHandler
 import textwrap
 import threading
 import time
@@ -3475,35 +3476,6 @@ class LogRecordFactoryTest(BaseTest):
         self.assert_log_lines([
            ('root', 'ERROR', '2'),
         ])
-
-
-class TestHandler(logging.handlers.BufferingHandler):
-    def __init__(self, matcher):
-        # BufferingHandler takes a "capacity" argument
-        # so as to know when to flush. As we're overriding
-        # shouldFlush anyway, we can set a capacity of zero.
-        # You can call flush() manually to clear out the
-        # buffer.
-        logging.handlers.BufferingHandler.__init__(self, 0)
-        self.matcher = matcher
-
-    def shouldFlush(self):
-        return False
-
-    def emit(self, record):
-        self.format(record)
-        self.buffer.append(record.__dict__)
-
-    def matches(self, **kwargs):
-        """
-        Look for a saved dict whose keys/values match the supplied arguments.
-        """
-        result = False
-        for d in self.buffer:
-            if self.matcher.matches(d, **kwargs):
-                result = True
-                break
-        return result
 
 
 class QueueHandlerTest(BaseTest):
