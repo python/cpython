1b31c16b98b58849402b8dd47dc9b04e3518bf4f
Lysandros Nikolaou <lisandrosnik@gmail.com>
2020-04-21 23:25:51 +0300

    Run test_peg_parser in order to catch false-negatives

--- Lib/test/test_peg_parser.py
+++ Lib/test/test_peg_parser.py
@@ -694,7 +694,6 @@ EXPRESSIONS_TEST_IDS, EXPRESSIONS_TEST_SOURCES = prepare_test_cases(
 
 
 class ASTGenerationTest(unittest.TestCase):
-    @unittest.skipIf(sys.flags.use_peg, "This tests nothing for now, since compile used pegen as well")
     def test_correct_ast_generation_on_source_files(self) -> None:
         self.maxDiff = None
         for source in TEST_SOURCES:
@@ -721,7 +720,7 @@ class ASTGenerationTest(unittest.TestCase):
                 f"Actual error message does not match expexted for {source}"
             )
 
-    @unittest.skipIf(sys.flags.use_peg, "This tests nothing for now, since compile used pegen as well")
+    @unittest.skipIf(sys.flags.use_peg, "This tests nothing for now, since compile uses pegen as well")
     @unittest.expectedFailure
     def test_correct_but_known_to_fail_ast_generation_on_source_files(self) -> None:
         for source in GOOD_BUT_FAIL_SOURCES:
@@ -733,7 +732,6 @@ class ASTGenerationTest(unittest.TestCase):
                 f"Wrong AST generation for source: {source}",
             )
 
-    @unittest.skipIf(sys.flags.use_peg, "This tests nothing for now, since compile used pegen as well")
     def test_correct_ast_generation_without_pos_info(self) -> None:
         for source in GOOD_BUT_FAIL_SOURCES:
             actual_ast = peg_parser.parse_string(source)
@@ -750,7 +748,6 @@ class ASTGenerationTest(unittest.TestCase):
                 peg_parser.parse_string(dedent(source))
             self.assertEqual(error_text, se.exception.text)
 
-    @unittest.skipIf(sys.flags.use_peg, "This tests nothing for now, since compile used pegen as well")
     def test_correct_ast_generatrion_eval(self) -> None:
         for source in EXPRESSIONS_TEST_SOURCES:
             actual_ast = peg_parser.parse_string(source, mode='eval')
