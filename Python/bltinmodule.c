5131901fb3f741fd70cfb293d074d611e2310c2a
Batuhan Taşkaya <batuhanosmantaskaya@gmail.com>
2020-04-22 19:09:03 +0300

    [3.8] bpo-39562: Prevent collision of future and compiler flags (GH-19230)
    The constant values of future flags in the __future__ module
    is updated in order to prevent collision with compiler flags.
    Previously PyCF_ALLOW_TOP_LEVEL_AWAIT was clashing
    with CO_FUTURE_DIVISION..
    (cherry picked from commit 4454057269b995341b04d13f0bf97f96080f27d0)
    
    Co-authored-by: Batuhan Taşkaya <batuhanosmantaskaya@gmail.com>

--- Python/bltinmodule.c
+++ Python/bltinmodule.c
@@ -738,7 +738,7 @@ builtin_compile_impl(PyObject *module, PyObject *source, PyObject *filename,
     }
 
     if (flags &
-        ~(PyCF_MASK | PyCF_MASK_OBSOLETE | PyCF_DONT_IMPLY_DEDENT | PyCF_ONLY_AST | PyCF_TYPE_COMMENTS))
+        ~(PyCF_MASK | PyCF_MASK_OBSOLETE | PyCF_COMPILE_MASK))
     {
         PyErr_SetString(PyExc_ValueError,
                         "compile(): unrecognised flags");
