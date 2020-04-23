7ceb4b308fef4e5241e28c1c15e200d90e12ced9
Victor Stinner <vstinner@python.org>
2020-04-23 19:03:52 +0200

    bpo-39983: Add test.support.print_warning() (GH-19683)
    Log "Warning -- ..." test warnings into sys.__stderr__ rather than
    sys.stderr, to ensure to display them even if sys.stderr is captured.
    
    test.libregrtest.utils.print_warning() now calls
    test.support.print_warning().

--- Lib/test/test_support.py
+++ Lib/test/test_support.py
@@ -430,8 +430,12 @@ class TestSupport(unittest.TestCase):
                 if time.monotonic() > deadline:
                     self.fail("timeout")
 
-                with contextlib.redirect_stderr(stderr):
+                old_stderr = sys.__stderr__
+                try:
+                    sys.__stderr__ = stderr
                     support.reap_children()
+                finally:
+                    sys.__stderr__ = old_stderr
 
                 # Use environment_altered to check if reap_children() found
                 # the child process
@@ -628,6 +632,24 @@ class TestSupport(unittest.TestCase):
         finally:
             os.close(fd)
         self.assertEqual(more - start, 1)
+
+    def check_print_warning(self, msg, expected):
+        stderr = io.StringIO()
+
+        old_stderr = sys.__stderr__
+        try:
+            sys.__stderr__ = stderr
+            support.print_warning(msg)
+        finally:
+            sys.__stderr__ = old_stderr
+
+        self.assertEqual(stderr.getvalue(), expected)
+
+    def test_print_warning(self):
+        self.check_print_warning("msg",
+                                 "Warning -- msg\n")
+        self.check_print_warning("a\nb",
+                                 'Warning -- a\nWarning -- b\n')
 
     # XXX -follows a list of untested API
     # make_legacy_pyc
