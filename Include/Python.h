7c59d7c9860cdbaf4a9c26c9142aebd3259d046e
Victor Stinner <vstinner@python.org>
2020-04-28 16:32:48 +0200

    bpo-40421: Add pyframe.h header file (GH-19755)
    Add a new separated pyframe.h header file of the PyFrame public C
    API: it is included by Python.h.
    
    Add PyFrame_GetLineNumber() to the limited C API.
    
    Replace "struct _frame" with "PyFrameObject" in header files.
    PyFrameObject is now defined as struct _frame by pyframe.h which is
    included early enough in Python.h.

--- Include/Python.h
+++ Include/Python.h
@@ -114,6 +114,7 @@
 #include "classobject.h"
 #include "fileobject.h"
 #include "pycapsule.h"
+#include "pyframe.h"
 #include "traceback.h"
 #include "sliceobject.h"
 #include "cellobject.h"
---
+++ Include/pyframe.h
@@ -0,0 +1,20 @@
+/* Limited C API of PyFrame API
+ *
+ * Include "frameobject.h" to get the PyFrameObject structure.
+ */
+
+#ifndef Py_PYFRAME_H
+#define Py_PYFRAME_H
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct _frame PyFrameObject;
+
+/* Return the line of code the frame is currently executing. */
+PyAPI_FUNC(int) PyFrame_GetLineNumber(PyFrameObject *);
+
+#ifdef __cplusplus
+}
+#endif
+#endif /* !Py_PYFRAME_H */
