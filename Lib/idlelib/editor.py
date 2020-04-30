1070711ff5ae5de97a7dfe74130578fae594f17b
E-Paine <63801254+E-Paine@users.noreply.github.com>
2020-04-30 16:33:37 +0100

    Fixed Windows support
    Windows requires a different mechanism to Linux (the platform I originally developed on). I do not have a MacOS machine so am unable to test on MacOS.

--- Lib/idlelib/editor.py
+++ Lib/idlelib/editor.py
@@ -107,8 +107,11 @@ class EditorWindow(object):
         self.root = root
         self.menubar = Menu(root)
         self.top = top = window.ListedToplevel(root, menu=self.menubar)
-        self.top.attributes('-zoomed', idleConf.GetOption(
-            'main', 'EditorWindow', 'max', type='bool'))
+        if idleConf.GetOption('main', 'EditorWindow', 'max', type='bool'):
+            if sys.platform[:3] == 'win':
+                self.top.state('zoomed')
+            else:
+                self.top.attributes('-zoomed', True)
         if flist:
             self.tkinter_vars = flist.vars
             #self.top.instance_dict makes flist.inversedict available to
