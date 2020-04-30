a30194abda5dba1f9ded0d2f6c39bd1744d1f80b
Vlad Serebrennikov <brainvlad@gmail.com>
2020-04-30 04:06:39 +0300

    bpo-40389: Improve repr of typing.Optional (#19714)

--- Lib/typing.py
+++ Lib/typing.py
@@ -691,6 +691,13 @@ class _GenericAlias(_Final, _root=True):
         return _GenericAlias(self.__origin__, params, name=self._name, inst=self._inst)
 
     def __repr__(self):
+        if (self.__origin__ == Union and len(self.__args__) == 2
+                and type(None) in self.__args__):
+            if self.__args__[0] is not type(None):
+                arg = self.__args__[0]
+            else:
+                arg = self.__args__[1]
+            return (f'typing.Optional[{_type_repr(arg)}]')
         if (self._name != 'Callable' or
                 len(self.__args__) == 2 and self.__args__[0] is Ellipsis):
             if self._name:
