bcc30360951a303aa72b0502b77aad2c5f09f30d
Pablo Galindo <Pablogsal@gmail.com>
2020-05-14 21:11:48 +0100

    bpo-40619: Correctly handle error lines in programs without file mode (GH-20090)

--- Lib/test/test_exceptions.py
+++ Lib/test/test_exceptions.py
@@ -228,6 +228,8 @@ class ExceptionTests(unittest.TestCase):
             def baz():
                 '''quux'''
             """, 9, 20)
+        check("pass\npass\npass\n(1+)\npass\npass\npass", 4, 4)
+        check("(1+)", 1, 4)
 
         # Errors thrown by symtable.c
         check('x = [(yield i) for i in range(3)]', 1, 5)
