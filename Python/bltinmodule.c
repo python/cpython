bf5a80bcbcf9fbb747b1f22272938937e5719165
Gregory P. Smith <greg@krypto.org>
2020-05-15 12:16:50 -0700

    Clarify the zip built-in docstring.
    This puts much simpler text up front along with an example.
    
    As it was, the zip built-in docstring was technically correct.  But too
    technical for the reader who shouldn't _need_ to know about `__next__` and
    `StopIteration` as most people do not need to understand the internal
    implementation details of the iterator protocol in their daily life.
    
    This is a documentation only change, intended to be backported to 3.8; it is
    only tangentially related to PEP-618 which might offer new behavior options
    in the future.

--- Python/bltinmodule.c
+++ Python/bltinmodule.c
@@ -2649,9 +2649,14 @@ static PyMethodDef zip_methods[] = {
 };
 
 PyDoc_STRVAR(zip_doc,
-"zip(*iterables) --> zip object\n\
+"zip(*iterables) --> An iterable zip object that yields tuples.\n\
 \n\
-Return a zip object whose .__next__() method returns a tuple where\n\
+Upon any of the input iterables being exhausted, the zip stops.\n\
+\n\
+   >>> list(zip('abcdefg', range(3), range(4)))\n\
+   [('a', 0, 0), ('b', 1, 1), ('c', 2, 2)]\n\
+\n\
+Returns a zip object whose .__next__() method returns a tuple where\n\
 the i-th element comes from the i-th iterable argument.  The .__next__()\n\
 method continues until the shortest iterable in the argument sequence\n\
 is exhausted and then it raises StopIteration.");
