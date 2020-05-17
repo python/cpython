c6e8a43a460d1d2adb66feec94c73b7cefd23ce9
Pablo Galindo <Pablogsal@gmail.com>
2020-05-17 02:51:22 +0100

    Update Lib/test/test_unparse.py
    Co-authored-by: Batuhan Taskaya <isidentical@gmail.com>

--- Lib/test/test_unparse.py
+++ Lib/test/test_unparse.py
@@ -136,8 +136,8 @@ class ASTTestCase(unittest.TestCase):
         return code1, code2
 
     def check_src_roundtrip(self, code1, code2=None):
+        code1, code2 = self.get_source(code1, code2)
         with self.subTest(code1=code1, code2=code2):
-            code1, code2 = self.get_source(code1, code2)
             self.assertEqual(code2, code1)
 
     def check_src_dont_roundtrip(self, code1, code2=None):
