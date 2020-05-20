fded615bea0bb70bb71103dbd29e5ae04baaf4b7
pxin <peixing.xin@windriver.com>
2020-05-20 17:32:27 +0800

    Fix os module failures for VxWorks RTOS

--- Lib/test/test_os.py
+++ Lib/test/test_os.py
@@ -102,7 +102,12 @@ class MiscTests(unittest.TestCase):
         # On Windows, the test can stop when trying to create a path longer
         # than MAX_PATH if long paths support is disabled:
         # see RtlAreLongPathsEnabled().
-        min_len = 2000   # characters
+        #
+        # On VxWorks, PATH_MAX is defined as 1024 bytes.
+        if sys.platform == 'vxworks':
+            min_len = 1000
+        else:
+            min_len = 2000   # characters
         dirlen = 200     # characters
         dirname = 'python_test_dir_'
         dirname = dirname + ('a' * (dirlen - len(dirname)))
