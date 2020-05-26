caca129d7da99c4949bc47edf8adffe207684dcf
idomic <michael.ido@gmail.com>
2020-05-26 10:54:21 -0400

    bpo-39244: multiprocessing return default start method first on macOS (GH-18625) (cherry picked from commit db098bc1f05bd0773943e59f83489f05f28dedf8)
    Co-authored-by: idomic <michael.ido@gmail.com>

--- Lib/test/_test_multiprocessing.py
+++ Lib/test/_test_multiprocessing.py
@@ -5039,7 +5039,9 @@ class TestStartMethod(unittest.TestCase):
             self.assertEqual(methods, ['spawn'])
         else:
             self.assertTrue(methods == ['fork', 'spawn'] or
-                            methods == ['fork', 'spawn', 'forkserver'])
+                            methods == ['spawn', 'fork'] or
+                            methods == ['fork', 'spawn', 'forkserver'] or
+                            methods == ['spawn', 'fork', 'forkserver'])
 
     def test_preload_resources(self):
         if multiprocessing.get_start_method() != 'forkserver':
