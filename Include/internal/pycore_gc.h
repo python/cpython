3744ed2c9c0b3905947602fc375de49533790cb9
Victor Stinner <vstinner@python.org>
2020-06-05 01:39:24 +0200

    bpo-40521: Make frame free list per-interpreter (GH-20638)
    Each interpreter now has its own frame free list:
    
    * Move frame free list into PyInterpreterState.
    * Add _Py_frame_state structure.
    * Add tstate parameter to _PyFrame_ClearFreeList()
      and _PyFrame_Fini().
    * Remove "#if PyFrame_MAXFREELIST > 0".
    * Remove "#ifdef EXPERIMENTAL_ISOLATED_SUBINTERPRETERS".

--- Include/internal/pycore_gc.h
+++ Include/internal/pycore_gc.h
@@ -165,7 +165,7 @@ PyAPI_FUNC(void) _PyGC_InitState(struct _gc_runtime_state *);
 
 
 // Functions to clear types free lists
-extern void _PyFrame_ClearFreeList(void);
+extern void _PyFrame_ClearFreeList(PyThreadState *tstate);
 extern void _PyTuple_ClearFreeList(PyThreadState *tstate);
 extern void _PyFloat_ClearFreeList(PyThreadState *tstate);
 extern void _PyList_ClearFreeList(void);
