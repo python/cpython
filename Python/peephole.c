ccf83741f8deaff99f8c8f429f130a30cd7997dd
Brandt Bucher <brandtbucher@gmail.com>
2020-06-05 07:14:03 -0700

    Tell the peepholer about all of the jumping we do

--- Python/peephole.c
+++ Python/peephole.c
@@ -199,6 +199,11 @@ markblocks(_Py_CODEUNIT *code, Py_ssize_t len)
             case SETUP_FINALLY:
             case SETUP_WITH:
             case SETUP_ASYNC_WITH:
+            case MATCH:
+            case MATCH_MAP:
+            case MATCH_MAP_STAR:
+            case MATCH_SEQ:
+            case MATCH_SEQ_STAR:
                 j = GETJUMPTGT(code, i);
                 assert(j < len);
                 blocks[j] = 1;
@@ -503,6 +508,11 @@ PyCode_Optimize(PyObject *code, PyObject* consts, PyObject *names,
             case SETUP_FINALLY:
             case SETUP_WITH:
             case SETUP_ASYNC_WITH:
+            case MATCH:
+            case MATCH_MAP:
+            case MATCH_MAP_STAR:
+            case MATCH_SEQ:
+            case MATCH_SEQ_STAR:
                 j = blocks[j / sizeof(_Py_CODEUNIT) + i + 1] - blocks[i] - 1;
                 j *= sizeof(_Py_CODEUNIT);
                 break;
