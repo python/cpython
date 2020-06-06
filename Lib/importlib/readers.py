5ad02bf4189ccdaf8867e176826057532bb5e7c6
Jason R. Coombs <jaraco@jaraco.com>
2020-06-05 22:59:38 -0400

    Add zip reader and rely on it for a TraversableResources object on zipimporter.

--- Lib/importlib/readers.py
+++ Lib/importlib/readers.py
@@ -1,4 +1,5 @@
 import pathlib
+import zipfile
 from .abc import TraversableResources
 
 
@@ -8,3 +9,22 @@ class FileReader(TraversableResources):
 
     def files(self):
         return self.path
+
+
+class ZipReader(FileReader):
+    def __init__(self, loader, module):
+        _, _, name = module.rpartition('.')
+        prefix = loader.prefix + name + '/'
+        self.path = zipfile.Path(loader.archive, prefix)
+
+    def open_resource(self, resource):
+        try:
+            return super().open_resource(resource)
+        except KeyError as exc:
+            raise FileNotFoundError(exc.args[0])
+
+    def is_resource(self, path):
+        # workaround for `zipfile.Path.is_file` returning true
+        # for non-existent paths.
+        target = self.files().joinpath(path)
+        return target.is_file() and target.exists()
