2ff85fff204b8405d793f963741a32e37e43e185
Atsuo Ishimoto <atsuoishimoto@gmail.com>
2020-06-09 13:44:03 +0900

    add test script

---
+++ test_json.py
@@ -0,0 +1,17 @@
+a=100
+b = {1:200}
+c = 20
+d = 30
+
+print(`{a, **b})
+
+print(`{a})
+print(`{a:200})
+
+
+print(`{a, **b})
+print(`{a, **b, c, d})
+print(`{a, b, c, d})
+print(`{**b,**b, a})
+print(`{**b, a:200})
+print(`{a:200, **b})
