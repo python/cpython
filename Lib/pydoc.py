cf622a415e8819c1083a760a5916dd42ade4b1a5
Lysandros Nikolaou <lisandrosnik@gmail.com>
2020-06-11 19:09:21 +0300

    bpo-40939: Remove PEG parser easter egg (__new_parser__) (#20802)
    It no longer serves a purpose (there's only one parser) and having "new" in any name will eventually look odd. Also, it impinges on a potential sub-namespace, `__new_...__`.

--- Lib/pydoc.py
+++ Lib/pydoc.py
@@ -1817,7 +1817,6 @@ class Helper:
         'False': '',
         'None': '',
         'True': '',
-        '__new_parser__': '',
         'and': 'BOOLEAN',
         'as': 'with',
         'assert': ('assert', ''),
