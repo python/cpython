111838027d29b2004ef5a5c17aff666339b939a0
Pablo Galindo <pablogsal@gmail.com>
2020-06-11 12:33:31 +0100

    Restore tests in test_string_literals

--- Lib/test/test_string_literals.py
+++ Lib/test/test_string_literals.py
@@ -167,6 +167,7 @@ class TestLiterals(unittest.TestCase):
             eval("b'''\n\\z'''")
         self.assertEqual(len(w), 1)
         self.assertEqual(w[0].filename, '<string>')
+        self.assertEqual(w[0].lineno, 1)
 
         with warnings.catch_warnings(record=True) as w:
             warnings.simplefilter('error', category=DeprecationWarning)
@@ -175,6 +176,7 @@ class TestLiterals(unittest.TestCase):
             exc = cm.exception
         self.assertEqual(w, [])
         self.assertEqual(exc.filename, '<string>')
+        self.assertEqual(exc.lineno, 1)
 
     def test_eval_bytes_raw(self):
         self.assertEqual(eval(""" br'x' """), b'x')
