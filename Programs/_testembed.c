20a9e53b50b5bb419a745305c1457f4ab27f7bcb
Pablo Galindo <Pablogsal@gmail.com>
2020-06-11 18:08:05 +0100

    bpo-40939: Remove some extra references to PYTHONOLDPARSER (GH-20815)
    Automerge-Triggered-By: @pablogsal

--- Programs/_testembed.c
+++ Programs/_testembed.c
@@ -485,8 +485,6 @@ static int test_init_from_config(void)
 
     config.install_signal_handlers = 0;
 
-    putenv("PYTHONOLDPARSER=1");
-
     /* FIXME: test use_environment */
 
     putenv("PYTHONHASHSEED=42");
@@ -673,7 +671,6 @@ static void set_most_env_vars(void)
     putenv("PYTHONNOUSERSITE=1");
     putenv("PYTHONFAULTHANDLER=1");
     putenv("PYTHONIOENCODING=iso8859-1:replace");
-    putenv("PYTHONOLDPARSER=1");
     putenv("PYTHONPLATLIBDIR=env_platlibdir");
 }
 
