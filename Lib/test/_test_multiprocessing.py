e0f8ad172321fa0816d0359b38f347ad33e42332
Victor Stinner <vstinner@python.org>
2020-06-18 17:19:59 +0200

    bpo-38377: Add support.skip_if_broken_multiprocessing_synchronize() (GH-20944) (GH-20962)
    On Linux, skip tests using multiprocessing if the current user cannot
    create a file in /dev/shm/ directory. Add the
    skip_if_broken_multiprocessing_synchronize() function to the
    test.support module.
    
    (cherry picked from commit ddbeb2f3e02a510c5784ffd74c5e09e8c70b5881)
    (cherry picked from commit b1e736113484c99acb57e4acb417b91a9e58e7ff)

--- Lib/test/_test_multiprocessing.py
+++ Lib/test/_test_multiprocessing.py
@@ -31,7 +31,7 @@ from test import support
 # Skip tests if _multiprocessing wasn't built.
 _multiprocessing = test.support.import_module('_multiprocessing')
 # Skip tests if sem_open implementation is broken.
-test.support.import_module('multiprocessing.synchronize')
+support.skip_if_broken_multiprocessing_synchronize()
 import threading
 
 import multiprocessing.connection
