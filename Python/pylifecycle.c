1bcf0a77cc9451427137564fb13b25dec574f929
Victor Stinner <vstinner@python.org>
2020-06-23 14:07:52 +0200

    bpo-40521: Make the empty frozenset per interpreter (GH-21068)
    Each interpreter now has its own empty frozenset singleton.

--- Python/pylifecycle.c
+++ Python/pylifecycle.c
@@ -1255,9 +1255,7 @@ finalize_interp_types(PyThreadState *tstate, int is_main_interp)
     _PyAsyncGen_Fini(tstate);
     _PyContext_Fini(tstate);
 
-    if (is_main_interp) {
-        _PySet_Fini();
-    }
+    _PySet_Fini(tstate);
     _PyDict_Fini(tstate);
     _PyList_Fini(tstate);
     _PyTuple_Fini(tstate);
