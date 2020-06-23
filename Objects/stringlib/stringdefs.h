1156129b0a949e9a6fe495bdf19a3862b3f4975a
Victor Stinner <vstinner@python.org>
2020-06-23 15:54:35 +0200

    bpo-40521: Make bytes singletons per interpreter (GH-21074)
    Each interpreter now has its own empty bytes string and single byte
    character singletons.
    
    Replace STRINGLIB_EMPTY macro with STRINGLIB_GET_EMPTY() macro.

--- Objects/stringlib/stringdefs.h
+++ Objects/stringlib/stringdefs.h
@@ -1,6 +1,10 @@
 #ifndef STRINGLIB_STRINGDEFS_H
 #define STRINGLIB_STRINGDEFS_H
 
+#ifndef STRINGLIB_GET_EMPTY
+#  error "STRINGLIB_GET_EMPTY macro must be defined"
+#endif
+
 /* this is sort of a hack.  there's at least one place (formatting
    floats) where some stringlib code takes a different path if it's
    compiled as unicode. */
@@ -13,7 +17,6 @@
 #define STRINGLIB_CHAR           char
 #define STRINGLIB_TYPE_NAME      "string"
 #define STRINGLIB_PARSE_CODE     "S"
-#define STRINGLIB_EMPTY          nullstring
 #define STRINGLIB_ISSPACE        Py_ISSPACE
 #define STRINGLIB_ISLINEBREAK(x) ((x == '\n') || (x == '\r'))
 #define STRINGLIB_ISDECIMAL(x)   ((x >= '0') && (x <= '9'))
