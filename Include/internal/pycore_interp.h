aafdc84b92a53c5b7692d292ac0fe518ddca3030
Victor Stinner <vstinner@python.org>
2020-06-24 02:22:21 +0200

    bpo-40521: Make Unicode latin1 singletons per interpreter (GH-21101)
    Each interpreter now has its own Unicode latin1 singletons.
    
    Remove "ifdef EXPERIMENTAL_ISOLATED_SUBINTERPRETERS"
    and "ifdef LATIN1_SINGLETONS": always enable latin1 singletons.
    
    Optimize unicode_result_ready(): only attempt to get a latin1
    singleton for PyUnicode_1BYTE_KIND.

--- Include/internal/pycore_interp.h
+++ Include/internal/pycore_interp.h
@@ -73,6 +73,9 @@ struct _Py_bytes_state {
 struct _Py_unicode_state {
     // The empty Unicode object is a singleton to improve performance.
     PyObject *empty;
+    /* Single character Unicode strings in the Latin-1 range are being
+       shared as well. */
+    PyObject *latin1[256];
     struct _Py_unicode_fs_codec fs_codec;
 };
 
