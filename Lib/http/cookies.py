74366e0414b6881b2416605dde2663b43d72ff20
Ido Michael <michael.ido@gmail.com>
2020-06-27 14:18:18 -0400

    Fixed typo + reverted the url parse last section

--- Lib/http/cookies.py
+++ Lib/http/cookies.py
@@ -598,8 +598,7 @@ class BaseCookie(dict):
 
 
 class SimpleCookie(BaseCookie):
-    """
-    SimpleCookie supports strings as cookie values.  When setting
+    """SimpleCookie supports strings as cookie values.  When setting
     the value using the dictionary assignment notation, SimpleCookie
     calls the builtin str() to convert the value to a string.  Values
     received from HTTP are kept as strings.
