b98e7790c77a4378ec4b1c71b84138cb930b69b7
Tapas Kundu <39723251+tapakund@users.noreply.github.com>
2020-07-01 00:50:21 +0530

    [3.7] bpo-41004: Resolve hash collisions for IPv4Interface and IPv6Interface (GH-21033) (GH-21231)
    CVE-2020-14422
    The __hash__() methods of classes IPv4Interface and IPv6Interface had issue
    of generating constant hash values of 32 and 128 respectively causing hash collisions.
    The fix uses the hash() function to generate hash values for the objects
    instead of XOR operation
    (cherry picked from commit b30ee26e366bf509b7538d79bfec6c6d38d53f28)
    
    Co-authored-by: Ravi Teja P <rvteja92@gmail.com>
    
    Signed-off-by: Tapas Kundu <tkundu@vmware.com>

--- Lib/ipaddress.py
+++ Lib/ipaddress.py
@@ -1442,7 +1442,7 @@ class IPv4Interface(IPv4Address):
             return False
 
     def __hash__(self):
-        return self._ip ^ self._prefixlen ^ int(self.network.network_address)
+        return hash((self._ip, self._prefixlen, int(self.network.network_address)))
 
     __reduce__ = _IPAddressBase.__reduce__
 
@@ -2088,7 +2088,7 @@ class IPv6Interface(IPv6Address):
             return False
 
     def __hash__(self):
-        return self._ip ^ self._prefixlen ^ int(self.network.network_address)
+        return hash((self._ip, self._prefixlen, int(self.network.network_address)))
 
     __reduce__ = _IPAddressBase.__reduce__
 
