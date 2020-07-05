97b5228a01dbfd995a2b815b184ff2f0962b1928
Serhiy Storchaka <storchaka@gmail.com>
2020-07-05 20:23:03 +0300

    Use HAVE_UNICODE_WCHAR_CACHE instead of USE_UNICODE_WCHAR_CACHE in _Py_DumpASCII().

--- Python/traceback.c
+++ Python/traceback.c
@@ -684,9 +684,9 @@ _Py_DumpASCII(int fd, PyObject *text)
     int truncated;
     int kind;
     void *data = NULL;
-#if USE_UNICODE_WCHAR_CACHE
+#if HAVE_UNICODE_WCHAR_CACHE
     wchar_t *wstr = NULL;
-#endif /* USE_UNICODE_WCHAR_CACHE */
+#endif /* HAVE_UNICODE_WCHAR_CACHE */
     Py_UCS4 ch;
 
     if (!PyUnicode_Check(text))
@@ -694,7 +694,7 @@ _Py_DumpASCII(int fd, PyObject *text)
 
     size = ascii->length;
     kind = ascii->state.kind;
-#if USE_UNICODE_WCHAR_CACHE
+#if HAVE_UNICODE_WCHAR_CACHE
     if (kind == PyUnicode_WCHAR_KIND) {
         wstr = ((PyASCIIObject *)text)->wstr;
         if (wstr == NULL)
@@ -702,7 +702,7 @@ _Py_DumpASCII(int fd, PyObject *text)
         size = ((PyCompactUnicodeObject *)text)->wstr_length;
     }
     else
-#endif /* USE_UNICODE_WCHAR_CACHE */
+#endif /* HAVE_UNICODE_WCHAR_CACHE */
     if (ascii->state.compact) {
         if (ascii->state.ascii)
             data = ((PyASCIIObject*)text) + 1;
@@ -724,11 +724,11 @@ _Py_DumpASCII(int fd, PyObject *text)
     }
 
     for (i=0; i < size; i++) {
-#if USE_UNICODE_WCHAR_CACHE
+#if HAVE_UNICODE_WCHAR_CACHE
         if (kind == PyUnicode_WCHAR_KIND)
             ch = wstr[i];
         else
-#endif /* USE_UNICODE_WCHAR_CACHE */
+#endif /* HAVE_UNICODE_WCHAR_CACHE */
             ch = PyUnicode_READ(kind, data, i);
         if (' ' <= ch && ch <= 126) {
             /* printable ASCII character */
