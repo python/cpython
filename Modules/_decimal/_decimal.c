c9babe5a627921ba389c48e9053fddc5c1084c21
Felix Yan <felixonmars@archlinux.org>
2020-07-15 20:14:11 +0800

    bpo-41302: Fix build with system libmpdec (GH-21481)
    Move definition of UNUSED from modified headers of libmpdec to
    _decimal.c itself. This makes the vendored source closer to the
    standalone library and fixes build with --with-system-libmpdec.
    
    Tested to build fine with either system libmpdec or the vendored one.

--- Modules/_decimal/_decimal.c
+++ Modules/_decimal/_decimal.c
@@ -56,6 +56,11 @@
 
 #define BOUNDS_CHECK(x, MIN, MAX) x = (x < MIN || MAX < x) ? MAX : x
 
+#if defined(__GNUC__) && !defined(__INTEL_COMPILER)
+  #define UNUSED __attribute__((unused))
+#else
+  #define UNUSED
+#endif
 
 /* _Py_DEC_MINALLOC >= MPD_MINALLOC */
 #define _Py_DEC_MINALLOC 4
