5639f66f6707bf3f41a0bbd09318ab1c4b6e1b29
Artem Bulgakov <ArtemSBulgakov@ya.ru>
2020-07-16 21:39:30 +0300

    bpo-41316: Make tarfile follow specs for FNAME
    tarfile writes full path to FNAME field of GZIP format instead of just basename if user specified absolute path. Some archive viewers may process file incorrectly. Also it creates security issue because anyone can know structure of directories on system and know username or other personal information.
    
    RFC1952 says about FNAME:
    This is the original name of the file being compressed, with any directory components removed.
    
    So tarfile must remove directory names from FNAME and write only basename of file.

--- Lib/tarfile.py
+++ Lib/tarfile.py
@@ -420,6 +420,8 @@ class _Stream:
         self.__write(b"\037\213\010\010" + timestamp + b"\002\377")
         if self.name.endswith(".gz"):
             self.name = self.name[:-3]
+        # Remove directory components
+        self.name = os.path.basename(self.name)
         # RFC1952 says we must use ISO-8859-1 for the FNAME field.
         self.__write(self.name.encode("iso-8859-1", "replace") + NUL)
 
