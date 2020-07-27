e5d6feab4e178d449be0b0502dc786cd89014947
Lysandros Nikolaou <lisandrosnik@gmail.com>
2020-07-27 21:21:45 +0200

    Remove more references to the old parser in c-analyzer

--- Tools/c-analyzer/cpython/_generate.py
+++ Tools/c-analyzer/cpython/_generate.py
@@ -183,7 +183,6 @@ MULTILINE = {
     'float_format': 'static float_format_type ',
     'detected_double_format': 'static float_format_type ',
     'detected_float_format': 'static float_format_type ',
-    # Parser/listnode.c
     'level': 'static int ',
     'atbol': 'static int ',
     # Python/dtoa.c
