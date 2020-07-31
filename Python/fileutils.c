5a4082e5e1790809495971bba47b763143770aed
pxin <peixing.xin@windriver.com>
2020-07-31 17:37:16 +0800

    Fix _Py_set_blocking() for VxWorks RTOS

--- Python/fileutils.c
+++ Python/fileutils.c
@@ -2007,7 +2007,7 @@ _Py_get_blocking(int fd)
 int
 _Py_set_blocking(int fd, int blocking)
 {
-#if defined(HAVE_SYS_IOCTL_H) && defined(FIONBIO)
+#if defined(HAVE_SYS_IOCTL_H) && defined(FIONBIO) && !defined(__VXWORKS__)
     int arg = !blocking;
     if (ioctl(fd, FIONBIO, &arg) < 0)
         goto error;
