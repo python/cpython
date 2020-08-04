4199b8807a1992ecda199af7cf8bab22a0878d84
E. Madison Bray <erik.bray@lri.fr>
2020-08-04 12:51:18 +0200

    bpo-20082: fix misbehavior of buffered writes to raw files in append mode

--- Modules/_io/bufferedio.c
+++ Modules/_io/bufferedio.c
@@ -228,6 +228,7 @@ typedef struct {
     int detached;
     int readable;
     int writable;
+    int appending;
     char finalizing;
 
     /* True if this is a vanilla Buffered object (rather than a user derived
@@ -1350,6 +1351,33 @@ _io__Buffered_tell_impl(buffered *self)
     return PyLong_FromOff_t(pos);
 }
 
+static PyObject *
+_buffered_seek_unlocked(buffered *self, Py_off_t target, int whence)
+{
+    Py_off_t n;
+    PyObject *res = NULL;
+
+    if (self->writable) {
+        res = _bufferedwriter_flush_unlocked(self);
+        if (res == NULL)
+            return res;
+        Py_CLEAR(res);
+    }
+
+    /* TODO: align on block boundary and read buffer if needed? */
+    if (whence == 1)
+        target -= RAW_OFFSET(self);
+    n = _buffered_raw_seek(self, target, whence);
+    if (n == -1)
+        return res;
+    self->raw_pos = -1;
+    res = PyLong_FromOff_t(n);
+    if (res != NULL && self->readable)
+        _bufferedreader_reset_buf(self);
+
+    return res;
+}
+
 /*[clinic input]
 @critical_section
 _io._Buffered.seek
@@ -1362,7 +1390,7 @@ static PyObject *
 _io__Buffered_seek_impl(buffered *self, PyObject *targetobj, int whence)
 /*[clinic end generated code: output=7ae0e8dc46efdefb input=b5a12be70e0ad07b]*/
 {
-    Py_off_t target, n;
+    Py_off_t target;
     PyObject *res = NULL;
 
     CHECK_INITIALIZED(self)
@@ -1430,25 +1458,8 @@ _io__Buffered_seek_impl(buffered *self, PyObject *targetobj, int whence)
         return NULL;
 
     /* Fallback: invoke raw seek() method and clear buffer */
-    if (self->writable) {
-        res = _bufferedwriter_flush_unlocked(self);
-        if (res == NULL)
-            goto end;
-        Py_CLEAR(res);
-    }
+    res = _buffered_seek_unlocked(self, target, whence);
 
-    /* TODO: align on block boundary and read buffer if needed? */
-    if (whence == 1)
-        target -= RAW_OFFSET(self);
-    n = _buffered_raw_seek(self, target, whence);
-    if (n == -1)
-        goto end;
-    self->raw_pos = -1;
-    res = PyLong_FromOff_t(n);
-    if (res != NULL && self->readable)
-        _bufferedreader_reset_buf(self);
-
-end:
     LEAVE_BUFFERED(self)
     return res;
 }
@@ -1926,6 +1937,27 @@ _bufferedwriter_reset_buf(buffered *self)
     self->write_end = -1;
 }
 
+static void
+_bufferedwriter_set_append(buffered *self)
+{
+    PyObject *mode;
+
+    mode = _PyObject_GetAttrId(self->raw, &PyId_mode);
+    if (mode != NULL) {
+        /* Raw fileobj has no mode string so as far as we can know it has
+           normal write behavior */
+        if (PyUnicode_FindChar(mode, 'a', 0, PyUnicode_GET_LENGTH(mode), 1) != -1) {
+            self->appending = 1;
+        } else {
+            self->appending = 0;
+        }
+        Py_DECREF(mode);
+    } else {
+        PyErr_Clear();
+        self->appending = 0;
+    }
+}
+
 /*[clinic input]
 _io.BufferedWriter.__init__
     raw: object
@@ -1955,6 +1987,8 @@ _io_BufferedWriter___init___impl(buffered *self, PyObject *raw,
     Py_XSETREF(self->raw, raw);
     self->readable = 0;
     self->writable = 1;
+
+    _bufferedwriter_set_append(self);
 
     self->buffer_size = buffer_size;
     if (_buffered_init(self) < 0)
@@ -2111,6 +2145,14 @@ _io_BufferedWriter_write_impl(buffered *self, Py_buffer *buffer)
         self->pos = 0;
         self->raw_pos = 0;
     }
+
+    if (self->appending) {
+        res = _buffered_seek_unlocked(self, 0, SEEK_END);
+        if (res == NULL)
+            goto error;
+        Py_DECREF(res);
+    }
+
     avail = Py_SAFE_DOWNCAST(self->buffer_size - self->pos, Py_off_t, Py_ssize_t);
     if (buffer->len <= avail && buffer->len < self->buffer_size) {
         memcpy(self->buffer + self->pos, buffer->buf, buffer->len);
@@ -2503,6 +2545,8 @@ _io_BufferedRandom___init___impl(buffered *self, PyObject *raw,
     self->buffer_size = buffer_size;
     self->readable = 1;
     self->writable = 1;
+
+    _bufferedwriter_set_append(self);
 
     if (_buffered_init(self) < 0)
         return -1;
