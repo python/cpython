e23dbe6311774f144b08aa9e7e6c51dd578133d1
Nathan M <nathanmaynes@gmail.com>
2020-08-06 18:09:40 -0400

    bpo-41371: Handle lzma lib import error in test_zoneinfo.py (GH-21734)

--- Lib/test/test_zoneinfo/test_zoneinfo.py
+++ Lib/test/test_zoneinfo/test_zoneinfo.py
@@ -6,7 +6,6 @@ import dataclasses
 import importlib.metadata
 import io
 import json
-import lzma
 import os
 import pathlib
 import pickle
@@ -20,7 +19,9 @@ from functools import cached_property
 
 from . import _support as test_support
 from ._support import OS_ENV_LOCK, TZPATH_TEST_LOCK, ZoneInfoTestBase
+from test.support.import_helper import import_module
 
+lzma = import_module('lzma')
 py_zoneinfo, c_zoneinfo = test_support.get_modules()
 
 try:
