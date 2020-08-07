2e46cf9d619dc43c4fba19143e137e4c9d1752b5
Inada Naoki <songofacandy@gmail.com>
2020-08-07 16:31:53 +0900

    bpo-41098: Doc: Add missing deprecated directives (GH-21162)
    PyUnicodeEncodeError_Create has been deprecated with
    `Py_DEPRECATED` macro. But it was not documented.
    (cherry picked from commit 46e19b61d31ba99f049258efa4ff1334856a3643)
    
    Co-authored-by: Inada Naoki <songofacandy@gmail.com>

--- Include/cpython/pyerrors.h
+++ Include/cpython/pyerrors.h
@@ -148,7 +148,10 @@ PyAPI_FUNC(PyObject *) PyErr_ProgramTextObject(
     PyObject *filename,
     int lineno);
 
-/* Create a UnicodeEncodeError object */
+/* Create a UnicodeEncodeError object.
+ *
+ * TODO: This API will be removed in Python 3.11.
+ */
 Py_DEPRECATED(3.3) PyAPI_FUNC(PyObject *) PyUnicodeEncodeError_Create(
     const char *encoding,       /* UTF-8 encoded string */
     const Py_UNICODE *object,
@@ -158,7 +161,10 @@ Py_DEPRECATED(3.3) PyAPI_FUNC(PyObject *) PyUnicodeEncodeError_Create(
     const char *reason          /* UTF-8 encoded string */
     );
 
-/* Create a UnicodeTranslateError object */
+/* Create a UnicodeTranslateError object.
+ *
+ * TODO: This API will be removed in Python 3.11.
+ */
 Py_DEPRECATED(3.3) PyAPI_FUNC(PyObject *) PyUnicodeTranslateError_Create(
     const Py_UNICODE *object,
     Py_ssize_t length,
