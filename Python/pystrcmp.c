597fe28c4190de5239fd281c948b29917a59a12f
William Meehan <wmeehan@fb.com>
2020-08-12 13:46:19 -0400

    Address comments

--- Python/pystrcmp.c
+++ Python/pystrcmp.c
@@ -9,11 +9,11 @@ PyOS_mystrnicmp(const char *s1, const char *s2, Py_ssize_t size)
     const unsigned char *p1, *p2;
     if (size == 0)
         return 0;
-    p1 = (void *)s1;
-    p2 = (void *)s2;
-    for (; (--size > 0) && (tolower(*p1) == tolower(*p2)); p1++, p2++) {
-        if (!*p1 || !*p2)
-            break;
+    p1 = (const unsigned char *)s1;
+    p2 = (const unsigned char *)s2;
+    for (; (--size > 0) && *p1 && *p2 && (tolower(*p1) == tolower(*p2));
+         p1++, p2++) {
+        ;
     }
     return tolower(*p1) - tolower(*p2);
 }
@@ -21,8 +21,9 @@ PyOS_mystrnicmp(const char *s1, const char *s2, Py_ssize_t size)
 int
 PyOS_mystricmp(const char *s1, const char *s2)
 {
-    const unsigned char *p1 = (void *)s1, *p2 = (void *)s2;
-    for (; *p1 && (tolower(*p1) == tolower(*p2)); p1++, p2++) {
+    const unsigned char *p1 = (const unsigned char *)s1;
+    const unsigned char *p2 = (const unsigned char *)s2;
+    for (; *p1 && *p2 && (tolower(*p1) == tolower(*p2)); p1++, p2++) {
         ;
     }
     return (tolower(*p1) - tolower(*p2));
