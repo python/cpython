d102f29a4124c119ae0f07ce4dd967c2f7bf90fd
William Meehan <wmeehan@fb.com>
2020-08-12 11:57:13 -0400

    bpo-41524: fix pointer bug in PyOS_mystr{n}icmp
    The existing implementations of PyOS_mystrnicmp and PyOS_mystricmp
    can increment pointers beyond the end of a string.
    
    This commit fixes those cases by moving the mutation out of the condition.

--- Python/pystrcmp.c
+++ Python/pystrcmp.c
@@ -6,21 +6,24 @@
 int
 PyOS_mystrnicmp(const char *s1, const char *s2, Py_ssize_t size)
 {
+    const unsigned char *p1, *p2;
     if (size == 0)
         return 0;
-    while ((--size > 0) &&
-           (tolower((unsigned)*s1) == tolower((unsigned)*s2))) {
-        if (!*s1++ || !*s2++)
+    p1 = (void *)s1;
+    p2 = (void *)s2;
+    for (; (--size > 0) && (tolower(*p1) == tolower(*p2)); p1++, p2++) {
+        if (!*p1 || !*p2)
             break;
     }
-    return tolower((unsigned)*s1) - tolower((unsigned)*s2);
+    return tolower(*p1) - tolower(*p2);
 }
 
 int
 PyOS_mystricmp(const char *s1, const char *s2)
 {
-    while (*s1 && (tolower((unsigned)*s1++) == tolower((unsigned)*s2++))) {
+    const unsigned char *p1 = (void *)s1, *p2 = (void *)s2;
+    for (; *p1 && (tolower(*p1) == tolower(*p2)); p1++, p2++) {
         ;
     }
-    return (tolower((unsigned)*s1) - tolower((unsigned)*s2));
+    return (tolower(*p1) - tolower(*p2));
 }
