b4cdc05f3dd8ae8620fb0ac2919546d334d9a582
Rishav Kundu <rk@rishav.io>
2020-08-14 07:03:14 +0530

    bpo-41410: Fix outdated info in mkstemp docs (GH-21701)
    Automerge-Triggered-By: @ericvsmith
    (cherry picked from commit e55de68be3e5b977a17d3c0ac9805b0feff8fedc)
    
    Co-authored-by: Rishav Kundu <rk@rishav.io>

--- Lib/tempfile.py
+++ Lib/tempfile.py
@@ -307,8 +307,7 @@ def mkstemp(suffix=None, prefix=None, dir=None, text=False):
     otherwise a default directory is used.
 
     If 'text' is specified and true, the file is opened in text
-    mode.  Else (the default) the file is opened in binary mode.  On
-    some operating systems, this makes no difference.
+    mode.  Else (the default) the file is opened in binary mode.
 
     If any of 'suffix', 'prefix' and 'dir' are not None, they must be the
     same type.  If they are bytes, the returned name will be bytes; str
