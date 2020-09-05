efeb0e402b1f651223325c70fd57358b5d505d03
llamafilm <3991046+llamafilm@users.noreply.github.com>
2020-09-05 16:52:59 -0700

    Debug logging when adding TarInfo object
    Use debug level 3 which is currently unused although it is documented.

--- Lib/tarfile.py
+++ Lib/tarfile.py
@@ -2008,6 +2008,8 @@ class TarFile(object):
                 self.fileobj.write(NUL * (BLOCKSIZE - remainder))
                 blocks += 1
             self.offset += blocks * BLOCKSIZE
+        
+        self._dbg(3, "%s, size: %i, mtime: %.0f, mode: %i, uname: %s, gname: %s" % (tarinfo.name, tarinfo.size, tarinfo.mtime, tarinfo.mode, tarinfo.uname, tarinfo.gname))
 
         self.members.append(tarinfo)
 
