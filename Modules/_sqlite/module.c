9226ab5992b755e74712011c7bf53d9975f7ac26
Erlend E. Aasland <erlend.aasland@innova.no>
2020-09-08 23:16:47 +0200

    Convert RowType to heap type

--- Modules/_sqlite/module.c
+++ Modules/_sqlite/module.c
@@ -352,7 +352,7 @@ PyMODINIT_FUNC PyInit__sqlite3(void)
     module = PyModule_Create(&_sqlite3module);
 
     if (!module ||
-        (pysqlite_row_setup_types() < 0) ||
+        (pysqlite_row_setup_types(module) < 0) ||
         (pysqlite_cursor_setup_types() < 0) ||
         (pysqlite_connection_setup_types() < 0) ||
         (pysqlite_cache_setup_types(module) < 0) ||
@@ -366,7 +366,7 @@ PyMODINIT_FUNC PyInit__sqlite3(void)
     ADD_TYPE(module, pysqlite_ConnectionType);
     ADD_TYPE(module, pysqlite_CursorType);
     ADD_TYPE(module, *pysqlite_PrepareProtocolType);
-    ADD_TYPE(module, pysqlite_RowType);
+    ADD_TYPE(module, *pysqlite_RowType);
 
     if (!(dict = PyModule_GetDict(module))) {
         goto error;
