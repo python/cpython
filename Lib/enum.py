6b1e590a119f2107986129f7eb59640b9eead4fe
Zackery Spytz <zspytz@gmail.com>
2020-09-13 14:27:51 -0600

    bpo-38967: Improve the error msg for reserved _sunder_ names in enum (GH-18370)

--- Lib/enum.py
+++ Lib/enum.py
@@ -76,7 +76,8 @@ class _EnumDict(dict):
                     '_order_', '_create_pseudo_member_',
                     '_generate_next_value_', '_missing_', '_ignore_',
                     ):
-                raise ValueError('_names_ are reserved for future Enum use')
+                raise ValueError(f'_sunder_ names, such as "{key}", are '
+                                  'reserved for future Enum use')
             if key == '_generate_next_value_':
                 # check if members already defined as auto()
                 if self._auto_called:
