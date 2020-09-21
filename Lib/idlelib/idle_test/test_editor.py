8dfbd49b3a5a9ab338f020aab9a5d6377e7ad6f6
Terry Jan Reedy <tjreedy@udel.edu>
2020-09-20 21:46:54 -0400

    Remove test of removed _filename_to_unicode method.

--- Lib/idlelib/idle_test/test_editor.py
+++ Lib/idlelib/idle_test/test_editor.py
@@ -33,15 +33,6 @@ def tearDownModule():
     del root
 
 
-class Editor_func_test(unittest.TestCase):
-    def test_filename_to_unicode(self):
-        func = editor.EditorWindow._filename_to_unicode
-        class dummy(): filesystemencoding = 'utf-8'
-        pairs = (('abc', 'abc'), ('a\U00011111c', 'a\ufffdc'),
-                 (b'abc', 'abc'), (b'a\xf0\x91\x84\x91c', 'a\ufffdc'))
-        for inp, out in pairs:
-            self.assertEqual(func(dummy, inp), out)
-
 class EditorWindowTest(unittest.TestCase):
 
     @classmethod
