626af398de36723d29d8d27e8f9c4d736c975030
Erlend E. Aasland <erlend.aasland@innova.no>
2020-09-26 22:16:50 +0200

    Remove superfluous Python.h include

--- Modules/_sqlite/microprotocols.c
+++ Modules/_sqlite/microprotocols.c
@@ -23,8 +23,6 @@
  * 3. This notice may not be removed or altered from any source distribution.
  */
 
-#include <Python.h>
-
 #include "cursor.h"
 #include "microprotocols.h"
 #include "prepare_protocol.h"
