932c1430187b141da33c793b9404c9b3b68c6096
Erlend Egeberg Aasland <erlend.aasland@innova.no>
2020-09-27 14:14:50 +0200

    bpo-41861: Convert _sqlite3 cache and node static types to heap types (GH-22417)

--- Modules/_sqlite/module.c
+++ Modules/_sqlite/module.c
@@ -355,7 +355,7 @@ PyMODINIT_FUNC PyInit__sqlite3(void)
         (pysqlite_row_setup_types() < 0) ||
         (pysqlite_cursor_setup_types() < 0) ||
         (pysqlite_connection_setup_types() < 0) ||
-        (pysqlite_cache_setup_types() < 0) ||
+        (pysqlite_cache_setup_types(module) < 0) ||
         (pysqlite_statement_setup_types() < 0) ||
         (pysqlite_prepare_protocol_setup_types() < 0)
        ) {
