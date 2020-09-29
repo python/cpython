b53b34e57e6bec59ddb6175b21ce6d4ca6695feb
Anatoliy Platonov <p4m.dev@gmail.com>
2020-09-29 16:05:13 +0300

    Add test for font representation

--- Lib/tkinter/test/test_tkinter/test_font.py
+++ Lib/tkinter/test/test_tkinter/test_font.py
@@ -100,6 +100,10 @@ class FontTest(AbstractTkTest, unittest.TestCase):
             self.assertIsInstance(name, str)
             self.assertTrue(name)
         self.assertIn(fontname, names)
+    
+    def test_repr(self):
+        assert repr(self.font) == f'<{self.font.__class__.__module__}.{self.__class__.__qualname__} object {fontname!r}>'
+        
 
 tests_gui = (FontTest, )
 
