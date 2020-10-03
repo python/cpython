84ca5e54d2ddbe2245c2c5a6543a0fd904dc986d
Ram Rachum <ram@rachum.com>
2020-10-03 13:43:47 +0300

    bpo-41867: List options for timespec in docstrings of isoformat methods (GH-22418) (cherry picked from commit 52301312bb9de2299b0c42468fd1936d869e651c)
    Co-authored-by: Ram Rachum <ram@rachum.com>

--- Lib/datetime.py
+++ Lib/datetime.py
@@ -1418,7 +1418,8 @@ class time:
         part is omitted if self.microsecond == 0.
 
         The optional argument timespec specifies the number of additional
-        terms of the time to include.
+        terms of the time to include. Valid options are 'auto', 'hours',
+        'minutes', 'seconds', 'milliseconds' and 'microseconds'.
         """
         s = _format_time(self._hour, self._minute, self._second,
                           self._microsecond, timespec)
@@ -1902,7 +1903,8 @@ class datetime(date):
         time, default 'T'.
 
         The optional argument timespec specifies the number of additional
-        terms of the time to include.
+        terms of the time to include. Valid options are 'auto', 'hours',
+        'minutes', 'seconds', 'milliseconds' and 'microseconds'.
         """
         s = ("%04d-%02d-%02d%c" % (self._year, self._month, self._day, sep) +
              _format_time(self._hour, self._minute, self._second,
