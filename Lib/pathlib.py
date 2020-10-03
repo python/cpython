114830f50548c390ebe17567ed7773a0fd7da3d1
Ram Rachum <ram@rachum.com>
2020-10-03 12:52:13 +0300

    bpo-40833: Clarify Path.rename doc-string regarding relative paths (GH-20554) (cherry picked from commit f97e42ef4d97dee64f45ed65170a6e77c8e46fdf)
    Co-authored-by: Ram Rachum <ram@rachum.com>

--- Lib/pathlib.py
+++ Lib/pathlib.py
@@ -1353,8 +1353,13 @@ class Path(PurePath):
 
     def rename(self, target):
         """
-        Rename this path to the given path,
-        and return a new Path instance pointing to the given path.
+        Rename this path to the target path.
+
+        The target path may be absolute or relative. Relative paths are
+        interpreted relative to the current working directory, *not* the
+        directory of the Path object.
+
+        Returns the new Path instance pointing to the target path.
         """
         if self._closed:
             self._raise_closed()
@@ -1363,9 +1368,13 @@ class Path(PurePath):
 
     def replace(self, target):
         """
-        Rename this path to the given path, clobbering the existing
-        destination if it exists, and return a new Path instance
-        pointing to the given path.
+        Rename this path to the target path, overwriting if that path exists.
+
+        The target path may be absolute or relative. Relative paths are
+        interpreted relative to the current working directory, *not* the
+        directory of the Path object.
+
+        Returns the new Path instance pointing to the target path.
         """
         if self._closed:
             self._raise_closed()
