c6f41e62f55933f9735e9b9c103c6860685b028a
Karthikeyan Singaravelan <tir.karthi@gmail.com>
2020-10-09 15:38:42 +0530

    [3.9] bpo-41970: Avoid test failure in test_lib2to3 if the module is already imported (GH-22595) (GH-22609)
    (cherry picked from commit 4a9f82f50d957b6cf3fd207de8b583d9137316b8)
    
    Co-authored-by: Pablo Galindo <Pablogsal@gmail.com>

--- Lib/test/test_lib2to3.py
+++ Lib/test/test_lib2to3.py
@@ -1,8 +1,8 @@
 import unittest
-from test.support import check_warnings
+from test.support import check_warnings, import_fresh_module
 
 with check_warnings(("", PendingDeprecationWarning)):
-    from lib2to3.tests import load_tests
+    load_tests = import_fresh_module('lib2to3.tests', fresh=['lib2to3']).load_tests
 
 if __name__ == '__main__':
     unittest.main()
