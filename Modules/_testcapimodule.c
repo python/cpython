da7a55b454884b1520a1feceed36d6958e6121d0
Vladimir Matveev <vladima@fb.com>
2020-10-09 17:15:15 -0700

    bpo-41756: Add PyIter_Send function (#22443)

--- Modules/_testcapimodule.c
+++ Modules/_testcapimodule.c
@@ -5028,6 +5028,7 @@ dict_get_version(PyObject *self, PyObject *args)
 static PyObject *
 raise_SIGINT_then_send_None(PyObject *self, PyObject *args)
 {
+    _Py_IDENTIFIER(send);
     PyGenObject *gen;
 
     if (!PyArg_ParseTuple(args, "O!", &PyGen_Type, &gen))
@@ -5044,7 +5045,7 @@ raise_SIGINT_then_send_None(PyObject *self, PyObject *args)
          because we check for signals before every bytecode operation.
      */
     raise(SIGINT);
-    return _PyGen_Send(gen, Py_None);
+    return _PyObject_CallMethodIdOneArg((PyObject *)gen, &PyId_send, Py_None);
 }
 
 
