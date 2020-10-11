db6c08471fda67b462bb5890919f3bed1913cb8b
chilaxan <chilaxan@gmail.com>
2020-10-11 14:21:51 -0400

    Fix typo in listobject.h (GH-22588)

--- Include/cpython/listobject.h
+++ Include/cpython/listobject.h
@@ -26,7 +26,7 @@ PyAPI_FUNC(void) _PyList_DebugMallocStats(FILE *out);
 
 /* Macro, trading safety for speed */
 
-/* Cast argument to PyTupleObject* type. */
+/* Cast argument to PyListObject* type. */
 #define _PyList_CAST(op) (assert(PyList_Check(op)), (PyListObject *)(op))
 
 #define PyList_GET_ITEM(op, i) (_PyList_CAST(op)->ob_item[i])
