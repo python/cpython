f97cec2cda3c3e6bc1085bedf1b11866ec40fed7
Vivek Kumar Srivastava <43187055+Viv786ek@users.noreply.github.com>
2020-10-12 11:29:33 +0530

    Create Py.c

---
+++ Py.c
@@ -0,0 +1,18 @@
+/* Minimal Main Program :- Everything is loaded from the library. */
+
+
+#include "Python.h"
+
+#ifdef MS_WINDOWS
+int
+wmain(int argc, wchar_t **argv)
+{
+    return Py_Main(argc, argv);
+}
+#else
+int
+main(int argc, char **argv)
+{
+    return Py_BytesMain(argc, argv);
+}
+#endif
