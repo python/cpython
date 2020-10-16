52335445dc875579f9b52b3e44b93be1c4d286ea
Bar Harel <bzvi7919@gmail.com>
2020-10-16 22:44:53 +0300

    add test

--- Lib/test/test_sched.py
+++ Lib/test/test_sched.py
@@ -142,6 +142,16 @@ class TestCase(unittest.TestCase):
         self.assertTrue(q.empty())
         self.assertEqual(timer.time(), 4)
 
+    def test_cancel_correct_event(self):
+        # bpo-19270
+        events = []
+        scheduler = sched.scheduler()
+        scheduler.enterabs(1, 1, events.append, ("a",))
+        b = scheduler.enterabs(1, 1, events.append, ("b",))
+        scheduler.cancel(b)
+        scheduler.run()
+        self.assertEqual(events[0], "a")
+
     def test_empty(self):
         l = []
         fun = lambda x: l.append(x)
