5fce619a4e9e2f81e13c12f3be721e222f6dc912
Evan Wang <evnw1409@hotmail.com>
2020-10-17 18:25:36 -0400

    Update test_fileio.py

--- Lib/test/test_fileio.py
+++ Lib/test/test_fileio.py
@@ -74,19 +74,17 @@ class AutoFileTests:
         self.assertEqual(self.f._blksize, blksize)
 
     def testReadWithWritingMode(self):
-        r, w = os.pipe()
-        w = os.fdopen(w, "w")
-        w.write("hello")
-        w.close()
+        with os.pipe() as r, w:
+            w = os.fdopen(w, "w")
+            w.write("hello")
         with io.FileIO(r, mode="w") as f:
             with self.assertRaises(_io.UnsupportedOperation):
                 f.read()
 
     def testReadallWithWritingMode(self):
-        r, w = os.pipe()
-        w = os.fdopen(w, "w")
-        w.write("hello")
-        w.close()
+        with os.pipe() as r, w
+            w = os.fdopen(w, "w")
+            w.write("hello")
         with io.FileIO(r, mode="w") as f:
             with self.assertRaises(_io.UnsupportedOperation):
                 f.readall()
