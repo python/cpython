c620b7846c7deb8222b68657e845643f758190c9
Pablo Galindo <Pablogsal@gmail.com>
2020-10-20 06:22:44 +0100

    bpo-42093: Add opcode cache for LOAD_ATTR (GH-22803)

--- Include/cpython/dictobject.h
+++ Include/cpython/dictobject.h
@@ -71,6 +71,7 @@ PyAPI_FUNC(void) _PyDict_DebugMallocStats(FILE *out);
 
 int _PyObjectDict_SetItem(PyTypeObject *tp, PyObject **dictptr, PyObject *name, PyObject *value);
 PyObject *_PyDict_LoadGlobal(PyDictObject *, PyDictObject *, PyObject *);
+Py_ssize_t _PyDict_GetItemHint(PyDictObject *, PyObject *, Py_ssize_t, PyObject **);
 
 /* _PyDictView */
 
