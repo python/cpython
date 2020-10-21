fca8adc5b0bfb7f1f24f1fd9ca7805c4f35bd6b3
Lumir Balhar <lbalhar@redhat.com>
2020-10-21 14:10:30 +0200

    Improve or create setUp/tearDown methods for tests where sysconfig._CONFIG_VARS might be modified

--- Lib/distutils/tests/test_build_ext.py
+++ Lib/distutils/tests/test_build_ext.py
@@ -35,6 +35,7 @@ class BuildExtTestCase(TempdirManager,
         site.USER_BASE = self.mkdtemp()
         from distutils.command import build_ext
         build_ext.USER_BASE = site.USER_BASE
+        self.old_config_vars = dict(sysconfig._config_vars)
 
         # bpo-30132: On Windows, a .pdb file may be created in the current
         # working directory. Create a temporary working directory to cleanup
@@ -48,6 +49,8 @@ class BuildExtTestCase(TempdirManager,
         site.USER_BASE = self.old_user_base
         from distutils.command import build_ext
         build_ext.USER_BASE = self.old_user_base
+        sysconfig._config_vars.clear()
+        sysconfig._config_vars.update(self.old_config_vars)
         super(BuildExtTestCase, self).tearDown()
 
     def build_ext(self, *args, **kwargs):
