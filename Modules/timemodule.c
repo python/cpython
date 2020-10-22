587e53e5b3f210e7473330b922950544cbdbd9de
Ronald Oussoren <ronaldoussoren@mac.com>
2020-10-22 20:52:59 +0200

    One more attempt at building on Linux...

--- Modules/timemodule.c
+++ Modules/timemodule.c
@@ -51,9 +51,12 @@
 #define _Py_tzname tzname
 #endif
 
-#if defined(__APPLE__ ) && defined(__has_builtin) && __has_builtin(__builtin_available)
-#  define HAVE_CLOCK_GETTIME_RUNTIME __builtin_available(macOS 10.12, iOS 10.0, tvOS 10.0, watchOS 3.0, *)
-#else
+#if defined(__APPLE__ ) && defined(__has_builtin) 
+#  if __has_builtin(__builtin_available)
+#    define HAVE_CLOCK_GETTIME_RUNTIME __builtin_available(macOS 10.12, iOS 10.0, tvOS 10.0, watchOS 3.0, *)
+#  endif
+#endif
+#ifndef HAVE_CLOCK_GETTIME_RUNTIME
 #  define HAVE_CLOCK_GETTIME_RUNTIME 1
 #endif
 
