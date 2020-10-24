1e268cb751d8350d754aa1a887524e3d146617cd
Lisa Roach <lisroach@fb.com>
2020-10-24 15:32:55 -0700

    Moves System and Keyboard exception to use raise, Cancelled does not.

--- Lib/unittest/test/test_async_case.py
+++ Lib/unittest/test/test_async_case.py
@@ -193,21 +193,30 @@ class TestAsyncCase(unittest.TestCase):
     def test_base_exception_from_async_method(self):
         events = []
         class Test(unittest.IsolatedAsyncioTestCase):
-            async def test_a(self):
-                events.append("test_a")
+            async def test_base(self):
+                events.append("test_base")
                 raise BaseException()
                 events.append("not it")
 
-            async def test_b(self):
-                events.append("test_b")
+            async def test_no_err(self):
+                events.append("test_no_err")
 
-        test = Test("test_a")
+            async def test_cancel(self):
+                raise asyncio.CancelledError()
+
+        test = Test("test_base")
         output = test.run()
         self.assertFalse(output.wasSuccessful())
 
-        test = Test("test_b")
+        test = Test("test_no_err")
         test.run()
-        self.assertEqual(events, ['test_a', 'test_b'])
+        self.assertEqual(events, ['test_base', 'test_no_err'])
+
+        test = Test("test_cancel")
+        output = test.run()
+        self.assertFalse(output.wasSuccessful())
+
+
 
 if __name__ == "__main__":
     unittest.main()
