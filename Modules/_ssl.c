83c86cf54b36a7325f615f5adf22b28e48f0e72d
Miss Skeleton (bot) <31488909+miss-islington@users.noreply.github.com>
2020-10-25 11:23:09 -0700

    bpo-42144: Add a missing "goto error;" in the _ssl module (GH-22959)
    (cherry picked from commit c32f2976b8f4034724c3270397aa16f38daf470f)
    
    Co-authored-by: Zackery Spytz <zspytz@gmail.com>

--- Modules/_ssl.c
+++ Modules/_ssl.c
@@ -899,6 +899,7 @@ _ssl_configure_hostname(PySSLSocket *self, const char* server_hostname)
     if (ip == NULL) {
         if (!SSL_set_tlsext_host_name(self->ssl, server_hostname)) {
             _setSSLError(NULL, 0, __FILE__, __LINE__);
+            goto error;
         }
     }
     if (self->ctx->check_hostname) {
