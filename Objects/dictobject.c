4b1f8f482adec7d5c8c8baba709fcbd3e1ed5192
Marco Sulla <github@marco.sulla.e4ward.com>
2020-10-30 15:31:10 +0100

    supporting only combined dicts

--- Objects/dictobject.c
+++ Objects/dictobject.c
@@ -1160,7 +1160,7 @@ Fail:
 // you have to change it after calling this function (probably at the end of 
 // a loop)
 static int
-insertdict_init(PyDictObject *mp, PyObject *key, Py_hash_t hash, PyObject *value, int empty)
+insertdict_init(PyDictObject *mp, PyObject *key, Py_hash_t hash, PyObject *value, int not_empty)
 {
     PyObject *old_value = NULL;
     PyDictKeyEntry *ep;
@@ -1171,17 +1171,17 @@ insertdict_init(PyDictObject *mp, PyObject *key, Py_hash_t hash, PyObject *value
     Py_INCREF(value);
     MAINTAIN_TRACKING(mp, key, value);
 
-    if (! empty) {
+    if (not_empty) {
         ix = keys->dk_lookup(mp, key, hash, &old_value);
         if (ix == DKIX_ERROR)
             goto Fail;
 
         assert(PyUnicode_CheckExact(key) || keys->dk_lookup == lookdict);
 
-        empty = (ix == DKIX_EMPTY);
+        not_empty = (ix != DKIX_EMPTY);
     }
     
-    if (empty) {
+    if (! not_empty) {
         /* Insert into new slot. */
         assert(old_value == NULL);
         const Py_ssize_t hashpos = find_empty_slot(keys, hash);
@@ -1190,13 +1190,7 @@ insertdict_init(PyDictObject *mp, PyObject *key, Py_hash_t hash, PyObject *value
         dictkeys_set_index(keys, hashpos, dk_nentries);
         ep->me_key = key;
         ep->me_hash = hash;
-        if (mp->ma_values) {
-            assert (mp->ma_values[dk_nentries] == NULL);
-            mp->ma_values[dk_nentries] = value;
-        }
-        else {
-            ep->me_value = value;
-        }
+        ep->me_value = value;
         mp->ma_used++;
         keys->dk_usable--;
         keys->dk_nentries++;
@@ -1206,18 +1200,8 @@ insertdict_init(PyDictObject *mp, PyObject *key, Py_hash_t hash, PyObject *value
     }
 
     if (old_value != value) {
-        if (_PyDict_HasSplitTable(mp)) {
-            mp->ma_values[ix] = value;
-            if (old_value == NULL) {
-                /* pending state */
-                assert(ix == mp->ma_used);
-                mp->ma_used++;
-            }
-        }
-        else {
-            assert(old_value != NULL);
-            DK_ENTRIES(keys)[ix].me_value = value;
-        }
+        assert(old_value != NULL);
+        DK_ENTRIES(keys)[ix].me_value = value;
     }
     Py_XDECREF(old_value); /* which **CAN** re-enter (see issue #22653) */
     ASSERT_CONSISTENT(mp);
@@ -1664,7 +1648,7 @@ PyDict_SetItem(PyObject *op, PyObject *key, PyObject *value)
 
 // See insertdict_init()
 static int
-dict_set_item_init(PyObject *op, PyObject *key, PyObject *value, int empty)
+dict_set_item_init(PyObject *op, PyObject *key, PyObject *value, int not_empty)
 {
     Py_hash_t hash;
     assert(PyDict_Check(op));
@@ -1679,7 +1663,7 @@ dict_set_item_init(PyObject *op, PyObject *key, PyObject *value, int empty)
             return -1;
     }
 
-    return insertdict_init(mp, key, hash, value, empty);
+    return insertdict_init(mp, key, hash, value, not_empty);
 }
 
 int
@@ -3544,15 +3528,12 @@ dict_vectorcall(PyObject *type, PyObject * const*args,
         return NULL;
     }
 
-    int empty = 1;
-
     if (nargs == 1) {
         if (dict_update_arg(self, args[0]) < 0) {
             Py_DECREF(self);
             return NULL;
         }
 
-        empty = 0;
         args++;
     }
 
@@ -3568,11 +3549,21 @@ dict_vectorcall(PyObject *type, PyObject * const*args,
             }
         }
 
-        for (Py_ssize_t i = 0; i < kw_size; i++) {
-            if (dict_set_item_init(self, PyTuple_GET_ITEM(kwnames, i), args[i], empty) < 0) {
-                Py_DECREF(self);
-                return NULL;
-            }
+        if (_PyDict_HasSplitTable(mp)) {
+	        for (Py_ssize_t i = 0; i < kw_size; i++) {
+	            if (PyDict_SetItem(self, PyTuple_GET_ITEM(kwnames, i), args[i]) < 0) {
+	                Py_DECREF(self);
+	                return NULL;
+	            }
+	        }
+        }
+        else {
+	        for (Py_ssize_t i = 0; i < kw_size; i++) {
+	            if (dict_set_item_init(self, PyTuple_GET_ITEM(kwnames, i), args[i], nargs) < 0) {
+	                Py_DECREF(self);
+	                return NULL;
+	            }
+	        }
         }
 
         mp->ma_version_tag = DICT_NEXT_VERSION();
