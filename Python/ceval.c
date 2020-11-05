b5da42dcd9ca6e2d7b50fceb4a11136dd49f523b
Pablo Galindo <Pablogsal@gmail.com>
2020-11-05 09:23:15 +0000

    bpo-42266: Handle monkey-patching descriptors in LOAD_ATTR cache (GH-23157)

--- Python/ceval.c
+++ Python/ceval.c
@@ -3179,7 +3179,6 @@ main_loop:
                 if (co_opcache != NULL && /* co_opcache can be NULL after a DEOPT() call. */
                     type->tp_getattro == PyObject_GenericGetAttr)
                 {
-                    PyObject *descr;
                     Py_ssize_t ret;
 
                     if (type->tp_dictoffset > 0) {
@@ -3190,12 +3189,7 @@ main_loop:
                                 goto error;
                             }
                         }
-
-                        descr = _PyType_Lookup(type, name);
-                        if (descr == NULL ||
-                            Py_TYPE(descr)->tp_descr_get == NULL ||
-                            !PyDescr_IsData(descr))
-                        {
+                        if (_PyType_Lookup(type, name) == NULL) {
                             dictptr = (PyObject **) ((char *)owner + type->tp_dictoffset);
                             dict = *dictptr;
 
