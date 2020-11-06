4fcf1e02d9c1f317ecd0ea5c7cd63276bdd67abe
Eric Snow <ericsnowcurrently@gmail.com>
2020-11-06 15:50:45 -0700

    Clean up "ignored" rows.

--- Tools/c-analyzer/c_analyzer/datafiles.py
+++ Tools/c-analyzer/c_analyzer/datafiles.py
@@ -96,8 +96,14 @@ def read_ignored(infile, relroot=fsutil.USE_CWD):
 def _iter_ignored(infile, relroot):
     if relroot and relroot is not fsutil.USE_CWD:
         relroot = os.path.abspath(relroot)
+    bogus = {_tables.EMPTY, _tables.UNKNOWN}
     for row in _tables.read_table(infile, IGNORED_HEADER, sep='\t'):
         *varidinfo, reason = row
+        if _tables.EMPTY in varidinfo or _tables.UNKNOWN in varidinfo:
+            varidinfo = tuple(None if v in bogus else v
+                              for v in varidinfo)
+        if reason in bogus:
+            reason = None
         varid = _info.DeclID.from_row(varidinfo)
         varid = varid.fix_filename(relroot, formatted=False, fixroot=False)
         yield varid, reason
