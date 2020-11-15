7014f966489cb7b581eed73c2724527f27546adb
Serhiy Storchaka <storchaka@gmail.com>
2020-11-15 14:33:37 +0200

    Fix possible leaks in xxlimited

--- Modules/xxlimited.c
+++ Modules/xxlimited.c
@@ -252,29 +252,29 @@ xx_modexec(PyObject *m)
     /* Add some symbolic constants to the module */
     if (ErrorObject == NULL) {
         ErrorObject = PyErr_NewException("xxlimited.error", NULL, NULL);
-        if (ErrorObject == NULL)
-            goto fail;
     }
-    Py_INCREF(ErrorObject);
-    PyModule_AddObject(m, "error", ErrorObject);
+    Py_XINCREF(ErrorObject);
+    if (PyModule_Add(m, "error", ErrorObject) < 0) {
+        goto fail;
+    }
 
     /* Add Xxo */
     o = PyType_FromSpec(&Xxo_Type_spec);
-    if (o == NULL)
+    if (PyModule_Add(m, "Xxo", o) < 0) {
         goto fail;
-    PyModule_AddObject(m, "Xxo", o);
+    }
 
     /* Add Str */
     o = PyType_FromSpec(&Str_Type_spec);
-    if (o == NULL)
+    if (PyModule_Add(m, "Str", o) < 0) {
         goto fail;
-    PyModule_AddObject(m, "Str", o);
+    }
 
     /* Add Null */
     o = PyType_FromSpec(&Null_Type_spec);
-    if (o == NULL)
+    if (PyModule_Add(m, "Null", o) < 0) {
         goto fail;
-    PyModule_AddObject(m, "Null", o);
+    }
     return 0;
  fail:
     Py_XDECREF(m);
