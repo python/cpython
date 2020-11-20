5f723dbcb8938ec86d5f729649dd9ba097c109f9
pxin <peixing.xin@windriver.com>
2020-11-20 15:10:28 +0800

    fix test_doctest.py failures for VxWorks

--- Lib/test/test_doctest.py
+++ Lib/test/test_doctest.py
@@ -3039,6 +3039,8 @@ Invalid file name:
     ...         '-m', 'doctest', 'nosuchfile')
     >>> rc, out
     (1, b'')
+    >>> if sys.platform == "vxworks":
+    ...     err = err.replace(b'no such file', b'No such file')
     >>> print(normalize(err))                    # doctest: +ELLIPSIS
     Traceback (most recent call last):
       ...
