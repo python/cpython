a1e5031985a7c0fc1b732c3b4a386e6c5a07c7fc
Alfred Perlstein <alfred@freebsd.org>
2020-11-22 10:31:08 -0800

    bpo-35753: fix expected output for basics() 1/3

--- Lib/test/test_doctest.py
+++ Lib/test/test_doctest.py
@@ -443,7 +443,7 @@ We'll simulate a __file__ attr that ends in pyc:
     >>> tests = finder.find(sample_func)
 
     >>> print(tests)  # doctest: +ELLIPSIS
-    [<DocTest sample_func from ...:27 (1 example)>]
+    [<DocTest sample_func from test_doctest.py:28 (1 example)>]
 
 The exact name depends on how test_doctest was invoked, so allow for
 leading path components.
