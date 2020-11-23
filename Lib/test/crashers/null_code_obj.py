bad51a7e3a8fadaf4d2a3a8ff9221dcb1c4d11ff
Sofian Brabez <sbz@6dev.net>
2020-11-23 16:31:01 +0000

    Restrict to crash 38 code only

--- Lib/test/crashers/null_code_obj.py
+++ Lib/test/crashers/null_code_obj.py
@@ -6,23 +6,11 @@ python versions. Currently there is no bytecode verifier to prevent those
 crashes related to malformed bytecode
 """
 
-
-def crash_27():
-    exec((lambda f:f).func_code.__class__(0,0,0,0,"\x01",(),(),(),"","",0,""))
-
-
-def crash_36():
-    exec((lambda f:f).__code__.__class__(0,0,0,0,0,b"\x01",(),(),(),"","",0,b""))
-
-
 def crash_38():
     exec((lambda f: f).__code__.__class__(0,0,0,0,0,0,b"\x01",(),(),(),"","",0,b""))
 
 
 def crash():
-    locals()['crash_27'] = crash_27
-    locals()['crash_36'] = crash_36
-    locals()['crash_37'] = crash_36
     locals()['crash_38'] = crash_38
     locals()['crash_39'] = crash_38
     locals()['crash_310'] = crash_38
