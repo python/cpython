4d4b4f4e5824424ce1795d49cbe55bd93b4345ed
Julien Palard <julien@palard.fr>
2020-11-25 10:23:17 +0100

    bpo-12800: tarfile: Restore fix from 011525ee9 (GH-21409)
    Restore fix from 011525ee92eb1c13ad1a62d28725a840e28f8160.

--- Lib/tarfile.py
+++ Lib/tarfile.py
@@ -2237,6 +2237,9 @@ class TarFile(object):
         try:
             # For systems that support symbolic and hard links.
             if tarinfo.issym():
+                if os.path.lexists(targetpath):
+                    # Avoid FileExistsError on following os.symlink.
+                    os.unlink(targetpath)
                 os.symlink(tarinfo.linkname, targetpath)
             else:
                 # See extract().
