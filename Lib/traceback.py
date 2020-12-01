ef9426c2b3a646e4c6d1aa46bc1857cd48b28ccc
Irit Katriel <iritkatriel@yahoo.com>
2020-12-01 01:35:25 +0000

    bpo-42482: remove reference to exc_traceback from TracebackException (GH-23531) (cherry picked from commit 427613f005f0f412d12f0d775d2b609bae0ae1ad)
    Co-authored-by: Irit Katriel <iritkatriel@yahoo.com>

--- Lib/traceback.py
+++ Lib/traceback.py
@@ -500,7 +500,6 @@ class TracebackException:
                 _seen=_seen)
         else:
             context = None
-        self.exc_traceback = exc_traceback
         self.__cause__ = cause
         self.__context__ = context
         self.__suppress_context__ = \
@@ -617,7 +616,7 @@ class TracebackException:
                 not self.__suppress_context__):
                 yield from self.__context__.format(chain=chain)
                 yield _context_message
-        if self.exc_traceback is not None:
+        if self.stack:
             yield 'Traceback (most recent call last):\n'
-        yield from self.stack.format()
+            yield from self.stack.format()
         yield from self.format_exception_only()
