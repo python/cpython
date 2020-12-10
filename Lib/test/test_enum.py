2324bdc5eb23e563389400f76ecdea5d5c5a1490
Ethan Furman <ethan@stoneleaf.us>
2020-12-09 17:12:11 -0800

    bpo-42517: [Enum] do not convert private names into members (GH-23722)
    private names, such as `_Color__hue` and `_Color__hue_` are now normal attributes, and do not become members nor raise exceptions
    (cherry picked from commit 7cf0aad96d1d20f07d7f0e374885f327c2d5ff27)
    
    Co-authored-by: Ethan Furman <ethan@stoneleaf.us>

--- Lib/test/test_enum.py
+++ Lib/test/test_enum.py
@@ -1135,6 +1135,7 @@ class TestEnum(unittest.TestCase):
         class auto_enum(type(Enum)):
             def __new__(metacls, cls, bases, classdict):
                 temp = type(classdict)()
+                temp._cls_name = cls
                 names = set(classdict._member_names)
                 i = 0
                 for k in classdict._member_names:
@@ -2035,6 +2036,30 @@ class TestEnum(unittest.TestCase):
         global_ns = {}
         local_ls = {}
         exec(code, global_ns, local_ls)
+
+
+    @unittest.skipUnless(
+            sys.version_info[:2] == (3, 9),
+            'private variables are now normal attributes',
+            )
+    def test_warning_for_private_variables(self):
+        with self.assertWarns(DeprecationWarning):
+            class Private(Enum):
+                __corporal = 'Radar'
+        self.assertEqual(Private._Private__corporal.value, 'Radar')
+        try:
+            with self.assertWarns(DeprecationWarning):
+                class Private(Enum):
+                    __major_ = 'Hoolihan'
+        except ValueError:
+            pass
+
+    def test_private_variable_is_normal_attribute(self):
+        class Private(Enum):
+            __corporal = 'Radar'
+            __major_ = 'Hoolihan'
+        self.assertEqual(Private._Private__corporal, 'Radar')
+        self.assertEqual(Private._Private__major_, 'Hoolihan')
 
 
 class TestOrder(unittest.TestCase):
