40188ccd52a31ae36763f11b0282039a59c45fd1
Fernando O <fochoao@pm.me>
2020-12-13 01:46:19 -0600

    OpenSSL Files of 1.1.1h
    OpenSSL Files of 1.1.1h x86 and x64, compiled by me.

---
+++ externals/openssl-1.1.1g/tmpARM64dll/test/buildtest_dtls1.c
@@ -0,0 +1,16 @@
+/*
+ * Generated with test/generate_buildtest.pl, to check that such a simple
+ * program builds.
+ */
+#include <openssl/opensslconf.h>
+#ifndef OPENSSL_NO_STDIO
+# include <stdio.h>
+#endif
+#ifndef OPENSSL_NO_DTLS1
+# include <openssl/dtls1.h>
+#endif
+
+int main(void)
+{
+    return 0;
+}
---
+++ externals/openssl-1.1.1g/tmpARMdll/crypto/asn1/a_time.d
@@ -0,0 +1,53 @@
+a_time.c
+Note: including file: C:\Program Files (x86)\Windows Kits\10\Include\10.0.19041.0\ucrt\stdio.h
+Note: including file:  C:\Program Files (x86)\Windows Kits\10\Include\10.0.19041.0\ucrt\corecrt.h
+Note: including file:   D:\MicrosoftVisualStudio\2019\Community\VC\Tools\MSVC\14.28.29333\include\vcruntime.h
+Note: including file:    D:\MicrosoftVisualStudio\2019\Community\VC\Tools\MSVC\14.28.29333\include\sal.h
+Note: including file:     D:\MicrosoftVisualStudio\2019\Community\VC\Tools\MSVC\14.28.29333\include\concurrencysal.h
+Note: including file:    D:\MicrosoftVisualStudio\2019\Community\VC\Tools\MSVC\14.28.29333\include\vadefs.h
+Note: including file:  C:\Program Files (x86)\Windows Kits\10\Include\10.0.19041.0\ucrt\corecrt_wstdio.h
+Note: including file:   C:\Program Files (x86)\Windows Kits\10\Include\10.0.19041.0\ucrt\corecrt_stdio_config.h
+Note: including file: C:\Program Files (x86)\Windows Kits\10\Include\10.0.19041.0\ucrt\time.h
+Note: including file:  C:\Program Files (x86)\Windows Kits\10\Include\10.0.19041.0\ucrt\corecrt_wtime.h
+Note: including file: C:\CPython\externals\openssl-1.1.1g\include\crypto/ctype.h
+Note: including file: C:\CPython\externals\openssl-1.1.1g\include\internal/cryptlib.h
+Note: including file:  C:\Program Files (x86)\Windows Kits\10\Include\10.0.19041.0\ucrt\stdlib.h
+Note: including file:   C:\Program Files (x86)\Windows Kits\10\Include\10.0.19041.0\ucrt\corecrt_malloc.h
+Note: including file:   C:\Program Files (x86)\Windows Kits\10\Include\10.0.19041.0\ucrt\corecrt_search.h
+Note: including file:    C:\Program Files (x86)\Windows Kits\10\Include\10.0.19041.0\ucrt\stddef.h
+Note: including file:   C:\Program Files (x86)\Windows Kits\10\Include\10.0.19041.0\ucrt\corecrt_wstdlib.h
+Note: including file:   D:\MicrosoftVisualStudio\2019\Community\VC\Tools\MSVC\14.28.29333\include\limits.h
+Note: including file:  C:\Program Files (x86)\Windows Kits\10\Include\10.0.19041.0\ucrt\string.h
+Note: including file:   C:\Program Files (x86)\Windows Kits\10\Include\10.0.19041.0\ucrt\corecrt_memory.h
+Note: including file:    C:\Program Files (x86)\Windows Kits\10\Include\10.0.19041.0\ucrt\corecrt_memcpy_s.h
+Note: including file:     C:\Program Files (x86)\Windows Kits\10\Include\10.0.19041.0\ucrt\errno.h
+Note: including file:     D:\MicrosoftVisualStudio\2019\Community\VC\Tools\MSVC\14.28.29333\include\vcruntime_string.h
+Note: including file:   C:\Program Files (x86)\Windows Kits\10\Include\10.0.19041.0\ucrt\corecrt_wstring.h
+Note: including file:  C:\CPython\externals\openssl-1.1.1g\include\openssl/crypto.h
+Note: including file:   C:\CPython\externals\openssl-1.1.1g\include\openssl/e_os2.h
+Note: including file:    C:\CPython\externals\openssl-1.1.1g\tmpARMdll\include\openssl/opensslconf.h
+Note: including file:     C:\CPython\externals\openssl-1.1.1g\include\openssl/opensslv.h
+Note: including file:    D:\MicrosoftVisualStudio\2019\Community\VC\Tools\MSVC\14.28.29333\include\stdint.h
+Note: including file:   C:\CPython\externals\openssl-1.1.1g\include\openssl/safestack.h
+Note: including file:    C:\CPython\externals\openssl-1.1.1g\include\openssl/stack.h
+Note: including file:   C:\CPython\externals\openssl-1.1.1g\include\openssl/ossl_typ.h
+Note: including file:   C:\CPython\externals\openssl-1.1.1g\tmpARMdll\include\openssl/opensslconf.h
+Note: including file:   C:\CPython\externals\openssl-1.1.1g\include\openssl/cryptoerr.h
+Note: including file:    C:\CPython\externals\openssl-1.1.1g\include\openssl/symhacks.h
+Note: including file:  C:\CPython\externals\openssl-1.1.1g\include\openssl/buffer.h
+Note: including file:   C:\CPython\externals\openssl-1.1.1g\include\openssl/buffererr.h
+Note: including file:   C:\Program Files (x86)\Windows Kits\10\Include\10.0.19041.0\ucrt\sys/types.h
+Note: including file:  C:\CPython\externals\openssl-1.1.1g\include\openssl/bio.h
+Note: including file:   D:\MicrosoftVisualStudio\2019\Community\VC\Tools\MSVC\14.28.29333\include\stdarg.h
+Note: including file:   C:\CPython\externals\openssl-1.1.1g\include\openssl/bioerr.h
+Note: including file:  C:\CPython\externals\openssl-1.1.1g\include\openssl/err.h
+Note: including file:   C:\CPython\externals\openssl-1.1.1g\include\openssl/lhash.h
+Note: including file:  C:\CPython\externals\openssl-1.1.1g\include\internal/nelem.h
+Note: including file: C:\CPython\externals\openssl-1.1.1g\include\openssl/asn1t.h
+Note: including file:  C:\CPython\externals\openssl-1.1.1g\include\openssl/asn1.h
+Note: including file:   C:\CPython\externals\openssl-1.1.1g\tmpARMdll\include\openssl/opensslconf.h
+Note: including file:   C:\CPython\externals\openssl-1.1.1g\include\openssl/asn1err.h
+Note: including file:   C:\CPython\externals\openssl-1.1.1g\include\openssl/bn.h
+Note: including file:    C:\CPython\externals\openssl-1.1.1g\tmpARMdll\include\openssl/opensslconf.h
+Note: including file:    C:\CPython\externals\openssl-1.1.1g\include\openssl/bnerr.h
+Note: including file: C:\CPython\externals\openssl-1.1.1g\crypto\asn1\asn1_local.h
