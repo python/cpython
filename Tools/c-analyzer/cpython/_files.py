acc696de733b0adf2f79d9808d07d6598d23d218
Eric Snow <ericsnowcurrently@gmail.com>
2020-12-19 18:41:51 -0700

    Add iter_header_files().

--- Tools/c-analyzer/cpython/_files.py
+++ Tools/c-analyzer/cpython/_files.py
@@ -16,6 +16,11 @@ GLOBS = [
     'Python/**/*.h',
     'Parser/**/*.c',
 ]
+LEVEL_GLOBS = {
+    'stable': 'Include/*.h',
+    'cpython': 'Include/cpython/*.h',
+    'internal': 'Include/internal/*.h',
+}
 
 
 def resolve_filename(filename):
@@ -36,3 +41,29 @@ def iter_filenames(*, search=False):
     else:
         globs = (os.path.join(REPO_ROOT, file) for file in GLOBS)
         yield from expand_filenames(globs)
+
+
+def iter_header_files(filenames=None, *, levels=None):
+    if not filenames:
+        if levels:
+            levels = set(levels)
+            if 'private' in levels:
+                levels.add('stable')
+                levels.add('cpython')
+            for level, glob in LEVEL_GLOBS.items():
+                if level in levels:
+                    yield from expand_filenames([glob])
+        else:
+            yield from iter_files_by_suffix(INCLUDE_DIRS, ('.h',))
+        return
+
+    for filename in filenames:
+        orig = filename
+        filename = resolve_filename(filename)
+        if filename.endswith(os.path.sep):
+            yield from iter_files_by_suffix(INCLUDE_DIRS, ('.h',))
+        elif filename.endswith('.h'):
+            yield filename
+        else:
+            # XXX Log it and continue instead?
+            raise ValueError(f'expected .h file, got {orig!r}')
