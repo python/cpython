c20eb768ee5b8c8720e61425297d6a331bde0303
Dominik1123 <>
2020-12-24 02:18:51 +0100

    Limit to 79 characters per line

--- Lib/unittest/main.py
+++ Lib/unittest/main.py
@@ -65,7 +65,8 @@ class TestProgram(object):
     def __init__(self, module='__main__', defaultTest=None, argv=None,
                     testRunner=None, testLoader=loader.defaultTestLoader,
                     exit=True, verbosity=1, failfast=None, catchbreak=None,
-                    buffer=None, warnings=None, *, tb_locals=False, debug=False):
+                    buffer=None, warnings=None, *, tb_locals=False,
+                    debug=False):
         if isinstance(module, str):
             self.module = __import__(module)
             for part in module.split('.')[1:]:
