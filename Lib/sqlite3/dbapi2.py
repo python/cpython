cd11c78270bfc9ef0ef8a75645c383e4000994db
Erlend E. Aasland <erlend.aasland@innova.no>
2021-01-04 23:38:14 +0100

    Address review: Improve formatting

--- Lib/sqlite3/dbapi2.py
+++ Lib/sqlite3/dbapi2.py
@@ -89,12 +89,12 @@ register_adapters_and_converters()
 from _sqlite3 import enable_shared_cache as _old_enable_shared_cache
 def enable_shared_cache(enable):
     import warnings
-    msg = ("""
-        enable_shared_cache is deprecated and will be removed in Python 3.12.
-        Shared cache is strongly discouraged by the SQLite 3 documentation.
-        If shared cache must be used, open the database in URI mode using
-        the cache=shared query parameter.
-    """)
+    msg = (
+        "enable_shared_cache is deprecated and will be removed in Python 3.12. "
+        "Shared cache is strongly discouraged by the SQLite 3 documentation. "
+        "If shared cache must be used, open the database in URI mode using"
+        "the cache=shared query parameter."
+    )
     warnings.warn(msg, DeprecationWarning, stacklevel=2)
     return _old_enable_shared_cache
 
