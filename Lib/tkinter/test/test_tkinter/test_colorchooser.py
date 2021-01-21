dbf6ca95e656022895c41e838aef479252bc8bf4
Cheryl Sabella <cheryl.sabella@gmail.com>
2021-01-21 14:14:04 -0500

    bpo-33289: Return RGB triplet of ints instead of floats from tkinter.colorchooser (GH-6578)

--- Lib/tkinter/test/test_tkinter/test_colorchooser.py
+++ Lib/tkinter/test/test_tkinter/test_colorchooser.py
@@ -1,11 +1,42 @@
 import unittest
 import tkinter
 from test.support import requires, run_unittest, swap_attr
-from tkinter.test.support import AbstractDefaultRootTest
-from tkinter.commondialog import Dialog
+from tkinter.test.support import AbstractDefaultRootTest, AbstractTkTest
+from tkinter import colorchooser
 from tkinter.colorchooser import askcolor
+from tkinter.commondialog import Dialog
 
 requires('gui')
+
+
+class ChooserTest(AbstractTkTest, unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        AbstractTkTest.setUpClass.__func__(cls)
+        cls.cc = colorchooser.Chooser(initialcolor='dark blue slate')
+
+    def test_fixoptions(self):
+        cc = self.cc
+        cc._fixoptions()
+        self.assertEqual(cc.options['initialcolor'], 'dark blue slate')
+
+        cc.options['initialcolor'] = '#D2D269691E1E'
+        cc._fixoptions()
+        self.assertEqual(cc.options['initialcolor'], '#D2D269691E1E')
+
+        cc.options['initialcolor'] = (210, 105, 30)
+        cc._fixoptions()
+        self.assertEqual(cc.options['initialcolor'], '#d2691e')
+
+    def test_fixresult(self):
+        cc = self.cc
+        self.assertEqual(cc._fixresult(self.root, ()), (None, None))
+        self.assertEqual(cc._fixresult(self.root, ''), (None, None))
+        self.assertEqual(cc._fixresult(self.root, 'chocolate'),
+                         ((210, 105, 30), 'chocolate'))
+        self.assertEqual(cc._fixresult(self.root, '#4a3c8c'),
+                         ((74, 60, 140), '#4a3c8c'))
 
 
 class DefaultRootTest(AbstractDefaultRootTest, unittest.TestCase):
@@ -33,7 +64,7 @@ class DefaultRootTest(AbstractDefaultRootTest, unittest.TestCase):
             self.assertRaises(RuntimeError, askcolor)
 
 
-tests_gui = (DefaultRootTest,)
+tests_gui = (ChooserTest, DefaultRootTest,)
 
 if __name__ == "__main__":
     run_unittest(*tests_gui)
