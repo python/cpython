2e96ea757a45a4e5c7de9075d6a75f0085072a27
SJ <76181208+imaginary-person@users.noreply.github.com>
2021-02-05 23:33:52 -0600

    Do not add duplicate FDs in duplicate_for_child()
    While spawning, if the code built on top of python's multiprocessing module calls DupFd (in  /Lib/multiprocessing/reduction.py) multiple times on the same File descriptor by mistake, then it ends up invoking duplicate_for_child(fd)) of the class Popen, which, in turn, adds a duplicate FD to a list.
    This list is then used in spawnv_passfds() in multiprocessing/util.py, which uses that list as an argument in a call of _posixsubprocess.fork_exec().
    
    In Modules/_posixsubprocess.c, checks exist to ensure that all the FDs in the list are unique, positive, and in a sorted order.
    If duplicate entries are found, a 'ValueError: bad value(s) in fds_to_keep' exception is raised, and the execution is terminated because this exception is not handled by Python.
    Even if it were, a developer can't change the aforementioned FD list in Python without breaking modularity.
    
    It's better to let developers get away with calling DupFd (in  /Lib/multiprocessing/reduction.py) by not accepting duplicate entries in the first place,
    so that spawning a process can be successful.

--- Lib/multiprocessing/popen_spawn_posix.py
+++ Lib/multiprocessing/popen_spawn_posix.py
@@ -32,7 +32,8 @@ class Popen(popen_fork.Popen):
         super().__init__(process_obj)
 
     def duplicate_for_child(self, fd):
-        self._fds.append(fd)
+        if fd not in self._fds:
+            self._fds.append(fd)
         return fd
 
     def _launch(self, process_obj):
