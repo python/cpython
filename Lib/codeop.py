8c96239fdbc2f34beb5709cc29ec00fcbe11816b
Pablo Galindo <Pablogsal@gmail.com>
2021-02-09 10:58:21 +0000

    Update Lib/codeop.py

--- Lib/codeop.py
+++ Lib/codeop.py
@@ -110,7 +110,7 @@ def _maybe_compile(compiler, source, filename, symbol):
 def _is_syntax_error(err1, err2):
     rep1 = repr(err1)
     rep2 = repr(err2)
-    if "was never closed" in rep1 and"was never closed" in rep2:
+    if "was never closed" in rep1 and "was never closed" in rep2:
         return False
     if rep1 == rep2:
         return True
