af69662f19615e032bf5fa0a081e87d8a56fc9d2
Neil Schemenauer <nas-github@arctrix.com>
2021-02-18 16:49:12 -0800

    bpo-39448: Add regen-frozen makefile target. (GH-18174)
    Add the "regen-frozen" makefile target that regenerates the code for the
    frozen __hello__ module.

--- Python/frozen.c
+++ Python/frozen.c
@@ -1,5 +1,5 @@
 
-/* Dummy frozen modules initializer */
+/* Frozen modules initializer */
 
 #include "Python.h"
 #include "importlib.h"
@@ -10,21 +10,11 @@
    define a single frozen module, __hello__.  Loading it will print
    some famous words... */
 
-/* To regenerate this data after the bytecode or marshal format has changed,
-   go to ../Tools/freeze/ and freeze the flag.py file; then copy and paste
-   the appropriate bytes from M___main__.c. */
-
-static unsigned char M___hello__[] = {
-    227,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-    0,2,0,0,0,64,0,0,0,115,16,0,0,0,100,0,
-    90,0,101,1,100,1,131,1,1,0,100,2,83,0,41,3,
-    84,122,12,72,101,108,108,111,32,119,111,114,108,100,33,78,
-    41,2,218,11,105,110,105,116,105,97,108,105,122,101,100,218,
-    5,112,114,105,110,116,169,0,114,3,0,0,0,114,3,0,
-    0,0,250,20,84,111,111,108,115,47,102,114,101,101,122,101,
-    47,102,108,97,103,46,112,121,218,8,60,109,111,100,117,108,
-    101,62,1,0,0,0,115,2,0,0,0,4,1,
-};
+/* Run "make regen-frozen" to regen the file below (e.g. after a bytecode
+ * format change).  The file is created by Tools/frozen/regen_frozen.py.  The
+ * include file defines M___hello__ as an array of bytes.
+ */
+#include "frozen_hello.h"
 
 #define SIZE (int)sizeof(M___hello__)
 
