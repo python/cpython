ffa55d21b4a86ad8b4a43a9f597151e526541130
Neil Schemenauer <nas-github@arctrix.com>
2021-02-18 16:49:12 -0800

    bpo-39448: Add regen-frozen makefile target. (GH-18174)
    Add the "regen-frozen" makefile target that regenerates the code for the
    frozen __hello__ module.

--- Lib/ctypes/test/test_values.py
+++ Lib/ctypes/test/test_values.py
@@ -80,9 +80,9 @@ class PythonValuesTestCase(unittest.TestCase):
                 continue
             items.append((entry.name.decode("ascii"), entry.size))
 
-        expected = [("__hello__", 141),
-                    ("__phello__", -141),
-                    ("__phello__.spam", 141),
+        expected = [("__hello__", 125),
+                    ("__phello__", -125),
+                    ("__phello__.spam", 125),
                     ]
         self.assertEqual(items, expected, "PyImport_FrozenModules example "
             "in Doc/library/ctypes.rst may be out of date")
