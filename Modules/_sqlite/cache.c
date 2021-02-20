cc192f49d02b45afb6435bc222e7bc7853f8f3c5
Erlend E. Aasland <erlend.aasland@innova.no>
2021-02-20 23:51:34 +0100

    bpo-43269: Remove redundant extern keywords

--- Modules/_sqlite/cache.c
+++ Modules/_sqlite/cache.c
@@ -291,7 +291,8 @@ static PyType_Spec cache_spec = {
 };
 PyTypeObject *pysqlite_CacheType = NULL;
 
-extern int pysqlite_cache_setup_types(PyObject *mod)
+int
+pysqlite_cache_setup_types(PyObject *mod)
 {
     pysqlite_NodeType = (PyTypeObject *)PyType_FromModuleAndSpec(mod, &node_spec, NULL);
     if (pysqlite_NodeType == NULL) {
