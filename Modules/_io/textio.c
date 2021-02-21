51635af855ff46ebc79e683375ff0b6c36fd002d
Inada Naoki <songofacandy@gmail.com>
2021-02-22 08:29:30 +0900

    [3.8] bpo-43260: io: Prevent large data remains in textio buffer. (GH-24592)
    When very large data remains in TextIOWrapper, flush() may fail forever.
    
    So prevent that data larger than chunk_size is remained in TextIOWrapper internal
    buffer.
    
    Co-Authored-By: Eryk Sun.
    (cherry picked from commit 01806d5beba3d208bb56adba6829097d803bf54f)
    
    Co-authored-by: Inada Naoki <songofacandy@gmail.com>

--- Modules/_io/textio.c
+++ Modules/_io/textio.c
@@ -1558,6 +1558,8 @@ _textiowrapper_writeflush(textio *self)
                                          _PyIO_str_write, b, NULL);
     } while (ret == NULL && _PyIO_trap_eintr());
     Py_DECREF(b);
+    // NOTE: We cleared buffer but we don't know how many bytes are actually written
+    // when an error occurred.
     if (ret == NULL)
         return -1;
     Py_DECREF(ret);
@@ -1615,7 +1617,10 @@ _io_TextIOWrapper_write_impl(textio *self, PyObject *text)
 
     /* XXX What if we were just reading? */
     if (self->encodefunc != NULL) {
-        if (PyUnicode_IS_ASCII(text) && is_asciicompat_encoding(self->encodefunc)) {
+        if (PyUnicode_IS_ASCII(text) &&
+                // See bpo-43260
+                PyUnicode_GET_LENGTH(text) <= self->chunk_size &&
+                is_asciicompat_encoding(self->encodefunc)) {
             b = text;
             Py_INCREF(b);
         }
@@ -1624,9 +1629,10 @@ _io_TextIOWrapper_write_impl(textio *self, PyObject *text)
         }
         self->encoding_start_of_stream = 0;
     }
-    else
+    else {
         b = PyObject_CallMethodObjArgs(self->encoder,
                                        _PyIO_str_encode, text, NULL);
+    }
 
     Py_DECREF(text);
     if (b == NULL)
@@ -1651,6 +1657,14 @@ _io_TextIOWrapper_write_impl(textio *self, PyObject *text)
         self->pending_bytes_count = 0;
         self->pending_bytes = b;
     }
+    else if (self->pending_bytes_count + bytes_len > self->chunk_size) {
+        // Prevent to concatenate more than chunk_size data.
+        if (_textiowrapper_writeflush(self) < 0) {
+            Py_DECREF(b);
+            return NULL;
+        }
+        self->pending_bytes = b;
+    }
     else if (!PyList_CheckExact(self->pending_bytes)) {
         PyObject *list = PyList_New(2);
         if (list == NULL) {
@@ -1670,7 +1684,7 @@ _io_TextIOWrapper_write_impl(textio *self, PyObject *text)
     }
 
     self->pending_bytes_count += bytes_len;
-    if (self->pending_bytes_count > self->chunk_size || needflush ||
+    if (self->pending_bytes_count >= self->chunk_size || needflush ||
         text_needflush) {
         if (_textiowrapper_writeflush(self) < 0)
             return NULL;
