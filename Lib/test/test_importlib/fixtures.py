2a67f425d7571e296a5d7644ac7aceb9177b0ad0
Neil Schemenauer <nas-github@arctrix.com>
2021-02-21 13:48:18 -0800

    bpo-43288: Fix bug in test_importlib test. (GH-24612) (cherry picked from commit 50288aa8c955f66ab67a7dadf250ea5f4238eb67)
    Co-authored-by: Neil Schemenauer <nas-github@arctrix.com>

--- Lib/test/test_importlib/fixtures.py
+++ Lib/test/test_importlib/fixtures.py
@@ -5,6 +5,7 @@ import pathlib
 import tempfile
 import textwrap
 import contextlib
+import unittest
 
 
 @contextlib.contextmanager
@@ -174,6 +175,9 @@ class LocalPackage:
         self.addCleanup(self.fixtures.close)
         self.fixtures.enter_context(tempdir_as_cwd())
         build_files(self.files)
+
+    def skip(self, reason):
+        raise unittest.SkipTest(reason)
 
 
 def build_files(file_defs, prefix=pathlib.Path()):
