56d005a8f70ffd65ebad4f53e02b66c4cd2b46ce
Pablo Galindo <Pablogsal@gmail.com>
2021-02-28 22:41:09 +0000

    bpo-37146: Deactivate opcode cache only when using huntrleaks in the test suite (GH-24643)

--- Lib/test/libregrtest/setup.py
+++ Lib/test/libregrtest/setup.py
@@ -62,6 +62,7 @@ def setup_tests(ns):
 
     if ns.huntrleaks:
         unittest.BaseTestSuite._cleanup = False
+        sys._deactivate_opcache()
 
     if ns.memlimit is not None:
         support.set_memlimit(ns.memlimit)
