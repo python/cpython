0ff632fd8dcc1e42fbc9512d554959a3281f969a
Gregory Starck <gregory.starck@algolux.com>
2021-03-08 08:50:54 -0500

    Not needed

--- Lib/test/test_email/test_email.py
+++ Lib/test/test_email/test_email.py
@@ -4261,7 +4261,7 @@ class TestBase64(unittest.TestCase):
 
     def test_encode(self):
         eq = self.assertEqual
-        eq(base64mime.body_encode(b''), '')  # bpo-43125
+        eq(base64mime.body_encode(b''), '')
         eq(base64mime.body_encode(b'hello'), 'aGVsbG8=\n')
         # Test the binary flag
         eq(base64mime.body_encode(b'hello\n'), 'aGVsbG8K\n')
