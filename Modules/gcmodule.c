a2eb72a51caa1d79e1e2d3392d591ad43beb21d7
Pablo Galindo <pablogsal@gmail.com>
2021-03-13 05:17:42 +0000

    bpo-43439: Wrapt the tuple in the audit events for the gc module

--- Modules/gcmodule.c
+++ Modules/gcmodule.c
@@ -1690,7 +1690,7 @@ Return the list of objects that directly refer to any of objs.");
 static PyObject *
 gc_get_referrers(PyObject *self, PyObject *args)
 {
-    if (PySys_Audit("gc.get_referrers", "O", args) < 0) {
+    if (PySys_Audit("gc.get_referrers", "(O)", args) < 0) {
         return NULL;
     }
 
@@ -1724,7 +1724,7 @@ static PyObject *
 gc_get_referents(PyObject *self, PyObject *args)
 {
     Py_ssize_t i;
-    if (PySys_Audit("gc.get_referents", "O", args) < 0) {
+    if (PySys_Audit("gc.get_referents", "(O)", args) < 0) {
         return NULL;
     }
     PyObject *result = PyList_New(0);
