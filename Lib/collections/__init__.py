5fae3e70742cdc7641c8b057b8f52a66046858e4
Kamil Turek <kamil.turek@hotmail.com>
2021-03-14 04:15:44 +0100

    bpo-43245: Add keyword argument support to ChainMap.new_child() (GH-24788)

--- Lib/collections/__init__.py
+++ Lib/collections/__init__.py
@@ -1010,12 +1010,15 @@ class ChainMap(_collections_abc.MutableMapping):
 
     __copy__ = copy
 
-    def new_child(self, m=None):                # like Django's Context.push()
+    def new_child(self, m=None, **kwargs):      # like Django's Context.push()
         '''New ChainMap with a new map followed by all previous maps.
         If no map is provided, an empty dict is used.
+        Keyword arguments update the map or new empty dict.
         '''
         if m is None:
-            m = {}
+            m = kwargs
+        elif kwargs:
+            m.update(kwargs)
         return self.__class__(m, *self.maps)
 
     @property
