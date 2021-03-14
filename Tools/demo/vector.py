a17196b7ec275fd08c71088afb870cd35502f49d
Raymond Hettinger <raymond.hettinger@gmail.com>
2021-03-13 18:51:54 -0800

    Minor updated to the vector demo class:
    * __repr__ to follow best practices and not hardwire the class name.
    * Convert comments to docstrings.
    * Enhance the demo with one non-dunder method, a dot product.

--- Tools/demo/vector.py
+++ Tools/demo/vector.py
@@ -27,9 +27,15 @@ class Vec:
     or on the right
     >>> a * 3.0
     Vec(3.0, 6.0, 9.0)
+
+    and dot product
+    >>> a.dot(b)
+    10
+
     """
+
     def __init__(self, *v):
-        self.v = list(v)
+        self.v = v
 
     @classmethod
     def fromlist(cls, v):
@@ -40,8 +46,8 @@ class Vec:
         return inst
 
     def __repr__(self):
-        args = ', '.join(repr(x) for x in self.v)
-        return 'Vec({})'.format(args)
+        args = ', '.join([repr(x) for x in self.v])
+        return f'{type(self).__name__}({args})'
 
     def __len__(self):
         return len(self.v)
@@ -50,21 +56,27 @@ class Vec:
         return self.v[i]
 
     def __add__(self, other):
-        # Element-wise addition
+        "Element-wise addition"
         v = [x + y for x, y in zip(self.v, other.v)]
         return Vec.fromlist(v)
 
     def __sub__(self, other):
-        # Element-wise subtraction
+        "Element-wise subtraction"
         v = [x - y for x, y in zip(self.v, other.v)]
         return Vec.fromlist(v)
 
     def __mul__(self, scalar):
-        # Multiply by scalar
+        "Multiply by scalar"
         v = [x * scalar for x in self.v]
         return Vec.fromlist(v)
 
     __rmul__ = __mul__
+
+    def dot(self, other):
+        "Vector dot product"
+        if not isinstance(other, Vec):
+            raise TypeError
+        return sum(x_i * y_i for (x_i, y_i) in zip(self, other))
 
 
 def test():
