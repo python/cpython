b76da3ef759208ce431f851595701cb5da640d2d
Segev Finer <segev208@gmail.com>
2021-03-16 16:54:29 +0200

    Fix faulty call to IOBase.close introduced by automatic merge

--- Modules/_io/winconsoleio.c
+++ Modules/_io/winconsoleio.c
@@ -196,8 +196,8 @@ _io__WindowsConsoleIO_close_impl(winconsoleio *self)
     PyObject *exc, *val, *tb;
     int rc;
     _Py_IDENTIFIER(close);
-    res = _PyObject_CallMethodIdObjArgs((PyObject*)&PyRawIOBase_Type,
-                                        &PyId_close, (PyObject*)self);
+    res = _PyObject_CallMethodIdOneArg((PyObject*)&PyRawIOBase_Type,
+                                       &PyId_close, (PyObject*)self);
     if (!self->closefd) {
         self->fd = -1;
         return res;
