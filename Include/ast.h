557f72b138e6a03271aa1cc889333c8c3caba776
Victor Stinner <vstinner@python.org>
2021-03-17 23:50:50 +0100

    bpo-43244: Add pycore_ast.h header file (GH-24908)
    Move _PyAST_GetDocString() and _PyAST_ExprAsUnicode() functions the
    internal C API: from Include/ast.h to a new
    Include/internal/pycore_ast.h header file. Don't export these
    functions anymore: replace PyAPI_FUNC() with extern.
    
    Remove also unused includes.

--- Include/ast.h
+++ Include/ast.h
@@ -9,14 +9,6 @@ extern "C" {
 
 PyAPI_FUNC(int) PyAST_Validate(mod_ty);
 
-/* _PyAST_ExprAsUnicode is defined in ast_unparse.c */
-PyAPI_FUNC(PyObject *) _PyAST_ExprAsUnicode(expr_ty);
-
-/* Return the borrowed reference to the first literal string in the
-   sequence of statements or NULL if it doesn't start from a literal string.
-   Doesn't set exception. */
-PyAPI_FUNC(PyObject *) _PyAST_GetDocString(asdl_stmt_seq *);
-
 #ifdef __cplusplus
 }
 #endif
