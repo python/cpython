29c451c6989c3c94fa0a9facf187c24f3cbf2420
Carl Meyer <carl@oddbird.net>
2021-03-27 15:52:28 -0600

    bpo-43562: fix test_ssl to skip on unreachable network (GH-24937)
    This test checks result code of the connection directly, so it never raises an exception that can be suppressed by `support.transient_internet`. Directly support skipping the test in case of unreachable network.

--- Lib/test/test_ssl.py
+++ Lib/test/test_ssl.py
@@ -2301,6 +2301,8 @@ class NetworkedTests(unittest.TestCase):
             rc = s.connect_ex((REMOTE_HOST, 443))
             if rc == 0:
                 self.skipTest("REMOTE_HOST responded too quickly")
+            elif rc == errno.ENETUNREACH:
+                self.skipTest("Network unreachable.")
             self.assertIn(rc, (errno.EAGAIN, errno.EWOULDBLOCK))
 
     @unittest.skipUnless(socket_helper.IPV6_ENABLED, 'Needs IPv6')
