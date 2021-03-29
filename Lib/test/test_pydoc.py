ed753d94856213ae9fc028195f670e66a24e2334
Miss Islington (bot) <31488909+miss-islington@users.noreply.github.com>
2021-03-29 06:08:00 -0700

    bpo-42988: Remove the pydoc getfile feature (GH-25015)
    CVE-2021-3426: Remove the "getfile" feature of the pydoc module which
    could be abused to read arbitrary files on the disk (directory
    traversal vulnerability). Moreover, even source code of Python
    modules can contain sensitive data like passwords. Vulnerability
    reported by David Schwörer.
    (cherry picked from commit 9b999479c0022edfc9835a8a1f06e046f3881048)
    
    Co-authored-by: Victor Stinner <vstinner@python.org>

--- Lib/test/test_pydoc.py
+++ Lib/test/test_pydoc.py
@@ -1374,17 +1374,11 @@ class PydocUrlHandlerTest(PydocBaseTest):
             ("topic?key=def", "Pydoc: KEYWORD def"),
             ("topic?key=STRINGS", "Pydoc: TOPIC STRINGS"),
             ("foobar", "Pydoc: Error - foobar"),
-            ("getfile?key=foobar", "Pydoc: Error - getfile?key=foobar"),
             ]
 
         with self.restrict_walk_packages():
             for url, title in requests:
                 self.call_url_handler(url, title)
-
-            path = string.__file__
-            title = "Pydoc: getfile " + path
-            url = "getfile?key=" + path
-            self.call_url_handler(url, title)
 
 
 class TestHelper(unittest.TestCase):
