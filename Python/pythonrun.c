ff4715a7332123713de9920e724bb6c412d8ccef
Miss Islington (bot) <31488909+miss-islington@users.noreply.github.com>
2021-03-29 16:24:33 -0700

    bpo-43660: Fix crash when displaying exceptions with custom values for sys.stderr (GH-25075) (GH-25083)
    (cherry picked from commit 09b90a037d18f5d4acdf1b14082e57bda78e85d3)
    
    Co-authored-by: Pablo Galindo <Pablogsal@gmail.com>
    
    Co-authored-by: Pablo Galindo <Pablogsal@gmail.com>

--- Python/pythonrun.c
+++ Python/pythonrun.c
@@ -1072,8 +1072,9 @@ PyErr_Display(PyObject *exception, PyObject *value, PyObject *tb)
     if (file == Py_None) {
         return;
     }
-
+    Py_INCREF(file);
     _PyErr_Display(file, exception, value, tb);
+    Py_DECREF(file);
 }
 
 PyObject *
