73b20ae2fb7a5c1374aa5c3719f64c53d29fa0d2
Antoine Pitrou <antoine@python.org>
2021-03-30 18:11:06 +0200

    bpo-41369: Finish updating the vendored libmpdec to version 2.5.1 (GH-24962)
    Complete the update to libmpdec-2.5.1.
    
    Co-authored-by: Stefan Krah <skrah@bytereef.org>

--- Modules/_decimal/_decimal.c
+++ Modules/_decimal/_decimal.c
@@ -3293,7 +3293,7 @@ dec_format(PyObject *dec, PyObject *args)
     }
     else {
         size_t n = strlen(spec.dot);
-        if (n > 1 || (n == 1 && !isascii((uchar)spec.dot[0]))) {
+        if (n > 1 || (n == 1 && !isascii((unsigned char)spec.dot[0]))) {
             /* fix locale dependent non-ascii characters */
             dot = dotsep_as_utf8(spec.dot);
             if (dot == NULL) {
@@ -3302,7 +3302,7 @@ dec_format(PyObject *dec, PyObject *args)
             spec.dot = PyBytes_AS_STRING(dot);
         }
         n = strlen(spec.sep);
-        if (n > 1 || (n == 1 && !isascii((uchar)spec.sep[0]))) {
+        if (n > 1 || (n == 1 && !isascii((unsigned char)spec.sep[0]))) {
             /* fix locale dependent non-ascii characters */
             sep = dotsep_as_utf8(spec.sep);
             if (sep == NULL) {
--- setup.py
+++ setup.py
@@ -2290,7 +2290,7 @@ class PyBuildExt(build_ext):
         undef_macros = []
         if '--with-system-libmpdec' in sysconfig.get_config_var("CONFIG_ARGS"):
             include_dirs = []
-            libraries = [':libmpdec.so.2']
+            libraries = ['mpdec']
             sources = ['_decimal/_decimal.c']
             depends = ['_decimal/docstrings.h']
         else:
