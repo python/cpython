7867619371a262dbe4477eb16abb98010e272653
David Bolen <db3l.net@gmail.com>
2021-04-02 22:45:34 +0100

    bpo-43709: Remove pyc/pyo files from Tools and Parser in buildbot clean.bat
    The Windows buildbot clean.bat script needs to remove pyc/pyo files in the
    Tools and Parser directories (in addition to the existing Lib directory)
    to avoid incompatible files between builds.

--- Tools/buildbot/clean.bat
+++ Tools/buildbot/clean.bat
@@ -11,6 +11,8 @@ call "%pcbuild%\build.bat" -t Clean -k -d %*
 
 echo Deleting .pyc/.pyo files ...
 del /s "%root%\Lib\*.pyc" "%root%\Lib\*.pyo"
+del /s "%root%\Tools\*.pyc" "%root%\Tools\*.pyo"
+del /s "%root%\Parser\*.pyc" "%root%\Parser\*.pyo"
 
 echo Deleting test leftovers ...
 rmdir /s /q "%root%\build"
