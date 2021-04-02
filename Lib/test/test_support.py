454ac03baf0a4aea3c81006e0a499c1f3c13dbba
Inada Naoki <songofacandy@gmail.com>
2021-04-02 12:53:46 +0900

    bpo-43651: PEP 597: Fix EncodingWarning in some tests (GH-25142)
    * test__xxsubinterpreters
    * test_builtin
    * test_doctest
    * test_exceptions
    * test_opcodes
    * test_support
    * test_argparse
    * test_baseexception
    * test_bdb
    * test_bool
    * test_asdl_parser

--- Lib/test/test_support.py
+++ Lib/test/test_support.py
@@ -47,7 +47,7 @@ class TestSupport(unittest.TestCase):
         self.assertNotIn("sched", sys.modules)
 
     def test_unlink(self):
-        with open(TESTFN, "w") as f:
+        with open(TESTFN, "w", encoding="utf-8") as f:
             pass
         os_helper.unlink(TESTFN)
         self.assertFalse(os.path.exists(TESTFN))
@@ -79,7 +79,7 @@ class TestSupport(unittest.TestCase):
 
     def test_forget(self):
         mod_filename = TESTFN + '.py'
-        with open(mod_filename, 'w') as f:
+        with open(mod_filename, 'w', encoding="utf-8") as f:
             print('foo = 1', file=f)
         sys.path.insert(0, os.curdir)
         importlib.invalidate_caches()
