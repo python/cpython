20d56bd41b56023ce9fa3739c0c9aa8be8d48bfa
Markus Gerstel <2102431+Anthchirp@users.noreply.github.com>
2021-04-09 00:29:19 +0100

    bpo-39674: Fix collections ABC deprecation notice (GH-25281)
    The deprecation originally slated for 3.9 was deferred to 3.10
    (bpo-39674, GH-18545) and the documentation on the 3.8 release was
    updated accordingly (GH-18748). However the deprecation notice in
    the code was left as is, and still indicates deprecation with 3.9.

--- Lib/collections/__init__.py
+++ Lib/collections/__init__.py
@@ -48,7 +48,7 @@ def __getattr__(name):
         import warnings
         warnings.warn("Using or importing the ABCs from 'collections' instead "
                       "of from 'collections.abc' is deprecated since Python 3.3, "
-                      "and in 3.9 it will stop working",
+                      "and in 3.10 it will stop working",
                       DeprecationWarning, stacklevel=2)
         globals()[name] = obj
         return obj
