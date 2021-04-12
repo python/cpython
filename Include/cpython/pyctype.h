468e639cbd8260d9472d2b91271825068d557ddf
Andrew V. Jones <andrew.jones@vector.com>
2021-04-12 16:17:12 +0100

    bpo-43816: add 'extern "C"' to pyctype.h
    Signed-off-by: Andrew V. Jones <andrew.jones@vector.com>

--- Include/cpython/pyctype.h
+++ Include/cpython/pyctype.h
@@ -2,6 +2,10 @@
 #ifndef PYCTYPE_H
 #define PYCTYPE_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #define PY_CTF_LOWER  0x01
 #define PY_CTF_UPPER  0x02
 #define PY_CTF_ALPHA  (PY_CTF_LOWER|PY_CTF_UPPER)
@@ -28,6 +32,10 @@ PyAPI_DATA(const unsigned char) _Py_ctype_toupper[256];
 
 #define Py_TOLOWER(c) (_Py_ctype_tolower[Py_CHARMASK(c)])
 #define Py_TOUPPER(c) (_Py_ctype_toupper[Py_CHARMASK(c)])
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* !PYCTYPE_H */
 #endif /* !Py_LIMITED_API */
