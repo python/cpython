8827074d0841b473e65eb2b8d3e460d922cca4d7
Erlend Egeberg Aasland <erlend.aasland@innova.no>
2021-04-14 14:28:55 +0200

    bpo-20364: Improve sqlite3 placeholder docs (GH-25003) (cherry picked from commit 3386ca0b36327afeef8d7eff277b2aed1030c08d)
    Co-authored-by: Erlend Egeberg Aasland <erlend.aasland@innova.no>

--- Doc/includes/sqlite3/execute_1.py
+++ Doc/includes/sqlite3/execute_1.py
@@ -2,17 +2,22 @@ import sqlite3
 
 con = sqlite3.connect(":memory:")
 cur = con.cursor()
-cur.execute("create table people (name_last, age)")
-
-who = "Yeltsin"
-age = 72
+cur.execute("create table lang (lang_name, lang_age)")
 
 # This is the qmark style:
-cur.execute("insert into people values (?, ?)", (who, age))
+cur.execute("insert into lang values (?, ?)", ("C", 49))
+
+# The qmark style used with executemany():
+lang_list = [
+    ("Fortran", 64),
+    ("Python", 30),
+    ("Go", 11),
+]
+cur.executemany("insert into lang values (?, ?)", lang_list)
 
 # And this is the named style:
-cur.execute("select * from people where name_last=:who and age=:age", {"who": who, "age": age})
-
-print(cur.fetchone())
+cur.execute("select * from lang where lang_name=:name and lang_age=:age",
+            {"name": "C", "age": 49})
+print(cur.fetchall())
 
 con.close()
