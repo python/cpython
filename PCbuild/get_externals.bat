bf0c7c0147b73738cac63eb27ef48430284ff121
Erlend Egeberg Aasland <erlend.aasland@innova.no>
2021-04-27 18:23:53 +0200

    bpo-43492: Upgrade Windows installer to use SQLite 3.35.5 (GH-25641)

--- PCbuild/get_externals.bat
+++ PCbuild/get_externals.bat
@@ -54,7 +54,7 @@ set libraries=
 set libraries=%libraries%                                       bzip2-1.0.6
 if NOT "%IncludeLibffiSrc%"=="false" set libraries=%libraries%  libffi
 if NOT "%IncludeSSLSrc%"=="false" set libraries=%libraries%     openssl-1.1.1k
-set libraries=%libraries%                                       sqlite-3.34.0.0
+set libraries=%libraries%                                       sqlite-3.35.5.0
 if NOT "%IncludeTkinterSrc%"=="false" set libraries=%libraries% tcl-core-8.6.11.0
 if NOT "%IncludeTkinterSrc%"=="false" set libraries=%libraries% tk-8.6.11.0
 if NOT "%IncludeTkinterSrc%"=="false" set libraries=%libraries% tix-8.4.3.6
