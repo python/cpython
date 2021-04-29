76cd81d60310d65d01f9d7b48a8985d8ab89c8b4
Senthil Kumaran <senthil@uthcode.com>
2021-04-29 10:16:50 -0700

    bpo-43882 - urllib.parse should sanitize urls containing ASCII newline and tabs. (GH-25595)
    * issue43882 - urllib.parse should sanitize urls containing ASCII newline and tabs.
    
    Co-authored-by: Gregory P. Smith <greg@krypto.org>
    Co-authored-by: Serhiy Storchaka <storchaka@gmail.com>

--- Lib/urllib/parse.py
+++ Lib/urllib/parse.py
@@ -78,6 +78,9 @@ scheme_chars = ('abcdefghijklmnopqrstuvwxyz'
                 '0123456789'
                 '+-.')
 
+# Unsafe bytes to be removed per WHATWG spec
+_UNSAFE_URL_BYTES_TO_REMOVE = ['\t', '\r', '\n']
+
 # XXX: Consider replacing with functools.lru_cache
 MAX_CACHE_SIZE = 20
 _parse_cache = {}
@@ -468,6 +471,9 @@ def urlsplit(url, scheme='', allow_fragments=True):
                 break
         else:
             scheme, url = url[:i].lower(), url[i+1:]
+
+    for b in _UNSAFE_URL_BYTES_TO_REMOVE:
+        url = url.replace(b, "")
 
     if url[:2] == '//':
         netloc, url = _splitnetloc(url, 2)
