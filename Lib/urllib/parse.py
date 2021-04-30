dc4d94b7199cd893724d2d4f4499672b8b3bbe89
Dong-hee Na <donghee.na@python.org>
2021-05-01 04:01:55 +0900

    bpo-43979: Remove unnecessary operation from urllib.parse.parse_qsl (GH-25756)
    Automerge-Triggered-By: GH:gpshead

--- Lib/urllib/parse.py
+++ Lib/urllib/parse.py
@@ -752,9 +752,8 @@ def parse_qsl(qs, keep_blank_values=False, strict_parsing=False,
         if max_num_fields < num_fields:
             raise ValueError('Max number of fields exceeded')
 
-    pairs = [s1 for s1 in qs.split(separator)]
     r = []
-    for name_value in pairs:
+    for name_value in qs.split(separator):
         if not name_value and not strict_parsing:
             continue
         nv = name_value.split('=', 1)
