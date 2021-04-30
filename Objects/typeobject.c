52795a49b2c607be10efca76dfadf686db9f55eb
Erlend E. Aasland <erlend.aasland@innova.no>
2021-04-30 13:17:15 +0200

    Fix formatting

--- Objects/typeobject.c
+++ Objects/typeobject.c
@@ -463,13 +463,13 @@ check_set_special_type_attr(PyTypeObject *type, PyObject *value, const char *nam
 {
     if (_PyType_HasFeature(type, Py_TPFLAGS_IMMUTABLETYPE)) {
         PyErr_Format(PyExc_TypeError,
-                     "cannot set '%r' attribute of immutable type '%s'",
+                     "cannot set '%R' attribute of immutable type '%s'",
                      name, type->tp_name);
         return 0;
     }
     if (!value) {
         PyErr_Format(PyExc_TypeError,
-                     "cannot delete '%r' attribute of immutable type '%s'",
+                     "cannot delete '%R' attribute of immutable type '%s'",
                      name, type->tp_name);
         return 0;
     }
@@ -3951,8 +3951,8 @@ type_setattro(PyTypeObject *type, PyObject *name, PyObject *value)
     if (type->tp_flags & Py_TPFLAGS_IMMUTABLETYPE) {
         PyErr_Format(
             PyExc_TypeError,
-            "cannot set attributes of immutable type '%s'",
-            type->tp_name);
+            "cannot set '%R' attribute of immutable type '%s'",
+            name, type->tp_name);
         return -1;
     }
     if (PyUnicode_Check(name)) {
