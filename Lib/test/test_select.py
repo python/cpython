387397f8a4244c983f4568c16a28842e3268fe5d
Erlend Egeberg Aasland <erlend.aasland@innova.no>
2021-04-30 15:49:17 +0200

    bpo-43916: select.poll uses Py_TPFLAGS_DISALLOW_INSTANTIATION (GH-25750)

--- Lib/test/test_select.py
+++ Lib/test/test_select.py
@@ -87,6 +87,10 @@ class SelectTestCase(unittest.TestCase):
         a[:] = [F()] * 10
         self.assertEqual(select.select([], a, []), ([], a[:5], []))
 
+    def test_disallow_instantiation(self):
+        tp = type(select.poll())
+        self.assertRaises(TypeError, tp)
+
 def tearDownModule():
     support.reap_children()
 
