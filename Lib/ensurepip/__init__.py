6034c4aa58fe7257d39b53c77944393700c66396
Stéphane Bidoul <stephane.bidoul@gmail.com>
2021-05-01 17:50:07 +0200

    [3.8] bpo-43993: Update vendored pip to 21.1.1 (GH-25761). (GH-25783)

--- Lib/ensurepip/__init__.py
+++ Lib/ensurepip/__init__.py
@@ -11,7 +11,7 @@ __all__ = ["version", "bootstrap"]
 
 _SETUPTOOLS_VERSION = "56.0.0"
 
-_PIP_VERSION = "21.1"
+_PIP_VERSION = "21.1.1"
 
 _PROJECTS = [
     ("setuptools", _SETUPTOOLS_VERSION, "py3"),
