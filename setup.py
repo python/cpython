3e65c287ea808404852a8566a4b7237670e68964
Ned Batchelder <ned@nedbatchelder.com>
2021-05-01 12:06:50 -0400

    bpo-41129: fix Mac Catalina filesystem confusion during build
    Catalina puts files in /System/Volumes/Data, so the CPython sources can
    be there.  setup.py shouldn't assume /System means SDK files.
    
    The fix is Ned Deily's suggestion from the bpo.

--- setup.py
+++ setup.py
@@ -230,7 +230,9 @@ def is_macosx_sdk_path(path):
     Returns True if 'path' can be located in an OSX SDK
     """
     return ( (path.startswith('/usr/') and not path.startswith('/usr/local'))
-                or path.startswith('/System/')
+                or (path.startswith('/System/') and not path.startswith('/System/Volumes/Data'))
+                or path.startswith('/System/Library')
+                or path.startswith('/System/iOSSupport')
                 or path.startswith('/Library/') )
 
 
