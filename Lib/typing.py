b11557973476963f7c0fd0041eac175bc0d4c514
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2021-05-04 02:29:49 -0700

    bpo-38352: Add to typing.__all__ (#25821)
    This adds IO, TextIO, BinaryIO, Match, and Pattern.
    
    Co-authored-by: Łukasz Langa <lukasz@langa.pl>
    Co-authored-by: Ken Jin <28750310+Fidget-Spinner@users.noreply.github.com>

--- Lib/typing.py
+++ Lib/typing.py
@@ -99,6 +99,13 @@ __all__ = [
     'NamedTuple',  # Not really a type.
     'TypedDict',  # Not really a type.
     'Generator',
+ 
+    # Other concrete types.
+    'BinaryIO',
+    'IO',
+    'Match',
+    'Pattern',
+    'TextIO',
 
     # One-off things.
     'AnyStr',
