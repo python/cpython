0f154712b77d5ccfaf7d08abff8fbf2c8271a75f
Raymond Hettinger <rhettinger@users.noreply.github.com>
2021-05-06 07:43:13 -0700

    Eliminate duplicated calculations and unnecessary work for linear regression (GH-25922) (cherry picked from commit 55b78ce3c4e23abe4f27bf16d7968f8851532e47)
    Co-authored-by: Raymond Hettinger <rhettinger@users.noreply.github.com>

--- Lib/statistics.py
+++ Lib/statistics.py
@@ -952,11 +952,16 @@ def linear_regression(regressor, dependent_variable, /):
         raise StatisticsError('linear regression requires that both inputs have same number of data points')
     if n < 2:
         raise StatisticsError('linear regression requires at least two data points')
+    x, y = regressor, dependent_variable
+    xbar = fsum(x) / n
+    ybar = fsum(y) / n
+    sxy = fsum((xi - xbar) * (yi - ybar) for xi, yi in zip(x, y))
+    s2x = fsum((xi - xbar) ** 2.0 for xi in x)
     try:
-        slope = covariance(regressor, dependent_variable) / variance(regressor)
+        slope = sxy / s2x
     except ZeroDivisionError:
         raise StatisticsError('regressor is constant')
-    intercept = fmean(dependent_variable) - slope * fmean(regressor)
+    intercept = ybar - slope * xbar
     return LinearRegression(intercept=intercept, slope=slope)
 
 
