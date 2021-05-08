ec0ea5ac59174e108ce9ef2f1c11ca9f4b866f85
Shreyan Avigyan <shreyan.avigyan@gmail.com>
2021-05-09 00:35:47 +0530

    Update PC/winreg.c
    Co-authored-by: Erlend Egeberg Aasland <erlend.aasland@innova.no>

--- PC/winreg.c
+++ PC/winreg.c
@@ -591,7 +591,8 @@ Py2Reg(PyObject *value, DWORD typ, BYTE **retDataBuf, DWORD *retDataSize)
                 if (d == (DWORD64)(-1) && PyErr_Occurred()) {
                     return FALSE;
                 }
-            } else if (value != Py_None) {
+            }
+            else if (value != Py_None) {
                 return FALSE;
             }
             *retDataBuf = (BYTE *)PyMem_NEW(DWORD64, 1);
@@ -2119,4 +2120,3 @@ PyMODINIT_FUNC PyInit_winreg(void)
     ADD_INT(REG_RESOURCE_REQUIREMENTS_LIST);
     return m;
 }
-
