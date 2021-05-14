9af95ca2a6b7d2950ad73a222dc6c5ce9e1c2ced
Erlend Egeberg Aasland <erlend.aasland@innova.no>
2021-05-14 12:27:21 +0200

    [3.9] sqlite3 test suite now works with SQLITE_DQS=0 (GH-26032). (cherry picked from commit be7e467bcf5e419302d887904ef3e8fd310c68e7)
    Co-authored-by: Erlend Egeberg Aasland <erlend.aasland@innova.no>

--- Lib/sqlite3/test/hooks.py
+++ Lib/sqlite3/test/hooks.py
@@ -242,7 +242,7 @@ class TraceCallbackTests(unittest.TestCase):
         # Can't execute bound parameters as their values don't appear
         # in traced statements before SQLite 3.6.21
         # (cf. http://www.sqlite.org/draft/releaselog/3_6_21.html)
-        con.execute('insert into foo(x) values ("%s")' % unicode_value)
+        con.execute("insert into foo(x) values ('%s')" % unicode_value)
         con.commit()
         self.assertTrue(any(unicode_value in stmt for stmt in traced_statements),
                         "Unicode data %s garbled in trace callback: %s"
