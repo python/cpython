790721d93ad6ff7304c23bfc94779f5c660a844d
barneygale <barney.gale@gmail.com>
2021-05-17 01:07:19 +0100

    bpo-38671: Add test that `pathlib.Path.resolve()` returns an absolute path.
    Authored by Tzu-ping Chung (uranusjr)

--- Lib/test/test_pathlib.py
+++ Lib/test/test_pathlib.py
@@ -1799,6 +1799,16 @@ class _BasePathTest(object):
         # Non-strict
         self.assertEqual(r.resolve(strict=False), p / '3' / '4')
 
+    def test_resolve_nonexist_relative_issue38671(self):
+        p = self.cls('non', 'exist')
+
+        old_cwd = os.getcwd()
+        os.chdir(BASE)
+        try:
+            self.assertEqual(p.resolve(), self.cls(BASE, p))
+        finally:
+            os.chdir(old_cwd)
+
     def test_with(self):
         p = self.cls(BASE)
         it = p.iterdir()
