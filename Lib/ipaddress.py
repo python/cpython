d4d4b036fba2e0492faafacd691e8be6738a6b53
wjohnston99 <wayne@teamjohnston.com>
2021-05-17 20:19:33 -0600

    bpo-42937 : Update ipaddress.py to include additional private range of IPs
    There is a lot more IPs in the private address range.   I will create a separate issue for that.  This makes this a very simple request.  I verified the IP and that it is not routeable globally according according to IANA.  This is my first PR for cpython.   Please let me know if bpo or anything like that is not correct.
        
    https://www.iana.org/assignments/iana-ipv4-special-registry/iana-ipv4-special-registry.xhtml

--- Lib/ipaddress.py
+++ Lib/ipaddress.py
@@ -1547,6 +1547,7 @@ class _IPv4Constants:
         IPv4Network('169.254.0.0/16'),
         IPv4Network('172.16.0.0/12'),
         IPv4Network('192.0.0.0/29'),
+        IPv4Network('192.0.0.8/32'),
         IPv4Network('192.0.0.170/31'),
         IPv4Network('192.0.2.0/24'),
         IPv4Network('192.168.0.0/16'),
