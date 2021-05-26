4f725261c6cf23d259e8fdc205e12b76ef4d2d31
Erlend Egeberg Aasland <erlend.aasland@innova.no>
2021-05-26 16:15:27 +0200

    bpo-43988: Add test.support.check_disallow_instantiation() (GH-25757)

--- Lib/test/test_array.py
+++ Lib/test/test_array.py
@@ -42,9 +42,9 @@ class MiscTest(unittest.TestCase):
 
     @support.cpython_only
     def test_disallow_instantiation(self):
-        # Ensure that the type disallows instantiation (bpo-43916)
-        tp = type(iter(array.array('I')))
-        self.assertRaises(TypeError, tp)
+        my_array = array.array("I")
+        tp = type(iter(my_array))
+        support.check_disallow_instantiation(self, tp, my_array)
 
     @support.cpython_only
     def test_immutable(self):
