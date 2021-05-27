35d8f789fad64674a7c7d254f2d5808049f0e504
Kevin Follstad <kfollstad@gmail.com>
2021-05-27 11:09:36 -0700

    bpo-24132: Fix inconsistent organization of is_mount in pathlib
    Existing is_mount method in Path was Posix only and not cross-
    platform even though it was in a cross-platform base class. The existing
    design relied upon being overriden later in WindowsPath. Consolidated
    code from both into new Path.is_mount() which is now cross-plaform
    similiar to all of the other Path methods.

--- Lib/pathlib.py
+++ Lib/pathlib.py
@@ -1322,6 +1322,9 @@ class Path(PurePath):
         """
         Check if this path is a POSIX mount point
         """
+        if os.name != "posix":
+            raise NotImplementedError("Path.is_mount() is "
+                                      "unsupported on this system")
         # Need to exist and be a dir
         if not self.exists() or not self.is_dir():
             return False
@@ -1444,6 +1447,3 @@ class WindowsPath(Path, PureWindowsPath):
     On a Windows system, instantiating a Path should return this object.
     """
     __slots__ = ()
-
-    def is_mount(self):
-        raise NotImplementedError("Path.is_mount() is unsupported on this system")
