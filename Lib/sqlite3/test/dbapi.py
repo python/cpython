66695711071aed166d0d88f55836426de036b58a
Erlend E. Aasland <erlend.aasland@innova.no>
2021-06-02 15:13:52 +0200

    bpo-44165: Add tests for sqlite3 SQL string length

--- Lib/sqlite3/test/dbapi.py
+++ Lib/sqlite3/test/dbapi.py
@@ -229,6 +229,13 @@ class CursorTests(unittest.TestCase):
             */
             """)
 
+    def test_execute_too_long_string(self):
+        # The default value of SQLITE_MAX_LENGTH is 1_000_000_000, but it may
+        # be up to 2_147_483_647.
+        too_long = " " * 2_147_483_648
+        self.assertRaises(sqlite.DataError, self.cx, too_long)
+        self.assertRaises(sqlite.DataError, self.cu.executescript, too_long)
+
     def test_execute_wrong_sql_arg(self):
         with self.assertRaises(TypeError):
             self.cu.execute(42)
