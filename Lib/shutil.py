d340c878ac99cdf4792e1ae5fdeff2561266af98
Jack DeVries <jdevries3133@gmail.com>
2021-06-11 15:36:04 -0400

    bpo-44347: [doc|fix]: add missed updated docstring from previous commit

--- Lib/shutil.py
+++ Lib/shutil.py
@@ -539,9 +539,10 @@ def copytree(src, dst, symlinks=False, ignore=None, copy_function=copy2,
     destination path as arguments. By default, copy2() is used, but any
     function that supports the same signature (like copy()) can be used.
 
-    If dirs_exist_ok is false (the default), a FileExistsError is raised if dst
-    already exists. If dirs_exist_ok is true, dst as well as any any parent
-    directory in the path to dst will be created if they do not exist already.
+    If dirs_exist_ok is false (the default) and dst already exists, a
+    FileExistsError is raised. If dirs_exist_ok is true, dst as well as any
+    parent directory in the path to dst will be created if they do not exist
+    already.
     """
     sys.audit("shutil.copytree", src, dst)
     with os.scandir(src) as itr:
