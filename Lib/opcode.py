50f1b16da362b5961297cc7ac854d6ef77d72518
Mark Shannon <mark@hotpy.org>
2021-06-14 11:04:09 +0100

    bpo-44338: Port LOAD_GLOBAL to PEP 659 adaptive interpreter (GH-26638)
    * Add specializations of LOAD_GLOBAL.
    
    * Add more stats.
    
    * Remove old opcache; it is no longer used.
    
    * Add NEWS

--- Lib/opcode.py
+++ Lib/opcode.py
@@ -226,4 +226,7 @@ _specialized_instructions = [
     "LOAD_ATTR_WITH_HINT",
     "LOAD_ATTR_SLOT",
     "LOAD_ATTR_MODULE",
+    "LOAD_GLOBAL_ADAPTIVE",
+    "LOAD_GLOBAL_MODULE",
+    "LOAD_GLOBAL_BUILTIN",
 ]
