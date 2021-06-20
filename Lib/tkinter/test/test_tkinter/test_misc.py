66eb6215b987d481e9dd276c5954c8721b2b1c8e
E. Paine <63801254+E-Paine@users.noreply.github.com>
2021-06-20 20:43:14 +0100

    Add tests

--- Lib/tkinter/test/test_tkinter/test_misc.py
+++ Lib/tkinter/test/test_tkinter/test_misc.py
@@ -98,6 +98,17 @@ class MiscTest(AbstractTkTest, unittest.TestCase):
         with self.assertRaises(tkinter.TclError):
             root.tk.call(script)
 
+        # Call with a callable class
+        count = 0
+        timer1 = root.after(
+            0,
+            type("callback", (),
+                 {"__call__": staticmethod(callback)})(),
+            42,
+            11)
+        root.update()  # Process all pending events.
+        self.assertEqual(count, 53)
+
     def test_after_idle(self):
         root = self.root
 
