a50e28377bcf37121b55c2de70d95a5386c478f8
Erlend Egeberg Aasland <erlend.aasland@innova.no>
2021-06-23 14:56:40 +0200

    bpo-42064: Move `sqlite3` exceptions to global state, part 1 of 2 (GH-26745)
    Also adds a test to verify the (borrowed) exceptions in `sqlite3.Connection`.

--- Modules/_sqlite/module.h
+++ Modules/_sqlite/module.h
@@ -30,6 +30,11 @@
 #define MODULE_NAME "sqlite3"
 
 typedef struct {
+    PyObject *DatabaseError;
+    PyObject *Error;
+    PyObject *InterfaceError;
+    PyObject *InternalError;
+    PyObject *Warning;
     PyObject *lru_cache;
     PyTypeObject *ConnectionType;
     PyTypeObject *CursorType;
@@ -46,11 +51,6 @@ pysqlite_get_state(PyObject *Py_UNUSED(module))
     return &pysqlite_global_state;
 }
 
-extern PyObject* pysqlite_Error;
-extern PyObject* pysqlite_Warning;
-extern PyObject* pysqlite_InterfaceError;
-extern PyObject* pysqlite_DatabaseError;
-extern PyObject* pysqlite_InternalError;
 extern PyObject* pysqlite_OperationalError;
 extern PyObject* pysqlite_ProgrammingError;
 extern PyObject* pysqlite_IntegrityError;
