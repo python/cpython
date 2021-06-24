d9f21a95da1f54bf355264fbd8c2e9e11f36cf6e
kuzmoyev <ykuzmovych@luc.id>
2021-06-24 14:14:46 +0200

    Fix typo in the 'Mapping.get' docstring

--- Lib/_collections_abc.py
+++ Lib/_collections_abc.py
@@ -811,7 +811,7 @@ class Mapping(Collection):
         raise KeyError
 
     def get(self, key, default=None):
-        'D.get(k[,d]) -> D[k] if k in D, else d.  d defaults to None.'
+        'D.get(k[,d]) -> D[k] if k in D else d.  d defaults to None.'
         try:
             return self[key]
         except KeyError:
