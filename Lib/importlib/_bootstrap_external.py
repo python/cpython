ea17a6718e2753ab0aa88d772995d1f83b296457
Brandt Bucher <brandt@python.org>
2021-06-28 08:29:46 -0700

    Bump magic number

--- Lib/importlib/_bootstrap_external.py
+++ Lib/importlib/_bootstrap_external.py
@@ -360,6 +360,7 @@ _code_type = type(_write_atomic.__code__)
 #     Python 3.11a1 3455 (add MAKE_CELL bpo-43693)
 #     Python 3.11a1 3456 (interleave cell args bpo-43693)
 #     Python 3.11a1 3457 (Change localsplus to a bytes object bpo-43693)
+#     Python 3.11a1 3458 (bpo-44511: remove COPY_DICT_WITHOUT_KEYS and change MATCH_KEYS)
 
 #
 # MAGIC must change whenever the bytecode emitted by the compiler may no
@@ -369,7 +370,7 @@ _code_type = type(_write_atomic.__code__)
 # Whenever MAGIC_NUMBER is changed, the ranges in the magic_values array
 # in PC/launcher.c must also be updated.
 
-MAGIC_NUMBER = (3457).to_bytes(2, 'little') + b'\r\n'
+MAGIC_NUMBER = (3458).to_bytes(2, 'little') + b'\r\n'
 _RAW_MAGIC_NUMBER = int.from_bytes(MAGIC_NUMBER, 'little')  # For import.c
 
 _PYCACHE = '__pycache__'
