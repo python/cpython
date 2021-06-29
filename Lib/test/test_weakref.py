402c9945236ee438388ad49d7e4eca960ef8c743
Pablo Galindo <Pablogsal@gmail.com>
2021-06-29 23:58:45 +0100

    bpo-44523: Remove the pass-through for hash() in weakref proxy objects (GH-26950) (cherry picked from commit e2fea101fd5517f33371b04432842b971021c3bf)
    Co-authored-by: Pablo Galindo <Pablogsal@gmail.com>

--- Lib/test/test_weakref.py
+++ Lib/test/test_weakref.py
@@ -422,14 +422,20 @@ class ReferencesTestCase(TestBase):
         self.assertEqual("".join(reversed(weakref.proxy(obj))), "cba")
 
     def test_proxy_hash(self):
-        cool_hash = 299_792_458
-
         class MyObj:
             def __hash__(self):
-                return cool_hash
+                return 42
 
         obj = MyObj()
-        self.assertEqual(hash(weakref.proxy(obj)), cool_hash)
+        with self.assertRaises(TypeError):
+            hash(weakref.proxy(obj))
+
+        class MyObj:
+            __hash__ = None
+
+        obj = MyObj()
+        with self.assertRaises(TypeError):
+            hash(weakref.proxy(obj))
 
     def test_getweakrefcount(self):
         o = C()
