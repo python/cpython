863e3d5c7e037b24b8294b041ed7686b522973d8
Steve Dower <steve.dower@python.org>
2021-06-30 18:52:39 +0100

    bpo-41180: Replace marshal code.__new__ audit event with marshal.load[s] and marshal.dumps (GH-26971)

--- Lib/test/test_audit.py
+++ Lib/test/test_audit.py
@@ -51,6 +51,11 @@ class AuditTest(unittest.TestCase):
     def test_block_add_hook_baseexception(self):
         self.do_test("test_block_add_hook_baseexception")
 
+    def test_marshal(self):
+        support.import_module("marshal")
+
+        self.do_test("test_marshal")
+
     def test_pickle(self):
         support.import_module("pickle")
 
