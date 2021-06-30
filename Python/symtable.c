f3f0a37b1489f8f4b0738a03ff038f1402f9f384
Batuhan Taskaya <batuhan@python.org>
2021-07-01 01:53:36 +0300

    bpo-44313: generate LOAD_ATTR/CALL_FUNCTION for top-level imported objects (GH-26677)

--- Python/symtable.c
+++ Python/symtable.c
@@ -391,7 +391,7 @@ PySymtable_Lookup(struct symtable *st, void *key)
     return (PySTEntryObject *)v;
 }
 
-static long
+long
 _PyST_GetSymbol(PySTEntryObject *ste, PyObject *name)
 {
     PyObject *v = PyDict_GetItemWithError(ste->ste_symbols, name);
