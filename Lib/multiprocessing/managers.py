d0c55ddfade3fb67bc78ca536a058f4901ad3ce9
finefoot <33361833+finefoot@users.noreply.github.com>
2021-07-02 05:45:02 +0200

    bpo-30256: Add manager_owned keyword arg to AutoProxy (GH-16341)
    Co-authored-by: Jordan Speicher <jordan@jspeicher.com>

--- Lib/multiprocessing/managers.py
+++ Lib/multiprocessing/managers.py
@@ -967,7 +967,7 @@ def MakeProxyType(name, exposed, _cache={}):
 
 
 def AutoProxy(token, serializer, manager=None, authkey=None,
-              exposed=None, incref=True):
+              exposed=None, incref=True, manager_owned=False):
     '''
     Return an auto-proxy for `token`
     '''
@@ -987,7 +987,7 @@ def AutoProxy(token, serializer, manager=None, authkey=None,
 
     ProxyType = MakeProxyType('AutoProxy[%s]' % token.typeid, exposed)
     proxy = ProxyType(token, serializer, manager=manager, authkey=authkey,
-                      incref=incref)
+                      incref=incref, manager_owned=manager_owned)
     proxy._isauto = True
     return proxy
 
