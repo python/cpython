dd42fdf14bd801663fb15400e52e1b84c835e758
Erlend E. Aasland <erlend.aasland@innova.no>
2021-07-02 23:24:23 +0200

    Py_TPFLAGS_METHOD_DESCRIPTOR is inherited if Py_TPFLAGS_IMMUTABLETYPE is set

--- Objects/typeobject.c
+++ Objects/typeobject.c
@@ -5915,8 +5915,8 @@ inherit_slots(PyTypeObject *type, PyTypeObject *base)
          * but only for extension types */
         if (base->tp_descr_get &&
             type->tp_descr_get == base->tp_descr_get &&
-            !(type->tp_flags & Py_TPFLAGS_HEAPTYPE) &&
-            (base->tp_flags & Py_TPFLAGS_METHOD_DESCRIPTOR))
+            _PyType_HasFeature(type, Py_TPFLAGS_IMMUTABLETYPE) &&
+            _PyType_HasFeature(base, Py_TPFLAGS_METHOD_DESCRIPTOR))
         {
             type->tp_flags |= Py_TPFLAGS_METHOD_DESCRIPTOR;
         }
