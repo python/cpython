7ddd0ec800140640974d75c383b021c708615a5a
Dong-hee Na <donghee.na@python.org>
2021-07-05 18:04:36 +0900

    bpo-44558: Make the implementation consistency of operator.indexOf (GH-27012) (cherry picked from commit 09302405d22e86884d6058226790c0cdf5b72f14)
    Co-authored-by: Dong-hee Na <donghee.na@python.org>

--- Lib/test/test_operator.py
+++ Lib/test/test_operator.py
@@ -188,6 +188,9 @@ class OperatorTestCase:
         self.assertRaises(ZeroDivisionError, operator.indexOf, BadIterable(), 1)
         self.assertEqual(operator.indexOf([4, 3, 2, 1], 3), 1)
         self.assertRaises(ValueError, operator.indexOf, [4, 3, 2, 1], 0)
+        nan = float("nan")
+        self.assertEqual(operator.indexOf([nan, nan, 21], nan), 0)
+        self.assertEqual(operator.indexOf([{}, 1, {}, 2], {}), 0)
 
     def test_invert(self):
         operator = self.module
