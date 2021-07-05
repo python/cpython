9f47d872dbebdd95fd6b0a3351aeafaf82dfd203
Miss Islington (bot) <31488909+miss-islington@users.noreply.github.com>
2021-07-05 02:52:04 -0700

    bpo-44558: Make the implementation consistency of operator.indexOf (GH-27012)
    (cherry picked from commit 09302405d22e86884d6058226790c0cdf5b72f14)
    
    Co-authored-by: Dong-hee Na <donghee.na@python.org>

--- Lib/operator.py
+++ Lib/operator.py
@@ -173,7 +173,7 @@ def getitem(a, b):
 def indexOf(a, b):
     "Return the first index of b in a."
     for i, j in enumerate(a):
-        if j == b:
+        if j is b or j == b:
             return i
     else:
         raise ValueError('sequence.index(x): x not in sequence')
