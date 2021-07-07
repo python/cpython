387f16e4ddff94afc6eab9d76a7f9e0f8a9ffce7
Germán Méndez Bravo <german.mb@gmail.com>
2021-07-07 09:52:07 -0700

    Remove pull request number from comment
    Co-authored-by: Ken Jin <28750310+Fidget-Spinner@users.noreply.github.com>

--- Lib/test/typed_dict.py
+++ Lib/test/typed_dict.py
@@ -1,5 +1,5 @@
 """Script used to test `get_type_hints()` on a class formed by inheriting
-a `TypedDict` with postponed annotations, for bpo-41249, pull request #27017.
+a `TypedDict` with postponed annotations, for bpo-41249.
 
 This script uses future annotations to postpone a type that won't be available
 on the module inheriting from to `Foo`. The subclass in the other module should
