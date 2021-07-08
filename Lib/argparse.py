336bd2faf6de65055e900d027ab696f2a052d357
Toshio Kuratomi <a.badger@gmail.com>
2021-07-08 13:02:53 -0700

    Make BooleanOptionalAction's addition of default to help more similar to other actions.
    Help for other actions omit the default value if default is SUPPRESS or
    already contains the special format string '%(default)'.  Add those
    special cases to BooleanOptionalAction's help formatting too.
    
    Other actions emit (default: None) when the default value is None.  This
    allows documenting that the unset case is treated differently.  Add this
    functionality to BooleanOptionalAction as well.
    
    Fixes https://bugs.python.org/issue44587 so that default=SUPPRESS is not
    emitted.
    
    Fixes https://bugs.python.org/issue38956 as this code will detect
    whether '%(default)s' has already been specified in the help string.

--- Lib/argparse.py
+++ Lib/argparse.py
@@ -153,6 +153,27 @@ def _copy_items(items):
 # Formatting Help
 # ===============
 
+def _add_default_to_help_string(action):
+    """
+    Add the default value to the option help message.
+
+    ArgumentDefaultsHelpFormatter and BooleanOptionalAction both want to add
+    the default value to the help message when it isn't already present.  This
+    code will do that, detecting cornercases to prevent duplicates or cases
+    where it wouldn't make sense to the end user.
+    """
+    help = action.help
+    if help is None:
+        help = ''
+
+    if '%(default)' not in help:
+        if action.default is not SUPPRESS:
+            defaulting_nargs = [OPTIONAL, ZERO_OR_MORE]
+            if action.option_strings or action.nargs in defaulting_nargs:
+                help += ' (default: %(default)s)'
+    return help
+
+
 class HelpFormatter(object):
     """Formatter for generating usage messages and argument help strings.
 
@@ -695,13 +716,7 @@ class ArgumentDefaultsHelpFormatter(HelpFormatter):
     """
 
     def _get_help_string(self, action):
-        help = action.help
-        if '%(default)' not in action.help:
-            if action.default is not SUPPRESS:
-                defaulting_nargs = [OPTIONAL, ZERO_OR_MORE]
-                if action.option_strings or action.nargs in defaulting_nargs:
-                    help += ' (default: %(default)s)'
-        return help
+        return _add_default_to_help_string(action)
 
 
 class MetavarTypeHelpFormatter(HelpFormatter):
@@ -717,7 +732,6 @@ class MetavarTypeHelpFormatter(HelpFormatter):
 
     def _get_default_metavar_for_positional(self, action):
         return action.type.__name__
-
 
 
 # =====================
@@ -882,9 +896,6 @@ class BooleanOptionalAction(Action):
                 option_string = '--no-' + option_string[2:]
                 _option_strings.append(option_string)
 
-        if help is not None and default is not None and default is not SUPPRESS:
-            help += " (default: %(default)s)"
-
         super().__init__(
             option_strings=_option_strings,
             dest=dest,
@@ -895,6 +906,9 @@ class BooleanOptionalAction(Action):
             required=required,
             help=help,
             metavar=metavar)
+
+        self.help = _add_default_to_help_string(self)
+
 
     def __call__(self, parser, namespace, values, option_string=None):
         if option_string in self.option_strings:
