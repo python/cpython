83501c42814246d2603e8c33b7c4e2bb372efea9
Leonardo Freua <leonardo.batista.freua@gmail.com>
2021-07-15 11:22:35 -0300

    bpo-44631: Remove the _mock_os_environ_repr method
    * Update test_os.py

--- Lib/test/test_os.py
+++ Lib/test/test_os.py
@@ -991,15 +991,6 @@ class EnvironTests(mapping_tests.BasicTestMappingProtocol):
         os.environ.clear()
         return os.environ
 
-    def _mock_os_environ_repr(self):
-        """Returns a mocked represeting the repr() of os.environ."""
-        environ_items = [
-            f"{key!r}: {value!r}"
-            for key, value in os.environ.items()
-        ]
-        formatted_repr_items = ", ".join(environ_items)
-        return f"environ({{{formatted_repr_items}}})"
-
     # Bug 1110478
     @unittest.skipUnless(unix_shell and os.path.exists(unix_shell),
                          'requires a shell')
@@ -1037,7 +1028,12 @@ class EnvironTests(mapping_tests.BasicTestMappingProtocol):
     # Issue 7310
     def test___repr__(self):
         """Check that the repr() of os.environ looks like environ({...})."""
-        self.assertEqual(repr(os.environ), self._mock_os_environ_repr())
+        env = os.environ
+        formatted_items = ", ".join(
+            f"{key!r}: {value!r}"
+            for key, value in env.items()
+        )
+        self.assertEqual(repr(env), f"environ({{{formatted_items}}})")
 
     def test_get_exec_path(self):
         defpath_list = os.defpath.split(os.pathsep)
