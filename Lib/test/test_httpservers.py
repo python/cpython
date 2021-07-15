0bf4ed08f84f2dc5cb6689334fbee68566637c69
Łukasz Langa <lukasz@langa.pl>
2021-07-15 17:25:23 +0200

    Use str() since os.environ is test.support.os_helper.EnvironmentVarGuard in tests.

--- Lib/test/test_httpservers.py
+++ Lib/test/test_httpservers.py
@@ -855,7 +855,7 @@ class CGIHTTPServerTestCase(BaseTestCase):
             with self.subTest(headers):
                 res = self.request('/cgi-bin/file6.py', 'GET', headers=headers)
                 self.assertEqual(http.HTTPStatus.OK, res.status)
-                print(count, self.HOST, self.PORT, res.length, res.status, repr(res.headers), repr(os.environ))
+                print(count, self.HOST, self.PORT, res.length, res.status, repr(str(res.headers)), str(os.environ))
                 expected = f"'HTTP_ACCEPT': {expected!r}"
                 self.assertIn(expected.encode('ascii'), res.read())
 
