72143efaade208f684c1801daa00a9c502f82ceb
sweeneyde <sweeney.427@osu.edu>
2021-07-15 23:22:25 -0400

    Tweak thresholds based on most recent comparison.

--- Objects/stringlib/fastsearch.h
+++ Objects/stringlib/fastsearch.h
@@ -760,10 +760,11 @@ FASTSEARCH(const STRINGLIB_CHAR* s, Py_ssize_t n,
     }
 
     if (mode != FAST_RSEARCH) {
-        if (n < 3000 || (m < 100 && n < 30000) || m < 6) {
+        if (n < 2500 || (m < 100 && n < 30000) || m < 6) {
             return STRINGLIB(default_find)(s, n, p, m, maxcount, mode);
         }
-        else if (6 * m < n) {
+        else if ((m >> 2) * 3 > (n >> 2)) {
+            /* 33% threshold, but don't overflow. */
             /* For larger problems where the needle isn't a huge
                percentage of the size of the haystack, the relatively
                expensive O(m) startup cost of the two-way algorithm
