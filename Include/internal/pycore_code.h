bbc269026ad9a0a678977e1321185b7e8e6cb5ec
Ken Jin <28750310+Fidget-Spinner@users.noreply.github.com>
2021-07-16 23:49:35 +0800

    Remove legacy opcache structs (GH-27164)

--- Include/internal/pycore_code.h
+++ Include/internal/pycore_code.h
@@ -4,29 +4,6 @@
 extern "C" {
 #endif
 
-/* Legacy Opcache */
-
-typedef struct {
-    PyObject *ptr;  /* Cached pointer (borrowed reference) */
-    uint64_t globals_ver;  /* ma_version of global dict */
-    uint64_t builtins_ver; /* ma_version of builtin dict */
-} _PyOpcache_LoadGlobal;
-
-typedef struct {
-    PyTypeObject *type;
-    Py_ssize_t hint;
-    unsigned int tp_version_tag;
-} _PyOpCodeOpt_LoadAttr;
-
-struct _PyOpcache {
-    union {
-        _PyOpcache_LoadGlobal lg;
-        _PyOpCodeOpt_LoadAttr la;
-    } u;
-    char optimized;
-};
-
-
 /* PEP 659
  * Specialization and quickening structs and helper functions
  */
