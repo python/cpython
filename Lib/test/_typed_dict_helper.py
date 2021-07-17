1ed104a4f2898a4d327cbc102c571439295bf142
Guido van Rossum <gvanrossum@gmail.com>
2021-07-16 20:23:44 -0700

    Update module name in docstring.

--- Lib/test/_typed_dict_helper.py
+++ Lib/test/_typed_dict_helper.py
@@ -4,7 +4,7 @@ This script uses future annotations to postpone a type that won't be available
 on the module inheriting from to `Foo`. The subclass in the other module should
 look something like this:
 
-    class Bar(typed_dict.Foo, total=False):
+    class Bar(_typed_dict_helper.Foo, total=False):
         b: int
 """
 
