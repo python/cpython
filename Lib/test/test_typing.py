fa674bdea3bbb20ad6ccd95b3849fc4995bc37e0
Ken Jin <28750310+Fidget-Spinner@users.noreply.github.com>
2021-07-17 17:36:38 +0800

    [3.9] bpo-41249: Fix postponed annotations for TypedDict (GH-27017) (GH-27205)
    This fixes TypedDict to work with get_type_hints and postponed evaluation of annotations across modules.
    
    This is done by adding the module name to ForwardRef at the time the object is created and using that to resolve the globals during the evaluation.
    
    Co-authored-by: Ken Jin <28750310+Fidget-Spinner@users.noreply.github.com>
    Co-authored-by: Germán Méndez Bravo <german.mb@gmail.com>
    Co-authored-by: Łukasz Langa <lukasz@langa.pl>

--- Lib/test/test_typing.py
+++ Lib/test/test_typing.py
@@ -29,6 +29,7 @@ import weakref
 import types
 
 from test import mod_generics_cache
+from test import _typed_dict_helper
 
 
 class BaseTestCase(TestCase):
@@ -2808,6 +2809,9 @@ class Point2D(TypedDict):
     x: int
     y: int
 
+class Bar(_typed_dict_helper.Foo, total=False):
+    b: int
+
 class LabelPoint2D(Point2D, Label): ...
 
 class Options(TypedDict, total=False):
@@ -3943,6 +3947,12 @@ class TypedDictTests(BaseTestCase):
             'tail': bool,
             'voice': str,
         }
+
+    def test_get_type_hints(self):
+        self.assertEqual(
+            get_type_hints(Bar),
+            {'a': typing.Optional[int], 'b': int}
+        )
 
 
 class IOTests(BaseTestCase):
