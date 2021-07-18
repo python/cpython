f60d59f22b0bbcb86219db7d47a00805f8714297
Serhiy Storchaka <storchaka@gmail.com>
2021-07-18 11:59:43 +0300

    bpo-44654: Do not export the union type related symbols

--- Include/internal/pycore_unionobject.h
+++ Include/internal/pycore_unionobject.h
@@ -8,13 +8,13 @@ extern "C" {
 #  error "this header requires Py_BUILD_CORE define"
 #endif
 
-PyAPI_DATA(PyTypeObject) _PyUnion_Type;
+extern PyTypeObject _PyUnion_Type;
 #define _PyUnion_Check(op) Py_IS_TYPE(op, &_PyUnion_Type)
-PyAPI_FUNC(PyObject *) _Py_union_type_or(PyObject *, PyObject *);
+extern PyObject *_Py_union_type_or(PyObject *, PyObject *);
 
 #define _PyGenericAlias_Check(op) PyObject_TypeCheck(op, &Py_GenericAliasType)
-PyAPI_FUNC(PyObject *) _Py_subs_parameters(PyObject *, PyObject *, PyObject *, PyObject *);
-PyAPI_FUNC(PyObject *) _Py_make_parameters(PyObject *);
+extern PyObject *_Py_subs_parameters(PyObject *, PyObject *, PyObject *, PyObject *);
+extern PyObject *_Py_make_parameters(PyObject *);
 
 #ifdef __cplusplus
 }
