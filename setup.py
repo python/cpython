c2e0b131c7d1db2e479908859ca9abded0c1b04e
Pablo Galindo Salgado <Pablogsal@gmail.com>
2021-07-30 16:14:28 +0100

    [3.10] Fail the CI if an optional module fails to compile (GH-27466). (GH-27481)
    (cherry picked from commit 7cad0bee80a536c7e47f54cf43174175834f30a0)
    
    Co-authored-by: Pablo Galindo Salgado <Pablogsal@gmail.com>

--- setup.py
+++ setup.py
@@ -571,6 +571,9 @@ class PyBuildExt(build_ext):
                 print("Custom linker flags may require --with-openssl-rpath=auto")
             print()
 
+        if os.environ.get("PYTHONSTRICTEXTENSIONBUILD") and (self.failed or self.failed_on_import):
+            raise RuntimeError("Failed to build some stdlib modules")
+
     def build_extension(self, ext):
 
         if ext.name == '_ctypes':
