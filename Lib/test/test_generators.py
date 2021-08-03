0b551db04a99a97abb1e44a071c688c3ca704b67
Miss Islington (bot) <31488909+miss-islington@users.noreply.github.com>
2021-08-03 03:10:54 -0700

    bpo-39091: Fix segfault when Exception constructor returns non-exception for gen.throw. (GH-17658) (GH-27573)
    Co-authored-by: Benjamin Peterson <benjamin@python.org>
    (cherry picked from commit 83ca46b7784b7357d82ec47b33295e09ed7380cb)
    
    Co-authored-by: Noah <33094578+coolreader18@users.noreply.github.com>

--- Lib/test/test_generators.py
+++ Lib/test/test_generators.py
@@ -270,6 +270,32 @@ class ExceptionTest(unittest.TestCase):
         self.assertEqual(next(g), "done")
         self.assertEqual(sys.exc_info(), (None, None, None))
 
+    def test_except_throw_bad_exception(self):
+        class E(Exception):
+            def __new__(cls, *args, **kwargs):
+                return cls
+
+        def boring_generator():
+            yield
+
+        gen = boring_generator()
+
+        err_msg = 'should have returned an instance of BaseException'
+
+        with self.assertRaisesRegex(TypeError, err_msg):
+            gen.throw(E)
+
+        self.assertRaises(StopIteration, next, gen)
+
+        def generator():
+            with self.assertRaisesRegex(TypeError, err_msg):
+                yield
+
+        gen = generator()
+        next(gen)
+        with self.assertRaises(StopIteration):
+            gen.throw(E)
+
     def test_stopiteration_error(self):
         # See also PEP 479.
 
