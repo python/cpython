772a29c344da3c9a1b15c8903e6ebfc239472d51
Erlend E. Aasland <erlend.aasland@innova.no>
2021-08-09 21:24:10 +0200

    Fix complete_statement.py example
    - Use correct attribute name
    - Normalise error variable naming
    - Remove 'Errno' from printed string to avoid confusion

--- Doc/includes/sqlite3/complete_statement.py
+++ Doc/includes/sqlite3/complete_statement.py
@@ -24,10 +24,10 @@ while True:
             if buffer.lstrip().upper().startswith("SELECT"):
                 print(cur.fetchall())
         except sqlite3.Error as e:
-            msg = str(e)
-            error_code = e.sqlite_errorcode
-            error_name = e.sqlite_name
-            print(f"Error {error_name} [Errno {error_code}]: {msg}")
+            err_msg = str(e)
+            err_code = e.sqlite_errorcode
+            err_name = e.sqlite_errorname
+            print(f"{err_name} ({err_code}): {err_msg}")
         buffer = ""
 
 con.close()
