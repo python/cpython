e9ec71ad2c7a1c83f03e172ba7c9f534912b8ba6
Irit Katriel <1055913+iritkatriel@users.noreply.github.com>
2021-08-10 08:56:14 +0100

    bpo-44872: use new trashcan macros in framobject.c (GH-27683) (GH-27690)

--- Objects/frameobject.c
+++ Objects/frameobject.c
@@ -581,7 +581,7 @@ frame_dealloc(PyFrameObject *f)
         _PyObject_GC_UNTRACK(f);
     }
 
-    Py_TRASHCAN_SAFE_BEGIN(f)
+    Py_TRASHCAN_BEGIN(f, frame_dealloc);
     /* Kill all local variables */
     PyObject **valuestack = f->f_valuestack;
     for (PyObject **p = f->f_localsplus; p < valuestack; p++) {
@@ -621,7 +621,7 @@ frame_dealloc(PyFrameObject *f)
     }
 
     Py_DECREF(co);
-    Py_TRASHCAN_SAFE_END(f)
+    Py_TRASHCAN_END;
 }
 
 static inline Py_ssize_t
