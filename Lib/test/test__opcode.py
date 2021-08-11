7433456c15a804a4120b56ed4b66a91be06a8301
Irit Katriel <iritkatriel@yahoo.com>
2021-08-11 16:24:14 +0100

    wrap long line

--- Lib/test/test__opcode.py
+++ Lib/test/test__opcode.py
@@ -85,7 +85,9 @@ class SpecializationStatsTests(unittest.TestCase):
                 stat_names + ['specialization_failure_kinds'])
             for sn in stat_names:
                 self.assertIsInstance(stats['load_attr'][sn], int)
-            self.assertIsInstance(stats['load_attr']['specialization_failure_kinds'], tuple)
+            self.assertIsInstance(
+                stats['load_attr']['specialization_failure_kinds'],
+                tuple)
             for v in stats['load_attr']['specialization_failure_kinds']:
                 self.assertIsInstance(v, int)
         else:
