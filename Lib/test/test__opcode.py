e00b64c34def26d244399f136848a573cbd49de4
Irit Katriel <iritkatriel@yahoo.com>
2021-08-11 16:18:10 +0100

    update test__opcode following changes in GH-27701

--- Lib/test/test__opcode.py
+++ Lib/test/test__opcode.py
@@ -82,12 +82,14 @@ class SpecializationStatsTests(unittest.TestCase):
             self.assertCountEqual(stats.keys(), specialized_opcodes)
             self.assertCountEqual(
                 stats['load_attr'].keys(),
-                stat_names + ['fails'])
+                stat_names + ['specialization_failure_kinds'])
             for sn in stat_names:
                 self.assertIsInstance(stats['load_attr'][sn], int)
             self.assertIsInstance(stats['load_attr']['specialization_failure_kinds'], tuple)
             for v in stats['load_attr']['specialization_failure_kinds']:
                 self.assertIsInstance(v, int)
+        else:
+            print('No specialization stats to check')
 
 
 if __name__ == "__main__":
