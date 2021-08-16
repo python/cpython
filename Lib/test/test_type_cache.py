d7ba8f06215e75c74b1191b642a8fc5133439c81
Fidget-Spinner <28750310+Fidget-Spinner@users.noreply.github.com>
2021-08-16 23:12:20 +0800

    Revert changes in sys module, use _testcapimodule, handle overflow

--- Lib/test/test_type_cache.py
+++ Lib/test/test_type_cache.py
@@ -2,35 +2,43 @@
 import unittest
 from collections import Counter
 from test import support
+from test.support import import_helper
 try:
-    from sys import _clear_type_cache, _get_type_version_tag
+    from sys import _clear_type_cache
 except ImportError:
     _clear_type_cache = None
 
+# Skip this test if the _testcapi module isn't available.
+type_get_version = import_helper.import_module('_testcapi').type_get_version
+
 msg = "requires sys._clear_type_cache and sys._get_type_version_tag"
-@unittest.skipIf(_clear_type_cache is None, msg)
 @support.cpython_only
+@unittest.skipIf(_clear_type_cache is None, msg)
 class TypeCacheTests(unittest.TestCase):
     def test_tp_version_tag_unique(self):
         """tp_version_tag should be unique assuming no overflow, even after
         clearing type cache.
         """
-        all_version_tags = []
-        append_result = all_version_tags.append
+        # Check if global version tag has already overflowed.
+        Y = type('Y', (), {})
+        Y.x = 1
+        Y.x  # Force a _PyType_Lookup, populating version tag
+        y_ver = type_get_version(Y)
+        # Overflow, or not enough left to conduct the test.
+        if y_ver == 0 or y_ver > 0xFFFFF000:
+            self.skipTest("Out of type version tags")
         # Note: try to avoid any method lookups within this loop,
         # It will affect global version tag.
+        all_version_tags = []
+        append_result = all_version_tags.append
+        assertNotEqual = self.assertNotEqual
         for _ in range(30):
             _clear_type_cache()
-            class C:
-                # __init__ is only here to force
-                # C.tp_version_tag to update when initializing
-                # object C().
-                def __init__(self):
-                    pass
-            x = C()
-            tp_version_tag_after = _get_type_version_tag(C)
-            # Try to avoid self.assertNotEqual to not overflow type cache.
-            assert tp_version_tag_after != 0
+            X = type('Y', (), {})
+            X.x = 1
+            X.x
+            tp_version_tag_after = type_get_version(X)
+            assertNotEqual(tp_version_tag_after, 0, msg="Version overflowed")
             append_result(tp_version_tag_after)
         tp_version_tag, count = Counter(all_version_tags).most_common(1)[0]
         self.assertEqual(count, 1,
