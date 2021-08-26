2df71773b80ff5037b7bac35a0c5d1b727f3f4cc
Łukasz Langa <lukasz@langa.pl>
2021-08-26 20:13:34 +0200

    Use a shorter test per Serhiy's review

--- Lib/test/test_range.py
+++ Lib/test/test_range.py
@@ -400,19 +400,11 @@ class RangeTest(unittest.TestCase):
     def test_iterator_pickling_overflowing_index(self):
         for proto in range(pickle.HIGHEST_PROTOCOL + 1):
             with self.subTest(proto=proto):
-                it = itorg = iter(range(2**32 + 2))
-                data = list(range(2**32 + 2)[2**32:])
-
-                d = pickle.dumps(it, proto)
-                it = pickle.loads(d)
-                self.assertEqual(type(itorg), type(it))
-
-                it = pickle.loads(d)
+                it = iter(range(2**32 + 2))
                 it.__setstate__(2**32 + 1)  # undocumented way to set r->index
                 d = pickle.dumps(it, proto)
                 it = pickle.loads(d)
-                self.assertEqual(list(it), data[1:])
-
+                self.assertEqual(next(it), 2**32 + 1)
 
     def test_exhausted_iterator_pickling(self):
         for proto in range(pickle.HIGHEST_PROTOCOL + 1):
