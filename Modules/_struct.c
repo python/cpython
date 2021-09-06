3df665231dc0474908276129fa9a7824b0398433
sobolevn <mail@sobolevn.me>
2021-09-06 16:24:12 +0300

    Fixes `np_short` error message

--- Modules/_struct.c
+++ Modules/_struct.c
@@ -589,9 +589,10 @@ np_short(_structmodulestate *state, char *p, PyObject *v, const formatdef *f)
     if (get_long(state, v, &x) < 0)
         return -1;
     if (x < SHRT_MIN || x > SHRT_MAX) {
-        PyErr_SetString(state->StructError,
-                        "short format requires " Py_STRINGIFY(SHRT_MIN)
-                        " <= number <= " Py_STRINGIFY(SHRT_MAX));
+        PyErr_Format(state->StructError,
+                     "short format requires %d <= number <= %d",
+                     SHRT_MIN,
+                     SHRT_MAX);
         return -1;
     }
     y = (short)x;
@@ -609,7 +610,7 @@ np_ushort(_structmodulestate *state, char *p, PyObject *v, const formatdef *f)
     if (x < 0 || x > USHRT_MAX) {
         PyErr_Format(state->StructError,
                      "ushort format requires 0 <= number <= %u",
-                     (unsigned int)(USHRT_MAX));
+                     (unsigned int)USHRT_MAX);
         return -1;
     }
     y = (unsigned short)x;
