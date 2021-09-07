101253ab39cb71b6e08e7c72b7276039f638f2c6
Erlend E. Aasland <erlend.aasland@innova.no>
2021-09-07 21:58:42 +0200

    Use AC to simplify reference handling in __init__

--- Modules/_sqlite/connection.c
+++ Modules/_sqlite/connection.c
@@ -33,6 +33,32 @@
 #define HAVE_TRACE_V2
 #endif
 
+static int
+clinic_fsconverter(PyObject *pathlike, const char **result)
+{
+    PyObject *bytes = NULL;
+    Py_ssize_t len;
+    char *str;
+
+    if (!PyUnicode_FSConverter(pathlike, &bytes)) {
+        goto error;
+    }
+    else if (PyBytes_AsStringAndSize(bytes, &str, &len) < 0) {
+        goto error;
+    }
+    else if ((*result = PyMem_Malloc(len))) {
+        goto error;
+    }
+
+    memcpy((void *)(*result), str, len);
+    Py_DECREF(bytes);
+    return 1;
+
+error:
+    Py_XDECREF(bytes);
+    return 0;
+}
+
 #define clinic_state() (pysqlite_get_state(NULL))
 #include "clinic/connection.c.h"
 #undef clinic_state
@@ -83,10 +109,21 @@ new_statement_cache(pysqlite_Connection *self, pysqlite_state *state,
     return res;
 }
 
+/*[python input]
+class FSConverter_converter(CConverter):
+    type = "const char *"
+    converter = "clinic_fsconverter"
+    def converter_init(self):
+        self.c_default = "NULL"
+    def cleanup(self):
+        return f"PyMem_Free((void *){self.name});\n"
+[python start generated code]*/
+/*[python end generated code: output=da39a3ee5e6b4b0d input=7b3be538bc4058c0]*/
+
 /*[clinic input]
 _sqlite3.Connection.__init__ as pysqlite_connection_init
 
-    database as database_obj: object(converter='PyUnicode_FSConverter')
+    database: FSConverter
     timeout: double = 5.0
     detect_types: int = 0
     isolation_level: object = NULL
@@ -98,14 +135,13 @@ _sqlite3.Connection.__init__ as pysqlite_connection_init
 
 static int
 pysqlite_connection_init_impl(pysqlite_Connection *self,
-                              PyObject *database_obj, double timeout,
+                              const char *database, double timeout,
                               int detect_types, PyObject *isolation_level,
                               int check_same_thread, PyObject *factory,
                               int cached_statements, int uri)
-/*[clinic end generated code: output=dc19df1c0e2b7b77 input=aa1f21bf12fe907a]*/
+/*[clinic end generated code: output=bc39e55eb0b68783 input=f8d1f7efc0d84104]*/
 {
-    if (PySys_Audit("sqlite3.connect", "O", database_obj) < 0) {
-        Py_DECREF(database_obj);  // needed bco. the AC FSConverter
+    if (PySys_Audit("sqlite3.connect", "s", database) < 0) {
         return -1;
     }
 
@@ -116,7 +152,6 @@ pysqlite_connection_init_impl(pysqlite_Connection *self,
 
     // Create and configure SQLite database object
     sqlite3 *db;
-    const char *database = PyBytes_AsString(database_obj);
     int rc;
     Py_BEGIN_ALLOW_THREADS
     rc = sqlite3_open_v2(database, &db,
@@ -126,8 +161,6 @@ pysqlite_connection_init_impl(pysqlite_Connection *self,
         (void)sqlite3_busy_timeout(db, (int)(timeout*1000));
     }
     Py_END_ALLOW_THREADS
-
-    Py_DECREF(database_obj);  // needed bco. the AC FSConverter
 
     pysqlite_state *state = pysqlite_get_state_by_type(Py_TYPE(self));
     if (rc != SQLITE_OK) {
