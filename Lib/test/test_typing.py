ab284379fa6bbaa6c8a397c72a5fbfa491c2383e
Fidget-Spinner <28750310+Fidget-Spinner@users.noreply.github.com>
2021-09-08 18:32:27 +0800

    remove unrelated test

--- Lib/test/test_typing.py
+++ Lib/test/test_typing.py
@@ -1459,13 +1459,6 @@ class ProtocolTests(BaseTestCase):
         class CustomContextManager(typing.ContextManager, Protocol):
             pass
 
-    def test_non_runtime_protocol_isinstance_check(self):
-        class P(Protocol):
-            x: int
-
-        with self.assertRaisesRegex(TypeError, "@runtime_checkable"):
-            isinstance(1, P)
-
     def test_super_call_init(self):
         class P(Protocol):
             x: int
