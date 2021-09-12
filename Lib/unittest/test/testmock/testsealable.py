8d7824bc7bc0118eaea68169bb5b3eceae017023
sobolevn <mail@sobolevn.me>
2021-09-13 02:07:04 +0300

    Simplifies test case

--- Lib/unittest/test/testmock/testsealable.py
+++ Lib/unittest/test/testmock/testsealable.py
@@ -171,66 +171,64 @@ class TestSealable(unittest.TestCase):
             m.test1().test2.test3().test4()
         self.assertIn("mock.test1().test2.test3().test4", str(cm.exception))
 
-    def test_seal_with_attr_autospec(self):
+    def test_seal_with_autospec(self):
         # https://bugs.python.org/issue45156
         class Foo:
             foo = 0
+            def bar1(self):
+                return 1
+            def bar2(self):
+                return 2
+
+            class Baz:
+                baz = 3
+                def ban(self):
+                    return 4
 
         for spec_set in (True, False):
             with self.subTest(spec_set=spec_set):
                 foo = mock.create_autospec(Foo, spec_set=spec_set)
+                foo.bar1.return_value = 'a'
+                foo.Baz.ban.return_value = 'b'
+
                 mock.seal(foo)
 
                 self.assertIsInstance(foo.foo, mock.NonCallableMagicMock)
+                self.assertIsInstance(foo.bar1, mock.MagicMock)
+                self.assertIsInstance(foo.bar2, mock.MagicMock)
+                self.assertIsInstance(foo.Baz, mock.MagicMock)
+                self.assertIsInstance(foo.Baz.baz, mock.NonCallableMagicMock)
+                self.assertIsInstance(foo.Baz.ban, mock.MagicMock)
+
+                self.assertEqual(foo.bar1(), 'a')
+                foo.bar1.return_value = 'new_a'
+                self.assertEqual(foo.bar1(), 'new_a')
+                self.assertEqual(foo.Baz.ban(), 'b')
+                foo.Baz.ban.return_value = 'new_b'
+                self.assertEqual(foo.Baz.ban(), 'new_b')
+
                 with self.assertRaises(TypeError):
                     foo.foo()
                 with self.assertRaises(AttributeError):
                     foo.bar = 1
                 with self.assertRaises(AttributeError):
-                    foo.missing_attr
-                with self.assertRaises(AttributeError):
-                    foo.missing_method()
+                    foo.bar2()
 
-    def test_seal_with_method_autospec(self):
-        # https://bugs.python.org/issue45156
-        class Foo:
-            def foo(self):
-                return 1
+                foo.bar2.return_value = 'bar2'
+                self.assertEqual(foo.bar2(), 'bar2')
 
-        for spec_set in (True, False):
-            with self.subTest(spec_set=spec_set):
-                foo = mock.create_autospec(Foo, spec_set=spec_set)
-                foo.foo.return_value = 0
-                mock.seal(foo)
-
-                self.assertIsInstance(foo.foo, mock.MagicMock)
-                self.assertEqual(foo.foo(), 0)
-                with self.assertRaises(AttributeError):
-                    foo.bar = 1
                 with self.assertRaises(AttributeError):
                     foo.missing_attr
                 with self.assertRaises(AttributeError):
+                    foo.missing_attr = 1
+                with self.assertRaises(AttributeError):
                     foo.missing_method()
-
-    def test_seal_with_nested_class_autospec(self):
-        # https://bugs.python.org/issue45156
-        class Foo:
-            class Baz:
-                def baz(self):
-                    return 1
-
-        for spec_set in (True, False):
-            with self.subTest(spec_set=spec_set):
-                foo = mock.create_autospec(Foo, spec_set=spec_set)
-                foo.Baz.baz.return_value = 0
-                mock.seal(foo)
-
-                self.assertIsInstance(foo.Baz.baz, mock.MagicMock)
-                self.assertEqual(foo.Baz.baz(), 0)
+                with self.assertRaises(TypeError):
+                    foo.Baz.baz()
                 with self.assertRaises(AttributeError):
-                    foo.bar = 1
+                    foo.Baz.missing_attr
                 with self.assertRaises(AttributeError):
-                    foo.Baz.bar = 1
+                    foo.Baz.missing_attr = 1
                 with self.assertRaises(AttributeError):
                     foo.Baz.missing_method()
 
