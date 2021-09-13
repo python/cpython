48741cb4d3b6619cf534a2d39afb2e9746d0507c
Erlend E. Aasland <erlend.aasland@innova.no>
2021-09-13 10:57:01 +0200

    Address review

--- Lib/test/test_support.py
+++ Lib/test/test_support.py
@@ -426,10 +426,14 @@ class TestSupport(unittest.TestCase):
                              extra=extra,
                              not_exported=not_exported)
 
-        extra = {'TextTestResult', 'installHandler'}
+        extra = {
+            'TextTestResult',
+            'findTestCases',
+            'getTestCaseNames',
+            'installHandler',
+            'makeSuite',
+        }
         not_exported = {'load_tests', "TestProgram", "BaseTestSuite"}
-
-        extra = {'makeSuite', 'findTestCases', 'getTestCaseNames'}
         support.check__all__(self,
                              unittest,
                              ("unittest.result", "unittest.case",
