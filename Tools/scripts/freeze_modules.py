2fbef5c1a1207b2860a08e6144dae2cefcf2e88f
Eric Snow <ericsnowcurrently@gmail.com>
2021-09-24 09:47:47 -0600

    Don't write the generated files if they haven't changed.

--- Tools/scripts/freeze_modules.py
+++ Tools/scripts/freeze_modules.py
@@ -554,10 +554,13 @@ def regen_makefile(modules):
         pyfiles.append(f'\t\t{pyfile} \\')
 
         freeze = (f'Programs/_freeze_module {src.frozenid} '
-                  f'$(srcdir)/{pyfile} $(srcdir)/{header}')
+                  f'$(srcdir)/{pyfile} $(srcdir)/{header}.new')
+        update = (f'$(UPDATE_FILE) --create $(srcdir)/{header} '
+                  f'$(srcdir)/{header}.new')
         rules.extend([
             f'{header}: Programs/_freeze_module {pyfile}',
             f'\t{freeze}',
+            f'\t{update}',
             '',
         ])
     pyfiles[-1] = pyfiles[-1].rstrip(" \\")
