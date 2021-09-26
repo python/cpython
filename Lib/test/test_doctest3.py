b59ce9b1c8abdba972c5e2959f7974a5b26ecdfe
andrei kulakov <andrei.avk@gmail.com>
2021-09-26 10:07:16 -0400

    simplify unit test

--- Lib/test/test_doctest3.py
+++ Lib/test/test_doctest3.py
@@ -3,16 +3,11 @@ import io
 import contextlib
 import doctest
 
-class Test(unittest.TestCase):
+class TestDoctest(unittest.TestCase):
     def test_syntax_error(self):
         f = io.StringIO()
         with contextlib.redirect_stdout(f):
-            try:
-                doctest.run_docstring_examples("""
-                                             >>> 1 1
-                                             """, globals())
-            except Exception as e:
-                print('!!!!!', str(e))
+            doctest.run_docstring_examples('>>> 1 1', globals())
         self.assertIn('''
         1 1
         ^^^
