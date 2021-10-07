ccd82a080056f21da2041d4c5d0ac4421ad0c8a8
Christian Clauss <cclauss@me.com>
2021-10-07 17:30:08 +0200

    [3.10] Fix typos in the Python directory (GH-28767) (GH-28799)
    (cherry picked from commit db693df3e112c5a61f2cbef63eedce3a36520ded)

--- Python/pythonrun.c
+++ Python/pythonrun.c
@@ -2,7 +2,7 @@
 /* Top level execution of Python code (including in __main__) */
 
 /* To help control the interfaces between the startup, execution and
- * shutdown code, the phases are split across separate modules (boostrap,
+ * shutdown code, the phases are split across separate modules (bootstrap,
  * pythonrun, shutdown)
  */
 
@@ -943,7 +943,7 @@ print_exception(PyObject *f, PyObject *value)
                 if (end_lineno > lineno) {
                     end_offset = (error_line != NULL) ? line_size : -1;
                 }
-                // Limit the ammount of '^' that we can display to
+                // Limit the amount of '^' that we can display to
                 // the size of the text in the source line.
                 if (error_line != NULL && end_offset > line_size + 1) {
                     end_offset = line_size + 1;
