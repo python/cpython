68384c4cf715c3f84b993d9998b9cf411f620e9d
Manish Reddy <manishreddyu3@gmail.com>
2021-10-10 22:01:49 +0530

    Create cagr.py

---
+++ cagr.py
@@ -0,0 +1,31 @@
+# =============================================================================
+# Measuring the return of a buy and hold strategy - CAGR
+# Author : Mayank Rasu (http://rasuquant.com/wp/)
+
+# Please report bug/issues in the Q&A section
+# =============================================================================
+
+# Import necesary libraries
+import yfinance as yf
+
+# Download historical data for required stocks
+tickers = ["AMZN","GOOG","MSFT"]
+ohlcv_data = {}
+
+# looping over tickers and storing OHLCV dataframe in dictionary
+for ticker in tickers:
+    temp = yf.download(ticker,period='7mo',interval='1d')
+    temp.dropna(how="any",inplace=True)
+    ohlcv_data[ticker] = temp
+
+def CAGR(DF):
+    "function to calculate the Cumulative Annual Growth Rate of a trading strategy"
+    df = DF.copy()
+    df["return"] = DF["Adj Close"].pct_change()
+    df["cum_return"] = (1 + df["return"]).cumprod()
+    n = len(df)/252
+    CAGR = (df["cum_return"][-1])**(1/n) - 1
+    return CAGR
+
+for ticker in ohlcv_data:
+    print("CAGR of {} = {}".format(ticker,CAGR(ohlcv_data[ticker])))
