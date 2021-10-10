7ffdd1d0f222d6efd32946f5c6686b644b10c77b
Serhiy Storchaka <storchaka@gmail.com>
2021-10-10 22:39:02 +0300

    Simplify

--- Lib/doctest.py
+++ Lib/doctest.py
@@ -1034,12 +1034,8 @@ class DocTestFinder:
         if inspect.isclass(obj) and self._recurse:
             for valname, val in obj.__dict__.items():
                 # Special handling for staticmethod/classmethod.
-                if isinstance(val, staticmethod):
-                    val = getattr(obj, valname)
-                if isinstance(val, classmethod):
-                    # Lookup via __dict__ instead of getattr
-                    # in case it is a classmethod property.
-                    val = obj.__dict__[valname].__func__
+                if isinstance(val, (staticmethod, classmethod)):
+                    val = val.__func__
 
                 # Recurse to methods, properties, and nested classes.
                 if ((inspect.isroutine(val) or inspect.isclass(val) or
