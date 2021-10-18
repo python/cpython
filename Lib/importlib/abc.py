13a6e1c3c760111d441ed1ee80978955e49c13f5
Filipe Laíns <lains@riseup.net>
2021-10-18 20:12:28 +0100

    bpo-45516: add protocol description to the Traversable documentation
    Signed-off-by: Filipe Laíns <lains@riseup.net>

--- Lib/importlib/abc.py
+++ Lib/importlib/abc.py
@@ -381,7 +381,7 @@ class Traversable(Protocol):
     @abc.abstractmethod
     def is_dir(self) -> bool:
         """
-        Return True if self is a dir
+        Return True if self is a directory
         """
 
     @abc.abstractmethod
