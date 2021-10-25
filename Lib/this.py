daa30426c3b7314e6c7065f2222707d6b0c0385c
Aswin Murali <aswinmurali.co@gmail.com>
2021-10-25 22:19:27 +0530

    replaced unneeded comprehension with generator

--- Lib/this.py
+++ Lib/this.py
@@ -23,6 +23,6 @@ Anzrfcnprf ner bar ubaxvat terng vqrn -- yrg'f qb zber bs gubfr!"""
 d = {}
 for c in (65, 97):
     for i in range(26):
-        d[chr(i+c)] = chr((i+13) % 26 + c)
+        d[chr(i + c)] = chr((i + 13) % 26 + c)
 
-print("".join([d.get(c, c) for c in s]))
+print("".join(d.get(c, c) for c in s))
