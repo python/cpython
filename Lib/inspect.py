7226d7df6e7a29d9ae5581a8bb41526f8ef444eb
andrei kulakov <andrei.avk@gmail.com>
2021-10-27 09:29:10 -0400

    guard against access of classproperties

--- Lib/inspect.py
+++ Lib/inspect.py
@@ -546,6 +546,9 @@ def classify_class_attrs(cls):
             try:
                 if name == '__dict__':
                     raise Exception("__dict__ is special, don't want the proxy")
+                o = cls.__dict__.get(name)
+                if isinstance(o, classmethod) and isinstance(o.__func__, property):
+                    raise Exception("class property")
                 get_obj = getattr(cls, name)
             except Exception as exc:
                 pass
