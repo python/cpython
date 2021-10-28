b97322e589f3e33fdd734950a561f95ce4b59cb9
Fidget-Spinner <28750310+Fidget-Spinner@users.noreply.github.com>
2021-10-28 22:37:22 +0800

    Add recursive checks for `CALL_FUNCTION_BUILTIN_O`

--- Python/ceval.c
+++ Python/ceval.c
@@ -4742,8 +4742,12 @@ check_eval_breaker:
             STAT_INC(CALL_FUNCTION, hit);
 
             PyCFunction cfunc = PyCFunction_GET_FUNCTION(callable);
+            if (_Py_EnterRecursiveCall(tstate, " while calling a Python object")) {
+                goto error;
+            }
             PyObject *arg = POP();
             PyObject *res = cfunc(PyCFunction_GET_SELF(callable), arg);
+            _Py_LeaveRecursiveCall(tstate);
             assert((res != NULL) ^ (_PyErr_Occurred(tstate) != NULL));
 
             /* Clear the stack of the function object. */
