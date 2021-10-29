75fe05b804f319ea43b57751f513ed525bef9268
Erlend E. Aasland <erlend.aasland@innova.no>
2021-10-29 23:08:08 +0200

    Add comment and clean up __init__

--- Lib/test/test_sqlite3/__init__.py
+++ Lib/test/test_sqlite3/__init__.py
@@ -1,6 +1,4 @@
-import test.support
-from test.support import import_helper
-from test.support import load_package_tests
+from test.support import import_helper, load_package_tests, verbose
 
 # Skip test if _sqlite3 module not installed
 import_helper.import_module('_sqlite3')
@@ -9,11 +7,12 @@ import unittest
 import os
 import sqlite3
 
+# Implement the unittest "load tests" protocol
 def load_tests(*args):
     pkg_dir = os.path.dirname(__file__)
     return load_package_tests(pkg_dir, *args)
 
-if test.support.verbose:
+if verbose:
     print("test_sqlite3: testing with version",
           "{!r}, sqlite_version {!r}".format(sqlite3.version,
                                              sqlite3.sqlite_version))
