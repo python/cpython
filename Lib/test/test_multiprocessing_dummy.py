9ccb81236563ade902254210d4a6c61a95b26962
Simon-Martin Schröder <martin.schroeder@nerdluecht.de>
2021-11-07 08:53:30 +0100

    Add test_multiprocessing_dummy

---
+++ Lib/test/test_multiprocessing_dummy.py
@@ -0,0 +1,12 @@
+import unittest
+import test._test_multiprocessing
+
+from test import support
+
+if support.PGO:
+    raise unittest.SkipTest("test is not helpful for PGO")
+
+test._test_multiprocessing.install_tests_in_module_dict(globals(), 'dummy')
+
+if __name__ == '__main__':
+    unittest.main()
