f13421af3fc20dd6f0fea5616e752c1003ae3c94
Weipeng Hong <hongweichen8888@sina.com>
2021-11-10 20:01:00 +0800

    Update Lib/inspect.py
    Co-authored-by: Itamar Ostricher <itamarost@gmail.com>

--- Lib/inspect.py
+++ Lib/inspect.py
@@ -492,8 +492,16 @@ def getmembers(object, predicate=None):
 
 def getmembers_static(object, predicate=None):
     """Return all members of an object as (name, value) pairs sorted by name
-    without calling properties and other dynamic. Optionally, only return
-    members that satisfy a given predicate."""
+    without triggering dynamic lookup via the descriptor protocol,
+    __getattr__ or __getattribute__. Optionally, only return members that
+    satisfy a given predicate.
+    
+    Note: this function may not be able to retrieve all members
+       that getmembers can fetch (like dynamically created attributes)
+       and may find members that getmembers can't (like descriptors
+       that raise AttributeError). It can also return descriptor objects
+       instead of instance members in some cases.
+    """
     return _getmembers(object, predicate, getattr_static)
 
 Attribute = namedtuple('Attribute', 'name kind defining_class object')
