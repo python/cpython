ecefcec701d955a3895a6d76905836e31cbda9f3
Erlend E. Aasland <erlend.aasland@innova.no>
2021-11-15 11:00:10 +0100

    Also explicitly test allowed isolation levels

--- Lib/test/test_sqlite3/test_dbapi.py
+++ Lib/test/test_sqlite3/test_dbapi.py
@@ -517,6 +517,11 @@ class ConnectionTests(unittest.TestCase):
         with self.assertRaisesRegex(ValueError, msg):
             memory_database(isolation_level="BOGUS")
 
+    def test_connection_init_good_isolation_levels(self):
+        for level in "", "DEFERRED", "IMMEDIATE", "EXCLUSIVE", None:
+            with memory_database(isolation_level=level) as cx:
+                cx.execute("select 'ok'")
+
 
 class UninitialisedConnectionTests(unittest.TestCase):
     def setUp(self):
