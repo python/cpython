e344ed698827441a8e8b4cea7c9ffd83c84d6b05
Sam Gross <colesbury@gmail.com>
2021-11-18 11:12:33 -0800

    [3.9] bpo-45838: Fix incorrect line numbers in Tools/gdb/libpython.py
    The line number calculation in libpython.py did not properly handle
    negative (signed) line table deltas.

--- Tools/gdb/libpython.py
+++ Tools/gdb/libpython.py
@@ -659,7 +659,10 @@ class PyCodeObjectPtr(PyObjectPtr):
             addr += ord(addr_incr)
             if addr > addrq:
                 return lineno
-            lineno += ord(line_incr)
+            line_delta = ord(line_incr)
+            if line_delta >= 128:
+                line_delta -= 256
+            lineno += line_delta
         return lineno
 
 
