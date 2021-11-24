520e216793e6b5ba2b02a58c4960037e90668ebc
Raymond Hettinger <raymond.hettinger@gmail.com>
2021-11-23 21:12:58 -0700

    Add type hints to the internal function.

--- Lib/statistics.py
+++ Lib/statistics.py
@@ -304,14 +304,14 @@ def _fail_neg(values, errmsg='negative value'):
             raise StatisticsError(errmsg)
         yield x
 
-def _isqrt_frac_rto(n, m):
+def _isqrt_frac_rto(n: int, m: int) -> float:
     'Square root of n/m, rounded to the nearest integer using round-to-odd.'
     a = math.isqrt(n*m) // m
     return a | (a*a*m != n)
 
-def _sqrt_frac(n, m):
+def _sqrt_frac(n: int, m: int) -> float:
     'Square root of n/m as a float, correctly rounded.'
-    q = (n.bit_length() - m.bit_length() - 109) // 2
+    q: int = (n.bit_length() - m.bit_length() - 109) // 2
     if q >= 0:
         return float(_isqrt_frac_rto(n, m << 2 * q) << q)
     else:
