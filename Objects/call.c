8a3096bf233d3be742335e886dd27455390fe18e
Eric Snow <ericsnowcurrently@gmail.com>
2021-12-02 11:42:04 -0700

    Add _PyObject_CallMethod().

--- Objects/call.c
+++ Objects/call.c
@@ -607,7 +607,6 @@ callmethod(PyThreadState *tstate, PyObject* callable, const char *format, va_lis
     return _PyObject_CallFunctionVa(tstate, callable, format, va, is_size_t);
 }
 
-
 PyObject *
 PyObject_CallMethod(PyObject *obj, const char *name, const char *format, ...)
 {
@@ -678,6 +677,17 @@ _PyObject_CallMethodId(PyObject *obj, _Py_Identifier *name,
     va_end(va);
 
     Py_DECREF(callable);
+    return retval;
+}
+
+
+PyObject * _PyObject_CallMethod(PyThreadState *tstate, PyObject *callable,
+                                const char *format, ...)
+{
+    va_list va;
+    va_start(va, format);
+    PyObject *retval = callmethod(tstate, callable, format, va, 0);
+    va_end(va);
     return retval;
 }
 
