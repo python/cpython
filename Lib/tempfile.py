bfd39eaf1346ab7ea1eeda4e07d7d0b91bb4c0ec
Fidget-Spinner <28750310+Fidget-Spinner@users.noreply.github.com>
2021-12-08 23:33:18 +0800

    Address Serhiy's code review

--- Lib/tempfile.py
+++ Lib/tempfile.py
@@ -824,12 +824,15 @@ class TemporaryDirectory:
                         _os.unlink(path)
                     except IsADirectoryError:
                         cls._rmtree(path, ignore_errors=ignore_errors)
-                    # PermissionError is raised on FreeBSD for directories
                     except PermissionError:
+                        # The PermissionError handler was originally added for
+                        # FreeBSD in directories, but it seems that it is raised
+                        # on Windows too.
                         # bpo-43153: Calling _rmtree again may
                         # raise NotADirectoryError and mask the PermissionError.
-                        # So we must re-raise the current PermissionError.
-                        if not _os.path.isdir(path):
+                        # So we must re-raise the current PermissionError if
+                        # path is not a directory.
+                        if _os.path.islink(path) or not _os.path.isdir(path):
                             if ignore_errors:
                                 return
                             raise
