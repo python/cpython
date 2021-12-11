ca950df91335205cd1a15bfcd4d9f48b2ec3dc4f
Serhiy Storchaka <storchaka@gmail.com>
2021-12-11 15:45:32 +0200

    bpo-46032: Check types in singledispatch's register() at declaration time
    The registry() method of functools.singledispatch() functions checks now
    the first argument or the first parameter annotation and raises a TypeError if it is
    not supported. Previously unsupported "types" were ignored (e.g. typing.List[int])
    or caused an error at calling time (e.g. list[int]).

--- Lib/functools.py
+++ Lib/functools.py
@@ -740,6 +740,7 @@ def _compose_mro(cls, types):
     # Remove entries which are already present in the __mro__ or unrelated.
     def is_related(typ):
         return (typ not in bases and hasattr(typ, '__mro__')
+                                 and not isinstance(typ, GenericAlias)
                                  and issubclass(cls, typ))
     types = [n for n in types if is_related(n)]
     # Remove entries which are strict bases of other entries (they will end up
@@ -843,7 +844,9 @@ def singledispatch(func):
 
     def _is_valid_union_type(cls):
         from typing import get_args
-        return _is_union_type(cls) and all(isinstance(arg, type) for arg in get_args(cls))
+        return (_is_union_type(cls) and
+                all(isinstance(arg, type) and not isinstance(arg, GenericAlias)
+                    for arg in get_args(cls)))
 
     def register(cls, func=None):
         """generic_func.register(cls, func) -> func
@@ -852,9 +855,17 @@ def singledispatch(func):
 
         """
         nonlocal cache_token
-        if func is None:
-            if isinstance(cls, type) or _is_valid_union_type(cls):
+        if ((isinstance(cls, type) and not isinstance(cls, GenericAlias))
+                or _is_valid_union_type(cls)):
+            if func is None:
                 return lambda f: register(cls, f)
+        else:
+            if func is not None:
+                raise TypeError(
+                    f"Invalid first argument to `register()`: {cls!r}. "
+                    f"Use either `@register(some_class)` or plain `@register` "
+                    f"on an annotated function."
+                )
             ann = getattr(cls, '__annotations__', {})
             if not ann:
                 raise TypeError(
@@ -867,7 +878,8 @@ def singledispatch(func):
             # only import typing if annotation parsing is necessary
             from typing import get_type_hints
             argname, cls = next(iter(get_type_hints(func).items()))
-            if not isinstance(cls, type) and not _is_valid_union_type(cls):
+            if not ((isinstance(cls, type) and not isinstance(cls, GenericAlias))
+                    or _is_valid_union_type(cls)):
                 if _is_union_type(cls):
                     raise TypeError(
                         f"Invalid annotation for {argname!r}. "
