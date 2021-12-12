94483f1e3cec182fabe19268e579f63045bc984a
Miss Islington (bot) <31488909+miss-islington@users.noreply.github.com>
2021-12-12 08:52:49 -0800

    bpo-46054: Fix parsing error when parsing non-utf8 characters in source files (GH-30068) (GH-30069)
    (cherry picked from commit 4325a766f5f603ef6dfb8c4d5798e5e73cb5efd5)
    
    Co-authored-by: Pablo Galindo Salgado <Pablogsal@gmail.com>
    
    Co-authored-by: Pablo Galindo Salgado <Pablogsal@gmail.com>

--- Lib/test/test_exceptions.py
+++ Lib/test/test_exceptions.py
@@ -2368,6 +2368,18 @@ class SyntaxErrorTests(unittest.TestCase):
         finally:
             unlink(TESTFN)
 
+    def test_non_utf8(self):
+        # Check non utf-8 characters
+        try:
+            with open(TESTFN, 'bw') as testfile:
+                testfile.write(b"\x89")
+            rc, out, err = script_helper.assert_python_failure('-Wd', '-X', 'utf8', TESTFN)
+            err = err.decode('utf-8').splitlines()
+
+            self.assertIn("SyntaxError: Non-UTF-8 code starting with '\\x89' in file", err[-1])
+        finally:
+            unlink(TESTFN)
+
     def test_attributes_new_constructor(self):
         args = ("bad.py", 1, 2, "abcdefg", 1, 100)
         the_exception = SyntaxError("bad bad", args)
