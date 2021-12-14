2c2dcf0c1e108e5e2b7680c62cb33f49aee70caa
DonaldDuck313 <40611243+DonaldDuck313@users.noreply.github.com>
2021-12-14 20:09:34 +0100

    Add support for Termux
    Add support for Termux (https://termux.com/)

--- Lib/webbrowser.py
+++ Lib/webbrowser.py
@@ -545,6 +545,9 @@ def register_standard_browsers():
                         "netscape", "opera", iexplore):
             if shutil.which(browser):
                 register(browser, None, BackgroundBrowser(browser))
+    elif "com.termux" in sys.executable and hasattr(sys, "getandroidapilevel"):
+        # Termux
+        register("termux-open-url", None, GenericBrowser("termux-open-url"))
     else:
         # Prefer X browsers if present
         if os.environ.get("DISPLAY") or os.environ.get("WAYLAND_DISPLAY"):
