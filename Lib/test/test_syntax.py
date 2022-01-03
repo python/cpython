21cb6c35083e4ea242df02783302780fe1fd5b25
Pablo Galindo Salgado <Pablogsal@gmail.com>
2022-01-03 18:29:18 +0000

    Revert "bpo-46110: Add a recursion check to avoid stack overflow in the PEG parser (GH-30177)" (GH-30363)
    This reverts commit cb6e0a3d573cd15bc8a646aa145959790678ec9c temporarily as we want to confirm if this commit is the cause of a slowdown at startup time.

--- Lib/test/test_syntax.py
+++ Lib/test/test_syntax.py
@@ -1729,14 +1729,6 @@ while 1:
 """
         self._check_error(source, "too many statically nested blocks")
 
-    @support.cpython_only
-    def test_error_on_parser_stack_overflow(self):
-        source = "-" * 100000 + "4"
-        for mode in ["exec", "eval", "single"]:
-            with self.subTest(mode=mode):
-                with self.assertRaises(MemoryError):
-                    compile(source, "<string>", mode)
-
 
 def load_tests(loader, tests, pattern):
     tests.addTest(doctest.DocTestSuite())
