2e63b3ef0002caf40c43996e69dea7b7a83b387a
Erlend E. Aasland <erlend.aasland@innova.no>
2022-01-03 13:49:52 +0100

    Simplify example

--- Doc/includes/sqlite3/blob_with.py
+++ Doc/includes/sqlite3/blob_with.py
@@ -2,7 +2,7 @@ import sqlite3
 
 con = sqlite3.connect(":memory:")
 
-con.execute("create table test(id integer primary key, blob_col blob)")
+con.execute("create table test(blob_col blob)")
 con.execute("insert into test(blob_col) values (zeroblob(10))")
 
 with con.open_blob("test", "blob_col", 1) as blob:
