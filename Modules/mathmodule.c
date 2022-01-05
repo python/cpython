1f1a0c70465b63a5d4b8edbba8c9fc580668b145
Mark Dickinson <dickinsm@gmail.com>
2022-01-05 17:11:51 +0000

    Save another Newton step by further expanding the lookup table

--- Modules/mathmodule.c
+++ Modules/mathmodule.c
@@ -1718,23 +1718,46 @@ completes the proof sketch.
 
 */
 
+/*
+    The _approximate_isqrt_tab table provides approximate square roots for
+    16-bit integers. It can be verified by direct computation that for each n
+    in the range 2**14 <= n < 2**16, the value a =
+    _approximate_isqrt_tab[(n >> 8) - 64] is an 8-bit approximate square root
+    of n, satisfying (a - 1)**2 < n < (a + 1)**2.
+
+    The table was computed in Python using the expression:
+
+        [min(round(sqrt(256*n + 128)), 255) for n in range(64, 256)]
+*/
+
+static const uint8_t _approximate_isqrt_tab[192] = {
+    128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139,
+    140, 141, 142, 143, 144, 144, 145, 146, 147, 148, 149, 150,
+    151, 151, 152, 153, 154, 155, 156, 156, 157, 158, 159, 160,
+    160, 161, 162, 163, 164, 164, 165, 166, 167, 167, 168, 169,
+    170, 170, 171, 172, 173, 173, 174, 175, 176, 176, 177, 178,
+    179, 179, 180, 181, 181, 182, 183, 183, 184, 185, 186, 186,
+    187, 188, 188, 189, 190, 190, 191, 192, 192, 193, 194, 194,
+    195, 196, 196, 197, 198, 198, 199, 200, 200, 201, 201, 202,
+    203, 203, 204, 205, 205, 206, 206, 207, 208, 208, 209, 210,
+    210, 211, 211, 212, 213, 213, 214, 214, 215, 216, 216, 217,
+    217, 218, 219, 219, 220, 220, 221, 221, 222, 223, 223, 224,
+    224, 225, 225, 226, 227, 227, 228, 228, 229, 229, 230, 230,
+    231, 232, 232, 233, 233, 234, 234, 235, 235, 236, 237, 237,
+    238, 238, 239, 239, 240, 240, 241, 241, 242, 242, 243, 243,
+    244, 244, 245, 246, 246, 247, 247, 248, 248, 249, 249, 250,
+    250, 251, 251, 252, 252, 253, 253, 254, 254, 255, 255, 255,
+};
 
 /* Approximate square root of a large 64-bit integer.
 
    Given `n` satisfying `2**62 <= n < 2**64`, return `a`
    satisfying `(a - 1)**2 < n < (a + 1)**2`. */
 
-/* Lookup table to short-circuit the first division step in _approximate_isqrt.
-   [8*round(sqrt(72+16*n)) for n in range(12)].  */
-static const uint8_t isqrt_tab[12] = {
-    64, 72, 80, 88, 88, 96, 104, 104, 112, 112, 120, 128,
-};
-
 static inline uint32_t
 _approximate_isqrt(uint64_t n)
 {
-    uint32_t u = isqrt_tab[(n >> 60) - 4];
-    u = u + (uint32_t)(n >> 50) / u;
+    uint32_t u = _approximate_isqrt_tab[(n >> 56) - 64];
     u = (u << 7) + (uint32_t)(n >> 41) / u;
     return (u << 15) + (uint32_t)((n >> 17) / u);
 }
