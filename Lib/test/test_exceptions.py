dde12bb72c2e362fbe89a235d615fe1c03215255
Pablo Galindo Salgado <Pablogsal@gmail.com>
2022-01-08 00:23:40 +0000

    bpo-46237: Fix the line number of tokenizer errors inside f-strings (GH-30463)

--- Lib/test/test_exceptions.py
+++ Lib/test/test_exceptions.py
@@ -268,6 +268,18 @@ class ExceptionTests(unittest.TestCase):
         check("(1+)", 1, 4)
         check("[interesting\nfoo()\n", 1, 1)
         check(b"\xef\xbb\xbf#coding: utf8\nprint('\xe6\x88\x91')\n", 0, -1)
+        check("""f'''
+            {
+            (123_a)
+            }'''""", 3, 17)
+        check("""f'''
+            {
+            f\"\"\"
+            {
+            (123_a)
+            }
+            \"\"\"
+            }'''""", 5, 17)
 
         # Errors thrown by symtable.c
         check('x = [(yield i) for i in range(3)]', 1, 7)
