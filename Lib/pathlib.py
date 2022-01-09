a8dd5eabb9dfc70f6a039084962d3e7831ab99f7
barneygale <barney.gale@gmail.com>
2022-01-09 02:21:03 +0000

    bpo-46148: optimize `pathlib.Path.iterdir()`
    `os.listdir()` doesn't return entries for `.` or `..`, so we don't need to
    check for them here.

--- Lib/pathlib.py
+++ Lib/pathlib.py
@@ -1013,9 +1013,6 @@ class Path(PurePath):
         result for the special paths '.' and '..'.
         """
         for name in self._accessor.listdir(self):
-            if name in {'.', '..'}:
-                # Yielding a path object for these makes little sense
-                continue
             yield self._make_child_relpath(name)
 
     def glob(self, pattern):
