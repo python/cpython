6cb3aa50c1bc003a1febd13fd9942484b56cbd1c
Matthew Rahtz <mrahtz@google.com>
2022-01-20 16:43:25 +0000

    PEP 646: Add test for assigning to a slice containing a star

--- Lib/test/test_pep646_syntax.py
+++ Lib/test/test_pep646_syntax.py
@@ -2,16 +2,24 @@ doctests = """
 
 Setup
 
-    >>> class A:
-    ...    def __class_getitem__(cls, x):
-    ...        return ParameterisedA(x)
-    >>> class ParameterisedA:
-    ...    def __init__(self, params):
-    ...        self._params = params
+    >>> class AClass:
+    ...    def __init__(self):
+    ...        self._setitem_name = None
+    ...        self._setitem_val = None
+    ...    def __setitem__(self, name, val):
+    ...        self._setitem_name = name
+    ...        self._setitem_val = val
     ...    def __repr__(self):
-    ...        return f"A[{self._params}]"
+    ...        return f"A[{self._setitem_name}]={self._setitem_val}"
+    ...    def __getitem__(self, name):
+    ...        return ParameterisedA(name)
+    >>> class ParameterisedA:
+    ...    def __init__(self, name):
+    ...        self._name = name
+    ...    def __repr__(self):
+    ...        return f"A[{self._name}]"
     ...    def __iter__(self):
-    ...        for p in self._params:
+    ...        for p in self._name:
     ...            yield p
     >>> class B:
     ...    def __iter__(self):
@@ -21,6 +29,7 @@ Setup
     >>> class StarredB:
     ...    def __repr__(self):
     ...        return "StarredB"
+    >>> A = AClass()
     >>> b = B()
 
 Slices that are supposed to work, starring our custom B class
@@ -52,6 +61,35 @@ Slices that are supposed to work, starring our custom B class
     >>> A[*A[b, ...]]
     A[(B, Ellipsis)]
 
+Slices assignments that are supposed to work, starring our custom B class
+
+    >>> A[*b] = 1; A
+    A[(StarredB,)]=1
+    >>> A[*b, *b] = 1; A
+    A[(StarredB, StarredB)]=1
+    >>> A[b, *b] = 1; A
+    A[(B, StarredB)]=1
+    >>> A[*b, b] = 1; A
+    A[(StarredB, B)]=1
+    >>> A[b, b, *b] = 1; A
+    A[(B, B, StarredB)]=1
+    >>> A[*b, b, b] = 1; A
+    A[(StarredB, B, B)]=1
+    >>> A[b, *b, b] = 1; A
+    A[(B, StarredB, B)]=1
+    >>> A[b, b, *b, b] = 1; A
+    A[(B, B, StarredB, B)]=1
+    >>> A[b, *b, b, b] = 1; A
+    A[(B, StarredB, B, B)]=1
+    >>> A[A[b, *b, b]] = 1; A
+    A[A[(B, StarredB, B)]]=1
+    >>> A[*A[b, *b, b]] = 1; A
+    A[(B, StarredB, B)]=1
+    >>> A[b, ...] = 1; A
+    A[(B, Ellipsis)]=1
+    >>> A[*A[b, ...]] = 1; A
+    A[(B, Ellipsis)]=1
+
 Slices that are supposed to work, starring a list
 
     >>> l = [1, 2, 3]
@@ -66,6 +104,18 @@ Slices that are supposed to work, starring a list
     >>> repr(A[1:2, *l]) == repr(A[1:2, 1, 2, 3])
     True
 
+Slice assignments that are supposed to work, starring a list
+
+    >>> l = [1, 2, 3]
+    >>> A[*l] = 1; A
+    A[(1, 2, 3)]=1
+    >>> A[*l, 4] = 1; A
+    A[(1, 2, 3, 4)]=1
+    >>> A[0, *l] = 1; A
+    A[(0, 1, 2, 3)]=1
+    >>> A[1:2, *l] = 1; A
+    A[(slice(1, 2, None), 1, 2, 3)]=1
+
 Slices that are supposed to work, starring a tuple
 
     >>> t = (1, 2, 3)
@@ -79,6 +129,18 @@ Slices that are supposed to work, starring a tuple
     A[(slice(1, 2, None), 1, 2, 3)]
     >>> repr(A[1:2, *t]) == repr(A[1:2, 1, 2, 3])
     True
+
+Slice assignments that are supposed to work, starring a tuple
+
+    >>> t = (1, 2, 3)
+    >>> A[*t] = 1; A
+    A[(1, 2, 3)]=1
+    >>> A[*t, 4] = 1; A
+    A[(1, 2, 3, 4)]=1
+    >>> A[0, *t] = 1; A
+    A[(0, 1, 2, 3)]=1
+    >>> A[1:2, *t] = 1; A
+    A[(slice(1, 2, None), 1, 2, 3)]=1
 
 Starring an expression (rather than a name) in a slice
 
@@ -97,6 +159,23 @@ Starring an expression (rather than a name) in a slice
     >>> A[*returns_list(), *returns_list()]
     A[(1, 2, 3, 1, 2, 3)]
 
+Starring an expression (rather than a name) in a slice assignment
+
+    >>> def returns_list():
+    ...     return [1, 2, 3]
+    >>> A[returns_list()] = 1; A
+    A[[1, 2, 3]]=1
+    >>> A[returns_list(), 4] = 1; A
+    A[([1, 2, 3], 4)]=1
+    >>> A[*returns_list()] = 1; A
+    A[(1, 2, 3)]=1
+    >>> A[*returns_list(), 4] = 1; A
+    A[(1, 2, 3, 4)]=1
+    >>> A[0, *returns_list()] = 1; A
+    A[(0, 1, 2, 3)]=1
+    >>> A[*returns_list(), *returns_list()] = 1; A
+    A[(1, 2, 3, 1, 2, 3)]=1
+
 Slices that should fail
 
     >>> A[:*b]
@@ -112,6 +191,25 @@ Slices that should fail
         ...
     SyntaxError: invalid syntax
     >>> A[**b]
+    Traceback (most recent call last):
+        ...
+    SyntaxError: invalid syntax
+
+Slice assignments that should fail
+
+    >>> A[:*b] = 1
+    Traceback (most recent call last):
+        ...
+    SyntaxError: invalid syntax
+    >>> A[*b:] = 1
+    Traceback (most recent call last):
+        ...
+    SyntaxError: invalid syntax
+    >>> A[*b:*b] = 1
+    Traceback (most recent call last):
+        ...
+    SyntaxError: invalid syntax
+    >>> A[**b] = 1
     Traceback (most recent call last):
         ...
     SyntaxError: invalid syntax
@@ -134,9 +232,18 @@ Slices that should fail
     >>> f4.__annotations__
     {'args': StarredB, 'arg1': <class 'int'>}
 
+    >>> def f5(*args: *b = (1,)): pass
+    Traceback (most recent call last):
+        ...
+    SyntaxError: invalid syntax
+
 Other uses of starred expressions as annotations should fail
 
     >>> def f6(x: *b): pass
+    Traceback (most recent call last):
+        ...
+    SyntaxError: invalid syntax
+    >>> def f7(x: *b = 1): pass
     Traceback (most recent call last):
         ...
     SyntaxError: invalid syntax
@@ -144,7 +251,10 @@ Other uses of starred expressions as annotations should fail
     Traceback (most recent call last):
         ...
     SyntaxError: invalid syntax
-
+    >>> x: *b = 1
+    Traceback (most recent call last):
+        ...
+    SyntaxError: invalid syntax
 """
 
 __test__ = {'doctests' : doctests}
