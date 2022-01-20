0be610ed0e5f08f1f58a07337e9c69ef6e36aeba
Pablo Galindo <pablogsal@gmail.com>
2022-01-20 12:22:44 +0000

    Fix interactive mode

--- Parser/pegen.c
+++ Parser/pegen.c
@@ -436,13 +436,14 @@ get_error_line(Parser *p, Py_ssize_t lineno)
 
     char *cur_line = p->tok->fp_interactive ? p->tok->interactive_src_start : p->tok->str;
     assert(cur_line != NULL);
+    const char* buf_end = p->tok->fp_interactive ? p->tok->interactive_src_end : p->tok->inp;
 
     Py_ssize_t relative_lineno = p->starting_lineno ? lineno - p->starting_lineno + 1 : lineno;
 
     for (int i = 0; i < relative_lineno - 1; i++) {
         char *new_line = strchr(cur_line, '\n') + 1;
-        assert(new_line != NULL && new_line < p->tok->inp);
-        if (new_line == NULL || new_line >= p->tok->inp) {
+        assert(new_line != NULL && new_line <= buf_end);
+        if (new_line == NULL || new_line > buf_end) {
             break;
         }
         cur_line = new_line;
