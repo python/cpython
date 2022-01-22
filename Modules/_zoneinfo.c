3e7d06a1fa2102723314552b37410d11fefa928a
Kumar Aditya <59607654+kumaraditya303@users.noreply.github.com>
2022-01-22 16:35:55 +0530

    [3.9] bpo-46383: Fix signature of zoneinfo module_free function (GH-3… (GH-30611)
    …0607)

--- Modules/_zoneinfo.c
+++ Modules/_zoneinfo.c
@@ -2613,7 +2613,7 @@ static PyTypeObject PyZoneInfo_ZoneInfoType = {
 // Specify the _zoneinfo module
 static PyMethodDef module_methods[] = {{NULL, NULL}};
 static void
-module_free()
+module_free(void *m)
 {
     Py_XDECREF(_tzpath_find_tzfile);
     _tzpath_find_tzfile = NULL;
