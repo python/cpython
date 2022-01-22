3c4a3745b900e748f99e80fc3728b534e857d1ff
Miss Islington (bot) <31488909+miss-islington@users.noreply.github.com>
2022-01-22 03:54:07 -0800

    bpo-46425: fix direct invocation of `asyncio` tests (GH-30725)
    (cherry picked from commit 5a5340044ca98cbe6297668d91bccba04b102923)
    
    Co-authored-by: Nikita Sobolev <mail@sobolevn.me>

--- Lib/test/test_asyncio/test_sock_lowlevel.py
+++ Lib/test/test_asyncio/test_sock_lowlevel.py
@@ -1,5 +1,4 @@
 import socket
-import time
 import asyncio
 import sys
 import unittest
@@ -508,3 +507,7 @@ else:
 
         def create_event_loop(self):
             return asyncio.SelectorEventLoop(selectors.SelectSelector())
+
+
+if __name__ == '__main__':
+    unittest.main()
