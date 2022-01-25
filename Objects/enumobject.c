0a02936ee0d1c476cb3ec231815968728dc84134
Eric Snow <ericsnowcurrently@gmail.com>
2022-01-25 11:04:35 -0700

    Get rid of _Py_IDENTIFIER() in enumobject.c.

--- Objects/enumobject.c
+++ Objects/enumobject.c
@@ -356,9 +356,9 @@ reversed_new_impl(PyTypeObject *type, PyObject *seq)
     Py_ssize_t n;
     PyObject *reversed_meth;
     reversedobject *ro;
-    _Py_IDENTIFIER(__reversed__);
 
-    reversed_meth = _PyObject_LookupSpecialId(seq, &PyId___reversed__);
+    PyObject *attr = _Py_GET_GLOBAL_IDENTIFIER(__reversed__);
+    reversed_meth = _PyObject_LookupSpecial(seq, attr);
     if (reversed_meth == Py_None) {
         Py_DECREF(reversed_meth);
         PyErr_Format(PyExc_TypeError,
