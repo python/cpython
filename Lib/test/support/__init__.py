cd983cc750a266c502fb54ce62578595d50b5379
Pablo Galindo Salgado <Pablogsal@gmail.com>
2022-01-25 23:14:03 +0000

    Add skips to crashing tests under sanitizers instead of manually skipping them (GH-30897)
    (cherry picked from commit a27505345e34d462139f5f8b6b5e7c9a59955150)

--- Lib/test/support/__init__.py
+++ Lib/test/support/__init__.py
@@ -41,7 +41,7 @@ __all__ = [
     "requires_IEEE_754", "requires_zlib",
     "anticipate_failure", "load_package_tests", "detect_api_mismatch",
     "check__all__", "skip_if_buggy_ucrt_strfptime",
-    "check_disallow_instantiation", "skip_if_sanitizer",
+    "check_disallow_instantiation", "check_sanitizer", "skip_if_sanitizer",
     # sys
     "is_jython", "is_android", "check_impl_detail", "unix_shell",
     "setswitchinterval",
@@ -367,13 +367,11 @@ def requires_mac_ver(*min_version):
     return decorator
 
 
-def skip_if_sanitizer(reason=None, *, address=False, memory=False, ub=False):
-    """Decorator raising SkipTest if running with a sanitizer active."""
+def check_sanitizer(*, address=False, memory=False, ub=False):
+    """Returns True if Python is compiled with sanitizer support"""
     if not (address or memory or ub):
         raise ValueError('At least one of address, memory, or ub must be True')
 
-    if not reason:
-        reason = 'not working with sanitizers active'
 
     _cflags = sysconfig.get_config_var('CFLAGS') or ''
     _config_args = sysconfig.get_config_var('CONFIG_ARGS') or ''
@@ -389,11 +387,18 @@ def skip_if_sanitizer(reason=None, *, address=False, memory=False, ub=False):
         '-fsanitize=undefined' in _cflags or
         '--with-undefined-behavior-sanitizer' in _config_args
     )
-    skip = (
+    return (
         (memory and memory_sanitizer) or
         (address and address_sanitizer) or
         (ub and ub_sanitizer)
     )
+
+
+def skip_if_sanitizer(reason=None, *, address=False, memory=False, ub=False):
+    """Decorator raising SkipTest if running with a sanitizer active."""
+    if not reason:
+        reason = 'not working with sanitizers active'
+    skip = check_sanitizer(address=address, memory=memory, ub=ub)
     return unittest.skipIf(skip, reason)
 
 
