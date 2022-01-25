dd3900fc80c9cc45df1d264daa02b065081bf863
Eric Snow <ericsnowcurrently@gmail.com>
2022-01-25 11:30:10 -0700

    Get rid of _Py_IDENTIFIER() in listobject.c.

--- Objects/listobject.c
+++ Objects/listobject.c
@@ -3505,25 +3505,28 @@ listreviter_setstate(listreviterobject *it, PyObject *state)
 static PyObject *
 listiter_reduce_general(void *_it, int forward)
 {
-    _Py_IDENTIFIER(iter);
-    _Py_IDENTIFIER(reversed);
-    PyObject *list;
+    PyObject *list, *attr;
 
     /* the objects are not the same, index is of different types! */
     if (forward) {
         listiterobject *it = (listiterobject *)_it;
-        if (it->it_seq)
-            return Py_BuildValue("N(O)n", _PyEval_GetBuiltinId(&PyId_iter),
+        if (it->it_seq) {
+            attr = _Py_GET_GLOBAL_IDENTIFIER(iter);
+            return Py_BuildValue("N(O)n", _PyEval_GetBuiltin(attr),
                                  it->it_seq, it->it_index);
+        }
     } else {
         listreviterobject *it = (listreviterobject *)_it;
-        if (it->it_seq)
-            return Py_BuildValue("N(O)n", _PyEval_GetBuiltinId(&PyId_reversed),
+        if (it->it_seq) {
+            attr = _Py_GET_GLOBAL_IDENTIFIER(reversed);
+            return Py_BuildValue("N(O)n", _PyEval_GetBuiltin(attr),
                                  it->it_seq, it->it_index);
+        }
     }
     /* empty iterator, create an empty list */
     list = PyList_New(0);
     if (list == NULL)
         return NULL;
-    return Py_BuildValue("N(N)", _PyEval_GetBuiltinId(&PyId_iter), list);
+    attr = _Py_GET_GLOBAL_IDENTIFIER(iter);
+    return Py_BuildValue("N(N)", _PyEval_GetBuiltin(attr), list);
 }
