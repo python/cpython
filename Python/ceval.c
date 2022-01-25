4fe944b6129f3a14d0f41f0c14ecfcfc18f578a7
Kumar Aditya <59607654+kumaraditya303@users.noreply.github.com>
2022-01-25 17:09:44 +0530

    use existing label

--- Python/ceval.c
+++ Python/ceval.c
@@ -3094,12 +3094,7 @@ handle_eval_breaker:
                 SETLOCAL(oparg, NULL);
                 DISPATCH();
             }
-            format_exc_check_arg(
-                tstate, PyExc_UnboundLocalError,
-                UNBOUNDLOCAL_ERROR_MSG,
-                PyTuple_GetItem(frame->f_code->co_localsplusnames, oparg)
-                );
-            goto error;
+            goto unbound_local_error;
         }
 
         TARGET(MAKE_CELL) {
