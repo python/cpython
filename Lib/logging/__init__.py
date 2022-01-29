84869597fbd2da337d2ced7e215f9e4fc259da72
mnito <mpnitowski@gmail.com>
2022-01-29 08:02:52 -0500

    Reference bpo issue number in comment

--- Lib/logging/__init__.py
+++ Lib/logging/__init__.py
@@ -2246,7 +2246,7 @@ def _showwarning(message, category, filename, lineno, file=None, line=None):
         logger = getLogger("py.warnings")
         if not logger.handlers:
             logger.addHandler(NullHandler())
-        # Log str(s) directly instead of with logger.warning("%s", s)
+        # bpo-46557: Log str(s) as msg instead of logger.warning("%s", s)
         # since some log aggregation tools group logs by the msg arg
         logger.warning(str(s))
 
