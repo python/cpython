1390ba5e86b7d909472e8a49d48e91e4b8edad8b
CharlieZhao <zhaoyu_hit@qq.com>
2022-01-29 11:34:27 +0800

    Fix test case in `Process`.

--- Lib/test/_test_multiprocessing.py
+++ Lib/test/_test_multiprocessing.py
@@ -268,7 +268,7 @@ class _TestProcess(BaseTestCase):
             [tuple_in_list, lambda arg: self.assertEqual(arg, (1,))]
         ]
         for test_case in test_cases:
-            p = multiprocessing.Process(target=test_case[1], args=test_case[0])
+            p = self.Process(target=test_case[1], args=test_case[0])
             p.start()
 
     def test_daemon_argument(self):
