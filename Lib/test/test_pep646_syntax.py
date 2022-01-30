c0531ec6b3e82c971c763dee5965ccd2ab2aa668
Matthew Rahtz <matthew.rahtz@gmail.com>
2022-01-30 20:27:24 +0000

    Add tests for del

--- Lib/test/test_pep646_syntax.py
+++ Lib/test/test_pep646_syntax.py
@@ -6,13 +6,22 @@ Setup
     ...    def __init__(self):
     ...        self._setitem_name = None
     ...        self._setitem_val = None
+    ...        self._delitem_name = None
     ...    def __setitem__(self, name, val):
+    ...        self._delitem_name = None
     ...        self._setitem_name = name
     ...        self._setitem_val = val
     ...    def __repr__(self):
-    ...        return f"A[{self._setitem_name}]={self._setitem_val}"
+    ...        if self._setitem_name is not None:
+    ...            return f"A[{self._setitem_name}]={self._setitem_val}"
+    ...        elif self._delitem_name is not None:
+    ...            return f"delA[{self._delitem_name}]"
     ...    def __getitem__(self, name):
     ...        return ParameterisedA(name)
+    ...    def __delitem__(self, name):
+    ...        self._setitem_name = None
+    ...        self._delitem_name = name
+    ...        
     >>> class ParameterisedA:
     ...    def __init__(self, name):
     ...        self._name = name
@@ -31,150 +40,216 @@ Setup
     ...        return "StarredB"
     >>> A = AClass()
     >>> b = B()
-
+    
 Slices that are supposed to work, starring our custom B class
 
     >>> A[*b]
     A[(StarredB,)]
-    >>> A[*b, *b]
-    A[(StarredB, StarredB)]
-    >>> A[b, *b]
-    A[(B, StarredB)]
-    >>> A[*b, b]
-    A[(StarredB, B)]
-    >>> A[b, b, *b]
-    A[(B, B, StarredB)]
-    >>> A[*b, b, b]
-    A[(StarredB, B, B)]
-    >>> A[b, *b, b]
-    A[(B, StarredB, B)]
-    >>> A[b, b, *b, b]
-    A[(B, B, StarredB, B)]
-    >>> A[b, *b, b, b]
-    A[(B, StarredB, B, B)]
-    >>> A[A[b, *b, b]]
-    A[A[(B, StarredB, B)]]
-    >>> A[*A[b, *b, b]]
-    A[(B, StarredB, B)]
-    >>> A[b, ...]
-    A[(B, Ellipsis)]
-    >>> A[*A[b, ...]]
-    A[(B, Ellipsis)]
-
-Slices assignments that are supposed to work, starring our custom B class
-
     >>> A[*b] = 1; A
     A[(StarredB,)]=1
+    >>> del A[*b]; A
+    delA[(StarredB,)]
+    
+    >>> A[*b, *b]
+    A[(StarredB, StarredB)]
     >>> A[*b, *b] = 1; A
     A[(StarredB, StarredB)]=1
+    >>> del A[*b, *b]; A
+    delA[(StarredB, StarredB)]
+    
+    >>> A[b, *b]
+    A[(B, StarredB)]
     >>> A[b, *b] = 1; A
     A[(B, StarredB)]=1
+    >>> del A[b, *b]; A
+    delA[(B, StarredB)]
+    
+    >>> A[*b, b]
+    A[(StarredB, B)]
     >>> A[*b, b] = 1; A
     A[(StarredB, B)]=1
+    >>> del A[*b, b]; A
+    delA[(StarredB, B)]
+    
+    >>> A[b, b, *b]
+    A[(B, B, StarredB)]
     >>> A[b, b, *b] = 1; A
     A[(B, B, StarredB)]=1
+    >>> del A[b, b, *b]; A
+    delA[(B, B, StarredB)]
+    
+    >>> A[*b, b, b]
+    A[(StarredB, B, B)]
     >>> A[*b, b, b] = 1; A
     A[(StarredB, B, B)]=1
+    >>> del A[*b, b, b]; A
+    delA[(StarredB, B, B)]
+    
+    >>> A[b, *b, b]
+    A[(B, StarredB, B)]
     >>> A[b, *b, b] = 1; A
     A[(B, StarredB, B)]=1
+    >>> del A[b, *b, b]; A
+    delA[(B, StarredB, B)]
+    
+    >>> A[b, b, *b, b]
+    A[(B, B, StarredB, B)]
     >>> A[b, b, *b, b] = 1; A
     A[(B, B, StarredB, B)]=1
+    >>> del A[b, b, *b, b]; A
+    delA[(B, B, StarredB, B)]
+    
+    >>> A[b, *b, b, b]
+    A[(B, StarredB, B, B)]
     >>> A[b, *b, b, b] = 1; A
     A[(B, StarredB, B, B)]=1
+    >>> del A[b, *b, b, b]; A
+    delA[(B, StarredB, B, B)]
+    
+    >>> A[A[b, *b, b]]
+    A[A[(B, StarredB, B)]]
     >>> A[A[b, *b, b]] = 1; A
     A[A[(B, StarredB, B)]]=1
+    >>> del A[A[b, *b, b]]; A
+    delA[A[(B, StarredB, B)]]
+    
+    >>> A[*A[b, *b, b]]
+    A[(B, StarredB, B)]
     >>> A[*A[b, *b, b]] = 1; A
     A[(B, StarredB, B)]=1
+    >>> del A[*A[b, *b, b]]; A
+    delA[(B, StarredB, B)]
+    
+    >>> A[b, ...]
+    A[(B, Ellipsis)]
     >>> A[b, ...] = 1; A
     A[(B, Ellipsis)]=1
+    >>> del A[b, ...]; A
+    delA[(B, Ellipsis)]
+    
+    >>> A[*A[b, ...]]
+    A[(B, Ellipsis)]
     >>> A[*A[b, ...]] = 1; A
     A[(B, Ellipsis)]=1
+    >>> del A[*A[b, ...]]; A
+    delA[(B, Ellipsis)]
 
 Slices that are supposed to work, starring a list
 
     >>> l = [1, 2, 3]
+    
     >>> A[*l]
     A[(1, 2, 3)]
-    >>> A[*l, 4]
-    A[(1, 2, 3, 4)]
-    >>> A[0, *l]
-    A[(0, 1, 2, 3)]
-    >>> A[1:2, *l]
-    A[(slice(1, 2, None), 1, 2, 3)]
-    >>> repr(A[1:2, *l]) == repr(A[1:2, 1, 2, 3])
-    True
-
-Slice assignments that are supposed to work, starring a list
-
-    >>> l = [1, 2, 3]
     >>> A[*l] = 1; A
     A[(1, 2, 3)]=1
+    >>> del A[*l]; A
+    delA[(1, 2, 3)]
+    
+    >>> A[*l, 4]
+    A[(1, 2, 3, 4)]
     >>> A[*l, 4] = 1; A
     A[(1, 2, 3, 4)]=1
+    >>> del A[*l, 4]; A
+    delA[(1, 2, 3, 4)]
+    
+    >>> A[0, *l]
+    A[(0, 1, 2, 3)]
     >>> A[0, *l] = 1; A
     A[(0, 1, 2, 3)]=1
+    >>> del A[0, *l]; A
+    delA[(0, 1, 2, 3)]
+    
+    >>> A[1:2, *l]
+    A[(slice(1, 2, None), 1, 2, 3)]
     >>> A[1:2, *l] = 1; A
     A[(slice(1, 2, None), 1, 2, 3)]=1
+    >>> del A[1:2, *l]; A
+    delA[(slice(1, 2, None), 1, 2, 3)]
+    
+    >>> repr(A[1:2, *l]) == repr(A[1:2, 1, 2, 3])
+    True
 
 Slices that are supposed to work, starring a tuple
 
     >>> t = (1, 2, 3)
+    
     >>> A[*t]
     A[(1, 2, 3)]
-    >>> A[*t, 4]
-    A[(1, 2, 3, 4)]
-    >>> A[0, *t]
-    A[(0, 1, 2, 3)]
-    >>> A[1:2, *t]
-    A[(slice(1, 2, None), 1, 2, 3)]
-    >>> repr(A[1:2, *t]) == repr(A[1:2, 1, 2, 3])
-    True
-
-Slice assignments that are supposed to work, starring a tuple
-
-    >>> t = (1, 2, 3)
     >>> A[*t] = 1; A
     A[(1, 2, 3)]=1
+    >>> del A[*t]; A
+    delA[(1, 2, 3)]
+    
+    >>> A[*t, 4]
+    A[(1, 2, 3, 4)]
     >>> A[*t, 4] = 1; A
     A[(1, 2, 3, 4)]=1
+    >>> del A[*t, 4]; A
+    delA[(1, 2, 3, 4)]
+    
+    >>> A[0, *t]
+    A[(0, 1, 2, 3)]
     >>> A[0, *t] = 1; A
     A[(0, 1, 2, 3)]=1
+    >>> del A[0, *t]; A
+    delA[(0, 1, 2, 3)]
+    
+    >>> A[1:2, *t]
+    A[(slice(1, 2, None), 1, 2, 3)]
     >>> A[1:2, *t] = 1; A
     A[(slice(1, 2, None), 1, 2, 3)]=1
+    >>> del A[1:2, *t]; A
+    delA[(slice(1, 2, None), 1, 2, 3)]
+    
+    >>> repr(A[1:2, *t]) == repr(A[1:2, 1, 2, 3])
+    True
 
 Starring an expression (rather than a name) in a slice
 
     >>> def returns_list():
     ...     return [1, 2, 3]
+    
     >>> A[returns_list()]
     A[[1, 2, 3]]
-    >>> A[returns_list(), 4]
-    A[([1, 2, 3], 4)]
-    >>> A[*returns_list()]
-    A[(1, 2, 3)]
-    >>> A[*returns_list(), 4]
-    A[(1, 2, 3, 4)]
-    >>> A[0, *returns_list()]
-    A[(0, 1, 2, 3)]
-    >>> A[*returns_list(), *returns_list()]
-    A[(1, 2, 3, 1, 2, 3)]
-
-Starring an expression (rather than a name) in a slice assignment
-
-    >>> def returns_list():
-    ...     return [1, 2, 3]
     >>> A[returns_list()] = 1; A
     A[[1, 2, 3]]=1
+    >>> del A[returns_list()]; A
+    delA[[1, 2, 3]]
+    
+    >>> A[returns_list(), 4]
+    A[([1, 2, 3], 4)]
     >>> A[returns_list(), 4] = 1; A
     A[([1, 2, 3], 4)]=1
+    >>> del A[returns_list(), 4]; A
+    delA[([1, 2, 3], 4)]
+    
+    >>> A[*returns_list()]
+    A[(1, 2, 3)]
     >>> A[*returns_list()] = 1; A
     A[(1, 2, 3)]=1
+    >>> del A[*returns_list()]; A
+    delA[(1, 2, 3)]
+    
+    >>> A[*returns_list(), 4]
+    A[(1, 2, 3, 4)]
     >>> A[*returns_list(), 4] = 1; A
     A[(1, 2, 3, 4)]=1
+    >>> del A[*returns_list(), 4]; A
+    delA[(1, 2, 3, 4)]
+    
+    >>> A[0, *returns_list()]
+    A[(0, 1, 2, 3)]
     >>> A[0, *returns_list()] = 1; A
     A[(0, 1, 2, 3)]=1
+    >>> del A[0, *returns_list()]; A
+    delA[(0, 1, 2, 3)]
+    
+    >>> A[*returns_list(), *returns_list()]
+    A[(1, 2, 3, 1, 2, 3)]
     >>> A[*returns_list(), *returns_list()] = 1; A
     A[(1, 2, 3, 1, 2, 3)]=1
+    >>> del A[*returns_list(), *returns_list()]; A
+    delA[(1, 2, 3, 1, 2, 3)]
 
 Slices that should fail
 
@@ -182,22 +257,16 @@ Slices that should fail
     Traceback (most recent call last):
         ...
     SyntaxError: invalid syntax
-    >>> A[*b:]
-    Traceback (most recent call last):
-        ...
-    SyntaxError: invalid syntax
-    >>> A[*b:*b]
-    Traceback (most recent call last):
-        ...
-    SyntaxError: invalid syntax
-    >>> A[**b]
-    Traceback (most recent call last):
-        ...
-    SyntaxError: invalid syntax
-
-Slice assignments that should fail
-
     >>> A[:*b] = 1
+    Traceback (most recent call last):
+        ...
+    SyntaxError: invalid syntax
+    >>> del A[:*b]
+    Traceback (most recent call last):
+        ...
+    SyntaxError: invalid syntax
+    
+    >>> A[*b:]
     Traceback (most recent call last):
         ...
     SyntaxError: invalid syntax
@@ -205,11 +274,33 @@ Slice assignments that should fail
     Traceback (most recent call last):
         ...
     SyntaxError: invalid syntax
+    >>> del A[*b:]
+    Traceback (most recent call last):
+        ...
+    SyntaxError: invalid syntax
+    
+    >>> A[*b:*b]
+    Traceback (most recent call last):
+        ...
+    SyntaxError: invalid syntax
     >>> A[*b:*b] = 1
     Traceback (most recent call last):
         ...
     SyntaxError: invalid syntax
+    >>> del A[*b:*b]
+    Traceback (most recent call last):
+        ...
+    SyntaxError: invalid syntax
+    
+    >>> A[**b]
+    Traceback (most recent call last):
+        ...
+    SyntaxError: invalid syntax
     >>> A[**b] = 1
+    Traceback (most recent call last):
+        ...
+    SyntaxError: invalid syntax
+    >>> del A[**b]
     Traceback (most recent call last):
         ...
     SyntaxError: invalid syntax
