8a70ff27381937709ad4f4689e19c823083475e8
matrixx3 <72743302+matrixx3@users.noreply.github.com>
2022-02-02 21:33:42 +0530

    changed _io to io (because _io does not exist)

--- Lib/io.py
+++ Lib/io.py
@@ -48,10 +48,10 @@ __all__ = ["BlockingIOError", "open", "open_code", "IOBase", "RawIOBase",
            "UnsupportedOperation", "SEEK_SET", "SEEK_CUR", "SEEK_END"]
 
 
-import _io
+import io
 import abc
 
-from _io import (DEFAULT_BUFFER_SIZE, BlockingIOError, UnsupportedOperation,
+from io import (DEFAULT_BUFFER_SIZE, BlockingIOError, UnsupportedOperation,
                  open, open_code, FileIO, BytesIO, StringIO, BufferedReader,
                  BufferedWriter, BufferedRWPair, BufferedRandom,
                  IncrementalNewlineDecoder, text_encoding, TextIOWrapper)
@@ -61,7 +61,7 @@ def __getattr__(name):
     if name == "OpenWrapper":
         # bpo-43680: Until Python 3.9, _pyio.open was not a static method and
         # builtins.open was set to OpenWrapper to not become a bound method
-        # when set to a class variable. _io.open is a built-in function whereas
+        # when set to a class variable. io.open is a built-in function whereas
         # _pyio.open is a Python function. In Python 3.10, _pyio.open() is now
         # a static method, and builtins.open() is now io.open().
         import warnings
@@ -84,17 +84,17 @@ SEEK_END = 2
 # Declaring ABCs in C is tricky so we do it here.
 # Method descriptions and default implementations are inherited from the C
 # version however.
-class IOBase(_io._IOBase, metaclass=abc.ABCMeta):
-    __doc__ = _io._IOBase.__doc__
+class IOBase(io._IOBase, metaclass=abc.ABCMeta):
+    __doc__ = io._IOBase.__doc__
 
-class RawIOBase(_io._RawIOBase, IOBase):
-    __doc__ = _io._RawIOBase.__doc__
+class RawIOBase(io._RawIOBase, IOBase):
+    __doc__ = io._RawIOBase.__doc__
 
-class BufferedIOBase(_io._BufferedIOBase, IOBase):
-    __doc__ = _io._BufferedIOBase.__doc__
+class BufferedIOBase(io._BufferedIOBase, IOBase):
+    __doc__ = io._BufferedIOBase.__doc__
 
-class TextIOBase(_io._TextIOBase, IOBase):
-    __doc__ = _io._TextIOBase.__doc__
+class TextIOBase(io._TextIOBase, IOBase):
+    __doc__ = io._TextIOBase.__doc__
 
 RawIOBase.register(FileIO)
 
@@ -107,7 +107,7 @@ for klass in (StringIO, TextIOWrapper):
 del klass
 
 try:
-    from _io import _WindowsConsoleIO
+    from io import _WindowsConsoleIO
 except ImportError:
     pass
 else:
