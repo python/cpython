a9cbe0fc0233190c4f9dc274f5373cb810293609
Kumar Aditya <59607654+kumaraditya303@users.noreply.github.com>
2022-02-03 09:26:16 +0530

    add backwards compatibility

--- Python/import.c
+++ Python/import.c
@@ -1299,6 +1299,11 @@ find_frozen(PyObject *nameobj, struct frozen_info *info)
         info->get_code = p->get_code;
         info->size = p->size;
         info->is_package = p->is_package;
+        if (p->size < 0 ) {
+            // backward compatibility with negative size values
+            info->size = -(p->size);
+            info->is_package = true;
+        }
         info->origname = name;
         info->is_alias = resolve_module_alias(name, _PyImport_FrozenAliases,
                                               &info->origname);
