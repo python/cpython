6c20542cc833c83fbb9603f1580d815cf0711b50
GBeauregard <greg@greg.red>
2022-02-05 14:19:13 -0800

    remove list of types exempt from callable check

--- Lib/typing.py
+++ Lib/typing.py
@@ -178,8 +178,6 @@ def _type_check(arg, msg, is_argument=True, module=None, *, allow_special_forms=
         return arg
     if isinstance(arg, _SpecialForm) or arg in (Generic, Protocol):
         raise TypeError(f"Plain {arg} is not valid as type argument")
-    if isinstance(arg, (type, TypeVar, ForwardRef, types.UnionType, ParamSpec)):
-        return arg
     if type(arg) is tuple:
         raise TypeError(f"{msg} Got {arg!r:.100}.")
     return arg
