31c55e815675b66dd87069b2bbab07b3007a4e9c
Victor Stinner <vstinner@python.org>
2022-02-07 01:05:24 +0100

    bpo-46670: Test if a macro is defined, not its value
    * audioop.c: #ifdef WORDS_BIGENDIAN
    * ctypes.h: #ifdef USING_MALLOC_CLOSURE_DOT_C
    * pytime.c: #ifdef __APPLE__
    * unicodeobject.c: #ifdef HAVE_NON_UNICODE_WCHAR_T_REPRESENTATION

--- Modules/_ctypes/callbacks.c
+++ Modules/_ctypes/callbacks.c
@@ -418,7 +418,7 @@ CThunkObject *_ctypes_alloc_callback(PyObject *callable,
         goto error;
     }
 #if HAVE_FFI_PREP_CLOSURE_LOC
-#   if USING_APPLE_OS_LIBFFI
+#   ifdef USING_APPLE_OS_LIBFFI
 #      define HAVE_FFI_PREP_CLOSURE_LOC_RUNTIME __builtin_available(macos 10.15, ios 13, watchos 6, tvos 13, *)
 #   else
 #      define HAVE_FFI_PREP_CLOSURE_LOC_RUNTIME 1
@@ -430,7 +430,7 @@ CThunkObject *_ctypes_alloc_callback(PyObject *callable,
     } else
 #endif
     {
-#if USING_APPLE_OS_LIBFFI && defined(__arm64__)
+#if defined(USING_APPLE_OS_LIBFFI) && defined(__arm64__)
         PyErr_Format(PyExc_NotImplementedError, "ffi_prep_closure_loc() is missing");
         goto error;
 #else
--- Modules/_ctypes/callproc.c
+++ Modules/_ctypes/callproc.c
@@ -826,7 +826,7 @@ static int _call_function_pointer(int flags,
         cc = FFI_STDCALL;
 #endif
 
-#   if USING_APPLE_OS_LIBFFI
+#   ifdef USING_APPLE_OS_LIBFFI
 #      define HAVE_FFI_PREP_CIF_VAR_RUNTIME __builtin_available(macos 10.15, ios 13, watchos 6, tvos 13, *)
 #   elif HAVE_FFI_PREP_CIF_VAR
 #      define HAVE_FFI_PREP_CIF_VAR_RUNTIME true
