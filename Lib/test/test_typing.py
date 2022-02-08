bb73e48935f6a3f5f1e40f57ad2191c10545fb3d
sobolevn <mail@sobolevn.me>
2022-02-08 23:07:41 +0300

    AllTests -> DunderAllTests

--- Lib/test/test_typing.py
+++ Lib/test/test_typing.py
@@ -5441,7 +5441,7 @@ class RevealTypeTests(BaseTestCase):
         self.assertEqual(stderr.getvalue(), "Runtime type is 'object'\n")
 
 
-class AllTests(BaseTestCase):
+class DunderAllTests(BaseTestCase):
     """Tests for __all__."""
 
     def test_all(self):
