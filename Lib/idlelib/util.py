a5890997d835a9840ecbada3bb896dd773db4ccd
Terry Jan Reedy <tjreedy@udel.edu>
2022-02-11 00:04:18 -0500

    Revise util, removing new functions.

--- Lib/idlelib/util.py
+++ Lib/idlelib/util.py
@@ -15,25 +15,7 @@ TODO:
 from os import path
 
 # .pyw is for Windows; .pyi is for stub files.
-PYTHON_EXTENSIONS = frozenset(('.py', '.pyw', '.pyi'))
-
-
-def is_python_extension(extension, valid_extensions=PYTHON_EXTENSIONS):
-    "Identify whether a given string is a valid Python file extension."
-    return extension in valid_extensions
-
-
-def is_python_source(filepath, firstline=None):
-    """Identify whether a given path is valid as a Python module."""
-    if not filepath or path.isdir(filepath):
-        return True
-    base, ext = path.splitext(path.basename(filepath))
-    return is_python_extension(path.normcase(ext)) or (
-        firstline is not None
-        and firstline.startswith('#!')
-        and 'python' in firstline
-    )
-
+py_extensions = frozenset(('.py', '.pyw', '.pyi'))
 
 if __name__ == '__main__':
     from unittest import main
