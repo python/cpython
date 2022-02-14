84811787ad10ce83823a6856223ad383cdd5bbe7
Eric Snow <ericsnowcurrently@gmail.com>
2022-02-14 14:48:10 -0700

    Discover the identifiers.

--- Tools/scripts/generate_global_objects.py
+++ Tools/scripts/generate_global_objects.py
@@ -27,283 +27,107 @@ STRING_LITERALS = {
     'anon_string': '<string>',
     'dot_locals': '.<locals>',
 }
+IGNORED = {
+    'ACTION',  # Python/_warnings.c
+    'ATTR',  # Python/_warnings.c and Objects/funcobject.c
+    'DUNDER',  # Objects/typeobject.c
+    'RDUNDER',  # Objects/typeobject.c
+    'SPECIAL',  # Objects/weakrefobject.c
+}
 IDENTIFIERS = [
-    'Py_Repr',
-    'TextIOWrapper',
+    # from ADD() Python/_warnings.c
+    'default',
+    'ignore',
+
+    # from GET_WARNINGS_ATTR() in Python/_warnings.c
     'WarningMessage',
-    '_',
-    '__IOBase_closed',
-    '__abc_tpflags__',
-    '__abs__',
-    '__abstractmethods__',
-    '__add__',
-    '__aenter__',
-    '__aexit__',
-    '__aiter__',
-    '__all__',
-    '__and__',
-    '__anext__',
-    '__annotations__',
-    '__args__',
-    '__await__',
-    '__bases__',
-    '__bool__',
-    '__build_class__',
-    '__builtins__',
+    '_showwarnmsg',
+    '_warn_unawaited_coroutine',
+    'defaultaction',
+    'filters',
+    'onceregistry',
+
+    # from WRAP_METHOD() in Objects/weakrefobject.c
     '__bytes__',
-    '__call__',
-    '__cantrace__',
-    '__class__',
-    '__class_getitem__',
-    '__classcell__',
-    '__complex__',
-    '__contains__',
-    '__copy__',
-    '__del__',
-    '__delattr__',
-    '__delete__',
-    '__delitem__',
-    '__dict__',
-    '__dir__',
-    '__divmod__',
+    '__reversed__',
+
+    # from COPY_ATTR() in Objects/funcobject.c
+    '__module__',
+    '__name__',
+    '__qualname__',
     '__doc__',
-    '__enter__',
-    '__eq__',
-    '__exit__',
-    '__file__',
+    '__annotations__',
+
+    # from SLOT* in Objects/typeobject.c
+    '__abs__',
+    '__add__',
+    '__and__',
+    '__divmod__',
     '__float__',
     '__floordiv__',
-    '__format__',
-    '__fspath__',
-    '__ge__',
-    '__get__',
-    '__getattr__',
-    '__getattribute__',
-    '__getinitargs__',
     '__getitem__',
-    '__getnewargs__',
-    '__getnewargs_ex__',
-    '__getstate__',
-    '__gt__',
-    '__hash__',
     '__iadd__',
     '__iand__',
     '__ifloordiv__',
     '__ilshift__',
     '__imatmul__',
     '__imod__',
-    '__import__',
     '__imul__',
-    '__index__',
-    '__init__',
-    '__init_subclass__',
-    '__instancecheck__',
     '__int__',
     '__invert__',
     '__ior__',
-    '__ipow__',
     '__irshift__',
-    '__isabstractmethod__',
     '__isub__',
-    '__iter__',
     '__itruediv__',
     '__ixor__',
-    '__le__',
-    '__len__',
-    '__length_hint__',
-    '__loader__',
     '__lshift__',
-    '__lt__',
-    '__ltrace__',
-    '__main__',
     '__matmul__',
-    '__missing__',
     '__mod__',
-    '__module__',
-    '__mro_entries__',
     '__mul__',
-    '__name__',
-    '__ne__',
     '__neg__',
-    '__new__',
-    '__newobj__',
-    '__newobj_ex__',
-    '__next__',
-    '__note__',
     '__or__',
-    '__origin__',
-    '__package__',
-    '__parameters__',
-    '__path__',
     '__pos__',
     '__pow__',
-    '__prepare__',
-    '__qualname__',
     '__radd__',
     '__rand__',
     '__rdivmod__',
-    '__reduce__',
-    '__reduce_ex__',
-    '__repr__',
-    '__reversed__',
     '__rfloordiv__',
     '__rlshift__',
     '__rmatmul__',
     '__rmod__',
     '__rmul__',
     '__ror__',
-    '__round__',
     '__rpow__',
     '__rrshift__',
     '__rshift__',
     '__rsub__',
     '__rtruediv__',
     '__rxor__',
-    '__set__',
-    '__set_name__',
-    '__setattr__',
-    '__setitem__',
-    '__setstate__',
-    '__sizeof__',
-    '__slotnames__',
-    '__slots__',
-    '__spec__',
     '__str__',
     '__sub__',
-    '__subclasscheck__',
-    '__subclasshook__',
     '__truediv__',
-    '__trunc__',
-    '__warningregistry__',
-    '__weakref__',
     '__xor__',
-    '_abc_impl',
-    '_blksize',
-    '_dealloc_warn',
-    '_finalizing',
-    '_find_and_load',
-    '_fix_up_module',
-    '_get_sourcefile',
-    '_handle_fromlist',
-    '_initializing',
-    '_is_text_encoding',
-    '_lock_unlock_module',
-    '_showwarnmsg',
-    '_shutdown',
-    '_slotnames',
-    '_strptime_time',
-    '_uninitialized_submodules',
-    '_warn_unawaited_coroutine',
-    '_xoptions',
-    'add',
-    'append',
-    'big',
-    'buffer',
-    'builtins',
-    'clear',
-    'close',
-    'code',
-    'copy',
-    'copyreg',
-    'decode',
-    'default',
-    'defaultaction',
-    'difference_update',
-    'dispatch_table',
-    'displayhook',
-    'enable',
-    'encoding',
-    'end_lineno',
-    'end_offset',
-    'errors',
-    'excepthook',
-    'extend',
-    'filename',
-    'fileno',
-    'fillvalue',
-    'filters',
-    'find_class',
-    'flush',
-    'get',
-    'get_source',
-    'getattr',
-    'ignore',
-    'importlib',
-    'intersection',
-    'isatty',
-    'items',
-    'iter',
-    'keys',
-    'last_traceback',
-    'last_type',
-    'last_value',
-    'latin1',
-    'lineno',
-    'little',
-    'match',
-    'metaclass',
-    'mode',
-    'modules',
-    'mro',
-    'msg',
-    'n_fields',
-    'n_sequence_fields',
-    'n_unnamed_fields',
-    'name',
-    'obj',
-    'offset',
-    'onceregistry',
-    'open',
-    'parent',
-    'partial',
-    'path',
-    'peek',
-    'persistent_id',
-    'persistent_load',
-    'print_file_and_line',
-    'ps1',
-    'ps2',
-    'raw',
-    'read',
-    'read1',
-    'readable',
-    'readall',
-    'readinto',
-    'readinto1',
-    'readline',
-    'reducer_override',
-    'reload',
-    'replace',
-    'reset',
-    'return',
-    'reversed',
-    'seek',
-    'seekable',
-    'send',
-    'setstate',
-    'sort',
-    'stderr',
-    'stdin',
-    'stdout',
-    'strict',
-    'symmetric_difference_update',
-    'tell',
-    'text',
-    'threading',
-    'throw',
-    'unraisablehook',
-    'values',
-    'version',
-    'warnings',
-    'warnoptions',
-    'writable',
-    'write',
-    'zipimporter',
 ]
 
 
 #######################################
 # helpers
+
+def iter_identifiers():
+    regex = re.compile(r'^.*_Py_ID\((\w+)\)')
+    for dirname, _, files in os.walk(ROOT):
+        if os.path.relpath(dirname, ROOT).startswith('Include'):
+            continue
+        for name in files:
+            if not name.endswith(('.c', '.h')):
+                continue
+            filename = os.path.join(dirname, name)
+            with open(os.path.join(filename), encoding='utf-8') as infile:
+                for line in infile:
+                    m = regex.match(line)
+                    if m:
+                        identifier, = m.groups()
+                        yield identifier, filename, line
 
 def iter_to_marker(lines, marker):
     for line in lines:
@@ -354,7 +178,7 @@ START = '/* The following is auto-generated by Tools/scripts/generate_global_obj
 END = '/* End auto-generated code */'
 
 
-def generate_global_strings():
+def generate_global_strings(identifiers):
     filename = os.path.join(INTERNAL, 'pycore_global_strings.h')
 
     # Read the non-generated part of the file.
@@ -379,14 +203,14 @@ def generate_global_strings():
                         printer.write(f'STRUCT_FOR_STR({name}, "{literal}")')
             outfile.write('\n')
             with printer.block('struct', ' identifiers;'):
-                for name in sorted(IDENTIFIERS):
+                for name in sorted(identifiers):
                     assert name.isidentifier(), name
                     printer.write(f'STRUCT_FOR_ID({name})')
         printer.write(END)
         printer.write(after)
 
 
-def generate_runtime_init():
+def generate_runtime_init(identifiers):
     # First get some info from the declarations.
     nsmallposints = None
     nsmallnegints = None
@@ -438,7 +262,7 @@ def generate_runtime_init():
                             else:
                                 printer.write(f'INIT_STR({name}, "{literal}"),')
                     with printer.block('.identifiers =', ','):
-                        for name in sorted(IDENTIFIERS):
+                        for name in sorted(identifiers):
                             assert name.isidentifier(), name
                             printer.write(f'INIT_ID({name}),')
         printer.write(END)
@@ -507,9 +331,9 @@ TYPESLOTS_RE = re.compile(r'''
      )
 ''', re.VERBOSE)
 
-def check_orphan_strings():
+def check_orphan_strings(identifiers):
     literals = set(n for n, s in STRING_LITERALS.items() if s)
-    identifiers = set(IDENTIFIERS)
+    identifiers = set(identifiers)
     files = glob.iglob(os.path.join(ROOT, '**', '*.[ch]'), recursive=True)
     for i, filename in enumerate(files, start=1):
         print('.', end='')
@@ -586,11 +410,16 @@ def check_orphan_strings():
 # the script
 
 def main(*, check=False) -> None:
-    generate_global_strings()
-    generate_runtime_init()
+    identifiers = {name
+                   for name, _, _ in iter_identifiers()
+                   if name not in IGNORED}
+    identifiers.update(IDENTIFIERS)
+
+    generate_global_strings(identifiers)
+    generate_runtime_init(identifiers)
 
     if check:
-        check_orphan_strings()
+        check_orphan_strings(identifiers)
 
 
 if __name__ == '__main__':
