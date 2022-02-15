137ebe6de92a3c7995c434e01f0fe501ed441123
Guido van Rossum <guido@python.org>
2022-02-14 20:28:07 -0800

    Replace EdgeDb copyright with a simpler attribution
    Yury, the author, re-licenses the code under PSFL,
    and since he's signed Python's contributor agreement he has no problem
    assigning his copyright on the implementation and tests to the PSF.

--- Lib/asyncio/taskgroups.py
+++ Lib/asyncio/taskgroups.py
@@ -1,20 +1,4 @@
-#
-# This source file is part of the EdgeDB open source project.
-#
-# Copyright 2016-present MagicStack Inc. and the EdgeDB authors.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+# Adapted with permission from the EdgeDb project.
 
 
 __all__ = ["TaskGroup"]
