e5e5a60b82254e0f88d6fdbbd1c00e5e89fd4c2c
Terry Jan Reedy <tjreedy@udel.edu>
2022-02-17 15:34:00 -0500

    Simpler fix

--- Lib/os.py
+++ Lib/os.py
@@ -331,7 +331,7 @@ def walk(top, topdown=True, onerror=None, followlinks=False):
     import os
     from os.path import join, getsize
     for root, dirs, files in os.walk('python/Lib/email'):
-        print(root, "consumes", end=" ")
+        print(root, "consumes ")
         print(sum(getsize(join(root, name)) for name in files), end=" ")
         print("bytes in", len(files), "non-directory files")
         if 'CVS' in dirs:
