9028eec9799ace18b55ee5dde6033a9b7be59566
Andrew Svetlov <andrew.svetlov@gmail.com>
2022-02-20 13:10:21 +0200

    Fix

--- Lib/asyncio/tasks.py
+++ Lib/asyncio/tasks.py
@@ -823,7 +823,6 @@ def _gather(*coros_or_futures, loop=None, return_exceptions=False):
     children = []
     nfuts = 0
     nfinished = 0
-    loop = None
     outer = None  # bpo-46672
     for arg in coros_or_futures:
         if arg not in arg_to_fut:
