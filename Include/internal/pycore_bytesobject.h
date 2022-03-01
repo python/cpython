fe3ccbaef8db2d57f1636f21ba053477756832db
sweeneyde <sweeney.427@osu.edu>
2022-02-28 20:57:21 -0500

    PyAPI_FUNC --> extern

--- Include/internal/pycore_bytesobject.h
+++ Include/internal/pycore_bytesobject.h
@@ -15,12 +15,12 @@ extern PyStatus _PyBytes_InitTypes(PyInterpreterState *);
 
 /* Substring Search */
 
-PyAPI_FUNC(Py_ssize_t)
+extern Py_ssize_t
 _PyBytes_Find(const char *haystack, Py_ssize_t len_haystack,
               const char *needle, Py_ssize_t len_needle,
               Py_ssize_t offset);
 
-PyAPI_FUNC(Py_ssize_t)
+extern Py_ssize_t
 _PyBytes_ReverseFind(const char *haystack, Py_ssize_t len_haystack,
                      const char *needle, Py_ssize_t len_needle,
                      Py_ssize_t offset);
