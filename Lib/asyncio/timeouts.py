dac58742d134a945388179641886f1a58bd38811
Andrew Svetlov <andrew.svetlov@gmail.com>
2022-03-02 14:57:41 +0200

    bpo-46771: Implement asyncio context managers for handling timeouts

---
+++ Lib/asyncio/timeouts.py
@@ -0,0 +1,141 @@
+import enum
+
+from types import TracebackType
+from typing import final, Any, Dict, Optional, Type
+
+from . import events
+from . import tasks
+
+
+__all__ = (
+    "Timeout",
+    "timeout",
+    "timeout_at",
+)
+
+
+class _State(enum.Enum):
+    CREATED = "created"
+    ENTERED = "active"
+    EXPIRING = "expiring"
+    EXPIRED = "expired"
+    EXITED = "exited"
+
+
+@final
+class Timeout:
+
+    def __init__(self, deadline: Optional[float]) -> None:
+        self._state = _State.CREATED
+
+        self._timeout_handler: Optional[events.TimerHandle] = None
+        self._task: Optional[tasks.Task[Any]] = None
+        self._deadline = deadline
+
+    def when(self) -> Optional[float]:
+        return self._deadline
+
+    def reschedule(self, deadline: Optional[float]) -> None:
+        assert self._state != _State.CREATED
+        if self._state != _State.ENTERED:
+            raise RuntimeError(
+                f"Cannot change state of {self._state} CancelScope",
+            )
+
+        self._deadline = deadline
+
+        if self._timeout_handler is not None:
+            self._timeout_handler.cancel()
+
+        if deadline is None:
+            self._timeout_handler = None
+        else:
+            loop = events.get_running_loop()
+            self._timeout_handler = loop.call_at(
+                deadline,
+                self._on_timeout,
+            )
+
+    def expired(self) -> bool:
+        """Is timeout expired during execution?"""
+        return self._state in (_State.EXPIRING, _State.EXPIRED)
+
+    def __repr__(self) -> str:
+        info = [str(self._state)]
+        if self._state is _State.ENTERED and self._deadline is not None:
+            info.append(f"deadline={self._deadline}")
+        cls_name = self.__class__.__name__
+        return f"<{cls_name} at {id(self):#x}, {' '.join(info)}>"
+
+    async def __aenter__(self) -> "Timeout":
+        self._state = _State.ENTERED
+        self._task = tasks.current_task()
+        if self._task is None:
+            raise RuntimeError("Timeout should be used inside a task")
+        self.reschedule(self._deadline)
+        return self
+
+    async def __aexit__(
+        self,
+        exc_type: Optional[Type[BaseException]],
+        exc_val: Optional[BaseException],
+        exc_tb: Optional[TracebackType],
+    ) -> Optional[bool]:
+        assert self._state in (_State.ENTERED, _State.EXPIRING)
+
+        if self._timeout_handler is not None:
+            self._timeout_handler.cancel()
+            self._timeout_handler = None
+
+        if self._state is _State.EXPIRING:
+            self._state = _State.EXPIRED
+
+            if self._task.uncancel() == 0:
+                # Since there are no outstanding cancel requests, we're
+                # handling this.
+                raise TimeoutError
+        elif self._state is _State.ENTERED:
+            self._state = _State.EXITED
+
+        return None
+
+    def _on_timeout(self) -> None:
+        assert self._state is _State.ENTERED
+        self._task.cancel()
+        self._state = _State.EXPIRING
+        # drop the reference early
+        self._timeout_handler = None
+
+
+def timeout(delay: Optional[float]) -> Timeout:
+    """timeout context manager.
+
+    Useful in cases when you want to apply timeout logic around block
+    of code or in cases when asyncio.wait_for is not suitable. For example:
+
+    >>> with timeout(10):  # 10 seconds timeout
+    ...     await long_running_task()
+
+
+    delay - value in seconds or None to disable timeout logic
+    """
+    loop = events.get_running_loop()
+    return Timeout(loop.time() + delay if delay is not None else None)
+
+
+def timeout_at(deadline: Optional[float]) -> Timeout:
+    """Schedule the timeout at absolute time.
+
+    deadline argument points on the time in the same clock system
+    as loop.time().
+
+    Please note: it is not POSIX time but a time with
+    undefined starting base, e.g. the time of the system power on.
+
+    >>> async with timeout_at(loop.time() + 10):
+    ...     async with aiohttp.get('https://github.com') as r:
+    ...         await r.text()
+
+
+    """
+    return Timeout(deadline)
---
+++ Lib/test/test_asyncio/test_timeouts.py
@@ -0,0 +1,198 @@
+"""Tests for asyncio/timeouts.py"""
+
+import unittest
+import time
+
+import asyncio
+from asyncio import tasks
+
+
+def tearDownModule():
+    asyncio.set_event_loop_policy(None)
+
+
+class BaseTimeoutTests:
+    Task = None
+
+    def new_task(self, loop, coro, name='TestTask'):
+        return self.__class__.Task(coro, loop=loop, name=name)
+
+    def _setupAsyncioLoop(self):
+        assert self._asyncioTestLoop is None, 'asyncio test loop already initialized'
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+        loop.set_debug(True)
+        self._asyncioTestLoop = loop
+        loop.set_task_factory(self.new_task)
+        fut = loop.create_future()
+        self._asyncioCallsTask = loop.create_task(self._asyncioLoopRunner(fut))
+        loop.run_until_complete(fut)
+
+    async def test_timeout_basic(self):
+        with self.assertRaises(TimeoutError):
+            async with asyncio.timeout(0.01) as cm:
+                await asyncio.sleep(10)
+        self.assertTrue(cm.expired())
+
+    async def test_timeout_at_basic(self):
+        loop = asyncio.get_running_loop()
+
+        with self.assertRaises(TimeoutError):
+            deadline = loop.time() + 0.01
+            async with asyncio.timeout_at(deadline) as cm:
+                await asyncio.sleep(10)
+        self.assertTrue(cm.expired())
+        self.assertEqual(deadline, cm.when())
+
+    async def test_nested_timeouts(self):
+        cancel = False
+        with self.assertRaises(TimeoutError):
+            async with asyncio.timeout(0.01) as cm1:
+                try:
+                    async with asyncio.timeout(0.01) as cm2:
+                        await asyncio.sleep(10)
+                except asyncio.CancelledError:
+                    cancel = True
+                    raise
+                except TimeoutError:
+                    self.fail(
+                        "The only topmost timed out context manager "
+                        "raises TimeoutError"
+                    )
+        self.assertTrue(cancel)
+        self.assertTrue(cm1.expired())
+        self.assertTrue(cm2.expired())
+
+    async def test_waiter_cancelled(self):
+        with self.assertRaises(TimeoutError):
+            async with asyncio.timeout(0.01):
+                with self.assertRaises(asyncio.CancelledError):
+                    await asyncio.sleep(10)
+
+    async def test_timeout_not_called(self):
+        loop = asyncio.get_running_loop()
+        t0 = loop.time()
+        async with asyncio.timeout(10) as cm:
+            await asyncio.sleep(0.01)
+        t1 = loop.time()
+
+        self.assertFalse(cm.expired())
+        # finised fast. Very busy CI box requires high enough limit,
+        # that's why 0.01 cannot be used
+        self.assertLess(t1-t0, 2)
+
+    async def test_timeout_disabled(self):
+        loop = asyncio.get_running_loop()
+        t0 = loop.time()
+        async with asyncio.timeout(None) as cm:
+            await asyncio.sleep(0.01)
+        t1 = loop.time()
+
+        self.assertFalse(cm.expired())
+        self.assertIsNone(cm.when())
+        # finised fast. Very busy CI box requires high enough limit,
+        # that's why 0.01 cannot be used
+        self.assertLess(t1-t0, 2)
+
+    async def test_timeout_at_disabled(self):
+        loop = asyncio.get_running_loop()
+        t0 = loop.time()
+        async with asyncio.timeout(None) as cm:
+            await asyncio.sleep(0.01)
+        t1 = loop.time()
+
+        self.assertFalse(cm.expired())
+        self.assertIsNone(cm.when())
+        # finised fast. Very busy CI box requires high enough limit,
+        # that's why 0.01 cannot be used
+        self.assertLess(t1-t0, 2)
+
+    async def test_timeout_zero(self):
+        loop = asyncio.get_running_loop()
+        t0 = loop.time()
+        with self.assertRaises(TimeoutError):
+            async with asyncio.timeout(0) as cm:
+                await asyncio.sleep(10)
+        t1 = loop.time()
+        self.assertTrue(cm.expired())
+        # finised fast. Very busy CI box requires high enough limit,
+        # that's why 0.01 cannot be used
+        self.assertLess(t1-t0, 2)
+
+    async def test_foreign_exception_passed(self):
+        with self.assertRaises(KeyError):
+            async with asyncio.timeout(0.01) as cm:
+                raise KeyError
+        self.assertFalse(cm.expired())
+
+    async def test_foreign_cancel_doesnt_timeout_if_not_expired(self):
+        with self.assertRaises(asyncio.CancelledError):
+            async with asyncio.timeout(10) as cm:
+                raise asyncio.CancelledError
+        self.assertFalse(cm.expired())
+
+    async def test_outer_task_is_not_cancelled(self):
+
+        has_timeout = False
+
+        async def outer() -> None:
+            nonlocal has_timeout
+            try:
+                async with asyncio.timeout(0.001):
+                    await asyncio.sleep(1)
+            except asyncio.TimeoutError:
+                has_timeout = True
+
+        task = asyncio.create_task(outer())
+        await task
+        assert has_timeout
+        assert not task.cancelled()
+        assert task.done()
+
+    async def test_nested_timeouts_concurrent(self):
+        with self.assertRaises(TimeoutError):
+            async with asyncio.timeout(0.002):
+                try:
+                    async with asyncio.timeout(0.003):
+                        # Pretend we crunch some numbers.
+                        time.sleep(0.005)
+                        await asyncio.sleep(1)
+                except asyncio.TimeoutError:
+                    pass
+
+    async def test_nested_timeouts_loop_busy(self):
+        """
+        After the inner timeout is an expensive operation which should
+        be stopped by the outer timeout.
+
+        Note: this fails for now.
+        """
+        start = time.perf_counter()
+        try:
+            async with asyncio.timeout(0.002):
+                try:
+                    async with asyncio.timeout(0.001):
+                        # Pretend the loop is busy for a while.
+                        time.sleep(0.010)
+                        await asyncio.sleep(0.001)
+                except asyncio.TimeoutError:
+                    # This sleep should be interrupted.
+                    await asyncio.sleep(10)
+        except asyncio.TimeoutError:
+            pass
+        took = time.perf_counter() - start
+        self.assertTrue(took <= 1)
+
+
+@unittest.skipUnless(hasattr(tasks, '_CTask'),
+                     'requires the C _asyncio module')
+class Timeout_CTask_Tests(BaseTimeoutTests, unittest.IsolatedAsyncioTestCase):
+    Task = getattr(tasks, '_CTask', None)
+
+
+class Timeout_PyTask_Tests(BaseTimeoutTests, unittest.IsolatedAsyncioTestCase):
+    Task = tasks._PyTask
+
+
+if __name__ == '__main__':
+    unittest.main()
