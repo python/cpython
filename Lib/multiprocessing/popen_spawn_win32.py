941e4c074aae3a3e89476e987c46d2222690fca0
Géry Ogam <gery.ogam@gmail.com>
2022-03-05 23:36:37 +0100

    Update popen_spawn_win32.py

--- Lib/multiprocessing/popen_spawn_win32.py
+++ Lib/multiprocessing/popen_spawn_win32.py
@@ -84,7 +84,7 @@ class Popen(object):
             self._handle = hp
             self.sentinel = int(hp)
             self.finalizer = util.Finalize(self, _close_handles,
-                                           (self.sentinel, int(rhandle)))
+                                           (self.sentinel, int(rhandle)))._key
 
             # send information to child
             set_spawning_popen(self)
@@ -128,4 +128,4 @@ class Popen(object):
     kill = terminate
 
     def close(self):
-        self.finalizer()
+        util._finalizer_registry[self.finalizer()]()
