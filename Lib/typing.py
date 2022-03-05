11b5a057a58e97b84bcd23697e542f32d7cecde4
Matt Bogosian <eb3f73+github+com@yaymail.com>
2022-03-04 20:51:55 -0600

    Lib/typing.py copy edits originating from GH-31061 (GH-31684) (cherry picked from commit 2031149b9a7dfab5f5bad63f417e19f4fc2b9661)
    Co-authored-by: Matt Bogosian <eb3f73+github+com@yaymail.com>

--- Lib/typing.py
+++ Lib/typing.py
@@ -285,8 +285,8 @@ def _tp_cache(func=None, /, *, typed=False):
 def _eval_type(t, globalns, localns, recursive_guard=frozenset()):
     """Evaluate all forward references in the given type t.
     For use of globalns and localns see the docstring for get_type_hints().
-    recursive_guard is used to prevent prevent infinite recursion
-    with recursive ForwardRef.
+    recursive_guard is used to prevent infinite recursion with a recursive
+    ForwardRef.
     """
     if isinstance(t, ForwardRef):
         return t._evaluate(globalns, localns, recursive_guard)
@@ -705,7 +705,7 @@ class _BaseGenericAlias(_Final, _root=True):
 
     def __getattr__(self, attr):
         # We are careful for copy and pickle.
-        # Also for simplicity we just don't relay all dunder names
+        # Also for simplicity we don't relay any dunder names
         if '__origin__' in self.__dict__ and not _is_dunder(attr):
             return getattr(self.__origin__, attr)
         raise AttributeError(attr)
