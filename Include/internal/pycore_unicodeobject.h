c41d5025341458f056603c398528fb77f045b262
Kumar Aditya <59607654+kumaraditya303@users.noreply.github.com>
2022-03-10 04:32:00 +0530

    bpo-46881: Statically allocate and initialize the latin1 characters. (GH-31616)

--- Include/internal/pycore_unicodeobject.h
+++ Include/internal/pycore_unicodeobject.h
@@ -44,9 +44,6 @@ struct _Py_unicode_ids {
 };
 
 struct _Py_unicode_state {
-    /* Single character Unicode strings in the Latin-1 range are being
-       shared as well. */
-    PyObject *latin1[256];
     struct _Py_unicode_fs_codec fs_codec;
 
     // Unicode identifiers (_Py_Identifier): see _PyUnicode_FromId()
