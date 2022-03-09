4b9631937b98f18d2fcdcacd45902e5b808b5797
Crowthebird <78076854+thatbirdguythatuknownot@users.noreply.github.com>
2022-03-09 08:09:25 +0800

    bpo-43574 + bpo-39829: Optimize list allocations

--- Objects/listobject.c
+++ Objects/listobject.c
@@ -68,15 +68,17 @@ list_resize(PyListObject *self, Py_ssize_t newsize)
      *       is PY_SSIZE_T_MAX * (9 / 8) + 6 which always fits in a size_t.
      */
     new_allocated = ((size_t)newsize + (newsize >> 3) + 6) & ~(size_t)3;
-    /* Do not overallocate if the new size is closer to overallocated size
-     * than to the old size.
-     */
-    if (newsize - Py_SIZE(self) > (Py_ssize_t)(new_allocated - newsize))
-        new_allocated = ((size_t)newsize + 3) & ~(size_t)3;
 
     /* Don't overallocate for lists that start empty or are set to empty. */
     if (newsize == 0 || Py_SIZE(self) == 0)
         new_allocated = newsize;
+    else if (newsize - Py_SIZE(self) > (Py_ssize_t)(new_allocated - newsize)) {
+        /* Do not overallocate if the new size is closer to overallocated size
+        * than to the old size.
+        */
+        new_allocated = ((size_t)newsize + 3) & ~(size_t)3;
+    }
+
     num_allocated_bytes = new_allocated * sizeof(PyObject *);
     items = (PyObject **)PyMem_Realloc(self->ob_item, num_allocated_bytes);
     if (items == NULL) {
