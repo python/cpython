e65d766b6c8d5e3f0295faea4871c7b95986a429
Andrew Svetlov <andrew.svetlov@gmail.com>
2022-03-10 05:29:09 +0200

    Don't duplicate py/c tests, timeout has no C accelerators

--- Lib/test/test_asyncio/test_timeouts.py
+++ Lib/test/test_asyncio/test_timeouts.py
@@ -11,22 +11,7 @@ def tearDownModule():
     asyncio.set_event_loop_policy(None)
 
 
-class BaseTimeoutTests:
-    Task = None
-
-    def new_task(self, loop, coro, name='TestTask'):
-        return self.__class__.Task(coro, loop=loop, name=name)
-
-    def _setupAsyncioLoop(self):
-        assert self._asyncioTestLoop is None, 'asyncio test loop already initialized'
-        loop = asyncio.new_event_loop()
-        asyncio.set_event_loop(loop)
-        loop.set_debug(True)
-        self._asyncioTestLoop = loop
-        loop.set_task_factory(self.new_task)
-        fut = loop.create_future()
-        self._asyncioCallsTask = loop.create_task(self._asyncioLoopRunner(fut))
-        loop.run_until_complete(fut)
+class TimeoutTests(unittest.IsolatedAsyncioTestCase):
 
     async def test_timeout_basic(self):
         with self.assertRaises(TimeoutError):
@@ -238,16 +223,6 @@ class BaseTimeoutTests:
                     with self.assertRaises(TimeoutError):
                         async with asyncio.timeout(0.01):
                             await asyncio.sleep(10)
-
-
-@unittest.skipUnless(hasattr(tasks, '_CTask'),
-                     'requires the C _asyncio module')
-class Timeout_CTask_Tests(BaseTimeoutTests, unittest.IsolatedAsyncioTestCase):
-    Task = getattr(tasks, '_CTask', None)
-
-
-class Timeout_PyTask_Tests(BaseTimeoutTests, unittest.IsolatedAsyncioTestCase):
-    Task = tasks._PyTask
 
 
 if __name__ == '__main__':
