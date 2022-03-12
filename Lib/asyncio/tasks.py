067c8e53b674ed905d9796d2eaa260bf18937b01
Andrew Svetlov <andrew.svetlov@gmail.com>
2022-03-12 19:41:02 +0200

    Update Lib/asyncio/tasks.py

--- Lib/asyncio/tasks.py
+++ Lib/asyncio/tasks.py
@@ -70,7 +70,7 @@ def _set_task_name(task, name):
             warnings.warn("Task.set_name() was added in Python 3.8, "
                       "the method support will be mandatory for third-party "
                       "task implementations since 3.13.",
-                      DeprecationWarning, stacklevel=2)
+                      DeprecationWarning, stacklevel=3)
         else:
             set_name(name)
 
