cebdc325580b49f4c7eb3c61a24c9e7f41ca736b
Miss Islington (bot) <31488909+miss-islington@users.noreply.github.com>
2022-03-14 16:51:55 -0700

    bpo-43721: Fix docstrings for property.getter/setter/deleter (GH-31046)
    (cherry picked from commit e3d348a5252549708fd19338b675a2c23b60d677)
    
    Co-authored-by: Irit Katriel <1055913+iritkatriel@users.noreply.github.com>

--- Objects/descrobject.c
+++ Objects/descrobject.c
@@ -1510,7 +1510,7 @@ static PyMemberDef property_members[] = {
 
 
 PyDoc_STRVAR(getter_doc,
-             "Descriptor to change the getter on a property.");
+             "Descriptor to obtain a copy of the property with a different getter.");
 
 static PyObject *
 property_getter(PyObject *self, PyObject *getter)
@@ -1520,7 +1520,7 @@ property_getter(PyObject *self, PyObject *getter)
 
 
 PyDoc_STRVAR(setter_doc,
-             "Descriptor to change the setter on a property.");
+             "Descriptor to obtain a copy of the property with a different setter.");
 
 static PyObject *
 property_setter(PyObject *self, PyObject *setter)
@@ -1530,7 +1530,7 @@ property_setter(PyObject *self, PyObject *setter)
 
 
 PyDoc_STRVAR(deleter_doc,
-             "Descriptor to change the deleter on a property.");
+             "Descriptor to obtain a copy of the property with a different deleter.");
 
 static PyObject *
 property_deleter(PyObject *self, PyObject *deleter)
