9087243e2c167e38570e819b228efc3492c38c9c
Victor Stinner <vstinner@python.org>
2022-03-21 02:24:00 +0100

    bpo-46850: Remove _PyEval_GetCoroutineOriginTrackingDepth() (GH-32018)
    Remove the private undocumented function
    _PyEval_GetCoroutineOriginTrackingDepth() from the C API. Call the
    public sys.get_coroutine_origin_tracking_depth() function instead.
    
    Change the internal function
    _PyEval_SetCoroutineOriginTrackingDepth():
    
    * Remove the 'tstate' parameter;
    * Add return value and raises an exception if depth is negative;
    * No longer export the function: call the public
      sys.set_coroutine_origin_tracking_depth() function instead.
    
    Uniformize also function declarations in pycore_ceval.h.

--- Include/cpython/ceval.h
+++ Include/cpython/ceval.h
@@ -8,7 +8,6 @@ PyAPI_FUNC(void) PyEval_SetProfile(Py_tracefunc, PyObject *);
 PyAPI_DATA(int) _PyEval_SetProfile(PyThreadState *tstate, Py_tracefunc func, PyObject *arg);
 PyAPI_FUNC(void) PyEval_SetTrace(Py_tracefunc, PyObject *);
 PyAPI_FUNC(int) _PyEval_SetTrace(PyThreadState *tstate, Py_tracefunc func, PyObject *arg);
-PyAPI_FUNC(int) _PyEval_GetCoroutineOriginTrackingDepth(void);
 
 /* Helper to look up a builtin object */
 PyAPI_FUNC(PyObject *) _PyEval_GetBuiltin(PyObject *);
