0985d95f0fdc721c7f9b70a20880f9b9c71d447b
Christian Heimes <christian@python.org>
2022-03-22 17:40:43 +0200

    bpo-45150: Fix testing under FIPS mode (GH-32046)

--- Lib/test/test_hashlib.py
+++ Lib/test/test_hashlib.py
@@ -379,6 +379,11 @@ class HashLibTestCase(unittest.TestCase):
 
     def check_file_digest(self, name, data, hexdigest):
         hexdigest = hexdigest.lower()
+        try:
+            hashlib.new(name)
+        except ValueError:
+            # skip, algorithm is blocked by security policy.
+            return
         digests = [name]
         digests.extend(self.constructors_to_test[name])
 
