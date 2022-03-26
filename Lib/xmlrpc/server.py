4fa505b5d37afdbc9e76decd6a964e81afdec81b
Hugo van Kemenade <hugovk@users.noreply.github.com>
2022-03-26 12:23:30 +0200

    bpo-47126: Update to canonical PEP URLs

--- Lib/xmlrpc/server.py
+++ Lib/xmlrpc/server.py
@@ -734,7 +734,7 @@ class ServerHTMLDoc(pydoc.HTMLDoc):
                 url = 'http://www.rfc-editor.org/rfc/rfc%d.txt' % int(rfc)
                 results.append('<a href="%s">%s</a>' % (url, escape(all)))
             elif pep:
-                url = 'https://www.python.org/dev/peps/pep-%04d/' % int(pep)
+                url = 'https://peps.python.org/pep-%04d/' % int(pep)
                 results.append('<a href="%s">%s</a>' % (url, escape(all)))
             elif text[end:end+1] == '(':
                 results.append(self.namelink(name, methods, funcs, classes))
