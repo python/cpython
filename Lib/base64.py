d2fcc74473f89b3bbc94b2c93e63be07afd8cab8
Nick Drozd <nicholasdrozd@gmail.com>
2022-03-28 17:14:25 -0500

    One more walrus

--- Lib/base64.py
+++ Lib/base64.py
@@ -509,10 +509,7 @@ MAXBINSIZE = (MAXLINESIZE//4)*3
 def encode(input, output):
     """Encode a file; input and output are binary files."""
     while s := input.read(MAXBINSIZE):
-        while len(s) < MAXBINSIZE:
-            ns = input.read(MAXBINSIZE-len(s))
-            if not ns:
-                break
+        while len(s) < MAXBINSIZE and ns := input.read(MAXBINSIZE-len(s)):
             s += ns
         line = binascii.b2a_base64(s)
         output.write(line)
