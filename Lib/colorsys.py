7fd60e4e3a6dbd637c0b0c324c1d07f3c21011fe
ravi140222 <ts-ravi.jain@rakuten.com>
2022-03-31 12:09:10 +0530

    reduced number of lines of code

--- Lib/colorsys.py
+++ Lib/colorsys.py
@@ -151,16 +151,7 @@ def hsv_to_rgb(h, s, v):
     q = v*(1.0 - s*f)
     t = v*(1.0 - s*(1.0-f))
     i = i%6
-    if i == 0:
-        return v, t, p
-    if i == 1:
-        return q, v, p
-    if i == 2:
-        return p, v, t
-    if i == 3:
-        return p, q, v
-    if i == 4:
-        return t, p, v
-    if i == 5:
-        return v, p, q
+
+    l = [(v, t, p), (q, v, p), (p, v, t), (p, q, v), (t, p, v), (v, p, q)]
+    return l[i]
     # Cannot get here
