9fe2e98cd5ff423624a18d71983dd81c0f965ed7
Sam Ezeh <sam.z.ezeh@gmail.com>
2022-04-03 21:30:44 +0100

    Change variable name

--- Lib/test/test_zipfile.py
+++ Lib/test/test_zipfile.py
@@ -1729,13 +1729,13 @@ class OtherTests(unittest.TestCase):
 
     def test_negative_central_directory_offset_raises_BadZipFile(self):
         # Zip file containing an empty EOCD record
-        b = [80, 75, 5, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        buffer = [80, 75, 5, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
         # Set the size of the central directory bytes to become 1,
         # causing the central directory offset to become negative
-        b[12] = 1
+        buffer[12] = 1
 
-        f = io.BytesIO(bytes(b))
+        f = io.BytesIO(bytes(buffer))
         self.assertRaises(zipfile.BadZipFile, zipfile.ZipFile, f)
 
     def test_closed_zip_raises_ValueError(self):
