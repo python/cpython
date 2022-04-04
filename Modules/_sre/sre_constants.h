146bb42b5f7257d81a2cb88c577cf19810406c76
Serhiy Storchaka <storchaka@gmail.com>
2022-04-04 10:53:26 +0300

    bpo-47152: Move sources of the _sre module into a subdirectory (GH-32290)

--- Modules/_sre/sre_constants.h
+++ Modules/_sre/sre_constants.h
@@ -8,7 +8,7 @@
  *
  * Copyright (c) 1997-2001 by Secret Labs AB.  All rights reserved.
  *
- * See the _sre.c file for information on usage and redistribution.
+ * See the sre.c file for information on usage and redistribution.
  */
 
 #define SRE_MAGIC 20220402
