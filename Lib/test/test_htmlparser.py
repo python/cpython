ff38a95e48cad22013539d3f996b2a3b7f063877
Ezio Melotti <ezio.melotti@gmail.com>
2022-04-06 10:40:51 +0200

    Add comments pointing to the bpo issue and remove whitespace.

--- Lib/test/test_htmlparser.py
+++ Lib/test/test_htmlparser.py
@@ -788,29 +788,25 @@ class AttributesTestCase(TestCaseBase):
                                 [('action', 'bogus|&#()value')])])
 
     def test_invalid_keyword_error_exception(self):
+        # bpo-34480: check that subclasses that define an
+        # error method that raises an exception work
         class InvalidMarkupException(Exception):
             pass
-
         class MyHTMLParser(html.parser.HTMLParser):
-
             def error(self, message):
                 raise InvalidMarkupException(message)
-
         parser = MyHTMLParser()
         with self.assertRaises(InvalidMarkupException):
             parser.feed('<![invalid>')
 
     def test_invalid_keyword_error_pass(self):
+        # bpo-34480: check that subclasses that define an
+        # error method that doesn't raise an exception work
         class MyHTMLParser(html.parser.HTMLParser):
-
             def error(self, message):
                 pass
-
         parser = MyHTMLParser()
-        self.assertEqual(
-            parser.feed('<![invalid>'),
-            None
-        )
+        self.assertEqual(parser.feed('<![invalid>'), None)
 
 
 if __name__ == "__main__":
