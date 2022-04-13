caf04690018e6f6b6a20fa51b45c283da9837d49
Nikita Sobolev <mail@sobolevn.me>
2022-04-13 10:41:06 +0300

    Update Lib/lib2to3/tests/__init__.py
    Co-authored-by: Jelle Zijlstra <jelle.zijlstra@gmail.com>

--- Lib/lib2to3/tests/__init__.py
+++ Lib/lib2to3/tests/__init__.py
@@ -7,5 +7,5 @@ from test.support import load_package_tests
 
 def load_tests(*args):
     with warnings.catch_warnings():
-        warnings.filterwarnings('ignore', category=DeprecationWarning, module='lib2to3')
+        warnings.filterwarnings('ignore', category=DeprecationWarning, message='lib2to3')
         return load_package_tests(os.path.dirname(__file__), *args)
