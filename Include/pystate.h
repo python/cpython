7dba38c16358defb7efbe8d5cbcfe6a3f636efdf
Petr Viktorin <encukou@gmail.com>
2022-04-21 18:34:47 +0200

    Exclude semistable/ from the limited API
    d'oh!

--- Include/pystate.h
+++ Include/pystate.h
@@ -120,11 +120,11 @@ PyAPI_FUNC(void) PyGILState_Release(PyGILState_STATE);
 PyAPI_FUNC(PyThreadState *) PyGILState_GetThisThreadState(void);
 
 
-#define Py_SEMISTABLE_PYSTATE_H
-#include "semistable/pystate.h"
-#undef Py_SEMISTABLE_PYSTATE_H
-
 #ifndef Py_LIMITED_API
+#  define Py_SEMISTABLE_PYSTATE_H
+#  include "semistable/pystate.h"
+#  undef Py_SEMISTABLE_PYSTATE_H
+
 #  define Py_CPYTHON_PYSTATE_H
 #  include "cpython/pystate.h"
 #  undef Py_CPYTHON_PYSTATE_H
