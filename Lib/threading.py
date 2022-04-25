90183f8a234c93626d819804d5774540e16c3402
Barney Stratford <barney_stratford@fastmail.fm>
2022-04-25 17:24:01 +0100

    Forgot to change threading.__all__ again.

--- Lib/threading.py
+++ Lib/threading.py
@@ -24,7 +24,7 @@ except ImportError:
 # Java inspired names.
 
 __all__ = ['get_ident', 'active_count', 'Condition', 'current_thread',
-           'enumerate', 'main_thread', 'TIMEOUT_MAX', 'thread',
+           'enumerate', 'main_thread', 'TIMEOUT_MAX', 'threaded',
            'Event', 'Lock', 'RLock', 'Semaphore', 'BoundedSemaphore', 'Thread',
            'Barrier', 'BrokenBarrierError', 'Timer', 'ThreadError',
            'setprofile', 'settrace', 'local', 'stack_size',
