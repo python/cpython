9950aaa248e9189677727e9d3ff6e1928af880d9
Géry Ogam <gery.ogam@gmail.com>
2022-04-26 09:41:55 +0200

    Close self._reader from the queue thread instead of the main thread

--- Lib/multiprocessing/queues.py
+++ Lib/multiprocessing/queues.py
@@ -139,13 +139,10 @@ class Queue(object):
 
     def close(self):
         self._closed = True
-        try:
-            self._reader.close()
-        finally:
-            close = self._close
-            if close:
-                self._close = None
-                close()
+        close = self._close
+        if close:
+            self._close = None
+            close()
 
     def join_thread(self):
         debug('Queue.join_thread()')
@@ -165,16 +162,13 @@ class Queue(object):
         debug('Queue._start_thread()')
 
         # Start thread which transfers data from buffer to pipe
-        # Pass a reference to the reader end of the pipe to the thread
-        # to prevent the garbage collector from closing it before the
-        # thread has sent all buffered data to the writer end of the
-        # pipe, thereby avoiding a BrokenPipeError
         self._buffer.clear()
         self._thread = threading.Thread(
             target=Queue._feed,
             args=(self._buffer, self._notempty, self._send_bytes,
-                  self._wlock, self._writer.close, self._ignore_epipe,
-                  self._on_queue_feeder_error, self._sem, self._reader),
+                  self._wlock, self._reader.close, self._writer.close,
+                  self._ignore_epipe, self._on_queue_feeder_error,
+                  self._sem),
             name='QueueFeederThread'
         )
         self._thread.daemon = True
@@ -215,8 +209,8 @@ class Queue(object):
             notempty.notify()
 
     @staticmethod
-    def _feed(buffer, notempty, send_bytes, writelock, close, ignore_epipe,
-              onerror, queue_sem, reader):
+    def _feed(buffer, notempty, send_bytes, writelock, reader_close,
+              writer_close, ignore_epipe, onerror, queue_sem, reader):
         debug('starting thread to feed data to pipe')
         nacquire = notempty.acquire
         nrelease = notempty.release
@@ -242,7 +236,8 @@ class Queue(object):
                         obj = bpopleft()
                         if obj is sentinel:
                             debug('feeder thread got sentinel -- exiting')
-                            close()
+                            reader_close()
+                            writer_close()
                             return
 
                         # serialize the data before acquiring the lock
