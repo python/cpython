020f5c47d8137802d0f058ab2ed2dcb939a2eb8f
Miss Islington (bot) <31488909+miss-islington@users.noreply.github.com>
2022-05-03 05:34:50 -0700

    bpo-46415: Use f-string for ValueError in ipaddress.ip_{address,network,interface} helper functions (GH-30642)
    `IPv*Network` and `IPv*Interface` constructors accept a 2-tuple of
    (address description, netmask) as the address parameter.
    When the tuple-based address is used errors are not propagated
    correctly through the `ipaddress.ip_*` helper because of the %-formatting now expecting several arguments:
    
    	In [7]: ipaddress.ip_network(("192.168.100.0", "fooo"))
            ...
    	TypeError: not all arguments converted during string formatting
    
    Compared to:
    
    	In [8]: ipaddress.IPv4Network(("192.168.100.0", "foo"))
            ...
    	NetmaskValueError: 'foo' is not a valid netmask
    
    Use an f-string to make sure the error is always properly formatted.
    
    Co-authored-by: Jelle Zijlstra <jelle.zijlstra@gmail.com>
    (cherry picked from commit 52dc9c3066bcdc67a7a45d41cf158ecb1434d5f3)
    
    Co-authored-by: Thomas Cellerier <thomascellerier@gmail.com>

--- Lib/ipaddress.py
+++ Lib/ipaddress.py
@@ -50,8 +50,7 @@ def ip_address(address):
     except (AddressValueError, NetmaskValueError):
         pass
 
-    raise ValueError('%r does not appear to be an IPv4 or IPv6 address' %
-                     address)
+    raise ValueError(f'{address!r} does not appear to be an IPv4 or IPv6 address')
 
 
 def ip_network(address, strict=True):
@@ -80,8 +79,7 @@ def ip_network(address, strict=True):
     except (AddressValueError, NetmaskValueError):
         pass
 
-    raise ValueError('%r does not appear to be an IPv4 or IPv6 network' %
-                     address)
+    raise ValueError(f'{address!r} does not appear to be an IPv4 or IPv6 network')
 
 
 def ip_interface(address):
@@ -115,8 +113,7 @@ def ip_interface(address):
     except (AddressValueError, NetmaskValueError):
         pass
 
-    raise ValueError('%r does not appear to be an IPv4 or IPv6 interface' %
-                     address)
+    raise ValueError(f'{address!r} does not appear to be an IPv4 or IPv6 interface')
 
 
 def v4_int_to_packed(address):
@@ -159,7 +156,7 @@ def _split_optional_netmask(address):
     """Helper to split the netmask and raise AddressValueError if needed"""
     addr = str(address).split('/')
     if len(addr) > 2:
-        raise AddressValueError("Only one '/' permitted in %r" % address)
+        raise AddressValueError(f"Only one '/' permitted in {address!r}")
     return addr
 
 
@@ -1303,7 +1300,7 @@ class IPv4Address(_BaseV4, _BaseAddress):
         # which converts into a formatted IP string.
         addr_str = str(address)
         if '/' in addr_str:
-            raise AddressValueError("Unexpected '/' in %r" % address)
+            raise AddressValueError(f"Unexpected '/' in {address!r}")
         self._ip = self._ip_int_from_string(addr_str)
 
     @property
@@ -1912,7 +1909,7 @@ class IPv6Address(_BaseV6, _BaseAddress):
         # which converts into a formatted IP string.
         addr_str = str(address)
         if '/' in addr_str:
-            raise AddressValueError("Unexpected '/' in %r" % address)
+            raise AddressValueError(f"Unexpected '/' in {address!r}")
         addr_str, self._scope_id = self._split_scope_id(addr_str)
 
         self._ip = self._ip_int_from_string(addr_str)
