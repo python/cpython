07f76fc4a63d7f288ba0c4d6b2515df23f8c0075
Ezio Melotti <ezio.melotti@gmail.com>
2022-05-04 17:03:25 +0200

    No reason to ignore the README.rst.

--- Doc/make.bat
+++ Doc/make.bat
@@ -181,7 +181,7 @@ goto end
 
 :check
 rem --severity=0 enables all checks, but we don't want to check for long lines
-cmd /S /C "%SPHINXLINT% -i tools -i README.rst --severity=0 --disable='line too long'"
+cmd /S /C "%SPHINXLINT% -i tools --severity=0 --disable='line too long'"
 cmd /S /C "%SPHINXLINT% --severity=0 --disable='line too long' ..\Misc\NEWS.d\next\ "
 goto end
 
