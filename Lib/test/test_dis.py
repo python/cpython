91d280c05caf9dbe3114745ef9314afed6643aa4
sweeneyde <sweeney.427@osu.edu>
2022-05-09 21:52:20 -0400

    Fix test_dis

--- Lib/test/test_dis.py
+++ Lib/test/test_dis.py
@@ -1040,7 +1040,7 @@ class DisTests(DisTestBase):
                     for cache in caches:
                         self.assertRegex(cache, pattern)
                     self.assertEqual(caches.count(""), 8)
-                    self.assertEqual(len(caches), 25)
+                    self.assertEqual(len(caches), 26)
 
 
 class DisWithFileTests(DisTests):
