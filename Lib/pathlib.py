a51baec9ce0eae2b4db069a55daf8f03be3ab2f4
Miss Islington (bot) <31488909+miss-islington@users.noreply.github.com>
2022-05-09 17:39:43 -0700

    gh-92550 - Fix regression in `pathlib.Path.rglob()` (GH-92583)
    We could try to remedy this by taking a slice, but we then run into an issue where the empty string will match altsep on POSIX. That rabbit hole could keep getting deeper.
    
    A proper fix for the original issue involves making pathlib's path normalisation more configurable - in this case we want to retain trailing slashes, but in other we might want to preserve `./` prefixes, or elide `../` segments when we're sure we won't encounter symlinks.
    
    This reverts commit ea2f5bcda1a392804487e6883be89fbad38a01a5.
    (cherry picked from commit dcdf250d2de1428f7d8b4e9ecf51d2fd8200e21a)
    
    Co-authored-by: Barney Gale <barney.gale@gmail.com>

--- Lib/pathlib.py
+++ Lib/pathlib.py
@@ -281,8 +281,6 @@ _posix_flavour = _PosixFlavour()
 def _make_selector(pattern_parts, flavour):
     pat = pattern_parts[0]
     child_parts = pattern_parts[1:]
-    if not pat:
-        return _TerminatingSelector()
     if pat == '**':
         cls = _RecursiveWildcardSelector
     elif '**' in pat:
@@ -945,8 +943,6 @@ class Path(PurePath):
         drv, root, pattern_parts = self._flavour.parse_parts((pattern,))
         if drv or root:
             raise NotImplementedError("Non-relative patterns are unsupported")
-        if pattern[-1] in (self._flavour.sep, self._flavour.altsep):
-            pattern_parts.append('')
         selector = _make_selector(tuple(pattern_parts), self._flavour)
         for p in selector.select_from(self):
             yield p
@@ -960,8 +956,6 @@ class Path(PurePath):
         drv, root, pattern_parts = self._flavour.parse_parts((pattern,))
         if drv or root:
             raise NotImplementedError("Non-relative patterns are unsupported")
-        if pattern[-1] in (self._flavour.sep, self._flavour.altsep):
-            pattern_parts.append('')
         selector = _make_selector(("**",) + tuple(pattern_parts), self._flavour)
         for p in selector.select_from(self):
             yield p
