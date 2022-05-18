f96150587daa573c819101aa361b6f7a677bb890
Pieter Eendebak <pieter.eendebak@gmail.com>
2022-05-18 10:52:24 +0200

    whitespace

--- Lib/urllib/request.py
+++ Lib/urllib/request.py
@@ -2492,6 +2492,7 @@ class ftpwrapper:
 # Proxy handling
 def getproxies_environment():
     """Return a dictionary of scheme -> proxy server URL mappings.
+
     Scan the environment for variables named <scheme>_proxy;
     this seems to be the standard convention.  If you need a
     different way, you can pass a proxies dictionary to the
