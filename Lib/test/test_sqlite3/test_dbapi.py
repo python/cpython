b9be4a2f55d9ea458b960cd44eb0a00ef41ef212
Serhiy Storchaka <storchaka@gmail.com>
2022-05-18 19:39:06 +0300

    Test with escaped URIs

--- Lib/test/test_sqlite3/test_dbapi.py
+++ Lib/test/test_sqlite3/test_dbapi.py
@@ -32,6 +32,7 @@ from test.support import SHORT_TIMEOUT, bigmemtest, check_disallow_instantiation
 from test.support import threading_helper
 from _testcapi import INT_MAX, ULLONG_MAX
 from os import SEEK_SET, SEEK_CUR, SEEK_END
+from urllib.request import pathname2url
 from test.support.os_helper import TESTFN, TESTFN_UNDECODABLE, unlink, temp_dir, FakePath
 
 
@@ -665,23 +666,33 @@ class OpenTests(unittest.TestCase):
             cx.execute(self._sql)
 
     def test_open_uri(self):
-        with managed_connect(f"file:{TESTFN}", uri=True) as cx:
+        uri = "file:" + pathname2url(os.fsencode(TESTFN))
+        with managed_connect(uri, uri=True) as cx:
             self.assertTrue(os.path.exists(TESTFN))
             cx.execute(self._sql)
 
     def test_open_uri_readonly(self):
+        uri = "file:" + pathname2url(os.fsencode(TESTFN)) + "?mode=ro"
         self.addCleanup(unlink, TESTFN)
         # Cannot create new DB
         with self.assertRaises(sqlite.OperationalError):
-            with sqlite.connect(f"file:{TESTFN}?mode=ro", uri=True):
+            with sqlite.connect(uri, uri=True):
                 pass
         self.assertFalse(os.path.exists(TESTFN))
         with sqlite.connect(TESTFN) as cx:
             self.assertTrue(os.path.exists(TESTFN))
         # Cannot modify new DB
-        with sqlite.connect(f"file:{TESTFN}?mode=ro", uri=True) as cx:
+        with sqlite.connect(uri, uri=True) as cx:
             with self.assertRaises(sqlite.OperationalError):
                 cx.execute(self._sql)
+
+    @unittest.skipUnless(TESTFN_UNDECODABLE, "only works if there are undecodable paths")
+    def test_open_undecodable_uri(self):
+        uri = "file:" + pathname2url(TESTFN_UNDECODABLE)
+        self.addCleanup(unlink, TESTFN_UNDECODABLE)
+        with managed_connect(uri, uri=True) as cx:
+            self.assertTrue(os.path.exists(TESTFN_UNDECODABLE))
+            cx.execute(self._sql)
 
     def test_database_keyword(self):
         with sqlite.connect(database=":memory:") as cx:
