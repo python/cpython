8d6eec42933957c93693c242d0db012fe2724e74
Steve Dower <steve.dower@python.org>
2022-05-19 22:41:36 +0100

    Simplify test and fix missing variable

--- Lib/test/test_launcher.py
+++ Lib/test/test_launcher.py
@@ -251,7 +251,7 @@ class RunPyMixin:
         venv_exe = (venv / Path(sys.executable).name)
         venv_exe.touch()
         try:
-            yield {"VIRTUAL_ENV": str(venv.parent)}
+            yield venv_exe, {"VIRTUAL_ENV": str(venv.parent)}
         finally:
             shutil.rmtree(venv)
 
@@ -462,7 +462,7 @@ class TestLauncher(unittest.TestCase, RunPyMixin):
         self.assertEqual("PythonTestSuite/3.100", default)
 
     def test_virtualenv_in_list(self):
-        with self.test_venv() as env:
+        with self.test_venv() as (venv_exe, env):
             data = self.run_py(["-0p"], env=env)
             for line in data["stdout"].splitlines():
                 m = re.match(r"\s*\*\s+(.+)$", line)
@@ -482,21 +482,11 @@ class TestLauncher(unittest.TestCase, RunPyMixin):
                 self.fail("did not find active venv entry")
 
     def test_virtualenv_with_env(self):
-        with self.test_venv() as env:
-            # Establish what a default venv launch looks like
-            data1 = self.run_py([], env={**env})
-            # also an overridden launch
-            data2 = self.run_py(["-3"], env={**env})
-            # The actual test scenario
-            data3 = self.run_py([], env={**env, "PY_PYTHON": "-3"})
-        # Default and overridden launches should not match
-        self.assertNotEqual(data1["LaunchCommand"], data2["LaunchCommand"])
-        # Env-overridden and default launches *should* match
-        self.assertEqual(data1["LaunchCommand"], data3["LaunchCommand"])
-        # Only the PY_PYTHON case should be a low priority tag
-        self.assertEqual(data1["SearchInfo.lowPriorityTag"], "False")
-        self.assertEqual(data2["SearchInfo.lowPriorityTag"], "False")
-        self.assertEqual(data3["SearchInfo.lowPriorityTag"], "True")
+        with self.test_venv() as (venv_exe, env):
+            data = self.run_py([], env={**env, "PY_PYTHON": "-3"})
+        # Compare stdout, because stderr goes via ascii
+        self.assertEqual(data["stdout"].strip(), str(venv_exe))
+        self.assertEqual(data["SearchInfo.lowPriorityTag"], "True")
 
     def test_py_shebang(self):
         with self.py_ini(TEST_PY_COMMANDS):
