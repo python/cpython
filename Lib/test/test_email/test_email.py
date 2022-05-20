d88f7f50de0435601772146ed11cac0d80e64ee8
oda-gitso <105083118+oda-gitso@users.noreply.github.com>
2022-05-21 00:23:23 +0700

    Update test_email.py

--- Lib/test/test_email/test_email.py
+++ Lib/test/test_email/test_email.py
@@ -19,25 +19,25 @@ import email
 import email.policy
 
 from email.charset import Charset
-from email.header import Header, decode_header, make_header
-from email.parser import Parser, HeaderParser
 from email.generator import Generator, DecodedGenerator, BytesGenerator
+from email.header import Header, decode_header, make_header
+from email.headerregistry import HeaderRegistry
 from email.message import Message
 from email.mime.application import MIMEApplication
 from email.mime.audio import MIMEAudio
-from email.mime.text import MIMEText
-from email.mime.image import MIMEImage
 from email.mime.base import MIMEBase
+from email.mime.image import MIMEImage
 from email.mime.message import MIMEMessage
 from email.mime.multipart import MIMEMultipart
 from email.mime.nonmultipart import MIMENonMultipart
-from email.headerregistry import HeaderRegistry
-from email import utils
-from email import errors
-from email import encoders
-from email import iterators
+from email.mime.text import MIMEText
+from email.parser import Parser, HeaderParser
 from email import base64mime
+from email import encoders
+from email import errors
+from email import iterators
 from email import quoprimime
+from email import utils
 
 from test.support import threading_helper
 from test.support.os_helper import unlink
