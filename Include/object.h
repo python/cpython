b4a478cda8d7d0e543011e0c84336d0a7c58aca9
Wenzel Jakob <wenzel.jakob@epfl.ch>
2022-05-25 20:15:43 +0200

    fix Py_LIMITED_API check in object.h

--- Include/object.h
+++ Include/object.h
@@ -256,6 +256,8 @@ PyAPI_FUNC(void *) PyType_GetModuleState(PyTypeObject *);
 #if !defined(Py_LIMITED_API) || Py_LIMITED_API+0 >= 0x030B0000
 PyAPI_FUNC(PyObject *) PyType_GetName(PyTypeObject *);
 PyAPI_FUNC(PyObject *) PyType_GetQualName(PyTypeObject *);
+#endif
+#if !defined(Py_LIMITED_API) || Py_LIMITED_API+0 >= 0x030C0000
 PyAPI_FUNC(PyObject *) PyType_FromMetaclass(PyTypeObject*, PyObject*, PyType_Spec*, PyObject*);
 #endif
 
