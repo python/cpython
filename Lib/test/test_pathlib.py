efebe7ab8951b38ac756d78b9e04bcac78fa4088
Barney Gale <barney.gale@gmail.com>
2022-05-26 23:07:18 +0100

    gh-93156 - fix negative indexing into absolute `pathlib.PurePath().parents`
    When a `_PathParents` object has a drive or a root, the length of the
    object is *one less* than than the length of `self._parts`, which resulted
    in an off-by-one error when `path.parents[-n]` was fed through to
    `self._parts[:-n - 1]`. In particular, `path.parents[-1]` was a malformed
    path object with spooky properties.
    
    This is addressed by adding `len(self)` to negative indices.

--- Lib/test/test_pathlib.py
+++ Lib/test/test_pathlib.py
@@ -465,6 +465,9 @@ class _BasePurePathTest(object):
         self.assertEqual(par[0], P('/a/b'))
         self.assertEqual(par[1], P('/a'))
         self.assertEqual(par[2], P('/'))
+        self.assertEqual(par[-1], P('/'))
+        self.assertEqual(par[-2], P('/a'))
+        self.assertEqual(par[-3], P('/a/b'))
         self.assertEqual(par[0:1], (P('/a/b'),))
         self.assertEqual(par[:2], (P('/a/b'), P('/a')))
         self.assertEqual(par[:-1], (P('/a/b'), P('/a')))
@@ -472,6 +475,8 @@ class _BasePurePathTest(object):
         self.assertEqual(par[::2], (P('/a/b'), P('/')))
         self.assertEqual(par[::-1], (P('/'), P('/a'), P('/a/b')))
         self.assertEqual(list(par), [P('/a/b'), P('/a'), P('/')])
+        with self.assertRaises(IndexError):
+            par[-4]
         with self.assertRaises(IndexError):
             par[3]
 
