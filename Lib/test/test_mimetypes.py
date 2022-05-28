96e127b63d906ff128540d0d1c8390f6077dde58
Oleg Iarygin <oleg@arhadthedev.net>
2022-05-28 21:01:14 +0300

    macOS, maybe Midi is what you don't override?

--- Lib/test/test_mimetypes.py
+++ Lib/test/test_mimetypes.py
@@ -324,16 +324,16 @@ class MimetypesCliTestCase(unittest.TestCase):
         self.assertEqual(err, '')
 
     def test_guess_type(self):
-        retcode, out, err = self.mimetypes_cmd('-l', 'foo.xul')
+        retcode, out, err = self.mimetypes_cmd('-l', 'foo.mid')
         self.assertEqual(retcode, 0)
-        self.assertEqual(out, 'type: text/xul encoding: None')
+        self.assertEqual(out, 'type: audio/midi encoding: None')
         self.assertEqual(err, '')
 
         # Previously, there was .pic format that macOS knew as strict
-        retcode, out, err = self.mimetypes_cmd('foo.xul')
+        retcode, out, err = self.mimetypes_cmd('foo.mid')
         self.assertEqual(retcode, 1)
         self.assertEqual(out, '')
-        self.assertEqual(err, "I don't know anything about type foo.xul")
+        self.assertEqual(err, "I don't know anything about type foo.mid")
 
 if __name__ == "__main__":
     unittest.main()
