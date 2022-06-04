fa80dd62d6ad1c125778dc19d00f7f8c2f86cc88
Kalyan <kalyan.ben10@live.com>
2022-06-04 16:35:23 +0530

    Update Lib/sqlite3/__init__.py
    Co-authored-by: Alex Waygood <Alex.Waygood@Gmail.com>

--- Lib/sqlite3/__init__.py
+++ Lib/sqlite3/__init__.py
@@ -63,7 +63,7 @@ from sqlite3.dbapi2 import (_deprecated_names,
 def __getattr__(name):
     if name in _deprecated_names:
         from warnings import warn
-        warn(f"{name} is deprecated. Will be removed in python 3.14",
+        warn(f"{name} is deprecated and will be removed in Python 3.14",
              DeprecationWarning, stacklevel=2)
         return globals()[f"_deprecated_{name}"]
     raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
