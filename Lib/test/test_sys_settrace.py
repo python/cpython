1ee2f1c8abdde1cb6707dfa8972c6daef84230fe
Mark Shannon <mark@hotpy.org>
2022-06-07 11:55:55 +0100

    Add test for tracing when quickening calls to Python classes.

--- Lib/test/test_sys_settrace.py
+++ Lib/test/test_sys_settrace.py
@@ -1570,6 +1570,28 @@ class TraceTestCase(unittest.TestCase):
 
         self.run_and_compare(func, EXPECTED_EVENTS)
 
+    def test_correct_tracing_quickened_call_class_init(self):
+
+        class C:
+            def __init__(self):
+                self
+
+        def func():
+            C()
+
+        EXPECTED_EVENTS = [
+            (0, 'call'),
+            (1, 'line'),
+            (-3, 'call'),
+            (-2, 'line'),
+            (-2, 'return'),
+            (1, 'return')]
+
+        self.run_and_compare(func, EXPECTED_EVENTS)
+        # Quicken
+        for _ in range(100):
+            func()
+        self.run_and_compare(func, EXPECTED_EVENTS)
 
 EVENT_NAMES = [
     'call',
