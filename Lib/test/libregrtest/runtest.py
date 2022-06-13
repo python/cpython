a3603f0dcc17b95da87b35da7203357b5a5e8f9e
Victor Stinner <vstinner@python.org>
2022-06-13 17:41:11 +0200

    gh-93353: regrtest checks for leaked temporary files
    When running tests with -jN, create a temporary directory per process
    and mark a test as "environment changed" if a test leaks a temporary
    file or directory.

--- Lib/test/libregrtest/runtest.py
+++ Lib/test/libregrtest/runtest.py
@@ -80,6 +80,11 @@ class EnvChanged(Failed):
     def __str__(self) -> str:
         return f"{self.name} failed (env changed)"
 
+    @staticmethod
+    # Convert Passed to EnvChanged
+    def from_passed(other):
+        return EnvChanged(other.name, other.duration_sec, other.xml_data)
+
 
 class RefLeak(Failed):
     def __str__(self) -> str:
