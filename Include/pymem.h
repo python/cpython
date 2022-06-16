f4dc85959bfae8e78877091f11e9f2f75a37a333
Victor Stinner <vstinner@python.org>
2022-06-16 22:37:40 +0200

    Revert invalid changes

--- Include/pymem.h
+++ Include/pymem.h
@@ -83,9 +83,9 @@ PyAPI_FUNC(void) PyMem_Free(void *ptr);
 // PyMem_Del and PyMem_DEL are defined with no parameter to be able to use
 // them as function pointers (ex: dealloc = PyMem_Del).
 #define PyMem_MALLOC(n)           PyMem_Malloc((n))
-#define PyMem_NEW(type, n)        PyMem_New((type), (n))
+#define PyMem_NEW(type, n)        PyMem_New(type, (n))
 #define PyMem_REALLOC(p, n)       PyMem_Realloc((p), (n))
-#define PyMem_RESIZE(p, type, n)  PyMem_Resize((p), (type), (n))
+#define PyMem_RESIZE(p, type, n)  PyMem_Resize((p), type, (n))
 #define PyMem_FREE(p)             PyMem_Free((p))
 #define PyMem_Del(p)              PyMem_Free((p))
 #define PyMem_DEL(p)              PyMem_Free((p))
