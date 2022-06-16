8266ba9c658609a25cbe15dd66bc63b4f58878f1
Brandt Bucher <brandtbucher@microsoft.com>
2022-06-15 17:40:26 -0700

    Feedback from code review

--- Lib/test/test_dis.py
+++ Lib/test/test_dis.py
@@ -1756,20 +1756,24 @@ class InstructionTests(InstructionTestCase):
     @requires_debug_ranges()
     def test_co_positions_with_lots_of_caches(self):
         def roots(a, b, c):
-            d = (b**2 - 4 * a * c) ** 0.5
-            yield (-b - d) / (2 * a)
+            d = b**2 - 4 * a * c
+            yield (-b - cmath.sqrt(d)) / (2 * a)
             if d:
-                yield (-b + d) / (2 * a)
+                yield (-b + cmath.sqrt(d)) / (2 * a)
         code = roots.__code__
+        ops = code.co_code[::2]
+        cache = opcode.opmap["CACHE"]
+        caches = sum(op == cache for op in ops)
+        non_caches = len(ops) - caches
+        # Make sure we have "lots of caches". If not, roots should be updated:
+        assert 1 / 3 <= caches / non_caches, "this test needs more caches!"
         for show_caches in (False, True):
             for adaptive in (False, True):
                 with self.subTest(f"{adaptive=}, {show_caches=}"):
                     co_positions = [
                         positions
-                        for op, positions in zip(
-                            code.co_code[::2], code.co_positions(), strict=True
-                        )
-                        if show_caches or op != opcode.opmap["CACHE"]
+                        for op, positions in zip(ops, code.co_positions(), strict=True)
+                        if show_caches or op != cache
                     ]
                     dis_positions = [
                         instruction.positions
