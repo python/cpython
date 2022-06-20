34a8a1b6ed71a43c1a4670bf3141f4b88dae4f41
Victor Stinner <vstinner@python.org>
2022-06-20 23:19:24 +0200

    * Rewrite __init__.py and __main__.py

--- Lib/test/test_unittest/__init__.py
+++ Lib/test/test_unittest/__init__.py
@@ -1,25 +1,6 @@
-import os
-import sys
-import unittest
+import os.path
+from test.support import load_package_tests
 
 
-here = os.path.dirname(__file__)
-loader = unittest.defaultTestLoader
-
-def suite():
-    suite = unittest.TestSuite()
-    for fn in os.listdir(here):
-        if fn.startswith("test") and fn.endswith(".py"):
-            modname = "test.test_unittest." + fn[:-3]
-            try:
-                __import__(modname)
-            except unittest.SkipTest:
-                continue
-            module = sys.modules[modname]
-            suite.addTest(loader.loadTestsFromModule(module))
-    suite.addTest(loader.loadTestsFromName('test.test_unittest.testmock'))
-    return suite
-
-
-if __name__ == "__main__":
-    unittest.main(defaultTest="suite")
+def load_tests(*args):
+    return load_package_tests(os.path.dirname(__file__), *args)
