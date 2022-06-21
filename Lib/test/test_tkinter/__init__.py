8ee5ffe744d68908b2bba40f62bfeb2c1424d95b
Victor Stinner <vstinner@python.org>
2022-06-21 17:22:46 +0200

    Add Lib/test/test_tkinter/__init__.py
    Remove Lib/test/test_tk.py.

---
+++ Lib/test/test_tkinter/__init__.py
@@ -0,0 +1,17 @@
+import os.path
+from test import support
+from test.support import import_helper
+
+
+if support.check_sanitizer(address=True, memory=True):
+    raise unittest.SkipTest("Tests involvin libX11 can SEGFAULT on ASAN/MSAN builds")
+
+# Skip test if _tkinter wasn't built.
+import_helper.import_module('_tkinter')
+
+# Skip test if tk cannot be initialized.
+support.requires('gui')
+
+
+def load_tests(*args):
+    return support.load_package_tests(os.path.dirname(__file__), *args)
