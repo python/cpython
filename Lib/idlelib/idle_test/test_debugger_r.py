4bbc8466e706da27a3a054942c50d275b9eabfc1
Terry Jan Reedy <tjreedy@udel.edu>
2022-06-22 17:23:39 -0400

    Update test_debugger_r.py

--- Lib/idlelib/idle_test/test_debugger_r.py
+++ Lib/idlelib/idle_test/test_debugger_r.py
@@ -3,15 +3,20 @@
 from idlelib import debugger_r
 import unittest
 
+# Boilerplate likely to be needed for future test classes.
+##from test.support import requires
+##from tkinter import Tk
+##class Test(unittest.TestCase):
+##    @classmethod
+##    def setUpClass(cls):
+##        requires('gui')
+##        cls.root = Tk()
+##    @classmethod
+##    def tearDownClass(cls):
+##        cls.root.destroy()
 
-class Test(unittest.TestCase):
-
-    def test_init(self):
-        self.assertTrue(True)  # Get coverage of import
-
-
-# Classes GUIProxy, IdbAdapter, FrameProxy, CodeProxy, DictProxy,
-# GUIAdapter, IdbProxy plus 7 module functions.
+# GUIProxy, IdbAdapter, FrameProxy, CodeProxy, DictProxy,
+# GUIAdapter, IdbProxy, and 7 functions still need tests.
 
 class IdbAdapterTest(unittest.TestCase):
 
