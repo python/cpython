d9c99200eaeaf82417990f99f47efd8e94e7e880
Victor Stinner <vstinner@python.org>
2022-06-23 00:37:24 +0200

    Complete test_idle/__init__.py
    Copy code from remove Lib/test/test_idle.py

--- Lib/test/test_idle/__init__.py
+++ Lib/test/test_idle/__init__.py
@@ -6,6 +6,25 @@ This package and its contained modules are subject to change and
 any direct use is at your own risk.
 '''
 from os.path import dirname
+from test import support
+from test.support.import_helper import import_module
+
+
+if support.check_sanitizer(address=True, memory=True):
+    raise unittest.SkipTest("Tests involving libX11 can SEGFAULT on ASAN/MSAN builds")
+
+# Skip test_idle if _tkinter wasn't built, if tkinter is missing,
+# if tcl/tk is not the 8.5+ needed for ttk widgets,
+# or if idlelib is missing (not installed).
+tk = import_module('tkinter')  # Also imports _tkinter.
+if tk.TkVersion < 8.5:
+    raise unittest.SkipTest("IDLE requires tk 8.5 or later.")
+idlelib = import_module('idlelib')
+
+# Before importing and executing more of idlelib,
+# tell IDLE to avoid changing the environment.
+idlelib.testing = True
+
 
 def load_tests(loader, standard_tests, pattern):
     this_dir = dirname(__file__)
