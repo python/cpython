df497a9325f85b4e60e451a0b04b49df304e5772
Erlend E. Aasland <erlend.aasland@protonmail.com>
2022-06-26 00:12:35 +0200

    gh-93370: Catch sqlite3.version deprecation warning in pythoninfo.py

--- Lib/test/pythoninfo.py
+++ Lib/test/pythoninfo.py
@@ -603,7 +603,8 @@ def collect_sqlite(info_add):
         return
 
     attributes = ('version', 'sqlite_version')
-    copy_attributes(info_add, sqlite3, 'sqlite3.%s', attributes)
+    with warnings.catch_warnings(record=True):
+        copy_attributes(info_add, sqlite3, 'sqlite3.%s', attributes)
 
 
 def collect_zlib(info_add):
