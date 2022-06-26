b325a0ca456fab296a5b76014541c4b94e4e2f36
Erlend E. Aasland <erlend.aasland@protonmail.com>
2022-06-26 10:55:34 +0200

    Address review: Drop sqlite3.version

--- Lib/test/pythoninfo.py
+++ Lib/test/pythoninfo.py
@@ -602,9 +602,8 @@ def collect_sqlite(info_add):
     except ImportError:
         return
 
-    attributes = ('version', 'sqlite_version')
-    with warnings.catch_warnings(record=True):
-        copy_attributes(info_add, sqlite3, 'sqlite3.%s', attributes)
+    attributes = ('sqlite_version',)
+    copy_attributes(info_add, sqlite3, 'sqlite3.%s', attributes)
 
 
 def collect_zlib(info_add):
