5e88846c32da51a33f11ad53e5b82ca21f6a0a87
Yonatan Perry <yonatan.perry@gmail.com>
2022-06-30 18:57:35 -0400

    gh-94440: Fix issue of ProcessPoolExecutor shutdown hanging
    - documentation enhancement

--- Lib/concurrent/futures/process.py
+++ Lib/concurrent/futures/process.py
@@ -364,9 +364,9 @@ class _ExecutorManagerThread(threading.Thread):
             if self.is_shutting_down():
                 self.flag_executor_shutting_down()
 
-                # If only canceled futures remain in pending_work_items, we
-                # should purge them now to avoid waiting forever in our
-                # subsequent call to wait_result_broken_or_wakeup.
+                # When only canceled futures remain in pending_work_items, our
+                # next call to wait_result_broken_or_wakeup would hang forever.
+                # This makes sure we have some running futures or none at all.
                 self.add_call_item_to_queue()
 
                 # Since no new work items can be added, it is safe to shutdown
