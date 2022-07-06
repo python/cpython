14fea6b4d25658bc00adbb97dd40ea3d3e6843ad
Kumar Aditya <59607654+kumaraditya303@users.noreply.github.com>
2022-07-06 20:48:21 +0530

    GH-93896: AAlways set event loop in asyncio.run and IsolatedAsyncioTestCase (#94593)

--- Lib/asyncio/runners.py
+++ Lib/asyncio/runners.py
@@ -52,6 +52,7 @@ class Runner:
         self._loop = None
         self._context = None
         self._interrupt_count = 0
+        self._set_event_loop = False
 
     def __enter__(self):
         self._lazy_init()
@@ -70,6 +71,8 @@ class Runner:
             loop.run_until_complete(loop.shutdown_asyncgens())
             loop.run_until_complete(loop.shutdown_default_executor())
         finally:
+            if self._set_event_loop:
+                events.set_event_loop(None)
             loop.close()
             self._loop = None
             self._state = _State.CLOSED
@@ -111,6 +114,8 @@ class Runner:
 
         self._interrupt_count = 0
         try:
+            if self._set_event_loop:
+                events.set_event_loop(self._loop)
             return self._loop.run_until_complete(task)
         except exceptions.CancelledError:
             if self._interrupt_count > 0 and task.uncancel() == 0:
@@ -130,6 +135,7 @@ class Runner:
             return
         if self._loop_factory is None:
             self._loop = events.new_event_loop()
+            self._set_event_loop = True
         else:
             self._loop = self._loop_factory()
         if self._debug is not None:
