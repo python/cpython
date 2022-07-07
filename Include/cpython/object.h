7b299d19fda0d9173e7d8899a5226872acf7a86c
Eric Snow <ericsnowcurrently@gmail.com>
2022-07-07 16:48:47 -0600

    Add a note about tp_cache.

--- Include/cpython/object.h
+++ Include/cpython/object.h
@@ -217,7 +217,7 @@ struct _typeobject {
     inquiry tp_is_gc; /* For PyObject_IS_GC */
     PyObject *tp_bases;
     PyObject *tp_mro; /* method resolution order */
-    PyObject *tp_cache;
+    PyObject *tp_cache; /* no longer used */
     PyObject *tp_subclasses;
     PyObject *tp_weaklist;
     destructor tp_del;
