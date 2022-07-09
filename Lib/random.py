f806df3da119bd7239d626b896148200d75cd124
Raymond Hettinger <raymond.hettinger@gmail.com>
2022-07-09 09:45:20 -0500

    Add section comments to link back to original text

--- Lib/random.py
+++ Lib/random.py
@@ -780,11 +780,14 @@ class Random(_random.Random):
         # https://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.47.8407&rep=rep1&type=pdf
         assert n*p >= 10.0 and p <= 0.5
 
+        # Step 0: Setup for step 1
         spq = _sqrt(n * p * (1.0 - p))
         b = 1.15 + 2.53 * spq
         a = -0.0873 + 0.0248 * b + 0.01 * p
         c = n * p + 0.5
         vr = 0.92 - 4.2 / b
+
+        # Step 3.0: Setup for 3.1
         alpha = (2.83 + 5.1 / b) * spq
         lpq = _log(p / (1.0 - p))
         m = _floor((n + 1) * p)
@@ -792,17 +795,20 @@ class Random(_random.Random):
 
         while True:
 
+            # Step 1: Generate two uniform random numbers
             u = random()
             v = random()
             u -= 0.5
             us = 0.5 - _fabs(u)
             k = _floor((2.0 * a / us + b) * u + c)
 
+            # Step 2: Skip invalid k and test bounding box
             if k < 0 or k > n:
                 continue
             if us >= 0.07 and v <= vr:
                 return k
 
+            # Step 3.1: Acceptance-rejection test
             # Original paper errorneously omits the call to log()
             v = _log(v * alpha / (a / (us * us) + b))
             if v <= h - _logfact(k) - _logfact(n - k) + (k - m) * lpq:
