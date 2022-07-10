e5c8ad3e1518f81f7651d82bbadf5af5e3f402a7
Brandt Bucher <brandtbucher@microsoft.com>
2022-07-09 22:12:45 -0700

    [3.11] GH-94694: Fix column offsets for multi-line method lookups (GH-94721)
    (cherry picked from commit 264b3ddfd561d97204ffb30be6a7d1fb0555e560)

--- Python/compile.c
+++ Python/compile.c
@@ -4788,8 +4788,15 @@ update_location_to_match_attr(struct compiler *c, expr_ty meth)
 {
     if (meth->lineno != meth->end_lineno) {
         // Make start location match attribute
-        c->u->u_lineno = meth->end_lineno;
-        c->u->u_col_offset = meth->end_col_offset - (int)PyUnicode_GetLength(meth->v.Attribute.attr)-1;
+        c->u->u_lineno = c->u->u_end_lineno = meth->end_lineno;
+        int len = (int)PyUnicode_GET_LENGTH(meth->v.Attribute.attr);
+        if (len <= meth->end_col_offset) {
+            c->u->u_col_offset = meth->end_col_offset - len;
+        }
+        else {
+            // GH-94694: Somebody's compiling weird ASTs. Just drop the columns:
+            c->u->u_col_offset = c->u->u_end_col_offset = -1;
+        }
     }
 }
 
