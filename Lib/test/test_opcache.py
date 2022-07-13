dbc624cfa9db28cdbf66b90ca313fad96a75c74b
Brandt Bucher <brandtbucher@microsoft.com>
2022-07-13 16:37:40 -0700

    Add failing regression tests

--- Lib/test/test_opcache.py
+++ Lib/test/test_opcache.py
@@ -21,3 +21,88 @@ class TestLoadAttrCache(unittest.TestCase):
         Descriptor.__set__ = lambda *args: None
 
         self.assertEqual(f(o), 2)
+
+    def test_metaclass_descriptor_added_after_optimization(self):
+        class Descriptor:
+            pass
+
+        class Metaclass(type):
+            attribute = Descriptor()
+
+        class Class(metaclass=Metaclass):
+            attribute = True
+
+        def f():
+            return Class.attribute
+
+        for _ in range(1025):
+            self.assertTrue(f())
+
+        Descriptor.__get__ = lambda self, instance, value: False
+        Descriptor.__set__ = lambda *args: None
+
+        self.assertFalse(f())
+
+    def test_metaclass_descriptor_shadows_class_attribute(self):
+        class Metaclass(type):
+            @property
+            def attribute(self):
+                return True
+
+        class Class(metaclass=Metaclass):
+            attribute = False
+
+        def f():
+            return Class.attribute
+
+        for _ in range(1025):
+            self.assertTrue(f())
+
+    def test_metaclass_set_descriptor_after_optimization(self):
+        class Metaclass(type):
+            pass
+
+        class Class(metaclass=Metaclass):
+            attribute = True
+
+        @property
+        def attribute(self):
+            return False
+
+        def f():
+            return Class.attribute
+
+        for _ in range(1025):
+            self.assertTrue(f())
+
+        Metaclass.attribute = attribute
+        self.assertFalse(f())
+
+    def test_metaclass_del_descriptor_after_optimization(self):
+        class Metaclass(type):
+            @property
+            def attribute(self):
+                return True
+
+        class Class(metaclass=Metaclass):
+            attribute = False
+
+        def f():
+            return Class.attribute
+
+        for _ in range(1025):
+            self.assertTrue(f())
+        
+        del Metaclass.attribute
+        self.assertFalse(f())
+
+    def test_type_descriptor_shadows_attribute(self):
+        class Class:
+            __name__ = "Spam"
+
+        for _ in range(1025):
+            self.assertEqual(Class.__name__, "Class")
+
+if __name__ == "__main__":
+    import unittest
+    unittest.main()
