cddb98d9d47bf635449f013ba24c78a557ab7abd
Brandt Bucher <brandtbucher@microsoft.com>
2022-07-17 11:39:20 -0700

    Add failing regression tests

--- Lib/test/test_sys_settrace.py
+++ Lib/test/test_sys_settrace.py
@@ -2652,6 +2652,27 @@ output.append(4)
         )
         output.append(15)
 
+    @jump_test(2, 3, [1, 3])
+    def test_jump_extended_args_unpack_ex(output):
+        output.append(1)
+        _, *_, _ = spam 
+        output.append(3)
+
+    def test_jump_extended_args_for_iter(self):
+        # In addition to failing when extended arg handling is broken, this can
+        # also hit quadratic time complexity and hang for a *very* long time:
+        source = [
+            "def f(output):",
+            "    output.append(1)",
+            "    for _ in spam:",
+            *(f"        output.append({i})" for i in range(3, 100_000)),
+            f"    output.append(100_000)",
+        ]
+        namespace = {}
+        exec("\n".join(source), namespace)
+        f = namespace["f"]
+        self.run_test(f,  2, 100_000, [1, 100_000])
+
 class TestExtendedArgs(unittest.TestCase):
 
     def setUp(self):
