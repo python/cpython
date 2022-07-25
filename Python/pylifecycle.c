3b7e85d2e89c1ed69ed35595b2957c2f42b64172
Eric Snow <ericsnowcurrently@gmail.com>
2022-07-25 14:23:41 -0600

    gh-94673: Always Finalize Static Builtin Types (#95153)
    Static builtin types are finalized by calling _PyStaticType_Dealloc().  Before this change, we were skipping finalizing such a type if it still had subtypes (i.e. its tp_subclasses hadn't been cleared yet).  The problem is that types hold several heap objects, which leak if we skip the type's finalization.  This change addresses that.
    
    For context, there's an old comment (from 0bcaccd4369) that says the following:
    
       // If a type still has subtypes, it cannot be deallocated.
       // A subtype can inherit attributes and methods of its parent type,
       // and a type must no longer be used once it's deallocated.
    
    However, it isn't clear that is actually still true.  Clearing tp_dict should mean it isn't a problem.
    
    Furthermore, the only subtypes that might still be around come from extension modules that didn't clean them up when unloaded (i.e. extensions that do not implement multi-phase initialization, AKA PEP 489).  Those objects are already leaking, so this change doesn't change anything in that regard.  Instead, this change means more objects gets cleaned up that before.

--- Python/pylifecycle.c
+++ Python/pylifecycle.c
@@ -1672,8 +1672,9 @@ finalize_interp_types(PyInterpreterState *interp)
     _PyLong_FiniTypes(interp);
     _PyThread_FiniType(interp);
     _PyErr_FiniTypes(interp);
-    _PyTypes_Fini(interp);
     _PyTypes_FiniTypes(interp);
+
+    _PyTypes_Fini(interp);
 
     // Call _PyUnicode_ClearInterned() before _PyDict_Fini() since it uses
     // a dict internally.
