db13c0c1b8bc8c15d920a94ca008aabea4037697
Christian Heimes <christian@python.org>
2022-07-31 17:39:41 +0200

    [3.11] gh-95174: Handle missing dup() and constants in WASI (GH-95229) (GH-95272)
    Co-authored-by: Christian Heimes <christian@python.org>

--- Python/fileutils.c
+++ Python/fileutils.c
@@ -2379,7 +2379,7 @@ _Py_dup(int fd)
         return -1;
     }
 
-#else
+#elif HAVE_DUP
     Py_BEGIN_ALLOW_THREADS
     _Py_BEGIN_SUPPRESS_IPH
     fd = dup(fd);
@@ -2396,6 +2396,10 @@ _Py_dup(int fd)
         _Py_END_SUPPRESS_IPH
         return -1;
     }
+#else
+    errno = ENOTSUP;
+    PyErr_SetFromErrno(PyExc_OSError);
+    return -1;
 #endif
     return fd;
 }
