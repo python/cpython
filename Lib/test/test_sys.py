e62a0dfab2bbc29324ca1879e287fff7fa8f21e0
Christian Heimes <christian@python.org>
2022-08-01 16:37:45 +0200

    [3.11] gh-95174: Add pthread stubs for WASI (GH-95234) (#95503)
    Co-authored-by: Brett Cannon <brett@python.org>.
    (cherry picked from commit 0fe645d6fd22a6f57e777a29e65cf9a4ff9785ae)
    
    Co-authored-by: Christian Heimes <christian@python.org>

--- Lib/test/test_sys.py
+++ Lib/test/test_sys.py
@@ -626,7 +626,7 @@ class SysModuleTest(unittest.TestCase):
     def test_thread_info(self):
         info = sys.thread_info
         self.assertEqual(len(info), 3)
-        self.assertIn(info.name, ('nt', 'pthread', 'solaris', None))
+        self.assertIn(info.name, ('nt', 'pthread', 'pthread-stubs', 'solaris', None))
         self.assertIn(info.lock, ('semaphore', 'mutex+cond', None))
 
     @unittest.skipUnless(support.is_emscripten, "only available on Emscripten")
