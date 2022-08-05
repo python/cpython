5fc5996aa9f9217086c9e940ecb0944e5f92c349
Petr Viktorin <encukou@gmail.com>
2022-08-05 16:42:44 +0200

    Test receiving vectorcall in limited API

--- Modules/_testcapi/parts.h
+++ Modules/_testcapi/parts.h
@@ -1,4 +1,5 @@
 #include "Python.h"
 
 int _PyTestCapi_Init_Vectorcall(PyObject *module);
+int _PyTestCapi_Init_VectorcallLimited(PyObject *module);
 int _PyTestCapi_Init_Heaptype(PyObject *module);
