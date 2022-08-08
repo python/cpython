4ed63c9899845eba43c00f75a2193369bbc60ea3
Petr Viktorin <encukou@gmail.com>
2022-08-08 16:11:07 +0200

    Revert "gh-93274: Expose receiving vectorcall in the Limited API (GH-95717)"
    This reverts commit 656dad702d3b25bf678ee9bd7109d98876946258.

--- Modules/_testcapi/parts.h
+++ Modules/_testcapi/parts.h
@@ -1,5 +1,4 @@
 #include "Python.h"
 
 int _PyTestCapi_Init_Vectorcall(PyObject *module);
-int _PyTestCapi_Init_VectorcallLimited(PyObject *module);
 int _PyTestCapi_Init_Heaptype(PyObject *module);
