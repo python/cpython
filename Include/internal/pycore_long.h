da97e659e2258b6db720ed45e0099a0ecabdb2b9
Gregory P. Smith [Google LLC] <gps@google.com>
2022-08-10 00:26:05 -0700

    Raise the default limit and the threshold.
    Intended to minimize disruption. Based on real world testing across a
    huge Python code base including a large corpus of open source project
    test suites.

--- Include/internal/pycore_long.h
+++ Include/internal/pycore_long.h
@@ -12,27 +12,34 @@ extern "C" {
 #include "pycore_runtime.h"       // _PyRuntime
 
 /*
- * Default int base conversion size limitation.
+ * Default int base conversion size limitation: Denial of Service prevention.
  *
- * Chosen such that this isn't wildly slow on modern hardware:
- * % python -m timeit -s 's = "1"*2000; v = int(s)' 'str(int(s))'
- * 2000 loops, best of 5: 100 usec per loop
+ * Chosen such that this isn't wildly slow on modern hardware and so that
+ * everyone's existing deployed numpy test suite passes before
+ * https://github.com/numpy/numpy/issues/22098 is widely available.
  *
- * 2000 decimal digits fits a ~6643 bit number.
+ * $ python -m timeit -s 's = * "1"*4300' 'int(s)'
+ * 2000 loops, best of 5: 125 usec per loop
+ * $ python -m timeit -s 's = * "1"*4300; v = int(s)' 'str(v)'
+ * 1000 loops, best of 5: 311 usec per loop
+ * (zen2 cloud VM)
+ *
+ * 4300 decimal digits fits a ~14284 bit number.
  */
-#define _PY_LONG_DEFAULT_MAX_STR_DIGITS 2000
+#define _PY_LONG_DEFAULT_MAX_STR_DIGITS 4300
 /*
  * Threshold for max digits check.  For performance reasons int() and
- * int.__str__ don't checks values that are smaller than this
+ * int.__str__() don't checks values that are smaller than this
  * threshold.  Acts as a guaranteed minimum size limit for bignums that
  * applications can expect from CPython.
  *
- * % python -m timeit -s 's = "1"*333; v = int(s)' 'str(int(s))'
- * 100000 loops, best of 5: 3.94 usec per loop
+ * % python -m timeit -s 's = "1"*640; v = int(s)' 'str(int(s))'
+ * 20000 loops, best of 5: 12 usec per loop
  *
- * 333 decimal digits fits a ~1106 bit number.
+ * "640 digits should be enough for anyone." - gps
+ * fits a ~2126 bit decimal number.
  */
-#define _PY_LONG_MAX_STR_DIGITS_THRESHOLD 333
+#define _PY_LONG_MAX_STR_DIGITS_THRESHOLD 640
 
 #if ((_PY_LONG_DEFAULT_MAX_STR_DIGITS != 0) && \
    (_PY_LONG_DEFAULT_MAX_STR_DIGITS < _PY_LONG_MAX_STR_DIGITS_THRESHOLD))
