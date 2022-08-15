df4918672ca1c0682d54a74c07bae6e954cf76d0
Petr Viktorin <encukou@gmail.com>
2022-08-15 15:44:35 +0200

    Remove repeated #undef NDEBU

--- Modules/_testcapi/parts.h
+++ Modules/_testcapi/parts.h
@@ -24,9 +24,6 @@
 
 #include "Python.h"
 
-/* Always enable assertions */
-#undef NDEBUG
-
 int _PyTestCapi_Init_Vectorcall(PyObject *module);
 int _PyTestCapi_Init_Heaptype(PyObject *module);
 int _PyTestCapi_Init_Unicode(PyObject *module);
