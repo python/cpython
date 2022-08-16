313ab6d5ca1ab40144c0d3a590c9ee633798ce1b
Gregory P. Smith [Google LLC] <gps@google.com>
2022-08-16 11:47:36 -0700

    Properly clear & free the prior exception.

--- Parser/pegen.c
+++ Parser/pegen.c
@@ -651,15 +651,22 @@ _PyPegen_number_token(Parser *p)
         // PyLong_FromString hitting a length limit.
         if (tstate->curexc_type == PyExc_ValueError &&
             tstate->curexc_value != NULL) {
-            /* Intentionally omitting columns to avoid a giant wall of ^s on
-             * the error message. Nobody is going to overlook their huge
+            PyObject *type, *value, *tb;
+            // This acts as PyErr_Clear() as we're replacing curexc.
+            PyErr_Fetch(&type, &value, &tb);
+            Py_XDECREF(tb);
+            Py_DECREF(type);
+            /* Intentionally omitting columns to avoid a wall of 1000s of '^'s
+             * on the error message. Nobody is going to overlook their huge
              * numeric literal once given the line. */
-            return RAISE_ERROR_KNOWN_LOCATION(
+            RAISE_ERROR_KNOWN_LOCATION(
                 p, PyExc_SyntaxError,
                 t->lineno, -1 /* col_offset */,
                 t->end_lineno, -1 /* end_col_offset */,
-                "%S - Consider hexidecimal for huge integer literals to avoid decimal conversion limits.",
-                tstate->curexc_value);
+                "%S - Consider hexidecimal for huge integer literals "
+                "to avoid decimal conversion limits.",
+                value);
+            Py_DECREF(value);
         }
         return NULL;
     }
