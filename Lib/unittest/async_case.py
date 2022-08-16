9d515997f943b7b510268448f372dabcbf957858
Kumar Aditya <59607654+kumaraditya303@users.noreply.github.com>
2022-08-16 21:22:06 +0530

    GH-95736: fix IsolatedAsyncioTestCase to initialize Runner before calling setup functions  (#95898)

--- Lib/unittest/async_case.py
+++ Lib/unittest/async_case.py
@@ -116,6 +116,10 @@ class IsolatedAsyncioTestCase(TestCase):
         assert self._asyncioRunner is None, 'asyncio runner is already initialized'
         runner = asyncio.Runner(debug=True)
         self._asyncioRunner = runner
+        # Force loop to be initialized and set as the current loop
+        # so that setUp functions can use get_event_loop() and get the
+        # correct loop instance.
+        runner.get_loop()
 
     def _tearDownAsyncioRunner(self):
         runner = self._asyncioRunner
