430d12c2375494f69a2e7380e1b4499a23bef028
Noam Cohen <noam@noam.me>
2022-08-17 07:51:15 +0300

    regen clinic

--- Modules/clinic/syslogmodule.c.h
+++ Modules/clinic/syslogmodule.c.h
@@ -2,6 +2,12 @@
 preserve
 [clinic start generated code]*/
 
+#if defined(Py_BUILD_CORE) && !defined(Py_BUILD_CORE_MODULE)
+#  include "pycore_gc.h"            // PyGC_Head
+#  include "pycore_runtime.h"       // _Py_ID()
+#endif
+
+
 PyDoc_STRVAR(syslog_openlog__doc__,
 "openlog($module, /, ident=<unrepresentable>, logoption=0,\n"
 "        facility=LOG_USER)\n"
@@ -20,8 +26,31 @@ static PyObject *
 syslog_openlog(PyObject *module, PyObject *const *args, Py_ssize_t nargs, PyObject *kwnames)
 {
     PyObject *return_value = NULL;
+    #if defined(Py_BUILD_CORE) && !defined(Py_BUILD_CORE_MODULE)
+
+    #define NUM_KEYWORDS 3
+    static struct {
+        PyGC_Head _this_is_not_used;
+        PyObject_VAR_HEAD
+        PyObject *ob_item[NUM_KEYWORDS];
+    } _kwtuple = {
+        .ob_base = PyVarObject_HEAD_INIT(&PyTuple_Type, NUM_KEYWORDS)
+        .ob_item = { &_Py_ID(ident), &_Py_ID(logoption), &_Py_ID(facility), },
+    };
+    #undef NUM_KEYWORDS
+    #define KWTUPLE (&_kwtuple.ob_base.ob_base)
+
+    #else  // !Py_BUILD_CORE
+    #  define KWTUPLE NULL
+    #endif  // !Py_BUILD_CORE
+
     static const char * const _keywords[] = {"ident", "logoption", "facility", NULL};
-    static _PyArg_Parser _parser = {NULL, _keywords, "openlog", 0};
+    static _PyArg_Parser _parser = {
+        .keywords = _keywords,
+        .fname = "openlog",
+        .kwtuple = KWTUPLE,
+    };
+    #undef KWTUPLE
     PyObject *argsbuf[3];
     Py_ssize_t noptargs = nargs + (kwnames ? PyTuple_GET_SIZE(kwnames) : 0) - 0;
     PyObject *ident = NULL;
@@ -225,4 +254,4 @@ syslog_LOG_UPTO(PyObject *module, PyObject *arg)
 exit:
     return return_value;
 }
-/*[clinic end generated code: output=23b0c39420c43124 input=a9049054013a1b77]*/
+/*[clinic end generated code: output=3b1bdb16565b8fda input=a9049054013a1b77]*/
