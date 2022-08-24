02983cc3dbea2ac6f276bdf5f0d7a1da9a70f5cf
Serhiy Storchaka <storchaka@gmail.com>
2022-08-24 13:15:07 +0300

    Call support.gc_collect() after test._tearDownAsyncioRunner(), not before it.

--- Lib/test/test_unittest/test_async_case.py
+++ Lib/test/test_unittest/test_async_case.py
@@ -43,10 +43,10 @@ VAR = contextvars.ContextVar('VAR', default=())
 class TestAsyncCase(unittest.TestCase):
     maxDiff = None
 
-    def tearDown(self):
+    def setUp(self):
         # Ensure that IsolatedAsyncioTestCase instances are destroyed before
         # starting a new event loop
-        support.gc_collect()
+        self.addCleanup(support.gc_collect)
 
     def test_full_cycle(self):
         class Test(unittest.IsolatedAsyncioTestCase):
