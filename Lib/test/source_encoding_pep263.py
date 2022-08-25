880fda6ed5758c480884175be391830dc0a0affb
Michael Droettboom <mdboom@gmail.com>
2022-08-25 13:56:38 -0400

    Fix test

--- Lib/test/source_encoding_pep263.py
+++ Lib/test/source_encoding_pep263.py
@@ -1,11 +1,4 @@
 # -*- coding: koi8-r -*-
 
-def test_pep263(self):
-    self.assertEqual(
-        "Питон".encode("utf-8"),
-        b'\xd0\x9f\xd0\xb8\xd1\x82\xd0\xbe\xd0\xbd'
-    )
-    self.assertEqual(
-        "\П".encode("utf-8"),
-        b'\\\xd0\x9f'
-    )
+assert "Питон".encode("utf-8") == b'\xd0\x9f\xd0\xb8\xd1\x82\xd0\xbe\xd0\xbd'
+assert "\П".encode("utf-8") == b'\\\xd0\x9f'
