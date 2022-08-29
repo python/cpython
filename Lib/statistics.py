109543d28919dc4c2175da5fa308220a14538969
Raymond Hettinger <raymond.hettinger@gmail.com>
2022-08-28 23:06:51 -0500

    Adjust line spacing

--- Lib/statistics.py
+++ Lib/statistics.py
@@ -355,6 +355,7 @@ def _fail_neg(values, errmsg='negative value'):
             raise StatisticsError(errmsg)
         yield x
 
+
 def _rank(data, /, *, key=None, reverse=False, ties='average', start=1) -> list[float]:
     """Rank order a dataset. The lowest value has rank 1.
 
@@ -407,6 +408,7 @@ def _rank(data, /, *, key=None, reverse=False, ties='average', start=1) -> list[
             result[orig_pos] = rank
         i += size
     return result
+
 
 def _integer_sqrt_of_frac_rto(n: int, m: int) -> int:
     """Square root of n/m, rounded to the nearest integer using round-to-odd."""
