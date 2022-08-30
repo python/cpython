1613e646cd9f6f75d33e5b2ebb88d8e38aeb34d3
CharlieZhao <zhaoyu_hit@qq.com>
2022-08-30 11:37:56 +0800

    Clean previous instances of ouput files in `Python\deepfreeze` and `Python\frozen_modules` directories.

--- PCbuild/clean.bat
+++ PCbuild/clean.bat
@@ -2,4 +2,13 @@
 rem A batch program to clean a particular configuration,
 rem just for convenience.
 
-call "%~dp0build.bat" -t Clean %*
+set dir=%~dp0
+
+call "%dir%build.bat" -t Clean %*
+
+rem Clean previous instances of ouput files in `Python\deepfreeze`
+rem and `Python\frozen_modules` directories to ensure successful building
+rem on Windows.
+
+del "%dir%\..\Python\deepfreeze\*.c"
+del "%dir%\..\Python\frozen_modules\*.h"
