d5e701991f2e3ba0d2cc298be03cca91d03a25e1
sobolevn <mail@sobolevn.me>
2022-08-30 12:32:20 +0300

    gh-96415: Remove `types._cell_factory` from a module namespace

--- Lib/types.py
+++ Lib/types.py
@@ -60,7 +60,7 @@ except TypeError as exc:
 GetSetDescriptorType = type(FunctionType.__code__)
 MemberDescriptorType = type(FunctionType.__globals__)
 
-del sys, _f, _g, _C, _c, _ag  # Not for export
+del sys, _f, _g, _C, _c, _ag, _cell_factory  # Not for export
 
 
 # Provide a PEP 3115 compliant mechanism for class creation
