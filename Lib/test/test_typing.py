0cd33e11fe6c77c423dbf3a7a9920daff012f006
Anh71me <iyumelive@gmail.com>
2022-09-01 07:02:24 +0800

    GH-96079 Fix missing field name for _AnnotatedAlias (#96080)

--- Lib/test/test_typing.py
+++ Lib/test/test_typing.py
@@ -7143,6 +7143,7 @@ class SpecialAttrsTests(BaseTestCase):
             typing.Self: 'Self',
             # Subscribed special forms
             typing.Annotated[Any, "Annotation"]: 'Annotated',
+            typing.Annotated[int, 'Annotation']: 'Annotated',
             typing.ClassVar[Any]: 'ClassVar',
             typing.Concatenate[Any, SpecialAttrsP]: 'Concatenate',
             typing.Final[Any]: 'Final',
