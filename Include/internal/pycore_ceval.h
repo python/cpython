ee2511cda2de67126241444a50ee388a307d61ee
Mark Shannon <mark@hotpy.org>
2022-09-01 12:53:33 +0100

    Add required symbol back.

--- Include/internal/pycore_ceval.h
+++ Include/internal/pycore_ceval.h
@@ -119,7 +119,7 @@ PyAPI_FUNC(int) _Py_CheckRecursiveCall(
     const char *where);
 
 
-int _Py_StackOverflowCheckCall(
+PyAPI_FUNC(int) _Py_StackOverflowCheckCall(
     PyThreadState *tstate,
     const char *where,
     intptr_t scaled_location);
