1039ba2bca0d7d79cb4cebfe3217eabe1d0606df
Gleb Smirnoff <glebius@FreeBSD.org>
2022-09-03 09:02:34 +0000

    gh-96534: socketmodule: support FreeBSD divert(4) socket

--- Modules/socketmodule.c
+++ Modules/socketmodule.c
@@ -1850,6 +1850,11 @@ getsockaddrarg(PySocketSockObject *s, PyObject *args,
         /* RDS sockets use sockaddr_in: fall-through */
 #endif /* AF_RDS */
 
+#ifdef AF_DIVERT
+    case AF_DIVERT:
+        /* FreeBSD divert(4) sockets use sockaddr_in: fall-through */
+#endif /* AF_DIVERT */
+
     case AF_INET:
     {
         struct maybe_idna host = {NULL, NULL};
@@ -7626,6 +7631,14 @@ PyInit__socket(void)
 #endif
 #ifdef AF_SYSTEM
     PyModule_AddIntMacro(m, AF_SYSTEM);
+#endif
+
+/* FreeBSD divert(4) */
+#ifdef PF_DIVERT
+    PyModule_AddIntMacro(m, PF_DIVERT);
+#endif
+#ifdef AF_DIVERT
+    PyModule_AddIntMacro(m, AF_DIVERT);
 #endif
 
 #ifdef AF_PACKET
