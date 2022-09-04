7f911c1058acb6571998b9098598f87cd5a05dac
Gregory P. Smith [Google LLC] <greg@krypto.org>
2022-09-04 09:53:54 -0700

    backport cherry pick fix: lookup max from the right place.

--- Objects/longobject.c
+++ Objects/longobject.c
@@ -1614,8 +1614,7 @@ long_to_decimal_string_internal(PyObject *aa,
     */
     if (size_a >= 10 * _PY_LONG_MAX_STR_DIGITS_THRESHOLD
                   / (3 * PyLong_SHIFT) + 2) {
-        PyInterpreterState *interp = _PyInterpreterState_GET();
-        int max_str_digits = interp->int_max_str_digits;
+        int max_str_digits = _PyRuntime.int_max_str_digits;
         if ((max_str_digits > 0) &&
             (max_str_digits / (3 * PyLong_SHIFT) <= (size_a - 11) / 10)) {
             PyErr_Format(PyExc_ValueError, _MAX_STR_DIGITS_ERROR_FMT_TO_STR,
