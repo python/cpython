25aa726d894645646b36c60d5e14de4efe65576b
Gregory P. Smith [Google] <greg@krypto.org>
2022-09-04 22:01:49 +0000

    Revert "Undo addition to PyConfig to ease backporting."
    This reverts commit 1ad88f551dd2dbcbcb1d8f57a4f05dfddc39d587.

--- Include/internal/pycore_initconfig.h
+++ Include/internal/pycore_initconfig.h
@@ -170,8 +170,6 @@ extern void _Py_DumpPathConfig(PyThreadState *tstate);
 
 PyAPI_FUNC(PyObject*) _Py_Get_Getpath_CodeObject(void);
 
-extern int _Py_global_config_int_max_str_digits;  // TODO(gpshead): move this into PyConfig in 3.12 after the backports ship.
-
 
 /* --- Function used for testing ---------------------------------- */
 
