cc6aed553792531448ecfd77db5007949a2e046d
Kumar Aditya <59607654+kumaraditya303@users.noreply.github.com>
2022-09-05 15:04:44 +0000

    fix malloc failure

--- Python/pystate.c
+++ Python/pystate.c
@@ -1413,7 +1413,12 @@ _PyThread_CurrentFrames(void)
             if (id == NULL) {
                 goto fail;
             }
-            int stat = PyDict_SetItem(result, id, (PyObject *)_PyFrame_GetFrameObject(frame));
+            PyObject *frameobj = (PyObject *)_PyFrame_GetFrameObject(frame);
+            if (frameobj == NULL) {
+                Py_DECREF(id);
+                goto fail;
+            }
+            int stat = PyDict_SetItem(result, id, frameobj);
             Py_DECREF(id);
             if (stat < 0) {
                 goto fail;
