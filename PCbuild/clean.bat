2e178d9c159b4b403ef92c34a87582155f49df39
CharlieZhao <zhaoyu_hit@qq.com>
2022-09-07 11:05:13 +0800

    Clean freeze modules

--- PCbuild/clean.bat
+++ PCbuild/clean.bat
@@ -2,13 +2,4 @@
 rem A batch program to clean a particular configuration,
 rem just for convenience.
 
-set dir=%~dp0
-
-call "%dir%build.bat" -t Clean %*
-
-rem Clean previous instances of ouput files in `Python\deepfreeze`
-rem and `Python\frozen_modules` directories to ensure successful building
-rem on Windows.
-
-del "%dir%\..\Python\deepfreeze\*.c"
-del "%dir%\..\Python\frozen_modules\*.h"
+call "%~dp0build.bat" -t Clean %*
