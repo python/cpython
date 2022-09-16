bac61bc5b132221dde406f0700e238c302029698
Miss Islington (bot) <31488909+miss-islington@users.noreply.github.com>
2022-09-16 11:33:27 -0700

    gh-95778: Mention sys.set_int_max_str_digits() in error message (GH-96874)
    When ValueError is raised if an integer is larger than the limit,
    mention sys.set_int_max_str_digits() in the error message.
    (cherry picked from commit e841ffc915e82e5ea6e3b473205417d63494808d)
    
    Co-authored-by: Victor Stinner <vstinner@python.org>

--- Objects/longobject.c
+++ Objects/longobject.c
@@ -36,8 +36,8 @@ medium_value(PyLongObject *x)
 #define IS_SMALL_INT(ival) (-_PY_NSMALLNEGINTS <= (ival) && (ival) < _PY_NSMALLPOSINTS)
 #define IS_SMALL_UINT(ival) ((ival) < _PY_NSMALLPOSINTS)
 
-#define _MAX_STR_DIGITS_ERROR_FMT_TO_INT "Exceeds the limit (%d) for integer string conversion: value has %zd digits"
-#define _MAX_STR_DIGITS_ERROR_FMT_TO_STR "Exceeds the limit (%d) for integer string conversion"
+#define _MAX_STR_DIGITS_ERROR_FMT_TO_INT "Exceeds the limit (%d) for integer string conversion: value has %zd digits; use sys.set_int_max_str_digits() to increase the limit"
+#define _MAX_STR_DIGITS_ERROR_FMT_TO_STR "Exceeds the limit (%d) for integer string conversion; use sys.set_int_max_str_digits() to increase the limit"
 
 static inline void
 _Py_DECREF_INT(PyLongObject *op)
