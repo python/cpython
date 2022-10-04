7fcfa94a856afecd20f7f66b6c248f776724bf7c
Miss Islington (bot) <31488909+miss-islington@users.noreply.github.com>
2022-10-04 11:59:43 -0700

    [3.7] gh-95778: Mention sys.set_int_max_str_digits() in error message (GH-96874) (GH-96877) (GH-97836)
    [3.9] gh-95778: Mention sys.set_int_max_str_digits() in error message (GH-96874) (GH-96877)
    
    When ValueError is raised if an integer is larger than the limit,
    mention sys.set_int_max_str_digits() in the error message.
    
    (cherry picked from commit e841ffc915e82e5ea6e3b473205417d63494808d)
    
    Co-authored-by: Ned Deily <nad@python.org>
    (cherry picked from commit 41188134bd2120f0cedd681ed88743c11c7f3742)
    
    Co-authored-by: Victor Stinner <vstinner@python.org>

--- Objects/longobject.c
+++ Objects/longobject.c
@@ -47,8 +47,8 @@ static PyLongObject small_ints[NSMALLNEGINTS + NSMALLPOSINTS];
 Py_ssize_t quick_int_allocs, quick_neg_int_allocs;
 #endif
 
-#define _MAX_STR_DIGITS_ERROR_FMT_TO_INT "Exceeds the limit (%d) for integer string conversion: value has %zd digits"
-#define _MAX_STR_DIGITS_ERROR_FMT_TO_STR "Exceeds the limit (%d) for integer string conversion"
+#define _MAX_STR_DIGITS_ERROR_FMT_TO_INT "Exceeds the limit (%d) for integer string conversion: value has %zd digits; use sys.set_int_max_str_digits() to increase the limit"
+#define _MAX_STR_DIGITS_ERROR_FMT_TO_STR "Exceeds the limit (%d) for integer string conversion; use sys.set_int_max_str_digits() to increase the limit"
 
 static PyObject *
 get_small_int(sdigit ival)
