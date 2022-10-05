23998e4403b11b857502e9420ded0989b370c701
Irit Katriel <1055913+iritkatriel@users.noreply.github.com>
2022-10-05 08:52:35 +0100

    gh-87092: bring compiler code closer to a preprocessing-opt-assembler organisation (GH-97644)

--- Lib/test/test_compile.py
+++ Lib/test/test_compile.py
@@ -670,10 +670,22 @@ if 1:
 
         self.assertIs(f1.__code__.co_linetable, f2.__code__.co_linetable)
 
+    @support.cpython_only
+    def test_strip_unused_consts(self):
+        def f():
+            "docstring"
+            if True:
+                return "used"
+            else:
+                return "unused"
+
+        self.assertEqual(f.__code__.co_consts,
+                         ("docstring", True, "used"))
+
     # Stripping unused constants is not a strict requirement for the
     # Python semantics, it's a more an implementation detail.
     @support.cpython_only
-    def test_strip_unused_consts(self):
+    def test_strip_unused_None(self):
         # Python 3.10rc1 appended None to co_consts when None is not used
         # at all. See bpo-45056.
         def f1():
