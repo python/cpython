8aa1b3dff4865535d9b4bbefe5a6f6c02425536a
Tiger Nie <tnie@tuta.io>
2022-10-10 19:12:21 -0500

    removed links to the bugs

--- Lib/dataclasses.py
+++ Lib/dataclasses.py
@@ -1329,7 +1329,6 @@ def _asdict_inner(obj, dict_factory):
         if hasattr(type(obj), 'default_factory'):
             # obj is a defaultdict, which has a different constructor from
             # dict as it requires the default_factory as its first arg.
-            # https://bugs.python.org/issue35540
             result = type(obj)(getattr(obj, 'default_factory'))
             for k, v in obj.items():
                 result[_asdict_inner(k, dict_factory)] = _asdict_inner(v, dict_factory)
@@ -1389,7 +1388,6 @@ def _astuple_inner(obj, tuple_factory):
         if hasattr(type(obj), 'default_factory'):
             # obj is a defaultdict, which has a different constructor from
             # dict as it requires the default_factory as its first arg.
-            # https://bugs.python.org/issue35540
             result = type(obj)(getattr(obj, 'default_factory'))
             for k, v in obj.items():
                 result[_astuple_inner(k, tuple_factory)] = _asdict_inner(v, tuple_factory)
