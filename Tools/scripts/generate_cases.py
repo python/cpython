7def42b5866d0d50aa168d7c2d419c62751b3034
Guido van Rossum <guido@python.org>
2022-10-13 21:54:29 -0700

    Start writing a real parser
    (This is a checkpoint, doesn't work yet.)

--- Tools/scripts/generate_cases.py
+++ Tools/scripts/generate_cases.py
@@ -10,11 +10,146 @@ import dis
 import re
 import sys
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-i", "--input", type=str, default="Python/bytecodes.inst")
-parser.add_argument("-o", "--output", type=str, default="Python/cases.h")
-parser.add_argument("-c", "--compare", action="store_true")
-parser.add_argument("-q", "--quiet", action="store_true")
+import c_lexer as clx  # Tools/scripts/c_lexer.py
+
+arg_parser = argparse.ArgumentParser()
+arg_parser.add_argument("-i", "--input", type=str, default="Python/bytecodes.inst")
+arg_parser.add_argument("-o", "--output", type=str, default="Python/cases.h")
+arg_parser.add_argument("-c", "--compare", action="store_true")
+arg_parser.add_argument("-q", "--quiet", action="store_true")
+
+
+class Lexer:
+    def __init__(self, filename):
+        with eopen(filename) as f:
+            self.src = f.read()
+        self.all_tokens = list(clx.tokenize(self.src))
+        self.tokens = [tkn for tkn in self.all_tokens if tkn.kind != "COMMENT"]
+        self.pos = 0
+
+    def getpos(self):
+        return self.pos
+    
+    def reset(self, pos):
+        assert 0 <= pos < len(self.tokens), (pos, len(self.tokens))
+        self.pos = pos
+
+    def peek(self):
+        if self.pos < len(self.tokens):
+            return self.tokens[self.pos]
+        return None
+
+    def next(self):
+        if self.pos < len(self.tokens):
+            self.pos += 1
+            return self.tokens[self.pos - 1]
+        return None
+    
+    def eof(self):
+        return self.pos >= len(self.tokens)
+
+    def backup(self):
+        assert self.pos > 0
+        self.pos -= 1
+    
+    def expect(self, kind):
+        tkn = self.next()
+        if tkn is not None and tkn.kind == kind:
+            return tkn
+        self.backup()
+        return None
+
+
+@dataclass
+class InstHeader:
+    name: str
+    inputs: list[str]
+    outputs: list[str]
+
+class Parser:
+    def __init__(self, lexer):
+        self.lexer = lexer
+        self.getpos = lexer.getpos
+        self.reset = lexer.reset
+        self.peek = lexer.peek
+        self.next = lexer.next
+        self.eof = lexer.eof
+        self.backup = lexer.backup
+        self.expect = lexer.expect
+    
+    def c_blob(self):
+        tokens = []
+        level = 0
+        while True:
+            tkn = self.next()
+            if tkn is None:
+                break
+            if tkn.kind in (clx.LBRACE, clx.LPAREN, clx.LBRACKET):
+                level += 1
+            elif tkn.kind in (clx.RBRACE, clx.RPAREN, clx.RBRACKET):
+                level -= 1
+                if level < 0:
+                    self.backup()
+                    break
+            tokens.append(tkn)
+        return tokens
+
+    def inst_header(self):
+        # inst(NAME, (inputs -- outputs)) {
+        # TODO: Error out when there is something unexpected.
+        here = self.getpos()
+        if (tkn := self.expect(clx.IDENTIFIER)) and tkn.text == "inst":
+            if (self.expect(clx.LPAREN)
+                    and (tkn := self.expect(clx.IDENTIFIER))):
+                name = tkn.text
+                if self.expect(clx.COMMA) and self.expect(clx.LPAREN):
+                    inp = self.inputs() or []
+                    if self.expect(clx.MINUSMINUS):
+                        outp = self.outputs() or []
+                        if (self.expect(clx.RPAREN)
+                                and self.expect(clx.RPAREN)
+                                and self.expect(clx.LBRACE)):
+                            return InstHeader(name, inp, outp)
+        self.reset(here)
+        return None
+
+    def inputs(self):
+        # input (, input)*
+        here = self.getpos()
+        if inp := self.input():
+            near = self.getpos()
+            if self.expect(clx.COMMA):
+                if rest := self.input_list():
+                    return [inp] + rest
+                self.reset(near)
+                return [inp]
+        self.reset(here)
+        return None
+
+    def input(self):
+        # IDENTIFIER
+        if (tkn := self.expect(clx.IDENTIFIER)):
+            return tkn.text
+        return None
+
+    def outputs(self):
+        # output (, output)*
+        here = self.getpos()
+        if outp := self.output():
+            near = self.getpos()
+            if self.expect(clx.COMMA):
+                if rest := self.output_list():
+                    return [outp] + rest
+                self.reset(near)
+                return [outp]
+        self.reset(here)
+        return None
+
+    def output(self):
+        # IDENTIFIER
+        if (tkn := self.expect(clx.IDENTIFIER)):
+            return tkn.text
+        return None
 
 
 def eopen(filename, mode="r"):
@@ -146,7 +281,16 @@ def write_cases(f, instrs):
 
 
 def main():
-    args = parser.parse_args()
+    args = arg_parser.parse_args()
+    if 0:
+        lxr = Lexer(args.input)
+        psr = Parser(lxr)
+        inst = psr.inst_header()
+        print(inst)
+        blob = psr.c_blob()
+        print(clx.to_text(blob))
+        assert psr.expect(clx.RBRACE)
+        return
     with eopen(args.input) as f:
         instrs = parse_instrs(f)
     if not args.quiet:
