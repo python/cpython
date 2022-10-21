d08c830f75b518758e9a25923d0a10246f38d721
Dong-hee Na <donghee.na@python.org>
2022-10-21 14:24:32 +0900

    gh-84032: Fix tarfile to catch BrokenPipeError.

--- Lib/tarfile.py
+++ Lib/tarfile.py
@@ -2548,73 +2548,78 @@ def main():
     group.add_argument('-t', '--test', metavar='<tarfile>',
                        help='Test if a tarfile is valid')
     args = parser.parse_args()
+    try:
+        if args.test is not None:
+            src = args.test
+            if is_tarfile(src):
+                with open(src, 'r') as tar:
+                    tar.getmembers()
+                    print(tar.getmembers(), file=sys.stderr)
+                if args.verbose:
+                    print('{!r} is a tar archive.'.format(src))
+            else:
+                parser.exit(1, '{!r} is not a tar archive.\n'.format(src))
 
-    if args.test is not None:
-        src = args.test
-        if is_tarfile(src):
-            with open(src, 'r') as tar:
-                tar.getmembers()
-                print(tar.getmembers(), file=sys.stderr)
+        elif args.list is not None:
+            src = args.list
+            if is_tarfile(src):
+                with TarFile.open(src, 'r:*') as tf:
+                    tf.list(verbose=args.verbose)
+            else:
+                parser.exit(1, '{!r} is not a tar archive.\n'.format(src))
+
+        elif args.extract is not None:
+            if len(args.extract) == 1:
+                src = args.extract[0]
+                curdir = os.curdir
+            elif len(args.extract) == 2:
+                src, curdir = args.extract
+            else:
+                parser.exit(1, parser.format_help())
+
+            if is_tarfile(src):
+                with TarFile.open(src, 'r:*') as tf:
+                    tf.extractall(path=curdir)
+                if args.verbose:
+                    if curdir == '.':
+                        msg = '{!r} file is extracted.'.format(src)
+                    else:
+                        msg = ('{!r} file is extracted '
+                            'into {!r} directory.').format(src, curdir)
+                    print(msg)
+            else:
+                parser.exit(1, '{!r} is not a tar archive.\n'.format(src))
+
+        elif args.create is not None:
+            tar_name = args.create.pop(0)
+            _, ext = os.path.splitext(tar_name)
+            compressions = {
+                # gz
+                '.gz': 'gz',
+                '.tgz': 'gz',
+                # xz
+                '.xz': 'xz',
+                '.txz': 'xz',
+                # bz2
+                '.bz2': 'bz2',
+                '.tbz': 'bz2',
+                '.tbz2': 'bz2',
+                '.tb2': 'bz2',
+            }
+            tar_mode = 'w:' + compressions[ext] if ext in compressions else 'w'
+            tar_files = args.create
+
+            with TarFile.open(tar_name, tar_mode) as tf:
+                for file_name in tar_files:
+                    tf.add(file_name)
+
             if args.verbose:
-                print('{!r} is a tar archive.'.format(src))
-        else:
-            parser.exit(1, '{!r} is not a tar archive.\n'.format(src))
-
-    elif args.list is not None:
-        src = args.list
-        if is_tarfile(src):
-            with TarFile.open(src, 'r:*') as tf:
-                tf.list(verbose=args.verbose)
-        else:
-            parser.exit(1, '{!r} is not a tar archive.\n'.format(src))
-
-    elif args.extract is not None:
-        if len(args.extract) == 1:
-            src = args.extract[0]
-            curdir = os.curdir
-        elif len(args.extract) == 2:
-            src, curdir = args.extract
-        else:
-            parser.exit(1, parser.format_help())
-
-        if is_tarfile(src):
-            with TarFile.open(src, 'r:*') as tf:
-                tf.extractall(path=curdir)
-            if args.verbose:
-                if curdir == '.':
-                    msg = '{!r} file is extracted.'.format(src)
-                else:
-                    msg = ('{!r} file is extracted '
-                           'into {!r} directory.').format(src, curdir)
-                print(msg)
-        else:
-            parser.exit(1, '{!r} is not a tar archive.\n'.format(src))
-
-    elif args.create is not None:
-        tar_name = args.create.pop(0)
-        _, ext = os.path.splitext(tar_name)
-        compressions = {
-            # gz
-            '.gz': 'gz',
-            '.tgz': 'gz',
-            # xz
-            '.xz': 'xz',
-            '.txz': 'xz',
-            # bz2
-            '.bz2': 'bz2',
-            '.tbz': 'bz2',
-            '.tbz2': 'bz2',
-            '.tb2': 'bz2',
-        }
-        tar_mode = 'w:' + compressions[ext] if ext in compressions else 'w'
-        tar_files = args.create
-
-        with TarFile.open(tar_name, tar_mode) as tf:
-            for file_name in tar_files:
-                tf.add(file_name)
-
-        if args.verbose:
-            print('{!r} file created.'.format(tar_name))
+                print('{!r} file created.'.format(tar_name))
+    finally:
+        sys.stdout.close()
 
 if __name__ == '__main__':
-    main()
+    try:
+        main()
+    except BrokenPipeError as exc:
+        sys.exit(exc.errno)
