1eb005362bc16e1e6cffa41e9197b6020605954a
Kumar Aditya <59607654+kumaraditya303@users.noreply.github.com>
2022-10-21 14:50:57 +0000

    remove outdated tests

--- Lib/test/test_asyncio/test_futures2.py
+++ Lib/test/test_asyncio/test_futures2.py
@@ -76,21 +76,6 @@ class CFutureTests(FutureTests, unittest.IsolatedAsyncioTestCase):
 class PyFutureTests(FutureTests, unittest.IsolatedAsyncioTestCase):
     cls = tasks._PyTask
 
-class FutureReprTests(unittest.IsolatedAsyncioTestCase):
-
-    async def test_recursive_repr_for_pending_tasks(self):
-        # The call crashes if the guard for recursive call
-        # in base_futures:_future_repr_info is absent
-        # See Also: https://bugs.python.org/issue42183
-
-        async def func():
-            return asyncio.all_tasks()
-
-        # The repr() call should not raise RecursiveError at first.
-        # The check for returned string is not very reliable but
-        # exact comparison for the whole string is even weaker.
-        self.assertIn('...', repr(await asyncio.wait_for(func(), timeout=10)))
-
 
 if __name__ == '__main__':
     unittest.main()
