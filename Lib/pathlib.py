e6cf222257b744a714ed3ab438da833e0ab2da0d
Domenico Ragusa <domenicoragusa@gmail.com>
2022-10-22 03:43:19 +0200

    remove trailing spaces

--- Lib/pathlib.py
+++ Lib/pathlib.py
@@ -630,7 +630,7 @@ class PurePath(object):
         """Return the relative path to another path identified by the passed
         arguments.  If the operation is not possible (because this is not
         related to the other path), raise ValueError.
-        
+
         The *walk_up* parameter controls whether `..` may be used to resolve
         the path.
         """
