86703dea483ef5d1d916ac39f82b44ea8ef8e5f9
colorfulappl <colorfulappl@qq.com>
2022-10-25 23:03:10 +0800

    Merge TestClinicFunctionalityC class into TestClinicFunctionality

--- Lib/test/test_clinic_functionality.py
+++ Lib/test/test_clinic_functionality.py
@@ -5,6 +5,9 @@ ac_tester = import_helper.import_module('_testclinic')
 
 
 class TestClinicFunctionality(unittest.TestCase):
+    locals().update((name, getattr(ac_tester, name))
+                    for name in dir(ac_tester) if name.startswith('test_'))
+
     def test_gh_32092_oob(self):
         res = ac_tester.gh_32092_oob(1, 2, 3, 4, kw1=5, kw2=6)
         expect = (1, 2, (3, 4), 5, 6)
@@ -14,8 +17,3 @@ class TestClinicFunctionality(unittest.TestCase):
         res = ac_tester.gh_32092_kw_pass(1, 2, 3)
         expect = (1, (2, 3), None)
         self.assertEqual(res, expect)
-
-
-class TestClinicFunctionalityC(unittest.TestCase):
-    locals().update((name, getattr(ac_tester, name))
-                    for name in dir(ac_tester) if name.startswith('test_'))
