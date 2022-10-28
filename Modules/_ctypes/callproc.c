5123aaa9905ef0a9851871d1d6f1bba33c193a9a
Kamil Turek <kamil.turek@hotmail.com>
2022-10-28 22:19:53 +0200

    Use PyType_GetName

--- Modules/_ctypes/callproc.c
+++ Modules/_ctypes/callproc.c
@@ -1019,7 +1019,7 @@ void _ctypes_extend_error(PyObject *exc_class, const char *fmt, ...)
 
     PyErr_Fetch(&tp, &v, &tb);
     PyErr_NormalizeException(&tp, &v, &tb);
-    cls_str = PyUnicode_FromString(((PyTypeObject *)tp)->tp_name);
+    cls_str = PyType_GetName((PyTypeObject *)tp);
     if (cls_str) {
         PyUnicode_AppendAndDel(&s, cls_str);
         PyUnicode_AppendAndDel(&s, PyUnicode_FromString(": "));
