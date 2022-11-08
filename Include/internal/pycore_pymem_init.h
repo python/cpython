ac5cfb0e036fff326ae31c64acb590b17f50dec9
Eric Snow <ericsnowcurrently@gmail.com>
2022-11-07 17:08:07 -0700

    Move the arena allocator to pycore_pymem_init.h.

--- Include/internal/pycore_pymem_init.h
+++ Include/internal/pycore_pymem_init.h
@@ -11,6 +11,69 @@ extern "C" {
 #include "pycore_allocators.h"
 
 
+/***************************************/
+/* the object allocator implementation */
+
+#ifdef WITH_PYMALLOC
+#  ifdef MS_WINDOWS
+#    include <windows.h>
+#  elif defined(HAVE_MMAP)
+#    include <sys/mman.h>
+#    ifdef MAP_ANONYMOUS
+#      define ARENAS_USE_MMAP
+#    endif
+#  endif
+#endif
+
+#ifdef MS_WINDOWS
+static void *
+_PyObject_ArenaVirtualAlloc(void *Py_UNUSED(ctx), size_t size)
+{
+    return VirtualAlloc(NULL, size,
+                        MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+}
+
+static void
+_PyObject_ArenaVirtualFree(void *Py_UNUSED(ctx), void *ptr,
+    size_t Py_UNUSED(size))
+{
+    VirtualFree(ptr, 0, MEM_RELEASE);
+}
+
+#elif defined(ARENAS_USE_MMAP)
+static void *
+_PyObject_ArenaMmap(void *Py_UNUSED(ctx), size_t size)
+{
+    void *ptr;
+    ptr = mmap(NULL, size, PROT_READ|PROT_WRITE,
+               MAP_PRIVATE|MAP_ANONYMOUS, -1, 0);
+    if (ptr == MAP_FAILED)
+        return NULL;
+    assert(ptr != NULL);
+    return ptr;
+}
+
+static void
+_PyObject_ArenaMunmap(void *Py_UNUSED(ctx), void *ptr, size_t size)
+{
+    munmap(ptr, size);
+}
+
+#else
+static void *
+_PyObject_ArenaMalloc(void *Py_UNUSED(ctx), size_t size)
+{
+    return malloc(size);
+}
+
+static void
+_PyObject_ArenaFree(void *Py_UNUSED(ctx), void *ptr, size_t Py_UNUSED(size))
+{
+    free(ptr);
+}
+#endif
+
+
 /********************************/
 /* the allocators' initializers */
 
