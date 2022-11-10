6fef74293cd3a2c26ddafc7b6019bce285c26d97
Mark Shannon <mark@hotpy.org>
2022-11-10 04:34:57 -0800

    GH-96421: Insert shim frame on entry to interpreter (GH-96319)
    * Adds EXIT_INTERPRETER instruction to exit PyEval_EvalDefault()
    
    * Simplifies RETURN_VALUE, YIELD_VALUE and RETURN_GENERATOR instructions as they no longer need to check for entry frames.

--- Python/pystate.c
+++ Python/pystate.c
@@ -454,6 +454,7 @@ interpreter_clear(PyInterpreterState *interp, PyThreadState *tstate)
     PyDict_Clear(interp->builtins);
     Py_CLEAR(interp->sysdict);
     Py_CLEAR(interp->builtins);
+    Py_CLEAR(interp->interpreter_trampoline);
 
     for (int i=0; i < DICT_MAX_WATCHERS; i++) {
         interp->dict_watchers[i] = NULL;
