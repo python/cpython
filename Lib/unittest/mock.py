7e742379af27758a48a464b4f51eecabdc7f816a
Miss Islington (bot) <31488909+miss-islington@users.noreply.github.com>
2022-11-19 02:10:42 -0800

    [3.11] gh-98086: Now ``patch.dict`` can decorate async functions (GH-98095) (#99365)
    gh-98086: Now ``patch.dict`` can decorate async functions (GH-98095)
    (cherry picked from commit 67b4d2772c5124b908f8ed9b13166a79bbeb88d2)
    
    Co-authored-by: Nikita Sobolev <mail@sobolevn.me>
    
    Co-authored-by: Nikita Sobolev <mail@sobolevn.me>

--- Lib/unittest/mock.py
+++ Lib/unittest/mock.py
@@ -1809,11 +1809,29 @@ class _patch_dict(object):
     def __call__(self, f):
         if isinstance(f, type):
             return self.decorate_class(f)
+        if inspect.iscoroutinefunction(f):
+            return self.decorate_async_callable(f)
+        return self.decorate_callable(f)
+
+
+    def decorate_callable(self, f):
         @wraps(f)
         def _inner(*args, **kw):
             self._patch_dict()
             try:
                 return f(*args, **kw)
+            finally:
+                self._unpatch_dict()
+
+        return _inner
+
+
+    def decorate_async_callable(self, f):
+        @wraps(f)
+        async def _inner(*args, **kw):
+            self._patch_dict()
+            try:
+                return await f(*args, **kw)
             finally:
                 self._unpatch_dict()
 
