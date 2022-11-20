920236c72cd7bf3a6a6f644a945a0ce318917ae5
Lysandros Nikolaou <lisandrosnik@gmail.com>
2022-11-20 18:11:02 +0100

    gh-99211: Point to except/except* on syntax errors when mixing them (GH-99215)
    Automerge-Triggered-By: GH:lysnikolaou

--- Lib/test/test_syntax.py
+++ Lib/test/test_syntax.py
@@ -2017,12 +2017,12 @@ class SyntaxTestCase(unittest.TestCase):
     def test_except_then_except_star(self):
         self._check_error("try: pass\nexcept ValueError: pass\nexcept* TypeError: pass",
                           r"cannot have both 'except' and 'except\*' on the same 'try'",
-                          lineno=1, end_lineno=1, offset=1, end_offset=4)
+                          lineno=3, end_lineno=3, offset=1, end_offset=8)
 
     def test_except_star_then_except(self):
         self._check_error("try: pass\nexcept* ValueError: pass\nexcept TypeError: pass",
                           r"cannot have both 'except' and 'except\*' on the same 'try'",
-                          lineno=1, end_lineno=1, offset=1, end_offset=4)
+                          lineno=3, end_lineno=3, offset=1, end_offset=7)
 
     def test_empty_line_after_linecont(self):
         # See issue-40847
