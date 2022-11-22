9ba7386130925fe2148bbf98d318d02cf7bfec5d
Victor Stinner <vstinner@python.org>
2022-11-22 14:22:22 +0100

    gh-99537: Use Py_SETREF() function in C code (#99656)
    Fix potential race condition in code patterns:
    
    * Replace "Py_DECREF(var); var = new;" with "Py_SETREF(var, new);"
    * Replace "Py_XDECREF(var); var = new;" with "Py_XSETREF(var, new);"
    * Replace "Py_CLEAR(var); var = new;" with "Py_XSETREF(var, new);"
    
    Other changes:
    
    * Replace "old = var; var = new; Py_DECREF(var)"
      with "Py_SETREF(var, new);"
    * Replace "old = var; var = new; Py_XDECREF(var)"
      with "Py_XSETREF(var, new);"
    * And remove the "old" variable.

--- Doc/includes/custom2.c
+++ Doc/includes/custom2.c
@@ -42,7 +42,7 @@ static int
 Custom_init(CustomObject *self, PyObject *args, PyObject *kwds)
 {
     static char *kwlist[] = {"first", "last", "number", NULL};
-    PyObject *first = NULL, *last = NULL, *tmp;
+    PyObject *first = NULL, *last = NULL;
 
     if (!PyArg_ParseTupleAndKeywords(args, kwds, "|OOi", kwlist,
                                      &first, &last,
@@ -50,14 +50,10 @@ Custom_init(CustomObject *self, PyObject *args, PyObject *kwds)
         return -1;
 
     if (first) {
-        tmp = self->first;
-        self->first = Py_NewRef(first);
-        Py_XDECREF(tmp);
+        Py_XSETREF(self->first, Py_NewRef(first));
     }
     if (last) {
-        tmp = self->last;
-        self->last = Py_NewRef(last);
-        Py_XDECREF(tmp);
+        Py_XSETREF(self->last, Py_NewRef(last));
     }
     return 0;
 }
