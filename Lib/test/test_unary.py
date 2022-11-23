9720240aec3a3975cc8ea3e0faaed66fa014d31c
Isaac-Lee <isaac.lee.3412@gmail.com>
2022-11-23 10:47:17 +0900

    Remove redundant test cases

--- Lib/test/test_unary.py
+++ Lib/test/test_unary.py
@@ -8,7 +8,6 @@ class UnaryOpTestCase(unittest.TestCase):
         self.assertTrue(-2 == 0 - 2)
         self.assertEqual(-0, 0)
         self.assertEqual(--2, 2)
-        self.assertTrue(-2 == 0 - 2)
         self.assertTrue(-2.0 == 0 - 2.0)
         self.assertTrue(-2j == 0 - 2j)
 
@@ -16,7 +15,6 @@ class UnaryOpTestCase(unittest.TestCase):
         self.assertEqual(+2, 2)
         self.assertEqual(+0, 0)
         self.assertEqual(++2, 2)
-        self.assertEqual(+2, 2)
         self.assertEqual(+2.0, 2.0)
         self.assertEqual(+2j, 2j)
 
