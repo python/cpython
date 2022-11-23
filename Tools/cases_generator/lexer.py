84f4ad897dcfa52750f8acc3b531cdc2c85002a8
Guido van Rossum <guido@python.org>
2022-11-22 16:04:57 -0800

    GH-98831: Add `macro` and `op` and their implementation to DSL (#99495)
    Newly supported interpreter definition syntax:
    - `op(NAME, (input_stack_effects -- output_stack_effects)) { ... }`
    - `macro(NAME) = OP1 + OP2;`
    
    Also some other random improvements:
    - Convert `WITH_EXCEPT_START` to use stack effects
    - Fix lexer to balk at unrecognized characters, e.g. `@`
    - Fix moved output names; support object pointers in cache
    - Introduce `error()` method to print errors
    - Introduce read_uint16(p) as equivalent to `*p`
    
    Co-authored-by: Brandt Bucher <brandtbucher@gmail.com>

--- Tools/cases_generator/lexer.py
+++ Tools/cases_generator/lexer.py
@@ -112,7 +112,8 @@ comment_re = r'//.*|/\*([^*]|\*[^/])*\*/'
 COMMENT = 'COMMENT'
 
 newline = r"\n"
-matcher = re.compile(choice(id_re, number_re, str_re, char, newline, macro, comment_re, *operators.values()))
+invalid = r"\S"  # A single non-space character that's not caught by any of the other patterns
+matcher = re.compile(choice(id_re, number_re, str_re, char, newline, macro, comment_re, *operators.values(), invalid))
 letter = re.compile(r'[a-zA-Z_]')
 
 kwds = (
@@ -177,7 +178,6 @@ class Token:
 
 def tokenize(src, line=1, filename=None):
     linestart = -1
-    # TODO: finditer() skips over unrecognized characters, e.g. '@'
     for m in matcher.finditer(src):
         start, end = m.span()
         text = m.group(0)
