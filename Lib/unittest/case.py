36490133fa5995e2021d9801c14a13db6e253fda
Giampaolo Rodola <g.rodola@gmail.com>
2022-11-24 19:41:37 +0100

    calculate duration time after cleanup methods

--- Lib/unittest/case.py
+++ Lib/unittest/case.py
@@ -635,8 +635,8 @@ class TestCase(object):
                     outcome.expecting_failure = False
                     with outcome.testPartExecutor(self):
                         self._callTearDown()
-                self._addDuration(result, (time.perf_counter() - start_time))
                 self.doCleanups()
+                self._addDuration(result, (time.perf_counter() - start_time))
 
                 if outcome.success:
                     if expecting_failure:
