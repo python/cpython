087895c645157587d53f1069fa9658008f65af3a
Abdul Rafey <abdul.rafey@arbisoft.com>
2022-11-25 22:58:36 +0500

    assertion msg fixed :hammer:

--- Lib/test/test_argparse.py
+++ Lib/test/test_argparse.py
@@ -2193,9 +2193,11 @@ class TestAddSubparsers(TestCase):
         subparsers.add_parser('bar')
         with self.assertRaises(ArgumentParserError) as excinfo:
             parser.parse_args(('baz',))
-        self.assertRegex(
+        self.assertEqual(
             excinfo.exception.stderr,
-            r"error: argument {foo,bar}: invalid choice: 'baz', maybe you meant 'bar'? (choose from 'foo', 'bar')"
+            "usage: __main__.py [-h] {foo,bar} ...\\n__main__.py: error: argument"
+            " {foo,bar}: invalid choice: 'baz', maybe you meant 'bar'?"
+            " (choose from 'foo', 'bar')\\n"
         )
 
     def test_optional_subparsers(self):
