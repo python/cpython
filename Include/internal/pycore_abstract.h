45d4abf36fb4167f0a06e5d33fc4733941c82b64
Pieter Eendebak <pieter.eendebak@gmail.com>
2022-11-26 22:35:36 +0100

    change static inline to macro

--- Include/internal/pycore_abstract.h
+++ Include/internal/pycore_abstract.h
@@ -8,6 +8,16 @@ extern "C" {
 #  error "this header requires Py_BUILD_CORE define"
 #endif
 
+/* Return 1 if the specified index is outside the range[0, limit) */
+#define valid_index(index, limit) ((size_t)index < (size_t)limit)
+    /* The cast to size_t lets us use just a single comparison
+        to check whether index is in the range: 0 <= index < limit.
+
+        See:  Section 14.2 "Bounds Checking" in the Agner Fog
+        optimization manual found at:
+        https://www.agner.org/optimize/optimizing_cpp.pdf
+    */
+
 // Fast inlined version of PyIndex_Check()
 static inline int
 _PyIndex_Check(PyObject *obj)
