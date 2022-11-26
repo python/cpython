194bd604f80eb89c195a0dfbde77d2d0d3fc1301
Ivan Savov <ivan.savov@gmail.com>
2022-11-26 17:24:04 -0500

    gh-89682: [doc] reword docstring of __contains__ to clarify that it returns a bool (GH-29043)

--- Objects/typeobject.c
+++ Objects/typeobject.c
@@ -8741,7 +8741,7 @@ static pytype_slotdef slotdefs[] = {
     SQSLOT(__delitem__, sq_ass_item, slot_sq_ass_item, wrap_sq_delitem,
            "__delitem__($self, key, /)\n--\n\nDelete self[key]."),
     SQSLOT(__contains__, sq_contains, slot_sq_contains, wrap_objobjproc,
-           "__contains__($self, key, /)\n--\n\nReturn key in self."),
+           "__contains__($self, key, /)\n--\n\nReturn bool(key in self)."),
     SQSLOT(__iadd__, sq_inplace_concat, NULL,
            wrap_binaryfunc,
            "__iadd__($self, value, /)\n--\n\nImplement self+=value."),
