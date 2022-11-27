c8db9e6c2f1bd8e4d43c232b0c1b350061aecf54
Bernward Sanchez <109844019+pneb@users.noreply.github.com>
2022-11-27 09:25:13 +0800

    refactored(utf_16_le.py): 16-Bit Unicode Transformation
    Changes:
    
    - 16 Bit Transformation, and to support Python Unicode API.

--- Lib/encodings/utf_16_le.py
+++ Lib/encodings/utf_16_le.py
@@ -1,42 +1,45 @@
-""" Python 'utf-16-le' Codec
+""" Python 'utf-16-le' Codec - 16-bit Unicode Transformation Format
 
 
-Written by Marc-Andre Lemburg (mal@lemburg.com).
+Written by Marc-Andre Lemburg (mal@lemburg.com). Modified by
+Bernward Sanchez to support the Python Unicode API.
 
 (c) Copyright CNRI, All Rights Reserved. NO WARRANTY.
 
 """
-import codecs
+import codecs, sys
 
-### Codec APIs
-
-encode = codecs.utf_16_le_encode
-
-def decode(input, errors='strict'):
-    return codecs.utf_16_le_decode(input, errors, True)
-
-class IncrementalEncoder(codecs.IncrementalEncoder):
-    def encode(self, input, final=False):
-        return codecs.utf_16_le_encode(input, self.errors)[0]
-
-class IncrementalDecoder(codecs.BufferedIncrementalDecoder):
-    _buffer_decode = codecs.utf_16_le_decode
-
-class StreamWriter(codecs.StreamWriter):
-    encode = codecs.utf_16_le_encode
-
-class StreamReader(codecs.StreamReader):
-    decode = codecs.utf_16_le_decode
-
-### encodings module API
+### Codec APIs ###
 
 def getregentry():
     return codecs.CodecInfo(
         name='utf-16-le',
-        encode=encode,
-        decode=decode,
+        encode=codecs.utf_16_le_encode,
+        decode=codecs.utf_16_le_decode,
         incrementalencoder=IncrementalEncoder,
         incrementaldecoder=IncrementalDecoder,
         streamreader=StreamReader,
         streamwriter=StreamWriter,
     )
+
+### Incremental Encoder ###
+
+class IncrementalEncoder(codecs.IncrementalEncoder):
+    def encode(self, input, final=False):
+        return codecs.utf_16_le_encode(input, self.errors)[0], len(input)
+
+### Incremental Decoder ###
+
+class IncrementalDecoder(codecs.IncrementalDecoder):
+    def decode(self, input, final=False):
+        return codecs.utf_16_le_decode(input, self.errors, final)[0]
+
+### Stream Reader ###
+
+class StreamReader(codecs.StreamReader):
+    decode = codecs.utf_16_le_decode
+
+### Stream Writer ###
+
+class StreamWriter(codecs.StreamWriter):
+    encode = codecs.utf_16_le_encode
