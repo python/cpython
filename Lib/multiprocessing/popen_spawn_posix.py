88056fe4676e0504ebb4a170426f88657de4cbbb
Géry Ogam <gery.ogam@gmail.com>
2022-11-30 17:06:31 +0100

    Revert "Update popen_spawn_posix.py"
    This reverts commit d628fc1bf59fcf1e82a4a6aa912c0cbcf8f4381f.

--- Lib/multiprocessing/popen_spawn_posix.py
+++ Lib/multiprocessing/popen_spawn_posix.py
@@ -65,7 +65,7 @@ class Popen(popen_fork.Popen):
             for fd in (parent_r, parent_w):
                 if fd is not None:
                     fds_to_close.append(fd)
-            self.finalizer = util.Finalize(self, util.close_fds, fds_to_close)._key
+            self.finalizer = util.Finalize(self, util.close_fds, fds_to_close)
 
             for fd in (child_r, child_w):
                 if fd is not None:
