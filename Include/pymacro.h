cdd52ce3f7427f94e9199dda72da56f8da7c9e6b
Victor Stinner <vstinner@python.org>
2022-12-03 14:43:41 +0100

    Update Include/pymacro.h

--- Include/pymacro.h
+++ Include/pymacro.h
@@ -15,7 +15,7 @@
 // MSVC makes static_assert a keyword, contrary to the C standard.
 //
 // In C++ 11 static_assert is a keyword, redefining is undefined behaviour.
-// So only define if building as C, not C++.
+// So only define if building as C (if __STDC_VERSION__ is defined), not C++.
 #if !defined(static_assert) && defined(__GNUC__) \
      && defined(__STDC_VERSION__) && __STDC_VERSION__ >= 201112L
 #  define static_assert _Static_assert
