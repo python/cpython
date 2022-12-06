fe6e8d26c1cbba9a0f44a204367415101da8b166
Eric Snow <ericsnowcurrently@gmail.com>
2022-12-06 11:08:06 -0700

    Add _PyRuntimeState.os.

---
+++ Include/internal/pycore_os.h
@@ -0,0 +1,22 @@
+#ifndef Py_INTERNAL_OS_H
+#define Py_INTERNAL_OS_H
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifndef Py_BUILD_CORE
+#  error "this header requires Py_BUILD_CORE define"
+#endif
+
+
+struct _os_runtime_state {
+    int _not_used;
+};
+
+#define _OS_RUNTIME_INIT {0}
+
+
+#ifdef __cplusplus
+}
+#endif
+#endif /* !Py_INTERNAL_OS_H */
