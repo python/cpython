a75be9b1de163ae3c175cd5c76df551ef4755517
Eric Snow <ericsnowcurrently@gmail.com>
2022-12-05 18:13:11 -0700

    Move trampoline state to _PyRuntimeState.

--- Include/internal/pycore_ceval_state.h
+++ Include/internal/pycore_ceval_state.h
@@ -13,7 +13,37 @@ extern "C" {
 #include "pycore_gil.h"             // struct _gil_runtime_state
 
 
+typedef enum {
+    PERF_STATUS_FAILED = -1,  // Perf trampoline is in an invalid state
+    PERF_STATUS_NO_INIT = 0,  // Perf trampoline is not initialized
+    PERF_STATUS_OK = 1,       // Perf trampoline is ready to be executed
+} perf_status_t;
+
+
+#ifdef PY_HAVE_PERF_TRAMPOLINE
+struct code_arena_st;
+
+struct trampoline_api_st {
+    void* (*init_state)(void);
+    void (*write_state)(void* state, const void *code_addr,
+                        unsigned int code_size, PyCodeObject* code);
+    int (*free_state)(void* state);
+    void *state;
+};
+#endif
+
 struct _ceval_runtime_state {
+    struct {
+#ifdef PY_HAVE_PERF_TRAMPOLINE
+        perf_status_t status;
+        Py_ssize_t extra_code_index;
+        struct code_arena_st *code_arena;
+        struct trampoline_api_st trampoline_api;
+        FILE *map_file;
+#else
+        int _not_used;
+#endif
+    } perf;
     /* Request for checking signals. It is shared by all interpreters (see
        bpo-40513). Any thread of any interpreter can receive a signal, but only
        the main thread of the main interpreter can handle signals: see
@@ -21,6 +51,16 @@ struct _ceval_runtime_state {
     _Py_atomic_int signals_pending;
     struct _gil_runtime_state gil;
 };
+
+#ifdef PY_HAVE_PERF_TRAMPOLINE
+# define _PyEval_RUNTIME_PERF_INIT \
+    { \
+        .status = PERF_STATUS_NO_INIT, \
+        .extra_code_index = -1, \
+    }
+#else
+# define _PyEval_RUNTIME_PERF_INIT {0}
+#endif
 
 
 struct _pending_calls {
