2997f3913a7f644352a1d4013588451837d2ac58
Miss Islington (bot) <31488909+miss-islington@users.noreply.github.com>
2022-12-07 09:26:11 -0800

    gh-83035: handle decorator with nested parens in inspect.getsource (GH-99654)
    (cherry picked from commit 68e41295b8611a990de68f15c89f1eb3dea51867)
    
    Co-authored-by: Carl Meyer <carl@oddbird.net>

--- Lib/inspect.py
+++ Lib/inspect.py
@@ -1175,7 +1175,6 @@ class BlockFinder:
         self.started = False
         self.passline = False
         self.indecorator = False
-        self.decoratorhasargs = False
         self.last = 1
         self.body_col0 = None
 
@@ -1190,13 +1189,6 @@ class BlockFinder:
                     self.islambda = True
                 self.started = True
             self.passline = True    # skip to the end of the line
-        elif token == "(":
-            if self.indecorator:
-                self.decoratorhasargs = True
-        elif token == ")":
-            if self.indecorator:
-                self.indecorator = False
-                self.decoratorhasargs = False
         elif type == tokenize.NEWLINE:
             self.passline = False   # stop skipping when a NEWLINE is seen
             self.last = srowcol[0]
@@ -1204,7 +1196,7 @@ class BlockFinder:
                 raise EndOfBlock
             # hitting a NEWLINE when in a decorator without args
             # ends the decorator
-            if self.indecorator and not self.decoratorhasargs:
+            if self.indecorator:
                 self.indecorator = False
         elif self.passline:
             pass
