0e2c7839bd297ad284fd07bf3736f722b87175df
Miss Islington (bot) <31488909+miss-islington@users.noreply.github.com>
2022-12-08 01:02:28 -0800

    [3.10] bpo-44817: Ignore additional errors in ntpath.realpath (GH-27574) (GH-100023)
    (cherry picked from commit 124ecd657646f808d1d3282c37ee19aae6bcb47f)
    
    Co-authored-by: Michael Förderer <michael.foerderer@gmx.de>

--- Lib/ntpath.py
+++ Lib/ntpath.py
@@ -625,12 +625,15 @@ else:
         # 21: ERROR_NOT_READY (implies drive with no media)
         # 32: ERROR_SHARING_VIOLATION (probably an NTFS paging file)
         # 50: ERROR_NOT_SUPPORTED
+        # 53: ERROR_BAD_NETPATH
+        # 65: ERROR_NETWORK_ACCESS_DENIED
         # 67: ERROR_BAD_NET_NAME (implies remote server unavailable)
         # 87: ERROR_INVALID_PARAMETER
         # 123: ERROR_INVALID_NAME
+        # 161: ERROR_BAD_PATHNAME
         # 1920: ERROR_CANT_ACCESS_FILE
         # 1921: ERROR_CANT_RESOLVE_FILENAME (implies unfollowable symlink)
-        allowed_winerror = 1, 2, 3, 5, 21, 32, 50, 67, 87, 123, 1920, 1921
+        allowed_winerror = 1, 2, 3, 5, 21, 32, 50, 53, 65, 67, 87, 123, 161, 1920, 1921
 
         # Non-strict algorithm is to find as much of the target directory
         # as we can and join the rest.
