1ddf3a96f3845f3353bd9b903ec7607f724166ad
Mark Shannon <mark@hotpy.org>
2022-12-15 09:45:03 +0000

    Move stats for the method cache into the `Py_STAT` machinery (GH-100255)

--- Include/pystats.h
+++ Include/pystats.h
@@ -65,7 +65,14 @@ typedef struct _object_stats {
     uint64_t dict_materialized_new_key;
     uint64_t dict_materialized_too_big;
     uint64_t dict_materialized_str_subclass;
+    uint64_t type_cache_hits;
+    uint64_t type_cache_misses;
+    uint64_t type_cache_dunder_hits;
+    uint64_t type_cache_dunder_misses;
+    uint64_t type_cache_collisions;
 } ObjectStats;
+
+#
 
 typedef struct _stats {
     OpcodeStats opcode_stats[256];
