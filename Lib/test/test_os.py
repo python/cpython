8d2befbf48064809cafbfa762b33e45c02fe57b2
Jon Burdo <jon@jonburdo.com>
2022-12-20 09:40:35 -0500

    run test_walk_above_recursion_limit for fwalk

--- Lib/test/test_os.py
+++ Lib/test/test_os.py
@@ -1586,8 +1586,6 @@ class FwalkTests(WalkTests):
 
     # fwalk() keeps file descriptors open
     test_walk_many_open_files = None
-    # fwalk() still uses recursion
-    test_walk_above_recursion_limit = None
 
 
 class BytesWalkTests(WalkTests):
