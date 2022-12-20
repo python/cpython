4f2de627040f6c5b7fec5088d1c1be556cada890
Stanislav Zmiev <szmiev2000@gmail.com>
2022-12-20 15:54:06 +0400

    Remove unused import

--- Lib/pathlib.py
+++ Lib/pathlib.py
@@ -19,7 +19,6 @@ from errno import ENOENT, ENOTDIR, EBADF, ELOOP
 from operator import attrgetter
 from stat import S_ISDIR, S_ISLNK, S_ISREG, S_ISSOCK, S_ISBLK, S_ISCHR, S_ISFIFO
 from urllib.parse import quote_from_bytes as urlquote_from_bytes
-from collections import deque
 
 
 __all__ = [
