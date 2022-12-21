3a9d856962939c8f6593d479d80e419cc8c11792
Dominic Socular <BBH@awsl.rip>
2022-12-21 21:25:04 +0800

    gh-100374: Fixed a bug in socket.getfqdn() (gh-100375) (cherry picked from commit 12be23cf3c1301be2c6b8fd4cb2cd35a567d2ea2)
    Co-authored-by: Dominic Socular <BBH@awsl.rip>

--- Lib/socket.py
+++ Lib/socket.py
@@ -783,11 +783,11 @@ def getfqdn(name=''):
 
     First the hostname returned by gethostbyaddr() is checked, then
     possibly existing aliases. In case no FQDN is available and `name`
-    was given, it is returned unchanged. If `name` was empty or '0.0.0.0',
+    was given, it is returned unchanged. If `name` was empty, '0.0.0.0' or '::',
     hostname from gethostname() is returned.
     """
     name = name.strip()
-    if not name or name == '0.0.0.0':
+    if not name or name in ('0.0.0.0', '::'):
         name = gethostname()
     try:
         hostname, aliases, ipaddrs = gethostbyaddr(name)
