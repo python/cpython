7997d57ca06d3741642264c73b73ed863e489d6f
Eddie Elizondo <eelizondo@fb.com>
2022-12-20 20:10:06 -0800

    Windows fix

--- Include/cpython/unicodeobject.h
+++ Include/cpython/unicodeobject.h
@@ -135,7 +135,7 @@ typedef struct {
         unsigned int ascii:1;
         /* Padding to ensure that PyUnicode_DATA() is always aligned to
            4 bytes (see issue #19537 on m68k). */
-        unsigned int :26;
+        unsigned int :25;
     } state;
 } PyASCIIObject;
 
--- Include/internal/pycore_typeobject.h
+++ Include/internal/pycore_typeobject.h
@@ -35,8 +35,7 @@ struct type_cache_entry {
     PyObject *value;       // borrowed reference or NULL
 };
 
-// TODO(eelizondo): Windows builds fail with 12 temporarily remove
-#define MCACHE_SIZE_EXP 11
+#define MCACHE_SIZE_EXP 12
 
 struct type_cache {
     struct type_cache_entry hashtable[1 << MCACHE_SIZE_EXP];
