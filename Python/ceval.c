dea531ce4234eeb68f457bc4c7b5c9b567b05ad3
Irit Katriel <iritkatriel@yahoo.com>
2022-12-23 19:27:02 +0000

    Revert "set VERBOSE to 0"
    This reverts commit 107c557b6c5b6117cf44e38641d93324d44d3fa1.

--- Python/ceval.c
+++ Python/ceval.c
@@ -746,7 +746,7 @@ GETITEM(PyObject *v, Py_ssize_t i) {
 
 /* Code access macros */
 
-#define VERBOSE 0
+#define VERBOSE 1
 
 /* The integer overflow is checked by an assertion below. */
 #define INSTR_OFFSET() ((int)(next_instr - _PyCode_CODE(frame->f_code)))
@@ -1179,6 +1179,7 @@ _PyEval_EvalFrameDefault(PyThreadState *tstate, _PyInterpreterFrame *frame, int 
         if(!_PyErr_Occurred(tstate)) PyObject_Print(frame->f_code->co_name, stderr, 0); \
         fprintf(stderr, "\n"); \
     } \
+    /* next_instr = frame->prev_instr + 1; */ \
     next_instr = frame->prev_instr + 1; \
     stack_pointer = _PyFrame_GetStackPointer(frame); \
     /* Set stackdepth to -1. \
