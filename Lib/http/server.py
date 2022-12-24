ecbf136702267857b261a81b64f234965b9de913
Miss Islington (bot) <31488909+miss-islington@users.noreply.github.com>
2022-12-24 12:29:21 -0800

    [3.10] gh-100474: Fix handling of dirs named index.html in http.server (GH-100504)
    Co-authored-by: James Frost <git@frost.cx>

--- Lib/http/server.py
+++ Lib/http/server.py
@@ -709,7 +709,7 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
                 return None
             for index in "index.html", "index.htm":
                 index = os.path.join(path, index)
-                if os.path.exists(index):
+                if os.path.isfile(index):
                     path = index
                     break
             else:
