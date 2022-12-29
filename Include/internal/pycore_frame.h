04b77acc91c0214a1d4a1deb4ef76b327b2a2c6b
Brandt Bucher <brandtbucher@microsoft.com>
2022-12-29 04:41:22 -0800

    Add _PyFrame_GetComplete/_PyThreadState_GetFrame

--- Include/internal/pycore_frame.h
+++ Include/internal/pycore_frame.h
@@ -153,6 +153,21 @@ _PyFrame_IsIncomplete(_PyInterpreterFrame *frame)
     frame->prev_instr < _PyCode_CODE(frame->f_code) + frame->f_code->_co_firsttraceable;
 }
 
+static inline _PyInterpreterFrame *
+_PyFrame_GetComplete(_PyInterpreterFrame *frame)
+{
+    while (frame && _PyFrame_IsIncomplete(frame)) {
+        frame = frame->previous;
+    }
+    return frame;
+}
+
+static inline _PyInterpreterFrame *
+_PyThreadState_GetFrame(PyThreadState *tstate)
+{
+    return _PyFrame_GetComplete(tstate->cframe->current_frame);
+}
+
 /* For use by _PyFrame_GetFrameObject
   Do not call directly. */
 PyFrameObject *
