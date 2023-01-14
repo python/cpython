b8421a1e6fcdb497081a84193dd2a8d64bbd9d19
Pradyun Gedam <pradyunsg@users.noreply.github.com>
2023-01-14 22:35:17 +0000

    Update test_cppext to stop invoking setuptools-based script as a CLI
    This allows the "package" to be built using regular Python packaging
    workflows instead of relying on `setuptools` being present in the
    virtual environment.

--- Lib/test/test_cppext.py
+++ Lib/test/test_cppext.py
@@ -1,6 +1,10 @@
 # gh-91321: Build a basic C++ test extension to check that the Python C API is
 # compatible with C++ and does not emit C++ compiler warnings.
 import os.path
+try:
+    import ssl
+except ImportError:
+    ssl = None
 import sys
 import unittest
 import subprocess
@@ -12,7 +16,7 @@ from test.support import os_helper
 MS_WINDOWS = (sys.platform == 'win32')
 
 
-SETUP_TESTCPPEXT = support.findfile('setup_testcppext.py')
+PKG_CPPEXTDATA = os.path.dirname(support.findfile('setup.py', subdir="cppextdata"))
 
 
 @support.requires_subprocess()
@@ -31,6 +35,8 @@ class TestCPPExt(unittest.TestCase):
     @unittest.skipIf(
         '-fsanitize' in (sysconfig.get_config_var('PY_CFLAGS') or ''),
         'test does not work with analyzing builds')
+    # the test uses pip which needs a TLS connection to PyPI
+    @unittest.skipIf(ssl is None, 'No ssl module')
     # the test uses venv+pip: skip if it's not available
     @support.requires_venv_with_pip()
     def check_build(self, std_cpp03, extension_name):
@@ -76,14 +82,9 @@ class TestCPPExt(unittest.TestCase):
                     self.fail(
                         f"{operation} failed with exit code {proc.returncode}")
 
-        # Build the C++ extension
+        # Build and install the C++ extension
         cmd = [python, '-X', 'dev',
-               SETUP_TESTCPPEXT, 'build_ext', '--verbose']
-        run_cmd('Build', cmd)
-
-        # Install the C++ extension
-        cmd = [python, '-X', 'dev',
-               SETUP_TESTCPPEXT, 'install']
+               '-m', 'pip', 'install', PKG_CPPEXTDATA]
         run_cmd('Install', cmd)
 
         # Do a reference run. Until we test that running python
