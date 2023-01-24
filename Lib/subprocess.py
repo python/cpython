8dab7f110bcb8646f2b69fbd1192d314880bb291
Oleg Iarygin <oleg@arhadthedev.net>
2023-01-24 19:49:25 +0300

    Address the review-2
    Co-authored-by: Eryk Sun <eryksun@gmail.com>

--- Lib/subprocess.py
+++ Lib/subprocess.py
@@ -1482,16 +1482,20 @@ class Popen:
                 startupinfo.wShowWindow = _winapi.SW_HIDE
                 # gh-101283: with no full path, Windows looks into a
                 # current directory first so no plain "cmd.exe".
-                system_drive = os.environ.get('SystemDrive') or 'C:'
-                system_root = os.environ.get('SystemRoot') or os.path.join(
-                    system_drive, 'Windows')
-                comspec = os.environ.get('ComSpec') or os.path.join(
-                    system_root, 'System32', 'cmd.exe')
-                if not os.path.isfile(comspec):
-                    # Windows is installed into a non-standard location
-                    # or the system environment variables are broken.
-                    # It's highly unlikely and we cannot help here.
-                    comspec = "cmd.exe"
+                comspec = os.environ.get('ComSpec')
+                if not comspec:
+                    system_drive = os.environ.get('SystemDrive') or 'C:'
+                    system_root = os.environ.get('SystemRoot') or os.path.join(
+                        system_drive, 'Windows')
+                    comspec = os.path.join(system_root, 'System32', 'cmd.exe')
+                    if not os.path.isfile(comspec):
+                        # cmd.exe is missing, or the system environment
+                        # variables are broken, or they're undefined and the
+                        # system is installed into a non-standard location.
+                        # This is highly unlikely, and we cannot help here.
+                        comspec = 'cmd.exe'
+                if not executable and os.path.isabs(comspec):
+                    executable = comspec
 
                 args = '{} /c "{}"'.format (comspec, args)
 
