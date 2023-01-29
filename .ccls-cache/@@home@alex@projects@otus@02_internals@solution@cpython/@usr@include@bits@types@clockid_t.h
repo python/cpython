e81a5473092128c33de8272af6ea4ee547112029
rekukha.aa <aarekuha@gmail.com>
2023-01-29 13:52:32 +0500

    remove cache

--- .ccls-cache/@@home@alex@projects@otus@02_internals@solution@cpython/@usr@include@bits@types@clockid_t.h
+++
@@ -1,9 +0,0 @@
-#ifndef __clockid_t_defined
-#define __clockid_t_defined 1
-
-#include <bits/types.h>
-
-/* Clock ID used in clock and timer functions.  */
-typedef __clockid_t clockid_t;
-
-#endif
--- .ccls-cache/@@home@alex@projects@otus@02_internals@solution@cpython/@usr@include@bits@types@timer_t.h
+++
@@ -1,9 +0,0 @@
-#ifndef __timer_t_defined
-#define __timer_t_defined 1
-
-#include <bits/types.h>
-
-/* Timer ID returned by `timer_create'.  */
-typedef __timer_t timer_t;
-
-#endif
