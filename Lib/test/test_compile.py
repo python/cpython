ce2f9f6d994a31bc4eea4e9ff8730b061ed2404c
Dong-hee Na <donghee.na@python.org>
2023-01-30 23:51:21 +0900

    refactor test

--- Lib/test/test_compile.py
+++ Lib/test/test_compile.py
@@ -847,14 +847,10 @@ if 1:
             self.assertEqual('RETURN_VALUE', opcodes[2].opname)
 
     def test_unloop_break_continue(self):
-        source_template = "with object() as obj:\n    {}"
-        sources = []
         for stmt in ('break', 'continue'):
-            sources.append((stmt, source_template.format(stmt)))
-
-        for stmt, source in sources:
             with self.subTest(stmt=stmt):
                 with self.assertRaises(SyntaxError) as err_ctx:
+                    source = f"with object() as obj:\n    {stmt}"
                     compile(source, f"<unloop_{stmt}>", "exec")
                 exc = err_ctx.exception
                 self.assertEqual(exc.lineno, 2)
