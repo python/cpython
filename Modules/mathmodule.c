d1daa27a72dd61bc96f0a90dc31818345d2f8a03
Mark Dickinson <dickinsm@gmail.com>
2023-02-05 12:46:10 +0000

    Revert "gh-89381: Fix invalid signatures of math/cmath.log (#101404)"
    This reverts commit 0ef92d979311ba82d4c41b22ef38e12e1b08b13d.

--- Modules/mathmodule.c
+++ Modules/mathmodule.c
@@ -2366,24 +2366,26 @@ loghelper(PyObject* arg, double (*func)(double))
 math.log
 
     x:    object
-    base: object = None
+    [
+    base: object(c_default="NULL") = math.e
+    ]
     /
 
 Return the logarithm of x to the given base.
 
-If the base is not specified or is None, returns the natural
-logarithm (base e) of x.
+If the base not specified, returns the natural logarithm (base e) of x.
 [clinic start generated code]*/
 
 static PyObject *
-math_log_impl(PyObject *module, PyObject *x, PyObject *base)
-/*[clinic end generated code: output=1dead263cbb1e854 input=ef032cc9837943e1]*/
+math_log_impl(PyObject *module, PyObject *x, int group_right_1,
+              PyObject *base)
+/*[clinic end generated code: output=7b5a39e526b73fc9 input=0f62d5726cbfebbd]*/
 {
     PyObject *num, *den;
     PyObject *ans;
 
     num = loghelper(x, m_log);
-    if (num == NULL || base == Py_None)
+    if (num == NULL || base == NULL)
         return num;
 
     den = loghelper(base, m_log);
