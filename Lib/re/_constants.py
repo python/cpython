5274810db159a8494a3c54d4b29577c3b9c35a17
Adam Chhina <amanschhina@gmail.com>
2023-02-08 01:20:01 -0500

    GH-83162: Update _compiler and _constants to new exception name.

--- Lib/re/_constants.py
+++ Lib/re/_constants.py
@@ -17,10 +17,10 @@ MAGIC = 20221023
 
 from _sre import MAXREPEAT, MAXGROUPS
 
-# SRE standard exception (access as sre.error)
+# SRE standard exception (access as sre.ReCompileError)
 # should this really be here?
 
-class error(Exception):
+class ReCompileError(Exception):
     """Exception raised for invalid regular expressions.
 
     Attributes:
@@ -52,6 +52,9 @@ class error(Exception):
             self.lineno = self.colno = None
         super().__init__(msg)
 
+
+# Backward compatibility after renaming in 3.12
+error = ReCompileError
 
 class _NamedIntConstant(int):
     def __new__(cls, value, name):
