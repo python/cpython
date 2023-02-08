b6938950f0996dd6af0f7efcd9b7c2e21e3d22db
Erlend E. Aasland <erlend.aasland@protonmail.com>
2023-02-08 18:21:03 +0100

    gh-101698: Deprecate using named placeholders with sequences

--- Modules/_sqlite/cursor.c
+++ Modules/_sqlite/cursor.c
@@ -662,6 +662,17 @@ bind_parameters(pysqlite_state *state, pysqlite_Statement *self,
             return;
         }
         for (i = 0; i < num_params; i++) {
+            const char *name = sqlite3_bind_parameter_name(self->st, i+1);
+            if (name != NULL) {
+                PyErr_WarnFormat(PyExc_DeprecationWarning, 1,
+                                 "Binding %d ('%s') is a named parameter, "
+                                 "but you supplied a sequence which requires "
+                                 "nameless (qmark) placeholders. "
+                                 "Starting with Python 3.14 an "
+                                 "sqlite3.ProgrammingError will be raised.",
+                                 i+1, name);
+            }
+
             if (PyTuple_CheckExact(parameters)) {
                 PyObject *item = PyTuple_GET_ITEM(parameters, i);
                 current_param = Py_NewRef(item);
