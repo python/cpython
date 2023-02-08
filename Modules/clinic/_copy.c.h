c556530a8a875398943afec563f4cca1c08e7663
Pieter Eendebak <pieter.eendebak@gmail.com>
2023-02-08 14:12:42 +0100

    ac

--- Modules/clinic/_copy.c.h
+++ Modules/clinic/_copy.c.h
@@ -3,7 +3,7 @@ preserve
 [clinic start generated code]*/
 
 #if defined(Py_BUILD_CORE) && !defined(Py_BUILD_CORE_MODULE)
-#  include "pycore_gc.h"
+#  include "pycore_gc.h"            // PyGC_Head
 #  include "pycore_runtime.h"       // _Py_ID()
 #endif
 
@@ -16,6 +16,8 @@ PyDoc_STRVAR(_copy_deepcopy__doc__,
 "\n"
 "See the documentation for the copy module for details.");
 
+#define _COPY_DEEPCOPY_METHODDEF    \
+    {"deepcopy", _PyCFunction_CAST(_copy_deepcopy), METH_FASTCALL, _copy_deepcopy__doc__},
 
 static PyObject *
 _copy_deepcopy_impl(PyObject *module, PyObject *x, PyObject *memo);
@@ -23,15 +25,22 @@ _copy_deepcopy_impl(PyObject *module, PyObject *x, PyObject *memo);
 static PyObject *
 _copy_deepcopy(PyObject *module, PyObject *const *args, Py_ssize_t nargs)
 {
+    PyObject *return_value = NULL;
+    PyObject *x;
     PyObject *memo = Py_None;
 
     if (!_PyArg_CheckPositional("deepcopy", nargs, 1, 2)) {
-        return NULL;
+        goto exit;
     }
-    if (nargs == 2) {
-        memo = args[1];
+    x = args[0];
+    if (nargs < 2) {
+        goto skip_optional;
     }
-    return _copy_deepcopy_impl(module, args[0], memo);
+    memo = args[1];
+skip_optional:
+    return_value = _copy_deepcopy_impl(module, x, memo);
 
+exit:
+    return return_value;
 }
-/*[clinic end generated code: output=5af3e3be221c0b3e input=a9049054013a1b77]*/
+/*[clinic end generated code: output=ab88e7f79337ebab input=a9049054013a1b77]*/
