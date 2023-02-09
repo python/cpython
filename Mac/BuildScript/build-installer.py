f42aca4d8d3af9243b5b3ef8f9982e0cb8380194
Gregory P. Smith <greg@krypto.org>
2023-02-09 09:40:51 -0800

    gh-101726: Update the OpenSSL version to 1.1.1t (GH-101727)
    Fixes CVE-2023-0286 (High) and a couple of Medium security issues.
    https://www.openssl.org/news/secadv/20230207.txt

--- Mac/BuildScript/build-installer.py
+++ Mac/BuildScript/build-installer.py
@@ -242,9 +242,9 @@ def library_recipes():
 
     result.extend([
           dict(
-              name="OpenSSL 1.1.1n",
-              url="https://www.openssl.org/source/openssl-1.1.1n.tar.gz",
-              checksum='2aad5635f9bb338bc2c6b7d19cbc9676',
+              name="OpenSSL 1.1.1t",
+              url="https://www.openssl.org/source/openssl-1.1.1t.tar.gz",
+              checksum='8dee9b24bdb1dcbf0c3d1e9b02fb8f6bf22165e807f45adeb7c9677536859d3b',
               buildrecipe=build_universal_openssl,
               configure=None,
               install=None,
