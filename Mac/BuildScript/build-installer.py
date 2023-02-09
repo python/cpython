5e05ca7367e9da70adfd622aeb351cbcfe242b84
Ned Deily <nad@python.org>
2023-02-09 14:37:51 -0500

    Older versions of build-installer.py require md5 checksums

--- Mac/BuildScript/build-installer.py
+++ Mac/BuildScript/build-installer.py
@@ -246,7 +246,7 @@ def library_recipes():
           dict(
               name="OpenSSL 1.1.1t",
               url="https://www.openssl.org/source/openssl-1.1.1t.tar.gz",
-              checksum='8dee9b24bdb1dcbf0c3d1e9b02fb8f6bf22165e807f45adeb7c9677536859d3b',
+              checksum='1cfee919e0eac6be62c88c5ae8bcd91e',
               buildrecipe=build_universal_openssl,
               configure=None,
               install=None,
