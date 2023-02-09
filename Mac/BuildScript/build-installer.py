91196b6e7041b649250c46d0ad2641ae11767dec
Gregory P. Smith <greg@krypto.org>
2023-02-09 09:40:51 -0800

    gh-101726: Update the OpenSSL version to 1.1.1t (GH-101727)
    Fixes CVE-2023-0286 (High) and a couple of Medium security issues.
    https://www.openssl.org/news/secadv/20230207.txt

--- Mac/BuildScript/build-installer.py
+++ Mac/BuildScript/build-installer.py
@@ -246,9 +246,9 @@ def library_recipes():
 
     result.extend([
           dict(
-              name="OpenSSL 1.1.1s",
-              url="https://www.openssl.org/source/openssl-1.1.1s.tar.gz",
-              checksum='c5ac01e760ee6ff0dab61d6b2bbd30146724d063eb322180c6f18a6f74e4b6aa',
+              name="OpenSSL 1.1.1t",
+              url="https://www.openssl.org/source/openssl-1.1.1t.tar.gz",
+              checksum='8dee9b24bdb1dcbf0c3d1e9b02fb8f6bf22165e807f45adeb7c9677536859d3b',
               buildrecipe=build_universal_openssl,
               configure=None,
               install=None,
--- PCbuild/get_externals.bat
+++ PCbuild/get_externals.bat
@@ -53,7 +53,7 @@ echo.Fetching external libraries...
 set libraries=
 set libraries=%libraries%                                       bzip2-1.0.8
 if NOT "%IncludeLibffiSrc%"=="false" set libraries=%libraries%  libffi-3.3.0
-if NOT "%IncludeSSLSrc%"=="false" set libraries=%libraries%     openssl-1.1.1s
+if NOT "%IncludeSSLSrc%"=="false" set libraries=%libraries%     openssl-1.1.1t
 set libraries=%libraries%                                       sqlite-3.39.4.0
 if NOT "%IncludeTkinterSrc%"=="false" set libraries=%libraries% tcl-core-8.6.12.0
 if NOT "%IncludeTkinterSrc%"=="false" set libraries=%libraries% tk-8.6.12.0
@@ -76,9 +76,15 @@ for %%e in (%libraries%) do (
 echo.Fetching external binaries...
 
 set binaries=
+<<<<<<< HEAD
 if NOT "%IncludeLibffi%"=="false"  set binaries=%binaries% libffi-3.3.0
 if NOT "%IncludeSSL%"=="false"     set binaries=%binaries% openssl-bin-1.1.1s
 if NOT "%IncludeTkinter%"=="false" set binaries=%binaries% tcltk-8.6.12.0
+=======
+if NOT "%IncludeLibffi%"=="false"  set binaries=%binaries% libffi-3.4.3
+if NOT "%IncludeSSL%"=="false"     set binaries=%binaries% openssl-bin-1.1.1t
+if NOT "%IncludeTkinter%"=="false" set binaries=%binaries% tcltk-8.6.12.1
+>>>>>>> 6eb7d45f83 (gh-101726: Update the OpenSSL version to 1.1.1t (GH-101727))
 if NOT "%IncludeSSLSrc%"=="false"  set binaries=%binaries% nasm-2.11.06
 
 for %%b in (%binaries%) do (
