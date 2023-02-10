00cfab800ebb0da60e6adfee302027f90c4ab7ed
Bart Broere <mail@bartbroere.eu>
2023-02-10 18:35:38 +0000

    Resolve some tests

--- Lib/test/test_itertools.py
+++ Lib/test/test_itertools.py
@@ -2453,33 +2453,33 @@ class SizeofTest(unittest.TestCase):
     def test_product_sizeof(self):
         basesize = support.calcobjsize('3Pi')
         check = self.check_sizeof
-        check(product('ab', '12'), basesize + 2 * self.ssize_t)
-        check(product(*(('abc',) * 10)), basesize + 10 * self.ssize_t)
+        check(product('ab', '12'), basesize + 3 * self.ssize_t)
+        check(product(*(('abc',) * 10)), basesize + 11 * self.ssize_t)
 
     def test_combinations_sizeof(self):
         basesize = support.calcobjsize('3Pni')
         check = self.check_sizeof
-        check(combinations('abcd', 3), basesize + 3 * self.ssize_t)
-        check(combinations(range(10), 4), basesize + 4 * self.ssize_t)
+        check(combinations('abcd', 3), basesize + 4 * self.ssize_t)
+        check(combinations(range(10), 4), basesize + 5 * self.ssize_t)
 
     def test_combinations_with_replacement_sizeof(self):
         cwr = combinations_with_replacement
         basesize = support.calcobjsize('3Pni')
         check = self.check_sizeof
-        check(cwr('abcd', 3), basesize + 3 * self.ssize_t)
-        check(cwr(range(10), 4), basesize + 4 * self.ssize_t)
+        check(cwr('abcd', 3), basesize + 4 * self.ssize_t)
+        check(cwr(range(10), 4), basesize + 5 * self.ssize_t)
 
     def test_permutations_sizeof(self):
         basesize = support.calcobjsize('4Pni')
         check = self.check_sizeof
         check(permutations('abcd'),
-              basesize + 4 * self.ssize_t + 4 * self.ssize_t)
+              basesize + 4 * self.ssize_t + 5 * self.ssize_t)
         check(permutations('abcd', 3),
-              basesize + 4 * self.ssize_t + 3 * self.ssize_t)
+              basesize + 4 * self.ssize_t + 4 * self.ssize_t)
         check(permutations('abcde', 3),
-              basesize + 5 * self.ssize_t + 3 * self.ssize_t)
+              basesize + 5 * self.ssize_t + 4 * self.ssize_t)
         check(permutations(range(10), 4),
-              basesize + 10 * self.ssize_t + 4 * self.ssize_t)
+              basesize + 10 * self.ssize_t + 5 * self.ssize_t)
 
 
 def load_tests(loader, tests, pattern):
