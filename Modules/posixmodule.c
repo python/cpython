94b1a6686154dde3e224cfa66749a2228b82b4bf
Sam James <sam@gentoo.org>
2023-02-12 22:37:00 +0000

    gh-101857: Allow xattr detection on musl libc
    Previously, we checked exclusively for __GLIBC__ (AND'd with some other
    conditions). Checking for __linux__ instead should be fine.
    
    This fixes using e.g. os.listxattr() on systems using musl libc.
    
    Bug: https://bugs.gentoo.org/894130

--- Modules/posixmodule.c
+++ Modules/posixmodule.c
@@ -274,8 +274,9 @@ corresponding Unix manual entries for more information on calls.");
 #  undef HAVE_SCHED_SETAFFINITY
 #endif
 
-#if defined(HAVE_SYS_XATTR_H) && defined(__GLIBC__) && !defined(__FreeBSD_kernel__) && !defined(__GNU__)
+#if defined(HAVE_SYS_XATTR_H) && defined(__linux__) && !defined(__FreeBSD_kernel__) && !defined(__GNU__)
 #  define USE_XATTRS
+#  include <linux/limits.h>
 #endif
 
 #ifdef USE_XATTRS
