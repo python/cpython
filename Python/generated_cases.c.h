68bd8c5e2efab64ff9d38a214775164182179431
Eclips4 <80244920+Eclips4@users.noreply.github.com>
2023-02-16 20:46:43 +0300

    gh-101952: Fix possible segfault in `BUILD_SET` opcode (#101958)

--- Python/generated_cases.c.h
+++ Python/generated_cases.c.h
@@ -1649,6 +1649,8 @@
             PyObject **values = &PEEK(oparg);
             PyObject *set;
             set = PySet_New(NULL);
+            if (set == NULL)
+                goto error;
             int err = 0;
             for (int i = 0; i < oparg; i++) {
                 PyObject *item = values[i];
