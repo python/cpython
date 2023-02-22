21ff2b9896902ca04d882acb2f9f8c800c848b24
Konstantin Belousov <kib@FreeBSD.org>
2023-02-22 16:37:36 +0200

    Don't define 'static_assert' in C++11 for the FreeBSD workaround

--- Include/pymacro.h
+++ Include/pymacro.h
@@ -6,7 +6,7 @@
 // the static_assert() macro. Define the static_assert() macro in Python until
 // <sys/cdefs.h> suports C11:
 // https://bugs.freebsd.org/bugzilla/show_bug.cgi?id=255290
-#if defined(__FreeBSD__) && !defined(static_assert)
+#if defined(__FreeBSD__) && !defined(__cplusplus) && !defined(static_assert)
 #  define static_assert _Static_assert
 #endif
 
