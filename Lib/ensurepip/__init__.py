27b76e664a8ba8b245afe29eaa650f65f6f6afdc
Hugo van Kemenade <hugovk@users.noreply.github.com>
2023-02-24 11:38:51 +0200

    gh-102202: Update bundled setuptools to 67.4.0

--- Lib/ensurepip/__init__.py
+++ Lib/ensurepip/__init__.py
@@ -10,7 +10,7 @@ from importlib import resources
 
 __all__ = ["version", "bootstrap"]
 _PACKAGE_NAMES = ('setuptools', 'pip')
-_SETUPTOOLS_VERSION = "65.5.0"
+_SETUPTOOLS_VERSION = "67.4.0"
 _PIP_VERSION = "23.0"
 _PROJECTS = [
     ("setuptools", _SETUPTOOLS_VERSION, "py3"),
