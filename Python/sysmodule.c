fa526b693cebdca07d63443114707c398dab23ef
Max Bachmann <kontakt@maxbachmann.de>
2023-02-26 05:36:46 +0100

    do not read version from kernel32.dll

--- Python/sysmodule.c
+++ Python/sysmodule.c
@@ -1497,6 +1497,9 @@ static PyStructSequence_Desc windows_version_desc = {
 static PyObject *
 _sys_getwindowsversion_from_kernel32()
 {
+#ifdef MS_WINDOWS_NON_DESKTOP
+    return NULL;
+#else
     HANDLE hKernel32;
     wchar_t kernel32_path[MAX_PATH];
     LPVOID verblock;
@@ -1530,6 +1533,7 @@ _sys_getwindowsversion_from_kernel32()
     realBuild = HIWORD(ffi->dwProductVersionLS);
     PyMem_RawFree(verblock);
     return Py_BuildValue("(kkk)", realMajor, realMinor, realBuild);
+#endif /* MS_WINDOWS_NON_DESKTOP */
 }
 
 /* Disable deprecation warnings about GetVersionEx as the result is
