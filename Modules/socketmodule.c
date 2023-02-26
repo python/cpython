c1bf0e26114fe07533a7e221b1ab0a70b8410ffd
Max Bachmann <oss@maxbachmann.de>
2023-02-26 20:17:29 +0100

    follow pep7

--- Modules/socketmodule.c
+++ Modules/socketmodule.c
@@ -383,7 +383,9 @@ remove_unusable_flags(PyObject *m)
             VER_MAJORVERSION|VER_MINORVERSION|VER_BUILDNUMBER,
             dwlConditionMask);
 #else
-        BOOL isSupported = (info.dwMajorVersion >= 10) && (info.dwMinorVersion >= 0) && (info.dwBuildNumber >= win_runtime_flags[i].build_number);
+        BOOL isSupported = info.dwMajorVersion >= 10 &&
+            info.dwMinorVersion >= 0 &&
+            info.dwBuildNumber >= win_runtime_flags[i].build_number;
 #endif
         if (isSupported) {
             break;
