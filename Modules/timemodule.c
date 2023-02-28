fc6b942f98104f23d2f3f7bba37c9c6199627757
Max Bachmann <oss@maxbachmann.de>
2023-02-28 21:54:24 +0100

    fix guard

--- Modules/timemodule.c
+++ Modules/timemodule.c
@@ -1137,7 +1137,7 @@ time_tzset(PyObject *self, PyObject *unused)
         return NULL;
     }
 
-#ifdef MS_WINDOWS_DESKTOP_APP
+#if !defined(MS_WINDOWS) || defined(MS_WINDOWS_DESKTOP_APP)
     tzset();
 #endif
 
@@ -1757,7 +1757,7 @@ init_timezone(PyObject *m)
      */
 #ifdef HAVE_DECL_TZNAME
     PyObject *otz0, *otz1;
-#ifdef MS_WINDOWS_DESKTOP_APP
+#if !defined(MS_WINDOWS) || defined(MS_WINDOWS_DESKTOP_APP)
     tzset();
 #endif
     PyModule_AddIntConstant(m, "timezone", _Py_timezone);
