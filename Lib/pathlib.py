3b53e277b7d4eda2e87f6c51951dd0e6eebd3483
barneygale <barney.gale@gmail.com>
2023-03-11 21:55:26 +0000

    Remove unused import

--- Lib/pathlib.py
+++ Lib/pathlib.py
@@ -16,7 +16,6 @@ import sys
 import warnings
 from _collections_abc import Sequence
 from errno import ENOENT, ENOTDIR, EBADF, ELOOP
-from operator import attrgetter
 from stat import S_ISDIR, S_ISLNK, S_ISREG, S_ISSOCK, S_ISBLK, S_ISCHR, S_ISFIFO
 from urllib.parse import quote_from_bytes as urlquote_from_bytes
 
