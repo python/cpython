c9a4e4d39acb2dd607b3ddffa3d539f772f5ab86
Raymond Hettinger <raymond.hettinger@gmail.com>
2023-03-13 15:48:28 -0500

    Coerce result to a float

--- Lib/statistics.py
+++ Lib/statistics.py
@@ -1139,7 +1139,7 @@ def linear_regression(x, y, /, *, proportional=False):
         ybar = fsum(y) / n
         x = [xi - xbar for xi in x]  # List because used three times below
         y = (yi - ybar for yi in y)  # Generator because only used once below
-    sxy = sumprod(x, y)
+    sxy = sumprod(x, y) + 0.0        # Add zero to coerce result to a float
     sxx = sumprod(x, x)
     try:
         slope = sxy / sxx   # equivalent to:  covariance(x, y) / variance(x)
