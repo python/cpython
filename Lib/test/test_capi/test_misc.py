a2aaf8d3a713e5dfabf968ba4d4bf7488c5f5cc3
furkanonder <furkanonder@protonmail.com>
2023-03-15 18:23:24 +0300

    Gh-68586: use run_python_until_end in test_capi

--- Lib/test/test_capi/test_misc.py
+++ Lib/test/test_capi/test_misc.py
@@ -21,7 +21,7 @@ from test.support import MISSING_C_DOCSTRINGS
 from test.support import import_helper
 from test.support import threading_helper
 from test.support import warnings_helper
-from test.support.script_helper import assert_python_failure, assert_python_ok
+from test.support.script_helper import assert_python_failure, assert_python_ok, run_python_until_end
 try:
     import _posixsubprocess
 except ImportError:
@@ -69,14 +69,10 @@ class CAPITest(unittest.TestCase):
 
     @support.requires_subprocess()
     def test_no_FatalError_infinite_loop(self):
-        with support.SuppressCrashReport():
-            p = subprocess.Popen([sys.executable, "-c",
-                                  'import _testcapi;'
-                                  '_testcapi.crash_no_current_thread()'],
-                                 stdout=subprocess.PIPE,
-                                 stderr=subprocess.PIPE,
-                                 text=True)
-        (out, err) = p.communicate()
+        run_result, _cmd_line = run_python_until_end(
+            '-c', 'import _testcapi;' '_testcapi.crash_no_current_thread()',
+        )
+        _rc, out, err = run_result
         self.assertEqual(out, '')
         # This used to cause an infinite loop.
         msg = ("Fatal Python error: PyThreadState_Get: "
