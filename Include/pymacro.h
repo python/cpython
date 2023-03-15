3ca0a69a7e8a588dafecb0eb3b46d7e47ec77ef3
Joshua Root <jmr@macports.org>
2023-03-16 06:25:08 +1100

    C2x compatibility for static_assert
    The C2x draft (currently expected to become C23) makes static_assert
    a keyword to match C++. So only define the macro for up to C17.

--- Include/pymacro.h
+++ Include/pymacro.h
@@ -12,12 +12,14 @@
 // static_assert is defined in glibc from version 2.16. Compiler support for
 // the C11 _Static_assert keyword is in gcc >= 4.6.
 //
-// MSVC makes static_assert a keyword, contrary to the C standard.
+// MSVC makes static_assert a keyword in C11-17, contrary to the standards.
 //
-// In C++ 11 static_assert is a keyword, redefining is undefined behaviour.
-// So only define if building as C (if __STDC_VERSION__ is defined), not C++.
+// In C++11 and C2x, static_assert is a keyword, redefining is undefined
+// behaviour. So only define if building as C (if __STDC_VERSION__ is defined),
+// not C++, and only for C11-17.
 #if !defined(static_assert) && (defined(__GNUC__) || defined(__clang__)) \
-     && defined(__STDC_VERSION__) && __STDC_VERSION__ >= 201112L
+     && defined(__STDC_VERSION__) && __STDC_VERSION__ >= 201112L \
+     && __STDC_VERSION__ <= 201710L
 #  define static_assert _Static_assert
 #endif
 
