d207fb1851c77b728d32c7eeb0d0d3751759d3c8
Kristján Valur Jónsson <sweskman@gmail.com>
2023-03-18 13:48:46 +0000

    explicitly raise from cancelled error

--- Lib/asyncio/timeouts.py
+++ Lib/asyncio/timeouts.py
@@ -96,7 +96,7 @@ class Timeout:
             if self._task.uncancel() <= self._cancelling and exc_type is exceptions.CancelledError:
                 # Since there are no new cancel requests, we're
                 # handling this.
-                raise TimeoutError
+                raise TimeoutError from exc_val
         elif self._state is _State.ENTERED:
             self._state = _State.EXITED
 
