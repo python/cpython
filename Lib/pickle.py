92362c126eedd41e83fac0cc24f0d6d3677702af
Pieter Eendebak <pieter.eendebak@gmail.com>
2023-03-26 21:34:22 +0200

    remove unused exception

--- Lib/pickle.py
+++ Lib/pickle.py
@@ -1612,7 +1612,7 @@ class _Unpickler:
         i = self.read(1)[0]
         try:
             self.append(self.memo[i])
-        except KeyError as exc:
+        except KeyError:
             msg = f'Memo value not found at index {i}'
             raise UnpicklingError(msg) from None
     dispatch[BINGET[0]] = load_binget
@@ -1621,7 +1621,7 @@ class _Unpickler:
         i, = unpack('<I', self.read(4))
         try:
             self.append(self.memo[i])
-        except KeyError as exc:
+        except KeyError:
             msg = f'Memo value not found at index {i}'
             raise UnpicklingError(msg) from None
     dispatch[LONG_BINGET[0]] = load_long_binget
