5f66a410243e1d5ebaf5a7a33624b2e410e21bf6
gaogaotiantian <gaogaotiantian@hotmail.com>
2023-03-27 13:37:22 -0700

    gh-103023: Add SyntaxError check in pdb's `display` command (#103024)
    Co-authored-by: Łukasz Langa <lukasz@langa.pl>

--- Lib/pdb.py
+++ Lib/pdb.py
@@ -399,7 +399,7 @@ class Pdb(bdb.Bdb, cmd.Cmd):
         displaying = self.displaying.get(self.curframe)
         if displaying:
             for expr, oldvalue in displaying.items():
-                newvalue = self._getval_except(expr)
+                newvalue, _ = self._getval_except(expr)
                 # check for identity first; this prevents custom __eq__ to
                 # be called at every loop, and also prevents instances whose
                 # fields are changed to be displayed
@@ -1246,13 +1246,12 @@ class Pdb(bdb.Bdb, cmd.Cmd):
     def _getval_except(self, arg, frame=None):
         try:
             if frame is None:
-                return eval(arg, self.curframe.f_globals, self.curframe_locals)
+                return eval(arg, self.curframe.f_globals, self.curframe_locals), None
             else:
-                return eval(arg, frame.f_globals, frame.f_locals)
-        except:
-            exc_info = sys.exc_info()[:2]
-            err = traceback.format_exception_only(*exc_info)[-1].strip()
-            return _rstr('** raised %s **' % err)
+                return eval(arg, frame.f_globals, frame.f_locals), None
+        except BaseException as exc:
+            err = traceback.format_exception_only(exc)[-1].strip()
+            return _rstr('** raised %s **' % err), exc
 
     def _error_exc(self):
         exc_info = sys.exc_info()[:2]
@@ -1437,13 +1436,19 @@ class Pdb(bdb.Bdb, cmd.Cmd):
         Without expression, list all display expressions for the current frame.
         """
         if not arg:
-            self.message('Currently displaying:')
-            for item in self.displaying.get(self.curframe, {}).items():
-                self.message('%s: %r' % item)
+            if self.displaying:
+                self.message('Currently displaying:')
+                for item in self.displaying.get(self.curframe, {}).items():
+                    self.message('%s: %r' % item)
+            else:
+                self.message('No expression is being displayed')
         else:
-            val = self._getval_except(arg)
-            self.displaying.setdefault(self.curframe, {})[arg] = val
-            self.message('display %s: %r' % (arg, val))
+            val, exc = self._getval_except(arg)
+            if isinstance(exc, SyntaxError):
+                self.message('Unable to display %s: %r' % (arg, val))
+            else:
+                self.displaying.setdefault(self.curframe, {})[arg] = val
+                self.message('display %s: %r' % (arg, val))
 
     complete_display = _complete_expression
 
