a88f2d554128d1052d1ef254235357da3bc3ef05
Brandt Bucher <brandtbucher@microsoft.com>
2023-03-29 15:53:30 -0700

    GH-89987: Shrink the BINARY_SUBSCR caches (GH-103022)

--- Include/internal/pycore_code.h
+++ Include/internal/pycore_code.h
@@ -47,8 +47,6 @@ typedef struct {
 
 typedef struct {
     uint16_t counter;
-    uint16_t type_version[2];
-    uint16_t func_version;
 } _PyBinarySubscrCache;
 
 #define INLINE_CACHE_ENTRIES_BINARY_SUBSCR CACHE_ENTRIES(_PyBinarySubscrCache)
