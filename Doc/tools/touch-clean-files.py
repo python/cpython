198bd9db81c88d0e9aba0107c124c7d8e4ce4813
Hugo van Kemenade <hugovk@users.noreply.github.com>
2023-03-29 19:32:51 +0300

    gh-101100: Touch files that must pass Sphinx nit-picky mode

---
+++ Doc/tools/touch-clean-files.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Touch files that must pass Sphinx nit-picky mode
+so they are rebuilt and we can catch regressions.
+"""
+
+from pathlib import Path
+
+CLEAN = [
+    "Doc/library/gc.rst",
+    "Doc/library/sqlite3.rst",
+    "Doc/whatsnew/3.12.rst",
+]
+print("Touching:")
+for filename in CLEAN:
+    print(f"{filename}")
+    Path(filename).touch()
