f3a172bd5524241fdb5aeefe4251620de4b26697
Eric Snow <ericsnowcurrently@gmail.com>
2023-03-31 09:02:02 -0600

    Decref the key in the right interpreter in _extensions_cache_set().

--- Python/import.c
+++ Python/import.c
@@ -983,13 +983,13 @@ _extensions_cache_set(PyObject *filename, PyObject *name, PyModuleDef *def)
     res = 0;
 
 finally:
+    Py_XDECREF(key);
     if (oldts != NULL) {
         _PyThreadState_Swap(interp->runtime, oldts);
         _PyThreadState_UnbindDetached(main_tstate);
         Py_DECREF(name);
         Py_DECREF(filename);
     }
-    Py_XDECREF(key);
     extensions_lock_release();
     return res;
 }
