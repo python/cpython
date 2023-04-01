84c590c41d63ce7b36f15fc6f60fe6c21baaaad8
Brett Cannon <brett@python.org>
2023-03-31 17:45:36 -0700

    GH-102700: allow built-in modules to be submodules

--- Lib/test/test_importlib/builtin/test_finder.py
+++ Lib/test/test_importlib/builtin/test_finder.py
@@ -1,13 +1,13 @@
 from test.test_importlib import abc, util
 
-machinery = util.import_importlib('importlib.machinery')
+machinery = util.import_importlib("importlib.machinery")
 
 import sys
 import unittest
 import warnings
 
 
-@unittest.skipIf(util.BUILTINS.good_name is None, 'no reasonable builtin module')
+@unittest.skipIf(util.BUILTINS.good_name is None, "no reasonable builtin module")
 class FindSpecTests(abc.FinderTests):
 
     """Test find_spec() for built-in modules."""
@@ -17,7 +17,7 @@ class FindSpecTests(abc.FinderTests):
         with util.uncache(util.BUILTINS.good_name):
             found = self.machinery.BuiltinImporter.find_spec(util.BUILTINS.good_name)
             self.assertTrue(found)
-            self.assertEqual(found.origin, 'built-in')
+            self.assertEqual(found.origin, "built-in")
 
     # Built-in modules cannot be a package.
     test_package = None
@@ -32,25 +32,18 @@ class FindSpecTests(abc.FinderTests):
     test_package_over_module = None
 
     def test_failure(self):
-        name = 'importlib'
+        name = "importlib"
         assert name not in sys.builtin_module_names
         spec = self.machinery.BuiltinImporter.find_spec(name)
         self.assertIsNone(spec)
 
-    def test_ignore_path(self):
-        # The value for 'path' should always trigger a failed import.
-        with util.uncache(util.BUILTINS.good_name):
-            spec = self.machinery.BuiltinImporter.find_spec(util.BUILTINS.good_name,
-                                                            ['pkg'])
-            self.assertIsNone(spec)
+
+(Frozen_FindSpecTests, Source_FindSpecTests) = util.test_both(
+    FindSpecTests, machinery=machinery
+)
 
 
-(Frozen_FindSpecTests,
- Source_FindSpecTests
- ) = util.test_both(FindSpecTests, machinery=machinery)
-
-
-@unittest.skipIf(util.BUILTINS.good_name is None, 'no reasonable builtin module')
+@unittest.skipIf(util.BUILTINS.good_name is None, "no reasonable builtin module")
 class FinderTests(abc.FinderTests):
 
     """Test find_module() for built-in modules."""
@@ -60,9 +53,11 @@ class FinderTests(abc.FinderTests):
         with util.uncache(util.BUILTINS.good_name):
             with warnings.catch_warnings():
                 warnings.simplefilter("ignore", DeprecationWarning)
-                found = self.machinery.BuiltinImporter.find_module(util.BUILTINS.good_name)
+                found = self.machinery.BuiltinImporter.find_module(
+                    util.BUILTINS.good_name
+                )
             self.assertTrue(found)
-            self.assertTrue(hasattr(found, 'load_module'))
+            self.assertTrue(hasattr(found, "load_module"))
 
     # Built-in modules cannot be a package.
     test_package = test_package_in_package = test_package_over_module = None
@@ -71,27 +66,17 @@ class FinderTests(abc.FinderTests):
     test_module_in_package = None
 
     def test_failure(self):
-        assert 'importlib' not in sys.builtin_module_names
+        assert "importlib" not in sys.builtin_module_names
         with warnings.catch_warnings():
             warnings.simplefilter("ignore", DeprecationWarning)
-            loader = self.machinery.BuiltinImporter.find_module('importlib')
+            loader = self.machinery.BuiltinImporter.find_module("importlib")
         self.assertIsNone(loader)
 
-    def test_ignore_path(self):
-        # The value for 'path' should always trigger a failed import.
-        with util.uncache(util.BUILTINS.good_name):
-            with warnings.catch_warnings():
-                warnings.simplefilter("ignore", DeprecationWarning)
-                loader = self.machinery.BuiltinImporter.find_module(
-                                                        util.BUILTINS.good_name,
-                                                        ['pkg'])
-            self.assertIsNone(loader)
+
+(Frozen_FinderTests, Source_FinderTests) = util.test_both(
+    FinderTests, machinery=machinery
+)
 
 
-(Frozen_FinderTests,
- Source_FinderTests
- ) = util.test_both(FinderTests, machinery=machinery)
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
