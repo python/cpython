4bb6dfce44bcb20208d29e9b6e9c0f9a5529795e
Hugo van Kemenade <hugovk@users.noreply.github.com>
2023-04-02 18:55:48 +0300

    Add list of excluded subdirs, rename to .nitignore

--- Doc/tools/touch-clean-files.py
+++ Doc/tools/touch-clean-files.py
@@ -8,22 +8,31 @@ from pathlib import Path
 
 # Exclude these whether they're dirty or clean,
 # because they trigger a rebuild of dirty files.
-EXCLUDES = {
-    Path("Doc/includes/wasm-notavail.rst"),
+EXCLUDE_FILES = {
     Path("Doc/whatsnew/changelog.rst"),
 }
 
-# Exclude "Doc/venv/"
-ALL_RST = {rst for rst in Path("Doc/").rglob("*.rst") if rst.parts[1] != "venv"}
+# Subdirectories of Doc/ to exclude.
+EXCLUDE_SUBDIRS = {
+    ".env",
+    ".venv",
+    "env",
+    "includes",
+    "venv",
+}
 
-with Path("Doc/tools/dirty-files.txt").open() as clean_files:
+ALL_RST = {
+    rst for rst in Path("Doc/").rglob("*.rst") if rst.parts[1] not in EXCLUDE_SUBDIRS
+}
+
+with Path("Doc/tools/.nitignore").open() as clean_files:
     DIRTY = {
         Path(filename.strip())
         for filename in clean_files
         if filename.strip() and not filename.startswith("#")
     }
 
-CLEAN = ALL_RST - DIRTY - EXCLUDES
+CLEAN = ALL_RST - DIRTY - EXCLUDE_FILES
 
 print("Touching:")
 for filename in sorted(CLEAN):
