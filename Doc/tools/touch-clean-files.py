900d31c4f54a5137a838b3c2e203172710c59740
Hugo van Kemenade <hugovk@users.noreply.github.com>
2023-04-02 16:43:22 +0300

    Use list of 'dirty' docs with warnings, instead of a clean list

--- Doc/tools/touch-clean-files.py
+++ Doc/tools/touch-clean-files.py
@@ -6,16 +6,27 @@ so they are rebuilt and we can catch regressions.
 
 from pathlib import Path
 
-# Input file has blank line between entries to reduce merge conflicts
-with Path("Doc/tools/clean-files.txt").open() as clean_files:
-    CLEAN = [
+# Exclude these whether they're dirty or clean,
+# because they trigger a rebuild of dirty files.
+EXCLUDES = {
+    Path("Doc/includes/wasm-notavail.rst"),
+    Path("Doc/whatsnew/changelog.rst"),
+}
+
+# Exclude "Doc/venv/"
+ALL_RST = {rst for rst in Path("Doc/").rglob("*.rst") if rst.parts[1] != "venv"}
+
+with Path("Doc/tools/dirty-files.txt").open() as clean_files:
+    DIRTY = {
         Path(filename.strip())
         for filename in clean_files
         if filename.strip() and not filename.startswith("#")
-    ]
+    }
+
+CLEAN = ALL_RST - DIRTY - EXCLUDES
 
 print("Touching:")
-for filename in CLEAN:
+for filename in sorted(CLEAN):
     print(filename)
     filename.touch()
 print(f"Touched {len(CLEAN)} files")
