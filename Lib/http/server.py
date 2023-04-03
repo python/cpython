161058da077003eaa72e765f2206c2160ce3f773
Ben Kallus <benjamin.p.kallus.gr@dartmouth.edu>
2023-04-02 22:58:34 -0400

    gh-103204: Enforce that HTTP version numbers must consist only of digits

--- Lib/http/server.py
+++ Lib/http/server.py
@@ -300,6 +300,9 @@ class BaseHTTPRequestHandler(socketserver.StreamRequestHandler):
                 #   - Leading zeros MUST be ignored by recipients.
                 if len(version_number) != 2:
                     raise ValueError
+                for component in version_number:
+                    if not component.isdigit():
+                        raise ValueError
                 version_number = int(version_number[0]), int(version_number[1])
             except (ValueError, IndexError):
                 self.send_error(
