ede88da1f0ff406501099b2daa66810b2f0869e5
Irit Katriel <iritkatriel@yahoo.com>
2023-04-06 12:56:39 +0100

    gh-102799: use sys.exception() instead of sys.exc_info() in contextlib

--- Lib/test/test_contextlib.py
+++ Lib/test/test_contextlib.py
@@ -1074,7 +1074,7 @@ class TestBaseExitStack:
 class TestExitStack(TestBaseExitStack, unittest.TestCase):
     exit_stack = ExitStack
     callback_error_internal_frames = [
-        ('__exit__', 'raise exc_details[1]'),
+        ('__exit__', 'raise exc'),
         ('__exit__', 'if cb(*exc_details):'),
     ]
 
