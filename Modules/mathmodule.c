9c32ae022f07ea90892175a2d7f5c777621be0e0
Sergey B Kirpichev <skirpichev@gmail.com>
2023-04-07 15:54:42 +0300

    Simplify math_1, like other functions
    Now exception messages kept in is_error().  This improve
    coverage for L1007.

--- Modules/mathmodule.c
+++ Modules/mathmodule.c
@@ -990,24 +990,16 @@ math_1(PyObject *arg, double (*func) (double), int can_overflow)
         return NULL;
     errno = 0;
     r = (*func)(x);
-    if (Py_IS_NAN(r) && !Py_IS_NAN(x)) {
-        PyErr_SetString(PyExc_ValueError,
-                        "math domain error"); /* invalid arg */
-        return NULL;
-    }
+    if (Py_IS_NAN(r) && !Py_IS_NAN(x))
+        errno = EDOM;
     if (Py_IS_INFINITY(r) && Py_IS_FINITE(x)) {
         if (can_overflow)
-            PyErr_SetString(PyExc_OverflowError,
-                            "math range error"); /* overflow */
+            errno = ERANGE;
         else
-            PyErr_SetString(PyExc_ValueError,
-                            "math domain error"); /* singularity */
-        return NULL;
+            errno = EDOM;
     }
-    if (Py_IS_FINITE(r) && errno && is_error(r))
-        /* this branch unnecessary on most platforms */
+    if (errno && is_error(r))
         return NULL;
-
     return PyFloat_FromDouble(r);
 }
 
