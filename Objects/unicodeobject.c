74b6e7b5374f48f1d7252a3cf1f6bdc74c17774c
Eddie Elizondo <eduardo.elizondorueda@gmail.com>
2023-04-07 12:24:13 -0400

    Nits

--- Objects/unicodeobject.c
+++ Objects/unicodeobject.c
@@ -14681,7 +14681,7 @@ _PyUnicode_ClearInterned(PyInterpreterState *interp)
      * Therefore, this should remain disabled for until there is a strict guarantee
      * that no memory will be left after `Py_Finalize`.
      */
-#if Py_DEBUG
+#ifdef Py_DEBUG
     /* For all non-singleton interned strings, restore the two valid references
        to that instance from within the intern string dictionary and let the
        normal reference counting process clean up these instances. */
