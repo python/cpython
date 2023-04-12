18fad8020e81c83e5104a1bbf224b0673b351959
Mark Shannon <mark@hotpy.org>
2023-04-13 00:12:36 +0100

    Add explanatory comment for return_offset.

--- Include/internal/pycore_frame.h
+++ Include/internal/pycore_frame.h
@@ -61,6 +61,12 @@ typedef struct _PyInterpreterFrame {
     // over, or (in the case of a newly-created frame) a totally invalid value:
     _Py_CODEUNIT *prev_instr;
     int stacktop;  /* Offset of TOS from localsplus  */
+    /* The return_offset determines where a `RETURN` should go in the caller,
+     * relative to `prev_instr`.
+     * It is only meaningful to the callee,
+     * so it needs to be set in any CALL (to a Python function)
+     * or SEND (to a coroutine or generator).
+     * If there is no callee, then it is meaningless. */
     uint16_t return_offset;
     char owner;
     /* Locals and stack */
