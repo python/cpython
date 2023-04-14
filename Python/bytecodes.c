775ed0feeac0a004cb236bc7ae68868e8bc270e3
Carl Meyer <carl@oddbird.net>
2023-04-14 08:35:55 -0600

    Apply suggestions from code review
    Co-authored-by: Dong-hee Na <donghee.na92@gmail.com>

--- Python/bytecodes.c
+++ Python/bytecodes.c
@@ -1575,7 +1575,7 @@ dummy_func(
             } else {
                 PyObject *super;
                 if (oparg & 2) {
-                    super = PyObject_Vectorcall(global_super, NULL, 0, NULL);
+                    super = PyObject_CallNoArgs(global_super);
                 } else {
                     PyObject *stack[] = {class, self};
                     super = PyObject_Vectorcall(global_super, stack, 2, NULL);
