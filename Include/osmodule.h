392e842852430da7e92323f84c41e56cf6ddd2a6
gsallam <123525874+gsallam@users.noreply.github.com>
2023-04-14 12:27:20 -0700

    Update osmodule.h

--- Include/osmodule.h
+++ Include/osmodule.h
@@ -9,8 +9,7 @@ extern "C" {
 
 #if !defined(Py_LIMITED_API) || Py_LIMITED_API+0 >= 0x03060000
 PyAPI_FUNC(PyObject *) PyOS_FSPath(PyObject *path);
-#endif
-    
+
 typedef struct {
      FILE* perf_map;
     PyThread_type_lock map_lock;
@@ -21,6 +20,7 @@ PyAPI_FUNC(void *) PyOS_PerfMapState_Init(void);
 PyAPI_FUNC(int) PyOS_WritePerfMapEntry(const void *code_addr, unsigned int code_size, const char *entry_name);
 
 PyAPI_FUNC(void) PyOS_PerfMapState_Fini(void);
+#endif
     
 #ifdef __cplusplus
 }
