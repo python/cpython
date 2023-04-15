6ee3fdb95310319bb055f8c0de898629aff73786
kenjin-work <kenjin@python.org>
2023-04-16 00:43:17 +0800

    Update bm_float_unboxed.py

--- bm_float_unboxed.py
+++ bm_float_unboxed.py
@@ -1,17 +1,18 @@
 import time
 
-def f(a, b):
-    for _ in range(10_000_000):
-        return a + b + a * a + b - a - b
+def f():
+    a = 1.0
+    b = 2.0
+    return a + b + a * a + b - a - b
 
 # Warmup
-f(1.0, 2.0)
-f(1.0, 2.0)
+f()
+f()
 
 # Running the actual benchmark
 
 print("Starting benchmark...")
 start = time.perf_counter()
 for _ in range(100_000_000):
-    f(1.0, 2.0)
+    f()
 print("Time taken is", time.perf_counter() - start, "s")
