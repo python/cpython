82945b28261c6e7ce9cf703ba67271f73d88b6a7
Carl Meyer <carl@oddbird.net>
2023-04-17 07:02:21 -0700

    review comments

--- Python/bytecodes.c
+++ Python/bytecodes.c
@@ -1565,6 +1565,7 @@ dummy_func(
                     Py_DECREF(self);
                     ERROR_IF(true, error);
                 }
+                // Works with CALL, pushes two values: either `meth | self` or `NULL | meth`.
                 if (meth_found) {
                     res2 = res;
                     res = self;  // transfer ownership
