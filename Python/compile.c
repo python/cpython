20d0554986b15caab29797fef6df8f55f6b50abb
hauntsaninja <hauntsaninja@gmail.com>
2023-04-24 14:16:49 -0600

    use infer_type

--- Python/compile.c
+++ Python/compile.c
@@ -2269,6 +2269,8 @@ check_is_arg(expr_ty e)
          || value == Py_Ellipsis);
 }
 
+static PyTypeObject * infer_type(expr_ty e);
+
 /* Check operands of identity checks ("is" and "is not").
    Emit a warning if any operand is a constant except named singletons.
  */
@@ -2290,7 +2292,7 @@ check_compare(struct compiler *c, expr_ty e)
                         : "\"is not\" with '%.200s' literal. Did you mean \"!=\"?";
                 expr_ty literal = !left ? left_expr : right_expr;
                 return compiler_warn(
-                    c, LOC(e), msg, Py_TYPE(literal->v.Constant.value)->tp_name
+                    c, LOC(e), msg, infer_type(literal)->tp_name
                 );
             }
         }
