3a8185ddfbb4b40aec26ef844e82c03f0ca492c6
Guido van Rossum <guido@python.org>
2023-04-25 10:50:08 -0600

    Add some basic tests

---
+++ Lib/test/test_tag_strings.py
@@ -0,0 +1,40 @@
+import unittest
+
+class TagStringTests(unittest.TestCase):
+
+    def test_basic(self):
+        def tag(*args):
+            return "Spam"
+        self.assertEqual(tag"ham", "Spam")
+        self.assertEqual(tag"...{ham}...{eggs}...", "Spam")
+
+    def test_tag_call(self):
+        def tag(*args):
+            return args
+        self.assertEqual(tag"ham", ("ham",))
+        res = tag"... {ham and eggs} ..."
+        self.assertEqual(len(res), 3)
+        self.assertEqual(res[0], "... ")
+        func, string, conv, spec = res[1]
+        ham = 42
+        eggs = "delicious"
+        self.assertEqual(func(), "delicious")
+        self.assertEqual(string, "ham and eggs")
+        self.assertEqual(conv, None)
+        self.assertEqual(spec, None)
+        self.assertEqual(res[2], " ...")
+
+    def test_tag_call_with_conversion(self):
+        def tag(*args):
+            return args
+        res = tag"{ham!r:spec}"
+        func, string, conv, spec = res[0]
+        ham = 42
+        self.assertEqual(func(), 42)
+        self.assertEqual(string, "ham")
+        self.assertEqual(conv, "r")
+        self.assertEqual(spec, "spec")
+
+
+if __name__ == "__main__":
+    unittest.main()
