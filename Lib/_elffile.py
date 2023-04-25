ee37ed7f93b9f4aa1464ed720c47b42e1e67ea2b
Manuel Kaufmann <humitos@gmail.com>
2023-04-25 11:40:32 -0600

    Remove unused classes from `_elffile.py`

--- Lib/_elffile.py
+++ Lib/_elffile.py
@@ -23,24 +23,6 @@ class ELFInvalid(ValueError):
     pass
 
 
-class EIClass(enum.IntEnum):
-    C32 = 1
-    C64 = 2
-
-
-class EIData(enum.IntEnum):
-    Lsb = 1
-    Msb = 2
-
-
-class EMachine(enum.IntEnum):
-    I386 = 3
-    S390 = 22
-    Arm = 40
-    X8664 = 62
-    AArc64 = 183
-
-
 class ELFFile:
     """
     Representation of an ELF executable.
