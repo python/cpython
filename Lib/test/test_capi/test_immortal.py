94ca054672e057db9b0e49db462ae0b7b8c57974
Dong-hee Na <donghee.na@python.org>
2023-04-28 11:31:19 +0900

    Add test_immortal.py

---
+++ Lib/test/test_capi/test_immortal.py
@@ -0,0 +1,19 @@
+import unittest
+from test.support import import_helper
+
+_testcapi = import_helper.import_module('_testcapi')
+
+
+class TestCAPI(unittest.TestCase):
+    def test_immortal_bool(self):
+        _testcapi.test_immortal_bool()
+
+    def test_immortal_none(self):
+        _testcapi.test_immortal_none()
+    
+    def test_immortal_ellipsis(self):
+        _testcapi.test_immortal_ellipsis()
+
+
+if __name__ == "__main__":
+    unittest.main()
