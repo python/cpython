d8a1365214f2c70a9955a1c735f47467de3abfb3
Prince Roshan <princekrroshan01@gmail.com>
2023-04-30 01:45:11 +0530

    fix test

--- Lib/test/test_mmap.py
+++ Lib/test/test_mmap.py
@@ -264,7 +264,7 @@ class MmapTests(unittest.TestCase):
         # Try opening a bad file descriptor...
         self.assertRaises(OSError, mmap.mmap, -2, 4096)
 
-    def test_unexpected_mmap_close(self):
+    def test_unexpected_mmap_close_scenario_1(self):
         # See gh-103987
         with open(TESTFN, "wb+") as f:
             data = b'aabaac\x00deef\x00\x00aa\x00'
@@ -283,7 +283,29 @@ class MmapTests(unittest.TestCase):
         with self.assertRaises(ValueError):
             m[X()] # should not crash
 
+        with self.assertRaises(ValueError):
+            m[X():5] # should not crash
+
+        m.close() # first CHECK_VALID will pick and raise ValueError
+        with self.assertRaises(ValueError):
+            m[0:5] # should not crash
+
+
+    def test_unexpected_mmap_close_scenario_2(self):
+
+        with open(TESTFN, "wb+") as f:
+            data = b'aabaac\x00deef\x00\x00aa\x00'
+            n = len(data)
+            f.write(data)
+            f.flush()
+
+            m = mmap.mmap(f.fileno(), n, access=mmap.ACCESS_READ)
+
         m.close()
+        with self.assertRaises(ValueError):
+            m["abc"] # first CHECK_VALID will pick and raise ValueError
+
+
 
     def test_tougher_find(self):
         # Do a tougher .find() test.  SF bug 515943 pointed out that, in 2.2,
