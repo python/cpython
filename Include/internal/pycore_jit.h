606a17d23f8f50865553f87a52f49b2a4d8b9e54
Brandt Bucher <brandtbucher@microsoft.com>
2023-04-30 13:54:41 -0700

    Name _PyJITFunction's parameters

--- Include/internal/pycore_jit.h
+++ Include/internal/pycore_jit.h
@@ -1,4 +1,4 @@
-typedef int (*_PyJITFunction)(PyThreadState *, _PyInterpreterFrame *, PyObject **, _Py_CODEUNIT *);
+typedef int (*_PyJITFunction)(PyThreadState *tstate, _PyInterpreterFrame *frame, PyObject **stack_pointer, _Py_CODEUNIT *next_instr);
 
 PyAPI_FUNC(_PyJITFunction)_PyJIT_CompileTrace(int size, _Py_CODEUNIT **trace);
 PyAPI_FUNC(void)_PyJIT_Free(_PyJITFunction trace);
