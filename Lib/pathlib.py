b2c4b47b5e746c2254e28580ffdb5866dabee4d7
barneygale <barney.gale@gmail.com>
2023-05-04 10:22:10 +0100

    Pass flavour to selector initialiser

--- Lib/pathlib.py
+++ Lib/pathlib.py
@@ -62,7 +62,7 @@ def _is_case_sensitive(flavour):
 #
 
 @functools.lru_cache()
-def _make_selector(pattern_parts, case_sensitive):
+def _make_selector(pattern_parts, flavour, case_sensitive):
     pat = pattern_parts[0]
     child_parts = pattern_parts[1:]
     if not pat:
@@ -75,17 +75,17 @@ def _make_selector(pattern_parts, case_sensitive):
         raise ValueError("Invalid pattern: '**' can only be an entire path component")
     else:
         cls = _WildcardSelector
-    return cls(pat, child_parts, case_sensitive)
+    return cls(pat, child_parts, flavour, case_sensitive)
 
 
 class _Selector:
     """A selector matches a specific glob pattern part against the children
     of a given path."""
 
-    def __init__(self, child_parts, case_sensitive):
+    def __init__(self, child_parts, flavour, case_sensitive):
         self.child_parts = child_parts
         if child_parts:
-            self.successor = _make_selector(child_parts, case_sensitive)
+            self.successor = _make_selector(child_parts, flavour, case_sensitive)
             self.dironly = True
         else:
             self.successor = _TerminatingSelector()
@@ -109,8 +109,8 @@ class _TerminatingSelector:
 
 class _ParentSelector(_Selector):
 
-    def __init__(self, name, child_parts, case_sensitive):
-        _Selector.__init__(self, child_parts, case_sensitive)
+    def __init__(self, name, child_parts, flavour, case_sensitive):
+        _Selector.__init__(self, child_parts, flavour, case_sensitive)
 
     def _select_from(self,  parent_path, scandir):
         path = parent_path._make_child_relpath('..')
@@ -120,10 +120,13 @@ class _ParentSelector(_Selector):
 
 class _WildcardSelector(_Selector):
 
-    def __init__(self, pat, child_parts, case_sensitive):
+    def __init__(self, pat, child_parts, flavour, case_sensitive):
+        _Selector.__init__(self, child_parts, flavour, case_sensitive)
+        if case_sensitive is None:
+            # TODO: evaluate case-sensitivity of each directory in _select_from()
+            case_sensitive = _is_case_sensitive(flavour)
         flags = re.NOFLAG if case_sensitive else re.IGNORECASE
         self.match = re.compile(fnmatch.translate(pat), flags=flags).fullmatch
-        _Selector.__init__(self, child_parts, case_sensitive)
 
     def _select_from(self, parent_path, scandir):
         try:
@@ -154,8 +157,8 @@ class _WildcardSelector(_Selector):
 
 class _RecursiveWildcardSelector(_Selector):
 
-    def __init__(self, pat, child_parts, case_sensitive):
-        _Selector.__init__(self, child_parts, case_sensitive)
+    def __init__(self, pat, child_parts, flavour, case_sensitive):
+        _Selector.__init__(self, child_parts, flavour, case_sensitive)
 
     def _iterate_directories(self, parent_path, scandir):
         yield parent_path
@@ -832,9 +835,7 @@ class Path(PurePath):
             raise NotImplementedError("Non-relative patterns are unsupported")
         if pattern[-1] in (self._flavour.sep, self._flavour.altsep):
             pattern_parts.append('')
-        if case_sensitive is None:
-            case_sensitive = _is_case_sensitive(self._flavour)
-        selector = _make_selector(tuple(pattern_parts), case_sensitive)
+        selector = _make_selector(tuple(pattern_parts), self._flavour, case_sensitive)
         for p in selector.select_from(self):
             yield p
 
@@ -849,9 +850,7 @@ class Path(PurePath):
             raise NotImplementedError("Non-relative patterns are unsupported")
         if pattern and pattern[-1] in (self._flavour.sep, self._flavour.altsep):
             pattern_parts.append('')
-        if case_sensitive is None:
-            case_sensitive = _is_case_sensitive(self._flavour)
-        selector = _make_selector(("**",) + tuple(pattern_parts), case_sensitive)
+        selector = _make_selector(("**",) + tuple(pattern_parts), self._flavour, case_sensitive)
         for p in selector.select_from(self):
             yield p
 
