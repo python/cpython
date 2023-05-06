74d38eaae640eb1b3bc9cc2154bd7aed1c3fce6e
Eric Snow <ericsnowcurrently@gmail.com>
2023-05-06 15:59:30 -0600

    gh-99113: Make Sure the GIL is Acquired at the Right Places (gh-104208)
    This is a pre-requisite for a per-interpreter GIL.  Without it this change isn't strictly necessary.  However, there is no real downside otherwise.

--- Include/internal/pycore_ceval.h
+++ Include/internal/pycore_ceval.h
@@ -100,7 +100,9 @@ extern int _PyEval_ThreadsInitialized(void);
 extern PyStatus _PyEval_InitGIL(PyThreadState *tstate, int own_gil);
 extern void _PyEval_FiniGIL(PyInterpreterState *interp);
 
+extern void _PyEval_AcquireLock(PyThreadState *tstate);
 extern void _PyEval_ReleaseLock(PyThreadState *tstate);
+extern PyThreadState * _PyThreadState_SwapNoGIL(PyThreadState *);
 
 extern void _PyEval_DeactivateOpCache(void);
 
