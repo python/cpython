e786382fadc037a1261adfd5e1bb22bd2dca4df2
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2023-05-06 18:56:54 -0700

    Fix a test

--- Objects/typeobject.c
+++ Objects/typeobject.c
@@ -1458,7 +1458,7 @@ type_get_annotations(PyTypeObject *type, void *context)
 static PyObject *
 type_get_type_params(PyTypeObject *type, void *context)
 {
-    PyObject *params = PyDict_GetItem(type->tp_dict, &_Py_ID(__type_params__));
+    PyObject *params = PyDict_GetItem(lookup_tp_dict(type), &_Py_ID(__type_params__));
 
     if (params) {
         return Py_NewRef(params);
