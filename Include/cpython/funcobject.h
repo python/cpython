2a8f0efca1528d6a1a12cc85e1a8a604985d0d71
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2023-05-09 07:06:28 -0700

    Remove stray function declarations

--- Include/cpython/funcobject.h
+++ Include/cpython/funcobject.h
@@ -68,7 +68,6 @@ PyAPI_FUNC(PyObject *) PyFunction_New(PyObject *, PyObject *);
 PyAPI_FUNC(PyObject *) PyFunction_NewWithQualName(PyObject *, PyObject *, PyObject *);
 PyAPI_FUNC(PyObject *) PyFunction_GetCode(PyObject *);
 PyAPI_FUNC(PyObject *) PyFunction_GetGlobals(PyObject *);
-PyAPI_FUNC(PyObject *) PyFunction_GetClassDict(PyObject *);
 PyAPI_FUNC(PyObject *) PyFunction_GetModule(PyObject *);
 PyAPI_FUNC(PyObject *) PyFunction_GetDefaults(PyObject *);
 PyAPI_FUNC(int) PyFunction_SetDefaults(PyObject *, PyObject *);
