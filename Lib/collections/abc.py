3215237abfaaa458cf916cb78e364c28bda42d09
Alex Waygood <Alex.Waygood@Gmail.com>
2023-05-12 07:01:31 +0100

    gh-91896: Improve visibility of `ByteString` deprecation warnings (#104294)

--- Lib/collections/abc.py
+++ Lib/collections/abc.py
@@ -1,3 +1,12 @@
 from _collections_abc import *
 from _collections_abc import __all__
 from _collections_abc import _CallableGenericAlias
+
+_deprecated_ByteString = globals().pop("ByteString")
+
+def __getattr__(attr):
+    if attr == "ByteString":
+        import warnings
+        warnings._deprecated("collections.abc.ByteString", remove=(3, 14))
+        return _deprecated_ByteString
+    raise AttributeError(f"module 'collections.abc' has no attribute {attr!r}")
