f08da6663158eeb61c9db288bd78783e14933c5f
Zac Hatfield-Dodds <zac.hatfield.dodds@gmail.com>
2023-05-13 22:39:05 -0700

    gh-86275: Don't fail PBTs for running slowly

--- Lib/test/support/hypothesis_helper.py
+++ Lib/test/support/hypothesis_helper.py
@@ -2,3 +2,12 @@ try:
     import hypothesis
 except ImportError:
     from . import _hypothesis_stubs as hypothesis
+else:
+    # When using the real Hypothesis, we'll configure it to ignore occasional
+    # slow tests (avoiding flakiness from random VM slowness in CI).
+    hypothesis.settings.register_profile(
+        "slow-is-ok",
+        deadline=None,
+        suppress_health_check=[hypothesis.HealthCheck.too_slow],
+    )
+    hypothesis.settings.load_profile("slow-is-ok")
