125513affcaea3fd410f22680d157d3956a59c4c
Carl Meyer <carl@oddbird.net>
2023-05-19 20:39:48 -0700

    move headers to sysmodule.h

--- Include/osmodule.h
+++ Include/osmodule.h
@@ -11,19 +11,6 @@ extern "C" {
 PyAPI_FUNC(PyObject *) PyOS_FSPath(PyObject *path);
 #endif
 
-#if !defined(Py_LIMITED_API)
-typedef struct {
-     FILE* perf_map;
-    PyThread_type_lock map_lock;
-} PerfMapState;
-
-PyAPI_FUNC(int) PyUnstable_PerfMapState_Init(void);
-
-PyAPI_FUNC(int) PyUnstable_WritePerfMapEntry(const void *code_addr, unsigned int code_size, const char *entry_name);
-
-PyAPI_FUNC(void) PyUnstable_PerfMapState_Fini(void);
-#endif
-
 #ifdef __cplusplus
 }
 #endif
