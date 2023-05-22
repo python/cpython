93020266b7049789fc52f7450ee7ec48385f50a0
Miss Islington (bot) <31488909+miss-islington@users.noreply.github.com>
2023-05-22 03:42:37 -0700

    [3.8] gh-102153: Start stripping C0 control and space chars in `urlsplit` (GH-102508) (GH-104575) (GH-104592) (#104593)
    gh-102153: Start stripping C0 control and space chars in `urlsplit` (GH-102508)
    
    `urllib.parse.urlsplit` has already been respecting the WHATWG spec a bit GH-25595.
    
    This adds more sanitizing to respect the "Remove any leading C0 control or space from input" [rule](https://url.spec.whatwg.org/GH-url-parsing:~:text=Remove%20any%20leading%20and%20trailing%20C0%20control%20or%20space%20from%20input.) in response to [CVE-2023-24329](https://nvd.nist.gov/vuln/detail/CVE-2023-24329).
    
    I simplified the docs by eliding the state of the world explanatory
    paragraph in this security release only backport.  (people will see
    that in the mainline /3/ docs)
    
    (cherry picked from commit d7f8a5fe07b0ff3a419ccec434cc405b21a5a304)
    (cherry picked from commit 2f630e1ce18ad2e07428296532a68b11dc66ad10)
    (cherry picked from commit 610cc0ab1b760b2abaac92bd256b96191c46b941)
    (cherry picked from commit f48a96a28012d28ae37a2f4587a780a5eb779946)
    
    Co-authored-by: Illia Volochii <illia.volochii@gmail.com>
    Co-authored-by: Gregory P. Smith [Google] <greg@krypto.org>

--- Lib/urllib/parse.py
+++ Lib/urllib/parse.py
@@ -25,6 +25,10 @@ currently not entirely compliant with this RFC due to defacto
 scenarios for parsing, and for backward compatibility purposes, some
 parsing quirks from older RFCs are retained. The testcases in
 test_urlparse.py provides a good indicator of parsing behavior.
+
+The WHATWG URL Parser spec should also be considered.  We are not compliant with
+it either due to existing user code API behavior expectations (Hyrum's Law).
+It serves as a useful guide when making changes.
 """
 
 import re
@@ -76,6 +80,10 @@ scheme_chars = ('abcdefghijklmnopqrstuvwxyz'
                 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
                 '0123456789'
                 '+-.')
+
+# Leading and trailing C0 control and space to be stripped per WHATWG spec.
+# == "".join([chr(i) for i in range(0, 0x20 + 1)])
+_WHATWG_C0_CONTROL_OR_SPACE = '\x00\x01\x02\x03\x04\x05\x06\x07\x08\t\n\x0b\x0c\r\x0e\x0f\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1a\x1b\x1c\x1d\x1e\x1f '
 
 # Unsafe bytes to be removed per WHATWG spec
 _UNSAFE_URL_BYTES_TO_REMOVE = ['\t', '\r', '\n']
@@ -431,6 +439,10 @@ def urlsplit(url, scheme='', allow_fragments=True):
     url, scheme, _coerce_result = _coerce_args(url, scheme)
     url = _remove_unsafe_bytes_from_url(url)
     scheme = _remove_unsafe_bytes_from_url(scheme)
+    # Only lstrip url as some applications rely on preserving trailing space.
+    # (https://url.spec.whatwg.org/#concept-basic-url-parser would strip both)
+    url = url.lstrip(_WHATWG_C0_CONTROL_OR_SPACE)
+    scheme = scheme.strip(_WHATWG_C0_CONTROL_OR_SPACE)
     allow_fragments = bool(allow_fragments)
     key = url, scheme, allow_fragments, type(url), type(scheme)
     cached = _parse_cache.get(key, None)
