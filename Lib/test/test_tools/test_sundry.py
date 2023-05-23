813cbbb88481e1555865477b55a1c9094deb7ad7
Victor Stinner <vstinner@python.org>
2023-05-23 04:32:43 +0200

    Remove outdated test_sundry.py comment

--- Lib/test/test_tools/test_sundry.py
+++ Lib/test/test_tools/test_sundry.py
@@ -14,9 +14,6 @@ from test.test_tools import scriptsdir, import_tool, skip_if_missing
 skip_if_missing()
 
 class TestSundryScripts(unittest.TestCase):
-    # At least make sure the rest don't have syntax errors.  When tests are
-    # added for a script it should be added to the allowlist below.
-
     # import logging registers "atfork" functions which keep indirectly the
     # logging module dictionary alive. Mock the function to be able to unload
     # cleanly the logging module.
