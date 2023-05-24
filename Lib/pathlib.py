f257e4826e93ed192854e0c57d267c806229db87
barneygale <barney.gale@gmail.com>
2023-05-24 16:05:43 +0100

    Revert "Document __bytes__ in PurePath docstring"
    This reverts commit d1b23c88334136b70e2c12476ff6839fdbc20ee1.

--- Lib/pathlib.py
+++ Lib/pathlib.py
@@ -674,11 +674,6 @@ class PurePath(_BasePurePath):
     instantiating a PurePath will return either a PurePosixPath or a
     PureWindowsPath object.  You can also instantiate either of these classes
     directly, regardless of your system.
-
-    On Posix, calling ``bytes(path)`` gives the raw filesystem path as
-    a bytes object, as encoded by ``os.fsencode()``. On Windows, the unicode
-    form is the canonical representation of fileysstem paths, and so calling
-    ``bytes()`` on a path is not recommended.
     """
     __slots__ = ()
 
