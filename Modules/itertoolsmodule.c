168700523b3ce6c8808c273b1fa7aaa7fcb65e05
Raymond Hettinger <raymond.hettinger@gmail.com>
2023-05-26 01:20:37 -0500

    Expand warning message to cover copy and deepcopy as well

--- Modules/itertoolsmodule.c
+++ Modules/itertoolsmodule.c
@@ -95,11 +95,12 @@ class itertools.pairwise "pairwiseobject *" "clinic_state()->pairwise_type"
 
 /* Deprecation of pickle support:  GH-101588 *********************************/
 
-#define ITERTOOL_PICKLE_DEPRECATION                                                        \
-    if (PyErr_WarnEx(                                                               \
-            PyExc_DeprecationWarning,                                               \
-            "Itertool pickle support will be removed in a Python 3.14.", 1) < 0) {  \
-        Py_RETURN_NONE;                                                             \
+#define ITERTOOL_PICKLE_DEPRECATION                                           \
+    if (PyErr_WarnEx(                                                         \
+            PyExc_DeprecationWarning,                                         \
+            "Itertool pickle/copy/deepcopy support "                          \
+            "will be removed in a Python 3.14.", 1) < 0) {                    \
+        Py_RETURN_NONE;                                                       \
     }
 
 /* batched object ************************************************************/
