b7ceb3c6c6e8b1e82d9690335cfed826d2656a00
Tomas R <tomas.roun8@gmail.com>
2023-05-26 08:34:17 +0200

    gh-104943: Remove mentions of old Python versions (#104945)

--- Lib/typing.py
+++ Lib/typing.py
@@ -2709,7 +2709,7 @@ class NamedTupleMeta(type):
 def NamedTuple(typename, fields=None, /, **kwargs):
     """Typed version of namedtuple.
 
-    Usage in Python versions >= 3.6::
+    Usage::
 
         class Employee(NamedTuple):
             name: str
@@ -2726,9 +2726,6 @@ def NamedTuple(typename, fields=None, /, **kwargs):
 
         Employee = NamedTuple('Employee', name=str, id=int)
 
-    In Python versions <= 3.5 use::
-
-        Employee = NamedTuple('Employee', [('name', str), ('id', int)])
     """
     if fields is None:
         fields = kwargs.items()
