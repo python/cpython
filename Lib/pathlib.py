0e89d55e3d26a989dd4f842b8edbf2e8e228b0c2
Barney Gale <barney.gale@gmail.com>
2023-05-29 22:18:20 +0100

    Update Lib/pathlib.py
    Co-authored-by: Alex Waygood <Alex.Waygood@Gmail.com>

--- Lib/pathlib.py
+++ Lib/pathlib.py
@@ -715,7 +715,8 @@ class PurePath:
                 return False
         return True
 
-# Subclassing os.PathLike makes __instancecheck__ slower. Register instead!
+# Subclassing os.PathLike makes isinstance() checks slower,
+# which in turn makes Path construction slower. Register instead!
 os.PathLike.register(PurePath)
 
 
