cda1bd3c9d3b2cecdeeba0c498cd2df83fbdb535
Steve Dower <steve.dower@python.org>
2023-05-30 11:00:29 +0100

    gh-88745: Add _winapi.CopyFile2 and update shutil.copy2 to use it (GH-105055)

--- Include/internal/pycore_runtime_init_generated.h
+++ Include/internal/pycore_runtime_init_generated.h
@@ -913,6 +913,7 @@ extern "C" {
     INIT_ID(exc_value), \
     INIT_ID(excepthook), \
     INIT_ID(exception), \
+    INIT_ID(existing_file_name), \
     INIT_ID(exp), \
     INIT_ID(extend), \
     INIT_ID(extra_tokens), \
@@ -1065,6 +1066,7 @@ extern "C" {
     INIT_ID(namespaces), \
     INIT_ID(narg), \
     INIT_ID(ndigits), \
+    INIT_ID(new_file_name), \
     INIT_ID(new_limit), \
     INIT_ID(newline), \
     INIT_ID(newlines), \
@@ -1119,6 +1121,7 @@ extern "C" {
     INIT_ID(priority), \
     INIT_ID(progress), \
     INIT_ID(progress_handler), \
+    INIT_ID(progress_routine), \
     INIT_ID(proto), \
     INIT_ID(protocol), \
     INIT_ID(ps1), \
