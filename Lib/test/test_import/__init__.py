695fd16ae82064c57dcc57298d109e3a6612b575
sunmy2019 <59365878+sunmy2019@users.noreply.github.com>
2023-06-01 10:29:44 +0800

    rebase after changes to #105085

--- Lib/test/test_import/__init__.py
+++ Lib/test/test_import/__init__.py
@@ -2327,8 +2327,6 @@ class SinglephaseInitTests(unittest.TestCase):
             self.assertIs(basic.look_up_self(), basic_lookedup)
             self.assertEqual(basic.initialized_count(), expected_init_count)
 
-            loaded.module._clear_module_state()
-
     def test_basic_reloaded(self):
         # m_copy is copied into the existing module object.
         # Global state is not changed.
@@ -2416,11 +2414,6 @@ class SinglephaseInitTests(unittest.TestCase):
                                        loaded.snapshot.state_initialized)
 
                 self.assertIs(reloaded.snapshot.cached, reloaded.module)
-
-                if hasattr(loaded.module, '_clear_module_state'):
-                    loaded.module._clear_module_state()
-                if hasattr(reloaded.module, '_clear_module_state'):
-                    reloaded.module._clear_module_state()
 
     # Currently, for every single-phrase init module loaded
     # in multiple interpreters, those interpreters share a
