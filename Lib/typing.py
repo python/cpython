3c5f0eadd800c2d66f5249398a8f6ef72972fbd0
Miss Islington (bot) <31488909+miss-islington@users.noreply.github.com>
2023-06-08 08:12:35 -0700

    [3.12] Further improve docs for `typing.Annotated` (GH-105498) (#105503)
    Further improve docs for `typing.Annotated` (GH-105498)
    (cherry picked from commit d213c2990f0db62ed39fc5468977f989c2658675)
    
    Co-authored-by: Alex Waygood <Alex.Waygood@Gmail.com>

--- Lib/typing.py
+++ Lib/typing.py
@@ -2021,7 +2021,7 @@ class Annotated:
 
         assert Annotated[int, '$'].__metadata__ == ('$',)
 
-    - Nested Annotated are flattened::
+    - Nested Annotated types are flattened::
 
         assert Annotated[Annotated[T, Ann1, Ann2], Ann3] == Annotated[T, Ann1, Ann2, Ann3]
 
@@ -2032,15 +2032,17 @@ class Annotated:
 
     - Annotated can be used as a generic type alias::
 
-        Optimized = Annotated[T, runtime.Optimize()]
-        assert Optimized[int] == Annotated[int, runtime.Optimize()]
+        type Optimized[T] = Annotated[T, runtime.Optimize()]
+        # type checker will treat Optimized[int]
+        # as equivalent to Annotated[int, runtime.Optimize()]
 
-        OptimizedList = Annotated[List[T], runtime.Optimize()]
-        assert OptimizedList[int] == Annotated[List[int], runtime.Optimize()]
+        type OptimizedList[T] = Annotated[list[T], runtime.Optimize()]
+        # type checker will treat OptimizedList[int]
+        # as equivalent to Annotated[list[int], runtime.Optimize()]
 
     - Annotated cannot be used with an unpacked TypeVarTuple::
 
-        Annotated[*Ts, Ann1]  # NOT valid
+        type Variadic[*Ts] = Annotated[*Ts, Ann1]  # NOT valid
 
       This would be equivalent to::
 
