8c28ddb1561655229c9c5a7e30b92b4da963855a
Erlend E. Aasland <erlend.aasland@protonmail.com>
2023-06-09 21:41:30 +0200

    gh-105375: Improve error handling in _ctypes
    Prevent repeated PyLong_FromVoidPtr() from possibly overwriting the
    current exception.

--- Modules/_ctypes/callbacks.c
+++ Modules/_ctypes/callbacks.c
@@ -479,13 +479,20 @@ long Call_GetClassObject(REFCLSID rclsid, REFIID riid, LPVOID *ppv)
 
     {
         PyObject *py_rclsid = PyLong_FromVoidPtr((void *)rclsid);
+        if (py_rclsid == NULL) {
+            PyErr_WriteUnraisable(context ? context : Py_None);
+            return E_FAIL;
+        }
         PyObject *py_riid = PyLong_FromVoidPtr((void *)riid);
+        if (py_riid == NULL) {
+            Py_DECREF(py_rclsid);
+            PyErr_WriteUnraisable(context ? context : Py_None);
+            return E_FAIL;
+        }
         PyObject *py_ppv = PyLong_FromVoidPtr(ppv);
-        if (!py_rclsid || !py_riid || !py_ppv) {
-            Py_XDECREF(py_rclsid);
-            Py_XDECREF(py_riid);
-            Py_XDECREF(py_ppv);
-            Py_DECREF(func);
+        if (py_ppv == NULL) {
+            Py_DECREF(py_rclsid);
+            Py_DECREF(py_riid);
             PyErr_WriteUnraisable(context ? context : Py_None);
             return E_FAIL;
         }
