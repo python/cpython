15c591572c0f8ce97d2fd105d19011cf3980a5b1
Nikita Sobolev <mail@sobolevn.me>
2023-06-09 11:52:58 +0300

    gh-105545: Remove deprecated `MacOSXOSAScript._name` (gh-105546)

--- Lib/webbrowser.py
+++ Lib/webbrowser.py
@@ -578,20 +578,6 @@ if sys.platform == 'darwin':
         def __init__(self, name='default'):
             super().__init__(name)
 
-        @property
-        def _name(self):
-            warnings.warn(f'{self.__class__.__name__}._name is deprecated in 3.11'
-                          f' use {self.__class__.__name__}.name instead.',
-                          DeprecationWarning, stacklevel=2)
-            return self.name
-
-        @_name.setter
-        def _name(self, val):
-            warnings.warn(f'{self.__class__.__name__}._name is deprecated in 3.11'
-                          f' use {self.__class__.__name__}.name instead.',
-                          DeprecationWarning, stacklevel=2)
-            self.name = val
-
         def open(self, url, new=0, autoraise=True):
             if self.name == 'default':
                 script = 'open location "%s"' % url.replace('"', '%22') # opens in default browser
