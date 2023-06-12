5609f6490fa2298d2370c6770b4160237748edb9
Carl Meyer <carl@oddbird.net>
2023-06-12 15:57:21 -0700

    gh-105340: include hidden fast-locals in locals()

--- Include/cpython/ceval.h
+++ Include/cpython/ceval.h
@@ -31,3 +31,5 @@ _PyEval_RequestCodeExtraIndex(freefunc f) {
 
 PyAPI_FUNC(int) _PyEval_SliceIndex(PyObject *, Py_ssize_t *);
 PyAPI_FUNC(int) _PyEval_SliceIndexNotNone(PyObject *, Py_ssize_t *);
+
+PyAPI_FUNC(PyObject *) PyEval_GetFrameLocals(void);
