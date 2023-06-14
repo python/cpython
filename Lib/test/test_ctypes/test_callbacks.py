c57187b245e874c0ad71aeb6f3db519fa4b002b9
Victor Stinner <vstinner@python.org>
2023-06-15 00:53:31 +0200

    gh-105751, test_ctypes: Remove disabled tests
    * The following tests were disabled since the initial ctypes commit
      in 2006, commit babddfca758abe34ff12023f63b18d745fae7ca9:
    
      * Callbacks.test_char_p()
      * DeletePointerTestCase.test_X()
      * NumberTestCase.test_perf()
      * StructureTestCase.test_subclass_creation()
      * Tests.test_X() of test_byteswap
    
    * NumberTestCase.test_bool_from_address() was disabled in 2007 by
      commit 5dc4fe09b7648f9801558e766b21a3d3b2dcad3b.
    * Remove check_perf() and run_test() of test_numbers.

--- Lib/test/test_ctypes/test_callbacks.py
+++ Lib/test/test_ctypes/test_callbacks.py
@@ -8,7 +8,7 @@ import unittest
 from _ctypes import CTYPES_MAX_ARGCOUNT
 from ctypes import (CDLL, cdll, Structure, CFUNCTYPE,
                     ArgumentError, POINTER, sizeof,
-                    c_byte, c_ubyte, c_char, c_char_p,
+                    c_byte, c_ubyte, c_char,
                     c_short, c_ushort, c_int, c_uint,
                     c_long, c_longlong, c_ulonglong, c_ulong,
                     c_float, c_double, c_longdouble, py_object)
@@ -91,14 +91,6 @@ class Callbacks(unittest.TestCase):
     def test_char(self):
         self.check_type(c_char, b"x")
         self.check_type(c_char, b"a")
-
-    # disabled: would now (correctly) raise a RuntimeWarning about
-    # a memory leak.  A callback function cannot return a non-integral
-    # C type without causing a memory leak.
-    @unittest.skip('test disabled')
-    def test_char_p(self):
-        self.check_type(c_char_p, "abc")
-        self.check_type(c_char_p, "def")
 
     def test_pyobject(self):
         o = ()
