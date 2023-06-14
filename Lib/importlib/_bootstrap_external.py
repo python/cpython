1d857da7f0e4858e561223f319ae5afe737d5657
Mark Shannon <mark@hotpy.org>
2023-06-14 16:15:08 +0100

    GH-77273: Better bytecodes for f-strings (GH-6132)

--- Lib/importlib/_bootstrap_external.py
+++ Lib/importlib/_bootstrap_external.py
@@ -447,7 +447,9 @@ _code_type = type(_write_atomic.__code__)
 #     Python 3.12b1 3531 (Add PEP 695 changes)
 #     Python 3.13a1 3550 (Plugin optimizer support)
 #     Python 3.13a1 3551 (Compact superinstructions)
-#     Python 3.13a1 3552 (Add SET_FUNCTION_ATTRIBUTE)
+#     Python 3.13a1 3552 (Remove LOAD_FAST__LOAD_CONST and LOAD_CONST__LOAD_FAST)
+#     Python 3.13a1 3553 (Add SET_FUNCTION_ATTRIBUTE)
+#     Python 3.13a1 3554 (more efficient bytecodes for f-strings)
 
 #     Python 3.14 will start with 3600
 
@@ -464,7 +466,7 @@ _code_type = type(_write_atomic.__code__)
 # Whenever MAGIC_NUMBER is changed, the ranges in the magic_values array
 # in PC/launcher.c must also be updated.
 
-MAGIC_NUMBER = (3552).to_bytes(2, 'little') + b'\r\n'
+MAGIC_NUMBER = (3554).to_bytes(2, 'little') + b'\r\n'
 
 _RAW_MAGIC_NUMBER = int.from_bytes(MAGIC_NUMBER, 'little')  # For import.c
 
