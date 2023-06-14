a2d6407edc72b08012be95f60d5f5d36c22b380e
Victor Stinner <vstinner@python.org>
2023-06-14 05:07:21 +0200

    Add missing import

--- Lib/test/test_ctypes/test_numbers.py
+++ Lib/test/test_ctypes/test_numbers.py
@@ -5,7 +5,7 @@ import unittest
 from array import array
 from operator import truth
 from ctypes import (byref, sizeof, alignment, _SimpleCData,
-                    c_byte, c_ubyte, c_short, c_ushort, c_int, c_uint,
+                    c_char, c_byte, c_ubyte, c_short, c_ushort, c_int, c_uint,
                     c_long, c_ulong, c_longlong, c_ulonglong,
                     c_float, c_double, c_longdouble, c_bool)
 
