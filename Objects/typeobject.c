4224df2355f5e1575156af2be9cd0f58f5890306
Serhiy Storchaka <storchaka@gmail.com>
2023-06-23 20:10:32 +0300

    [3.12] gh-106033: Get rid of new occurrences of PyDict_GetItem and PyObject_HasAttr (GH-106034)
    These functions are broken by design because they discard any exceptions raised
    inside, including MemoryError and KeyboardInterrupt.  They should not be
    used in new code.
    (cherry picked from commit 1d33d5378058671bfabb6f4d4b5bfd4726973ff9)
    
    Co-authored-by: Serhiy Storchaka <storchaka@gmail.com>

--- Objects/typeobject.c
+++ Objects/typeobject.c
@@ -1507,10 +1507,13 @@ type_set_annotations(PyTypeObject *type, PyObject *value, void *context)
 static PyObject *
 type_get_type_params(PyTypeObject *type, void *context)
 {
-    PyObject *params = PyDict_GetItem(lookup_tp_dict(type), &_Py_ID(__type_params__));
+    PyObject *params = PyDict_GetItemWithError(lookup_tp_dict(type), &_Py_ID(__type_params__));
 
     if (params) {
         return Py_NewRef(params);
+    }
+    if (PyErr_Occurred()) {
+        return NULL;
     }
 
     return PyTuple_New(0);
