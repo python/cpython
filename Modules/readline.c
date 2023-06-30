15a0296c0d34aae4a747ad8052beb21c2ab4d128
Dong-hee Na <donghee.na@python.org>
2023-06-30 23:36:17 +0900

    gh-105323: Suppress function pointer warning by type casting

--- Modules/readline.c
+++ Modules/readline.c
@@ -1249,9 +1249,9 @@ setup_readline(readlinestate *mod_state)
     sigwinch_ohandler = PyOS_setsig(SIGWINCH, readline_sigwinch_handler);
 #endif
     /* Set our hook functions */
-    rl_startup_hook = on_startup_hook;
+    rl_startup_hook = (Function *)on_startup_hook;
 #ifdef HAVE_RL_PRE_INPUT_HOOK
-    rl_pre_input_hook = on_pre_input_hook;
+    rl_pre_input_hook = (Function *)on_pre_input_hook;
 #endif
     /* Set our completion function */
     rl_attempted_completion_function = flex_complete;
