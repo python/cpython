cd5d675373565347ce828230dd06baa45f62e3c7
Erlend E. Aasland <erlend@python.org>
2023-07-03 13:04:14 +0200

    More accurate type for self.self_converter

--- Tools/clinic/clinic.py
+++ Tools/clinic/clinic.py
@@ -38,6 +38,7 @@ from typing import (
     TypeGuard,
     overload,
 )
+from __future__ import annotations
 
 # TODO:
 #
@@ -2467,7 +2468,7 @@ class Function:
         self.docstring = docstring or ''
         self.kind = kind
         self.coexist = coexist
-        self.self_converter: CConverter | None = None
+        self.self_converter: self_converter | None = None
         # docstring_only means "don't generate a machine-readable
         # signature, just a normal docstring".  it's True for
         # functions with optional groups because we can't represent
