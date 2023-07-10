c0f039337cb9fa5fd45bf7a11531c78236b89789
Dong-hee Na <donghee.na92@gmail.com>
2023-07-11 07:27:50 +0900

    Update Lib/test/test_compile.py
    Co-authored-by: Carl Meyer <carl@oddbird.net>

--- Lib/test/test_compile.py
+++ Lib/test/test_compile.py
@@ -1189,7 +1189,7 @@ class TestSpecifics(unittest.TestCase):
     def test_variable_dependent(self):
         # gh-104635: Since the value of b is dependent on the value of a
         # the first STORE_FAST for a should not be skipped. (e.g POP_TOP).
-        # This test case is added for prevent future regression from aggresive optimization.
+        # This test case is added to prevent potential regression from aggressive optimization.
         def f():
             a = 42; b = a + 54; a = 54
             return a, b
