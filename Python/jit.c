01a120e612e70feb7369080aa369b791b73ce4bb
Brandt Bucher <brandtbucher@microsoft.com>
2023-07-13 15:53:55 -0700

    fixup

--- Python/jit.c
+++ Python/jit.c
@@ -68,7 +68,7 @@ patch_one(unsigned char *location, HoleKind kind, uint64_t value, uint64_t adden
             assert(((instruction & 0x3B000000) == 0x39000000) ||
                    ((instruction & 0x11C00000) == 0x11000000));
             if ((value + addend) & 0x7) {  // XXX: Remote debugging info.
-                printf("PATCH_ABS_12: possibly unaligned value %llu + %llu (at %p)\n", value, addend, location);
+                printf("PATCH_ABS_12: possibly unaligned value %lu + %lu (at %p)\n", value, addend, location);
             };
             value = (value + addend) & ((1 << 12) - 1);
             int implicit_shift = 0;
