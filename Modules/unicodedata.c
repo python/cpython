04e1ef430393954168e30d97feb17bdc38235d7c
Serhiy Storchaka <storchaka@gmail.com>
2023-07-18 23:59:53 +0300

    gh-86493: Use PyModule_Add() instead of PyModule_AddObjectRef() (GH-106860)

--- Modules/unicodedata.c
+++ Modules/unicodedata.c
@@ -1496,13 +1496,7 @@ unicodedata_exec(PyObject *module)
     }
 
     /* Export C API */
-    PyObject *capsule = unicodedata_create_capi();
-    if (capsule == NULL) {
-        return -1;
-    }
-    int rc = PyModule_AddObjectRef(module, "_ucnhash_CAPI", capsule);
-    Py_DECREF(capsule);
-    if (rc < 0) {
+    if (PyModule_Add(module, "_ucnhash_CAPI", unicodedata_create_capi()) < 0) {
         return -1;
     }
     return 0;
