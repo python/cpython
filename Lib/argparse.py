a48b9fefa38e11c1c1b10333bc970b8ca9696a21
Rodrigo Girão Serrão <5621605+rodrigogiraoserrao@users.noreply.github.com>
2023-07-23 22:02:52 +0200

    Use f-strings and add explicit length validation
    The formatting done with %-style formatting was being used to check if the length of the metavar matched the action.nargs type.
    This TypeError was then caught in one of the call sites of _format_args and used to raise a user-facing ValueError.
    This change uses the match statement to make this validation explicit and then raises a TypeError in case the validation fails so that the remainder of the code works the same way.

--- Lib/argparse.py
+++ Lib/argparse.py
@@ -598,30 +598,36 @@ class HelpFormatter(object):
 
     def _format_args(self, action, default_metavar):
         get_metavar = self._metavar_formatter(action, default_metavar)
-        if action.nargs is None:
-            result = '%s' % get_metavar(1)
-        elif action.nargs == OPTIONAL:
-            result = '[%s]' % get_metavar(1)
-        elif action.nargs == ZERO_OR_MORE:
-            metavar = get_metavar(1)
-            if len(metavar) == 2:
-                result = '[%s [%s ...]]' % metavar
-            else:
-                result = '[%s ...]' % metavar
-        elif action.nargs == ONE_OR_MORE:
-            result = '%s [%s ...]' % get_metavar(2)
-        elif action.nargs == REMAINDER:
-            result = '...'
-        elif action.nargs == PARSER:
-            result = '%s ...' % get_metavar(1)
-        elif action.nargs == SUPPRESS:
-            result = ''
-        else:
-            try:
-                formats = ['%s' for _ in range(action.nargs)]
-            except TypeError:
-                raise ValueError("invalid nargs value") from None
-            result = ' '.join(formats) % get_metavar(action.nargs)
+        metavar = get_metavar(1)
+        match (action.nargs, metavar):
+            case (None, (var,)):
+                result = f'{var}'
+            case ('?', (var,)):  # OPTIONAL
+                result = f'[{var}]'
+            case ('*', (var,)):  # ZERO_OR_MORE
+                result = f'[{var} ...]'
+            case ('*', (var1, var2)):  # ZERO_OR_MORE
+                result = f'[{var1} [{var2} ...]]'
+            case ('+', _) if len(_metavar := get_metavar(2)) == 2:  # ONE_OR_MORE
+                result = f'{_metavar[0]} [{_metavar[1]} ...]'
+            case ('...', _):  # REMAINDER
+                result = '...'
+            case ('A...', (var,)):  # PARSER
+                result = f'{var} ...'
+            case ('==SUPPRESS==', _):  # SUPPRESS
+                result = ''
+            case (nargs, _) if nargs in {
+                None, OPTIONAL, ZERO_OR_MORE, ONE_OR_MORE, REMAINDER, PARSER, SUPPRESS
+            }:
+                raise TypeError('nargs and metavar do not match')
+            case _:
+                if not isinstance(action.nargs, int):
+                    raise ValueError('invalid nargs value') from None
+                metavar = get_metavar(action.nargs)
+                if len(metavar) != action.nargs:
+                    raise TypeError('nargs and metavar lengths do not match')
+                result = ' '.join([str(var) for var in metavar])
+
         return result
 
     def _expand_help(self, action):
