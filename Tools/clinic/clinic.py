ff5dad7b0f3aaeb3d9e3955bf860fa97afb5aa6b
AlexWaygood <alex.waygood@gmail.com>
2023-07-27 11:46:28 +0100

    Argument clinic: cleanup `state_modulename_name()`

--- Tools/clinic/clinic.py
+++ Tools/clinic/clinic.py
@@ -4743,23 +4743,21 @@ class DSLParser:
         return_converter = None
         if returns:
             ast_input = f"def x() -> {returns}: pass"
-            module = None
             try:
-                module = ast.parse(ast_input)
+                module_node = ast.parse(ast_input)
             except SyntaxError:
-                pass
-            if not module:
                 fail("Badly-formed annotation for " + full_name + ": " + returns)
+            function_node = module_node.body[0]
+            assert isinstance(function_node, ast.FunctionDef)
             try:
-                name, legacy, kwargs = self.parse_converter(module.body[0].returns)
+                name, legacy, kwargs = self.parse_converter(function_node.returns)
                 if legacy:
-                    fail("Legacy converter {!r} not allowed as a return converter"
-                         .format(name))
+                    fail(f"Legacy converter {name!r} not allowed as a return converter")
                 if name not in return_converters:
-                    fail("No available return converter called " + repr(name))
+                    fail(f"No available return converter called {name!r}")
                 return_converter = return_converters[name](**kwargs)
             except ValueError:
-                fail("Badly-formed annotation for " + full_name + ": " + returns)
+                fail(f"Badly-formed annotation for {full_name}: {returns}")
 
         fields = [x.strip() for x in full_name.split('.')]
         function_name = fields.pop()
