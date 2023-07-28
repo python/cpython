51b302e3f322eba7933da87ff1e6dd462c925859
Miss Islington (bot) <31488909+miss-islington@users.noreply.github.com>
2023-07-27 17:36:34 -0700

    [3.11] gh-106368: Argument clinic tests: improve failure message when tests in `ClinicExternalTests` fail (GH-107364) (#107366)
    gh-106368: Argument clinic tests: improve failure message when tests in `ClinicExternalTests` fail (GH-107364)
    (cherry picked from commit 76c26eaca4147ba7e3e8d7379c5a828f0b512a46)
    
    Co-authored-by: Alex Waygood <Alex.Waygood@Gmail.com>

--- Lib/test/test_clinic.py
+++ Lib/test/test_clinic.py
@@ -1306,7 +1306,7 @@ class ClinicExternalTest(TestCase):
         ) as proc:
             proc.wait()
             if expect_success and proc.returncode:
-                self.fail("".join(proc.stderr))
+                self.fail("".join([*proc.stdout, *proc.stderr]))
             stdout = proc.stdout.read()
             stderr = proc.stderr.read()
             # Clinic never writes to stderr.
