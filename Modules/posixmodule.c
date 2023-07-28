c4dba235887e860131a89df0b01cc41dc43317bb
justdan6 <134341009+justdan6@users.noreply.github.com>
2023-07-28 16:08:43 -0600

    [3.12] gh-106881: Check for linux/limits.h before including it (#107397)
    * Check for linux/limits.h before including it
    
    Co-authored-by: Erlend E. Aasland <erlend.aasland@protonmail.com>
    (cherry picked from commit 11c055f5ff1a353de6d2e77f2af24aaa782878ba)

--- Modules/posixmodule.c
+++ Modules/posixmodule.c
@@ -286,7 +286,7 @@ corresponding Unix manual entries for more information on calls.");
 #  undef HAVE_SCHED_SETAFFINITY
 #endif
 
-#if defined(HAVE_SYS_XATTR_H) && defined(__linux__) && !defined(__FreeBSD_kernel__) && !defined(__GNU__)
+#if defined(HAVE_SYS_XATTR_H) && defined(HAVE_LINUX_LIMITS_H) && !defined(__FreeBSD_kernel__) && !defined(__GNU__)
 #  define USE_XATTRS
 #  include <linux/limits.h>  // Needed for XATTR_SIZE_MAX on musl libc.
 #endif
