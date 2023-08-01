ed8a2b3a1714cc7ee407c10b7eed2fb2f0814b2b
CharlieZhao <zhaoyu_hit@qq.com>
2023-08-01 15:31:44 +0800

    Move `minalloc_is_set` outside

--- Modules/_decimal/_decimal.c
+++ Modules/_decimal/_decimal.c
@@ -5876,12 +5876,7 @@ error:
     return NULL;
 }
 
-
-/* Suppress the warning caused by multi-phase initialization */
-__attribute__((constructor)) void minalloc_init(void)
-{
-    mpd_setminalloc(_Py_DEC_MINALLOC);
-}
+static int minalloc_is_set = 0;
 
 static int
 _decimal_exec(PyObject *m)
@@ -5904,6 +5899,12 @@ _decimal_exec(PyObject *m)
     mpd_reallocfunc = PyMem_Realloc;
     mpd_callocfunc = mpd_callocfunc_em;
     mpd_free = PyMem_Free;
+
+    /* Suppress the warning caused by multi-phase initialization */
+    if (minalloc_is_set) {
+        mpd_setminalloc(_Py_DEC_MINALLOC);
+        minalloc_is_set = 1;
+    }
 
     decimal_state *state = get_module_state(m);
 
