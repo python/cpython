2d7a150650c889d90fe7d6cf85b43a9c93a0eccf
Kumar Aditya <59607654+kumaraditya303@users.noreply.github.com>
2023-08-03 05:47:09 +0000

    fix test_tools refleak

--- Lib/test/test_tools/test_sundry.py
+++ Lib/test/test_tools/test_sundry.py
@@ -7,29 +7,19 @@ tests of their own.
 
 import os
 import unittest
-from test.support import import_helper
 
 from test.test_tools import scriptsdir, import_tool, skip_if_missing
 
 skip_if_missing()
 
 class TestSundryScripts(unittest.TestCase):
-    # import logging registers "atfork" functions which keep indirectly the
-    # logging module dictionary alive. Mock the function to be able to unload
-    # cleanly the logging module.
-    @import_helper.mock_register_at_fork
-    def test_sundry(self, mock_os):
-        old_modules = import_helper.modules_setup()
-        try:
-            for fn in os.listdir(scriptsdir):
-                if not fn.endswith('.py'):
-                    continue
 
-                name = fn[:-3]
-                import_tool(name)
-        finally:
-            # Unload all modules loaded in this test
-            import_helper.modules_cleanup(*old_modules)
+    def test_sundry(self,):
+        for fn in os.listdir(scriptsdir):
+            if not fn.endswith('.py'):
+                continue
+            name = fn[:-3]
+            import_tool(name)
 
 
 if __name__ == '__main__':
