f609dbcdbd032fabab5b6978178e101984334664
Kumar Aditya <59607654+kumaraditya303@users.noreply.github.com>
2023-08-03 05:51:25 +0000

    mock fork handler

--- Lib/test/test_tools/test_sundry.py
+++ Lib/test/test_tools/test_sundry.py
@@ -7,14 +7,18 @@ tests of their own.
 
 import os
 import unittest
+from test.support import import_helper
 
 from test.test_tools import scriptsdir, import_tool, skip_if_missing
 
 skip_if_missing()
 
 class TestSundryScripts(unittest.TestCase):
-
-    def test_sundry(self,):
+    # import logging registers "atfork" functions which keep indirectly the
+    # logging module dictionary alive. Mock the function to be able to unload
+    # cleanly the logging module.
+    @import_helper.mock_register_at_fork
+    def test_sundry(self, mock_os):
         for fn in os.listdir(scriptsdir):
             if not fn.endswith('.py'):
                 continue
