06a470d85007410156609b27ecbc9b3300173505
Guido van Rossum <guido@python.org>
2023-08-09 12:53:14 -0700

    DO NOT MERGE: Always use -Xuops

--- Python/pylifecycle.c
+++ Python/pylifecycle.c
@@ -1198,6 +1198,7 @@ init_interp_main(PyThreadState *tstate)
         if (_Py_get_xoption(&config->xoptions, L"uops") != NULL) {
             enabled = 1;
         }
+        enabled = 1;  // TEMPORARY: always enable
         if (enabled) {
             PyObject *opt = PyUnstable_Optimizer_NewUOpOptimizer();
             if (opt == NULL) {
