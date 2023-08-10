3d16a48e4f53087c52ecb703fdfcbf98c6111fc8
Marc Mueller <30130371+cdce8p@users.noreply.github.com>
2023-08-10 17:11:21 +0200

    Adjust DeprecationWarning

--- Objects/typeobject.c
+++ Objects/typeobject.c
@@ -4264,8 +4264,8 @@ _PyType_FromMetaclass_impl(
         if (_allow_tp_new) {
             if (PyErr_WarnFormat(
                     PyExc_DeprecationWarning, 1,
-                    "Type %s uses PyType_Spec with metaclasses that have custom "
-                    "tp_new is deprecated and will no longer be allowed in "
+                    "Type %s uses PyType_Spec with a metaclass that has custom "
+                    "tp_new. This is deprecated and will no longer be allowed in "
                     "Python 3.14.", spec->name) < 0) {
                 goto finally;
             }
