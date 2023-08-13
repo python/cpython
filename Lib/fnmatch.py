75129c8dbe3c6969d08ad2819d1ca4d1f0124a6c
barneygale <barney.gale@gmail.com>
2023-08-13 21:33:44 +0100

    Deduplicate code to handle character sets

--- Lib/fnmatch.py
+++ Lib/fnmatch.py
@@ -71,6 +71,50 @@ def fnmatchcase(name, pat):
     return match(name) is not None
 
 
+def _translate_char_set(stuff):
+    if '-' not in stuff:
+        stuff = stuff.replace('\\', r'\\')
+    else:
+        chunks = []
+        i = 0
+        k = 2 if stuff[0] == '!' else 1
+        while True:
+            k = stuff.find('-', k)
+            if k < 0:
+                break
+            chunks.append(stuff[i:k])
+            i = k + 1
+            k = k + 3
+        chunk = stuff[i:]
+        if chunk:
+            chunks.append(chunk)
+        else:
+            chunks[-1] += '-'
+        # Remove empty ranges -- invalid in RE.
+        for k in range(len(chunks) - 1, 0, -1):
+            if chunks[k - 1][-1] > chunks[k][0]:
+                chunks[k - 1] = chunks[k - 1][:-1] + chunks[k][1:]
+                del chunks[k]
+        # Escape backslashes and hyphens for set difference (--).
+        # Hyphens that create ranges shouldn't be escaped.
+        stuff = '-'.join(s.replace('\\', r'\\').replace('-', r'\-')
+                         for s in chunks)
+    # Escape set operations (&&, ~~ and ||).
+    stuff = re.sub(r'([&~|])', r'\\\1', stuff)
+    if not stuff:
+        # Empty range: never match.
+        return '(?!)'
+    elif stuff == '!':
+        # Negated empty range: match any character.
+        return '.'
+    else:
+        if stuff[0] == '!':
+            stuff = '^' + stuff[1:]
+        elif stuff[0] in ('^', '['):
+            stuff = '\\' + stuff
+        return f'[{stuff}]'
+
+
 def translate(pat):
     """Translate a shell PATTERN to a regular expression.
 
@@ -101,48 +145,8 @@ def translate(pat):
             if j >= n:
                 add('\\[')
             else:
-                stuff = pat[i:j]
-                if '-' not in stuff:
-                    stuff = stuff.replace('\\', r'\\')
-                else:
-                    chunks = []
-                    k = i+2 if pat[i] == '!' else i+1
-                    while True:
-                        k = pat.find('-', k, j)
-                        if k < 0:
-                            break
-                        chunks.append(pat[i:k])
-                        i = k+1
-                        k = k+3
-                    chunk = pat[i:j]
-                    if chunk:
-                        chunks.append(chunk)
-                    else:
-                        chunks[-1] += '-'
-                    # Remove empty ranges -- invalid in RE.
-                    for k in range(len(chunks)-1, 0, -1):
-                        if chunks[k-1][-1] > chunks[k][0]:
-                            chunks[k-1] = chunks[k-1][:-1] + chunks[k][1:]
-                            del chunks[k]
-                    # Escape backslashes and hyphens for set difference (--).
-                    # Hyphens that create ranges shouldn't be escaped.
-                    stuff = '-'.join(s.replace('\\', r'\\').replace('-', r'\-')
-                                     for s in chunks)
-                # Escape set operations (&&, ~~ and ||).
-                stuff = re.sub(r'([&~|])', r'\\\1', stuff)
-                i = j+1
-                if not stuff:
-                    # Empty range: never match.
-                    add('(?!)')
-                elif stuff == '!':
-                    # Negated empty range: match any character.
-                    add('.')
-                else:
-                    if stuff[0] == '!':
-                        stuff = '^' + stuff[1:]
-                    elif stuff[0] in ('^', '['):
-                        stuff = '\\' + stuff
-                    add(f'[{stuff}]')
+                add(_translate_char_set(pat[i:j]))
+                i = j + 1
         else:
             add(re.escape(c))
     assert i == n
--- Lib/glob.py
+++ Lib/glob.py
@@ -280,7 +280,6 @@ def translate(pat, *, recursive=False, seps=None):
         c = pat[i]
         i = i+1
         if c == '*':
-            # compress consecutive `*` into one
             h = i-1
             while i < n and pat[i] == '*':
                 i = i+1
@@ -314,48 +313,8 @@ def translate(pat, *, recursive=False, seps=None):
             if j >= n:
                 add('\\[')
             else:
-                stuff = pat[i:j]
-                if '-' not in stuff:
-                    stuff = stuff.replace('\\', r'\\')
-                else:
-                    chunks = []
-                    k = i+2 if pat[i] == '!' else i+1
-                    while True:
-                        k = pat.find('-', k, j)
-                        if k < 0:
-                            break
-                        chunks.append(pat[i:k])
-                        i = k+1
-                        k = k+3
-                    chunk = pat[i:j]
-                    if chunk:
-                        chunks.append(chunk)
-                    else:
-                        chunks[-1] += '-'
-                    # Remove empty ranges -- invalid in RE.
-                    for k in range(len(chunks)-1, 0, -1):
-                        if chunks[k-1][-1] > chunks[k][0]:
-                            chunks[k-1] = chunks[k-1][:-1] + chunks[k][1:]
-                            del chunks[k]
-                    # Escape backslashes and hyphens for set difference (--).
-                    # Hyphens that create ranges shouldn't be escaped.
-                    stuff = '-'.join(s.replace('\\', r'\\').replace('-', r'\-')
-                                     for s in chunks)
-                # Escape set operations (&&, ~~ and ||).
-                stuff = re.sub(r'([&~|])', r'\\\1', stuff)
-                i = j+1
-                if not stuff:
-                    # Empty range: never match.
-                    add('(?!)')
-                elif stuff == '!':
-                    # Negated empty range: match any character.
-                    add(not_sep)
-                else:
-                    if stuff[0] == '!':
-                        stuff = '^' + stuff[1:]
-                    elif stuff[0] in ('^', '['):
-                        stuff = '\\' + stuff
-                    add(f'[{stuff}]')
+                add(fnmatch._translate_char_set(pat[i:j]))
+                i = j + 1
         else:
             add(re.escape(c))
     res = "".join(res)
