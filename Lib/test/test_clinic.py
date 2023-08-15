4c09da8ac02a76e059a6274e99f8f3a4607652ae
Erlend E. Aasland <erlend@python.org>
2023-08-15 21:28:50 +0200

    Don't create spurious clinic directories

--- Lib/test/test_clinic.py
+++ Lib/test/test_clinic.py
@@ -656,6 +656,9 @@ class ClinicWholeFileTest(TestCase):
     def test_cloned_with_custom_c_basename(self):
         raw = dedent("""
             /*[clinic input]
+            # Suppress output, to make sure we don't create spurious clinic/
+            # directories.
+            output everything suppress
             foo2
             [clinic start generated code]*/
             /*[clinic input]
