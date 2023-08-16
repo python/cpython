f6099871fac0581ed4d9bcd9b15ce136fb0de8d6
brandonardenwalli <142186236+brandonardenwalli@users.noreply.github.com>
2023-08-16 11:14:14 +0200

    gh-107955 Remove old comment about increasing the reference count in usage of Py_None (#107993)

--- Include/object.h
+++ Include/object.h
@@ -845,8 +845,6 @@ static inline PyObject* _Py_XNewRef(PyObject *obj)
 /*
 _Py_NoneStruct is an object of undefined type which can be used in contexts
 where NULL (nil) is not suitable (since NULL often means 'error').
-
-Don't forget to apply Py_INCREF() when returning this value!!!
 */
 PyAPI_DATA(PyObject) _Py_NoneStruct; /* Don't use this directly */
 #define Py_None (&_Py_NoneStruct)
