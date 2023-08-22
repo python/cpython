d71662500f6d51ab1393daff599f3a23fac1fb7f
Victor Stinner <vstinner@python.org>
2023-08-22 20:30:18 +0200

    gh-108303: Add Lib/test/test_cppext/ sub-directory (GH-108325)
    * Move test_cppext to its own directory
    * Rename setup_testcppext.py to setup.py
    * Rename _testcppext.cpp to extension.cpp
    * The source (extension.cpp) is now also copied by the test.
    (cherry picked from commit 21dda09600848ac280481f7c64f8d9516dc69bb2)
    
    Co-authored-by: Victor Stinner <vstinner@python.org>

--- Lib/test/test_cppext/__init__.py
+++ Lib/test/test_cppext/__init__.py
@@ -11,9 +11,8 @@ from test.support import os_helper
 
 
 MS_WINDOWS = (sys.platform == 'win32')
-
-
-SETUP_TESTCPPEXT = support.findfile('setup_testcppext.py')
+SOURCE = os.path.join(os.path.dirname(__file__), 'extension.cpp')
+SETUP = os.path.join(os.path.dirname(__file__), 'setup.py')
 
 
 @support.requires_subprocess()
@@ -42,7 +41,8 @@ class TestCPPExt(unittest.TestCase):
     def _check_build(self, std_cpp03, extension_name, python_exe):
         pkg_dir = 'pkg'
         os.mkdir(pkg_dir)
-        shutil.copy(SETUP_TESTCPPEXT, os.path.join(pkg_dir, "setup.py"))
+        shutil.copy(SETUP, os.path.join(pkg_dir, os.path.basename(SETUP)))
+        shutil.copy(SOURCE, os.path.join(pkg_dir, os.path.basename(SOURCE)))
 
         def run_cmd(operation, cmd):
             env = os.environ.copy()
