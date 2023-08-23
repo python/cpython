7500a999fe808968dc92ad6283f68756f26bd846
sobolevn <mail@sobolevn.me>
2023-08-23 09:31:15 +0300

    gh-108303: Move `ann_module*.py` files to `typinganndata/` folder

--- Lib/test/test_module/__init__.py
+++ Lib/test/test_module/__init__.py
@@ -324,7 +324,9 @@ a = A(destroyed)"""
             del foo.__annotations__
 
     def test_annotations_are_created_correctly(self):
-        ann_module4 = import_helper.import_fresh_module('test.ann_module4')
+        ann_module4 = import_helper.import_fresh_module(
+            'test.typinganndata.ann_module4',
+        )
         self.assertTrue("__annotations__" in ann_module4.__dict__)
         del ann_module4.__annotations__
         self.assertFalse("__annotations__" in ann_module4.__dict__)
