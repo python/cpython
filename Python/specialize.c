6c633644caccd1cf0e2dea27a4bcd6991d791646
Dong-hee Na <donghee.na@python.org>
2023-08-25 07:23:10 +0900

    gh-107265: Ensure _PyCode_Quicken does not handle ENTER_EXECUTOR

--- Python/specialize.c
+++ Python/specialize.c
@@ -300,6 +300,8 @@ _PyCode_Quicken(PyCodeObject *code)
     for (int i = 0; i < Py_SIZE(code); i++) {
         opcode = _Py_GetBaseOpcode(code, i);
         assert(opcode < MIN_INSTRUMENTED_OPCODE);
+        // _PyCode_Quicken is only called when initializing a fresh code object.
+        assert(opcode != ENTER_EXECUTOR);
         int caches = _PyOpcode_Caches[opcode];
         if (caches) {
             instructions[i + 1].cache = adaptive_counter_warmup();
