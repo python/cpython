652a598746234021def7901c63f7e65ceb3abc77
Victor Stinner <vstinner@python.org>
2023-08-26 02:24:27 +0200

    gh-106320: Remove private _PyLong converter functions (#108499)
    Move these private functions to the internal C API (pycore_long.h):
    
    * _PyLong_UnsignedInt_Converter()
    * _PyLong_UnsignedLongLong_Converter()
    * _PyLong_UnsignedLong_Converter()
    * _PyLong_UnsignedShort_Converter()
    
    Argument Clinic now emits #include "pycore_long.h" when these
    functions are used.

--- Modules/clinic/_testclinic.c.h
+++ Modules/clinic/_testclinic.c.h
@@ -7,6 +7,7 @@ preserve
 #  include "pycore_runtime.h"       // _Py_ID()
 #endif
 
+#include "pycore_long.h"           // _PyLong_UnsignedShort_Converter()
 
 PyDoc_STRVAR(test_empty_function__doc__,
 "test_empty_function($module, /)\n"
@@ -3069,4 +3070,4 @@ skip_optional_pos:
 exit:
     return return_value;
 }
-/*[clinic end generated code: output=86396cbed6eb8b65 input=a9049054013a1b77]*/
+/*[clinic end generated code: output=41f439aab0cb809d input=a9049054013a1b77]*/
