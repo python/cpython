5719b65bf16590fc7272fe073a8f1e5b723ad826
Victor Stinner <vstinner@python.org>
2023-08-29 04:29:01 +0200

    gh-106320: Remove private _PyObject_GetState()
    Move the private _PyObject_GetState() function to the internal C API
    (pycore_object.h). No longer export the function.

--- Include/object.h
+++ Include/object.h
@@ -437,12 +437,6 @@ PyAPI_FUNC(void) PyObject_ClearWeakRefs(PyObject *);
 */
 PyAPI_FUNC(PyObject *) PyObject_Dir(PyObject *);
 
-/* Pickle support. */
-#ifndef Py_LIMITED_API
-PyAPI_FUNC(PyObject *) _PyObject_GetState(PyObject *);
-#endif
-
-
 /* Helpers for printing recursive container types */
 PyAPI_FUNC(int) Py_ReprEnter(PyObject *);
 PyAPI_FUNC(void) Py_ReprLeave(PyObject *);
