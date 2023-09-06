c76f29d10e2ed5ee2926c55701ad227ede5fe4ff
Davide Rizzo <sorcio@gmail.com>
2023-09-06 13:24:59 +0200

    gh-91960: skip test_gdb when built with clang
    test_gdb should be skipped on all platforms when CPython is compiled with Clang, not only Darwin.

--- Lib/test/test_gdb.py
+++ Lib/test/test_gdb.py
@@ -55,7 +55,7 @@ if gdb_major_version < 7:
 if not sysconfig.is_python_build():
     raise unittest.SkipTest("test_gdb only works on source builds at the moment.")
 
-if 'Clang' in platform.python_compiler() and sys.platform == 'darwin':
+if 'Clang' in platform.python_compiler():
     raise unittest.SkipTest("test_gdb doesn't work correctly when python is"
                             " built with LLVM clang")
 
