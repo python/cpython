6509e855d703be718dfd596cf0e5c61248c48ca1
Irit Katriel <1055913+iritkatriel@users.noreply.github.com>
2023-09-09 22:03:22 +0100

    use subtest
    Co-authored-by: Alex Waygood <Alex.Waygood@Gmail.com>

--- Lib/test/test_traceback.py
+++ Lib/test/test_traceback.py
@@ -1601,25 +1601,26 @@ class BaseExceptionReportingTests:
 
     def test_exception_with_multiple_notes(self):
         for e in [ValueError(42), SyntaxError('bad syntax')]:
-            vanilla = self.get_report(e)
-
-            e.add_note('Note 1')
-            e.add_note('Note 2')
-            e.add_note('Note 3')
-
-            self.assertEqual(
-                self.get_report(e),
-                vanilla + 'Note 1\n' + 'Note 2\n' + 'Note 3\n')
-
-            del e.__notes__
-            e.add_note('Note 4')
-            del e.__notes__
-            e.add_note('Note 5')
-            e.add_note('Note 6')
-
-            self.assertEqual(
-                self.get_report(e),
-                vanilla + 'Note 5\n' + 'Note 6\n')
+            with self.subTest(e=e):
+                vanilla = self.get_report(e)
+    
+                e.add_note('Note 1')
+                e.add_note('Note 2')
+                e.add_note('Note 3')
+    
+                self.assertEqual(
+                    self.get_report(e),
+                    vanilla + 'Note 1\n' + 'Note 2\n' + 'Note 3\n')
+    
+                del e.__notes__
+                e.add_note('Note 4')
+                del e.__notes__
+                e.add_note('Note 5')
+                e.add_note('Note 6')
+    
+                self.assertEqual(
+                    self.get_report(e),
+                    vanilla + 'Note 5\n' + 'Note 6\n')
 
     def test_exception_qualname(self):
         class A:
