6b8fd912c2830265f25d73adb67b37d5d7582020
Victor Stinner <vstinner@python.org>
2023-09-13 00:41:25 +0200

    gh-109276: libregrtest: WASM use filename for JSON (#109340)
    On Emscripten and WASI platforms, or if --python command line option
    is used, libregrtest now uses a filename for the JSON file.
    
    Emscripten and WASI buildbot workers run the main test process with a
    different Python (Linux) which spawns Emscripten/WASI processes using
    the command specified in --python command line option. Passing a file
    descriptor from the parent process to the child process doesn't work
    in this case.
    
    * Add JsonFile and JsonFileType classes
    * Add RunTests.json_file_use_filename() method.
    * Add a test in test_regrtest on the --python command line option.
    * test_regrtest: add parallel=False parameter.
    * Split long RunWorkers._runtest() function into sub-functions.

--- Lib/test/libregrtest/runtests.py
+++ Lib/test/libregrtest/runtests.py
@@ -1,9 +1,60 @@
+import contextlib
 import dataclasses
 import json
+import os
+import subprocess
 from typing import Any
+
+from test import support
 
 from .utils import (
     StrPath, StrJSON, TestTuple, FilterTuple, FilterDict)
+
+
+class JsonFileType:
+    UNIX_FD = "UNIX_FD"
+    WINDOWS_HANDLE = "WINDOWS_HANDLE"
+    FILENAME = "FILENAME"
+
+
+@dataclasses.dataclass(slots=True, frozen=True)
+class JsonFile:
+    # See RunTests.json_file_use_filename()
+    file: int | StrPath
+    file_type: str
+
+    def configure_subprocess(self, popen_kwargs: dict) -> None:
+        match self.file_type:
+            case JsonFileType.UNIX_FD:
+                # Unix file descriptor
+                popen_kwargs['pass_fds'] = [self.file]
+            case JsonFileType.WINDOWS_HANDLE:
+                # Windows handle
+                startupinfo = subprocess.STARTUPINFO()
+                startupinfo.lpAttributeList = {"handle_list": [self.file]}
+                popen_kwargs['startupinfo'] = startupinfo
+            case JsonFileType.FILENAME:
+                # Filename: nothing to do to
+                pass
+
+    @contextlib.contextmanager
+    def inherit_subprocess(self):
+        if self.file_type == JsonFileType.WINDOWS_HANDLE:
+            os.set_handle_inheritable(self.file, True)
+            try:
+                yield
+            finally:
+                os.set_handle_inheritable(self.file, False)
+        else:
+            yield
+
+    def open(self, mode='r', *, encoding):
+        file = self.file
+        if self.file_type == JsonFileType.WINDOWS_HANDLE:
+            import msvcrt
+            # Create a file descriptor from the handle
+            file = msvcrt.open_osfhandle(file, os.O_WRONLY)
+        return open(file, mode, encoding=encoding)
 
 
 @dataclasses.dataclass(slots=True, frozen=True)
@@ -38,9 +89,7 @@ class RunTests:
     python_cmd: tuple[str] | None
     randomize: bool
     random_seed: int | None
-    # On Unix, it's a file descriptor.
-    # On Windows, it's a handle.
-    json_fd: int | None
+    json_file: JsonFile | None
 
     def copy(self, **override):
         state = dataclasses.asdict(self)
@@ -74,6 +123,17 @@ class RunTests:
     def from_json(worker_json: StrJSON) -> 'RunTests':
         return json.loads(worker_json, object_hook=_decode_runtests)
 
+    def json_file_use_filename(self) -> bool:
+        # json_file type depends on the platform:
+        # - Unix: file descriptor (int)
+        # - Windows: handle (int)
+        # - Emscripten/WASI or if --python is used: filename (str)
+        return (
+            bool(self.python_cmd)
+            or support.is_emscripten
+            or support.is_wasi
+        )
+
 
 class _EncodeRunTests(json.JSONEncoder):
     def default(self, o: Any) -> dict[str, Any]:
@@ -90,6 +150,8 @@ def _decode_runtests(data: dict[str, Any]) -> RunTests | dict[str, Any]:
         data.pop('__runtests__')
         if data['hunt_refleak']:
             data['hunt_refleak'] = HuntRefleak(**data['hunt_refleak'])
+        if data['json_file']:
+            data['json_file'] = JsonFile(**data['json_file'])
         return RunTests(**data)
     else:
         return data
