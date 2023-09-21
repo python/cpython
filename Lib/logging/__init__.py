c73e2b714a69d38833aee8aaeda0877814ee400d
Dale Collison <dale.a.collison@gmail.com>
2023-09-21 18:26:19 +0100

    Someone managed to duplicate a line in the docstring

--- Lib/logging/__init__.py
+++ Lib/logging/__init__.py
@@ -2212,20 +2212,22 @@ def shutdown(handlerList=_handlerList):
         try:
             h = wr()
             if h:
-                with h.lock:
-                    try:
-                        # MemoryHandlers might not want to be flushed on close,
-                        # but circular imports prevent us scoping this to just
-                        # those handlers.  hence the default to True.
-                        if getattr(h, 'flushOnClose', True):
-                            h.flush()
-                        h.close()
-                    except (OSError, ValueError):
-                        # Ignore errors which might be caused
-                        # because handlers have been closed but
-                        # references to them are still around at
-                        # application exit.
-                        pass
+                try:
+                    h.acquire()
+                    # MemoryHandlers might not want to be flushed on close,
+                    # but circular imports prevent us scoping this to just
+                    # those handlers.  hence the default to True.
+                    if getattr(h, 'flushOnClose', True):
+                        h.flush()
+                    h.close()
+                except (OSError, ValueError):
+                    # Ignore errors which might be caused
+                    # because handlers have been closed but
+                    # references to them are still around at
+                    # application exit.
+                    pass
+                finally:
+                    h.release()
         except: # ignore everything, as we're shutting down
             if raiseExceptions:
                 raise
