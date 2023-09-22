b4939148aeb51c2c7b4cd42cf3a64d9905224b57
Marcel Martin <marcel.martin@scilifelab.se>
2023-09-22 09:49:55 +0200

    Update docs, factor out peek_bytes, semantics
    Semantic change: The default argument for peek is now size=1.

--- Modules/_io/bytesio.c
+++ Modules/_io/bytesio.c
@@ -420,8 +420,9 @@ _io_BytesIO_tell_impl(bytesio *self)
     return PyLong_FromSsize_t(self->pos);
 }
 
+// Read without advancing position
 static PyObject *
-read_bytes_lock_held(bytesio *self, Py_ssize_t size)
+peek_bytes_lock_held(bytesio *self, Py_ssize_t size)
 {
     _Py_CRITICAL_SECTION_ASSERT_OBJECT_LOCKED(self);
 
@@ -432,7 +433,6 @@ read_bytes_lock_held(bytesio *self, Py_ssize_t size)
     if (size > 1 &&
         self->pos == 0 && size == PyBytes_GET_SIZE(self->buf) &&
         FT_ATOMIC_LOAD_SSIZE_RELAXED(self->exports) == 0) {
-        self->pos += size;
         return Py_NewRef(self->buf);
     }
 
@@ -444,8 +444,14 @@ read_bytes_lock_held(bytesio *self, Py_ssize_t size)
     }
 
     output = PyBytes_AS_STRING(self->buf) + self->pos;
-    self->pos += size;
     return PyBytes_FromStringAndSize(output, size);
+}
+
+static PyObject *
+read_bytes_lock_held(bytesio *self, Py_ssize_t size) {
+    PyObject *bytes = peek_bytes_lock_held(self, size);
+    self->pos += size;
+    return bytes;
 }
 
 /*[clinic input]
@@ -502,17 +508,18 @@ _io_BytesIO_read1_impl(bytesio *self, Py_ssize_t size)
 
 /*[clinic input]
 _io.BytesIO.peek
-    size: Py_ssize_t(accept={int, NoneType}) = -1
+    size: Py_ssize_t = 1
     /
 
 Return bytes from the stream without advancing the position.
 
+If the size argument is zero or negative, read until EOF is reached.
 Return an empty bytes object at EOF.
 [clinic start generated code]*/
 
 static PyObject *
 _io_BytesIO_peek_impl(bytesio *self, Py_ssize_t size)
-/*[clinic end generated code: output=fa4d8ce28b35db9b input=afc80e71b37e7c59]*/
+/*[clinic end generated code: output=fa4d8ce28b35db9b input=cb06614a3ed0496e]*/
 {
     Py_ssize_t n;
 
@@ -525,11 +532,7 @@ _io_BytesIO_peek_impl(bytesio *self, Py_ssize_t size)
         if (size < 0)
             size = 0;
     }
-    Py_ssize_t prev_pos = self->pos;
-    PyObject* result = read_bytes_lock_held(self, size);
-    self->pos = prev_pos;
-
-    return result;
+    return peek_bytes_lock_held(self, size);
 }
 
 
--- Modules/_io/clinic/bytesio.c.h
+++ Modules/_io/clinic/bytesio.c.h
@@ -288,11 +288,12 @@ exit:
 }
 
 PyDoc_STRVAR(_io_BytesIO_peek__doc__,
-"peek($self, size=-1, /)\n"
+"peek($self, size=1, /)\n"
 "--\n"
 "\n"
 "Return bytes from the stream without advancing the position.\n"
 "\n"
+"If the size argument is zero or negative, read until EOF is reached.\n"
 "Return an empty bytes object at EOF.");
 
 #define _IO_BYTESIO_PEEK_METHODDEF    \
@@ -305,7 +306,7 @@ static PyObject *
 _io_BytesIO_peek(PyObject *self, PyObject *const *args, Py_ssize_t nargs)
 {
     PyObject *return_value = NULL;
-    Py_ssize_t size = -1;
+    Py_ssize_t size = 1;
 
     if (!_PyArg_CheckPositional("peek", nargs, 0, 1)) {
         goto exit;
@@ -313,8 +314,17 @@ _io_BytesIO_peek(PyObject *self, PyObject *const *args, Py_ssize_t nargs)
     if (nargs < 1) {
         goto skip_optional;
     }
-    if (!_Py_convert_optional_to_ssize_t(args[0], &size)) {
-        goto exit;
+    {
+        Py_ssize_t ival = -1;
+        PyObject *iobj = _PyNumber_Index(args[0]);
+        if (iobj != NULL) {
+            ival = PyLong_AsSsize_t(iobj);
+            Py_DECREF(iobj);
+        }
+        if (ival == -1 && PyErr_Occurred()) {
+            goto exit;
+        }
+        size = ival;
     }
 skip_optional:
     return_value = _io_BytesIO_peek_impl((bytesio *)self, size);
@@ -673,4 +683,4 @@ skip_optional_pos:
 exit:
     return return_value;
 }
-/*[clinic end generated code: output=c21a2170e9f7efa5 input=a9049054013a1b77]*/
+/*[clinic end generated code: output=02299ed558ba722f input=a9049054013a1b77]*/
