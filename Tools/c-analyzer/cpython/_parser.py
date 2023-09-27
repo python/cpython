2165c5f57ead8204fc3ec4f4e61d4a9eefa508d6
Dino Viehland <dinoviehland@fb.com>
2023-09-26 18:39:53 -0700

    Add includes to analyzer
    Summary:
    
    Test Plan:
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:

--- Tools/c-analyzer/cpython/_parser.py
+++ Tools/c-analyzer/cpython/_parser.py
@@ -89,6 +89,11 @@ Python/abstract_interp_cases.c.h
 # not actually source
 Python/bytecodes.c
 
+# mimalloc
+Objects/mimalloc/*.c
+Include/mimalloc/*.h
+Include/mimalloc/mimalloc/*.h
+
 # @end=conf@
 ''')
 
@@ -109,6 +114,7 @@ glob	dirname
 *	.
 *	./Include
 *	./Include/internal
+*   ./Include/mimalloc
 
 Modules/_decimal/**/*.c	Modules/_decimal/libmpdec
 Modules/_elementtree.c	Modules/expat
