a6773ec099469bee0af02f086da6463283b2589e
Miss Islington (bot) <31488909+miss-islington@users.noreply.github.com>
2023-10-02 02:59:52 -0700

    [3.12] gh-109991: Update Windows build to use OpenSSL 3.0.11 (GH-110054) (#110056)
    gh-109991: Update Windows build to use OpenSSL 3.0.11 (GH-110054)
    (cherry picked from commit cf4c29725636e1a0dd2ebab443613b56ca6c9486)
    
    Co-authored-by: Zachary Ware <zach@python.org>

--- PCbuild/get_externals.bat
+++ PCbuild/get_externals.bat
@@ -53,7 +53,7 @@ echo.Fetching external libraries...
 set libraries=
 set libraries=%libraries%                                       bzip2-1.0.8
 if NOT "%IncludeLibffiSrc%"=="false" set libraries=%libraries%  libffi-3.4.4
-if NOT "%IncludeSSLSrc%"=="false" set libraries=%libraries%     openssl-3.0.10
+if NOT "%IncludeSSLSrc%"=="false" set libraries=%libraries%     openssl-3.0.11
 set libraries=%libraries%                                       sqlite-3.42.0.0
 if NOT "%IncludeTkinterSrc%"=="false" set libraries=%libraries% tcl-core-8.6.13.0
 if NOT "%IncludeTkinterSrc%"=="false" set libraries=%libraries% tk-8.6.13.0
@@ -77,7 +77,7 @@ echo.Fetching external binaries...
 
 set binaries=
 if NOT "%IncludeLibffi%"=="false"  set binaries=%binaries% libffi-3.4.4
-if NOT "%IncludeSSL%"=="false"     set binaries=%binaries% openssl-bin-3.0.10
+if NOT "%IncludeSSL%"=="false"     set binaries=%binaries% openssl-bin-3.0.11
 if NOT "%IncludeTkinter%"=="false" set binaries=%binaries% tcltk-8.6.13.0
 if NOT "%IncludeSSLSrc%"=="false"  set binaries=%binaries% nasm-2.11.06
 
