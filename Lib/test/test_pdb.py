775fd782fca77037c3f9bca6f7c528bcef669acb
Miss Islington (bot) <31488909+miss-islington@users.noreply.github.com>
2023-10-02 07:58:22 -0700

    [3.12] gh-109375: Fix bug where pdb registers an alias without an associated command (GH-109376) (#109429)
    gh-109375: Fix bug where pdb registers an alias without an associated command (GH-109376)
    (cherry picked from commit 68a6f21f47e779ddd70e33cf04d170a63f077fcd)
    
    Co-authored-by: buermarc <44375277+buermarc@users.noreply.github.com>

--- Lib/test/test_pdb.py
+++ Lib/test/test_pdb.py
@@ -662,8 +662,10 @@ def test_pdb_alias_command():
     ...     o.method()
 
     >>> with PdbTestInput([  # doctest: +ELLIPSIS
+    ...     'alias pi',
     ...     'alias pi for k in %1.__dict__.keys(): print(f"%1.{k} = {%1.__dict__[k]}")',
     ...     'alias ps pi self',
+    ...     'alias ps',
     ...     'pi o',
     ...     's',
     ...     'ps',
@@ -672,8 +674,12 @@ def test_pdb_alias_command():
     ...    test_function()
     > <doctest test.test_pdb.test_pdb_alias_command[1]>(4)test_function()
     -> o.method()
+    (Pdb) alias pi
+    *** Unknown alias 'pi'
     (Pdb) alias pi for k in %1.__dict__.keys(): print(f"%1.{k} = {%1.__dict__[k]}")
     (Pdb) alias ps pi self
+    (Pdb) alias ps
+    ps = pi self
     (Pdb) pi o
     o.attr1 = 10
     o.attr2 = str
