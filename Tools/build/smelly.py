f590f2c908249bd09b53426483295464c2df70c9
Victor Stinner <vstinner@python.org>
2023-10-02 13:34:00 +0200

    gh-110014: Remove PY_TIMEOUT_MAX from limited C API
    If the timeout is greater than PY_TIMEOUT_MAX,
    PyThread_acquire_lock_timed() uses a timeout of PY_TIMEOUT_MAX
    microseconds, which is around 280.6 years. This case is unlikely and
    limiting a timeout to 280.6 years sounds like a reasonable trade-off.
    
    The constant PY_TIMEOUT_MAX is not used in PyPI top 5,000 projects.

--- Tools/build/smelly.py
+++ Tools/build/smelly.py
@@ -11,11 +11,6 @@ ALLOWED_PREFIXES = ('Py', '_Py')
 if sys.platform == 'darwin':
     ALLOWED_PREFIXES += ('__Py',)
 
-# "Legacy": some old symbols are prefixed by "PY_".
-EXCEPTIONS = frozenset({
-    'PY_TIMEOUT_MAX',
-})
-
 IGNORED_EXTENSION = "_ctypes_test"
 # Ignore constructor and destructor functions
 IGNORED_SYMBOLS = {'_init', '_fini'}
@@ -77,7 +72,7 @@ def get_smelly_symbols(stdout):
         symbol = parts[-1]
         result = '%s (type: %s)' % (symbol, symtype)
 
-        if symbol.startswith(ALLOWED_PREFIXES) or symbol in EXCEPTIONS:
+        if symbol.startswith(ALLOWED_PREFIXES):
             python_symbols.append(result)
             continue
 
