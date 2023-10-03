60b4666d7b43ba13a477d4d0fe6ddb1314f31775
sunmy2019 <59365878+sunmy2019@users.noreply.github.com>
2023-10-04 07:39:57 +0800

    count non empty nodes first

--- Parser/action_helpers.c
+++ Parser/action_helpers.c
@@ -1002,34 +1002,35 @@ _PyPegen_setup_full_format_spec(Parser *p, Token *colon, asdl_expr_seq *spec, in
     // spec is parsed as an *empty* JoinedStr node, instead of having an empty
     // constant in it.
     Py_ssize_t n_items = asdl_seq_LEN(spec);
-    asdl_expr_seq *seq = _Py_asdl_expr_seq_new(n_items, p->arena);
-    if (seq == NULL) {
-        return NULL;
-    }
-    Py_ssize_t index = 0;
+    Py_ssize_t non_empty_count = 0;
     for (Py_ssize_t i = 0; i < n_items; i++) {
         expr_ty item = asdl_seq_GET(spec, i);
-        if (item->kind == Constant_kind &&
-            PyUnicode_CheckExact(item->v.Constant.value) &&
-            PyUnicode_GET_LENGTH(item->v.Constant.value) == 0) {
-            continue;
-        }
-        asdl_seq_SET(seq, index++, item);
+        non_empty_count += !(item->kind == Constant_kind &&
+                             PyUnicode_CheckExact(item->v.Constant.value) &&
+                             PyUnicode_GET_LENGTH(item->v.Constant.value) == 0);
     }
-    asdl_expr_seq *resized_specs;
-    if (index != n_items) {
-        resized_specs = _Py_asdl_expr_seq_new(index, p->arena);
-        if (resized_specs == NULL) {
+    asdl_expr_seq *resized_spec;
+    if (non_empty_count != n_items) {
+        resized_spec = _Py_asdl_expr_seq_new(non_empty_count, p->arena);
+        if (resized_spec == NULL) {
             return NULL;
         }
-        for (Py_ssize_t i = 0; i < index; i++) {
-            asdl_seq_SET(resized_specs, i, asdl_seq_GET(seq, i));
+        Py_ssize_t j = 0;
+        for (Py_ssize_t i = 0; i < n_items; i++) {
+            expr_ty item = asdl_seq_GET(spec, i);
+            if (item->kind == Constant_kind &&
+                PyUnicode_CheckExact(item->v.Constant.value) &&
+                PyUnicode_GET_LENGTH(item->v.Constant.value) == 0) {
+                continue;
+            }
+            asdl_seq_SET(resized_spec, j++, item);
         }
+        assert(j == non_empty_count);
     } else {
-        resized_specs = seq;
+        resized_spec = spec;
     }
-    expr_ty res = _PyAST_JoinedStr(resized_specs, lineno, col_offset,
-                                   end_lineno, end_col_offset, p->arena);
+    expr_ty res = _PyAST_JoinedStr(resized_spec, lineno, col_offset, end_lineno,
+                                   end_col_offset, p->arena);
     if (!res) {
         return NULL;
     }
