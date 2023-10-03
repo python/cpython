faf099e0358a83345533f21a1dbe0fed019fb304
Guido van Rossum <guido@python.org>
2023-10-03 10:34:29 -0700

    Unify _GUARD_TYPE_VERSION{,_STORE}

--- Python/generated_cases.c.h
+++ Python/generated_cases.c.h
@@ -2616,7 +2616,7 @@
         TARGET(STORE_ATTR_INSTANCE_VALUE) {
             PyObject *owner;
             PyObject *value;
-            // _GUARD_TYPE_VERSION_STORE
+            // _GUARD_TYPE_VERSION
             owner = stack_pointer[-1];
             {
                 uint32_t type_version = read_u32(&next_instr[1].cache);
@@ -2705,7 +2705,7 @@
         TARGET(STORE_ATTR_SLOT) {
             PyObject *owner;
             PyObject *value;
-            // _GUARD_TYPE_VERSION_STORE
+            // _GUARD_TYPE_VERSION
             owner = stack_pointer[-1];
             {
                 uint32_t type_version = read_u32(&next_instr[1].cache);
