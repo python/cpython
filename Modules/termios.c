4d5ae6663203ff4ab646933d11a3aa57b772f3ed
sobolevn <mail@sobolevn.me>
2023-10-04 18:57:02 +0300

    gh-110365: Fix error overwrite in `termios.tcsetattr`

--- Modules/termios.c
+++ Modules/termios.c
@@ -212,17 +212,31 @@ termios_tcsetattr_impl(PyObject *module, int fd, int when, PyObject *term)
         return PyErr_SetFromErrno(state->TermiosError);
     }
 
-    mode.c_iflag = (tcflag_t) PyLong_AsLong(PyList_GetItem(term, 0));
-    mode.c_oflag = (tcflag_t) PyLong_AsLong(PyList_GetItem(term, 1));
-    mode.c_cflag = (tcflag_t) PyLong_AsLong(PyList_GetItem(term, 2));
-    mode.c_lflag = (tcflag_t) PyLong_AsLong(PyList_GetItem(term, 3));
-    speed_t ispeed = (speed_t) PyLong_AsLong(PyList_GetItem(term, 4));
-    speed_t ospeed = (speed_t) PyLong_AsLong(PyList_GetItem(term, 5));
-    PyObject *cc = PyList_GetItem(term, 6);
+    long item;
+#define SAFE_LONG_ITEM(obj) \
+    item = PyLong_AsLong(obj); \
+    if (PyErr_Occurred()) { \
+        return NULL; \
+    }
+
+    SAFE_LONG_ITEM(PyList_GET_ITEM(term, 0));
+    mode.c_iflag = (tcflag_t) item;
+    SAFE_LONG_ITEM(PyList_GET_ITEM(term, 1));
+    mode.c_oflag = (tcflag_t) item;
+    SAFE_LONG_ITEM(PyList_GET_ITEM(term, 2));
+    mode.c_cflag = (tcflag_t) item;
+    SAFE_LONG_ITEM(PyList_GET_ITEM(term, 3));
+    mode.c_lflag = (tcflag_t) item;
+    SAFE_LONG_ITEM(PyList_GET_ITEM(term, 4));
+    speed_t ispeed = (speed_t) item;
+    SAFE_LONG_ITEM(PyList_GET_ITEM(term, 5));
+    speed_t ospeed = (speed_t) item;
+#undef SAFE_LONG_ITEM
+
+    PyObject *cc = PyList_GET_ITEM(term, 6);
     if (PyErr_Occurred()) {
         return NULL;
     }
-
     if (!PyList_Check(cc) || PyList_Size(cc) != NCCS) {
         PyErr_Format(PyExc_TypeError,
             "tcsetattr: attributes[6] must be %d element list",
