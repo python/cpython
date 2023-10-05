bd3d0e8b8258686dd217ba237b9389746a7eddc8
Sam Gross <colesbury@gmail.com>
2023-10-05 15:46:33 +0000

    gh-109549: Add new states to PyThreadState to support PEP 703 (gh-109915)
    This adds a new field 'state' to PyThreadState that can take on one of three values: _Py_THREAD_ATTACHED, _Py_THREAD_DETACHED, or _Py_THREAD_GC.  The "attached" and "detached" states correspond closely to acquiring and releasing the GIL.  The "gc" state is current unused, but will be used to implement stop-the-world GC for --disable-gil builds in the near future.

--- Include/cpython/pystate.h
+++ Include/cpython/pystate.h
@@ -102,6 +102,10 @@ struct _ts {
 #endif
     int _whence;
 
+    /* Thread state (_Py_THREAD_ATTACHED, _Py_THREAD_DETACHED, _Py_THREAD_GC).
+       See Include/internal/pycore_pystate.h for more details. */
+    int state;
+
     int py_recursion_remaining;
     int py_recursion_limit;
 
