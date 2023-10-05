af29282fce117cb10f00907fd46d56c2fa6142f5
Victor Stinner <vstinner@python.org>
2023-10-05 14:42:36 +0200

    gh-110367: Fix regrtest test_worker_output_on_failure() on ASAN build (#110387)
    Set ASAN_OPTIONS="handle_segv=0" env var to run the test.

--- Lib/test/test_faulthandler.py
+++ Lib/test/test_faulthandler.py
@@ -67,11 +67,7 @@ class FaultHandlerTests(unittest.TestCase):
 
         # Sanitizers must not handle SIGSEGV (ex: for test_enable_fd())
         option = 'handle_segv=0'
-        for name in ('ASAN_OPTIONS', 'MSAN_OPTIONS', 'UBSAN_OPTIONS'):
-            if name in env:
-                env[name] += f':{option}'
-            else:
-                env[name] = option
+        support.set_sanitizer_env_var(env, option)
 
         with support.SuppressCrashReport():
             process = script_helper.spawn_python('-c', code,
