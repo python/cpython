347881e161d4c22d55103b08f1b6a5afafbfc21a
Serhiy Storchaka <storchaka@gmail.com>
2023-10-07 13:21:40 +0300

    gh-110378: Close invalid generators in contextmanager and asynccontextmanager
    contextmanager and asynccontextmanager context managers now close an invalid
    underlying generator object that yields more then one value.

--- Lib/test/test_contextlib_async.py
+++ Lib/test/test_contextlib_async.py
@@ -216,6 +216,9 @@ class AsyncContextManagerTestCase(unittest.TestCase):
         await ctx.__aenter__()
         with self.assertRaises(RuntimeError):
             await ctx.__aexit__(TypeError, TypeError('foo'), None)
+        if support.check_impl_detail(cpython=True):
+            # The "gen" attribute is an implementation detail.
+            self.assertFalse(ctx.gen.ag_suspended)
 
     @_async_test
     async def test_contextmanager_trap_no_yield(self):
@@ -237,6 +240,9 @@ class AsyncContextManagerTestCase(unittest.TestCase):
         await ctx.__aenter__()
         with self.assertRaises(RuntimeError):
             await ctx.__aexit__(None, None, None)
+        if support.check_impl_detail(cpython=True):
+            # The "gen" attribute is an implementation detail.
+            self.assertFalse(ctx.gen.ag_suspended)
 
     @_async_test
     async def test_contextmanager_non_normalised(self):
