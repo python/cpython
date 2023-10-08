e19a4378b4f572458940b65093b2d5e9d96d37e8
Donghee Na <donghee.na@python.org>
2023-10-08 13:00:17 +0900

    Revert "Add comment"
    This reverts commit 3696f01993c6ced3fa84a7eebeea2ea0aab7451a.

--- Include/internal/pycore_signal.h
+++ Include/internal/pycore_signal.h
@@ -38,10 +38,6 @@ PyAPI_FUNC(void) _Py_RestoreSignals(void);
 
 struct _signals_runtime_state {
     struct {
-        /* To access tripped / func with "relaxed" memory order
-         * _signals_runtime_state.is_tripped should be accessed by
-         * "sequentially consistent" order as the memory barrier.
-         */
         int tripped;
         /* func is atomic to ensure that PyErr_SetInterrupt is async-signal-safe
          * (even though it would probably be otherwise, anyway).
@@ -66,9 +62,7 @@ struct _signals_runtime_state {
 #endif
     } wakeup;
 
-    /* Speed up sigcheck() when none tripped and
-     * act as a memory barrier("sequentially consistent" order)
-     */
+    /* Speed up sigcheck() when none tripped */
     int is_tripped;
 
     /* These objects necessarily belong to the main interpreter. */
