061265e51b54ed1e7c26caf6423e4511e7eb66aa
Donghee Na <donghee.na@python.org>
2023-10-14 02:05:34 +0900

    Revert header

--- Python/ceval_gil.c
+++ Python/ceval_gil.c
@@ -1,5 +1,6 @@
 
 #include "Python.h"
+#include "pycore_atomic.h"        // _Py_ANNOTATE_RWLOCK_XXXX
 #include "pycore_ceval.h"         // _PyEval_SignalReceived()
 #include "pycore_initconfig.h"    // _PyStatus_OK()
 #include "pycore_interp.h"        // _Py_RunGC()
