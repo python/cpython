7800b1b3994708a386a07865d5c461a48c74c4ef
Serhiy Storchaka <storchaka@gmail.com>
2023-10-21 22:18:34 +0300

    gh-111085: Fix invalid state handling in TaskGroup and Timeout (#111111)
    asyncio.TaskGroup and asyncio.Timeout classes now raise proper RuntimeError
    if they are improperly used.
    
    * When they are used without entering the context manager.
    * When they are used after finishing.
    * When the context manager is entered more than once (simultaneously or
      sequentially).
    * If there is no current task when entering the context manager.
    
    They now remain in a consistent state after an exception is thrown,
    so subsequent operations can be performed correctly (if they are allowed).
    
    Co-authored-by: James Hilton-Balfe <gobot1234yt@gmail.com>

--- Lib/asyncio/taskgroups.py
+++ Lib/asyncio/taskgroups.py
@@ -54,16 +54,14 @@ class TaskGroup:
     async def __aenter__(self):
         if self._entered:
             raise RuntimeError(
-                f"TaskGroup {self!r} has been already entered")
-        self._entered = True
-
+                f"TaskGroup {self!r} has already been entered")
         if self._loop is None:
             self._loop = events.get_running_loop()
-
         self._parent_task = tasks.current_task(self._loop)
         if self._parent_task is None:
             raise RuntimeError(
                 f'TaskGroup {self!r} cannot determine the parent task')
+        self._entered = True
 
         return self
 
