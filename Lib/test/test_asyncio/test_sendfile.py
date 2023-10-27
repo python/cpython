272ab330629cb6125b10d8e83b1171fa84309d01
Guozhang Wu <30565051+zcxsythenew@users.noreply.github.com>
2023-10-28 07:57:34 +0800

    gh-111347: Remove wrong assertion in test_sendfile

--- Lib/test/test_asyncio/test_sendfile.py
+++ Lib/test/test_asyncio/test_sendfile.py
@@ -470,6 +470,10 @@ class SendfileMixin(SendfileBase):
 
         self.assertTrue(1024 <= srv_proto.nbytes < len(self.DATA),
                         srv_proto.nbytes)
+        if sys.platform != 'win32' or \
+                        not isinstance(self.loop, asyncio.ProactorEventLoop):
+            self.assertTrue(1024 <= self.file.tell() < len(self.DATA),
+                            self.file.tell())
         self.assertTrue(cli_proto.transport.is_closing())
 
     def test_sendfile_fallback_close_peer_in_the_middle_of_receiving(self):
