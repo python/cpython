45fbc14af017fecd838524d42d90985752b5a8a5
Nadir Chowdhury <nchowdhury@midjourney.com>
2023-10-28 18:30:18 +0000

    support resolving typevars through generic classes

--- Lib/typing.py
+++ Lib/typing.py
@@ -1287,66 +1287,7 @@ class _GenericAlias(_BaseGenericAlias, _root=True):
         return tuple(self._make_substitution(self.__args__, new_arg_by_param))
 
     def _make_substitution(self, args, new_arg_by_param):
-        """Create a list of new type arguments."""
-        new_args = []
-        for old_arg in args:
-            if isinstance(old_arg, type):
-                new_args.append(old_arg)
-                continue
-
-            substfunc = getattr(old_arg, '__typing_subst__', None)
-            if substfunc:
-                new_arg = substfunc(new_arg_by_param[old_arg])
-            else:
-                subparams = getattr(old_arg, '__parameters__', ())
-                if not subparams:
-                    new_arg = old_arg
-                else:
-                    subargs = []
-                    for x in subparams:
-                        if isinstance(x, TypeVarTuple):
-                            subargs.extend(new_arg_by_param[x])
-                        else:
-                            subargs.append(new_arg_by_param[x])
-                    new_arg = old_arg[tuple(subargs)]
-
-            if self.__origin__ == collections.abc.Callable and isinstance(new_arg, tuple):
-                # Consider the following `Callable`.
-                #   C = Callable[[int], str]
-                # Here, `C.__args__` should be (int, str) - NOT ([int], str).
-                # That means that if we had something like...
-                #   P = ParamSpec('P')
-                #   T = TypeVar('T')
-                #   C = Callable[P, T]
-                #   D = C[[int, str], float]
-                # ...we need to be careful; `new_args` should end up as
-                # `(int, str, float)` rather than `([int, str], float)`.
-                new_args.extend(new_arg)
-            elif _is_unpacked_typevartuple(old_arg):
-                # Consider the following `_GenericAlias`, `B`:
-                #   class A(Generic[*Ts]): ...
-                #   B = A[T, *Ts]
-                # If we then do:
-                #   B[float, int, str]
-                # The `new_arg` corresponding to `T` will be `float`, and the
-                # `new_arg` corresponding to `*Ts` will be `(int, str)`. We
-                # should join all these types together in a flat list
-                # `(float, int, str)` - so again, we should `extend`.
-                new_args.extend(new_arg)
-            elif isinstance(old_arg, tuple):
-                # Corner case:
-                #    P = ParamSpec('P')
-                #    T = TypeVar('T')
-                #    class Base(Generic[P]): ...
-                # Can be substituted like this:
-                #    X = Base[[int, T]]
-                # In this case, `old_arg` will be a tuple:
-                new_args.append(
-                    tuple(self._make_substitution(old_arg, new_arg_by_param)),
-                )
-            else:
-                new_args.append(new_arg)
-        return new_args
+        return _make_substitution(self.__origin__, args, new_arg_by_param)
 
     def copy_with(self, args):
         return self.__class__(self.__origin__, args, name=self._name, inst=self._inst)
@@ -1391,6 +1332,68 @@ class _GenericAlias(_BaseGenericAlias, _root=True):
     def __iter__(self):
         yield Unpack[self]
 
+
+def _make_substitution(origin, args, new_arg_by_param):
+    """Create a list of new type arguments."""
+    new_args = []
+    for old_arg in args:
+        if isinstance(old_arg, type):
+            new_args.append(old_arg)
+            continue
+
+        substfunc = getattr(old_arg, '__typing_subst__', None)
+        if substfunc:
+            new_arg = substfunc(new_arg_by_param[old_arg])
+        else:
+            subparams = getattr(old_arg, '__parameters__', ())
+            if not subparams:
+                new_arg = old_arg
+            else:
+                subargs = []
+                for x in subparams:
+                    if isinstance(x, TypeVarTuple):
+                        subargs.extend(new_arg_by_param[x])
+                    else:
+                        subargs.append(new_arg_by_param[x])
+                new_arg = old_arg[tuple(subargs)]
+
+        if origin == collections.abc.Callable and isinstance(new_arg, tuple):
+            # Consider the following `Callable`.
+            #   C = Callable[[int], str]
+            # Here, `C.__args__` should be (int, str) - NOT ([int], str).
+            # That means that if we had something like...
+            #   P = ParamSpec('P')
+            #   T = TypeVar('T')
+            #   C = Callable[P, T]
+            #   D = C[[int, str], float]
+            # ...we need to be careful; `new_args` should end up as
+            # `(int, str, float)` rather than `([int, str], float)`.
+            new_args.extend(new_arg)
+        elif _is_unpacked_typevartuple(old_arg):
+            # Consider the following `_GenericAlias`, `B`:
+            #   class A(Generic[*Ts]): ...
+            #   B = A[T, *Ts]
+            # If we then do:
+            #   B[float, int, str]
+            # The `new_arg` corresponding to `T` will be `float`, and the
+            # `new_arg` corresponding to `*Ts` will be `(int, str)`. We
+            # should join all these types together in a flat list
+            # `(float, int, str)` - so again, we should `extend`.
+            new_args.extend(new_arg)
+        elif isinstance(old_arg, tuple):
+            # Corner case:
+            #    P = ParamSpec('P')
+            #    T = TypeVar('T')
+            #    class Base(Generic[P]): ...
+            # Can be substituted like this:
+            #    X = Base[[int, T]]
+            # In this case, `old_arg` will be a tuple:
+            new_args.append(
+                tuple(_make_substitution(origin, old_arg, new_arg_by_param)),
+            )
+        else:
+            new_args.append(new_arg)
+    return new_args
 
 # _nparams is the number of accepted parameters, e.g. 0 for Hashable,
 # 1 for List and 2 for Dict.  It may be -1 if variable number of
@@ -2147,8 +2150,42 @@ def get_type_hints(obj, globalns=None, localns=None, include_extras=False):
         return {}
     # Classes require a special treatment.
     if isinstance(obj, type):
+        # mapping[cls, list[types]]
+        parameters = defaultdict(list)
         hints = {}
+        previous_base = None
         for base in reversed(obj.__mro__):
+            if issubclass(base, Generic) and base is not Generic:
+                orig_bases = base.__orig_bases__
+                for orig_base in orig_bases:
+                    origin = get_origin(orig_base)
+                    args = get_args(orig_base)
+
+                    access = origin
+                    if origin is Generic:
+                        access = base
+
+                    parameters[access].append(args)
+
+            for attribute, hint in hints.items():
+                substitutions = parameters[previous_base]
+                mapping = {
+                    substitutions[0][i]: substitutions[-1][i] for i in range(len(substitutions))}
+
+                sub = hint
+
+                origin = get_origin(hint)
+                if origin is not None:
+                    subs = tuple(_make_substitution(origin, get_args(hint), mapping))
+                    if isinstance(origin, _GenericAlias):
+                        sub = origin.copy_with(subs)
+                    else:
+                        # how to copy_with types.GenericAlias?
+                        sub = origin[subs]
+                elif isinstance(hint, TypeVar):
+                    sub = mapping[hint]
+                hints[attribute] = sub
+
             if globalns is None:
                 base_globals = getattr(sys.modules.get(base.__module__, None), '__dict__', {})
             else:
@@ -2172,6 +2209,8 @@ def get_type_hints(obj, globalns=None, localns=None, include_extras=False):
                     value = ForwardRef(value, is_argument=False, is_class=True)
                 value = _eval_type(value, base_globals, base_locals)
                 hints[name] = value
+
+            previous_base = base
         return hints if include_extras else {k: _strip_annotations(t) for k, t in hints.items()}
 
     if globalns is None:
