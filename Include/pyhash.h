0ce377a5933a55b05e0ba5cdac243f978f8efb11
Sergey B Kirpichev <skirpichev@gmail.com>
2023-10-28 05:03:20 +0300

    gh-111389: restoring _PyHASH_INF/BITS/MODULUS/IMAG macroses as public
    This partially reverts #107026.

--- Include/pyhash.h
+++ Include/pyhash.h
@@ -16,6 +16,22 @@ typedef struct {
 PyAPI_FUNC(PyHash_FuncDef*) PyHash_GetFuncDef(void);
 #endif
 
+/* Prime multiplier used in string and various other hashes. */
+#define _PyHASH_MULTIPLIER 1000003UL  /* 0xf4243 */
+
+/* Parameters used for the numeric hash implementation.  See notes for
+   _Py_HashDouble in Python/pyhash.c.  Numeric hashes are based on
+   reduction modulo the prime 2**_PyHASH_BITS - 1. */
+
+#if SIZEOF_VOID_P >= 8
+#  define _PyHASH_BITS 61
+#else
+#  define _PyHASH_BITS 31
+#endif
+
+#define _PyHASH_MODULUS (((size_t)1 << _PyHASH_BITS) - 1)
+#define _PyHASH_INF 314159
+#define _PyHASH_IMAG _PyHASH_MULTIPLIER
 
 /* Cutoff for small string DJBX33A optimization in range [1, cutoff).
  *
