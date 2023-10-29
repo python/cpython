a65aefff640b82c9b9d5a7861b54d2d040176fb1
Savannah Ostrowski <sostrowski@microsoft.com>
2023-10-28 19:58:43 -0700

    Update pycore_optimizer.h
    Co-authored-by: Brandt Bucher <brandtbucher@gmail.com>

--- Include/internal/pycore_optimizer.h
+++ Include/internal/pycore_optimizer.h
@@ -14,15 +14,10 @@ int _Py_uop_analyze_and_optimize(PyCodeObject *code,
     _PyUOpInstruction *trace, int trace_len, int curr_stackentries);
 
 extern PyTypeObject _PyCounterExecutor_Type;
-#define _PyCounterExecutor_Check(op) Py_IS_TYPE((op), &_PyCounterExecutor_Type)
 extern PyTypeObject _PyCounterOptimizer_Type;
-#define _PyCounterOptimizer_Check(op) Py_IS_TYPE((op), &_PyCounterOptimizer_Type)
 extern PyTypeObject _PyDefaultOptimizer_Type;
-#define _PyDefaultOptimizer_Check(op) Py_IS_TYPE((op), &_PyDefaultOptimizer_Type)
 extern PyTypeObject _PyUOpExecutor_Type;
-#define _PyUOpExecutor_Check(op) Py_IS_TYPE((op), &_PyUOpExecutor_Type)
 extern PyTypeObject _PyUOpOptimizer_Type;
-#define _PyUOpOptimizer_Check(op) Py_IS_TYPE((op), &_PyUOpOptimizer_Type)
 
 #ifdef __cplusplus
 }
