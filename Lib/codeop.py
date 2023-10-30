e2e586d02caad0d0f10ed3ebd5f9aab19140834a
Pablo Galindo <pablogsal@gmail.com>
2023-10-30 19:29:02 +0000

    fixup! [3.12] gh-111366: Correctly show custom syntax error messages in the codeop module functions (GH-111384). (cherry picked from commit cd6e0a04a16535d8bc727c84f73730c53267184e)

--- Lib/codeop.py
+++ Lib/codeop.py
@@ -72,6 +72,14 @@ def _maybe_compile(compiler, source, filename, symbol):
 
     return compiler(source, filename, symbol, incomplete_input=False)
 
+def _is_syntax_error(err1, err2):
+    rep1 = repr(err1)
+    rep2 = repr(err2)
+    if "was never closed" in rep1 and "was never closed" in rep2:
+        return False
+    if rep1 == rep2:
+        return True
+    return False
 
 def _compile(source, filename, symbol, incomplete_input=True):
     flags = 0
