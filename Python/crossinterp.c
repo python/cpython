8022f49393ea4b9fd95ade2f4dbb5c0d93e26549
Anthony Shaw <anthony.p.shaw@gmail.com>
2023-10-31 23:17:20 +0900

    GH-111438: Add Support for Sharing Floats Between Interpreters (gh-111439)
    This only affects users of the APIs in pycore_crossinterp.h (AKA _xxsubinterpretersmodule.c and _xxinterpchannels.c).

--- Python/crossinterp.c
+++ Python/crossinterp.c
@@ -586,6 +586,29 @@ _long_shared(PyThreadState *tstate, PyObject *obj,
 }
 
 static PyObject *
+_new_float_object(_PyCrossInterpreterData *data)
+{
+    double * value_ptr = data->data;
+    return PyFloat_FromDouble(*value_ptr);
+}
+
+static int
+_float_shared(PyThreadState *tstate, PyObject *obj,
+             _PyCrossInterpreterData *data)
+{
+    if (_PyCrossInterpreterData_InitWithSize(
+            data, tstate->interp, sizeof(double), NULL,
+            _new_float_object
+            ) < 0)
+    {
+        return -1;
+    }
+    double *shared = (double *)data->data;
+    *shared = PyFloat_AsDouble(obj);
+    return 0;
+}
+
+static PyObject *
 _new_none_object(_PyCrossInterpreterData *data)
 {
     // XXX Singleton refcounts are problematic across interpreters...
@@ -623,5 +646,10 @@ _register_builtins_for_crossinterpreter_data(struct _xidregistry *xidregistry)
     // str
     if (_xidregistry_add_type(xidregistry, &PyUnicode_Type, _str_shared) != 0) {
         Py_FatalError("could not register str for cross-interpreter sharing");
+    }
+
+    // float
+    if (_xidregistry_add_type(xidregistry, &PyFloat_Type, _float_shared) != 0) {
+        Py_FatalError("could not register float for cross-interpreter sharing");
     }
 }
