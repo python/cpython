6dd1c3a8fb605f033497c00e5d2fbf59800422fc
Michael Droettboom <mdboom@gmail.com>
2023-10-30 20:10:07 -0400

    gh-111277: In summarize_stats.py, don't fail fast on invalid ratios (#111278)

--- Tools/scripts/summarize_stats.py
+++ Tools/scripts/summarize_stats.py
@@ -421,8 +421,6 @@ class Ratio:
         self.num = num
         self.den = den
         self.percentage = percentage
-        if den == 0 and num != 0:
-            raise ValueError("Invalid denominator")
 
     def __float__(self):
         if self.den == 0:
@@ -433,7 +431,11 @@ class Ratio:
             return self.num / self.den
 
     def markdown(self) -> str:
-        if self.den == 0 or self.den is None:
+        if self.den is None:
+            return ""
+        elif self.den == 0:
+            if self.num != 0:
+                return f"{self.num:,} / 0 !!"
             return ""
         elif self.percentage:
             return f"{self.num / self.den:,.01%}"
