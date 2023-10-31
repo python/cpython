17d60d15c8c47a29a3a15e1588938b7e0e20402b
kalyanr <kalyan.ben10@live.com>
2023-10-31 23:47:38 +0530

    add 2 tests

--- Lib/test/test_capi/test_list.py
+++ Lib/test/test_capi/test_list.py
@@ -1,10 +1,14 @@
 import unittest
 import sys
 from test.support import import_helper
+from collections import UserList
 
 _testcapi = import_helper.import_module('_testcapi')
 
 NULL = None
+
+class ListSubclass(list):
+    ...
 
 class CAPITest(unittest.TestCase):
     def test_check(self):
@@ -12,9 +16,25 @@ class CAPITest(unittest.TestCase):
         check = _testcapi.list_check
         self.assertTrue(check([1, 2]))
         self.assertTrue(check([]))
+        self.assertTrue(check(ListSubclass([1, 2])))
         self.assertFalse(check({1: 2}))
         self.assertFalse(check((1, 2)))
-        self.assertFalse(check('abc'))
-        self.assertFalse(check(b'abc'))
         self.assertFalse(check(42))
         self.assertFalse(check(object()))
+
+    def test_list_check_exact(self):
+        check = _testcapi.list_check_exact
+        self.assertTrue(check([1]))
+        self.assertTrue(check([]))
+        self.assertFalse(check(ListSubclass([1])))
+        self.assertFalse(check(UserList([1, 2])))
+        self.assertFalse(check({1: 2}))
+        self.assertFalse(check(object()))
+
+    def test_list_new(self):
+        list_new = _testcapi.list_new
+        lst = list_new()
+        self.assertEqual(lst, [])
+        self.assertIs(type(lst), list)
+        lst2 = list_new()
+        self.assertIsNot(lst2, lst)
--- Modules/_testcapi/list.c
+++ Modules/_testcapi/list.c
@@ -2,14 +2,30 @@
 #include "util.h"
 
 static PyObject *
-list_check(PyObject* self, PyObject *obj)
+list_check(PyObject* Py_UNUSED(module), PyObject *obj)
 {
     NULLABLE(obj);
     return PyLong_FromLong(PyList_Check(obj));
 }
 
+static PyObject *
+list_check_exact(PyObject* Py_UNUSED(module), PyObject *obj)
+{
+    NULLABLE(obj);
+    return PyLong_FromLong(PyList_CheckExact(obj));
+}
+
+static PyObject *
+list_new(PyObject *self, PyObject *obj)
+{
+    return PyList_New(obj);
+}
+
+
 static PyMethodDef test_methods[] = {
-    {"list_check", list_check, METH_O}
+    {"list_check", list_check, METH_O},
+    {"list_check_exact", list_check_exact, METH_O},
+    {"list_new", list_new, METH_NOARGS},
 };
 
 int
