07b480a84a497ccf5c11d0840accf5fc4bb200fb
Irit Katriel <iritkatriel@yahoo.com>
2023-11-06 21:56:02 +0000

    Revert "update traceback module to use the new arg"
    This reverts commit 0e570d069f7d87b56f67fce27019a8a19dbb0768.

--- Lib/traceback.py
+++ Lib/traceback.py
@@ -251,7 +251,7 @@ def clear_frames(tb):
     "Clear all references to local variables in the frames of a traceback."
     while tb is not None:
         try:
-            tb.tb_frame.clear(True)
+            tb.tb_frame.clear()
         except RuntimeError:
             # Ignore the exception raised if the frame is still executing.
             pass
