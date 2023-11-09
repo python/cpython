226f4bc69234cb64e52c027e548d4a6dc7f8f0c5
Brett Cannon <brett@python.org>
2023-11-09 15:36:15 -0800

    [3.11] GH-111804: Drop posix.fallocate() under WASI (GH-111869) (GH-111920)
    GH-111804: Drop posix.fallocate() under WASI (GH-111869)
    
    Drop posix.fallocate() under WASI.
    
    The underlying POSIX function, posix_fallocate(), was found to vary too
    much between implementations to remain in WASI. As such, while it was
    available in WASI preview1, it's been dropped in preview2.

--- Modules/posixmodule.c
+++ Modules/posixmodule.c
@@ -10987,7 +10987,10 @@ os_truncate_impl(PyObject *module, path_t *path, Py_off_t length)
 #endif
 
 
-#if defined(HAVE_POSIX_FALLOCATE) && !defined(POSIX_FADVISE_AIX_BUG)
+/* GH-111804: Due to posix_fallocate() not having consistent semantics across
+   OSs, support was dropped in WASI preview2. */
+#if defined(HAVE_POSIX_FALLOCATE) && !defined(POSIX_FADVISE_AIX_BUG) && \
+    !defined(__wasi__)
 /*[clinic input]
 os.posix_fallocate
 
@@ -11025,7 +11028,7 @@ os_posix_fallocate_impl(PyObject *module, int fd, Py_off_t offset,
     errno = result;
     return posix_error();
 }
-#endif /* HAVE_POSIX_FALLOCATE) && !POSIX_FADVISE_AIX_BUG */
+#endif /* HAVE_POSIX_FALLOCATE) && !POSIX_FADVISE_AIX_BUG && !defined(__wasi__) */
 
 
 #if defined(HAVE_POSIX_FADVISE) && !defined(POSIX_FADVISE_AIX_BUG)
