9e149056d74a90beebb43eddc4380dc9d8874ad5
Sergey B Kirpichev <skirpichev@gmail.com>
2023-11-13 10:57:24 +0300

    Use macroses

--- Modules/_testcapi/numbers.c
+++ Modules/_testcapi/numbers.c
@@ -9,433 +9,85 @@ number_check(PyObject *Py_UNUSED(module), PyObject *obj)
     return PyLong_FromLong(PyNumber_Check(obj));
 }
 
-static PyObject *
-number_add(PyObject *Py_UNUSED(module), PyObject *args)
-{
-    PyObject *o1, *o2;
-
-    if (!PyArg_ParseTuple(args, "OO", &o1, &o2)) {
-        return NULL;
-    }
-
-    NULLABLE(o1);
-    NULLABLE(o2);
-    return PyNumber_Add(o1, o2);
-}
-
-static PyObject *
-number_subtract(PyObject *Py_UNUSED(module), PyObject *args)
-{
-    PyObject *o1, *o2;
-
-    if (!PyArg_ParseTuple(args, "OO", &o1, &o2)) {
-        return NULL;
-    }
-
-    NULLABLE(o1);
-    NULLABLE(o2);
-    return PyNumber_Subtract(o1, o2);
-}
-
-static PyObject *
-number_multiply(PyObject *Py_UNUSED(module), PyObject *args)
-{
-    PyObject *o1, *o2;
-
-    if (!PyArg_ParseTuple(args, "OO", &o1, &o2)) {
-        return NULL;
-    }
-
-    NULLABLE(o1);
-    NULLABLE(o2);
-    return PyNumber_Multiply(o1, o2);
-}
-
-static PyObject *
-number_matrixmultiply(PyObject *Py_UNUSED(module), PyObject *args)
-{
-    PyObject *o1, *o2;
-
-    if (!PyArg_ParseTuple(args, "OO", &o1, &o2)) {
-        return NULL;
-    }
-
-    NULLABLE(o1);
-    NULLABLE(o2);
-    return PyNumber_MatrixMultiply(o1, o2);
-}
-
-static PyObject *
-number_floordivide(PyObject *Py_UNUSED(module), PyObject *args)
-{
-    PyObject *o1, *o2;
-
-    if (!PyArg_ParseTuple(args, "OO", &o1, &o2)) {
-        return NULL;
-    }
-
-    NULLABLE(o1);
-    NULLABLE(o2);
-    return PyNumber_FloorDivide(o1, o2);
-}
-
-static PyObject *
-number_truedivide(PyObject *Py_UNUSED(module), PyObject *args)
-{
-    PyObject *o1, *o2;
-
-    if (!PyArg_ParseTuple(args, "OO", &o1, &o2)) {
-        return NULL;
-    }
-
-    NULLABLE(o1);
-    NULLABLE(o2);
-    return PyNumber_TrueDivide(o1, o2);
-}
-
-static PyObject *
-number_remainder(PyObject *Py_UNUSED(module), PyObject *args)
-{
-    PyObject *o1, *o2;
-
-    if (!PyArg_ParseTuple(args, "OO", &o1, &o2)) {
-        return NULL;
-    }
-
-    NULLABLE(o1);
-    NULLABLE(o2);
-    return PyNumber_Remainder(o1, o2);
-}
-
-static PyObject *
-number_divmod(PyObject *Py_UNUSED(module), PyObject *args)
-{
-    PyObject *o1, *o2;
-
-    if (!PyArg_ParseTuple(args, "OO", &o1, &o2)) {
-        return NULL;
-    }
-
-    NULLABLE(o1);
-    NULLABLE(o2);
-    return PyNumber_Divmod(o1, o2);
-}
-
-static PyObject *
-number_power(PyObject *Py_UNUSED(module), PyObject *args)
-{
-    PyObject *o1, *o2, *o3;
-
-    if (!PyArg_ParseTuple(args, "OOO", &o1, &o2, &o3)) {
-        return NULL;
-    }
-
-    NULLABLE(o1);
-    NULLABLE(o2);
-    return PyNumber_Power(o1, o2, o3);
-}
-
-static PyObject *
-number_negative(PyObject *Py_UNUSED(module), PyObject *obj)
-{
-    NULLABLE(obj);
-    return PyNumber_Negative(obj);
-}
-
-static PyObject *
-number_positive(PyObject *Py_UNUSED(module), PyObject *obj)
-{
-    NULLABLE(obj);
-    return PyNumber_Positive(obj);
-}
-
-static PyObject *
-number_absolute(PyObject *Py_UNUSED(module), PyObject *obj)
-{
-    NULLABLE(obj);
-    return PyNumber_Absolute(obj);
-}
-
-static PyObject *
-number_invert(PyObject *Py_UNUSED(module), PyObject *obj)
-{
-    NULLABLE(obj);
-    return PyNumber_Invert(obj);
-}
-
-static PyObject *
-number_lshift(PyObject *Py_UNUSED(module), PyObject *args)
-{
-    PyObject *o1, *o2;
-
-    if (!PyArg_ParseTuple(args, "OO", &o1, &o2)) {
-        return NULL;
-    }
-
-    NULLABLE(o1);
-    NULLABLE(o2);
-    return PyNumber_Lshift(o1, o2);
-}
-
-static PyObject *
-number_rshift(PyObject *Py_UNUSED(module), PyObject *args)
-{
-    PyObject *o1, *o2;
-
-    if (!PyArg_ParseTuple(args, "OO", &o1, &o2)) {
-        return NULL;
-    }
-
-    NULLABLE(o1);
-    NULLABLE(o2);
-    return PyNumber_Rshift(o1, o2);
-}
-
-static PyObject *
-number_and(PyObject *Py_UNUSED(module), PyObject *args)
-{
-    PyObject *o1, *o2;
-
-    if (!PyArg_ParseTuple(args, "OO", &o1, &o2)) {
-        return NULL;
-    }
-
-    NULLABLE(o1);
-    NULLABLE(o2);
-    return PyNumber_And(o1, o2);
-}
-
-static PyObject *
-number_xor(PyObject *Py_UNUSED(module), PyObject *args)
-{
-    PyObject *o1, *o2;
-
-    if (!PyArg_ParseTuple(args, "OO", &o1, &o2)) {
-        return NULL;
-    }
-
-    NULLABLE(o1);
-    NULLABLE(o2);
-    return PyNumber_Xor(o1, o2);
-}
-
-static PyObject *
-number_or(PyObject *Py_UNUSED(module), PyObject *args)
-{
-    PyObject *o1, *o2;
-
-    if (!PyArg_ParseTuple(args, "OO", &o1, &o2)) {
-        return NULL;
-    }
-
-    NULLABLE(o1);
-    NULLABLE(o2);
-    return PyNumber_Or(o1, o2);
-}
-
-static PyObject *
-number_inplaceadd(PyObject *Py_UNUSED(module), PyObject *args)
-{
-    PyObject *o1, *o2;
-
-    if (!PyArg_ParseTuple(args, "OO", &o1, &o2)) {
-        return NULL;
-    }
-
-    NULLABLE(o1);
-    NULLABLE(o2);
-    return PyNumber_InPlaceAdd(o1, o2);
-}
-
-static PyObject *
-number_inplacesubtract(PyObject *Py_UNUSED(module), PyObject *args)
-{
-    PyObject *o1, *o2;
-
-    if (!PyArg_ParseTuple(args, "OO", &o1, &o2)) {
-        return NULL;
-    }
-
-    NULLABLE(o1);
-    NULLABLE(o2);
-    return PyNumber_InPlaceSubtract(o1, o2);
-}
-
-static PyObject *
-number_inplacemultiply(PyObject *Py_UNUSED(module), PyObject *args)
-{
-    PyObject *o1, *o2;
-
-    if (!PyArg_ParseTuple(args, "OO", &o1, &o2)) {
-        return NULL;
-    }
-
-    NULLABLE(o1);
-    NULLABLE(o2);
-    return PyNumber_InPlaceMultiply(o1, o2);
-}
-
-static PyObject *
-number_inplacematrixmultiply(PyObject *Py_UNUSED(module), PyObject *args)
-{
-    PyObject *o1, *o2;
-
-    if (!PyArg_ParseTuple(args, "OO", &o1, &o2)) {
-        return NULL;
-    }
-
-    NULLABLE(o1);
-    NULLABLE(o2);
-    return PyNumber_InPlaceMatrixMultiply(o1, o2);
-}
-
-static PyObject *
-number_inplacefloordivide(PyObject *Py_UNUSED(module), PyObject *args)
-{
-    PyObject *o1, *o2;
-
-    if (!PyArg_ParseTuple(args, "OO", &o1, &o2)) {
-        return NULL;
-    }
-
-    NULLABLE(o1);
-    NULLABLE(o2);
-    return PyNumber_InPlaceFloorDivide(o1, o2);
-}
-
-static PyObject *
-number_inplacetruedivide(PyObject *Py_UNUSED(module), PyObject *args)
-{
-    PyObject *o1, *o2;
-
-    if (!PyArg_ParseTuple(args, "OO", &o1, &o2)) {
-        return NULL;
-    }
-
-    NULLABLE(o1);
-    NULLABLE(o2);
-    return PyNumber_InPlaceTrueDivide(o1, o2);
-}
-
-static PyObject *
-number_inplaceremainder(PyObject *Py_UNUSED(module), PyObject *args)
-{
-    PyObject *o1, *o2;
-
-    if (!PyArg_ParseTuple(args, "OO", &o1, &o2)) {
-        return NULL;
-    }
-
-    NULLABLE(o1);
-    NULLABLE(o2);
-    return PyNumber_InPlaceRemainder(o1, o2);
-}
-
-static PyObject *
-number_inplacepower(PyObject *Py_UNUSED(module), PyObject *args)
-{
-    PyObject *o1, *o2, *o3;
-
-    if (!PyArg_ParseTuple(args, "OOO", &o1, &o2, &o3)) {
-        return NULL;
-    }
-
-    NULLABLE(o1);
-    NULLABLE(o2);
-    NULLABLE(o3);
-    return PyNumber_InPlacePower(o1, o2, o3);
-}
-
-static PyObject *
-number_inplacelshift(PyObject *Py_UNUSED(module), PyObject *args)
-{
-    PyObject *o1, *o2;
-
-    if (!PyArg_ParseTuple(args, "OO", &o1, &o2)) {
-        return NULL;
-    }
-
-    NULLABLE(o1);
-    NULLABLE(o2);
-    return PyNumber_InPlaceLshift(o1, o2);
-}
-
-static PyObject *
-number_inplacershift(PyObject *Py_UNUSED(module), PyObject *args)
-{
-    PyObject *o1, *o2;
-
-    if (!PyArg_ParseTuple(args, "OO", &o1, &o2)) {
-        return NULL;
-    }
-
-    NULLABLE(o1);
-    NULLABLE(o2);
-    return PyNumber_InPlaceRshift(o1, o2);
-}
-
-static PyObject *
-number_inplaceand(PyObject *Py_UNUSED(module), PyObject *args)
-{
-    PyObject *o1, *o2;
-
-    if (!PyArg_ParseTuple(args, "OO", &o1, &o2)) {
-        return NULL;
-    }
-
-    NULLABLE(o1);
-    NULLABLE(o2);
-    return PyNumber_InPlaceAnd(o1, o2);
-}
-
-static PyObject *
-number_inplacexor(PyObject *Py_UNUSED(module), PyObject *args)
-{
-    PyObject *o1, *o2;
-
-    if (!PyArg_ParseTuple(args, "OO", &o1, &o2)) {
-        return NULL;
-    }
-
-    NULLABLE(o1);
-    NULLABLE(o2);
-    return PyNumber_InPlaceXor(o1, o2);
-}
-
-static PyObject *
-number_inplaceor(PyObject *Py_UNUSED(module), PyObject *args)
-{
-    PyObject *o1, *o2;
-
-    if (!PyArg_ParseTuple(args, "OO", &o1, &o2)) {
-        return NULL;
-    }
-
-    NULLABLE(o1);
-    NULLABLE(o2);
-    return PyNumber_InPlaceOr(o1, o2);
-}
-
-static PyObject *
-number_long(PyObject *Py_UNUSED(module), PyObject *obj)
-{
-    NULLABLE(obj);
-    return PyNumber_Long(obj);
-}
-
-static PyObject *
-number_float(PyObject *Py_UNUSED(module), PyObject *obj)
-{
-    NULLABLE(obj);
-    return PyNumber_Float(obj);
-}
-
-static PyObject *
-number_index(PyObject *Py_UNUSED(module), PyObject *obj)
-{
-    NULLABLE(obj);
-    return PyNumber_Index(obj);
-}
+#define BINARYFUNC(funcsuffix, methsuffix)                           \
+    static PyObject *                                                \
+    number_##methsuffix(PyObject *Py_UNUSED(module), PyObject *args) \
+    {                                                                \
+        PyObject *o1, *o2;                                           \
+                                                                     \
+        if (!PyArg_ParseTuple(args, "OO", &o1, &o2)) {               \
+            return NULL;                                             \
+        }                                                            \
+                                                                     \
+        NULLABLE(o1);                                                \
+        NULLABLE(o2);                                                \
+        return PyNumber_##funcsuffix(o1, o2);                        \
+    };
+
+BINARYFUNC(Add, add)
+BINARYFUNC(Subtract, subtract)
+BINARYFUNC(Multiply, multiply)
+BINARYFUNC(MatrixMultiply, matrixmultiply)
+BINARYFUNC(FloorDivide, floordivide)
+BINARYFUNC(TrueDivide, truedivide)
+BINARYFUNC(Remainder, remainder)
+BINARYFUNC(Divmod, divmod)
+
+#define TERNARYFUNC(funcsuffix, methsuffix)                          \
+    static PyObject *                                                \
+    number_##methsuffix(PyObject *Py_UNUSED(module), PyObject *args) \
+    {                                                                \
+        PyObject *o1, *o2, *o3;                                      \
+                                                                     \
+        if (!PyArg_ParseTuple(args, "OOO", &o1, &o2, &o3)) {         \
+            return NULL;                                             \
+        }                                                            \
+                                                                     \
+        NULLABLE(o1);                                                \
+        NULLABLE(o2);                                                \
+        return PyNumber_Power(o1, o2, o3);                           \
+    };
+
+TERNARYFUNC(Power, power)
+
+#define UNARYFUNC(funcsuffix, methsuffix)                            \
+    static PyObject *                                                \
+    number_##methsuffix(PyObject *Py_UNUSED(module), PyObject *obj)  \
+    {                                                                \
+        NULLABLE(obj);                                               \
+        return PyNumber_##funcsuffix(obj);                           \
+    };
+
+UNARYFUNC(Negative, negative)
+UNARYFUNC(Positive, positive)
+UNARYFUNC(Absolute, absolute)
+UNARYFUNC(Invert, invert)
+
+BINARYFUNC(Lshift, lshift)
+BINARYFUNC(Rshift, rshift)
+BINARYFUNC(And, and)
+BINARYFUNC(Xor, xor)
+BINARYFUNC(Or, or)
+
+BINARYFUNC(InPlaceAdd, inplaceadd)
+BINARYFUNC(InPlaceSubtract, inplacesubtract)
+BINARYFUNC(InPlaceMultiply, inplacemultiply)
+BINARYFUNC(InPlaceMatrixMultiply, inplacematrixmultiply)
+BINARYFUNC(InPlaceFloorDivide, inplacefloordivide)
+BINARYFUNC(InPlaceTrueDivide, inplacetruedivide)
+BINARYFUNC(InPlaceRemainder, inplaceremainder)
+
+TERNARYFUNC(InPlacePower, inplacepower)
+
+BINARYFUNC(InPlaceLshift, inplacelshift)
+BINARYFUNC(InPlaceRshift, inplacershift)
+BINARYFUNC(InPlaceAnd, inplaceand)
+BINARYFUNC(InPlaceXor, inplacexor)
+BINARYFUNC(InPlaceOr, inplaceor)
+
+UNARYFUNC(Long, long)
+UNARYFUNC(Float, float)
+UNARYFUNC(Index, index)
 
 static PyObject *
 number_tobase(PyObject *Py_UNUSED(module), PyObject *args)
