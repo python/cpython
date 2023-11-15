6bd60a7847307de0b5b235dc6441d1a4e5cf69e2
Victor Stinner <vstinner@python.org>
2023-11-15 21:47:14 +0100

    gh-112088: Run autoreconf in GHA check_generated_files (#112090)
    The "Check if generated files are up to date" job of GitHub Actions
    now runs the "autoreconf -ivf -Werror" command instead of the "make
    regen-configure" command to avoid depending on the external quay.io
    server.
    
    Add Tools/build/regen-configure.sh script to regenerate the configure
    with an Ubuntu container image. The
    "quay.io/tiran/cpython_autoconf:271" container image
    (https://github.com/tiran/cpython_autoconf) is no longer used.

--- .github/workflows/posix-deps-apt.sh
+++ .github/workflows/posix-deps-apt.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 apt-get update
 
+# autoconf-archive is needed by autoreconf (check_generated_files job)
 apt-get -yq install \
     build-essential \
     pkg-config \
+    autoconf-archive \
     ccache \
     gdb \
     lcov \
