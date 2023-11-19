2303e79484c214d1d7951470ba34d751cb10af83
Gregory P. Smith <greg@krypto.org>
2023-11-19 14:22:09 -0800

    Add an assertion that enc_name starts with [ before appending ]

--- Lib/http/client.py
+++ Lib/http/client.py
@@ -176,6 +176,7 @@ def _strip_ipv6_iface(enc_name: bytes) -> bytes:
     """Remove interface scope from IPv6 address."""
     enc_name, percent, _ = enc_name.partition(b"%")
     if percent:
+        assert enc_name.startswith(b'['), enc_name
         enc_name += b']'
     return enc_name
 
