ddba5ed2be9f52c4fc5b72fc5124f0be8c380699
Guido van Rossum <guido@python.org>
2023-11-20 10:09:49 -0800

    Use CURRENT_OPARG() and CURRENT_OPERAND() macros
    This prepares us for the JIT template, hopefully.

--- Tools/cases_generator/generate_cases.py
+++ Tools/cases_generator/generate_cases.py
@@ -811,8 +811,7 @@ class Generator(Analyzer):
                     self.out.emit("")
                     with self.out.block(f"case {instr.name}:"):
                         if instr.instr_flags.HAS_ARG_FLAG:
-                            # TODO: Skip this when generating JIT code
-                            self.out.emit("oparg = next_uop[-1].oparg;")
+                            self.out.emit("oparg = CURRENT_OPARG();")
                         stacking.write_single_instr(instr, self.out, tier=TIER_TWO)
                         if instr.check_eval_breaker:
                             self.out.emit("CHECK_EVAL_BREAKER();")
