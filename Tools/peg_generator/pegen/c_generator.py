9085f8fd8879a963395ef9bbc23a39ca6fdbebb0
Brett Cannon <brett@python.org>
2023-11-20 05:27:33 -0800

    GH-111807: Lower the parser stack depth under WASI debug builds (#112225)

--- Tools/peg_generator/pegen/c_generator.py
+++ Tools/peg_generator/pegen/c_generator.py
@@ -38,7 +38,11 @@ EXTENSION_PREFIX = """\
 #endif
 
 #ifdef __wasi__
-#  define MAXSTACK 4000
+#  ifdef Py_DEBUG
+#    define MAXSTACK 1000
+#  else
+#    define MAXSTACK 4000
+#  endif
 #else
 #  define MAXSTACK 6000
 #endif
