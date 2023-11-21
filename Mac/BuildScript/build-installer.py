9404f465f9a09503f0117673601fa7be1eed2fea
Christopher Chavez <chrischavez@gmx.us>
2023-11-21 02:12:19 -0600

    gh-110950: add upstream Tk fixes to macOS installer. (GH-111041)
    Add upstream Tk patches for three problems affecting tkinter users:
    
    - Update macOS installer to include a fix accepted by upstream Tcl/Tk
    for a crash encountered after the first :meth:`tkinter.Tk` instance
    is destroyed. (gh-92603)
    
    - Update macOS installer to include an upstream Tcl/Tk fix
    for the ``ttk::ThemeChanged`` error encountered in Tkinter. (gh-71383)
    
    - Update macOS installer to include an upstream Tcl/Tk fix for the
    ``Secure coding is not enabled for restorable state!`` warning
    encountered in Tkinter on macOS 14 Sonoma. (gh-110950)
    
    Co-authored-by: Ned Deily <nad@python.org>

--- Mac/BuildScript/build-installer.py
+++ Mac/BuildScript/build-installer.py
@@ -261,14 +261,14 @@ def library_recipes():
             tcl_checksum='81656d3367af032e0ae6157eff134f89'
 
             tk_checksum='5e0faecba458ee1386078fb228d008ba'
-            tk_patches = ['tk868_on_10_8_10_9.patch']
+            tk_patches = ['backport_gh71383_fix.patch', 'tk868_on_10_8_10_9.patch', 'backport_gh110950_fix.patch']
 
         else:
             tcl_tk_ver='8.6.13'
             tcl_checksum='43a1fae7412f61ff11de2cfd05d28cfc3a73762f354a417c62370a54e2caf066'
 
             tk_checksum='2e65fa069a23365440a3c56c556b8673b5e32a283800d8d9b257e3f584ce0675'
-            tk_patches = [ ]
+            tk_patches = ['backport_gh92603_fix.patch', 'backport_gh71383_fix.patch', 'backport_gh110950_fix.patch']
 
 
         base_url = "https://prdownloads.sourceforge.net/tcl/{what}{version}-src.tar.gz"
