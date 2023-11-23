789338532f84589ec88448826a01eb683e4c7ca8
Brett Cannon <brett@python.org>
2023-11-23 14:13:31 -0800

    Add `configure-host`

--- Tools/wasm/wasi.py
+++ Tools/wasm/wasi.py
@@ -13,10 +13,12 @@ import sys
 import sysconfig
 import tempfile
 
+
 CHECKOUT = pathlib.Path(__file__).parent.parent.parent
 CROSS_BUILD_DIR = CHECKOUT / "cross-build"
 BUILD_DIR = CROSS_BUILD_DIR / "build"
 HOST_TRIPLE = "wasm32-wasi"
+HOST_DIR = CROSS_BUILD_DIR / HOST_TRIPLE
 
 
 def section(working_dir):
@@ -28,6 +30,29 @@ def section(working_dir):
         terminal_width = 80
     print("#" * terminal_width)
     print("📁", working_dir)
+
+
+def updated_env(updates):
+    """Create a new dict representing the environment to use.
+
+    The changes made to the execution environment are printed out.
+    """
+    # Python's first commit:
+    # Thu, 09 Aug 1990 14:25:15 +0000 (1990-08-09)
+    # https://hg.python.org/cpython/rev/3cd033e6b530
+    env_defaults = {"SOURCE_DATE_EPOCH": "650211915"}
+    environment = env_defaults | os.environ | updates
+
+    env_diff = {}
+    for key, value in environment.items():
+        if os.environ.get(key) != value:
+            env_diff[key] = value
+
+    print("Environment changes:")
+    for key in sorted(env_diff.keys()):
+        print(f"  {key}={env_diff[key]}")
+
+    return environment
 
 
 def subdir(working_dir, *, clean_ok=False):
@@ -48,6 +73,7 @@ def subdir(working_dir, *, clean_ok=False):
         return wrapper
 
     return decorator
+
 
 def call(command, *, quiet, **kwargs):
     """Execute a command.
@@ -75,6 +101,17 @@ def build_platform():
     return sysconfig.get_config_var("BUILD_GNU_TYPE")
 
 
+def build_python_path():
+    """The path to the build Python binary."""
+    binary = BUILD_DIR / "python"
+    if not binary.is_file():
+        binary = binary.with_suffix(".exe")
+        if not binary.is_file():
+            raise FileNotFoundError(f"Unable to find `python(.exe)` in {BUILD_DIR}")
+
+    return binary
+
+
 @subdir(CHECKOUT)
 def prep_checkout(context, working_dir):
     """Prepare the source checkout for cross-compiling."""
@@ -100,15 +137,11 @@ def configure_build_python(context, working_dir):
 
 @subdir(BUILD_DIR)
 def make_build_python(context, working_dir):
-    """Make/build the build/host Python."""
+    """Make/build the build Python."""
     call(["make", "--jobs", str(cpu_count()), "all"],
             quiet=context.quiet)
 
-    binary = working_dir / "python"
-    if not binary.is_file():
-        binary = binary.with_suffix(".exe")
-        if not binary.is_file():
-            raise FileNotFoundError(f"Unable to find `python(.exe)` in {working_dir}")
+    binary = build_python_path()
     cmd = [binary, "-c",
             "import sys; "
             "print(f'{sys.version_info.major}.{sys.version_info.minor}')"]
@@ -118,7 +151,7 @@ def make_build_python(context, working_dir):
 
 
 def compile_host_python(context):
-    """Compile the build/host Python.
+    """Compile the build Python.
 
     Returns the path to the new interpreter and it's major.minor version.
     """
@@ -195,6 +228,59 @@ def wasi_sdk_env(context):
     return env
 
 
+@subdir(HOST_DIR, clean_ok=True)
+def configure_wasi_python(context, working_dir):
+    """Configure the WASI/host build."""
+    if not context.wasi_sdk_path or not context.wasi_sdk_path.exists():
+            raise ValueError("wasi-sdk not found or specified; "
+                            "download from https://github.com/WebAssembly/wasi-sdk")
+
+    config_site = os.fsdecode(CHECKOUT / "Tools" / "wasm" / "config.site-wasm32-wasi")
+
+    wasi_build_dir = working_dir.relative_to(CHECKOUT)
+
+    python_build_dir = BUILD_DIR / "build"
+    lib_dirs = list(python_build_dir.glob("lib.*"))
+    assert len(lib_dirs) == 1, f"Expected one lib.* directory in {python_build_dir}"
+    lib_dir = os.fsdecode(lib_dirs[0])
+    pydebug = lib_dir.endswith("-pydebug")
+    python_version = lib_dir.removesuffix("-pydebug").rpartition("-")[-1]
+    sysconfig_data = f"{wasi_build_dir}/build/lib.wasi-wasm32-{python_version}"
+    if pydebug:
+        sysconfig_data += "-pydebug"
+
+    # Use PYTHONPATH to include sysconfig data which must be anchored to the
+    # WASI guest's `/` directory.
+    host_runner = context.host_runner.format(GUEST_DIR="/",
+                                             HOST_DIR=CHECKOUT,
+                                             ENV_VAR_NAME="PYTHONPATH",
+                                             ENV_VAR_VALUE=f"/{sysconfig_data}",
+                                             PYTHON_WASM=working_dir / "python.wasm")
+    env_additions = {"CONFIG_SITE": config_site, "HOSTRUNNER": host_runner}
+    build_python = os.fsdecode(build_python_path())
+    # The path to `configure` MUST be relative, else `python.wasm` is unable
+    # to find the stdlib due to Python not recognizing that it's being
+    # executed from within a checkout.
+    configure = [os.path.relpath(CHECKOUT / 'configure', working_dir),
+                    f"--host={HOST_TRIPLE}",
+                    f"--build={build_platform()}",
+                    f"--with-build-python={build_python}"]
+    if pydebug:
+        configure.append("--with-pydebug")
+    if context.args:
+        configure.extend(context.args)
+    call(configure,
+         env=updated_env(env_additions | wasi_sdk_env(context)),
+         quiet=context.quiet)
+
+    exec_script = working_dir / "python.sh"
+    with exec_script.open("w", encoding="utf-8") as file:
+        file.write(f'#!/bin/sh\nexec {host_runner} "$@"\n')
+    exec_script.chmod(0o755)
+    print(f"Created {exec_script} for easier execution ... ")
+    sys.stdout.flush()
+
+
 def compile_wasi_python(context, build_python, version):
     """Compile the wasm32-wasi Python."""
     build_dir = CROSS_BUILD_DIR / HOST_TRIPLE
@@ -224,7 +310,8 @@ def compile_wasi_python(context, build_python, version):
                      # Python's first commit:
                      # Thu, 09 Aug 1990 14:25:15 +0000 (1990-08-09)
                      # https://hg.python.org/cpython/rev/3cd033e6b530
-                     "SOURCE_DATE_EPOCH": "650211915"}
+                     "SOURCE_DATE_EPOCH":
+                        os.environ.get("SOURCE_DATE_EPOCH", "650211915")}
 
     with contextlib.chdir(build_dir):
         # The path to `configure` MUST be relative, else `python.wasm` is unable
@@ -260,6 +347,20 @@ def compile_wasi_python(context, build_python, version):
 
 
 def main():
+    default_host_runner = (f"{shutil.which('wasmtime')} run "
+                        # Make sure the stack size will work for a pydebug build.
+                        # The 8388608 value comes from `ulimit -s` under Linux which
+                        # is 8291 KiB.
+                        "--wasm max-wasm-stack=8388608 "
+                        # Enable thread support.
+                        "--wasm threads=y --wasi threads=y "
+                        # Map the checkout to / to load the stdlib from /Lib.
+                        "--dir {HOST_DIR}::{GUEST_DIR} "
+                        # Set PYTHONPATH to the sysconfig data.
+                        "--env {ENV_VAR_NAME}={ENV_VAR_VALUE} "
+                        # Path to the WASM binary.
+                        "{PYTHON_WASM}")
+
     parser = argparse.ArgumentParser()
     subcommands = parser.add_subparsers(dest="subcommand")
     build = subcommands.add_parser("build", help="Build everything")
@@ -267,17 +368,29 @@ def main():
                                              help="Run `configure` for the build Python")
     make_build = subcommands.add_parser("make-build-python",
                                         help="Run `make` for the build Python")
-    for subcommand in build, configure_build, make_build:
+    configure_host = subcommands.add_parser("configure-host",
+                                            help="Run `configure` for the host/WASI")
+    for subcommand in build, configure_build, make_build, configure_host:
         subcommand.add_argument("--quiet", action="store_true", default=False,
                         dest="quiet",
                         help="Redirect output from subprocesses to a log file")
-    for subcommand in build, configure_build:
+    for subcommand in build, configure_build, configure_host:
         subcommand.add_argument("--clean", action="store_true", default=False,
                         dest="clean",
                         help="Delete any relevant directories before building")
-    # configure-build-python
-    configure_build.add_argument("args", nargs="*",
-                                 help="Extra arguments to pass to `configure`")
+    for subcommand in configure_build, configure_host:
+        subcommand.add_argument("args", nargs="*",
+                                help="Extra arguments to pass to `configure`")
+    for subcommand in build, configure_host:
+        subcommand.add_argument("--wasi-sdk", type=pathlib.Path, dest="wasi_sdk_path",
+                        default=find_wasi_sdk(),
+                        help="Path to wasi-sdk; defaults to "
+                                "$WASI_SDK_PATH or /opt/wasi-sdk")
+        subcommand.add_argument("--host-runner", action="store",
+                        default=default_host_runner, dest="host_runner",
+                        help="Command template for running the WebAssembly code "
+                                "(default meant for wasmtime 14 or newer: "
+                                f"`{default_host_runner}`)")
     # build
     build.add_argument("--with-pydebug", action="store_true", default=False,
                        dest="debug",
@@ -286,28 +399,6 @@ def main():
                        default="all",
                        help="specify which platform(s) to build for "
                             "(default is 'all')")
-    build.add_argument("--wasi-sdk", type=pathlib.Path, dest="wasi_sdk_path",
-                       default=find_wasi_sdk(),
-                       help="Path to wasi-sdk; defaults to "
-                            "$WASI_SDK_PATH or /opt/wasi-sdk")
-    default_host_runner = (f"{shutil.which('wasmtime')} run "
-                    # Make sure the stack size will work for a pydebug build.
-                    # The 8388608 value comes from `ulimit -s` under Linux which
-                    # is 8291 KiB.
-                    "--wasm max-wasm-stack=8388608 "
-                    # Enable thread support.
-                    "--wasm threads=y --wasi threads=y "
-                    # Map the checkout to / to load the stdlib from /Lib.
-                    "--dir {HOST_DIR}::{GUEST_DIR} "
-                    # Set PYTHONPATH to the sysconfig data.
-                    "--env {ENV_VAR_NAME}={ENV_VAR_VALUE} "
-                    # Path to the WASM binary.
-                    "{PYTHON_WASM}")
-    build.add_argument("--host-runner", action="store",
-                       default=default_host_runner, dest="host_runner",
-                       help="Command template for running the WebAssembly code "
-                            "(default for wasmtime 14 or newer: "
-                            f"`{default_host_runner}`)")
     build.add_argument("--threads", action="store_true", default=False,
                        dest="threads",
                        help="Compile with threads support (off by default as "
@@ -315,11 +406,13 @@ def main():
 
     context = parser.parse_args()
     if context.subcommand == "configure-build-python":
-        prep_checkout(context)
+        prep_checkout(context)  # TODO: merge w/ configure_build_python() once `build` is removed
         print()
         configure_build_python(context)
     elif context.subcommand == "make-build-python":
         make_build_python(context)
+    elif context.subcommand == "configure-host":
+        configure_wasi_python(context)
     else:
         if not context.wasi_sdk_path or not context.wasi_sdk_path.exists():
             raise ValueError("wasi-sdk not found or specified; "
