a25bb4e1ca893202f5fe57b66b521e53bb3e38be
AlexWaygood <alex.waygood@gmail.com>
2023-11-27 23:53:32 +0000

    cleanup

--- Lib/test/test_module/__init__.py
+++ Lib/test/test_module/__init__.py
@@ -267,8 +267,8 @@ a = A(destroyed)"""
 
     def test_module_repr_with_namespace_package(self):
         m = ModuleType('foo')
-        loader = importlib.machinery.NamespaceLoader("foo", ["bar"], "baz")
-        spec = importlib.machinery.ModuleSpec("foo", loader)
+        loader = importlib.machinery.NamespaceLoader('foo', ['bar'], 'baz')
+        spec = importlib.machinery.ModuleSpec('foo', loader)
         m.__loader__ = loader
         m.__spec__ = spec
         self.assertEqual(repr(m), "<module 'foo' (namespace) from ['bar']>")
@@ -276,7 +276,7 @@ a = A(destroyed)"""
     def test_module_repr_with_namespace_package_and_custom_loader(self):
         m = ModuleType('foo')
         loader = BareLoader()
-        spec = importlib.machinery.ModuleSpec("foo", loader)
+        spec = importlib.machinery.ModuleSpec('foo', loader)
         m.__loader__ = loader
         m.__spec__ = spec
         expected_repr_pattern = r"<module 'foo' \(<.*\.BareLoader object at .+>\)>"
@@ -287,12 +287,13 @@ a = A(destroyed)"""
         m = ModuleType('foo')
         loader = BareLoader()
         loader._path = ['spam']
-        spec = importlib.machinery.ModuleSpec("foo", loader)
+        spec = importlib.machinery.ModuleSpec('foo', loader)
         m.__loader__ = loader
         m.__spec__ = spec
         expected_repr_pattern = r"<module 'foo' \(<.*\.BareLoader object at .+>\)>"
         self.assertRegex(repr(m), expected_repr_pattern)
         self.assertNotIn('from', repr(m))
+
     def test_module_finalization_at_shutdown(self):
         # Module globals and builtins should still be available during shutdown
         rc, out, err = assert_python_ok("-c", "from test.test_module import final_a")
