abe507d912c40e54589022bb5043e91bdbb63898
sweeneyde <sweeney.427@osu.edu>
2023-11-30 18:35:45 -0500

    LOAD_GLOBAL can only include one PUSH_NULL

--- Python/flowgraph.c
+++ Python/flowgraph.c
@@ -1554,7 +1554,7 @@ optimize_basic_block(PyObject *const_cache, basicblock *bb, PyObject *consts)
             case KW_NAMES:
                 break;
             case PUSH_NULL:
-                if (nextop == LOAD_GLOBAL && (inst[1].i_opcode & 1) == 0) {
+                if (nextop == LOAD_GLOBAL && (inst[1].i_oparg & 1) == 0) {
                     INSTR_SET_OP0(inst, NOP);
                     inst[1].i_oparg |= 1;
                 }
