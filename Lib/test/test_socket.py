0349f63185365d216306b06556b38206ddaa01e1
Serhiy Storchaka <storchaka@gmail.com>
2023-12-01 15:34:26 +0200

    Fix tests.

--- Lib/test/test_socket.py
+++ Lib/test/test_socket.py
@@ -4705,14 +4705,10 @@ class NonBlockingTCPTests(ThreadedTCPSocketTest):
             self.skipTest('needs UINT_MAX < ULONG_MAX')
 
         self.serv.setblocking(False)
-        if fcntl:
-            self.assertFalse(_is_fd_in_blocking_mode(self.serv))
-        self.assertEqual(self.serv.gettimeout(), 0.0)
+        self.assert_sock_timeout(self.serv, 0.0)
 
         self.serv.setblocking(_testcapi.UINT_MAX + 1)
-        self.assertIsNone(self.serv.gettimeout())
-        if fcntl:
-            self.assertTrue(_is_fd_in_blocking_mode(self.serv))
+        self.assert_sock_timeout(self.serv, None)
 
     _testSetBlocking_overflow = support.cpython_only(_testSetBlocking)
 
