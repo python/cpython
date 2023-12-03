5608112fa92fc1f49924601bd5cc5a24b411f36e
Miss Islington (bot) <31488909+miss-islington@users.noreply.github.com>
2023-12-03 18:39:26 +0100

    [3.12] gh-101100: Fix most Sphinx nitpicks in `inspect.rst` (GH-112662) (#112666)
    gh-101100: Fix most Sphinx nitpicks in `inspect.rst` (GH-112662)
    (cherry picked from commit 45650d1c479a8b0370f126d821718dd3c502f333)
    
    Co-authored-by: Alex Waygood <Alex.Waygood@Gmail.com>

--- Doc/conf.py
+++ Doc/conf.py
@@ -157,6 +157,9 @@ nitpick_ignore = [
     ('envvar', 'USER'),
     ('envvar', 'USERNAME'),
     ('envvar', 'USERPROFILE'),
+    # Deprecated function that was never documented:
+    ('py:func', 'getargspec'),
+    ('py:func', 'inspect.getargspec'),
 ]
 
 # Temporary undocumented names.
