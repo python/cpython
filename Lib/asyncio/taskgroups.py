36c6dba3bf3019a8322159f0973d91699d983bd7
ordinary-jamie <101677823+ordinary-jamie@users.noreply.github.com>
2023-12-03 20:08:38 +1100

    Pass name to loop create_task for TaskGroup
    `TaskGroup.create_task` follows the same pattern as
    `asyncio.create_task`, also including a redundant call to
    `Task.set_name`. Note that `BaseEventLoop.create_task` uses the
    `task_factory` if it is set, in which does not accept a `name` kwarg.
    Hence, while we can remove the redundant call in `TaskGroup` we did not
    remove it in the `asyncio.create_task`

--- Lib/asyncio/taskgroups.py
+++ Lib/asyncio/taskgroups.py
@@ -158,10 +158,10 @@ class TaskGroup:
         if self._aborting:
             raise RuntimeError(f"TaskGroup {self!r} is shutting down")
         if context is None:
-            task = self._loop.create_task(coro)
+            task = self._loop.create_task(coro, name=name)
         else:
-            task = self._loop.create_task(coro, context=context)
-        task.set_name(name)
+            task = self._loop.create_task(coro, name=name, context=context)
+
         # optimization: Immediately call the done callback if the task is
         # already done (e.g. if the coro was able to complete eagerly),
         # and skip scheduling a done callback
