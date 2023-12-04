fa7a6f23036537567592647d15f043722c7144ad
Pablo Galindo <pablogsal@gmail.com>
2023-12-04 17:54:29 +0000

    Python 3.11.7

--- Include/patchlevel.h
+++ Include/patchlevel.h
@@ -18,12 +18,12 @@
 /*--start constants--*/
 #define PY_MAJOR_VERSION        3
 #define PY_MINOR_VERSION        11
-#define PY_MICRO_VERSION        6
+#define PY_MICRO_VERSION        7
 #define PY_RELEASE_LEVEL        PY_RELEASE_LEVEL_FINAL
 #define PY_RELEASE_SERIAL       0
 
 /* Version as a string */
-#define PY_VERSION              "3.11.6+"
+#define PY_VERSION              "3.11.7"
 /*--end constants--*/
 
 /* Version as a single 4-byte hex number, e.g. 0x010502B2 == 1.5.2b2.
--- README.rst
+++ README.rst
@@ -1,4 +1,4 @@
-This is Python version 3.11.6
+This is Python version 3.11.7
 =============================
 
 .. image:: https://github.com/python/cpython/workflows/Tests/badge.svg
