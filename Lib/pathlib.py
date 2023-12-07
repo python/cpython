0f75804f52d2754c652e3faa4b7711cef374ab48
barneygale <barney.gale@gmail.com>
2023-12-07 04:54:09 +0000

    Fix pickle roundtripping

--- Lib/pathlib.py
+++ Lib/pathlib.py
@@ -643,9 +643,7 @@ class PurePath:
         self._resolving = False
 
     def __reduce__(self):
-        # Using the parts tuple helps share interned path parts
-        # when pickling related paths.
-        return (self.__class__, self.parts)
+        return (self.__class__, tuple(self._raw_paths))
 
     def __fspath__(self):
         return str(self)
