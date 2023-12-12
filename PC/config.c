002d6fb19e217afdce2e3a5cce426fc11d3e90ff
Eric Snow <ericsnowcurrently@gmail.com>
2023-12-12 10:43:30 -0700

    gh-76785: More Fixes for test.support.interpreters (gh-113012)
    This brings the module (along with the associated extension modules) mostly in sync with PEP 734.  There are only a few small things to wrap up.

--- PC/config.c
+++ PC/config.c
@@ -37,6 +37,7 @@ extern PyObject* PyInit__weakref(void);
 extern PyObject* PyInit_xxsubtype(void);
 extern PyObject* PyInit__xxsubinterpreters(void);
 extern PyObject* PyInit__xxinterpchannels(void);
+extern PyObject* PyInit__xxinterpqueues(void);
 extern PyObject* PyInit__random(void);
 extern PyObject* PyInit_itertools(void);
 extern PyObject* PyInit__collections(void);
@@ -142,6 +143,7 @@ struct _inittab _PyImport_Inittab[] = {
     {"xxsubtype", PyInit_xxsubtype},
     {"_xxsubinterpreters", PyInit__xxsubinterpreters},
     {"_xxinterpchannels", PyInit__xxinterpchannels},
+    {"_xxinterpqueues", PyInit__xxinterpqueues},
 #ifdef _Py_HAVE_ZLIB
     {"zlib", PyInit_zlib},
 #endif
