2a73912c9cbb579207ff6ca4eeff78c2fddd3d69
Daniel Hollas <daniel.hollas@bristol.ac.uk>
2023-12-14 04:28:27 +0000

    Speed up import of threading module
    Delayed import of functools leads to 50% speedup
    of import time.

--- Lib/threading.py
+++ Lib/threading.py
@@ -3,7 +3,6 @@
 import os as _os
 import sys as _sys
 import _thread
-import functools
 import warnings
 import _weakref
 
@@ -1630,6 +1629,7 @@ def _register_atexit(func, *arg, **kwargs):
     if _SHUTTING_DOWN:
         raise RuntimeError("can't register atexit after shutdown")
 
+    import functools
     call = functools.partial(func, *arg, **kwargs)
     _threading_atexits.append(call)
 
