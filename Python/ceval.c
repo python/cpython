40b42e1bba74db0f49b9c6b821981a32cc285a46
Serhiy Storchaka <storchaka@gmail.com>
2023-12-14 14:24:24 +0200

    [3.12] gh-112716: Fix SystemError when __builtins__ is not a dict (GH-112770)
    It was raised in two cases:
    * in the import statement when looking up __import__
    * in pickling some builtin type when looking up built-ins iter, getattr, etc.
    (cherry picked from commit 1161c14e8c68296fc465cd48970b32be9bee012e)
    
    Co-authored-by: Serhiy Storchaka <storchaka@gmail.com>

--- Python/ceval.c
+++ Python/ceval.c
@@ -2308,11 +2308,8 @@ PyObject *
 _PyEval_GetBuiltin(PyObject *name)
 {
     PyThreadState *tstate = _PyThreadState_GET();
-    PyObject *attr = PyDict_GetItemWithError(PyEval_GetBuiltins(), name);
-    if (attr) {
-        Py_INCREF(attr);
-    }
-    else if (!_PyErr_Occurred(tstate)) {
+    PyObject *attr = PyObject_GetItem(PyEval_GetBuiltins(), name);
+    if (attr == NULL && _PyErr_ExceptionMatches(tstate, PyExc_KeyError)) {
         _PyErr_SetObject(tstate, PyExc_AttributeError, name);
     }
     return attr;
@@ -2467,9 +2464,9 @@ import_name(PyThreadState *tstate, _PyInterpreterFrame *frame,
     PyObject *import_func, *res;
     PyObject* stack[5];
 
-    import_func = _PyDict_GetItemWithError(frame->f_builtins, &_Py_ID(__import__));
+    import_func = PyObject_GetItem(frame->f_builtins, &_Py_ID(__import__));
     if (import_func == NULL) {
-        if (!_PyErr_Occurred(tstate)) {
+        if (_PyErr_ExceptionMatches(tstate, PyExc_KeyError)) {
             _PyErr_SetString(tstate, PyExc_ImportError, "__import__ not found");
         }
         return NULL;
@@ -2477,6 +2474,7 @@ import_name(PyThreadState *tstate, _PyInterpreterFrame *frame,
     PyObject *locals = frame->f_locals;
     /* Fast path for not overloaded __import__. */
     if (_PyImport_IsDefaultImportFunc(tstate->interp, import_func)) {
+        Py_DECREF(import_func);
         int ilevel = _PyLong_AsInt(level);
         if (ilevel == -1 && _PyErr_Occurred(tstate)) {
             return NULL;
@@ -2489,8 +2487,6 @@ import_name(PyThreadState *tstate, _PyInterpreterFrame *frame,
                         ilevel);
         return res;
     }
-
-    Py_INCREF(import_func);
 
     stack[0] = name;
     stack[1] = frame->f_globals;
