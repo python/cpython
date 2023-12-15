bbdac949d918601a9f7707a02b3526ee6b922c6a
Mark Shannon <mark@hotpy.org>
2023-12-15 17:37:34 +0000

    Further reduce C recursion limit for s390

--- Include/cpython/pystate.h
+++ Include/cpython/pystate.h
@@ -224,7 +224,7 @@ struct _ts {
    // recursions, sometimes less. 500 is a more conservative limit.
 #  define Py_C_RECURSION_LIMIT 500
 #elif defined(__s390x__)
-#  define Py_C_RECURSION_LIMIT 1500
+#  define Py_C_RECURSION_LIMIT 1200
 #else
    // This value is duplicated in Lib/test/support/__init__.py
 #  define Py_C_RECURSION_LIMIT 8000
