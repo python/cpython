eba985667c591f2ae27f13ece33c7906f93ebde8
Brandt Bucher <brandt@python.org>
2023-12-15 08:32:13 -0800

    Don't skip, just fix

--- Lib/test/test_dis.py
+++ Lib/test/test_dis.py
@@ -1208,8 +1208,14 @@ class DisTests(DisTestBase):
         self.code_quicken(loop_test, 1)
         got = self.get_disassembly(loop_test, adaptive=True)
         expected = dis_loop_test_quickened_code
-        if _testinternalcapi.get_optimizer() and "ENTER_EXECUTOR" in got:
-            raise unittest.SkipTest("ENTER_EXECUTOR")
+        if _testinternalcapi.get_optimizer():
+             # We *may* see ENTER_EXECUTOR in the disassembly. This is a
+             # temporary hack to keep the test working until dis is able to
+             # handle the instruction correctly (GH-112383):
+             got = got.replace(
+                 "ENTER_EXECUTOR          16",
+                 "JUMP_BACKWARD           16 (to L1)",
+            )
         self.do_disassembly_compare(got, expected)
 
     @cpython_only
