cca33e8207c244ceb6d7c68edf57587810bb1d81
barneygale <barney.gale@gmail.com>
2023-12-18 05:35:16 +0000

    GH-112855: Slightly improve tests for `pathlib.PurePath` pickling
    Add a few more simple test cases, like non-anchored paths. Remove misplaced
    and indirect test that pickling doesn't change the `stat()` value.

--- Lib/test/test_pathlib/test_pathlib_abc.py
+++ Lib/test/test_pathlib/test_pathlib_abc.py
@@ -1655,13 +1655,6 @@ class DummyPathTest(DummyPurePathTest):
         self.assertIs((P / 'fileA\udfff').is_char_device(), False)
         self.assertIs((P / 'fileA\x00').is_char_device(), False)
 
-    def test_pickling_common(self):
-        p = self.cls(self.base, 'fileA')
-        for proto in range(0, pickle.HIGHEST_PROTOCOL + 1):
-            dumped = pickle.dumps(p, proto)
-            pp = pickle.loads(dumped)
-            self.assertEqual(pp.stat(), p.stat())
-
     def test_parts_interning(self):
         P = self.cls
         p = P('/usr/bin/foo')
