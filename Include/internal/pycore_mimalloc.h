2501f7e2495d747180e8eaff684e4018ef1588a3
Sam Gross <colesbury@gmail.com>
2023-12-20 20:28:51 +0000

    Use enum instead of defines and guard mimalloc includes.
    * The enum typedef will be convenient for future PRs that use the type.
    * Guarding the mimalloc includes allows us to unconditionally include
      pycore_mimalloc.h from other header files that rely on things like
      `struct _mimalloc_thread_state`.

--- Include/internal/pycore_mimalloc.h
+++ Include/internal/pycore_mimalloc.h
@@ -9,13 +9,17 @@
 #  error "pycore_mimalloc.h must be included before mimalloc.h"
 #endif
 
-#define _Py_MIMALLOC_HEAP_MEM       0   // PyMem_Malloc() and friends
-#define _Py_MIMALLOC_HEAP_OBJECT    1   // non-GC objects
-#define _Py_MIMALLOC_HEAP_GC        2   // GC objects without pre-header
-#define _Py_MIMALLOC_HEAP_GC_PRE    3   // GC objects with pre-header
-#define _Py_MIMALLOC_HEAP_COUNT     4
+typedef enum {
+    _Py_MIMALLOC_HEAP_MEM = 0,      // PyMem_Malloc() and friends
+    _Py_MIMALLOC_HEAP_OBJECT = 1,   // non-GC objects
+    _Py_MIMALLOC_HEAP_GC = 2,       // GC objects without pre-header
+    _Py_MIMALLOC_HEAP_GC_PRE = 3,   // GC objects with pre-header
+    _Py_MIMALLOC_HEAP_COUNT
+} _Py_mimalloc_heap_id;
 
 #include "pycore_pymem.h"
+
+#ifdef WITH_MIMALLOC
 #define MI_DEBUG_UNINIT     PYMEM_CLEANBYTE
 #define MI_DEBUG_FREED      PYMEM_DEADBYTE
 #define MI_DEBUG_PADDING    PYMEM_FORBIDDENBYTE
@@ -28,6 +32,7 @@
 #include "mimalloc.h"
 #include "mimalloc/types.h"
 #include "mimalloc/internal.h"
+#endif
 
 struct _mimalloc_thread_state {
 #ifdef Py_GIL_DISABLED
