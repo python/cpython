1b423c1e2490643798b2b1ab362dcb0f3b351382
AlexWaygood <alex.waygood@gmail.com>
2023-12-23 16:43:44 +0000

    `black Tools/clinic/libclinic/cli.py --line-length 90`

--- Tools/clinic/libclinic/cli.py
+++ Tools/clinic/libclinic/cli.py
@@ -18,44 +18,69 @@ The purpose of the Argument Clinic is automating all the boilerplate involved
 with writing argument parsing code for builtins and providing introspection
 signatures ("docstrings") for CPython builtins.
 
-For more information see https://devguide.python.org/development-tools/clinic/""")
-    cmdline.add_argument("-f", "--force", action='store_true',
-                         help="force output regeneration")
-    cmdline.add_argument("-o", "--output", type=str,
-                         help="redirect file output to OUTPUT")
-    cmdline.add_argument("-v", "--verbose", action='store_true',
-                         help="enable verbose mode")
-    cmdline.add_argument("--converters", action='store_true',
-                         help=("print a list of all supported converters "
-                               "and return converters"))
-    cmdline.add_argument("--make", action='store_true',
-                         help="walk --srcdir to run over all relevant files")
-    cmdline.add_argument("--srcdir", type=str, default=os.curdir,
-                         help="the directory tree to walk in --make mode")
-    cmdline.add_argument("--exclude", type=str, action="append",
-                         help=("a file to exclude in --make mode; "
-                               "can be given multiple times"))
-    cmdline.add_argument("--limited", dest="limited_capi", action='store_true',
-                         help="use the Limited C API")
-    cmdline.add_argument("filename", metavar="FILE", type=str, nargs="*",
-                         help="the list of files to process")
+For more information see https://devguide.python.org/development-tools/clinic/""",
+    )
+    cmdline.add_argument(
+        "-f", "--force", action="store_true", help="force output regeneration"
+    )
+    cmdline.add_argument(
+        "-o", "--output", type=str, help="redirect file output to OUTPUT"
+    )
+    cmdline.add_argument(
+        "-v", "--verbose", action="store_true", help="enable verbose mode"
+    )
+    cmdline.add_argument(
+        "--converters",
+        action="store_true",
+        help=("print a list of all supported converters " "and return converters"),
+    )
+    cmdline.add_argument(
+        "--make",
+        action="store_true",
+        help="walk --srcdir to run over all relevant files",
+    )
+    cmdline.add_argument(
+        "--srcdir",
+        type=str,
+        default=os.curdir,
+        help="the directory tree to walk in --make mode",
+    )
+    cmdline.add_argument(
+        "--exclude",
+        type=str,
+        action="append",
+        help=("a file to exclude in --make mode; " "can be given multiple times"),
+    )
+    cmdline.add_argument(
+        "--limited",
+        dest="limited_capi",
+        action="store_true",
+        help="use the Limited C API",
+    )
+    cmdline.add_argument(
+        "filename",
+        metavar="FILE",
+        type=str,
+        nargs="*",
+        help="the list of files to process",
+    )
     return cmdline
 
 
 def run_clinic(parser: argparse.ArgumentParser, ns: argparse.Namespace) -> None:
     if ns.converters:
         if ns.filename:
-            parser.error(
-                "can't specify --converters and a filename at the same time"
-            )
+            parser.error("can't specify --converters and a filename at the same time")
         converters: list[tuple[str, str]] = []
         return_converters: list[tuple[str, str]] = []
-        ignored = set("""
+        ignored = set(
+            """
             add_c_converter
             add_c_return_converter
             add_default_legacy_c_converter
             add_legacy_c_converter
-            """.strip().split())
+            """.strip().split()
+        )
 
         module = vars(clinic)
         for name in module:
@@ -72,13 +97,13 @@ def run_clinic(parser: argparse.ArgumentParser, ns: argparse.Namespace) -> None:
 
         print("Legacy converters:")
         legacy = sorted(clinic.legacy_converters)
-        print('    ' + ' '.join(c for c in legacy if c[0].isupper()))
-        print('    ' + ' '.join(c for c in legacy if c[0].islower()))
+        print("    " + " ".join(c for c in legacy if c[0].isupper()))
+        print("    " + " ".join(c for c in legacy if c[0].islower()))
         print()
 
         for title, attribute, ids in (
-            ("Converters", 'converter_init', converters),
-            ("Return converters", 'return_converter_init', return_converters),
+            ("Converters", "converter_init", converters),
+            ("Return converters", "return_converter_init", return_converters),
         ):
             print(title + ":")
             longest = -1
@@ -94,13 +119,15 @@ def run_clinic(parser: argparse.ArgumentParser, ns: argparse.Namespace) -> None:
                 for parameter_name, parameter in signature.parameters.items():
                     if parameter.kind == inspect.Parameter.KEYWORD_ONLY:
                         if parameter.default != inspect.Parameter.empty:
-                            s = f'{parameter_name}={parameter.default!r}'
+                            s = f"{parameter_name}={parameter.default!r}"
                         else:
                             s = parameter_name
                         parameters.append(s)
-                print('    {}({})'.format(short_name, ', '.join(parameters)))
+                print("    {}({})".format(short_name, ", ".join(parameters)))
             print()
-        print("All converters also accept (c_default=None, py_default=None, annotation=None).")
+        print(
+            "All converters also accept (c_default=None, py_default=None, annotation=None)."
+        )
         print("All return converters also accept (py_default=None).")
         return
 
@@ -115,12 +142,12 @@ def run_clinic(parser: argparse.ArgumentParser, ns: argparse.Namespace) -> None:
         else:
             excludes = []
         for root, dirs, files in os.walk(ns.srcdir):
-            for rcs_dir in ('.svn', '.git', '.hg', 'build', 'externals'):
+            for rcs_dir in (".svn", ".git", ".hg", "build", "externals"):
                 if rcs_dir in dirs:
                     dirs.remove(rcs_dir)
             for filename in files:
                 # handle .c, .cpp and .h files
-                if not filename.endswith(('.c', '.cpp', '.h')):
+                if not filename.endswith((".c", ".cpp", ".h")):
                     continue
                 path = os.path.join(root, filename)
                 path = os.path.normpath(path)
@@ -128,8 +155,7 @@ def run_clinic(parser: argparse.ArgumentParser, ns: argparse.Namespace) -> None:
                     continue
                 if ns.verbose:
                     print(path)
-                clinic.parse_file(path,
-                                  verify=not ns.force, limited_capi=ns.limited_capi)
+                clinic.parse_file(path, verify=not ns.force, limited_capi=ns.limited_capi)
         return
 
     if not ns.filename:
@@ -141,8 +167,12 @@ def run_clinic(parser: argparse.ArgumentParser, ns: argparse.Namespace) -> None:
     for filename in ns.filename:
         if ns.verbose:
             print(filename)
-        clinic.parse_file(filename, output=ns.output,
-                          verify=not ns.force, limited_capi=ns.limited_capi)
+        clinic.parse_file(
+            filename,
+            output=ns.output,
+            verify=not ns.force,
+            limited_capi=ns.limited_capi,
+        )
 
 
 def main(argv: list[str] | None = None) -> NoReturn:
