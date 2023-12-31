908ef135a8dea022be98192886b4a7fd8ff5c8aa
Donghee Na <donghee.na@python.org>
2023-12-31 14:39:36 +0900

    pep7

--- Include/internal/pycore_pystate.h
+++ Include/internal/pycore_pystate.h
@@ -240,7 +240,8 @@ PyAPI_FUNC(const PyConfig*) _Py_GetConfig(void);
 // See also PyInterpreterState_Get() and _PyInterpreterState_GET().
 extern PyInterpreterState* _PyGILState_GetInterpreterStateUnsafe(void);
 
-static inline PyFreeListState* _PyFreeListState_GET(void) {
+static inline PyFreeListState* _PyFreeListState_GET(void)
+{
     PyThreadState *tstate = _PyThreadState_GET();
 #ifdef Py_DEBUG
     _Py_EnsureTstateNotNULL(tstate);
