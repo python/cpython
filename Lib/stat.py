b68cf311e0f96097c049f957d1ba080ea4ea026b
Ronald Oussoren <ronaldoussoren@mac.com>
2024-01-04 17:41:51 +0100

    Fix typo

--- Lib/stat.py
+++ Lib/stat.py
@@ -117,7 +117,7 @@ if sys.platform == "darwin":
 
     UF_SETTABLE  = 0x0000ffff # owner settable flags
     SF_SUPPORTED = 0x009f0000 # superuser supported flags
-    SF_SETTABLE  = 0x3ffff000 # superuser settable flags
+    SF_SETTABLE  = 0x3fff0000 # superuser settable flags
     SF_SYNTHETIC = 0xc0000000 # system read-only synthetic flags
 
 UF_NODUMP    = 0x00000001  # do not dump file
