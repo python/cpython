d6a2feb422c5825c79b64706fe8fb0a582ff7566
Donghee Na <donghee.na@python.org>
2024-01-06 08:28:16 +0900

    Add files

---
+++ Python/gc_free_threading.c
@@ -0,0 +1,31 @@
+#ifdef Py_GIL_DISABLED
+
+#include "Python.h"
+#include "pycore_pystate.h"   // _PyFreeListState_GET()
+#include "pycore_tstate.h"    // _PyThreadStateImpl
+
+/* Clear all free lists
+ * All free lists are cleared during the collection of the highest generation.
+ * Allocated items in the free list may keep a pymalloc arena occupied.
+ * Clearing the free lists may give back memory to the OS earlier.
+ */
+void
+_PyGC_Clear_FreeList(PyInterpreterState *interp)
+{
+    _PyTuple_ClearFreeList(interp);
+    _PyFloat_ClearFreeList(interp);
+    _PyList_ClearFreeList(interp);
+    _PyDict_ClearFreeList(interp);
+    _PyAsyncGen_ClearFreeLists(interp);
+    _PyContext_ClearFreeList(interp);
+    HEAD_LOCK(&_PyRuntime);
+    _PyThreadStateImpl *tstate = (_PyThreadStateImpl *)interp->threads.head;
+    while (tstate != NULL) {
+        _PyList_ClearFreeList(&tstate->freelist_state);
+        tstate = tstate->base.next;
+    }
+    HEAD_UNLOCK(&_PyRuntime);
+}
+
+
+#endif
---
+++ Python/gc_gil.c
@@ -0,0 +1,24 @@
+#ifndef Py_GIL_DISABLED
+
+#include "Python.h"
+#include "pycore_pystate.h"   // _PyFreeListState_GET()
+
+/* Clear all free lists
+ * All free lists are cleared during the collection of the highest generation.
+ * Allocated items in the free list may keep a pymalloc arena occupied.
+ * Clearing the free lists may give back memory to the OS earlier.
+ */
+void
+_PyGC_Clear_FreeList(PyInterpreterState *interp)
+{
+    _PyTuple_ClearFreeList(interp);
+    _PyFloat_ClearFreeList(interp);
+    _PyList_ClearFreeList(interp);
+    _PyDict_ClearFreeList(interp);
+    _PyAsyncGen_ClearFreeLists(interp);
+    _PyContext_ClearFreeList(interp);
+
+    _PyFreeListState* state = _PyFreeListState_GET();
+    _PyList_ClearFreeList(state);
+}
+#endif
