ee6cc9979169c59d65cdd1a537e45c775bd7463f
barneygale <barney.gale@gmail.com>
2024-01-08 01:16:46 +0000

    Tweak `parents` expected results slightly

--- Lib/test/test_pathlib/test_pathlib_abc.py
+++ Lib/test/test_pathlib/test_pathlib_abc.py
@@ -264,17 +264,17 @@ class DummyPurePathTest(unittest.TestCase):
         self.assertEqual(len(par), 3)
         self.assertEqual(par[0], P('a/b'))
         self.assertEqual(par[1], P('a'))
-        self.assertEqual(par[2], P())
-        self.assertEqual(par[-1], P())
+        self.assertEqual(par[2], P(''))
+        self.assertEqual(par[-1], P(''))
         self.assertEqual(par[-2], P('a'))
         self.assertEqual(par[-3], P('a/b'))
         self.assertEqual(par[0:1], (P('a/b'),))
         self.assertEqual(par[:2], (P('a/b'), P('a')))
         self.assertEqual(par[:-1], (P('a/b'), P('a')))
-        self.assertEqual(par[1:], (P('a'), P()))
-        self.assertEqual(par[::2], (P('a/b'), P()))
-        self.assertEqual(par[::-1], (P(), P('a'), P('a/b')))
-        self.assertEqual(list(par), [P('a/b'), P('a'), P()])
+        self.assertEqual(par[1:], (P('a'), P('')))
+        self.assertEqual(par[::2], (P('a/b'), P('')))
+        self.assertEqual(par[::-1], (P(''), P('a'), P('a/b')))
+        self.assertEqual(list(par), [P('a/b'), P('a'), P('')])
         with self.assertRaises(IndexError):
             par[-4]
         with self.assertRaises(IndexError):
