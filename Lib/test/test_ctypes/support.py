6a0676c273c486e5a43f5f836c67d2f40d66ed25
AN Long <aisk@users.noreply.github.com>
2024-01-09 20:39:38 +0800

    move CField to support.py

--- Lib/test/test_ctypes/support.py
+++ Lib/test/test_ctypes/support.py
@@ -1,10 +1,15 @@
 # Some classes and types are not export to _ctypes module directly.
 
+import ctypes
 from _ctypes import Structure, Union, _Pointer, Array, _SimpleCData, CFuncPtr
 
 
 _CData = Structure.__base__
 assert _CData.__name__ == "_CData"
+
+class _X(Structure):
+    _fields_ = [("x", ctypes.c_int)]
+CField = type(_X.x)
 
 # metaclasses
 PyCStructType = type(Structure)
