cdd703d1313b546823882663ca94cf536f025f4a
Miss Islington (bot) <31488909+miss-islington@users.noreply.github.com>
2024-01-09 09:13:41 +0100

    [3.12] gh-113692: skip a test if multiprocessing isn't available. (GH-113704) (GH-113844)
    (cherry picked from commit 842b738129021f52293dc053e014ecb4fe095baa)

--- Lib/test/test_logging.py
+++ Lib/test/test_logging.py
@@ -43,6 +43,7 @@ import sys
 import tempfile
 from test.support.script_helper import assert_python_ok, assert_python_failure
 from test import support
+from test.support import import_helper
 from test.support import os_helper
 from test.support import socket_helper
 from test.support import threading_helper
@@ -3894,7 +3895,8 @@ class ConfigDictTest(BaseTest):
 
     def test_111615(self):
         # See gh-111615
-        import multiprocessing as mp
+        import_helper.import_module('_multiprocessing')  # see gh-113692
+        mp = import_helper.import_module('multiprocessing')
 
         config = {
             'version': 1,
