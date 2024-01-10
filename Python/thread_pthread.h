21dbbaedc38d82876fa95a2aef8b459287c9d6bb
Sergey Fedorov <vital.had@gmail.com>
2024-01-10 09:02:25 +0800

    thread_pthread.h: provide fallback if pthread_threadid_np is unavailable on older macOS SDK
    Fixes: https://github.com/python/cpython/issues/113889

--- Python/thread_pthread.h
+++ Python/thread_pthread.h
@@ -32,6 +32,10 @@
 #   include <sys/lwp.h>         /* lwp_gettid() */
 #endif
 
+#if defined(__APPLE__)
+#include <AvailabilityMacros.h>
+#endif
+
 /* The POSIX spec requires that use of pthread_attr_setstacksize
    be conditional on _POSIX_THREAD_ATTR_STACKSIZE being defined. */
 #ifdef _POSIX_THREAD_ATTR_STACKSIZE
@@ -379,7 +383,17 @@ PyThread_get_thread_native_id(void)
         PyThread_init_thread();
 #ifdef __APPLE__
     uint64_t native_id;
+#if MAC_OS_X_VERSION_MAX_ALLOWED < 1060 || defined(__POWERPC__)
+    native_id = pthread_mach_thread_np(pthread_self());
+#elif MAC_OS_X_VERSION_MIN_REQUIRED < 1060
+    if (&pthread_threadid_np != NULL) {
+        (void) pthread_threadid_np(NULL, &native_id);
+    } else {
+        native_id = pthread_mach_thread_np(pthread_self());
+    }
+#else
     (void) pthread_threadid_np(NULL, &native_id);
+#endif
 #elif defined(__linux__)
     pid_t native_id;
     native_id = syscall(SYS_gettid);
