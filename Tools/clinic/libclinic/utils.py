3c708fd566e6508d7c82bdfeead0ac372f7d8f22
Erlend E. Aasland <erlend@python.org>
2024-01-12 15:36:55 +0100

    gh-113317: Move global utility functions into libclinic
    Establish Tools/clinic/libclinic/utils.py and move the following
    functions over there:
    
    - compute_checksum()
    - create_regex()
    - write_file()

---
+++ Tools/clinic/libclinic/utils.py
@@ -0,0 +1,44 @@
+import hashlib
+import re
+import os
+
+
+def write_file(filename: str, new_contents: str) -> None:
+    """Write new content to file, iff the content changed."""
+    try:
+        with open(filename, encoding="utf-8") as fp:
+            old_contents = fp.read()
+
+        if old_contents == new_contents:
+            # no change: avoid modifying the file modification time
+            return
+    except FileNotFoundError:
+        pass
+    # Atomic write using a temporary file and os.replace()
+    filename_new = f"{filename}.new"
+    with open(filename_new, "w", encoding="utf-8") as fp:
+        fp.write(new_contents)
+    try:
+        os.replace(filename_new, filename)
+    except:
+        os.unlink(filename_new)
+        raise
+
+
+def compute_checksum(input_: str, length: int | None = None) -> str:
+    checksum = hashlib.sha1(input_.encode("utf-8")).hexdigest()
+    if length:
+        checksum = checksum[:length]
+    return checksum
+
+
+def create_regex(
+    before: str, after: str, word: bool = True, whole_line: bool = True
+) -> re.Pattern[str]:
+    """Create a regex object for matching marker lines."""
+    group_re = r"\w+" if word else ".+"
+    pattern = r"{}({}){}"
+    if whole_line:
+        pattern = "^" + pattern + "$"
+    pattern = pattern.format(re.escape(before), group_re, re.escape(after))
+    return re.compile(pattern)
