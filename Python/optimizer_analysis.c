42374f448adf9f7520a90e5bf629c0fb668b323b
Mark Shannon <mark@hotpy.org>
2024-01-17 23:40:23 +0000

    Perform a couple of peephole optimizations for tier 2.

--- Python/optimizer_analysis.c
+++ Python/optimizer_analysis.c
@@ -13,7 +13,7 @@
 #include "pycore_optimizer.h"
 
 static void
-inline_consts(PyCodeObject *co, _PyUOpInstruction *buffer, int buffer_size)
+peephole_opt(PyCodeObject *co, _PyUOpInstruction *buffer, int buffer_size)
 {
 
     for (int pc = 0; pc < buffer_size; pc++) {
@@ -24,6 +24,15 @@ inline_consts(PyCodeObject *co, _PyUOpInstruction *buffer, int buffer_size)
                 PyObject *val = PyTuple_GET_ITEM(co->co_consts, buffer[pc].oparg);
                 buffer[pc].opcode = _Py_IsImmortal(val) ? _LOAD_CONST_INLINE_BORROW : _LOAD_CONST_INLINE;
                 buffer[pc].operand = (uintptr_t)val;
+                break;
+            }
+            case _CHECK_PEP_523:
+            {
+                /* Setting the eval frame function invalidates
+                 * all executors, so no need to check dynamically */
+                if (_PyInterpreterState_GET()->eval_frame == NULL) {
+                    buffer[pc].opcode = _NOP;
+                }
                 break;
             }
             case _PUSH_FRAME:
@@ -84,7 +93,7 @@ _Py_uop_analyze_and_optimize(
     int curr_stacklen
 )
 {
-    inline_consts(co, buffer, buffer_size);
+    peephole_opt(co, buffer, buffer_size);
     remove_unneeded_uops(buffer, buffer_size);
     return 0;
 }
