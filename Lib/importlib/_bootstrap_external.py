77314a956788a51558034e7c1316a457a6ed57e2
Jakub Stasiak <jakub@stasiak.at>
2024-01-17 07:52:35 +0100

    Update Lib/importlib/_bootstrap_external.py
    Co-authored-by: Adam Turner <9087854+AA-Turner@users.noreply.github.com>

--- Lib/importlib/_bootstrap_external.py
+++ Lib/importlib/_bootstrap_external.py
@@ -1271,7 +1271,7 @@ def _exclude_directory_from_backups(directory):
         with _io.FileIO(_path_join(directory, 'CACHEDIR.TAG'), 'x') as f:
             f.write(b"""Signature: 8a477f597d28d172789f06886806bc55
 # This file is a cache directory tag created by CPython.
-# For information about cache directory tags see https://bford.info/cachedir/
+# For information about cache directory tags, see: https://bford.info/cachedir/
 """)
     except (FileExistsError, PermissionError):
         pass
