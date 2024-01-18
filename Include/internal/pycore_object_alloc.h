b6c3506584dcc15d39fd95895cbb2154ab52b38a
Sam Gross <colesbury@gmail.com>
2024-01-18 16:51:01 +0000

    Refactor out setting heap based on type

--- Include/internal/pycore_object_alloc.h
+++ Include/internal/pycore_object_alloc.h
@@ -13,6 +13,23 @@ extern "C" {
 #  error "this header requires Py_BUILD_CORE define"
 #endif
 
+#ifdef Py_GIL_DISABLED
+static inline mi_heap_t *
+_PyObject_GetAllocationHeap(_PyThreadStateImpl *tstate, PyTypeObject *tp)
+{
+    struct _mimalloc_thread_state *m = &tstate->mimalloc;
+    if (_PyType_HasFeature(tp, Py_TPFLAGS_PREHEADER)) {
+        return &m->heaps[_Py_MIMALLOC_HEAP_GC_PRE];
+    }
+    else if (_PyType_IS_GC(tp)) {
+        return &m->heaps[_Py_MIMALLOC_HEAP_GC];
+    }
+    else {
+        return &m->heaps[_Py_MIMALLOC_HEAP_OBJECT];
+    }
+}
+#endif
+
 // Sets the heap used for PyObject_Malloc(), PyObject_Realloc(), etc. calls in
 // Py_GIL_DISABLED builds. We use different heaps depending on if the object
 // supports GC and if it has a pre-header. We smuggle the choice of heap
@@ -24,12 +41,7 @@ _PyObject_MallocWithType(PyTypeObject *tp, size_t size)
 #ifdef Py_GIL_DISABLED
     _PyThreadStateImpl *tstate = (_PyThreadStateImpl *)_PyThreadState_GET();
     struct _mimalloc_thread_state *m = &tstate->mimalloc;
-    if (_PyType_HasFeature(tp, Py_TPFLAGS_PREHEADER)) {
-        m->current_object_heap = &m->heaps[_Py_MIMALLOC_HEAP_GC_PRE];
-    }
-    else if (_PyType_IS_GC(tp)) {
-        m->current_object_heap = &m->heaps[_Py_MIMALLOC_HEAP_GC];
-    }
+    m->current_object_heap = _PyObject_GetAllocationHeap(tstate, tp);
 #endif
     void *mem = PyObject_Malloc(size);
 #ifdef Py_GIL_DISABLED
@@ -44,12 +56,7 @@ _PyObject_ReallocWithType(PyTypeObject *tp, void *ptr, size_t size)
 #ifdef Py_GIL_DISABLED
     _PyThreadStateImpl *tstate = (_PyThreadStateImpl *)_PyThreadState_GET();
     struct _mimalloc_thread_state *m = &tstate->mimalloc;
-    if (_PyType_HasFeature(tp, Py_TPFLAGS_PREHEADER)) {
-        m->current_object_heap = &m->heaps[_Py_MIMALLOC_HEAP_GC_PRE];
-    }
-    else if (_PyType_IS_GC(tp)) {
-        m->current_object_heap = &m->heaps[_Py_MIMALLOC_HEAP_GC];
-    }
+    m->current_object_heap = _PyObject_GetAllocationHeap(tstate, tp);
 #endif
     void *mem = PyObject_Realloc(ptr, size);
 #ifdef Py_GIL_DISABLED
