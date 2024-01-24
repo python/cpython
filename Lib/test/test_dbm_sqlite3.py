54751ba95c683b0d5fc57eeaa7a5fe98e1f740f3
Erlend E. Aasland <erlend@python.org>
2024-01-24 09:44:49 +0100

    test namespacing

--- Lib/test/test_dbm_sqlite3.py
+++ Lib/test/test_dbm_sqlite3.py
@@ -34,22 +34,22 @@ class ReadOnly(_SQLiteDbmTests):
         self.db.close()
         super().tearDown()
 
-    def test_read(self):
+    def test_readonly_read(self):
         self.assertEqual(self.db["key1"], "value1")
         self.assertEqual(self.db["key2"], "value2")
 
-    def test_write(self):
+    def test_readonly_write(self):
         with self.assertRaises(dbm_sqlite3.error):
             self.db["new"] = "value"
 
-    def test_delete(self):
+    def test_readonly_delete(self):
         with self.assertRaises(dbm_sqlite3.error):
             del self.db["key1"]
 
-    def test_keys(self):
+    def test_readonly_keys(self):
         self.assertEqual(self.db.keys(), ["key1", "key2"])
 
-    def test_iter(self):
+    def test_readonly_iter(self):
         self.assertEqual([k for k in self.db], ["key1", "key2"])
 
 
@@ -63,20 +63,20 @@ class Misuse(_SQLiteDbmTests):
         self.db.close()
         super().tearDown()
 
-    def test_double_close(self):
+    def test_misuse_double_close(self):
         self.db.close()
 
-    def test_invalid_flag(self):
+    def test_misuse_invalid_flag(self):
         with self.assertRaises(ValueError):
             dbm_sqlite3.open(self.filename, flag="invalid")
 
-    def test_double_delete(self):
+    def test_misuse_double_delete(self):
         self.db["key"] = "value"
         del self.db["key"]
         with self.assertRaises(KeyError):
             del self.db["key"]
 
-    def test_invalid_key(self):
+    def test_misuse_invalid_key(self):
         with self.assertRaises(KeyError):
             self.db["key"]
 
@@ -93,13 +93,13 @@ class DataTypes(_SQLiteDbmTests):
         self.db.close()
         super().tearDown()
 
-    def test_values(self):
+    def test_datatypes_values(self):
         for value in self.dataset:
             with self.subTest(value=value):
                 self.db["key"] = value
                 self.assertEqual(self.db["key"], value)
 
-    def test_keys(self):
+    def test_datatypes_keys(self):
         for key in self.dataset:
             with self.subTest(key=key):
                 self.db[key] = "value"
@@ -145,7 +145,7 @@ class CorruptDatabase(_SQLiteDbmTests):
     def len_(db):
         len(db)
 
-    def test_readwrite(self):
+    def test_corrupt_readwrite(self):
         for flag in "r", "w", "c":
             with self.subTest(flag=flag):
                 check = partial(self.check, flag=flag)
@@ -156,7 +156,7 @@ class CorruptDatabase(_SQLiteDbmTests):
                 check(fn=self.del_)
                 check(fn=self.len_)
 
-    def test_new(self):
+    def test_corrupt_force_new(self):
         with closing(dbm_sqlite3.open(self.filename, "n")) as db:
             db["foo"] = "write"
             _ = db["foo"]
