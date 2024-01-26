2872cfa0cf1012079a850bb23ea804eb046060c7
NewUserHa <32261870+NewUserHa@users.noreply.github.com>
2024-01-27 03:31:54 +0800

    Update process.py

--- Lib/concurrent/futures/process.py
+++ Lib/concurrent/futures/process.py
@@ -190,12 +190,6 @@ class _SafeQueue(Queue):
             super()._on_queue_feeder_error(e, obj)
 
 
-def _get_chunks(*iterables, chunksize):
-    """ Iterates over zip()ed iterables in chunks. """
-    it = zip(*iterables)
-    return itertools.batched(it, chunksize)
-
-
 def _process_chunk(fn, chunk):
     """ Processes a chunk of an iterable passed to map.
 
@@ -843,7 +837,7 @@ class ProcessPoolExecutor(_base.Executor):
             raise ValueError("chunksize must be >= 1.")
 
         results = super().map(partial(_process_chunk, fn),
-                              _get_chunks(*iterables, chunksize=chunksize),
+                              itertools.batched(zip(*iterables), chunksize),
                               timeout=timeout)
         return _chain_from_iterable_of_lists(results)
 
