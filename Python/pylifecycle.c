0a2cf0f30b2482dce5110507fdf8bf136a78a986
Neil Schemenauer <nas-github@arctrix.com>
2024-01-26 19:38:14 -0800

    gh-113055: Use pointer for interp->obmalloc state (gh-113412)
    For interpreters that share state with the main interpreter, this points
    to the same static memory structure.  For interpreters with their own
    obmalloc state, it is heap allocated.  Add free_obmalloc_arenas() which
    will free the obmalloc arenas and radix tree structures for interpreters
    with their own obmalloc state.
    
    Co-authored-by: Eric Snow <ericsnowcurrently@gmail.com>

--- Python/pylifecycle.c
+++ Python/pylifecycle.c
@@ -32,6 +32,7 @@
 #include "pycore_typevarobject.h" // _Py_clear_generic_types()
 #include "pycore_unicodeobject.h" // _PyUnicode_InitTypes()
 #include "pycore_weakref.h"       // _PyWeakref_GET_REF()
+#include "pycore_obmalloc.h"      // _PyMem_init_obmalloc()
 
 #include "opcode.h"
 
@@ -643,6 +644,13 @@ pycore_create_interpreter(_PyRuntimeState *runtime,
     status = init_interp_settings(interp, &config);
     if (_PyStatus_EXCEPTION(status)) {
         return status;
+    }
+
+    // initialize the interp->obmalloc state.  This must be done after
+    // the settings are loaded (so that feature_flags are set) but before
+    // any calls are made to obmalloc functions.
+    if (_PyMem_init_obmalloc(interp) < 0) {
+        return  _PyStatus_NO_MEMORY();
     }
 
     PyThreadState *tstate = _PyThreadState_New(interp,
@@ -2141,6 +2149,14 @@ new_interpreter(PyThreadState **tstate_p, const PyInterpreterConfig *config)
     /* This does not require that the GIL be held. */
     status = init_interp_settings(interp, config);
     if (_PyStatus_EXCEPTION(status)) {
+        goto error;
+    }
+
+    // initialize the interp->obmalloc state.  This must be done after
+    // the settings are loaded (so that feature_flags are set) but before
+    // any calls are made to obmalloc functions.
+    if (_PyMem_init_obmalloc(interp) < 0) {
+        status = _PyStatus_NO_MEMORY();
         goto error;
     }
 
