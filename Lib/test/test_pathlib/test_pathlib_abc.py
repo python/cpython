f293c1c9fab2b6e794bbeb62e53174f36c5d1f41
Barney Gale <barney.gale@gmail.com>
2024-01-27 19:59:51 +0000

    GH-79634: Speed up pathlib globbing by removing `joinpath()` call. (#114623)
    Remove `self.joinpath('')` call that should have been removed in 2f682854.
    
    This makes `PathBase.glob('')` yield itself *without* adding a trailing slash. It's hard to say whether this is more or less correct, but at least everything else is faster, and there's no behaviour change in the public classes where empty glob patterns are disallowed.

--- Lib/test/test_pathlib/test_pathlib_abc.py
+++ Lib/test/test_pathlib/test_pathlib_abc.py
@@ -1733,12 +1733,11 @@ class DummyPathTest(DummyPurePathTest):
         self.assertEqual(set(map(str, p.glob("F*a"))), {f"{p}\\fileA"})
 
     def test_glob_empty_pattern(self):
-        def _check(glob, expected):
-            self.assertEqual(set(glob), { P(self.base, q) for q in expected })
         P = self.cls
         p = P(self.base)
-        _check(p.glob(""), [""])
-        _check(p.glob("."), ["."])
+        self.assertEqual(list(p.glob("")), [p])
+        self.assertEqual(list(p.glob(".")), [p / "."])
+        self.assertEqual(list(p.glob("./")), [p / "./"])
 
     def test_glob_case_sensitive(self):
         P = self.cls
