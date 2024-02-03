5748b6465d8b0cffe8a912d434765934677d050f
Serhiy Storchaka <storchaka@gmail.com>
2024-02-03 18:18:46 +0200

    gh-114959: tarfile: do not ignore errors when extract a directory on top of a file (GH-114960)
    Also, add tests common to tarfile and zipfile.

--- Lib/tarfile.py
+++ Lib/tarfile.py
@@ -2456,7 +2456,8 @@ class TarFile(object):
                 # later in _extract_member().
                 os.mkdir(targetpath, 0o700)
         except FileExistsError:
-            pass
+            if not os.path.isdir(targetpath):
+                raise
 
     def makefile(self, tarinfo, targetpath):
         """Make a file called targetpath.
