b280e9d509a660f59f88536dc9cd971712caa1f0
Carl Meyer <carl@oddbird.net>
2024-02-09 08:21:37 -0800

    review comments on comments

--- Lib/typing.py
+++ Lib/typing.py
@@ -1136,13 +1136,12 @@ class _BaseGenericAlias(_Final, _root=True):
         if self.__origin__ not in bases:
             res.append(self.__origin__)
 
-        # Check if any base that occurs after us in the bases list is either
-        # itself a subclass of Generic, or something which will add a subclass
-        # of Generic via its __mro_entries__ (which includes another
-        # _BaseGenericAlias). If not, add Generic ourselves. The goal is to
-        # ensure that Generic (or a subclass) will appear exactly once in the
-        # final bases list. If we let it appear multiple times, we risk "can't
-        # form a consistent MRO" errors.
+        # Check if any base that occurs after us in `bases` is either itself a
+        # subclass of Generic, or something which will add a subclass of Generic
+        # to `__bases__` via its `__mro_entries__`. If not, add Generic
+        # ourselves. The goal is to ensure that Generic (or a subclass) will
+        # appear exactly once in the final bases tuple. If we let it appear
+        # multiple times, we risk "can't form a consistent MRO" errors.
         i = bases.index(self)
         for b in bases[i+1:]:
             if isinstance(b, _BaseGenericAlias):
