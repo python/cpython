dd821c26a169eaeba291a727baee63d7cac57caf
sobolevn <mail@sobolevn.me>
2024-02-12 17:08:21 +0300

    gh-87804: Fix the refleak in error handling of `_pystatvfs_fromstructstatfs`

--- Modules/posixmodule.c
+++ Modules/posixmodule.c
@@ -12916,14 +12916,15 @@ _pystatvfs_fromstructstatfs(PyObject *module, struct statfs st) {
 
     _Static_assert(sizeof(st.f_blocks) == sizeof(long long), "assuming large file");
 
-#define SET_ITEM(v, index, item)                   \
-    do {                                           \
-        if (item == NULL) {                        \
-            Py_DECREF(v);                          \
-            return NULL;                           \
-        }                                          \
-        PyStructSequence_SET_ITEM(v, index, item); \
-    } while (0)                                    \
+#define SET_ITEM(v, index, expr)                    \
+    do {                                            \
+        PyObject *obj = (expr);                     \
+        if (obj == NULL || PyErr_Occurred()) {      \
+            Py_DECREF(v);                           \
+            return NULL;                            \
+        }                                           \
+        PyStructSequence_SET_ITEM(v, (index), obj); \
+    } while (0)                                     \
 
     SET_ITEM(v, 0, PyLong_FromLong((long) st.f_iosize));
     SET_ITEM(v, 1, PyLong_FromLong((long) st.f_bsize));
