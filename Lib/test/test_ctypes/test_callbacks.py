4fd9d9d05149a1b757246db163beed4b5140cde3
Kirill Podoprigora <kirill.bast9@mail.ru>
2024-02-16 19:34:35 +0200

    gh-115567: Catch test output

--- Lib/test/test_ctypes/test_callbacks.py
+++ Lib/test/test_ctypes/test_callbacks.py
@@ -149,8 +149,9 @@ class Callbacks(unittest.TestCase):
             return c
         dll = cdll[_ctypes_test.__file__]
         # With no fix for i38748, the next line will raise OSError and cause the test to fail.
-        self.assertEqual(dll._test_i38748_runCallback(callback, 5, 10), 15)
-
+        with support.captured_stdout() as out:
+            self.assertEqual(dll._test_i38748_runCallback(callback, 5, 10), 15)
+            self.assertEqual(out.getvalue(), "a=5, b=10, c=15\n")
 
 if hasattr(ctypes, 'WINFUNCTYPE'):
     class StdcallCallbacks(Callbacks):
