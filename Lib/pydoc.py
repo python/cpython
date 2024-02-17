0e21eda4edda653a37c13710908a8b785d16d7a9
Kirill Podoprigora <kirill.bast9@mail.ru>
2024-02-17 15:47:51 +0300

    gh-107155: Fix help() for lambda function with return annotation (GH-107401)

--- Lib/pydoc.py
+++ Lib/pydoc.py
@@ -1144,7 +1144,8 @@ class HTMLDoc(Doc):
                 # XXX lambda's won't usually have func_annotations['return']
                 # since the syntax doesn't support but it is possible.
                 # So removing parentheses isn't truly safe.
-                argspec = argspec[1:-1] # remove parentheses
+                if not object.__annotations__:
+                    argspec = argspec[1:-1] # remove parentheses
         if not argspec:
             argspec = '(...)'
 
@@ -1586,7 +1587,8 @@ location listed above.
                 # XXX lambda's won't usually have func_annotations['return']
                 # since the syntax doesn't support but it is possible.
                 # So removing parentheses isn't truly safe.
-                argspec = argspec[1:-1] # remove parentheses
+                if not object.__annotations__:
+                    argspec = argspec[1:-1]
         if not argspec:
             argspec = '(...)'
         decl = asyncqualifier + title + argspec + note
