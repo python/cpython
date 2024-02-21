4d58a1d8fb27048c11bcbda3da1bebf78f979335
Miss Islington (bot) <31488909+miss-islington@users.noreply.github.com>
2024-02-21 16:22:09 +0100

    [3.8] gh-107077: Raise SSLCertVerificationError even if the error is set via SSL_ERROR_SYSCALL (GH-107586) (GH-107591)
    (cherry picked from commit 77e09192b5f1caf14cd5f92ccb53a4592e83e8bc)
    
    Co-authored-by: Pablo Galindo Salgado <Pablogsal@gmail.com>
    Co-authored-by: T. Wouters <thomas@python.org>
    Co-authored-by: Łukasz Langa <lukasz@langa.pl>
    Co-authored-by: Serhiy Storchaka <storchaka@gmail.com>

--- Modules/_ssl.c
+++ Modules/_ssl.c
@@ -819,6 +819,10 @@ PySSL_SetError(PySSLSocket *sslsock, int ret, const char *filename, int lineno)
                     errstr = "Some I/O error occurred";
                 }
             } else {
+                if (ERR_GET_LIB(e) == ERR_LIB_SSL &&
+                        ERR_GET_REASON(e) == SSL_R_CERTIFICATE_VERIFY_FAILED) {
+                    type = PySSLCertVerificationErrorObject;
+                }
                 p = PY_SSL_ERROR_SYSCALL;
             }
             break;
