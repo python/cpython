f7fa43a2b56453b20ca3880614bd3cbf72663c63
Liam Swayne <liamtswayne@gmail.com>
2024-02-22 11:41:56 -0500

    replace if block with match block

--- Lib/fnmatch.py
+++ Lib/fnmatch.py
@@ -89,67 +89,68 @@ def _translate(pat, STAR, QUESTION_MARK):
     while i < n:
         c = pat[i]
         i = i+1
-        if c == '*':
-            # compress consecutive `*` into one
-            if (not res) or res[-1] is not STAR:
-                add(STAR)
-        elif c == '?':
-            add(QUESTION_MARK)
-        elif c == '[':
-            j = i
-            if j < n and pat[j] == '!':
-                j = j+1
-            if j < n and pat[j] == ']':
-                j = j+1
-            while j < n and pat[j] != ']':
-                j = j+1
-            if j >= n:
-                add('\\[')
-            else:
-                stuff = pat[i:j]
-                if '-' not in stuff:
-                    stuff = stuff.replace('\\', r'\\')
+        match c:
+            case '*':
+                # compress consecutive `*` into one
+                if (not res) or res[-1] is not STAR:
+                    add(STAR)
+            case '?':
+                add(QUESTION_MARK)
+            case '[':
+                j = i
+                if j < n and pat[j] == '!':
+                    j = j+1
+                if j < n and pat[j] == ']':
+                    j = j+1
+                while j < n and pat[j] != ']':
+                    j = j+1
+                if j >= n:
+                    add('\\[')
                 else:
-                    chunks = []
-                    k = i+2 if pat[i] == '!' else i+1
-                    while True:
-                        k = pat.find('-', k, j)
-                        if k < 0:
-                            break
-                        chunks.append(pat[i:k])
-                        i = k+1
-                        k = k+3
-                    chunk = pat[i:j]
-                    if chunk:
-                        chunks.append(chunk)
+                    stuff = pat[i:j]
+                    if '-' not in stuff:
+                        stuff = stuff.replace('\\', r'\\')
                     else:
-                        chunks[-1] += '-'
-                    # Remove empty ranges -- invalid in RE.
-                    for k in range(len(chunks)-1, 0, -1):
-                        if chunks[k-1][-1] > chunks[k][0]:
-                            chunks[k-1] = chunks[k-1][:-1] + chunks[k][1:]
-                            del chunks[k]
-                    # Escape backslashes and hyphens for set difference (--).
-                    # Hyphens that create ranges shouldn't be escaped.
-                    stuff = '-'.join(s.replace('\\', r'\\').replace('-', r'\-')
-                                     for s in chunks)
-                # Escape set operations (&&, ~~ and ||).
-                stuff = re.sub(r'([&~|])', r'\\\1', stuff)
-                i = j+1
-                if not stuff:
-                    # Empty range: never match.
-                    add('(?!)')
-                elif stuff == '!':
-                    # Negated empty range: match any character.
-                    add('.')
-                else:
-                    if stuff[0] == '!':
-                        stuff = '^' + stuff[1:]
-                    elif stuff[0] in ('^', '['):
-                        stuff = '\\' + stuff
-                    add(f'[{stuff}]')
-        else:
-            add(re.escape(c))
+                        chunks = []
+                        k = i+2 if pat[i] == '!' else i+1
+                        while True:
+                            k = pat.find('-', k, j)
+                            if k < 0:
+                                break
+                            chunks.append(pat[i:k])
+                            i = k+1
+                            k = k+3
+                        chunk = pat[i:j]
+                        if chunk:
+                            chunks.append(chunk)
+                        else:
+                            chunks[-1] += '-'
+                        # Remove empty ranges -- invalid in RE.
+                        for k in range(len(chunks)-1, 0, -1):
+                            if chunks[k-1][-1] > chunks[k][0]:
+                                chunks[k-1] = chunks[k-1][:-1] + chunks[k][1:]
+                                del chunks[k]
+                        # Escape backslashes and hyphens for set difference (--).
+                        # Hyphens that create ranges shouldn't be escaped.
+                        stuff = '-'.join(s.replace('\\', r'\\').replace('-', r'\-')
+                                            for s in chunks)
+                    # Escape set operations (&&, ~~ and ||).
+                    stuff = re.sub(r'([&~|])', r'\\\1', stuff)
+                    i = j+1
+                    if not stuff:
+                        # Empty range: never match.
+                        add('(?!)')
+                    elif stuff == '!':
+                        # Negated empty range: match any character.
+                        add('.')
+                    else:
+                        if stuff[0] == '!':
+                            stuff = '^' + stuff[1:]
+                        elif stuff[0] in ('^', '['):
+                            stuff = '\\' + stuff
+                        add(f'[{stuff}]')
+            case _:
+                add(re.escape(c))
     assert i == n
     return res
 
