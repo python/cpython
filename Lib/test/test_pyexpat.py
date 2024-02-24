dbbd98c2f56020685a49236c2a2fd22be9e31465
Sebastian Pipping <sebastian@pipping.org>
2024-02-24 14:29:36 +0100

    pyexpat: Cover (Get|Set)ReparseDeferralEnabled by tests

--- Lib/test/test_pyexpat.py
+++ Lib/test/test_pyexpat.py
@@ -755,5 +755,83 @@ class ForeignDTDTests(unittest.TestCase):
         self.assertEqual(handler_call_args, [("bar", "baz")])
 
 
+class ReparseDeferralTest(unittest.TestCase):
+    def test_getter_initial_value(self):
+        if expat.version_info >= (2, 6, 0):
+            expected = True
+        else:
+            expected = False
+
+        parser = expat.ParserCreate()
+        actual = parser.GetReparseDeferralEnabled()
+
+        self.assertEqual(actual, expected)
+
+    def test_getter_setter_round_trip(self):
+        parser = expat.ParserCreate()
+        was_enabled = parser.GetReparseDeferralEnabled()
+
+        # We'll flip the value back and forth, and we expect ..
+        if expat.version_info >= (2, 6, 0):
+            # that flipping worked both ways for >=2.6.0
+            expected_1 = not was_enabled
+            expected_2 = was_enabled
+        else:
+            # that flipping did not have any effect for <2.6.0
+            expected_1 = False
+            expected_2 = False
+
+        parser.SetReparseDeferralEnabled(expected_1)
+        actual_1 = parser.GetReparseDeferralEnabled()
+
+        self.assertEqual(actual_1, expected_1)
+
+        parser.SetReparseDeferralEnabled(expected_2)
+        actual_2 = parser.GetReparseDeferralEnabled()
+
+        self.assertEqual(actual_2, expected_2)
+
+    def test_reparse_deferral_enabled(self):
+        if expat.version_info < (2, 6, 0):
+            return
+
+        started = []
+
+        def start_element(name, _):
+            started.append(name)
+
+        parser = expat.ParserCreate()
+        parser.StartElementHandler = start_element
+        self.assertTrue(parser.GetReparseDeferralEnabled())
+
+        for chunk in (b'<doc', b'/>'):
+            parser.Parse(chunk, False)
+
+        # The key test: Have handlers already fired?  Expecting: no.
+        self.assertEqual(started, [])
+
+        parser.Parse(b'', True)
+
+        self.assertEqual(started, ['doc'])
+
+    def test_reparse_deferral_disabled(self):
+        started = []
+
+        def start_element(name, _):
+            started.append(name)
+
+        parser = expat.ParserCreate()
+        parser.StartElementHandler = start_element
+        if expat.version_info >= (2, 6, 0):
+            parser.SetReparseDeferralEnabled(False)
+        self.assertFalse(parser.GetReparseDeferralEnabled())
+
+        for chunk in (b'<doc', b'/>'):
+            parser.Parse(chunk, False)
+
+        # The key test: Have handlers already fired?  Expecting: yes.
+        self.assertEqual(started, ['doc'])
+
+
 if __name__ == "__main__":
     unittest.main()
