a743ffec57b1c9d4ff5fef1265479a7ff1cbffaa
sobolevn <mail@sobolevn.me>
2024-02-25 11:00:08 +0300

    Do not run `make`, read file by lines

--- Lib/test/test_tools/test_makefile.py
+++ Lib/test/test_tools/test_makefile.py
@@ -6,36 +6,31 @@ import os
 import unittest
 from test import support
 
-SPLITTER = '@-----@'
 MAKEFILE = os.path.join(support.REPO_ROOT, 'Makefile')
 
 if not support.check_impl_detail(cpython=True):
     raise unittest.SkipTest('cpython only')
-if support.MS_WINDOWS:
-    raise unittest.SkipTest('Windows does not support make')
 if not os.path.exists(MAKEFILE) or not os.path.isfile(MAKEFILE):
     raise unittest.SkipTest('Makefile could not be found')
 
-try:
-    import _testcapi
-except ImportError:
-    TEST_MODULES = False
-else:
-    TEST_MODULES = True
 
-
-@support.requires_subprocess()
 class TestMakefile(unittest.TestCase):
     def list_test_dirs(self):
-        import subprocess
-        return subprocess.check_output(
-            ['make', '-s', '-C', support.REPO_ROOT, 'listtestmodules'],
-            universal_newlines=True,
-            encoding='utf-8',
-        )
+        result = []
+        found_testsubdirs = False
+        with open(MAKEFILE, 'r', encoding='utf-8') as f:
+            for line in f:
+                if line.startswith('TESTSUBDIRS='):
+                    found_testsubdirs = True
+                    continue
+                if found_testsubdirs:
+                    if '\t' not in line:
+                        break
+                    result.append(line.replace('\\', '').strip())
+        return result
 
-    def check_existing_test_modules(self, result):
-        test_dirs = list(filter(None, result.split(' ')))
+    def test_makefile_test_folders(self):
+        test_dirs = self.list_test_dirs()
         idle_test = 'idlelib/idle_test'
         self.assertIn(idle_test, test_dirs)
 
@@ -64,19 +59,3 @@ class TestMakefile(unittest.TestCase):
             set(),
         )
         self.assertEqual(len(test_dirs), len(unique_test_dirs))
-
-    def check_missing_test_modules(self, result):
-        self.assertEqual(result, '')
-
-    def test_makefile_test_folders(self):
-        result = self.list_test_dirs().strip()
-        self.assertEqual(
-            result.count(SPLITTER), 1,
-            msg=f'{SPLITTER} should be contained in the output exactly once',
-        )
-        _, actual_result = result.split(SPLITTER)
-        actual_result = actual_result.strip()
-        if TEST_MODULES:
-            self.check_existing_test_modules(actual_result)
-        else:
-            self.check_missing_test_modules(actual_result)
