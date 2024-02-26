4d959eaf7edef8eab684740e9beb805bc6c6e477
Gregory P. Smith <greg@krypto.org>
2024-02-25 16:02:56 -0800

    gh-71052: fix test_concurrent_futures wasi regression. (#115923)
    Fix the WASI test_concurrent_futures regression from #115917.

--- Lib/test/test_concurrent_futures/__init__.py
+++ Lib/test/test_concurrent_futures/__init__.py
@@ -1,7 +1,11 @@
 import os.path
 import unittest
 from test import support
-from test.support import import_helper
+from test.support import threading_helper
+
+
+# Adjust if we ever have a platform with processes but not threads.
+threading_helper.requires_working_threading(module=True)
 
 
 if support.check_sanitizer(address=True, memory=True):
