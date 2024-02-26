53b84e772cac6e4a55cebf908d6bb9c48fe254dc
Alex Waygood <Alex.Waygood@Gmail.com>
2024-02-26 15:45:38 +0000

    [3.12] gh-115881: Ensure `ast.parse()` parses conditional context managers even with low `feature_version` passed (#115920) (#115959)

--- Parser/parser.c
+++ Parser/parser.c
@@ -6566,7 +6566,7 @@ with_stmt_rule(Parser *p)
             UNUSED(_end_lineno); // Only used by EXTRA macro
             int _end_col_offset = _token->end_col_offset;
             UNUSED(_end_col_offset); // Only used by EXTRA macro
-            _res = CHECK_VERSION ( stmt_ty , 9 , "Parenthesized context managers are" , _PyAST_With ( a , b , NULL , EXTRA ) );
+            _res = _PyAST_With ( a , b , NULL , EXTRA );
             if (_res == NULL && PyErr_Occurred()) {
                 p->error_indicator = 1;
                 p->level--;
