1b73a03802f03348c2b0f63aa190fe3c2f1318dc
Skip Montanaro <skip.montanaro@gmail.com>
2024-02-29 13:32:27 -0600

    fix several typos (sheesh, what a maroon)...

--- Tools/build/makedep.sh
+++ Tools/build/makedep.sh
@@ -1,12 +1,12 @@
-#!/usr?/bin/bash -x
+#!/usr/bin/bash
 
 obj=$1
 
-src=$(echo $obj | sed -e 's/[.]o$/[.]S/')
+src=$(echo $obj | sed -e 's/[.]o$/.S/')
 if [ -r $src ] ; then
     :
 else
-    src=$(echo $obj | sed -e 's/[.]o$/[.]c/')
+    src=$(echo $obj | sed -e 's/[.]o$/.c/')
 fi
 
 $CC $CPPFLAGS -MM -MT $obj $src
