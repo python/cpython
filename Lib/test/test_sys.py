ff886ffe922d4ae29aeaca6e38ad806fede1cc9e
Tian Gao <gaogaotiantian@hotmail.com>
2024-03-01 18:34:17 -0800

    Update tests for the new f_locals

--- Lib/test/test_sys.py
+++ Lib/test/test_sys.py
@@ -1550,7 +1550,7 @@ class SizeofTest(unittest.TestCase):
         def func():
             return sys._getframe()
         x = func()
-        check(x, size('3Pi3c7P2ic??2P'))
+        check(x, size('3Pi3cP7P2ic??2P'))
         # function
         def func(): pass
         check(func, size('15Pi'))
