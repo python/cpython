7ffbb2fd37eb44638f7170cd45b3c213e4ba856b
Brett Simmers <swtaarrs@users.noreply.github.com>
2024-03-04 08:29:39 -0800

    gh-115832: Fix instrumentation version mismatch during interpreter shutdown (#115856)
    A previous commit introduced a bug to `interpreter_clear()`: it set
    `interp->ceval.instrumentation_version` to 0, without making the corresponding
    change to `tstate->eval_breaker` (which holds a thread-local copy of the
    version). After this happens, Python code can still run due to object finalizers
    during a GC, and the version check in bytecodes.c will see a different result
    than the one in instrumentation.c causing an infinite loop.
    
    The fix itself is straightforward: clear `tstate->eval_breaker` when clearing
    `interp->ceval.instrumentation_version`.

--- Lib/test/test_monitoring.py
+++ Lib/test/test_monitoring.py
@@ -9,7 +9,8 @@ import textwrap
 import types
 import unittest
 import asyncio
-from test.support import requires_specialization
+from test import support
+from test.support import requires_specialization, script_helper
 
 PAIR = (0,1)
 
@@ -1858,3 +1859,12 @@ class TestTier2Optimizer(CheckEvents):
             sys.monitoring.register_callback(TEST_TOOL, E.LINE, None)
             sys.monitoring.set_events(TEST_TOOL, 0)
         self.assertGreater(len(events), 250)
+
+class TestMonitoringAtShutdown(unittest.TestCase):
+
+    def test_monitoring_live_at_shutdown(self):
+        # gh-115832: An object destructor running during the final GC of
+        # interpreter shutdown triggered an infinite loop in the
+        # instrumentation code.
+        script = support.findfile("_test_monitoring_shutdown.py")
+        script_helper.run_test_script(script)
