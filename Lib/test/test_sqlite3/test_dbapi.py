9b3f0f7bb4f845b0af332cc3efdef161f71b27e0
Erlend E. Aasland <erlend@python.org>
2024-03-04 14:35:06 +0100

    gh-116303: Skip some sqlite3 tests if testcapi is unavailable

--- Lib/test/test_sqlite3/test_dbapi.py
+++ Lib/test/test_sqlite3/test_dbapi.py
@@ -31,11 +31,10 @@ import urllib.parse
 
 from test.support import (
     SHORT_TIMEOUT, check_disallow_instantiation, requires_subprocess,
-    is_apple, is_emscripten, is_wasi
+    is_apple, is_emscripten, is_wasi, import_helper
 )
 from test.support import gc_collect
 from test.support import threading_helper
-from _testcapi import INT_MAX, ULLONG_MAX
 from os import SEEK_SET, SEEK_CUR, SEEK_END
 from test.support.os_helper import TESTFN, TESTFN_UNDECODABLE, unlink, temp_dir, FakePath
 
@@ -1200,6 +1199,7 @@ class BlobTests(unittest.TestCase):
         self.assertEqual(self.blob.tell(), 40)
 
     def test_blob_seek_error(self):
+        testcapi = import_helper.import_module("_testcapi")
         msg_oor = "offset out of blob range"
         msg_orig = "'origin' should be os.SEEK_SET, os.SEEK_CUR, or os.SEEK_END"
         msg_of = "seek offset results in overflow"
@@ -1217,9 +1217,9 @@ class BlobTests(unittest.TestCase):
         # Force overflow errors
         self.blob.seek(1, SEEK_SET)
         with self.assertRaisesRegex(OverflowError, msg_of):
-            self.blob.seek(INT_MAX, SEEK_CUR)
+            self.blob.seek(testcapi.INT_MAX, SEEK_CUR)
         with self.assertRaisesRegex(OverflowError, msg_of):
-            self.blob.seek(INT_MAX, SEEK_END)
+            self.blob.seek(testcapi.INT_MAX, SEEK_END)
 
     def test_blob_read(self):
         buf = self.blob.read()
@@ -1374,13 +1374,14 @@ class BlobTests(unittest.TestCase):
             self.blob["a"] = b"b"
 
     def test_blob_get_item_error(self):
+        testcapi = import_helper.import_module("_testcapi")
         dataset = [len(self.blob), 105, -105]
         for idx in dataset:
             with self.subTest(idx=idx):
                 with self.assertRaisesRegex(IndexError, "index out of range"):
                     self.blob[idx]
         with self.assertRaisesRegex(IndexError, "cannot fit 'int'"):
-            self.blob[ULLONG_MAX]
+            self.blob[testcapi.ULLONG_MAX]
 
         # Provoke read error
         self.cx.execute("update test set b='aaaa' where rowid=1")
