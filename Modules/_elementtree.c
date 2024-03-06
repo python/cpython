854f6452947d9c6124f8f30f0423e8766dab972f
Sebastian Pipping <sebastian@pipping.org>
2024-03-07 00:03:44 +0100

    [3.8] gh-115398: Expose Expat >=2.6.0 reparse deferral API (CVE-2023-52425) (GH-115623) (GH-116275)
    Allow controlling Expat >=2.6.0 reparse deferral (CVE-2023-52425) by adding five new methods:
    
    - `xml.etree.ElementTree.XMLParser.flush`
    - `xml.etree.ElementTree.XMLPullParser.flush`
    - `xml.parsers.expat.xmlparser.GetReparseDeferralEnabled`
    - `xml.parsers.expat.xmlparser.SetReparseDeferralEnabled`
    - `xml.sax.expatreader.ExpatParser.flush`
    
    Based on the "flush" idea from https://github.com/python/cpython/pull/115138#issuecomment-1932444270 .
    
    Includes code suggested-by: Snild Dolkow <snild@sony.com>
    and by core dev Serhiy Storchaka.
    
    Co-authored-by: Gregory P. Smith <greg@krypto.org>

--- Modules/_elementtree.c
+++ Modules/_elementtree.c
@@ -3946,6 +3946,38 @@ _elementtree_XMLParser_close_impl(XMLParserObject *self)
 }
 
 /*[clinic input]
+_elementtree.XMLParser.flush
+
+[clinic start generated code]*/
+
+static PyObject *
+_elementtree_XMLParser_flush_impl(XMLParserObject *self)
+/*[clinic end generated code: output=42fdb8795ca24509 input=effbecdb28715949]*/
+{
+    if (!_check_xmlparser(self)) {
+        return NULL;
+    }
+
+    if (EXPAT(SetReparseDeferralEnabled) == NULL) {
+        Py_RETURN_NONE;
+    }
+
+    // NOTE: The Expat parser in the C implementation of ElementTree is not
+    //       exposed to the outside; as a result we known that reparse deferral
+    //       is currently enabled, or we would not even have access to function
+    //       XML_SetReparseDeferralEnabled in the first place (which we checked
+    //       for, a few lines up).
+
+    EXPAT(SetReparseDeferralEnabled)(self->parser, XML_FALSE);
+
+    PyObject *res = expat_parse(self, "", 0, XML_FALSE);
+
+    EXPAT(SetReparseDeferralEnabled)(self->parser, XML_TRUE);
+
+    return res;
+}
+
+/*[clinic input]
 _elementtree.XMLParser.feed
 
     data: object
@@ -4375,6 +4407,7 @@ static PyTypeObject TreeBuilder_Type = {
 static PyMethodDef xmlparser_methods[] = {
     _ELEMENTTREE_XMLPARSER_FEED_METHODDEF
     _ELEMENTTREE_XMLPARSER_CLOSE_METHODDEF
+    _ELEMENTTREE_XMLPARSER_FLUSH_METHODDEF
     _ELEMENTTREE_XMLPARSER__PARSE_WHOLE_METHODDEF
     _ELEMENTTREE_XMLPARSER__SETEVENTS_METHODDEF
     {NULL, NULL}
