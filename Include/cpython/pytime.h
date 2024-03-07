21ce6f7ec383d56bf308787002072ec06ab9b229
Petr Viktorin <encukou@gmail.com>
2024-03-07 13:26:04 +0100

    gh-110850: Expose _PyTime_*Unchecked functions for now

--- Include/cpython/pytime.h
+++ Include/cpython/pytime.h
@@ -16,6 +16,14 @@ PyAPI_FUNC(int) PyTime_Monotonic(PyTime_t *result);
 PyAPI_FUNC(int) PyTime_PerfCounter(PyTime_t *result);
 PyAPI_FUNC(int) PyTime_Time(PyTime_t *result);
 
+// Expose internal API until we figure out what to do about it,
+// see https://github.com/capi-workgroup/decisions/issues/15
+// Do not use; like any names with a leading underscore, these may go away
+// at any time.
+PyAPI_FUNC(PyTime_t) _PyTime_TimeUnchecked(void);
+PyAPI_FUNC(PyTime_t) _PyTime_MonotonicUnchecked(void);
+PyAPI_FUNC(PyTime_t) _PyTime_PerfCounterUnchecked(void);
+
 #ifdef __cplusplus
 }
 #endif
