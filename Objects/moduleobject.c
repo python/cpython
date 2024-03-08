d7f1bc089b222f4993c206d9369e86cff8bc0215
hauntsaninja <hauntsaninja@gmail.com>
2024-03-08 00:51:54 -0800

    Revert "remove explicit safe path check"
    This reverts commit 3ec37d19b2b80aa093f1358254e895668981e2c2.

--- Objects/moduleobject.c
+++ Objects/moduleobject.c
@@ -825,12 +825,9 @@ _is_module_possibly_shadowing(PyObject *origin)
         return 0;
     }
     const PyConfig *config = _Py_GetConfig();
-    // sys.path[0] or os.getcwd()
-    wchar_t *sys_path_0 = config->sys_path_0;
-    if (sys_path_0 == NULL) {
+    if (config->safe_path) {
         return 0;
     }
-    assert(!config->safe_path);
 
     // os.path.dirname(origin)
     wchar_t origin_dirname[MAXPATHLEN + 1];
@@ -846,6 +843,12 @@ _is_module_possibly_shadowing(PyObject *origin)
         return 0;
     }
     *sep = L'\0';
+
+    // sys.path[0] or os.getcwd()
+    wchar_t *sys_path_0 = config->sys_path_0;
+    if (!sys_path_0) {
+        return 0;
+    }
 
     wchar_t sys_path_0_buf[MAXPATHLEN];
     if (sys_path_0[0] == L'\0') {
