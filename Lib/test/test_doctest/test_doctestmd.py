8d228d9394f6c2cc4b323818b860ae0c24badf55
Mike Foster <i.am.mike.foster@gmail.com>
2024-03-09 08:30:52 +0000

    extend coverage to include multiple codeblocks in docstring

--- Lib/test/test_doctest/test_doctestmd.py
+++ Lib/test/test_doctest/test_doctestmd.py
@@ -13,8 +13,31 @@ def dummyfunction_codeblocks():
     """
     pass
 
+def dummyfunction_multiplecodeblocks():
+    """
+    A dummy function that uses codeblocks in the examples.
+
+    It is used like this:
+    ```
+    >>> 1 == 1
+    True
+    ```
+
+    Or like this:
+    ```
+    >>> 1 + 2
+    3
+    ```
+    """
+    pass
+
 class TestMarkdownDocstring(unittest.TestCase):
-    def test_DoctestRunner(self):
+    def test_DoctestRunner_codeblocks(self):
         test = doctest.DocTestFinder().find(dummyfunction_codeblocks)[0]
         results = doctest.DocTestRunner().run(test)
         self.assertEqual(results, (0,1))
+
+    def test_DoctestRunner_multiplecodeblocks(self):
+        test = doctest.DocTestFinder().find(dummyfunction_multiplecodeblocks)[0]
+        results = doctest.DocTestRunner().run(test)
+        self.assertEqual(results, (0,2))
