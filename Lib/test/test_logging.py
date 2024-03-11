d9f6c990802146eef9a0ab779c5027005e1af372
Serhiy Storchaka <storchaka@gmail.com>
2024-03-11 10:42:00 +0200

    Share code between test_rollover_at_midnight and test_rollover_at_weekday.

--- Lib/test/test_logging.py
+++ Lib/test/test_logging.py
@@ -6080,7 +6080,7 @@ class TimedRotatingFileHandlerTest(BaseFileTest):
                     print(tf.read())
         self.assertTrue(found, msg=msg)
 
-    def test_rollover_at_midnight(self):
+    def test_rollover_at_midnight(self, weekly=False):
         os_helper.unlink(self.fn)
         now = datetime.datetime.now()
         atTime = now.time()
@@ -6092,9 +6092,10 @@ class TimedRotatingFileHandlerTest(BaseFileTest):
             atTime = now.time()
         atTime = atTime.replace(microsecond=0)
         fmt = logging.Formatter('%(asctime)s %(message)s')
+        when = f'W{now.weekday()}' if weekly else 'MIDNIGHT'
         for i in range(3):
             fh = logging.handlers.TimedRotatingFileHandler(
-                self.fn, encoding="utf-8", when='MIDNIGHT', atTime=atTime)
+                self.fn, encoding="utf-8", when=when, atTime=atTime)
             fh.setFormatter(fmt)
             r2 = logging.makeLogRecord({'msg': f'testing1 {i}'})
             fh.emit(r2)
@@ -6107,12 +6108,12 @@ class TimedRotatingFileHandlerTest(BaseFileTest):
         os.utime(self.fn, (now.timestamp() - 1,)*2)
         for i in range(2):
             fh = logging.handlers.TimedRotatingFileHandler(
-                self.fn, encoding="utf-8", when='MIDNIGHT', atTime=atTime)
+                self.fn, encoding="utf-8", when=when, atTime=atTime)
             fh.setFormatter(fmt)
             r2 = logging.makeLogRecord({'msg': f'testing2 {i}'})
             fh.emit(r2)
             fh.close()
-        rolloverDate = now - datetime.timedelta(days=1)
+        rolloverDate = now - datetime.timedelta(days=7 if weekly else 1)
         otherfn = f'{self.fn}.{rolloverDate:%Y-%m-%d}'
         self.assertLogFile(otherfn)
         with open(self.fn, encoding="utf-8") as f:
@@ -6123,46 +6124,7 @@ class TimedRotatingFileHandlerTest(BaseFileTest):
                 self.assertIn(f'testing1 {i}', line)
 
     def test_rollover_at_weekday(self):
-        os_helper.unlink(self.fn)
-        now = datetime.datetime.now()
-        atTime = now.time()
-        if not 0.1 < atTime.microsecond/1e6 < 0.9:
-            # The test requires all records to be emitted within
-            # the range of the same whole second.
-            time.sleep((0.1 - atTime.microsecond/1e6) % 1.0)
-            now = datetime.datetime.now()
-            atTime = now.time()
-        atTime = atTime.replace(microsecond=0)
-        fmt = logging.Formatter('%(asctime)s %(message)s')
-        for i in range(3):
-            fh = logging.handlers.TimedRotatingFileHandler(
-                self.fn, encoding="utf-8", when=f'W{now.weekday()}', atTime=atTime)
-            fh.setFormatter(fmt)
-            r2 = logging.makeLogRecord({'msg': f'testing1 {i}'})
-            fh.emit(r2)
-            fh.close()
-        self.assertLogFile(self.fn)
-        with open(self.fn, encoding="utf-8") as f:
-            for i, line in enumerate(f):
-                self.assertIn(f'testing1 {i}', line)
-
-        os.utime(self.fn, (now.timestamp() - 1,)*2)
-        for i in range(2):
-            fh = logging.handlers.TimedRotatingFileHandler(
-                self.fn, encoding="utf-8", when=f'W{now.weekday()}', atTime=atTime)
-            fh.setFormatter(fmt)
-            r2 = logging.makeLogRecord({'msg': f'testing2 {i}'})
-            fh.emit(r2)
-            fh.close()
-        rolloverDate = now - datetime.timedelta(days=7)
-        otherfn = f'{self.fn}.{rolloverDate:%Y-%m-%d}'
-        self.assertLogFile(otherfn)
-        with open(self.fn, encoding="utf-8") as f:
-            for i, line in enumerate(f):
-                self.assertIn(f'testing2 {i}', line)
-        with open(otherfn, encoding="utf-8") as f:
-            for i, line in enumerate(f):
-                self.assertIn(f'testing1 {i}', line)
+        self.test_rollover_at_midnight(weekly=True)
 
     def test_invalid(self):
         assertRaises = self.assertRaises
