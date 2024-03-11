e5df8d67c28626bdc5cfe41f8cd0a11a812917a8
Ethan Furman <ethan@stoneleaf.us>
2024-03-11 13:42:01 -0700

    gh-116040: [Enum] fix test_empty_names test (GH-116508)
    * and fix _not_given usage
    (cherry picked from commit 3c0dcef9808e34744096769b15bad4f1f97569f0)
    
    Co-authored-by: Ethan Furman <ethan@stoneleaf.us>

--- Lib/enum.py
+++ Lib/enum.py
@@ -169,8 +169,6 @@ def _dedent(text):
 class _not_given:
     def __repr__(self):
         return('<not given>')
-    def __bool__(self):
-        return False
 _not_given = _not_given()
 
 class _auto_null:
@@ -765,7 +763,7 @@ class EnumType(type):
                     )
         return cls._create_(
                 class_name=value,
-                names=names or None,
+                names=None if names is _not_given else names,
                 module=module,
                 qualname=qualname,
                 type=type,
