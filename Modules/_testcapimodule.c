9d8638e45a653d7059dedd8e3233cf756531b416
Victor Stinner <vstinner@python.org>
2024-03-11 11:28:16 +0100

    gh-116417: Move 4 limited C API test files to _testlimitedcapi (#116571)
    Move the following files from Modules/_testcapi/ to
    Modules/_testlimitedcapi/:
    
    * bytearray.c
    * bytes.c
    * pyos.c
    * sys.c
    
    Changes:
    
    * Replace PyBytes_AS_STRING() with PyBytes_AsString().
    * Replace PyBytes_GET_SIZE() with PyBytes_Size().
    * Update related test_capi tests.
    * Copy Modules/_testcapi/util.h to Modules/_testlimitedcapi/util.h.

--- Modules/_testcapimodule.c
+++ Modules/_testcapimodule.c
@@ -4017,12 +4017,6 @@ PyInit__testcapi(void)
     if (_PyTestCapi_Init_Abstract(m) < 0) {
         return NULL;
     }
-    if (_PyTestCapi_Init_ByteArray(m) < 0) {
-        return NULL;
-    }
-    if (_PyTestCapi_Init_Bytes(m) < 0) {
-        return NULL;
-    }
     if (_PyTestCapi_Init_Unicode(m) < 0) {
         return NULL;
     }
@@ -4077,16 +4071,10 @@ PyInit__testcapi(void)
     if (_PyTestCapi_Init_Buffer(m) < 0) {
         return NULL;
     }
-    if (_PyTestCapi_Init_PyOS(m) < 0) {
-        return NULL;
-    }
     if (_PyTestCapi_Init_File(m) < 0) {
         return NULL;
     }
     if (_PyTestCapi_Init_Codec(m) < 0) {
-        return NULL;
-    }
-    if (_PyTestCapi_Init_Sys(m) < 0) {
         return NULL;
     }
     if (_PyTestCapi_Init_Immortal(m) < 0) {
