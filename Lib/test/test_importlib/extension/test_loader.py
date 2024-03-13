e529b4803275c14628a68df1ac81f61e160e9f9c
Brett Cannon <brett@python.org>
2024-03-13 13:24:28 -0700

    [3.11] GH-115979: update test_importlib to work under WASI SDK 21 (GH-116754) (cherry picked from commit 61733a2fb9dc36d2246d922146a3462a2248832d)
    Co-authored-by: Brett Cannon <brett@python.org>

--- Lib/test/test_importlib/extension/test_loader.py
+++ Lib/test/test_importlib/extension/test_loader.py
@@ -19,7 +19,7 @@ class LoaderTests(abc.LoaderTests):
     """Test load_module() for extension modules."""
 
     def setUp(self):
-        if not self.machinery.EXTENSION_SUFFIXES:
+        if not self.machinery.EXTENSION_SUFFIXES or not util.EXTENSIONS:
             raise unittest.SkipTest("Requires dynamic loading support.")
         if util.EXTENSIONS.name in sys.builtin_module_names:
             raise unittest.SkipTest(
@@ -99,7 +99,7 @@ class MultiPhaseExtensionModuleTests(abc.LoaderTests):
     # Test loading extension modules with multi-phase initialization (PEP 489).
 
     def setUp(self):
-        if not self.machinery.EXTENSION_SUFFIXES:
+        if not self.machinery.EXTENSION_SUFFIXES or not util.EXTENSIONS:
             raise unittest.SkipTest("Requires dynamic loading support.")
         self.name = '_testmultiphase'
         if self.name in sys.builtin_module_names:
