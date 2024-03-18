cd2ed917801b93fb46d1dcf19dd480e5146932d8
AN Long <aisk@users.noreply.github.com>
2024-03-18 19:48:50 +0800

    gh-115538: Emit warning when use bool as fd in _io.WindowsConsoleIO (GH-116925)

--- Modules/_io/winconsoleio.c
+++ Modules/_io/winconsoleio.c
@@ -298,6 +298,13 @@ _io__WindowsConsoleIO___init___impl(winconsoleio *self, PyObject *nameobj,
             self->fd = -1;
     }
 
+    if (PyBool_Check(nameobj)) {
+        if (PyErr_WarnEx(PyExc_RuntimeWarning,
+                "bool is used as a file descriptor", 1))
+        {
+            return -1;
+        }
+    }
     fd = PyLong_AsInt(nameobj);
     if (fd < 0) {
         if (!PyErr_Occurred()) {
