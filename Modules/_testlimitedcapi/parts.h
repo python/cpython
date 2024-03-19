12cb8327193106c00ae42fa58edac49187c5fd16
Victor Stinner <vstinner@python.org>
2024-03-19 15:04:23 +0100

    gh-116417: Move limited C API long.c tests to _testlimitedcapi (#117001)
    * Split long.c tests of _testcapi into two parts: limited C API tests
      in _testlimitedcapi and non-limited C API tests in _testcapi.
    * Move testcapi_long.h from Modules/_testcapi/ to
      Modules/_testlimitedcapi/.
    * Add MODULE__TESTLIMITEDCAPI_DEPS to Makefile.pre.in.

--- Modules/_testlimitedcapi/parts.h
+++ Modules/_testlimitedcapi/parts.h
@@ -28,6 +28,7 @@ int _PyTestLimitedCAPI_Init_Bytes(PyObject *module);
 int _PyTestLimitedCAPI_Init_Float(PyObject *module);
 int _PyTestLimitedCAPI_Init_HeaptypeRelative(PyObject *module);
 int _PyTestLimitedCAPI_Init_List(PyObject *module);
+int _PyTestLimitedCAPI_Init_Long(PyObject *module);
 int _PyTestLimitedCAPI_Init_PyOS(PyObject *module);
 int _PyTestLimitedCAPI_Init_Set(PyObject *module);
 int _PyTestLimitedCAPI_Init_Sys(PyObject *module);
