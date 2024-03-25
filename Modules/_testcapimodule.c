abf998e373b31fb9943d1519032b6f9d896210af
Petr Viktorin <encukou@gmail.com>
2024-03-25 15:36:29 +0100

    Move the new tests

--- Modules/_testcapimodule.c
+++ Modules/_testcapimodule.c
@@ -672,116 +672,6 @@ pyobject_bytes_from_null(PyObject *self, PyObject *Py_UNUSED(ignored))
 }
 
 static PyObject *
-call_pyobject_print(PyObject *self, PyObject * args)
-{
-    PyObject *object;
-    PyObject *filename;
-    PyObject *print_raw;
-    FILE *fp;
-    int flags = 0;
-
-    if (!PyArg_UnpackTuple(args, "call_pyobject_print", 3, 3, &object, &filename, &print_raw)) {
-        return NULL;
-    }
-
-    fp = _Py_fopen_obj(filename, "w+");
-
-    if (Py_IsTrue(print_raw)) {
-        flags = Py_PRINT_RAW;
-    }
-
-    if (PyObject_Print(object, fp, flags) < 0) {
-        fclose(fp);
-        return NULL;
-    }
-
-    fclose(fp);
-
-    Py_RETURN_NONE;
-}
-
-static PyObject *
-pyobject_print_null(PyObject *self, PyObject *args)
-{
-    PyObject *filename;
-    FILE *fp;
-
-    if (!PyArg_UnpackTuple(args, "call_pyobject_print", 1, 1, &filename)) {
-        return NULL;
-    }
-
-    fp = _Py_fopen_obj(filename, "w+");
-
-    if (PyObject_Print(NULL, fp, 0) < 0) {
-        fclose(fp);
-        return NULL;
-    }
-
-    fclose(fp);
-
-    Py_RETURN_NONE;
-}
-
-static PyObject *
-pyobject_print_noref_object(PyObject *self, PyObject *args)
-{
-    PyObject *test_string;
-    PyObject *filename;
-    FILE *fp;
-    char correct_string[100];
-
-    test_string = PyUnicode_FromString("Spam spam spam");
-
-    test_string -> ob_refcnt = 0;
-
-    snprintf(correct_string, 100, "<refcnt %zd at %p>", Py_REFCNT(test_string), (void *)test_string);
-
-    if (!PyArg_UnpackTuple(args, "call_pyobject_print", 1, 1, &filename)) {
-        return NULL;
-    }
-
-    fp = _Py_fopen_obj(filename, "w+");
-
-    if (PyObject_Print(test_string, fp, 0) < 0){
-        fclose(fp);
-        return NULL;
-    }
-
-    fclose(fp);
-
-    test_string -> ob_refcnt = 1;
-    Py_DECREF(test_string);
-
-    return PyUnicode_FromString(correct_string);
-}
-
-static PyObject *
-pyobject_print_os_error(PyObject *self, PyObject *args)
-{
-    PyObject *test_string;
-    PyObject *filename;
-    FILE *fp;
-
-    test_string = PyUnicode_FromString("Spam spam spam");
-
-    if (!PyArg_UnpackTuple(args, "call_pyobject_print", 1, 1, &filename)) {
-        return NULL;
-    }
-
-    // open file in read mode to induce OSError
-    fp = _Py_fopen_obj(filename, "r");
-
-    if (PyObject_Print(test_string, fp, 0) < 0) {
-        fclose(fp);
-        return NULL;
-    }
-
-    fclose(fp);
-
-    Py_RETURN_NONE;
-}
-
-static PyObject *
 set_errno(PyObject *self, PyObject *args)
 {
     int new_errno;
@@ -3366,10 +3256,6 @@ static PyMethodDef TestMethods[] = {
     {"pyobject_repr_from_null", pyobject_repr_from_null, METH_NOARGS},
     {"pyobject_str_from_null",  pyobject_str_from_null, METH_NOARGS},
     {"pyobject_bytes_from_null", pyobject_bytes_from_null, METH_NOARGS},
-    {"call_pyobject_print", call_pyobject_print, METH_VARARGS},
-    {"pyobject_print_null", pyobject_print_null, METH_VARARGS},
-    {"pyobject_print_noref_object", pyobject_print_noref_object, METH_VARARGS},
-    {"pyobject_print_os_error", pyobject_print_os_error, METH_VARARGS},
     {"test_string_to_double",   test_string_to_double,           METH_NOARGS},
     {"test_capsule", (PyCFunction)test_capsule, METH_NOARGS},
     {"test_from_contiguous", (PyCFunction)test_from_contiguous, METH_NOARGS},
@@ -4158,6 +4044,9 @@ PyInit__testcapi(void)
         return NULL;
     }
     if (_PyTestCapi_Init_Time(m) < 0) {
+        return NULL;
+    }
+    if (_PyTestCapi_Init_Object(m) < 0) {
         return NULL;
     }
 
