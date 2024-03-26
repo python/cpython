a470cd71ae90cd4e40f07dd9cd8c86216e7e90e9
AN Long <aisk@users.noreply.github.com>
2024-03-27 02:26:48 +0800

    [3.12] gh-104242: Enable test_is_char_device_true in pathlib test on all platform (GH-116983) (cherry picked from commit af1b0e94400d1bf732466d675054df8cf7dfb62d)
    Co-authored-by: AN Long <aisk@users.noreply.github.com>

--- Lib/test/test_pathlib.py
+++ Lib/test/test_pathlib.py
@@ -2634,15 +2634,15 @@ class _BasePathTest(object):
         self.assertIs((P / 'fileA\x00').is_char_device(), False)
 
     def test_is_char_device_true(self):
-        # Under Unix, /dev/null should generally be a char device.
-        P = self.cls('/dev/null')
+        # os.devnull should generally be a char device.
+        P = self.cls(os.devnull)
         if not P.exists():
-            self.skipTest("/dev/null required")
+            self.skipTest("null device required")
         self.assertTrue(P.is_char_device())
         self.assertFalse(P.is_block_device())
         self.assertFalse(P.is_file())
-        self.assertIs(self.cls('/dev/null\udfff').is_char_device(), False)
-        self.assertIs(self.cls('/dev/null\x00').is_char_device(), False)
+        self.assertIs(self.cls(f'{os.devnull}\udfff').is_char_device(), False)
+        self.assertIs(self.cls(f'{os.devnull}\x00').is_char_device(), False)
 
     def test_pickling_common(self):
         p = self.cls(BASE, 'fileA')
