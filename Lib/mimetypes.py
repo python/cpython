5f2da2e5e118c4216581afda8cc9e141def4f4c8
Serhiy Storchaka <storchaka@gmail.com>
2024-03-26 13:26:45 +0200

    gh-66543: Fix mimetype.guess_type() (GH-117217)
    Fix parsing of the following corner cases:
    
    * URLs with only a host name
    * URLs containing a fragment
    * URLs containing a query
    * filenames with only a UNC sharepoint on Windows
    
    Co-authored-by: Dong-hee Na <donghee.na92@gmail.com>

--- Lib/mimetypes.py
+++ Lib/mimetypes.py
@@ -120,7 +120,13 @@ class MimeTypes:
         but non-standard types.
         """
         url = os.fspath(url)
-        scheme, url = urllib.parse._splittype(url)
+        p = urllib.parse.urlparse(url)
+        if p.scheme and len(p.scheme) > 1:
+            scheme = p.scheme
+            url = p.path
+        else:
+            scheme = None
+            url = os.path.splitdrive(url)[1]
         if scheme == 'data':
             # syntax of data URLs:
             # dataurl   := "data:" [ mediatype ] [ ";base64" ] "," data
