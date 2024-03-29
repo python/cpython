63200cdb9f6f5c026f7f9708a83247304c9f1def
Matt Page <mpage@meta.com>
2024-03-28 17:35:39 -0700

    Don't explicitly zero initialize striped lock

--- Python/pystate.c
+++ Python/pystate.c
@@ -625,9 +625,6 @@ init_interpreter(PyInterpreterState *interp,
     _PyType_InitCache(interp);
 #ifdef Py_GIL_DISABLED
     _Py_brc_init_state(interp);
-    for (int i = 0; i < NUM_WEAKREF_LIST_LOCKS; i++) {
-        interp->weakref_locks[i] = (PyMutex){0};
-    }
 #endif
     llist_init(&interp->mem_free_queue.head);
     for (int i = 0; i < _PY_MONITORING_UNGROUPED_EVENTS; i++) {
