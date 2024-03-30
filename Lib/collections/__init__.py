46c37e9911f95a5eaf8f6e8b7e8fde87eabb39f3
Wes Turner <50891+westurner@users.noreply.github.com>
2024-03-30 03:01:49 -0400

    DOC: cpython/Lib/collections:Counter.elements: fix docstring

--- Lib/collections/__init__.py
+++ Lib/collections/__init__.py
@@ -639,14 +639,15 @@ class Counter(dict):
         >>> sorted(c.elements())
         ['A', 'A', 'B', 'B', 'C', 'C']
 
-        # Knuth's example for prime factors of 1836:  2**2 * 3**3 * 17**1
+        Knuth's example for prime factors of 1836:  ``2**2 * 3**3 * 17**1``:
+        
         >>> import math
         >>> prime_factors = Counter({2: 2, 3: 3, 17: 1})
         >>> math.prod(prime_factors.elements())
         1836
 
         Note, if an element's count has been set to zero or is a negative
-        number, elements() will ignore it.
+        number, ``elements()`` will ignore it.
 
         '''
         # Emulate Bag.do from Smalltalk and Multiset.begin from C++.
