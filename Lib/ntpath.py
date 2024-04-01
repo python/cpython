b5fdd278fa235860e46a794746f02362f12ed48b
Nineteendo <nineteendo19d0@gmail.com>
2024-04-01 20:28:28 +0200

    ;(

--- Lib/ntpath.py
+++ Lib/ntpath.py
@@ -334,8 +334,8 @@ def isreserved(path):
     """Return true if the pathname is reserved by the system."""
     # Refer to "Naming Files, Paths, and Namespaces":
     # https://docs.microsoft.com/en-us/windows/win32/fileio/naming-a-file
-    path = os.fsdecode(splitroot(path)[2]).replace('/', '\\')
-    return any(_isreservedname(name) for name in reversed(path.split('\\')))
+    path = os.fsdecode(splitroot(path)[2]).replace(altsep, sep)
+    return any(_isreservedname(name) for name in reversed(path.split(sep)))
 
 def _isreservedname(name):
     """Return true if the filename is reserved by the system."""
