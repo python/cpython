de54cf5be371a6f5e2e9f208c38def5f81d3ef02
Pablo Galindo <pablogsal@gmail.com>
2024-04-02 09:24:04 +0100

    Python 3.11.9

--- Include/patchlevel.h
+++ Include/patchlevel.h
@@ -18,12 +18,12 @@
 /*--start constants--*/
 #define PY_MAJOR_VERSION        3
 #define PY_MINOR_VERSION        11
-#define PY_MICRO_VERSION        8
+#define PY_MICRO_VERSION        9
 #define PY_RELEASE_LEVEL        PY_RELEASE_LEVEL_FINAL
 #define PY_RELEASE_SERIAL       0
 
 /* Version as a string */
-#define PY_VERSION              "3.11.8+"
+#define PY_VERSION              "3.11.9"
 /*--end constants--*/
 
 /* Version as a single 4-byte hex number, e.g. 0x010502B2 == 1.5.2b2.
