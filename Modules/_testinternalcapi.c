857d3151c9efa029268e8249e91d26eb1b31c2fd
Eric Snow <ericsnowcurrently@gmail.com>
2024-04-02 17:16:50 -0600

    gh-76785: Consolidate Some Interpreter-related Testing Helpers (gh-117485)
    This eliminates the duplication of functionally identical helpers in the _testinternalcapi and _xxsubinterpreters modules.

--- Modules/_testinternalcapi.c
+++ Modules/_testinternalcapi.c
@@ -23,7 +23,6 @@
 #include "pycore_initconfig.h"    // _Py_GetConfigsAsDict()
 #include "pycore_interp.h"        // _PyInterpreterState_GetConfigCopy()
 #include "pycore_long.h"          // _PyLong_Sign()
-#include "pycore_namespace.h"     // _PyNamespace_New()
 #include "pycore_object.h"        // _PyObject_IsFreed()
 #include "pycore_optimizer.h"     // _Py_UopsSymbol, etc.
 #include "pycore_pathconfig.h"    // _PyPathConfig_ClearGlobal()
@@ -831,6 +830,7 @@ _testinternalcapi_assemble_code_object_impl(PyObject *module,
 }
 
 
+// Maybe this could be replaced by get_interpreter_config()?
 static PyObject *
 get_interp_settings(PyObject *self, PyObject *args)
 {
@@ -1357,129 +1357,6 @@ dict_getitem_knownhash(PyObject *self, PyObject *args)
 }
 
 
-static int
-init_named_interp_config(PyInterpreterConfig *config, const char *name)
-{
-    if (name == NULL) {
-        name = "isolated";
-    }
-
-    if (strcmp(name, "isolated") == 0) {
-        *config = (PyInterpreterConfig)_PyInterpreterConfig_INIT;
-    }
-    else if (strcmp(name, "legacy") == 0) {
-        *config = (PyInterpreterConfig)_PyInterpreterConfig_LEGACY_INIT;
-    }
-    else if (strcmp(name, "empty") == 0) {
-        *config = (PyInterpreterConfig){0};
-    }
-    else {
-        PyErr_Format(PyExc_ValueError,
-                     "unsupported config name '%s'", name);
-        return -1;
-    }
-    return 0;
-}
-
-static PyObject *
-new_interp_config(PyObject *self, PyObject *args, PyObject *kwds)
-{
-    const char *name = NULL;
-    if (!PyArg_ParseTuple(args, "|s:new_config", &name)) {
-        return NULL;
-    }
-    PyObject *overrides = kwds;
-
-    if (name == NULL) {
-        name = "isolated";
-    }
-
-    PyInterpreterConfig config;
-    if (init_named_interp_config(&config, name) < 0) {
-        return NULL;
-    }
-
-    if (overrides != NULL && PyDict_GET_SIZE(overrides) > 0) {
-        if (_PyInterpreterConfig_UpdateFromDict(&config, overrides) < 0) {
-            return NULL;
-        }
-    }
-
-    PyObject *dict = _PyInterpreterConfig_AsDict(&config);
-    if (dict == NULL) {
-        return NULL;
-    }
-
-    PyObject *configobj = _PyNamespace_New(dict);
-    Py_DECREF(dict);
-    return configobj;
-}
-
-static PyObject *
-get_interp_config(PyObject *self, PyObject *args, PyObject *kwds)
-{
-    static char *kwlist[] = {"id", NULL};
-    PyObject *idobj = NULL;
-    if (!PyArg_ParseTupleAndKeywords(args, kwds,
-                                     "O:get_config", kwlist, &idobj))
-    {
-        return NULL;
-    }
-
-    PyInterpreterState *interp;
-    if (idobj == NULL) {
-        interp = PyInterpreterState_Get();
-    }
-    else {
-        interp = _PyInterpreterState_LookUpIDObject(idobj);
-        if (interp == NULL) {
-            return NULL;
-        }
-    }
-
-    PyInterpreterConfig config;
-    if (_PyInterpreterConfig_InitFromState(&config, interp) < 0) {
-        return NULL;
-    }
-    PyObject *dict = _PyInterpreterConfig_AsDict(&config);
-    if (dict == NULL) {
-        return NULL;
-    }
-
-    PyObject *configobj = _PyNamespace_New(dict);
-    Py_DECREF(dict);
-    return configobj;
-}
-
-static int
-interp_config_from_object(PyObject *configobj, PyInterpreterConfig *config)
-{
-    if (configobj == NULL || configobj == Py_None) {
-        if (init_named_interp_config(config, NULL) < 0) {
-            return -1;
-        }
-    }
-    else if (PyUnicode_Check(configobj)) {
-        if (init_named_interp_config(config, PyUnicode_AsUTF8(configobj)) < 0) {
-            return -1;
-        }
-    }
-    else {
-        PyObject *dict = PyObject_GetAttrString(configobj, "__dict__");
-        if (dict == NULL) {
-            PyErr_Format(PyExc_TypeError, "bad config %R", configobj);
-            return -1;
-        }
-        int res = _PyInterpreterConfig_InitFromDict(config, dict);
-        Py_DECREF(dict);
-        if (res < 0) {
-            return -1;
-        }
-    }
-    return 0;
-}
-
-
 /* To run some code in a sub-interpreter. */
 static PyObject *
 run_in_subinterp_with_config(PyObject *self, PyObject *args, PyObject *kwargs)
@@ -1495,7 +1372,14 @@ run_in_subinterp_with_config(PyObject *self, PyObject *args, PyObject *kwargs)
     }
 
     PyInterpreterConfig config;
-    if (interp_config_from_object(configobj, &config) < 0) {
+    PyObject *dict = PyObject_GetAttrString(configobj, "__dict__");
+    if (dict == NULL) {
+        PyErr_Format(PyExc_TypeError, "bad config %R", configobj);
+        return NULL;
+    }
+    int res = _PyInterpreterConfig_InitFromDict(&config, dict);
+    Py_DECREF(dict);
+    if (res < 0) {
         return NULL;
     }
 
@@ -1544,58 +1428,6 @@ unused_interpreter_id(PyObject *self, PyObject *Py_UNUSED(ignored))
     int64_t interpid = INT64_MAX;
     assert(interpid > _PyRuntime.interpreters.next_id);
     return PyLong_FromLongLong(interpid);
-}
-
-static PyObject *
-new_interpreter(PyObject *self, PyObject *args)
-{
-    PyObject *configobj = NULL;
-    if (!PyArg_ParseTuple(args, "|O:new_interpreter", &configobj)) {
-        return NULL;
-    }
-
-    PyInterpreterConfig config;
-    if (interp_config_from_object(configobj, &config) < 0) {
-        return NULL;
-    }
-
-    // Unlike _interpreters.create(), we do not automatically link
-    // the interpreter to its refcount.
-    PyThreadState *save_tstate = PyThreadState_Get();
-    PyThreadState *tstate = NULL;
-    PyStatus status = Py_NewInterpreterFromConfig(&tstate, &config);
-    PyThreadState_Swap(save_tstate);
-    if (PyStatus_Exception(status)) {
-        _PyErr_SetFromPyStatus(status);
-        return NULL;
-    }
-    PyInterpreterState *interp = PyThreadState_GetInterpreter(tstate);
-
-    if (_PyInterpreterState_IDInitref(interp) < 0) {
-        goto error;
-    }
-
-    int64_t interpid = PyInterpreterState_GetID(interp);
-    if (interpid < 0) {
-        goto error;
-    }
-    PyObject *idobj = PyLong_FromLongLong(interpid);
-    if (idobj == NULL) {
-        goto error;
-    }
-
-    PyThreadState_Swap(tstate);
-    PyThreadState_Clear(tstate);
-    PyThreadState_Swap(save_tstate);
-    PyThreadState_Delete(tstate);
-
-    return idobj;
-
-error:
-    save_tstate = PyThreadState_Swap(tstate);
-    Py_EndInterpreter(tstate);
-    PyThreadState_Swap(save_tstate);
-    return NULL;
 }
 
 static PyObject *
@@ -1658,28 +1490,6 @@ interpreter_refcount_linked(PyObject *self, PyObject *idobj)
         Py_RETURN_TRUE;
     }
     Py_RETURN_FALSE;
-}
-
-static PyObject *
-interpreter_incref(PyObject *self, PyObject *idobj)
-{
-    PyInterpreterState *interp = _PyInterpreterState_LookUpIDObject(idobj);
-    if (interp == NULL) {
-        return NULL;
-    }
-    _PyInterpreterState_IDIncref(interp);
-    Py_RETURN_NONE;
-}
-
-static PyObject *
-interpreter_decref(PyObject *self, PyObject *idobj)
-{
-    PyInterpreterState *interp = _PyInterpreterState_LookUpIDObject(idobj);
-    if (interp == NULL) {
-        return NULL;
-    }
-    _PyInterpreterState_IDDecref(interp);
-    Py_RETURN_NONE;
 }
 
 
@@ -1928,23 +1738,16 @@ static PyMethodDef module_functions[] = {
     {"get_object_dict_values", get_object_dict_values, METH_O},
     {"hamt", new_hamt, METH_NOARGS},
     {"dict_getitem_knownhash",  dict_getitem_knownhash,          METH_VARARGS},
-    {"new_interp_config", _PyCFunction_CAST(new_interp_config),
-     METH_VARARGS | METH_KEYWORDS},
-    {"get_interp_config", _PyCFunction_CAST(get_interp_config),
-     METH_VARARGS | METH_KEYWORDS},
     {"run_in_subinterp_with_config",
      _PyCFunction_CAST(run_in_subinterp_with_config),
      METH_VARARGS | METH_KEYWORDS},
     {"normalize_interp_id", normalize_interp_id, METH_O},
     {"unused_interpreter_id", unused_interpreter_id, METH_NOARGS},
-    {"new_interpreter", new_interpreter, METH_VARARGS},
     {"interpreter_exists", interpreter_exists, METH_O},
     {"get_interpreter_refcount", get_interpreter_refcount, METH_O},
     {"link_interpreter_refcount", link_interpreter_refcount,     METH_O},
     {"unlink_interpreter_refcount", unlink_interpreter_refcount, METH_O},
     {"interpreter_refcount_linked", interpreter_refcount_linked, METH_O},
-    {"interpreter_incref", interpreter_incref, METH_O},
-    {"interpreter_decref", interpreter_decref, METH_O},
     {"compile_perf_trampoline_entry", compile_perf_trampoline_entry, METH_VARARGS},
     {"perf_trampoline_set_persist_after_fork", perf_trampoline_set_persist_after_fork, METH_VARARGS},
     {"get_crossinterp_data",    get_crossinterp_data,            METH_VARARGS},
