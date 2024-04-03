214f7934ae4db7e7dd32e76a038a4332fa59b8bf
rsp4jack <rsp4jack@outlook.com>
2024-04-03 21:00:10 +0800

    Remove redundant check for TimeoutError As https://github.com/python/cpython/issues/117459#issuecomment-2034320722 said, TimeoutError is a builtin exception now

--- Lib/asyncio/futures.py
+++ Lib/asyncio/futures.py
@@ -320,8 +320,6 @@ def _convert_future_exc(exc):
     exc_class = type(exc)
     if exc_class is concurrent.futures.CancelledError:
         return exceptions.CancelledError(*exc.args).with_traceback(exc.__traceback__)
-    elif exc_class is concurrent.futures.TimeoutError:
-        return exceptions.TimeoutError(*exc.args).with_traceback(exc.__traceback__)
     elif exc_class is concurrent.futures.InvalidStateError:
         return exceptions.InvalidStateError(*exc.args).with_traceback(exc.__traceback__)
     else:
