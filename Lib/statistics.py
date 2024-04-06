75680e5fca6aa01f12b963fcca333725ab0a58e8
Raymond Hettinger <raymond.hettinger@gmail.com>
2024-04-05 21:24:33 -0500

    Auto detect size changes

--- Lib/statistics.py
+++ Lib/statistics.py
@@ -976,9 +976,11 @@ def kde(data, h, kernel='normal', *, cumulative=False):
     if support is None:
 
         def pdf(x):
+            n = len(data)
             return sum(K((x - x_i) / h) for x_i in data) / (n * h)
 
         def cdf(x):
+            n = len(data)
             return sum(I((x - x_i) / h) for x_i in data) / n
 
     else:
@@ -987,12 +989,20 @@ def kde(data, h, kernel='normal', *, cumulative=False):
         bandwidth = h * support
 
         def pdf(x):
+            nonlocal n, sample
+            if len(data) != n:
+                sample = sorted(data)
+                n = len(data)
             i = bisect_left(sample, x - bandwidth)
             j = bisect_right(sample, x + bandwidth)
             supported = sample[i : j]
             return sum(K((x - x_i) / h) for x_i in supported) / (n * h)
 
         def cdf(x):
+            nonlocal n, sample
+            if len(data) != n:
+                sample = sorted(data)
+                n = len(data)
             i = bisect_left(sample, x - bandwidth)
             j = bisect_right(sample, x + bandwidth)
             supported = sample[i : j]
