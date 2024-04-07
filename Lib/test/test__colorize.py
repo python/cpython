95b983134099aa29f00d9cf3ffe9690e2fd1ece8
Hugo van Kemenade <1324225+hugovk@users.noreply.github.com>
2024-04-07 20:54:55 +0300

    Use unittest's enterContext
    Co-authored-by: Alex Waygood <Alex.Waygood@Gmail.com>

--- Lib/test/test__colorize.py
+++ Lib/test/test__colorize.py
@@ -1,21 +1,11 @@
-import contextlib
 import sys
-import traceback
 import unittest
 import unittest.mock
 import _colorize
-from test.support import captured_output
 
 
 class TestColorizeFunction(unittest.TestCase):
     def test_colorized_detection_checks_for_environment_variables(self):
-        if sys.platform == "win32":
-            virtual_patching = unittest.mock.patch(
-                "nt._supports_virtual_terminal", return_value=True
-            )
-        else:
-            virtual_patching = contextlib.nullcontext()
-
         env_vars_expected = [
             ({'TERM': 'dumb'}, False),
             ({'PYTHON_COLORS': '1'}, True),
@@ -27,17 +17,23 @@ class TestColorizeFunction(unittest.TestCase):
             ({'FORCE_COLOR': '1', "PYTHON_COLORS": '0'}, False),
         ]
 
-        with virtual_patching:
-            with unittest.mock.patch("os.isatty") as isatty_mock:
-                isatty_mock.return_value = True
+        if sys.platform == "win32":
+            self.enterContext(
+                unittest.mock.patch(
+                    "nt._supports_virtual_terminal", return_value=True
+                )
+            )
 
-                for env_vars, expected in env_vars_expected:
-                    with self.subTest(env_vars=env_vars, expected_color=expected):
-                        with unittest.mock.patch("os.environ", env_vars):
-                            self.assertEqual(_colorize.can_colorize(), expected)
+        isatty_mock = self.enterContext(unittest.mock.patch("os.isatty"))
+        isatty_mock.return_value = True
 
-                isatty_mock.return_value = False
-                self.assertEqual(_colorize.can_colorize(), False)
+        for env_vars, expected in env_vars_expected:
+            with self.subTest(env_vars=env_vars, expected_color=expected):
+                with unittest.mock.patch("os.environ", env_vars):
+                    self.assertEqual(_colorize.can_colorize(), expected)
+
+        isatty_mock.return_value = False
+        self.assertEqual(_colorize.can_colorize(), False)
 
 
 if __name__ == "__main__":
