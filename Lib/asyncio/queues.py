5b41217df27d6053e1a57d734bfbea840ff6f45b
Laurie O <laurie_opperman@hotmail.com>
2024-04-08 15:16:55 +1000

    Mention gets are always unblocked

--- Lib/asyncio/queues.py
+++ Lib/asyncio/queues.py
@@ -256,8 +256,9 @@ class Queue(mixins._LoopBoundMixin):
         By default, gets will only raise once the queue is empty. Set
         'immediate' to True to make gets raise immediately instead.
 
-        All blocked callers of put() will be unblocked, and also get()
-        and join() if 'immediate'.
+        All blocked callers of put() and get() will be unblocked. If
+        'immediate', a task is marked as done for each item remaining in
+        the queue, which may unblock callers of join().
         """
         self._is_shutdown = True
         if immediate:
