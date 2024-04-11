1397e91d094cc9735d5f7282edc17c6f09962f3d
Raymond Hettinger <raymond.hettinger@gmail.com>
2024-04-11 03:17:40 -0500

    .

--- Lib/statistics.py
+++ Lib/statistics.py
@@ -1730,17 +1730,17 @@ def _simple_s_curve(power):
                       1 - (2 - 2*p) ** power)
 
 _parabolic_invcdf = _newton_raphson(
-    f_inv_estimate = _simple_s_curve(0.583367470424302),
+    f_inv_estimate = _simple_s_curve(0.583367470424302),  # (7/12)
     f = lambda t: -1/4 * t**3 + 3/4 * t + 1/2,
     f_prime = lambda t: 3/4 * (1.0 - t * t))
 
 _quartic_invcdf = _newton_raphson(
-    f_inv_estimate = _simple_s_curve(0.4258865685331),
+    f_inv_estimate = _simple_s_curve(0.4258865685331),   # (204/479)
     f = lambda t: 3/16 * t**5 - 5/8 * t**3 + 15/16 * t + 1/2,
     f_prime = lambda t: 15/16 * (1.0 - t * t) ** 2)
 
 _triweight_invcdf = _newton_raphson(
-    f_inv_estimate = _simple_s_curve(0.3400218741872791),
+    f_inv_estimate = _simple_s_curve(0.3400218741872791), (17/50)
     f = lambda t: 35/32 * (-1/7*t**7 + 3/5*t**5 - t**3 + t) + 1/2,
     f_prime = lambda t: 35/32 * (1.0 - t * t) ** 3)
 
