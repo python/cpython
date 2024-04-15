45be1581263bff1758e2abcaad6432c261fd90a4
furkanonder <furkanonder@protonmail.com>
2024-04-16 02:08:52 +0300

    Sync-up docstring for min_mag function

--- Lib/_pydecimal.py
+++ Lib/_pydecimal.py
@@ -5089,16 +5089,16 @@ class Context(object):
     def min_mag(self, a, b):
         """Compares the values numerically with their sign ignored.
 
-        >>> ExtendedContext.min_mag(Decimal('3'), Decimal('-2'))
-        Decimal('-2')
-        >>> ExtendedContext.min_mag(Decimal('-3'), Decimal('NaN'))
-        Decimal('-3')
-        >>> ExtendedContext.min_mag(1, -2)
-        Decimal('1')
-        >>> ExtendedContext.min_mag(Decimal(1), -2)
-        Decimal('1')
-        >>> ExtendedContext.min_mag(1, Decimal(-2))
-        Decimal('1')
+            >>> ExtendedContext.min_mag(Decimal('3'), Decimal('-2'))
+            Decimal('-2')
+            >>> ExtendedContext.min_mag(Decimal('-3'), Decimal('NaN'))
+            Decimal('-3')
+            >>> ExtendedContext.min_mag(1, -2)
+            Decimal('1')
+            >>> ExtendedContext.min_mag(Decimal(1), -2)
+            Decimal('1')
+            >>> ExtendedContext.min_mag(1, Decimal(-2))
+            Decimal('1')
         """
         a = _convert_other(a, raiseit=True)
         return a.min_mag(b, context=self)
