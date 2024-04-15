1fd52d204bb19d395a9be5fa1cf851d0c9987790
furkanonder <furkanonder@protonmail.com>
2024-04-16 02:05:20 +0300

    Sync-up docstring for logical_invert function

--- Lib/_pydecimal.py
+++ Lib/_pydecimal.py
@@ -4946,16 +4946,16 @@ class Context(object):
 
         The operand must be a logical number.
 
-        >>> ExtendedContext.logical_invert(Decimal('0'))
-        Decimal('111111111')
-        >>> ExtendedContext.logical_invert(Decimal('1'))
-        Decimal('111111110')
-        >>> ExtendedContext.logical_invert(Decimal('111111111'))
-        Decimal('0')
-        >>> ExtendedContext.logical_invert(Decimal('101010101'))
-        Decimal('10101010')
-        >>> ExtendedContext.logical_invert(1101)
-        Decimal('111110010')
+            >>> ExtendedContext.logical_invert(Decimal('0'))
+            Decimal('111111111')
+            >>> ExtendedContext.logical_invert(Decimal('1'))
+            Decimal('111111110')
+            >>> ExtendedContext.logical_invert(Decimal('111111111'))
+            Decimal('0')
+            >>> ExtendedContext.logical_invert(Decimal('101010101'))
+            Decimal('10101010')
+            >>> ExtendedContext.logical_invert(1101)
+            Decimal('111110010')
         """
         a = _convert_other(a, raiseit=True)
         return a.logical_invert(context=self)
