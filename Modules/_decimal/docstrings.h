d76c6dccb3c61d20e9edb126e8c6cf98e8a65302
Furkan Onder <furkanonder@protonmail.com>
2024-04-15 21:40:26 +0300

    Use the imperative mood
    Co-authored-by: Erlend E. Aasland <erlend.aasland@protonmail.com>

--- Modules/_decimal/docstrings.h
+++ Modules/_decimal/docstrings.h
@@ -209,7 +209,7 @@ Defined as exp + digits - 1.\n\
 
 PyDoc_STRVAR(doc_as_tuple,
 "as_tuple($self, /)\n--\n\n\
-Represents the number as a triple tuple.\n\
+Represent the number as a triple tuple.\n\
 \n\
 To show the internals exactly as they are.\n\
 \n");
