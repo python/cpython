6b134834ba0021d41df70dc845bf117034fee62e
furkanonder <furkanonder@protonmail.com>
2024-04-16 02:07:52 +0300

    Sync-up docstring for max_mag function

--- Lib/_pydecimal.py
+++ Lib/_pydecimal.py
@@ -5045,16 +5045,16 @@ class Context(object):
         """Similar to the max() method, but the comparison is done using the absolute
         values of the operands.
 
-        >>> ExtendedContext.max_mag(Decimal('7'), Decimal('NaN'))
-        Decimal('7')
-        >>> ExtendedContext.max_mag(Decimal('7'), Decimal('-10'))
-        Decimal('-10')
-        >>> ExtendedContext.max_mag(1, -2)
-        Decimal('-2')
-        >>> ExtendedContext.max_mag(Decimal(1), -2)
-        Decimal('-2')
-        >>> ExtendedContext.max_mag(1, Decimal(-2))
-        Decimal('-2')
+            >>> ExtendedContext.max_mag(Decimal('7'), Decimal('NaN'))
+            Decimal('7')
+            >>> ExtendedContext.max_mag(Decimal('7'), Decimal('-10'))
+            Decimal('-10')
+            >>> ExtendedContext.max_mag(1, -2)
+            Decimal('-2')
+            >>> ExtendedContext.max_mag(Decimal(1), -2)
+            Decimal('-2')
+            >>> ExtendedContext.max_mag(1, Decimal(-2))
+            Decimal('-2')
         """
         a = _convert_other(a, raiseit=True)
         return a.max_mag(b, context=self)
