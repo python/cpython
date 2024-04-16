11d450cc80dc77025459b26b9b916c19fbd1e6ec
Dino Viehland <dinoviehland@fb.com>
2024-04-16 16:46:29 -0700

    Cleanup _PyObject_SetManagedDict, use cst for inline valid flag, fix issue w/ deleted dict

--- Include/internal/pycore_pyatomic_ft_wrappers.h
+++ Include/internal/pycore_pyatomic_ft_wrappers.h
@@ -21,6 +21,7 @@ extern "C" {
 
 #ifdef Py_GIL_DISABLED
 #define FT_ATOMIC_LOAD_PTR(value) _Py_atomic_load_ptr(&value)
+#define FT_ATOMIC_STORE_PTR(value, new_value) _Py_atomic_store_ptr(&value, new_value)
 #define FT_ATOMIC_LOAD_SSIZE(value) _Py_atomic_load_ssize(&value)
 #define FT_ATOMIC_LOAD_SSIZE_ACQUIRE(value) \
     _Py_atomic_load_ssize_acquire(&value)
@@ -34,10 +35,10 @@ extern "C" {
     _Py_atomic_load_uintptr_acquire(&value)
 #define FT_ATOMIC_LOAD_PTR_RELAXED(value) \
     _Py_atomic_load_ptr_relaxed(&value)
-#define FT_ATOMIC_LOAD_UINT8_RELAXED(value) \
-    _Py_atomic_load_uint8_relaxed(&value)
-#define FT_ATOMIC_STORE_UINT8_RELAXED(value, new_value) \
-    _Py_atomic_store_uint8_relaxed(&value, new_value)
+#define FT_ATOMIC_LOAD_UINT8(value) \
+    _Py_atomic_load_uint8(&value)
+#define FT_ATOMIC_STORE_UINT8(value, new_value) \
+    _Py_atomic_store_uint8(&value, new_value)
 #define FT_ATOMIC_STORE_PTR_RELAXED(value, new_value) \
     _Py_atomic_store_ptr_relaxed(&value, new_value)
 #define FT_ATOMIC_STORE_PTR_RELEASE(value, new_value) \
@@ -51,6 +52,7 @@ extern "C" {
 
 #else
 #define FT_ATOMIC_LOAD_PTR(value) value
+#define FT_ATOMIC_STORE_PTR(value, new_value) value = new_value
 #define FT_ATOMIC_LOAD_SSIZE(value) value
 #define FT_ATOMIC_LOAD_SSIZE_ACQUIRE(value) value
 #define FT_ATOMIC_LOAD_SSIZE_RELAXED(value) value
@@ -58,8 +60,8 @@ extern "C" {
 #define FT_ATOMIC_LOAD_PTR_ACQUIRE(value) value
 #define FT_ATOMIC_LOAD_UINTPTR_ACQUIRE(value) value
 #define FT_ATOMIC_LOAD_PTR_RELAXED(value) value
-#define FT_ATOMIC_LOAD_UINT8_RELAXED(value) value
-#define FT_ATOMIC_STORE_UINT8_RELAXED(value, new_value) value = new_value
+#define FT_ATOMIC_LOAD_UINT8(value) value
+#define FT_ATOMIC_STORE_UINT8(value, new_value) value = new_value
 #define FT_ATOMIC_STORE_PTR_RELAXED(value, new_value) value = new_value
 #define FT_ATOMIC_STORE_PTR_RELEASE(value, new_value) value = new_value
 #define FT_ATOMIC_STORE_UINTPTR_RELEASE(value, new_value) value = new_value
