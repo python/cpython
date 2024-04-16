a5b578cef19802650e6fbf89d1bacae0ae886ea9
Serhiy Storchaka <storchaka@gmail.com>
2024-04-16 22:26:02 +0300

    Fix more cases.

--- Lib/email/_header_value_parser.py
+++ Lib/email/_header_value_parser.py
@@ -1519,11 +1519,13 @@ def get_obs_local_part(value):
             "expected obs-local-part but found '{}'".format(value))
     if (obs_local_part[0].token_type == 'dot' or
             obs_local_part[0].token_type=='cfws' and
+            len(obs_local_part) > 1 and
             obs_local_part[1].token_type=='dot'):
         obs_local_part.defects.append(errors.InvalidHeaderDefect(
             "Invalid leading '.' in local part"))
     if (obs_local_part[-1].token_type == 'dot' or
             obs_local_part[-1].token_type=='cfws' and
+            len(obs_local_part) > 1 and
             obs_local_part[-2].token_type=='dot'):
         obs_local_part.defects.append(errors.InvalidHeaderDefect(
             "Invalid trailing '.' in local part"))
