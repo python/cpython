c8b531dc07184f80c12fc26e8c937428dba18d15
Donghee Na <donghee.na@python.org>
2024-04-18 01:46:25 +0900

    nit

--- Modules/_pickle.c
+++ Modules/_pickle.c
@@ -3415,11 +3415,11 @@ save_set(PickleState *state, PicklerObject *self, PyObject *obj)
         if (_Pickler_Write(self, &mark_op, 1) < 0)
             return -1;
 
-        int err;
+        int err = 0;
         Py_BEGIN_CRITICAL_SECTION(obj);
         while (_PySet_NextEntry(obj, &ppos, &item, &hash)) {
             Py_INCREF(item);
-            int err = save(state, self, item, 0);
+            err = save(state, self, item, 0);
             Py_CLEAR(item);
             if (err < 0)
                 break;
