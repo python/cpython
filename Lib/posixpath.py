4e502a4997af4c8042a6ac13115a3f8ba31520ea
Serhiy Storchaka <storchaka@gmail.com>
2024-04-17 12:58:19 +0300

    gh-117394: Speed up os.path.ismount() on Posix (GH-117447)
    It is now 2-3 times faster if the user has permissions.

--- Lib/posixpath.py
+++ Lib/posixpath.py
@@ -206,11 +206,14 @@ def ismount(path):
         parent = join(path, b'..')
     else:
         parent = join(path, '..')
-    parent = realpath(parent)
     try:
         s2 = os.lstat(parent)
-    except (OSError, ValueError):
-        return False
+    except OSError:
+        parent = realpath(parent)
+        try:
+            s2 = os.lstat(parent)
+        except OSError:
+            return False
 
     # path/.. on a different device as path or the same i-node as path
     return s1.st_dev != s2.st_dev or s1.st_ino == s2.st_ino
