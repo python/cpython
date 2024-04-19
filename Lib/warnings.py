bbb1a8e7686353e1ae848ee77453195d1a6373e4
Tian Gao <gaogaotiantian@hotmail.com>
2024-04-18 21:07:05 -0700

    [3.12] gh-117535: Ignore made up file name "sys" for warnings (#118014)

--- Lib/warnings.py
+++ Lib/warnings.py
@@ -36,7 +36,9 @@ def _formatwarnmsg_impl(msg):
     category = msg.category.__name__
     s =  f"{msg.filename}:{msg.lineno}: {category}: {msg.message}\n"
 
-    if msg.line is None:
+    # "sys" is a made up file name when we are not able to get the frame
+    # so do not try to get the source line
+    if msg.line is None and msg.filename != "sys":
         try:
             import linecache
             line = linecache.getline(msg.filename, msg.lineno)
