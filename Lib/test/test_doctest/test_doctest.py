e3671ead9419c7afab6a4e501ade86bc9bc10950
Nikita Sobolev <mail@sobolevn.me>
2024-04-21 10:41:54 +0300

    [3.12] gh-118121: Fix `test_doctest.test_look_in_unwrapped` (#118122) (#118129)

--- Lib/test/test_doctest/test_doctest.py
+++ Lib/test/test_doctest/test_doctest.py
@@ -2488,12 +2488,27 @@ class Wrapper:
         self.func(*args, **kwargs)
 
 @Wrapper
-def test_look_in_unwrapped():
+def wrapped():
     """
     Docstrings in wrapped functions must be detected as well.
 
     >>> 'one other test'
     'one other test'
+    """
+
+def test_look_in_unwrapped():
+    """
+    Ensure that wrapped doctests work correctly.
+
+    >>> import doctest
+    >>> doctest.run_docstring_examples(
+    ...     wrapped, {}, name=wrapped.__name__, verbose=True)
+    Finding tests in wrapped
+    Trying:
+        'one other test'
+    Expecting:
+        'one other test'
+    ok
     """
 
 if support.check_impl_detail(cpython=True):
