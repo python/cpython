ea81a4cc795320b08c50bbe72698c614d90d6bc0
Sam Gross <colesbury@gmail.com>
2024-04-26 17:14:23 +0000

    Adjust formatting

--- Python/pystate.c
+++ Python/pystate.c
@@ -1569,7 +1569,9 @@ new_threadstate(PyInterpreterState *interp, int whence)
     }
     else {
 #ifdef Py_GIL_DISABLED
-        if (interp->gc.immortalize.enable_on_thread_created && !interp->gc.immortalize.enabled) {
+        if (interp->gc.immortalize.enable_on_thread_created &&
+            !interp->gc.immortalize.enabled)
+        {
             // Immortalize objects marked as using deferred reference counting
             // the first time a non-main thread is created.
             _PyGC_ImmortalizeDeferredObjects(interp);
