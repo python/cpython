71a203af16e283dfadae216494a5d9f30c538332
Mark Shannon <mark@hotpy.org>
2024-04-29 10:21:01 +0100

    _PyOpcode_Deopt isn't available to the JIT

--- Python/generated_cases.c.h
+++ Python/generated_cases.c.h
@@ -6035,10 +6035,12 @@
             gen_frame->previous = NULL;
             /* We don't know which of these is relevant here, so keep them equal */
             assert(INLINE_CACHE_ENTRIES_SEND == INLINE_CACHE_ENTRIES_FOR_ITER);
+            #if TIER_ONE
             assert(_PyOpcode_Deopt[frame->instr_ptr->op.code] == SEND ||
                    _PyOpcode_Deopt[frame->instr_ptr->op.code] == FOR_ITER ||
                    _PyOpcode_Deopt[frame->instr_ptr->op.code] == INTERPRETER_EXIT ||
                    _PyOpcode_Deopt[frame->instr_ptr->op.code] == ENTER_EXECUTOR);
+            #endif
             LOAD_IP(1 + INLINE_CACHE_ENTRIES_SEND);
             LOAD_SP();
             value = retval;
