8b94283b14f56c8f5156c8403f257e6c53a2fca8
mpage <mpage@meta.com>
2024-04-29 09:56:51 -0700

    gh-118331: Don't raise an error if tuple allocation fails when clearing weakrefs (#118338)
    It's not safe to raise an exception in `PyObject_ClearWeakRefs()` if one
    is not already set, since it may be called by `_Py_Dealloc()`, which
    requires that the active exception does not change.
    
    Additionally, make sure we clear the weakrefs even when tuple allocation
    fails.

--- Lib/test/test_weakref.py
+++ Lib/test/test_weakref.py
@@ -10,6 +10,7 @@ import copy
 import threading
 import time
 import random
+import textwrap
 
 from test import support
 from test.support import script_helper, ALWAYS_EQ
@@ -1008,6 +1009,31 @@ class ReferencesTestCase(TestBase):
         ref1 = weakref.ref(x, lambda ref: support.gc_collect())
         del x
         support.gc_collect()
+
+    @support.cpython_only
+    def test_no_memory_when_clearing(self):
+        # gh-118331: Make sure we do not raise an exception from the destructor
+        # when clearing weakrefs if allocating the intermediate tuple fails.
+        code = textwrap.dedent("""
+        import _testcapi
+        import weakref
+
+        class TestObj:
+            pass
+
+        def callback(obj):
+            pass
+
+        obj = TestObj()
+        # The choice of 50 is arbitrary, but must be large enough to ensure
+        # the allocation won't be serviced by the free list.
+        wrs = [weakref.ref(obj, callback) for _ in range(50)]
+        _testcapi.set_nomemory(0)
+        del obj
+        """).strip()
+        res, _ = script_helper.run_python_until_end("-c", code)
+        stderr = res.err.decode("ascii", "backslashreplace")
+        self.assertNotRegex(stderr, "_Py_Dealloc: Deallocator of type 'TestObj'")
 
 
 class SubclassableWeakrefTestCase(TestBase):
