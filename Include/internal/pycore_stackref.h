6bf6d440142b8226d92d2a4b71dc748cc3ab4bb6
Ken Jin <28750310+Fidget-Spinner@users.noreply.github.com>
2024-04-30 21:23:20 +0800

    try fix Windows for real this time
    Co-Authored-By: Sam Gross <655866+colesbury@users.noreply.github.com>

--- Include/internal/pycore_stackref.h
+++ Include/internal/pycore_stackref.h
@@ -30,7 +30,7 @@ typedef union {
     uintptr_t bits;
 } _PyStackRef;
 
-static const _PyStackRef Py_STACKREF_NULL = ((_PyStackRef) { .bits = 0 });
+static const _PyStackRef Py_STACKREF_NULL = { .bits = 0 };
 
 #ifdef Py_GIL_DISABLED
     #define Py_TAG_DEFERRED (1)
