f7febe188cf93ca7687008fa34ca2637e4b03e6c
Ned Deily <nad@python.org>
2024-05-02 18:00:10 -0400

    fix library_recipes for mpdecimal

--- Mac/BuildScript/build-installer.py
+++ Mac/BuildScript/build-installer.py
@@ -382,11 +382,9 @@ def library_recipes():
               name="libmpdec 2.5.1",
               url="https://www.bytereef.org/software/mpdecimal/releases/mpdecimal-2.5.1.tar.gz",
               checksum="9f9cd4c041f99b5c49ffb7b59d9f12d95b683d88585608aa56a6307667b2b21f",
-              extra_cflags=(
-                  "MACHINE=universal",
-              ),
               configure_pre=[
                   "--disable-cxx",
+                  "MACHINE=universal",
               ]
           ),
         ])
