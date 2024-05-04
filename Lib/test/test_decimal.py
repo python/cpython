a73b81138f2e5c3b89d887f757f7d9f6797aed26
Tim Peters <tim.peters@gmail.com>
2024-05-03 23:45:17 -0500

    Fixed a comment, and fleshed out an existing test that appeared unfinished.

--- Lib/test/test_decimal.py
+++ Lib/test/test_decimal.py
@@ -4716,12 +4716,23 @@ class PyWhitebox(unittest.TestCase):
 
             c.prec = 1
             x = Decimal("152587890625") ** Decimal('-0.5')
+            self.assertEqual(x, Decimal('3e-6'))
+            c.prec = 2
+            x = Decimal("152587890625") ** Decimal('-0.5')
+            self.assertEqual(x, Decimal('2.6e-6'))
+            c.prec = 3
+            x = Decimal("152587890625") ** Decimal('-0.5')
+            self.assertEqual(x, Decimal('2.56e-6'))
+            c.prec = 28
+            x = Decimal("152587890625") ** Decimal('-0.5')
+            self.assertEqual(x, Decimal('2.56e-6'))
+
             c.prec = 201
             x = Decimal(2**578) ** Decimal("-0.5")
 
             # See https://github.com/python/cpython/issues/118027
             # Testing for an exact power could appear to hang, in the Python
-            # version, as it attempted to compute 10**MAX_EMAX.
+            # version, as it attempted to compute 10**(MAX_EMAX + 1).
             # Fixed via https://github.com/python/cpython/pull/118503.
             c.prec = P.MAX_PREC
             c.Emax = P.MAX_EMAX
