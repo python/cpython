e84f9434f3c4b239bdcae5a334a19983cfee033e
Lysandros Nikolaou <lisandrosnik@gmail.com>
2024-05-06 19:20:14 +0200

    Mock tcgetattr as well

--- Lib/test/test_pyrepl.py
+++ Lib/test/test_pyrepl.py
@@ -988,6 +988,39 @@ TERM_CAPABILITIES = {
     lambda s, *args: s + b":" + b",".join(str(i).encode() for i in args),
 )
 @patch("_pyrepl.curses.setupterm", lambda a, b: None)
+@patch(
+    "termios.tcgetattr",
+    lambda _: [
+        27394,
+        3,
+        19200,
+        536872399,
+        38400,
+        38400,
+        [
+            b"\x04",
+            b"\xff",
+            b"\xff",
+            b"\x7f",
+            b"\x17",
+            b"\x15",
+            b"\x12",
+            b"\x00",
+            b"\x03",
+            b"\x1c",
+            b"\x1a",
+            b"\x19",
+            b"\x11",
+            b"\x13",
+            b"\x16",
+            b"\x0f",
+            b"\x01",
+            b"\x00",
+            b"\x14",
+            b"\x00",
+        ],
+    ],
+)
 @patch("termios.tcsetattr", lambda a, b, c: None)
 @patch("os.write")
 class TestConsole(TestCase):
