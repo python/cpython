c7125c9a62bb3b21ac0576b0fee4aadcb1e693b8
Mark Shannon <mark@hotpy.org>
2024-05-06 19:25:39 +0100

    #define away _Py_Executors_InvalidateDependency in non-tier2 build.

--- Python/optimizer.c
+++ Python/optimizer.c
@@ -1,7 +1,6 @@
-#include "Python.h"
-
 #ifdef _Py_TIER2
 
+#include "Python.h"
 #include "opcode.h"
 #include "pycore_interp.h"
 #include "pycore_backoff.h"
@@ -1720,16 +1719,6 @@ _Py_Executors_InvalidateAll(PyInterpreterState *interp, int is_invalidation)
             OPT_STAT_INC(executors_invalidated);
         }
     }
-}
-
-#else
-
-void
-_Py_Executors_InvalidateDependency(PyInterpreterState *interp, void *obj, int is_invalidation)
-{
-    (void)interp;
-    (void)obj;
-    (void)is_invalidation;
 }
 
 #endif /* _Py_TIER2 */
