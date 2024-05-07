5f6e72897a1e077e35a32e0739ae41d2e73f1bd9
Nikita Sobolev <mail@sobolevn.me>
2024-05-07 15:43:18 +0300

    Remove several unused imports in `_pyrepl` (#118668)

--- Lib/_pyrepl/reader.py
+++ Lib/_pyrepl/reader.py
@@ -23,7 +23,6 @@ from __future__ import annotations
 
 from contextlib import contextmanager
 from dataclasses import dataclass, field, fields
-import re
 import unicodedata
 from _colorize import can_colorize, ANSIColors  # type: ignore[import-not-found]
 
