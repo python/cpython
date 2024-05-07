6d5311ec4854f45cff5b8cccc5ab5ef4a38c120c
Eric Snow <ericsnowcurrently@gmail.com>
2024-05-06 21:40:58 -0600

    Disable all interpreters tests under Py_GIL_DISABLED.

--- Lib/test/test_interpreters/test_lifecycle.py
+++ Lib/test/test_interpreters/test_lifecycle.py
@@ -115,7 +115,6 @@ class StartupTests(TestBase):
         self.assertEqual(proc.stderr, '')
         return proc.stdout
 
-    @support.requires_gil_enabled
     def test_sys_path_0(self):
         # The main interpreter's sys.path[0] should be used by subinterpreters.
         script = '''
@@ -165,7 +164,6 @@ class StartupTests(TestBase):
 
 class FinalizationTests(TestBase):
 
-    @support.requires_gil_enabled
     @support.requires_subprocess()
     def test_gh_109793(self):
         # Make sure finalization finishes and the correct error code
