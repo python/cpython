048342f88bb1d1311eaa3b2edcc78d65417c1885
Eric Snow <ericsnowcurrently@gmail.com>
2024-05-06 21:25:44 -0600

    Fix the tests.

--- Lib/test/test_interpreters/test_lifecycle.py
+++ Lib/test/test_interpreters/test_lifecycle.py
@@ -115,7 +115,7 @@ class StartupTests(TestBase):
         self.assertEqual(proc.stderr, '')
         return proc.stdout
 
-    @requires_gil_enabled
+    @support.requires_gil_enabled
     def test_sys_path_0(self):
         # The main interpreter's sys.path[0] should be used by subinterpreters.
         script = '''
@@ -165,7 +165,7 @@ class StartupTests(TestBase):
 
 class FinalizationTests(TestBase):
 
-    @requires_gil_enabled
+    @support.requires_gil_enabled
     @support.requires_subprocess()
     def test_gh_109793(self):
         # Make sure finalization finishes and the correct error code
