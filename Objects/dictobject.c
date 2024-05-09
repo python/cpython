65a3b80df571ec7fbea1613eef8db930b053be11
Nikita Sobolev <mail@sobolevn.me>
2024-05-09 20:15:14 +0300

    gh-118849: Fix "code will never be executed" warning in `dictobject.c` (#118850)

--- Objects/dictobject.c
+++ Objects/dictobject.c
@@ -5396,6 +5396,7 @@ static int
 dictiter_iternext_threadsafe(PyDictObject *d, PyObject *self,
                              PyObject **out_key, PyObject **out_value)
 {
+    int res;
     dictiterobject *di = (dictiterobject *)self;
     Py_ssize_t i;
     PyDictKeysObject *k;
@@ -5491,7 +5492,6 @@ fail:
     Py_DECREF(d);
     return -1;
 
-    int res;
 try_locked:
     Py_BEGIN_CRITICAL_SECTION(d);
     res = dictiter_iternextitem_lock_held(d, self, out_key, out_value);
