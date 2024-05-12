9e2170c76ef56a18061e4e01d66254db9776ca73
Daniel Hollas <daniel.hollas@bristol.ac.uk>
2024-05-12 15:56:24 +0100

    WIP: Add test
    This test does not work yet :-(

--- Lib/test/test_pyrepl.py
+++ Lib/test/test_pyrepl.py
@@ -607,7 +607,7 @@ class TestPyReplCompleter(TestCase):
         output = multiline_input(reader, namespace)
         self.assertEqual(output, "python")
 
-    def test_updown_arrow_with_completion_menu(self):
+    def test_up_down_arrows_with_completion_menu(self):
         """Up arrow in the middle of unfinished tab completion when the menu is displayed
         should work and trigger going back in history. Down arrow should subsequently
         get us back to the incomplete command."""
@@ -632,6 +632,29 @@ class TestPyReplCompleter(TestCase):
         output = multiline_input(reader, namespace)
         self.assertEqual(output, "os.")
 
+
+    def test_right_down_arrows_with_completion_menu(self):
+        """Right / Down arrows while the tab completion menu is displayed
+        should do nothing"""
+        code = "os.\t\t"
+        namespace = {"os": os}
+
+        events = itertools.chain(
+            code_to_events(code),
+            [
+                Event(evt="key", data="down", raw=bytearray(b"\x1bOB")),
+                Event(evt="key", data="right", raw=bytearray(b"\x1bOC")),
+            ],
+            code_to_events("\n")
+        )
+        reader = self.prepare_reader(events, namespace=namespace)
+        output = multiline_input(reader, namespace)
+        self.assertEqual(output, "os.")
+        # When we press right and/or down arrow while
+        # the completions menu is displayed,
+        # the cursor should stay where it was on the line.
+        self.assertEqual(reader.cmpltn_menu_vis, 1)
+        self.assertEqual(reader.cxy, (6, 0))
 
 @patch("_pyrepl.curses.tigetstr", lambda x: b"")
 class TestUnivEventQueue(TestCase):
