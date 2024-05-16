ad85b834dda671a8ff7942258cece9275bb0817b
Serhiy Storchaka <storchaka@gmail.com>
2024-05-16 17:34:30 +0300

    gh-118643: Fix AttributeError in the email module
    Fix regression introduced in gh-100884: AttributeError when re-fold a long
    address list.
    
    Also fix more cases of incorrect encoding of the address separator in the
    address list missed in gh-100884.

--- Lib/email/_header_value_parser.py
+++ Lib/email/_header_value_parser.py
@@ -956,6 +956,7 @@ class _InvalidEwError(errors.HeaderParseError):
 DOT = ValueTerminal('.', 'dot')
 ListSeparator = ValueTerminal(',', 'list-separator')
 ListSeparator.as_ew_allowed = False
+ListSeparator.syntactic_break = False
 RouteComponentMarker = ValueTerminal('@', 'route-component-marker')
 
 #
@@ -2838,7 +2839,9 @@ def _refold_parse_tree(parse_tree, *, policy):
             if not hasattr(part, 'encode'):
                 # It's not a Terminal, do each piece individually.
                 parts = list(part) + parts
-            else:
+                want_encoding = False
+                continue
+            elif part.as_ew_allowed:
                 # It's a terminal, wrap it as an encoded word, possibly
                 # combining it with previously encoded words if allowed.
                 if (last_ew is not None and
@@ -2849,8 +2852,14 @@ def _refold_parse_tree(parse_tree, *, policy):
                 last_ew = _fold_as_ew(tstr, lines, maxlen, last_ew,
                                       part.ew_combine_allowed, charset)
                 last_charset = charset
-            want_encoding = False
-            continue
+                want_encoding = False
+                continue
+            else:
+                # It's a terminal which should be kept non-encoded
+                # (e.g. a ListSeparator).
+                last_ew = None
+                want_encoding = False
+                pass
         if len(tstr) <= maxlen - len(lines[-1]):
             lines[-1] += tstr
             continue
