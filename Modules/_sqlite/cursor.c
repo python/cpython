4bde862e026dbcdddbdb10f1fb75d92b07f559e6
Erlend E. Aasland <erlend@python.org>
2024-05-19 23:07:06 -0400

    gh-118928: sqlite3: correctly bail if sequences of params are used with named placeholders
    Fix up post gh-118929.

--- Modules/_sqlite/cursor.c
+++ Modules/_sqlite/cursor.c
@@ -670,7 +670,7 @@ bind_parameters(pysqlite_state *state, pysqlite_Statement *self,
         for (i = 0; i < num_params; i++) {
             const char *name = sqlite3_bind_parameter_name(self->st, i+1);
             if (name != NULL && name[0] != '?') {
-                PyErr_Format(state->ProgrammingError,
+                return PyErr_Format(state->ProgrammingError,
                         "Binding %d ('%s') is a named parameter, but you "
                         "supplied a sequence which requires nameless (qmark) "
                         "placeholders.",
