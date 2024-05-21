eb4b00a65dedb6cddff8f410ce1617243de620aa
Victor Stinner <vstinner@python.org>
2024-05-21 19:54:41 +0200

    Use False on ImportError

--- Lib/site.py
+++ Lib/site.py
@@ -527,7 +527,7 @@ def register_readline():
                 # _pyrepl.__main__ is executed as the __main__ module
                 from __main__ import CAN_USE_PYREPL
             except ImportError:
-                CAN_USE_PYREPL = True
+                CAN_USE_PYREPL = False
 
             try:
                 if os.getenv("PYTHON_BASIC_REPL") or not CAN_USE_PYREPL:
