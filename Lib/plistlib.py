6969658543a73ec7f81a37d5ae9f27952d9c531b
Serhiy Storchaka <storchaka@gmail.com>
2024-05-21 20:40:25 +0300

    gh-119342: Fix OOM vulnerability in plistlib
    Reading a specially prepared small Plist file could cause OOM because file's
    read(n) preallocates a bytes object for reading the specified amount of
    data. Now plistlib reads large data by chunks, therefore the upper limit of
    consumed memory is proportional to the size of the input file.

--- Lib/plistlib.py
+++ Lib/plistlib.py
@@ -508,12 +508,31 @@ class _BinaryPlistParser:
 
         return tokenL
 
+    def _read(self, size):
+        if 0:
+            data = self._fp.read(size)
+            if len(data) != size:
+                raise InvalidFileException
+            return data
+        cursize = min(size, 1 << 20)
+        data = self._fp.read(cursize)
+        while True:
+            if len(data) != cursize:
+                raise InvalidFileException
+            if cursize == size:
+                return data
+            delta = min(cursize, size - cursize)
+            data += self._fp.read(delta)
+            cursize += delta
+
     def _read_ints(self, n, size):
-        data = self._fp.read(size * n)
+        data = self._read(size * n)
+        #print('XXX', n, size)
+        #assert n < 100
         if size in _BINARY_FORMAT:
             return struct.unpack(f'>{n}{_BINARY_FORMAT[size]}', data)
         else:
-            if not size or len(data) != size * n:
+            if not size:
                 raise InvalidFileException()
             return tuple(int.from_bytes(data[i: i + size], 'big')
                          for i in range(0, size * n, size))
@@ -573,22 +592,16 @@ class _BinaryPlistParser:
 
         elif tokenH == 0x40:  # data
             s = self._get_size(tokenL)
-            result = self._fp.read(s)
-            if len(result) != s:
-                raise InvalidFileException()
+            result = self._read(s)
 
         elif tokenH == 0x50:  # ascii string
             s = self._get_size(tokenL)
-            data = self._fp.read(s)
-            if len(data) != s:
-                raise InvalidFileException()
+            data = self._read(s)
             result = data.decode('ascii')
 
         elif tokenH == 0x60:  # unicode string
             s = self._get_size(tokenL) * 2
-            data = self._fp.read(s)
-            if len(data) != s:
-                raise InvalidFileException()
+            data = self._read(s)
             result = data.decode('utf-16be')
 
         elif tokenH == 0x80:  # UID
