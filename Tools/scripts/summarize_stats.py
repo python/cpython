6ed4e91683b98f31048bc0b5b4515e70ae476b16
Jeff Glass <glass.jeffrey@gmail.com>
2024-05-21 15:48:34 -0400

    Pre-commit fixes

--- Tools/scripts/summarize_stats.py
+++ Tools/scripts/summarize_stats.py
@@ -1508,7 +1508,7 @@ def main():
 
 """ TESTS (Run with pytest) """
 
-def test_opcode_input_overlap():    
+def test_opcode_input_overlap():
     opcode_flags = {
         'CODE_ONE': ['HAS_OPARG_AND_1_FLAG', 'HAS_OPERAND_FLAG', 'HAS_JUMP_FLAG'],
         'CODE_TWO': ['HAS_OPERAND_FLAG', 'HAS_JUMP_FLAG'],
@@ -1518,7 +1518,7 @@ def test_opcode_input_overlap():
     assert opcode_input_overlap(opcode_flags, 'CODE_ONE', 'CODE_TWO') == 'Conflict. Both Use: Operand,Target'
     assert opcode_input_overlap(opcode_flags, 'CODE_ONE', 'CODE_THREE') == 'Conflict. Both Use: Target'
     assert opcode_input_overlap(opcode_flags, 'CODE_ONE', 'CODE_FOUR') == 'No Conflict'
-    
+
 
 if __name__ == "__main__":
     main()
