ba7b94287840c0c0fa9adbe796fda85cec659a47
Eugene Triguba <eugenetriguba@gmail.com>
2024-05-21 13:06:33 -0400

    Add blurb

--- Lib/site.py
+++ Lib/site.py
@@ -523,9 +523,9 @@ def register_readline():
             pass
 
         def write_history():
-            from _pyrepl import env
+            from _pyrepl.env import CAN_USE_PYREPL
             try:
-                if os.getenv("PYTHON_BASIC_REPL") or not env.CAN_USE_PYREPL:
+                if os.getenv("PYTHON_BASIC_REPL") or not CAN_USE_PYREPL:
                     readline.write_history_file(history)
                 else:
                     _pyrepl.readline.write_history_file(history)
