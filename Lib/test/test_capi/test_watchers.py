53ab26294ced214a434bd254ac0e4388e78e6dc9
Saul Shanabrook <s.shanabrook@gmail.com>
2024-05-22 10:14:02 -0400

    Update type watcher ID in test bc we know have a default one at 0

--- Lib/test/test_capi/test_watchers.py
+++ Lib/test/test_capi/test_watchers.py
@@ -282,8 +282,10 @@ class TestTypeWatchers(unittest.TestCase):
             self.watch(wid, C)
             with catch_unraisable_exception() as cm:
                 C.foo = "bar"
-                self.assertEqual(cm.unraisable.err_msg,
-                    f"Exception ignored in type watcher callback #0 for {C!r}")
+                self.assertEqual(
+                    cm.unraisable.err_msg,
+                    f"Exception ignored in type watcher callback #1 for {C!r}",
+                )
                 self.assertIs(cm.unraisable.object, None)
                 self.assertEqual(str(cm.unraisable.exc_value), "boom!")
             self.assert_events([])
