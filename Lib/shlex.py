5be56a065161687211243d0399a74b01cc7439a3
jb2170 <email@jb2170.com>
2024-05-28 17:38:17 +0100

    Simplify existing control flow logic of shlex.quote
    Removes early return for the special case of an empty string

--- Lib/shlex.py
+++ Lib/shlex.py
@@ -322,15 +322,12 @@ _find_unsafe = re.compile(r'[^\w@%+=:,./-]', re.ASCII).search
 
 def quote(s):
     """Return a shell-escaped version of the string *s*."""
-    if not s:
-        return "''"
-    if _find_unsafe(s) is None:
-        return s
+    if not s or _find_unsafe(s) is not None:
+        # use single quotes, and put single quotes into double quotes
+        # the string $'b is then quoted as '$'"'"'b'
+        return "'" + s.replace("'", "'\"'\"'") + "'"
 
-    # use single quotes, and put single quotes into double quotes
-    # the string $'b is then quoted as '$'"'"'b'
-    return "'" + s.replace("'", "'\"'\"'") + "'"
-
+    return s
 
 def _print_tokens(lexer):
     while tt := lexer.get_token():
