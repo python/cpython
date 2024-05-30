48f21b3631eb20871fe234e9714b19aa76cf3a49
Petr Viktorin <encukou@gmail.com>
2024-05-30 09:27:32 +0200

    gh-118235: Move RAISE_SYNTAX_ERROR actions to invalid rules and make sure they stay there (GH-119731)
    The Full Grammar specification in the docs omits rule actions, so grammar rules that raise a syntax error looked like valid syntax.
    This was solved in ef940de by hiding those rules in the custom syntax highlighter.
    
    This moves all syntax-error alternatives to invalid rules, adds a validator that ensures that actions containing RAISE_SYNTAX_ERROR are in invalid rules, and reverts the syntax highlighter hack.

--- Lib/test/test_peg_generator/test_grammar_validator.py
+++ Lib/test/test_peg_generator/test_grammar_validator.py
@@ -4,7 +4,7 @@ from test import test_tools
 test_tools.skip_if_missing("peg_generator")
 with test_tools.imports_under_tool("peg_generator"):
     from pegen.grammar_parser import GeneratedParser as GrammarParser
-    from pegen.validator import SubRuleValidator, ValidationError
+    from pegen.validator import SubRuleValidator, ValidationError, RaiseRuleValidator
     from pegen.testutil import parse_string
     from pegen.grammar import Grammar
 
@@ -46,6 +46,16 @@ class TestPegen(unittest.TestCase):
         """
         grammar: Grammar = parse_string(grammar_source, GrammarParser)
         validator = SubRuleValidator(grammar)
+        with self.assertRaises(ValidationError):
+            for rule_name, rule in grammar.rules.items():
+                validator.validate_rule(rule_name, rule)
+
+    def test_raising_valid_rule(self) -> None:
+        grammar_source = """
+        start: NAME { RAISE_SYNTAX_ERROR("this is not allowed") }
+        """
+        grammar: Grammar = parse_string(grammar_source, GrammarParser)
+        validator = RaiseRuleValidator(grammar)
         with self.assertRaises(ValidationError):
             for rule_name, rule in grammar.rules.items():
                 validator.validate_rule(rule_name, rule)
