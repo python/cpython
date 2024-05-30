121bb1b3b9f04c3a3ebf2086e38b5e33293509a7
sobolevn <mail@sobolevn.me>
2024-05-30 12:52:39 +0300

    gh-119775: Remove ability to create immutable types with mutable bases

--- Objects/typeobject.c
+++ Objects/typeobject.c
@@ -4613,16 +4613,13 @@ _PyType_FromMetaclass_impl(
                 goto finally;
             }
             if (!_PyType_HasFeature(b, Py_TPFLAGS_IMMUTABLETYPE)) {
-                if (PyErr_WarnFormat(
-                    PyExc_DeprecationWarning,
-                    0,
-                    "Creating immutable type %s from mutable base %s is "
-                    "deprecated, and slated to be disallowed in Python 3.14.",
+                PyErr_Format(
+                    PyExc_TypeError,
+                    "Creating immutable type %s from mutable base %s",
                     spec->name,
-                    b->tp_name))
-                {
-                    goto finally;
-                }
+                    b->tp_name
+                );
+                goto finally;
             }
         }
     }
