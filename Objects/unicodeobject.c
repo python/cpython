ff6eea201b3d135bf479634834fafea81ffb62c6
Nineteendo <nineteendo19d0@gmail.com>
2024-06-02 23:07:55 +0200

    Try to prevent segmentation fault

--- Objects/unicodeobject.c
+++ Objects/unicodeobject.c
@@ -9346,7 +9346,10 @@ any_find_first_slice(PyObject *strobj, const char *function_name,
 exit:
     if (heap_subs) {
         for (Py_ssize_t i = 0; i < subs_len; i++) {
-            PyMem_Free((void *)heap_subs[i]);
+            void *heap_sub = heap_subs[i];
+            if (heap_sub) {
+                PyMem_Free(heap_sub);
+            }
         }
     }
     PyMem_RawFree(heap_subs);
