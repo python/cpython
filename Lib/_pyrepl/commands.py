25f8d80a5db91620e96e3b4e09fc42a608f14091
Lysandros Nikolaou <lisandrosnik@gmail.com>
2024-06-04 19:26:44 +0200

    gh-120041: Refactor check for visible completion menu in completing_reader (#120055)

--- Lib/_pyrepl/commands.py
+++ Lib/_pyrepl/commands.py
@@ -365,12 +365,7 @@ class self_insert(EditCommand):
         r = self.reader
         text = self.event * r.get_arg()
         r.insert(text)
-        if (
-            len(text) == 1 and
-            r.pos == len(r.buffer) and
-            not r.cmpltn_menu_visible and  # type: ignore[attr-defined]
-            not r.cmpltn_message_visible  # type: ignore[attr-defined]
-        ):
+        if len(text) == 1 and r.pos == len(r.buffer):
             r.calc_screen = r.append_to_screen
 
 
