9af3a743efc5237f6cb2b3b22d7e656c2f475b22
blhsing <blhsing@gmail.com>
2024-06-05 15:34:54 +0800

    Update testthreadingmock.py

--- Lib/test/test_unittest/testmock/testthreadingmock.py
+++ Lib/test/test_unittest/testmock/testthreadingmock.py
@@ -3,7 +3,7 @@ import unittest
 import concurrent.futures
 
 from test.support import threading_helper
-from unittest.mock import patch, ThreadingMock
+from unittest.mock import patch, ThreadingMock, MagicMock
 
 
 threading_helper.requires_working_threading(module=True)
