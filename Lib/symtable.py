bbfdcb73bc80d46367a78a0db20c288771d74dd3
Bénédikt Tran <10796600+picnixz@users.noreply.github.com>
2024-06-05 09:35:16 +0200

    Use enumeration members instead of `.value`.

--- Lib/symtable.py
+++ Lib/symtable.py
@@ -81,19 +81,19 @@ class SymbolTable:
         the ``SymbolTableType`` enumeration.
         """
         if self._table.type == _symtable.TYPE_MODULE:
-            return SymbolTableType.MODULE.value
+            return SymbolTableType.MODULE
         if self._table.type == _symtable.TYPE_FUNCTION:
-            return SymbolTableType.FUNCTION.value
+            return SymbolTableType.FUNCTION
         if self._table.type == _symtable.TYPE_CLASS:
-            return SymbolTableType.CLASS.value
+            return SymbolTableType.CLASS
         if self._table.type == _symtable.TYPE_ANNOTATION:
-            return SymbolTableType.ANNOTATION.value
+            return SymbolTableType.ANNOTATION
         if self._table.type == _symtable.TYPE_TYPE_ALIAS:
-            return SymbolTableType.TYPE_ALIAS.value
+            return SymbolTableType.TYPE_ALIAS
         if self._table.type == _symtable.TYPE_TYPE_PARAMETERS:
-            return SymbolTableType.TYPE_PARAMETERS.value
+            return SymbolTableType.TYPE_PARAMETERS
         if self._table.type == _symtable.TYPE_TYPE_VARIABLE:
-            return SymbolTableType.TYPE_VARIABLE.value
+            return SymbolTableType.TYPE_VARIABLE
         assert False, f"unexpected type: {self._table.type}"
 
     def get_id(self):
