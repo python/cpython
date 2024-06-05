4e9740d95d24d39a2b7c410c568be97dac5319cb
Ken Jin <28750310+Fidget-Spinner@users.noreply.github.com>
2024-06-05 20:09:38 +0800

    Revert "use macros instead of static inline for most things"
    This reverts commit 4a5ae8c5d2af1b0f351f338e62b9e3392ffc14d2.

--- Include/internal/pycore_stackref.h
+++ Include/internal/pycore_stackref.h
@@ -47,40 +47,41 @@ PyStackRef_IsDeferred(_PyStackRef ref)
     return ((ref.bits & Py_TAG) == Py_TAG_DEFERRED);
 }
 
-#ifdef Py_GIL_DISABLED
 // Gets a PyObject * from a _PyStackRef
 static inline PyObject *
 PyStackRef_AsPyObjectBorrow(_PyStackRef tagged)
 {
+#ifdef Py_GIL_DISABLED
     PyObject *cleared = ((PyObject *)((tagged).bits & (~Py_TAG)));
     return cleared;
-}
 #else
-// Need to define as macro because some platforms have very sensitive stack sizes.
-#   define PyStackRef_AsPyObjectBorrow(tagged) ((PyObject *)(tagged).bits)
+    assert((tagged.bits & Py_TAG) == 0);
+    return (PyObject *)tagged.bits;
 #endif
+}
 
 // Converts a PyObject * to a PyStackRef, stealing the reference
 static inline _PyStackRef
 _PyStackRef_FromPyObjectSteal(PyObject *obj)
 {
+#ifdef Py_GIL_DISABLED
     // Make sure we don't take an already tagged value.
     assert(((uintptr_t)obj & Py_TAG) == 0);
     int tag = (obj == NULL || _Py_IsImmortal(obj)) ? (Py_TAG_DEFERRED) : Py_TAG_PTR;
     return ((_PyStackRef){.bits = ((uintptr_t)(obj)) | tag});
-}
-
-
-#ifdef Py_GIL_DISABLED
-#   define PyStackRef_FromPyObjectSteal(obj) _PyStackRef_FromPyObjectSteal(_PyObject_CAST(obj))
 #else
-#   define PyStackRef_FromPyObjectSteal(obj) ((_PyStackRef){.bits = ((uintptr_t)(obj))})
+    // Make sure we don't take an already tagged value.
+    assert(((uintptr_t)obj & Py_TAG) == 0);
+    return ((_PyStackRef){.bits = ((uintptr_t)(obj))});
 #endif
+}
+#define PyStackRef_FromPyObjectSteal(obj) _PyStackRef_FromPyObjectSteal(_PyObject_CAST(obj))
 
 // Converts a PyObject * to a PyStackRef, with a new reference
 static inline _PyStackRef
 PyStackRef_FromPyObjectNew(PyObject *obj)
 {
+#ifdef Py_GIL_DISABLED
     // Make sure we don't take an already tagged value.
     assert(((uintptr_t)obj & Py_TAG) == 0);
     assert(obj != NULL);
@@ -91,29 +92,26 @@ PyStackRef_FromPyObjectNew(PyObject *obj)
     else {
         return (_PyStackRef){ .bits = (uintptr_t)(Py_NewRef(obj)) | Py_TAG_PTR };
     }
-}
-
-#ifdef Py_GIL_DISABLED
-#   define PyStackRef_FromPyObjectNew(obj) PyStackRef_FromPyObjectNew(_PyObject_CAST(obj))
 #else
-#   define PyStackRef_FromPyObjectNew(obj) ((_PyStackRef){ .bits = (uintptr_t)(Py_NewRef(obj)) })
+    return (_PyStackRef){ .bits = (uintptr_t)(Py_NewRef(obj)) };
 #endif
+}
+#define PyStackRef_FromPyObjectNew(obj) PyStackRef_FromPyObjectNew(_PyObject_CAST(obj))
 
-#ifdef Py_GIL_DISABLED
+
 // Converts a PyStackRef back to a PyObject *, converting deferred references
 // to new references.
 static inline PyObject *
 PyStackRef_AsPyObjectNew(_PyStackRef tagged)
 {
+#ifdef Py_GIL_DISABLED
     if (!PyStackRef_IsNull(tagged) && PyStackRef_IsDeferred(tagged)) {
         assert(_Py_IsImmortal(PyStackRef_AsPyObjectBorrow(tagged)));
         return Py_NewRef(PyStackRef_AsPyObjectBorrow(tagged));
     }
+#endif
     return PyStackRef_AsPyObjectBorrow(tagged);
 }
-#else
-#   define PyStackRef_AsPyObjectNew(tagged) PyStackRef_AsPyObjectBorrow(tagged)
-#endif
 
 static inline void
 _Py_untag_stack_borrowed(PyObject **dst, const _PyStackRef *src, size_t length)
@@ -158,20 +156,18 @@ _Py_untag_stack_steal(PyObject **dst, const _PyStackRef *src, size_t length)
         } \
     } while (0)
 
-#ifdef Py_GIL_DISABLED
 static inline void
 PyStackRef_CLOSE(_PyStackRef tagged)
 {
+#ifdef Py_GIL_DISABLED
     if (PyStackRef_IsDeferred(tagged)) {
         // No assert for being immortal or deferred here.
         // The GC unsets deferred objects right before clearing.
         return;
     }
+#endif
     Py_DECREF(PyStackRef_AsPyObjectBorrow(tagged));
 }
-#else
-#define PyStackRef_CLOSE(tagged) Py_DECREF(PyStackRef_AsPyObjectBorrow(tagged))
-#endif
 
 static inline void
 PyStackRef_XCLOSE(_PyStackRef tagged)
