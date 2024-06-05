c49cf1cc454df334fca7824f1fc267f39c2e96c4
Ken Jin <28750310+Fidget-Spinner@users.noreply.github.com>
2024-06-05 20:15:10 +0800

    double WASI stack size of pydebug

--- Tools/wasm/wasi.py
+++ Tools/wasm/wasi.py
@@ -280,9 +280,9 @@ def main():
     default_host_runner = (f"{shutil.which('wasmtime')} run "
                         # Make sure the stack size will work for a pydebug
                         # build.
-                        # The 8388608 value comes from `ulimit -s` under Linux
-                        # which equates to 8291 KiB.
-                        "--wasm max-wasm-stack=8388608 "
+                        # The 8388608 value comes from double of `ulimit -s` under Linux
+                        # which equates to 8291*2 KiB.
+                        "--wasm max-wasm-stack=16777216 "
                         # Use WASI 0.2 primitives.
                         "--wasi preview2 "
                         # Enable thread support; causes use of preview1.
