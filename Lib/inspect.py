1f89b78f6809fa2df44c59383faa9017376e6061
Ben Hsing <blhsing@gmail.com>
2024-06-06 03:54:56 +0000

    gh-70764: getclosurevars now identifies global variables from LOAD_GLOBAL instructions instead

--- Lib/inspect.py
+++ Lib/inspect.py
@@ -1593,18 +1593,19 @@ def getclosurevars(func):
     global_vars = {}
     builtin_vars = {}
     unbound_names = set()
-    for name in code.co_names:
-        if name in ("None", "True", "False"):
-            # Because these used to be builtins instead of keywords, they
-            # may still show up as name references. We ignore them.
-            continue
-        try:
-            global_vars[name] = global_ns[name]
-        except KeyError:
+    for instruction in dis.get_instructions(code):
+        opname = instruction.opname
+        name = instruction.argval
+        if opname == "LOAD_GLOBAL":
             try:
-                builtin_vars[name] = builtin_ns[name]
+                global_vars[name] = global_ns[name]
             except KeyError:
-                unbound_names.add(name)
+                try:
+                    builtin_vars[name] = builtin_ns[name]
+                except KeyError:
+                    unbound_names.add(name)
+        elif opname == "LOAD_ATTR":
+            unbound_names.add(name)
 
     return ClosureVars(nonlocal_vars, global_vars,
                        builtin_vars, unbound_names)
