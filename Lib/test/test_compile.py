866002e7d2bf65ec209f019da9d32e1b9488193a
Irit Katriel <1055913+iritkatriel@users.noreply.github.com>
2024-06-07 22:37:35 +0100

    gh-120225: fix crash in compiler on empty block at end of exception handler (#120235)

--- Lib/test/test_compile.py
+++ Lib/test/test_compile.py
@@ -1409,6 +1409,16 @@ class TestSpecifics(unittest.TestCase):
         for kw in ("except", "except*"):
             exec(code % kw, g, l);
 
+    def test_regression_gh_120225(self):
+        async def name_4():
+            match b'':
+                case True:
+                    pass
+                case name_5 if f'e':
+                    {name_3: name_4 async for name_2 in name_5}
+                case []:
+                    pass
+            [[]]
 
 @requires_debug_ranges()
 class TestSourcePositions(unittest.TestCase):
