084608bea5e84015747b627626d31e8bcbc58072
Ken Jin <28750310+Fidget-Spinner@users.noreply.github.com>
2024-06-07 18:07:27 +0800

    Make PyType_HasFeature atomic

--- Include/internal/pycore_object.h
+++ Include/internal/pycore_object.h
@@ -262,7 +262,7 @@ extern int _PyTraceMalloc_TraceRef(PyObject *op, PyRefTracerEvent event, void*);
 // Fast inlined version of PyType_HasFeature()
 static inline int
 _PyType_HasFeature(PyTypeObject *type, unsigned long feature) {
-    return ((type->tp_flags & feature) != 0);
+    return ((FT_ATOMIC_LOAD_ULONG_RELAXED(type->tp_flags) & feature) != 0);
 }
 
 extern void _PyType_InitCache(PyInterpreterState *interp);
