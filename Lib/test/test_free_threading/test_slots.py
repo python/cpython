737eb09212029ac639e8e1501566043490782f59
parmeggiani <parmeggiani@spaziodati.eu>
2024-06-07 18:30:59 +0200

    remove commented code

--- Lib/test/test_free_threading/test_slots.py
+++ Lib/test/test_free_threading/test_slots.py
@@ -3,9 +3,6 @@ from test.support import threading_helper
 from unittest import TestCase
 
 
-# import _testcapi
-
-
 def run_in_threads(targets):
     """Run `targets` in separate threads"""
     threads = [
@@ -24,8 +21,7 @@ class TestSlots(TestCase):
     def test_object(self):
         class Spam:
             __slots__ = [
-                "eggs",  # the Py_T_OBJECT_EX type is missing in
-                         # Modules/_testcapi/structmember.c
+                "eggs",
             ]
 
             def __init__(self, initial_value):
@@ -45,21 +41,3 @@ class TestSlots(TestCase):
                 assert 0 <= eggs <= iters
 
         run_in_threads([writer, reader, reader, reader])
-
-    # def test_bool(self):
-    #     spam_old = _testcapi._test_structmembersType_OldAPI()
-    #     spam_new = _testcapi._test_structmembersType_NewAPI()
-    #
-    #     def writer():
-    #         for _ in range(1_000):
-    #             spam_old.T_BOOL = True
-    #             spam_old.T_BOOL = False  # separate code path for True/False
-    #             spam_new.T_BOOL = True
-    #             spam_new.T_BOOL = False
-    #
-    #     def reader():
-    #         for _ in range(1_000):
-    #             spam_old.T_BOOL
-    #             spam_new.T_BOOL
-    #
-    #     run_in_threads([writer, reader, reader, reader])
