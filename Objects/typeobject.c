167cbc18ceea278dbef137e6363c36dbf6798ec1
Ken Jin <kenjin@python.org>
2024-06-12 03:10:23 +0800

    gh-120198: Fix race condition when editing __class__ with an audit hook active (GH-120195)

--- Objects/typeobject.c
+++ Objects/typeobject.c
@@ -6522,7 +6522,6 @@ differs:
 static int
 object_set_class(PyObject *self, PyObject *value, void *closure)
 {
-    PyTypeObject *oldto = Py_TYPE(self);
 
     if (value == NULL) {
         PyErr_SetString(PyExc_TypeError,
@@ -6541,6 +6540,8 @@ object_set_class(PyObject *self, PyObject *value, void *closure)
                     self, "__class__", value) < 0) {
         return -1;
     }
+
+    PyTypeObject *oldto = Py_TYPE(self);
 
     /* In versions of CPython prior to 3.5, the code in
        compatible_for_assignment was not set up to correctly check for memory
