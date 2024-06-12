08985ac6c19f73db9118654499212f158ec81157
Bénédikt Tran <10796600+picnixz@users.noreply.github.com>
2024-06-12 17:50:32 +0200

    add comments

--- Lib/test/_test_multiprocessing.py
+++ Lib/test/_test_multiprocessing.py
@@ -1333,13 +1333,17 @@ class _TestQueue(BaseTestCase):
         self.assertTrue(not_serializable_obj.on_queue_feeder_error_was_called)
 
     def test_closed_queue_empty_exceptions(self):
+        # Assert that checking the emptiness of an unused closed queue
+        # does not raise an OSError. The rationale is that q.close() is
+        # a no-op upon construction and becomes effective once the queue
+        # has been used (e.g., by calling q.put()).
         for q in multiprocessing.Queue(), multiprocessing.JoinableQueue():
             q.close()  # this is a no-op since the feeder thread is None
             q.join_thread()  # this is also a no-op
             self.assertTrue(q.empty())
 
         for q in multiprocessing.Queue(), multiprocessing.JoinableQueue():
-            q.put('foo')
+            q.put('foo')  # make sure that the queue is 'used'
             q.close()  # close the feeder thread
             q.join_thread()  # make sure to join the feeder thread
             with self.assertRaisesRegex(OSError, 'is closed'):
@@ -5829,6 +5833,9 @@ class TestSimpleQueue(unittest.TestCase):
             parent_can_continue.set()
 
     def test_empty_exceptions(self):
+        # Assert that checking emptiness of a closed queue raises
+        # an OSError, independently of whether the queue was used
+        # or not. This differs from Queue and JoinableQueue.
         q = multiprocessing.SimpleQueue()
         q.close()  # close the pipe
         with self.assertRaisesRegex(OSError, 'is closed'):
