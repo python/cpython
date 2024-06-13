c523386d1e6b11f03c18f777688f2d05be3398ee
Ken Jin <28750310+Fidget-Spinner@users.noreply.github.com>
2024-06-14 01:14:43 +0800

    try fix on msvc
    Co-Authored-By: Mark Shannon <9448417+markshannon@users.noreply.github.com>

--- Include/internal/pycore_stackref.h
+++ Include/internal/pycore_stackref.h
@@ -32,19 +32,17 @@ typedef union {
 
 
 #ifdef Py_GIL_DISABLED
-    static const _PyStackRef PyStackRef_True = {.bits = ((uintptr_t)&_Py_TrueStruct) +
-                                         Py_TAG_DEFERRED };
+    static const _PyStackRef PyStackRef_True = {.bits = (uintptr_t) (((char *)&_Py_TrueStruct) + Py_TAG_DEFERRED) };
 #else
-    static const _PyStackRef PyStackRef_True = {.bits = ((uintptr_t)Py_True)};
+    static const _PyStackRef PyStackRef_True = {.bits = ((uintptr_t)_Py_TrueStruct)};
 #endif
 
 
 
 #ifdef Py_GIL_DISABLED
-    static const _PyStackRef PyStackRef_False = {.bits = ((uintptr_t)&_Py_FalseStruct) +
-                                            Py_TAG_DEFERRED };
+    static const _PyStackRef PyStackRef_False = {.bits = (uintptr_t) (((char *)&_Py_FalseStruct) + Py_TAG_DEFERRED) };
 #else
-    static const _PyStackRef PyStackRef_False = {.bits = ((uintptr_t)Py_False)};
+    static const _PyStackRef PyStackRef_False = {.bits = ((uintptr_t)_Py_FalseStruct)};
 #endif
 
 
