fc66d1c7a57ea9bd4a5817a5beee61b9fe05bf2d
Ken Jin <28750310+Fidget-Spinner@users.noreply.github.com>
2024-06-14 20:04:56 +0800

    Change is_abstract to extract_bits

--- Tools/cases_generator/stack.py
+++ Tools/cases_generator/stack.py
@@ -125,7 +125,7 @@ class Stack:
         self.variables: list[StackItem] = []
         self.defined: set[str] = set()
 
-    def pop(self, var: StackItem, is_abstract: bool = False) -> str:
+    def pop(self, var: StackItem, extract_bits: bool = False) -> str:
         self.top_offset.pop(var)
         if not var.peek:
             self.peek_offset.pop(var)
@@ -155,7 +155,7 @@ class Stack:
         else:
             self.defined.add(var.name)
         cast = f"({var.type})" if (not indirect and var.type) else ""
-        bits = ".bits" if cast and not is_abstract else ""
+        bits = ".bits" if cast and not extract_bits else ""
         assign = (
             f"{var.name} = {cast}{indirect}stack_pointer[{self.base_offset.to_c()}]{bits};"
         )
@@ -179,12 +179,12 @@ class Stack:
             self.top_offset.push(var)
             return ""
 
-    def flush(self, out: CWriter, cast_type: str = "uintptr_t", is_abstract: bool = False) -> None:
+    def flush(self, out: CWriter, cast_type: str = "uintptr_t", extract_bits: bool = False) -> None:
         out.start_line()
         for var in self.variables:
             if not var.peek:
                 cast = f"({cast_type})" if var.type else ""
-                bits = ".bits" if cast and not is_abstract else ""
+                bits = ".bits" if cast and not extract_bits else ""
                 if var.name not in UNUSED and not var.is_array():
                     if var.condition:
                         if var.condition == "0":
