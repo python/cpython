e1ee78f571995a531d4d8221d3dae2db1bb160dc
Serhiy Storchaka <storchaka@gmail.com>
2024-06-15 20:56:40 +0300

    gh-120541: Improve the "less" prompt in pydoc (GH-120543)
    When help() is called with non-string argument, use __qualname__ or
    __name__ if available, otherwise use "{typename} object".
    (cherry picked from commit 31d1d72d7e24e0427df70f7dd14b9baff28a4f89)
    
    Co-authored-by: Serhiy Storchaka <storchaka@gmail.com>

--- Lib/pydoc.py
+++ Lib/pydoc.py
@@ -1753,7 +1753,14 @@ def doc(thing, title='Python Library Documentation: %s', forceload=0,
     """Display text documentation, given an object or a path to an object."""
     if output is None:
         try:
-            what = thing if isinstance(thing, str) else type(thing).__name__
+            if isinstance(thing, str):
+                what = thing
+            else:
+                what = getattr(thing, '__qualname__', None)
+                if not isinstance(what, str):
+                    what = getattr(thing, '__name__', None)
+                    if not isinstance(what, str):
+                        what = type(thing).__name__ + ' object'
             pager(render_doc(thing, title, forceload), f'Help on {what!s}')
         except ImportError as exc:
             if is_cli:
