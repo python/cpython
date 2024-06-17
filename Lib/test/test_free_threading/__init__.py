89cf80dc0814403f4a86bcb0fdff64ea2474faa6
Nice Zombies <nineteendo19d0@gmail.com>
2024-06-17 22:35:06 +0200

    Don't import `os` when not necessary

--- Lib/test/test_free_threading/__init__.py
+++ Lib/test/test_free_threading/__init__.py
@@ -1,8 +1,8 @@
-import os
-
 from test import support
 
 
 if support.Py_GIL_DISABLED:
+    import os
+
     def load_tests(*args):
         return support.load_package_tests(os.path.dirname(__file__), *args)
