41793d41d8af3ec86677c8081d9d99d100b4a848
Nice Zombies <nineteendo19d0@gmail.com>
2024-06-18 07:40:56 +0200

    Use `unittest.skipUnless`

--- Lib/test/test_free_threading/__init__.py
+++ Lib/test/test_free_threading/__init__.py
@@ -1,8 +1,9 @@
 import os
 
 from test import support
+from unittest import skipUnless
 
 
-@support.skipUnless(support.Py_GIL_DISABLED, "GIL enabled")
+@skipUnless(support.Py_GIL_DISABLED, "GIL enabled")
 def load_tests(*args):
     return support.load_package_tests(os.path.dirname(__file__), *args)
