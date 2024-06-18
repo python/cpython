465759aa980c03bd14f24a3f31e9127d0315eaa9
Nineteendo <nineteendo19d0@gmail.com>
2024-06-18 09:34:46 +0200

    Raise at module level
    Co-authored-by: Victor Stinner <vstinner@python.org>

--- Lib/test/test_free_threading/__init__.py
+++ Lib/test/test_free_threading/__init__.py
@@ -1,9 +1,11 @@
 import os
+import unittest
 
 from test import support
-from unittest import skipUnless
 
 
-@skipUnless(support.Py_GIL_DISABLED, "GIL enabled")
+if not support.Py_GIL_DISABLED:
+    raise unittest.SkipTest("GIL enabled")
+
 def load_tests(*args):
     return support.load_package_tests(os.path.dirname(__file__), *args)
