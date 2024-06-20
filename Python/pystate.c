4e8a976102fbf81a0b16a4c1b9c7d3e1fc799220
Eric Snow <ericsnowcurrently@gmail.com>
2024-06-20 14:52:59 -0600

    Add the option to not reuse the initial tstate.

--- Python/pystate.c
+++ Python/pystate.c
@@ -630,6 +630,10 @@ init_interpreter(PyInterpreterState *interp,
     assert(next != NULL || (interp == runtime->interpreters.main));
     interp->next = next;
 
+#ifdef Py_DEBUG
+    interp->threads.reuse_init_tstate = 1;
+#endif
+
     PyStatus status = _PyObject_InitState(interp);
     if (_PyStatus_EXCEPTION(status)) {
         return status;
@@ -1555,7 +1559,12 @@ new_threadstate(PyInterpreterState *interp, int whence)
 
     // Allocate the thread state and add it to the interpreter.
     PyThreadState *old_head = interp->threads.head;
-    if (old_head == NULL) {
+    if (old_head == NULL
+#ifdef Py_DEBUG
+        && interp->threads.reuse_init_tstate
+#endif
+        )
+    {
         // It's the interpreter's initial thread state.
         used_newtstate = 0;
         tstate = &interp->_initial_thread;
@@ -1564,7 +1573,7 @@ new_threadstate(PyInterpreterState *interp, int whence)
     else {
         // Every valid interpreter must have at least one thread.
         assert(id > 1);
-        assert(old_head->prev == NULL);
+        assert(old_head == NULL || old_head->prev == NULL);
         used_newtstate = 1;
         tstate = new_tstate;
         // Set to _PyThreadState_INIT.
