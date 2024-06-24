0764640aad77f1ca0862fb11ed242a97e92d6e90
Lysandros Nikolaou <lisandrosnik@gmail.com>
2024-06-24 17:23:01 +0200

    gh-120956: Avoid comparison of int to Py_ssize_t in parser

--- Tools/peg_generator/pegen/c_generator.py
+++ Tools/peg_generator/pegen/c_generator.py
@@ -645,7 +645,7 @@ class CParserGenerator(ParserGenerator, GrammarVisitor):
                 self.print("}")
             self.print("asdl_seq *_seq = (asdl_seq*)_Py_asdl_generic_seq_new(_n, p->arena);")
             self.out_of_memory_return(f"!_seq", cleanup_code="PyMem_Free(_children);")
-            self.print("for (int i = 0; i < _n; i++) asdl_seq_SET_UNTYPED(_seq, i, _children[i]);")
+            self.print("for (Py_ssize_t i = 0; i < _n; i++) asdl_seq_SET_UNTYPED(_seq, i, _children[i]);")
             self.print("PyMem_Free(_children);")
             if memoize and node.name:
                 self.print(f"_PyPegen_insert_memo(p, _start_mark, {node.name}_type, _seq);")
