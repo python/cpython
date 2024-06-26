f8557375d90f1448fc0ba71f616a95cee4232648
Bénédikt Tran <10796600+picnixz@users.noreply.github.com>
2024-06-26 12:48:12 +0200

    remove un-necessary test

--- Lib/test/test_io.py
+++ Lib/test/test_io.py
@@ -669,9 +669,6 @@ class IOTest(unittest.TestCase):
     def test_backreadline(self):
         self.skipTest("TODO")
 
-    def test_backreadline_nonsizeable(self):
-        self.skipTest("TODO")
-
     def test_raw_bytes_io(self):
         f = self.BytesIO()
         self.write_ops(f)
