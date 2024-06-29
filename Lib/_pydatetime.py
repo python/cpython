fcc7c7f8109cf1bebfbb9f60d05d80fa111635da
blhsing <blhsing@gmail.com>
2024-06-29 08:42:09 +0800

    Update Lib/_pydatetime.py
    Co-authored-by: Erlend E. Aasland <erlend.aasland@protonmail.com>

--- Lib/_pydatetime.py
+++ Lib/_pydatetime.py
@@ -273,8 +273,8 @@ def _wrap_strftime(object, format, timetuple):
                                 Zreplace = s.replace('%', '%%')
                     newformat.append(Zreplace)
                 elif ch in 'YG' and object.year < 1000 and _need_normalize_century():
-                    # note that datetime(1000, 1, 1).strftime('%G') == '1000' so
-                    # year 1000 for %G can go on the fast path
+                    # Note that datetime(1000, 1, 1).strftime('%G') == '1000' so
+                    # year 1000 for %G can go on the fast path.
                     if ch == 'G':
                         year = int(_time.strftime("%G", timetuple))
                     else:
