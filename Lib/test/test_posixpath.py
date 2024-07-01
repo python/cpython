00c7059f6229bfebfd55b2243b56d990322c1ebb
Victor Stinner <vstinner@python.org>
2024-07-01 11:43:59 +0200

    gh-121200: Log pwd entry in test_expanduser_pwd2() (GH-121207)
    Use subTest() to log the pwd entry in test_expanduser_pwd2() of
    test_posixpath to help debugging.
    (cherry picked from commit 05a6f8da6042cc87da1cd3824c1375d12753e5a1)
    
    Co-authored-by: Victor Stinner <vstinner@python.org>

--- Lib/test/test_posixpath.py
+++ Lib/test/test_posixpath.py
@@ -347,13 +347,14 @@ class PosixPathTest(unittest.TestCase):
                      "no home directory on VxWorks")
     def test_expanduser_pwd2(self):
         pwd = import_helper.import_module('pwd')
-        for e in pwd.getpwall():
-            name = e.pw_name
-            home = e.pw_dir
+        for entry in pwd.getpwall():
+            name = entry.pw_name
+            home = entry.pw_dir
             home = home.rstrip('/') or '/'
-            self.assertEqual(posixpath.expanduser('~' + name), home)
-            self.assertEqual(posixpath.expanduser(os.fsencode('~' + name)),
-                             os.fsencode(home))
+            with self.subTest(pwd=entry):
+                self.assertEqual(posixpath.expanduser('~' + name), home)
+                self.assertEqual(posixpath.expanduser(os.fsencode('~' + name)),
+                                 os.fsencode(home))
 
     NORMPATH_CASES = [
         ("", "."),
