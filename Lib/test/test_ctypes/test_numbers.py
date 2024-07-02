a8f25eaa5116e7c88941582729305ee120df82c6
Sergey B Kirpichev <skirpichev@gmail.com>
2024-07-02 11:04:26 +0300

    Apply suggestions from code review
    Co-authored-by: Victor Stinner <vstinner@python.org>

--- Lib/test/test_ctypes/test_numbers.py
+++ Lib/test/test_ctypes/test_numbers.py
@@ -165,7 +165,7 @@ class NumberTestCase(unittest.TestCase):
         for z in values:
             for t in [ctypes.c_double_complex, ctypes.c_float_complex,
                       ctypes.c_longdouble_complex]:
-                with self.subTest(z=z, t=t):
+                with self.subTest(z=z, type=t):
                     self.assertComplexesAreIdentical(z, t(z).value)
 
     def test_integers(self):
