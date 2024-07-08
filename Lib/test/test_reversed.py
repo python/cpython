081ba4082458607dfde8a64e113cfc0710f3f269
Pieter Eendebak <pieter.eendebak@gmail.com>
2024-07-08 21:33:42 +0200

    review comments: use for loop instead of comprehension

--- Lib/test/test_reversed.py
+++ Lib/test/test_reversed.py
@@ -7,11 +7,13 @@ from test.support import threading_helper
 @threading_helper.requires_working_threading()
 def test_reversed_threading(self):
     # Test reading out the iterator with multiple threads cannot corrupt
-    # the reversed iterator.
+    # the reversed iterator state.
     # The reversed iterator is not guaranteed to be thread safe
 
+    number_of_threads = 10
     size = 4_000
-    def work(r, output):
+
+    def work(r):
         while True:
             try:
                  l = r.__length_hint__()
@@ -19,14 +21,15 @@ def test_reversed_threading(self):
             except StopIteration:
                 break
             assert 0 <= l <= size
-    number_of_threads = 10
     x = tuple(range(size))
     r = reversed(x)
     worker_threads = []
     for ii in range(number_of_threads):
         worker_threads.append(Thread(target=work, args=[r]))
-    _ = [t.start() for t in worker_threads]
-    _ = [t.join() for t in worker_threads]
+    for t in worker_threads:
+        t.start()
+    for t in worker_threads:
+        t.join()
 
 if __name__ == "__main__":
     unittest.main()
