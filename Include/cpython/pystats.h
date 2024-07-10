e48b8ca834e0865b227260fa1ec3cee6bf42db45
Michael Droettboom <mdboom@gmail.com>
2024-07-10 11:33:04 -0400

    Update Include/cpython/pystats.h
    Co-authored-by: Peter Bierma <zintensitydev@gmail.com>

--- Include/cpython/pystats.h
+++ Include/cpython/pystats.h
@@ -118,7 +118,7 @@ typedef struct _optimization_stats {
     uint64_t recursive_call;
     uint64_t low_confidence;
     uint64_t executors_invalidated;
-    UOpStats opcode[PYSTATS_MAX_UOP_ID+1];
+    UOpStats opcode[PYSTATS_MAX_UOP_ID + 1];
     uint64_t unsupported_opcode[256];
     uint64_t trace_length_hist[_Py_UOP_HIST_SIZE];
     uint64_t trace_run_length_hist[_Py_UOP_HIST_SIZE];
