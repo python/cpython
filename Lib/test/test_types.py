f283c7ede3a506e3faf169ce71d2781b4390b12d
Miguel Liezun <muragnarokstaff@gmail.com>
2024-07-10 16:23:36 -0300

    Test types in subinterpreters Slot wrappers should behave the same in the main interpreter as well as in all subinterpreters.

--- Lib/test/test_types.py
+++ Lib/test/test_types.py
@@ -2345,5 +2345,40 @@ class FunctionTests(unittest.TestCase):
             )
 
 
+class SubinterpreterTests(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        global interpreters
+        try:
+            from test.support import interpreters
+        except ModuleNotFoundError:
+            raise unittest.Skip('subinterpreters required')
+        import test.support.interpreters.channels
+
+    @cpython_only
+    def test_slot_wrappers(self):
+        rch, sch = interpreters.channels.create()
+
+        # For now it's sufficient to check int.__str__.
+        # See https://github.com/python/cpython/issues/117482
+        # and https://github.com/python/cpython/pull/117660.
+        script = '''if True:
+            text = repr(int.__str__)
+            sch.send_nowait(text)
+            '''
+
+        exec(script)
+        expected = rch.recv()
+
+        interp = interpreters.create()
+        interp.exec('from test.support import interpreters')
+        interp.prepare_main(sch=sch)
+        interp.exec(script)
+        results = rch.recv()
+
+        self.assertEqual(results, expected)
+
+
 if __name__ == '__main__':
     unittest.main()
