b869b74b378ab3948b6acdb9293c412b74b8a36a
Eric Snow <ericsnowcurrently@gmail.com>
2024-07-10 14:24:49 -0600

    Reset the static type during finalization.

--- Objects/typeobject.c
+++ Objects/typeobject.c
@@ -269,7 +269,7 @@ managed_static_type_state_init(PyInterpreterState *interp, PyTypeObject *self,
 
 /* Reset the type's per-interpreter state.
    This basically undoes what managed_static_type_state_init() did. */
-static void
+static PyTypeObject *
 managed_static_type_state_clear(PyInterpreterState *interp, PyTypeObject *self,
                                 int isbuiltin, int final)
 {
@@ -277,6 +277,7 @@ managed_static_type_state_clear(PyInterpreterState *interp, PyTypeObject *self,
     size_t full_index = isbuiltin
         ? index
         : index + _Py_MAX_MANAGED_STATIC_BUILTIN_TYPES;
+    PyTypeObject *def = &_PyRuntime.types.managed_static.types[full_index].def;
 
     managed_static_type_state *state = isbuiltin
         ? &(interp->types.builtins.initialized[index])
@@ -312,6 +313,8 @@ managed_static_type_state_clear(PyInterpreterState *interp, PyTypeObject *self,
         }
         PyMutex_Unlock(&interp->types.mutex);
     }
+
+    return def;
 }
 
 static PyTypeObject *
@@ -5849,8 +5852,13 @@ fini_static_type(PyInterpreterState *interp, PyTypeObject *type,
     }
 
     _PyStaticType_ClearWeakRefs(interp, type);
-    managed_static_type_state_clear(interp, type, isbuiltin, final);
-    /* We leave _Py_TPFLAGS_STATIC_BUILTIN set on tp_flags. */
+    PyTypeObject *def =
+        managed_static_type_state_clear(interp, type, isbuiltin, final);
+    /* For now we exclude extension module types. */
+    if (final && isbuiltin) {
+        /* Restore the static type to it's (mostly) original values. */
+        memcpy(type, def, sizeof(PyTypeObject));
+    }
 }
 
 void
