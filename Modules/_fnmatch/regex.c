2ef61ad556f9afb3ce08aced5340d8af0eb14059
Bénédikt Tran <10796600+picnixz@users.noreply.github.com>
2024-07-12 12:08:52 +0200

    simplify implementation

--- Modules/_fnmatch/regex.c
+++ Modules/_fnmatch/regex.c
@@ -1,5 +1,3 @@
-#include "Python.h"
-
 #include "_fnmatchmodule.h" // for pre-declarations
 
 // ==== API implementation ====================================================
@@ -9,7 +7,6 @@ _Py_fnmatch_fnmatch(PyObject *matcher, PyObject *name)
 {
     // If 'name' is of incorrect type, it will be detected when calling
     // the matcher function (we emulate 're.compile(...).match(name)').
-    assert(PyCallable_Check(matcher));
     PyObject *match = PyObject_CallOneArg(matcher, name);
     if (match == NULL) {
         return -1;
@@ -22,40 +19,27 @@ _Py_fnmatch_fnmatch(PyObject *matcher, PyObject *name)
 PyObject *
 _Py_fnmatch_filter(PyObject *matcher, PyObject *names)
 {
-    assert(PyCallable_Check(matcher));
     PyObject *iter = PyObject_GetIter(names);
     if (iter == NULL) {
         return NULL;
     }
-
     PyObject *res = PyList_New(0);
     if (res == NULL) {
         Py_DECREF(iter);
         return NULL;
     }
-
     PyObject *name = NULL;
     while ((name = PyIter_Next(iter))) {
         int matching = _Py_fnmatch_fnmatch(matcher, name);
-        if (matching < 0) {
-            assert(PyErr_Occurred());
+        if (matching < 0 || (matching == 1 && PyList_Append(res, name) < 0)) {
             goto abort;
         }
-        if (matching == 1) {
-            if (PyList_Append(res, name) < 0) {
-                goto abort;
-            }
-        }
         Py_DECREF(name);
-        if (PyErr_Occurred()) {
-            goto error;
-        }
     }
     Py_DECREF(iter);
     return res;
 abort:
-    Py_XDECREF(name);
-error:
+    Py_DECREF(name);
     Py_DECREF(iter);
     Py_DECREF(res);
     return NULL;
@@ -64,18 +48,15 @@ error:
 PyObject *
 _Py_fnmatch_filter_normalized(PyObject *matcher, PyObject *names, PyObject *normcase)
 {
-    assert(PyCallable_Check(matcher));
     PyObject *iter = PyObject_GetIter(names);
     if (iter == NULL) {
         return NULL;
     }
-
     PyObject *res = PyList_New(0);
     if (res == NULL) {
         Py_DECREF(iter);
         return NULL;
     }
-
     PyObject *name = NULL;
     while ((name = PyIter_Next(iter))) {
         PyObject *normalized = PyObject_CallOneArg(normcase, name);
@@ -84,26 +65,16 @@ _Py_fnmatch_filter_normalized(PyObject *matcher, PyObject *names, PyObject *norm
         }
         int matching = _Py_fnmatch_fnmatch(matcher, normalized);
         Py_DECREF(normalized);
-        if (matching < 0) {
-            assert(PyErr_Occurred());
+        // add the non-normalized name if its normalization matches
+        if (matching < 0 || (matching == 1 && PyList_Append(res, name) < 0)) {
             goto abort;
         }
-        if (matching == 1) {
-            // add the non-normalized name if its normalization matches
-            if (PyList_Append(res, name) < 0) {
-                goto abort;
-            }
-        }
         Py_DECREF(name);
-        if (PyErr_Occurred()) {
-            goto error;
-        }
     }
     Py_DECREF(iter);
     return res;
 abort:
-    Py_XDECREF(name);
-error:
+    Py_DECREF(name);
     Py_DECREF(iter);
     Py_DECREF(res);
     return NULL;
