0678911f0e9c3acd94ec23cdbbd39b06044ffaa7
Tian Gao <gaogaotiantian@hotmail.com>
2024-07-13 11:06:24 -0700

    Change comments

--- Lib/test/test_pdb.py
+++ Lib/test/test_pdb.py
@@ -3391,9 +3391,9 @@ def bœr():
             resources.enter_context(patch('sys.stdout', stdout))
             # patch pdb.Pdb.set_trace() to avoid entering the debugger
             resources.enter_context(patch.object(pdb.Pdb, 'set_trace'))
-            # Because pdb.Pdb.set_trace() is patched, we need to manually
-            # clear _last_pdb_instance so a new instance with stdout redirected
-            # could be created when pdb.set_trace() is called.
+            # We need to manually clear pdb.Pdb._last_pdb_instance so a
+            # new instance with stdout redirected could be created when
+            # pdb.set_trace() is called.
             pdb.Pdb._last_pdb_instance = None
             pdb.set_trace(header=header)
         self.assertEqual(stdout.getvalue(), header + '\n')
