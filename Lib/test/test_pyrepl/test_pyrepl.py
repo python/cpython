aa9a3a39e9f6d5a194c578fef7812516cc7120ff
Daniel Hollas <daniel.hollas@bristol.ac.uk>
2024-07-14 23:28:17 +0100

    Remove test

--- Lib/test/test_pyrepl/test_pyrepl.py
+++ Lib/test/test_pyrepl/test_pyrepl.py
@@ -702,33 +702,6 @@ class TestPyReplCompleter(TestCase):
         # so we should end up where we were when we initiated tab completion.
         output = multiline_input(reader, namespace)
         self.assertEqual(output, "os.")
-        # TODO: The menu should be visible, but currently isn't?
-        # self.assertEqual(reader.cmpltn_menu_visible, True)
-
-    # TODO: This test doesn't seem to work as intended.
-    def test_right_down_arrows_with_completion_menu(self):
-        """Right / Down arrows while the tab completion menu is displayed
-        should do nothing"""
-        code = "os.\t\t"
-        namespace = {"os": os}
-
-        events = itertools.chain(
-            code_to_events(code),
-            [
-                Event(evt="key", data="down", raw=bytearray(b"\x1bOB")),
-                Event(evt="key", data="right", raw=bytearray(b"\x1bOC")),
-            ],
-            code_to_events("\n")
-        )
-        reader = self.prepare_reader(events, namespace=namespace)
-        output = multiline_input(reader, namespace)
-        self.assertEqual(output, "os.")
-        # When we press right and/or down arrow while
-        # the completions menu is displayed,
-        # the cursor should stay where it was on the line.
-        self.assertEqual(reader.cxy, (6, 0))
-        # TODO: The menu should be visible, but currently isn't?
-        # self.assertEqual(reader.cmpltn_menu_visible, True)
 
     @patch("_pyrepl.readline._ReadlineWrapper.get_reader")
     @patch("sys.stderr", new_callable=io.StringIO)
