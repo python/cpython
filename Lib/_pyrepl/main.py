a623107acc51297691aae52936935eb77a34a0ae
Łukasz Langa <lukasz@langa.pl>
2024-07-15 21:31:21 +0200

    nitpicks and clarifications

--- Lib/_pyrepl/main.py
+++ Lib/_pyrepl/main.py
@@ -1,31 +1,32 @@
+import errno
 import os
 import sys
 
-CAN_USE_PYREPL: bool
-if sys.platform != "win32":
-    CAN_USE_PYREPL = True
-else:
-    CAN_USE_PYREPL = sys.getwindowsversion().build >= 10586  # Windows 10 TH2
 
-FAIL_MSG = ""
+CAN_USE_PYREPL: bool
+FAIL_REASON: str
 try:
-    import errno
+    if sys.platform == "win32" and sys.getwindowsversion().build < 10586:
+        raise RuntimeError("Windows 10 TH2 or later required")
     if not os.isatty(sys.stdin.fileno()):
         raise OSError(errno.ENOTTY, "tty required", "stdin")
     from .simple_interact import check
     if err := check():
         raise RuntimeError(err)
 except Exception as e:
-    FAIL_MSG = f"warning: can't use pyrepl: {e}"
     CAN_USE_PYREPL = False
+    FAIL_REASON = f"warning: can't use pyrepl: {e}"
+else:
+    CAN_USE_PYREPL = True
+    FAIL_REASON = ""
 
 
 def interactive_console(mainmodule=None, quiet=False, pythonstartup=False):
     if not CAN_USE_PYREPL:
-        if not os.environ.get('PYTHON_BASIC_REPL', None):
+        if not os.environ.get('PYTHON_BASIC_REPL', None) and FAIL_REASON:
             from .trace import trace
-            trace(FAIL_MSG)
-            print(FAIL_MSG, file=sys.stderr)
+            trace(FAIL_REASON)
+            print(FAIL_REASON, file=sys.stderr)
         return sys._baserepl()
 
     if mainmodule:
@@ -35,6 +36,7 @@ def interactive_console(mainmodule=None, quiet=False, pythonstartup=False):
         namespace = __main__.__dict__
         namespace.pop("__pyrepl_interactive_console", None)
 
+    # sys._baserepl() above does this internally, we do it here
     startup_path = os.getenv("PYTHONSTARTUP")
     if pythonstartup and startup_path:
         import tokenize
