d513ade847d9d312b62a4956e65ba9124179a5d1
Serhiy Storchaka <storchaka@gmail.com>
2024-07-19 11:06:02 +0300

    [3.13] gh-121905: Consistently use "floating-point" instead of "floating point" (GH-121907) (cherry picked from commit 1a0c7b9ba48a2dffb70bb0c7327abae1d3e87356)
    Co-authored-by: Serhiy Storchaka <storchaka@gmail.com>

--- Objects/clinic/floatobject.c.h
+++ Objects/clinic/floatobject.c.h
@@ -197,7 +197,7 @@ PyDoc_STRVAR(float_new__doc__,
 "float(x=0, /)\n"
 "--\n"
 "\n"
-"Convert a string or number to a floating point number, if possible.");
+"Convert a string or number to a floating-point number, if possible.");
 
 static PyObject *
 float_new_impl(PyTypeObject *type, PyObject *x);
@@ -256,7 +256,7 @@ PyDoc_STRVAR(float___getformat____doc__,
 "It exists mainly to be used in Python\'s test suite.\n"
 "\n"
 "This function returns whichever of \'unknown\', \'IEEE, big-endian\' or \'IEEE,\n"
-"little-endian\' best describes the format of floating point numbers used by the\n"
+"little-endian\' best describes the format of floating-point numbers used by the\n"
 "C type named by typestr.");
 
 #define FLOAT___GETFORMAT___METHODDEF    \
@@ -318,4 +318,4 @@ float___format__(PyObject *self, PyObject *arg)
 exit:
     return return_value;
 }
-/*[clinic end generated code: output=c79743c8551c30d9 input=a9049054013a1b77]*/
+/*[clinic end generated code: output=d8bbcd83977d516f input=a9049054013a1b77]*/
