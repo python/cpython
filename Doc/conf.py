f0bebb308f9fbf6cf30fbb2c804f8cd9d8d9903f
Adam Turner <9087854+AA-Turner@users.noreply.github.com>
2024-07-20 14:44:43 +0100

    GH-121970: Modernise the patchlevel extension (GH-121995) (cherry picked from commit b7ad711fcb37dd001e6bf8466c9503eef6d20331)
    Co-authored-by: Adam Turner <9087854+AA-Turner@users.noreply.github.com>

--- Doc/conf.py
+++ Doc/conf.py
@@ -6,6 +6,7 @@
 # The contents of this file are pickled, so don't put values in the namespace
 # that aren't pickleable (module imports are okay, they're removed automatically).
 
+import importlib
 import os
 import sys
 import time
@@ -58,9 +59,8 @@ copyright = f"2001-{time.strftime('%Y')}, Python Software Foundation"
 
 # We look for the Include/patchlevel.h file in the current Python source tree
 # and replace the values accordingly.
-import patchlevel  # noqa: E402
-
-version, release = patchlevel.get_version_info()
+# See Doc/tools/extensions/patchlevel.py
+version, release = importlib.import_module('patchlevel').get_version_info()
 
 rst_epilog = f"""
 .. |python_version_literal| replace:: ``Python {version}``
