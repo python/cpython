0c0fc31105963a855053eea040ca05b44a6d050f
Nate Ohlson <nohlson@purdue.edu>
2024-07-26 17:40:02 -0500

    Enforce PEP8 on check_warnigns

--- Tools/build/check_warnings.py
+++ Tools/build/check_warnings.py
@@ -9,6 +9,7 @@ import re
 import sys
 from pathlib import Path
 
+
 def extract_warnings_from_compiler_output(compiler_output: str) -> list[dict]:
     """
     Extracts warnings from the compiler output when using -fdiagnostics-format=json
@@ -18,18 +19,26 @@ def extract_warnings_from_compiler_output(compiler_output: str) -> list[dict]:
     """
 
     # Regex to find json arrays at the top level of the file in the compiler output
-    json_arrays = re.findall(r'\[(?:[^\[\]]|\[(?:[^\[\]]|\[[^\[\]]*\])*\])*\]', compiler_output)
+    json_arrays = re.findall(
+        r"\[(?:[^\[\]]|\[(?:[^\[\]]|\[[^\[\]]*\])*\])*\]", compiler_output
+    )
     compiler_warnings = []
     for array in json_arrays:
         try:
             json_data = json.loads(array)
             json_objects_in_array = [entry for entry in json_data]
-            compiler_warnings.extend([entry for entry in json_objects_in_array
-                                       if entry.get('kind') == 'warning'])
+            compiler_warnings.extend(
+                [
+                    entry
+                    for entry in json_objects_in_array
+                    if entry.get("kind") == "warning"
+                ]
+            )
         except json.JSONDecodeError:
             continue  # Skip malformed JSON
 
     return compiler_warnings
+
 
 def get_warnings_by_file(warnings: list[dict]) -> dict[str, list[dict]]:
     """
@@ -37,17 +46,20 @@ def get_warnings_by_file(warnings: list[dict]) -> dict[str, list[dict]]:
     """
     warnings_by_file = {}
     for warning in warnings:
-        locations = warning['locations']
+        locations = warning["locations"]
         for location in locations:
-            for key in ['caret', 'start', 'end']:
+            for key in ["caret", "start", "end"]:
                 if key in location:
-                    file = location[key]['file']
-                    file = file.lstrip('./') # Remove leading current directory if present
+                    file = location[key]["file"]
+                    file = file.lstrip(
+                        "./"
+                    )  # Remove leading current directory if present
                     if file not in warnings_by_file:
                         warnings_by_file[file] = []
                     warnings_by_file[file].append(warning)
 
     return warnings_by_file
+
 
 def get_unexpected_warnings(
     warnings: list[dict],
@@ -71,6 +83,7 @@ def get_unexpected_warnings(
 
     return 0
 
+
 def get_unexpected_improvements(
     warnings: list[dict],
     files_with_expected_warnings: set[str],
@@ -93,31 +106,32 @@ def get_unexpected_improvements(
 
     return 0
 
+
 def main(argv: list[str] | None = None) -> int:
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "--compiler-output-file-path",
         type=str,
         required=True,
-        help="Path to the compiler output file"
+        help="Path to the compiler output file",
     )
     parser.add_argument(
         "--warning-ignore-file-path",
         type=str,
         required=True,
-        help="Path to the warning ignore file"
+        help="Path to the warning ignore file",
     )
     parser.add_argument(
         "--fail-on-regression",
         action="store_true",
         default=False,
-        help="Flag to fail if new warnings are found"
+        help="Flag to fail if new warnings are found",
     )
     parser.add_argument(
         "--fail-on-improvement",
         action="store_true",
         default=False,
-        help="Flag to fail if files that were expected to have warnings have no warnings"
+        help="Flag to fail if files that were expected to have warnings have no warnings",
     )
 
     args = parser.parse_args(argv)
@@ -126,34 +140,49 @@ def main(argv: list[str] | None = None) -> int:
 
     # Check that the compiler output file is a valid path
     if not Path(args.compiler_output_file_path).is_file():
-        print(f"Compiler output file does not exist: {args.compiler_output_file_path}")
+        print(
+            f"Compiler output file does not exist: {args.compiler_output_file_path}"
+        )
         return 1
 
     # Check that the warning ignore file is a valid path
     if not Path(args.warning_ignore_file_path).is_file():
-        print(f"Warning ignore file does not exist: {args.warning_ignore_file_path}")
+        print(
+            f"Warning ignore file does not exist: {args.warning_ignore_file_path}"
+        )
         return 1
 
     with Path(args.compiler_output_file_path).open(encoding="UTF-8") as f:
         compiler_output_file_contents = f.read()
 
-    with Path(args.warning_ignore_file_path).open(encoding="UTF-8") as clean_files:
+    with Path(args.warning_ignore_file_path).open(
+        encoding="UTF-8"
+    ) as clean_files:
         files_with_expected_warnings = {
             file.strip()
             for file in clean_files
             if file.strip() and not file.startswith("#")
         }
 
-    warnings = extract_warnings_from_compiler_output(compiler_output_file_contents)
+    warnings = extract_warnings_from_compiler_output(
+        compiler_output_file_contents
+    )
     files_with_warnings = get_warnings_by_file(warnings)
 
-    status = get_unexpected_warnings(warnings, files_with_expected_warnings, files_with_warnings)
-    if args.fail_on_regression: exit_code |= status
+    status = get_unexpected_warnings(
+        warnings, files_with_expected_warnings, files_with_warnings
+    )
+    if args.fail_on_regression:
+        exit_code |= status
 
-    status = get_unexpected_improvements(warnings, files_with_expected_warnings, files_with_warnings)
-    if args.fail_on_improvement: exit_code |= status
+    status = get_unexpected_improvements(
+        warnings, files_with_expected_warnings, files_with_warnings
+    )
+    if args.fail_on_improvement:
+        exit_code |= status
 
     return exit_code
+
 
 if __name__ == "__main__":
     sys.exit(main())
