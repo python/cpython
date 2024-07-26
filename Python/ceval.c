0c741e27b1f0d5efda9690717cc5f6b1b94bead6
Tushar Sadhwani <tushar.sadhwani000@gmail.com>
2024-07-26 14:56:52 +0530

    Update Python/ceval.c
    Co-authored-by: blhsing <blhsing@gmail.com>

--- Python/ceval.c
+++ Python/ceval.c
@@ -2165,10 +2165,7 @@ _PyEval_UnpackIterableStackRef(PyThreadState *tstate, _PyStackRef v_stackref,
         Py_DECREF(w);
 
         /* If v is a sequence or mapping, we can show its length in the error */
-        ll = PySequence_Size(v);
-        if (ll < 0) {
-            ll = PyMapping_Size(v);
-        }
+        ll = PyObject_Size(v);
         if (ll < 0) {
             _PyErr_Format(tstate, PyExc_ValueError,
                           "too many values to unpack (expected %d)",
