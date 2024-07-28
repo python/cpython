db88ff521f04a9bc8b4da8799456523104672666
Bénédikt Tran <10796600+picnixz@users.noreply.github.com>
2024-07-28 14:55:27 +0200

    update macros.h
    - add convenience macros
    - make some macros private

--- Modules/_fnmatch/macros.h
+++ Modules/_fnmatch/macros.h
@@ -8,6 +8,34 @@
 
 // ==== Macro definitions =====================================================
 
+/*
+ * Check that INTVAL is >= 0 or execute 'goto abort'.
+ *
+ * This macro is provided for convenience and should be
+ * carefully used if more resources should be released
+ * before jumping to the 'abort' label.
+ */
+#define CHECK_INTVAL_OR_ABORT(INTVAL)   \
+    do {                                \
+        if ((INTVAL) < 0) {             \
+            goto abort;                 \
+        }                               \
+    } while (0)
+
+/*
+ * Check that OBJ is not NULL or execute 'goto abort'.
+ *
+ * This macro is provided for convenience and should be
+ * carefully used if more resources should be released
+ * before jumping to the 'abort' label.
+ */
+#define CHECK_NON_NULL_OR_ABORT(OBJ)  \
+    do {                        \
+        if ((OBJ) == NULL) {    \
+            goto abort;         \
+        }                       \
+    } while (0)
+
 // The following _WRITE_* and _WRITE_*_OR macros do NOT check their inputs
 // since they directly delegate to the _PyUnicodeWriter_Write* underlying
 // function. In particular, the caller is responsible for type safety.
@@ -33,7 +61,7 @@
 // ----------------------------------------------------------------------------
 
 /* Write a character CHAR or execute the ON_ERROR statements if it fails. */
-#define WRITE_CHAR_OR(WRITER, CHAR, ON_ERROR)       \
+#define _WRITE_CHAR_OR(WRITER, CHAR, ON_ERROR)      \
     do {                                            \
         if (_WRITE_CHAR((WRITER), (CHAR)) < 0) {    \
             ON_ERROR;                               \
@@ -44,7 +72,7 @@
  * Write an ASCII string STRING of given length LENGTH,
  * or execute the ON_ERROR statements if it fails.
  */
-#define WRITE_ASCII_OR(WRITER, ASCII, LENGTH, ON_ERROR)         \
+#define _WRITE_ASCII_OR(WRITER, ASCII, LENGTH, ON_ERROR)        \
     do {                                                        \
         if (_WRITE_ASCII((WRITER), (ASCII), (LENGTH)) < 0) {    \
             ON_ERROR;                                           \
@@ -52,7 +80,7 @@
     } while (0)
 
 /* Write the string STRING or execute the ON_ERROR statements if it fails. */
-#define WRITE_STRING_OR(WRITER, STRING, ON_ERROR)       \
+#define _WRITE_STRING_OR(WRITER, STRING, ON_ERROR)      \
     do {                                                \
         if (_WRITE_STRING((WRITER), (STRING)) < 0) {    \
             ON_ERROR;                                   \
@@ -63,7 +91,7 @@
  * Write the substring STRING[START:STOP] (no-op if the substring is empty)
  * or execute the ON_ERROR statements if it fails.
  */
-#define WRITE_BLOCK_OR(WRITER, STRING, START, STOP, ON_ERROR)           \
+#define _WRITE_SUBSTRING_OR(WRITER, STRING, START, STOP, ON_ERROR)      \
     do {                                                                \
         /* intermediate variables to allow in-place operations */       \
         Py_ssize_t _i = (START), _j = (STOP);                           \
@@ -77,13 +105,13 @@
 // Macros which execute "goto abort" if an error occurs.
 
 #define WRITE_CHAR_OR_ABORT(WRITER, CHAR) \
-    WRITE_CHAR_OR((WRITER), (CHAR), goto abort)
+    _WRITE_CHAR_OR((WRITER), (CHAR), goto abort)
 #define WRITE_ASCII_OR_ABORT(WRITER, STRING, LENGTH) \
-    WRITE_ASCII_OR((WRITER), (STRING), (LENGTH), goto abort)
+    _WRITE_ASCII_OR((WRITER), (STRING), (LENGTH), goto abort)
 #define WRITE_STRING_OR_ABORT(WRITER, STRING) \
-    WRITE_STRING_OR((WRITER), (STRING), goto abort)
+    _WRITE_STRING_OR((WRITER), (STRING), goto abort)
 #define WRITE_BLOCK_OR_ABORT(WRITER, STRING, START, STOP) \
-    WRITE_BLOCK_OR((WRITER), (STRING), (START), (STOP), goto abort)
+    _WRITE_SUBSTRING_OR((WRITER), (STRING), (START), (STOP), goto abort)
 
 // ----------------------------------------------------------------------------
 
@@ -107,14 +135,17 @@
         NULL                                \
     )
 
-/* Escape set operations in STRING using re.sub(). */
-#define SETOPS_REPLACE(STATE, STRING, RE_SUB_FUNC)  \
-    PyObject_CallFunctionObjArgs(                   \
-        (RE_SUB_FUNC),                              \
-        (STATE)->setops_str,                        \
-        (STATE)->setops_repl_str,                   \
-        (STRING),                                   \
-        NULL                                        \
+/*
+ * Escape set operations in STRING using re.sub().
+ *
+ * SETOPS_RE_SUB_METH is a reference to re.compile('([&~|])').sub().
+ */
+#define SETOPS_REPLACE(STATE, STRING, SETOPS_RE_SUB_METH)   \
+    PyObject_CallFunctionObjArgs(                           \
+        (SETOPS_RE_SUB_METH),                               \
+        (STATE)->setops_repl_str,                           \
+        (STRING),                                           \
+        NULL                                                \
     )
 
 #endif // _FNMATCH_MACROS_H
