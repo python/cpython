51ed00955c0a37d70ad58a7c54bc4e0ff262e38a
Wulian233 <1055917385@qq.com>
2024-07-30 13:42:25 +0800

    tests

--- Lib/idlelib/replace.py
+++ Lib/idlelib/replace.py
@@ -86,11 +86,11 @@ class ReplaceDialog(SearchDialogBase):
         "Handle the Find button."
         self.do_find(False)
 
-    def replace_it(self, event=None): 
+    def replace_it(self, event=None):
         """Only used in test case
-  
-        If the find is successful, then perform replace. 
-        """ 
+
+        If the find is successful, then perform replace.
+        """
         if self.do_find(self.ok):
             self.do_replace()
 
