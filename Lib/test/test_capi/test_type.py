1fcda68d0d5ee0c8d31c630aefa08a51bd33eee5
Victor Stinner <vstinner@python.org>
2024-08-02 11:41:01 +0200

    Add more elaborate test on MRO

--- Lib/test/test_capi/test_type.py
+++ Lib/test/test_capi/test_type.py
@@ -9,19 +9,29 @@ class TypeTests(unittest.TestCase):
         # test PyType_Freeze()
         type_freeze = _testcapi.type_freeze
 
+        # simple case, no inherante
         class MyType:
             pass
         MyType.attr = "mutable"
 
         type_freeze(MyType)
         with self.assertRaises(TypeError):
+            # the class is now immutable
             MyType.attr = "immutable"
 
-        # PyType_Freeze() requires base classes to be immutable
-        class Mutable:
-            "mutable base class"
-            pass
-        class MyType2(Mutable):
-            pass
+        # test MRO: PyType_Freeze() requires base classes to be immutable
+        class A: pass
+        class B: pass
+        class C(B): pass
+        class D(A, C): pass
+
+        self.assertEqual(D.mro(), [D, A, C, B, object])
         with self.assertRaises(TypeError):
-            type_freeze(MyType2)
+            type_freeze(D)
+
+        type_freeze(A)
+        type_freeze(B)
+        type_freeze(C)
+        # all parent classes are now immutable, so D can be made immutable
+        # as well
+        type_freeze(D)
