b31764104ead12c6d6231abd729e87414e9e7644
Gregory P. Smith <greg@krypto.org>
2024-08-06 10:20:06 -0700

    [3.12] gh-119577: Adjust DeprecationWarning when testing element truth values in Element tree (GH-119762) (#120190)
    gh-119577: Adjust DeprecationWarning when testing element truth values in ElementTree (GH-119762)
    
    Adjust DeprecationWarning when testing element truth values in
    ElementTree, we're planning to go with the more natural True return
    rather than a disruptive harder to code around exception raise, and are
    deferring the behavior change for a few more releases.
    
    Co-authored-by: Jacob Walls <jacobtylerwalls@gmail.com>

--- Lib/xml/etree/ElementTree.py
+++ Lib/xml/etree/ElementTree.py
@@ -201,7 +201,7 @@ class Element:
 
     def __bool__(self):
         warnings.warn(
-            "Testing an element's truth value will raise an exception in "
+            "Testing an element's truth value will always return True in "
             "future versions.  "
             "Use specific 'len(elem)' or 'elem is not None' test instead.",
             DeprecationWarning, stacklevel=2
