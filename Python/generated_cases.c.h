f11d9846e6768c1672677c69ad83a1dc038b76cb
Matt Page <mpage@meta.com>
2024-08-07 15:47:32 -0700

    Regenerate files

--- Python/generated_cases.c.h
+++ Python/generated_cases.c.h
@@ -999,6 +999,7 @@
             PyFunctionObject *init = (PyFunctionObject *)cls->_spec_cache.init;
             PyCodeObject *code = (PyCodeObject *)init->func_code;
             DEOPT_IF(code->co_argcount != oparg+1, CALL);
+            DEOPT_IF(_Py_Specialize_CheckCode(code) != _Py_Specialize_Ok, CALL);
             DEOPT_IF(!_PyThreadState_HasStackSpace(tstate, code->co_framesize + _Py_InitCleanup.co_framesize), CALL);
             STAT_INC(CALL, hit);
             PyObject *self = _PyType_NewManagedObject(tp);
