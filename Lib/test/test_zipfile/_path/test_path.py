1670b18ddb582152e0a1a86e7ff3a9161b9a028e
Jason R. Coombs <jaraco@jaraco.com>
2024-08-11 19:48:50 -0400

    gh-122905: Sanitize names in zipfile.Path. (#122906)
    Ported from zipp 3.19.1; ref jaraco/zipp#119.

--- Lib/test/test_zipfile/_path/test_path.py
+++ Lib/test/test_zipfile/_path/test_path.py
@@ -577,3 +577,20 @@ class TestPath(unittest.TestCase):
         zipfile.Path(alpharep)
         with self.assertRaises(KeyError):
             alpharep.getinfo('does-not-exist')
+
+    def test_malformed_paths(self):
+        """
+        Path should handle malformed paths.
+        """
+        data = io.BytesIO()
+        zf = zipfile.ZipFile(data, "w")
+        zf.writestr("/one-slash.txt", b"content")
+        zf.writestr("//two-slash.txt", b"content")
+        zf.writestr("../parent.txt", b"content")
+        zf.filename = ''
+        root = zipfile.Path(zf)
+        assert list(map(str, root.iterdir())) == [
+            'one-slash.txt',
+            'two-slash.txt',
+            'parent.txt',
+        ]
