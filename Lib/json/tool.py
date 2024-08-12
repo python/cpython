9fd0c1c017638a7c163e7576bf9863ef237f840d
Trey Hunner <trey@treyhunner.com>
2024-08-12 07:10:32 -0700

    Reference json.__main__ in json.tool docstring
    Co-authored-by: Alyssa Coghlan <ncoghlan@gmail.com>

--- Lib/json/tool.py
+++ Lib/json/tool.py
@@ -1,3 +1,8 @@
+"""Command-line tool to validate and pretty-print JSON
+
+See `json.__main__` for a usage example (invocation as
+`python -m json.tool` is supported for backwards compatibility).
+"""
 import argparse
 import json
 import sys
