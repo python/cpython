dc6ba2f73a6fc04dbc1d06af7c56ac1308e5a6b6
Nate Ohlson <nohlson@purdue.edu>
2024-08-14 00:00:18 -0500

    Add debug prints

--- Tools/build/check_warnings.py
+++ Tools/build/check_warnings.py
@@ -30,6 +30,10 @@ def extract_warnings_from_compiler_output_clang(
     compiler_warnings = []
     for line in compiler_output.splitlines():
         if match := clang_warning_regex.match(line):
+            if match.group('line') == '4051':
+                print("THIS IS A VERY IMPORTANT LINE")
+                print(line)
+                print("################################")
             compiler_warnings.append(
                 {
                     "file": match.group("file").lstrip(path_prefix),
@@ -268,6 +272,16 @@ def main(argv: list[str] | None = None) -> int:
         )
 
     files_with_warnings = get_warnings_by_file(warnings)
+
+    print("######### Warnings by file #########")
+    for file in files_with_warnings.keys():
+        print(f"{file} {len(files_with_warnings[file])}")
+
+    print("######### Expected warnings by file #########")
+    for file in files_with_expected_warnings:
+        print(f"{file.name} {file.count}")
+
+    print("################")
 
     status = get_unexpected_warnings(
         files_with_expected_warnings, files_with_warnings
