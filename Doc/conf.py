7d213e320dc07a469f74e36ca5912e169545198f
Rafael Fontenelle <rffontenelle@users.noreply.github.com>
2024-08-14 18:35:23 -0300

    GH-103484: Tell linkcheck to ignore debian manpage redirects (#123019)

--- Doc/conf.py
+++ Doc/conf.py
@@ -546,6 +546,8 @@ linkcheck_allowed_redirects = {
     r'https://msdn.microsoft.com/.*': 'https://learn.microsoft.com/.*',
     r'https://docs.microsoft.com/.*': 'https://learn.microsoft.com/.*',
     r'https://go.microsoft.com/fwlink/\?LinkID=\d+': 'https://learn.microsoft.com/.*',
+    # Debian's man page redirects to its current stable version
+    r'https://manpages.debian.org/\w+\(\d(\w+)?\)': r'https://manpages.debian.org/\w+/[\w/\-\.]*\.\d(\w+)?\.en\.html',
     # Language redirects
     r'https://toml.io': 'https://toml.io/en/',
     r'https://www.redhat.com': 'https://www.redhat.com/en',
