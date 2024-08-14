2e12e226c46b585b9d2b433625de634d767869c0
Brian Ward <bward@flatironinstitute.org>
2024-08-14 12:43:19 -0400

    Test improvements

--- Lib/test/test_ctypes/test_dllist.py
+++ Lib/test/test_ctypes/test_dllist.py
@@ -4,6 +4,7 @@ import test.support
 import unittest
 from ctypes import CDLL
 from ctypes.util import dllist
+from test.support import import_helper
 
 
 WINDOWS = os.name == 'nt'
@@ -24,46 +25,34 @@ class ListSharedLibraries(unittest.TestCase):
     def test_lists_system(self):
         dlls = dllist()
 
-        if test.support.verbose:
-            print("Loaded shared libraries:")
-            if dlls is not None:
-                for dll in dlls:
-                    print("\t", dll)
-            else:
-                print("\tFunction returned None")
-
         if SYSTEM_LIBRARY is not None:
             self.assertIsNotNone(dlls)
-            self.assertGreater(len(dlls), 0)
-            self.assertTrue(any(SYSTEM_LIBRARY in dll for dll in dlls))
+            self.assertGreater(len(dlls), 0, f'loaded={dlls}')
+            self.assertTrue(any(SYSTEM_LIBRARY in dll for dll in dlls), f'loaded={dlls}')
         else:
             # unsupported platform
             self.assertIsNone(dlls)
 
     def test_lists_updates(self):
         dlls = dllist()
-
+        
         if dlls is not None:
-            from test.support import import_helper
+            if any("_ctypes_test" in dll for dll in dlls):
+                self.skipTest("Test library is already loaded")
+
             _ctypes_test = import_helper.import_module("_ctypes_test")
             test_module = CDLL(_ctypes_test.__file__)
-
-
-            if any("_ctypes_test" in dll for dll in dlls):
-                # The test module is already loaded
-                if test.support.verbose:
-                    print("The test module is already loaded")
-                return
-
             dlls2 = dllist()
+            self.assertIsNotNone(dlls2)
+
+            dlls1 = set(dlls)
+            dlls2 = set(dlls2)
             if test.support.verbose:
                 print("Newly loaded shared libraries:")
-                diff = set(dlls2) - set(dlls)
-                for dll in diff:
+                for dll in (dlls2 - dlls1):
                     print("\t", dll)
 
-            self.assertIsNotNone(dlls2)
-            self.assertGreater(len(dlls2), len(dlls))
+            self.assertGreater(dlls2, dlls1)
             self.assertTrue(any("_ctypes_test" in dll for dll in dlls2))
 
 
