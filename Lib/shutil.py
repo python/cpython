13c6e528fe572691fe58d1a797757c27c26a8939
Peter Bierma <zintensitydev@gmail.com>
2024-08-20 18:22:04 -0400

    Update shutil.py
    Co-authored-by: Barney Gale <barney.gale@gmail.com>

--- Lib/shutil.py
+++ Lib/shutil.py
@@ -74,7 +74,6 @@ class SpecialFileError(OSError):
     """Raised when trying to do a kind of operation (e.g. copying) which is
     not supported on a special file (e.g. a named pipe)"""
 
-_ExecError = RuntimeError
 
 class ReadError(OSError):
     """Raised when an archive cannot be read"""
