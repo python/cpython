2d092598872986d709a85b9a995086428270cb5c
Savannah Ostrowski <savannahostrowski@gmail.com>
2024-08-30 10:33:47 -0700

    fix typo

--- Include/internal/pycore_optimizer.h
+++ Include/internal/pycore_optimizer.h
@@ -32,7 +32,7 @@ typedef struct {
     uint16_t valid:1;
     uint16_t linked:1;
     bool was_run:1;
-    uint16_t chain_depth:13;  // Must be big engough for MAX_CHAIN_DEPTH - 1.
+    uint16_t chain_depth:13;  // Must be big enough for MAX_CHAIN_DEPTH - 1.
     int index;           // Index of ENTER_EXECUTOR (if code isn't NULL, below).
     _PyBloomFilter bloom;
     _PyExecutorLinkListNode links;
