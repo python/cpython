033a1ac112949a97f2e635af33e679f78dd81f70
Bénédikt Tran <10796600+picnixz@users.noreply.github.com>
2024-08-30 13:53:13 +0200

    fix typo

--- Objects/exceptions.c
+++ Objects/exceptions.c
@@ -2709,7 +2709,7 @@ set_unicodefromstring(PyObject **attr, const char *value)
 }
 
 /*
- * Ajust the (inclusive) 'start' value of a UnicodeError object.
+ * Adjust the (inclusive) 'start' value of a UnicodeError object.
  *
  * The START can be negative or not, but when adjusting the value,
  * we clip it in [0, MAX(0, OBJLEN - 1)] but do not intepret it as
@@ -2727,7 +2727,7 @@ set_unicodefromstring(PyObject **attr, const char *value)
     } while (0)
 
 /*
- * Ajust the (eclusive) 'end' value of a UnicodeError object.
+ * Adjust the (eclusive) 'end' value of a UnicodeError object.
  *
  * The END can be negative or not, but when adjusting the value,
  * we clip it in [MIN(1, OBJLEN), MAX(MIN(1, OBJLEN), OBJLEN)] but
