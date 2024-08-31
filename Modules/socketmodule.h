776d231a9ab80f2d9aeccd1c0aa6f8264060b0bd
unknown <nick@nickkinnan.com>
2024-08-31 01:54:13 -0700

    revert CR change due to CR comment updated

--- Modules/socketmodule.h
+++ Modules/socketmodule.h
@@ -326,7 +326,7 @@ typedef struct {
                                         0.0 means non-blocking */
     struct _socket_state *state;
 #ifdef MS_WINDOWS
-    unsigned int quickack: 1;
+    int quickack;
 #endif
 } PySocketSockObject;
 
