7cecfbdb9c9d565e154f85283e46392918118884
Victor Stinner <vstinner@python.org>
2024-09-03 01:24:17 +0200

    Reformat header file

--- Include/cpython/initconfig.h
+++ Include/cpython/initconfig.h
@@ -283,25 +283,32 @@ typedef struct PyInitConfig PyInitConfig;
 PyAPI_FUNC(PyInitConfig*) PyInitConfig_Create(void);
 PyAPI_FUNC(void) PyInitConfig_Free(PyInitConfig *config);
 
-PyAPI_FUNC(int) PyInitConfig_GetError(PyInitConfig* config, const char **err_msg);
-PyAPI_FUNC(int) PyInitConfig_GetExitCode(PyInitConfig* config, int *exitcode);
+PyAPI_FUNC(int) PyInitConfig_GetError(PyInitConfig* config,
+    const char **err_msg);
+PyAPI_FUNC(int) PyInitConfig_GetExitCode(PyInitConfig* config,
+    int *exitcode);
 
-PyAPI_FUNC(int) PyInitConfig_HasOption(PyInitConfig *config, const char *name);
-PyAPI_FUNC(int) PyInitConfig_GetInt(PyInitConfig *config, const char *name, int64_t *value);
-PyAPI_FUNC(int) PyInitConfig_GetStr(PyInitConfig *config, const char *name, char **value);
-PyAPI_FUNC(int) PyInitConfig_GetStrList(PyInitConfig *config, const char *name, size_t *length, char ***items);
+PyAPI_FUNC(int) PyInitConfig_HasOption(PyInitConfig *config,
+    const char *name);
+PyAPI_FUNC(int) PyInitConfig_GetInt(PyInitConfig *config,
+    const char *name,
+    int64_t *value);
+PyAPI_FUNC(int) PyInitConfig_GetStr(PyInitConfig *config,
+    const char *name,
+    char **value);
+PyAPI_FUNC(int) PyInitConfig_GetStrList(PyInitConfig *config,
+    const char *name,
+    size_t *length,
+    char ***items);
 PyAPI_FUNC(void) PyInitConfig_FreeStrList(size_t length, char **items);
 
-PyAPI_FUNC(int) PyInitConfig_SetInt(
-    PyInitConfig *config,
+PyAPI_FUNC(int) PyInitConfig_SetInt(PyInitConfig *config,
     const char *name,
     int64_t value);
-PyAPI_FUNC(int) PyInitConfig_SetStr(
-    PyInitConfig *config,
+PyAPI_FUNC(int) PyInitConfig_SetStr(PyInitConfig *config,
     const char *name,
     const char *value);
-PyAPI_FUNC(int) PyInitConfig_SetStrList(
-    PyInitConfig *config,
+PyAPI_FUNC(int) PyInitConfig_SetStrList(PyInitConfig *config,
     const char *name,
     size_t length,
     char * const *items);
