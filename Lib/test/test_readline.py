562ff730f3532489813289c50f2671bcd7f4c7a7
Miss Islington (bot) <31488909+miss-islington@users.noreply.github.com>
2024-09-05 14:25:38 +0200

    [3.12] gh-123321: Fix Parser/myreadline.c to prevent a segfault during a multi-threaded race (GH-123323) (#123677)
    * gh-123321: Fix Parser/myreadline.c to prevent a segfault during a multi-threaded race (GH-123323)
    (cherry picked from commit a4562fedadb73fe1e978dece65c3bcefb4606678)
    
    Co-authored-by: Bar Harel <bharel@barharel.com>
    
    * Remove @requires_gil_enabled for 3.12
    
    ---------
    
    Co-authored-by: Bar Harel <bharel@barharel.com>
    Co-authored-by: Sam Gross <colesbury@gmail.com>

--- Lib/test/test_readline.py
+++ Lib/test/test_readline.py
@@ -12,6 +12,7 @@ from test.support.import_helper import import_module
 from test.support.os_helper import unlink, temp_dir, TESTFN
 from test.support.pty_helper import run_pty
 from test.support.script_helper import assert_python_ok
+from test.support.threading_helper import requires_working_threading
 
 # Skip tests if there is no readline module
 readline = import_module('readline')
@@ -345,6 +346,30 @@ readline.write_history_file(history_file)
                 lines = f.readlines()
             self.assertEqual(len(lines), history_size)
             self.assertEqual(lines[-1].strip(), b"last input")
+
+    @requires_working_threading()
+    def test_gh123321_threadsafe(self):
+        """gh-123321: readline should be thread-safe and not crash"""
+        script = textwrap.dedent(r"""
+            import threading
+            from test.support.threading_helper import join_thread
+
+            def func():
+                input()
+
+            thread1 = threading.Thread(target=func)
+            thread2 = threading.Thread(target=func)
+            thread1.start()
+            thread2.start()
+            join_thread(thread1)
+            join_thread(thread2)
+            print("done")
+        """)
+
+        output = run_pty(script, input=b"input1\rinput2\r")
+
+        self.assertIn(b"done", output)
+
 
     def test_write_read_limited_history(self):
         previous_length = readline.get_history_length()
