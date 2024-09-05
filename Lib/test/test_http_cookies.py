1692faf81de347fe456a860cdbf648ad7adb82b8
Nano <nanoapezlk@gmail.com>
2024-09-06 02:02:32 +0800

    change EST to GMT in test cases

--- Lib/test/test_http_cookies.py
+++ Lib/test/test_http_cookies.py
@@ -62,21 +62,23 @@ class CookieTests(unittest.TestCase):
     def test_obsolete_rfc850_date_format(self):
         # Test cases with different days and dates in obsolete RFC 850 format
         test_cases = [
-            # test cases from RFC 850
+            # from RFC 850, change EST to GMT
             # https://datatracker.ietf.org/doc/html/rfc850#section-2
             {
-                'data': 'key=value; expires=Saturday, 01-Jan-83 00:00:00 EST',
-                'output': 'Saturday, 01-Jan-83 00:00:00 EST'
+                'data': 'key=value; expires=Saturday, 01-Jan-83 00:00:00 GMT',
+                'output': 'Saturday, 01-Jan-83 00:00:00 GMT'
             },
             {
-                'data': 'key=value; expires=Friday, 19-Nov-82 16:59:30 EST',
-                'output': 'Friday, 19-Nov-82 16:59:30 EST'
+                'data': 'key=value; expires=Friday, 19-Nov-82 16:59:30 GMT',
+                'output': 'Friday, 19-Nov-82 16:59:30 GMT'
             },
-            # other test cases
+            # from RFC 9110
+            # https://www.rfc-editor.org/rfc/rfc9110.html#section-5.6.7-6
             {
                 'data': 'key=value; expires=Sunday, 06-Nov-94 08:49:37 GMT',
                 'output': 'Sunday, 06-Nov-94 08:49:37 GMT'
             },
+            # other test cases
             {
                 'data': 'key=value; expires=Wednesday, 09-Nov-94 08:49:37 GMT',
                 'output': 'Wednesday, 09-Nov-94 08:49:37 GMT'
