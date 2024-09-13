9fba2cbe501d7d17b62afa569f14403a3f45ec8b
Sergey B Kirpichev <skirpichev@gmail.com>
2024-09-13 17:20:42 +0300

    Fix also dist test

--- Lib/test/test_math.py
+++ Lib/test/test_math.py
@@ -969,9 +969,9 @@ class MathTests(unittest.TestCase):
         self.assertEqual(dist((D(14), D(1)), (D(2), D(-4))), D(13))
         self.assertEqual(dist((F(14, 32), F(1, 32)), (F(2, 32), F(-4, 32))),
                          F(13, 32))
-        self.assertEqual(dist((True, True, False, True, False),
-                              (True, False, True, True, False)),
-                         sqrt(2.0))
+        self.assertEqual(dist((True, True, False, False, True, True),
+                              (True, False, True, False, False, False)),
+                         2.0)
 
         # Test corner cases
         self.assertEqual(dist((13.25, 12.5, -3.25),
