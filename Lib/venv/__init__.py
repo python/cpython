bc079eefef860a0f6f6593c1e839fca973268349
Jacek Duszenko <jacek.duszenko@gmail.com>
2024-09-18 17:43:11 +0200

    fix invalid variable name in test_venv.py

--- Lib/venv/__init__.py
+++ Lib/venv/__init__.py
@@ -393,7 +393,7 @@ class EnvBuilder:
                         os.symlink(src, dest)
                         to_unlink.append(dest)
                     except OSError:
-                        logger.warning('Unable to symlink %r to %r', src, dst)
+                        logger.warning('Unable to symlink %r to %r', src, dest)
                         do_copies = True
                         for f in to_unlink:
                             try:
