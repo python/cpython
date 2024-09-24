34b6c2b4448dbf3c72042a99ebe9fc554fc01d78
Serhiy Storchaka <storchaka@gmail.com>
2024-09-24 15:46:41 +0300

    gh-59317: Improve parsing optional positional arguments in argparse (GH-124303)
    Fix parsing positional argument with nargs equal to '?' or '*' if it is
    preceded by an option and another positional argument.

--- Lib/argparse.py
+++ Lib/argparse.py
@@ -2227,18 +2227,19 @@ class ArgumentParser(_AttributeHolder, _ActionsContainer):
     def _match_arguments_partial(self, actions, arg_strings_pattern):
         # progressively shorten the actions list by slicing off the
         # final actions until we find a match
-        result = []
         for i in range(len(actions), 0, -1):
             actions_slice = actions[:i]
             pattern = ''.join([self._get_nargs_pattern(action)
                                for action in actions_slice])
             match = _re.match(pattern, arg_strings_pattern)
             if match is not None:
-                result.extend([len(string) for string in match.groups()])
-                break
-
-        # return the list of arg string counts
-        return result
+                result = [len(string) for string in match.groups()]
+                if (match.end() < len(arg_strings_pattern)
+                    and arg_strings_pattern[match.end()] == 'O'):
+                    while result and not result[-1]:
+                        del result[-1]
+                return result
+        return []
 
     def _parse_optional(self, arg_string):
         # if it's an empty string, it was meant to be a positional
