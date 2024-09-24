6f155b6e289b00cee4bb08146e326a270bf87235
Kirill Podoprigora <kirill.bast9@mail.ru>
2024-09-25 00:08:58 +0300

    Skip some test cases if ``_testinternalcapi`` is not availble

--- Lib/test/test_compile.py
+++ Lib/test/test_compile.py
@@ -12,7 +12,10 @@ import tempfile
 import types
 import textwrap
 import warnings
-import _testinternalcapi
+try:
+    import _testinternalcapi
+except ImportError:
+    _testinternalcapi = None
 
 from test import support
 from test.support import (script_helper, requires_debug_ranges, run_code,
@@ -2605,6 +2608,7 @@ class TestStackSizeStability(unittest.TestCase):
             """
         self.check_stack_size(snippet, async_=True)
 
+@unittest.skipIf(_testinternalcapi is None, 'need _testinternalcapi module')
 class TestInstructionSequence(unittest.TestCase):
     def compare_instructions(self, seq, expected):
         self.assertEqual([(opcode.opname[i[0]],) + i[1:] for i in seq.get_instructions()],
