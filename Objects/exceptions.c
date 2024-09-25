8487b4630cb51a07ceb16569446733b1947df05f
Bénédikt Tran <10796600+picnixz@users.noreply.github.com>
2024-09-25 15:04:54 +0200

    Revert "fix size bug"
    This reverts commit 15b68116da1f46ebe195ef8e2d02feb129319b1f.

--- Objects/exceptions.c
+++ Objects/exceptions.c
@@ -2751,7 +2751,7 @@ PyUnicodeEncodeError_GetStart(PyObject *exc, Py_ssize_t *start)
     if (*start<0)
         *start = 0; /*XXX check for values <0*/
     if (*start>=size)
-        *start = size ? size-1 : 0;
+        *start = size-1;
     Py_DECREF(obj);
     return 0;
 }
@@ -2769,7 +2769,7 @@ PyUnicodeDecodeError_GetStart(PyObject *exc, Py_ssize_t *start)
     if (*start<0)
         *start = 0;
     if (*start>=size)
-        *start = size ? size-1 : 0;
+        *start = size-1;
     Py_DECREF(obj);
     return 0;
 }
