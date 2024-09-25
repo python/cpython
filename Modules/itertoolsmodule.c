4e4fad41367c2a76c88c449fe322ff4aec409e93
Raymond Hettinger <raymond.hettinger@gmail.com>
2024-09-25 08:35:06 -0700

    Call tee_copy() directly

--- Modules/itertoolsmodule.c
+++ Modules/itertoolsmodule.c
@@ -1036,7 +1036,7 @@ itertools_tee_impl(PyObject *module, PyObject *iterable, Py_ssize_t n)
 /*[clinic end generated code: output=1c64519cd859c2f0 input=c99a1472c425d66d]*/
 {
     Py_ssize_t i;
-    PyObject *it, *copyable, *copyfunc, *result;
+    PyObject *it, *to, *result;
 
     if (n < 0) {
         PyErr_SetString(PyExc_ValueError, "n must be >= 0");
@@ -1054,30 +1054,22 @@ itertools_tee_impl(PyObject *module, PyObject *iterable, Py_ssize_t n)
     }
 
     itertools_state *state = get_module_state(module);
-    copyable = tee_fromiterable(state, it);
+    to = tee_fromiterable(state, it);
     Py_DECREF(it);
-    if (copyable == NULL) {
-        Py_DECREF(result);
-        return NULL;
-    }
-    copyfunc = PyObject_GetAttr(copyable, &_Py_ID(__copy__));
-    if (copyfunc == NULL) {
-        Py_DECREF(copyable);
+    if (to == NULL) {
         Py_DECREF(result);
         return NULL;
     }
 
-    PyTuple_SET_ITEM(result, 0, copyable);
+    PyTuple_SET_ITEM(result, 0, to);
     for (i = 1; i < n; i++) {
-        copyable = _PyObject_CallNoArgs(copyfunc);
-        if (copyable == NULL) {
-            Py_DECREF(copyfunc);
+        to = tee_copy((teeobject *)to, NULL);
+        if (to == NULL) {
             Py_DECREF(result);
             return NULL;
         }
-        PyTuple_SET_ITEM(result, i, copyable);
+        PyTuple_SET_ITEM(result, i, to);
     }
-    Py_DECREF(copyfunc);
     return result;
 }
 
