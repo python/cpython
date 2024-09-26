9526d84aed752ee53800772121a5b34bc531ae63
Savannah Ostrowski <savannahostrowski@gmail.com>
2024-09-26 14:25:32 -0700

    Add -mno-outline-atomics flag

--- Tools/jit/_targets.py
+++ Tools/jit/_targets.py
@@ -139,6 +139,7 @@ class _Target(typing.Generic[_S, _R]):
             "-fno-plt",
             # Don't call stack-smashing canaries that we can't find or patch:
             "-fno-stack-protector",
+            "-mno-outline-atomics",
             "-std=c11",
             *self.args,
         ]
