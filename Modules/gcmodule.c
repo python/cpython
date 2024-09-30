1ba3555c287d7529f3f40e8999c777fd17f061cc
Thomas Wouters <thomas@python.org>
2024-09-30 02:49:23 +0200

    Reinstate cast.

--- Modules/gcmodule.c
+++ Modules/gcmodule.c
@@ -331,7 +331,7 @@ gc_get_objects_impl(PyObject *module, Py_ssize_t generation)
     }
 
     PyInterpreterState *interp = _PyInterpreterState_GET();
-    return _PyGC_GetObjects(interp, generation);
+    return _PyGC_GetObjects(interp, (int)generation);
 }
 
 /*[clinic input]
