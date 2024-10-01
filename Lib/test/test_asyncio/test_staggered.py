37e9ce1e6807e75be2bd60fe3f0ff60cca6b2c46
Peter Bierma <zintensitydev@gmail.com>
2024-09-30 21:37:27 -0400

    gh-124309: Revert eager task factory fix to prevent breaking downstream (#124810)
    * Revert "GH-124639: add back loop param to staggered_race (#124700)"
    
    This reverts commit e0a41a5dd12cb6e9277b05abebac5c70be684dd7.
    
    * Revert "gh-124309: Modernize the `staggered_race` implementation to support eager task factories (#124390)"
    
    This reverts commit de929f353c413459834a2a37b2d9b0240673d874.

--- Lib/test/test_asyncio/test_staggered.py
+++ Lib/test/test_asyncio/test_staggered.py
@@ -82,64 +82,16 @@ class StaggeredTests(unittest.IsolatedAsyncioTestCase):
         async def coro(index):
             raise ValueError(index)
 
-        for delay in [None, 0, 0.1, 1]:
-            with self.subTest(delay=delay):
-                winner, index, excs = await staggered_race(
-                    [
-                        lambda: coro(0),
-                        lambda: coro(1),
-                    ],
-                    delay=delay,
-                )
-
-                self.assertIs(winner, None)
-                self.assertIs(index, None)
-                self.assertEqual(len(excs), 2)
-                self.assertIsInstance(excs[0], ValueError)
-                self.assertIsInstance(excs[1], ValueError)
-
-    async def test_long_delay_early_failure(self):
-        async def coro(index):
-            await asyncio.sleep(0)  # Dummy coroutine for the 1 case
-            if index == 0:
-                await asyncio.sleep(0.1)  # Dummy coroutine
-                raise ValueError(index)
-
-            return f'Res: {index}'
-
         winner, index, excs = await staggered_race(
             [
                 lambda: coro(0),
                 lambda: coro(1),
             ],
-            delay=10,
+            delay=None,
         )
 
-        self.assertEqual(winner, 'Res: 1')
-        self.assertEqual(index, 1)
+        self.assertIs(winner, None)
+        self.assertIs(index, None)
         self.assertEqual(len(excs), 2)
         self.assertIsInstance(excs[0], ValueError)
-        self.assertIsNone(excs[1])
-
-    def test_loop_argument(self):
-        loop = asyncio.new_event_loop()
-        async def coro():
-            self.assertEqual(loop, asyncio.get_running_loop())
-            return 'coro'
-
-        async def main():
-            winner, index, excs = await staggered_race(
-                [coro],
-                delay=0.1,
-                loop=loop
-            )
-
-            self.assertEqual(winner, 'coro')
-            self.assertEqual(index, 0)
-
-        loop.run_until_complete(main())
-        loop.close()
-
-
-if __name__ == "__main__":
-    unittest.main()
+        self.assertIsInstance(excs[1], ValueError)
