97400c19af95ac8fbcabacc180e2da6a1966d92c
Serhiy Storchaka <storchaka@gmail.com>
2024-10-01 23:29:40 +0300

    gh-91818: Use default program name in the CLI of many modules
    As argparse now detects by default when the code was run as a module.
    
    This leads to using the actual executable name instead of simply "python"
    to display in the usage message ("usage: python -m ...").

--- Lib/tokenize.py
+++ Lib/tokenize.py
@@ -510,7 +510,7 @@ def main():
         sys.exit(1)
 
     # Parse the arguments and options
-    parser = argparse.ArgumentParser(prog='python -m tokenize')
+    parser = argparse.ArgumentParser()
     parser.add_argument(dest='filename', nargs='?',
                         metavar='filename.py',
                         help='the file to tokenize; defaults to stdin')
