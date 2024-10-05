73e617c3bb0bb2013bdf5ae9204ffb35c814715b
ruang <longjinyii@outlook.com>
2024-10-05 18:43:49 +0800

    Change comment

--- Include/internal/pycore_obmalloc.h
+++ Include/internal/pycore_obmalloc.h
@@ -253,14 +253,14 @@ typedef uint8_t pymem_block;
 /* Pool for small blocks. */
 struct pool_header {
     union { pymem_block *_padding;
-            uint count; } ref;          /* number of allocated blocks    */
-    pymem_block *freeblock;             /* pool's free list head         */
-    struct pool_header *nextpool;       /* next pool                     */
-    struct pool_header *prevpool;       /* previous pool                 */
-    uint arenaindex;                    /* index into arenas of base adr */
-    uint szidx;                         /* block size class index        */
-    uint nextoffset;                    /* bytes to virgin block         */
-    uint maxnextoffset;                 /* largest valid nextoffset      */
+            uint count; } ref;          /* number of allocated blocks           */
+    pymem_block *freeblock;             /* pool's free list head                */
+    struct pool_header *nextpool;       /* see Pool table below for details     */
+    struct pool_header *prevpool;       /* ""                                   */
+    uint arenaindex;                    /* index into arenas of base adr        */
+    uint szidx;                         /* block size class index               */
+    uint nextoffset;                    /* bytes to virgin block                */
+    uint maxnextoffset;                 /* largest valid nextoffset             */
 };
 
 typedef struct pool_header *poolp;
