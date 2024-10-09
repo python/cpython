c4598d26e69291b39c7d9ac35cbda3a6f43819f0
Victor Stinner <vstinner@python.org>
2024-10-09 16:19:07 +0200

    gh-115754: Use Py_GetConstant(Py_CONSTANT_EMPTY_STR)
    Replace PyUnicode_New(0, 0) with Py_GetConstant(Py_CONSTANT_EMPTY_STR).

--- Modules/_elementtree.c
+++ Modules/_elementtree.c
@@ -1317,7 +1317,7 @@ _elementtree_Element_findtext_impl(ElementObject *self, PyTypeObject *cls,
             PyObject* text = element_get_text((ElementObject*)item);
             if (text == Py_None) {
                 Py_DECREF(item);
-                return PyUnicode_New(0, 0);
+                return Py_GetConstant(Py_CONSTANT_EMPTY_STR);
             }
             Py_XINCREF(text);
             Py_DECREF(item);
