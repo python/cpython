3b81e78ae2e42d75f05ae01396d43706c6e2849c
Peter Bierma <zintensitydev@gmail.com>
2024-10-10 19:17:57 -0400

    Fix build complaints.

--- Modules/_ssl.c
+++ Modules/_ssl.c
@@ -4755,6 +4755,7 @@ X509_STORE_get1_objects(X509_STORE *store)
     ret = sk_X509_OBJECT_deep_copy(X509_STORE_get0_objects(store),
                                    x509_object_dup, X509_OBJECT_free);
     X509_STORE_unlock(store);
+    return ret;
 }
 #endif
 
