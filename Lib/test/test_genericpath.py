4ab19f912d9220d9762a1f2a0bda2add8af6ae94
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2024-10-11 15:18:46 -0700

    [3.12] gh-124917: Allow keyword args to os.path.exists/lexists on Windows (GH-124918) (#125334)
    (cherry picked from commit cc2938a18967c9d462ebb18bc09f73e4364aa7d2)

--- Lib/test/test_genericpath.py
+++ Lib/test/test_genericpath.py
@@ -158,6 +158,11 @@ class GenericTest:
             self.assertIs(self.pathmodule.lexists(filename + '\x00'), False)
             self.assertIs(self.pathmodule.lexists(bfilename + b'\x00'), False)
 
+        # Keyword arguments are accepted
+        self.assertIs(self.pathmodule.exists(path=filename), True)
+        if self.pathmodule is not genericpath:
+            self.assertIs(self.pathmodule.lexists(path=filename), True)
+
     @unittest.skipUnless(hasattr(os, "pipe"), "requires os.pipe()")
     @unittest.skipIf(is_emscripten, "Emscripten pipe fds have no stat")
     def test_exists_fd(self):
