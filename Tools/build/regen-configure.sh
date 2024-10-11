f18fdf0563c8d821a52a863fbd437ecb1f1f6101
Donghee Na <donghee.na@python.org>
2024-10-11 23:53:30 +0900

    gh-124612: Update autoconf container image

--- Tools/build/regen-configure.sh
+++ Tools/build/regen-configure.sh
@@ -5,7 +5,7 @@ set -e -x
 # The check_generated_files job of .github/workflows/build.yml must kept in
 # sync with this script. Use the same container image than the job so the job
 # doesn't need to run autoreconf in a container.
-IMAGE="ghcr.io/python/autoconf:2024.10.06.11200919239"
+IMAGE="ghcr.io/python/autoconf:2024.10.11.11293396815"
 AUTORECONF="autoreconf -ivf -Werror"
 
 WORK_DIR="/src"
