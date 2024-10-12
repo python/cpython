73c4bd31027f8cec011ce594821c16c4d6f0e6a0
Savannah Ostrowski <savannahostrowski@gmail.com>
2024-10-11 17:08:50 -0700

    fix merge conflict

--- Lib/argparse.py
+++ Lib/argparse.py
@@ -2581,25 +2581,25 @@ class ArgumentParser(_AttributeHolder, _ActionsContainer):
     def _check_value(self, action, value):
         # converted value must be one of the choices (if specified)
         choices = action.choices
-        if choices is not None:
-            if isinstance(choices, str):
-                choices = iter(choices)
-            if value not in choices:
-                args = {'value': value,
-                        'choices': ', '.join(map(repr, action.choices))}
-                msg = _('invalid choice: %(value)r (choose from %(choices)s)')
-                raise ArgumentError(action, msg % args)
-        
-            if self.suggest_on_error and isinstance(value, str):
-                    if all(isinstance(choice, str) for choice in action.choices):
-                        import difflib
-                        suggestions = difflib.get_close_matches(value, action.choices, 1)
-                        if suggestions:
-                            suggestions = suggestions[0]
-                            args['closest'] = suggestions
-                            msg = _('invalid choice: %(value)r, maybe you meant %(closest)r? '
-                                    '(choose from %(choices)s)')
+        if choices is None:
+            return
+    
+        if isinstance(choices, str):
+            choices = iter(choices)
 
+        if value not in choices:
+            args = {'value': value, 'choices': ', '.join(map(repr, action.choices))}
+            msg = _('invalid choice: %(value)r (choose from %(choices)s)')
+
+            if self.suggest_on_error and isinstance(value, str):
+                if all(isinstance(choice, str) for choice in action.choices):
+                    import difflib
+                    suggestions = difflib.get_close_matches(value, action.choices, 1)
+                    if suggestions:
+                        args['closest'] = suggestions[0]
+                        msg = _('invalid choice: %(value)r, maybe you meant %(closest)r? '
+                                '(choose from %(choices)s)')
+    
             raise ArgumentError(action, msg % args)
 
     # =======================
