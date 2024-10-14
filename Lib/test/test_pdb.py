348024b242a6ef68007160bbcf69197e1548de9e
Tian Gao <gaogaotiantian@hotmail.com>
2024-10-13 22:21:41 -0400

    Allow pdb to deal with empty file

--- Lib/test/test_pdb.py
+++ Lib/test/test_pdb.py
@@ -3948,6 +3948,13 @@ def bœr():
         # verify that pdb found the source of the "frozen" function
         self.assertIn('x = "Sentinel string for gh-93696"', stdout, "Sentinel statement not found")
 
+    def test_empty_file(self):
+        script = ''
+        commands = 'q\n'
+        # Anything reasonable is acceptable here, as long as it does not halt
+        _, _ = self.run_pdb_script(script, commands)
+        _, _ = self.run_pdb_module(script, commands)
+
     def test_non_utf8_encoding(self):
         script_dir = os.path.join(os.path.dirname(__file__), 'encoded_modules')
         for filename in os.listdir(script_dir):
