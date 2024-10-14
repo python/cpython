06ca33020e1168459fc6c3e0df93664daf801339
Mark Shannon <mark@hotpy.org>
2024-10-14 14:18:57 +0100

    GH-125323: Convert DECREF_INPUTS_AND_REUSE_FLOAT into a function that takes PyStackRefs. (GH-125439)

--- Tools/cases_generator/analyzer.py
+++ Tools/cases_generator/analyzer.py
@@ -575,6 +575,7 @@ NON_ESCAPING_FUNCTIONS = (
     "_PyDictValues_AddToInsertionOrder",
     "_PyErr_Occurred",
     "_PyEval_FrameClearAndPop",
+    "_PyFloat_FromDouble_ConsumeInputs",
     "_PyFrame_GetCode",
     "_PyFrame_IsIncomplete",
     "_PyFrame_PushUnchecked",
