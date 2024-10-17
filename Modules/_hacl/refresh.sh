a1d869fa2db5e7d08ddbd4956a78c49d1f7b06aa
Jonathan Protzenko <protz@microsoft.com>
2024-10-17 08:08:43 -0700

    GH-99108: Make vectorized versions of Blake2 available on x86, too (#125244)
    Accomplished by updating HACL* vendored code from hacl-star/hacl-star@a6a09496d9cff652b567d26f2c3ab012321b632a to hacl-star/hacl-star@315a9e491d2bc347b9dae99e0ea506995ea84d9d
    
    Co-authored-by: Victor Stinner <vstinner@python.org>
    Co-authored-by: Zachary Ware <zach@python.org>

--- Modules/_hacl/refresh.sh
+++ Modules/_hacl/refresh.sh
@@ -22,7 +22,7 @@ fi
 
 # Update this when updating to a new version after verifying that the changes
 # the update brings in are good.
-expected_hacl_star_rev=a6a09496d9cff652b567d26f2c3ab012321b632a
+expected_hacl_star_rev=315a9e491d2bc347b9dae99e0ea506995ea84d9d
 
 hacl_dir="$(realpath "$1")"
 cd "$(dirname "$0")"
