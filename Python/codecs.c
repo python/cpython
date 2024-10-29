20008c2c97127e82009b9231a73acd7981aa61ef
Rafid Aslam <rafidteam@gmail.com>
2024-10-29 07:22:01 +0700

    Update error message

--- Python/codecs.c
+++ Python/codecs.c
@@ -1526,7 +1526,7 @@ _PyCodec_InitRegistry(PyInterpreterState *interp)
     // search functions, so this is done after everything else is initialized.
     PyObject *mod = PyImport_ImportModule("encodings");
     if (mod == NULL) {
-        return PyStatus_Error("Failed to import encodings module. Are you sure PYTHONHOME is correct?");
+        return PyStatus_Error("Failed to import encodings module. Your environment probably has misconfigured variables!");
     }
     Py_DECREF(mod);
 
