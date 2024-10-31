be4e5eb315a71ae61f7f126e8a32331e2a9fe20e
Peter Bierma <zintensitydev@gmail.com>
2024-10-31 08:19:13 -0400

    Update test_api.py
    Co-authored-by: Bénédikt Tran <10796600+picnixz@users.noreply.github.com>

--- Lib/test/test_interpreters/test_api.py
+++ Lib/test/test_interpreters/test_api.py
@@ -54,7 +54,7 @@ class CreateTests(TestBase):
         self.assertIsInstance(interp, interpreters.Interpreter)
         self.assertIn(interp, interpreters.list_all())
 
-        # GH-126221: Passing an invalid unicode character used to cause a SystemError
+        # GH-126221: Passing an invalid Unicode character used to cause a SystemError
 		self.assertRaises(UnicodeEncodeError, _interpreters.create, '\udc80')
 
     def test_in_thread(self):
