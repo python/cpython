362dbabdedd1f6e17cbcb47610b4f643338f3005
RUANG <longjinyii@outlook.com>
2024-11-02 08:53:08 +0800

    Return _socket.socket

--- Lib/socket.py
+++ Lib/socket.py
@@ -71,7 +71,7 @@ def __getattr__(name):
     if name == "SocketType":
         import warnings
         warnings.warn(f"{name} is deprecated", DeprecationWarning)
-        return _socket.SockType
+        return _socket.socket
 
 # Set up the socket.AF_* socket.SOCK_* constants as members of IntEnums for
 # nicer string representations.
