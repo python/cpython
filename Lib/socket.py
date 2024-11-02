3e85bc7905511e69f739d65413688cc0e9a0b41e
RUANG <longjinyii@outlook.com>
2024-11-02 11:25:04 +0800

    warn change to _deprecated

--- Lib/socket.py
+++ Lib/socket.py
@@ -70,7 +70,7 @@ __all__.extend(os._get_exports_list(_socket))
 def __getattr__(name):
     if name == "SocketType":
         import warnings
-        warnings.warn(f"{name} is deprecated", DeprecationWarning)
+        warnings._deprecated(f"{name} is deprecated", remove=(3, 16))
         return _socket.socket
     raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
 # Set up the socket.AF_* socket.SOCK_* constants as members of IntEnums for
