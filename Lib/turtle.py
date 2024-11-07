f26a80ce5b1cdf670aba54aa9581d7e284f1295c
Terry Jan Reedy <tjreedy@udel.edu>
2024-11-06 23:33:51 -0500

    Update Lib/turtle.py

--- Lib/turtle.py
+++ Lib/turtle.py
@@ -2819,6 +2819,7 @@ class RawTurtle(TPen, TNavigator):
         self.turtle = None  # too make self deepcopy-able
 
         q = deepcopy(self)
+
         self.screen = screen
         self.turtle = turtle
 
