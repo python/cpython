43cfbb4aa1fa495f26c1da55c9d126fdb4f61a12
RUANG (James Roy) <longjinyii@outlook.com>
2024-11-11 22:29:38 +0800

    Add assert

--- Objects/longobject.c
+++ Objects/longobject.c
@@ -772,6 +772,7 @@ PyLong_AsUnsignedLongMask(PyObject *op)
 int
 PyLong_IsPositive(PyObject *obj)
 {
+    assert(obj != NULL);
     if (!PyLong_Check(obj)) {
         PyErr_Format(PyExc_TypeError, "expected int, got %T", obj);
         return -1;
@@ -782,6 +783,7 @@ PyLong_IsPositive(PyObject *obj)
 int
 PyLong_IsNegative(PyObject *obj)
 {
+    assert(obj != NULL);
     if (!PyLong_Check(obj)) {
         PyErr_Format(PyExc_TypeError, "expected int, got %T", obj);
         return -1;
@@ -792,6 +794,7 @@ PyLong_IsNegative(PyObject *obj)
 int
 PyLong_IsZero(PyObject *obj)
 {
+    assert(obj != NULL);
     if (!PyLong_Check(obj)) {
         PyErr_Format(PyExc_TypeError, "expected int, got %T", obj);
         return -1;
