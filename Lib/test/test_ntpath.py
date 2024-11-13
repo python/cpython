5174b5da73fa23f733a636cfec3f46714099635e
Nice Zombies <nineteendo19d0@gmail.com>
2024-11-13 20:12:55 +0100

    Fix `ntpath.normpath()` for drive-relative paths

--- Lib/test/test_ntpath.py
+++ Lib/test/test_ntpath.py
@@ -347,6 +347,7 @@ class TestNtpath(NtpathTestCase):
 
         tester("ntpath.normpath('..')", r'..')
         tester("ntpath.normpath('.')", r'.')
+        tester("ntpath.normpath('c:.')", 'c:')
         tester("ntpath.normpath('')", r'.')
         tester("ntpath.normpath('/')", '\\')
         tester("ntpath.normpath('c:/')", 'c:\\')
@@ -354,6 +355,8 @@ class TestNtpath(NtpathTestCase):
         tester("ntpath.normpath('c:/../../..')", 'c:\\')
         tester("ntpath.normpath('../.././..')", r'..\..\..')
         tester("ntpath.normpath('K:../.././..')", r'K:..\..\..')
+        tester("ntpath.normpath('./a/b')", r'a\b')
+        tester("ntpath.normpath('c:./a/b')", r'c:a\b')
         tester("ntpath.normpath('C:////a/b')", r'C:\a\b')
         tester("ntpath.normpath('//machine/share//a/b')", r'\\machine\share\a\b')
 
