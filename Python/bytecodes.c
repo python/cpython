b22403e80ad3d099d4285e8c7f7f233f02cdb8b3
Matt Page <mpage2@fb.com>
2024-11-17 10:44:15 -0800

    Fix bug pushing frame in _PY_FRAME_KW
    `_PY_FRAME_KW` pushes a pointer to the new frame onto the stack for
    consumption by the next uop. When pushing the frame fails, we do not
    want to push the result, `NULL`, to the stack because it is not
    a valid stackref. This works in the default build because `PyStackRef_NULL`
     and `NULL` are the same value, so the `PyStackRef_XCLOSE()` in the error
    handler ignores it. In the free-threaded build the values are not the same;
    `PyStackRef_XCLOSE()` will attempt to decref a null pointer.

--- Python/bytecodes.c
+++ Python/bytecodes.c
@@ -4311,7 +4311,7 @@ dummy_func(
             assert(Py_TYPE(callable_o) == &PyFunction_Type);
             int code_flags = ((PyCodeObject*)PyFunction_GET_CODE(callable_o))->co_flags;
             PyObject *locals = code_flags & CO_OPTIMIZED ? NULL : Py_NewRef(PyFunction_GET_GLOBALS(callable_o));
-            new_frame = _PyEvalFramePushAndInit(
+            _PyInterpreterFrame *temp = _PyEvalFramePushAndInit(
                 tstate, callable[0], locals,
                 args, positional_args, kwnames_o, frame
             );
@@ -4319,9 +4319,10 @@ dummy_func(
             // The frame has stolen all the arguments from the stack,
             // so there is no need to clean them up.
             SYNC_SP();
-            if (new_frame == NULL) {
+            if (temp == NULL) {
                 ERROR_NO_POP();
             }
+            new_frame = temp;
         }
 
         op(_CHECK_FUNCTION_VERSION_KW, (func_version/2, callable[1], self_or_null[1], unused[oparg], kwnames -- callable[1], self_or_null[1], unused[oparg], kwnames)) {
