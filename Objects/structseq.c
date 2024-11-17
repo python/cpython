d6582334a29284b9b05eacd9aca775d85144d0cd
Filipe Laíns <lains@riseup.net>
2024-11-17 04:44:29 +0000

    Put _asdict after dunder methods in structseq_methods
    Signed-off-by: Filipe Laíns <lains@riseup.net>

--- Objects/structseq.c
+++ Objects/structseq.c
@@ -480,10 +480,10 @@ error:
 
 static PyMethodDef structseq_methods[] = {
     {"__reduce__", (PyCFunction)structseq_reduce, METH_NOARGS, NULL},
-    {"_asdict", (PyCFunction)structseq_asdict, METH_NOARGS, NULL},
     {"__replace__", _PyCFunction_CAST(structseq_replace), METH_VARARGS | METH_KEYWORDS,
      PyDoc_STR("__replace__($self, /, **changes)\n--\n\n"
         "Return a copy of the structure with new values for the specified fields.")},
+    {"_asdict", (PyCFunction)structseq_asdict, METH_NOARGS, NULL},
     {NULL, NULL}  // sentinel
 };
 
