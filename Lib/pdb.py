d330cb6035910249d658d40c6cd827c165fa064d
Shantanu Jain <hauntsaninja@gmail.com>
2024-11-17 21:52:35 -0800

    gh-126944: Better error for missing arg in pdb

--- Lib/pdb.py
+++ Lib/pdb.py
@@ -1774,6 +1774,9 @@ class Pdb(bdb.Bdb, cmd.Cmd):
         try:
             return eval(arg, self.curframe.f_globals, self.curframe.f_locals)
         except:
+            if not arg:
+                self.error("Missing arg!")
+                raise
             self._error_exc()
             raise
 
