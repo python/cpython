f7ef0203d44acb21ab1c5ff0c3e15f9727862760
Serhiy Storchaka <storchaka@gmail.com>
2024-11-18 19:45:25 +0200

    gh-123803: Support arbitrary code page encodings on Windows (GH-123804)
    If the cpXXX encoding is not directly implemented in Python, fall back
    to use the Windows-specific API codecs.code_page_encode() and
    codecs.code_page_decode().

--- Lib/encodings/__init__.py
+++ Lib/encodings/__init__.py
@@ -156,19 +156,22 @@ def search_function(encoding):
 codecs.register(search_function)
 
 if sys.platform == 'win32':
-    # bpo-671666, bpo-46668: If Python does not implement a codec for current
-    # Windows ANSI code page, use the "mbcs" codec instead:
-    # WideCharToMultiByte() and MultiByteToWideChar() functions with CP_ACP.
-    # Python does not support custom code pages.
-    def _alias_mbcs(encoding):
-        try:
-            import _winapi
-            ansi_code_page = "cp%s" % _winapi.GetACP()
-            if encoding == ansi_code_page:
-                import encodings.mbcs
-                return encodings.mbcs.getregentry()
-        except ImportError:
-            # Imports may fail while we are shutting down
-            pass
+    from ._win_cp_codecs import create_win32_code_page_codec
 
-    codecs.register(_alias_mbcs)
+    def win32_code_page_search_function(encoding):
+        encoding = encoding.lower()
+        if not encoding.startswith('cp'):
+            return None
+        try:
+            cp = int(encoding[2:])
+        except ValueError:
+            return None
+        # Test if the code page is supported
+        try:
+            codecs.code_page_encode(cp, 'x')
+        except (OverflowError, OSError):
+            return None
+
+        return create_win32_code_page_codec(cp)
+
+    codecs.register(win32_code_page_search_function)
