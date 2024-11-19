d584e6ddb585d17d6bb3efbee11e605a4be4fe0d
RUANG (James Roy) <longjinyii@outlook.com>
2024-11-19 21:00:29 +0800

    Change warn desc

--- Modules/socketmodule.c
+++ Modules/socketmodule.c
@@ -5588,7 +5588,7 @@ socket_getattr(PyObject *self, PyObject *name)
     }
 
     if (!strcmp(attrname, "SocketType")) {
-        PyErr_Warn(PyExc_DeprecationWarning, "_socket.SocketType is deprecated and will be removed in 3.16. "
+        PyErr_Warn(PyExc_DeprecationWarning, "_socket.SocketType is deprecated and will be removed in Python 3.16. "
                                              "Use socket.socket instead");
         return sock_type;
     }
