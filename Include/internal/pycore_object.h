a3bd045803469652cf5134a4ff1243a9eccb310f
Pablo Galindo Salgado <Pablogsal@gmail.com>
2024-11-19 10:08:36 +0000

    Update Include/internal/pycore_object.h
    Co-authored-by: Bénédikt Tran <10796600+picnixz@users.noreply.github.com>

--- Include/internal/pycore_object.h
+++ Include/internal/pycore_object.h
@@ -98,7 +98,7 @@ PyAPI_FUNC(void) _Py_NO_RETURN _Py_FatalRefcountErrorFunc(
     do { \
         struct _reftracer_runtime_state *tracer = &_PyRuntime.ref_tracer; \
         if (tracer->tracer_func != NULL) { \
-            void* data = tracer->tracer_data; \
+            void *data = tracer->tracer_data; \
             tracer->tracer_func((obj), (operation), data); \
         } \
     } while(0)
