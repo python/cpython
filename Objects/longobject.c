d1e4aa268ed83dfcb35b18b040451a75bd003bee
Ken Jin <28750310+Fidget-Spinner@users.noreply.github.com>
2024-11-21 03:20:15 +0800

    Fixup

--- Objects/longobject.c
+++ Objects/longobject.c
@@ -3620,13 +3620,11 @@ long_richcompare(PyObject *self, PyObject *other, int op)
 void
 _PyLong_ExactDealloc(PyObject *self)
 {
-    if (PyLong_CheckExact(self)) {
-        if (_PyLong_IsCompact((PyLongObject *)self)) {
-            _Py_FREELIST_FREE(ints, self, PyObject_Free);
-            return;
-        }
+    if (_PyLong_IsCompact((PyLongObject *)self)) {
+        _Py_FREELIST_FREE(ints, self, PyObject_Free);
+        return;
     }
-    Py_TYPE(self)->tp_free(self);
+    PyObject_Free(self);
 }
 
 static void
@@ -3650,6 +3648,14 @@ long_dealloc(PyObject *self)
             }
         }
     }
+
+    if (PyLong_CheckExact(self)) {
+        if (_PyLong_IsCompact((PyLongObject *)self)) {
+            _Py_FREELIST_FREE(ints, self, PyObject_Free);
+            return;
+        }
+    }
+
     Py_TYPE(self)->tp_free(self);
 }
 
