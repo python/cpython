4dd695fddd13b6ab2a57ee40b612b0fbe0ac4e82
Jacob Bower <1978924+jbower-fb@users.noreply.github.com>
2024-11-21 15:26:25 -0800

    Improve comment for co_nlocalsplus (#126993)

--- Include/cpython/code.h
+++ Include/cpython/code.h
@@ -131,7 +131,8 @@ typedef struct {
                                                                                \
     /* redundant values (derived from co_localsplusnames and                   \
        co_localspluskinds) */                                                  \
-    int co_nlocalsplus;           /* number of local + cell + free variables */ \
+    int co_nlocalsplus;           /* number of spaces for holding local, cell, \
+                                     and free variables */                     \
     int co_framesize;             /* Size of frame in words */                 \
     int co_nlocals;               /* number of local variables */              \
     int co_ncellvars;             /* total number of cell variables */         \
