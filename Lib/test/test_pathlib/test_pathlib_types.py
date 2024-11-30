6e07a3c4d50b5cbb63ebb86a9aff6e238b38c95f
barneygale <barney.gale@gmail.com>
2024-11-30 19:42:59 +0000

    Add `pathlib._types.DirEntry`, which is a protocol for directory entries. Objects of this type provide a subset of the `os.DirEntry` API, specifically those methods and attributes needed to implement `glob()` and `walk()`.

--- Lib/test/test_pathlib/test_pathlib_types.py
+++ Lib/test/test_pathlib/test_pathlib_types.py
@@ -1,14 +1,32 @@
 import ntpath
+import os
 import posixpath
 import unittest
 
-from pathlib._types import Parser
+from pathlib._abc import PathBase
+from pathlib._types import Parser, DirEntry
+
+from test.support.os_helper import TESTFN
 
 
 class ParserTest(unittest.TestCase):
     def test_path_modules(self):
         self.assertTrue(isinstance(posixpath, Parser))
         self.assertTrue(isinstance(ntpath, Parser))
+
+
+class DirEntryTest(unittest.TestCase):
+    def test_os_direntry(self):
+        os.mkdir(TESTFN)
+        try:
+            with os.scandir() as entries:
+                entry = next(entries)
+                self.assertTrue(isinstance(entry, DirEntry))
+        finally:
+            os.rmdir(TESTFN)
+
+    def test_pathbase(self):
+        self.assertTrue(isinstance(PathBase(), DirEntry))
 
 
 if __name__ == "__main__":
