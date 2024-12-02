db50895f505def5bd15b13e253901d70720343e0
Victor Stinner <vstinner@python.org>
2024-12-02 16:51:50 +0100

    gh-126876: Fix test_socket.testLargeTimeout() for missing _testcapi (#127517)
    (cherry picked from commit c46acd3588864e97d0e0fe37a41aa5e94ac7af51)

--- Lib/test/test_socket.py
+++ Lib/test/test_socket.py
@@ -5063,7 +5063,10 @@ class NonBlockingTCPTests(ThreadedTCPSocketTest):
         # gh-126876: Check that a timeout larger than INT_MAX is replaced with
         # INT_MAX in the poll() code path. The following assertion must not
         # fail: assert(INT_MIN <= ms && ms <= INT_MAX).
-        large_timeout = _testcapi.INT_MAX + 1
+        if _testcapi is not None:
+            large_timeout = _testcapi.INT_MAX + 1
+        else:
+            large_timeout = 2147483648
 
         # test recv() with large timeout
         conn, addr = self.serv.accept()
@@ -5078,7 +5081,10 @@ class NonBlockingTCPTests(ThreadedTCPSocketTest):
 
     def _testLargeTimeout(self):
         # test sendall() with large timeout
-        large_timeout = _testcapi.INT_MAX + 1
+        if _testcapi is not None:
+            large_timeout = _testcapi.INT_MAX + 1
+        else:
+            large_timeout = 2147483648
         self.cli.connect((HOST, self.port))
         try:
             self.cli.settimeout(large_timeout)
