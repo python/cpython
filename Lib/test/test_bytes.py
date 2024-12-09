879994e2a6bdd91c5e71910dc6242d2001be7505
Srinivas Reddy Thatiparthy <thatiparthysreenivas@gmail.com>
2024-12-09 16:34:16 +0530

    Add some more tests

--- Lib/test/test_bytes.py
+++ Lib/test/test_bytes.py
@@ -460,9 +460,10 @@ class BaseBytesTest:
         self.assertRaises(ValueError, self.type2test.fromhex, '12   \x00   34')
 
         # For odd number of character(s)
-        with self.assertRaises(ValueError) as cm:
-            self.type2test.fromhex("a")
-        self.assertIn("fromhex() arg must be of even length", str(cm.exception))
+        for value in ("a", "a ", " a"," a ", "aaa", "aaa   ", "   aaa", "   aaa   "):
+            with self.assertRaises(ValueError) as cm:
+                self.type2test.fromhex(value)
+            self.assertIn("fromhex() arg must be of even length", str(cm.exception))
 
         for data, pos in (
             # invalid first hexadecimal character
