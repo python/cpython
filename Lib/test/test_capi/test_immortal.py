3788c60c8d3f26fb3e2b5e03a8d26657fd0572b0
Mark Shannon <mark@hotpy.org>
2024-12-11 10:06:32 +0000

    Add test

--- Lib/test/test_capi/test_immortal.py
+++ Lib/test/test_capi/test_immortal.py
@@ -2,6 +2,7 @@ import unittest
 from test.support import import_helper
 
 _testcapi = import_helper.import_module('_testcapi')
+_testinternalcapi = import_helper.import_module('_testinternalcapi')
 
 
 class TestCAPI(unittest.TestCase):
@@ -10,6 +11,18 @@ class TestCAPI(unittest.TestCase):
 
     def test_immortal_small_ints(self):
         _testcapi.test_immortal_small_ints()
+
+class TestInternalCAPI(unittest.TestCase):
+
+    def test_immortal_builtins(self):
+        for obj in range(-5, 256):
+            self.assertTrue(_testinternalcapi.is_static_immortal(obj))
+        for obj in (None, True, False, ..., NotImplemented, ()):
+            self.assertTrue(_testinternalcapi.is_static_immortal(obj))
+        for obj in range(300, 400):
+            self.assertFalse(_testinternalcapi.is_static_immortal(obj))
+        for obj in ([], {}, set()):
+            self.assertFalse(_testinternalcapi.is_static_immortal(obj))
 
 
 if __name__ == "__main__":
