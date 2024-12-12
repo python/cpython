deaf570fb05d584bd6273a5780e0622af11d9e92
Tomas Roun <tomas.roun8@gmail.com>
2024-12-12 17:52:37 +0100

    Fix compiler warning (-Wstringop-truncation)

--- Python/import.c
+++ Python/import.c
@@ -1176,7 +1176,7 @@ hashtable_key_from_2_strings(PyObject *str1, PyObject *str2, const char sep)
         return NULL;
     }
 
-    strncpy(key, str1_data, str1_len);
+    memcpy(key, str1_data, str1_len);
     key[str1_len] = sep;
     strncpy(key + str1_len + 1, str2_data, str2_len + 1);
     assert(strlen(key) == size - 1);
