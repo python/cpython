47e7b67bc2be16e881498b2ebe3ca1b4d1add567
Stephen Hansen <stephen.paul.hansen@gmail.com>
2024-12-12 13:45:20 -0500

    Linter fix

--- Lib/test/_test_multiprocessing.py
+++ Lib/test/_test_multiprocessing.py
@@ -6044,7 +6044,7 @@ class TestResourceTracker(unittest.TestCase):
                 self._test_resource_tracker_leak_resources(
                     cleanup=cleanup,
                 )
-    
+
     def test_resource_tracker_blocked_signals(self):
         #
         # gh-127586: Check that resource_tracker does not override blocked signals of caller.
