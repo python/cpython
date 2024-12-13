816261635ba8558a681bf82ff633a3244f8aa926
Bénédikt Tran <10796600+picnixz@users.noreply.github.com>
2024-12-13 10:14:22 +0100

    add HMAC module state
    This adds the `hmacmodule_state` structure.
    This structure will grow later, containing
    interned strings, specific exception types,
    object types as well as internal data.

--- Modules/hmacmodule.c
+++ Modules/hmacmodule.c
@@ -18,6 +18,19 @@
 
 #include "Python.h"
 
+// --- HMAC module state ------------------------------------------------------
+
+typedef struct hmacmodule_state {
+} hmacmodule_state;
+
+static inline hmacmodule_state *
+get_hmacmodule_state(PyObject *module)
+{
+    void *state = PyModule_GetState(module);
+    assert(state != NULL);
+    return (hmacmodule_state *)state;
+}
+
 // --- HMAC module clinic configuration ---------------------------------------
 
 /*[clinic input]
@@ -33,7 +46,36 @@ static PyMethodDef hmacmodule_methods[] = {
 
 // --- HMAC module initialization and finalization functions ------------------
 
+static int
+hmacmodule_exec(PyObject *module)
+{
+    hmacmodule_state *state = get_hmacmodule_state(module);
+    return 0;
+}
+
+static int
+hmacmodule_traverse(PyObject *mod, visitproc visit, void *arg)
+{
+    Py_VISIT(Py_TYPE(mod));
+    hmacmodule_state *state = get_hmacmodule_state(mod);
+    return 0;
+}
+
+static int
+hmacmodule_clear(PyObject *mod)
+{
+    hmacmodule_state *state = get_hmacmodule_state(mod);
+    return 0;
+}
+
+static inline void
+hmacmodule_free(void *mod)
+{
+    (void)hmacmodule_clear((PyObject *)mod);
+}
+
 static struct PyModuleDef_Slot hmacmodule_slots[] = {
+    {Py_mod_exec, hmacmodule_exec},
     {Py_mod_multiple_interpreters, Py_MOD_PER_INTERPRETER_GIL_SUPPORTED},
     {Py_mod_gil, Py_MOD_GIL_NOT_USED},
     {0, NULL} /* sentinel */
@@ -42,9 +84,12 @@ static struct PyModuleDef_Slot hmacmodule_slots[] = {
 static struct PyModuleDef _hmacmodule = {
     PyModuleDef_HEAD_INIT,
     .m_name = "_hmac",
-    .m_size = 0,
+    .m_size = sizeof(hmacmodule_state),
     .m_methods = hmacmodule_methods,
     .m_slots = hmacmodule_slots,
+    .m_traverse = hmacmodule_traverse,
+    .m_clear = hmacmodule_clear,
+    .m_free = hmacmodule_free,
 };
 
 PyMODINIT_FUNC
