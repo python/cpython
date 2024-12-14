9dbf0aa7980a394d4def86bf24e62db634ea37f4
Jun Komoda <45822440+junkmd@users.noreply.github.com>
2024-12-14 05:24:36 +0000

    Add

--- Lib/test/test_ctypes/test_win32_com_foreign_func.py
+++ Lib/test/test_ctypes/test_win32_com_foreign_func.py
@@ -281,5 +281,10 @@ class CopyComPointerTests(unittest.TestCase):
         self.assertEqual(0, dst_orig.Release())
 
 
+class SampleTest(unittest.TestCase):
+    def test_foo(self):
+        self.assertTrue(True)
+
+
 if __name__ == '__main__':
     unittest.main()
