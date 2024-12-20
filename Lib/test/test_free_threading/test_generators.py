690f45bf4fb828f1f7a29448a63b6016e971b633
Peter Bierma <zintensitydev@gmail.com>
2024-12-20 13:25:24 +0000

    Some small changes to the test.

--- Lib/test/test_free_threading/test_generators.py
+++ Lib/test/test_free_threading/test_generators.py
@@ -1,5 +1,4 @@
 import unittest
-import concurrent.futures
 
 from unittest import TestCase
 
@@ -7,16 +6,23 @@ from test.support import threading_helper
 
 @threading_helper.requires_working_threading()
 class TestGen(TestCase):
-    def test_generators_basic(self):
+    def test_generator_send(self):
+        import threading
+
         def gen():
             for _ in range(5000):
                 yield
 
 
         it = gen()
-        with concurrent.futures.ThreadPoolExecutor() as executor:
-            for _ in range(5000):
-                executor.submit(lambda: next(it))
+        with threading_helper.catch_threading_exception() as cm:
+            # next(it) is equivalent to it.send(None)
+            with threading_helper.start_threads(
+                (threading.Thread(target=lambda: next(it)) for _ in range(10))
+            ):
+                pass
+
+            self.assertIsNone(cm.exc_value)
 
 
 if __name__ == "__main__":
