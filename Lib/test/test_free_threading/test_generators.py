77a33bb4b3dfab87fdff44665c2d7f3636241255
Peter Bierma <zintensitydev@gmail.com>
2024-12-25 12:23:47 -0500

    Add tests for coroutines.

--- Lib/test/test_free_threading/test_generators.py
+++ Lib/test/test_free_threading/test_generators.py
@@ -4,6 +4,7 @@ from unittest import TestCase
 
 from test import support
 from test.support import threading_helper
+from test.support import import_helper
 from threading import Thread
 
 @threading_helper.requires_working_threading()
@@ -16,9 +17,17 @@ class TestGen(TestCase):
 
         return gen()
 
-    def stress_generator(self, *funcs):
+    def infinite_coroutine(self):
+        asyncio = import_helper.import_module("asyncio")
+
+        async def coro():
+            while True:
+                await asyncio.sleep(0)
+
+        return coro()
+
+    def _stress_genlike(self, gen, *funcs):
         threads = []
-        gen = self.infinite_generator()
 
         for func in funcs:
             for _ in range(10):
@@ -37,6 +46,12 @@ class TestGen(TestCase):
             for thread in threads:
                 thread.join()
 
+    def stress_generator(self, *funcs):
+        self._stress_genlike(self.infinite_generator(), *funcs)
+
+    def stress_coroutine(self, *funcs):
+        self._stress_genlike(self.infinite_coroutine(), *funcs)
+
     def test_generator_send(self):
         self.stress_generator(lambda gen: next(gen))
 
@@ -47,6 +62,24 @@ class TestGen(TestCase):
     def test_generator_state(self):
         self.stress_generator(lambda gen: next(gen), lambda gen: gen.gi_running)
         self.stress_generator(lambda gen: gen.gi_isrunning, lambda gen: gen.close())
+
+    def test_coroutine_send(self):
+        def try_send_non_none(coro):
+            try:
+                coro.send(42)
+            except ValueError:
+                # Can't send non-None to just started coroutine
+                coro.send(None)
+
+        self.stress_coroutine(lambda coro: next(coro))
+        self.stress_coroutine(try_send_non_none)
+
+    def test_coroutine_attributes(self):
+        self.stress_coroutine(
+            lambda coro: next(coro),
+            lambda coro: coro.cr_frame,
+            lambda coro: coro.cr_suspended,
+        )
 
 if __name__ == "__main__":
     unittest.main()
