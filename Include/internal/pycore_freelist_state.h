5d13675cd8f2add3984ec7250e47120f93d4bd5b
Pieter Eendebak <pieter.eendebak@gmail.com>
2025-01-01 22:39:01 +0100

    add PyCFunctionObject

--- Include/internal/pycore_freelist_state.h
+++ Include/internal/pycore_freelist_state.h
@@ -16,6 +16,8 @@ extern "C" {
 #  define Py_floats_MAXFREELIST 100
 #  define Py_ints_MAXFREELIST 100
 #  define Py_slices_MAXFREELIST 1
+#  define Py_pycfunctionobject_MAXFREELIST 40
+#  define Py_pycmethodobject_MAXFREELIST 40
 #  define Py_ranges_MAXFREELIST 10
 #  define Py_range_iters_MAXFREELIST 10
 #  define Py_shared_iters_MAXFREELIST 24
@@ -46,6 +48,8 @@ struct _Py_freelists {
     struct _Py_freelist dicts;
     struct _Py_freelist dictkeys;
     struct _Py_freelist slices;
+    struct _Py_freelist pycfunctionobject;
+    struct _Py_freelist pycmethodobject;
     struct _Py_freelist ranges;
     struct _Py_freelist range_iters;
     struct _Py_freelist shared_iters;
