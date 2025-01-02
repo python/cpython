fbc9c52afb543deae42bb25ba443e6b69bf2c2da
Donghee Na <donghee.na92@gmail.com>
2025-01-02 09:36:04 +0900

    Add extra padding

--- Include/cpython/unicodeobject.h
+++ Include/cpython/unicodeobject.h
@@ -145,10 +145,11 @@ typedef struct {
         /* The object is statically allocated. */
         unsigned short statically_allocated:1;
         /* Padding to ensure that PyUnicode_DATA() is always aligned to
-           4 bytes (see issue #19537 on m68k) and we use unsigned short to avoid
+           24 bits (see issue #19537 on m68k) and we use unsigned short to avoid
            the extra four bytes on 32-bit Windows. This is restricted features
            for specific compilers including GCC, MSVC, Clang and IBM's XL compiler. */
         unsigned short :10;
+        unsigned char  :8;
     } state;
 } PyASCIIObject;
 
