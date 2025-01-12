a7f9df3129e06117ddc74969493307f959c7740d
Xuanteng Huang <xuanteng.huang@outlook.com>
2025-01-12 21:55:58 +0800

    remove `_INTERNAL_INCREMENT_OPT_COUNTER`

--- Python/optimizer.c
+++ Python/optimizer.c
@@ -1352,43 +1352,6 @@ PyTypeObject _PyCounterExecutor_Type = {
     .tp_clear = (inquiry)executor_clear,
 };
 
-static int
-counter_optimize(
-    _PyOptimizerObject* self,
-    _PyInterpreterFrame *frame,
-    _Py_CODEUNIT *instr,
-    _PyExecutorObject **exec_ptr,
-    int Py_UNUSED(curr_stackentries),
-    bool Py_UNUSED(progress_needed)
-)
-{
-    PyCodeObject *code = _PyFrame_GetCode(frame);
-    int oparg = instr->op.arg;
-    while (instr->op.code == EXTENDED_ARG) {
-        instr++;
-        oparg = (oparg << 8) | instr->op.arg;
-    }
-    if (instr->op.code != JUMP_BACKWARD) {
-        /* Counter optimizer can only handle backward edges */
-        return 0;
-    }
-    _Py_CODEUNIT *target = instr + 1 + _PyOpcode_Caches[JUMP_BACKWARD] - oparg;
-    _PyUOpInstruction buffer[4] = {
-        { .opcode = _START_EXECUTOR, .jump_target = 3, .format=UOP_FORMAT_JUMP },
-        { .opcode = _LOAD_CONST_INLINE, .operand0 = (uintptr_t)self },
-        { .opcode = _INTERNAL_INCREMENT_OPT_COUNTER },
-        { .opcode = _EXIT_TRACE, .target = (uint32_t)(target - _PyCode_CODE(code)), .format=UOP_FORMAT_TARGET }
-    };
-    _PyExecutorObject *executor = make_executor_from_uops(buffer, 4, &EMPTY_FILTER);
-    if (executor == NULL) {
-        return -1;
-    }
-    Py_INCREF(self);
-    Py_SET_TYPE(executor, &_PyCounterExecutor_Type);
-    *exec_ptr = executor;
-    return 1;
-}
-
 static PyObject *
 counter_get_counter(PyObject *self, PyObject *args)
 {
--- Python/optimizer_cases.c.h
+++ Python/optimizer_cases.c.h
@@ -2620,12 +2620,6 @@
             break;
         }
 
-        case _INTERNAL_INCREMENT_OPT_COUNTER: {
-            stack_pointer += -1;
-            assert(WITHIN_STACK_BOUNDS());
-            break;
-        }
-
         case _DYNAMIC_EXIT: {
             break;
         }
