368313c4ac5eee024873e17812887617c97b009b
Josh Snyder <josh@code406.com>
2025-01-12 16:28:38 -0800

    try again

--- Lib/test/test_sys.py
+++ Lib/test/test_sys.py
@@ -1482,7 +1482,7 @@ class SizeofTest(unittest.TestCase):
     def test_inline_values(self):
         vsize = test.support.calcvobjsize
         gc_header_size = self.gc_headsize
-        inline_values_size = vsize('PP')
+        inline_values_size = vsize('P')
 
         linked_list = None
         for i in range(28):
