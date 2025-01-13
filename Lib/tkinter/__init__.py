2c6617cd3699899748edd496e6d69012b722525a
Zhikang Yan <2951256653@qq.com>
2025-01-13 23:54:46 +0800

    gh-128562: Fix generation of the tkinter widget names (GH-128604)
    There were possible conflicts if the widget class name ends with a digit.

--- Lib/tkinter/__init__.py
+++ Lib/tkinter/__init__.py
@@ -2741,6 +2741,8 @@ class BaseWidget(Misc):
             del cnf['name']
         if not name:
             name = self.__class__.__name__.lower()
+            if name[-1].isdigit():
+                name += "!"  # Avoid duplication when calculating names below
             if master._last_child_ids is None:
                 master._last_child_ids = {}
             count = master._last_child_ids.get(name, 0) + 1
