6e1e78054060ad326f26dd8dbf12adfedbb52883
sobolevn <mail@sobolevn.me>
2025-01-13 10:40:52 +0300

    gh-128759: fix data race in `type_modified_unlocked` (#128764)

--- Objects/typeobject.c
+++ Objects/typeobject.c
@@ -1038,7 +1038,7 @@ type_modified_unlocked(PyTypeObject *type)
        We don't assign new version tags eagerly, but only as
        needed.
      */
-    if (type->tp_version_tag == 0) {
+    if (FT_ATOMIC_LOAD_UINT_RELAXED(type->tp_version_tag) == 0) {
         return;
     }
     // Cannot modify static builtin types.
