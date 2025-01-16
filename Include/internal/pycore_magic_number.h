3e475e8fed61bdcd073e68efb1e67de48601d65e
Mark Shannon <mark@hotpy.org>
2025-01-16 12:20:20 +0000

    Use correct magic number

--- Include/internal/pycore_magic_number.h
+++ Include/internal/pycore_magic_number.h
@@ -279,7 +279,7 @@ PC/launcher.c must also be updated.
 
 */
 
-#define PYC_MAGIC_NUMBER 3615
+#define PYC_MAGIC_NUMBER 3614
 /* This is equivalent to converting PYC_MAGIC_NUMBER to 2 bytes
    (little-endian) and then appending b'\r\n'. */
 #define PYC_MAGIC_NUMBER_TOKEN \
