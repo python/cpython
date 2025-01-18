78643f00fee823fab0a5978fe380a283661c6309
Cody Maloney <cmaloney@theoreticalchaos.com>
2025-01-18 15:52:12 -0800

    Add conditional code for platforms without os.readv

--- Lib/_pyio.py
+++ Lib/_pyio.py
@@ -1695,12 +1695,18 @@ class FileIO(RawIOBase):
     def readinto(self, b):
         """Same as RawIOBase.readinto()."""
         m = memoryview(b).cast('B')
-        self._checkClosed()
-        self._checkReadable()
-        try:
-            return os.readv(self._fd, (m, ))
-        except BlockingIOError:
-            return None
+        if hasattr(os, 'readv'):
+            self._checkClosed()
+            self._checkReadable()
+            try:
+                return os.readv(self._fd, (m, ))
+            except BlockingIOError:
+                return None
+        else:
+            data = self.read(len(m))
+            n = len(data)
+            m[:n] = data
+            return n
 
     def write(self, b):
         """Write bytes b to file, return number written.
