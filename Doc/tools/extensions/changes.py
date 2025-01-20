a81d584b0b2e0f2d299a4b885ea83875e956013e
Adam Turner <9087854+aa-turner@users.noreply.github.com>
2025-01-20 23:40:21 +0000

    Convert to double quotes

--- Doc/tools/extensions/changes.py
+++ Doc/tools/extensions/changes.py
@@ -19,8 +19,8 @@ if TYPE_CHECKING:
 
 def expand_version_arg(argument: str, release: str) -> str:
     """Expand "next" to the current version"""
-    if argument == 'next':
-        return sphinx_gettext('{} (unreleased)').format(release)
+    if argument == "next":
+        return sphinx_gettext("{} (unreleased)").format(release)
     return argument
 
 
@@ -37,10 +37,10 @@ class DeprecatedRemoved(VersionChange):
     required_arguments = 2
 
     _deprecated_label = sphinx_gettext(
-        'Deprecated since version %s, will be removed in version %s'
+        "Deprecated since version %s, will be removed in version %s"
     )
     _removed_label = sphinx_gettext(
-        'Deprecated since version %s, removed in version %s'
+        "Deprecated since version %s, removed in version %s"
     )
 
     def run(self) -> list[Node]:
@@ -50,38 +50,38 @@ class DeprecatedRemoved(VersionChange):
             self.arguments[0], self.config.release
         )
         version_removed = self.arguments.pop(1)
-        if version_removed == 'next':
+        if version_removed == "next":
             raise ValueError(
-                'deprecated-removed:: second argument cannot be `next`'
+                "deprecated-removed:: second argument cannot be `next`"
             )
         self.arguments[0] = version_deprecated, version_removed
 
         # Set the label based on if we have reached the removal version
-        current_version = tuple(map(int, self.config.version.split('.')))
-        removed_version = tuple(map(int, version_removed.split('.')))
+        current_version = tuple(map(int, self.config.version.split(".")))
+        removed_version = tuple(map(int, version_removed.split(".")))
         if current_version < removed_version:
             versionlabels[self.name] = self._deprecated_label
-            versionlabel_classes[self.name] = 'deprecated'
+            versionlabel_classes[self.name] = "deprecated"
         else:
             versionlabels[self.name] = self._removed_label
-            versionlabel_classes[self.name] = 'removed'
+            versionlabel_classes[self.name] = "removed"
         try:
             return super().run()
         finally:
             # reset versionlabels and versionlabel_classes
-            versionlabels[self.name] = ''
-            versionlabel_classes[self.name] = ''
+            versionlabels[self.name] = ""
+            versionlabel_classes[self.name] = ""
 
 
 def setup(app: Sphinx) -> ExtensionMetadata:
     # Override Sphinx's directives with support for 'next'
-    app.add_directive('versionadded', PyVersionChange, override=True)
-    app.add_directive('versionchanged', PyVersionChange, override=True)
-    app.add_directive('versionremoved', PyVersionChange, override=True)
-    app.add_directive('deprecated', PyVersionChange, override=True)
+    app.add_directive("versionadded", PyVersionChange, override=True)
+    app.add_directive("versionchanged", PyVersionChange, override=True)
+    app.add_directive("versionremoved", PyVersionChange, override=True)
+    app.add_directive("deprecated", PyVersionChange, override=True)
 
     # Register the ``.. deprecated-removed::`` directive
-    app.add_directive('deprecated-removed', DeprecatedRemoved)
+    app.add_directive("deprecated-removed", DeprecatedRemoved)
 
     return {
         "version": "1.0",
