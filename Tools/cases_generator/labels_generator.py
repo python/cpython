5cccb986ba393cf38643b7ccb0b2d5cc28567188
Ken Jin <28750310+Fidget-Spinner@users.noreply.github.com>
2025-01-21 08:58:53 +0800

    Fix mypy

--- Tools/cases_generator/labels_generator.py
+++ Tools/cases_generator/labels_generator.py
@@ -7,123 +7,18 @@ import argparse
 
 from analyzer import (
     Analysis,
-    Instruction,
-    Uop,
-    Part,
     analyze_files,
-    Skip,
-    Flush,
-    analysis_error,
-    StackItem,
 )
 from generators_common import (
     DEFAULT_INPUT,
     ROOT,
     write_header,
-    type_and_null,
-    Emitter,
 )
 from cwriter import CWriter
 from typing import TextIO
-from stack import Local, Stack, StackError, get_stack_effect, Storage
 
 
 DEFAULT_OUTPUT = ROOT / "Python/generated_labels.c.h"
-
-
-
-def declare_variable(var: StackItem, out: CWriter) -> None:
-    type, null = type_and_null(var)
-    space = " " if type[-1].isalnum() else ""
-    if var.condition:
-        out.emit(f"{type}{space}{var.name} = {null};\n")
-    else:
-        out.emit(f"{type}{space}{var.name};\n")
-
-
-def declare_variables(inst: Instruction, out: CWriter) -> None:
-    try:
-        stack = get_stack_effect(inst)
-    except StackError as ex:
-        raise analysis_error(ex.args[0], inst.where) from None
-    required = set(stack.defined)
-    required.discard("unused")
-    for part in inst.parts:
-        if not isinstance(part, Uop):
-            continue
-        for var in part.stack.inputs:
-            if var.name in required:
-                required.remove(var.name)
-                declare_variable(var, out)
-        for var in part.stack.outputs:
-            if var.name in required:
-                required.remove(var.name)
-                declare_variable(var, out)
-
-
-def write_uop(
-    uop: Part,
-    emitter: Emitter,
-    offset: int,
-    stack: Stack,
-    inst: Instruction,
-    braces: bool,
-) -> tuple[int, Stack]:
-    # out.emit(stack.as_comment() + "\n")
-    if isinstance(uop, Skip):
-        entries = "entries" if uop.size > 1 else "entry"
-        emitter.emit(f"/* Skip {uop.size} cache {entries} */\n")
-        return (offset + uop.size), stack
-    if isinstance(uop, Flush):
-        emitter.emit(f"// flush\n")
-        stack.flush(emitter.out)
-        return offset, stack
-    try:
-        locals: dict[str, Local] = {}
-        emitter.out.start_line()
-        if braces:
-            emitter.out.emit(f"// {uop.name}\n")
-            emitter.emit("{\n")
-        code_list, storage = Storage.for_uop(stack, uop)
-        emitter._print_storage(storage)
-        for code in code_list:
-            emitter.emit(code)
-
-        for cache in uop.caches:
-            if cache.name != "unused":
-                if cache.size == 4:
-                    type = "PyObject *"
-                    reader = "read_obj"
-                else:
-                    type = f"uint{cache.size*16}_t "
-                    reader = f"read_u{cache.size*16}"
-                emitter.emit(
-                    f"{type}{cache.name} = {reader}(&this_instr[{offset}].cache);\n"
-                )
-                if inst.family is None:
-                    emitter.emit(f"(void){cache.name};\n")
-            offset += cache.size
-
-        storage = emitter.emit_tokens(uop, storage, inst)
-        if braces:
-            emitter.out.start_line()
-            emitter.emit("}\n")
-        # emitter.emit(stack.as_comment() + "\n")
-        return offset, storage.stack
-    except StackError as ex:
-        raise analysis_error(ex.args[0], uop.body[0])
-
-
-def uses_this(inst: Instruction) -> bool:
-    if inst.properties.needs_this:
-        return True
-    for uop in inst.parts:
-        if not isinstance(uop, Uop):
-            continue
-        for cache in uop.caches:
-            if cache.name != "unused":
-                return True
-    return False
 
 
 def generate_labels(
