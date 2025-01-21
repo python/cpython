2ac9464d5e69771632f5709b9a0f6b77e6fbe4b6
Bénédikt Tran <10796600+picnixz@users.noreply.github.com>
2025-01-21 15:28:08 +0100

    fix bounds

--- Python/codecs.c
+++ Python/codecs.c
@@ -776,8 +776,9 @@ PyObject *PyCodec_XMLCharRefReplaceErrors(PyObject *exc)
     // characters.
     if (slen > PY_SSIZE_T_MAX / (2 + 7 + 1)) {
         end = start + PY_SSIZE_T_MAX / (2 + 7 + 1);
+        end = Py_MIN(end, objlen);
+        slen = Py_MAX(0, end - start);
     }
-    end = Py_MIN(end, objlen);
 
     Py_ssize_t ressize = 0;
     for (Py_ssize_t i = start; i < end; ++i) {
