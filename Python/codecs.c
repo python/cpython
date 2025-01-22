39951be5d9c84171ad1e6200f09b807933c5662a
Bénédikt Tran <10796600+picnixz@users.noreply.github.com>
2025-01-22 14:44:12 +0100

    Revert "use memset instead of a for-loop"
    This reverts commit d71b3a592279455e733ef3b616ca98582d84bcbb.

--- Python/codecs.c
+++ Python/codecs.c
@@ -739,7 +739,9 @@ PyObject *PyCodec_ReplaceErrors(PyObject *exc)
         }
         assert(slen == 0 || PyUnicode_KIND(res) == PyUnicode_2BYTE_KIND);
         Py_UCS2 *outp = PyUnicode_2BYTE_DATA(res);
-        memset(outp, Py_UNICODE_REPLACEMENT_CHARACTER, sizeof(Py_UCS2) * slen);
+        for (Py_ssize_t i = 0; i < slen; ++i) {
+            outp[i] = Py_UNICODE_REPLACEMENT_CHARACTER;
+        }
         assert(_PyUnicode_CheckConsistency(res, 1));
         return Py_BuildValue("(Nn)", res, end);
     }
