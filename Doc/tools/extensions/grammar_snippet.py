ea9e2f093d0cd2c39611dc852dbe53b261065ba4
Petr Viktorin <encukou@gmail.com>
2025-01-22 16:42:16 +0100

    Turn the common function into a class with method; break make_link_to_token out

--- Doc/tools/extensions/grammar_snippet.py
+++ Doc/tools/extensions/grammar_snippet.py
@@ -7,7 +7,119 @@ from sphinx.util.docutils import SphinxDirective
 from sphinx.util.nodes import make_id
 
 
-class GrammarSnippetDirective(SphinxDirective):
+
+class GrammarSnippetBase(SphinxDirective):
+    """Common functionality for GrammarSnippetDirective & CompatProductionList.
+    """
+    # The option/argument handling is left to the individual classes.
+
+    def make_grammar_snippet(self, options, content):
+        """Create a literal block from options & content."""
+
+        group_name = options['group']
+
+        # Docutils elements have a `rawsource` attribute that is supposed to be
+        # set to the original ReST source.
+        # Sphinx does the following with it:
+        # - if it's empty, set it to `self.astext()`
+        # - if it matches `self.astext()` when generating the output,
+        #   apply syntax highlighting (which is based on the plain-text content
+        #   and thus discards internal formatting, like references).
+        # To get around this, we set it to this non-empty string:
+        rawsource = 'You should not see this.'
+
+        literal = nodes.literal_block(
+            rawsource,
+            '',
+            # TODO: Use a dedicated CSS class here and for strings.
+            # and add it to the theme too
+            classes=['highlight'],
+        )
+
+        grammar_re = re.compile(
+            """
+                (?P<rule_name>^[a-zA-Z0-9_]+)     # identifier at start of line
+                (?=:)                             # ... followed by a colon
+            |
+                [`](?P<rule_ref>[a-zA-Z0-9_]+)[`] # identifier in backquotes
+            |
+                (?P<single_quoted>'[^']*')        # string in 'quotes'
+            |
+                (?P<double_quoted>"[^"]*")        # string in "quotes"
+            """,
+            re.VERBOSE,
+        )
+
+        for line in content:
+            last_pos = 0
+            for match in grammar_re.finditer(line):
+                # Handle text between matches
+                if match.start() > last_pos:
+                    literal += nodes.Text(line[last_pos : match.start()])
+                last_pos = match.end()
+
+                # Handle matches
+                groupdict = {
+                    name: content
+                    for name, content in match.groupdict().items()
+                    if content is not None
+                }
+                match groupdict:
+                    case {'rule_name': name}:
+                        literal += self.make_link_to_token()
+                    case {'rule_ref': name}:
+                        ref_node = addnodes.pending_xref(
+                            name,
+                            reftype="token",
+                            refdomain="std",
+                            reftarget=f"{group_name}:{name}",
+                        )
+                        ref_node += nodes.Text(name)
+                        literal += ref_node
+                    case {'single_quoted': name} | {'double_quoted': name}:
+                        string_node = nodes.inline(classes=['nb'])
+                        string_node += nodes.Text(name)
+                        literal += string_node
+                    case _:
+                        raise ValueError('unhandled match')
+            literal += nodes.Text(line[last_pos:] + '\n')
+
+        node = nodes.paragraph(
+            '',
+            '',
+            literal,
+        )
+
+        return [node]
+
+    def make_link_to_token(self, group_name, name):
+        """Return a literal node that links to the given grammar token"""
+        name_node = addnodes.literal_strong()
+
+        # Cargo-culted magic to make `name_node` a link target
+        # similar to Sphinx `production`.
+        # This needs to be the same as what Sphinx does
+        # to avoid breaking existing links.
+        domain = self.env.domains['std']
+        obj_name = f"{group_name}:{name}"
+        prefix = f'grammar-token-{group_name}'
+        node_id = make_id(
+            self.env, self.state.document, prefix, name
+        )
+        name_node['ids'].append(node_id)
+        self.state.document.note_implicit_target(
+            name_node, name_node
+        )
+        domain.note_object(
+            'token', obj_name, node_id, location=name_node
+        )
+
+        text_node = nodes.Text(name)
+        name_node += text_node
+        return name_node
+
+
+class GrammarSnippetDirective(GrammarSnippetBase):
     """Transform a grammar-snippet directive to a Sphinx literal_block
 
     That is, turn something like:
@@ -40,116 +152,10 @@ class GrammarSnippetDirective(SphinxDirective):
     final_argument_whitespace = True
 
     def run(self):
-        return make_snippet(self, self.options, self.content)
+        return self.make_grammar_snippet(self.options, self.content)
 
 
-def make_snippet(directive, options, content):
-    """Create a literal block from options & content.
-
-    This implements the common functionality for GrammarSnippetDirective
-    and CompatProductionList.
-    """
-
-    group_name = options['group']
-
-    # Docutils elements have a `rawsource` attribute that is supposed to be
-    # set to the original ReST source.
-    # Sphinx does the following with it:
-    # - if it's empty, set it to `self.astext()`
-    # - if it matches `self.astext()` when generating the output,
-    #   apply syntax highlighting (which is based on the plain-text content
-    #   and thus discards internal formatting, like references).
-    # To get around this, we set it to this non-empty string:
-    rawsource = 'You should not see this.'
-
-    literal = nodes.literal_block(
-        rawsource,
-        '',
-        # TODO: Use a dedicated CSS class here and for strings.
-        # and add it to the theme too
-        classes=['highlight'],
-    )
-
-    grammar_re = re.compile(
-        """
-            (?P<rule_name>^[a-zA-Z0-9_]+)     # identifier at start of line
-            (?=:)                             # ... followed by a colon
-        |
-            [`](?P<rule_ref>[a-zA-Z0-9_]+)[`] # identifier in backquotes
-        |
-            (?P<single_quoted>'[^']*')        # string in 'quotes'
-        |
-            (?P<double_quoted>"[^"]*")        # string in "quotes"
-        """,
-        re.VERBOSE,
-    )
-
-    for line in content:
-        last_pos = 0
-        for match in grammar_re.finditer(line):
-            # Handle text between matches
-            if match.start() > last_pos:
-                literal += nodes.Text(line[last_pos : match.start()])
-            last_pos = match.end()
-
-            # Handle matches
-            groupdict = {
-                name: content
-                for name, content in match.groupdict().items()
-                if content is not None
-            }
-            match groupdict:
-                case {'rule_name': name}:
-                    name_node = addnodes.literal_strong()
-
-                    # Cargo-culted magic to make `name_node` a link target
-                    # similar to Sphinx `production`.
-                    # This needs to be the same as what Sphinx does
-                    # to avoid breaking existing links.
-                    domain = directive.env.domains['std']
-                    obj_name = f"{group_name}:{name}"
-                    prefix = f'grammar-token-{group_name}'
-                    node_id = make_id(
-                        directive.env, directive.state.document, prefix, name
-                    )
-                    name_node['ids'].append(node_id)
-                    directive.state.document.note_implicit_target(
-                        name_node, name_node
-                    )
-                    domain.note_object(
-                        'token', obj_name, node_id, location=name_node
-                    )
-
-                    text_node = nodes.Text(name)
-                    name_node += text_node
-                    literal += name_node
-                case {'rule_ref': name}:
-                    ref_node = addnodes.pending_xref(
-                        name,
-                        reftype="token",
-                        refdomain="std",
-                        reftarget=f"{group_name}:{name}",
-                    )
-                    ref_node += nodes.Text(name)
-                    literal += ref_node
-                case {'single_quoted': name} | {'double_quoted': name}:
-                    string_node = nodes.inline(classes=['nb'])
-                    string_node += nodes.Text(name)
-                    literal += string_node
-                case _:
-                    raise ValueError('unhandled match')
-        literal += nodes.Text(line[last_pos:] + '\n')
-
-    node = nodes.paragraph(
-        '',
-        '',
-        literal,
-    )
-
-    return [node]
-
-
-class CompatProductionList(SphinxDirective):
+class CompatProductionList(GrammarSnippetBase):
     """Create grammar snippets from ReST productionlist syntax
 
     This is intended to be a transitional directive, used while we switch
@@ -181,7 +187,7 @@ class CompatProductionList(SphinxDirective):
             else:
                 name_part = ''
             content.append(f'{name_part:<{align_column}}{text}')
-        return make_snippet(self, options, content)
+        return self.make_grammar_snippet(options, content)
 
 
 def setup(app):
