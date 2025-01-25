15da9c7a4280287df4624f51b83043c57619c82e
Ken Jin <28750310+Fidget-Spinner@users.noreply.github.com>
2025-01-25 11:23:19 +0800

    Squashed commit of the following:
    commit f89f147940645823bda29669b7870adb67530cdc
    Author: Ken Jin <28750310+Fidget-Spinner@users.noreply.github.com>
    Date:   Sat Jan 25 11:18:43 2025 +0800
    
        Address review
    
    commit e4a9de79140b7f5df66db581a9579616dbe942d5
    Author: Ken Jin <28750310+Fidget-Spinner@users.noreply.github.com>
    Date:   Thu Jan 23 02:05:13 2025 +0800
    
        Regen files
    
    commit 5d561305ee0db2c72e581b95751e80b2bd23c2bb
    Author: Ken Jin <28750310+Fidget-Spinner@users.noreply.github.com>
    Date:   Thu Jan 23 02:00:44 2025 +0800
    
        Address review by removing in test cases generator
    
    commit e1f94758cdec9d071e3951c5dad1b711e9bdd02e
    Author: Ken Jin <28750310+Fidget-Spinner@users.noreply.github.com>
    Date:   Wed Jan 22 09:39:54 2025 +0800
    
        Remove entry_frame
    
    commit 15ca6dd3927e599f884c7c4528a7bf280cea632f
    Author: Ken Jin <28750310+Fidget-Spinner@users.noreply.github.com>
    Date:   Wed Jan 22 09:27:37 2025 +0800
    
        fix upstream changes
    
    commit fdd45409d80d29e1933e3530b1b9559f1437c199
    Merge: 1ec80334516 86c1a60d5a2
    Author: Ken Jin <28750310+Fidget-Spinner@users.noreply.github.com>
    Date:   Wed Jan 22 09:27:31 2025 +0800
    
        Merge remote-tracking branch 'upstream/main' into labels-in-dsl
    
    commit 1ec803345165dc6b917e4ec66b90da364e9efa87
    Author: Ken Jin <28750310+Fidget-Spinner@users.noreply.github.com>
    Date:   Tue Jan 21 20:16:42 2025 +0800
    
        Lint
    
    commit c6df7a1285e76c0c92fdefb88478f24b2c00c90a
    Author: Ken Jin <28750310+Fidget-Spinner@users.noreply.github.com>
    Date:   Tue Jan 21 20:14:10 2025 +0800
    
        cleanup diff
    
    commit 63a88ab5677d70f47392b3868d148c2765e85c37
    Author: Ken Jin <28750310+Fidget-Spinner@users.noreply.github.com>
    Date:   Tue Jan 21 20:13:12 2025 +0800
    
        remove outdated file
    
    commit b911bb1bb60577e9708d3a92e9f17298d5f7d11b
    Author: Ken Jin <28750310+Fidget-Spinner@users.noreply.github.com>
    Date:   Tue Jan 21 20:11:27 2025 +0800
    
        Move labels into tier 1 generator
    
    commit af4bf1a34d389bff47761eec7d2dd7f61973b421
    Author: Ken Jin <28750310+Fidget-Spinner@users.noreply.github.com>
    Date:   Tue Jan 21 15:52:53 2025 +0800
    
        Add to generated files
    
    commit 3ae88a5b8863063b84fcf0de2cc131b10822d415
    Author: Ken Jin <28750310+Fidget-Spinner@users.noreply.github.com>
    Date:   Tue Jan 21 09:01:39 2025 +0800
    
        lint
    
    commit 5cccb986ba393cf38643b7ccb0b2d5cc28567188
    Author: Ken Jin <28750310+Fidget-Spinner@users.noreply.github.com>
    Date:   Tue Jan 21 08:58:53 2025 +0800
    
        Fix mypy
    
    commit 20825373364ceec32fd3a115e3814f3d08420b16
    Author: Ken Jin <28750310+Fidget-Spinner@users.noreply.github.com>
    Date:   Tue Jan 21 08:54:50 2025 +0800
    
        Move labels in ceval.c to bytecodes.c

--- Tools/cases_generator/analyzer.py
+++ Tools/cases_generator/analyzer.py
@@ -256,6 +256,12 @@ class Instruction:
 
 
 @dataclass
+class Label:
+    name: str
+    body: list[lexer.Token]
+
+
+@dataclass
 class PseudoInstruction:
     name: str
     stack: StackEffect
@@ -288,6 +294,7 @@ class Analysis:
     uops: dict[str, Uop]
     families: dict[str, Family]
     pseudos: dict[str, PseudoInstruction]
+    labels: dict[str, Label]
     opmap: dict[str, int]
     have_arg: int
     min_instrumented: int
@@ -949,6 +956,13 @@ def add_pseudo(
     )
 
 
+def add_label(
+    label: parser.LabelDef,
+    labels: dict[str, Label],
+) -> None:
+    labels[label.name] = Label(label.name, label.block.tokens)
+
+
 def assign_opcodes(
     instructions: dict[str, Instruction],
     families: dict[str, Family],
@@ -1067,6 +1081,7 @@ def analyze_forest(forest: list[parser.AstNode]) -> Analysis:
     uops: dict[str, Uop] = {}
     families: dict[str, Family] = {}
     pseudos: dict[str, PseudoInstruction] = {}
+    labels: dict[str, Label] = {}
     for node in forest:
         match node:
             case parser.InstDef(name):
@@ -1081,6 +1096,8 @@ def analyze_forest(forest: list[parser.AstNode]) -> Analysis:
                 pass
             case parser.Pseudo():
                 pass
+            case parser.LabelDef():
+                pass
             case _:
                 assert False
     for node in forest:
@@ -1092,6 +1109,8 @@ def analyze_forest(forest: list[parser.AstNode]) -> Analysis:
                 add_family(node, instructions, families)
             case parser.Pseudo():
                 add_pseudo(node, instructions, pseudos)
+            case parser.LabelDef():
+                add_label(node, labels)
             case _:
                 pass
     for uop in uops.values():
@@ -1117,7 +1136,7 @@ def analyze_forest(forest: list[parser.AstNode]) -> Analysis:
         families["BINARY_OP"].members.append(inst)
     opmap, first_arg, min_instrumented = assign_opcodes(instructions, families, pseudos)
     return Analysis(
-        instructions, uops, families, pseudos, opmap, first_arg, min_instrumented
+        instructions, uops, families, pseudos, labels, opmap, first_arg, min_instrumented
     )
 
 
