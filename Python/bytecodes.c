c39ae8922bad3e5ceeafa05891536c1584b6f3db
Irit Katriel <1055913+iritkatriel@users.noreply.github.com>
2025-01-25 13:00:23 +0000

    gh-128799: Add frame of except* to traceback when wrapping a naked exception (#128971)

--- Python/bytecodes.c
+++ Python/bytecodes.c
@@ -2717,7 +2717,7 @@ dummy_func(
 
             PyObject *match_o = NULL;
             PyObject *rest_o = NULL;
-            int res = _PyEval_ExceptionGroupMatch(exc_value, match_type,
+            int res = _PyEval_ExceptionGroupMatch(frame, exc_value, match_type,
                                                   &match_o, &rest_o);
             DECREF_INPUTS();
             ERROR_IF(res < 0, error);
