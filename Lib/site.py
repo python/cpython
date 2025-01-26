ca98ad77d5f17977ee6b7b79c7b5020e158b8481
Victor Stinner <vstinner@python.org>
2025-01-26 12:14:22 +0100

    Apply Filipe's suggestion

--- Lib/site.py
+++ Lib/site.py
@@ -633,14 +633,9 @@ def venv(known_paths):
         # Doing this here ensures venv takes precedence over user-site
         addsitepackages(known_paths, [sys.prefix])
 
-        # addsitepackages will process site_prefix again if its in PREFIXES,
-        # but that's ok; known_paths will prevent anything being added twice
         if system_site == "true":
-            PREFIXES.insert(0, sys.prefix)
-            if sys.base_exec_prefix != sys.exec_prefix:
-                PREFIXES.append(sys.base_exec_prefix)
+            PREFIXES += [sys.base_prefix, sys.base_exec_prefix]
         else:
-            PREFIXES = [sys.prefix]
             ENABLE_USER_SITE = False
 
     return known_paths
