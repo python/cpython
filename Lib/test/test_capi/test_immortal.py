10560ef620b3474df9042416f60c4c25e11f3068
Peter Bierma <zintensitydev@gmail.com>
2025-01-27 10:30:31 -0500

    Revert "Test for sys._is_immortal in the existing case."
    This reverts commit 64c9a5209425a16d7790c428c608924d4634e472.

--- Lib/test/test_capi/test_immortal.py
+++ Lib/test/test_capi/test_immortal.py
@@ -1,31 +1,25 @@
 import unittest
 from test.support import import_helper
-import sys
 
 _testcapi = import_helper.import_module('_testcapi')
 _testinternalcapi = import_helper.import_module('_testinternalcapi')
 
 
-class TestImmortalAPI(unittest.TestCase):
-    def immortal_checking(self, func):
+class TestUnstableCAPI(unittest.TestCase):
+    def test_immortal(self):
         # Not extensive
         known_immortals = (True, False, None, 0, ())
         for immortal in known_immortals:
             with self.subTest(immortal=immortal):
-                self.assertTrue(func(immortal))
+                self.assertTrue(_testcapi.is_immortal(immortal))
 
         # Some arbitrary mutable objects
         non_immortals = (object(), self, [object()])
         for non_immortal in non_immortals:
             with self.subTest(non_immortal=non_immortal):
-                self.assertFalse(func(non_immortal))
+                self.assertFalse(_testcapi.is_immortal(non_immortal))
 
-    def test_unstable_c_api(self):
-        self.immortal_checking(_testcapi.is_immortal)
         # CRASHES _testcapi.is_immortal(NULL)
-
-    def test_sys(self):
-        self.immortal_checking(sys._is_immortal)
 
 
 class TestInternalCAPI(unittest.TestCase):
