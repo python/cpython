1220739c4797a4f33ee2ab777526e45bbc7e2e41
Barney Gale <barney.gale@gmail.com>
2025-01-28 20:22:55 +0000

    GH-127381: pathlib ABCs: remove `JoinablePath.match()` (#129147)
    Unlike `ReadablePath.[r]glob()` and `JoinablePath.full_match()`, the
    `JoinablePath.match()` method doesn't support the recursive wildcard `**`,
    and matches from the right when a fully relative pattern is given. These
    quirks means its probably unsuitable for inclusion in the pathlib ABCs,
    especially given `full_match()` handles the same use case.

--- Lib/pathlib/_abc.py
+++ Lib/pathlib/_abc.py
@@ -358,33 +358,6 @@ class JoinablePath:
             parent = split(path)[0]
         return tuple(parents)
 
-    def match(self, path_pattern, *, case_sensitive=None):
-        """
-        Return True if this path matches the given pattern. If the pattern is
-        relative, matching is done from the right; otherwise, the entire path
-        is matched. The recursive wildcard '**' is *not* supported by this
-        method.
-        """
-        if not isinstance(path_pattern, JoinablePath):
-            path_pattern = self.with_segments(path_pattern)
-        if case_sensitive is None:
-            case_sensitive = _is_case_sensitive(self.parser)
-        sep = path_pattern.parser.sep
-        path_parts = self.parts[::-1]
-        pattern_parts = path_pattern.parts[::-1]
-        if not pattern_parts:
-            raise ValueError("empty pattern")
-        if len(path_parts) < len(pattern_parts):
-            return False
-        if len(path_parts) > len(pattern_parts) and path_pattern.anchor:
-            return False
-        globber = PathGlobber(sep, case_sensitive)
-        for path_part, pattern_part in zip(path_parts, pattern_parts):
-            match = globber.compile(pattern_part)
-            if match(path_part) is None:
-                return False
-        return True
-
     def full_match(self, pattern, *, case_sensitive=None):
         """
         Return True if this path matches the given glob-style pattern. The
