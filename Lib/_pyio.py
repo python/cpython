2bd94d3e2ee8efef6f808b298f62db9475417768
Cody Maloney <cmaloney@users.noreply.github.com>
2025-01-28 03:40:44 -0800

    gh-129005: Avoid copy in _pyio.FileIO.readinto() (#129324)
    `os.read()` allocated and filled a buffer by calling `read(2)`, than that
    data was copied into the user provied buffer. Read directly into the
    caller's buffer instead by using `os.readinto()`.
    
    `os.readinto()` uses `PyObject_GetBuffer()` to make sure the passed
    in buffer is writeable and bytes-like, drop the manual check.

--- Lib/_pyio.py
+++ Lib/_pyio.py
@@ -1692,13 +1692,14 @@ class FileIO(RawIOBase):
 
         return bytes(result)
 
-    def readinto(self, b):
+    def readinto(self, buffer):
         """Same as RawIOBase.readinto()."""
-        m = memoryview(b).cast('B')
-        data = self.read(len(m))
-        n = len(data)
-        m[:n] = data
-        return n
+        self._checkClosed()
+        self._checkReadable()
+        try:
+            return os.readinto(self._fd, buffer)
+        except BlockingIOError:
+            return None
 
     def write(self, b):
         """Write bytes b to file, return number written.
