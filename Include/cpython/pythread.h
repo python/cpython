11212711209b5fea5c4f1d19784c447a743bf807
Zanie Blue <contact@zanie.dev>
2025-01-29 09:06:19 -0600

    gh-129296: Fix `pythread.h` include paths (#129320)
    Use relative includes in Include/cpython/pythread.h for
    pthread_stubs.h.
    
    (cherry picked from commit 3a974e39d54902699f360bc4db2fd351a6baf3ef)

--- Include/cpython/pythread.h
+++ Include/cpython/pythread.h
@@ -21,7 +21,7 @@ PyAPI_FUNC(int) _PyThread_at_fork_reinit(PyThread_type_lock *lock);
     */
 #   define NATIVE_TSS_KEY_T     unsigned long
 #elif defined(HAVE_PTHREAD_STUBS)
-#   include "cpython/pthread_stubs.h"
+#   include "pthread_stubs.h"
 #   define NATIVE_TSS_KEY_T     pthread_key_t
 #else
 #   error "Require native threads. See https://bugs.python.org/issue31370"
