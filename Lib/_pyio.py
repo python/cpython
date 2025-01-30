ba8f61fd5faabfbd8f0cfc92906660fa1ae5ab8c
Cody Maloney <cmaloney@users.noreply.github.com>
2025-01-30 03:23:25 -0800

    gh-129005: _pyio.BufferedIO remove copy on readall (#129454)
    Slicing buf and appending chunk would always result in a copy. Commonly
    in a readall() there is no already read data in buf, and the amount of
    data read may be large, so the copy is expensive.

--- Lib/_pyio.py
+++ Lib/_pyio.py
@@ -1062,6 +1062,9 @@ class BufferedReader(_BufferedIOMixin):
                 if chunk is None:
                     return buf[pos:] or None
                 else:
+                    # Avoid slice + copy if there is no data in buf
+                    if not buf:
+                        return chunk
                     return buf[pos:] + chunk
             chunks = [buf[pos:]]  # Strip the consumed bytes.
             current_size = 0
