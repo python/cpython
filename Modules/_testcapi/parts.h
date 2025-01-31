5424e3b0348c6029e96d4cb106cbcd5cca86d980
Victor Stinner <vstinner@python.org>
2025-01-31 15:03:54 +0100

    gh-93649: Add Modules/_testcapi/type.c file (#129516)
    Move PyType C API tests to a new file.
    
    Move following tests from test_capi.test_misc to test_capi.test_type:
    
    * BuiltinStaticTypesTests
    * test_get_type_name()
    * test_get_base_by_token()

--- Modules/_testcapi/parts.h
+++ Modules/_testcapi/parts.h
@@ -63,5 +63,6 @@ int _PyTestCapi_Init_Object(PyObject *module);
 int _PyTestCapi_Init_Config(PyObject *mod);
 int _PyTestCapi_Init_Import(PyObject *mod);
 int _PyTestCapi_Init_Frame(PyObject *mod);
+int _PyTestCapi_Init_Type(PyObject *mod);
 
 #endif // Py_TESTCAPI_PARTS_H
