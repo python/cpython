000253287a7ac28992f9a5e7d9e28ab89ea57efa
Valery Fedorenko <federicovalenso@gmail.com>
2025-01-31 22:36:30 +0800

    gh-126108: Fix potential null pointer dereference in `PySys_AddWarnOptionUnicode` (#126118)

--- Python/sysmodule.c
+++ Python/sysmodule.c
@@ -2847,6 +2847,7 @@ PySys_ResetWarnOptions(void)
 static int
 _PySys_AddWarnOptionWithError(PyThreadState *tstate, PyObject *option)
 {
+    assert(tstate != NULL);
     PyObject *warnoptions = get_warnoptions(tstate);
     if (warnoptions == NULL) {
         return -1;
@@ -2862,11 +2863,11 @@ PyAPI_FUNC(void)
 PySys_AddWarnOptionUnicode(PyObject *option)
 {
     PyThreadState *tstate = _PyThreadState_GET();
+    _Py_EnsureTstateNotNULL(tstate);
+    assert(!_PyErr_Occurred(tstate));
     if (_PySys_AddWarnOptionWithError(tstate, option) < 0) {
         /* No return value, therefore clear error state if possible */
-        if (tstate) {
-            _PyErr_Clear(tstate);
-        }
+        _PyErr_Clear(tstate);
     }
 }
 
