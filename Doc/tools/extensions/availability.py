f273e6edd9a08953d133b726bb0f18b9025f9bae
Maciej Olko <maciej.olko@affirm.com>
2025-02-03 22:40:56 +0100

    Rename formatting function in a call
    Co-authored-by: Hugo van Kemenade <1324225+hugovk@users.noreply.github.com>

--- Doc/tools/extensions/availability.py
+++ Doc/tools/extensions/availability.py
@@ -71,7 +71,7 @@ class Availability(SphinxDirective):
         )
         sep = nodes.Text(": ")
         platforms = self.parse_platforms()
-        platforms_text = f"{format_list(list(starmap(_print_platform, platforms.items())), locale=self.config.language)}."
+        platforms_text = f"{format_list(list(starmap(_format_platform, platforms.items())), locale=self.config.language)}."
         parsed, msgs = self.state.inline_text(platforms_text, self.lineno)
         pnode = nodes.paragraph(title, "", refnode, sep, *parsed, *msgs)
         self.set_source_info(pnode)
