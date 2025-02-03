31f1d4ebda3f69e96b82c2e2947e4b514e858d5e
Yan Yanchii <yyanchiy@gmail.com>
2025-02-03 10:32:38 +0100

    rename macro

--- Python/codegen.c
+++ Python/codegen.c
@@ -284,7 +284,7 @@ codegen_addop_load_const(compiler *c, location loc, PyObject *o)
     if (PyLong_CheckExact(o)) {
         int overflow;
         long val = PyLong_AsLongAndOverflow(o, &overflow);
-        if (!overflow && _IS_LOAD_SMALL_INT(val)) {
+        if (!overflow && _PY_IS_SMALL_INT(val)) {
             ADDOP_I(c, loc, LOAD_SMALL_INT, val);
             return SUCCESS;
         }
