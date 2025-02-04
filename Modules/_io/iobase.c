0020046f93656fd7121d4ffb135ef78f97974b76
Victor Stinner <vstinner@python.org>
2025-02-04 16:21:10 +0100

    gh-129354: Use PyErr_FormatUnraisable() function (#129656)
    Replace PyErr_WriteUnraisable() with PyErr_FormatUnraisable().

--- Modules/_io/iobase.c
+++ Modules/_io/iobase.c
@@ -314,7 +314,8 @@ iobase_finalize(PyObject *self)
             PyErr_Clear();
         res = PyObject_CallMethodNoArgs((PyObject *)self, &_Py_ID(close));
         if (res == NULL) {
-            PyErr_WriteUnraisable(self);
+            PyErr_FormatUnraisable("Exception ignored "
+                                   "while finalizing file %R", self);
         }
         else {
             Py_DECREF(res);
--- Modules/socketmodule.c
+++ Modules/socketmodule.c
@@ -5359,7 +5359,8 @@ sock_finalize(PyObject *self)
         if (PyErr_ResourceWarning((PyObject *)s, 1, "unclosed %R", s)) {
             /* Spurious errors can appear at shutdown */
             if (PyErr_ExceptionMatches(PyExc_Warning)) {
-                PyErr_WriteUnraisable((PyObject *)s);
+                PyErr_FormatUnraisable("Exception ignored while "
+                                       "finalizing socket %R", s);
             }
         }
 
