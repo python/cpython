e8f2896ee1d9fea0304e7263328fe39fced9a23a
Tomasz Pytel <tompytel@gmail.com>
2025-02-04 08:08:01 -0500

    test cleanup

--- Lib/test/test_unparse.py
+++ Lib/test/test_unparse.py
@@ -570,7 +570,10 @@ class UnparseTestCase(ASTTestCase):
             "def f(a=2, *args, c=5, d, **kwds):",
             "def f(*args, **kwargs):",
         ):
-            self.check_src_roundtrip(statement + '\n i=1;j=2', statement + '\n    i = 1\n    j = 2', mode='single')
+            src = statement + '\n i=1;j=2'
+            out = statement + '\n    i = 1\n    j = 2'
+
+            self.check_src_roundtrip(src, out, mode='single')
 
 
 class CosmeticTestCase(ASTTestCase):
