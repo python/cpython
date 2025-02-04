49f02be2a35085e1e3a867251e44688c18af2c84
Adam Turner <9087854+aa-turner@users.noreply.github.com>
2025-02-04 11:12:40 +0000

    Standardise workflow variables:
    * Rename `config_hash` to `config-hash`
    * Rename `run_tests` to `run-tests`
    * Rename `run-win-msi` to `run-windows-msi`
    * Rename `run_hypothesis` to `run-hypothesis`
    * Rename `run_cifuzz` to `run-ci-fuzz`

--- Tools/build/compute-changes.py
+++ Tools/build/compute-changes.py
@@ -180,11 +180,11 @@ def write_github_output(outputs: Outputs) -> None:
         return
 
     with open(os.environ["GITHUB_OUTPUT"], "a", encoding="utf-8") as f:
-        f.write(f"run-cifuzz={bool_lower(outputs.run_ci_fuzz)}\n")
+        f.write(f"run-ci-fuzz={bool_lower(outputs.run_ci_fuzz)}\n")
         f.write(f"run-docs={bool_lower(outputs.run_docs)}\n")
         f.write(f"run-hypothesis={bool_lower(outputs.run_hypothesis)}\n")
         f.write(f"run-tests={bool_lower(outputs.run_tests)}\n")
-        f.write(f"run-win-msi={bool_lower(outputs.run_windows_msi)}\n")
+        f.write(f"run-windows-msi={bool_lower(outputs.run_windows_msi)}\n")
 
 
 def bool_lower(value: bool, /) -> str:
