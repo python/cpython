50389df0a5f34d5d8ec0017ca9f4e341f0f89761
Cody Maloney <cmaloney@theoreticalchaos.com>
2025-02-05 12:52:46 -0800

    gh-12005: Align FileIO.readall between _pyio and _io
    Utilize `bytearray.resize()` and `os.readinto()` to reduce copies
    and match behavior of `_io.FileIO.readall()`.
    
    There is still an extra copy which means twice the memory required
    compared to FileIO because there isn't a zero-copy  path from
    `bytearray` -> `bytes` currently.
    
    On my system reading a 2GB file
    `./python -m test -M8g -uall test_largefile -m test.test_largefile.PyLargeFileTest.test_large_read -v`
    
    Goes from ~2.7 seconds -> ~2.2 seconds

--- Lib/_pyio.py
+++ Lib/_pyio.py
@@ -1674,22 +1674,31 @@ class FileIO(RawIOBase):
                 except OSError:
                     pass
 
-        result = bytearray()
+        result = bytearray(bufsize)
+        bytes_read = 0
         while True:
-            if len(result) >= bufsize:
-                bufsize = len(result)
-                bufsize += max(bufsize, DEFAULT_BUFFER_SIZE)
-            n = bufsize - len(result)
+            if bytes_read >= bufsize:
+                # Parallels _io/fileio.c new_buffersize
+                if bufsize > 65536:
+                    addend = bufsize >> 3
+                else:
+                    addend = 256 + bufsize
+                if addend < DEFAULT_BUFFER_SIZE:
+                    addend = DEFAULT_BUFFER_SIZE
+                bufsize += addend
+                result.resize(bufsize)
+
+            assert bufsize - bytes_read > 0, "Should always try and read at least one byte"
             try:
-                chunk = os.read(self._fd, n)
+                n = os.readinto(self._fd, memoryview(result)[bytes_read:])
             except BlockingIOError:
-                if result:
+                if bytes_read:
                     break
                 return None
-            if not chunk: # reached the end of the file
+            if n == 0:  # Reached the end of the file
                 break
-            result += chunk
-
+            bytes_read += n
+        result.resize(bytes_read)
         return bytes(result)
 
     def readinto(self, buffer):
