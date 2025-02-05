024d343642cee6dd91834b5c48c027b2026eff2b
Andrey Efremov <duxus@yandex.ru>
2025-02-05 15:22:18 +0700

    ConfigParser: do not write an empty unnamed section

--- Lib/test/test_configparser.py
+++ Lib/test/test_configparser.py
@@ -2161,6 +2161,13 @@ class SectionlessTestCase(unittest.TestCase):
         self.assertEqual('1', cfg2[configparser.UNNAMED_SECTION]['a'])
         self.assertEqual('2', cfg2[configparser.UNNAMED_SECTION]['b'])
 
+    def test_empty_unnamed_section(self):
+        expected_output = '[sect]\n\n'
+        cfg = self.fromstring(expected_output)
+        output = io.StringIO()
+        cfg.write(output)
+        self.assertEqual(output.getvalue(), expected_output)
+
     def test_add_section(self):
         cfg = configparser.ConfigParser(allow_unnamed_section=True)
         cfg.add_section(configparser.UNNAMED_SECTION)
