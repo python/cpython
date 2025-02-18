fc3e457659bc750fd221b14a6bd7d93dd9174763
Hugo van Kemenade <1324225+hugovk@users.noreply.github.com>
2025-02-18 18:29:59 +0200

    Fix for 3.9 branch

--- Lib/tkinter/test/test_tkinter/test_widgets.py
+++ Lib/tkinter/test/test_tkinter/test_widgets.py
@@ -651,7 +651,7 @@ class TextTest(AbstractWidgetTest, unittest.TestCase):
             self.checkParam(widget, 'tabs', (10.2, 20.7, '1i', '2i'))
         self.checkParam(widget, 'tabs', '10.2 20.7 1i 2i',
                         expected=(10.2, 20.7, '1i', '2i')
-                                 if get_tk_patchlevel(self.root) >= (8, 6, 14)
+                                 if get_tk_patchlevel() >= (8, 6, 14)
                                  else ('10.2', '20.7', '1i', '2i'))
         self.checkParam(widget, 'tabs', '2c left 4c 6c center',
                         expected=('2c', 'left', '4c', '6c', 'center'))
@@ -832,7 +832,7 @@ class ListboxTest(AbstractWidgetTest, unittest.TestCase):
             widget.itemconfigure()
         with self.assertRaisesRegex(TclError, 'bad listbox index "red"'):
             widget.itemconfigure('red')
-        if get_tk_patchlevel(self.root) >= (8, 6, 14):
+        if get_tk_patchlevel() >= (8, 6, 14):
             prefix = ('background', '', '', '')
         else:
             prefix = ('background', 'background', 'Background', '')
