4055dbed316e0a10b71ba6a8876d16fde8a0b98b
donBarbos <donbarbos@proton.me>
2025-02-18 04:55:53 +0400

    Improve speed of ftplib.parse150 by replacing re

--- Lib/ftplib.py
+++ Lib/ftplib.py
@@ -787,24 +787,24 @@ else:
     all_errors = (Error, OSError, EOFError, ssl.SSLError)
 
 
-_150_re = None
-
 def parse150(resp):
     '''Parse the '150' response for a RETR request.
     Returns the expected transfer size or None; size is not guaranteed to
     be present in the 150 message.
     '''
-    if resp[:3] != '150':
+    if not resp.startswith('150'):
         raise error_reply(resp)
-    global _150_re
-    if _150_re is None:
-        import re
-        _150_re = re.compile(
-            r"150 .* \((\d+) bytes\)", re.IGNORECASE | re.ASCII)
-    m = _150_re.match(resp)
-    if not m:
+
+    start = resp.find('(')
+    end = resp.find(' bytes)')
+
+    if start == -1 or end == -1 or start >= end:
         return None
-    return int(m.group(1))
+
+    try:
+        return int(resp[start + 1:end])
+    except ValueError:
+        return None
 
 
 _227_re = None
