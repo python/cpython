a527aaf4e459146c1514940ccbeda9634a92fbf2
Mark Shannon <mark@hotpy.org>
2025-02-18 15:17:53 +0000

    Fix immortality test

--- Python/sysmodule.c
+++ Python/sysmodule.c
@@ -1954,9 +1954,6 @@ static Py_ssize_t
 sys_getrefcount_impl(PyObject *module, PyObject *object)
 /*[clinic end generated code: output=5fd477f2264b85b2 input=bf474efd50a21535]*/
 {
-    if (_Py_IsImmortal(object)) {
-        return _Py_IMMORTAL_INITIAL_REFCNT;
-    }
     return Py_REFCNT(object);
 }
 
