4e0e7029616f29a3eebe2880b65d0e809c210387
Raymond Hettinger <rhettinger@users.noreply.github.com>
2025-02-21 11:33:10 -0600

    gh-130285: Fix handling of zero or empty counts in random.sample() (gh-130291) (cherry picked from commit 286c517db0fedf8d923d6f9620bcbbac4333880d)
    Co-authored-by: Raymond Hettinger <rhettinger@users.noreply.github.com>

--- Lib/random.py
+++ Lib/random.py
@@ -417,11 +417,11 @@ class Random(_random.Random):
             cum_counts = list(_accumulate(counts))
             if len(cum_counts) != n:
                 raise ValueError('The number of counts does not match the population')
-            total = cum_counts.pop()
+            total = cum_counts.pop() if cum_counts else 0
             if not isinstance(total, int):
                 raise TypeError('Counts must be integers')
-            if total <= 0:
-                raise ValueError('Total of counts must be greater than zero')
+            if total < 0:
+                raise ValueError('Counts must be non-negative')
             selections = self.sample(range(total), k=k)
             bisect = _bisect
             return [population[bisect(cum_counts, s)] for s in selections]
