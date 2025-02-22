80c1c6cd0621d7f1bc258bd42a5918eab2f6cddb
fluhus <amitlavon1.spam@gmail.com>
2025-02-22 12:38:37 -0800

    Add failing regression test for _TO_BOOL_STR

--- Lib/test/test_capi/test_opt.py
+++ Lib/test/test_capi/test_opt.py
@@ -1499,6 +1499,39 @@ class TestUopsOptimization(unittest.TestCase):
         # But all of the appends we care about are still there:
         self.assertEqual(uops.count("_CALL_LIST_APPEND"), len("ABCDEFG"))
 
+    def test_narrow_type_to_constant_str_empty(self):
+        def f(n):
+            trace = []
+            for i in range(n):
+                empty = ""
+                # Hopefully the optimizer can't guess what the value is.
+                # f is always "", but we can only prove that it's a string:
+                f = empty + empty
+                trace.append("A")
+                if not f:  # Kept.
+                    trace.append("B")
+                    if not f:  # Removed!
+                        trace.append("C")
+                    trace.append("D")
+                    if f:  # Removed!
+                        trace.append("X")
+                    trace.append("E")
+                trace.append("F")
+                if f:  # Removed!
+                    trace.append("X")
+                trace.append("G")
+            return trace
+
+        trace, ex = self._run_with_optimizer(f, TIER2_THRESHOLD)
+        self.assertEqual(trace, list("ABCDEFG") * TIER2_THRESHOLD)
+        self.assertIsNotNone(ex)
+        uops = get_opnames(ex)
+        # Only one guard remains:
+        self.assertEqual(uops.count("_GUARD_IS_FALSE_POP"), 1)
+        self.assertEqual(uops.count("_GUARD_IS_TRUE_POP"), 0)
+        # But all of the appends we care about are still there:
+        self.assertEqual(uops.count("_CALL_LIST_APPEND"), len("ABCDEFG"))
+
 
 def global_identity(x):
     return x
