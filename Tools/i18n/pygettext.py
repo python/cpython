a3ef55bc629d77a0a673d94621d77dac61bb07b4
Tomas Roun <tomas.roun8@gmail.com>
2025-02-24 20:55:46 +0100

    Revert changes to parse_spec

--- Tools/i18n/pygettext.py
+++ Tools/i18n/pygettext.py
@@ -352,28 +352,28 @@ def parse_spec(spec):
             raise ValueError(f'Invalid keyword spec {spec!r}: '
                              'argument positions must be strictly positive')
 
-        if pos in result:
+        if pos in result.values():
             raise ValueError(f'Invalid keyword spec {spec!r}: '
                              'duplicate positions')
 
         if is_context:
-            if 'msgctxt' in result.values():
+            if 'msgctxt' in result:
                 raise ValueError(f'Invalid keyword spec {spec!r}: '
                                  'msgctxt can only appear once')
-            result[pos] = 'msgctxt'
-        elif 'msgid' not in result.values():
-            result[pos] = 'msgid'
-        elif 'msgid_plural' not in result.values():
-            result[pos] = 'msgid_plural'
+            result['msgctxt'] = pos
+        elif 'msgid' not in result:
+            result['msgid'] = pos
+        elif 'msgid_plural' not in result:
+            result['msgid_plural'] = pos
         else:
             raise ValueError(f'Invalid keyword spec {spec!r}: '
                              'too many positions')
 
-    if 'msgid' not in result.values() and 'msgctxt' in result.values():
+    if 'msgid' not in result and 'msgctxt' in result:
         raise ValueError(f'Invalid keyword spec {spec!r}: '
                          'msgctxt cannot appear without msgid')
 
-    return name, result
+    return name, {v: k for k, v in result.items()}
 
 
 @dataclass(frozen=True)
