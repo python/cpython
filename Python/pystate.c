fbcf533de3645a925737b88f4ea1e19107fae6b3
Sam Gross <colesbury@gmail.com>
2025-02-27 08:27:54 -0500

    gh-130421: Fix data race on timebase initialization (gh-130592)
    Windows and macOS require precomputing a "timebase" in order to convert
    OS timestamps into nanoseconds. Retrieve and compute this value during
    runtime initialization to avoid data races when accessing the time.

--- Python/pystate.c
+++ Python/pystate.c
@@ -20,6 +20,7 @@
 #include "pycore_pystate.h"
 #include "pycore_runtime_init.h"  // _PyRuntimeState_INIT
 #include "pycore_stackref.h"      // Py_STACKREF_DEBUG
+#include "pycore_time.h"          // _PyTime_Init()
 #include "pycore_obmalloc.h"      // _PyMem_obmalloc_state_on_heap()
 #include "pycore_uniqueid.h"      // _PyObject_FinalizePerThreadRefcounts()
 
@@ -459,6 +460,11 @@ _PyRuntimeState_Init(_PyRuntimeState *runtime)
         // Preserve the cookie from the original runtime.
         memcpy(runtime->debug_offsets.cookie, _Py_Debug_Cookie, 8);
         assert(!runtime->_initialized);
+    }
+
+    PyStatus status = _PyTime_Init(&runtime->time);
+    if (_PyStatus_EXCEPTION(status)) {
+        return status;
     }
 
     if (gilstate_tss_init(runtime) != 0) {
