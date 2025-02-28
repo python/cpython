1d17afe577b2ba8edde2fa8a8d1e8eb226ef803f
Brandt Bucher <brandt@python.org>
2025-02-27 16:46:03 -0800

    is

--- Lib/test/test_capi/test_opt.py
+++ Lib/test/test_capi/test_opt.py
@@ -1441,7 +1441,7 @@ class TestUopsOptimization(unittest.TestCase):
         def f(n):
             trace = []
             for i in range(n):
-                # false *usually* False, but we can only prove that it's a bool:
+                # false is *usually* False, but we can only prove that it's a bool:
                 false = i % 100 == 0 
                 trace.append("A")
                 if not false:  # Kept.
@@ -1472,7 +1472,7 @@ class TestUopsOptimization(unittest.TestCase):
         def f(n):
             trace = []
             for i in range(n):
-                # true *usually* True, but we can only prove that it's a bool:
+                # true is *usually* True, but we can only prove that it's a bool:
                 true = i % 100 != 0 
                 trace.append("A")
                 if true:  # Kept.
