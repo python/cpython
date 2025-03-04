09b97d9a9f8fb34fbe8637600fbea3be54106413
s-ball <43739830+s-ball@users.noreply.github.com>
2025-03-04 19:13:05 +0100

    Update Lib/test/test_tools/test_msgfmt.py
    Co-authored-by: Stan Ulbrych <89152624+StanFromIreland@users.noreply.github.com>

--- Lib/test/test_tools/test_msgfmt.py
+++ Lib/test/test_tools/test_msgfmt.py
@@ -1,6 +1,6 @@
 """Tests for the Tools/i18n/msgfmt.py tool.
 
-Those tests use data files (po and mo) in the msgfmt_data folder.
+These tests use data files (po and mo) in the msgfmt_data folder.
 The mo files can be generated (if the po file changes, or if msgfmt.py
 slightly changes its output format) by using the --snapshot-update flag
 with this script:
