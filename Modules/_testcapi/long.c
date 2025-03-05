d8a39639bb34bea896880d10edf448b456e9fb73
Peter Bierma <zintensitydev@gmail.com>
2025-03-05 03:58:39 -0500

    [3.12] gh-130824: Add tests for `NULL` in `PyLong_*AndOverflow` functions (GH-130828)
    (cherry picked from commit 90130807d9c5a55b2a64024f5dfbee4785b9a27c)
    
    Co-authored-by: Peter Bierma <zintensitydev@gmail.com>
    Co-authored-by: Sergey B Kirpichev <skirpichev@gmail.com>

--- Modules/_testcapi/long.c
+++ Modules/_testcapi/long.c
@@ -629,7 +629,8 @@ pylong_aslongandoverflow(PyObject *module, PyObject *arg)
     int overflow = UNINITIALIZED_INT;
     long value = PyLong_AsLongAndOverflow(arg, &overflow);
     if (value == -1 && PyErr_Occurred()) {
-        assert(overflow == -1);
+        // overflow can be 0 if a separate exception occurred
+        assert(overflow == -1 || overflow == 0);
         return NULL;
     }
     return Py_BuildValue("li", value, overflow);
@@ -675,7 +676,8 @@ pylong_aslonglongandoverflow(PyObject *module, PyObject *arg)
     int overflow = UNINITIALIZED_INT;
     long long value = PyLong_AsLongLongAndOverflow(arg, &overflow);
     if (value == -1 && PyErr_Occurred()) {
-        assert(overflow == -1);
+        // overflow can be 0 if a separate exception occurred
+        assert(overflow == -1 || overflow == 0);
         return NULL;
     }
     return Py_BuildValue("Li", value, overflow);
