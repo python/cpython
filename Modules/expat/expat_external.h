01183d746dec877aac6caf204b7beac2c67ee26c
Chris Eibl <138194463+chris-eibl@users.noreply.github.com>
2025-03-09 11:50:25 +0100

    Enable XML_ATTR_MALLOC and XML_ATTR_ALLOC_SIZE for
    clang-cl on Windows

--- Modules/expat/expat_external.h
+++ Modules/expat/expat_external.h
@@ -112,14 +112,14 @@
 #  define XMLIMPORT
 #endif
 
-#if defined(__GNUC__)                                                          \
+#if defined(__clang__) || defined(__GNUC__)                                    \
     && (__GNUC__ > 2 || (__GNUC__ == 2 && __GNUC_MINOR__ >= 96))
 #  define XML_ATTR_MALLOC __attribute__((__malloc__))
 #else
 #  define XML_ATTR_MALLOC
 #endif
 
-#if defined(__GNUC__)                                                          \
+#if defined(__clang__) || defined(__GNUC__)                                    \
     && ((__GNUC__ > 4) || (__GNUC__ == 4 && __GNUC_MINOR__ >= 3))
 #  define XML_ATTR_ALLOC_SIZE(x) __attribute__((__alloc_size__(x)))
 #else
