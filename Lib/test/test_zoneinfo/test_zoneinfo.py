c6020d2f7921fb2d52d44853a31dbb537a34ac84
Stan Ulbrych <stanulbrych@gmail.com>
2025-03-11 17:30:25 +0000

    Add test skip

--- Lib/test/test_zoneinfo/test_zoneinfo.py
+++ Lib/test/test_zoneinfo/test_zoneinfo.py
@@ -1915,7 +1915,8 @@ class TestModule(ZoneInfoTestBase):
 class CTestModule(TestModule):
     module = c_zoneinfo
 
-
+@unittest.skipIf(__import__("sys").modules.get("_zoneinfo") is None,
+                 "_zoneinfo C module not available")
 class ExtensionBuiltTest(unittest.TestCase):
     """Smoke test to ensure that the C and Python extensions are both tested.
 
