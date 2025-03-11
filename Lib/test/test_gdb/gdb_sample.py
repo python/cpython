ebc24d54bcf554403e9bf4b590d5c1f49e648e0d
Pablo Galindo Salgado <Pablogsal@gmail.com>
2025-03-11 23:17:58 +0000

    gh-117174: Fix reference leak and gdb tests (#131095)

--- Lib/test/test_gdb/gdb_sample.py
+++ Lib/test/test_gdb/gdb_sample.py
@@ -1,5 +1,4 @@
 # Sample script for use by test_gdb
-from _typing import _idfunc
 
 def foo(a, b, c):
     bar(a=a, b=b, c=c)
@@ -8,6 +7,6 @@ def bar(a, b, c):
     baz(a, b, c)
 
 def baz(*args):
-    _idfunc(42)
+    id(42)
 
 foo(1, 2, 3)
