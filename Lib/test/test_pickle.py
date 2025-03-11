f6f4e1865f9f95f9e9411d255d1ba17c005c413b
donBarbos <donbarbos@proton.me>
2025-03-11 12:09:34 +0400

    Revert adding doctest to unittests

--- Lib/test/test_pickle.py
+++ Lib/test/test_pickle.py
@@ -700,7 +700,7 @@ class CompatPickleTests(unittest.TestCase):
 
 
 def load_tests(loader, tests, pattern):
-    tests.addTest(doctest.DocTestSuite(pickle))
+    tests.addTest(doctest.DocTestSuite())
     return tests
 
 
