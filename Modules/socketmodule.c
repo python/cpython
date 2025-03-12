8290b4c1c3dbea5d8650973b9a77b91eb3c6caca
Furkan Onder <furkanonder@protonmail.com>
2025-03-13 00:12:31 +0300

    Replace `#if !defined(__NetBSD__)` with #ifdef CAN_BCM

--- Modules/socketmodule.c
+++ Modules/socketmodule.c
@@ -2273,7 +2273,7 @@ getsockaddrarg(PySocketSockObject *s, PyObject *args,
         switch (s->sock_proto) {
 #ifdef CAN_RAW
         case CAN_RAW:
-        #if !defined(__NetBSD__)
+        #ifdef CAN_BCM
             _Py_FALLTHROUGH;
         #endif
 #endif
