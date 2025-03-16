dc0a5211058387aaed467bcb39c4fd4c2a130442
Bénédikt Tran <10796600+picnixz@users.noreply.github.com>
2025-03-16 14:23:36 +0100

    Victor's review (part 1)

--- Modules/_ssl.c
+++ Modules/_ssl.c
@@ -5318,9 +5318,9 @@ _ssl_MemoryBIO_impl(PyTypeObject *type)
 }
 
 static int
-memory_bio_traverse(PyObject *op, visitproc visit, void *arg)
+memory_bio_traverse(PyObject *self, visitproc visit, void *arg)
 {
-    Py_VISIT(Py_TYPE(op));
+    Py_VISIT(Py_TYPE(self));
     return 0;
 }
 
@@ -5535,11 +5535,9 @@ PySSLSession_richcompare(PyObject *left, PyObject *right, int op)
 
     int result;
 
-    PySSLSession *self = PySSLSession_CAST(left);
-    PyTypeObject *sesstype = Py_TYPE(self);
-    assert(sesstype == self->ctx->state->PySSLSession_Type);
+    PyTypeObject *sesstype = PySSLSession_CAST(left)->ctx->state->PySSLSession_Type;
 
-    if (!Py_IS_TYPE(right, sesstype)) {
+    if (!Py_IS_TYPE(left, sesstype) || !Py_IS_TYPE(right, sesstype)) {
         Py_RETURN_NOTIMPLEMENTED;
     }
 
@@ -5548,8 +5546,9 @@ PySSLSession_richcompare(PyObject *left, PyObject *right, int op)
     } else {
         const unsigned char *left_id, *right_id;
         unsigned int left_len, right_len;
-        left_id = SSL_SESSION_get_id(self->session, &left_len);
-        right_id = SSL_SESSION_get_id(((PySSLSession *)right)->session,
+        left_id = SSL_SESSION_get_id(PySSLSession_CAST(left)->session,
+                                     &left_len);
+        right_id = SSL_SESSION_get_id(PySSLSession_CAST(right)->session,
                                       &right_len);
         if (left_len == right_len) {
             result = memcmp(left_id, right_id, left_len);
