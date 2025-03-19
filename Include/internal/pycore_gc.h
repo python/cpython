ff806602a54a60b3cba7155b6dda683b2210477f
Victor Stinner <vstinner@python.org>
2025-03-19 23:22:11 +0100

    gh-131238: Use pycore_interp_structs.h header
    Replace pycore_runtime_structs.h include with pycore_interp_structs.h
    include in internal headers.

--- Include/internal/pycore_gc.h
+++ Include/internal/pycore_gc.h
@@ -8,8 +8,7 @@ extern "C" {
 #  error "this header requires Py_BUILD_CORE define"
 #endif
 
-#include "pycore_pystate.h"
-#include "pycore_runtime_structs.h"
+#include "pycore_interp_structs.h" // PyGC_Head
 #include "pycore_typedefs.h"      // _PyInterpreterFrame
 
 
