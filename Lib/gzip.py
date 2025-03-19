99d4a47bb1f52255927db238064763d91ac8d672
Thomas Grainger <tagrain@gmail.com>
2025-03-19 14:26:48 +0000

    Update Lib/gzip.py

--- Lib/gzip.py
+++ Lib/gzip.py
@@ -255,7 +255,6 @@ class GzipFile(_compression.BaseStream):
                     self.myfileobj.close()
             finally:
                 self.fileobj = None
-            raise
 
     @property
     def mtime(self):
