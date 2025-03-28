6af2e6f9ea9b8467c52b09ca79117077e68a3c4b
bswck <bartosz@ilikepython.com>
2025-03-28 13:20:01 +0100

    Don't examine for literals from test inputs

--- Lib/test/test_repl.py
+++ Lib/test/test_repl.py
@@ -296,7 +296,7 @@ class TestInteractiveModeSyntaxErrors(unittest.TestCase):
 
 class TestAsyncioREPL(unittest.TestCase):
     def test_multiple_statements_fail_early(self):
-        user_input = "1 / 0; print('afterwards')"
+        user_input = "1 / 0; print('after' + 'wards')"
         p = spawn_repl("-m", "asyncio")
         p.stdin.write(user_input)
         output = kill_python(p)
