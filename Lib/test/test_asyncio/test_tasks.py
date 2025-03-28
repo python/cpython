924a6ceb810065299d4b70dd23a52dfb422c1f50
Miss Islington (bot) <31488909+miss-islington@users.noreply.github.com>
2025-03-28 15:02:03 +0100

    [3.13] gh-127949: fix resource warnings in `test_tasks.py` (GH-128172) (#131805)
    gh-127949: fix resource warnings in `test_tasks.py` (GH-128172)
    (cherry picked from commit b66a4ad9fc32b63da2ba10db24cbc8f4e29f781a)
    
    Co-authored-by: Thomas Grainger <tagrain@gmail.com>

--- Lib/test/test_asyncio/test_tasks.py
+++ Lib/test/test_asyncio/test_tasks.py
@@ -2694,17 +2694,17 @@ class BaseTaskTests:
         initial_refcount = sys.getrefcount(obj)
 
         coro = coroutine_function()
-        loop = asyncio.new_event_loop()
-        task = asyncio.Task.__new__(asyncio.Task)
+        with contextlib.closing(asyncio.EventLoop()) as loop:
+            task = asyncio.Task.__new__(asyncio.Task)
 
-        for _ in range(5):
-            with self.assertRaisesRegex(RuntimeError, 'break'):
-                task.__init__(coro, loop=loop, context=obj, name=Break())
+            for _ in range(5):
+                with self.assertRaisesRegex(RuntimeError, 'break'):
+                    task.__init__(coro, loop=loop, context=obj, name=Break())
 
-        coro.close()
-        del task
+            coro.close()
+            del task
 
-        self.assertEqual(sys.getrefcount(obj), initial_refcount)
+            self.assertEqual(sys.getrefcount(obj), initial_refcount)
 
 
 def add_subclass_tests(cls):
