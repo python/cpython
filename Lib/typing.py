8681c97c0ec3c73332ba54a48b9c450192704cce
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2025-04-03 23:06:54 -0700

    gh-105499: Defer "import warnings" in typing
    A bunch of other warnings in typing.py were already deferred, but
    I added a few non-lazy ones.

--- Lib/typing.py
+++ Lib/typing.py
@@ -30,7 +30,6 @@ import operator
 import sys
 import types
 from types import GenericAlias
-import warnings
 
 from _typing import (
     _idfunc,
@@ -1626,14 +1625,17 @@ class _TupleType(_SpecialGenericAlias, _root=True):
 
 class _UnionGenericAliasMeta(type):
     def __instancecheck__(self, inst: object) -> bool:
+        import warnings
         warnings._deprecated("_UnionGenericAlias", remove=(3, 17))
         return isinstance(inst, Union)
 
     def __subclasscheck__(self, inst: type) -> bool:
+        import warnings
         warnings._deprecated("_UnionGenericAlias", remove=(3, 17))
         return issubclass(inst, Union)
 
     def __eq__(self, other):
+        import warnings
         warnings._deprecated("_UnionGenericAlias", remove=(3, 17))
         if other is _UnionGenericAlias or other is Union:
             return True
@@ -1650,6 +1652,7 @@ class _UnionGenericAlias(metaclass=_UnionGenericAliasMeta):
 
     """
     def __new__(cls, self_cls, parameters, /, *, name=None):
+        import warnings
         warnings._deprecated("_UnionGenericAlias", remove=(3, 17))
         return Union[parameters]
 
