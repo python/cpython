1ee9bf4d089bdcd3ee5e064420e2dec7a2e627e0
ebonnal <bonnal.enzo.dev@gmail.com>
2025-04-07 14:23:39 +0100

    add comment about the avoided ref cycle with exc_wrapper

--- Lib/concurrent/futures/interpreter.py
+++ Lib/concurrent/futures/interpreter.py
@@ -208,6 +208,7 @@ WorkerContext._send_script_result({self.resultsid})"""
             try:
                 raise pickle.loads(excdata) from exc_wrapper
             finally:
+                # avoid a ref cycle where exc_wrapper is captured in its traceback
                 exc_wrapper = None
         return pickle.loads(res) if pickled else res
 
