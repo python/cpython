d87de4106c6b2086ebd7d24fec7c3e5d39a02f93
Peter Bierma <zintensitydev@gmail.com>
2025-04-08 05:43:05 -0400

    Remove comment.

--- Lib/test/test_types.py
+++ Lib/test/test_types.py
@@ -677,8 +677,6 @@ class TypesTests(unittest.TestCase):
         # GH-131998: The specialized instruction would get tricked into dereferencing
         # a bound "self" that didn't exist if subsequently called unbound.
         code = """if True:
-        # The optimizer is finicky, and the easiest way (that I know of)
-        # to get it to reproduce in CI is by importing a Python module at runtime.
 
         def call(part):
             [] + ([] + [])
