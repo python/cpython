3e79eaefcfd653af59d82fff23642b5c1fa4ff05
Yves.Duprat <yduprat@gmail.com>
2025-04-09 19:32:41 +0200

    Initial commit

--- Lib/test/test_traceback.py
+++ Lib/test/test_traceback.py
@@ -3413,6 +3413,24 @@ class Unrepresentable:
     def __repr__(self) -> str:
         raise Exception("Unrepresentable")
 
+
+# Used in test_dont_swallow_cause_or_context_of_falsey_exception and
+# test_dont_swallow_subexceptions_of_falsey_exceptiongroup.
+class FalseyBoolException(Exception):
+    def __bool__(self):
+        return False
+
+
+class FalseyLenException(Exception):
+    def __len__(self):
+        return 0
+
+
+class FalseyExceptionGroup(ExceptionGroup):
+    def __bool__(self):
+        return False
+
+
 class TestTracebackException(unittest.TestCase):
     def do_test_smoke(self, exc, expected_type_str):
         try:
@@ -3759,6 +3777,26 @@ class TestTracebackException(unittest.TestCase):
              'ZeroDivisionError: division by zero',
              ''])
 
+    def test_dont_swallow_cause_or_context_of_falsey_exception(self):
+        # see gh-132308: Ensure that __cause__ or __context__ attributes of exceptions
+        # that evaluate as falsey are included in the output.
+        # Recall: `x` is falsey if `len(x)` returns 0 or `bool(x)` returns False.
+
+        for exc in (FalseyBoolException, FalseyLenException):
+            try:
+                raise exc(0) from KeyError
+            except exc as e:
+                self.assertIn(cause_message, traceback.format_exception(e))
+
+        for exc in (FalseyBoolException, FalseyLenException):
+            try:
+                try:
+                    1/0
+                except:
+                    raise exc(1)
+            except exc as e:
+                self.assertIn(context_message, traceback.format_exception(e))
+
 
 class TestTracebackException_ExceptionGroups(unittest.TestCase):
     def setUp(self):
@@ -3960,6 +3998,25 @@ class TestTracebackException_ExceptionGroups(unittest.TestCase):
         self.assertNotEqual(exc, object())
         self.assertEqual(exc, ALWAYS_EQ)
 
+    def test_dont_swallow_subexceptions_of_falsey_exceptiongroup(self):
+        # see gh-132308: Ensure that subexceptions of exception group
+        # that evaluate as falsey are displayed in the output.
+        # Recall: `x` is falsey if `len(x)` returns 0 or `bool(x)` returns False.
+
+        try:
+            raise FalseyExceptionGroup("Gih", (KeyError(2), NameError('Guh')))
+        except Exception as ee:
+            str_exc = ''.join(traceback.format_exception(ee))
+            self.assertIn('+---------------- 1 ----------------', str_exc)
+            self.assertIn('+---------------- 2 ----------------', str_exc)
+
+        # Test with a falsey exception, in last position, as sub-exceptions.
+        msg = 'bool'
+        try:
+            raise FalseyExceptionGroup("Gih", (KeyError(2), FalseyBoolException(msg)))
+        except Exception as ee:
+            str_exc = traceback.format_exception(ee)
+            self.assertIn(f'{FalseyBoolException.__name__}: {msg}', str_exc[-2])
 
 global_for_suggestions = None
 
