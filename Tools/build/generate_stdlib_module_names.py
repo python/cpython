3bdf24a9f2ef58022d5e2d0bfa7178aa196f0431
Bénédikt Tran <10796600+picnixz@users.noreply.github.com>
2025-04-10 15:27:36 +0200

    lint `Tools/build/generate_stdlib_module_names.py`

--- Tools/build/generate_stdlib_module_names.py
+++ Tools/build/generate_stdlib_module_names.py
@@ -7,7 +7,6 @@ import sysconfig
 
 from check_extension_modules import ModuleChecker
 
-
 SCRIPT_NAME = 'Tools/build/generate_stdlib_module_names.py'
 
 SRC_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
