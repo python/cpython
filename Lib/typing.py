758fdb036f685d5cb0d92f0ec761f01badbfd6a8
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2025-04-13 21:35:53 -0700

    gh-132493: Support deferred annotations in Protocols

--- Lib/typing.py
+++ Lib/typing.py
@@ -1801,7 +1801,9 @@ def _get_protocol_attrs(cls):
     for base in cls.__mro__[:-1]:  # without object
         if base.__name__ in {'Protocol', 'Generic'}:
             continue
-        annotations = getattr(base, '__annotations__', {})
+        annotations = _lazy_annotationlib.get_annotations(
+            base, format=_lazy_annotationlib.Format.FORWARDREF
+        )
         for attr in (*base.__dict__, *annotations):
             if not attr.startswith('_abc_') and attr not in EXCLUDED_ATTRIBUTES:
                 attrs.add(attr)
