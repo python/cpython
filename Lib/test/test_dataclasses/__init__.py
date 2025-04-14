a50aa3325b2df379355f1bf7f01b199cc553c708
Miss Islington (bot) <31488909+miss-islington@users.noreply.github.com>
2025-04-14 17:29:20 +0200

    [3.13] gh-132515: de-duplicate `test_dataclass_derived_generic_from_slotted_base` (GH-132516) (#132518)
    gh-132515: de-duplicate `test_dataclass_derived_generic_from_slotted_base` (GH-132516)
    (cherry picked from commit 45c447bf91ffabe4c0ba6d18f37d4e58925d5c91)
    
    Co-authored-by: Bénédikt Tran <10796600+picnixz@users.noreply.github.com>

--- Lib/test/test_dataclasses/__init__.py
+++ Lib/test/test_dataclasses/__init__.py
@@ -3590,7 +3590,6 @@ class TestSlots(unittest.TestCase):
         a_ref = weakref.ref(a)
         self.assertIs(a.__weakref__, a_ref)
 
-
     def test_dataclass_derived_weakref_slot(self):
         class A:
             pass
@@ -3670,7 +3669,7 @@ class TestSlots(unittest.TestCase):
         self.assertTrue(F.__weakref__)
         F()
 
-    def test_dataclass_derived_generic_from_slotted_base(self):
+    def test_dataclass_derived_generic_from_slotted_base_with_weakref(self):
         T = typing.TypeVar('T')
 
         class WithWeakrefSlot:
