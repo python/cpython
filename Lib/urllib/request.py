fc78d00e916b0f4e9c9d5efeb78d00bff97320af
barneygale <barney.gale@gmail.com>
2025-04-14 18:18:39 +0100

    GH-123599: Match `file:` URL hostname against machine hostname in urllib
    In `_is_local_authority()`, return early if the authority matches the
    machine hostname from `socket.gethostname()`, rather than resolving the
    names and matching IP addresses.

--- Lib/urllib/request.py
+++ Lib/urllib/request.py
@@ -1486,6 +1486,13 @@ def _is_local_authority(authority):
     if not authority or authority == 'localhost':
         return True
     try:
+        hostname = socket.gethostname()
+    except socket.gaierror:
+        pass
+    else:
+        if authority == hostname:
+            return True
+    try:
         address = socket.gethostbyname(authority)
     except (socket.gaierror, AttributeError):
         return False
