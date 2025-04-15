9e11cc732ce16f072111203272062d9993e6e311
Mark Shannon <mark@hotpy.org>
2025-04-15 18:19:22 +0100

    Push assert up to caller

--- Include/internal/pycore_stackref.h
+++ Include/internal/pycore_stackref.h
@@ -506,6 +506,11 @@ PyStackRef_AsPyObjectBorrow(_PyStackRef ref)
     return BITS_TO_PTR_MASKED(ref);
 }
 
+#ifdef Py_DEBUG
+#define PyStackRef_AsPyObjectBorrow(REF) \
+    (assert(!PyStackRef_IsTaggedInt(REF)), PyStackRef_AsPyObjectBorrow(REF))
+#endif
+
 static inline _PyStackRef
 PyStackRef_Borrow(_PyStackRef ref)
 {
