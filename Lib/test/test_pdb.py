fd022da9bff4df79ed72cd011c73a6ad345d0d19
Irit Katriel <iritkatriel@yahoo.com>
2025-04-15 16:21:45 +0100

    fix test_dis

--- Lib/test/test_pdb.py
+++ Lib/test/test_pdb.py
@@ -3039,7 +3039,7 @@ def test_pdb_issue_gh_108976():
     """
 
 def test_pdb_issue_gh_127321():
-    """See GH-127321
+    """See GH-127321, GH-132532
     breakpoint() should stop at a opcode that has a line number
     >>> def test_function():
     ...     import pdb; pdb_instance = pdb.Pdb(nosigint=True, readrc=False)
@@ -3050,7 +3050,7 @@ def test_pdb_issue_gh_127321():
     ... ]):
     ...    test_function()
     > <doctest test.test_pdb.test_pdb_issue_gh_127321[0]>(4)test_function()
-    -> a = 1
+    -> [1, 2] and pdb_instance.set_trace()
     (Pdb) continue
     """
 
