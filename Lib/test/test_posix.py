b4be5269d76ecf29069eb66114a8ffbc183b0b53
Serhiy Storchaka <storchaka@gmail.com>
2025-04-15 10:03:21 +0300

    Use subTest().

--- Lib/test/test_posix.py
+++ Lib/test/test_posix.py
@@ -1523,43 +1523,47 @@ class PosixTester(unittest.TestCase):
 
     @unittest.skipUnless(hasattr(os, "link"), "test needs os.link()")
     def test_link_follow_symlinks(self):
+        default_follow = sys.platform.startswith(
+            ('darwin', 'freebsd', 'netbsd', 'openbsd', 'dragonfly', 'sunos5'))
+        default_no_follow = sys.platform.startswith(('win32', 'linux'))
         orig = os_helper.TESTFN
         symlink = orig + 'symlink'
         posix.symlink(orig, symlink)
         self.addCleanup(os_helper.unlink, symlink)
 
-        link = orig + 'link'
-        posix.link(symlink, link)
-        self.addCleanup(os_helper.unlink, link)
-        default_follow = sys.platform.startswith(
-            ('darwin', 'freebsd', 'netbsd', 'openbsd', 'dragonfly', 'sunos5'))
-        default_no_follow = sys.platform.startswith(('win32', 'linux'))
-        if os.link in os.supports_follow_symlinks or default_follow:
-            self.assertEqual(posix.lstat(link), posix.lstat(orig))
-        elif default_no_follow:
-            self.assertEqual(posix.lstat(link), posix.lstat(symlink))
-
-        # follow_symlinks=False -> duplicate the symlink itself
-        link_nofollow = orig + 'link_nofollow'
-        try:
-            posix.link(symlink, link_nofollow, follow_symlinks=False)
-        except NotImplementedError:
-            if os.link in os.supports_follow_symlinks or default_no_follow:
-                raise
-        else:
-            self.addCleanup(os_helper.unlink, link_nofollow)
-            self.assertEqual(posix.lstat(link_nofollow), posix.lstat(symlink))
-
-        # follow_symlinks=True -> duplicate the target file
-        link_following = orig + 'link_following'
-        try:
-            posix.link(symlink, link_following, follow_symlinks=True)
-        except NotImplementedError:
+        with self.subTest('no follow_symlinks'):
+            # no follow_symlinks -> platform depending
+            link = orig + 'link'
+            posix.link(symlink, link)
+            self.addCleanup(os_helper.unlink, link)
             if os.link in os.supports_follow_symlinks or default_follow:
-                raise
-        else:
-            self.addCleanup(os_helper.unlink, link_following)
-            self.assertEqual(posix.lstat(link_following), posix.lstat(orig))
+                self.assertEqual(posix.lstat(link), posix.lstat(orig))
+            elif default_no_follow:
+                self.assertEqual(posix.lstat(link), posix.lstat(symlink))
+
+        with self.subTest('follow_symlinks=False'):
+            # follow_symlinks=False -> duplicate the symlink itself
+            link = orig + 'link_nofollow'
+            try:
+                posix.link(symlink, link, follow_symlinks=False)
+            except NotImplementedError:
+                if os.link in os.supports_follow_symlinks or default_no_follow:
+                    raise
+            else:
+                self.addCleanup(os_helper.unlink, link)
+                self.assertEqual(posix.lstat(link), posix.lstat(symlink))
+
+        with self.subTest('follow_symlinks=True'):
+            # follow_symlinks=True -> duplicate the target file
+            link = orig + 'link_following'
+            try:
+                posix.link(symlink, link, follow_symlinks=True)
+            except NotImplementedError:
+                if os.link in os.supports_follow_symlinks or default_follow:
+                    raise
+            else:
+                self.addCleanup(os_helper.unlink, link)
+                self.assertEqual(posix.lstat(link), posix.lstat(orig))
 
 
 # tests for the posix *at functions follow
