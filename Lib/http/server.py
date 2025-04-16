01d5fb85de67944fdfedd82ca8f60f30aa332c0c
ggqlq <124190229+ggqlq@users.noreply.github.com>
2025-04-16 21:32:46 +0800

    Update Lib/http/server.py
    Co-authored-by: Semyon Moroz <donbarbos@proton.me>

--- Lib/http/server.py
+++ Lib/http/server.py
@@ -1383,6 +1383,20 @@ def _main(args=None):
     else:
         handler_class = SimpleHTTPRequestHandler
 
+    # ensure dual-stack is not disabled; ref #38907
+    class DualStackServer(ThreadingHTTPServer):
+
+        def server_bind(self):
+            # suppress exception when protocol is IPv4
+            with contextlib.suppress(Exception):
+                self.socket.setsockopt(
+                    socket.IPPROTO_IPV6, socket.IPV6_V6ONLY, 0)
+            return super().server_bind()
+
+        def finish_request(self, request, client_address):
+            self.RequestHandlerClass(request, client_address, self,
+                                     directory=args.directory)
+
     test(
         HandlerClass=handler_class,
         ServerClass=ThreadingHTTPServer,
