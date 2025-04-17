6bfc8886ea8f44144ff533b8014de37e44c7cbdf
Lysandros Nikolaou <lisandrosnik@gmail.com>
2025-04-17 17:33:31 +0200

    Add test for interleaving

--- Lib/test/test_tstring.py
+++ Lib/test/test_tstring.py
@@ -55,6 +55,56 @@ world"""
         self.assertEqual(len(t.interpolations), 0)
         self.assertEqual(f(t), "Hello,\nworld")
 
+    def test_template_creation_interleaving(self):
+        # Should add strings on either side
+        t = Template(Interpolation("Maria", "name", None, ""))
+        self.assertEqual(t.strings, ("", ""))
+        self.assertEqual(t.interpolations[0].value, "Maria")
+        self.assertEqual(t.interpolations[0].expression, "name")
+        self.assertEqual(t.interpolations[0].conversion, None)
+        self.assertEqual(t.interpolations[0].format_spec, "")
+        self.assertEqual(f(t), "Maria")
+
+        # Should prepend empty string
+        t = Template(Interpolation("Maria", "name", None, ""), " is my name")
+        self.assertEqual(t.strings, ("", " is my name"))
+        self.assertEqual(t.interpolations[0].value, "Maria")
+        self.assertEqual(t.interpolations[0].expression, "name")
+        self.assertEqual(t.interpolations[0].conversion, None)
+        self.assertEqual(t.interpolations[0].format_spec, "")
+        self.assertEqual(f(t), "Maria is my name")
+
+        # Should append empty string
+        t = Template("Hello, ", Interpolation("Maria", "name", None, ""))
+        self.assertEqual(t.strings, ("Hello, ", ""))
+        self.assertEqual(t.interpolations[0].value, "Maria")
+        self.assertEqual(t.interpolations[0].expression, "name")
+        self.assertEqual(t.interpolations[0].conversion, None)
+        self.assertEqual(t.interpolations[0].format_spec, "")
+        self.assertEqual(f(t), "Hello, Maria")
+
+        # Should concatenate strings
+        t = Template("Hello", ", ", Interpolation("Maria", "name", None, ""), "!")
+        self.assertEqual(t.strings, ("Hello, ", "!"))
+        self.assertEqual(t.interpolations[0].value, "Maria")
+        self.assertEqual(t.interpolations[0].expression, "name")
+        self.assertEqual(t.interpolations[0].conversion, None)
+        self.assertEqual(t.interpolations[0].format_spec, "")
+        self.assertEqual(f(t), "Hello, Maria!")
+
+        # Should add strings on either side and in between
+        t = Template(Interpolation("Maria", "name", None, ""), Interpolation("Python", "language", None, ""))
+        self.assertEqual(t.strings, ("", "", ""))
+        self.assertEqual(t.interpolations[0].value, "Maria")
+        self.assertEqual(t.interpolations[0].expression, "name")
+        self.assertEqual(t.interpolations[0].conversion, None)
+        self.assertEqual(t.interpolations[0].format_spec, "")
+        self.assertEqual(t.interpolations[1].value, "Python")
+        self.assertEqual(t.interpolations[1].expression, "language")
+        self.assertEqual(t.interpolations[1].conversion, None)
+        self.assertEqual(t.interpolations[1].format_spec, "")
+        self.assertEqual(f(t), "MariaPython")
+
     def test_string_representation(self):
         # Test __repr__
         t = t"Hello"
