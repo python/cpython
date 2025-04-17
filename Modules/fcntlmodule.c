ddd9ee5123d2ff49dcf5ffd6c4a74154f0ca498b
Serhiy Storchaka <storchaka@gmail.com>
2025-04-17 15:07:12 +0300

    Fix compilation.

--- Modules/fcntlmodule.c
+++ Modules/fcntlmodule.c
@@ -2,6 +2,9 @@
 
 // Need limited C API version 3.13 for PyLong_AsInt()
 #include "pyconfig.h"   // Py_GIL_DISABLED
+#ifndef Py_BUILD_CORE_BUILTIN
+#  define Py_BUILD_CORE_MODULE 1
+#endif
 
 #include "Python.h"
 
