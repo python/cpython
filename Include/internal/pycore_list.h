c337181bc327135e367c7175d896673f2db1cb1e
Irit Katriel <iritkatriel@yahoo.com>
2025-04-17 12:35:26 +0100

    pacify the jit

--- Include/internal/pycore_list.h
+++ Include/internal/pycore_list.h
@@ -13,12 +13,12 @@ extern "C" {
 #endif
 
 PyAPI_FUNC(PyObject*) _PyList_Extend(PyListObject *, PyObject *);
+PyAPI_FUNC(PyObject) *_PyList_Subscript(PyObject*, PyObject*);
 extern void _PyList_DebugMallocStats(FILE *out);
 // _PyList_GetItemRef should be used only when the object is known as a list
 // because it doesn't raise TypeError when the object is not a list, whereas PyList_GetItemRef does.
 extern PyObject* _PyList_GetItemRef(PyListObject *, Py_ssize_t i);
 
-extern PyObject *_PyList_Subscript(PyObject*, PyObject*);
 
 #ifdef Py_GIL_DISABLED
 // Returns -1 in case of races with other threads.
