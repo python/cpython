234328dec471160571691fb33192f58911f1fd42
Eric Snow <ericsnowcurrently@gmail.com>
2025-04-21 16:35:02 -0600

    Fix refcounts.

--- Python/crossinterp_exceptions.h
+++ Python/crossinterp_exceptions.h
@@ -9,13 +9,13 @@ _ensure_current_cause(PyThreadState *tstate, PyObject *cause)
     assert(exc != NULL);
     PyObject *ctx = PyException_GetContext(exc);
     if (ctx == NULL) {
-        PyException_SetContext(exc, cause);
+        PyException_SetContext(exc, Py_NewRef(cause));
     }
     else {
         Py_DECREF(ctx);
     }
     assert(PyException_GetCause(exc) == NULL);
-    PyException_SetCause(exc, cause);
+    PyException_SetCause(exc, Py_NewRef(cause));
     _PyErr_SetRaisedException(tstate, exc);
 }
 
