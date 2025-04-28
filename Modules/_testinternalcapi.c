bdd23c0bb95faa37130fdf7af82f0fdddb41bae0
Eric Snow <ericsnowcurrently@gmail.com>
2025-04-28 17:23:46 -0600

    gh-132775: Add _PyMarshal_GetXIData() (gh-133108)
    Note that the bulk of this change is tests.

--- Modules/_testinternalcapi.c
+++ Modules/_testinternalcapi.c
@@ -1730,6 +1730,11 @@ get_crossinterp_data(PyObject *self, PyObject *args, PyObject *kwargs)
             goto error;
         }
     }
+    else if (strcmp(mode, "marshal") == 0) {
+        if (_PyMarshal_GetXIData(tstate, obj, xidata) != 0) {
+            goto error;
+        }
+    }
     else {
         PyErr_Format(PyExc_ValueError, "unsupported mode %R", modeobj);
         goto error;
