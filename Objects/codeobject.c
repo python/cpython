4fa2ecfbcccb38e0c91f3b7447e483fe7ce39564
Eric Snow <ericsnowcurrently@gmail.com>
2025-05-03 13:45:05 -0600

    Temporarily skip some code.

--- Objects/codeobject.c
+++ Objects/codeobject.c
@@ -1731,6 +1731,7 @@ identify_unbound_names(PyThreadState *tstate, PyCodeObject *co,
     struct co_unbound_counts unbound = {0};
     Py_ssize_t len = Py_SIZE(co);
     for (int i = 0; i < len; i++) {
+printf("%d\n", i);
         _Py_CODEUNIT inst = _Py_GetBaseCodeUnit(co, i);
 continue;
         if (inst.op.code == LOAD_ATTR) {
@@ -1781,9 +1782,9 @@ continue;
             }
         }
     }
-    if (counts != NULL) {
-        *counts = unbound;
-    }
+//    if (counts != NULL) {
+//        *counts = unbound;
+//    }
     return 0;
 }
 
