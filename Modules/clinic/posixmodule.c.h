5a57248b22ad3b9aafcaaadae2c304a1923daeca
Serhiy Storchaka <storchaka@gmail.com>
2025-05-04 17:24:10 +0300

    gh-81793: Always call linkat() from os.link(), if available (GH-132517)
    This fixes os.link() on platforms (like Linux and OpenIndiana) where the
    system link() function does not follow symlinks.
    
    * On Linux, it now follows symlinks by default and if
      follow_symlinks=True is specified.
    * On Windows, it now raises error if follow_symlinks=True is passed.
    * On macOS, it now raises error if follow_symlinks=False is passed and
      the system linkat() function is not available at runtime.
    * On other platforms, it now raises error if follow_symlinks is passed
      with a value that does not match the system link() function behavior
      if if the behavior is not known.
    
    Co-authored-by: Joachim Henke <37883863+jo-he@users.noreply.github.com>
    Co-authored-by: Thomas Kluyver <takowl@gmail.com>

--- Modules/clinic/posixmodule.c.h
+++ Modules/clinic/posixmodule.c.h
@@ -1471,7 +1471,7 @@ os_getcwdb(PyObject *module, PyObject *Py_UNUSED(ignored))
 
 PyDoc_STRVAR(os_link__doc__,
 "link($module, /, src, dst, *, src_dir_fd=None, dst_dir_fd=None,\n"
-"     follow_symlinks=True)\n"
+"     follow_symlinks=(os.name != \'nt\'))\n"
 "--\n"
 "\n"
 "Create a hard link to a file.\n"
@@ -1530,7 +1530,7 @@ os_link(PyObject *module, PyObject *const *args, Py_ssize_t nargs, PyObject *kwn
     path_t dst = PATH_T_INITIALIZE_P("link", "dst", 0, 0, 0, 0);
     int src_dir_fd = DEFAULT_DIR_FD;
     int dst_dir_fd = DEFAULT_DIR_FD;
-    int follow_symlinks = 1;
+    int follow_symlinks = -1;
 
     args = _PyArg_UnpackKeywords(args, nargs, NULL, kwnames, &_parser,
             /*minpos*/ 2, /*maxpos*/ 2, /*minkw*/ 0, /*varpos*/ 0, argsbuf);
@@ -13398,4 +13398,4 @@ os__emscripten_debugger(PyObject *module, PyObject *Py_UNUSED(ignored))
 #ifndef OS__EMSCRIPTEN_DEBUGGER_METHODDEF
     #define OS__EMSCRIPTEN_DEBUGGER_METHODDEF
 #endif /* !defined(OS__EMSCRIPTEN_DEBUGGER_METHODDEF) */
-/*[clinic end generated code: output=a5ca2541f2af5462 input=a9049054013a1b77]*/
+/*[clinic end generated code: output=f7b5635e0b948be4 input=a9049054013a1b77]*/
