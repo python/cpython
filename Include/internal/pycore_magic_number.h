78adb63ee198c94c6ce2a1634aa7ea1d47c011ad
mpage <mpage@meta.com>
2025-05-04 21:00:11 -0700

    gh-133371: Don't optimize `LOAD_FAST` instructions whose local is killed by `DELETE_FAST` (#133383)
    In certain cases it's possible for locals loaded by `LOAD_FAST` instructions
    to be on the stack when the local is killed by `DEL_FAST`. These `LOAD_FAST`
    instructions should not be optimized into `LOAD_FAST_BORROW` as the strong
    reference in the frame is killed while there is still a reference on the stack.

--- Include/internal/pycore_magic_number.h
+++ Include/internal/pycore_magic_number.h
@@ -276,6 +276,7 @@ Known values:
     Python 3.14a7 3621 (Optimize LOAD_FAST opcodes into LOAD_FAST_BORROW)
     Python 3.14a7 3622 (Store annotations in different class dict keys)
     Python 3.14a7 3623 (Add BUILD_INTERPOLATION & BUILD_TEMPLATE opcodes)
+    Python 3.14b1 3624 (Don't optimize LOAD_FAST when local is killed by DELETE_FAST)
 
     Python 3.15 will start with 3650
 
@@ -288,7 +289,7 @@ PC/launcher.c must also be updated.
 
 */
 
-#define PYC_MAGIC_NUMBER 3623
+#define PYC_MAGIC_NUMBER 3624
 /* This is equivalent to converting PYC_MAGIC_NUMBER to 2 bytes
    (little-endian) and then appending b'\r\n'. */
 #define PYC_MAGIC_NUMBER_TOKEN \
