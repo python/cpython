a7668050c006a330bdbbbd8ecf6e67846b651fb2
Tan Long <tanloong@foxmail.com>
2025-05-05 12:46:48 +0800

    Move keyword list to module level

--- Lib/sqlite3/_completer.py
+++ Lib/sqlite3/_completer.py
@@ -1,33 +1,35 @@
 from contextlib import contextmanager
 
 
+_keywords = ["ABORT", "ACTION", "ADD", "AFTER", "ALL", "ALTER", "ALWAYS",
+            "ANALYZE", "AND", "AS", "ASC", "ATTACH", "AUTOINCREMENT",
+            "BEFORE", "BEGIN", "BETWEEN", "BY", "CASCADE", "CASE", "CAST",
+            "CHECK", "COLLATE", "COLUMN", "COMMIT", "CONFLICT",
+            "CONSTRAINT", "CREATE", "CROSS", "CURRENT", "CURRENT_DATE",
+            "CURRENT_TIME", "CURRENT_TIMESTAMP", "DATABASE", "DEFAULT",
+            "DEFERRABLE", "DEFERRED", "DELETE", "DESC", "DETACH",
+            "DISTINCT", "DO", "DROP", "EACH", "ELSE", "END", "ESCAPE",
+            "EXCEPT", "EXCLUDE", "EXCLUSIVE", "EXISTS", "EXPLAIN", "FAIL",
+            "FILTER", "FIRST", "FOLLOWING", "FOR", "FOREIGN", "FROM",
+            "FULL", "GENERATED", "GLOB", "GROUP", "GROUPS", "HAVING", "IF",
+            "IGNORE", "IMMEDIATE", "IN", "INDEX", "INDEXED", "INITIALLY",
+            "INNER", "INSERT", "INSTEAD", "INTERSECT", "INTO", "IS",
+            "ISNULL", "JOIN", "KEY", "LAST", "LEFT", "LIKE", "LIMIT",
+            "MATCH", "MATERIALIZED", "NATURAL", "NO", "NOT", "NOTHING",
+            "NOTNULL", "NULL", "NULLS", "OF", "OFFSET", "ON", "OR",
+            "ORDER", "OTHERS", "OUTER", "OVER", "PARTITION", "PLAN",
+            "PRAGMA", "PRECEDING", "PRIMARY", "QUERY", "RAISE", "RANGE",
+            "RECURSIVE", "REFERENCES", "REGEXP", "REINDEX", "RELEASE",
+            "RENAME", "REPLACE", "RESTRICT", "RETURNING", "RIGHT",
+            "ROLLBACK", "ROW", "ROWS", "SAVEPOINT", "SELECT", "SET",
+            "TABLE", "TEMP", "TEMPORARY", "THEN", "TIES", "TO",
+            "TRANSACTION", "TRIGGER", "UNBOUNDED", "UNION", "UNIQUE",
+            "UPDATE", "USING", "VACUUM", "VALUES", "VIEW", "VIRTUAL",
+            "WHEN", "WHERE", "WINDOW", "WITH", "WITHOUT"]
+
+
 def _complete(text, state):
-    keywords = ["ABORT", "ACTION", "ADD", "AFTER", "ALL", "ALTER", "ALWAYS",
-                "ANALYZE", "AND", "AS", "ASC", "ATTACH", "AUTOINCREMENT",
-                "BEFORE", "BEGIN", "BETWEEN", "BY", "CASCADE", "CASE", "CAST",
-                "CHECK", "COLLATE", "COLUMN", "COMMIT", "CONFLICT",
-                "CONSTRAINT", "CREATE", "CROSS", "CURRENT", "CURRENT_DATE",
-                "CURRENT_TIME", "CURRENT_TIMESTAMP", "DATABASE", "DEFAULT",
-                "DEFERRABLE", "DEFERRED", "DELETE", "DESC", "DETACH",
-                "DISTINCT", "DO", "DROP", "EACH", "ELSE", "END", "ESCAPE",
-                "EXCEPT", "EXCLUDE", "EXCLUSIVE", "EXISTS", "EXPLAIN", "FAIL",
-                "FILTER", "FIRST", "FOLLOWING", "FOR", "FOREIGN", "FROM",
-                "FULL", "GENERATED", "GLOB", "GROUP", "GROUPS", "HAVING", "IF",
-                "IGNORE", "IMMEDIATE", "IN", "INDEX", "INDEXED", "INITIALLY",
-                "INNER", "INSERT", "INSTEAD", "INTERSECT", "INTO", "IS",
-                "ISNULL", "JOIN", "KEY", "LAST", "LEFT", "LIKE", "LIMIT",
-                "MATCH", "MATERIALIZED", "NATURAL", "NO", "NOT", "NOTHING",
-                "NOTNULL", "NULL", "NULLS", "OF", "OFFSET", "ON", "OR",
-                "ORDER", "OTHERS", "OUTER", "OVER", "PARTITION", "PLAN",
-                "PRAGMA", "PRECEDING", "PRIMARY", "QUERY", "RAISE", "RANGE",
-                "RECURSIVE", "REFERENCES", "REGEXP", "REINDEX", "RELEASE",
-                "RENAME", "REPLACE", "RESTRICT", "RETURNING", "RIGHT",
-                "ROLLBACK", "ROW", "ROWS", "SAVEPOINT", "SELECT", "SET",
-                "TABLE", "TEMP", "TEMPORARY", "THEN", "TIES", "TO",
-                "TRANSACTION", "TRIGGER", "UNBOUNDED", "UNION", "UNIQUE",
-                "UPDATE", "USING", "VACUUM", "VALUES", "VIEW", "VIRTUAL",
-                "WHEN", "WHERE", "WINDOW", "WITH", "WITHOUT"]
-    options = [c + " " for c in keywords if c.startswith(text.upper())]
+    options = [c + " " for c in _keywords if c.startswith(text.upper())]
     try:
         return options[state]
     except IndexError:
