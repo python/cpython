f29ced5417def8c1b7a029b9049398c234b24e96
Adam Turner <9087854+AA-Turner@users.noreply.github.com>
2025-05-10 22:25:22 +0100

    gh-132983: Reduce the size of ``_zstdmodule.h`` (GH-133793) (cherry picked from commit 1a548c0a50641aa3358966e0949d29bcfa9dd480)
    Co-authored-by: Adam Turner <9087854+AA-Turner@users.noreply.github.com>

--- Modules/_zstd/_zstdmodule.c
+++ Modules/_zstd/_zstdmodule.c
@@ -7,7 +7,13 @@ Python module.
 #  define Py_BUILD_CORE_MODULE 1
 #endif
 
+#include "Python.h"
+
 #include "_zstdmodule.h"
+#include "zstddict.h"
+
+#include <zstd.h>                 // ZSTD_*()
+#include <zdict.h>                // ZDICT_*()
 
 /*[clinic input]
 module _zstd
@@ -727,7 +733,7 @@ static struct PyModuleDef_Slot _zstd_slots[] = {
     {0, NULL},
 };
 
-struct PyModuleDef _zstdmodule = {
+static struct PyModuleDef _zstdmodule = {
     .m_base = PyModuleDef_HEAD_INIT,
     .m_name = "_zstd",
     .m_doc = "Implementation module for Zstandard compression.",
