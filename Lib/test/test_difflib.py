dbf0547885e992d4ea2044fef2759f624dc94201
Douglas Thor <doug.thor@gmail.com>
2025-05-10 21:02:24 -0700

    fixup tests

--- Lib/test/test_difflib.py
+++ Lib/test/test_difflib.py
@@ -361,11 +361,11 @@ class TestOutputFormat(unittest.TestCase):
         actual = list(difflib.unified_diff(*args, lineterm='', color=True))
 
         expect = [
-            "\033[1m--- Original\t2005-01-26 23:30:50\033[m",
-            "\033[1m+++ Current\t2010-04-02 10:20:52\033[m",
-             "\033[36m@@ -1,2 +1,2 @@\033[m",
-             "\033[31m-one\033[m",
-             "\033[32m+two\033[m",
+            "\033[1m--- Original\t2005-01-26 23:30:50\033[0m",
+            "\033[1m+++ Current\t2010-04-02 10:20:52\033[0m",
+             "\033[36m@@ -1,2 +1,2 @@\033[0m",
+             "\033[31m-one\033[0m",
+             "\033[32m+two\033[0m",
              " three",
         ]
         self.assertEqual(expect, actual)
