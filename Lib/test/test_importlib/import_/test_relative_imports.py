d9feb830f3f3481add51d95807d07af53e1daed0
Nico-Posada <posadanico@outlook.com>
2025-05-16 19:24:31 -0400

    fix testcase

--- Lib/test/test_importlib/import_/test_relative_imports.py
+++ Lib/test/test_importlib/import_/test_relative_imports.py
@@ -227,9 +227,12 @@ class RelativeImports:
         # testing for gh-134100
         import sys
         loooong = "".ljust(0x23000, "b")
-        sys.modules.update({f"a.{loooong}.c": {}})
-        with self.assertRaisesRegex(KeyError, r"'a\.b+' not in sys\.modules as expected"):
-            __import__(f"{loooong}.c", {"__package__": "a"}, level=1)
+        name = f"a.{loooong}.c"
+
+        with util.uncache(name):
+            sys.modules[name] = {}
+            with self.assertRaisesRegex(KeyError, r"'a\.b+' not in sys\.modules as expected"):
+                __import__(f"{loooong}.c", {"__package__": "a"}, level=1)
 
 
 (Frozen_RelativeImports,
