bd4282702e86c234259f76539ad9cac429e6a5a9
Bénédikt Tran <10796600+picnixz@users.noreply.github.com>
2025-05-19 20:42:11 +0200

    fix tests

--- Lib/test/test_httpservers.py
+++ Lib/test/test_httpservers.py
@@ -1514,7 +1514,8 @@ class CommandLineRunTimeTestCase(unittest.TestCase):
         return False
 
     def test_http_client(self):
-        _, (bind, port) = server._get_best_family(None, find_unused_port())
+        _, addr = server._get_best_family(None, find_unused_port())
+        bind, port = addr[:2]
         proc = spawn_python('-u', '-m', 'http.server', str(port), '-b', bind,
                             bufsize=1, text=True)
         self.addCleanup(kill_python, proc)
@@ -1524,7 +1525,8 @@ class CommandLineRunTimeTestCase(unittest.TestCase):
         self.assertEqual(res, self.served_data)
 
     def test_https_client(self):
-        _, (bind, port) = server._get_best_family(None, find_unused_port())
+        _, addr = server._get_best_family(None, find_unused_port())
+        bind, port = addr[:2]
         proc = spawn_python('-u', '-m', 'http.server', str(port), '-b', bind,
                             '--tls-cert', self.tls_cert,
                             '--tls-key', self.tls_key,
