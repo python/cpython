python/cpython

commit 121ed71f4e395948d313249b2ad33e1e21581f8a

gh-132983: Fix compiler warning about unused function ``mt_continue_should_break()`` (#133947)

Modules/_zstd/compressor.c
@@ -486,11 +486,13 @@ error:
     return NULL;
 }
 
+#ifdef Py_DEBUG
 static inline int
 mt_continue_should_break(ZSTD_inBuffer *in, ZSTD_outBuffer *out)
 {
     return in->size == in->pos && out->size != out->pos;
 }
+#endif
 
 static PyObject *
 compress_mt_continue_impl(ZstdCompressor *self, Py_buffer *data)

commit b3b34c86eefea9231f9cd543f572bc5d9b91dd3f

gh-134768: Fix definition of `mt_continue_should_break()` (GH-134769)
In 121ed71f4e395948d313249b2ad33e1e21581f8a, mt_continue_should_break
was changed to be guarded by `Py_DEBUG`, but it's used in `compress_mt_continue_lock_held`
with just `assert`, so it needs to be available when `NDEBUG` is undefined
too.

`Py_DEBUG` implies `NDEBUG` is undefined, so we can check just that.
(cherry picked from commit 2f2bee21118adce653ee5bc4eb31d30327465966)

Co-authored-by: Sam James <sam@gentoo.org>
Fixes: 121ed71f4e395948d313249b2ad33e1e21581f8a

Modules/_zstd/compressor.c
@@ -512,7 +512,7 @@ error:
     return NULL;
 }
 
-#ifdef Py_DEBUG
+#ifndef NDEBUG
 static inline int
 mt_continue_should_break(ZSTD_inBuffer *in, ZSTD_outBuffer *out)
 {