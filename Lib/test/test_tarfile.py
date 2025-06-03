6d4d4d63d36646ab5791c8999ed33912ee164c9d
Thomas Wouters <thomas@python.org>
2025-06-03 14:44:51 +0200

    Fix test_tarfile (for not exporting LinkFallbackError) and remove the non-strict test for unreadable symlinks, whch isn't supported before 3.13.

--- Lib/test/test_tarfile.py
+++ Lib/test/test_tarfile.py
@@ -2533,7 +2533,7 @@ class MiscTest(unittest.TestCase):
             "fully_trusted_filter", "data_filter",
             "tar_filter", "FilterError", "AbsoluteLinkError",
             "OutsideDestinationError", "SpecialFileError", "AbsolutePathError",
-            "LinkOutsideDestinationError",
+            "LinkOutsideDestinationError", "LinkFallbackError",
             }
         support.check__all__(self, tarfile, not_exported=not_exported)
 
