c0a6c73d43fd2fcbe72d429ba8eee7d47c4a3fc2
László Kiss Kollár <kiss.kollar.laszlo@gmail.com>
2025-06-07 13:20:23 +0100

    Add sampling profiler
    Implement a statistical sampling profiler that can profile external
    Python processes by PID. Uses the _remote_debugging module and converts
    the results to pstats-compatible format for analysis.

--- Lib/profile/__main__.py
+++ Lib/profile/__main__.py
@@ -6,18 +6,32 @@ from optparse import OptionParser
 
 from .profile import runctx
 
-def main():
 
+def main():
     usage = "profile.py [-o output_file_path] [-s sort] [-m module | scriptfile] [arg] ..."
     parser = OptionParser(usage=usage)
     parser.allow_interspersed_args = False
-    parser.add_option('-o', '--outfile', dest="outfile",
-        help="Save stats to <outfile>", default=None)
-    parser.add_option('-m', dest="module", action="store_true",
-        help="Profile a library module.", default=False)
-    parser.add_option('-s', '--sort', dest="sort",
+    parser.add_option(
+        "-o",
+        "--outfile",
+        dest="outfile",
+        help="Save stats to <outfile>",
+        default=None,
+    )
+    parser.add_option(
+        "-m",
+        dest="module",
+        action="store_true",
+        help="Profile a library module.",
+        default=False,
+    )
+    parser.add_option(
+        "-s",
+        "--sort",
+        dest="sort",
         help="Sort order when printing to stdout, based on pstats.Stats class",
-        default=-1)
+        default=-1,
+    )
 
     if not sys.argv[1:]:
         parser.print_usage()
@@ -34,24 +48,23 @@ def main():
     if len(args) > 0 or options.pid:
         if options.module:
             import runpy
+
             code = "run_module(modname, run_name='__main__')"
-            globs = {
-                'run_module': runpy.run_module,
-                'modname': args[0]
-            }
+            globs = {"run_module": runpy.run_module, "modname": args[0]}
         else:
             progname = args[0]
             sys.path.insert(0, os.path.dirname(progname))
             with io.open_code(progname) as fp:
-                code = compile(fp.read(), progname, 'exec')
-            spec = importlib.machinery.ModuleSpec(name='__main__', loader=None,
-                                                  origin=progname)
+                code = compile(fp.read(), progname, "exec")
+            spec = importlib.machinery.ModuleSpec(
+                name="__main__", loader=None, origin=progname
+            )
             globs = {
-                '__spec__': spec,
-                '__file__': spec.origin,
-                '__name__': spec.name,
-                '__package__': None,
-                '__cached__': None,
+                "__spec__": spec,
+                "__file__": spec.origin,
+                "__name__": spec.name,
+                "__package__": None,
+                "__cached__": None,
             }
         try:
             runctx(code, globs, None, options.outfile, options.sort)
@@ -63,5 +76,6 @@ def main():
         parser.print_usage()
     return parser
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
