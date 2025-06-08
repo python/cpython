62bd6532cfc7e7fc60e7a8150f116caada3b32f4
Peter Bierma <zintensitydev@gmail.com>
2025-06-08 17:07:53 -0400

    Remove broken assertion.

--- Objects/codeobject.c
+++ Objects/codeobject.c
@@ -1979,7 +1979,9 @@ _PyCode_CheckNoExternalState(PyCodeObject *co, _PyCode_var_counts_t *counts,
                              const char **p_errmsg)
 {
     const char *errmsg = NULL;
-    assert(counts->locals.hidden.total == 0);
+    // Why is it an assumption that there can't be any hidden
+    // locals?
+    //assert(counts->locals.hidden.total == 0);
     if (counts->numfree > 0) {  // It's a closure.
         errmsg = "closures not supported";
     }
