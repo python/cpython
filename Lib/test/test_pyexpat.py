52da4aeac97db1c839cd0badde19ff765635ec91
Bénédikt Tran <10796600+picnixz@users.noreply.github.com>
2025-06-10 11:04:54 +0200

    refine test

--- Lib/test/test_pyexpat.py
+++ Lib/test/test_pyexpat.py
@@ -9,11 +9,10 @@ import traceback
 from io import BytesIO
 from test import support
 from test.support import os_helper
-
+from test.support import sortdict
+from unittest import mock
 from xml.parsers import expat
 from xml.parsers.expat import errors
-
-from test.support import sortdict
 
 
 class SetAttributeTest(unittest.TestCase):
@@ -440,9 +439,12 @@ class BufferTextTest(unittest.TestCase):
         def handler(*args):
             parser.CharacterDataHandler = None
 
+        handler_wrapper = unittest.mock.Mock(wraps=handler)
         parser = expat.ParserCreate()
-        parser.CharacterDataHandler = handler
+        parser.CharacterDataHandler = handler_wrapper
         parser.Parse(b"<a>1<b/>2<c></c>3<!--abc-->4<!--def-->5</a> ", True)
+        handler_wrapper.assert_called_once()
+        self.assertIsNone(parser.CharacterDataHandler)
 
 
 # Test handling of exception from callback:
@@ -603,7 +605,7 @@ class ChardataBufferTest(unittest.TestCase):
     def test_disabling_buffer(self):
         xml1 = b"<?xml version='1.0' encoding='iso8859'?><a>" + b'a' * 512
         xml2 = b'b' * 1024
-        xml3 = b'c' * 1024 + b'</a>';
+        xml3 = b'c' * 1024 + b'</a>'
         parser = expat.ParserCreate()
         parser.CharacterDataHandler = self.counting_handler
         parser.buffer_text = 1
