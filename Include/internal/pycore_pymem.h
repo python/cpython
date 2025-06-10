822036767e491c046d53d1d1daa57726f71c189f
Donghee Na <donghee.na92@gmail.com>
2025-06-10 22:05:28 +0900

    Update Include/internal/pycore_pymem.h
    Co-authored-by: Victor Stinner <vstinner@python.org>

--- Include/internal/pycore_pymem.h
+++ Include/internal/pycore_pymem.h
@@ -102,7 +102,7 @@ static inline void _PyObject_XDecRefDelayed(PyObject *obj)
 
 #ifdef Py_GIL_DISABLED
 // Same as `Py_XSETREF` but in free-threading, it stores the object atomically
-// and queues the old object to be decrefed at a safe point using QSBR
+// and queues the old object to be decrefed at a safe point using QSBR.
 PyAPI_FUNC(void) _PyObject_XSetRefDelayed(PyObject **p_obj, PyObject *obj);
 #else
 static inline void _PyObject_XSetRefDelayed(PyObject **p_obj, PyObject *obj)
