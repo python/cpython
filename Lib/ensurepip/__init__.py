0940129a44c57675fafd44660636dd5225f5e243
Matěj Cepl <mcepl@cepl.eu>
2025-06-13 22:58:14 +0200

    bpo-31046: Fix ensurepip script shebangs with --root
    When using `python -m ensurepip` with the `--root` option for staged
    installations, the generated pip script contained an incorrect shebang
    that pointed into the staging directory. This made the installation
    unusable once the staging directory was removed.
    
    This commit fixes the issue by using the internal pip `--executable`
    option to force the shebang to point to the correct, final
    interpreter path.
    
    It also corrects related pathing issues:
    
    - Removes the check that incorrectly disallowed using --root and
      --prefix together.
    - Defaults the installation prefix to `/` when --root is used alone,
      ensuring installation occurs at the base of the staging directory.
    
    References: https://github.com/python/cpython/pull/17634#discussion_r1622453325
    Signed-off-by: Matěj Cepl <mcepl@cepl.eu>

--- Lib/ensurepip/__init__.py
+++ Lib/ensurepip/__init__.py
@@ -130,8 +130,6 @@ def _bootstrap(*, root=None, upgrade=False, user=False,
 
     Note that calling this function will alter both sys.path and os.environ.
     """
-    if root is not None and prefix is not None:
-        raise ValueError("Cannot use 'root' and 'prefix' together")
     if altinstall and default_pip:
         raise ValueError("Cannot use altinstall and default_pip together")
 
@@ -162,16 +160,37 @@ def _bootstrap(*, root=None, upgrade=False, user=False,
 
         # Construct the arguments to be passed to the pip command
         args = ["install", "--no-cache-dir", "--no-index", "--find-links", tmpdir]
-        if root:
-            args += ["--root", root]
-        if prefix:
-            args += ["--prefix", prefix]
         if upgrade:
             args += ["--upgrade"]
-        if user:
-            args += ["--user"]
         if verbosity:
             args += ["-" + "v" * verbosity]
+
+        if user:
+            # --user is mutually exclusive with --root/--prefix,
+            # pip will enforce this.
+            args += ["--user"]
+        else:
+            # Handle installation paths.
+            # If --root is given but not --prefix, we default to a prefix of "/"
+            # so that the install happens at the root of the --root directory.
+            # Otherwise, pip would use the configured sys.prefix, e.g.
+            # /usr/local, and install into ${root}/usr/local/.
+            effective_prefix = prefix
+            if root and not prefix:
+                effective_prefix = "/"
+
+            if root:
+                args += ["--root", root]
+
+            if effective_prefix:
+                args += ["--prefix", effective_prefix]
+
+                # Force the script shebang to point to the correct, final
+                # executable path. This is necessary when --root is used.
+                executable_path = (
+                    Path(effective_prefix) / "bin" / Path(sys.executable).name
+                )
+                args += ["--executable", os.fsdecode(executable_path)]
 
         return _run_pip([*args, "pip"], [os.fsdecode(tmp_wheel_path)])
 
