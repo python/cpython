2394b275be6d42fe04ea39e95150ab8d9509bdf0
Manjusaka <me@manjusaka.me>
2025-06-17 22:57:40 +0800

    Fix review idea
    Signed-off-by: Manjusaka <me@manjusaka.me>

--- Lib/test/support/__init__.py
+++ Lib/test/support/__init__.py
@@ -3082,11 +3082,11 @@ def _supports_remote_attaching():
 
 def support_remote_exec_only(test):
     if not sys.is_remote_debug_enabled():
-        return unittest.skip("Remote debugging is not enabled")
+        return unittest.skip("Remote debugging is not enabled")(test)
     if sys.platform != "darwin" and sys.platform != "linux" and sys.platform != "win32":
-        return unittest.skip("Test only runs on Linux, Windows and MacOS")
+        return unittest.skip("Test only runs on Linux, Windows and MacOS")(test)
     if sys.platform == "linux" and not _supports_remote_attaching():
-        return unittest.skip("Test only runs on Linux with process_vm_readv support")
+        return unittest.skip("Test only runs on Linux with process_vm_readv support")(test)
     return _id(test)
 
 class EqualToForwardRef:
