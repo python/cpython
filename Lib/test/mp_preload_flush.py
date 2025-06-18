fdb30e11b691225536ff3216612f888ff6d76141
Duane Griffin <duaneg@dghda.com>
2025-06-18 22:17:46 +1200

    Be consistent and do not rely on asserts being enabled

--- Lib/test/mp_preload_flush.py
+++ Lib/test/mp_preload_flush.py
@@ -3,7 +3,8 @@ import sys
 
 modname = 'preloaded_module'
 if __name__ == '__main__':
-    assert modname not in sys.modules
+    if modname in sys.modules:
+        raise AssertionError(f'{modname!r} is not in sys.modules')
     multiprocessing.set_start_method('forkserver')
     multiprocessing.set_forkserver_preload([modname])
     for _ in range(2):
