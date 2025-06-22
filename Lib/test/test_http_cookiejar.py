befc9e26fc7067ed7c299f7d6e34e4ce50aff7c7
Weilin Du <108666168+LamentXU123@users.noreply.github.com>
2025-06-22 16:48:03 +0800

    Update test_http_cookiejar.py

--- Lib/test/test_http_cookiejar.py
+++ Lib/test/test_http_cookiejar.py
@@ -16,7 +16,7 @@ from http.cookiejar import (time2isoz, http2time, iso2time, time2netscape,
      CookieJar, DefaultCookiePolicy, LWPCookieJar, MozillaCookieJar,
      LoadError, lwp_cookie_str, DEFAULT_HTTP_PORT, escape_path,
      reach, is_HDN, domain_match, user_domain_match, request_path,
-     request_port, request_host, is_ip_like)
+     request_port, request_host, is_ip_like_hostname)
 
 mswindows = (sys.platform == "win32")
 
@@ -868,15 +868,15 @@ class CookieTests(unittest.TestCase):
         self.assertFalse(is_HDN("..foo"))
         self.assertFalse(is_HDN("foo."))
 
-    def test_is_ip_like(self):
-        self.assertTrue(is_ip_like('[::1]'))
-        self.assertTrue(is_ip_like('[2001:db8:85a3::8a2e:370:7334]'))
-        self.assertTrue(is_ip_like('192.168.0.1'))
+    def test_is_ip_like_hostname(self):
+        self.assertTrue(is_ip_like_hostname('[::1]'))
+        self.assertTrue(is_ip_like_hostname('[2001:db8:85a3::8a2e:370:7334]'))
+        self.assertTrue(is_ip_like_hostname('192.168.0.1'))
 
-        self.assertFalse(is_ip_like('256.256.256.256'))
-        self.assertFalse(is_ip_like('[::2001:db8:85a3::]'))
-        self.assertFalse(is_ip_like('acme.com'))
-        self.assertFalse(is_ip_like(''))
+        self.assertFalse(is_ip_like_hostname('256.256.256.256'))
+        self.assertFalse(is_ip_like_hostname('[::2001:db8:85a3::]'))
+        self.assertFalse(is_ip_like_hostname('acme.com'))
+        self.assertFalse(is_ip_like_hostname(''))
 
     def test_reach(self):
         self.assertEqual(reach("www.acme.com"), ".acme.com")
