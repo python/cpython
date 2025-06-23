bda121862e7d7f4684d9f0281f7d1f408c0c740c
Noam Cohen <noam@noam.me>
2025-06-23 22:42:09 +0300

    gh-131798: Optimize `_UNARY_NEGATIVE` (GH-135223)

--- Lib/test/test_capi/test_opt.py
+++ Lib/test/test_capi/test_opt.py
@@ -2432,6 +2432,24 @@ class TestUopsOptimization(unittest.TestCase):
         self.assertIn("_POP_TOP_FLOAT", uops)
 
 
+    def test_unary_negative_long_float_type(self):
+        def testfunc(n):
+            for _ in range(n):
+                a = 9397
+                f = 9397.0
+                x = -a + -a
+                y = -f + -f
+
+        testfunc(TIER2_THRESHOLD)
+
+        ex = get_first_executor(testfunc)
+        self.assertIsNotNone(ex)
+        uops = get_opnames(ex)
+
+        self.assertNotIn("_GUARD_TOS_INT", uops)
+        self.assertNotIn("_GUARD_NOS_INT", uops)
+        self.assertNotIn("_GUARD_TOS_FLOAT", uops)
+        self.assertNotIn("_GUARD_NOS_FLOAT", uops)
 
 def global_identity(x):
     return x
