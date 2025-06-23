0f0276d92e3903b57d7e1f9b5422684e57dc2b77
Sobhan Mohammadpour <somo@mit.edu>
2025-06-23 10:34:32 -0400

    Use walrus operator

--- Lib/copy.py
+++ Lib/copy.py
@@ -129,32 +129,25 @@ def deepcopy(x, memo=None, _nil=[]):
     copier = _deepcopy_dispatch.get(cls)
     if copier is not None:
         y = copier(x, memo)
-    else:
-        if issubclass(cls, type):
+    elif issubclass(cls, type):
             y = x # atomic copy
+    elif (copier := getattr(x, "__deepcopy__", None)) is not None:
+        y = copier(memo)
+    else:
+        if (reductor := dispatch_table.get(cls)):
+            rv = reductor(x)
+        elif (reductor := getattr(x, "__reduce_ex__", None)) is not None:
+            rv = reductor(4)
+        elif (reductor := getattr(x, "__reduce__", None)):
+            rv = reductor()
         else:
-            copier = getattr(x, "__deepcopy__", None)
-            if copier is not None:
-                y = copier(memo)
-            else:
-                reductor = dispatch_table.get(cls)
-                if reductor:
-                    rv = reductor(x)
-                else:
-                    reductor = getattr(x, "__reduce_ex__", None)
-                    if reductor is not None:
-                        rv = reductor(4)
-                    else:
-                        reductor = getattr(x, "__reduce__", None)
-                        if reductor:
-                            rv = reductor()
-                        else:
-                            raise Error(
-                                "un(deep)copyable object of type %s" % cls)
-                if isinstance(rv, str):
-                    y = x
-                else:
-                    y = _reconstruct(x, memo, *rv)
+            raise Error(
+                "un(deep)copyable object of type %s" % cls)
+        
+        if isinstance(rv, str):
+            y = x
+        else:
+            y = _reconstruct(x, memo, *rv)
 
     # If is its own copy, don't memoize.
     if y is not x:
