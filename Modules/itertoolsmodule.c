42009b60e44164ebc1f1b282f5ab377ae1019036
Pieter Eendebak <pieter.eendebak@gmail.com>
2025-06-30 13:31:59 +0200

    [3.14] gh-123471: Make itertools.product and itertools.combinations thread-safe (GH-132814)
    (cherry picked from commit 847d1c2cb4014f122df64e0db0b3b554c01779c6)
    
    Co-authored-by: Pieter Eendebak <pieter.eendebak@gmail.com>
    Co-authored-by: Kumar Aditya <kumaraditya@python.org>

--- Modules/itertoolsmodule.c
+++ Modules/itertoolsmodule.c
@@ -2177,7 +2177,7 @@ product_traverse(PyObject *op, visitproc visit, void *arg)
 }
 
 static PyObject *
-product_next(PyObject *op)
+product_next_lock_held(PyObject *op)
 {
     productobject *lz = productobject_CAST(op);
     PyObject *pool;
@@ -2261,6 +2261,16 @@ product_next(PyObject *op)
 empty:
     lz->stopped = 1;
     return NULL;
+}
+
+static PyObject *
+product_next(PyObject *op)
+{
+    PyObject *result;
+    Py_BEGIN_CRITICAL_SECTION(op);
+    result = product_next_lock_held(op);
+    Py_END_CRITICAL_SECTION()
+    return result;
 }
 
 static PyMethodDef product_methods[] = {
@@ -2410,7 +2420,7 @@ combinations_traverse(PyObject *op, visitproc visit, void *arg)
 }
 
 static PyObject *
-combinations_next(PyObject *op)
+combinations_next_lock_held(PyObject *op)
 {
     combinationsobject *co = combinationsobject_CAST(op);
     PyObject *elem;
@@ -2493,6 +2503,16 @@ combinations_next(PyObject *op)
 empty:
     co->stopped = 1;
     return NULL;
+}
+
+static PyObject *
+combinations_next(PyObject *op)
+{
+    PyObject *result;
+    Py_BEGIN_CRITICAL_SECTION(op);
+    result = combinations_next_lock_held(op);
+    Py_END_CRITICAL_SECTION()
+    return result;
 }
 
 static PyMethodDef combinations_methods[] = {
