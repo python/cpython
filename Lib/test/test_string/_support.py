c8976f4ca96932af33f253601e896ac19439d746
Dave <davepeck@gmail.com>
2025-07-09 19:16:15 +0000

    Add support for Interpolation default values.

--- Lib/test/test_string/_support.py
+++ Lib/test/test_string/_support.py
@@ -2,6 +2,23 @@ from string.templatelib import Interpolation
 
 
 class TStringBaseCase:
+    def assertInterpolationEqual(self, i, exp):
+        """Test Interpolation equality.
+
+        The *i* argument must be an Interpolation instance.
+
+        The *exp* argument must be a tuple of the form
+        (value, expression, conversion, format_spec) where the final three
+        items may be omitted and are assumed to be '', None and '' respectively.
+        """
+        # Merge in defaults for expression, conversion, and format_spec
+        defaults = ('', None, '')
+        expected = exp + defaults[len(exp) - 1:]
+
+        actual = (i.value, i.expression, i.conversion, i.format_spec)
+        self.assertEqual(actual, expected)
+
+
     def assertTStringEqual(self, t, strings, interpolations):
         """Test template string literal equality.
 
@@ -9,26 +26,14 @@ class TStringBaseCase:
 
         The *interpolations* argument must be a sequence of tuples which are
         compared against *t.interpolations*. Each tuple consists of
-        (value, expression, conversion, format_spec), though the final two
-        items may be omitted, and are assumed to be None and '' respectively.
+        (value, expression, conversion, format_spec), though the final three
+        items may be omitted and are assumed to be '' None, and '' respectively.
         """
         self.assertEqual(t.strings, strings)
         self.assertEqual(len(t.interpolations), len(interpolations))
 
         for i, exp in zip(t.interpolations, interpolations, strict=True):
-            if len(exp) == 4:
-                actual = (i.value, i.expression, i.conversion, i.format_spec)
-                self.assertEqual(actual, exp)
-                continue
-
-            if len(exp) == 3:
-                self.assertEqual((i.value, i.expression, i.conversion), exp)
-                self.assertEqual(i.format_spec, '')
-                continue
-
-            self.assertEqual((i.value, i.expression), exp)
-            self.assertEqual(i.format_spec, '')
-            self.assertIsNone(i.conversion)
+            self.assertInterpolationEqual(i, exp)
 
 
 def convert(value, conversion):
