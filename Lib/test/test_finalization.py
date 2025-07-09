073409b66d91e89f1cbeb945cbe7d5f199c34c38
Neil Schemenauer <nas@arctrix.com>
2025-07-09 13:42:10 -0700

    Add additional tests for weakref clearing.

--- Lib/test/test_finalization.py
+++ Lib/test/test_finalization.py
@@ -276,6 +276,7 @@ class SelfCycleFinalizationTest(TestBase, unittest.TestCase):
             s = SelfCycleResurrector()
             ids = [id(s)]
             wr = weakref.ref(s)
+            wrc = weakref.ref(s, lambda x: None)
             del s
             gc.collect()
             self.assert_del_calls(ids)
@@ -283,6 +284,9 @@ class SelfCycleFinalizationTest(TestBase, unittest.TestCase):
             # This used to be None because weakrefs were cleared before
             # calling finalizers.  Now they are cleared after.
             self.assertIsNot(wr(), None)
+            # A weakref with a callback is still cleared before calling
+            # finalizers.
+            self.assertIsNone(wrc())
             # When trying to destroy the object a second time, __del__
             # isn't called anymore (and the object isn't resurrected).
             self.clear_survivors()
@@ -379,12 +383,15 @@ class CycleChainFinalizationTest(TestBase, unittest.TestCase):
 
     def check_resurrecting_chain(self, classes):
         N = len(classes)
+        def dummy_callback(ref):
+            pass
         with SimpleBase.test():
             nodes = self.build_chain(classes)
             N = len(nodes)
             ids = [id(s) for s in nodes]
             survivor_ids = [id(s) for s in nodes if isinstance(s, SimpleResurrector)]
             wrs = [weakref.ref(s) for s in nodes]
+            wrcs = [weakref.ref(s, dummy_callback) for s in nodes]
             del nodes
             gc.collect()
             self.assert_del_calls(ids)
@@ -393,6 +400,10 @@ class CycleChainFinalizationTest(TestBase, unittest.TestCase):
                 # These values used to be None because weakrefs were cleared
                 # before calling finalizers.  Now they are cleared after.
                 self.assertIsNotNone(wr())
+            for wr in wrcs:
+                # Weakrefs with callbacks are still cleared before calling
+                # finalizers.
+                self.assertIsNone(wr())
             self.clear_survivors()
             gc.collect()
             self.assert_del_calls(ids)
