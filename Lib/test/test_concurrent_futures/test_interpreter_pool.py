c0554688d342af5aad8f4985ea1814923cf99db3
AN Long <aisk1988@gmail.com>
2025-07-10 21:07:19 +0900

    Fix test on non-supported platform

--- Lib/test/test_concurrent_futures/test_interpreter_pool.py
+++ Lib/test/test_concurrent_futures/test_interpreter_pool.py
@@ -1,3 +1,4 @@
+import _thread
 import asyncio
 import contextlib
 import io
@@ -502,6 +503,8 @@ class InterpreterPoolExecutorTest(
         self.assertStartsWith(self.executor._thread_name_prefix,
                               "InterpreterPoolExecutor-")
 
+    @unittest.skipUnless(hasattr(_thread, '_get_name'), "missing _thread._get_name")
+    def test_thread_name_prefix_with_thread_get_name(self):
         def f():
             import _thread
             return _thread._get_name()
