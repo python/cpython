db64ba5dc0c25719a1cb0df23f0e84320caead8b
Bénédikt Tran <10796600+picnixz@users.noreply.github.com>
2025-07-13 14:22:29 +0200

    !!

--- Tools/build/check_extension_modules.py
+++ Tools/build/check_extension_modules.py
@@ -470,8 +470,9 @@ def main():
     args = parser.parse_args()
     if args.debug:
         args.verbose = True
+    args.verbose = args.debug = True
     logging.basicConfig(
-        level=logging.DEBUG,
+        level=logging.DEBUG if args.debug else logging.INFO,
         format="[%(levelname)s] %(message)s",
     )
 
