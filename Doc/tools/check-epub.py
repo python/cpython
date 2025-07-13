1f58864f658d5dc20bd481f839e897871be4afec
Maciej Olko <maciej.olko@affirm.com>
2025-07-14 00:18:50 +0200

    Remove superfluous variable

--- Doc/tools/check-epub.py
+++ Doc/tools/check-epub.py
@@ -3,9 +3,8 @@ from pathlib import Path
 
 
 def main() -> int:
-    wrong_directory_msg = "Must run this script from the repo root"
     if not Path("Doc").exists() or not Path("Doc").is_dir():
-        raise RuntimeError(wrong_directory_msg)
+        raise RuntimeError("Must run this script from the repo root")
 
     with Path("Doc/epubcheck.txt").open(encoding="UTF-8") as f:
         warnings = [warning.split(" - ") for warning in f.read().splitlines()]
