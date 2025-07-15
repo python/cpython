e0f674d3e4e628448b3c9a8892f4c111ddb8104e
Peter Bierma <zintensitydev@gmail.com>
2025-07-15 04:40:14 -0400

    Revert "Fix type name in test_frame."
    This reverts commit 58e8577a85f88be2b39d7ae5818596252d5e9974.

--- Lib/test/test_frame.py
+++ Lib/test/test_frame.py
@@ -577,8 +577,9 @@ class TestFrameLocals(unittest.TestCase):
                     proxy[obj] = 0
 
     def test_constructor(self):
-        from types import FrameLocalsProxyType as FrameLocalsProxy
-        self.assertEqual(FrameLocalsProxy.__name__, 'FrameLocalsProxyType')
+        FrameLocalsProxy = type([sys._getframe().f_locals
+                                 for x in range(1)][0])
+        self.assertEqual(FrameLocalsProxy.__name__, 'FrameLocalsProxy')
 
         def make_frame():
             x = 1
