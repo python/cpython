c4c3cccfa1df8672df442fff16d8f865c2a25a57
Brandt Bucher <brandt@python.org>
2025-07-17 15:28:24 -0700

    Ditto for Mach-O, and *way* simplify the parsing!

--- Tools/jit/_targets.py
+++ Tools/jit/_targets.py
@@ -400,6 +400,7 @@ class _ELF(
                 value = _stencils.HoleValue.DATA
                 stencil = group.data
             if section_type == "SHT_PROGBITS":
+                assert "SectionData" in section
                 section_data_bytes = section["SectionData"]["Bytes"]
             else:
                 # Zeroed BSS data:
@@ -474,7 +475,11 @@ class _MachO(
         self, section: _schema.MachOSection, group: _stencils.StencilGroup
     ) -> None:
         assert section["Address"] >= len(group.code.body)
-        assert "SectionData" in section
+        if "SectionData" in section:
+            section_data_bytes = section["SectionData"]["Bytes"]
+        else:
+            # Zeroed BSS data:
+            section_data_bytes = [0] * section["Size"]
         flags = {flag["Name"] for flag in section["Attributes"]["Flags"]}
         name = section["Name"]["Value"]
         name = name.removeprefix(self.symbol_prefix)
@@ -483,23 +488,23 @@ class _MachO(
         if "PureInstructions" in flags:
             value = _stencils.HoleValue.CODE
             stencil = group.code
-            start_address = 0
-            group.symbols[name] = value, section["Address"] - start_address
         else:
             value = _stencils.HoleValue.DATA
             stencil = group.data
-            start_address = len(group.code.body)
-            group.symbols[name] = value, len(group.code.body)
-        base = section["Address"] - start_address
+        segment = section["Segment"]["Value"]
+        if segment == "__DATA":
+            value = _stencils.HoleValue.WRITABLE
+            section_data_bytes = []
+        else:
+            assert segment == "__TEXT", segment
+        base = len(stencil.body)
+        group.symbols[name] = value, base
         group.symbols[section["Index"]] = value, base
-        stencil.body.extend(
-            [0] * (section["Address"] - len(group.code.body) - len(group.data.body))
-        )
-        stencil.body.extend(section["SectionData"]["Bytes"])
+        stencil.body.extend(section_data_bytes)
         assert "Symbols" in section
         for wrapped_symbol in section["Symbols"]:
             symbol = wrapped_symbol["Symbol"]
-            offset = symbol["Value"] - start_address
+            offset = symbol["Value"] - section["Address"] + base
             name = symbol["Name"]["Name"]
             name = name.removeprefix(self.symbol_prefix)
             group.symbols[name] = value, offset
