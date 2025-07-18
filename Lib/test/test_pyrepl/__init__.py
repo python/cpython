4b7076772bca077165ca77b69bc5692b92aca785
Łukasz Langa <lukasz@langa.pl>
2025-07-18 18:01:36 +0200

    Skip if termios unavailable

--- Lib/test/test_pyrepl/__init__.py
+++ Lib/test/test_pyrepl/__init__.py
@@ -1,5 +1,14 @@
 import os
 from test.support import load_package_tests
+import unittest
+
+
+try:
+    import termios
+except ImportError:
+    raise unittest.SkipTest("termios required")
+else:
+    del termios
 
 
 def load_tests(*args):
