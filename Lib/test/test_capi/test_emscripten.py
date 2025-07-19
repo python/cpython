b2e0b81d3fc83495e0e55889298cbe9e0c68ae75
Hood Chatham <roberthoodchatham@gmail.com>
2025-07-19 14:58:09 +0200

    gh-124621: Emscripten: Add support for async input devices
    This is useful for implementing proper `input()`. It requires the
    JavaScript engine to support the wasm JSPI spec which is now stage 4.
    It is supported on Chrome since version 137 and on Firefox and node
    behind a flag.
    
    We override the `__wasi_fd_read()` syscall with our own variant that
    checks for a readAsync operation. If it has it, we use our own async
    variant of `fd_read()`, otherwise we use the original `fd_read()`.
    We also add a variant of `FS.createDevice()` called
    `FS.createAsyncInputDevice()`.
    
    Finally, if JSPI is available, we wrap the `main()` symbol with
    `WebAssembly.promising()` so that we can stack switch from `fd_read()`.
    If JSPI is not available, attempting to read from an AsyncInputDevice
    will raise an `OSError`.

---
+++ Lib/test/test_capi/test_emscripten.py
@@ -0,0 +1,24 @@
+import unittest
+from test.support import is_emscripten
+
+unittest.skipUnless(is_emscripten, "only available on Emscripten")
+
+from _testinternalcapi import emscripten_set_up_async_input_device
+from pathlib import Path
+
+
+class EmscriptenAsyncInputDeviceTest(unittest.TestCase):
+    def test_emscripten_async_input_device(self):
+        supported = emscripten_set_up_async_input_device()
+        p = Path("/dev/blah")
+        self.addCleanup(p.unlink)
+        if not supported:
+            with open(p, "r") as f:
+                self.assertRaises(OSError, f.readline)
+            return
+
+        with open(p, "r") as f:
+            for _ in range(10):
+                self.assertEqual(f.readline().strip(), "ab")
+                self.assertEqual(f.readline().strip(), "fi")
+                self.assertEqual(f.readline().strip(), "xy")
