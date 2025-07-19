c31694ab98f361a8e34918a66e1b61f83b67aaf8
monadchains <monadchains@gmail.com>
2025-07-19 17:44:58 +0200

    improve code

--- Lib/traceback.py
+++ Lib/traceback.py
@@ -1107,9 +1107,10 @@ class TracebackException:
             if suggestion:
                 self._str += f". Did you mean: '{suggestion}'?"
         elif exc_type and issubclass(exc_type, ModuleNotFoundError) and \
-                sys.flags.no_site:
-            self._str += ". Site initialization is disabled, did you forget to add the \
-                site-package directory to sys.path?"
+                sys.flags.no_site and \
+                getattr(exc_value, "name", None) not in sys.builtin_module_names:
+            self._str += (". Site initialization is disabled, did you forget to "
+                + "add the site-package directory to sys.path?")
         elif exc_type and issubclass(exc_type, (NameError, AttributeError)) and \
                 getattr(exc_value, "name", None) is not None:
             wrong_name = getattr(exc_value, "name", None)
