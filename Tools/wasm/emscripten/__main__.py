33591b48ab968a72efa92bfe97e7f18d0d4f77d6
Hood Chatham <roberthoodchatham@gmail.com>
2025-07-19 16:13:35 +0200

    Fix whitespace

--- Tools/wasm/emscripten/__main__.py
+++ Tools/wasm/emscripten/__main__.py
@@ -276,7 +276,7 @@ def configure_emscripten_python(context, working_dir):
             # We compute our own path, not following symlinks and pass it in so that
             # node_entry.mjs can set sys.executable correctly.
             # Intentionally allow word splitting on NODEFLAGS.
-            exec {host_runner} $NODEFLAGS $ARGS  {node_entry} --this-program="$($REALPATH "$0")" "$@"
+            exec {host_runner} $NODEFLAGS $ARGS {node_entry} --this-program="$($REALPATH "$0")" "$@"
             """
         )
     )
