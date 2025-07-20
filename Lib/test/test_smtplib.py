5ffc0989ac66902bb1332a023b7ed2fb9bcc5520
ynir3 <ynir3@bloomberg.net>
2025-07-20 09:39:17 +0200

    add patch

--- Lib/test/test_smtplib.py
+++ Lib/test/test_smtplib.py
@@ -23,7 +23,7 @@ from test.support import socket_helper
 from test.support import threading_helper
 from test.support import asyncore
 from test.support import smtpd
-from unittest.mock import Mock
+from unittest.mock import Mock, patch
 
 
 support.requires_working_socket(module=True)
