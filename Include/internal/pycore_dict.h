cbe6ebe15b3b8db00d3ca82a408cfd62b6d93b7d
Kumar Aditya <kumaraditya@python.org>
2025-07-28 22:04:57 +0530

    gh-134043: use stackrefs for dict lookup in `_PyObject_GetMethodStackRef` (#136412)
    Co-authored-by: Sam Gross <colesbury@gmail.com>

--- Include/internal/pycore_dict.h
+++ Include/internal/pycore_dict.h
@@ -113,6 +113,8 @@ extern Py_ssize_t _Py_dict_lookup(PyDictObject *mp, PyObject *key, Py_hash_t has
 extern Py_ssize_t _Py_dict_lookup_threadsafe(PyDictObject *mp, PyObject *key, Py_hash_t hash, PyObject **value_addr);
 extern Py_ssize_t _Py_dict_lookup_threadsafe_stackref(PyDictObject *mp, PyObject *key, Py_hash_t hash, _PyStackRef *value_addr);
 
+extern int _PyDict_GetMethodStackRef(PyDictObject *dict, PyObject *name, _PyStackRef *method);
+
 extern Py_ssize_t _PyDict_LookupIndex(PyDictObject *, PyObject *);
 extern Py_ssize_t _PyDictKeys_StringLookup(PyDictKeysObject* dictkeys, PyObject *key);
 
