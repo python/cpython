18786cfd9a96a5a34b7bb190b13e8f0a6b9d5e89
Uxio Garcia Andrade <uxiog21@gmail.com>
2025-08-01 18:05:38 +0200

    gh-137293: Print Exceptions in searching ELF File in Remote Debug

--- Python/remote_debug.h
+++ Python/remote_debug.h
@@ -732,6 +732,9 @@ search_linux_map_for_section(proc_handle_t *handle, const char* secname, const c
             if (retval) {
                 break;
             }
+            if (PyErr_Occured()){
+                PyErr_Clear();
+            }
         }
     }
 
