ecf492fbed3a3a0c980d65373ae39764acdf1132
Savannah Bailey <savannahostrowski@gmail.com>
2025-08-02 12:13:14 -0700

    Update to use pc, since that is what it is it :)

--- Tools/jit/_targets.py
+++ Tools/jit/_targets.py
@@ -593,7 +593,7 @@ def get_target(host: str) -> _COFF32 | _COFF64 | _ELF | _MachO:
         optimizer = _optimizers.OptimizerX86
         target = _COFF64(host, condition, args=args, optimizer=optimizer)
     elif re.fullmatch(r"x86_64-.*-linux-gnu", host):
-        host = "x86_64-unknown-linux-gnu"
+        host = "x86_64-pcn-linux-gnu"
         condition = "defined(__x86_64__) && defined(__linux__)"
         args = ["-fno-pic", "-mcmodel=medium", "-mlarge-data-threshold=0"]
         optimizer = _optimizers.OptimizerX86
