27726833424076d645adda45df16f44586a5f911
sobolevn <mail@sobolevn.me>
2025-08-02 10:28:44 +0300

    gh-137308: Replace a single docstring with `pass`

--- Lib/test/test_ast/test_ast.py
+++ Lib/test/test_ast/test_ast.py
@@ -220,6 +220,29 @@ class AST_Tests(unittest.TestCase):
                 # This also must not crash:
                 ast.parse(tree, optimize=2)
 
+    def test_docstring_optimization_single_node(self):
+        # https://github.com/python/cpython/issues/137308
+        for code in [
+            'class A: """Docstring"""',
+            'def func(): """Docstring"""',
+            'async def func(): """Docstring"""',
+        ]:
+            for opt_level in [0, 1, 2]:
+                with self.subTest(code=code, opt_level=opt_level):
+                    mod = ast.parse(code, optimize=opt_level)
+                    self.assertEqual(len(mod.body[0].body), 1)
+                    if opt_level == 2:
+                        self.assertIsInstance(mod.body[0].body[0], ast.Pass)
+                    else:
+                        self.assertIsInstance(mod.body[0].body[0], ast.Expr)
+                        self.assertIsInstance(
+                            mod.body[0].body[0].value,
+                            ast.Constant,
+                        )
+
+                    compile(mod, "a", "exec")
+                    compile(mod, "a", "exec", optimize=opt_level)
+
     def test_slice(self):
         slc = ast.parse("x[::]").body[0].value.slice
         self.assertIsNone(slc.upper)
