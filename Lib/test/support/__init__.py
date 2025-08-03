64dc75da2e49fb9cea1dd7f708391098feccad10
Serhiy Storchaka <storchaka@gmail.com>
2025-08-03 19:41:12 +0300

    [3.14] gh-137341: Remove more word duplications (GH-137342) (cherry picked from commit 1612dcbafe763014deefd679fe75ac5831a14a43)
    Co-authored-by: Serhiy Storchaka <storchaka@gmail.com>

--- Lib/test/support/__init__.py
+++ Lib/test/support/__init__.py
@@ -1679,7 +1679,7 @@ def check__all__(test_case, module, name_of_module=None, extra=(),
     'module'.
 
     The 'name_of_module' argument can specify (as a string or tuple thereof)
-    what module(s) an API could be defined in in order to be detected as a
+    what module(s) an API could be defined in order to be detected as a
     public API. One case for this is when 'module' imports part of its public
     API from other modules, possibly a C backend (like 'csv' and its '_csv').
 
