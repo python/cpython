22baff634a791fc60c966eeaafe1159c305d7d00
donBarbos <donbarbos@proton.me>
2025-08-13 10:43:43 +0400

    Use %T

--- Objects/typeobject.c
+++ Objects/typeobject.c
@@ -3516,8 +3516,8 @@ mro_check(PyTypeObject *type, PyObject *mro)
         if (!is_subtype_with_mro(lookup_tp_mro(solid), solid, solid_base(base))) {
             PyErr_Format(
                 PyExc_TypeError,
-                "%N.mro() returned base with unsuitable layout ('%.500s')",
-                type, base->tp_name);
+                "%N.mro() returned base with unsuitable layout ('%T')",
+                type, base);
             return -1;
         }
     }
