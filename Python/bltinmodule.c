0919dec7fae523b39e6d17831a60780bc1b18e27
tangyuan0821 <tangyuan0821@hotmail.com>
2025-08-18 16:07:54 +0800

    Fix segmentation fault in deeply nested filter() iterator chains

--- Python/bltinmodule.c
+++ Python/bltinmodule.c
@@ -579,6 +579,14 @@ filter_traverse(PyObject *self, visitproc visit, void *arg)
     return 0;
 }
 
+static int
+filter_clear(filterobject *lz)
+{
+    Py_CLEAR(lz->it);
+    Py_CLEAR(lz->func);
+    return 0;
+}
+
 static PyObject *
 filter_next(PyObject *self)
 {
@@ -660,8 +668,8 @@ PyTypeObject PyFilter_Type = {
     Py_TPFLAGS_DEFAULT | Py_TPFLAGS_HAVE_GC |
         Py_TPFLAGS_BASETYPE,            /* tp_flags */
     filter_doc,                         /* tp_doc */
-    filter_traverse,                    /* tp_traverse */
-    0,                                  /* tp_clear */
+    (traverseproc)filter_traverse,      /* tp_traverse */
+    (inquiry)filter_clear,              /* tp_clear */
     0,                                  /* tp_richcompare */
     0,                                  /* tp_weaklistoffset */
     PyObject_SelfIter,                  /* tp_iter */
