35b44708c4bcf414c0d3447fa183b1de53208c39
adam j hartz <hz@mit.edu>
2025-08-19 10:20:37 -0400

    revert changes to help(help); those should probably be a separate PR

--- Lib/pydoc.py
+++ Lib/pydoc.py
@@ -2189,9 +2189,7 @@ has the same effect as typing a particular string at the help> prompt.
     def help(self, request, is_cli=False):
         if isinstance(request, str):
             request = request.strip()
-            if request == 'help':
-                self.helphelp()
-            elif request == 'keywords': self.listkeywords()
+            if request == 'keywords': self.listkeywords()
             elif request == 'symbols': self.listsymbols()
             elif request == 'topics': self.listtopics()
             elif request == 'specialnames':
@@ -2207,35 +2205,9 @@ has the same effect as typing a particular string at the help> prompt.
             elif request in self.topics: self.showtopic(request)
             elif request: doc(request, 'Help on %s:', output=self._output, is_cli=is_cli)
             else: doc(str, 'Help on %s:', output=self._output, is_cli=is_cli)
-        elif request is builtins.help:
-            self.helphelp()
+        elif isinstance(request, Helper): self()
         else: doc(request, 'Help on %s:', output=self._output, is_cli=is_cli)
         self.output.write('\n')
-
-    def helphelp(self):
-        pager(textwrap.dedent("""\
-            help - Interactive Help
-            =======================
-
-            The built-in help function implements an interactive help utility.  You
-            can make use of it in a few different ways:
-
-            * Calling help() with no arguments starts an interactive help session.
-
-            * The behavior of help(x) depends on x's type:
-
-                * If x is a string, help(x) provides information about the given
-                  topic.  For example, help("class") will provide information about
-                  the "class" keyword, and help("math.sqrt") will provide
-                  information about the "math.sqrt" function.
-
-                * If x is a class or a built-in type, help(x) provides information
-                  about that type.  For example, help(str) will provide information
-                  about the str type.
-
-                * For all other objects, help(x) prints information about x's type.
-                  For example, help(20) will provide information about the int type.
-            """))
 
     def intro(self):
         self.output.write(_introdoc())
