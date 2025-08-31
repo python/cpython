58136554e649bbdf6770b39bd4f775d3c7b37e73
Tomasz Pytel <tompytel@gmail.com>
2025-08-31 16:37:27 -0400

    put back assert

--- Include/cpython/pyatomic_std.h
+++ Include/cpython/pyatomic_std.h
@@ -1096,9 +1096,9 @@ static inline void *
 _Py_atomic_memcpy_ptr_store_relaxed(void *dest, void *src, size_t n)
 {
     // _Py_USING_STD;
-    // assert(((uintptr_t)dest & (sizeof (void *) - 1)) == 0);
-    // assert(((uintptr_t)src & (sizeof (void *) - 1)) == 0);
-    // assert(n % sizeof(void *) == 0);
+    assert(((uintptr_t)dest & (sizeof (void *) - 1)) == 0);
+    assert(((uintptr_t)src & (sizeof (void *) - 1)) == 0);
+    assert(n % sizeof(void *) == 0);
 
     // memcpy(dest, src, n);
 
@@ -1121,10 +1121,10 @@ static inline void *
 _Py_atomic_memmove_ptr_store_relaxed(void *dest, void *src, size_t n)
 {
     // _Py_USING_STD;
-    // assert(((uintptr_t)dest & (sizeof (void *) - 1)) == 0);
-    // assert(((uintptr_t)src & (sizeof (void *) - 1)) == 0);
-    // assert(n % sizeof(void *) == 0);
-
+    assert(((uintptr_t)dest & (sizeof (void *) - 1)) == 0);
+    assert(((uintptr_t)src & (sizeof (void *) - 1)) == 0);
+    assert(n % sizeof(void *) == 0);
+`
     // memmove(dest, src, n);
 
     if (dest < src || dest >= (void *)((char *)src + n)) {
