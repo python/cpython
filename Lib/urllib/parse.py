4934ff283f80a94141aa927d9bc33949fa68100f
DavdaJames <jrdavda007@gmail.com>
2025-08-31 18:25:25 +0530

    replaced regex with char.isascii() and char.isalnum() and manual check for performance

--- Lib/urllib/parse.py
+++ Lib/urllib/parse.py
@@ -92,7 +92,7 @@ _WHATWG_C0_CONTROL_OR_SPACE = '\x00\x01\x02\x03\x04\x05\x06\x07\x08\t\n\x0b\x0c\
 _UNSAFE_URL_BYTES_TO_REMOVE = ['\t', '\r', '\n']
 
 # Allowed valid characters in parse_qsl
-_VALID_QUERY_CHARS = re.compile(r"^[A-Za-z0-9\-._~!$&'()*+,;=:@/?%]*$")
+_VALID_QUERY_CHARS = "-._~!$&'()*+,;=:@/?%"
 
 def clear_cache():
     """Clear internal performance caches. Undocumented; some tests want it."""
@@ -781,6 +781,15 @@ def parse_qs(qs, keep_blank_values=False, strict_parsing=False,
             parsed_result[name] = [value]
     return parsed_result
 
+def _is_valid_query(to_check: str) -> bool:
+    """Return True if all characters are valid per RFC 3986."""
+    for ch in to_check:
+        if not ch.isascii():
+            return False
+        if ch.isalnum() or ch in _VALID_QUERY_CHARS:
+            continue
+        return False
+    return True
 
 def parse_qsl(qs, keep_blank_values=False, strict_parsing=False,
               encoding='utf-8', errors='replace', max_num_fields=None, separator='&', *, _stacklevel=1):
@@ -860,7 +869,7 @@ def parse_qsl(qs, keep_blank_values=False, strict_parsing=False,
             if strict_parsing:
                 # Validate RFC3986 characters
                 to_check = (name_value.decode() if isinstance(name_value, bytes) else name_value)
-                if not _VALID_QUERY_CHARS.match(to_check):
+                if not _is_valid_query(to_check):
                     raise ValueError(f"Invalid characters in query string per RFC 3986: {name_value!r}")
             if value or keep_blank_values:
                 name = _unquote(name)
