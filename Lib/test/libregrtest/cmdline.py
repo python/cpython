6f17532a25ad641fb468031d088ca3bce998d77a
Victor Stinner <vstinner@python.org>
2025-09-09 00:45:42 +0200

    [3.13] gh-137242: Add a --no-randomize option, and use it in Android CI (GH-138649)
    Adds a --no-randomize option to the CI runner, so that randomisation can be easily
    disabled for --fast-ci and --slow-ci configurations on single-threaded testing platforms
    like Android, iOS, and Emscripten.
    
    ---------
    (cherry picked from commit 01895d233b4370e0c99f16c01a57cfd73c434b28)
    
    Co-authored-by: Victor Stinner <vstinner@python.org>
    Co-authored-by: Malcolm Smith <smith@chaquo.com>

--- Lib/test/libregrtest/cmdline.py
+++ Lib/test/libregrtest/cmdline.py
@@ -259,6 +259,9 @@ def _create_parser():
     group = parser.add_argument_group('Selecting tests')
     group.add_argument('-r', '--randomize', action='store_true',
                        help='randomize test execution order.' + more_details)
+    group.add_argument('--no-randomize', dest='no_randomize', action='store_true',
+                       help='do not randomize test execution order, even if '
+                       'it would be implied by another option')
     group.add_argument('-f', '--fromfile', metavar='FILE',
                        help='read names of tests to run from a file.' +
                             more_details)
@@ -512,6 +515,8 @@ def _parse_args(args, **kwargs):
                     ns.use_resources.append(r)
     if ns.random_seed is not None:
         ns.randomize = True
+    if ns.no_randomize:
+        ns.randomize = False
     if ns.verbose:
         ns.header = True
 
