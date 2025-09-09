431fd4c87160c33755f90418ccb5246d7d55feba
Maciej Olko <maciej.olko@affirm.com>
2025-09-10 00:08:04 +0200

    Add closing quote

--- Lib/gettext.py
+++ Lib/gettext.py
@@ -411,7 +411,7 @@ class GNUTranslations(NullTranslations):
                             self._charset = v.split('charset=')[1]
                         except IndexError:
                             raise ValueError(
-                                f"invalid content-type syntax in '{filename}: '{v}'. Expected 'charset='."
+                                f"invalid content-type syntax in '{filename}': '{v}'. Expected 'charset='."
                             ) from None
                     elif k == 'plural-forms':
                         v = v.split(';')
