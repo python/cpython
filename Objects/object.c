c5180d74e199e5bdb8f1c149dc3e956071ebf365
Pablo Galindo Salgado <Pablogsal@gmail.com>
2025-09-15 11:39:41 +0100

    gh-137992: Stop the world when calling PyRefTracer_SetTracer (#137994)

--- Objects/object.c
+++ Objects/object.c
@@ -3287,6 +3287,8 @@ _Py_SetRefcnt(PyObject *ob, Py_ssize_t refcnt)
 
 int PyRefTracer_SetTracer(PyRefTracer tracer, void *data) {
     _Py_AssertHoldsTstate();
+
+    _PyEval_StopTheWorldAll(&_PyRuntime);
     if (_PyRuntime.ref_tracer.tracer_func != NULL) {
         _PyReftracerTrack(NULL, PyRefTracer_TRACKER_REMOVED);
         if (PyErr_Occurred()) {
@@ -3295,6 +3297,7 @@ int PyRefTracer_SetTracer(PyRefTracer tracer, void *data) {
     }
     _PyRuntime.ref_tracer.tracer_func = tracer;
     _PyRuntime.ref_tracer.tracer_data = data;
+    _PyEval_StartTheWorldAll(&_PyRuntime);
     return 0;
 }
 
