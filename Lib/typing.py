1d35e9b6169f2d4c9a5d25783600ca7bdbefce98
Xiaoming Wang <bzoracler@gmail.com>
2025-09-15 14:04:00 +1200

    Simplify args tuple creation

--- Lib/typing.py
+++ Lib/typing.py
@@ -1113,7 +1113,7 @@ def _paramspec_prepare_subst(self, alias, args):
     params = alias.__parameters__
     i = params.index(self)
     if i == len(args) and self.has_default():
-        args = [*args, self.__default__]
+        args = (*args, self.__default__)
     if i >= len(args):
         raise TypeError(f"Too few arguments for {alias}")
     # Special case where Z[[int, str, bool]] == Z[int, str, bool] in PEP 612.
@@ -1123,8 +1123,6 @@ def _paramspec_prepare_subst(self, alias, args):
     # Convert lists to tuples to help other libraries cache the results.
     elif isinstance(args[i], list):
         args = (*args[:i], tuple(args[i]), *args[i+1:])
-    else:
-        args = tuple(args)
     return args
 
 
