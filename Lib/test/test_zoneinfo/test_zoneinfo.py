abb9d1674fe770212926c40a92ee62345bd5ca34
Stan Ulbrych <89152624+StanFromIreland@users.noreply.github.com>
2025-09-16 16:27:59 +0100

    Lint

--- Lib/test/test_zoneinfo/test_zoneinfo.py
+++ Lib/test/test_zoneinfo/test_zoneinfo.py
@@ -22,7 +22,6 @@ from test.support.os_helper import EnvironmentVarGuard, FakePath
 from test.test_zoneinfo import _support as test_support
 from test.test_zoneinfo._support import TZPATH_TEST_LOCK, ZoneInfoTestBase
 from test.support.import_helper import import_module, CleanImport
-from test.support.script_helper import assert_python_ok
 
 lzma = import_module('lzma')
 py_zoneinfo, c_zoneinfo = test_support.get_modules()
