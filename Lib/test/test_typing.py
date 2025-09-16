25ab548444a8b152247e6152f968b1cc70556d1d
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2025-09-16 07:28:39 -0700

    [3.14] gh-137226: Fix get_type_hints() on generic TypedDict with stringified annotations (GH-138953)
    This issue appears specifically for TypedDicts because the TypedDict constructor
    code converts string annotations to ForwardRef objects, and those are not evaluated
    properly by the get_type_hints() stack because of other shenanigans with type
    parameters.
    
    This issue does not affect normal generic classes because their annotations are not
    pre-converted to ForwardRefs.
    
    The fix attempts to restore the pre- GH-137227 behavior in the narrow scenario where
    the issue manifests. It mostly makes changes only in the paths accessible from get_type_hints(),
    ensuring that newer APIs (such as evaluate_forward_ref() and annotationlib) are not affected
    by get_type_hints()'s past odd choices. This PR does not fix issue GH-138949, an older issue I
    discovered while playing around with this one; we'll need a separate and perhaps more
    invasive fix for that, but it should wait until after 3.14.0.
    (cherry picked from commit 6d6aba252301cdf9d5ae3189629e1e43101dd58f)
    
    Co-authored-by: Jelle Zijlstra <jelle.zijlstra@gmail.com>

--- Lib/test/test_typing.py
+++ Lib/test/test_typing.py
@@ -7132,6 +7132,19 @@ class GetTypeHintsTests(BaseTestCase):
         right_hints = get_type_hints(t.add_right, globals(), locals())
         self.assertEqual(right_hints['node'], Node[T])
 
+    def test_stringified_typeddict(self):
+        ns = run_code(
+            """
+            from __future__ import annotations
+            from typing import TypedDict
+            class TD[UniqueT](TypedDict):
+                a: UniqueT
+            """
+        )
+        TD = ns['TD']
+        self.assertEqual(TD.__annotations__, {'a': EqualToForwardRef('UniqueT', owner=TD, module=TD.__module__)})
+        self.assertEqual(get_type_hints(TD), {'a': TD.__type_params__[0]})
+
 
 class GetUtilitiesTestCase(TestCase):
     def test_get_origin(self):
@@ -8668,8 +8681,8 @@ class TypedDictTests(BaseTestCase):
                     child = _make_td(
                         child_future, "Child", {"child": "int"}, "Base", {"Base": base}
                     )
-                    base_anno = ForwardRef("int", module="builtins") if base_future else int
-                    child_anno = ForwardRef("int", module="builtins") if child_future else int
+                    base_anno = ForwardRef("int", module="builtins", owner=base) if base_future else int
+                    child_anno = ForwardRef("int", module="builtins", owner=child) if child_future else int
                     self.assertEqual(base.__annotations__, {'base': base_anno})
                     self.assertEqual(
                         child.__annotations__, {'child': child_anno, 'base': base_anno}
