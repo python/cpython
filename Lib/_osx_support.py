8a5e3de5029fece4f39f4b1c8ccd50db460bed5a
yihong0618 <zouzou0208@gmail.com>
2025-09-18 15:39:44 +0800

    fix: drop unused code in _osx_support.py
    Signed-off-by: yihong0618 <zouzou0208@gmail.com>

--- Lib/_osx_support.py
+++ Lib/_osx_support.py
@@ -149,32 +149,6 @@ def _save_modified_value(_config_vars, cv, newvalue):
     _config_vars[cv] = newvalue
 
 
-_cache_default_sysroot = None
-def _default_sysroot(cc):
-    """ Returns the root of the default SDK for this system, or '/' """
-    global _cache_default_sysroot
-
-    if _cache_default_sysroot is not None:
-        return _cache_default_sysroot
-
-    contents = _read_output('%s -c -E -v - </dev/null' % (cc,), True)
-    in_incdirs = False
-    for line in contents.splitlines():
-        if line.startswith("#include <...>"):
-            in_incdirs = True
-        elif line.startswith("End of search list"):
-            in_incdirs = False
-        elif in_incdirs:
-            line = line.strip()
-            if line == '/usr/include':
-                _cache_default_sysroot = '/'
-            elif line.endswith(".sdk/usr/include"):
-                _cache_default_sysroot = line[:-12]
-    if _cache_default_sysroot is None:
-        _cache_default_sysroot = '/'
-
-    return _cache_default_sysroot
-
 def _supports_universal_builds():
     """Returns True if universal builds are supported on this system"""
     # As an approximation, we assume that if we are running on 10.4 or above,
