e6feee9c6b369ae7f9592381d53852f204e50db8
Locked-chess-official <13140752715@163.com>
2025-10-05 19:17:11 +0800

    remove the extra spaces

--- Lib/idlelib/run.py
+++ Lib/idlelib/run.py
@@ -266,13 +266,13 @@ def print_exception():
                 for subline in line.rstrip().splitlines():
                     print(f"{prefix2}| {subline}", file=efile)
         lines = get_message_lines(typ, exc, tb)
-        for line in lines:            
+        for line in lines:
             print(f"{prefix2}| {line}", end="", file=efile)
         for i, sub in enumerate(exc.exceptions, 1):
             if i == 1:
                 first_line_pre = "+-"
             else:
-                first_line_pre = "  "                
+                first_line_pre = "  "
             print(f"{prefix2}{first_line_pre}+---------------- {i} ----------------", file=efile)
             if id(sub) not in seen:
                 if not prefix:
@@ -280,7 +280,7 @@ def print_exception():
                 else:
                     print_exc(type(sub), sub, sub.__traceback__, prefix + "  ")
                 need_print_underline = not isinstance(sub, BaseExceptionGroup)
-            else:                
+            else:
                 print(f"{prefix2}  | <exception {type(sub).__name__} has printed>")
                 need_print_underline = True
             if need_print_underline:
@@ -289,8 +289,8 @@ def print_exception():
     def print_exc(typ, exc, tb, prefix=""):
         seen.add(id(exc))
         context = exc.__context__
-        cause = exc.__cause__        
-        prefix2 = f"{prefix}| " if prefix else "" 
+        cause = exc.__cause__
+        prefix2 = f"{prefix}| " if prefix else ""
         if cause is not None and id(cause) not in seen:
             print_exc(type(cause), cause, cause.__traceback__, prefix)            
             print(f"{prefix2}\n{prefix2}The above exception was the direct cause "
@@ -298,15 +298,14 @@ def print_exception():
         elif (context is not None and
               not exc.__suppress_context__ and
               id(context) not in seen):
-            print_exc(type(context), context, context.__traceback__, prefix)
-            
+            print_exc(type(context), context, context.__traceback__, prefix)            
             print(f"{prefix2}\n{prefix2}During handling of the above exception, "
                   f"another exception occurred:\n{prefix2}", file=efile)
         if isinstance(exc, BaseExceptionGroup):
             print_exc_group(typ, exc, tb, prefix=prefix)
         else:
-            if tb:                
-                print(f"{prefix2}Traceback (most recent call last):", file=efile)                
+            if tb:
+                print(f"{prefix2}Traceback (most recent call last):", file=efile)
                 tbe = traceback.extract_tb(tb)
                 cleanup_traceback(tbe, exclude)
                 if prefix:
