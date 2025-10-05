d48c82e0d7829aa98e3b2acb1e4f9c877533dd60
Locked-chess-official <13140752715@163.com>
2025-10-05 19:22:31 +0800

    remove

--- Lib/idlelib/run.py
+++ Lib/idlelib/run.py
@@ -292,13 +292,13 @@ def print_exception():
         cause = exc.__cause__
         prefix2 = f"{prefix}| " if prefix else ""
         if cause is not None and id(cause) not in seen:
-            print_exc(type(cause), cause, cause.__traceback__, prefix)            
+            print_exc(type(cause), cause, cause.__traceback__, prefix)
             print(f"{prefix2}\n{prefix2}The above exception was the direct cause "
                   f"of the following exception:\n{prefix2}", file=efile)
         elif (context is not None and
               not exc.__suppress_context__ and
               id(context) not in seen):
-            print_exc(type(context), context, context.__traceback__, prefix)            
+            print_exc(type(context), context, context.__traceback__, prefix)
             print(f"{prefix2}\n{prefix2}During handling of the above exception, "
                   f"another exception occurred:\n{prefix2}", file=efile)
         if isinstance(exc, BaseExceptionGroup):
