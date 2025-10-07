aa85f9d79e7c6bfd9f2fa6ed70a00ed9c9a39f61
Dino Viehland <dinoviehland@fb.com>
2025-10-07 14:52:23 -0700

    dir() doesn't reify module

--- Lib/test/test_import/__init__.py
+++ Lib/test/test_import/__init__.py
@@ -2558,6 +2558,25 @@ class LazyImportTests(unittest.TestCase):
 
         self.assertFalse("test.test_import.data.lazy_imports.basic2" in sys.modules)
 
+    def test_basic_unused_dir(self):
+        try:
+            import test.test_import.data.lazy_imports.basic_unused
+        except ImportError as e:
+            self.fail('lazy import failed')
+
+        x = dir(test.test_import.data.lazy_imports.basic_unused)
+        self.assertIn("test", x)
+        self.assertFalse("test.test_import.data.lazy_imports.basic2" in sys.modules)
+
+    def test_basic_dir(self):
+        try:
+            from test.test_import.data.lazy_imports import basic_dir
+        except ImportError as e:
+            self.fail('lazy import failed')
+
+        self.assertIn("test", basic_dir.x)
+        self.assertFalse("test.test_import.data.lazy_imports.basic2" in sys.modules)
+
     def test_basic_used(self):
         try:
             import test.test_import.data.lazy_imports.basic_used
