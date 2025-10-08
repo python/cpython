d729fcf6e372ccc49ee15948ef1bab320af95a0b
Miss Islington (bot) <31488909+miss-islington@users.noreply.github.com>
2025-10-08 15:52:08 +0200

    [3.14] gh-137589: Zipfile tests: close file objects (GH-138080) (#139766)
    gh-137589: Zipfile tests: close file objects (GH-138080)
    
    Zipfile tests: close file objects
    (cherry picked from commit 5cea8435943b4f9d22c89c80d86c2ba3b392c6f9)
    
    Co-authored-by: Rogdham <3994389+Rogdham@users.noreply.github.com>

--- Lib/test/test_zipfile/_path/test_path.py
+++ Lib/test/test_zipfile/_path/test_path.py
@@ -274,7 +274,8 @@ class TestPath(unittest.TestCase):
         """
         zipfile_ondisk = self.zipfile_ondisk(alpharep)
         pathlike = FakePath(str(zipfile_ondisk))
-        zipfile.Path(pathlike)
+        root = zipfile.Path(pathlike)
+        root.root.close()
 
     @pass_alpharep
     def test_traverse_pathlike(self, alpharep):
@@ -373,6 +374,7 @@ class TestPath(unittest.TestCase):
         root = zipfile.Path(self.zipfile_ondisk(alpharep))
         assert root.name == 'alpharep.zip' == root.filename.name
         assert root.stem == 'alpharep' == root.filename.stem
+        root.root.close()
 
     @pass_alpharep
     def test_suffix(self, alpharep):
@@ -574,11 +576,13 @@ class TestPath(unittest.TestCase):
     )
     def test_pickle(self, alpharep, path_type, subpath):
         zipfile_ondisk = path_type(str(self.zipfile_ondisk(alpharep)))
-
-        saved_1 = pickle.dumps(zipfile.Path(zipfile_ondisk, at=subpath))
+        root = zipfile.Path(zipfile_ondisk, at=subpath)
+        saved_1 = pickle.dumps(root)
+        root.root.close()
         restored_1 = pickle.loads(saved_1)
         first, *rest = restored_1.iterdir()
         assert first.read_text(encoding='utf-8').startswith('content of ')
+        restored_1.root.close()
 
     @pass_alpharep
     def test_extract_orig_with_implied_dirs(self, alpharep):
@@ -590,6 +594,7 @@ class TestPath(unittest.TestCase):
         # wrap the zipfile for its side effect
         zipfile.Path(zf)
         zf.extractall(source_path.parent)
+        zf.close()
 
     @pass_alpharep
     def test_getinfo_missing(self, alpharep):
