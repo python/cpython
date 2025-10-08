44e9cf74ab63eb2fc36ca3ee757f9869894ca981
Shamil Abdulaev <ashm.tech@proton.me>
2025-10-09 02:10:29 +0300

    remove LOAD_GLOBAL

--- Python/optimizer.c
+++ Python/optimizer.c
@@ -737,11 +737,6 @@ translate_bytecode_to_trace(
                 ADD_TO_TRACE(_TIER2_RESUME_CHECK, 0, 0, target);
                 break;
 
-            case LOAD_GLOBAL:
-                _Py_BloomFilter_Add(dependencies, frame->f_globals);
-                _Py_BloomFilter_Add(dependencies, frame->f_builtins);
-                _Py_FALLTHROUGH;
-
             default:
             {
                 const struct opcode_macro_expansion *expansion = &_PyOpcode_macro_expansion[opcode];
