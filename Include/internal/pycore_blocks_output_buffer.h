29a8230e5060a47396a93156849eaed3baedd26b
Emma Harper Smith <emma@emmatyping.dev>
2025-10-11 18:48:08 -0700

    Fix lint

--- Include/internal/pycore_blocks_output_buffer.h
+++ Include/internal/pycore_blocks_output_buffer.h
@@ -243,7 +243,7 @@ static inline PyObject *
 _BlocksOutputBuffer_Finish(_BlocksOutputBuffer *buffer,
                            const Py_ssize_t avail_out)
 {
-    return PyBytesWriter_FinishWithSize(buffer->writer, 
+    return PyBytesWriter_FinishWithSize(buffer->writer,
                                         buffer->allocated - avail_out);
 }
 
