27ac85647702e075bd670f247016a9fcd8723702
Rafael Weingartner-Ortner <weingartner.rafael@hotmail.com>
2025-10-12 08:34:26 +0200

    Add supported sunder names to Enum `__dir__`
    This change adds the sunder names `_generate_next_value_`
    and `_missing_` to the `__dir__` method of `EnumType` and `Enum`.
    In Addition, The instance level sunder names
    `_add_alias_` and `_add_value_alias_` are added to `Enum.__dir__`.
    
    With the sunder names exposed in the `dir()` method,
    the REPL autocomplete will also show them.
    
    Co-Authored-By: SimonGPrs <107691772+SimonGPrs@users.noreply.github.com>

--- Lib/test/test_enum.py
+++ Lib/test/test_enum.py
@@ -5105,6 +5105,8 @@ class TestStdLib(unittest.TestCase):
                 ('__qualname__', 'TestStdLib.Color'),
                 ('__init_subclass__', getattr(self.Color, '__init_subclass__')),
                 ('__iter__', self.Color.__iter__),
+                ('_missing_', self.Color._missing_),
+                ('_generate_next_value_', self.Color._generate_next_value_),
                 ))
         result = dict(inspect.getmembers(self.Color))
         self.assertEqual(set(values.keys()), set(result.keys()))
@@ -5147,6 +5149,10 @@ class TestStdLib(unittest.TestCase):
                     defining_class=self.Color, object='Color'),
                 Attribute(name='__qualname__', kind='data',
                     defining_class=self.Color, object='TestStdLib.Color'),
+                Attribute(name='_missing_', kind='class method',
+                    defining_class=Enum, object=self.Color._missing_),
+                Attribute(name='_generate_next_value_', kind='static method',
+                    defining_class=self.Color, object=staticmethod(self.Color._generate_next_value_)),
                 Attribute(name='YELLOW', kind='data',
                     defining_class=self.Color, object=self.Color.YELLOW),
                 Attribute(name='MAGENTA', kind='data',
@@ -5533,6 +5539,7 @@ def enum_dir(cls):
             '__class__', '__contains__', '__doc__', '__getitem__',
             '__iter__', '__len__', '__members__', '__module__',
             '__name__', '__qualname__',
+            '_generate_next_value_', '_missing_',
             ]
             + cls._member_names_
             )
@@ -5548,7 +5555,8 @@ def enum_dir(cls):
 
 def member_dir(member):
     if member.__class__._member_type_ is object:
-        allowed = set(['__class__', '__doc__', '__eq__', '__hash__', '__module__', 'name', 'value'])
+        allowed = set(['__class__', '__doc__', '__eq__', '__hash__', '__module__', 'name', 'value',
+                       '_generate_next_value_', '_missing_', '_add_alias_', '_add_value_alias_'])
     else:
         allowed = set(dir(member))
     for cls in member.__class__.mro():
