0b773f9ba0687dbcaac20fa44b87495000898726
Savannah Ostrowski <savannah@python.org>
2025-10-16 20:29:07 +0200

    remove fno-pic

--- Tools/jit/_targets.py
+++ Tools/jit/_targets.py
@@ -585,9 +585,8 @@ def get_target(host: str) -> _COFF32 | _COFF64 | _ELF | _MachO:
     elif re.fullmatch(r"x86_64-apple-darwin.*", host):
         host = "x86_64-apple-darwin"
         condition = "defined(__x86_64__) && defined(__APPLE__)"
-        args = ["-fno-pic"]
         optimizer = _optimizers.OptimizerX86
-        target = _MachO(host, condition, args=args, optimizer=optimizer)
+        target = _MachO(host, condition, optimizer=optimizer)
     elif re.fullmatch(r"x86_64-pc-windows-msvc", host):
         host = "x86_64-pc-windows-msvc"
         condition = "defined(_M_X64)"
