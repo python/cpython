2735a71d05ff2c8b63631b972d1400eebf0c7f37
Pieter Eendebak <pieter.eendebak@gmail.com>
2025-10-17 08:00:06 +0200

    Update Objects/setobject.c
    Co-authored-by: Mikhail Efimov <efimov.mikhail@gmail.com>

--- Objects/setobject.c
+++ Objects/setobject.c
@@ -1177,7 +1177,7 @@ make_new_set_basetype(PyTypeObject *type, PyObject *iterable)
 void
 _PyFrozenSet_MaybeUntrack(PyObject *op)
 {
-    if ((op ==NULL) || !(PyFrozenSet_CheckExact(op))) {
+    if ((op == NULL) || !(PyFrozenSet_CheckExact(op))) {
         return;
     }
     // if all elements of a frozenset are not tracked, we untrack the object
