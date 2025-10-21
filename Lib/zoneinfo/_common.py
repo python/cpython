bc9a0fb8a9f70dc85ee41375022c7d15e862a85b
Weilin Du <108666168+LamentXU123@users.noreply.github.com>
2025-10-22 00:47:06 +0800

    Improve error message

--- Lib/zoneinfo/_common.py
+++ Lib/zoneinfo/_common.py
@@ -14,17 +14,19 @@ def load_tzdata(key):
         if path.is_dir():
             raise IsADirectoryError
         return path.open("rb")
+    except UnicodeEncodeError:
+        # This error occurs because of unsupported filesystem encoding which
+        # depends on the locale, such as keys containing a surrogate character.
+        ZoneInfoNotFoundError(f"key {key} contains non-UTF8 character(s)")
     except (ImportError, FileNotFoundError, UnicodeEncodeError, IsADirectoryError):
-        # There are four types of exception that can be raised that all amount
-        # to "we cannot find this key":
+        # There are three other types of exception that can be raised that
+        # all amount to "we cannot find this key":
         #
         # ImportError: If package_name doesn't exist (e.g. if tzdata is not
         #   installed, or if there's an error in the folder name like
         #   Amrica/New_York)
         # FileNotFoundError: If resource_name doesn't exist in the package
         #   (e.g. Europe/Krasnoy)
-        # UnicodeEncodeError: If package_name or resource_name are not UTF-8,
-        #   such as keys containing a surrogate character.
         # IsADirectoryError: If package_name without a resource_name specified.
         raise ZoneInfoNotFoundError(f"No time zone found with key {key}")
 
