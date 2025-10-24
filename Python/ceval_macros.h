96ff7b83cc3a14d8b1c685dcbcb37633017114aa
Bonu Krishna Chaitanya <bkc14042005@gmail.com>
2025-10-24 22:46:27 +0530

    commit

--- Python/ceval_macros.h
+++ Python/ceval_macros.h
@@ -83,10 +83,9 @@
 #        if !_Py__has_attribute(preserve_none) || !_Py__has_attribute(musttail)
 #            error "This compiler does not have support for efficient tail calling."
 #        endif
+#    elif defined(_MSC_VER) && (_MSC_VER < 1950)
+#        error "You need atleast VS 2026 / PlatformToolset v145 for tail calling."
 #    endif
-#elif defined(_MSC_VER) && (_MSC_VER < 1950)
-#    error "You need atleast VS 2026 / PlatformToolset v145 for tail calling."
-#endif
 
     // Note: [[clang::musttail]] works for GCC 15, but not __attribute__((musttail)) at the moment.
 #   define Py_MUSTTAIL [[clang::musttail]]
