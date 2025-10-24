d8901b1eb1b0fa15f2f97a001b371367de3507ec
Bonu Krishna Chaitanya <bkc14042005@gmail.com>
2025-10-24 19:25:39 +0530

    add space after #endif

--- Python/ceval_macros.h
+++ Python/ceval_macros.h
@@ -83,6 +83,7 @@
 #        error "This compiler does not have support for efficient tail calling."
 #    endif
 #endif
+
     // Note: [[clang::musttail]] works for GCC 15, but not __attribute__((musttail)) at the moment.
 #   define Py_MUSTTAIL [[clang::musttail]]
 #   define Py_PRESERVE_NONE_CC __attribute__((preserve_none))
