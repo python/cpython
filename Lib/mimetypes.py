00558b5d0714eb7c357efa569f8868244330becf
John Franey <1728528+johnfraney@users.noreply.github.com>
2025-11-03 04:36:51 -0400

    Add .cjs mimetype to common_types
    Adds .cjs mimetype of `text/javascript` for CommonJS modules to
    common_types. See here for more information:
    https://nodejs.org/api/modules.html#enabling

--- Lib/mimetypes.py
+++ Lib/mimetypes.py
@@ -681,6 +681,7 @@ def _default_mime_types():
 
     # Please sort these too
     common_types = _common_types_default = {
+        '.cjs' : 'application/node',
         '.rtf' : 'application/rtf',
         '.apk' : 'application/vnd.android.package-archive',
         '.midi': 'audio/midi',
