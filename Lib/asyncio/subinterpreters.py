c8dce266a84d2f850a25aa1ddbc81ec819b6acfb
Timofey Ivankov <playboicartina@gmail.com>
2025-11-04 19:54:59 +0300

    changed run_in_subinterpreter to async version

--- Lib/asyncio/subinterpreters.py
+++ Lib/asyncio/subinterpreters.py
@@ -1,6 +1,6 @@
 """High-level support for working with subinterpreters in asyncio"""
 
-import os
+import asyncio
 from concurrent.futures.interpreter import InterpreterPoolExecutor
 
 __all__ = "run_in_subinterpreter",
@@ -12,9 +12,7 @@ async def run_in_subinterpreter(func, /, *args, **kwargs):
     Run a Python function in a subinterpreter asynchronously.
 
     This function executes the given callable `func` in a subinterpreter
-    using `InterpreterPoolExecutor`. It chooses the number of 
-    subinterpreters based on the CPU count of the host system, 
-    defaulting to half the available cores if detectable.
+    using `InterpreterPoolExecutor`.
 
     Args:
         func (Callable): The function to run in the subinterpreter.
@@ -23,16 +21,8 @@ async def run_in_subinterpreter(func, /, *args, **kwargs):
 
     Returns:
         Any: The result returned by `func`.
-
-    Raises:
-        RuntimeError: If CPU count cannot be determined and a default 
-        cannot be safely chosen.
     """
     
-    n: int | None = os.cpu_count()
-    worker_count: int | None = None
-    if n:
-        worker_count = max(1, n // 2)  
-    with InterpreterPoolExecutor(max_workers=worker_count) as pool:
-        future = pool.submit(func, *args, **kwargs)
-        return future.result()
+    with InterpreterPoolExecutor() as pool:
+        loop = asyncio.get_running_loop()
+        return await loop.run_in_executor(pool, func, *args, **kwargs)
