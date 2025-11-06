70f543300d1524f894ae50fb6ae4708751553148
Victor Stinner <vstinner@python.org>
2025-11-06 12:45:18 +0100

    Fix NULL test on macOS and Windows

--- Lib/test/test_capi/test_object.py
+++ Lib/test/test_capi/test_object.py
@@ -297,8 +297,8 @@ class CAPITest(unittest.TestCase):
         # test NULL object
         output = self.pyobject_dump(NULL)
         hex_regex = r'(0x)?[0-9a-fA-F]+'
-        self.assertEqual(output,
-                         '<object at (nil) is freed>')
+        self.assertRegex(output,
+                         r'<object at (\(nil\)|0x0|0+) is freed>')
 
 
 if __name__ == "__main__":
