3752fa0d6f2932c674dd73ef255a88dbf3755b17
Zachary Spector <public@zacharyspector.com>
2025-11-08 11:42:47 +1300

    Extend `functools.cached_property` so the user can select the attrname
    A class with `__slots__`, which ordinarily keeps them empty to minimize memory use, may define some properties as `@cached_property(slot)` to instantiate them only on demand.

--- Lib/functools.py
+++ Lib/functools.py
@@ -1110,14 +1110,34 @@ _NOT_FOUND = object()
 
 class cached_property:
     def __init__(self, func):
+        if callable(func):
+            self.func = func
+            self.attrname = None
+            self.slot = False
+            self.__doc__ = func.__doc__
+            self.__module__ = func.__module__
+        else:
+            self.attrname = func
+            self.slot = True
+            self.func = None
+
+    def __call__(self, func):
+        if self.slot is None:
+            raise TypeError("Can only use one function per 'cached_property'")
+        if not callable(func):
+            raise TypeError(
+                "Can only cache functions as properties, not this", func
+            )
         self.func = func
-        self.attrname = None
         self.__doc__ = func.__doc__
         self.__module__ = func.__module__
+        return self
 
     def __set_name__(self, owner, name):
         if self.attrname is None:
             self.attrname = name
+        elif self.slot:
+            return
         elif name != self.attrname:
             raise TypeError(
                 "Cannot assign the same cached_property to two different names "
@@ -1129,7 +1149,22 @@ class cached_property:
             return self
         if self.attrname is None:
             raise TypeError(
-                "Cannot use cached_property instance without calling __set_name__ on it.")
+                "Cannot use cached_property instance without an attribute name"
+            )
+        if self.slot:
+            val = getattr(instance, self.attrname, _NOT_FOUND)
+            if val is _NOT_FOUND:
+                val = self.func(instance)
+                try:
+                    setattr(instance, self.attrname, val)
+                except AttributeError as err:
+                    msg = (
+                        f"The class {type(instance).__name__!r} does not "
+                        f"have the requested slot, {self.attrname!r}, "
+                        "in its '__slots__' attribute"
+                    )
+                    raise TypeError(msg) from err
+            return val
         try:
             cache = instance.__dict__
         except AttributeError:  # not all objects have __dict__ (e.g. class defines slots)
