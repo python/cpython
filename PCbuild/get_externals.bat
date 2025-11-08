0ca5966752f465118539e95e66f842c838e59733
Steve Dower <steve.dower@python.org>
2025-11-08 06:28:01 +0000

    gh-140849: Update bundled liblzma to 5.8.1 on Windows (#141022)

--- PCbuild/get_externals.bat
+++ PCbuild/get_externals.bat
@@ -59,7 +59,7 @@ set libraries=%libraries%                                       mpdecimal-4.0.0
 set libraries=%libraries%                                       sqlite-3.50.4.0
 if NOT "%IncludeTkinterSrc%"=="false" set libraries=%libraries% tcl-core-8.6.15.0
 if NOT "%IncludeTkinterSrc%"=="false" set libraries=%libraries% tk-8.6.15.0
-set libraries=%libraries%                                       xz-5.2.5
+set libraries=%libraries%                                       xz-5.8.1.1
 set libraries=%libraries%                                       zlib-ng-2.2.4
 set libraries=%libraries%                                       zstd-1.5.7
 
