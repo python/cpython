ca6272b981c517a59a70a0694fa9eb44b92b0b31
Locked-chess-official <13140752715@163.com>
2025-11-13 21:47:56 +0800

    Cancel change in track.h

--- Include/internal/mimalloc/mimalloc/track.h
+++ Include/internal/mimalloc/mimalloc/track.h
@@ -5,8 +5,8 @@ terms of the MIT license. A copy of the license can be found in the file
 "LICENSE" at the root of this distribution.
 -----------------------------------------------------------------------------*/
 #pragma once
-#ifndef Py_MIMALLOC_TRACK_H
-#define Py_MIMALLOC_TRACK_H
+#ifndef MIMALLOC_TRACK_H
+#define MIMALLOC_TRACK_H
 
 /* ------------------------------------------------------------------------------------------------------
 Track memory ranges with macros for tools like Valgrind address sanitizer, or other memory checkers.
