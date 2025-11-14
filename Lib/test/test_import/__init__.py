8bbd43bf24cc8b576a00b071f479947cc969748c
Peter Bierma <zintensitydev@gmail.com>
2025-11-14 09:22:36 -0500

    gh-131229: Temporarily skip `test_basic_multiple_interpreters_deleted_no_reset` (GH-141552)
    This is a temporary band-aid to unblock other PRs.
    
    Co-authored-by: Kumar Aditya <kumaraditya@python.org>

--- Lib/test/test_import/__init__.py
+++ Lib/test/test_import/__init__.py
@@ -3261,6 +3261,7 @@ class SinglephaseInitTests(unittest.TestCase):
         #  * m_copy was copied from interp2 (was from interp1)
         #  * module's global state was updated, not reset
 
+    @unittest.skip("gh-131229: This is suddenly very flaky")
     @no_rerun(reason="rerun not possible; module state is never cleared (see gh-102251)")
     @requires_subinterpreters
     def test_basic_multiple_interpreters_deleted_no_reset(self):
