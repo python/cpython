869a48a5a0ae8c9e8c5eede137fc9cc72babe0dd
yihong0618 <zouzou0208@gmail.com>
2025-11-16 15:36:10 +0800

    fix: do not print the message twice in sqlite test verbose
    Signed-off-by: yihong0618 <zouzou0208@gmail.com>

--- Lib/test/test_sqlite3/__init__.py
+++ Lib/test/test_sqlite3/__init__.py
@@ -7,8 +7,9 @@ import os
 import sqlite3
 
 # Implement the unittest "load tests" protocol.
-def load_tests(*args):
-    if verbose:
+def load_tests(loader, tests, pattern):
+    # Only print on the top-level call
+    if verbose and pattern is None:
         print(f"test_sqlite3: testing with SQLite version {sqlite3.sqlite_version}")
     pkg_dir = os.path.dirname(__file__)
-    return load_package_tests(pkg_dir, *args)
+    return load_package_tests(pkg_dir, loader, tests, pattern)
