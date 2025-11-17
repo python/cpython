77c88bbb4557e0a56b1229ff9b8d2558fd2b3b0a
Mikhail Efimov <efimov.mikhail@gmail.com>
2025-11-17 18:48:40 +0300

    Improvements in test

--- Lib/test/test_stackrefs.py
+++ Lib/test/test_stackrefs.py
@@ -9,6 +9,8 @@ except ImportError:
 @cpython_only
 class TestDefinition(unittest.TestCase):
 
+    BIG_REFCOUNT = 1000
+
     def test_equivalence(self):
         def run_with_refcount_check(self, func, obj):
             refcount = sys.getrefcount(obj)
@@ -34,23 +36,30 @@ class TestDefinition(unittest.TestCase):
         for obj in immortal_objs:
             results = set()
             for func in funcs_with_incref + funcs_with_borrow:
-                res = run_with_refcount_check(self, func, obj)
-                results.add(res)
+                refcount, flags = run_with_refcount_check(self, func, obj)
+                self.assertGreater(refcount, self.BIG_REFCOUNT)
+                self.assertIn(flags, (1, -1))
+                results.add((refcount, flags))
+
             self.assertEqual(len(results), 1)
 
-        mortal_objs = (5000, 3+2j, range(10), object())
+        mortal_objs = (object(), range(10), iter([1, 2, 3]))
 
         for obj in mortal_objs:
             results = set()
             for func in funcs_with_incref:
-                res = run_with_refcount_check(self, func, obj)
-                results.add(res)
+                refcount, flags = run_with_refcount_check(self, func, obj)
+                self.assertLess(refcount, self.BIG_REFCOUNT)
+                self.assertEqual(flags, 0)
+                results.add((refcount, flags))
             self.assertEqual(len(results), 1)
 
             results = set()
             for func in funcs_with_borrow:
-                res = run_with_refcount_check(self, func, obj)
-                results.add(res)
+                refcount, flags = run_with_refcount_check(self, func, obj)
+                self.assertLess(refcount, self.BIG_REFCOUNT)
+                self.assertEqual(flags, 1)
+                results.add((refcount, flags))
             self.assertEqual(len(results), 1)
 
 
