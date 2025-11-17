0d8d17a6692c6a2ebcade6aed80d72dba3dfcdcf
Krishna Chaitanya <141550576+XChaitanyaX@users.noreply.github.com>
2025-11-17 22:59:06 +0530

    [3.13] gh-141497: Make ipaddress.IP{v4,v6}Network.hosts() always returning an iterator (GH-141547) (cherry picked from commit 6b1bdf6c7a6c87f12a247a125e25f8e721cc731e)
    Co-authored-by: Krishna Chaitanya <141550576+XChaitanyaX@users.noreply.github.com>

--- Lib/ipaddress.py
+++ Lib/ipaddress.py
@@ -1558,8 +1558,8 @@ class IPv4Network(_BaseV4, _BaseNetwork):
 
         if self._prefixlen == (self._max_prefixlen - 1):
             self.hosts = self.__iter__
-        elif self._prefixlen == (self._max_prefixlen):
-            self.hosts = lambda: [IPv4Address(addr)]
+        elif self._prefixlen == (self.max_prefixlen):
+            self.hosts = lambda: iter((IPv4Address(addr),))
 
     @property
     @functools.lru_cache()
@@ -2358,8 +2358,8 @@ class IPv6Network(_BaseV6, _BaseNetwork):
 
         if self._prefixlen == (self._max_prefixlen - 1):
             self.hosts = self.__iter__
-        elif self._prefixlen == self._max_prefixlen:
-            self.hosts = lambda: [IPv6Address(addr)]
+        elif self._prefixlen == self.max_prefixlen:
+            self.hosts = lambda: iter((IPv6Address(addr),))
 
     def hosts(self):
         """Generate Iterator over usable hosts in a network.
