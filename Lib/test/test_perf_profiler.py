759c7cc95617995fffc664d17c8699600965e767
Bénédikt Tran <10796600+picnixz@users.noreply.github.com>
2025-11-21 16:34:55 +0100

    Update Lib/test/test_perf_profiler.py

--- Lib/test/test_perf_profiler.py
+++ Lib/test/test_perf_profiler.py
@@ -160,12 +160,12 @@ class TestPerfTrampoline(unittest.TestCase):
         self.assertIn(f"py::bar_fork:{script}", child_perf_file_contents)
         self.assertIn(f"py::baz_fork:{script}", child_perf_file_contents)
 
-        # The parent's map should not contain the child's symbols
+        # The parent's map should not contain the child's symbols.
         self.assertNotIn(f"py::foo_fork:{script}", perf_file_contents)
         self.assertNotIn(f"py::bar_fork:{script}", perf_file_contents)
         self.assertNotIn(f"py::baz_fork:{script}", perf_file_contents)
 
-        # The child's map should not contain the parent's symbols
+        # The child's map should not contain the parent's symbols.
         self.assertNotIn(f"py::foo:{script}", child_perf_file_contents)
         self.assertNotIn(f"py::bar:{script}", child_perf_file_contents)
         self.assertNotIn(f"py::baz:{script}", child_perf_file_contents)
