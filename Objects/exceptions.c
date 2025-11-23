480139929914e5a810067ecb752999495d46bcbc
dr-carlos <dr_steven_carlos@outlook.com>
2025-11-24 07:27:26 +1030

    Remove unused `name` variable with new `%Y` format

--- Objects/exceptions.c
+++ Objects/exceptions.c
@@ -1080,7 +1080,6 @@ BaseExceptionGroup_repr(PyObject *op)
         excs_orig = Py_NewRef(self->excs);
     }
 
-    const char *name = _PyType_Name(Py_TYPE(self));
     PyObject *repr = PyUnicode_FromFormat(
         "%T(%R, %R)", self, self->msg, excs_orig);
 
