bc231a32b0df1783a8c1e523a7c6ac9140889d35
Paresh Joshi <rahulj9223@gmail.com>
2025-11-24 15:22:53 +0530

    Fix indentation in test_find_function_found_with_encoding_cookie

--- Lib/test/test_pdb.py
+++ Lib/test/test_pdb.py
@@ -3594,9 +3594,9 @@ def quux():
         # Check if the custom prefix appeared in the output
         self.assertIn('CUSTOM_PREFIX> ', stdout.getvalue())
 
-        def test_find_function_found_with_encoding_cookie(self):
-            self._assert_find_function(
-                """\
+    def test_find_function_found_with_encoding_cookie(self):
+        self._assert_find_function(
+            """\
 # coding: iso-8859-15
 def foo():
     pass
