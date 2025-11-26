ea64957ef2af87d67e46148d19b269f6b78db3d9
Frost Ming <me@frostming.com>
2025-11-26 14:49:31 +0800

    fix: use list unpacking to avoid unnecessary list copies
    Signed-off-by: Frost Ming <me@frostming.com>

--- Lib/zoneinfo/_common.py
+++ Lib/zoneinfo/_common.py
@@ -4,9 +4,8 @@ import struct
 def load_tzdata(key):
     from importlib import resources
 
-    components = key.split("/")
-    package_name = ".".join(["tzdata.zoneinfo"] + components[:-1])
-    resource_name = components[-1]
+    *components, resource_name = key.split("/")
+    package_name = ".".join(["tzdata.zoneinfo"] + components)
 
     try:
         path = resources.files(package_name).joinpath(resource_name)
