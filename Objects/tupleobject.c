08134481fb20b94617a82ad9460ff6176a01a044
Maurycy Pawłowski-Wieroński <5383+maurycy@users.noreply.github.com>
2025-11-27 20:38:04 +0100

    Revert "leave only the early out"
    This reverts commit 25d53ebaea394b7e53b4d404f91c16b098a9ed41.

--- Objects/tupleobject.c
+++ Objects/tupleobject.c
@@ -666,6 +666,10 @@ tuple_richcompare(PyObject *v, PyObject *w, int op)
     if (!PyTuple_Check(v) || !PyTuple_Check(w))
         Py_RETURN_NOTIMPLEMENTED;
 
+    if (v == w) {
+        Py_RETURN_RICHCOMPARE(0, 0, op);
+    }
+
     vt = (PyTupleObject *)v;
     wt = (PyTupleObject *)w;
 
