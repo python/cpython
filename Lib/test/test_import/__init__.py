68e2b7eaf24fd71ca23826bb95e2d2b7f7f6776e
dr-carlos <dr_steven_carlos@outlook.com>
2025-11-29 08:49:06 +1030

    Add tests for internal and nonexistent modules passed to `create_builtin()`

--- Lib/test/test_import/__init__.py
+++ Lib/test/test_import/__init__.py
@@ -1253,6 +1253,19 @@ os.does_not_exist
                 origin = "a\x00b"
             _imp.create_dynamic(Spec2())
 
+    def test_create_builtin(self):
+        for internal_mod in (sys, builtins):
+            class Spec:
+                name = internal_mod.__name__
+
+            self.assertIs(_imp.create_builtin(Spec()), internal_mod)
+
+        class Spec:
+            name = "nonexistent_lib"
+
+        # gh-142029
+        self.assertIs(_imp.create_builtin(Spec()), None)
+
     def test_filter_syntax_warnings_by_module(self):
         module_re = r'test\.test_import\.data\.syntax_warnings\z'
         unload('test.test_import.data.syntax_warnings')
