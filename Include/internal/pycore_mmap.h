8ec4d73676dbda3f9e28befc3ae1e1c4990efc44
Donghee Na <donghee.na@python.org>
2025-12-04 03:17:06 +0900

    Add Include/internal/pycore_mmap.h

---
+++ Include/internal/pycore_mmap.h
@@ -0,0 +1,33 @@
+#ifndef Py_INTERNAL_MMAP_H
+#define Py_INTERNAL_MMAP_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifndef Py_BUILD_CORE
+#  error "this header requires Py_BUILD_CORE define"
+#endif
+
+#if defined(Py_DEBUG) && defined(HAVE_PR_SET_VMA_ANON_NAME) && defined(__linux__)
+#  include <linux/prctl.h>
+#  include <sys/prctl.h>
+#endif
+
+static inline int
+_PyAnnotateMemoryMap(void *addr, size_t size, const char *name)
+{
+#if defined(Py_DEBUG) && defined(HAVE_PR_SET_VMA_ANON_NAME) && defined(__linux__)
+    assert(strlen(name) < 80);
+    prctl(PR_SET_VMA, PR_SET_VMA_ANON_NAME, (unsigned long)addr, size, name);
+    // Ignore errno from prctl
+    // See: https://bugzilla.redhat.com/show_bug.cgi?id=2302746
+    errno = 0;
+#endif
+    return 0;
+}
+
+#ifdef __cplusplus
+}
+#endif
+#endif  // !Py_INTERNAL_MMAP_H
