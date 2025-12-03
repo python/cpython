c0e08b66ffe1da9c49e3ca4af5bf27feb6e14176
Donghee Na <donghee.na@python.org>
2025-12-03 18:24:56 +0000

    fix

--- Include/internal/pycore_mmap.h
+++ Include/internal/pycore_mmap.h
@@ -9,7 +9,9 @@ extern "C" {
 #  error "this header requires Py_BUILD_CORE define"
 #endif
 
-#if defined(Py_DEBUG) && defined(HAVE_PR_SET_VMA_ANON_NAME) && defined(__linux__)
+#include "pycore_pystate.h"
+
+#if defined(HAVE_PR_SET_VMA_ANON_NAME) && defined(__linux__)
 #  include <linux/prctl.h>
 #  include <sys/prctl.h>
 #endif
@@ -17,12 +19,14 @@ extern "C" {
 static inline int
 _PyAnnotateMemoryMap(void *addr, size_t size, const char *name)
 {
-#if defined(Py_DEBUG) && defined(HAVE_PR_SET_VMA_ANON_NAME) && defined(__linux__)
-    assert(strlen(name) < 80);
-    prctl(PR_SET_VMA, PR_SET_VMA_ANON_NAME, (unsigned long)addr, size, name);
-    // Ignore errno from prctl
-    // See: https://bugzilla.redhat.com/show_bug.cgi?id=2302746
-    errno = 0;
+#if defined(HAVE_PR_SET_VMA_ANON_NAME) && defined(__linux__)
+    if (_Py_GetConfig()->dev_mode) {
+        assert(strlen(name) < 80);
+        prctl(PR_SET_VMA, PR_SET_VMA_ANON_NAME, (unsigned long)addr, size, name);
+        // Ignore errno from prctl
+        // See: https://bugzilla.redhat.com/show_bug.cgi?id=2302746
+        errno = 0;
+    }
 #endif
     return 0;
 }
