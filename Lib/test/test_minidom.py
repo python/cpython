f118f9f3818aade8eca5a3ae68c57a67ff95f146
Seth Michael Larson <seth@python.org>
2025-12-03 01:16:37 -0600

    gh-142145: Remove quadratic behavior in node ID cache clearing (GH-142146)
    * Remove quadratic behavior in node ID cache clearing
    
    Co-authored-by: Jacob Walls <38668450+jacobtylerwalls@users.noreply.github.com>
    
    * Add news fragment
    
    ---------
    
    Co-authored-by: Jacob Walls <38668450+jacobtylerwalls@users.noreply.github.com>

--- Lib/test/test_minidom.py
+++ Lib/test/test_minidom.py
@@ -2,6 +2,7 @@
 
 import copy
 import pickle
+import time
 import io
 from test import support
 import unittest
@@ -172,6 +173,23 @@ class MinidomTest(unittest.TestCase):
         self.assertEqual(dom.documentElement.childNodes[-1].nodeName, "#comment")
         self.assertEqual(dom.documentElement.childNodes[-1].data, "Hello")
         dom.unlink()
+
+    def testAppendChildNoQuadraticComplexity(self):
+        impl = getDOMImplementation()
+
+        newdoc = impl.createDocument(None, "some_tag", None)
+        top_element = newdoc.documentElement
+        children = [newdoc.createElement(f"child-{i}") for i in range(1, 2 ** 15 + 1)]
+        element = top_element
+
+        start = time.time()
+        for child in children:
+            element.appendChild(child)
+            element = child
+        end = time.time()
+
+        # This example used to take at least 30 seconds.
+        self.assertLess(end - start, 1)
 
     def testAppendChildFragment(self):
         dom, orig, c1, c2, c3, frag = self._create_fragment_test_nodes()
