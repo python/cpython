3567e94d79802ff3d59cba1bf0777e6227528eff
Guido van Rossum <gvanrossum@gmail.com>
2025-12-05 06:34:45 -0800

    Too many blank lines (my fault)

--- Lib/test/test_asyncio/test_tasks.py
+++ Lib/test/test_asyncio/test_tasks.py
@@ -3688,7 +3688,6 @@ class RunCoroutineThreadsafeTests(test_utils.TestCase):
 
             thread_future.cancel()
 
-
             await asyncio.sleep(0)
 
         self.loop.run_until_complete(target())
