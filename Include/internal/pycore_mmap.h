805c539e40776fac45c7b4d4075113aeaaac1b22
Donghee Na <donghee.na@python.org>
2025-12-05 23:08:09 +0900

    Address code review

--- Include/internal/pycore_mmap.h
+++ Include/internal/pycore_mmap.h
@@ -16,28 +16,29 @@ extern "C" {
 #  include <sys/prctl.h>
 #endif
 
-#if defined(HAVE_PR_SET_VMA_ANON_NAME) && defined(__linux__) && defined(Py_DEBUG)
-#  define _PyAnnotateMemoryMap(addr, size, name) \
-    do { \
-        assert(strlen(name) < 80); \
-        prctl(PR_SET_VMA, PR_SET_VMA_ANON_NAME, (unsigned long)(addr), (size), (name)); \
-        /* Ignore errno from prctl */ \
-        /* See: https://bugzilla.redhat.com/show_bug.cgi?id=2302746 */ \
-        errno = 0; \
-    } while (0)
-#elif defined(HAVE_PR_SET_VMA_ANON_NAME) && defined(__linux__)
-#  define _PyAnnotateMemoryMap(addr, size, name) \
-    do { \
-        if (_Py_GetConfig()->dev_mode) { \
-            assert(strlen(name) < 80); \
-            prctl(PR_SET_VMA, PR_SET_VMA_ANON_NAME, (unsigned long)(addr), (size), (name)); \
-            /* Ignore errno from prctl */ \
-            /* See: https://bugzilla.redhat.com/show_bug.cgi?id=2302746 */ \
-            errno = 0; \
-        } \
-    } while (0)
+#if defined(HAVE_PR_SET_VMA_ANON_NAME) && defined(__linux__)
+static inline void
+_PyAnnotateMemoryMap(void *addr, size_t size, const char *name)
+{
+#ifndef Py_DEBUG
+    if (!_Py_GetConfig()->dev_mode) {
+        return;
+    }
+#endif
+    assert(strlen(name) < 80);
+    prctl(PR_SET_VMA, PR_SET_VMA_ANON_NAME, (unsigned long)(addr), size, name);
+    /* Ignore errno from prctl */
+    /* See: https://bugzilla.redhat.com/show_bug.cgi?id=2302746 */
+    errno = 0;
+}
 #else
-#  define _PyAnnotateMemoryMap(addr, size, name) ((void)0)
+static inline void
+_PyAnnotateMemoryMap(void *addr, size_t size, const char *name)
+{
+    (void)addr;
+    (void)size;
+    (void)name;
+}
 #endif
 
 #ifdef __cplusplus
