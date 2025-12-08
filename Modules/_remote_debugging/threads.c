661329478d5835e52cf0c92b783dea276c6b9ac5
AZero13 <gfunni234@gmail.com>
2025-12-08 17:52:27 -0500

    Silence warning on conversion from uint64_t to uintptr_t

--- Modules/_remote_debugging/threads.c
+++ Modules/_remote_debugging/threads.c
@@ -405,7 +405,8 @@ unwind_stack_for_thread(
             goto error;
         }
         // Update last_profiled_frame for next sample
-        uintptr_t lpf_addr = *current_tstate + unwinder->debug_offsets.thread_state.last_profiled_frame;
+        uintptr_t lpf_addr =
+            *current_tstate + (uintptr_t)unwinder->debug_offsets.thread_state.last_profiled_frame;
         if (_Py_RemoteDebug_WriteRemoteMemory(&unwinder->handle, lpf_addr,
                                               sizeof(uintptr_t), &frame_addr) < 0) {
             PyErr_Clear();  // Non-fatal
