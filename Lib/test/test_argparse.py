2c5f4931c0cdb04d98f5e6dbd063c510b87e3572
Fabian Henze <32638720+henzef@users.noreply.github.com>
2025-12-09 17:48:35 +0100

    [3.14] gh-112527: Fix help text for required options in argparse (GH-112528)
    For optional arguments with required=True, the ArgumentDefaultsHelpFormatter
    would always add a " (default: None)" to the end of the help text.
    Since that's a bit misleading, it is removed with this commit.
    (cherry picked from commit 1adb17b1a26e1547d14ca15f915e605cfdda3edd)
    
    Co-authored-by: Fabian Henze <32638720+henzef@users.noreply.github.com>

--- Lib/test/test_argparse.py
+++ Lib/test/test_argparse.py
@@ -5328,6 +5328,7 @@ class TestHelpArgumentDefaults(HelpTestCase):
     argument_signatures = [
         Sig('--foo', help='foo help - oh and by the way, %(default)s'),
         Sig('--bar', action='store_true', help='bar help'),
+        Sig('--required', required=True, help='some help'),
         Sig('--taz', action=argparse.BooleanOptionalAction,
             help='Whether to taz it', default=True),
         Sig('--corge', action=argparse.BooleanOptionalAction,
@@ -5341,8 +5342,8 @@ class TestHelpArgumentDefaults(HelpTestCase):
          [Sig('--baz', type=int, default=42, help='baz help')]),
     ]
     usage = '''\
-        usage: PROG [-h] [--foo FOO] [--bar] [--taz | --no-taz] [--corge | --no-corge]
-                    [--quux QUUX] [--baz BAZ]
+        usage: PROG [-h] [--foo FOO] [--bar] --required REQUIRED [--taz | --no-taz]
+                    [--corge | --no-corge] [--quux QUUX] [--baz BAZ]
                     spam [badger]
         '''
     help = usage + '''\
@@ -5357,6 +5358,7 @@ class TestHelpArgumentDefaults(HelpTestCase):
           -h, --help           show this help message and exit
           --foo FOO            foo help - oh and by the way, None
           --bar                bar help (default: False)
+          --required REQUIRED  some help
           --taz, --no-taz      Whether to taz it (default: True)
           --corge, --no-corge  Whether to corge it
           --quux QUUX          Set the quux (default: 42)
