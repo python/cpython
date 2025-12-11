d260cf93a99128b722442be914299bcae4e698b4
Dino Viehland <dinoviehland@meta.com>
2025-12-11 00:54:29 -0800

    gh-123241: Don't modify ref count during visitation (GH-142232)

--- Include/internal/pycore_typeobject.h
+++ Include/internal/pycore_typeobject.h
@@ -155,6 +155,11 @@ extern int _PyType_CacheGetItemForSpecialization(PyHeapTypeObject *ht, PyObject 
 // Precalculates count of non-unique slots and fills wrapperbase.name_count.
 extern int _PyType_InitSlotDefs(PyInterpreterState *interp);
 
+// Like PyType_GetBaseByToken, but does not modify refcounts.
+// Cannot fail; arguments must be valid.
+PyAPI_FUNC(int)
+_PyType_GetBaseByToken_Borrow(PyTypeObject *type, void *token, PyTypeObject **result);
+
 #ifdef __cplusplus
 }
 #endif
