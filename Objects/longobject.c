b46a25e6be587540ff1ef812c1b018a633f42317
Bénédikt Tran <10796600+picnixz@users.noreply.github.com>
2025-12-14 09:38:23 +0100

    gh-142554: avoid `divmod` crashes due to bad `_pylong.int_divmod` (#142673)

--- Objects/longobject.c
+++ Objects/longobject.c
@@ -4434,10 +4434,10 @@ pylong_int_divmod(PyLongObject *v, PyLongObject *w,
     if (result == NULL) {
         return -1;
     }
-    if (!PyTuple_Check(result)) {
+    if (!PyTuple_Check(result) || PyTuple_GET_SIZE(result) != 2) {
         Py_DECREF(result);
         PyErr_SetString(PyExc_ValueError,
-                        "tuple is required from int_divmod()");
+                        "tuple of length 2 is required from int_divmod()");
         return -1;
     }
     PyObject *q = PyTuple_GET_ITEM(result, 0);
