92f629bded7155c96f98ba585aaa607c52f7affc
johnslavik <bartosz@ilikepython.com>
2025-12-16 04:36:36 +0100

    Employ `asyncio.Runner` in the asyncio REPL

--- Lib/asyncio/__main__.py
+++ Lib/asyncio/__main__.py
@@ -187,8 +187,8 @@ if __name__ == '__main__':
         from _pyrepl.main import CAN_USE_PYREPL
 
     return_code = 0
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
+    runner = asyncio.Runner()
+    loop = runner.get_loop()
 
     repl_locals = {'asyncio': asyncio}
     for key in {'__name__', '__package__',
@@ -240,4 +240,5 @@ if __name__ == '__main__':
             break
 
     console.write('exiting asyncio REPL...\n')
+    runner.close()
     sys.exit(return_code)
