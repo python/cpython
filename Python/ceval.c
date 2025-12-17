832e413a9a92a4f3d8abb06ef2ce4326a1aa88c6
Sam Gross <colesbury@gmail.com>
2025-12-17 14:24:05 -0500

    Set generator_return_kind in clear_gen_frame
    Generator calls may be nested so it's not sufficient to set the field
    before the call to PyEval_Frame.

--- Python/ceval.c
+++ Python/ceval.c
@@ -2305,6 +2305,7 @@ clear_gen_frame(PyThreadState *tstate, _PyInterpreterFrame * frame)
     assert(frame->owner == FRAME_OWNED_BY_GENERATOR);
     PyGenObject *gen = _PyGen_GetGeneratorFromFrame(frame);
     FT_ATOMIC_STORE_INT8_RELEASE(gen->gi_frame_state, FRAME_CLEARED);
+    ((_PyThreadStateImpl *)tstate)->generator_return_kind = GENERATOR_RETURN;
     assert(tstate->exc_info == &gen->gi_exc_state);
     tstate->exc_info = gen->gi_exc_state.previous_item;
     gen->gi_exc_state.previous_item = NULL;
