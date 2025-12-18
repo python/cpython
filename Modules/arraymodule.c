dc4af2aed26407fdd70584f005f012825c87b1db
fatelei <fatelei@gmail.com>
2025-12-18 11:43:38 +0800

    gh-142884: Use-After-Free Vulnerability Fixed in CPython array.array.tofile()

--- Modules/arraymodule.c
+++ Modules/arraymodule.c
@@ -1587,35 +1587,47 @@ static PyObject *
 array_array_tofile_impl(arrayobject *self, PyTypeObject *cls, PyObject *f)
 /*[clinic end generated code: output=4560c628d9c18bc2 input=5a24da7a7b407b52]*/
 {
-    Py_ssize_t nbytes = Py_SIZE(self) * self->ob_descr->itemsize;
     /* Write 64K blocks at a time */
     /* XXX Make the block size settable */
     int BLOCKSIZE = 64*1024;
-    Py_ssize_t nblocks = (nbytes + BLOCKSIZE - 1) / BLOCKSIZE;
-    Py_ssize_t i;
+    Py_ssize_t offset = 0;
 
     if (Py_SIZE(self) == 0)
         goto done;
 
-
     array_state *state = get_array_state_by_class(cls);
     assert(state != NULL);
 
-    for (i = 0; i < nblocks; i++) {
-        char* ptr = self->ob_item + i*BLOCKSIZE;
+    while (1) {
+        if (self->ob_item == NULL || Py_SIZE(self) == 0) {
+            break;
+        }
+
+        Py_ssize_t current_nbytes = Py_SIZE(self) * self->ob_descr->itemsize;
+
+        if (offset >= current_nbytes) {
+            break;
+        }
+
         Py_ssize_t size = BLOCKSIZE;
+        if (offset + size > current_nbytes) {
+            size = current_nbytes - offset;
+        }
+
+        char* ptr = self->ob_item + offset;
         PyObject *bytes, *res;
 
-        if (i*BLOCKSIZE + size > nbytes)
-            size = nbytes - i*BLOCKSIZE;
         bytes = PyBytes_FromStringAndSize(ptr, size);
         if (bytes == NULL)
             return NULL;
+
         res = PyObject_CallMethodOneArg(f, state->str_write, bytes);
         Py_DECREF(bytes);
         if (res == NULL)
             return NULL;
-        Py_DECREF(res); /* drop write result */
+        Py_DECREF(res);
+
+        offset += size;
     }
 
   done:
