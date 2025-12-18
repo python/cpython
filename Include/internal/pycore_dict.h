4d1129bd0297369ecbbcb12175839be49f76ce09
Thomas Wouters <thomas@python.org>
2025-12-18 16:12:04 +0100

    Get rid of a merge snafu.

--- Include/internal/pycore_dict.h
+++ Include/internal/pycore_dict.h
@@ -36,15 +36,7 @@ extern int _PyDict_DelItem_KnownHash_LockHeld(PyObject *mp, PyObject *key,
 
 extern int _PyDict_Contains_KnownHash(PyObject *, PyObject *, Py_hash_t);
 
-// "Id" variants
-extern PyObject* _PyDict_GetItemIdWithError(PyObject *dp,
-                                            _Py_Identifier *key);
-extern int _PyDict_ContainsId(PyObject *, _Py_Identifier *);
-extern int _PyDict_SetItemId(PyObject *dp, _Py_Identifier *key,
-                             PyObject *item);
-extern int _PyDict_DelItemId(PyObject *mp, _Py_Identifier *key);
 extern void _PyDict_ClearKeysVersion(PyObject *mp);
-
 
 extern int _PyDict_Next(
     PyObject *mp, Py_ssize_t *pos, PyObject **key, PyObject **value, Py_hash_t *hash);
