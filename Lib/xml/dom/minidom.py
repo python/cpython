9c9dda6625a2a90d2a06c657eee021d6be19842d
Miss Islington (bot) <31488909+miss-islington@users.noreply.github.com>
2025-12-22 14:48:49 +0100

    [3.12] gh-142145: Remove quadratic behavior in node ID cache clearing (GH-142146) (#142211)
    * gh-142145: Remove quadratic behavior in node ID cache clearing (GH-142146)
    * gh-142754: Ensure that Element & Attr instances have the ownerDocument attribute (GH-142794)
    (cherry picked from commit 1cc7551b3f9f71efbc88d96dce90f82de98b2454)
    (cherry picked from commit 08d8e18ad81cd45bc4a27d6da478b51ea49486e4)
    (cherry picked from commit 8d2d7bb2e754f8649a68ce4116271a4932f76907)
    
    Co-authored-by: Jacob Walls <38668450+jacobtylerwalls@users.noreply.github.com>
    Co-authored-by: Seth Michael Larson <seth@python.org>
    Co-authored-by: Petr Viktorin <encukou@gmail.com>
    Co-authored-by: Hugo van Kemenade <1324225+hugovk@users.noreply.github.com>
    Co-authored-by: Gregory P. Smith <68491+gpshead@users.noreply.github.com>
    Co-authored-by: Hugo van Kemenade <1324225+hugovk@users.noreply.github.com>
    Co-authored-by: Gregory P. Smith <68491+gpshead@users.noreply.github.com>
    Co-authored-by: Gregory P. Smith <greg@krypto.org>

--- Lib/xml/dom/minidom.py
+++ Lib/xml/dom/minidom.py
@@ -292,13 +292,6 @@ def _append_child(self, node):
     childNodes.append(node)
     node.parentNode = self
 
-def _in_document(node):
-    # return True iff node is part of a document tree
-    while node is not None:
-        if node.nodeType == Node.DOCUMENT_NODE:
-            return True
-        node = node.parentNode
-    return False
 
 def _write_data(writer, data):
     "Writes datachars to writer."
@@ -355,6 +348,7 @@ class Attr(Node):
     def __init__(self, qName, namespaceURI=EMPTY_NAMESPACE, localName=None,
                  prefix=None):
         self.ownerElement = None
+        self.ownerDocument = None
         self._name = qName
         self.namespaceURI = namespaceURI
         self._prefix = prefix
@@ -680,6 +674,7 @@ class Element(Node):
 
     def __init__(self, tagName, namespaceURI=EMPTY_NAMESPACE, prefix=None,
                  localName=None):
+        self.ownerDocument = None
         self.parentNode = None
         self.tagName = self.nodeName = tagName
         self.prefix = prefix
@@ -1539,7 +1534,7 @@ def _clear_id_cache(node):
     if node.nodeType == Node.DOCUMENT_NODE:
         node._id_cache.clear()
         node._id_search_stack = None
-    elif _in_document(node):
+    elif node.ownerDocument:
         node.ownerDocument._id_cache.clear()
         node.ownerDocument._id_search_stack= None
 
