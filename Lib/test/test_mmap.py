ba674d53ceca4388cd50ef25a8a9bd9a5044237b
AN Long <aisk1988@gmail.com>
2025-12-28 02:11:57 +0800

    Check the error on FreeBSD

--- Lib/test/test_mmap.py
+++ Lib/test/test_mmap.py
@@ -1173,8 +1173,12 @@ class MmapTests(unittest.TestCase):
             if hasattr(mmap, 'MS_INVALIDATE'):
                 m.flush(PAGESIZE * 2, flags=mmap.MS_INVALIDATE)
             if hasattr(mmap, 'MS_ASYNC') and hasattr(mmap, 'MS_INVALIDATE'):
-                if sys.platform != 'freebsd':
+                if sys.platform == 'freebsd':
                     # FreeBSD doesn't support this combination
+                    with self.assertRaises(OSError) as cm:
+                        m.flush(0, PAGESIZE, flags=mmap.MS_ASYNC | mmap.MS_INVALIDATE)
+                    self.assertEqual(cm.exception.errno, errno.EINVAL)
+                else:
                     m.flush(0, PAGESIZE, flags=mmap.MS_ASYNC | mmap.MS_INVALIDATE)
 
     @unittest.skipUnless(sys.platform == 'linux', 'Linux only')
