9f9c4a429a99ecac5fe617ed7902b12a49e26878
Bénédikt Tran <10796600+picnixz@users.noreply.github.com>
2026-01-03 23:46:45 +0100

    Update Lib/test/test_os.py

--- Lib/test/test_os.py
+++ Lib/test/test_os.py
@@ -2263,7 +2263,8 @@ class ExecTests(unittest.TestCase):
         # Regression test for https://github.com/python/cpython/issues/143309.
 
         message = "hello from execve"
-        code = """import os, sys
+        code = """\
+        import os, sys
 
         class MyPath:
             def __fspath__(self):
