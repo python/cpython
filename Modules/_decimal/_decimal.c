81304ef2f25fa3cb2e27df7877a208691d4e601a
donbarbos <donbarbos@proton.me>
2026-01-05 21:21:39 +0400

    Update deprecation message

--- Modules/_decimal/_decimal.c
+++ Modules/_decimal/_decimal.c
@@ -3764,7 +3764,8 @@ _decimal_Decimal___format___impl(PyObject *dec, PyTypeObject *cls,
 
     if (size > 0 && fmt[size-1] == 'N') {
         if (PyErr_WarnEx(PyExc_DeprecationWarning,
-                         "Format specifier 'N' is deprecated", 1) < 0) {
+                         "Format specifier 'N' is deprecated and "
+                         "slated for removal in Python 3.18", 1) < 0) {
             return NULL;
         }
     }
