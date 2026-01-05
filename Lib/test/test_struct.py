f5bddb56c746b59d488418dcb3b020b5c96114ff
Henry Chen <henry.chen@oss.cipunited.com>
2026-01-05 18:15:06 +0800

    Use the unversioned gcc doc link

--- Lib/test/test_struct.py
+++ Lib/test/test_struct.py
@@ -944,7 +944,7 @@ class UnpackIteratorTest(unittest.TestCase):
 
         # Skip NaN encoding checks for MIPS because `math.nan` changes its value
         # depending on toolchain settings. See:
-        # https://gcc.gnu.org/onlinedocs/gcc-15.2.0/gcc/MIPS-Options.html#index-mnan_003d2008
+        # https://gcc.gnu.org/onlinedocs/gcc/MIPS-Options.html#index-mnan_003d2008
         if not platform.machine().startswith('mips'):
             packed = struct.pack('<e', math.nan)
             self.assertEqual(packed[1] & 0x7e, expected)
