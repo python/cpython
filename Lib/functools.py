4ef7c7cc980cb0b52fa36235dd1942f8915f22a4
johnslavik <bartosz@ilikepython.com>
2026-01-06 08:50:03 +0100

    Rename `ref_or_typeform` to `fwdref_or_typeform`

--- Lib/functools.py
+++ Lib/functools.py
@@ -901,26 +901,26 @@ def _get_dispatch_annotation(func, param):
     import annotationlib
     annotations = annotationlib.get_annotations(func, format=annotationlib.Format.FORWARDREF)
     try:
-        ref_or_typeform = annotations[param]
+        fwdref_or_typeform = annotations[param]
     except KeyError:
         raise TypeError(
             f"Invalid first argument to `register()`: {param!r}. "
             f"Add missing annotation to parameter {param!r} of {func.__qualname__!r} "
             f"or use `@register(some_class)`."
         ) from None
-    if isinstance(ref_or_typeform, str):
-        ref_or_typeform = annotationlib.ForwardRef(ref_or_typeform, owner=func)
-    if isinstance(ref_or_typeform, annotationlib.ForwardRef):
+    if isinstance(fwdref_or_typeform, str):
+        fwdref_or_typeform = annotationlib.ForwardRef(fwdref_or_typeform, owner=func)
+    if isinstance(fwdref_or_typeform, annotationlib.ForwardRef):
         try:
-            ref_or_typeform = ref_or_typeform.evaluate(owner=func)
+            fwdref_or_typeform = fwdref_or_typeform.evaluate(owner=func)
         except Exception:
             pass  # Forward reference is unresolved.
-    if ref_or_typeform is None:
-        ref_or_typeform = type(None)
-    if not isinstance(ref_or_typeform, annotationlib.ForwardRef):
+    if fwdref_or_typeform is None:
+        fwdref_or_typeform = type(None)
+    if not isinstance(fwdref_or_typeform, annotationlib.ForwardRef):
         import typing
-        ref_or_typeform = typing._strip_annotations(ref_or_typeform)
-    return ref_or_typeform
+        fwdref_or_typeform = typing._strip_annotations(fwdref_or_typeform)
+    return fwdref_or_typeform
 
 def _get_dispatch_param_and_annotation(func, *, skip_first=False):
     param = _get_dispatch_param_name(func, skip_first=skip_first)
