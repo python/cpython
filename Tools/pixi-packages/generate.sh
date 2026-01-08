456503b2531f7ca292b5d743a2f485ef6c3a2c8e
Isuru Fernando <isuruf@gmail.com>
2026-01-08 14:27:32 -0800

    fix typo

--- Tools/pixi-packages/generate.sh
+++ Tools/pixi-packages/generate.sh
@@ -4,9 +4,9 @@
 
 for name in default asan free-threading tsan-free-threading; do
   if [[ "${name}" == *free-threading* ]]; then
-    tag="cp35t"
+    tag="cp315t"
   else
-    tag="cp35"
+    tag="cp315"
   fi
   sed -e 's/variant: "default"/variant: "'${name}'"/g' -e 's/${{ abi_tag }}/'${tag}'/g' Tools/pixi-packages/recipe.yaml > Tools/pixi-packages/${name}/recipe.yaml
 done
