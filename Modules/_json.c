b2383451c5e3200cc293f1eaa9a1b4d70121ea1d
VanshAgarwal24036 <vanshagbly6@gmail.com>
2026-01-09 17:30:27 +0530

    gh-143544: Fix use-after-free in _json.raise_errmsg

--- Modules/_json.c
+++ Modules/_json.c
@@ -417,19 +417,22 @@ raise_errmsg(const char *msg, PyObject *s, Py_ssize_t end)
     _Py_DECLARE_STR(json_decoder, "json.decoder");
 
     PyObject *JSONDecodeError =
-        PyImport_ImportModuleAttr(&_Py_STR(json_decoder), &_Py_ID(JSONDecodeError));
+        PyImport_ImportModuleAttr(&_Py_STR(json_decoder),
+                                  &_Py_ID(JSONDecodeError));
     if (JSONDecodeError == NULL) {
         return;
     }
 
     PyObject *exc = PyObject_CallFunction(JSONDecodeError, "zOn", msg, s, end);
-    if (exc != NULL) {
+    if (exc) {
         PyErr_SetObject(JSONDecodeError, exc);
         Py_DECREF(exc);
     }
 
+    /* Move DECREF after PyErr_SetObject */
     Py_DECREF(JSONDecodeError);
 }
+
 
 static void
 raise_stop_iteration(Py_ssize_t idx)
