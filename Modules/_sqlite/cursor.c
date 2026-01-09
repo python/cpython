fe5b799d76812cef70039a27c01e2901a8ea33a1
Bénédikt Tran <10796600+picnixz@users.noreply.github.com>
2026-01-09 14:19:07 +0100

    Reduce the diff again

--- Modules/_sqlite/cursor.c
+++ Modules/_sqlite/cursor.c
@@ -1020,7 +1020,6 @@ _pysqlite_query_execute(pysqlite_Cursor *self, int multiple, PyObject *operation
         Py_BEGIN_ALLOW_THREADS
         numcols = sqlite3_column_count(self->statement->st);
         Py_END_ALLOW_THREADS
-
         if (self->description == Py_None && numcols > 0) {
             Py_SETREF(self->description, PyTuple_New(numcols));
             if (!self->description) {
