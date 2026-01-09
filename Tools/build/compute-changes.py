ba9156f350a9d188257b9d7e758efcb9964e1ab4
Stan Ulbrych <stan@ulbrych.org>
2026-01-09 16:11:32 +0000

    Merge sets as req by Peter

--- Tools/build/compute-changes.py
+++ Tools/build/compute-changes.py
@@ -19,18 +19,16 @@ if TYPE_CHECKING:
     from collections.abc import Set
 
 GITHUB_DEFAULT_BRANCH = os.environ["GITHUB_DEFAULT_BRANCH"]
-GITHUB_CODEOWNERS_PATH = Path(".github/CODEOWNERS")
 GITHUB_WORKFLOWS_PATH = Path(".github/workflows")
 
 RUN_TESTS_IGNORE = frozenset({
     Path("Tools/check-c-api-docs/ignored_c_api.txt"),
+    Path(".github/CODEOWNERS"),
+    Path(".pre-commit-config.yaml"),
+    Path(".ruff.toml"),
+    Path("mypy.ini"),
 })
 
-CONFIGURATION_FILE_NAMES = frozenset({
-    ".pre-commit-config.yaml",
-    ".ruff.toml",
-    "mypy.ini",
-})
 UNIX_BUILD_SYSTEM_FILE_NAMES = frozenset({
     Path("aclocal.m4"),
     Path("config.guess"),
@@ -178,8 +176,6 @@ def process_changed_files(changed_files: Set[Path]) -> Outputs:
 
         if not (
             doc_file
-            or file == GITHUB_CODEOWNERS_PATH
-            or file.name in CONFIGURATION_FILE_NAMES
             or file in RUN_TESTS_IGNORE
         ):
             run_tests = True
