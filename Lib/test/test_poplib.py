22fa4adc99806daaf68d270cf21beb5f6b9b421e
Seth Michael Larson <seth@python.org>
2026-01-16 10:54:09 -0600

    [CVE-2025-15367] Add 'test.support' fixture for C0 control characters
    This is a security fix to `poplib` that makes the client raise an error
    when POP3 commands (such as those built from untrusted header or data
    values) contain ASCII control characters, preventing injection of stray
    control sequences/commands into the POP3 protocol stream.
    
    Fixes: bsc#1257041 (CVE-2025-15367)
    Fixes: gh#python/cpython#143923
    From-PR: gh#python/cpython!143924
    Patch: CVE-2025-15367-poplib-ctrl-chars.patch

--- Lib/test/test_poplib.py
+++ Lib/test/test_poplib.py
@@ -12,6 +12,7 @@ import errno
 
 from unittest import TestCase, skipUnless
 from test import support as test_support
+from test.support import control_characters_c0
 threading = test_support.import_module('threading')
 
 HOST = test_support.HOST
@@ -348,6 +349,13 @@ class TestPOP3Class(TestCase):
         self.assertTrue(resp)
         self.assertIsNone(self.client.sock)
         self.assertIsNone(self.client.file)
+
+    def test_control_characters(self):
+        for c0 in control_characters_c0():
+            with self.assertRaises(ValueError):
+                self.client.user(f'user{c0}')
+            with self.assertRaises(ValueError):
+                self.client.pass_(f'{c0}pass')
 
     @requires_ssl
     def test_stls_capa(self):
