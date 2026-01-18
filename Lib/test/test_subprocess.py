612597694f46b4ecdab67f352dd8a8d908c8ec04
Giampaolo Rodola <g.rodola@gmail.com>
2026-01-18 23:49:06 +0100

    Write test for pidfd_open() failing

--- Lib/test/test_subprocess.py
+++ Lib/test/test_subprocess.py
@@ -1428,6 +1428,18 @@ class ProcessTestCase(BaseTestCase):
         self.assertIn("0.0001", str(c.exception))  # For coverage of __str__.
         self.assertEqual(p.wait(timeout=support.SHORT_TIMEOUT), 0)
 
+
+    @unittest.skipIf(not hasattr(os, "pidfd_open"), reason="LINUX only")
+    def test_wait_pidfd_open_error(self):
+        exc = OSError(errno.EMFILE, os.strerror(errno.EMFILE))
+        with mock.patch("os.pidfd_open", side_effect=exc) as m:
+            p = subprocess.Popen([sys.executable,
+                                  "-c", "import time; time.sleep(0.3)"])
+            with self.assertRaises(subprocess.TimeoutExpired) as c:
+                p.wait(timeout=0.0001)
+            self.assertEqual(p.wait(timeout=support.SHORT_TIMEOUT), 0)
+        assert m.called
+
     def test_invalid_bufsize(self):
         # an invalid type of the bufsize argument should raise
         # TypeError.
