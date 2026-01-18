5fee79161a9c1603d1c42d21bada1ceb66927ee6
tonghuaroot (童话) <tonghuaroot@gmail.com>
2026-01-18 13:07:36 +0800

    gh-143984: Replace optparse with argparse in Lib/test/test_decimal.py
    The optparse module is deprecated since Python 3.2. This change migrates
    Lib/test/test_decimal.py to use the argparse module for command line
    argument parsing.
    
    This involves:
    - Importing argparse instead of optparse.
    - Using ArgumentParser instead of OptionParser.
    - Updating argument definitions (add_argument).
    - Escaping '%' characters in help strings.
    - Updating execution logic to use the argparse Namespace object.

--- Lib/test/test_decimal.py
+++ Lib/test/test_decimal.py
@@ -6028,15 +6028,16 @@ def test(arith=None, verbose=None, todo_tests=None, debug=None):
 
 
 if __name__ == '__main__':
-    import optparse
-    p = optparse.OptionParser("test_decimal.py [--debug] [{--skip | test1 [test2 [...]]}]")
-    p.add_option('--debug', '-d', action='store_true', help='shows the test number and context before each test')
-    p.add_option('--skip',  '-s', action='store_true', help='skip over 90% of the arithmetic tests')
-    (opt, args) = p.parse_args()
+    import argparse
+    parser = argparse.ArgumentParser(usage="test_decimal.py [--debug] [{--skip | test1 [test2 [...]]}]")
+    parser.add_argument('--debug', '-d', action='store_true', help='shows the test number and context before each test')
+    parser.add_argument('--skip',  '-s', action='store_true', help='skip over 90%% of the arithmetic tests')
+    parser.add_argument('tests', nargs='*', help='specific tests to run')
+    args = parser.parse_args()
 
-    if opt.skip:
+    if args.skip:
         test(arith=False, verbose=True)
-    elif args:
-        test(arith=True, verbose=True, todo_tests=args, debug=opt.debug)
+    elif args.tests:
+        test(arith=True, verbose=True, todo_tests=args.tests, debug=args.debug)
     else:
         test(arith=True, verbose=True)
