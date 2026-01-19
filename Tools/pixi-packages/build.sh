ad6c74054b3d5bef02bd262ce1b9648e0aad3b09
Isuru Fernando <isuruf@gmail.com>
2026-01-19 08:58:31 -0800

    Use libcompiler-rt and remove hack

--- Tools/pixi-packages/build.sh
+++ Tools/pixi-packages/build.sh
@@ -37,9 +37,3 @@ touch configure-done
 
 make -j"${CPU_COUNT}" install
 ln -sf "${PREFIX}/bin/python3" "${PREFIX}/bin/python"
-
-# https://github.com/prefix-dev/rattler-build/issues/2012
-if [[ ${OSTYPE} == "darwin"* ]]; then
-    cp "${BUILD_PREFIX}/lib/clang/21/lib/darwin/libclang_rt.asan_osx_dynamic.dylib" "${PREFIX}/lib/"
-    cp "${BUILD_PREFIX}/lib/clang/21/lib/darwin/libclang_rt.tsan_osx_dynamic.dylib" "${PREFIX}/lib/"
-fi
