282bef7f8e95d0f4d0d02793c504d4b1758fb709
Filipe Laíns <lains@riseup.net>
2026-01-20 01:28:15 +0000

    Use context manager and add error handling to os.scandir
    Signed-off-by: Filipe Laíns <lains@riseup.net>

--- Lib/importlib/_bootstrap_external.py
+++ Lib/importlib/_bootstrap_external.py
@@ -1490,19 +1490,26 @@ class FileFinder:
         return path_hook_for_FileFinder
 
     def _find_children(self):
-        for entry in _os.scandir(self.path):
-            if entry.name == _PYCACHE:
-                continue
-            # packages
-            if entry.is_dir() and '.' not in entry.name:
-                yield entry.name
-            # files
-            if entry.is_file():
-                yield from [
-                    entry.name.removesuffix(suffix)
-                    for suffix, _ in self._loaders
-                    if entry.name.endswith(suffix)
-                ]
+        with _os.scandir(self.path) as scan_iterator:
+            while True:
+                try:
+                    entry = next(scan_iterator)
+                    if entry.name == _PYCACHE:
+                        continue
+                    # packages
+                    if entry.is_dir() and '.' not in entry.name:
+                        yield entry.name
+                    # files
+                    if entry.is_file():
+                        yield from [
+                            entry.name.removesuffix(suffix)
+                            for suffix, _ in self._loaders
+                            if entry.name.endswith(suffix)
+                        ]
+                except OSError:
+                    pass  # ignore exceptions from next(scan_iterator) and os.DirEntry
+                except StopIteration:
+                    break
 
     def discover(self, parent=None):
         module_prefix = f'{parent.name}.' if parent else ''
