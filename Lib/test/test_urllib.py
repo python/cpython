2c9c746077d8119b5bcf5142316992e464594946
Seth Michael Larson <seth@python.org>
2026-01-20 14:45:58 -0600

    [3.13] gh-143925: Reject control characters in data: URL mediatypes (cherry picked from commit f25509e78e8be6ea73c811ac2b8c928c28841b9f)
    Co-authored-by: Seth Michael Larson <seth@python.org>

--- Lib/test/test_urllib.py
+++ Lib/test/test_urllib.py
@@ -12,6 +12,7 @@ from test import support
 from test.support import os_helper
 from test.support import socket_helper
 from test.support import warnings_helper
+from test.support import control_characters_c0
 from test.support.testcase import ExtraAssertions
 import os
 try:
@@ -677,6 +678,13 @@ class urlopen_DataTests(unittest.TestCase, ExtraAssertions):
         # missing padding character
         self.assertRaises(ValueError,urllib.request.urlopen,'data:;base64,Cg=')
 
+    def test_invalid_mediatype(self):
+        for c0 in control_characters_c0():
+            self.assertRaises(ValueError,urllib.request.urlopen,
+                              f'data:text/html;{c0},data')
+        for c0 in control_characters_c0():
+            self.assertRaises(ValueError,urllib.request.urlopen,
+                              f'data:text/html{c0};base64,ZGF0YQ==')
 
 class urlretrieve_FileTests(unittest.TestCase):
     """Test urllib.urlretrieve() on local files"""
