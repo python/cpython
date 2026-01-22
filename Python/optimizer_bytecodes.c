030bc286d3171182ddc2e5286af59274e55fefe9
reidenong <reidenong02@gmail.com>
2026-01-22 22:24:14 +0800

    Refine unit test names

--- Python/optimizer_bytecodes.c
+++ Python/optimizer_bytecodes.c
@@ -514,11 +514,6 @@ dummy_func(void) {
     }
 
     op(_COMPARE_OP_INT, (left, right -- res, l, r)) {
-        /* Comparison oparg masks */
-        const int COMPARE_LT_MASK = 2;
-        const int COMPARE_GT_MASK = 4;
-        const int COMPARE_EQ_MASK = 8;
-
         int cmp_mask = oparg & (COMPARE_LT_MASK | COMPARE_GT_MASK | COMPARE_EQ_MASK);
 
         if (cmp_mask == COMPARE_EQ_MASK) {
@@ -536,7 +531,17 @@ dummy_func(void) {
     }
 
     op(_COMPARE_OP_FLOAT, (left, right -- res, l, r)) {
-        res = sym_new_type(ctx, &PyBool_Type);
+        int cmp_mask = oparg & (COMPARE_LT_MASK | COMPARE_GT_MASK | COMPARE_EQ_MASK);
+
+        if (cmp_mask == COMPARE_EQ_MASK) {
+            res = sym_new_predicate(ctx, left, right, JIT_PRED_EQ);
+        }
+        else if (cmp_mask == (COMPARE_LT_MASK | COMPARE_GT_MASK)) {
+            res = sym_new_predicate(ctx, left, right, JIT_PRED_NE);
+        }
+        else {
+            res = sym_new_type(ctx, &PyBool_Type);
+        }
         l = left;
         r = right;
         REPLACE_OPCODE_IF_EVALUATES_PURE(left, right, res);
