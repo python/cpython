583b2b97755458c88e92b1e273d77c24200fd396
benediktjohannes <benedikt.johannes.hofer@gmail.com>
2026-01-25 23:22:43 +0100

    gh-144217: Add dicom support (medical imaging) to mimetypes (GH-144218)
    Co-authored-by: blurb-it[bot] <43283697+blurb-it[bot]@users.noreply.github.com>
    Co-authored-by: Peter Bierma <zintensitydev@gmail.com>

--- Lib/mimetypes.py
+++ Lib/mimetypes.py
@@ -477,6 +477,7 @@ def _default_mime_types():
     types_map = _types_map_default = {
         '.js'     : 'text/javascript',
         '.mjs'    : 'text/javascript',
+        '.dcm'    : 'application/dicom',
         '.epub'   : 'application/epub+zip',
         '.gz'     : 'application/gzip',
         '.json'   : 'application/json',
