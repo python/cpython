1dc12b2883c89045469f3c401157d4df13f535f0
Hai Zhu <haiizhu@outlook.com>
2026-01-30 23:25:19 +0800

    gh-144145: Track nullness of properties in the Tier 2 JIT optimizer (GH-144122)

--- Python/optimizer_cases.c.h
+++ Python/optimizer_cases.c.h
@@ -1936,8 +1936,7 @@
             JitOptRef o;
             owner = stack_pointer[-1];
             uint16_t index = (uint16_t)this_instr->operand0;
-            attr = sym_new_not_null(ctx);
-            (void)index;
+            attr = sym_get_attr(ctx, owner, (uint16_t)index);
             o = owner;
             CHECK_STACK_BOUNDS(1);
             stack_pointer[-1] = attr;
@@ -2012,6 +2011,25 @@
             owner = stack_pointer[-1];
             value = stack_pointer[-2];
             uint16_t offset = (uint16_t)this_instr->operand0;
+            JitOptRef old_value = sym_set_attr(ctx, owner, (uint16_t)offset, value);
+            if (sym_is_null(old_value)) {
+                ADD_OP(_STORE_ATTR_INSTANCE_VALUE_NULL, 0, offset);
+            }
+            o = owner;
+            CHECK_STACK_BOUNDS(-1);
+            stack_pointer[-2] = o;
+            stack_pointer += -1;
+            ASSERT_WITHIN_STACK_BOUNDS(__FILE__, __LINE__);
+            break;
+        }
+
+        case _STORE_ATTR_INSTANCE_VALUE_NULL: {
+            JitOptRef owner;
+            JitOptRef value;
+            JitOptRef o;
+            owner = stack_pointer[-1];
+            value = stack_pointer[-2];
+            uint16_t offset = (uint16_t)this_instr->operand0;
             (void)value;
             o = owner;
             CHECK_STACK_BOUNDS(-1);
@@ -2044,7 +2062,25 @@
             owner = stack_pointer[-1];
             value = stack_pointer[-2];
             uint16_t index = (uint16_t)this_instr->operand0;
-            (void)index;
+            JitOptRef old_value = sym_set_attr(ctx, owner, (uint16_t)index, value);
+            if (sym_is_null(old_value)) {
+                ADD_OP(_STORE_ATTR_SLOT_NULL, 0, index);
+            }
+            o = owner;
+            CHECK_STACK_BOUNDS(-1);
+            stack_pointer[-2] = o;
+            stack_pointer += -1;
+            ASSERT_WITHIN_STACK_BOUNDS(__FILE__, __LINE__);
+            break;
+        }
+
+        case _STORE_ATTR_SLOT_NULL: {
+            JitOptRef owner;
+            JitOptRef value;
+            JitOptRef o;
+            owner = stack_pointer[-1];
+            value = stack_pointer[-2];
+            uint16_t index = (uint16_t)this_instr->operand0;
             (void)value;
             o = owner;
             CHECK_STACK_BOUNDS(-1);
@@ -3192,11 +3228,15 @@
             self_or_null = stack_pointer[-1 - oparg];
             callable = stack_pointer[-2 - oparg];
             uint32_t type_version = (uint32_t)this_instr->operand0;
-            (void)type_version;
             (void)args;
             callable = sym_new_not_null(ctx);
-            self_or_null = sym_new_not_null(ctx);
             stack_pointer[-2 - oparg] = callable;
+            PyTypeObject *tp = _PyType_LookupByVersion(type_version);
+            if (tp != NULL) {
+                self_or_null = sym_new_descr_object(ctx, type_version);
+            } else {
+                self_or_null = sym_new_not_null(ctx);
+            }
             stack_pointer[-1 - oparg] = self_or_null;
             break;
         }
