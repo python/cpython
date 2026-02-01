8361602daab546646a483a2903675e2882003eb5
Ken Jin <kenjin4096@gmail.com>
2026-02-01 12:41:51 +0000

    Update ASCII art, address review

--- Python/optimizer_symbols.c
+++ Python/optimizer_symbols.c
@@ -25,24 +25,24 @@ state represents no information, and the BOTTOM state represents contradictory
 information. Though symbols logically progress through all intermediate nodes,
 we often skip in-between states for convenience:
 
-   UNKNOWN-------------------+
-   |     |                   |
-NULL     |                   |
-|        |                   |   <- Anything below this level is an object.
-|        NON_NULL-+          |
-|          |      |          |   <- Anything below this level has a known type version.
-|    TYPE_VERSION |          |
-|    |            |          |   <- Anything below this level has a known type.
-|    KNOWN_CLASS  |          |
-|    |  |  |   |  |          PREDICATE
-|    |  | INT* |  |          |
-|    |  |  |   |  |          |   <- Anything below this level has a known truthiness.
-|    |  |  |   |  TRUTHINESS |
-|    |  |  |   |  |          |
-| TUPLE |  |   |  |          |
-|    |  |  |   |  |          |   <- Anything below this level is a known constant.
-|    KNOWN_VALUE--+----------+
-|    |                           <- Anything below this level is unreachable.
+   UNKNOWN-------------------+------------+
+   |     |                   |            |
+NULL     |                   |            |
+|        |                   |            |  <- Anything below this level is an object.
+|        NON_NULL-+          |            |
+|          |      |          |            |  <- Anything below this level has a known type version.
+|    TYPE_VERSION |          |            |
+|    |            |          |            |  <- Anything below this level has a known type.
+|    KNOWN_CLASS  |          |            |
+|    |  |  |   |  |          PREDICATE    DESCR
+|    |  | INT* |  |          |            |
+|    |  |  |   |  |          |            |  <- Anything below this level has a known truthiness.
+|    |  |  |   |  TRUTHINESS |            |
+|    |  |  |   |  |          |            |
+| TUPLE |  |   |  |          |            |
+|    |  |  |   |  |          |            |  <- Anything below this level is a known constant.
+|    KNOWN_VALUE--+----------+------------+
+|    |                                        <- Anything below this level is unreachable.
 BOTTOM
 
 For example, after guarding that the type of an UNKNOWN local is int, we can
@@ -1031,6 +1031,7 @@ _Py_uop_sym_set_attr(JitOptContext *ctx, JitOptRef ref, uint16_t slot_index, Jit
             // Check escape
             if (sym->descr.last_escape_index < ctx->last_escape_index) {
                 sym->descr.num_descrs = 0;
+                sym->descr.last_escape_index = uop_buffer_length(&ctx->out_buffer);
                 return _Py_uop_sym_new_unknown(ctx);
             }
 
