7d89766fc938146640f1fe628a459894354cd515
Sam Gross <colesbury@gmail.com>
2026-02-05 10:52:50 -0500

    Apply suggestion from @vstinner
    Co-authored-by: Victor Stinner <vstinner@python.org>

--- Objects/funcobject.c
+++ Objects/funcobject.c
@@ -1761,7 +1761,7 @@ sm_new(PyTypeObject *type, PyObject *args, PyObject *kwds)
     if (sm == NULL) {
         return NULL;
     }
-    _PyObject_SetDeferredRefcount(sm);
+    _PyObject_SetDeferredRefcount((PyObject*)sm);
     if (sm_set_callable(sm, callable) < 0) {
         Py_DECREF(sm);
         return NULL;
