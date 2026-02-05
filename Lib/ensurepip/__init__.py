762b4e8ea4cacd58bec5e3589b2319017b5310a1
Davide Melfi <dmelfi@amazon.com>
2026-02-05 13:30:47 +0000

    chore: update pip version and setuptools

--- Lib/ensurepip/__init__.py
+++ Lib/ensurepip/__init__.py
@@ -11,8 +11,8 @@ from importlib import resources
 
 __all__ = ["version", "bootstrap"]
 _PACKAGE_NAMES = ('setuptools', 'pip')
-_SETUPTOOLS_VERSION = "79.0.1"
-_PIP_VERSION = "23.0.1"
+_SETUPTOOLS_VERSION = "80.10.2"
+_PIP_VERSION = "26.0.1"
 _PROJECTS = [
     ("setuptools", _SETUPTOOLS_VERSION, "py3"),
     ("pip", _PIP_VERSION, "py3"),
