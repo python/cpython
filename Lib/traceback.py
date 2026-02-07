d0f68a067e0e5ac8ebdce5276c68de3c66613b03
johnslavik <bartosz@ilikepython.com>
2026-02-07 16:28:53 +0100

    Don't prepend with dots in non-ASCII case
    This is extremely unlikely
    Keeping parity with this case would just be unhelpful

--- Lib/traceback.py
+++ Lib/traceback.py
@@ -1133,9 +1133,12 @@ class TracebackException:
             suggestion = _compute_suggestion_error(exc_value, exc_traceback, wrong_name)
             if suggestion:
                 if suggestion.isascii():
+                    # Prepending attribute accesseses with a dot makes the message much
+                    # easier to understand in the most common cases.
+                    # See GH-144285.
                     self._str += f". Did you mean '.{suggestion}' instead of '.{wrong_name}'?"
                 else:
-                    self._str += f". Did you mean '.{suggestion}' ({suggestion!a}) instead of '.{wrong_name}'?"
+                    self._str += f". Did you mean: '{suggestion}' ({suggestion!a})?"
         elif exc_type and issubclass(exc_type, NameError) and \
                 getattr(exc_value, "name", None) is not None:
             wrong_name = getattr(exc_value, "name", None)
