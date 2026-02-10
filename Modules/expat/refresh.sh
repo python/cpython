7b7483bfb4016a3a52d9617ab209b4fcf03d28f5
Petr Viktorin <encukou@gmail.com>
2026-02-10 10:49:00 +0100

    [3.13] gh-131261: generate_sbom.py: Exclude the refresh script from SBOM (GH-131359) (GH-144625)
    - generate_sbom.py: Exclude the refresh script from SBOM
    - Modules/expat/refresh.sh: Expand the list of manual steps
    (cherry picked from commit bad4bc4603fea4541b9e3db45b4050104e461c0a)
    
    Co-authored-by: Petr Viktorin <encukou@gmail.com>
    (cherry picked from commit 51d309988b3041cae20cca89172b4fd825c9a1cc)

--- Modules/expat/refresh.sh
+++ Modules/expat/refresh.sh
@@ -54,4 +54,11 @@ rm libexpat.tar.gz
 # Step 3: Add the namespacing include to expat_external.h
 sed -i 's/#  define Expat_External_INCLUDED 1/&\n\/* Namespace external symbols to allow multiple libexpat version to\n   co-exist. \*\/\n#include "pyexpatns.h"/' expat_external.h
 
-echo "Updated; verify all is okay using git diff and git status."
+echo "
+Updated! next steps:
+- Verify all is okay:
+    git diff
+    git status
+- Regenerate the sbom file
+    make regen-sbom
+"
