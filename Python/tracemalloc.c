00c69d22d7e1549a99fc7c171847500d36c4d562
Victor Stinner <vstinner@python.org>
2026-02-13 11:12:30 +0100

    gh-144763: Don't detach the GIL in tracemalloc
    tracemalloc no longer detachs the GIL to acquire its internal lock.

--- Python/tracemalloc.c
+++ Python/tracemalloc.c
@@ -36,7 +36,7 @@ static int _PyTraceMalloc_TraceRef(PyObject *op, PyRefTracerEvent event,
    the GIL held from PyMem_RawFree(). It cannot acquire the lock because it
    would introduce a deadlock in _PyThreadState_DeleteCurrent(). */
 #define tables_lock _PyRuntime.tracemalloc.tables_lock
-#define TABLES_LOCK() PyMutex_Lock(&tables_lock)
+#define TABLES_LOCK() PyMutex_LockFlags(&tables_lock, _Py_LOCK_DONT_DETACH)
 #define TABLES_UNLOCK() PyMutex_Unlock(&tables_lock)
 
 
@@ -224,13 +224,20 @@ tracemalloc_get_frame(_PyInterpreterFrame *pyframe, frame_t *frame)
     assert(PyStackRef_CodeCheck(pyframe->f_executable));
     frame->filename = &_Py_STR(anon_unknown);
 
-    int lineno = PyUnstable_InterpreterFrame_GetLine(pyframe);
+    int lineno = -1;
+    PyCodeObject *code = _PyFrame_GetCode(pyframe);
+    // PyUnstable_InterpreterFrame_GetLine() cannot but used, since it uses
+    // a critical section which can trigger a deadlock.
+    int lasti = _PyFrame_SafeGetLasti(pyframe);
+    if (lasti >= 0) {
+        lineno = _PyCode_SafeAddr2Line(code, lasti);
+    }
     if (lineno < 0) {
         lineno = 0;
     }
     frame->lineno = (unsigned int)lineno;
 
-    PyObject *filename = _PyFrame_GetCode(pyframe)->co_filename;
+    PyObject *filename = code->co_filename;
     if (filename == NULL) {
 #ifdef TRACE_DEBUG
         tracemalloc_error("failed to get the filename of the code object");
