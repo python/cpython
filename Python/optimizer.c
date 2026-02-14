f168dc543e835f0999cc975ce73e70eccd0cbf6c
Chris Eibl <138194463+chris-eibl@users.noreply.github.com>
2026-02-14 14:17:41 +0100

    fix warnings in jit builds

--- Python/optimizer.c
+++ Python/optimizer.c
@@ -1045,7 +1045,7 @@ _PyJit_TryInitializeTracing(
     tracer->initial_state.func = (PyFunctionObject *)Py_NewRef(func);
     tracer->initial_state.executor = (_PyExecutorObject *)Py_XNewRef(current_executor);
     tracer->initial_state.exit = exit;
-    tracer->initial_state.stack_depth = stack_pointer - _PyFrame_Stackbase(frame);
+    tracer->initial_state.stack_depth = (int)((uintptr_t)stack_pointer - (uintptr_t)_PyFrame_Stackbase(frame));
     tracer->initial_state.chain_depth = chain_depth;
     tracer->prev_state.dependencies_still_valid = true;
     tracer->prev_state.instr_code = (PyCodeObject *)Py_NewRef(_PyFrame_GetCode(frame));
@@ -1487,7 +1487,7 @@ stack_allocate(_PyUOpInstruction *buffer, _PyUOpInstruction *output, int length)
         write++;
         depth = _PyUop_Caching[uop].entries[depth].output;
     }
-    return write - output;
+    return (int)((uintptr_t)write - (uintptr_t)output);
 }
 
 static int
@@ -2131,6 +2131,7 @@ executor_to_gv(_PyExecutorObject *executor, FILE *out)
             assert(inst->format == UOP_FORMAT_JUMP);
             _PyUOpInstruction const *exit_inst = &executor->trace[inst->jump_target];
             uint16_t base_exit_opcode = _PyUop_Uncached[exit_inst->opcode];
+            (void)base_exit_opcode;
             assert(base_exit_opcode == _EXIT_TRACE || base_exit_opcode == _DYNAMIC_EXIT);
             exit = (_PyExitData *)exit_inst->operand0;
         }
