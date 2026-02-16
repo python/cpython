37920da03c9221fc2fc7f048c706981b396525ca
Pablo Galindo Salgado <pablogsal@gmail.com>
2026-02-16 21:17:21 +0000

    gh-144878: Gate PEP 810 builtins in xpickle compat tests

--- Lib/test/pickletester.py
+++ Lib/test/pickletester.py
@@ -3202,6 +3202,7 @@ class AbstractPickleTests:
             'ExceptionGroup': (3, 11),
             '_IncompleteInputError': (3, 13),
             'PythonFinalizationError': (3, 13),
+            'ImportCycleError': (3, 15),
         }
         for t in builtins.__dict__.values():
             if isinstance(t, type) and issubclass(t, BaseException):
@@ -3228,6 +3229,7 @@ class AbstractPickleTests:
             'breakpoint': (3, 7),
             'aiter': (3, 10),
             'anext': (3, 10),
+            '__lazy_import__': (3, 15),
         }
         for t in builtins.__dict__.values():
             if isinstance(t, types.BuiltinFunctionType):
