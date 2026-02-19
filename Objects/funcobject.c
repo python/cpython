5a4cbbdadc3810d24670039f6376b448540cbdbc
Ken Jin <kenjin@python.org>
2026-02-19 11:44:50 +0800

    gh-144888: Don't invalidate executors during function deallocation (#144974)

--- Objects/funcobject.c
+++ Objects/funcobject.c
@@ -1126,10 +1126,6 @@ func_dealloc(PyObject *self)
     if (_PyObject_ResurrectEnd(self)) {
         return;
     }
-#if _Py_TIER2
-    _Py_Executors_InvalidateDependency(_PyInterpreterState_GET(), self, 1);
-    _PyJit_Tracer_InvalidateDependency(_PyThreadState_GET(), self);
-#endif
     _PyObject_GC_UNTRACK(op);
     FT_CLEAR_WEAKREFS(self, op->func_weakreflist);
     (void)func_clear((PyObject*)op);
