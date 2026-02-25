cd4787004beb0b84f51ce91df21c518d36f7309b
Filipe Laíns <lains@riseup.net>
2026-02-25 12:45:43 +0000

    Fix "lazy from (...) import (...)" tests
    Signed-off-by: Filipe Laíns <lains@riseup.net>

--- Lib/test/test_import/data/lazy_imports/global_filter_from.py
+++ Lib/test/test_import/data/lazy_imports/global_filter_from.py
@@ -1,11 +1,11 @@
-import importlib
+import sys
 
 def filter(module_name, imported_name, from_list):
     assert module_name == __name__
     assert imported_name == "test.test_import.data.lazy_imports.basic2"
-    assert from_list == ['f']
+    assert from_list == ('f',)
     return False
 
-importlib.set_lazy_imports(None, filter)
+sys.set_lazy_imports_filter(filter)
 
-lazy from import test.test_import.data.lazy_imports.basic2 import f
+lazy from test.test_import.data.lazy_imports.basic2 import f
