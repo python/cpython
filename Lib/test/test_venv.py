b11fea0fd74173d5f3d055d3c062cd621417bc1d
Shrey Naithani <shrey.naithani@shelllite.tech>
2026-03-05 19:49:49 +0530

    gh-145417: Do not preserve SELinux context when copying venv scripts (GH-145454) (cherry picked from commit dbe0007ab2ff679c85d88e62fb875437b2dc2522)
    Co-authored-by: Shrey Naithani <shrey.naithani@shelllite.tech>
    Co-authored-by: Miro Hrončok <miro@hroncok.cz>
    Co-authored-by: blurb-it[bot] <43283697+blurb-it[bot]@users.noreply.github.com>
    Co-authored-by: Victor Stinner <vstinner@python.org>

--- Lib/test/test_venv.py
+++ Lib/test/test_venv.py
@@ -11,13 +11,13 @@ import os
 import os.path
 import pathlib
 import re
+import shlex
 import shutil
 import struct
 import subprocess
 import sys
 import sysconfig
 import tempfile
-import shlex
 from test.support import (captured_stdout, captured_stderr,
                           skip_if_broken_multiprocessing_synchronize, verbose,
                           requires_subprocess, is_android, is_apple_mobile,
@@ -378,6 +378,16 @@ class BasicTest(BaseTest):
             fn = os.path.join(d, filename)
             with open(fn, 'wb') as f:
                 f.write(b'Still here?')
+
+    @unittest.skipUnless(hasattr(os, 'listxattr'), 'test requires os.listxattr')
+    def test_install_scripts_selinux(self):
+        """
+        gh-145417: Test that install_scripts does not copy SELinux context
+        when copying scripts.
+        """
+        with patch('os.listxattr') as listxattr_mock:
+            venv.create(self.env_dir)
+            listxattr_mock.assert_not_called()
 
     def test_overwrite_existing(self):
         """
