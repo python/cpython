9f1b62f14ed307dec79a00fa0e27334f5fcc1a67
Petr Viktorin <encukou@gmail.com>
2026-03-10 16:01:52 +0100

    [3.13] gh-145548: Don't use VMADDR_CID_LOCAL from `socket` (GH-145735)
    VMADDR_CID_LOCAL was added to `socekt` in 3.14. The test needs a
    local constant in setUp(), as in clientSetUp().

--- Lib/test/test_socket.py
+++ Lib/test/test_socket.py
@@ -499,7 +499,7 @@ class ThreadedVSOCKSocketStreamTest(unittest.TestCase, ThreadableTest):
         self.addCleanup(self.serv.close)
         cid = get_cid()
         if cid in (socket.VMADDR_CID_HOST, socket.VMADDR_CID_ANY):
-            cid = socket.VMADDR_CID_LOCAL
+            cid = VMADDR_CID_LOCAL
         try:
             self.serv.bind((cid, VSOCKPORT))
         except OSError as exc:
