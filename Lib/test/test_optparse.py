a27f627c7d57cce990c2b75b2b6c7e56c96ef404
Stefan Zetzsche <stefanzetzsche@gmail.com>
2026-03-11 17:48:34 +0000

    test(optparse): add regression test for tuple callback_kwargs raising OptionError
    A tuple callback_kwargs should raise OptionError with the repr of the
    tuple, not TypeError from string formatting unpacking the tuple.

--- Lib/test/test_optparse.py
+++ Lib/test/test_optparse.py
@@ -301,6 +301,16 @@ class TestOptionChecks(BaseTest):
                      'callback': self.dummy,
                      'callback_kwargs': 'foo'})
 
+    def test_callback_kwargs_tuple_not_dict(self):
+        # A tuple callback_kwargs should raise OptionError, not TypeError
+        # from string formatting unpacking the tuple.
+        self.assertOptionError(
+            "option -b: callback_kwargs, if supplied, "
+            "must be a dict: not (1, 2)",
+            ["-b"], {'action': 'callback',
+                     'callback': self.dummy,
+                     'callback_kwargs': (1, 2)})
+
     def test_no_callback_for_action(self):
         self.assertOptionError(
             "option -b: callback supplied ('foo') for non-callback option",
