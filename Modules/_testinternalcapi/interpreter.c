ec9b77593e6d3977038c6e6108494934e2d33399
Shamil Abdulaev <ashm.tech@proton.me>
2026-03-11 20:38:03 +0300

    use extern for _Py_jit_entry in _testinternalcapi

--- Modules/_testinternalcapi/interpreter.c
+++ Modules/_testinternalcapi/interpreter.c
@@ -21,7 +21,7 @@ stop_tracing_and_jit(PyThreadState *tstate, _PyInterpreterFrame *frame)
 }
 #endif
 
-_PyJitEntryFuncPtr _Py_jit_entry;
+extern _PyJitEntryFuncPtr _Py_jit_entry;
 
 #if _Py_TAIL_CALL_INTERP
 #include "test_targets.h"
