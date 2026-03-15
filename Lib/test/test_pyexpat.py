04a88d8785eed23276192bcd3700b86c94f0a8aa
Stan Ulbrych <89152624+StanFromIreland@users.noreply.github.com>
2026-03-15 22:17:06 +0000

    Remvoe `skip_if_unlimited_stack_size` decorator

--- Lib/test/test_pyexpat.py
+++ Lib/test/test_pyexpat.py
@@ -688,7 +688,6 @@ class ElementDeclHandlerTest(unittest.TestCase):
         parser.ElementDeclHandler = lambda _1, _2: None
         self.assertRaises(TypeError, parser.Parse, data, True)
 
-    @support.skip_if_unlimited_stack_size
     @support.skip_emscripten_stack_overflow()
     @support.skip_wasi_stack_overflow()
     def test_deeply_nested_content_model(self):
