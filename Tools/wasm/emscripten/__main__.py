520024243f0d16f87b6690e197b8ba44c169c502
Hood Chatham <roberthoodchatham@gmail.com>
2026-03-16 11:51:33 +0100

    Add deprecation message to old __main__.py script

--- Tools/wasm/emscripten/__main__.py
+++ Tools/wasm/emscripten/__main__.py
@@ -1,6 +1,13 @@
 if __name__ == "__main__":
     import pathlib
     import runpy
+    import sys
+
+    print(
+        "⚠️ WARNING: This script is deprecated and slated for removal in Python 3.20; "
+        "execute the `Platforms/emscripten/` directory instead (i.e. `python Platforms/emscripten`)\n",
+        file=sys.stderr,
+    )
 
     checkout = pathlib.Path(__file__).parents[3]
     emscripten_dir = (checkout / "Platforms/emscripten").absolute()
