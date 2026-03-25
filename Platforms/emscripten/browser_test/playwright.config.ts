9bb12d0591fbc8a0bfa8a64458e6185e16d227fe
Hood Chatham <roberthoodchatham@gmail.com>
2026-03-25 10:43:21 +0100

    Also support CROSS_BUILD_DIR env variable in browser_test

--- Platforms/emscripten/browser_test/playwright.config.ts
+++ Platforms/emscripten/browser_test/playwright.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig, devices } from '@playwright/test';
+import { resolve } from "node:path";
 
 const port = process.env.PORT ?? "8787";
+const crossBuildDir = resolve("../../../", process.env.CROSS_BUILD_DIR ?? "cross-build");
 
 export default defineConfig({
   testDir: '.',
@@ -18,7 +20,7 @@ export default defineConfig({
     },
   ],
   webServer: {
-    command: `npx http-server ../../../cross-build/wasm32-emscripten/build/python/web_example_pyrepl_jspi/ -p ${port}`,
+    command: `npx http-server ${crossBuildDir}/wasm32-emscripten/build/python/web_example_pyrepl_jspi/ -p ${port}`,
     url: `http://localhost:${port}`,
   },
 });
