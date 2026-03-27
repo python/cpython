4d61fd6b7a706762d962c4a23313d57167796a12
Miss Islington (bot) <31488909+miss-islington@users.noreply.github.com>
2026-03-27 03:37:53 +0100

    [3.14] gh-146244: Fix initconfig.c SET_ITEM macro leaks dict on expression failure (GH-146246) (GH-146432)
    (cherry picked from commit 9343518c6f413b2231b17c56065e5cf823aa0d2a)
    
    Co-authored-by: Wulian233 <1055917385@qq.com>

--- Python/initconfig.c
+++ Python/initconfig.c
@@ -502,7 +502,7 @@ _Py_COMP_DIAG_IGNORE_DEPR_DECLS
         do { \
             obj = (EXPR); \
             if (obj == NULL) { \
-                return NULL; \
+                goto fail; \
             } \
             int res = PyDict_SetItemString(dict, (KEY), obj); \
             Py_DECREF(obj); \
