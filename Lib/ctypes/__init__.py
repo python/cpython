aada86660013859510eadef4c5d2b31f93f4581a
Jonathan Dung <jonathandung@yahoo.com>
2026-03-28 11:38:13 +0800

    Raise SystemError instead of plain Exception in case of internal version inconsistency

--- Lib/ctypes/__init__.py
+++ Lib/ctypes/__init__.py
@@ -19,7 +19,7 @@ from _ctypes import CField
 from struct import calcsize as _calcsize
 
 if __version__ != _ctypes_version:
-    raise Exception("Version number mismatch", __version__, _ctypes_version)
+    raise SystemError("Version number mismatch", __version__, _ctypes_version)
 
 if _os.name == "nt":
     from _ctypes import COMError, CopyComPointer, FormatError
