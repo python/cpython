cb38aa40d5ea5129a0c61c3ec077657a7c441810
Locked-chess-official <13140752715@163.com>
2026-03-30 21:08:41 +0800

    check type in _find_incompatible_extension_module and add try-except

--- Lib/traceback.py
+++ Lib/traceback.py
@@ -1907,26 +1907,29 @@ def _find_incompatible_extension_module(module_name):
     import importlib.machinery
     import importlib.resources.readers
 
-    if not module_name or not importlib.machinery.EXTENSION_SUFFIXES:
+    if not isinstance(module_name, str) or not importlib.machinery.EXTENSION_SUFFIXES:
         return
 
     # We assume the last extension is untagged (eg. .so, .pyd)!
     # tests.test_traceback.MiscTest.test_find_incompatible_extension_modules
     # tests that assumption.
-    untagged_suffix = importlib.machinery.EXTENSION_SUFFIXES[-1]
-    # On Windows the debug tag is part of the module file stem, instead of the
-    # extension (eg. foo_d.pyd), so let's remove it and just look for .pyd.
-    if os.name == 'nt':
-        untagged_suffix = untagged_suffix.removeprefix('_d')
+    try:
+        untagged_suffix = importlib.machinery.EXTENSION_SUFFIXES[-1]
+        # On Windows the debug tag is part of the module file stem, instead of the
+        # extension (eg. foo_d.pyd), so let's remove it and just look for .pyd.
+        if os.name == 'nt':
+            untagged_suffix = untagged_suffix.removeprefix('_d')
 
-    parent, _, child = module_name.rpartition('.')
-    if parent:
-        traversable = importlib.resources.files(parent)
-    else:
-        traversable = importlib.resources.readers.MultiplexedPath(
-            *map(pathlib.Path, filter(os.path.isdir, sys.path))
-        )
+        parent, _, child = module_name.rpartition('.')
+        if parent:
+            traversable = importlib.resources.files(parent)
+        else:
+            traversable = importlib.resources.readers.MultiplexedPath(
+                *map(pathlib.Path, filter(os.path.isdir, sys.path))
+            )
 
-    for entry in traversable.iterdir():
-        if entry.name.startswith(child + '.') and entry.name.endswith(untagged_suffix):
-            return entry.name
+        for entry in traversable.iterdir():
+            if entry.name.startswith(child + '.') and entry.name.endswith(untagged_suffix):
+                return entry.name
+    except Exception:
+        return
