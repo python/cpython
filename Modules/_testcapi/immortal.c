25b48b84b85a08384479d2636a01d1d52fa97a6b
Miss Islington (bot) <31488909+miss-islington@users.noreply.github.com>
2026-03-31 16:14:12 +0200

    [3.14] gh-143050: Correct PyLong_FromString() to use _PyLong_Negate() (GH-145901) (#147331)
    gh-143050: Correct PyLong_FromString() to use _PyLong_Negate() (GH-145901)
    
    The long_from_string_base() might return a small integer, when the
    _pylong.py is used to do conversion.  Hence, we must be careful here to
    not smash it "small int" bit by using the _PyLong_FlipSign().
    (cherry picked from commit db5936c5b89aa19e04d63120e0cf5bbc73bf2420)
    
    Co-authored-by: Sergey B Kirpichev <skirpichev@gmail.com>
    Co-authored-by: Victor Stinner <vstinner@python.org>

--- Modules/_testcapi/immortal.c
+++ Modules/_testcapi/immortal.c
@@ -31,13 +31,13 @@ test_immortal_small_ints(PyObject *self, PyObject *Py_UNUSED(ignored))
     for (int i = -5; i <= 256; i++) {
         PyObject *obj = PyLong_FromLong(i);
         assert(verify_immortality(obj));
-        int has_int_immortal_bit = ((PyLongObject *)obj)->long_value.lv_tag & IMMORTALITY_BIT_MASK;
+        int has_int_immortal_bit = _PyLong_IsSmallInt((PyLongObject *)obj);
         assert(has_int_immortal_bit);
     }
     for (int i = 257; i <= 260; i++) {
         PyObject *obj = PyLong_FromLong(i);
         assert(obj);
-        int has_int_immortal_bit = ((PyLongObject *)obj)->long_value.lv_tag & IMMORTALITY_BIT_MASK;
+        int has_int_immortal_bit = _PyLong_IsSmallInt((PyLongObject *)obj);
         assert(!has_int_immortal_bit);
         Py_DECREF(obj);
     }
