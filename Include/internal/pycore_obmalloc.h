116b29b3516e7ae8d6b0df55d7e626fd09107e2f
Tim Peters <tim.peters@gmail.com>
2026-04-05 18:55:50 -0500

    Vladimir's original overviews, from 1998, are still good, but going on 30 years later details have changed. Note that, but rather try to keep up with moving targets in a different file, point to sys._debugmallocstats() as the sure way to discover precise current details.
    No code changes, just added a block comment.

--- Include/internal/pycore_obmalloc.h
+++ Include/internal/pycore_obmalloc.h
@@ -14,6 +14,12 @@ typedef unsigned int pymem_uint;  /* assuming >= 16 bits */
 #undef  uint
 #define uint pymem_uint
 
+/* NOTE: the following overviews were in the initial checkin, in 1998. In
+ * 2026, they're still helpful, but some details have changed. For example,
+ * we now use 32 size clesses 16 bytes apart, and an arena is generally at
+ * last 1MB. Use sys._debugmallocstats() to see exact current details for
+ * the specific version of CPython is use.
+ */
 
 /* An object allocator for Python.
 
