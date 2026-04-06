a2a444df2e7ffec9fe58a7d6cf1466f1998d482c
Stan Ulbrych <stan@python.org>
2026-04-06 15:52:16 +0100

    wrong branch:-/

--- Tools/build/compute-changes.py
+++ Tools/build/compute-changes.py
@@ -123,14 +123,25 @@ class Outputs:
 
 def compute_changes() -> None:
     target_ref, head_ref = git_refs()
-    if os.environ.get("GITHUB_EVENT_NAME", "") == "pull_request":
+    if True:
+        outputs = Outputs(
+            run_ubuntu=True,
+        )
+    elif os.environ.get("GITHUB_EVENT_NAME", "") == "pull_request":
         # Getting changed files only makes sense on a pull request
         files = get_changed_files(target_ref, head_ref)
         outputs = process_changed_files(files)
     else:
         # Otherwise, just run the tests
         outputs = Outputs(
+            run_android=True,
+            run_emscripten=True,
+            run_ios=True,
+            run_macos=True,
+            run_tests=True,
             run_ubuntu=True,
+            run_wasi=True,
+            run_windows_tests=True,
         )
     target_branch = target_ref.removeprefix("origin/")
     outputs = process_target_branch(outputs, target_branch)
