4ddf024b64901bdf543d7149b96b819e87c7c2ed
Łukasz Langa <lukasz@langa.pl>
2026-04-07 14:03:01 +0200

    Move str_width and wlen to the place in the file where they were before

--- Lib/_pyrepl/utils.py
+++ Lib/_pyrepl/utils.py
@@ -25,32 +25,6 @@ KEYWORD_CONSTANTS = frozenset({"True", "False", "None"})
 BUILTINS = frozenset({str(name) for name in dir(builtins) if not name.startswith('_')})
 
 
-@functools.cache
-def str_width(c: str) -> int:
-    if ord(c) < 128:
-        return 1
-    # gh-139246 for zero-width joiner and combining characters
-    if unicodedata.combining(c):
-        return 0
-    category = unicodedata.category(c)
-    if category == "Cf" and c != "\u00ad":
-        return 0
-    w = unicodedata.east_asian_width(c)
-    if w in ("N", "Na", "H", "A"):
-        return 1
-    return 2
-
-
-def wlen(s: str) -> int:
-    if len(s) == 1 and s != "\x1a":
-        return str_width(s)
-    length = sum(str_width(i) for i in s)
-    # remove lengths of any escape sequences
-    sequence = ANSI_ESCAPE_SEQUENCE.findall(s)
-    ctrl_z_cnt = s.count("\x1a")
-    return length - sum(len(i) for i in sequence) + ctrl_z_cnt
-
-
 def THEME(**kwargs):
     # Not cached: the user can modify the theme inside the interactive session.
     return _colorize.get_theme(**kwargs).syntax
@@ -84,6 +58,34 @@ class Span(NamedTuple):
 class ColorSpan(NamedTuple):
     span: Span
     tag: str
+
+
+@functools.cache
+def str_width(c: str) -> int:
+    if ord(c) < 128:
+        return 1
+    # gh-139246 for zero-width joiner and combining characters
+    if unicodedata.combining(c):
+        return 0
+    category = unicodedata.category(c)
+    if category == "Cf" and c != "\u00ad":
+        return 0
+    w = unicodedata.east_asian_width(c)
+    if w in ("N", "Na", "H", "A"):
+        return 1
+    return 2
+
+
+def wlen(s: str) -> int:
+    if len(s) == 1 and s != "\x1a":
+        return str_width(s)
+    length = sum(str_width(i) for i in s)
+    # remove lengths of any escape sequences
+    sequence = ANSI_ESCAPE_SEQUENCE.findall(s)
+    ctrl_z_cnt = s.count("\x1a")
+    return length - sum(len(i) for i in sequence) + ctrl_z_cnt
+
+
 
 
 def unbracket(s: str, including_content: bool = False) -> str:
