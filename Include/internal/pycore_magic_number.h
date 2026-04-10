dfb7fdbef5783cfed681ed4f030782765553e931
AN Long <aisk1988@gmail.com>
2026-04-11 00:12:26 +0800

    Fix magic number

--- Include/internal/pycore_magic_number.h
+++ Include/internal/pycore_magic_number.h
@@ -294,7 +294,7 @@ Known values:
     Python 3.15a4 3661 (Lazy imports IMPORT_NAME opcode changes)
     Python 3.15a8 3662 (Add counter to RESUME)
     Python 3.15a8 3663 (Merge GET_ITER and GET_YIELD_FROM_ITER. Modify SEND to make it a bit more like FOR_ITER)
-    Python 3.15a8 3663 (Remove DELETE_ATTR opcode)
+    Python 3.15a8 3664 (Remove DELETE_ATTR opcode)
 
 
     Python 3.16 will start with 3700
