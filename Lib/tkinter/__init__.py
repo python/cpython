995d6a1e736d7201d2741ad7fd329cad32293cc8
Rihaan Meher <sharktidedev@gmail.com>
2026-04-11 09:03:12 -0500

    Update usage in _stringify

--- Lib/tkinter/__init__.py
+++ Lib/tkinter/__init__.py
@@ -75,7 +75,7 @@ def _stringify(value):
     if isinstance(value, (list, tuple)):
         if len(value) == 1:
             value = _stringify(value[0])
-            if _magic_re.search(value):
+            if _get_magic_re().search(value):
                 value = '{%s}' % value
         else:
             value = '{%s}' % _join(value)
@@ -86,14 +86,14 @@ def _stringify(value):
             value = str(value)
         if not value:
             value = '{}'
-        elif _magic_re.search(value):
+        elif _get_magic_re().search(value):
             # add '\' before special characters and spaces
-            value = _magic_re.sub(r'\\\1', value)
+            value = _get_magic_re().sub(r'\\\1', value)
             value = value.replace('\n', r'\n')
-            value = _space_re.sub(r'\\\1', value)
+            value = _get_space_re().sub(r'\\\1', value)
             if value[0] == '"':
                 value = '\\' + value
-        elif value[0] == '"' or _space_re.search(value):
+        elif value[0] == '"' or _get_space_re().search(value):
             value = '{%s}' % value
     return value
 
