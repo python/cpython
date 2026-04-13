f4654824ae0850ac87227fb270f9057477946769
Stan Ulbrych <stan@python.org>
2026-04-13 22:41:51 +0100

    [3.11] gh-148169: Fix webbrowser `%action` substitution bypass of dash-prefix check (GH-148170) (#148520)
    (cherry picked from commit d22922c8a7958353689dc4763dd72da2dea03fff)

--- Lib/webbrowser.py
+++ Lib/webbrowser.py
@@ -265,7 +265,6 @@ class UnixBrowser(BaseBrowser):
 
     def open(self, url, new=0, autoraise=True):
         sys.audit("webbrowser.open", url)
-        self._check_url(url)
         if new == 0:
             action = self.remote_action
         elif new == 1:
@@ -279,7 +278,9 @@ class UnixBrowser(BaseBrowser):
             raise Error("Bad 'new' parameter to open(); " +
                         "expected 0, 1, or 2, got %s" % new)
 
-        args = [arg.replace("%s", url).replace("%action", action)
+        self._check_url(url.replace("%action", action))
+
+        args = [arg.replace("%action", action).replace("%s", url)
                 for arg in self.remote_args]
         args = [arg for arg in args if arg]
         success = self._invoke(args, True, autoraise, url)
