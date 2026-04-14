37305c73c8543e68442aa8403800729f74bc052f
Sergey B Kirpichev <skirpichev@gmail.com>
2026-04-15 02:04:16 +0300

    gh-148573: correct allocation of complex types in the ctypes
    Old code relying on implementation detail, that elements[1] for
    the FFI_TYPE_COMPLEX was never read.
    
    But this type actually shares same assumption as the FFI_TYPE_STRUCT:
    the elements field is a NULL-terminated array of pointers to ffi_type
    objects.  So far for primitive types - only complex types have this
    struct field as non-NULL (two element array).

--- Modules/_ctypes/_ctypes.c
+++ Modules/_ctypes/_ctypes.c
@@ -2382,7 +2382,8 @@ PyCSimpleType_init(PyObject *self, PyObject *args, PyObject *kwds)
         stginfo->ffi_type_pointer = *fmt->pffi_type;
     }
     else {
-        const size_t els_size = sizeof(fmt->pffi_type->elements);
+        assert(fmt->pffi_type->type == FFI_TYPE_COMPLEX);
+        const size_t els_size = sizeof(2 * sizeof(ffi_type *));
         stginfo->ffi_type_pointer.size = fmt->pffi_type->size;
         stginfo->ffi_type_pointer.alignment = fmt->pffi_type->alignment;
         stginfo->ffi_type_pointer.type = fmt->pffi_type->type;
