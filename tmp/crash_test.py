60378b068869746598dcb14ff2243179c027eb71
Prakash Sellathurai <prakashsellathurai@gmail.com>
2026-04-14 21:49:29 +0530

    Delete crash_test.py

--- tmp/crash_test.py
+++
@@ -1,8 +0,0 @@
-import types, ctypes
-class NoVectorCall:
-    def __class_getitem__(cls, item):
-        return types.GenericAlias(cls, item)
-    def __new__(cls, x):
-        return x * 2
-
-print(hasattr(NoVectorCall, "__vectorcalloffset__"))
