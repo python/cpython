c84a36a9bc357322186e22f3d0e887571e4b52a2
Neko Asakura <neko.asakura@outlook.com>
2026-04-14 12:29:34 -0400

    gh-148380: relax assert

--- Python/optimizer_bytecodes.c
+++ Python/optimizer_bytecodes.c
@@ -246,7 +246,7 @@ dummy_func(void) {
 
     op(_GUARD_TYPE_VERSION, (type_version/2, owner -- owner)) {
         assert(type_version);
-        assert(this_instr[-1].opcode == _RECORD_TOS_TYPE);
+        assert(this_instr[-1].opcode == _RECORD_TOS_TYPE || this_instr[-1].opcode == _RECORD_TOS);
         if (sym_matches_type_version(owner, type_version)) {
             ADD_OP(_NOP, 0, 0);
         }
