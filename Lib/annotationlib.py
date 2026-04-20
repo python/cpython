3d9b64a3da5b189b3391a4c0337398cd2b3542ac
David C Ellis <ducksual@gmail.com>
2026-04-20 22:52:56 +0100

    remove unnecessary and incorrect node 'fixup'.

--- Lib/annotationlib.py
+++ Lib/annotationlib.py
@@ -1198,7 +1198,5 @@ class _ExtraNameFixer(ast.NodeTransformer):
 
     def visit_Name(self, node: ast.Name):
         if (new_name := self.extra_names.get(node.id, _sentinel)) is not _sentinel:
-            new_node = ast.Name(id=type_repr(new_name))
-            ast.copy_location(node, new_node)
-            node = new_node
+            node = ast.Name(id=type_repr(new_name))
         return node
