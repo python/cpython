ea84b7b3e99bbaf515b0cb2531d1c6394649b796
Michiel de Hoon <mjldehoon@yahoo.com>
2026-04-21 16:39:15 +0900

    add one test

--- Lib/test/test_tkinter/test_tkinter_pipe.py
+++ Lib/test/test_tkinter/test_tkinter_pipe.py
@@ -8,8 +8,24 @@ from test import support
 @unittest.skipUnless(support.has_subprocess_support, "test requires subprocess")
 class TkinterPipeTest(unittest.TestCase):
 
-    def test_tkinter_pipe(self):
-        proc = subprocess.Popen([sys.executable, "-i", "-u"],
+    def test_tkinter_pipe_buffered(self):
+        args = [sys.executable, "-i"]
+        proc = subprocess.Popen(args,
+                                 stdin=subprocess.PIPE,
+                                 stdout=subprocess.PIPE,
+                                 stderr=subprocess.PIPE)
+        proc.stdin.write(b"import tkinter\n")
+        proc.stdin.write(b"interpreter = tkinter.Tcl()\n")
+        proc.stdin.write(b"print('hello')\n")
+        proc.stdin.write(b"print('goodbye')\n")
+        proc.stdin.write(b"quit()\n")
+        stdout, stderr = proc.communicate()
+        stdout = stdout.decode()
+        self.assertEqual(stdout.split(), ['hello', 'goodbye'])
+
+    def test_tkinter_pipe_unbuffered(self):
+        args = [sys.executable, "-i", "-u"]
+        proc = subprocess.Popen(args,
                                  stdin=subprocess.PIPE,
                                  stdout=subprocess.PIPE,
                                  stderr=subprocess.PIPE)
