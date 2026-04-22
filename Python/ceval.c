858e69eab0949852cc41733e8465250fc80d0b66
Gabriele N. Tornetta <P403n1x87@users.noreply.github.com>
2026-04-22 09:08:23 +0100

    gh-142186: Allow all PEP-669 events to be per-code object and disableable  (GH-146182)
    * Make the `PY_UNWIND` monitoring event available as a code-local
    event to allow trapping on function exit events when an exception
    bubbles up. This complements the PY_RETURN event by allowing to
    catch any function exit event.
    
    * Allow `PY_UNWIND`  to be `DISABLE`d; disabling it disables the event for the whole code object.
    
    * Do the above for `PY_THROW`, `RAISE`, `EXCEPTION_HANDLED`, and `RERAISE` events.

--- Python/ceval.c
+++ Python/ceval.c
@@ -2406,15 +2406,16 @@ void
 _PyEval_MonitorRaise(PyThreadState *tstate, _PyInterpreterFrame *frame,
               _Py_CODEUNIT *instr)
 {
-    if (no_tools_for_global_event(tstate, PY_MONITORING_EVENT_RAISE)) {
+    if (no_tools_for_local_event(tstate, frame, PY_MONITORING_EVENT_RAISE)) {
         return;
     }
     do_monitor_exc(tstate, frame, instr, PY_MONITORING_EVENT_RAISE);
 }
 
 bool
-_PyEval_NoToolsForUnwind(PyThreadState *tstate) {
-    return no_tools_for_global_event(tstate, PY_MONITORING_EVENT_PY_UNWIND);
+_PyEval_NoToolsForUnwind(PyThreadState *tstate, _PyInterpreterFrame *frame)
+{
+    return no_tools_for_local_event(tstate, frame, PY_MONITORING_EVENT_PY_UNWIND);
 }
 
 
