5c0999c68abfe0913911ef4c91aba7674212d07e
Mark Shannon <Mark.Shannon@arm.com>
2026-04-24 16:49:49 +0100

    Bump magic number

--- Include/internal/pycore_magic_number.h
+++ Include/internal/pycore_magic_number.h
@@ -296,6 +296,7 @@ Known values:
     Python 3.15a8 3663 (Merge GET_ITER and GET_YIELD_FROM_ITER. Modify SEND to make it a bit more like FOR_ITER)
     Python 3.15a8 3664 (Fix __qualname__ for __annotate__ functions)
     Python 3.15a8 3665 (Add FOR_ITER_VIRTUAL and GET_ITER specializations)
+    Python 3.15b1 3666 (Add SEND_VIRTUAL and SEND_ASYNC_GEN specializations)
 
 
     Python 3.16 will start with 3700
@@ -309,7 +310,7 @@ PC/launcher.c must also be updated.
 
 */
 
-#define PYC_MAGIC_NUMBER 3665
+#define PYC_MAGIC_NUMBER 3666
 /* This is equivalent to converting PYC_MAGIC_NUMBER to 2 bytes
    (little-endian) and then appending b'\r\n'. */
 #define PYC_MAGIC_NUMBER_TOKEN \
