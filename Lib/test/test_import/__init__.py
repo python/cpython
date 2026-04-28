f22ae49c3cead60bbbc5981394f5f6c8885641c0
ada <ada@cubething.dev>
2026-04-28 10:53:08 -0500

    tests: address PR review for wave 8 (#371, #490)

--- Lib/test/test_import/__init__.py
+++ Lib/test/test_import/__init__.py
@@ -24,13 +24,15 @@ import unittest
 # NSKIP050 https://github.com/nanvix/cpython/issues/530
 from test import support
 if support.is_nanvix and not support.is_nanvix_standalone:
-    raise unittest.SkipTest("NSKIP050: hosted Nanvix unable to run this module cleanly (rmdir errno 88 cascade and/or other linuxd VFS issues); not bisected, see #480")
+    raise unittest.SkipTest("NSKIP050: hosted Nanvix unable to run this module cleanly (rmdir errno 88 cascade and/or other linuxd VFS issues)")  # detail: not bisected, see #530
 from unittest import mock
 try:
     import _testinternalcapi
 except ImportError:
     # NSKIP002: _testinternalcapi not available on Nanvix
-    _testinternalcapi = None
+    if support.is_nanvix:
+        raise unittest.SkipTest("NSKIP002: _testinternalcapi not available on Nanvix")
+    raise
 import _imp
 
 from test.support import os_helper
