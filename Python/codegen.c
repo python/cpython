098880de36756c25f9c2588d11c8847c4266c9cf
Miss Islington (bot) <31488909+miss-islington@users.noreply.github.com>
2026-04-29 16:01:45 +0200

    [3.14] gh-149122: Fix segfault in compiler when certain builtin functions are passed a coroutine as arg (GH-149138) (#149151)
    gh-149122: Fix segfault in compiler when certain builtin functions are passed a coroutine as arg (GH-149138)
    (cherry picked from commit 16f292ef4e8c56bfd115ecdb91420c7b4006249f)
    
    Co-authored-by: Irit Katriel <1055913+iritkatriel@users.noreply.github.com>

--- Python/codegen.c
+++ Python/codegen.c
@@ -3861,6 +3861,12 @@ maybe_optimize_function_call(compiler *c, expr_ty e, jump_target_label end)
         return 0;
     }
 
+    expr_ty generator_exp = asdl_seq_GET(args, 0);
+    PySTEntryObject *generator_entry = _PySymtable_Lookup(SYMTABLE(c), (void *)generator_exp);
+    if (generator_entry->ste_coroutine) {
+        return 0;
+    }
+
     location loc = LOC(func);
 
     int optimized = 0;
@@ -3892,7 +3898,6 @@ maybe_optimize_function_call(compiler *c, expr_ty e, jump_target_label end)
         if (const_oparg == CONSTANT_BUILTIN_TUPLE) {
             ADDOP_I(c, loc, BUILD_LIST, 0);
         }
-        expr_ty generator_exp = asdl_seq_GET(args, 0);
         VISIT(c, expr, generator_exp);
 
         NEW_JUMP_TARGET_LABEL(c, loop);
