082a57ef141313e270ba7856e2c50525ecd32a7a
Shamil Abdulaev <ashm.tech@proton.me>
2026-04-29 19:29:24 +0300

    gh-144712: Fix _Py_jit_entry initializer after merge

--- Python/ceval.c
+++ Python/ceval.c
@@ -1305,7 +1305,7 @@ early_exit:
 }
 #ifdef _Py_TIER2
 #ifdef _Py_JIT
-PyAPI_DATA(_PyJitEntryFuncPtr) _Py_jit_entry = _Py_LazyJitShim;
+PyAPI_DATA(_PyJitEntryFuncPtr) _Py_jit_entry = _PyJIT;
 #else
 PyAPI_DATA(_PyJitEntryFuncPtr) _Py_jit_entry = _PyTier2Interpreter;
 #endif
