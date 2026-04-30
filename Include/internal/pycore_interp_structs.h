1e90d8972ac554974029a5989f3ffe350eae1b11
Petr Viktorin <encukou@gmail.com>
2026-04-30 09:53:30 +0200

    Adjust comment

--- Include/internal/pycore_interp_structs.h
+++ Include/internal/pycore_interp_structs.h
@@ -530,7 +530,10 @@ struct _py_func_state {
 /****** type state *********/
 
 /* For now we hard-code this to a value for which we are confident
-   all the static builtin types will fit (for all builds). */
+   all the static builtin types will fit (for all builds).
+   If you add a new static type to the standard library, you may have to
+   update one of these numbers.
+   */
 #define _Py_NUM_MANAGED_PREINITIALIZED_TYPES 120
 #define _Py_MAX_MANAGED_STATIC_BUILTIN_TYPES \
     (_Py_NUM_MANAGED_PREINITIALIZED_TYPES + 83)
