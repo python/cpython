bc26874c80206e77f5c1d4fbbb1748249768d941
Serhiy Storchaka <storchaka@gmail.com>
2026-04-30 20:42:47 +0300

    Use possesive quantifier for optimization.

--- Lib/xml/utils.py
+++ Lib/xml/utils.py
@@ -21,5 +21,5 @@ def is_valid_name(name):
             '\u200C\u200D\u203F\u2040'
             '\u2070-\u218F\u2C00-\u2FEF\u3001-\uD7FF'
             '\uF900-\uFDCF\uFDF0-\uFFFD\U00010000-\U000EFFFF'
-        ']*',
+        ']*+',
         name) is not None
