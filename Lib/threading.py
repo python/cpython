fa733cfb5a3aaeac4ad0fb9cc4557b2d1bc08442
Raymond Hettinger <raymond.hettinger@gmail.com>
2026-05-01 09:33:22 -0500

    Adopt Peter's suggestion to not use a lazy import

--- Lib/threading.py
+++ Lib/threading.py
@@ -7,7 +7,7 @@ import _contextvars
 
 from time import monotonic as _time
 from _weakrefset import WeakSet
-lazy from functools import wraps
+from functools import wraps
 from itertools import count as _count
 try:
     from _collections import deque as _deque
