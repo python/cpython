e7233dce7a773683208c2a541fffd3e4058661a7
lighting9999 <lightingcloud999@outlook.com>
2026-05-02 09:41:04 +0800

    Change test

--- Lib/test/test_random.py
+++ Lib/test/test_random.py
@@ -1076,9 +1076,9 @@ class TestDistributions(unittest.TestCase):
             self.assertAlmostEqual(s2/(N-1), sigmasqrd, places=2,
                                    msg='%s%r' % (variate.__name__, args))
                                       
-    @unittest.mock.patch.object(random.Random, 'random', return_value=0.0)
-    def test_binomialvariate_log_zero(self, mock_random):
-        # Test for gh-149222: binomialvariate should not crash when random()
+    def test_binomialvariate_log_zero(self):
+    # gh-149222: Variety random() return 0.0 no input Error
+    with unittest.mock.patch.object(random.Random, 'random', return_value=0.0):
         result = random.binomialvariate(10, 0.5)
         self.assertIsInstance(result, int)
         self.assertIn(result, range(11))
