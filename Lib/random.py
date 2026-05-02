1ca8d09c94cf8be2ee16ea75075af09198211f51
lighting9999 <lightingcloud999@outlook.com>
2026-05-02 11:13:40 +0800

    Fix handling of log(0) case in random.py

--- Lib/random.py
+++ Lib/random.py
@@ -840,12 +840,10 @@ class Random(_random.Random):
                     y += _floor(_log2(random()) / c) + 1
                 except ValueError:
                     # Handle rare case of log(0.0)
-                    if c < 0:
-                        # When c < 0, log(0) mathematically gives +infinity, so y > n.
-                        y = n + 1
+                    continue
                 if y > n: 
                     return x 
-                    x += 1
+                x += 1
 
         # BTRS: Transformed rejection with squeeze method by Wolfgang Hörmann
         # https://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.47.8407&rep=rep1&type=pdf
