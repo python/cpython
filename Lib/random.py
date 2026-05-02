d9b69a5ba3d58e9998bc395788400827e229c50f
Raymond Hettinger <raymond.hettinger@gmail.com>
2026-05-01 23:32:06 -0500

    gh-149221: Handle corner case in binomialvariate

--- Lib/random.py
+++ Lib/random.py
@@ -836,7 +836,11 @@ class Random(_random.Random):
             if not c:
                 return x
             while True:
-                y += _floor(_log2(random()) / c) + 1
+                try:
+                    y += _floor(_log2(random()) / c) + 1
+                except ValueError:
+                    # Handle rare case of where random() gives 0.0
+                    continue
                 if y > n:
                     return x
                 x += 1
