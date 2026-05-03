6ed96f1459c1af5c2c18d14d91b3d931b3ae05c7
sobolevn <mail@sobolevn.me>
2026-05-03 12:34:42 +0300

    Ignore generated files

--- jit_unwind_info-aarch64-apple-darwin.h
+++
@@ -1,4 +0,0 @@
-// c304ed754b0b65e0196cd50024bcbed7c38e65943d48292f97d34e35ddca13a6
-// $ python3.14 ./Tools/jit/build.py aarch64-apple-darwin24.6.0 --output-dir . --pyconfig-dir . --cflags= --llvm-version= --llvm-tools-install-dir= --debug
-
-#define JIT_UNWIND_INFO_SUPPORTED 0
--- jit_unwind_info.h
+++
@@ -1,6 +0,0 @@
-// $ python3.14 ./Tools/jit/build.py aarch64-apple-darwin24.6.0 --output-dir . --pyconfig-dir . --cflags= --llvm-version= --llvm-tools-install-dir= --debug
-#if defined(__aarch64__) && defined(__APPLE__)
-#include "jit_unwind_info-aarch64-apple-darwin.h"
-#else
-#error "unexpected target"
-#endif
