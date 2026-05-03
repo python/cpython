eb12cbccb096024523ad10fd247d3b9db70393ec
Shankarraospatil <shankarrao0420@gmail.com>
2026-05-03 13:53:51 +0530

    Regenerate Argument Clinic output after docstring fix

--- Modules/_remote_debugging/module.c
+++ Modules/_remote_debugging/module.c
@@ -904,9 +904,11 @@ exit:
 }
 
 /*[clinic input]
-@permit_long_summary
+@permit_long_docstring_body
 @critical_section
 _remote_debugging.RemoteUnwinder.get_all_awaited_by
+
+Get awaited_by relationships for tasks in the remote process.
 
 Returns:
     A list of AwaitedInfo objects, where each object contains:
@@ -946,7 +948,7 @@ Example output:
 
 static PyObject *
 _remote_debugging_RemoteUnwinder_get_all_awaited_by_impl(RemoteUnwinderObject *self)
-/*[clinic end generated code: output=6a49cd345e8aec53 input=c22bfee0612e0b69]*/
+/*[clinic end generated code: output=6a49cd345e8aec53 input=64456ccc05d9bfd2]*/
 {
     if (ensure_async_debug_offsets(self) < 0) {
         return NULL;
