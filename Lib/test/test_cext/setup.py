57871e99f817323de3a624ce99d1ec74e4bce87e
Steve Dower <steve.dower@python.org>
2026-05-04 16:21:48 +0100

    Satisfy linter

--- Lib/test/test_cext/setup.py
+++ Lib/test/test_cext/setup.py
@@ -1,7 +1,6 @@
 # gh-91321: Build a basic C test extension to check that the Python C API is
 # compatible with C and does not emit C compiler warnings.
 import os
-import platform
 import shlex
 import sys
 import sysconfig
