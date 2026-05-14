98c587683fcc2882a296af135403e57e6fbededc
Carlo Bramini <30959007+carlo-bramini@users.noreply.github.com>
2026-05-14 17:48:33 +0200

    gh-149831: fix wrong DLL library name for CYGWIN.

--- Lib/ctypes/__init__.py
+++ Lib/ctypes/__init__.py
@@ -549,9 +549,13 @@ pydll = LibraryLoader(PyDLL)
 
 if _os.name == "nt":
     pythonapi = PyDLL("python dll", None, _sys.dllhandle)
-elif _sys.platform in ["android", "cygwin"]:
+elif _sys.platform in ["android"]:
     # These are Unix-like platforms which use a dynamically-linked libpython.
     pythonapi = PyDLL(_sysconfig.get_config_var("LDLIBRARY"))
+elif _sys.platform in ["cygwin"]:
+    # These platforms must load the DLL file from DLLLIBRARY
+    # and not the import library provided by LDLIBRARY.
+    pythonapi = PyDLL(_sysconfig.get_config_var("DLLLIBRARY"))
 else:
     pythonapi = PyDLL(None)
 
