758eb917d6ea1c62272cd57e2eae775931d9f164
Alexey Katsman <alex2007508@gmail.com>
2026-05-19 15:11:17 -0700

    [3.14] gh-149816: Fix UAF in Modules/_pickle.c (GH-150024)
    (cherry picked from commit d095ceb0f420a22353271a8adaf5a83433d018e5)
    
    Co-authored-by: Gregory P. Smith <68491+gpshead@users.noreply.github.com>
    Co-authored-by: Alexey Katsman <alex2007508@gmail.com>

--- Modules/_pickle.c
+++ Modules/_pickle.c
@@ -3092,7 +3092,7 @@ static int
 batch_list_exact(PickleState *state, PicklerObject *self, PyObject *obj)
 {
     PyObject *item = NULL;
-    Py_ssize_t this_batch, total;
+    Py_ssize_t this_batch, total, list_size;
 
     const char append_op = APPEND;
     const char appends_op = APPENDS;
@@ -3102,9 +3102,14 @@ batch_list_exact(PickleState *state, PicklerObject *self, PyObject *obj)
     assert(self->proto > 0);
     assert(PyList_CheckExact(obj));
 
-    if (PyList_GET_SIZE(obj) == 1) {
-        item = PyList_GET_ITEM(obj, 0);
-        Py_INCREF(item);
+    list_size = PyList_GET_SIZE(obj);
+
+    if (list_size == 1) {
+        item = PyList_GetItemRef(obj, 0);
+        if (item == NULL) {
+            _PyErr_FormatNote("when serializing %T item 0", obj);
+            return -1;
+        }
         int err = save(state, self, item, 0);
         Py_DECREF(item);
         if (err < 0) {
@@ -3123,8 +3128,11 @@ batch_list_exact(PickleState *state, PicklerObject *self, PyObject *obj)
         if (_Pickler_Write(self, &mark_op, 1) < 0)
             return -1;
         while (total < PyList_GET_SIZE(obj)) {
-            item = PyList_GET_ITEM(obj, total);
-            Py_INCREF(item);
+            item = PyList_GetItemRef(obj, total);
+            if (item == NULL) {
+                _PyErr_FormatNote("when serializing %T item %zd", obj, total);
+                return -1;
+            }
             int err = save(state, self, item, 0);
             Py_DECREF(item);
             if (err < 0) {
@@ -3137,8 +3145,14 @@ batch_list_exact(PickleState *state, PicklerObject *self, PyObject *obj)
         }
         if (_Pickler_Write(self, &appends_op, 1) < 0)
             return -1;
+        if (PyList_GET_SIZE(obj) != list_size) {
+            PyErr_Format(
+                PyExc_RuntimeError,
+                "list changed size during iteration");
+            return -1;
+        }
 
-    } while (total < PyList_GET_SIZE(obj));
+    } while (total < list_size);
 
     return 0;
 }
