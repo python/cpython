33770c758890a72b224ff996762c401c87b98cef
Stephen Rosen <sirosen@globus.org>
2026-05-24 21:21:41 -0500

    Fix typo in tuple.__class_getitem__ docstring

--- Objects/tupleobject.c
+++ Objects/tupleobject.c
@@ -959,7 +959,7 @@ PyDoc_STRVAR(tuple_class_getitem_doc,
 "Tuples are generic over the types of their contents.\n\
 The type arguments to a tuple should match the types and arity of the contents.\n\n\
 For example, use ``tuple[int, str]`` for a pair whose first element is an int and second element is a string.\n\n\
-Tuples also the form ``tuple[T, ...]`` to indicate an arbitrary length tuple of elements of type T.");
+Tuples also support the form ``tuple[T, ...]`` to indicate an arbitrary length tuple of elements of type T.");
 
 static PyMethodDef tuple_methods[] = {
     TUPLE___GETNEWARGS___METHODDEF
