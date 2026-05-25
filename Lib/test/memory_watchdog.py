7a72e31c5e73648a924292c3d7fe0485a0c44f90
Victor Stinner <vstinner@python.org>
2026-05-25 16:18:25 +0200

    gh-150114: Use get_process_memory_usage() in memory watchdog
    In practice, this change adds Windows and FreeBSD support to the
    memory watchdog.

--- Lib/test/memory_watchdog.py
+++ Lib/test/memory_watchdog.py
@@ -8,14 +8,35 @@ and print it out, until terminated."""
 import sys
 import time
 from test.support import get_pagesize
+from test.libregrtest.utils import get_process_memory_usage
 
 
-while True:
-    page_size = get_pagesize()
-    sys.stdin.seek(0)
-    statm = sys.stdin.read()
-    data = int(statm.split()[5])
-    sys.stdout.write(" ... process data size: {data:.1f}G\n"
-                     .format(data=data * page_size / (1024 ** 3)))
-    sys.stdout.flush()
-    time.sleep(1)
+ONE_GIB = (1024 ** 3)
+
+
+def watchdog(pid):
+    while True:
+        mem = get_process_memory_usage(pid)
+        if mem is None:
+            # get_process_memory_usage() is not supported on the platform,
+            # or something went wrong. Exit since the next call is likely to
+            # fail the same way.
+            return
+
+        sys.stdout.write(f" ... process data size: {mem / ONE_GIB:.1f} GiB\n")
+        sys.stdout.flush()
+        time.sleep(1)
+
+def main():
+    if len(sys.argv) != 2:
+        print("usage: python {sys.argv[0]) pid")
+        sys.exit(1)
+    pid = int(sys.argv[1])
+
+    try:
+        watchdog(pid)
+    except KeyboardInterrupt:
+        pass
+
+if __name__ == "__main__":
+    main()
