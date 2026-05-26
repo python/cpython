3f9623ee60c27541ed5a917bf7720fe84820ebe0
Russell Keith-Magee <russell@keith-magee.com>
2026-05-26 10:10:11 +0800

    Add another common pattern for iOS SSL failures

--- Lib/test/test_ssl.py
+++ Lib/test/test_ssl.py
@@ -5698,15 +5698,18 @@ class TestPreHandshakeClose(unittest.TestCase):
             return  # Expect the full test setup to always work on Linux.
         if (isinstance(err, (ConnectionResetError, ConnectionAbortedError)) or
             (isinstance(err, OSError) and err.errno == errno.EINVAL) or
-            re.search('wrong.version.number', str(getattr(err, "reason", "")), re.I) or
-            re.search('record.layer.failure', str(getattr(err, "reason", "")), re.I)
+            re.search(
+                r'wrong.version.number|record.layer.failure|http.request',
+                str(getattr(err, "reason", "")),
+                re.I
+            )
         ):
             # On Windows the TCP RST leads to a ConnectionResetError
             # (ECONNRESET) which Linux doesn't appear to surface to userspace.
             # If wrap_socket() winds up on the "if connected:" path and doing
             # the actual wrapping... we get an SSLError from OpenSSL. This is
             # typically WRONG_VERSION_NUMBER. The same happens on iOS, but
-            # RECORD_LAYER_FAILURE is the error.
+            # RECORD_LAYER_FAILURE or HTTP_REQUEST is the error.
             #
             # While appropriate, neither is the scenario we're specifically
             # trying to test. The way this test is written is known to work on
