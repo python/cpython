3c11fb60c19771bfaa41fffe4ca0104ed28b02fe
Sergey B Kirpichev <skirpichev@gmail.com>
2026-05-31 10:29:44 +0300

    [3.14] Correct frexp() docs for zero and non-finite numbers (GH-149753)
    0.5 <= abs(m) < 1 is only true for finite nonzero numbers
    (cherry picked from commit 5b5ffce05c211a5b0b74cd95eeb4c463614ee136)
    
    Co-authored-by: Sergey B Kirpichev <skirpichev@gmail.com>

--- Modules/clinic/mathmodule.c.h
+++ Modules/clinic/mathmodule.c.h
@@ -130,8 +130,9 @@ PyDoc_STRVAR(math_frexp__doc__,
 "\n"
 "Return the mantissa and exponent of x, as pair (m, e).\n"
 "\n"
-"m is a float and e is an int, such that x = m * 2.**e.\n"
-"If x is 0, m and e are both 0.  Else 0.5 <= abs(m) < 1.0.");
+"If x is a finite nonzero number, then m is a float with\n"
+"0.5 <= abs(m) < 1.0 and an integer e is such that\n"
+"x == m * 2**e exactly.  Else, return (x, 0).");
 
 #define MATH_FREXP_METHODDEF    \
     {"frexp", (PyCFunction)math_frexp, METH_O, math_frexp__doc__},
@@ -1110,4 +1111,4 @@ math_ulp(PyObject *module, PyObject *arg)
 exit:
     return return_value;
 }
-/*[clinic end generated code: output=28730b795b51b528 input=a9049054013a1b77]*/
+/*[clinic end generated code: output=18e16eef548c9c43 input=a9049054013a1b77]*/
