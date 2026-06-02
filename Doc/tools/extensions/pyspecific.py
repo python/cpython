ac84ce422c491b93a2d07f5d445145ec456f02b1
Hugo van Kemenade <1324225+hugovk@users.noreply.github.com>
2026-06-02 14:56:26 +0300

    Update SOURCE_URI for 3.15

--- Doc/tools/extensions/pyspecific.py
+++ Doc/tools/extensions/pyspecific.py
@@ -22,7 +22,7 @@ from sphinx.locale import _ as sphinx_gettext
 from sphinx.util.docutils import SphinxDirective
 
 # Used in conf.py and updated here by python/release-tools/run_release.py
-SOURCE_URI = 'https://github.com/python/cpython/tree/main/%s'
+SOURCE_URI = 'https://github.com/python/cpython/tree/3.15/%s'
 
 class PyAwaitableMixin(object):
     def handle_signature(self, sig, signode):
