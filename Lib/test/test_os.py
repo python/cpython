2d322c4641c5f9ed6778134391e7752730fe92b2
Miss Islington (bot) <31488909+miss-islington@users.noreply.github.com>
2026-06-08 19:20:57 +0200

    [3.13] gh-151019: Fix test_os on 32-bit FreeBSD (GH-151087) (#151095)
    gh-151019: Fix test_os on 32-bit FreeBSD (GH-151087)
    
    Remove references to server.handler_instance. This attribute has been
    removed in 2022 by commit 3ae975f1ac880c47d51cca6c9e305547bd365be7.
    (cherry picked from commit a9002349cbe3f2243fe53a9fcadd02318dd0caf9)
    
    Co-authored-by: Victor Stinner <vstinner@python.org>

--- Lib/test/test_os.py
+++ Lib/test/test_os.py
@@ -3876,7 +3876,6 @@ class TestSendfile(unittest.IsolatedAsyncioTestCase):
     @requires_headers_trailers
     @requires_32b
     async def test_headers_overflow_32bits(self):
-        self.server.handler_instance.accumulate = False
         with self.assertRaises(OSError) as cm:
             await self.async_sendfile(self.sockno, self.fileno, 0, 0,
                                       headers=[b"x" * 2**16] * 2**15)
@@ -3885,7 +3884,6 @@ class TestSendfile(unittest.IsolatedAsyncioTestCase):
     @requires_headers_trailers
     @requires_32b
     async def test_trailers_overflow_32bits(self):
-        self.server.handler_instance.accumulate = False
         with self.assertRaises(OSError) as cm:
             await self.async_sendfile(self.sockno, self.fileno, 0, 0,
                                       trailers=[b"x" * 2**16] * 2**15)
