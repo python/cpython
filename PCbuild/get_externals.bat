e25cc9e93ce7755dba7467ec7d9566b9140e648a
Miss Islington (bot) <31488909+miss-islington@users.noreply.github.com>
2026-06-09 20:56:57 +0200

    [3.15] gh-151163: Update Android, macOS installer, and Windows builds to SQLite 3.53.2 (GH-151204)
    (cherry picked from commit ab8ebe9034a85c441d1bb4208fa2d667fd38cc4d)
    
    Co-authored-by: Zachary Ware <zach@python.org>

--- PCbuild/get_externals.bat
+++ PCbuild/get_externals.bat
@@ -56,7 +56,7 @@ set libraries=%libraries%                                       bzip2-1.0.8
 if NOT "%IncludeLibffiSrc%"=="false" set libraries=%libraries%  libffi-3.4.4
 if NOT "%IncludeSSLSrc%"=="false" set libraries=%libraries%     openssl-3.5.7
 set libraries=%libraries%                                       mpdecimal-4.0.0
-set libraries=%libraries%                                       sqlite-3.53.1.0
+set libraries=%libraries%                                       sqlite-3.53.2.0
 if NOT "%IncludeTkinterSrc%"=="false" set libraries=%libraries% tcl-9.0.3.0
 if NOT "%IncludeTkinterSrc%"=="false" set libraries=%libraries% tk-9.0.3.1
 set libraries=%libraries%                                       xz-5.8.1.1
