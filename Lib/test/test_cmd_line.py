ded830267aad9c90b50ab3892b3fed482991d743
Victor Stinner <vstinner@python.org>
2026-06-10 15:47:46 +0200

    Add unit test

--- Lib/test/test_cmd_line.py
+++ Lib/test/test_cmd_line.py
@@ -1314,6 +1314,14 @@ class CmdLineTest(unittest.TestCase):
         proc = assert_python_ok("-X", f"presite={entrypoint}", "-c", "pass")
         self.assertEqual(proc.out.rstrip(), b"presite func")
 
+    def test_dump_path_config(self):
+        # gh-151253: At the first import (import encodings) during Python
+        # startup, if the import fails, dump the Python path configuration.
+        nonexistent = '/nonexistent-python-path'
+        proc = assert_python_failure("-c", "pass", PYTHONHOME=nonexistent)
+        self.assertIn(b'Python path configuration:', proc.err)
+        self.assertIn(f"PYTHONHOME = '{nonexistent}'".encode(), proc.err)
+
 
 @unittest.skipIf(interpreter_requires_environment(),
                  'Cannot run -I tests when PYTHON env vars are required.')
