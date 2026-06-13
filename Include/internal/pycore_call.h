8931a67d00c7e66cb98206245c6ab9ad6fe4f16f
Peter Bierma <zintensitydev@gmail.com>
2026-06-13 14:45:57 -0400

    Fix _PyObject_Call redefinition.

--- Include/internal/pycore_call.h
+++ Include/internal/pycore_call.h
@@ -179,10 +179,6 @@ PyAPI_FUNC(void) _PyStack_UnpackDict_FreeNoDecRef(
 PyAPI_FUNC(PyObject *)
 _PyObject_CallOneArgTstate(PyThreadState *tstate, PyObject *func, PyObject *arg);
 
-PyAPI_FUNC(PyObject *)
-_PyObject_Call(PyThreadState *tstate, PyObject *callable,
-               PyObject *args, PyObject *kwargs);
-
 #ifdef __cplusplus
 }
 #endif
