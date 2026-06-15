51f5834b260ba7873c41f8e2a3fa2c4e48838473
JasonMendoza2008 <56092290+JasonMendoza2008@users.noreply.github.com>
2026-06-15 11:31:04 +0200

    gh-137759: Replace _PyObject_HashFast() with PyObject_Hash() in setobject.c (#137828)
    Replace also _PyObject_HashFast() with PyObject_Hash()
    in _collections._count_elements().
    
    Rename _PyObject_HashFast() to _PyObject_HashDictKey(),
    and mark it as Py_ALWAYS_INLINE.
    
    Only use _PyObject_HashDictKey() on dictionaries.

--- Objects/typeobject.c
+++ Objects/typeobject.c
@@ -3793,7 +3793,7 @@ solid_base(PyTypeObject *type)
 // or when __bases__ is re-assigned.  Since the slots are read without atomic
 // operations and without locking, we can only safely update them while the
 // world is stopped.  However, with the world stopped, we are very limited on
-// which APIs can be safely used.  For example, calling _PyObject_HashFast()
+// which APIs can be safely used.  For example, calling _PyObject_HashDictKey()
 // or _PyDict_GetItemRef_KnownHash() are not safe and can potentially cause
 // deadlocks.  Hashing can be re-entrant and _PyDict_GetItemRef_KnownHash can
 // acquire a lock if the dictionary is not owned by the current thread, to
@@ -6134,7 +6134,7 @@ PyObject_GetItemData(PyObject *obj)
 static int
 find_name_in_mro(PyTypeObject *type, PyObject *name, _PyStackRef *out)
 {
-    Py_hash_t hash = _PyObject_HashFast(name);
+    Py_hash_t hash = _PyObject_HashDictKey(name);
     if (hash == -1) {
         PyErr_Clear();
         return -1;
