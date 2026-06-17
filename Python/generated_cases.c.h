3e36e429f3f52c7d1d364cccf0e134b43c99543f
Maurycy Pawłowski-Wieroński <maurycy@maurycy.com>
2026-06-17 22:49:23 +0200

    gh-151436: Fix missing `tstate->last_profiled_frame` updates (GH-151437) (cherry picked from commit a8d74c062fe3c5cb2962dde8bee83704fcfa1bc9)
    Co-authored-by: Maurycy Pawłowski-Wieroński <maurycy@maurycy.com>

--- Python/generated_cases.c.h
+++ Python/generated_cases.c.h
@@ -7938,6 +7938,7 @@
                 gen->gi_exc_state.previous_item = NULL;
                 _Py_LeaveRecursiveCallPy(tstate);
                 _PyInterpreterFrame *gen_frame = frame;
+                _PyThreadState_UpdateLastProfiledFrame(tstate, gen_frame, gen_frame->previous);
                 frame = tstate->current_frame = frame->previous;
                 gen_frame->previous = NULL;
                 ((_PyThreadStateImpl *)tstate)->generator_return_kind = GENERATOR_YIELD;
@@ -11019,6 +11020,7 @@
             gen_frame->owner = FRAME_OWNED_BY_GENERATOR;
             _Py_LeaveRecursiveCallPy(tstate);
             _PyInterpreterFrame *prev = frame->previous;
+            _PyThreadState_UpdateLastProfiledFrame(tstate, frame, prev);
             _PyThreadState_PopFrame(tstate, frame);
             frame = tstate->current_frame = prev;
             LOAD_IP(frame->return_offset);
@@ -13047,6 +13049,7 @@
                 gen->gi_exc_state.previous_item = NULL;
                 _Py_LeaveRecursiveCallPy(tstate);
                 _PyInterpreterFrame *gen_frame = frame;
+                _PyThreadState_UpdateLastProfiledFrame(tstate, gen_frame, gen_frame->previous);
                 frame = tstate->current_frame = frame->previous;
                 gen_frame->previous = NULL;
                 ((_PyThreadStateImpl *)tstate)->generator_return_kind = GENERATOR_YIELD;
