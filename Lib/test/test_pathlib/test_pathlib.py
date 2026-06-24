bc690c4b5fbf27bfd43232926cd088df97f503ce
Miss Islington (bot) <31488909+miss-islington@users.noreply.github.com>
2026-06-24 18:50:36 +0200

    [3.13] Re-raise unexpected exceptions instead of swallowing them in various tests (GH-152019) (#152104)
    (cherry picked from commit 6c3da17d1f333f6bd4c07d11e2e304159d550822)
    
    Co-authored-by: An Long <aisk@users.noreply.github.com>
    Co-authored-by: Stan Ulbrych <stan@python.org>

--- Lib/test/test_pathlib/test_pathlib.py
+++ Lib/test/test_pathlib/test_pathlib.py
@@ -1109,6 +1109,7 @@ class PathTest(test_pathlib_abc.DummyPathTest, PurePathTest):
             if (isinstance(e, PermissionError) or
                     "AF_UNIX path too long" in str(e)):
                 self.skipTest("cannot bind Unix socket: " + str(e))
+            raise
         self.assertTrue(P.is_socket())
         self.assertFalse(P.is_fifo())
         self.assertFalse(P.is_file())
