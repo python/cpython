a580029f1168cf87707b157865b6a6b89a77b7ad
Ivy Xu <fakeshadow1337@gmail.com>
2026-06-25 18:47:25 +0800

    gh-151126: Add missing `PyErr_NoMemory` in `_winapi.c` (#151588)
    Co-authored-by: sobolevn <mail@sobolevn.me>

--- Modules/_winapi.c
+++ Modules/_winapi.c
@@ -1684,6 +1684,9 @@ _winapi_GetShortPathName_impl(PyObject *module, LPCWSTR path)
             }
             PyMem_Free((void *)buffer);
         }
+        else {
+            PyErr_NoMemory();
+        }
     } else {
         PyErr_SetFromWindowsErr(0);
     }
@@ -2394,6 +2397,7 @@ _winapi_BatchedWaitForMultipleObjects_impl(PyObject *module,
     while (i < nhandles) {
         BatchedWaitData *data = (BatchedWaitData*)PyMem_Malloc(sizeof(BatchedWaitData));
         if (!data) {
+            PyErr_NoMemory();
             goto error;
         }
         thread_data[thread_count++] = data;
