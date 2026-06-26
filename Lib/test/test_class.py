5bdda220bad6fc0c44888c84a3960df0127e30f6
sobolevn <mail@sobolevn.me>
2026-06-26 17:15:05 +0300

    Fix CI

--- Lib/test/test_class.py
+++ Lib/test/test_class.py
@@ -1,6 +1,7 @@
 "Test the functionality of Python classes implementing operators."
 
 import unittest
+from test import support
 from test.support import cpython_only, import_helper, script_helper
 
 testmeths = [
