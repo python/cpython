593060fbc6a78240fbc147cc02dcb960910442e2
sobolevn <mail@sobolevn.me>
2026-06-26 16:51:43 +0300

    Update Lib/test/test_atexit.py

--- Lib/test/test_atexit.py
+++ Lib/test/test_atexit.py
@@ -100,5 +100,6 @@ class SubinterpreterTest(unittest.TestCase):
         self.assertEqual(os.read(r, len(expected)), expected)
         os.close(r)
 
+
 if __name__ == "__main__":
     unittest.main()
