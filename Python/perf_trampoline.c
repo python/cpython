db0feddb4ca115cceb8c0fce3d16b1536ff859a9
Yuka Sadaoka <yukasadaoka@google.com>
2026-06-26 19:06:39 +0000

    Fix formatting

--- Python/perf_trampoline.c
+++ Python/perf_trampoline.c
@@ -273,22 +273,22 @@ static void
 perf_map_write_entry(void *state, const void *code_addr,
                          size_t code_size, PyCodeObject *co)
 {
-  const char *entry = "";
-  if (co->co_qualname != NULL) {
-    entry = PyUnicode_AsUTF8(co->co_qualname);
-  }
-  const char *filename = "";
-  if (co->co_filename != NULL) {
-    filename = PyUnicode_AsUTF8(co->co_filename);
-  }
-  size_t perf_map_entry_size = snprintf(NULL, 0, "py::%s:%s", entry, filename) + 1;
-  char* perf_map_entry = (char*)PyMem_RawMalloc(perf_map_entry_size);
-  if (perf_map_entry == NULL) {
-    return;
-  }
-  snprintf(perf_map_entry, perf_map_entry_size, "py::%s:%s", entry, filename);
-  PyUnstable_WritePerfMapEntry(code_addr, code_size, perf_map_entry);
-  PyMem_RawFree(perf_map_entry);
+    const char *entry = "";
+    if (co->co_qualname != NULL) {
+        entry = PyUnicode_AsUTF8(co->co_qualname);
+    }
+    const char *filename = "";
+    if (co->co_filename != NULL) {
+        filename = PyUnicode_AsUTF8(co->co_filename);
+    }
+    size_t perf_map_entry_size = snprintf(NULL, 0, "py::%s:%s", entry, filename) + 1;
+    char* perf_map_entry = (char*)PyMem_RawMalloc(perf_map_entry_size);
+    if (perf_map_entry == NULL) {
+        return;
+    }
+    snprintf(perf_map_entry, perf_map_entry_size, "py::%s:%s", entry, filename);
+    PyUnstable_WritePerfMapEntry(code_addr, code_size, perf_map_entry);
+    PyMem_RawFree(perf_map_entry);
 }
 
 static void*
