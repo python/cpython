f158c0a5851aec739b2020405164b7312aa286c2
Miss Islington (bot) <31488909+miss-islington@users.noreply.github.com>
2026-06-27 17:24:50 +0200

    [3.13] gh-151126: Sets missing exceptions in `tkinter` and `socket` modules initializations (GH-152418) (#152422)
    gh-151126: Sets missing exceptions in `tkinter` and `socket` modules initializations (GH-152418)
    (cherry picked from commit a9fa8560143098168e0380386acbf4846c37472b)
    
    Co-authored-by: sobolevn <mail@sobolevn.me>

--- Modules/socketmodule.c
+++ Modules/socketmodule.c
@@ -8942,6 +8942,7 @@ socket_exec(PyObject *m)
 #if defined(USE_GETHOSTBYNAME_LOCK)
     netdb_lock = PyThread_allocate_lock();
     if (netdb_lock == NULL) {
+        PyErr_NoMemory();
         goto error;
     }
 #endif
