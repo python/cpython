955c6f79f826f674b263cab8fedae9aaf633b9b1
tonghuaroot <tonghuaroot@gmail.com>
2026-06-27 19:27:20 +0800

    Address review: simplify the year-directive check and reword the NEWS entry

--- Lib/_pydatetime.py
+++ Lib/_pydatetime.py
@@ -272,8 +272,8 @@ def _wrap_strftime(object, format, timetuple):
                     newformat.append(Zreplace)
                 # Note that datetime(1000, 1, 1).strftime('%G') == '1000' so
                 # year 1000 for %G can go on the fast path.
-                elif ((ch in 'YG' or ch in 'FC') and
-                        timetuple[0] < 1000 and _need_normalize_century()):
+                elif (ch in 'YGFC' and timetuple[0] < 1000 and
+                        _need_normalize_century()):
                     if ch == 'G':
                         year = int(_time.strftime("%G", timetuple))
                     else:
