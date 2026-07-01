1d4d0295a2133cd26e48419ced3a24c31bc166a3
Serhiy Storchaka <storchaka@gmail.com>
2026-07-01 10:21:49 +0300

    gh-83637: Make IDLE calltip tests pass without docstrings
    Skip the tests that check docstrings with test.support.requires_docstrings,
    as test_inspect and test_pydoc do.  They failed under -OO.

--- Lib/idlelib/idle_test/test_calltip.py
+++ Lib/idlelib/idle_test/test_calltip.py
@@ -7,7 +7,7 @@ import textwrap
 import types
 import re
 from idlelib.idle_test.mock_tk import Text
-from test.support import MISSING_C_DOCSTRINGS
+from test.support import MISSING_C_DOCSTRINGS, requires_docstrings
 
 
 # Test Class TC is used in multiple get_argspec test methods
@@ -51,8 +51,7 @@ class Get_argspecTest(unittest.TestCase):
     # but a red buildbot is better than a user crash (as has happened).
     # For a simple mismatch, change the expected output to the actual.
 
-    @unittest.skipIf(MISSING_C_DOCSTRINGS,
-                     "Signature information for builtins requires docstrings")
+    @requires_docstrings
     def test_builtins(self):
 
         def tiptest(obj, out):
@@ -112,6 +111,7 @@ subclasses to override in order to tweak the default behaviour.
 If you want to completely replace the main wrapping algorithm,
 you\'ll probably have to override _wrap_chunks().''')
 
+    @requires_docstrings
     def test_properly_formatted(self):
 
         def foo(s='a'*100):
@@ -186,17 +186,19 @@ bytes() -> empty bytes object''')
             with self.subTest(func=func):
                 self.assertEqual(get_spec(func), func.tip + doc)
 
+    @requires_docstrings
     def test_methods(self):
-        doc = '\ndoc' if TC.__doc__ is not None else ''
+        doc = '\ndoc'
         for meth in (TC.t1, TC.t2, TC.t3, TC.t4, TC.t5, TC.t6, TC.__call__):
             with self.subTest(meth=meth):
                 self.assertEqual(get_spec(meth), meth.tip + doc)
         self.assertEqual(get_spec(TC.cm), "(a)" + doc)
         self.assertEqual(get_spec(TC.sm), "(b)" + doc)
 
+    @requires_docstrings
     def test_bound_methods(self):
         # test that first parameter is correctly removed from argspec
-        doc = '\ndoc' if TC.__doc__ is not None else ''
+        doc = '\ndoc'
         for meth, mtip  in ((tc.t1, "()"), (tc.t4, "(*args)"),
                             (tc.t6, "(self)"), (tc.__call__, '(ci)'),
                             (tc, '(ci)'), (TC.cm, "(a)"),):
