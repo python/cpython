0c1563856189c4578d9ac3524d01ce12c5fdabfe
sobolevn <mail@sobolevn.me>
2026-07-07 19:09:02 +0300

    [3.13] gh-153210: Restore accidental `array.ArrayType` removal (GH-153261) (cherry picked from commit ac2e14b4e29b2d11a14d289969cfffc298ea75d9)
    Co-authored-by: sobolevn <mail@sobolevn.me>

--- Modules/arraymodule.c
+++ Modules/arraymodule.c
@@ -3262,7 +3262,13 @@ array_modexec(PyObject *m)
     CREATE_TYPE(m, state->ArrayIterType, &arrayiter_spec);
     Py_SET_TYPE(state->ArrayIterType, &PyType_Type);
 
-    PyObject *mutablesequence = _PyImport_GetModuleAttrString(
+    // Older undocumented alias:
+    if (PyModule_AddObjectRef(m, "ArrayType",
+                              (PyObject *)state->ArrayType) < 0) {
+        return -1;
+    }
+
+    PyObject *mutablesequence = _PyImport_ImportModuleAttrString(
             "collections.abc", "MutableSequence");
     if (!mutablesequence) {
         return -1;
