b8eaa3d59db650bf6970da2ca5da6e608229313a
Victor Stinner <vstinner@python.org>
2026-07-08 00:33:45 +0200

    gh-153300: Set global configuration variables in PyConfig_Set()
    PyConfig_Set() now also set global configuration variables. For
    example, PyConfig_Set("inspect", value) now also sets Py_InspectFlag.
    
    Use PyConfig_Set() in main.c to set the inspect flag. Python code can
    now see the updated sys.flags.inspect value.

--- Modules/main.c
+++ Modules/main.c
@@ -529,11 +529,14 @@ error:
 static void
 pymain_set_inspect(PyConfig *config, int inspect)
 {
-    config->inspect = inspect;
-_Py_COMP_DIAG_PUSH
-_Py_COMP_DIAG_IGNORE_DEPR_DECLS
-    Py_InspectFlag = inspect;
-_Py_COMP_DIAG_POP
+    PyObject *value = PyLong_FromLong(inspect);
+    if (value == NULL || PyConfig_Set("inspect", value) < 0) {
+        PyErr_Print();
+    }
+    else {
+        assert(config->inspect == inspect);
+    }
+    Py_XDECREF(value);
 }
 
 
@@ -635,7 +638,7 @@ pymain_run_python(int *exitcode)
 {
     PyObject *main_importer_path = NULL;
     PyInterpreterState *interp = _PyInterpreterState_GET();
-    /* pymain_run_stdin() modify the config */
+    /* pymain_repl() and pymain_run_stdin() modify the config */
     PyConfig *config = (PyConfig*)_PyInterpreterState_GetConfig(interp);
 
     /* ensure path config is written into global variables */
