a23ded48f5f66dc4b232b8f40d61cf26afbc6688
sobolevn <mail@sobolevn.me>
2026-07-08 01:03:47 +0300

    gh-153292: Fix data race in `threading.RLock.__repr__` in FT builds

---
+++ Lib/test/test_free_threading/test_threading.py
@@ -0,0 +1,29 @@
+import unittest
+from test.support import threading_helper
+
+threading_helper.requires_working_threading(module=True)
+
+
+class TestRlock(unittest.TestCase):
+    def test_repr_race(self):
+        # gh-153292
+        import _thread
+        r = _thread.RLock()
+
+        def repr_thread():
+            for _ in range(2000):
+                try:
+                    repr(r)
+                except Exception:
+                    pass
+
+        def mutate_thread():
+            for _ in range(2000):
+                r.acquire()
+                r.release()
+
+        threading_helper.run_concurrently([repr_thread, mutate_thread])
+
+
+if __name__ == "__main__":
+    unittest.main()
