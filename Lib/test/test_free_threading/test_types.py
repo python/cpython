ebd98bd8ed0dc7e689b84d76e673363b4bae1895
sobolevn <mail@sobolevn.me>
2026-07-08 13:23:43 +0300

    gh-153298: Fix data race in `GenericAlias` parameter initialization on FT

---
+++ Lib/test/test_free_threading/test_types.py
@@ -0,0 +1,34 @@
+import unittest
+import types
+from typing import TypeVar
+from test.support import threading_helper
+
+threading_helper.requires_working_threading(module=True)
+
+
+class TestGenericAlias(unittest.TestCase):
+    def test_repr_race(self):
+        # gh-153298
+
+        T = TypeVar('T')
+        slot = [list[T]]
+
+        def access():
+            for _ in range(2000):
+                try:
+                    _ = slot[0].__parameters__
+                except Exception:
+                    pass
+
+        def refresh():
+            for _ in range(2000):
+                slot[0] = list[T]
+
+        threading_helper.run_concurrently([
+            *[access for _ in range(6)],
+            *[refresh for _ in range(2)],
+        ])
+
+
+if __name__ == "__main__":
+    unittest.main()
