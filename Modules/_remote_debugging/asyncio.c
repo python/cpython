116da45f642473a69ab724456f3496536dd5e419
maurycy <5383+maurycy@users.noreply.github.com>
2026-07-08 21:37:25 +0200

    add it in parse_async_frame_chain()

--- Modules/_remote_debugging/asyncio.c
+++ Modules/_remote_debugging/asyncio.c
@@ -776,7 +776,13 @@ parse_async_frame_chain(
         return -1;
     }
 
+    size_t frame_count = 0;
     while ((void*)address_of_current_frame != NULL) {
+        if (++frame_count > MAX_FRAME_CHAIN_DEPTH) {
+            PyErr_SetString(PyExc_RuntimeError, "Too many stack frames (possible infinite loop)");
+            set_exception_cause(unwinder, PyExc_RuntimeError, "Async frame chain iteration limit exceeded");
+            return -1;
+        }
         PyObject* frame_info = NULL;
         uintptr_t address_of_code_object;
         int res = parse_frame_object(
