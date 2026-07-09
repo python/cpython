69f4d61233c34f3e1dba38b38f39cd02d7bea079
sobolevn <mail@sobolevn.me>
2026-07-09 19:08:29 +0300

    gh-153444: Fix compile warning in `Modules/_interpchannelsmodule.c` (GH-153445) (cherry picked from commit 33678dc79f00059aff09ac88f188ef2192031a85)
    Co-authored-by: sobolevn <mail@sobolevn.me>

--- Modules/_interpchannelsmodule.c
+++ Modules/_interpchannelsmodule.c
@@ -2903,7 +2903,8 @@ channelsmod_create(PyObject *self, PyObject *args, PyObject *kwds)
 
     int64_t cid = channel_create(&_globals.channels, unboundop);
     if (cid < 0) {
-        (void)handle_channel_error(cid, self, cid);
+        // Negative `cid` can't be too big for a downcast:
+        (void)handle_channel_error((int)cid, self, cid);
         return NULL;
     }
     module_state *state = get_module_state(self);
