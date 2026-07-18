9bdf823d4120231cc738a3965b5e552c25e6d1c9
Massimiliano Bruni <massimiliano.bruni@prima.it>
2026-07-18 12:26:16 +0200

    fix(typing): deduplicate unhashable Literal params

--- Lib/test/test_typing.py
+++ Lib/test/test_typing.py
@@ -2801,8 +2801,9 @@ class LiteralTests(BaseTestCase):
         self.assertEqual(Literal[1, 2, 3].__args__, (1, 2, 3))
         self.assertEqual(Literal[1, 2, 3, 3].__args__, (1, 2, 3))
         self.assertEqual(Literal[1, Literal[2], Literal[3, 4]].__args__, (1, 2, 3, 4))
-        # Mutable arguments will not be deduplicated
-        self.assertEqual(Literal[[], []].__args__, ([], []))
+        # Mutable arguments will be deduplicated
+        self.assertEqual(Literal[[], []].__args__, ([],))
+        self.assertEqual(Literal[{"a": 1}, {"a": 1}].__args__, ({"a": 1},))
 
     def test_flatten(self):
         l1 = Literal[Literal[1], Literal[2], Literal[3]]
