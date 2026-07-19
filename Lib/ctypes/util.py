4f6996119ce840510136b5dad1f473f6d8f2ff83
Victor Stinner <vstinner@python.org>
2026-07-19 02:59:40 +0200

    gh-104533: Fix @ctypes.util.struct with string annotations
    Fix @ctypes.util.struct on structures declared in a module which uses
    "from __future__ import annotations". The decorator now calls
    get_annotations() with eval_str=True.

--- Lib/ctypes/util.py
+++ Lib/ctypes/util.py
@@ -512,7 +512,8 @@ def _process_struct(decorated_class, /, *, align, layout, endian, pack):
         fields.extend(decorated_class._fields_)
         anonymous.extend(decorated_class._anonymous_)
 
-    for name, hint in annotationlib.get_annotations(decorated_class).items():
+    annotations = annotationlib.get_annotations(decorated_class, eval_str=True)
+    for name, hint in annotations.items():
         if get_origin(hint) is ClassVar:
             continue
 
