78836118de238fb43ed8efa2d31d068a8ec304c2
Brett Cannon <brett@python.org>
2026-07-22 15:43:15 -0700

    Add `lib-dynload` to get rid of a warning when launching the REPL

--- Platforms/WASI/_package.py
+++ Platforms/WASI/_package.py
@@ -327,7 +327,11 @@ def gather(context):
 
     _shared.log("📁", "lib/", spacing=indent * 2)
     _shared.log("📁", f"python{py_version}/", spacing=indent * 3)
-    _shared.log("📁", "lib-wasm", spacing=indent * 4)
+    _shared.log("📁", "lib-dynload/", spacing=indent * 4)
+    (base / lib_python(context) / "lib-dynload").mkdir(parents=True, exist_ok=True)
+    _shared.log("📄", "<empty>", spacing=indent * 5)
+
+    _shared.log("📁", "lib-wasm/", spacing=indent * 4)
     _shared.log("📄", f"python{py_d_version}.wasm", spacing=indent * 5)
     copy_files([wasm_file(context)], base)
     _shared.log("📄", "LICENSE.txt", spacing=indent * 4)
