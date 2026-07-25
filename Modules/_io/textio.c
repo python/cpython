0ddcffa5ffab9a87a329e3038384a648bfc4fc75
Ivy Xu <fakeshadow1337@gmail.com>
2026-07-26 00:30:17 +0800

    guard `skip_bytes` in `tell_impl` against overflow

--- Modules/_io/textio.c
+++ Modules/_io/textio.c
@@ -2933,7 +2933,13 @@ _io_TextIOWrapper_tell_impl(textio *self)
 
     /* Fast search for an acceptable start point, close to our
        current pos */
-    skip_bytes = (Py_ssize_t) (self->b2cratio * chars_to_skip);
+    double skip_bytes_d = self->b2cratio * (double) chars_to_skip;
+    if (!(skip_bytes_d >= 0) || skip_bytes_d >= (double) PY_SSIZE_T_MAX) {
+        PyErr_SetString(PyExc_OverflowError,
+                        "chars_to_skip are too large");
+        goto fail;
+    }
+    skip_bytes = (Py_ssize_t) skip_bytes_d;
     skip_back = 1;
     assert(skip_bytes <= PyBytes_GET_SIZE(next_input));
     input = PyBytes_AS_STRING(next_input);
