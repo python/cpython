506242706e43af85f841027b7bd7f022f8acd939
Sergey Miryanov <sergey.miryanov@gmail.com>
2026-07-25 08:31:06 +0500

    GH-153809: Untrack TaskObj from GC before calling unregister_task (#154563)

--- Modules/_asynciomodule.c
+++ Modules/_asynciomodule.c
@@ -2966,13 +2966,17 @@ TaskObj_dealloc(PyObject *self)
     if (PyObject_CallFinalizerFromDealloc(self) < 0) {
         return; // resurrected
     }
+    // Untrack the object before unregistering the task, since the
+    // latter can cause a stop-the-world pause, after which another
+    // thread might call the GC and reach the object while it is
+    // semi-deallocated but still tracked
+    PyObject_GC_UnTrack(self);
+
     // unregister the task after finalization so that
     // if the task gets resurrected, it remains registered
     unregister_task((TaskObj *)self);
 
     PyTypeObject *tp = Py_TYPE(self);
-    PyObject_GC_UnTrack(self);
-
     PyObject_ClearWeakRefs(self);
 
     (void)TaskObj_clear(self);
