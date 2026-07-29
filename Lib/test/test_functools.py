93902025daf80db830ede420e9b66b06500c5ce0
Sebastian Berg <sebastianb@nvidia.com>
2026-07-29 20:52:31 +0200

    Address Petr's review comments

--- Lib/test/test_functools.py
+++ Lib/test/test_functools.py
@@ -3723,7 +3723,7 @@ class TestCachedProperty(unittest.TestCase):
 
         with self.assertRaisesRegex(
             TypeError,
-            "The '__dict__' attribute on 'MyMeta' instance does not support item assignment for caching 'prop' property.",
+            "The '__dict__' attribute on 'MyMeta' instance does not support 'setdefault' for caching 'prop' property.",
         ):
             MyClass.prop
 
