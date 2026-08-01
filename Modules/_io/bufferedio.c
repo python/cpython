dff7edea4dab7ba2c858d6ab564660b55996731f
Bhuvansh Kataria <bhuvanshkataria@gmail.com>
2026-08-01 10:15:01 +0000

    gh-154997: Declare raw_access_safe before use

--- Modules/_io/bufferedio.c
+++ Modules/_io/bufferedio.c
@@ -756,7 +756,11 @@ _io__Buffered_isatty_impl(buffered *self)
 
 /* Forward decls */
 static PyObject *
+raw_access_safe(buffered *self);
+
+static PyObject *
 _bufferedwriter_flush_unlocked(buffered *);
+
 static Py_ssize_t
 _bufferedreader_fill_buffer(buffered *self);
 static void
