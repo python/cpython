0a61d7f07c0f2ce19deea5ce99fcea016263e573
Victor Stinner <vstinner@python.org>
2026-08-08 00:37:30 +0200

    Fix test_profiling

--- Lib/test/test_profiling/test_sampling_profiler/_live_collector_helpers.py
+++ Lib/test/test_profiling/test_sampling_profiler/_live_collector_helpers.py
@@ -9,7 +9,9 @@ from profiling.sampling.constants import (
 
 
 # Matches the C structseq LocationInfo from _remote_debugging
-LocationInfo = namedtuple('LocationInfo', ['lineno', 'end_lineno', 'col_offset', 'end_col_offset'])
+LocationInfo = namedtuple('LocationInfo',
+    ['lineno', 'end_lineno', 'col_offset', 'end_col_offset'],
+    deprecate_tuple_api=False)
 
 
 class MockFrameInfo:
