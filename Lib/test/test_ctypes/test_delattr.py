116badf68bec25a2c5a27030311e1e52c92a441c
Serhiy Storchaka <storchaka@gmail.com>
2026-08-20 18:35:58 +0300

    Move the test for deleting Pointer.contents to the PR which fixes the crash

--- Lib/test/test_ctypes/test_delattr.py
+++ Lib/test/test_ctypes/test_delattr.py
@@ -1,5 +1,5 @@
 import unittest
-from ctypes import CDLL, POINTER, Structure, c_char, c_int
+from ctypes import CDLL, Structure, c_char, c_int
 from test.support import import_helper
 
 
@@ -26,11 +26,6 @@ class TestCase(unittest.TestCase):
         chararray = (c_char * 5)()
         with self.assertRaises(AttributeError):
             del chararray.raw
-
-    def test_pointer_contents(self):
-        ptr = POINTER(c_int)(c_int(42))
-        with self.assertRaises(TypeError):
-            del ptr.contents
 
     def test_func_pointer(self):
         # Deleting these attributes restores the default.
