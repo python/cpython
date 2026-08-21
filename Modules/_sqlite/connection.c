c1cd3d0d2df192bdb9e3c51e99a1e767a6155ce6
Serhiy Storchaka <storchaka@gmail.com>
2026-08-21 10:04:25 +0300

    gh-156100: Fix crashes in the sqlite3 Connection.autocommit setter (GH-156104)
    Deleting the attribute crashed, and setting it to an integer which does not
    fit in C long reported success with OverflowError set.
    
    (cherry picked from commit daebcac92d422d7fe9611cbe89044af23ea857c3)

--- Modules/_sqlite/connection.c
+++ Modules/_sqlite/connection.c
@@ -105,11 +105,16 @@ autocommit_converter(PyObject *val, enum autocommit_mode *result)
         *result = AUTOCOMMIT_DISABLED;
         return 1;
     }
-    if (PyLong_Check(val) &&
-        PyLong_AsLong(val) == LEGACY_TRANSACTION_CONTROL)
-    {
-        *result = AUTOCOMMIT_LEGACY;
-        return 1;
+    if (PyLong_Check(val)) {
+        int overflow;
+        long value = PyLong_AsLongAndOverflow(val, &overflow);
+        if (value == -1 && PyErr_Occurred()) {
+            return 0;
+        }
+        if (!overflow && value == LEGACY_TRANSACTION_CONTROL) {
+            *result = AUTOCOMMIT_LEGACY;
+            return 1;
+        }
     }
 
     PyErr_SetString(PyExc_ValueError,
@@ -2633,6 +2638,11 @@ get_autocommit(pysqlite_Connection *self, void *Py_UNUSED(ctx))
 static int
 set_autocommit(pysqlite_Connection *self, PyObject *val, void *Py_UNUSED(ctx))
 {
+    if (val == NULL) {
+        PyErr_SetString(PyExc_AttributeError,
+                        "cannot delete autocommit attribute");
+        return -1;
+    }
     if (!pysqlite_check_thread(self) || !pysqlite_check_connection(self)) {
         return -1;
     }
