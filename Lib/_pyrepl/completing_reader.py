d63a67bf513995b21a99a770dc4dd64316f2d8df
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2026-08-25 06:19:04 -0700

    more

--- Lib/_pyrepl/completing_reader.py
+++ Lib/_pyrepl/completing_reader.py
@@ -18,8 +18,6 @@
 # CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
 # CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
-from __future__ import annotations
-
 from dataclasses import dataclass, field
 
 import re
