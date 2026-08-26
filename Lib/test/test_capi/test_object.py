49d1779489465dd86783da28703774596dd65202
Donghee Na <donghee.na@python.org>
2026-08-27 00:03:47 +0900

    Improve speed at TSAN

--- Lib/test/test_capi/test_object.py
+++ Lib/test/test_capi/test_object.py
@@ -347,10 +347,10 @@ class RefTracerTest(unittest.TestCase):
     @support.skip_wasi_stack_overflow()
     def test_destroy_traced_for_trashcan_deferred_objects(self):
         depth = 200_000
-        chain = None
-        for _ in range(depth):
-            chain = [chain]
         with support.disable_gc():
+            chain = None
+            for _ in range(depth):
+                chain = [chain]
             _testcapi.start_counting_list_destroys()
             del chain
             destroys = _testcapi.stop_counting_list_destroys()
