bfd41e33fd299aa04f6202db1db535ee44cbf540
Serhiy Storchaka <storchaka@gmail.com>
2026-08-30 11:14:37 +0300

    [3.14] gh-84419: Fix the execute permissions in os.stat() on Windows (GH-155392) (GH-156630)
    Windows strips trailing dots and spaces from the last component of the path,
    so os.stat('spam.bat ') opened the same file as os.stat('spam.bat'), but did
    not set the execute permissions in st_mode, because the extension did not
    match.  They are now ignored, unless the \\?\ prefix disables the path
    normalization.
    
    (cherry picked from commit b7daede668ad42d11c28986f147eb6b2f68ef926)

--- Lib/test/test_os.py
+++ Lib/test/test_os.py
@@ -3614,6 +3614,45 @@ class Win32DeviceEncodingTests(unittest.TestCase):
             self.assertIsNone(encoding)
 
 
+@unittest.skipUnless(sys.platform == "win32", "Win32 specific tests")
+class Win32StatExecutableTests(unittest.TestCase):
+    # gh-84419: Windows strips trailing dots and spaces from the last
+    # component of the path, so they should be ignored when guessing
+    # the execute permissions from the file extension.
+
+    SUFFIXES = ['', ' ', '   ', '.', '..', ' . .']
+
+    def check(self, ext, mask):
+        filename = os_helper.TESTFN + ext
+        create_file(filename)
+        try:
+            for suffix in self.SUFFIXES:
+                with self.subTest(suffix=suffix):
+                    mode = os.stat(filename + suffix).st_mode
+                    self.assertEqual(mode & 0o111, mask)
+        finally:
+            os_helper.unlink(filename)
+
+    def test_executable_extension(self):
+        for ext in '.exe', '.bat', '.cmd', '.com', '.EXE', '.Bat':
+            with self.subTest(ext=ext):
+                self.check(ext, 0o111)
+
+    def test_not_executable_extension(self):
+        for ext in '.txt', '.py', '.exe.txt', '':
+            with self.subTest(ext=ext):
+                self.check(ext, 0)
+
+    def test_extended_path(self):
+        # The \\?\ prefix disables normalization: trailing spaces and dots
+        # are part of the file name.
+        filename = os.path.abspath(os_helper.TESTFN + '.exe')
+        create_file(filename)
+        self.addCleanup(os_helper.unlink, filename)
+        self.assertEqual(os.stat('\\\\?\\' + filename).st_mode & 0o111, 0o111)
+        self.assertRaises(OSError, os.stat, '\\\\?\\' + filename + ' ')
+
+
 @support.requires_subprocess()
 class PidTests(unittest.TestCase):
     @unittest.skipUnless(hasattr(os, 'getppid'), "test needs os.getppid")
