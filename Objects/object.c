3dfa5745f47370e86f075d356da1f4519e3077ac
Donghee Na <donghee.na@python.org>
2026-09-02 02:28:48 +0900

    gh-156371: Fix missing PyRefTracer_DESTROY events for trashcan objects (gh-156372)

--- Objects/object.c
+++ Objects/object.c
@@ -3228,6 +3228,10 @@ _PyTrash_thread_destroy_chain(PyThreadState *tstate)
          * up distorting allocation statistics.
          */
         _PyObject_ASSERT(op, Py_REFCNT(op) == 0);
+#ifdef Py_TRACE_REFS
+        _Py_ForgetReference(op);
+#endif
+        _PyReftracerTrack(op, PyRefTracer_DESTROY);
         (*dealloc)(op);
     }
 }
