b0d3b343ebc14d938ba6145579d177771c3a9a85
lipengyu <lipengyu@kylinos.cn>
2026-09-06 23:10:56 +0800

    update

--- Lib/asyncio/proactor_events.py
+++ Lib/asyncio/proactor_events.py
@@ -766,9 +766,8 @@ class BaseProactorEventLoop(base_events.BaseEventLoop):
     async def _sendfile_native(self, transp, file, offset, count):
         resume_reading = transp.is_reading()
         transp.pause_reading()
-        empty_waiter = transp._make_empty_waiter()
         try:
-            await empty_waiter
+            await transp._make_empty_waiter()
             return await self.sock_sendfile(transp._sock, file, offset, count,
                                             fallback=False)
         finally:
--- Lib/asyncio/selector_events.py
+++ Lib/asyncio/selector_events.py
@@ -739,9 +739,8 @@ class BaseSelectorEventLoop(base_events.BaseEventLoop):
         del self._transports[transp._sock_fd]
         resume_reading = transp.is_reading()
         transp.pause_reading()
-        empty_waiter = transp._make_empty_waiter()
         try:
-            await empty_waiter
+            await transp._make_empty_waiter()
             return await self.sock_sendfile(transp._sock, file, offset, count,
                                             fallback=False)
         finally:
