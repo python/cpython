ee6ae6f576d6c225428c9b1ad5733770779e1316
Victor Stinner <vstinner@python.org>
2026-09-08 10:11:32 -0400

    gh-121617: Fix Py_CLEAR() in C++: replace NULL with _Py_NULL (#157067)
    * Enhance Py_CLEAR() test in test_cext and test_cppext.
      Check that Py_CLEAR(obj) sets obj to NULL.
    * Add also tests on Py_SETREF() and Py_BEGIN_CRITICAL_SECTION().
    
    (cherry picked from commit 36c7440c480da622735788af656c728af28cd9eb)

--- Lib/test/test_cext/extension.c
+++ Lib/test/test_cext/extension.c
@@ -79,7 +79,7 @@ static PyMethodDef _testcext_methods[] = {
 static int
 _testcext_exec(PyObject *module)
 {
-    PyObject *result;
+    PyObject *result, *obj;
 
 #ifdef __STDC_VERSION__
     if (PyModule_AddIntMacro(module, __STDC_VERSION__) < 0) {
@@ -94,6 +94,22 @@ _testcext_exec(PyObject *module)
     // test Py_BUILD_ASSERT() and Py_BUILD_ASSERT_EXPR()
     Py_BUILD_ASSERT(sizeof(int) == sizeof(unsigned int));
     assert(Py_BUILD_ASSERT_EXPR(sizeof(int) == sizeof(unsigned int)) == 0);
+
+    // Test Py_CLEAR(): use typeof()/__typeof__() if available, or memcpy()
+    obj = Py_None;
+    Py_CLEAR(obj);
+    assert(obj == NULL);
+
+#ifndef Py_LIMITED_API
+    // Test Py_SETREF(): use typeof()/__typeof__() if available, or memcpy()
+    obj = Py_None;
+    Py_SETREF(obj, NULL);
+    assert(obj == NULL);
+
+    // Test that Py_BEGIN_CRITICAL_SECTION is available
+    Py_BEGIN_CRITICAL_SECTION(module);
+    Py_END_CRITICAL_SECTION();
+#endif
 
     return 0;
 }
