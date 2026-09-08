2f58bd3a3af01b480bfdb6f8def6ff041c0df77a
Victor Stinner <vstinner@python.org>
2026-09-08 16:43:40 +0200

    gh-121617: Fix Py_CLEAR() memcpy in C++: replace NULL with _Py_NULL

--- Include/refcount.h
+++ Include/refcount.h
@@ -494,7 +494,7 @@ static inline Py_ALWAYS_INLINE void Py_DECREF(PyObject *op)
     do { \
         PyObject **_tmp_op_ptr = _Py_CAST(PyObject**, &(op)); \
         PyObject *_tmp_old_op = (*_tmp_op_ptr); \
-        if (_tmp_old_op != NULL) { \
+        if (_tmp_old_op != _Py_NULL) { \
             PyObject *_null_ptr = _Py_NULL; \
             memcpy(_tmp_op_ptr, &_null_ptr, sizeof(PyObject*)); \
             Py_DECREF(_tmp_old_op); \
