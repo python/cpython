9abc7defc05fb0de6bb509c136e43eacec740e00
Victor Stinner <vstinner@python.org>
2026-09-08 10:11:32 -0400

    gh-121617: Fix Py_CLEAR() in C++: replace NULL with _Py_NULL (GH-157067)
    * Enhance Py_CLEAR() test in test_cext and test_cppext.
      Check that Py_CLEAR(obj) sets obj to NULL.
    * Add also tests on Py_SETREF() and Py_BEGIN_CRITICAL_SECTION().
    (cherry picked from commit 36c7440c480da622735788af656c728af28cd9eb)
    
    Co-authored-by: Victor Stinner <vstinner@python.org>

--- Include/refcount.h
+++ Include/refcount.h
@@ -484,7 +484,7 @@ static inline Py_ALWAYS_INLINE void Py_DECREF(PyObject *op)
     do { \
         _Py_TYPEOF(op)* _tmp_op_ptr = &(op); \
         _Py_TYPEOF(op) _tmp_old_op = (*_tmp_op_ptr); \
-        if (_tmp_old_op != NULL) { \
+        if (_tmp_old_op != _Py_NULL) { \
             *_tmp_op_ptr = _Py_NULL; \
             Py_DECREF(_tmp_old_op); \
         } \
