8b2dd805a79a7c35a4895d38634b080e2815b285
Ned Batchelder <ned@nedbatchelder.com>
2026-09-13 13:02:07 -0400

    [3.13] Docs: split builtins to their own page from library (GH-156682) (#157440)
    * [3.13] Docs: split builtins to their own page from library (GH-156682)
    
    * Docs: split builtins to their own page from library
    
    * review feedback
    
    * addressed Hugo's feedback
    
    * update the What's Next page
    
    * one more wording tweak
    
    * move builtins to their own directory. fix the reference name
    
    * moved pages need to be noted in tools/removed-ids.txt
    
    * add Python to the builtins reference title
    
    * add sphinxext-rediraffe for the library->builtins split
    
    * update check-html-ids to handle rediraffe redirects
    
    * now we don't need (page missing) for the redirected pages
    
    * update other references to moved pages
    
    * A seealso from builtins to library
    
    * make a nice section for rediraffe settings
    
    * move the builtins note to the end, as a seealso
    
    * address merwok's comments
    
    * cache looking for ids in files
    
    * simplify the intro paragraphs
    (cherry picked from commit 59c4bddb1762b4706c942d6403fb8df470f37e05)
    
    Co-authored-by: Ned Batchelder <ned@nedbatchelder.com>
    
    * remove references to non-existent pages

--- Tools/unicode/makeunicodedata.py
+++ Tools/unicode/makeunicodedata.py
@@ -42,7 +42,7 @@ VERSION = "3.3"
 # The Unicode Database
 # --------------------
 # When changing UCD version please update
-#   * Doc/library/stdtypes.rst, and
+#   * Doc/builtins/stdtypes.rst (four occurrences)
 #   * Doc/library/unicodedata.rst
 #   * Doc/reference/lexical_analysis.rst (two occurrences)
 UNIDATA_VERSION = "15.1.0"
