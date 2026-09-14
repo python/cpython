02d4c3b4347da57ba086f2ab7cdf6c0f56c9deeb
Miss Islington (bot) <31488909+miss-islington@users.noreply.github.com>
2026-09-14 08:09:26 -0700

    [3.14] gh-157378: Fix SyntaxError.offset for "Non-UTF-8 code" error (GH-157412) (#157489)
    gh-157378: Fix SyntaxError.offset for "Non-UTF-8 code" error (GH-157412)
    (cherry picked from commit e66bec09b722b8955d34f51f2c4b63793d59f9cf)
    
    Co-authored-by: Shamil <ashm.tech@proton.me>

--- Parser/tokenizer/helpers.c
+++ Parser/tokenizer/helpers.c
@@ -578,17 +578,14 @@ _PyTokenizer_ensure_utf8(const char *line, struct tok_state *tok, int lineno)
     const char *badchar = NULL;
     const char *c;
     int length;
-    int col_offset = 0;
     const char *line_start = line;
     for (c = line; *c; c += length) {
         if (!(length = valid_utf8((const unsigned char *)c))) {
             badchar = c;
             break;
         }
-        col_offset++;
         if (*c == '\n') {
             lineno++;
-            col_offset = 0;
             line_start = c + 1;
         }
     }
@@ -597,7 +594,8 @@ _PyTokenizer_ensure_utf8(const char *line, struct tok_state *tok, int lineno)
         tok->line_start = line_start;
         tok->cur = (char *)badchar;
         _PyTokenizer_syntaxerror_known_range(tok,
-                col_offset + 1, col_offset + 1,
+                (int)(badchar - line_start) + 1,
+                (int)(badchar - line_start) + 1,
                 "Non-UTF-8 code starting with '\\x%.2x'"
                 "%s%V on line %i, "
                 "but no encoding declared; "
