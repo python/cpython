21764e43ef30e4b9d45ed506deff92f0739456e5
Victor Stinner <vstinner@python.org>
2026-09-14 15:45:19 +0200

    gh-157495: Implement Py_MIN() and Py_MAX() using typeof()
    If typeof() is available, implement Py_MIN(), Py_MAX() and Py_ABS()
    using typeof() and statement expression to only evaluate each
    argument once.
    
    Test also these macros in test_cext and test_cppext to test different
    compiler flags and test the limited C API.

--- Include/pymacro.h
+++ Include/pymacro.h
@@ -107,15 +107,35 @@
 #   endif
 #endif
 
+#if defined(_Py_TYPEOF) && !defined(__cplusplus)
+   // Implement Py_MIN(), Py_MAX() and Py_ABS() using _Py_TYPEOF() and
+   // statement expression to only evaluate each argument only once.
+   // It cannot be used in C++: ISO C++ forbids braced-groups within
+   // expressions.
 
-/* Minimum value between x and y */
-#define Py_MIN(x, y) (((x) > (y)) ? (y) : (x))
+   /* Minimum value between x and y */
+#  define Py_MIN(x, y) \
+       ({ _Py_TYPEOF (x) _x = (x); \
+          _Py_TYPEOF (y) _y = (y); \
+          _x < _y ? _x : _y; })
+   /* Maximum value between x and y */
+#  define Py_MAX(x, y) \
+       ({ _Py_TYPEOF (x) _x = (x); \
+          _Py_TYPEOF (y) _y = (y); \
+          _x > _y ? _x : _y; })
+   /* Absolute value of the number x */
+#  define Py_ABS(x) \
+       ({ _Py_TYPEOF (x) _x = (x); \
+          _x < 0 ? -_x : _x; })
+#else
+   /* Minimum value between x and y */
+#  define Py_MIN(x, y) (((x) > (y)) ? (y) : (x))
+   /* Maximum value between x and y */
+#  define Py_MAX(x, y) (((x) > (y)) ? (x) : (y))
+   /* Absolute value of the number x */
+#  define Py_ABS(x) ((x) < 0 ? -(x) : (x))
+#endif
 
-/* Maximum value between x and y */
-#define Py_MAX(x, y) (((x) > (y)) ? (x) : (y))
-
-/* Absolute value of the number x */
-#define Py_ABS(x) ((x) < 0 ? -(x) : (x))
 /* Safer implementation that avoids an undefined behavior for the minimal
    value of the signed integer type if its absolute value is larger than
    the maximal value of the signed integer type (in the two's complement
