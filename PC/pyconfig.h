2e98b11167905f401e3b703339debe14e0f8b221
GruffElixir <136691411+gruffelixir@users.noreply.github.com>
2026-09-16 19:38:27 +0530

    gh-157611: Fix abi3t debug auto-linking on Windows

--- PC/pyconfig.h
+++ PC/pyconfig.h
@@ -336,7 +336,7 @@ Py_NO_ENABLE_SHARED to find out.  Also support MS_NO_COREDLL for b/w compat */
     && defined(_MSC_VER) && defined(Py_ENABLE_SHARED) \
     && !defined(Py_BUILD_CORE) && !defined(Py_BUILD_CORE_BUILTIN)
         /* not building the core - must be an ext */
-#       if defined(Py_GIL_DISABLED)
+#       if defined(Py_GIL_DISABLED) || defined(Py_TARGET_ABI3T)
 #           if defined(Py_DEBUG)
 #               pragma comment(lib,"python316t_d.lib")
 #           elif defined(Py_LIMITED_API) || defined(Py_TARGET_ABI3T)
