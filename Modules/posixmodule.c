ec7d6ed4fcc364f8b3ddb320d27218d4a4fb0883
search1ofall-maker <search1ofall@gmail.com>
2026-09-18 23:45:17 +0500

    Fix c-analyzer failure by declaring empty_environ as const

--- Modules/posixmodule.c
+++ Modules/posixmodule.c
@@ -13895,7 +13895,7 @@ os_unsetenv_impl(PyObject *module, PyObject *name)
 
 
 #ifdef HAVE_CLEARENV
-static char *empty_environ[] = { NULL };
+static char * const empty_environ[] = { NULL };
 
 /*[clinic input]
 os._clearenv
@@ -13918,7 +13918,7 @@ os__clearenv_impl(PyObject *module)
     /* glibc's clearenv() sets 'environ' to NULL. Point it to a static empty
        array to prevent crashes in third-party C libraries (e.g. Tcl/Tk)
        that access 'environ' without a NULL check. */
-    environ = empty_environ;
+    environ = (char **)empty_environ;
 
     Py_RETURN_NONE;
 }
