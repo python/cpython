a92e0f9b9fb2569776e9c8451dddeea589423de9
Victor Stinner <vstinner@python.org>
2026-09-19 19:53:35 +0200

    gh-157242: Use support.inject_memory_error_cm() in tests (#157811)
    Rename support.memory_error_cm() to support.inject_memory_error_cm().

--- Lib/test/test_str.py
+++ Lib/test/test_str.py
@@ -614,7 +614,7 @@ class StrTest(string_tests.StringLikeTest,
         s2 = "&"
         s3 = "&amp;"
         with self.assertRaises(MemoryError):
-            with support.memory_error_cm():
+            with support.inject_memory_error_cm():
                 s1.replace(s2, s3)  # this line used to crash before
 
     def test_repeat_id_preserving(self):
