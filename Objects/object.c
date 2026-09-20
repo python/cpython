086cb91a043bf3ab89d02ddeeaf166418aa7aac6
Peter Bierma <zintensitydev@gmail.com>
2026-09-20 11:53:45 -0400

    Improve performance for hasattr() with unassigned slots.

--- Objects/object.c
+++ Objects/object.c
@@ -1924,6 +1924,22 @@ _PyObject_GenericGetAttrWithDict(PyObject *obj, PyObject *name,
     if (descr != NULL) {
         f = Py_TYPE(descr)->tp_descr_get;
         if (f != NULL && PyDescr_IsData(descr)) {
+            // gh-157840: When using hasattr(), slot members without assigned values
+            // need to go through __get__(), which involves creating an AttributeError
+            // object (which is really slow). There's no way to supply "suppress"
+            // to the tp_descr_get() function, so we just special-case member
+            // descriptors here to improve performance.
+            if (suppress && Py_IS_TYPE(descr, &PyMemberDescr_Type)) {
+                PyMemberDef *member = ((PyMemberDescrObject *)descr)->d_member;
+                if (member->type == Py_T_OBJECT_EX
+                    && !(member->flags & Py_AUDIT_READ) &&
+                    PyObject_TypeCheck(obj, PyDescr_TYPE(descr))) {
+                    PyObject **addr = _PyMember_GetOffset(obj, member);
+                    if (FT_ATOMIC_LOAD_PTR(*addr) == NULL) {
+                        goto done;
+                    }
+                }
+            }
             res = f(descr, obj, (PyObject *)Py_TYPE(obj));
             if (res == NULL && suppress &&
                     PyErr_ExceptionMatches(PyExc_AttributeError)) {
