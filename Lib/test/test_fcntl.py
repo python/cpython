4137842b56c571aec89fac41d42652beafb7182b
Vasiliy Kiryanov <vasiliy.kiryanov@gmail.com>
2026-09-22 09:13:22 -0400

    gh-157949: Remove dead UnixWare, OpenServer and ReliantUNIX support code

--- Lib/test/test_fcntl.py
+++ Lib/test/test_fcntl.py
@@ -73,7 +73,7 @@ class TestFcntl(unittest.TestCase):
                                    fcntl.F_WRLCK, 0)
         elif sys.platform.startswith('gnukfreebsd'):
             lockdata = struct.pack('qqihhi', 0, 0, 0, fcntl.F_WRLCK, 0, 0)
-        elif sys.platform in ['hp-uxB', 'unixware7']:
+        elif sys.platform == 'hp-uxB':
             lockdata = struct.pack('hhlllii', fcntl.F_WRLCK, 0, 0, 0, 0, 0, 0)
         else:
             lockdata = struct.pack('hh'+start_len+'hh', fcntl.F_WRLCK, 0, 0, 0, 0, 0)
