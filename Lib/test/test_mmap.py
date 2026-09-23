6ab8d1fc7454b259ab0f586473af25a2232243fb
Victor Stinner <vstinner@python.org>
2026-09-23 22:56:38 +0200

    Skip tests if resize() raises SystemError

--- Lib/test/test_mmap.py
+++ Lib/test/test_mmap.py
@@ -980,8 +980,11 @@ class MmapTests(unittest.TestCase):
                 return 0
 
         with mmap.mmap(-1, size) as m:
-            with self.assertRaises(IndexError):
-                m[size - 1] = ResizeOnIndex(m)
+            try:
+                with self.assertRaises(IndexError):
+                    m[size - 1] = ResizeOnIndex(m)
+            except SystemError as exc:
+                self.skipTest(f"resize() is not available: {exc!r}")
             self.assertEqual(len(m), new_size)
 
     @unittest.skipUnless(hasattr(mmap.mmap, 'resize'), 'requires mmap.resize')
@@ -1003,8 +1006,11 @@ class MmapTests(unittest.TestCase):
 
         with mmap.mmap(-1, size) as m:
             value = ResizeOnBuffer(m, bytes(size))
-            with self.assertRaises(IndexError):
-                m[0:size] = value
+            try:
+                with self.assertRaises(IndexError):
+                    m[0:size] = value
+            except SystemError as exc:
+                self.skipTest(f"resize() is not available: {exc!r}")
             self.assertEqual(len(m), new_size)
 
     @unittest.skipUnless(os.name == 'nt', 'requires Windows')
