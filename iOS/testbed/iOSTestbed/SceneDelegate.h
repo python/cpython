8f9ebf1e6dff113d065ab1094027616a8181d498
Russell Keith-Magee <russell@keith-magee.com>
2026-09-24 14:54:43 +0800

    [3.13] gh-158051: Update testbed to support Xcode 27 (GH-158052)
    Modify testbed to target iOS 15 as a minimum, and use a UIScene lifecycle.
    These changes are required by Xcode 27, but are backwards compatible to
    at least Xcode 16.
    (cherry picked from commit daacd2d9a2b47ee277f398ed024afe6ced6f039f)
    
    Co-authored-by: Russell Keith-Magee <russell@keith-magee.com>

---
+++ iOS/testbed/iOSTestbed/SceneDelegate.h
@@ -0,0 +1,12 @@
+//
+//  SceneDelegate.h
+//  iOSTestbed
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
