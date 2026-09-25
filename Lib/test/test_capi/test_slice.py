1916314c4a327ba3ec8bb904fb13bb71065f4359
Serhiy Storchaka <storchaka@gmail.com>
2026-09-25 12:37:58 +0300

    Apply batched suggestions from code review
    Co-authored-by: Victor Stinner <vstinner@python.org>

--- Lib/test/test_capi/test_slice.py
+++ Lib/test/test_capi/test_slice.py
@@ -63,6 +63,8 @@ class SliceTest(unittest.TestCase):
         self.assertEqual(getindices(slice(None, None, -1), 10), (9, -1, -1))
         self.assertEqual(getindices(slice(-3, -1), 10), (7, 9, 1))
         self.assertEqual(getindices(slice(-1, -3, -1), 10), (9, 7, -1))
+        self.assertEqual(getindices(slice(None, None, -2), 0), (-1, -1, -2))
+        self.assertEqual(getindices(slice(-3, -5, 1), 0), (-3, -5, 1))        
 
         # It fails without setting an exception for out of bounds indices,
         # a zero step and non-integer indices.
@@ -73,6 +75,12 @@ class SliceTest(unittest.TestCase):
         self.assertIsNone(getindices(slice('a'), 10))
         self.assertIsNone(getindices(slice(1, 'a'), 10))
         self.assertIsNone(getindices(slice(1, 7, 'a'), 10))
+
+        # negative length
+        self.assertIsNone(getindices(slice(None), -1))
+        self.assertIsNone(getindices(slice(1, 7, 2), -1))
+        self.assertEqual(getindices(slice(-10, -5, 1), -1), (-11, -6, 1))
+        self.assertEqual(getindices(slice(-5, -10, -2), -1), (-6, -11, -2))
 
         # CRASHES getindices(NULL, 10)
         # CRASHES getindices(object(), 10)
