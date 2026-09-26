e16b12bb982bc916664850da1d0a78109b2145b8
Stan Ulbrych <stan@python.org>
2026-09-26 17:18:25 +0100

    [3.14] gh-158169: Clear `zlib.Decompress.unconsumed_tail` at end of stream (GH-158177) (#158229)
    (cherry picked from commit 8122ff4818f418e8ae984cf6964215689b00808d)
    
    Co-authored-by: GruffElixir <samarthat9@gmail.com>

--- Lib/test/test_zlib.py
+++ Lib/test/test_zlib.py
@@ -651,6 +651,18 @@ class CompressObjectTestCase(BaseCompressTestCase, unittest.TestCase):
                 self.assertEqual(dco.unconsumed_tail, b'')
                 self.assertEqual(dco.unused_data, remainder)
 
+    def test_decompress_unconsumed_tail_after_eof(self):
+        source = b'abcdefghijklmnopqrstuvwxyz'
+        remainder = b'0123456789'
+        dco = zlib.decompressobj()
+        data = dco.decompress(zlib.compress(source) + remainder, 1)
+        data += dco.decompress(dco.unconsumed_tail)
+
+        self.assertTrue(dco.eof)
+        self.assertEqual(data, source)
+        self.assertEqual(dco.unconsumed_tail, b'')
+        self.assertEqual(dco.unused_data, remainder)
+
     # issue27164
     def test_decompress_raw_with_dictionary(self):
         zdict = b'abcdefghijklmnopqrstuvwxyz'
